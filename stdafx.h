50e8a1a3f5203ab26e6cc4fa2636333629f97ba2
Eliot Horowitz <eliot@10gen.com>
2008-07-20 17:37:33 -0400

    added AGPL License Header to every file take 2

--- stdafx.h
+++ stdafx.h
@@ -3,6 +3,22 @@
 // are changed infrequently
 //
 
+/**
+*    Copyright (C) 2008 10gen Inc.
+*  
+*    This program is free software: you can redistribute it and/or  modify
+*    it under the terms of the GNU Affero General Public License, version 3,
+*    as published by the Free Software Foundation.
+*  
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*  
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #if defined(_WIN32)
@@ -157,4 +173,3 @@ using namespace boost::filesystem;
 
 #include "util/goodies.h"
 #include "util/log.h"
-
