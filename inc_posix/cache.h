bf35e4cca8b79b20978e9151917951305ae40caf
Keith Bostic <keith@wiredtiger.com>
2008-11-23 06:28:59 +1100

    Minor cleanup, first real working version.
    --HG--
    branch : keith

--- inc_posix/cache.h
+++ inc_posix/cache.h
@@ -11,11 +11,20 @@ extern "C" {
 #endif
 
 /*******************************************
- * WiredTiger general include files.
+ * WiredTiger public include file, and configuration control.
  *******************************************/
-#include "wiredtiger_config.h"
 #include "wiredtiger.h"
-#include "decl.h"
+#include "wiredtiger_config.h"
+
+/*******************************************
+ * WiredTiger system include files.
+ *******************************************/
+#include <stdlib.h>
+
+/*******************************************
+ * Everything without the heft to have its own file.
+ *******************************************/
+#include <laundry.h>
 
 /*******************************************
  * Internal forward declarations.
@@ -25,22 +34,29 @@ struct __wt_fh_t;	typedef struct __wt_fh_t WT_FH;
 /*******************************************
  * Internal versions of the database handle structure.
  *******************************************/
-struct __db_internal {
-	int unused;				/* placeholder */
+struct __idb {
+	DB *db;					/* Public object */
+
+#define	DB_ENV_IS_PRIVATE 0x01
+	u_int32_t flags;
 };
 
 /*******************************************
  * Internal versions of the cursor handle structure.
  *******************************************/
-struct __dbc_internal {
-	int unused;				/* placeholder */
+struct __idbc {
+	DBC *dbc;				/* Public object */
+
+	u_int32_t flags;
 };
 
 /*******************************************
  * Internal versions of the cursor database environment handle structure.
  *******************************************/
-struct __env_internal {
-	int unused;				/* placeholder */
+struct __ienv {
+	ENV *env;				/* Public object */
+
+	u_int32_t flags;
 };
 
 /*******************************************
@@ -49,6 +65,8 @@ struct __env_internal {
 #include "queue.h"
 #include "fh.h"
 #include "layout.h"
+
+#include "extern.h"
 
 #if defined(__cplusplus)
 }
