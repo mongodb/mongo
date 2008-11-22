01fd8dcbf4f7a41def169441bccfa819fcd4e6b7
Keith Bostic <keith@wiredtiger.com>
2008-11-23 06:28:04 +1100

    Add flag macros.
    --HG--
    branch : keith

--- inc_posix/misc.h
+++ inc_posix/misc.h
@@ -19,6 +19,19 @@ extern "C" {
 #define	WT_BLOCKS_TO_BYTES(blocks)	(blocks) * WT_BLOCK_SIZE
 
 /*
+ * Flag set, clear and test.  They come in 2 flavors: F_XXX (manipulates
+ * a field named "flags" in the structure referenced by its argument),
+ * and LF_XXX (manipulates a local variable named "flags").
+ */
+#define	F_CLR(p, mask)		(p)->flags &= ~(mask)
+#define	F_ISSET(p, mask)	((p)->flags & (mask))
+#define	F_SET(p, mask)		(p)->flags |= (mask)
+
+#define	LF_CLR(mask)		((flags) &= ~(mask))
+#define	LF_ISSET(mask)		((flags) & (mask))
+#define	LF_SET(mask)		((flags) |= (mask))
+
+/*
  * Statistics are optional to minimize our footprint.
  */
 #ifdef	HAVE_STATISTICS
@@ -32,6 +45,9 @@ typedef	u_int64_t	wt_stat_t;
 #define	WT_STAT_DECL(v)
 #define	WT_STAT(v)
 #endif
+
+/* A distinguished byte pattern to overwrite memory we are done using. */
+#define	OVERWRITE_BYTE	0xab
 
 #if defined(__cplusplus)
 }
