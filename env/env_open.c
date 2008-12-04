45fecca209059bebf486cd1c67415ef9946e255a
Keith Bostic <keith@wiredtiger.com>
2008-12-05 05:01:21 +1100

    Stub for Env.open.
    --HG--
    branch : keith

---
+++ env/env_open.c
@@ -0,0 +1,20 @@
+/*-
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2008 WiredTiger Software.
+ *	All rights reserved.
+ *
+ * $Id$
+ */
+
+#include "wt_internal.h"
+
+/*
+ * __wt_env_open --
+ *	Open a DB handle.
+ */
+int
+__wt_env_open(ENV *env, const char *home, mode_t mode, u_int32_t flags)
+{
+	return (0);
+}
