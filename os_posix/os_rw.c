12289dbbc1632ab3bee5fd4dfa0c0dee19d07da0
Keith Bostic <keith@wiredtiger.com>
2008-12-05 04:56:41 +1100

    Add support for an Env.set_verbose method so we can track system calls. Don't bother to return the number of bytes written, no caller will care.
    --HG--
    branch : keith

--- os_posix/os_rw.c
+++ os_posix/os_rw.c
@@ -14,23 +14,29 @@
  *	Read a file handle.
  */
 int
-__wt_read(IENV *ienv, WT_FH *fh, u_int32_t block_number,
-    u_int32_t blocks, void *buf, ssize_t *bytes_read_ret)
+__wt_read(IENV *ienv,
+    WT_FH *fh, u_int32_t block_number, u_int32_t blocks, void *buf)
 {
+	ENV *env;
 	ssize_t bytes_to_read, bytes_read;
 	int ret;
 
+	env = ienv->env;
+
 	WT_STAT(fh->read_count);
+	if (FLD_ISSET(env->verbose, WT_VERB_FILEOPS_ALL))
+		__wt_env_errx(env,
+		    "fileops: %s: read %lu 512B blocks at block %lu",
+		    fh->name, (u_long)blocks, (u_long)block_number);
 	
 	bytes_to_read = (ssize_t)WT_BLOCKS_TO_BYTES(blocks);
 	bytes_read = pread(fh->fd, buf,
 	    (ssize_t)WT_BLOCKS_TO_BYTES(blocks),
-	    (ssize_t)WT_BLOCKS_TO_BYTES(block_number));
-	if (bytes_to_read == bytes_read) {
-		*bytes_read_ret = bytes_read;
+	    (off_t)WT_BLOCKS_TO_BYTES(block_number));
+	if (bytes_to_read == bytes_read)
 		return (0);
-	}
-	*bytes_read_ret = 0;
+
+	__wt_env_err(env, errno, "Read error; file %s", fh->name);
 	return (WT_ERROR);
 }
 
@@ -39,21 +45,28 @@ __wt_read(IENV *ienv, WT_FH *fh, u_int32_t block_number,
  *	Write a file handle.
  */
 int
-__wt_write(IENV *ienv, WT_FH *fh, u_int32_t block_number,
-    u_int32_t blocks, void *buf, ssize_t *bytes_written_ret)
+__wt_write(IENV *ienv,
+    WT_FH *fh, u_int32_t block_number, u_int32_t blocks, void *buf)
 {
+	ENV *env;
 	ssize_t bytes_to_write, bytes_written;
 	int ret;
+
+	env = ienv->env;
 	
 	WT_STAT(fh->write_count);
+	if (FLD_ISSET(env->verbose, WT_VERB_FILEOPS_ALL))
+		__wt_env_errx(env,
+		    "fileops: %s: write %lu 512B blocks at block %lu",
+		    fh->name, (u_long)blocks, (u_long)block_number);
+
 	bytes_to_write = (ssize_t)WT_BLOCKS_TO_BYTES(blocks);
 	bytes_written = pwrite(fh->fd, buf,
 	    (ssize_t)WT_BLOCKS_TO_BYTES(blocks),
-	    (ssize_t)WT_BLOCKS_TO_BYTES(block_number));
-	if (bytes_to_write == bytes_written) {
-		*bytes_written_ret = bytes_written;
+	    (off_t)WT_BLOCKS_TO_BYTES(block_number));
+	if (bytes_to_write == bytes_written)
 		return (0);
-	}
-	*bytes_written_ret = 0;
+
+	__wt_env_err(env, errno, "Write error; file %s", fh->name);
 	return (WT_ERROR);
 }
