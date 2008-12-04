637e54861d555e15040eade434359a22f045b54e
Keith Bostic <keith@wiredtiger.com>
2008-12-05 04:54:14 +1100

    Change to handle more complex lists of arguments.
    --HG--
    branch : keith

--- dist/api.py
+++ dist/api.py
@@ -7,46 +7,86 @@
 #
 # Read the getset file and output C for the get/set pairs.
 
-import re, sys
+import re, string, sys
+
+def output():
+	# Output the getter
+	s = 'void\n__wt_' + handle + '_get_' +\
+	    list[0].split('\t')[0] +\
+	    '(\n\t' +\
+	    handle.upper() +\
+	    ' *handle'
+	argcnt = 1
+	for l in list:
+		s += ',\n\t' +\
+		    l.split('\t')[1].replace('@S', '*store' + str(argcnt) + 'p')
+		argcnt += 1
+	s = s + ')\n{\n'
+	argcnt = 1;
+	for l in list:
+		s += '\t*store' + str(argcnt) + 'p = ' +\
+		    'handle->' + l.split('\t')[0] + ';\n'
+		argcnt += 1
+	s = s + '}\n'
+	print s
+
+	# Output the setter
+	s = 'int\n__wt_' + handle + '_set_' +\
+	    list[0].split('\t')[0] +\
+	    '(\n\t' +\
+	    handle.upper() +\
+	    ' *handle'
+	argcnt = 1
+	for l in list:
+		s += ',\n\t' +\
+		    l.split('\t')[1].replace('@S', 'store' + str(argcnt))
+		argcnt += 1
+	s += ')\n{\n'
+	argcnt = 1;
+
+	# Verify means we call a standard verification routine because
+	# there are constraints or side-effects on setting the value.
+	if condition.count('verify'):
+		s += '\tint ret;\n\n' +\
+		    '\tif ((ret = __wt_' +\
+		    handle +\
+		    '_set_' +\
+		    list[0].split('\t')[0] +\
+		    '_verify(handle, '
+
+		argcnt = 1
+		for l in list:
+			if argcnt > 1:
+				s += ', '
+			s += 'store' + str(argcnt)
+			argcnt += 1
+		s += ')) != 0)\n\t\treturn (ret);\n\n'
+
+
+	argcnt = 1
+	for l in list:
+		s += '\thandle->' + l.split('\t')[0] +\
+		     ' = store' + str(argcnt) + ';\n'
+		argcnt += 1
+	s += '\treturn (0);\n}\n'
+	print s
 
 print '/* DO NOT EDIT: automatically built by getset.py. */'
 print '#include "wt_internal.h"'
 
-getset_re = \
-    re.compile(r'^([a-z][^\t\n]+)\t*([^\t\n]+)\t*([^\t\n]+)\t*([^\t\n]+)')
-
-# Read the api_defines file, building a list of flags for each method.
+setter_re = re.compile(r'^[a-z]')
+field_re = re.compile(r'^\t[a-z]')
+list = []
 for line in open('getset', 'r'):
-	if getset_re.match(line):
-		handle = getset_re.match(line).group(1)
-		field = getset_re.match(line).group(2)
-		conditions = getset_re.match(line).group(3)
-		type = getset_re.match(line).group(4)
-		type = type.replace('@H', handle.upper())
+	if setter_re.match(line):
+		if list:
+			output()
+			list=[]
+		s = line.split('\t')
+		handle = s[0]
+		condition = s[1]
+	elif field_re.match(line):
+		list.append(string.strip(line))
 
-		# Output the getter
-		print '\nvoid'
-		print '__wt_%s_get_%s(%s *handle, %s)' % (\
-		    handle, field, handle.upper(),\
-		    type.replace('@S', '*storep'))
-		print '{'
-		print '\t*storep = handle->%s;' % field
-		print '}'
-
-		# Output the setter
-		print '\nint'
-		print '__wt_%s_set_%s(%s *handle, %s)' % (\
-		    handle, field, handle.upper(),\
-		    type.replace('@S', 'store'))
-		print '{'
-
-		# Verify means we call a standard verification routine because
-		# there are constraints or side-effects on setting the value.
-		if conditions.count('verify'):
-			print '\tint ret;\n'
-			print '\tif ((ret = __wt_%s_set_%s_verify(handle, store)) != 0)' % (handle, field)
-			print '\t\treturn (ret);\n'
-
-		print '\thandle->%s = store;' % field
-		print '\treturn (0);'
-		print '}'
+if list:
+	output()
