433d539dbf2e179e033248500aeec68630b245f4
Keith Bostic <keith@wiredtiger.com>
2008-12-05 04:57:53 +1100

    Write some file-open code to suppor the initial btree code.
    --HG--
    branch : keith

---
+++ os_posix/os_open.c
@@ -0,0 +1,85 @@
+/*-
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2008 WiredTiger Software.
+ *	All rights reserved.
+ *
+ * $Id$
+ */
+
+#include "wt_internal.h"
+
+/*
+ * __wt_open --
+ *	Open a file handle.
+ */
+int
+__wt_open(IENV *ienv,
+    const char *name, mode_t mode, u_int32_t flags, WT_FH **fhp)
+{
+	ENV *env;
+	WT_FH *fh;
+	int f, fd, ret;
+
+	env = ienv->env;
+	fh = NULL;
+
+	if (FLD_ISSET(env->verbose, WT_VERB_FILEOPS | WT_VERB_FILEOPS_ALL))
+		__wt_env_errx(env, "fileops: %s: open", name);
+
+	f = O_RDWR;
+#ifdef O_BINARY
+	/* Windows clones: we always want to treat the file as a binary. */
+	f |= O_BINARY;
+#endif
+	if (LF_ISSET(WT_OPEN_CREATE))
+		f |= O_CREAT;
+
+	if ((fd = open(name, f, mode)) == -1) {
+		__wt_env_err(env, errno, "%s", name);
+		return (WT_ERROR);
+	}
+
+	if ((ret = __wt_calloc(ienv, 1, sizeof(WT_FH), &fh)) != 0)
+                return (ret);
+	if ((ret = __wt_strdup(ienv, name, &fh->name)) != 0)
+		goto err;
+
+#if defined(HAVE_FCNTL) && defined(FD_CLOEXEC)
+	/*
+	 * Security:
+	 * The application may spawn a new process, and we don't want another
+	 * process to have access to our file handles.  There's an obvious
+	 * race here...
+	 */
+	if ((f = fcntl(fd, F_GETFD)) == -1 ||
+	    fcntl(fd, F_SETFD, f | FD_CLOEXEC) == -1) {
+		__wt_env_err(env, errno, "%s: fcntl", name);
+		goto err;
+	}
+#endif
+
+	fh->fd = fd;
+	*fhp = fh;
+	return (0);
+
+err:	if (fh != NULL) {
+		if (fh->name != NULL)
+			__wt_free(ienv, fh->name);
+		__wt_free(ienv, fh);
+	}
+	(void)close(fd);
+	return (WT_ERROR);
+}
+
+/*
+ * __wt_close --
+ *	Close a file handle.
+ */
+int
+__wt_close(IENV *ienv, WT_FH *fh)
+{
+	__wt_free(ienv, fh->name);
+	__wt_free(ienv, fh);
+	return (0);
+}
