4d7546c6601e7f716abebaa43048c64e0231b5e0
Keith Bostic <keith@wiredtiger.com>
2008-12-06 02:59:32 +1100

    Add support for overflow records.
    --HG--
    branch : keith

---
+++ btree/bt_ovfl.c
@@ -0,0 +1,46 @@
+/*-
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2008 WiredTiger Software.
+ *	All rights reserved.
+ *
+ * $Id$
+ */
+
+#include "wt_internal.h"
+
+/*
+ * __wt_bt_ovfl_load --
+ *	Store an overflow item in the database, returning the starting
+ *	page.
+ */
+int
+__wt_bt_ovfl_load(DB *db, DBT *dbt, u_int32_t *addrp)
+{
+	WT_BTREE *bt;
+	WT_PAGE_HDR *hdr;
+	u_int32_t addr, frags;
+	u_int64_t bytes;
+	int ret;
+
+	bt = db->idb->btree;
+
+	/* Allocate a chunk of file space. */
+	WT_OVERFLOW_BYTES_TO_FRAGS(db, dbt->size, frags);
+	if ((ret = __wt_bt_falloc(bt, frags, &hdr, &addr)) != 0)
+		return (ret);
+
+	/* Initialize the returned space. */
+	hdr->type = WT_PAGE_BTREE_OVERFLOW;
+	hdr->entries = 1;
+
+	/* Copy the DBT into place. */
+	memcpy((u_int8_t *)hdr + WT_HDR_SIZE, dbt->data, dbt->size);
+
+	/* Write the overflow item back to the file. */
+	if ((ret = __wt_bt_fwrite(bt, addr, frags, hdr)) != 0)
+		return (ret);
+
+	*addrp = addr;
+	return (0);
+}
