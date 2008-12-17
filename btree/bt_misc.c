8d707b8d8b14573f2219603c59234d598e07baca
Keith Bostic <keith@wiredtiger.com>
2008-12-18 05:21:19 +1100

    Move hdr and item print routines into misc.c, verify needs them.
    --HG--
    branch : keith

--- btree/bt_misc.c
+++ btree/bt_misc.c
@@ -39,3 +39,61 @@ __wt_db_build_verify(IENV *ienv)
 
 	return (0);
 }
+
+/*
+ * __wt_db_hdr_type --
+ *	Return a string representing the page type.
+ */
+const char *
+__wt_db_hdr_type(u_int32_t type)
+{
+	switch (type) {
+	case WT_PAGE_INVALID:
+		return ("invalid");
+	case WT_PAGE_OVFL:
+		return ("overflow");
+	case WT_PAGE_ROOT:
+		return ("primary root");
+	case WT_PAGE_INT:
+		return ("primary internal");
+	case WT_PAGE_LEAF:
+		return ("primary leaf");
+	case WT_PAGE_DUP_ROOT:
+		return ("off-page duplicate root");
+	case WT_PAGE_DUP_INT:
+		return ("off-page duplicate internal");
+	case WT_PAGE_DUP_LEAF:
+		return ("off-page duplicate leaf");
+	default:
+		break;
+	}
+	return ("unknown");
+}
+
+/*
+ * __wt_db_item_type --
+ *	Return a string representing the item type.
+ */
+const char *
+__wt_db_item_type(u_int32_t type)
+{
+	switch (type) {
+	case WT_ITEM_KEY:
+		return ("key");
+	case WT_ITEM_DATA:
+		return ("data");
+	case WT_ITEM_DUP:
+		return ("duplicate");
+	case WT_ITEM_KEY_OVFL:
+		return ("key-overflow");
+	case WT_ITEM_DATA_OVFL:
+		return ("data-overflow");
+	case WT_ITEM_DUP_OVFL:
+		return ("duplicate-overflow");
+	case WT_ITEM_DUP_OFFPAGE:
+		return ("duplicate-offpage");
+	default:
+		break;
+	}
+	return ("unknown");
+}
