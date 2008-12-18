3a4a6783b6824ce3337a720f5a9536f7101f42fc
Keith Bostic <keith@wiredtiger.com>
2008-12-19 06:13:41 +1100

    Add skeleton Db.close method.
    --HG--
    branch : keith

---
+++ db/db_close.c
@@ -0,0 +1,22 @@
+/*-
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2008 WiredTiger Software.
+ *	All rights reserved.
+ *
+ * $Id$
+ */
+
+#include "wt_internal.h"
+
+/*
+ * __wt_db_close --
+ *	Close a DB handle.
+ */
+int
+__wt_db_close(DB *db, u_int32_t flags)
+{
+	DB_FLAG_CHK(db, "Db.close", flags, WT_APIMASK_DB_CLOSE);
+
+	return (0);
+}
