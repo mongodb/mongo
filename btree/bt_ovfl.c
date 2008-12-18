82f80fc6ba8fcbb00a44827bc6b8f0b6aae067c9
Keith Bostic <keith@wiredtiger.com>
2008-12-19 06:13:13 +1100

    Add routine to copy and overflow item -- used when an overflow item is promoted onto an internal page.
    --HG--
    branch : keith

--- btree/bt_ovfl.c
+++ btree/bt_ovfl.c
@@ -10,9 +10,49 @@
 #include "wt_internal.h"
 
 /*
+ * __wt_db_ovfl_copy --
+ *	Copy an overflow item in the database, returning the starting
+ *	addr.  This routine is used when an overflow item is promoted
+ *	to an internal page.
+ */
+int
+__wt_db_ovfl_copy(DB *db, WT_ITEM_OVFL *from, WT_ITEM_OVFL *copy)
+{
+	DBT dbt;
+	WT_BTREE *bt;
+	WT_PAGE_HDR *ovfl_hdr;
+	u_int32_t frags;
+	int ret, tret;
+
+	bt = db->idb->btree;
+
+	/* Read in the overflow record. */
+	WT_OVERFLOW_BYTES_TO_FRAGS(db, from->len, frags);
+	if ((ret = __wt_db_fread(bt, from->addr, frags, &ovfl_hdr)) != 0)
+		return (ret);
+
+	/*
+	 * Copy the overflow record to a new location, and set our return
+	 * information.
+	 */
+	WT_CLEAR(dbt);
+	dbt.data = WT_PAGE_BYTE(ovfl_hdr);
+	dbt.size = from->len;
+	ret = __wt_db_ovfl_write(db, &dbt, &copy->addr);
+	copy->len = from->len;
+
+	/* Discard the overflow record. */
+	if ((tret =
+	    __wt_db_fdiscard(bt, from->addr, ovfl_hdr)) != 0 && ret == 0)
+		ret = tret;
+
+	return (ret);
+}
+
+/*
  * __wt_db_ovfl_write --
  *	Store an overflow item in the database, returning the starting
- *	page.
+ *	addr.
  */
 int
 __wt_db_ovfl_write(DB *db, DBT *dbt, u_int32_t *addrp)
