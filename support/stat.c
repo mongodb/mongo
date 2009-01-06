798d19efb9f191223690ca5568475ea5422b695d
Keith Bostic <keith@wiredtiger.com>
2009-01-07 09:55:20 +1100

    Move the cache from the btree code to the environment; add support for environment stats calls.
    --HG--
    branch : keith

--- support/stat.c
+++ support/stat.c
@@ -43,6 +43,31 @@ __wt_stat_alloc_db(IENV *ienv, WT_STATS **statsp)
 	stats[WT_STAT_BULK_OVERFLOW_KEY].desc = "bulk overflow key items read";
 	stats[WT_STAT_BULK_OVERFLOW_DATA].desc =
 	    "bulk overflow data items read";
+
+	*statsp = stats;
+	return (0);
+}
+
+int
+__wt_stat_clear_db(WT_STATS *stats)
+{
+	stats[WT_STAT_BULK_PAIRS_READ].v = 0;
+	stats[WT_STAT_BULK_DUP_DATA_READ].v = 0;
+	stats[WT_STAT_BULK_OVERFLOW_KEY].v = 0;
+	stats[WT_STAT_BULK_OVERFLOW_DATA].v = 0;
+	return (0);
+}
+
+int
+__wt_stat_alloc_env(IENV *ienv, WT_STATS **statsp)
+{
+	WT_STATS *stats;
+	int ret;
+
+	if ((ret = __wt_calloc(ienv,
+	    WT_STAT_ENV_TOTAL_ENTRIES + 1, sizeof(WT_STATS), &stats)) != 0)
+		return (ret);
+
 	stats[WT_STAT_CACHE_ALLOC].desc = "pages allocated in the cache";
 	stats[WT_STAT_CACHE_HIT].desc = "reads found in the cache";
 	stats[WT_STAT_CACHE_MISS].desc = "reads not found in the cache";
@@ -57,12 +82,8 @@ __wt_stat_alloc_db(IENV *ienv, WT_STATS **statsp)
 }
 
 int
-__wt_stat_clear_db(WT_STATS *stats)
+__wt_stat_clear_env(WT_STATS *stats)
 {
-	stats[WT_STAT_BULK_PAIRS_READ].v = 0;
-	stats[WT_STAT_BULK_DUP_DATA_READ].v = 0;
-	stats[WT_STAT_BULK_OVERFLOW_KEY].v = 0;
-	stats[WT_STAT_BULK_OVERFLOW_DATA].v = 0;
 	stats[WT_STAT_CACHE_ALLOC].v = 0;
 	stats[WT_STAT_CACHE_HIT].v = 0;
 	stats[WT_STAT_CACHE_MISS].v = 0;
