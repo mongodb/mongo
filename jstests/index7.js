db445449db1a4526891684e42bd5543a3dac3cb0
Aaron <aaron@10gen.com>
2009-01-27 14:01:13 -0500

    Add reverse direction test

--- jstests/index7.js
+++ jstests/index7.js
@@ -56,9 +56,11 @@ index( f.find( { a: /^a/, b: 1 } ) );
 index( f.find( { b: 1, a: /^a/ } ) );
 start( { a: "a", b: 1 }, f.find( { a: /^a/, b: 1 } ) );
 start( { a: "a", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: 1, b: 1 } ) );
+start( { a: "b", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: -1, b: -1 } ) );
 start( { a: "a", b: 1 }, f.find( { b: 1, a: /^a/ } ) );
 end( { a: "b", b: 1 }, f.find( { a: /^a/, b: 1 } ) );
 end( { a: "b", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: 1, b: 1 } ) );
+end( { a: "a", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: -1, b: -1 } ) );
 end( { a: "b", b: 1 }, f.find( { b: 1, a: /^a/ } ) );
 
 start( { a: "z", b: 1 }, f.find( { a: /^z/, b: 1 } ) );
