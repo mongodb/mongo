6f60d40e6d4ed9a50f3f54552cc2f50eeb71931b
Eliot Horowitz <eliot@10gen.com>
2009-01-26 23:35:26 -0500

    better debugging

--- jstests/remove.js
+++ jstests/remove.js
@@ -8,9 +8,13 @@ function f(n,dir) {
     t.ensureIndex({x:dir||1});
     for( i = 0; i < n; i++ ) t.save( { x:3, z:"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" } );
 
+    assert.eq( n , t.find().count() );
     t.remove({x:3});
-    assert( t.findOne() == null );
-    assert( t.validate().valid );
+
+    assert.eq( 0 , t.find().count() );
+    
+    assert( t.findOne() == null , "A:" + tojson( t.findOne() ) );
+    assert( t.validate().valid , "B" );
 }
 
 t.drop();
@@ -18,4 +22,5 @@ f(300, 1);
 
 f(500, -1);
 
-assert(t.validate().valid);
+assert(t.validate().valid , "C" );
+
