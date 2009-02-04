d9fe088ac9725c4d15bb74d531e3268c4fdcd7d1
Aaron <aaron@10gen.com>
2009-02-04 10:58:55 -0500

    Clean up autoid test

--- jstests/autoid.js
+++ jstests/autoid.js
@@ -1,18 +1,11 @@
-p = function( o ) {
-    print( tojson( o ) );
-}
-
 f = db.jstests_autoid;
 f.drop();
 
 f.save( {z:1} );
 a = f.findOne( {z:1} );
-p( a );
 f.update( {z:1}, {z:2} );
 b = f.findOne( {z:2} );
-p( b );
-//assert.eq( a._id, b._id );
+assert.eq( a._id.str, b._id.str );
 c = f.update( {z:2}, {z:"abcdefgabcdefgabcdefg"} );
 c = f.findOne( {} );
-p( c );
-//assert.eq( a._id, c._id );
+assert.eq( a._id.str, c._id.str );
