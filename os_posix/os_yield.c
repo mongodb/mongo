c1568e9ebe9f6f506fd8b4905cbd8852491d73df
Keith Bostic <keith@wiredtiger.com>
2009-02-06 09:52:16 +1100

    Add a yield function.
    --HG--
    branch : keith

---
+++ os_posix/os_yield.c
@@ -0,0 +1,24 @@
+/*-
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2009 WiredTiger Software.
+ *	All rights reserved.
+ *
+ * $Id$
+ */
+
+#include "wt_internal.h"
+
+/*
+ * __wt_yield --
+ *	Yield the thread of control.
+ */
+void
+__wt_yield()
+{
+#ifdef HAVE_PTHREAD_YIELD
+	pthread_yield();
+#else
+	sched_yield();
+#endif
+}
