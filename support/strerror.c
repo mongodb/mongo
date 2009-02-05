99eaae0b13038b9f4ebb39c329ab1d5701867abf
Keith Bostic <keith@wiredtiger.com>
2009-02-06 09:51:15 +1100

    Make non-specific error more verbose.
    --HG--
    branch : keith

--- support/strerror.c
+++ support/strerror.c
@@ -17,9 +17,9 @@ wt_strerror(int error)
 
 	switch (error) {
 	case WT_ERROR:
-		return ("WT_ERROR: non-specific error");
+		return ("WT_ERROR: non-specific WiredTiger error");
 	case WT_NOTFOUND:
-		return ("WT_NOTFOUND: item not found");
+		return ("WT_NOTFOUND: database item not found");
 	case WT_TOOSMALL:
 		return ("WT_TOOSMALL: user-supplied DBT buffer is too small for returned item");
 	default:
