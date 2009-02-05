02f32761cab193c2bc7e50445107c560d40b76f6
Keith Bostic <keith@wiredtiger.com>
2009-02-06 09:42:56 +1100

    Make the WT_TOC "thread of control" a high-level object, passed through all of the API, WiredTiger can now run multiple threads in an event-style system. Add support for configuring WT to run with a single-thread of control. Rename flag-check macros to being with WT_. Destroy mutexes on cleanup.
    --HG--
    branch : keith

--- env/env_toc.c
+++ env/env_toc.c
@@ -21,6 +21,8 @@ wt_toc_create(WT_TOC **tocp, u_int32_t flags)
 	WT_TOC *toc;
 	int ret;
 
+	WT_ENV_FCHK(NULL, "wt_toc_create", flags, WT_APIMASK_WT_TOC_CREATE);
+
 	if ((ret = __wt_calloc(NULL, 1, sizeof(WT_TOC), &toc)) != 0)
 		return (ret);
 	if ((ret = __wt_calloc(NULL, 1, sizeof(WT_MTX), &toc->mtx)) != 0)
@@ -32,7 +34,10 @@ wt_toc_create(WT_TOC **tocp, u_int32_t flags)
 	if ((ret = __wt_lock(toc->mtx)) != 0)
 		goto err;
 
-	toc->slot = WT_GLOBAL(workq_next)++;
+	if (WT_GLOBAL(single_threaded))
+		F_SET(toc, WT_SINGLE_THREADED);
+	else
+		toc->slot = WT_GLOBAL(workq_next)++;
 	toc->destroy = __wt_toc_destroy;
 
 	*tocp = toc;
@@ -49,11 +54,21 @@ err:	(void)__wt_toc_destroy(toc, 0);
 static int
 __wt_toc_destroy(WT_TOC *toc, u_int32_t flags)
 {
+	int ret, tret;
+
+	ret = 0;
+
+	WT_ENV_FCHK_NOTFATAL(
+	    NULL, "WtToc.destroy", flags, WT_APIMASK_WT_TOC_DESTROY, ret);
+
+	if ((tret = __wt_mtx_destroy(toc->mtx)) != 0 && ret == 0)
+		ret = tret;
+
 	WT_FREE_AND_CLEAR(NULL, toc->mtx);
 
 	__wt_free(NULL, toc);
 
-	return (0);
+	return (ret);
 }
 
 /*
@@ -65,17 +80,19 @@ __wt_toc_sched(WT_TOC *toc)
 {
 	WT_TOC **q;
 
+	/*
+	 * The engine may be single-threaded or threads of control may re-enter
+	 * the API.  Instead of coding internal versions of the routines, allow
+	 * calls through the standard API layer.
+	 */
+	if (F_ISSET(toc, WT_RUNNING | WT_SINGLE_THREADED)) {
+		__wt_api_switch(toc);
+		return (toc->ret);
+	}
+
 	/* Don't schedule any operations if there's no queue. */
 	if (WT_GLOBAL(workq) == NULL)
 		return (WT_ERROR);
-
-	/*
-	 * Threads of control sometimes re-enter the API.  Rather than code
-	 * up lots of internal versions of the routine, allow calls through
-	 * the standard API layer.
-	 */
-	if (F_ISSET(toc, WT_RUNNING))
-		return (__wt_api_switch(toc));
 
 	/* Otherwise, schedule the call and go to sleep until it completes. */
 	q = WT_GLOBAL(workq) + toc->slot;
