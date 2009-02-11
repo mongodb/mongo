020b6035b4fb1e306272f621762c20eb70e1e5f9
Dwight <dmerriman@gmail.com>
2009-02-11 11:38:39 -0500

    more sensible vesrion #

--- db/db.cpp
+++ db/db.cpp
@@ -118,7 +118,7 @@ namespace mongo {
        115 replay, opLogging
     */
     void listen(int port) {
-        const char *Version = "db version 122";
+        const char *Version = "db version v0.8.0";
         log() << Version << ", pdfile version " << VERSION << "." << VERSION_MINOR << endl;
         pdfileInit();
         //testTheDb();
