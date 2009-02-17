b4a6a35ccff4cc71ef9299a2d66d1ae858ac75b3
Geir Magnusson Jr <geir@geir-magnusson-jrs-mac-pro.local>
2009-02-17 11:11:15 -0500

    Little scripty to announce which test and timing info

---
+++ jstests/_runner_sharding.js
@@ -0,0 +1,24 @@
+//
+// simple runner to run toplevel tests in jstests
+//
+var files = listFiles("jstests/sharding");
+
+files.forEach(
+    function(x) {
+        
+        if ( /_runner/.test(x.name) ||
+             /_lodeRunner/.test(x.name) ||
+             ! /\.js$/.test(x.name ) ){ 
+            print(" >>>>>>>>>>>>>>> skipping " + x.name);
+            return;
+        }
+        
+        
+        print(" *******************************************");
+        print("         Test : " + x.name + " ...");
+        print("                " + Date.timeFunc( function() { load(x.name); }, 1) + "ms");
+        
+    }
+);
+
+
