e5e195ee39dd5e1231c0229bcdf6d74a6ecbb27c
Dwight <dmerriman@gmail.com>
2009-02-23 13:56:54 -0500

    new file

--- s/request.h
+++ s/request.h
@@ -69,22 +69,6 @@ namespace mongo {
         ShardInfo * _shardInfo;
     };
 
-    class Strategy {
-    public:
-        Strategy(){}
-        virtual ~Strategy() {}
-        virtual void queryOp( Request& r ) = 0;
-        virtual void getMore( Request& r ) = 0;
-        virtual void writeOp( int op , Request& r ) = 0;
-
-    protected:
-        void doWrite( int op , Request& r , string server );
-        void doQuery( Request& r , string server );
-
-        void insert( string server , const char * ns , const BSONObj& obj );
-        
-    };
-    
-    extern Strategy * SINGLE;
-    extern Strategy * SHARDED;
 }
+
+#include "strategy.h"
---
+++ s/strategy.h
@@ -0,0 +1,27 @@
+// strategy.h
+
+
+#pragma once
+
+namespace mongo {
+    
+    class Strategy {
+    public:
+        Strategy(){}
+        virtual ~Strategy() {}
+        virtual void queryOp( Request& r ) = 0;
+        virtual void getMore( Request& r ) = 0;
+        virtual void writeOp( int op , Request& r ) = 0;
+
+    protected:
+        void doWrite( int op , Request& r , string server );
+        void doQuery( Request& r , string server );
+
+        void insert( string server , const char * ns , const BSONObj& obj );
+        
+    };
+    
+    extern Strategy * SINGLE;
+    extern Strategy * SHARDED;
+}
+
