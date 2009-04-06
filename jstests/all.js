795ad3a7c654a45a2a6a7be2974eaea3729ba1e2
Aaron <aaron@10gen.com>
2009-04-06 14:11:51 -0400

    forgot to add test for $all earlier

---
+++ jstests/all.js
@@ -0,0 +1,34 @@
+t = db.jstests_all;
+t.drop();
+
+doTest = function() {
+    
+    t.save( { a:[ 1,2,3 ] } );
+    t.save( { a:[ 1,2,4 ] } );
+    t.save( { a:[ 1,8,5 ] } );
+    t.save( { a:[ 1,8,6 ] } );
+    t.save( { a:[ 1,9,7 ] } );
+    
+    assert.eq( 5, t.find( { a: { $all: [ 1 ] } } ).count() );
+    assert.eq( 2, t.find( { a: { $all: [ 1, 2 ] } } ).count() );
+    assert.eq( 2, t.find( { a: { $all: [ 1, 8 ] } } ).count() );
+    assert.eq( 1, t.find( { a: { $all: [ 1, 3 ] } } ).count() );
+    assert.eq( 2, t.find( { a: { $all: [ 2 ] } } ).count() );
+    assert.eq( 1, t.find( { a: { $all: [ 2, 3 ] } } ).count() );
+    assert.eq( 2, t.find( { a: { $all: [ 2, 1 ] } } ).count() );
+    
+    t.save( { a: [ 2, 2 ] } );
+    assert.eq( 3, t.find( { a: { $all: [ 2, 2 ] } } ).count() );
+    
+    t.save( { a: [ [ 2 ] ] } );
+    assert.eq( 3, t.find( { a: { $all: [ 2 ] } } ).count() );    
+    
+    t.save( { a: [ { b: [ 10, 11 ] }, 11 ] } );
+    assert.eq( 1, t.find( { 'a.b': { $all: [ 10 ] } } ).count() );
+    assert.eq( 1, t.find( { a: { $all: [ 11 ] } } ).count() );
+}
+
+doTest();
+t.drop();
+t.ensureIndex( {a:1} );
+doTest();
