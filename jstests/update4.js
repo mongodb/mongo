d685743a1acb70b50f20682d89de5a2a8e9cd16b
Aaron <aaron@10gen.com>
2009-04-10 10:30:57 -0400

    lastError 'n' zero on failed non upsert update

--- jstests/update4.js
+++ jstests/update4.js
@@ -10,13 +10,17 @@ getLastError = function() {
 f.save( {a:1} );
 f.update( {a:1}, {a:2} );
 assert.eq( true, getLastError().updatedExisting );
+assert.eq( 1, getLastError().n );
 f.update( {a:1}, {a:2} );
 assert.eq( false, getLastError().updatedExisting );
+assert.eq( 0, getLastError().n );
 
 f.update( {a:1}, {a:1}, true );
 assert.eq( false, getLastError().updatedExisting );
+assert.eq( 1, getLastError().n );
 f.update( {a:1}, {a:1}, true );
 assert.eq( true, getLastError().updatedExisting );
+assert.eq( 1, getLastError().n );
 assert.eq( true, db.getPrevError().updatedExisting );
 assert.eq( 1, db.getPrevError().nPrev );
 
