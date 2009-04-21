904088e1310ee96afdecd8ffd72dc2db6a89f55d
Mike Dirolf <mike@10gen.com>
2009-04-21 15:11:29 -0400

    add 'ok' field to ismaster and isdbgrid commands

--- s/commands.cpp
+++ s/commands.cpp
@@ -478,6 +478,7 @@ namespace mongo {
             bool run(const char *ns, BSONObj& cmdObj, string& errmsg, BSONObjBuilder& result, bool) {
                 result.append("isdbgrid", 1);
                 result.append("hostname", ourHostname);
+                result << "ok" << 1;
                 return true;
             }
         } isdbgrid;
@@ -492,6 +493,7 @@ namespace mongo {
             virtual bool run(const char *ns, BSONObj& cmdObj, string& errmsg, BSONObjBuilder& result, bool) {
                 result.append("ismaster", 0.0);
                 result.append("msg", "isdbgrid");
+                result << "ok" << 1;
                 return true;
             }
         } ismaster;
