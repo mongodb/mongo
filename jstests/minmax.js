3beb7b4170c8cde4c91a3425f96f6f12b4d7e4ad
Aaron <aaron@10gen.com>
2009-04-30 17:36:44 -0400

    add test MINOR

---
+++ jstests/minmax.js
@@ -0,0 +1,38 @@
+// test min / max query parameters
+
+addData = function() {
+    t.save( { a: 1, b: 1 } );
+    t.save( { a: 1, b: 2 } );
+    t.save( { a: 2, b: 1 } );
+    t.save( { a: 2, b: 2 } );    
+}
+
+t = db.jstests_minmax;
+t.drop();
+t.ensureIndex( { a: 1, b: 1 } );
+addData();
+
+assert.eq( 2, t.find().min( { a: 1, b: 2 } ).max( { a: 2, b: 1 } ).toArray().length );
+
+// just one bound
+assert.eq( 3, t.find().min( { a: 1, b: 2 } ).toArray().length );
+assert.eq( 3, t.find().max( { a: 2, b: 1 } ).toArray().length );
+assert.eq( 3, t.find().min( { a: 1, b: 2 } ).hint( { a: 1, b: 1 } ).toArray().length );
+assert.eq( 3, t.find().max( { a: 2, b: 1 } ).hint( { a: 1, b: 1 } ).toArray().length );
+
+t.drop();
+t.ensureIndex( { a: 1, b: -1 } );
+addData();
+assert.eq( 4, t.find().min( { a: 1, b: 2 } ).toArray().length );
+assert.eq( 4, t.find().max( { a: 2, b: 1 } ).toArray().length );
+assert.eq( 1, t.find().min( { a: 2, b: 1 } ).toArray().length );
+assert.eq( 1, t.find().max( { a: 1, b: 2 } ).toArray().length );
+assert.eq( 4, t.find().min( { a: 1, b: 2 } ).hint( { a: 1, b: -1 } ).toArray().length );
+assert.eq( 4, t.find().max( { a: 2, b: 1 } ).hint( { a: 1, b: -1 } ).toArray().length );
+assert.eq( 1, t.find().min( { a: 2, b: 1 } ).hint( { a: 1, b: -1 } ).toArray().length );
+assert.eq( 1, t.find().max( { a: 1, b: 2 } ).hint( { a: 1, b: -1 } ).toArray().length );
+
+// hint doesn't match
+assert.throws( function() { t.find().min( { a: 1 } ).hint( { a: 1, b: -1 } ).toArray() } );
+assert.throws( function() { t.find().min( { a: 1, b: 1 } ).max( { a: 1 } ).hint( { a: 1, b: -1 } ).toArray() } );
+assert.throws( function() { t.find().min( { b: 1 } ).max( { a: 1, b: 2 } ).hint( { a: 1, b: -1 } ).toArray() } );
