7dd4ce550178558c3d32768f512dac9fb3b52eb9
Aaron <aaron@10gen.com>
2009-05-12 12:12:19 -0400

    diskfull test enhancements MINOR

--- jstests/disk/diskfull.js
+++ jstests/disk/diskfull.js
@@ -1,10 +1,14 @@
 port = allocatePorts( 1 )[ 0 ];
+emptyDbpath( "/data/db/diskfulltest" );
+doIt = true;
 try {
     m = startMongoProgram( "mongod", "--port", port, "--dbpath", "/data/db/diskfulltest", "--nohttpinterface" );
 } catch ( e ) {
     print( "path /data/db/diskfulltest/ missing, skipping diskfull test" );
-    quit();
+    doIt = false;
 }
 
-m.getDB( "diskfulltest" ).getCollection( "diskfulltest" ).save( { a: 6 } );
-assert( rawMongoProgramOutput().match( /dbexit: really exiting now/ ) );
+if ( doIt ) {
+    m.getDB( "diskfulltest" ).getCollection( "diskfulltest" ).save( { a: 6 } );
+    assert( rawMongoProgramOutput().match( /dbexit: really exiting now/ ) );
+}
