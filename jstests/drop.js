f565f7be926110a779bb228feb22ecb6cb1f3125
Eliot Horowitz <eliot@10gen.com>
2009-05-13 23:29:58 -0400

    update test for sm

--- jstests/drop.js
+++ jstests/drop.js
@@ -2,20 +2,20 @@ f = db.jstests_drop;
 
 f.drop();
 
-assert.eq( 0, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() );
+assert.eq( 0, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() , "A" );
 f.save( {} );
-assert.eq( 1, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() );
+assert.eq( 1, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() , "B" );
 f.ensureIndex( {a:1} );
-assert.eq( 2, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() );
+assert.eq( 2, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() , "C" );
 assert.commandWorked( db.runCommand( {drop:"jstests_drop"} ) );
-assert.eq( 0, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() );
+assert.eq( 0, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() , "D" );
 
-f = db.jstests_drop;
+f.resetIndexCache();
 f.ensureIndex( {a:1} );
-assert.eq( 2, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() );
+assert.eq( 2, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() , "E" );
 assert.commandWorked( db.runCommand( {deleteIndexes:"jstests_drop",index:"*"} ) );
-assert.eq( 1, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() );
+assert.eq( 1, db.system.indexes.find( {ns:"test.jstests_drop"} ).count() , "G" );
 
 // make sure we can still use it
 f.save( {} );
-assert.eq( 1, f.find().hint( {_id:ObjectId( "000000000000000000000000" )} ).toArray().length );
+assert.eq( 1, f.find().hint( {_id:new ObjectId( "000000000000000000000000" )} ).toArray().length , "H" );
