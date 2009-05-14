01f3e49668fd845cd8e7887cf6d7b150c037510a
Aaron <aaron@10gen.com>
2009-05-14 10:58:28 -0400

    port quit and allocatePorts

--- shell/utils.cpp
+++ shell/utils.cpp
@@ -92,9 +92,55 @@ namespace mongo {
             return ret.obj();
         }
         
-        void installShellUtils( Scope& scope ){
+        BSONObj Quit(const BSONObj& args) {
+            // If not arguments are given first element will be EOO, which
+            // converts to the integer value 0.
+            int exit_code = int( args.firstElement().number() );
+            ::exit(exit_code);
+            return undefined_;
+        }
+        
+#ifndef _WIN32
+        BSONObj AllocatePorts( const BSONObj &args ) {
+            uassert( "allocatePorts takes exactly 1 argument", args.nFields() == 1 );
+            uassert( "allocatePorts needs to be passed an integer", args.firstElement().isNumber() );
+            
+            int n = int( args.firstElement().number() );
+            
+            vector< int > ports;
+            for( int i = 0; i < n; ++i ) {
+                int s = socket( AF_INET, SOCK_STREAM, 0 );
+                assert( s );
+                
+                sockaddr_in address;
+                memset(address.sin_zero, 0, sizeof(address.sin_zero));
+                address.sin_family = AF_INET;
+                address.sin_port = 0;
+                address.sin_addr.s_addr = 0;        
+                assert( 0 == ::bind( s, (sockaddr*)&address, sizeof( address ) ) );
+                
+                sockaddr_in newAddress;
+                socklen_t len = sizeof( newAddress );
+                assert( 0 == getsockname( s, (sockaddr*)&newAddress, &len ) );
+                ports.push_back( ntohs( newAddress.sin_port ) );
+                
+                assert( 0 == close( s ) );
+            }
+            
+            sort( ports.begin(), ports.end() );
+            BSONObjBuilder b;
+            b.append( "", ports );
+            return b.obj();
+        }
+#endif
+        
+            void installShellUtils( Scope& scope ){
             scope.injectNative( "listFiles" , listFiles );
             scope.injectNative( "sleep" , JSSleep );
+            scope.injectNative( "quit", Quit );
+#if !defined(_WIN32)
+            scope.injectNative( "allocatePorts", AllocatePorts );
+#endif
         }
         
     }
