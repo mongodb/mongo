e7bc0e56d02ecbc321fec9038920289b0f8de06d
Aaron <aaron@10gen.com>
2009-05-26 14:01:33 -0400

    BUG SERVER-68 - regex matches within objects within arrays

---
+++ jstests/regex.js
@@ -0,0 +1,16 @@
+t = db.jstests_regex;
+
+t.drop();
+t.save( { a: "bcd" } );
+assert.eq( 1, t.count( { a: /b/ } ) );
+assert.eq( 1, t.count( { a: /bc/ } ) );
+assert.eq( 1, t.count( { a: /bcd/ } ) );
+assert.eq( 0, t.count( { a: /bcde/ } ) );
+
+t.drop();
+t.save( { a: { b: "cde" } } );
+assert.eq( 1, t.count( { 'a.b': /de/ } ) );
+
+t.drop();
+t.save( { a: [ { b: "cde" } ] } );
+assert.eq( 1, t.count( { 'a.b': /de/ } ) );
