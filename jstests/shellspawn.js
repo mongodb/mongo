4d77758502e0a8b5fceb2c1b3c58718e06377a2f
eliot <eliot@10gen.com>
2009-06-03 14:30:11 -0400

    don't do spawn test on win32

--- jstests/shellspawn.js
+++ jstests/shellspawn.js
@@ -2,18 +2,23 @@ baseName = "jstests_shellspawn";
 t = db.getCollection( baseName );
 t.drop();
 
-spawn = startMongoProgramNoConnect( "mongo", "--port", myPort(), "--eval", "sleep( 2000 ); db.getCollection( \"" + baseName + "\" ).save( {a:1} );" );
-
-assert.soon( function() { return 1 == t.count(); } );
-
-stopMongoProgramByPid( spawn );
-
-spawn = startMongoProgramNoConnect( "mongo", "--port", myPort(), "--eval", "print( \"I am a shell\" );" );
-
-spawn = startMongoProgramNoConnect( "mongo", "--port", myPort() );
-
-spawn = startMongoProgramNoConnect( "mongo", "--port", myPort() );
-
-stopMongoProgramByPid( spawn );
-
-// all these shells should be killed
+if ( typeof( _startMongoProgram ) == "undefined" ){
+    print( "no fork support" );
+}
+else {
+    spawn = startMongoProgramNoConnect( "mongo", "--port", myPort(), "--eval", "sleep( 2000 ); db.getCollection( \"" + baseName + "\" ).save( {a:1} );" );
+    
+    assert.soon( function() { return 1 == t.count(); } );
+    
+    stopMongoProgramByPid( spawn );
+    
+    spawn = startMongoProgramNoConnect( "mongo", "--port", myPort(), "--eval", "print( \"I am a shell\" );" );
+    
+    spawn = startMongoProgramNoConnect( "mongo", "--port", myPort() );
+    
+    spawn = startMongoProgramNoConnect( "mongo", "--port", myPort() );
+    
+    stopMongoProgramByPid( spawn );
+    
+    // all these shells should be killed
+}
