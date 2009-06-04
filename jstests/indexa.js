56b5ce222920e85f2d44b7635926c8009f5d68a0
Eliot Horowitz <eliot@10gen.com>
2009-06-04 13:33:07 -0400

    broken test MINOR

---
+++ jstests/indexa.js
@@ -0,0 +1,19 @@
+
+t = db.indexa;
+t.drop();
+
+t.ensureIndex( { x:1 }, true );
+
+t.insert( { 'x':'A' } );
+t.insert( { 'x':'B' } );
+t.insert( { 'x':'A' } );
+
+assert.eq( 2 , t.count() , "A" );
+
+t.update( {x:'B'}, { x:'A' } );
+
+a = t.find().toArray();
+u = a.map( function(z){ return z.x } ).unique();
+
+//assert( a.length == u.length , "unique index update is broken" );
+
