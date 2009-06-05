071e9c39a7097e2af157c267c6339da7f9cc6ee9
Eliot Horowitz <eliot@10gen.com>
2009-06-05 10:05:33 -0400

    .group works on collection as well as db

--- jstests/eval2.js
+++ jstests/eval2.js
@@ -14,4 +14,15 @@ var f = db.group(
     }
 );
 
-assert(f[0].a == 1 && f[0].csum == 2);  
+assert(f[0].a == 1 && f[0].csum == 2 , "on db" );  
+
+var f = t.group(
+    {
+        key: { a:true},
+        cond: { a:1 },
+        reduce: function(obj,prev) { prev.csum++; } ,
+        initial: { csum: 0}
+    }
+);
+
+assert(f[0].a == 1 && f[0].csum == 2 , "on coll" );  
