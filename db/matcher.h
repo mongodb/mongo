ea32d7806cd9b9948354e6be1f397b37db3f434f
Eliot Horowitz <eliot@10gen.com>
2009-06-24 14:03:35 -0400

    fix $in with NumberInt, NumberDouble SERVER-113

--- db/matcher.h
+++ db/matcher.h
@@ -78,8 +78,9 @@ namespace mongo {
             bool operator()(const BSONElement& l, const BSONElement& r) const
             {
                 int x = (int) l.type() - (int) r.type();
-                if ( x < 0 ) return true;
-                if ( x > 0 ) return false;
+                if ( x == ( NumberInt - NumberDouble ) || x == ( NumberDouble - NumberInt ) );
+                else if ( x < 0 ) return true;
+                else if ( x > 0 ) return false;
                 return compareElementValues(l,r) < 0;
             }
         };
