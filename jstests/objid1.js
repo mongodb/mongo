a96c97161eaff686449560e4f3f5029d91287656
Eliot Horowitz <eliot@10gen.com>
2009-07-06 13:47:00 -0400

    make objectid serialize to something parseable

--- jstests/objid1.js
+++ jstests/objid1.js
@@ -11,3 +11,6 @@ t.save( { a : a } )
 assert( t.findOne().a.isObjectId , "C" );
 assert.eq( a.str , t.findOne().a.str , "D" );
 
+x = { a : new ObjectId() };
+eval( " y = " + tojson( x ) );
+assert.eq( x.a.str , y.a.str , "E" );
