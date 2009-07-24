956fd780e23cebfaf09c6d2fb02c8c3a5e4a9dd3
Eliot Horowitz <eliot@10gen.com>
2009-07-24 11:01:04 -0400

    fix linux compile MINOR

--- util/top.h
+++ util/top.h
@@ -17,6 +17,8 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 namespace mongo {
 
 // Records per namespace utilization of the mongod process.
