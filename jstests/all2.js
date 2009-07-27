fbe8c8373fa7b18d9fda500b63e8d80609c90c59
Eliot Horowitz <eliot@10gen.com>
2009-07-27 11:47:28 -0400

    test for SERVER-187 - $all with embedded objects

---
+++ jstests/all2.js
@@ -0,0 +1,23 @@
+
+t = db.all2;
+t.drop();
+
+t.save( { a : [ { x : 1 } , { x : 2  } ] } )
+t.save( { a : [ { x : 2 } , { x : 3  } ] } )
+t.save( { a : [ { x : 3 } , { x : 4  } ] } )
+
+function check( n , q , e ){
+    assert.eq( n , t.find( q ).count() , tojson( q ) + " " + e );
+}
+
+check( 1 , { "a.x" : { $in : [ 1 ] } } , "A" );
+check( 2 , { "a.x" : { $in : [ 2 ] } } , "B" );
+
+check( 2 , { "a.x" : { $in : [ 1 , 2 ] } } , "C" );
+check( 3 , { "a.x" : { $in : [ 2 , 3 ] } } , "D" );
+check( 3 , { "a.x" : { $in : [ 1 , 3 ] } } , "E" );
+
+// TODO SERVER-187
+//check( 1 , { "a.x" : { all : [ 1 , 2 ] } } , "F" );
+//check( 1 , { "a.x" : { all : [ 2 , 3 ] } } , "G" );
+//check( 0 , { "a.x" : { all : [ 1 , 3 ] } } , "H" );
