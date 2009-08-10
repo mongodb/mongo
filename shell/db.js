e44b51ee69288b7fc1e04da3d8f25cb28c931c68
Eliot Horowitz <eliot@10gen.com>
2009-08-10 13:31:48 -0400

    make copyDatabase use admin db automatically SERVER-222

--- shell/db.js
+++ shell/db.js
@@ -42,6 +42,12 @@ DB.prototype.runCommand = function( obj ){
 
 DB.prototype._dbCommand = DB.prototype.runCommand;
 
+DB.prototype._adminCommand = function( obj ){
+    if ( this._name == "admin" )
+        return this.runCommand( obj );
+    return this.getSisterDB( "admin" ).runCommand( obj );
+}
+
 DB.prototype.addUser = function( username , pass ){
     var c = this.getCollection( "system.users" );
     
@@ -216,7 +222,7 @@ DB.prototype.copyDatabase = function(fromdb, todb, fromhost) {
     assert( isString(todb) && todb.length );
     fromhost = fromhost || "";
     //this.resetIndexCache();
-    return this._dbCommand( { copydb:1, fromhost:fromhost, fromdb:fromdb, todb:todb } );
+    return this._adminCommand( { copydb:1, fromhost:fromhost, fromdb:fromdb, todb:todb } );
 }
 
 /**
