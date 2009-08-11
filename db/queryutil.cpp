386aa02cce9b0d47cf1dfd932530234fe8f689a7
Eliot Horowitz <eliot@10gen.com>
2009-08-10 21:04:13 -0400

    fix test and clean code

--- db/queryutil.cpp
+++ db/queryutil.cpp
@@ -85,7 +85,7 @@ namespace mongo {
             default:
                 break;
         }
-        
+
         if ( lower_.type() != MinKey && upper_.type() == MaxKey ){
             BSONObjBuilder b;
             b.appendMaxForType( lower_.fieldName() , lower_.type() );
@@ -97,14 +97,6 @@ namespace mongo {
             lower_ = addObj( b.obj() ).firstElement();
         }
 
-        /*
-        if ( lower_.isNumber() && upper_.type() == MaxKey ){
-            upper_ = addObj( BSON( lower_.fieldName() << numeric_limits<double>::max() ) ).firstElement();
-        }
-        else if ( upper_.isNumber() && lower_.type() == MinKey ){
-            lower_ = addObj( BSON( upper_.fieldName() << - numeric_limits<double>::max() ) ).firstElement();
-        }
-        */
 
     }
     
