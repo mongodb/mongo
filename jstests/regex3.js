0e8de9950b3c3b71471e02473ffd9d65a5fa18cc
Eliot Horowitz <eliot@10gen.com>
2009-08-17 10:05:14 -0400

    broken regex index test

---
+++ jstests/regex3.js
@@ -0,0 +1,14 @@
+
+t = db.regex3;
+t.drop();
+
+t.save( { name : "eliot" } );
+t.save( { name : "emily" } );
+t.save( { name : "bob" } );
+t.save( { name : "aaron" } );
+
+assert.eq( 2 , t.find( { name : /^e.*/ } ).count() , "no index count" );
+assert.eq( 4 , t.find( { name : /^e.*/ } ).explain().nscanned , "no index explain" );
+t.ensureIndex( { name : 1 } );
+assert.eq( 2 , t.find( { name : /^e.*/ } ).count() , "index count" );
+//assert.eq( 2 , t.find( { name : /^e.*/ } ).explain().nscanned , "index explain" ); // SERVER-239
