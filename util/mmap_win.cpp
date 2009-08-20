c0994c6b3a6b9cab0bcda1d09fb55fbfc9f13b4f
dwight <dwight@Dwights-MacBook-2.local>
2009-08-20 15:28:04 -0400

    get rid of warning

--- util/mmap_win.cpp
+++ util/mmap_win.cpp
@@ -77,8 +77,6 @@ namespace mongo {
             out() << "CreateFile failed " << filename << endl;
             return 0;
         }
-        if ( mapped > 500000000 )
-            out() << "WARNING: too much mem mapped for win32" << endl;
 
         mapped += length;
 
