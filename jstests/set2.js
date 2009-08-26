daef3873b7e35719988d4ed1c70ab7ff3c7da4cf
Eliot Horowitz <eliot@10gen.com>
2009-08-26 15:13:58 -0400

    a test that works MINOR

--- jstests/set2.js
+++ jstests/set2.js
@@ -11,3 +11,8 @@ assert.eq( 17 , t.findOne().x );
 assert.eq( true , t.findOne().y.x );
 t.update( { _id : 1 } , { $set : { "y.x" : 17 } } );
 assert.eq( 17 , t.findOne().y.x );
+
+t.update( { _id : 1 } , { $set : { a : 2 , b : 3 } } );
+assert.eq( 2 , t.findOne().a );
+assert.eq( 3 , t.findOne().b );
+
