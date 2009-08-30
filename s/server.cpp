7a3868c91a3160f98149d484918eba4010ddb479
Eliot Horowitz <eliot@10gen.com>
2009-08-30 17:05:51 -0400

    change mongos version number

--- s/server.cpp
+++ s/server.cpp
@@ -156,7 +156,7 @@ int main(int argc, char* argv[], char *envp[] ) {
         return 1;
     }
 
-    log() << argv[0] << " v0.0.2 starting (--help for usage)" << endl;
+    log() << argv[0] << " v0.2.0- (alpha 2) starting (--help for usage)" << endl;
     printGitVersion();
     printSysInfo();
 
