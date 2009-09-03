222e767340ff1ec0bfd34a46213368db9d478aeb
Eliot Horowitz <eliot@10gen.com>
2009-09-03 14:40:08 -0400

    fix for solaris

--- buildscripts/__init__.py
+++ buildscripts/__init__.py
@@ -3,6 +3,6 @@ import hacks_ubuntu
 import os;
 
 def findHacks( un ):
-    if os.path.exists( "/etc/debian_version" ) or un[3].find( "Ubuntu" ) >= 0:
+    if un[0] == 'Linux' and ( os.path.exists( "/etc/debian_version" ) or un[3].find( "Ubuntu" ) >= 0 ):
         return hacks_ubuntu
     return None
