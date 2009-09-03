6bf502094539906bc576e7363f126257be30f37a
Eliot Horowitz <eliot@10gen.com>
2009-09-03 14:30:39 -0400

    made debian finding better

--- buildscripts/__init__.py
+++ buildscripts/__init__.py
@@ -1,7 +1,8 @@
 
 import hacks_ubuntu
+import os;
 
 def findHacks( un ):
-    if un[3].find( "Ubuntu" ) >= 0:
+    if os.path.exists( "/etc/debian_version" ) or un[3].find( "Ubuntu" ) >= 0:
         return hacks_ubuntu
     return None
