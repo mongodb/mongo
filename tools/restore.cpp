76c4935a58898abf681a6847a4c731ddc68442dd
Mike Dirolf <mike@10gen.com>
2009-09-15 17:02:12 -0400

    mongorestore: can specify db to restore to (only when restoring a directory containing a single databases files)

--- tools/restore.cpp
+++ tools/restore.cpp
@@ -31,7 +31,7 @@ namespace po = boost::program_options;
 
 class Restore : public Tool {
 public:
-    Restore() : Tool( "restore" ){
+    Restore() : Tool( "restore" , "" ){
         add_options()
             ("dir", po::value<string>()->default_value("dump"), "directory to restore from")
             ;
@@ -44,11 +44,17 @@ public:
             cerr << "\"" << root.string() << "\" is not a valid directory" << endl;
             return EXIT_BADOPTIONS;
         }
-        drillDown(root);
-        return 0;
+
+        /* If _db is not "" then the user specified a db name to restore as.
+         *
+         * In that case we better be given a root directory that contains only
+         * .bson files (a db)
+         */
+        drillDown(root, _db != "");
+        return EXIT_CLEAN;
     }
 
-    void drillDown( path root ) {
+    void drillDown( path root, bool use_db = false ) {
         log(2) << "drillDown: " << root.string() << endl;
 
         if ( is_directory( root ) ) {
@@ -56,7 +62,19 @@ public:
             directory_iterator i(root);
             while ( i != end ) {
                 path p = *i;
-                drillDown( p );
+
+                if (use_db) {
+                    if (is_directory(p) ||
+                        !(endsWith(p.string().c_str(), ".bson") ||
+                          endsWith(p.string().c_str(), ".bin" ))) {
+                        cerr << "ERROR: root directory must be a dump of a single database" << endl;
+                        cerr << "       when specifying a db name with --db" << endl;
+                        printHelp(cout);
+                        return;
+                    }
+                }
+
+                drillDown(p, use_db);
                 i++;
             }
             return;
@@ -71,7 +89,9 @@ public:
         out() << root.string() << endl;
 
         string ns;
-        {
+        if (use_db) {
+            ns += _db;
+        } else {
             string dir = root.branch_path().string();
             if ( dir.find( "/" ) == string::npos )
                 ns += dir;
@@ -93,22 +113,22 @@ public:
         }
 
         out() << "\t going into namespace [" << ns << "]" << endl;
-        
+
         string fileString = root.string();
         ifstream file( fileString.c_str() , ios_base::in | ios_base::binary);
         if ( ! file.is_open() ){
             log() << "error opening file: " << fileString << endl;
             return;
         }
-        
+
         log(1) << "\t file size: " << fileLength << endl;
-        
+
         long long read = 0;
         long long num = 0;
 
         int msgDelay = (int)(1000 * ( 1 + ( fileLength / ( 1024.0 * 1024 * 400 ) ) ) );
         log(1) << "\t msg delay: " << msgDelay << endl;
-        
+
         const int BUF_SIZE = 1024 * 1024 * 5;
         char * buf = (char*)malloc( BUF_SIZE );
 
@@ -116,19 +136,19 @@ public:
             file.read( buf , 4 );
             int size = ((int*)buf)[0];
             assert( size < BUF_SIZE );
-            
+
             file.read( buf + 4 , size - 4 );
-            
+
             BSONObj o( buf );
             conn().insert( ns.c_str() , o );
-            
+
             read += o.objsize();
             num++;
-            
+
             if ( ( logLevel > 0 && num < 10 ) || ! ( num % msgDelay ) )
                 out() << "read " << read << "/" << fileLength << " bytes so far. (" << (int)( (read * 100) / fileLength) << "%) " << num << " objects" << endl;
         }
-        
+
         free( buf );
         out() << "\t "  << num << " objects" << endl;
     }
