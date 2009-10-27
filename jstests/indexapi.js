63e0ee59e4c68754a900f77c673b153a8c482f34
Eliot Horowitz <eliot@10gen.com>
2009-10-26 21:10:45 -0400

    fix invalid ensureIndex crashing server SERVER-388

--- jstests/indexapi.js
+++ jstests/indexapi.js
@@ -35,3 +35,6 @@ assert.eq( 2 , idx.length , "M1" );
 assert.eq( key , idx[1].key , "M2" );
 assert( idx[1].unique , "M3" );
 printjson( idx );
+
+db.system.indexes.insert( { ns : "test" , key : { x : 1 } , name : "x" } );
+assert( db.getLastError().indexOf( "invalid" ) >= 0 , "Z1" );
