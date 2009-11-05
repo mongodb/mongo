acc2a3b0ff015d3dee4b1cd75b9cb9492fc499e1
Keith Bostic <keith@wiredtiger.com>
2009-11-06 01:22:24 +1100

    Replace flat-text API file with Python Api class. Merge flags generation into the API file.
    --HG--
    branch : keith

--- dist/api_class.py
+++ dist/api_class.py
@@ -11,300 +11,385 @@
 #	manual page headers
 #	structure method fields
 #
-# The WiredTiger primary handles are the WT_TOC, the DB and the ENV.  The DB/ENV
-# handles have the same functions as the Berkeley DB/ENV handles, that is, they
-# reference a database and a database environment.  WiredTiger adds the WT_TOC
-# handle, which identifies a single thread of control running in an environment.
+# The api file is a python script that loads two dictionaries: "methods" and
+# "flags".
 #
-# The initial line of each entry in the api file is the handle and method name,
-# separated by a single '.'.
-#
-# Subsequent lines are a keyword, followed by additional information.  Possible
-# keywords and their following information are as follows:
-#
-#	argument: method argument name/declaration
-#		An argument to the function.  The "argument" keyword can be
-#		listed multiple times, in the argument order, all of the
-#		others are expected to be single lines.
-#
-#		In a declaration, @S is replaced by the argument name when
-#		needed, sometimes we want the name, and sometimes we don't.
-#
-#	config: [arg, ] arg
+# The "methods" dictionary is a set of API class objects, keyed by the
+# method name.   The fields are:
+#	1: the method name
+#		 'handle' + '.' + 'method'
+#	2: a string of comma-separated configuration key words
 #		getter	-- getter method
 #		method	-- method returns an int
 #		methodV -- method returns void
 #		setter	-- setter method
 #		verify  -- setters call a function to validate the arguments
-#
-#	flag: [flag-name,] flag-name
-#		The name of a flag taken by this method or function.
-#
-#	off/on: [name, ] name
-#		List of method transitions to/from allowed/not-allowed.
+#	3: a list of argument and name/declaration pairs
+#		An argument to the method.  In an argument declaration, "@S"
+#		is replaced by the argument name when needed (sometimes we.
+#		need the name in a declaration, and sometimes we don't).
+#	4: a list of flags, if any.
+#		If there's a flags variable, but the method doesn't currently
+#		take any flags, enter '__NONE__'.
+#	5: a list of on-transitions, if any
+#	6: a list of off-transitions, if any
+
+flags = {}
+methods = {}
+
+class Api:
+	def __init__(self, method, config, args, f, on, off):
+		self.handle = method.split('.')[0]
+		self.method = method.split('.')[1]
+		self.config = config
+		self.args = args
+		if f:
+			flags[method] = f
+		self.on = on
+		self.off = off
+
 
 ###################################################
 # WT_TOC method declarations
 ###################################################
-wt_toc.close
-	config: method
-	argument: flags/u_int32_t @S
-	on: init
+methods['wt_toc.close'] = Api(
+	'wt_toc.close',
+	'method',
+	['flags/u_int32_t @S'],
+	['__NONE__'],
+	['init'], [])
 
 ###################################################
 # ENV method declarations
 ###################################################
-env.cachesize_get
-	config: method, getter
-	argument: cachesize/u_int32_t *@S
-	on: init
-env.cachesize_set
-	config: method, setter
-	argument: cachesize/u_int32_t @S
-	on: init
+methods['env.cachesize_get'] = Api(
+	'env.cachesize_get',
+	'method, getter',
+	['cachesize/u_int32_t *@S'],
+	[],
+	['init'], [])
+methods['env.cachesize_set'] = Api(
+	'env.cachesize_set',
+	'method, setter',
+	['cachesize/u_int32_t @S'],
+	[],
+	['init'], [])
 
-env.close
-	config: method
-	argument: flags/u_int32_t @S
-	on: init
+methods['env.close'] = Api(
+	'env.close',
+	'method',
+	['flags/u_int32_t @S'],
+	['__NONE__'],
+	['init'], [])
 
-env.db
-	config: method
-	argument: flags/u_int32_t @S
-	argument: dbp/DB **@S
-	on: open
+methods['env.db'] = Api(
+	'env.db',
+	'method',
+	['flags/u_int32_t @S', 'dbp/DB **@S'],
+	['__NONE__'],
+	['open'], [])
 
-env.err
-	config: methodV
-	argument: err/int @S
-	argument: fmt/const char *@S, ...
-	on: init
+methods['env.err'] = Api(
+	'env.err',
+	'methodV',
+	['err/int @S', 'fmt/const char *@S, ...'],
+	[],
+	['init'], [])
 
-env.errcall_get
-	config: method, getter
-	argument: errcall/void (**@S)(const ENV *, const char *)
-	on: init
-env.errcall_set
-	config: method, setter
-	argument: errcall/void (*@S)(const ENV *, const char *)
-	on: init
+methods['env.errcall_get'] = Api(
+	'env.errcall_get',
+	'method, getter',
+	['errcall/void (**@S)(const ENV *, const char *)'],
+	[],
+	['init'], [])
+methods['env.errcall_set'] = Api(
+	'env.errcall_set',
+	'method, setter',
+	['errcall/void (*@S)(const ENV *, const char *)'],
+	[],
+	['init'], [])
 
-env.errfile_get
-	config: method, getter
-	argument: errfile/FILE **@S
-	on: init
-env.errfile_set
-	config: method, setter
-	argument: errfile/FILE *@S
-	on: init
+methods['env.errfile_get'] = Api(
+	'env.errfile_get',
+	'method, getter',
+	['errfile/FILE **@S'],
+	[],
+	['init'], [])
+methods['env.errfile_set'] = Api(
+	'env.errfile_set',
+	'method, setter',
+	['errfile/FILE *@S'],
+	[],
+	['init'], [])
 
-env.errpfx_get
-	config: method, getter
-	argument: errpfx/const char **@S
-	on: init
-env.errpfx_set
-	config: method, setter
-	argument: errpfx/const char *@S
-	on: init
+methods['env.errpfx_get'] = Api(
+	'env.errpfx_get',
+	'method, getter',
+	['errpfx/const char **@S'],
+	[],
+	['init'], [])
+methods['env.errpfx_set'] = Api(
+	'env.errpfx_set',
+	'method, setter',
+	['errpfx/const char *@S'],
+	[],
+	['init'], [])
 
-env.errx
-	config: methodV
-	argument: fmt/const char *@S, ...
-	on: init
+methods['env.errx'] = Api(
+	'env.errx',
+	'methodV',
+	['fmt/const char *@S, ...'],
+	[],
+	['init'], [])
 
-env.open
-	config: method
-	argument: home/const char *@S
-	argument: mode/mode_t @S
-	argument: flags/u_int32_t @S
-	on: init
-	off: open
+methods['env.open'] = Api(
+	'env.open',
+	'method',
+	['home/const char *@S', 'mode/mode_t @S', 'flags/u_int32_t @S'],
+	['__NONE__'],
+	['init'], ['open'])
 
-env.stat_clear
-	config: method
-	argument: flags/u_int32_t @S
-	on: open
+methods['env.stat_clear'] = Api(
+	'env.stat_clear',
+	'method',
+	['flags/u_int32_t @S'],
+	['__NONE__'],
+	['init'], [])
 
-env.stat_print
-	config: method
-	argument: stream/FILE *@S
-	argument: flags/u_int32_t @S
-	on: open
+methods['env.stat_print'] = Api(
+	'env.stat_print',
+	'method',
+	['stream/FILE *@S', 'flags/u_int32_t @S'],
+	['__NONE__'],
+	['init'], [])
 
-env.toc
-	config: method
-	argument: flags/u_int32_t @S
-	argument: tocp/WT_TOC **@S
-	on: open
+methods['env.toc'] = Api(
+	'env.toc',
+	'method',
+	['flags/u_int32_t @S', 'tocp/WT_TOC **@S'],
+	['__NONE__'],
+	['open'], [])
 
-env.verbose_get
-	config: method, getter
-	argument: verbose/u_int32_t *@S
-	on: init
-env.verbose_set
-	config: method, setter, verify
-	argument: verbose/u_int32_t @S
-	on: init
+methods['env.verbose_get'] = Api(
+	'env.verbose_get',
+	'method, getter',
+	['verbose/u_int32_t *@S'],
+	[],
+	['init'], [])
+methods['env.verbose_set'] = Api(
+	'env.verbose_set',
+	'method, setter, verify',
+	['verbose/u_int32_t @S'],
+	[
+	'VERB_FILEOPS',
+	'VERB_FILEOPS_ALL'],
+	['init'], [])
 
 ###################################################
 # Db standalone method declarations
 ###################################################
-db.btree_compare_get
-	config: method, getter
-	argument: btree_compare/int (**@S)(DB *, const DBT *, const DBT *)
-	on: init
-db.btree_compare_set
-	config: method, setter
-	argument: btree_compare/int (*@S)(DB *, const DBT *, const DBT *)
-	off: open
-	on: init
+methods['db.btree_compare_get'] = Api(
+	'db.btree_compare_get',
+	'method, getter',
+	['btree_compare/int (**@S)(DB *, const DBT *, const DBT *)'],
+	[],
+	['init'], [])
+methods['db.btree_compare_set'] = Api(
+	'db.btree_compare_set',
+	'method, setter',
+	['btree_compare/int (*@S)(DB *, const DBT *, const DBT *)'],
+	[],
+	['init'], ['open'])
 
-db.btree_compare_dup_get
-	config: method, getter
-	argument: btree_compare_dup/int (**@S)(DB *, const DBT *, const DBT *)
-	on: init
-db.btree_compare_dup_set
-	config: method, setter
-	argument: btree_compare_dup/int (*@S)(DB *, const DBT *, const DBT *)
-	on: init
-	off: open
+methods['db.btree_compare_dup_get'] = Api(
+	'db.btree_compare_dup_get',
+	'method, getter',
+	['btree_compare_dup/int (**@S)(DB *, const DBT *, const DBT *)'],
+	[],
+	['init'], [])
+methods['db.btree_compare_dup_set'] = Api(
+	'db.btree_compare_dup_set',
+	'method, setter',
+	['btree_compare_dup/int (*@S)(DB *, const DBT *, const DBT *)'],
+	[],
+	['init'], ['open'])
 
-db.btree_compare_int_get
-	config: method, getter
-	argument: btree_compare_int/int *@S
-	on: init
-db.btree_compare_int_set
-	config: method, setter, verify
-	argument: btree_compare_int/int @S
-	on: init
-	off: open
+methods['db.btree_compare_int_get'] = Api(
+	'db.btree_compare_int_get',
+	'method, getter',
+	['btree_compare_int/int *@S'],
+	[],
+	['init'], [])
+methods['db.btree_compare_int_set'] = Api(
+	'db.btree_compare_int_set',
+	'method, setter, verify',
+	['btree_compare_int/int @S'],
+	[],
+	['init'], ['open'])
 
-db.btree_dup_offpage_get
-	config: method, getter
-	argument: btree_dup_offpage/u_int32_t *@S
-	on: init
-db.btree_dup_offpage_set
-	config: method, setter
-	argument: btree_dup_offpage/u_int32_t @S
-	on: init
-	off: open
+methods['db.btree_dup_offpage_get'] = Api(
+	'db.btree_dup_offpage_get',
+	'method, getter',
+	['btree_dup_offpage/u_int32_t *@S'],
+	[],
+	['init'], [])
+methods['db.btree_dup_offpage_set'] = Api(
+	'db.btree_dup_offpage_set',
+	'method, setter',
+	['btree_dup_offpage/u_int32_t @S'],
+	[],
+	['init'], ['open'])
 
-db.btree_itemsize_get
-	config: method, getter
-	argument: intlitemsize/u_int32_t *@S
-	argument: leafitemsize/u_int32_t *@S
-	on: init
-db.btree_itemsize_set
-	config: method, setter
-	argument: intlitemsize/u_int32_t @S
-	argument: leafitemsize/u_int32_t @S
-	on: init
-	off: open
+methods['db.btree_itemsize_get'] = Api(
+	'db.btree_itemsize_get',
+	'method, getter',
+	['intlitemsize/u_int32_t *@S', 'leafitemsize/u_int32_t *@S'],
+	[],
+	['init'], [])
+methods['db.btree_itemsize_set'] = Api(
+	'db.btree_itemsize_set',
+	'method, setter',
+	['intlitemsize/u_int32_t @S', 'leafitemsize/u_int32_t @S'],
+	[],
+	['init'], ['open'])
 
-db.btree_pagesize_get
-	config: method, getter
-	argument: allocsize/u_int32_t *@S
-	argument: intlsize/u_int32_t *@S
-	argument: leafsize/u_int32_t *@S
-	argument: extsize/u_int32_t *@S
-	on: init
-db.btree_pagesize_set
-	config: method, setter
-	argument: allocsize/u_int32_t @S
-	argument: intlsize/u_int32_t @S
-	argument: leafsize/u_int32_t @S
-	argument: extsize/u_int32_t @S
-	on: init
-	off: open
+methods['db.btree_pagesize_get'] = Api(
+	'db.btree_pagesize_get',
+	'method, getter',
+	['allocsize/u_int32_t *@S', 'intlsize/u_int32_t *@S',
+	    'leafsize/u_int32_t *@S', 'extsize/u_int32_t *@S'],
+	[],
+	['init'], [])
+methods['db.btree_pagesize_set'] = Api(
+	'db.btree_pagesize_set',
+	'method, setter',
+	['allocsize/u_int32_t @S', 'intlsize/u_int32_t @S',
+	    'leafsize/u_int32_t @S', 'extsize/u_int32_t @S'],
+	[],
+	['init'], ['open'])
 
-db.bulk_load
-	config: method
-	argument: flags/u_int32_t @S
-	argument: cb/int (*@S)(DB *, DBT **, DBT **)
-	on: open
+methods['db.bulk_load'] = Api(
+	'db.bulk_load',
+	'method',
+	['flags/u_int32_t @S', 'cb/int (*@S)(DB *, DBT **, DBT **)'],
+	[
+	'DUPLICATES',
+	'SORTED_INPUT' ],
+	['open'], [])
 
-db.close
-	config: method
-	argument: flags/u_int32_t @S
-	on: init
+methods['db.close'] = Api(
+	'db.close',
+	'method',
+	['flags/u_int32_t @S'],
+	['__NONE__'],
+	['init'], [])
 
-db.dump
-	config: method
-	argument: stream/FILE *@S
-	argument: flags/u_int32_t @S
-	on: open
+methods['db.dump'] = Api(
+	'db.dump',
+	'method',
+	['stream/FILE *@S', 'flags/u_int32_t @S'],
+	[ 'DEBUG', 'PRINTABLES' ],
+	['open'], [])
 
-db.errcall_get
-	config: method, getter
-	argument: errcall/void (**@S)(const DB *, const char *)
-	on: init
-db.errcall_set
-	config: method, setter
-	argument: errcall/void (*@S)(const DB *, const char *)
-	on: init
+methods['db.errcall_get'] = Api(
+	'db.errcall_get',
+	'method, getter',
+	['errcall/void (**@S)(const DB *, const char *)'],
+	[],
+	['init'], [])
+methods['db.errcall_set'] = Api(
+	'db.errcall_set',
+	'method, setter',
+	['errcall/void (*@S)(const DB *, const char *)'],
+	[],
+	['init'], [])
 
-db.errfile_get
-	config: method, getter
-	argument: errfile/FILE **@S
-	on: init
-db.errfile_set
-	config: method, setter
-	argument: errfile/FILE *@S
-	on: init
+methods['db.errfile_get'] = Api(
+	'db.errfile_get',
+	'method, getter',
+	['errfile/FILE **@S'],
+	[],
+	['init'], [])
+methods['db.errfile_set'] = Api(
+	'db.errfile_set',
+	'method, setter',
+	['errfile/FILE *@S'],
+	[],
+	['init'], [])
 
-db.errpfx_get
-	config: method, getter
-	argument: errpfx/const char **@S
-	on: init
-db.errpfx_set
-	config: method, setter
-	argument: errpfx/const char *@S
-	on: init
+methods['db.errpfx_get'] = Api(
+	'db.errpfx_get',
+	'method, getter',
+	['errpfx/const char **@S'],
+	[],
+	['init'], [])
+methods['db.errpfx_set'] = Api(
+	'db.errpfx_set',
+	'method, setter',
+	['errpfx/const char *@S'],
+	[],
+	['init'], [])
 
-db.get
-	config: method
-	argument: toc/WT_TOC *@S
-	argument: key/DBT *@S
-	argument: pkey/DBT *@S
-	argument: data/DBT *@S
-	argument: flags/u_int32_t @S
-	on: open
+methods['db.get'] = Api(
+	'db.get',
+	'method',
+	['toc/WT_TOC *@S', 'key/DBT *@S',
+	    'pkey/DBT *@S', 'data/DBT *@S', 'flags/u_int32_t @S'],
+	['__NONE__'],
+	['open'], [])
 
-db.get_recno
-	config: method
-	argument: toc/WT_TOC *@S
-	argument: recno/u_int64_t @S
-	argument: key/DBT *@S
-	argument: pkey/DBT *@S
-	argument: data/DBT *@S
-	argument: flags/u_int32_t @S
-	on: open
+methods['db.get_recno'] = Api(
+	'db.get_recno',
+	'method',
+	['toc/WT_TOC *@S', 'recno/u_int64_t @S', 'key/DBT *@S',
+	    'pkey/DBT *@S', 'data/DBT *@S', 'flags/u_int32_t @S'],
+	['__NONE__'],
+	['open'], [])
 
-db.open
-	config: method
-	argument: dbname/const char *@S
-	argument: mode/mode_t @S
-	argument: flags/u_int32_t @S
-	off: open
-	on: init
+methods['db.open'] = Api(
+	'db.open',
+	'method',
+	['dbname/const char *@S', 'mode/mode_t @S', 'flags/u_int32_t @S'],
+	[ 'CREATE' ],
+	['init'], [])
 
-db.stat_clear
-	config: method
-	argument: flags/u_int32_t @S
-	on: open
+methods['db.stat_clear'] = Api(
+	'db.stat_clear',
+	'method',
+	['flags/u_int32_t @S'],
+	['__NONE__'],
+	['open'], [])
 
-db.stat_print
-	config: method
-	argument: stream/FILE * @S
-	argument: flags/u_int32_t @S
-	on: open
+methods['db.stat_print'] = Api(
+	'db.stat_print',
+	'method',
+	['stream/FILE * @S', 'flags/u_int32_t @S'],
+	['__NONE__'],
+	['open'], [])
 
-db.sync
-	config: method
-	argument: flags/u_int32_t @S
-	on: open
+methods['db.sync'] = Api(
+	'db.sync',
+	'method',
+	['flags/u_int32_t @S'],
+	['__NONE__'],
+	['open'], [])
 
-db.verify
-	config: method
-	argument: flags/u_int32_t @S
-	on: open
+methods['db.verify'] = Api(
+	'db.verify',
+	'method',
+	['flags/u_int32_t @S'],
+	['__NONE__'],
+	['open'], [])
+
+###################################################
+# Structure and internal routine flag declarations
+###################################################
+flags['cache'] = [ 'INITIALIZED' ]
+flags['dbt'] = [ 'ALLOCATED' ]
+flags['ienv'] = [ 'RUNNING', 'SINGLE_THREADED' ]
+flags['toc'] = [ 'INVALID', 'SINGLE_THREADED' ]
+flags['wiredtiger_env_init'] = [ 'SINGLE_THREADED' ]
+flags['wt_cache_in'] = [ 'UNFORMATTED' ]
+flags['wt_cache_out'] = [ 'MODIFIED', 'UNFORMATTED' ]
+flags['wt_indx'] = [ 'ALLOCATED' ]
+flags['wt_page'] = [ 'ALLOCATED', 'MODIFIED' ]
