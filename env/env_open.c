230f6d96b1c609705d9b0f641d9a07d148eef361
Keith Bostic <keith@wiredtiger.com>
2009-11-27 06:56:28 +1100

    Add cache eviction support. Get rid of the SINGLE_THREADED flag -- we always have a cache server thread running to evict pages.
    --HG--
    branch : keith

--- env/env_open.c
+++ env/env_open.c
@@ -10,23 +10,17 @@
 #include "wt_internal.h"
 
 /*
- * __wt_api_env_open --
+ * __wt_env_open --
  *	Open a Env handle.
  */
 int
-__wt_api_env_open(ENV *env, const char *home, mode_t mode, u_int32_t flags)
+__wt_env_open(ENV *env, const char *home, mode_t mode, u_int32_t flags)
 {
 	IENV *ienv;
+	int ret;
 
 	ienv = env->ienv;
-
-	WT_ENV_FCHK(env, "Env.open", flags, WT_APIMASK_ENV_OPEN);
-
-	/* If we're not single-threaded, start the workQ thread. */
-	if (!F_ISSET(ienv, WT_SINGLE_THREADED)) {
-		F_SET(ienv, WT_RUNNING);
-		WT_RET(__wt_thread_create(&ienv->primary_tid,__wt_workq, env));
-	}
+	ret = 0;
 
 	/* Create the cache. */
 	WT_RET(__wt_cache_create(env));
@@ -34,15 +28,25 @@ __wt_api_env_open(ENV *env, const char *home, mode_t mode, u_int32_t flags)
 	/* Turn on the methods that require open. */
 	__wt_methods_env_open_transition(env);
 
+	/* Start worker threads. */
+	F_SET(ienv, WT_WORKQ_RUN | WT_SERVER_RUN);
+	WT_MEMORY_FLUSH;
+
+	WT_ERR(__wt_thread_create(&ienv->workq_tid, __wt_workq_srvr, env));
+	WT_ERR(__wt_thread_create(&ienv->cache_tid, __wt_cache_srvr, env));
+
 	return (0);
+
+err:	(void)__wt_env_close(env, 0);
+	return (ret);
 }
 
 /*
- * __wt_api_env_close --
+ * __wt_env_close --
  *	Close an Env handle.
  */
 int
-__wt_api_env_close(ENV *env, u_int32_t flags)
+__wt_env_close(ENV *env, u_int32_t flags)
 {
 	IDB *idb;
 	IENV *ienv;
@@ -51,16 +55,6 @@ __wt_api_env_close(ENV *env, u_int32_t flags)
 
 	ienv = env->ienv;
 	ret = secondary_err = 0;
-
-	WT_ENV_FCHK_NOTFATAL(
-	    env, "ENV.close", flags, WT_APIMASK_ENV_CLOSE, ret);
-
-	/* Close down the workQ server. */
-	if (!F_ISSET(env, WT_SINGLE_THREADED)) {
-		F_CLR(ienv, WT_RUNNING);
-		WT_FLUSH_MEMORY;
-		__wt_thread_join(ienv->primary_tid);
-	}
 
 	/* Complain if DB handles weren't closed. */
 	if (TAILQ_FIRST(&ienv->dbqh) != NULL) {
@@ -84,6 +78,23 @@ __wt_api_env_close(ENV *env, u_int32_t flags)
 		secondary_err = WT_ERROR;
 	}
 
+	/* Close down and wait for any server threads. */
+	F_CLR(ienv, WT_SERVER_RUN);
+	WT_MEMORY_FLUSH;
+
+	__wt_unlock(&ienv->cache.mtx);
+	__wt_thread_join(ienv->cache_tid);
+
+	/*
+	 * Close down and wait for the workQ thread; this only happens after
+	 * all other server threads have exited, as they may be waiting on a
+	 * request from the workQ.
+	 */
+	F_CLR(ienv, WT_WORKQ_RUN);
+	WT_MEMORY_FLUSH;
+
+	__wt_thread_join(ienv->workq_tid);
+
 	/* Discard the cache. */
 	WT_TRET(__wt_cache_destroy(env));
 
@@ -91,7 +102,7 @@ __wt_api_env_close(ENV *env, u_int32_t flags)
 	WT_TRET(__wt_ienv_destroy(env, 0));
 
 	/* Free the Env structure. */
-	memset(env, OVERWRITE_BYTE, sizeof(env));
+	memset(env, WT_OVERWRITE, sizeof(env));
 	__wt_free(NULL, env);
 
 	if (ret == 0)
