27d0af8a2a7fe1cb5671152fb6577e7f717f50cf
Keith Bostic <keith@wiredtiger.com>
2009-11-27 06:57:16 +1100

    Build single API descriptor file (merge Python flag script with API script). Add automatic flag checking in the API. Add function that prints out a statistics field, handling large numbers.
    --HG--
    branch : keith

--- db/db_stat.c
+++ db/db_stat.c
@@ -10,60 +10,49 @@
 #include "wt_internal.h"
 
 /*
- * __wt_api_db_stat_print --
+ * __wt_db_stat_print --
  *	Print DB handle statistics to a stream.
  */
 int
-__wt_api_db_stat_print(DB *db, FILE *stream, u_int32_t flags)
+__wt_db_stat_print(DB *db, FILE *stream, u_int32_t flags)
 {
+	ENV *env;
 	IDB *idb;
-	IENV *ienv;
-	WT_STATS *stats;
 
+	env = db->env;
 	idb = db->idb;
-	ienv = db->env->ienv;
-
-	WT_DB_FCHK(db, "Db.stat_print", flags, WT_APIMASK_DB_STAT_PRINT);
-
-	fprintf(stream, "Database statistics: %s\n", idb->dbname);
 
 	/* Clear the database stats, then call Btree stat to fill them in. */
 	__wt_stat_clear_idb_dstats(idb->dstats);
 	WT_RET(__wt_bt_stat(db));
 
-	for (stats = idb->dstats; stats->desc != NULL; ++stats)
-		WT_STAT_FIELD_PRINT(stream, stats);
+	fprintf(stream, "Database statistics: %s\n", idb->dbname);
+	__wt_stat_print(env, idb->dstats, stream);
 
 	/* Database handle statistics. */
-	fprintf(stream, "%s\n", ienv->sep);
 	fprintf(stream, "Database handle statistics: %s\n", idb->dbname);
-	for (stats = idb->stats; stats->desc != NULL; ++stats)
-		WT_STAT_FIELD_PRINT(stream, stats);
+	__wt_stat_print(env, idb->stats, stream);
 
 	/* Underlying file handle statistics. */
 	if (idb->fh != NULL) {
-		fprintf(stream, "%s\n", ienv->sep);
 		fprintf(stream,
 		    "Underlying file I/O statistics: %s\n", idb->dbname);
-		for (stats = idb->fh->stats; stats->desc != NULL; ++stats)
-			WT_STAT_FIELD_PRINT(stream, stats);
+		__wt_stat_print(env, idb->fh->stats, stream);
 	}
 
 	return (0);
 }
 
 /*
- * __wt_api_db_stat_clear --
+ * __wt_db_stat_clear --
  *	Clear DB handle statistics.
  */
 int
-__wt_api_db_stat_clear(DB *db, u_int32_t flags)
+__wt_db_stat_clear(DB *db, u_int32_t flags)
 {
 	IDB *idb;
 
 	idb = db->idb;
-
-	WT_DB_FCHK(db, "Db.stat_clear", flags, WT_APIMASK_DB_STAT_CLEAR);
 
 	__wt_stat_clear_idb_stats(idb->stats);
 	__wt_stat_clear_idb_dstats(idb->dstats);
--- env/env_stat.c
+++ env/env_stat.c
@@ -10,37 +10,31 @@
 #include "wt_internal.h"
 
 /*
- * __wt_api_env_stat_print --
+ * __wt_env_stat_print --
  *	Print ENV handle statistics to a stream.
  */
 int
-__wt_api_env_stat_print(ENV *env, FILE *stream, u_int32_t flags)
+__wt_env_stat_print(ENV *env, FILE *stream, u_int32_t flags)
 {
 	IDB *idb;
 	IENV *ienv;
-	WT_STATS *stats;
 
 	ienv = env->ienv;
 
-	WT_ENV_FCHK(env, "Env.stat_print", flags, WT_APIMASK_ENV_STAT_PRINT);
-
 	fprintf(stream, "Environment handle statistics:\n");
-	for (stats = ienv->stats; stats->desc != NULL; ++stats)
-		WT_STAT_FIELD_PRINT(stream, stats);
+	__wt_stat_print(env, ienv->stats, stream);
 
-	fprintf(stream, "%s\n", ienv->sep);
 	TAILQ_FOREACH(idb, &ienv->dbqh, q)
-		if (!F_ISSET(idb, WT_INVALID))
-			WT_RET(idb->db->stat_print(idb->db, stream, flags));
+		WT_RET(idb->db->stat_print(idb->db, stream, flags));
 	return (0);
 }
 
 /*
- * __wt_api_env_stat_clear --
+ * __wt_env_stat_clear --
  *	Clear ENV handle statistics.
  */
 int
-__wt_api_env_stat_clear(ENV *env, u_int32_t flags)
+__wt_env_stat_clear(ENV *env, u_int32_t flags)
 {
 	DB *db;
 	IDB *idb;
@@ -50,14 +44,34 @@ __wt_api_env_stat_clear(ENV *env, u_int32_t flags)
 	ienv = env->ienv;
 	ret = 0;
 
-	WT_ENV_FCHK(env, "Env.stat_clear", flags, WT_APIMASK_ENV_STAT_CLEAR);
-
-	TAILQ_FOREACH(idb, &ienv->dbqh, q)
-		if (!F_ISSET(idb, WT_INVALID)) {
-			db = idb->db;
-			WT_TRET(db->stat_clear(db, flags));
-		}
+	TAILQ_FOREACH(idb, &ienv->dbqh, q) {
+		db = idb->db;
+		WT_TRET(db->stat_clear(db, flags));
+	}
 
 	__wt_stat_clear_ienv_stats(ienv->stats);
 	return (ret);
+}
+
+/*
+ * __wt_stat_print --
+ *	Print out a statistics table.
+ */
+void
+__wt_stat_print(ENV *env, WT_STATS *s, FILE *stream)
+{
+	IENV *ienv;
+
+	ienv = env->ienv;
+
+	for (; s->desc != NULL; ++s)
+		if (s->v >= WT_BILLION)
+			fprintf(stream, "%lluB\t%s (%llu)\n",
+			    s->v / WT_BILLION, s->desc, s->v);
+		else if (s->v >= WT_MILLION)
+			fprintf(stream, "%lluM\t%s (%llu)\n",
+			    s->v / WT_MILLION, s->desc, s->v);
+		else
+			fprintf(stream, "%llu\t%s\n", s->v, s->desc);
+	fprintf(stream, "%s\n", ienv->sep);
 }
