fcd9b5ccd41e034b27965072c1c5785e45d287fa
Keith Bostic <keith@wiredtiger.com>
2009-11-27 06:41:46 +1100

    Get rid of internal DB handle WT_TOC, create a new WT_TOC as necessary.
    --HG--
    branch : keith

--- btree/bt_sync.c
+++ btree/bt_sync.c
@@ -14,7 +14,21 @@
  *	Sync a Btree.
  */
 int
-__wt_bt_sync(DB *db)
+__wt_bt_sync(DB *db, void (*f)(const char *, u_int32_t))
 {
-	return (__wt_cache_sync(db->env, db->idb->fh));
+	ENV *env;
+	WT_TOC *toc;
+	int ret;
+
+	env = db->env;
+	ret = 0;
+
+	WT_RET(env->toc(env, 0, &toc));
+	WT_TOC_DB_INIT(toc, db, "Db.sync");
+
+	ret = __wt_cache_sync(toc, db->idb->fh, f);
+
+	WT_TRET(toc->close(toc, 0));
+
+	return (ret);
 }
