4dc43abe4ee9922fe6357e35b4241447edb115ef
Keith Bostic <keith@wiredtiger.com>
2009-11-27 06:32:25 +1100

    Build single API descriptor file (merge Python flag script with API script). Add automatic flag checking in the API. Get rid of internal DB handle WT_TOC, create a new WT_TOC as necessary. Discard pinned root page when closing a database.
    --HG--
    branch : keith

--- btree/bt_close.c
+++ btree/bt_close.c
@@ -18,20 +18,32 @@ __wt_bt_close(DB *db)
 {
 	ENV *env;
 	IDB *idb;
+	WT_TOC *toc;
 	int ret;
 
 	env = db->env;
 	idb = db->idb;
 	ret = 0;
 
-	/* Flush any modified pages, and discard the tree. */
-	WT_TRET(__wt_bt_sync(db));
-	idb->root_page = NULL;
+	WT_RET(env->toc(env, 0, &toc));
+	WT_TOC_DB_INIT(toc, db, "Db.close");
+
+	/* Discard the pinned root page. */
+	if (idb->root_page != NULL) {
+		F_CLR(idb->root_page, WT_PINNED);
+		WT_TRET(__wt_bt_page_out(toc, idb->root_page, 0));
+		idb->root_page = NULL;
+	}
 	idb->root_addr = WT_ADDR_INVALID;
+
+	/* Flush any modified pages, and discard the tree. */
+	WT_TRET(__wt_bt_sync(db, NULL));
 
 	/* Close the underlying file handle. */
 	WT_TRET(__wt_close(env, idb->fh));
 	idb->fh = NULL;
+
+	WT_TRET(toc->close(toc, 0));
 
 	return (ret);
 }
