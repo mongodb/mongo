05d5719b1fda9e17b2c97b97ca91d311adaafa19
dwight <dwight@Dwights-MacBook-2.local>
2009-11-28 16:31:57 -0500

    add warning

--- db/db.cpp
+++ db/db.cpp
@@ -398,6 +398,7 @@ namespace mongo {
 
     void show_32_warning(){
 #if BOOST_VERSION < 103500
+#warning built with boost version <= 1.34, limited concurrency
         //cout << \n** NOTE: built with boost version <= 1.34, limited concurrency" << endl;
 #endif
 
