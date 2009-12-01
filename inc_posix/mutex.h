aa901a2229e89e756bb057ccdf9159310b49d28e
Keith Bostic <keith@wiredtiger.com>
2009-12-02 07:20:19 +1100

    Re-work implementation of "private serialization". Replace special toc->serial "waiting" value with a flag, we can never depend on it being set, so a flag makes more sense.
    --HG--
    branch : keith

--- inc_posix/mutex.h
+++ inc_posix/mutex.h
@@ -57,45 +57,100 @@ extern void *__wt_addr;
  * Serialization:
  *
  * Serialization support allows scheduling operations that require serialized
- * access to data structures, but which do not require readers to block from
- * using those data structures.  For example, updating the bytes allocated in
- * the cache does not require readers to quit reading, only that writers update
- * serially.
+ * access to a piece of data, where the data (1) is accessed only by serialized
+ * code, or where the data, when accessed by non-serialized code, can either
+ * (2) be read atomically, or (3) it doesn't matter if it's read incorrectly.
+ * In other words, the readers are key, and they are known to be indifferent
+ * to the serialization code modifying the data.
  *
- * Threads serializing set a function pointer/argument pair in their WT_TOC
- * handles, then flush memory and spin; the workQ thread notes the set and
- * calls the function.  When the function returns, the workQ thread clears
- * the WT_TOC field and the original thread can proceed.  Scheduling threads
- * usually divide work into two parts: work done before serialization is
- * required, and work requiring serialization.
+ * An example of #1 is updating the size of a database file.  The size is only
+ * changed in serialized code, and never read by anything else.  An example of
+ * #2 is updating a 32-bit value, because readers by definition get consistent
+ * views of 32-bit memory locations.   An example of #3 is updating a 64-bit
+ * value (such as the bytes allocated in the cache).  While there is a small
+ * possibility a reader sees a corrupted value, the value is only used for
+ * advisory actions, such as waking the cache thread to see if there's work to
+ * do.
+ *
+ * Threads serializing access to a data set a function pointer/argument pair
+ * in the WT_TOC handle, flush memory and spin; the workQ thread notes the set
+ * and calls the function.  When the function returns, the workQ thread flushes
+ * memory and clears the WT_TOC function pointer/argument pair, allowing the
+ * original thread to proceed.  (The flush is not necessary: we cannot depend
+ * on it, because serialization support doesn't lock other threads out of the
+ * data, so there's no ordering constraint.  However, we flush so other threads
+ * are as up-to-date as possible.)
  *
  * Private serialization:
  *
- * Private serialization support allows threads of control to block both
- * readers and writers from a data structure while an operation is done.
- * For example, removing pages from the cache requires that no threads be
- * using the page when it's discarded.
+ * Additionally, serialization optionally supports data access where readers
+ * are NOT indifferent to modifications of the data.   For example, a reader
+ * might retrieve a page from the cache and be descheduled.   A writer of the
+ * page must wait until the reader has discarded its reference to the page
+ * before it modifies the page.
  *
- * Threads needing private serialization set the function pointer/argument
- * described for serialization, an additionally set a pointer to a memory
- * location that acts as a serialization point.  The workQ thread sets this
- * memory location to an API generation number.   Threads with API generation
- * numbers equal-to-or-lower than the serialization point's value continue
- * through the serialization point.  Threads with API generation numbers
- * greater than the set value pause.  When the workQ determines no thread
- * in the system has an API generation number lower than the serialization
- * point's, it calls the serialization function.  When that function returns,
- * the serialization point is cleared, and all waiters proceed.
+ * This is supported by allowing a thread to request serialization, with the
+ * additional requirement that serialization shouldn't occur until every thread
+ * of control which might be in the process of reading the data has left the
+ * library.
  *
+ * Threads needing private serialization set the function pointer/argument as
+ * described for serialization, and additionally set a reference to a structure
+ * with two values: an API generation and a modification generation.
+ *
+ * The API generation is how we ensure potential readers have drained from the
+ * library before the serialization function runs.  Threads with API generation
+ * numbers equal-to-or-lower than the serialization point's continue through
+ * the serialization point.  Threads with API generation numbers greater than
+ * the point's value block.  When the workQ thread knows there are no lower API
+ * generation numbers in the library than the set point's, the serialization
+ * function is safe to run -- there can be no remaining readers of the data.
+ *
+ * This can stall.  For example, if two threads with the same API generation
+ * request private serialization, they will wait forever, each waiting for the
+ * other thread to leave the library.  To handle this, threads requesting
+ * private serialization do not block other requests for private serialization.
+ * This means no thread of control can access shared data, request private
+ * serialization, and then re-access that same shared data, without some
+ * higher-level lock in place to stop other threads from modifying the data,
+ * or using some method to determine if the data has been modified while the
+ * thread waited on its private serialization request.
+ *
+ * The modification generation value in the serialization point provides basic
+ * support for detection data modification.  The modification generation is
+ * incremented whenever a private serialization function runs.  Before running
+ * a private serialization function, the workQ first checks the modification
+ * generation.  If that value is unchanged since the private serialization
+ * request was scheduled, the function runs.  If the value has changed, the
+ * function isn't run and WT_RESTART is returned.
+ */
+
+/*
  * WT_TOC_SERIALIZE_REQUEST --
  *	Configure a serialization request.
  */
-#define	WT_TOC_SERIALIZE_REQUEST(toc, f, sp, args) do {			\
-	(toc)->serialize_private = sp;					\
-	(toc)->serialize_args = args;					\
-	(toc)->serialize = f;						\
-	while ((toc)->serialize != NULL)				\
+#define	WT_TOC_SERIALIZE_REQUEST(toc, f, sp, sa) do {			\
+	(toc)->serial_private = sp;					\
+	(toc)->serial_args = sa;					\
+	(toc)->serial = f;						\
+	while ((toc)->serial != NULL)					\
 		__wt_yield();						\
+} while (0)
+
+/*
+ * WT_TOC_SERIALIZE_WAIT --
+ *	Cause a reader to wait on another thread's private serialization
+ *	request, if the request's generation number is less than or equal
+ *	to the reader's generation number.
+ */
+#define	WT_TOC_SERIALIZE_WAIT(toc, sp) do {				\
+	while ((sp)->api_gen != 0 && (sp)->api_gen <= (toc)->api_gen) {	\
+		F_SET((toc), WT_WAITING);				\
+		while ((sp)->api_gen != 0 &&				\
+		    (sp)->api_gen <= (toc)->api_gen)			\
+			__wt_yield();					\
+		F_CLR((toc), WT_WAITING);				\
+	}								\
 } while (0)
 
 /*
@@ -106,35 +161,18 @@ extern void *__wt_addr;
 #define	WT_TOC_GEN_IGNORE	0
 
 /*
- * WT_TOC_WAITER --
- *	The serialization reference for a WT_TOC waiting on another thread's
- *	request.
- */
-#define	WT_TOC_WAITER		((void *)0x01)
-
-/*
- * WT_TOC_SERIALIZE_WAIT --
- *	Wait on another thread's private serialization request.
- */
-#define	WT_TOC_SERIALIZE_WAIT(toc, sp) do {				\
-	while (*(sp) != 0 && *(sp) <= (toc)->api_gen) {			\
-		(toc)->serialize = WT_TOC_WAITER;			\
-		while (*(sp) != 0 && *(sp) <= (toc)->api_gen)		\
-			__wt_yield();					\
-		(toc)->serialize = NULL;				\
-	}								\
-} while (0)
-/*
  * WT_TOC_SERIALIZE_VALUE --
- *	Wait on a memory location to go to zero.
+ *	Cause a thread of control to wait on a memory location.   Here we
+ *	don't care about API generation numbers, we only want to lock out
+ *	threads for some reason.
  */
 #define	WT_TOC_SERIALIZE_VALUE(toc, vp) do {				\
 	WT_TOC_API_IGNORE(toc);						\
 	while (*(vp) != 0) {						\
-		(toc)->serialize = WT_TOC_WAITER;			\
+		F_SET((toc), WT_WAITING);				\
 		while (*(vp) != 0)					\
 			__wt_yield();					\
-		(toc)->serialize = NULL;				\
+		F_CLR((toc), WT_WAITING);				\
 	}								\
 	WT_TOC_API_RESET(toc);						\
 } while (0)
