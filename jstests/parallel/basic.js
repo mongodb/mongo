3699192679c7aef16068d32f309c1df387a79a9e
Aaron <aaron@10gen.com>
2009-12-28 12:03:47 -0800

    SERVER-470 improve basic test

--- jstests/parallel/basic.js
+++ jstests/parallel/basic.js
@@ -3,11 +3,13 @@ f.drop();
 f.ensureIndex( {me:1} );
 
 // would be nice to configure and log a random seed so we can reproduce
-// behavior.  Seems like that's impossible using Math.random().
+// behavior.  Seems like that's impossible using Math.random(), though :(
 
 expTimeout = function( mean ) {
     return -Math.log( Math.random() ) * mean;
 }
+
+failed = false;
 
 test = function( mean, me ) {
     var m = new Mongo( db.getMongo().host );
@@ -15,7 +17,12 @@ test = function( mean, me ) {
     for( var i = 0; i < 1000; ++i ) {
         sleep( expTimeout( mean ) );
         if ( i % 50 == 0 ) {
-            assert.eq( i, t.count( { who:me } ) );
+            try {
+                assert.eq( i, t.count( { who:me } ) );
+            } catch ( e ) {
+                failed = true;
+                throw e;
+            }
             print( me + " " + i );
         }
         t.insert( { i:i, who:me } );
@@ -24,9 +31,12 @@ test = function( mean, me ) {
 
 runners = new Array();
 for( i = 0; i < 10; ++i ) {
-    runners.push( fork( test, Math.random() * 40, i ) );
+    runners.push( fork( test, Math.random() * 20, i ) );
 }
 
 runners.forEach( function( x ) { x.start() } );
 runners.forEach( function( x ) { x.join() } );
 
+assert( !failed, "one or more threads failed" );
+
+assert( f.validate().valid );
