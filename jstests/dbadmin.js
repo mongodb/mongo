5f7ea78daee28c948c4e1bd4684eec044ce87bee
Eliot Horowitz <eliot@10gen.com>
2010-01-02 08:41:12 -0500

    better debugging for solaris

--- jstests/dbadmin.js
+++ jstests/dbadmin.js
@@ -2,13 +2,13 @@
 t = db.dbadmin;
 t.save( { x : 1 } );
 
-before = db._adminCommand( "serverStatus" );
+before = db._adminCommand( "serverStatus" )
 db._adminCommand( "closeAllDatabases" );
 after = db._adminCommand( "serverStatus" );
-assert( before.mem.mapped > after.mem.mapped , "closeAllDatabases does something" );
+assert( before.mem.mapped > after.mem.mapped , "closeAllDatabases does something before:" + tojson( before ) + " after:" + tojson( after ) );
 
 t.save( { x : 1 } );
 
 res = db._adminCommand( "listDatabases" );
 assert( res.databases.length > 0 , "listDatabases 1" );
-printjson( res );
+// TODO: add more tests here
