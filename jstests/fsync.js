d77c57138b066585ea90e60bdd3903b68338be8d
Aaron <aaron@10gen.com>
2010-01-04 10:54:12 -0800

    MINOR revert accidental fsync.js changes

--- jstests/fsync.js
+++ jstests/fsync.js
@@ -1,9 +1,5 @@
 // test the lock/unlock snapshotting feature a bit
 
-debug = function( t ) {
-    print( t );
-}
-
 x=db.runCommand({fsync:1,lock:1});
 assert(!x.ok,"D");
 
@@ -16,22 +12,11 @@ assert(x.ok,"C");
 y = d.currentOp();
 assert(y.fsyncLock,"B");
 
-debug( "current op ok" );
-
-printjson( db.foo.find().toArray() );
-db.foo.insert({x:2});
-
-debug( "done insert attempt" );
-
 z = d.runCommand({unlock:1});
-
-debug( "sent unlock" );
 
 // it will take some time to unlock, and unlock does not block and wait for that
 // doing a write will make us wait until db is writeable.
 db.foo.insert({x:1});
-
-debug( "done insert" );
 
 assert( d.currentOp().fsyncLock == null, "A" );
 
