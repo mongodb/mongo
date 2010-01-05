e93fad288b48b23b27bba19eb10f299945fb0c4b
Aaron <aaron@10gen.com>
2010-01-05 10:48:26 -0800

    SERVER-470 avoid conflicting use of the popular 'foo' namespace

--- jstests/fsync.js
+++ jstests/fsync.js
@@ -16,7 +16,7 @@ z = d.$cmd.sys.unlock.findOne();
 
 // it will take some time to unlock, and unlock does not block and wait for that
 // doing a write will make us wait until db is writeable.
-db.foo.insert({x:1});
+db.jstests_fsync.insert({x:1});
 
 assert( d.currentOp().fsyncLock == null, "A" );
 
