ddf26da6eb406884dfe2421010ac27d86907de39
Aaron <aaron@10gen.com>
2010-01-05 18:58:50 -0800

    MINOR attempt to appease 64bit linux buildbot by reverting error1.js changes

--- jstests/error1.js
+++ jstests/error1.js
@@ -1,41 +1,40 @@
-db.jstests_error1.drop();
 
 // test 1
 db.$cmd.findOne({reseterror:1});
-assert( db.$cmd.findOne({getlasterror:1}).err == null, "A" );
-assert( db.$cmd.findOne({getpreverror:1}).err == null, "B" );
+assert( db.$cmd.findOne({getlasterror:1}).err == null );
+assert( db.$cmd.findOne({getpreverror:1}).err == null );
 
 db.resetError();
-assert( db.getLastError() == null, "C" );
+assert( db.getLastError() == null );
 assert( db.getPrevError().err == null , "preverror 1" );
 
 // test 2
 
 db.$cmd.findOne({forceerror:1});
-assert( db.$cmd.findOne({getlasterror:1}).err != null, "D" );
-assert( db.$cmd.findOne({getpreverror:1}).err != null, "E" );
+assert( db.$cmd.findOne({getlasterror:1}).err != null );
+assert( db.$cmd.findOne({getpreverror:1}).err != null );
 
 
-assert( db.getLastError() != null, "F" );
+assert( db.getLastError() != null );
 assert( db.getPrevError().err != null , "preverror 2" );
-assert( db.getPrevError().nPrev == 1, "G" );
+assert( db.getPrevError().nPrev == 1 );
 
-db.jstests_error1.findOne();
-assert( db.$cmd.findOne({getlasterror:1}).err == null, "H" );
-assert( db.$cmd.findOne({getpreverror:1}).err != null, "I" );
-assert( db.$cmd.findOne({getpreverror:1}).nPrev == 2, "J" );
+db.foo.findOne();
+assert( db.$cmd.findOne({getlasterror:1}).err == null );
+assert( db.$cmd.findOne({getpreverror:1}).err != null );
+assert( db.$cmd.findOne({getpreverror:1}).nPrev == 2 );
 
-db.jstests_error1.findOne();
-assert( db.$cmd.findOne({getlasterror:1}).err == null, "K" );
-assert( db.$cmd.findOne({getpreverror:1}).err != null, "L" );
-assert( db.$cmd.findOne({getpreverror:1}).nPrev == 3, "M" );
+db.foo.findOne();
+assert( db.$cmd.findOne({getlasterror:1}).err == null );
+assert( db.$cmd.findOne({getpreverror:1}).err != null );
+assert( db.$cmd.findOne({getpreverror:1}).nPrev == 3 );
 
 db.resetError();
 db.forceError();
-db.jstests_error1.findOne();
+db.foo.findOne();
 assert( db.getLastError() == null , "getLastError 5" );
 assert( db.getPrevError().err != null , "preverror 3" );
 
 // test 3
 db.$cmd.findOne({reseterror:1});
-assert( db.$cmd.findOne({getpreverror:1}).err == null, "N" );
+assert( db.$cmd.findOne({getpreverror:1}).err == null );
