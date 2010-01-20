15a31df5e98a1e2625f9327c8ca8f32161a6f097
Aaron <aaron@10gen.com>
2010-01-19 17:07:30 -0800

    MINOR cleaner directory check in diskfull.js test, validate dir in listFiles

--- jstests/disk/diskfull.js
+++ jstests/disk/diskfull.js
@@ -1,13 +1,12 @@
-port = allocatePorts( 1 )[ 0 ];
 doIt = true;
-try {
-    m = startMongoProgram( "mongod", "--port", port, "--dbpath", "/data/db/diskfulltest", "--nohttpinterface", "--bind_ip", "127.0.0.1" );
-} catch ( e ) {
+if ( !( "diskfulltest" in listFiles( "/data/db" ) ) ) {
     print( "path /data/db/diskfulltest/ missing, skipping diskfull test" );
     doIt = false;
 }
 
 if ( doIt ) {
+    port = allocatePorts( 1 )[ 0 ];
+    m = startMongoProgram( "mongod", "--port", port, "--dbpath", "/data/db/diskfulltest", "--nohttpinterface", "--bind_ip", "127.0.0.1" );
     m.getDB( "diskfulltest" ).getCollection( "diskfulltest" ).save( { a: 6 } );
     assert.soon( function() { return rawMongoProgramOutput().match( /dbexit: really exiting now/ ); }, "didn't see 'really exiting now'" );
     assert( !rawMongoProgramOutput().match( /Got signal/ ), "saw 'Got signal', not expected.  Output: " + rawMongoProgramOutput() );
