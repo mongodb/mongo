0cde1d796f820a2f359b34b718036e6919dc84cd
Aaron <aaron@10gen.com>
2010-01-19 17:20:34 -0800

    MINOR don't reuse port from other test

--- jstests/disk/preallocate.js
+++ jstests/disk/preallocate.js
@@ -1,18 +1,20 @@
+port = allocatePorts( 1 )[ 0 ]
+
 var baseName = "jstests_preallocate";
 
 vsize = function() {
     return m.getDB( "admin" ).runCommand( "serverStatus" ).mem.virtual;
 }
 
-var m = startMongod( "--port", "27018", "--dbpath", "/data/db/" + baseName );
+var m = startMongod( "--port", port, "--dbpath", "/data/db/" + baseName );
 
 m.getDB( baseName ).createCollection( baseName + "1" );
 
 vs = vsize();
 
-stopMongod( 27018 );
+stopMongod( port );
 
-var m = startMongoProgram( "mongod", "--port", "27018", "--dbpath", "/data/db/" + baseName );
+var m = startMongoProgram( "mongod", "--port", port, "--dbpath", "/data/db/" + baseName );
 
 m.getDB( baseName ).createCollection( baseName + "2" );
 
