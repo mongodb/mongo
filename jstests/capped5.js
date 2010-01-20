9d42a8e04bdc4aa99148122c0e51c3745f87f906
Aaron <aaron@10gen.com>
2010-01-20 11:35:54 -0800

    SERVER-525 enable case in capped5.js

--- jstests/capped5.js
+++ jstests/capped5.js
@@ -42,9 +42,9 @@ assert.eq( 1, db.system.indexes.count( {ns:"test."+tn} ) );
 assert.eq( 2, t.find().hint( {x:1} ).toArray().length );
 
 // SERVER-525
-//t.drop();
-//db.createCollection( tn , {capped: true, size: 1024 * 1024 * 1 } );
-//t.ensureIndex( { _id:1 } );
-//t.insert( { _id : 5 , x : 11 } );
-//t.insert( { _id : 5 , x : 12 } );
-//assert.eq( 1, t.find().toArray().length );
+t.drop();
+db.createCollection( tn , {capped: true, size: 1024 * 1024 * 1 } );
+t.ensureIndex( { _id:1 } );
+t.insert( { _id : 5 , x : 11 } );
+t.insert( { _id : 5 , x : 12 } );
+assert.eq( 1, t.find().toArray().length );
