39e349743a3df966ba394eb9fe4f123339f2d311
Aaron <aaron@10gen.com>
2010-01-25 11:39:28 -0800

    SERVER-512 MINOR looks like scope cache not used in shell, and releasing mutex is causing problems on server

--- scripting/engine.cpp
+++ scripting/engine.cpp
@@ -217,7 +217,6 @@ namespace mongo {
                 return;
             
             clear();
-            _mutex.unlock(); // boost will assert if the mutex is destroyed while locked
         }
 
         void done( const string& pool , Scope * s ){
