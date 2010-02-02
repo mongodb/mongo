95574b3c9ec842ac7636be8e8013150f110e2a59
Aaron <aaron@10gen.com>
2010-02-02 13:52:52 -0800

    SERVER-258 test debug

--- jstests/auth/auth1.js
+++ jstests/auth/auth1.js
@@ -66,4 +66,4 @@ var p = { key : { i : true } ,
 initial: { count: 0 }
 };
 
-assert.throws( function() { printjson( t.group( p ) ) }, "write reduce didn't fail" );
+assert.throws( function() { return t.group( p ) }, "write reduce didn't fail" );
