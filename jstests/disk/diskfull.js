e5a49e306fd7bf199493ae469d3bb3636ab79cf3
Aaron <aaron@10gen.com>
2010-02-04 19:01:48 -0800

    SERVER-228 update test t now that we don't exit when disk is full

--- jstests/disk/diskfull.js
+++ jstests/disk/diskfull.js
@@ -15,6 +15,9 @@ if ( doIt ) {
     port = allocatePorts( 1 )[ 0 ];
     m = startMongoProgram( "mongod", "--port", port, "--dbpath", "/data/db/diskfulltest", "--nohttpinterface", "--bind_ip", "127.0.0.1" );
     m.getDB( "diskfulltest" ).getCollection( "diskfulltest" ).save( { a: 6 } );
-    assert.soon( function() { return rawMongoProgramOutput().match( /dbexit: really exiting now/ ); }, "didn't see 'really exiting now'" );
-    assert( !rawMongoProgramOutput().match( /Got signal/ ), "saw 'Got signal', not expected.  Output: " + rawMongoProgramOutput() );
+    assert.soon( function() { return rawMongoProgramOutput().match( /file allocation failure/ ); }, "didn't see 'file allocation failure'" );
+    assert.soon( function() { return rawMongoProgramOutput().match( /Caught Assertion in insert , continuing/ ); }, "didn't see 'Caught Assertion...'" );
+    sleep( 3000 );
+    m2 = new Mongo( m.host );
+    printjson( m2.getDBs() );
 }
