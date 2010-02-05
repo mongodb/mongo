db1f3a7914e18a79fe478c03141b09ea7c461436
Richard Kreuter <richard@10gen.com>
2010-02-05 09:47:21 -0500

    More changes to frob_version.py for python 2.5 MINOR

--- buildscripts/frob_version.py
+++ buildscripts/frob_version.py
@@ -7,7 +7,12 @@ import re
 import os
 
 def opentemp(basename):
-    return tempfile.NamedTemporaryFile('w', -1, ".XXXXXX", basename, '.', False)
+    # The following doesn't work in python before 2.6
+#    return tempfile.NamedTemporaryFile('w', -1, ".XXXXXX", basename, '.', False)
+    fname = basename +".TMP"
+    if os.path.exists(fname):
+        raise "not clobbering file %s" % fname
+    return open(fname, 'w')
 
 def frob_debian_changelog(version):
     fname = 'debian/changelog'
