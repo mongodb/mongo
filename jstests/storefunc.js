cc3f8770efcc761f55d1a1fe65881bcbebacd3db
Eliot Horowitz <eliot@10gen.com>
2010-02-06 10:47:30 -0500

    assert checking for _Id lookup

--- jstests/storefunc.js
+++ jstests/storefunc.js
@@ -12,6 +12,8 @@ assert.eq( 0 , s.count() , "setup - D" );
 s.save( { _id : "x" , value : "4" } );
 assert.eq( 1 , s.count() , "setup - E" );
 
+assert.eq( 4 , s.findOne( { _id : "x" } ).value , "E2 " );
+
 assert.eq( 4 , s.findOne().value , "setup - F" );
 s.update( { _id : "x" } , { $set : { value : 5  } } );
 assert.eq( 1 , s.count() , "setup - G" );
@@ -29,3 +31,5 @@ assert.eq( 6 , db.eval( "return x" ) , "exec - 2 " );
 
 s.insert( { _id : "bar" , value : function( z ){ return 17 + z; } } );
 assert.eq( 22 , db.eval( "return bar(5);"  ) , "exec - 3 " );
+
+
