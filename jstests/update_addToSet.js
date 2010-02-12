4685677e23eb23aed5053b72e7a23eb5f3f9408f
Eliot Horowitz <eliot@10gen.com>
2010-02-12 14:44:21 -0500

    put test back

--- jstests/update_addToSet.js
+++ jstests/update_addToSet.js
@@ -1,6 +1,5 @@
 
 t = db.update_addToSet1;
-/*
 t.drop();
 
 o = { _id : 1 , a : [ 2 , 1 ] }
@@ -14,7 +13,6 @@ assert.eq( o , t.findOne() , "A2" );
 
 t.update( {} , { $addToSet : { a : 3 } } );
 assert.eq( o , t.findOne() , "A3" );
-*/
 
 // SERVER-628
 // t.update( {} , { $addToSet : { a : { $each : [ 3 , 5 , 6 ] } } } );
