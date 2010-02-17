e2985bbc0d90028e88f061a9838f9334e3504118
Keith Bostic <keith@wiredtiger.com>
2010-02-17 11:37:41 +1100

    The magic number should be a bit pattern, not an integer, otherwise different architectures might store it differently.
    --HG--
    branch : keith

--- inc_posix/btree.h
+++ inc_posix/btree.h
@@ -70,7 +70,7 @@ struct __wt_page_hdr;		typedef struct __wt_page_hdr WT_PAGE_HDR;
  * field on an 8-byte boundary.
  */
 struct __wt_page_desc {
-#define	WT_BTREE_MAGIC		120897
+#define	WT_BTREE_MAGIC		0x120897
 	u_int32_t magic;		/* 00-03: Magic number */
 #define	WT_BTREE_MAJOR_VERSION	0
 	u_int16_t majorv;		/* 04-05: Major version */
