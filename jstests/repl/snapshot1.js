37949e561c1f3cda3271b4a7939f4995cbcefa46
Aaron <aaron@10gen.com>
2010-02-16 16:25:38 -0800

    SERVER-623 use ReplTest fwk for test

--- jstests/repl/snapshot1.js
+++ jstests/repl/snapshot1.js
@@ -5,26 +5,32 @@ ports = allocatePorts( 3 );
 var baseName = "jstests_disk_slavefromsnapshot";
 var basePath = "/data/db/" + baseName;
 
-var m = startMongod( "--master", "--port", ports[ 0 ], "--dbpath", basePath + "_master", "--nohttpinterface", "--oplogSize", "1" );
+rt1 = new ReplTest( "repl_snapshot1-1", [ ports[ 0 ], ports[ 1 ] ] );
+rt2 = new ReplTest( "repl_snapshot1-2", [ ports[ 0 ], ports[ 2 ] ] );
+m = rt1.start( true );
+
+//var m = startMongod( "--master", "--port", ports[ 0 ], "--dbpath", basePath + "_master", "--nohttpinterface", "--oplogSize", "1", "--smallfiles" );
 
 big = new Array( 2000 ).toString();
 for( i = 0; i < 1000; ++i )
     m.getDB( baseName )[ baseName ].save( { _id: new ObjectId(), i: i, b: big } );
 
 m.getDB( "admin" ).runCommand( {fsync:1,lock:1} );
-copyDbpath( basePath + "_master", basePath + "_slave1" );
+copyDbpath( rt1.getPath( true ), rt1.getPath( false ) );
 m.getDB( "admin" ).$cmd.sys.unlock.findOne();
 
-var s1 = startMongoProgram( "mongod", "--slave", "--source", "127.0.0.1:" + ports[ 0 ], "--port", ports[ 1 ], "--dbpath", basePath + "_slave1", "--nohttpinterface" );
+s1 = rt1.start( false, null, true );
+//var s1 = startMongoProgram( "mongod", "--slave", "--source", "127.0.0.1:" + ports[ 0 ], "--port", ports[ 1 ], "--dbpath", basePath + "_slave1", "--nohttpinterface", "--smallfiles"  );
 assert.eq( 1000, s1.getDB( baseName )[ baseName ].count() );
 m.getDB( baseName )[ baseName ].save( {i:1000} );
 assert.soon( function() { return 1001 == s1.getDB( baseName )[ baseName ].count(); } );
 
 s1.getDB( "admin" ).runCommand( {fsync:1,lock:1} );
-copyDbpath( basePath + "_slave1", basePath + "_slave2" );
+copyDbpath( rt1.getPath( false ), rt2.getPath( false ) );
 s1.getDB( "admin" ).$cmd.sys.unlock.findOne();
 
-var s2 = startMongoProgram( "mongod", "--slave", "--source", "127.0.0.1:" + ports[ 0 ], "--port", ports[ 2 ], "--dbpath", basePath + "_slave2", "--nohttpinterface" );
+s2 = rt2.start( false, null, true );
+//var s2 = startMongoProgram( "mongod", "--slave", "--source", "127.0.0.1:" + ports[ 0 ], "--port", ports[ 2 ], "--dbpath", basePath + "_slave2", "--nohttpinterface", "--smallfiles"  );
 assert.eq( 1001, s2.getDB( baseName )[ baseName ].count() );
 m.getDB( baseName )[ baseName ].save( {i:1001} );
 assert.soon( function() { return 1002 == s2.getDB( baseName )[ baseName ].count(); } );
