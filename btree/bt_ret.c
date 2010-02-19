3153a6de25a1c41caca73a6c40d5fbbfefdd532c
Keith Bostic <keith@wiredtiger.com>
2010-02-20 01:39:27 +1100

    Rework the DBT return code for clarity and to correctly set the caller's key/data DBT fields.
    --HG--
    branch : keith

--- btree/bt_ret.c
+++ btree/bt_ret.c
@@ -9,8 +9,6 @@
 
 #include "wt_internal.h"
 
-static int __wt_bt_dbt_copyout(WT_TOC *, DBT *, DBT *, u_int8_t *, u_int32_t);
-
 /*
  * __wt_bt_dbt_return --
  *	Retrun a WT_PAGE/WT_{ROW,COL}_INDX pair to the application.
@@ -29,14 +27,18 @@ __wt_bt_dbt_return(WT_TOC *toc,
 	WT_PAGE *ovfl_page;
 	WT_ROW_INDX *rip;
 	WT_SDBT *sdbt;
+	void *orig;
+	u_int32_t size;
 	int (*callback)(DB *, DBT *, DBT *), ret;
 
 	db = toc->db;
 	env = toc->env;
 	idb = db->idb;
 	cip = ip;
+	ovfl = NULL;
 	rip = ip;
 	callback = data->callback;
+	ret = 0;
 
 	/*
 	 * Handle the key item -- the key may be unchanged, in which case we
@@ -63,15 +65,20 @@ __wt_bt_dbt_return(WT_TOC *toc,
 	 * add cursor support).
 	 */
 	if (key_return) {
-		key = &toc->key;
-		if (WT_KEY_PROCESS(rip))
-			WT_RET(__wt_bt_key_process(toc, rip, key));
-		else if (callback == NULL) {
+		if (WT_KEY_PROCESS(rip)) {
+			WT_RET(__wt_bt_key_process(toc, rip, &toc->key));
+
+			key->data = toc->key.data;
+			key->size = toc->key.size;
+		} else if (callback == NULL) {
 			if (key->data_len < rip->size)
 				WT_RET(__wt_realloc(env,
 				    &key->data_len, rip->size, &key->data));
-			key->size = rip->size;
-			memcpy(key->data, rip->data, rip->size);
+			memcpy(toc->key.data, rip->data, rip->size);
+			toc->key.size = rip->size;
+
+			key->data = toc->key.data;
+			key->size = toc->key.size;
 		} else {
 			WT_CLEAR(local_key);
 			key = &local_key;
@@ -83,8 +90,8 @@ __wt_bt_dbt_return(WT_TOC *toc,
 	/*
 	 * Handle the data item.
 	 *
-	 * If it's been updated, it's easy, take the last data item, it's just
-	 * a byte string.
+	 * If the item was ever replaced, it's easy, take the last replacement
+	 * data item, it's just a byte string.
 	 */
 	switch (page->hdr->type) {
 	case WT_PAGE_DUP_LEAF:
@@ -108,49 +115,27 @@ repl:			if (sdbt->data == WT_DATA_DELETED)
 	WT_ILLEGAL_FORMAT(db);
 	}
 
-	/*
-	 * Copy the data item out.   In the case of a variable-length column-
-	 * or row-store leaf page, we have to get it from the page.
-	 */
 	switch (page->hdr->type) {
 	case WT_PAGE_COL_VAR:
 		item = cip->page_data;
-		if (callback != NULL &&
-		    WT_ITEM_TYPE(item) == WT_ITEM_DATA &&
-		    idb->huffman_data == NULL) {
-			WT_CLEAR(local_data);
-			data = &local_data;
-			data->data = WT_ITEM_BYTE(item);
-			data->size = WT_ITEM_LEN(item);
-			break;
-		}
-		if (WT_ITEM_TYPE(item) != WT_ITEM_DATA) {
-			ovfl = WT_ITEM_BYTE_OVFL(cip->page_data);
-			goto overflow;
-		}
-
-		WT_RET(__wt_bt_dbt_copyout(toc,
-		    data, &toc->data, WT_ITEM_BYTE(item), WT_ITEM_LEN(item)));
-		break;
+		goto item_set;
 	case WT_PAGE_ROW_LEAF:
 		item = rip->page_data;
-		if (callback != NULL &&
+item_set:	if (callback != NULL &&
 		    WT_ITEM_TYPE(item) == WT_ITEM_DATA &&
 		    idb->huffman_data == NULL) {
 			WT_CLEAR(local_data);
 			data = &local_data;
 			data->data = WT_ITEM_BYTE(item);
 			data->size = WT_ITEM_LEN(item);
-			break;
+			return (callback(db, key, data));
 		}
 
-		if (WT_ITEM_TYPE(item) != WT_ITEM_DATA) {
-			ovfl = WT_ITEM_BYTE_OVFL(rip->page_data);
-			goto overflow;
-		}
-
-		WT_RET(__wt_bt_dbt_copyout(toc,
-		    data, &toc->data, WT_ITEM_BYTE(item), WT_ITEM_LEN(item)));
+		if (WT_ITEM_TYPE(item) == WT_ITEM_DATA) {
+			orig = WT_ITEM_BYTE(item);
+			size = WT_ITEM_LEN(item);
+		} else
+			ovfl = WT_ITEM_BYTE_OVFL(item);
 		break;
 	case WT_PAGE_DUP_LEAF:
 		item = rip->page_data;
@@ -161,57 +146,40 @@ repl:			if (sdbt->data == WT_DATA_DELETED)
 			data = &local_data;
 			data->data = rip->data;
 			data->size = rip->size;
-			break;
+			return (callback(db, key, data));
 		}
 
-		if (WT_ITEM_TYPE(item) != WT_ITEM_DUP) {
-			ovfl = WT_ITEM_BYTE_OVFL(rip->page_data);
-			goto overflow;
-		}
-
-		WT_RET(__wt_bt_dbt_copyout(toc,
-		    data, &toc->data, rip->data, rip->size));
+		if (WT_ITEM_TYPE(item) == WT_ITEM_DUP) {
+			orig = rip->data;
+			size = rip->size;
+		} else
+			ovfl = WT_ITEM_BYTE_OVFL(item);
 		break;
 	WT_ILLEGAL_FORMAT(db);
 	}
 
-	if (0) {
-overflow:	WT_RET(__wt_bt_ovfl_in(toc, ovfl->addr, ovfl->len, &ovfl_page));
-		ret = __wt_bt_dbt_copyout(toc,
-		    data, &toc->data, WT_PAGE_BYTE(ovfl_page), ovfl->len);
-		WT_TRET(__wt_bt_page_out(toc, ovfl_page, 0));
-		if (ret != 0)
-			return (ret);
+	if (ovfl != NULL) {
+		WT_RET(__wt_bt_ovfl_in(toc, ovfl->addr, ovfl->len, &ovfl_page));
+		orig = WT_PAGE_BYTE(ovfl_page);
+		size = ovfl->len;
 	}
 
-	return (callback == NULL ? 0 : callback(db, key, data));
-}
-
-/*
- * __wt_bt_dbt_copyout --
- *	Do the actual allocation and copy for a returned DBT.
- */
-static int
-__wt_bt_dbt_copyout(
-    WT_TOC *toc, DBT *dbt, DBT *local_dbt, u_int8_t *p, u_int32_t size)
-{
-	ENV *env;
-	IDB *idb;
-
-	env = toc->env;
-	idb = toc->db->idb;
-
 	if (idb->huffman_data == NULL) {
-		if (local_dbt->data_len < size)
-			WT_RET(__wt_realloc(env,
-			    &local_dbt->data_len, size, &local_dbt->data));
-		local_dbt->size = size;
-		memcpy(local_dbt->data, p, size);
+		if (toc->data.data_len < size)
+			WT_ERR(__wt_realloc(
+			    env, &toc->data.data_len, size, &toc->data.data));
+		memcpy(toc->data.data, orig, size);
+		toc->data.size = size;
 	} else
-		 WT_RET(__wt_huffman_decode(idb->huffman_data, p, size,
-		     &local_dbt->data, &local_dbt->data_len, &local_dbt->size));
+		 WT_ERR(__wt_huffman_decode(idb->huffman_data, orig, size,
+		     &toc->data.data, &toc->data.data_len, &toc->data.size));
 
-	dbt->data = local_dbt->data;
-	dbt->size = local_dbt->size;
-	return (0);
+	data->data = toc->data.data;
+	data->size = toc->data.size;
+
+err:	if (ovfl != NULL)
+		WT_TRET(__wt_bt_page_out(toc, ovfl_page, 0));
+
+	return (ret != 0 ? ret :
+	    (callback == NULL ? 0 : callback(db, key, data)));
 }
