fd3a8e76e55678b42d654c908f87bee8ca572974
Eliot Horowitz <eliot@10gen.com>
2010-02-23 10:41:07 -0500

    2d gis index

---
+++ jstests/geo1.js
@@ -0,0 +1,48 @@
+
+t = db.geo1
+t.drop();
+
+idx = { loc : "2d" , zip : 1 }
+
+t.insert( { zip : "06525" , loc : [ 41.352964 , 73.01212  ] } )
+t.insert( { zip : "10024" , loc : [ 40.786387 , 73.97709 ] } )
+t.insert( { zip : "94061" , loc : [ 37.463911 , 122.23396 ] } )
+assert.isnull( db.getLastError() )
+
+// test "2d" has to be first
+assert.eq( 1 , t.getIndexKeys().length , "S1" );
+t.ensureIndex( { zip : 1 , loc : "2d" } );
+assert.eq( 1 , t.getIndexKeys().length , "S2" );
+
+t.ensureIndex( idx );
+assert.eq( 2 , t.getIndexKeys().length , "S3" );
+
+p = function( q , correct , name ){
+    print( name );
+    assert.eq( correct , t.find( q ).count() , "A1 " + name + " count" );
+    t.find( q ).hint( idx )._addSpecial( "$returnKey" , true ).forEach( 
+        function(z){
+            print( "\t" + z.loc + "\t" + z.zip );
+            assert.eq( 52 , z.loc.length , "A2" )
+        }
+    );
+}
+
+p( {} , 3 , "ALL" );
+p( { loc : /^1100011/ } , 2 , "north east" );
+
+
+
+assert.eq( 3 , t.count() , "B1" );
+t.insert( { loc : [ 200 , 200 ]  } )
+assert( db.getLastError() , "B2" )
+assert.eq( 3 , t.count() , "B3" );
+
+// test config options
+
+t.drop();
+
+t.ensureIndex( { loc : "2d" } , { min : -500 , max : 500 , bits : 4 } );
+t.insert( { loc : [ 200 , 200 ]  } )
+assert.isnull( db.getLastError() , "C1" )
+assert.eq( 8 , t.find( {} ).hint( { loc : "2d" } )._addSpecial( "$returnKey" , true ).next().loc.length , "C2" )
---
+++ jstests/geo2.js
@@ -0,0 +1,31 @@
+
+t = db.geo2
+t.drop();
+
+n = 1
+for ( var x=-100; x<100; x+=2 ){
+    for ( var y=-100; y<100; y+=2 ){
+        t.insert( { _id : n++ , loc : [ x , y ] } )
+    }
+}
+
+t.ensureIndex( { loc : "2d" } )
+
+fast = db.runCommand( { geo2d : t.getName() , near : [ 50 , 50 ] , num : 10 } );
+slow = db.runCommand( { geo2d : t.getName() , near : [ 50 , 50 ] , num : 10 , start : "11" } );
+
+assert.lt( fast.stats.nscanned * 10 , slow.stats.nscanned , "A1" );
+assert.lt( fast.stats.objectsLoaded , slow.stats.objectsLoaded , "A2" );
+assert.eq( fast.stats.avgDistance , slow.stats.avgDistance , "A3" );
+
+/*
+function p( z ){
+    print( z.dis + "\t" + z.obj.loc )
+}
+
+printjson( fast.stats )
+fast.results.forEach( p )
+printjson( slow.stats )
+slow.results.forEach( p )
+
+*/
