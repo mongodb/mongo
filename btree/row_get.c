498861e790de4c643200f2529a7d99340cdb5745
Keith Bostic <keith@wiredtiger.com>
2010-02-26 07:44:08 +1100

    Add support for insert (convert the WT_{ROW,COL}_INDX array into a balanced binary tree, and then insert into that tree.  This is barely working: more to the point, there's not yet any reconciliation of pages before they're written to disk, that is, the trees are never migrated back into pages. Add err & errx methods for the DB handle.
    --HG--
    branch : keith

--- btree/row_get.c
+++ btree/row_get.c
@@ -19,12 +19,14 @@ __wt_db_get(DB *db, WT_TOC *toc, DBT *key, DBT *pkey, DBT *data)
 	IDB *idb;
 	WT_PAGE *page;
 	WT_ROW_INDX *ip;
+	WT_SRCH srch;
 	u_int32_t type;
 	int ret;
 
 	WT_ASSERT(toc->env, pkey == NULL);		/* NOT YET */
 
 	idb = db->idb;
+	page = NULL;
 
 	WT_STAT_INCR(idb->stats, DB_READ_BY_KEY);
 
@@ -35,26 +37,37 @@ __wt_db_get(DB *db, WT_TOC *toc, DBT *key, DBT *pkey, DBT *data)
 	WT_TOC_DB_INIT(toc, db, "Db.get");
 
 	/* Search the btree for the key. */
-	WT_ERR(__wt_bt_search(toc, key, &page, &ip));
+	WT_ERR(__wt_bt_search_key_row(toc, key, &srch, 0));
+	page = srch.page;
+	ip = srch.indx;
 
 	/*
 	 * The Db.get method can only return single key/data pairs.
 	 * If that's not what we found, we're done.
+	 *
+	 * XXX
+	 * Checking if page_data is NULL isn't the right thing to do
+	 * here.   Re-visit this when we figure out how we handle
+	 * dup inserts into the tree.
 	 */
-	type = WT_ITEM_TYPE(ip->page_data);
-	if (type != WT_ITEM_DATA && type != WT_ITEM_DATA_OVFL) {
-		__wt_db_errx(db,
-		    "the Db.get method cannot return keys with duplicate "
-		    "data items; use the Db.cursor method instead");
-		ret = WT_ERROR;
-	} else
-		ret = __wt_bt_dbt_return(toc, key, data, page, ip, 0);
+	if (ip->page_data != NULL) {
+		type = WT_ITEM_TYPE(ip->page_data);
+		if (type != WT_ITEM_DATA && type != WT_ITEM_DATA_OVFL) {
+			__wt_api_db_errx(db,
+			    "the Db.get method cannot return keys with "
+			    "duplicate data items; use the Db.cursor method "
+			    "instead");
+			ret = WT_ERROR;
+			goto err;
+		}
+	}
+	ret = __wt_bt_dbt_return(toc, key, data, page, ip, 0);
 
 	/* Discard the page. */
-	if (page != idb->root_page)
+err:	if (page != NULL && page != idb->root_page)
 		WT_TRET(__wt_bt_page_out(toc, page, 0));
 
-err:	WT_TOC_DB_CLEAR(toc);
+	WT_TOC_DB_CLEAR(toc);
 
 	return (ret);
 }
