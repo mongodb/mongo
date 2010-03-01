4ada6f436d7794078f3fe22e6af7fb8d0e0695a4
Eliot Horowitz <eliot@10gen.com>
2010-02-28 22:23:11 -0500

    fix test

--- jstests/sharding/sync1.js
+++ jstests/sharding/sync1.js
@@ -1,11 +1,12 @@
 
 test = new SyncCCTest( "sync1" )
 
-t = test.conn.getDB( "test" ).sync1
+db = test.conn.getDB( "test" )
+t = db.sync1
 t.save( { x : 1 } )
 assert.eq( 1 , t.find().itcount() , "A1" );
 t.save( { x : 2 } )
-assert.eq( 1 , t.find().itcount() , "A2" );
+assert.eq( 2 , t.find().itcount() , "A2" );
 
 
 test.stop();
