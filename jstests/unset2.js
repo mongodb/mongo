6546599f73b490d8f8ad4bb54a27ac99e809e5fb
Aaron <aaron@10gen.com>
2010-03-02 22:42:44 -0800

    SERVER-652 checkpoint

--- jstests/unset2.js
+++ jstests/unset2.js
@@ -8,3 +8,8 @@ t.update( {}, {$unset:{"a.1":1}} );
 assert.eq( ["a","c"], t.findOne().a );
 t.update( {}, {$unset:{"a.0":1}} );
 assert.eq( ["c"], t.findOne().a );
+
+t.drop();
+t.save( {a:["a","b","c","d","e"]} );
+t.update( {}, {$unset:{"a.2":1},$set:{"a.3":3,"a.4":4,"a.5":5}} );
+assert.eq( ["a","b",3,4,5], t.findOne().a );
