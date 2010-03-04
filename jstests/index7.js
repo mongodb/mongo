e259b94f552c0e5e8325424dfc9eed5ea8ddb83a
Mathias Stearn <mathias@10gen.com>
2010-03-03 20:20:00 -0500

    reenable regex index test

--- jstests/index7.js
+++ jstests/index7.js
@@ -8,18 +8,16 @@ function noIndex( q ) {
     assert( q.explain().cursor.match( /^BasicCursor/ ) , "noIndex assert" );
 }
 
-function start( k, q ) {
-    var s = q.explain().indexBounds[0][0];
+function start( k, q, rev) {
+    var s = q.explain().indexBounds[rev?1:0][0];
     assert.eq( k.a, s.a );
     assert.eq( k.b, s.b );
 }
-
-function end( k, q ) {
-    var e = q.explain().indexBounds[0][1];
+function end( k, q, rev) {
+    var e = q.explain().indexBounds[rev?1:0][1];
     assert.eq( k.a, e.a );
     assert.eq( k.b, e.b );
 }
-
 function both( k, q ) {
     start( k, q );
     end( k, q );
@@ -42,14 +40,13 @@ assert.eq( 1, f.find( { a: 1, c: 1 } ).hint( { a: 1, b: 1 } ).explain().indexBou
 assert.eq( null, f.find( { a: 1, c: 1 } ).hint( { a: 1, b: 1 } ).explain().indexBounds[0][0].c );
 assert.eq( null, f.find( { a: 1, c: 1 } ).hint( { a: 1, b: 1 } ).explain().indexBounds[0][1].c );
 
-/* TODO: Find a way to test indexing with multiple intervals
 start( { a: "a", b: 1 }, f.find( { a: /^a/, b: 1 } ).hint( { a: 1, b: 1 } ) );
 start( { a: "a", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: 1, b: 1 } ).hint( { a: 1, b: 1 } ) );
-start( { a: "b", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: -1, b: -1 } ).hint( { a: 1, b: 1 } ) );
+start( { a: "b", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: -1, b: -1 } ).hint( { a: 1, b: 1 } ), true );
 start( { a: "a", b: 1 }, f.find( { b: 1, a: /^a/ } ).hint( { a: 1, b: 1 } ) );
 end( { a: "b", b: 1 }, f.find( { a: /^a/, b: 1 } ).hint( { a: 1, b: 1 } ) );
 end( { a: "b", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: 1, b: 1 } ).hint( { a: 1, b: 1 } ) );
-end( { a: "a", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: -1, b: -1 } ).hint( { a: 1, b: 1 } ) );
+end( { a: "a", b: 1 }, f.find( { a: /^a/, b: 1 } ).sort( { a: -1, b: -1 } ).hint( { a: 1, b: 1 } ), true );
 end( { a: "b", b: 1 }, f.find( { b: 1, a: /^a/ } ).hint( { a: 1, b: 1 } ) );
 
 start( { a: "z", b: 1 }, f.find( { a: /^z/, b: 1 } ).hint( { a: 1, b: 1 } ) );
@@ -57,7 +54,6 @@ end( { a: "{", b: 1 }, f.find( { a: /^z/, b: 1 } ).hint( { a: 1, b: 1 } ) );
 
 start( { a: "az", b: 1 }, f.find( { a: /^az/, b: 1 } ).hint( { a: 1, b: 1 } ) );
 end( { a: "a{", b: 1 }, f.find( { a: /^az/, b: 1 } ).hint( { a: 1, b: 1 } ) );
-*/
 
 both( { a: 1, b: 3 }, f.find( { a: 1, b: 3 } ).hint( { a: 1, b: 1 } ) );
 
