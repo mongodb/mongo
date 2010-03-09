a31ec892fe1f4c84fb0e445519bf3725004c35ae
Mike Dirolf <mike@10gen.com>
2010-03-09 15:36:54 -0500

    failing test case for SERVER-727

---
+++ jstests/copydb2.js
@@ -0,0 +1,17 @@
+a = db.getSisterDB( "copydb-test-a" );
+b = db.getSisterDB( "copydb-test-b" );
+
+a.dropDatabase();
+b.dropDatabase();
+
+a.foo.save( { a : 1 } );
+
+a.addUser( "chevy" , "chase" );
+
+assert.eq( 1 , a.foo.count() , "A" );
+assert.eq( 0 , b.foo.count() , "B" );
+
+// SERVER-727
+// a.copyDatabase( a._name , b._name , "" , "chevy" , "chase" );
+// assert.eq( 1 , a.foo.count() , "C" );
+// assert.eq( 1 , b.foo.count() , "D" );
