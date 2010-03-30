2c7c558cf8995874279a5b6362ac6e375491b5a4
Aaron <aaron@10gen.com>
2010-03-30 11:58:21 -0700

    SERVER-761 debug

--- jstests/dbhash.js
+++ jstests/dbhash.js
@@ -5,6 +5,12 @@ b = db.dbhashb;
 a.drop();
 b.drop();
 
+// debug SERVER-761
+db.getCollectionNames().forEach( function( x ) { 
+                                v = db[ x ].validate()
+                                assert( v.valid, x + " " + tojson( v ) );
+                                } );
+
 function gh( coll , mydb ){
     if ( ! mydb ) mydb = db;
     var x = mydb.runCommand( "dbhash" ).collections[coll.getName()];
