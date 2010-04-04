f59b5173eaea8e7c312eba3efb7de41376e3a18f
Eliot Horowitz <eliot@10gen.com>
2010-04-03 21:15:00 -0400

    listCommands command and db.listCommands helper

--- db/dbcommands_admin.cpp
+++ db/dbcommands_admin.cpp
@@ -376,7 +376,7 @@ namespace mongo {
         }
         
     } fsyncCmd;
-
+    
     class LogRotateCmd : public Command {
     public:
         LogRotateCmd() : Command( "logRotate" ){}
@@ -389,6 +389,35 @@ namespace mongo {
         }        
         
     } logRotateCmd;
+    
+    class ListCommandsCmd : public Command {
+    public:
+        ListCommandsCmd() : Command( "listCommands" ){}
+        virtual LockType locktype(){ return NONE; } 
+        virtual bool slaveOk(){ return true; }
+        virtual bool adminOnly(){ return false; }
+        virtual bool run(const char *ns, BSONObj& cmdObj, string& errmsg, BSONObjBuilder& result, bool fromRepl) {
+            BSONObjBuilder b( result.subobjStart( "commands" ) );
+            for ( map<string,Command*>::iterator i=_commands->begin(); i!=_commands->end(); ++i ){
+                Command * c = i->second;
+                BSONObjBuilder temp( b.subobjStart( c->name.c_str() ) );
+
+                {
+                    stringstream help;
+                    c->help( help );
+                    temp.append( "help" , help.str() );
+                }
+                temp.append( "lockType" , c->locktype() );
+                temp.append( "slaveOk" , c->slaveOk() );
+                temp.append( "adminOnly" , c->adminOnly() );
+                temp.done();
+            }
+            b.done();
+
+            return 1;
+        }        
+
+    } listCommandsCmd;
 
 }
 
