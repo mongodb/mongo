fda3c1a58d7e922443aae6adb8f94cecd63a3e44
Keith Bostic <keith@wiredtiger.com>
2010-04-06 20:58:58 +1000

    Change search routines to return a PAGE/INDX pair in the WT_TOC, not on the stack.
    --HG--
    branch : keith

--- btree/col_srch.c
+++ btree/col_srch.c
@@ -14,8 +14,7 @@
  *	Search a column-store tree for a specific record-based key.
  */
 int
-__wt_bt_search_recno_col(
-    WT_TOC *toc, u_int64_t recno, WT_PAGE **pagep, void *ipp)
+__wt_bt_search_recno_col(WT_TOC *toc, u_int64_t recno)
 {
 	IDB *idb;
 	WT_COL_INDX *ip;
@@ -26,6 +25,10 @@ __wt_bt_search_recno_col(
 
 	idb = toc->db->idb;
 
+	/* Check for a record past the end of the database. */
+	if (idb->root_page->records < recno)
+		return (WT_NOTFOUND);
+
 	if ((page = idb->root_page) == NULL)
 		return (WT_NOTFOUND);
 	isleaf = page->hdr->type == WT_PAGE_COL_VAR ? 1 : 0;
@@ -34,10 +37,10 @@ __wt_bt_search_recno_col(
 	for (record_cnt = 0;;) {
 		/* If it's a leaf page, return the page and index. */
 		if (isleaf) {
-			*pagep = page;
-			*(WT_COL_INDX **)ipp =
+			toc->srch_page = page;
+			toc->srch_ip =
 			    page->u.c_indx + ((recno - record_cnt) - 1);
-			return (0);
+			break;
 		}
 
 		/* Walk the page, counting records. */
@@ -58,4 +61,5 @@ __wt_bt_search_recno_col(
 		/* Get the next page. */
 		WT_RET(__wt_bt_page_in(toc, addr, isleaf, 1, &page));
 	}
+	return (0);
 }
