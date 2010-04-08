6378b9da4ce466f21dfda1bbe675b517d474fc58
Mathias Stearn <mathias@10gen.com>
2010-04-08 14:20:52 -0400

    Winsock startup at static init

--- util/sock.h
+++ util/sock.h
@@ -221,6 +221,8 @@ namespace mongo {
         struct sockaddr_storage sa;
     };
 
+    extern SockAddr unknownAddress; // ( "0.0.0.0", 0 )
+
     const int MaxMTU = 16384;
 
     inline string getHostName() {
