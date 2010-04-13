3cf2d2dcd0c91e5575ee981ed8f36bbb230625bd
Keith Bostic <keith@wiredtiger.com>
2010-04-14 01:51:50 +1000

    Replace "NOFLUSH" argument to DB.close with "NOWRITE" and "OSWRITE" flags to the DB.close, and new flag "OSWRITE" to the DB.sync method.
    --HG--
    branch : keith

--- btree/bt_sync.c
+++ btree/bt_sync.c
@@ -14,7 +14,7 @@
  *	Sync a Btree.
  */
 int
-__wt_bt_sync(WT_TOC *toc, void (*f)(const char *, u_int64_t))
+__wt_bt_sync(WT_TOC *toc, void (*f)(const char *, u_int64_t), u_int32_t flags)
 {
-	return (__wt_cache_sync(toc, f));
+	return (__wt_cache_sync(toc, f, flags));
 }
