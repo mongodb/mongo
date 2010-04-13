f967c47acc76fa8df43ce4e34e4ce011837dfc66
Keith Bostic <keith@wiredtiger.com>
2010-04-14 01:59:19 +1000

    Rename DB "name" field from "dbname" to "name". Statistics may not be initialized when {ENV,DB}.{err,errx} called, don't track.
    --HG--
    branch : keith

--- db/db_err.c
+++ db/db_err.c
@@ -17,7 +17,7 @@
 	if ((db)->errcall != NULL)					\
 		__wt_msg_call((void *)((db)->errcall),			\
 		    (void *)(db), (db)->errpfx,				\
-		    (db)->idb == NULL ? NULL : (db)->idb->dbname,	\
+		    (db)->idb == NULL ? NULL : (db)->idb->name,		\
 		    error, fmt, __ap);					\
 	va_end(__ap);							\
 									\
@@ -30,7 +30,7 @@
 									\
 	va_start(__ap, fmt);						\
 	__wt_msg_stream((db)->errfile, (db)->errpfx,			\
-	    (db)->idb == NULL ? NULL : (db)->idb->dbname,		\
+	    (db)->idb == NULL ? NULL : (db)->idb->name,			\
 	    error, fmt, __ap);						\
 	va_end(__ap);							\
 }
@@ -42,7 +42,10 @@
 void
 __wt_api_db_err(DB *db, int error, const char *fmt, ...)
 {
-	WT_STAT_INCR(db->env->ienv->method_stats, DB_ERR);
+	/*
+	 * This function may be called at before/after the statistics memory
+	 * has been allocated/freed; don't increment method statistics here.
+	 */
 	WT_DB_ERR(db, error, fmt);
 }
 
@@ -53,6 +56,9 @@ __wt_api_db_err(DB *db, int error, const char *fmt, ...)
 void
 __wt_api_db_errx(DB *db, const char *fmt, ...)
 {
-	WT_STAT_INCR(db->env->ienv->method_stats, DB_ERRX);
+	/*
+	 * This function may be called at before/after the statistics memory
+	 * has been allocated/freed; don't increment method statistics here.
+	 */
 	WT_DB_ERR(db, 0, fmt);
 }
