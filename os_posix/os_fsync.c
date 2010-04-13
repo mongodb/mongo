03ebf9a28a96a3ada5dba866893d58f9bf1aa75e
Keith Bostic <keith@wiredtiger.com>
2010-04-14 02:02:17 +1000

    Add an OS-level fsync call.
    --HG--
    branch : keith

---
+++ os_posix/os_fsync.c
@@ -0,0 +1,29 @@
+/*-
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2008 WiredTiger Software.
+ *	All rights reserved.
+ *
+ * $Id$
+ */
+
+#include "wt_internal.h"
+
+/*
+ * __wt_fsync --
+ *	Flush a file handle.
+ */
+int
+__wt_fsync(ENV *env, WT_FH *fh)
+{
+
+	WT_STAT_INCR(fh->stats, FSYNC);
+
+	WT_VERBOSE(env, WT_VERB_FILEOPS, (env, "fileops: %s: fsync", fh->name));
+
+	if (fsync(fh->fd) == 0)
+		return (0);
+
+	__wt_api_env_err(env, errno, "%s fsync error", fh->name);
+	return (WT_ERROR);
+}
