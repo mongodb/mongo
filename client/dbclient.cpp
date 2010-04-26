027dbd515b9880ddb769b591432a3918fd21f174
Eliot Horowitz <eliot@10gen.com>
2010-04-26 12:51:11 -0400

    throw exception in client if cursor doesn't exist on server SERVER-193

--- client/dbclient.cpp
+++ client/dbclient.cpp
@@ -818,12 +818,14 @@ namespace mongo {
     void DBClientCursor::dataReceived() {
         QueryResult *qr = (QueryResult *) m->data;
         resultFlags = qr->resultFlags();
+
         if ( qr->resultFlags() & QueryResult::ResultFlag_CursorNotFound ) {
             // cursor id no longer valid at the server.
             assert( qr->cursorId == 0 );
             cursorId = 0; // 0 indicates no longer valid (dead)
-            // TODO: should we throw a UserException here???
+            throw UserException( 13127 , "getMore: cursor didn't exist on server, possible restart or timeout?" );
         }
+
         if ( cursorId == 0 || ! ( opts & QueryOption_CursorTailable ) ) {
             // only set initially: we don't want to kill it on end of data
             // if it's a tailable cursor
