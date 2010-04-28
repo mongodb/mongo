e4e164d4295c81a67dfb689bfd741d0d95d26a0d
Eliot Horowitz <eliot@10gen.com>
2010-04-28 12:28:47 -0400

    changed semantics a bit

--- jstests/datasize.js
+++ jstests/datasize.js
@@ -5,7 +5,7 @@ assert.eq( 0, db.runCommand( {datasize:"test.jstests_datasize"} ).size );
 f.save( {qq:'c'} );
 assert.eq( 32, db.runCommand( {datasize:"test.jstests_datasize"} ).size );
 f.save( {qq:'fg'} );
-assert.eq( 65, db.runCommand( {datasize:"test.jstests_datasize"} ).size );
+assert.eq( 68, db.runCommand( {datasize:"test.jstests_datasize"} ).size );
 
 f.drop();
 f.ensureIndex( {qq:1} );
@@ -13,14 +13,14 @@ assert.eq( 0, db.runCommand( {datasize:"test.jstests_datasize"} ).size );
 f.save( {qq:'c'} );
 assert.eq( 32, db.runCommand( {datasize:"test.jstests_datasize"} ).size );
 f.save( {qq:'fg'} );
-assert.eq( 65, db.runCommand( {datasize:"test.jstests_datasize"} ).size );
+assert.eq( 68, db.runCommand( {datasize:"test.jstests_datasize"} ).size );
 
 assert.eq( 0, db.runCommand( {datasize:"test.jstests_datasize", min:{qq:'a'}} ).ok );
 
-assert.eq( 65, db.runCommand( {datasize:"test.jstests_datasize", min:{qq:'a'}, max:{qq:'z' }} ).size );
+assert.eq( 68, db.runCommand( {datasize:"test.jstests_datasize", min:{qq:'a'}, max:{qq:'z' }} ).size );
 assert.eq( 32, db.runCommand( {datasize:"test.jstests_datasize", min:{qq:'a'}, max:{qq:'d' }} ).size );
 assert.eq( 32, db.runCommand( {datasize:"test.jstests_datasize", min:{qq:'a'}, max:{qq:'d' }, keyPattern:{qq:1}} ).size );
-assert.eq( 33, db.runCommand( {datasize:"test.jstests_datasize", min:{qq:'d'}, max:{qq:'z' }, keyPattern:{qq:1}} ).size );
+assert.eq( 36, db.runCommand( {datasize:"test.jstests_datasize", min:{qq:'d'}, max:{qq:'z' }, keyPattern:{qq:1}} ).size );
 
 assert.eq( 0, db.runCommand( {datasize:"test.jstests_datasize", min:{qq:'c'}, max:{qq:'c' }} ).size );
 assert.eq( 32, db.runCommand( {datasize:"test.jstests_datasize", min:{qq:'c'}, max:{qq:'d' }} ).size );
