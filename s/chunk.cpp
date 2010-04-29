846453e82a517832bc7d52ad9b23c6ab547ade9e
Mathias Stearn <mathias@10gen.com>
2010-04-29 14:05:52 -0400

    Debugging for chunk not found error

--- s/chunk.cpp
+++ s/chunk.cpp
@@ -523,8 +523,14 @@ namespace mongo {
         ChunkMap::iterator it = _chunkMap.upper_bound(key);
         if (it != _chunkMap.end()){
             Chunk* c = it->second;
-            if ( c->contains( obj ) )
+            if ( c->contains( obj ) ){
                 return *c;
+            }else{
+                PRINT(it->first);
+                PRINT(*c);
+                PRINT(key);
+                massert(13141, "Chunk map pointed to incorrect chunk", false);
+            }
         }
 
         stringstream ss;
