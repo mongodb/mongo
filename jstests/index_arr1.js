c540e566de6c1e2a478f62beb66efbf65a5fba56
Eliot Horowitz <eliot@10gen.com>
2010-05-04 00:12:36 -0400

    fix array index when empty array SERVER-1082 Note: this only increase index keys, not decrease or change, so old indexes won't cause corruption

--- jstests/index_arr1.js
+++ jstests/index_arr1.js
@@ -2,9 +2,15 @@
 t = db.index_arr1
 t.drop()
 
-t.insert( { _id : 1 , a : 5 , b : [] } )
-assert.eq( 1 , t.find( { a : 5 } ).itcount() , "A" )
+t.insert( { _id : 1 , a : 5 , b : [ { x : 1 } ] } )
+t.insert( { _id : 2 , a : 5 , b : [] } )
+t.insert( { _id : 3 , a : 5  } )
+
+assert.eq( 3 , t.find( { a : 5 } ).itcount() , "A" )
 
 t.ensureIndex( { a : 1 , "b.x" : 1 } )
-t.insert( { _id : 1 , a : 5 , b : [] } )
-// assert.eq( 1 , t.find( { a : 5 } ).itcount() , "B" ); // SERVER-1082
+
+//t.find().sort( { a : 1 } )._addSpecial( "$returnKey" , 1 ).forEach( printjson )
+//t.find( { a : 5 } ).forEach( printjson )
+
+assert.eq( 3 , t.find( { a : 5 } ).itcount() , "B" ); // SERVER-1082
