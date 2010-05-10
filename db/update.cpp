4ea65d35986af7bdafdca7cc9ec6a67b24e63bf5
Eliot Horowitz <eliot@10gen.com>
2010-05-10 14:38:10 -0400

    $ operator and indexed fields SERVER-1055

--- db/update.cpp
+++ db/update.cpp
@@ -679,14 +679,14 @@ namespace mongo {
     
     class UpdateOp : public QueryOp {
     public:
-        UpdateOp() : _nscanned() {}
+        UpdateOp( bool hasPositionalField ) : _nscanned(), _hasPositionalField( hasPositionalField ){}
         virtual void init() {
             BSONObj pattern = qp().query();
             _c = qp().newCursor();
             if ( ! _c->ok() )
                 setComplete();
             else
-                _matcher.reset( new CoveredIndexMatcher( pattern, qp().indexKey() ) );
+                _matcher.reset( new CoveredIndexMatcher( pattern, qp().indexKey() , _hasPositionalField ) );
         }
         virtual void next() {
             if ( ! _c->ok() ) {
@@ -710,7 +710,7 @@ namespace mongo {
 
         virtual bool mayRecordPlan() const { return false; }
         virtual QueryOp *clone() const {
-            return new UpdateOp();
+            return new UpdateOp( _hasPositionalField );
         }
         
         shared_ptr< Cursor > c() { return _c; }
@@ -720,6 +720,7 @@ namespace mongo {
     private:
         shared_ptr< Cursor > _c;
         long long _nscanned;
+        bool _hasPositionalField;
         auto_ptr< CoveredIndexMatcher > _matcher;
         MatchDetails _details;
     };
@@ -756,7 +757,7 @@ namespace mongo {
         set<DiskLoc> seenObjects;
         
         QueryPlanSet qps( ns, patternOrig, BSONObj() );
-        UpdateOp original;
+        UpdateOp original( mods.get() && mods->hasDynamicArray() );
         shared_ptr< UpdateOp > u = qps.runOp( original );
         massert( 10401 ,  u->exceptionMessage(), u->complete() );
         
--- jstests/update_arraymatch4.js
+++ jstests/update_arraymatch4.js
@@ -2,17 +2,17 @@
 t = db.update_arraymatch4
 t.drop()
 
-x = { _id : 1 , cast : ["John","Hugh","Halle"] , movie:"Swordfish" }
+x = { _id : 1 , arr : ["A1","B1","C1"] }
 t.insert( x )
 assert.eq( x , t.findOne() , "A1" )
 
-x.cast[0] = "John Travolta"
-t.update( { cast : "John" } , { $set : { "cast.$" : "John Travolta" } } )
+x.arr[0] = "A2"
+t.update( { arr : "A1" } , { $set : { "arr.$" : "A2" } } )
 assert.eq( x , t.findOne() , "A2" )
 
-t.ensureIndex( { cast : 1 } )
-x.cast[0] = "xxx"
-t.update( { cast : "John Travolta" } , { $set : { "cast.$" : "xxx" } } )
-//assert.eq( x , t.findOne() , "A3" ); // SERVER-1055
+t.ensureIndex( { arr : 1 } )
+x.arr[0] = "A3"
+t.update( { arr : "A2" } , { $set : { "arr.$" : "A3" } } )
+assert.eq( x , t.findOne() , "A3" ); // SERVER-1055
 
 
