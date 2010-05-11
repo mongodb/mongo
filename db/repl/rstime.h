9ba63e65c8092005c1db6a73f4b9d956b0f84ab2
Dwight <dwight@10gen.com>
2010-05-11 16:13:44 -0400

    file

---
+++ db/repl/rstime.h
@@ -0,0 +1,13 @@
+// @file rstime.h
+
+#pragma once
+
+namespace mongo {
+
+    struct rstime { 
+        unsigned long long v;
+        unsigned server() const { return v >> (64-8); }
+        unsigned op() const { return v & 0xffffffffffffff; }
+    };
+
+}
