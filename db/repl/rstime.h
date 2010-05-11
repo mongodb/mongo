6aa22d5172783e139415603eef01048ace13f456
Dwight <dwight@10gen.com>
2010-05-11 16:30:14 -0400

    compile?

--- db/repl/rstime.h
+++ db/repl/rstime.h
@@ -7,7 +7,7 @@ namespace mongo {
     struct rstime { 
         unsigned long long v;
         unsigned server() const { return (unsigned) (v >> (64-8)); }
-        unsigned long long op() const { return v & 0xffffffffffffffL; }
+        unsigned long long op() const { return v & 0xffffffffffffffLL; }
     };
 
 }
