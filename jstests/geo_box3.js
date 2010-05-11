bf780fbbfff29af8030aa56ca19f41d20995f08c
Richard Kreuter <richard@10gen.com>
2010-05-11 15:10:48 -0400

    Fix test file for SERVER-994.

--- jstests/geo_box3.js
+++ jstests/geo_box3.js
@@ -2,6 +2,6 @@ t=db.geo_box3;
 
 t.drop();
 // SERVER-994
-db.foo.insert({ point : { x : -15, y : 10 } });
-db.foo.ensureIndex( { point : "2d" } , { min : -21 , max : 21 } );
-db.foo.find({point: {"$within": {"$box": [[-20, 7], [0, 15]]} } });
+t.insert({ point : { x : -15, y : 10 } });
+t.ensureIndex( { point : "2d" } , { min : -21 , max : 21 } );
+t.find({point: {"$within": {"$box": [[-20, 7], [0, 15]]} } });
