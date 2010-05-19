4af35d003327620ff2ca57b91e972b6a218f234e
Dwight <dwight@10gen.com>
2010-05-19 14:57:27 -0400

    or4.js

--- jstests/or4.js
+++ jstests/or4.js
@@ -8,11 +8,14 @@ t.save( {a:2} );
 t.save( {b:3} );
 t.save( {a:2,b:3} );
 
-assert.eq( 3, t.count( {$or:[{a:2},{b:3}]} ) );
-assert.eq( 2, t.count( {$or:[{a:2},{a:2}]} ) );
+assert.eq( 3, t.count( {$or:[{a:2},{b:3}]} ), 'a' );
+assert.eq( 2, t.count( {$or:[{a:2},{a:2}]} ), 'b' );
 
-t.remove( {$or:[{a:2},{b:3}]} );
-assert.eq( 0, t.count() );
+t.remove({ $or: [{ a: 2 }, { b: 3}] });
+
+print("count:" + t.count());
+
+assert.eq( 0, t.count(), 'count' );
 
 t.save( {a:2} );
 t.save( {b:3} );
@@ -22,3 +25,6 @@ t.update( {$or:[{a:2},{b:3}]}, {$set:{z:1}}, false, true );
 assert.eq( 3, t.count( {z:1} ) );
 
 assert.eq( 3, t.find( {$or:[{a:2},{b:3}]} ).toArray().length );
+
+
+
