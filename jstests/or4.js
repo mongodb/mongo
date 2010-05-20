ecfa20fafef05388f9be97079910480bdc886b4a
Aaron <aaron@10gen.com>
2010-05-20 11:29:57 -0700

    SERVER-109 test count skip/limit

--- jstests/or4.js
+++ jstests/or4.js
@@ -6,10 +6,21 @@ t.ensureIndex( {b:1} );
 
 t.save( {a:2} );
 t.save( {b:3} );
+t.save( {b:3} );
 t.save( {a:2,b:3} );
 
-assert.eq.automsg( "3", "t.count( {$or:[{a:2},{b:3}]} )" );
+assert.eq.automsg( "4", "t.count( {$or:[{a:2},{b:3}]} )" );
 assert.eq.automsg( "2", "t.count( {$or:[{a:2},{a:2}]} )" );
+
+assert.eq.automsg( "2", "t.find( {} ).skip( 2 ).count( true )" );
+assert.eq.automsg( "2", "t.find( {$or:[{a:2},{b:3}]} ).skip( 2 ).count( true )" );
+assert.eq.automsg( "1", "t.find( {$or:[{a:2},{b:3}]} ).skip( 3 ).count( true )" );
+
+assert.eq.automsg( "2", "t.find( {} ).limit( 2 ).count( true )" );
+assert.eq.automsg( "2", "t.find( {$or:[{a:2},{b:3}]} ).limit( 2 ).count( true )" );
+assert.eq.automsg( "3", "t.find( {$or:[{a:2},{b:3}]} ).limit( 3 ).count( true )" );
+assert.eq.automsg( "4", "t.find( {$or:[{a:2},{b:3}]} ).limit( 4 ).count( true )" );
+
 t.remove({ $or: [{ a: 2 }, { b: 3}] });
 assert.eq.automsg( "0", "t.count()" );
 
