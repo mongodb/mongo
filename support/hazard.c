b0fa514407ee9f86c6765d2474c0744d9331134d
Keith Bostic <keith@wiredtiger.com>
2010-05-21 01:26:33 +1000

    Add WT_CACHE_ENTRY state check to hazard reference acquisition, otherwise it's easy to mess up.
    --HG--
    branch : keith

--- support/hazard.c
+++ support/hazard.c
@@ -13,13 +13,32 @@
  * __wt_hazard_set --
  *	Set a hazard reference.
  */
-void
-__wt_hazard_set(WT_TOC *toc, WT_PAGE *page)
+int
+__wt_hazard_set(WT_TOC *toc, WT_CACHE_ENTRY *e, WT_PAGE *page)
 {
 	ENV *env;
 	WT_PAGE **hp;
 
 	env = toc->env;
+
+	/*
+	 * This code is called in two ways -- first, if the cache entry is set,
+	 * we're reading the cache and we want to get a reference to the page
+	 * the cache entry references.   If the cache entry is not set, we're
+	 * allocating a new page, and it needs to be referenced before it's put
+	 * into the cache.
+	 */
+	WT_ASSERT(env, e == NULL || page == NULL);
+	if (e != NULL) {
+		/*
+		 * If page is NULL, we raced (that is, we were looking at a
+		 * cache entry, everything matched, and as we retrieved the
+		 * page address from memory for a hazard reference, the page
+		 * disappeared).   That makes us done.
+		 */
+		if ((page = e->page) == NULL)
+			return (0);
+	}
 
 	WT_VERBOSE(env,
 	    WT_VERB_HAZARD, (env, "toc %p hazard %p: set", toc, page));
@@ -33,10 +52,23 @@ __wt_hazard_set(WT_TOC *toc, WT_PAGE *page)
 			 * volatile, so an explicit memory flush is necessary.
 			 */
 			WT_MEMORY_FLUSH;
-			return;
+
+			/*
+			 * Check to see if the cache entry is still valid.  If
+			 * it is, we're good to go, the cache server won't take
+			 * this block.   Else, the cache server did take this
+			 * block, we can't have it.
+			 */
+			if (e != NULL && e->state != WT_OK) {
+				*hp = NULL;
+				return (0);
+			}
+
+			return (1);
 		}
 	__wt_api_env_errx(env, "WT_TOC has no more hazard reference slots");
 	WT_ASSERT(env, hp < toc->hazard + env->hazard_size);
+	return (0);
 }
 
 /*
@@ -49,6 +81,16 @@ __wt_hazard_clear(WT_TOC *toc, WT_PAGE *page)
 	ENV *env;
 	WT_PAGE **hp;
 
+	/*
+	 * If page is NULL, we raced (that is, we were looking at a cache entry,
+	 * everything matched, and as we retrieved the page address from memory
+	 * for a hazard reference, the page disappeared).  It's OK -- we ignored
+	 * the request to get a hazard reference, and we can ignore the request
+	 * to clear the hazard reference.
+	 */
+	if (page == NULL)
+		return;
+
 	env = toc->env;
 
 	WT_VERBOSE(env,
@@ -59,11 +101,12 @@ __wt_hazard_clear(WT_TOC *toc, WT_PAGE *page)
 		if (*hp == page) {
 			*hp = NULL;
 			/*
-			 * We don't have to flush memory here for correctness,
-			 * but it gives the cache drain thread immediate access
-			 * to any page our reference blocks.
+			 * We don't have to flush memory here for correctness;
+			 * it would give the cache server thread faster access
+			 * to the block were the block selected to be drained,
+			 * but the generation number was just set which makes
+			 * it unlikely to be selected for draining.
 			 */
-			WT_MEMORY_FLUSH;
 			return;
 		}
 	__wt_api_env_errx(env, "WT_TOC hazard reference not found");
