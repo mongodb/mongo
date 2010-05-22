3de0890b5e13af0e0a4793665d2bb8f996db07b7
Dwight Merriman <dwight@10gen.com>
2010-05-22 16:23:09 -0400

    starting to fool with more indexes

--- jstests/index_many.js
+++ jstests/index_many.js
@@ -1,5 +1,6 @@
-t = db.many;
+/* test using lots of indexes on one collection */
 
+t = db.many;
 t.drop();
 db.many2.drop();
 
@@ -7,7 +8,7 @@ t.save({x:9});
 t.save({x:19});
 
 x = 2;
-while( x < 60 ) { 
+while( x < 70 ) { 
     patt={};
     patt[x] = 1;
     if( x == 20 )
@@ -17,18 +18,16 @@ while( x < 60 ) {
 }
 
 // print( tojson(db.getLastErrorObj()) );
-assert( db.getLastError(), "should have an error 'too many indexes'" );
+assert( db.getLastError(), "should have got an error 'too many indexes'" );
 
 // 40 is the limit currently
 
-// print( t.getIndexes().length == 40, "40" );
-
 assert( t.getIndexes().length == 40, "40" );
 
-assert( t.find({x:9}).length() == 1, "b" ) ;
+assert( t.find({x:9}).length() == 1, "b" );
+assert(t.find({ x: 9 }).explain().cursor.match(/Btree/), "not using index?");
 
+/* check that renamecollection remaps all the indexes right */
 t.renameCollection( "many2" );
-
-assert( t.find({x:9}).length() == 0, "c" ) ;
-
-assert( db.many2.find({x:9}).length() == 1, "d" ) ;
+assert( t.find({x:9}).length() == 0, "many2a" ) ;
+assert( db.many2.find({x:9}).length() == 1, "many2b" ) ;
