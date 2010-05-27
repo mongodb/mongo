188c57c4f57d927f3d53d31864a954deee4a3278
Keith Bostic <keith@wiredtiger.com>
2010-05-27 22:53:38 +1000

    Rename structure page_data fields to "key" and "data". Always default to stderr if no file handle specified.
    --HG--
    branch : keith

--- btree/bt_debug.c
+++ btree/bt_debug.c
@@ -161,6 +161,9 @@ __wt_bt_debug_desc(WT_PAGE *page, FILE *fp)
 {
 	WT_PAGE_DESC *desc;
 
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
+
 	desc = (WT_PAGE_DESC *)WT_PAGE_BYTE(page);
 	fprintf(fp, "\tdescription record: {\n"),
 	fprintf(fp, "\t\tmagic: %lu, major: %lu, minor: %lu\n",
@@ -204,10 +207,7 @@ __wt_bt_debug_inmem(WT_TOC *toc, WT_PAGE *page, char *ofile, FILE *fp)
 	fprintf(fp, "addr: %lu-%lu {\n", (u_long)page->addr,
 	    (u_long)page->addr + (WT_OFF_TO_ADDR(db, page->size) - 1));
 
-	/*
-	 * If we've created the binary tree, dump it, otherwise dump the
-	 * WT_{ROW,COL}_INDX array itself.
-	 */
+	/* Dump the WT_{ROW,COL}_INDX array. */
 	switch (page->hdr->type) {
 	case WT_PAGE_DUP_INT:
 	case WT_PAGE_DUP_LEAF:
@@ -244,8 +244,11 @@ __wt_bt_debug_col_indx(WT_TOC *toc, WT_COL_INDX *cip, FILE *fp)
 {
 	WT_CC_QUIET(toc, NULL);
 
-	if (cip->page_data != NULL)
-		fprintf(fp, "\tpage_data: %p", cip->page_data);
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
+
+	if (cip->data != NULL)
+		fprintf(fp, "\tdata: %p", cip->data);
 	if (cip->repl != NULL)
 		__wt_bt_debug_repl(cip->repl, fp);
 	fprintf(fp, "\n");
@@ -258,15 +261,18 @@ __wt_bt_debug_col_indx(WT_TOC *toc, WT_COL_INDX *cip, FILE *fp)
 static int
 __wt_bt_debug_row_indx(WT_TOC *toc, WT_ROW_INDX *rip, FILE *fp)
 {
-	if (!WT_KEY_PROCESS(rip))
-		__wt_bt_debug_dbt("\tkey", rip, fp);
-	else
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
+
+	if (WT_KEY_PROCESS(rip))
 		fprintf(fp, "\tkey: requires processing\n");
+	else
+		__wt_bt_debug_dbt("\tkey", rip, fp);
 	if (rip->repl != NULL)
 		__wt_bt_debug_repl(rip->repl, fp);
-	if (rip->page_data != NULL) {
+	if (rip->data != NULL) {
 		fprintf(fp, "\tdata: {");
-		WT_RET(__wt_bt_debug_item_data(toc, rip->page_data, fp));
+		WT_RET(__wt_bt_debug_item_data(toc, rip->data, fp));
 		fprintf(fp, "}");
 	}
 	fprintf(fp, "\n");
@@ -282,6 +288,9 @@ __wt_bt_debug_repl(WT_REPL *repl, FILE *fp)
 {
 	WT_SDBT *sdbt;
 	u_int32_t repl_cnt;
+
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
 
 	for (sdbt = repl->data, repl_cnt = 0;
 	    repl_cnt < repl->repl_next; ++sdbt, ++repl_cnt)
@@ -301,6 +310,9 @@ __wt_bt_debug_page_item(WT_TOC *toc, WT_PAGE *page, FILE *fp)
 	WT_ITEM *item;
 	u_int32_t i;
 
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
+
 	WT_ITEM_FOREACH(page, item, i)
 		WT_RET(__wt_bt_debug_item(toc, item, fp));
 	return (0);
@@ -316,6 +328,9 @@ __wt_bt_debug_item(WT_TOC *toc, WT_ITEM *item, FILE *fp)
 	DB *db;
 	WT_OFF *off;
 	WT_OVFL *ovfl;
+
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
 
 	db = toc->db;
 
@@ -358,6 +373,9 @@ __wt_bt_debug_page_col_int(WT_PAGE *page, FILE *fp)
 	WT_OFF *off;
 	u_int32_t i;
 
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
+
 	WT_OFF_FOREACH(page, off, i)
 		fprintf(fp, "\toffpage,  addr %lu, size %lu, records %llu\n",
 		    (u_long)off->addr,
@@ -374,6 +392,9 @@ __wt_bt_debug_page_col_fix(DB *db, WT_PAGE *page, FILE *fp)
 	IDB *idb;
 	u_int32_t i;
 	u_int8_t *p;
+
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
 
 	idb = db->idb;
 
@@ -406,6 +427,9 @@ __wt_bt_debug_item_data(WT_TOC *toc, WT_ITEM *item, FILE *fp)
 	u_int8_t *p;
 	void *hp;
 	int ret;
+
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
 
 	db = toc->db;
 	idb = db->idb;
@@ -463,6 +487,9 @@ void
 __wt_bt_debug_dbt(const char *tag, void *arg_dbt, FILE *fp)
 {
 	DBT *dbt;
+
+	if (fp == NULL)				/* Default to stderr */
+		fp = stderr;
 
 	/*
 	 * The argument isn't necessarily a DBT structure, but the first two
