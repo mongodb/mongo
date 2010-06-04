e79e21e2e90209d9e1626ba7ddcac188a8fd4c32
Richard Kreuter <richard@10gen.com>
2010-06-04 10:25:07 -0400

    Add license headers for some files.

--- bson/bsondemo/bsondemo.cpp
+++ bson/bsondemo/bsondemo.cpp
@@ -6,6 +6,22 @@
     Works headers only (the parts actually exercised herein that is - some functions require .cpp files).
 */
 
+/*
+ *    Copyright 2010 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 #include "../bson.h"
 #include <iostream>
 #include <vector>
