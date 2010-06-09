8b0b62887561ae1221d9ab8eb63ce04fc2b73126
Aaron <aaron@10gen.com>
2010-06-09 14:50:08 -0700

    SERVER-1201 test case

--- jstests/or7.js
+++ jstests/or7.js
@@ -5,3 +5,15 @@ t.ensureIndex( {a:1} );
 t.save( {a:2} );
 
 assert.eq.automsg( "1", "t.count( {$or:[{a:{$in:[1,3]}},{a:2}]} )" );
+
+t.remove();
+
+t.save( {a:"aa"} );
+t.save( {a:"ac"} );
+
+//SERVER-1201
+if ( 0 ) {
+printjson( t.find( {$or:[{a:/^ab/},{a:/^a/}]} ).explain() );
+assert.eq.automsg( "2", "t.count( {$or:[{a:/^ab/},{a:/^a/}]} )" );
+}
+
