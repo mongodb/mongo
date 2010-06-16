d3481595d2fedb3774574387c06ab70679aae447
Eliot Horowitz <eliot@10gen.com>
2010-06-15 21:28:52 -0400

    test i forgot to commit for SERVER-1149

---
+++ jstests/dbcase.js
@@ -0,0 +1,13 @@
+
+a = db.getSisterDB( "test_dbnamea" )
+b = db.getSisterDB( "test_dbnameA" )
+
+a.dropDatabase();
+b.dropDatabase();
+
+a.foo.save( { x : 1 } )
+assert.eq( 0 , db.getLastErrorObj().code || 0 , "A" )
+
+b.foo.save( { x : 1 } )
+assert.eq( 13297 , db.getLastErrorObj().code || 0 , "A" )
+
