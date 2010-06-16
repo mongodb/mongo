c5d975b2b52fb093922c2ee5f4661f82b3ffa2cc
Eliot Horowitz <eliot@10gen.com>
2010-06-15 21:28:26 -0400

    more tests for SERVER-857

--- jstests/objid6.js
+++ jstests/objid6.js
@@ -3,4 +3,13 @@ assert(o.getTimestamp);
 
 a = new ObjectId("4c17f616a707427266a2801a");
 b = new ObjectId("4c17f616a707428966a2801c");
-assert.eq(a.getTimestamp(), b.getTimestamp());
+assert.eq(a.getTimestamp(), b.getTimestamp() , "A" );
+
+
+a = new ObjectId()
+
+x = Math.floor( (new Date()).getTime() / 1000 ) 
+y = Math.floor( a.getTimestamp().getTime() / 1000 )
+
+assert.eq( x , y , "B" )
+
