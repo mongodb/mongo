bbc51275c0ecf6e9bbfb11a167355a572a42b9c3
Aaron <aaron@10gen.com>
2010-06-16 01:20:46 -0700

    SERVER-857 test

--- jstests/objid6.js
+++ jstests/objid6.js
@@ -5,11 +5,10 @@ a = new ObjectId("4c17f616a707427266a2801a");
 b = new ObjectId("4c17f616a707428966a2801c");
 assert.eq(a.getTimestamp(), b.getTimestamp() , "A" );
 
+x = Math.floor( (new Date()).getTime() / 1000 );
+a = new ObjectId();
+z = Math.floor( (new Date()).getTime() / 1000 );
+y = a.getTimestamp().getTime() / 1000;
 
-a = new ObjectId()
-
-x = Math.floor( (new Date()).getTime() / 1000 ) 
-y = Math.floor( a.getTimestamp().getTime() / 1000 )
-
-assert.eq( x , y , "B" )
-
+assert( x <= y , "B" );
+assert( y <= z , "C" );
