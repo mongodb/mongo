dfedac14db32629868a5ad1484290bec5c5e054f
Mathias Stearn <mathias@10gen.com>
2010-07-02 12:01:32 -0400

    squash warning

--- db/extsort.cpp
+++ db/extsort.cpp
@@ -221,7 +221,7 @@ namespace mongo {
         long length;
         _buf = (char*)_file.map( file.c_str() , length , MemoryMappedFile::SEQUENTIAL );
         massert( 10308 ,  "mmap failed" , _buf );
-        assert( (unsigned long)length == file_size( file ) );
+        assert( (uintmax_t)length == file_size( file ) );
         _end = _buf + length;
     }
     BSONObjExternalSorter::FileIterator::~FileIterator(){
