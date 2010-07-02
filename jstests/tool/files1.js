542d21c7007f8a52a3832628657241861c857dc9
Mathias Stearn <mathias@10gen.com>
2010-07-02 14:10:13 -0400

    fix for windows

--- jstests/tool/files1.js
+++ jstests/tool/files1.js
@@ -18,11 +18,7 @@ md5_computed = db.runCommand({filemd5: file_obj._id}).md5;
 assert.eq( md5 , md5_stored , "A 1" );
 assert.eq( md5 , md5_computed, "A 2" );
 
-try {
-    listFiles(t.ext);
-} catch (e) {
-    runProgram('mkdir', t.ext);
-}
+mkdir(t.ext);
 
 t.runTool( "files" , "-d" , t.baseName , "get" , filename , '-l' , t.extFile );
 md5 = md5sumFile(t.extFile);
