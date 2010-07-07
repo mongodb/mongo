e7cb7ecb5c8d17c622ca1f701a3ba31d04ff5c1a
Alberto Lerner <alerner@10gen.com>
2010-07-07 00:53:13 -0400

    SERVER-1270 A string class that can take both a strig and a char *. We'll apply next

--- util/stringutils.h
+++ util/stringutils.h
@@ -15,9 +15,30 @@
  *    limitations under the License.
  */
 
+#ifndef UTIL_STRING_UTILS_HEADER
+#define UTIL_STRING_UTILS_HEADER
+
 namespace mongo {
 
     void splitStringDelim( const string& str , vector<string>* res , char delim );
+
     void joinStringDelim( const vector<string>& strs , string* res , char delim );
 
+    struct StringData {
+        const char* data;
+        unsigned    size;
+
+        StringData( const char * c ){
+            data = c;
+            size = strlen(c);
+        }
+
+        StringData( const string& s ){
+            data = s.c_str();
+            size = s.size();
+        }
+    };
+   
 } // namespace mongo
+
+#endif // UTIL_STRING_UTILS_HEADER
