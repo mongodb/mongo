c6a3e6caf2c3a086e282c8b94c330dd8d42e20fb
Aaron <aaron@10gen.com>
2010-07-08 00:55:42 -0700

    SERVER-1159 forgot test

---
+++ jstests/in6.js
@@ -0,0 +1,13 @@
+t = db.jstests_in6;
+t.drop();
+
+t.save( {} );
+
+function doTest() {
+    assert.eq.automsg( "1", "t.count( {i:null} )" );
+    assert.eq.automsg( "1", "t.count( {i:{$in:[null]}} )" );
+}
+
+doTest();
+t.ensureIndex( {i:1} );
+doTest();
