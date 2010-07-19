fc1707aa992d39d2a65ee070c5e55b559f31061c
Aaron <aaron@10gen.com>
2010-07-19 00:39:16 -0700

    SERVER-1437 update or range elimination

--- jstests/or6.js
+++ jstests/or6.js
@@ -27,5 +27,5 @@ t.drop();
 t.ensureIndex( {a:1,b:1} );
 assert.eq.automsg( "2", "t.find( {$or:[{a:{$in:[1,2]},b:5}, {a:2,b:6}]} ).explain().clauses.length" );
 assert.eq.automsg( "2", "t.find( {$or:[{a:{$gt:1,$lte:2},b:5}, {a:2,b:6}]} ).explain().clauses.length" );
-assert.eq.automsg( "null", "t.find( {$or:[{a:{$gt:1,$lte:3},b:5}, {a:2,b:6}]} ).explain().clauses" );
+assert.eq.automsg( "2", "t.find( {$or:[{a:{$gt:1,$lte:3},b:5}, {a:2,b:6}]} ).explain().clauses.length" );
 assert.eq.automsg( "null", "t.find( {$or:[{a:{$in:[1,2]}}, {a:2}]} ).explain().clauses" );
