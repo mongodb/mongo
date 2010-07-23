5c44eec46df9ce3a7ce8f000f677ecb8bb7c2967
Keith Bostic <keith@wiredtiger.com>
2010-07-24 00:41:50 +1000

    Remove the whole FLIST (free-list) structure support, we're no longer freeing anything based on the API generation numbers.  I suspect that support will be back in a future version, though. Add per-thread memory (update structure), add "next power-of-two" function.
    --HG--
    branch : keith

---
+++ support/pow.c
@@ -0,0 +1,25 @@
+/*-
+ * See the file LICENSE for redistribution information.
+ *
+ * Copyright (c) 2008 WiredTiger Software.
+ *	All rights reserved.
+ *
+ * $Id$
+ */
+
+#include "wt_internal.h"
+
+/*
+ * nlpo2 --
+ *	Return the next-largest power-of-two for a 32-bit unsigned value.
+ */
+u_int32_t
+__wt_nlpo2(u_int32_t x)
+{
+	x |= (x >> 1);
+	x |= (x >> 2);
+	x |= (x >> 4);
+	x |= (x >> 8);
+	x |= (x >> 16);
+	return (x + 1);
+}
