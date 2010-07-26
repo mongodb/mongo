d3d978a60187894e6f148ea35a2c7ba2ab6c0345
Eliot Horowitz <eliot@10gen.com>
2010-07-26 17:03:14 -0400

    remove assert that isn't always valid

--- dbtests/jsobjtests.cpp
+++ dbtests/jsobjtests.cpp
@@ -303,7 +303,7 @@ namespace JsobjTests {
                     ASSERT(tmp.hasField("a"));
                     ASSERT(!tmp.hasField("b"));
                     ASSERT(tmp == BSON("a" << 1));
-
+                    
                     //force a realloc
                     BSONArrayBuilder arr;
                     for (int i=0; i < 10000; i++){
@@ -314,7 +314,6 @@ namespace JsobjTests {
                     ASSERT(obj.valid());
                     ASSERT(obj.hasField("a"));
                     ASSERT(obj.hasField("b"));
-                    ASSERT_NOT_EQUALS(obj.objdata() , tmp.objdata());
                 }
             }
         };
