7f05f937e823c9b9081defd39b2b480512c22dc0
Kristina Chodorow <k@ubuntu.(none)>
2010-07-27 18:36:44 -0400

    failing rs test SERVER-1500

---
+++ jstests/replsets/replsetarb1.js
@@ -0,0 +1,27 @@
+// FAILING TEST
+// no primary is ever elected if the first server is an arbiter
+
+doTest = function( signal ) {
+
+    var replTest = new ReplSetTest( {name: 'testSet', nodes: 3, bridged: true} );
+    var nodes = replTest.nodeList();
+
+    replTest.startSet();
+    replTest.initiate({"_id" : "unicomplex", 
+                "members" : [
+                             {"_id" : 0, "host" : nodes[0], "arbiterOnly" : true}, 
+                             {"_id" : 1, "host" : nodes[1]}, 
+                             {"_id" : 2, "host" : nodes[2]}]});
+
+    // one of these should be master
+
+    var m2 = replTest.nodes[1].runCommand({ismaster:1})
+    var m3 = replTest.nodes[2].runCommand({ismaster:1})
+
+    // but they aren't! FAIL
+    assert(m2.ismaster || m3.ismaster, 'a master exists');
+
+    replTest.stopSet( signal );
+}
+
+//doTest( 15 );
