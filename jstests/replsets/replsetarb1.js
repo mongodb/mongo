bc9ffb8e0dc857eb25d0ca2e4920acfad300d5e7
Dwight <dwight@10gen.com>
2010-07-29 13:45:20 -0400

    jstests

--- jstests/replsets/replsetarb1.js
+++ jstests/replsets/replsetarb1.js
@@ -6,12 +6,26 @@ doTest = function( signal ) {
     var replTest = new ReplSetTest( {name: 'testSet', nodes: 3} );
     var nodes = replTest.nodeList();
 
-    replTest.startSet();
-    replTest.node[0].initiate({"_id" : "unicomplex", 
+    print(tojson(nodes));
+    print("***");
+
+    print("******** start " );
+    var conns = replTest.startSet();
+    print("*************************************************** cons");
+    printjson(conns);
+    return;
+    print("******** initiate " );
+    sleep(3000);
+    var r =conns[0].initiate({"_id" : "unicomplex", 
                 "members" : [
                              {"_id" : 0, "host" : nodes[0], "arbiterOnly" : true}, 
                              {"_id" : 1, "host" : nodes[1]}, 
                              {"_id" : 2, "host" : nodes[2]}]});
+    print("******** 3 " );
+    sleep(2000);
+    printjson(r);
+    sleep(5000);
+    //print("RES: " + tojson(res));
 
     // one of these should be master
 
