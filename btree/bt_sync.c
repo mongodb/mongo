90aba1e1bde031b8f015e98c12d08a9400e6910b
Keith Bostic <keith@wiredtiger.com>
2010-07-30 23:26:19 +1000

    Don't flush databases that were never opened.
    --HG--
    branch : keith

--- btree/bt_sync.c
+++ btree/bt_sync.c
@@ -16,5 +16,11 @@
 int
 __wt_bt_sync(WT_TOC *toc, void (*f)(const char *, u_int64_t), u_int32_t flags)
 {
+	IDB *idb;
+
+	idb = toc->db->idb;
+	if (WT_UNOPENED_DATABASE(idb))
+		return (0);
+
 	return (__wt_cache_sync(toc, f, flags));
 }
