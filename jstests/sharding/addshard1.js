71382e7fad89c0187817e108055647ce9a92d2ba
Eliot Horowitz <eliot@10gen.com>
2010-07-31 02:18:56 -0400

    make test robust

--- jstests/sharding/addshard1.js
+++ jstests/sharding/addshard1.js
@@ -7,14 +7,18 @@ assert.eq( 1, s.config.shards.count(), "initial server count wrong" );
 conn1 = startMongodTest( 29000 );
 db1 = conn1.getDB( "testDB" );
 db1.foo.save( {a:1} );
+db1.getLastError()
 assert( s.admin.runCommand( { addshard: "localhost:29000" } ).ok, "did not accepted non-duplicated shard" );
 
 // a mongod with an existing database name should not be allowed to become a shard
 conn2 = startMongodTest( 29001 );
 db2 = conn2.getDB( "otherDB" );
 db2.foo.save( {a:1} );
+db2.getLastError()
 db3 = conn2.getDB( "testDB" );
 db3.foo.save( {a:1} );
+db3.getLastError()
+s.config.databases.find().forEach( printjson )
 assert( ! s.admin.runCommand( { addshard: "localhost:29001" } ).ok, "accepted mongod with duplicate db" );
 
 // check that all collection that were local to the mongod's are accessible through the mongos
