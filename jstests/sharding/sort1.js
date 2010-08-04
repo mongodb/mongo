51ac96b84420846af5dc6e5ca8d142e48834bbb8
Eliot Horowitz <eliot@10gen.com>
2010-08-04 11:50:15 -0400

    fix sorting if sort field not included in projection SERVER-1561

--- jstests/sharding/sort1.js
+++ jstests/sharding/sort1.js
@@ -6,8 +6,14 @@ s.adminCommand( { shardcollection : "test.data" , key : { num : 1 } } );
 
 db = s.getDB( "test" );
 
-for ( i=0; i<100; i++ ){
-    db.data.insert( { _id : i , num : i } );
+N = 100
+
+forward = []
+backward = [] 
+for ( i=0; i<N; i++ ){
+    db.data.insert( { _id : i , num : i , x : N - i } )
+    forward.push( i )
+    backward.push( ( N - 1 ) - i )
 }
 db.getLastError();
 
@@ -23,7 +29,7 @@ assert.eq( temp[0].shard , temp[2].shard , "A2" );
 assert.neq( temp[0].shard , temp[1].shard , "A3" );
 
 temp = db.data.find().sort( { num : 1 } ).toArray();
-assert.eq( 100 , temp.length , "B1" );
+assert.eq( N , temp.length , "B1" );
 for ( i=0; i<100; i++ ){
     assert.eq( i , temp[i].num , "B2" )
 }
@@ -39,6 +45,37 @@ assert.eq( 67 , z.length , "C2" )
 
 print( "a: " + a + " b:" + b + " mongos slow down: " + Math.ceil( 100 * ( ( a - b ) / b ) ) + "%" )
 
-//assert.lt( a , b * 1.3 , "C3 - speed test" );
+// -- secondary index sorting
+
+function getSorted( by , want , dir , proj ){
+    var s = {}
+    s[by] = dir || 1;
+    printjson( s )
+    var cur = db.data.find( {} , proj || {} ).sort( s )
+    return terse( cur.map( function(z){ return z[want]; } ) );
+}
+
+function terse( a ){
+    var s = "";
+    for ( var i=0; i<a.length; i++ ){
+        if ( i > 0 )
+            s += ",";
+        s += a[i];
+    }
+    return s;
+}
+
+forward = terse(forward);
+backward = terse(backward);
+
+assert.eq( forward , getSorted( "num" , "num" , 1 ) , "D1" )
+assert.eq( backward , getSorted( "num" , "num" , -1 ) , "D2" )
+
+assert.eq( backward , getSorted( "x" , "num" , 1 ) , "D3" )
+assert.eq( forward , getSorted( "x" , "num" , -1 ) , "D4" )
+
+assert.eq( backward , getSorted( "x" , "num" , 1 , { num : 1 } ) , "D5" )
+assert.eq( forward , getSorted( "x" , "num" , -1 , { num : 1 } ) , "D6" )
+
 
 s.stop();
