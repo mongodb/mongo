c9d23ed9f75f18c54bbfac5056424f240f85b816
Kyle Banker <kylebanker@gmail.com>
2010-08-09 11:09:07 -0400

    SERVER-1595 test fix: when removing a replset node, make sure that _id for nodes remains the same

--- jstests/replsets/replset_remove_node.js
+++ jstests/replsets/replset_remove_node.js
@@ -27,8 +27,13 @@ doTest = function( signal ) {
     // Remove that node from the configuration
     replTest.remove( slaveId );
 
-    // Then, reinitiate
-    replTest.reInitiate();
+    // Now, re-initiate
+    var c = master.getDB("local")['system.replset'].findOne();
+    var config  = replTest.getReplSetConfig();
+    config.version = c.version + 1;
+    config.members = [ { "_id" : 0, "host" : "arete.local:31000" },
+                       { "_id" : 2, "host" : "arete.local:31002" } ]
+    replTest.initiate( config , 'replSetReconfig' );
 
     // Make sure that a new master comes up
     master = replTest.getMaster();
