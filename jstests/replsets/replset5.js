c7e11b85c0d0d9decba51360aa878f6434750740
dwight <dwight@10gen.com>
2010-08-10 16:25:22 -0400

    rs test

--- jstests/replsets/replset5.js
+++ jstests/replsets/replset5.js
@@ -1,15 +1,16 @@
-doTest = function( signal ) {
+doTest = function (signal) {
 
     // Test getLastError defaults
-    var replTest = new ReplSetTest( {name: 'testSet', nodes: 3} );
+    var replTest = new ReplSetTest({ name: 'testSet', nodes: 3 });
 
     var nodes = replTest.startSet();
 
     // Initiate set with default for getLastError
     var config = replTest.getReplSetConfig();
-    config.getLastErrorDefaults = {'w': 3, 'wtimeout': 10000};
+    config.settings = {};
+    config.settings.getLastErrorDefaults = { 'w': 3, 'wtimeout': 10000 };
 
-    replTest.initiate( config );
+    replTest.initiate(config);
 
     //
     var master = replTest.getMaster();
@@ -17,18 +18,18 @@ doTest = function( signal ) {
     var testDB = "foo";
 
     // Initial replication
-    master.getDB(testDB).bar.save({a: 1});
+    master.getDB(testDB).bar.save({ a: 1 });
     replTest.awaitReplication();
 
     var slaves = replTest.liveNodes.slaves;
 
     // These writes should be replicated immediately
-    master.getDB(testDB).foo.insert({n: 1});
-    master.getDB(testDB).foo.insert({n: 2});
-    master.getDB(testDB).foo.insert({n: 3});
+    master.getDB(testDB).foo.insert({ n: 1 });
+    master.getDB(testDB).foo.insert({ n: 2 });
+    master.getDB(testDB).foo.insert({ n: 3 });
 
     // *** NOTE ***: The slaves have the data when I run this:
-    master.getDB("admin").runCommand({getlasterror: 1, w: 3, wtimeout: 5000});
+    master.getDB("admin").runCommand({ getlasterror: 1, w: 3, wtimeout: 5000 });
 
     // But when I run the test with no defaults, they don't:
     // master.getDB("admin").runCommand({getlasterror: 1});
@@ -53,23 +54,25 @@ doTest = function( signal ) {
     // Let's re-initialize the replica set with a new getlasterror
     var config = replTest.getReplSetConfig();
     var c = master.getDB("local")['system.replset'].findOne();
-    printjson( c );
-    config.getLastErrorDefaults = {'w': 5, 'wtimeout': 1000000};
+    printjson(c);
+    config.settings = {};
+    config.settings.getLastErrorDefaults = { 'w': 5, 'wtimeout': 1000000 };
     config.version = c.version + 1;
 
-
     print("Reinitiating");
-    replTest.initiate( config , 'replSetReconfig' );
+    replTest.initiate(config, 'replSetReconfig');
 
     var master = replTest.getMaster();
 
-    master.getDB(testDB).baz.insert({n: 3});
+    master.getDB(testDB).baz.insert({ n: 3 });
 
     // *** NOTE ***: This should never return given the timeout set above, but it returns right away.
-    master.getDB(testDB).runCommand({getlasterror: 1, w: 5, wtimeout: 5000000});
+    print("NOTE");
+    master.getDB(testDB).runCommand({ getlasterror: 1, w: 5, wtimeout: 5000000 });
+    print("NOTE2");
 
     // End test
-    replTest.stopSet( signal );
+    replTest.stopSet(signal);
 }
 
-// doTest( 15 );
+//doTest( 15 );
