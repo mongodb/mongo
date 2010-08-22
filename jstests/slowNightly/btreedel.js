414a062529ba37417f0be315a61b51a558682eb8
dwight <dwight@10gen.com>
2010-08-22 17:09:46 -0400

    new test

---
+++ jstests/slowNightly/btreedel.js
@@ -0,0 +1,39 @@
+// btreedel.js
+
+t = db.foo;
+t.remove({});
+
+for (var i = 0; i < 1000000; i++) {
+    t.insert({ _id: i, x: 'a                                                                               b' });
+}
+
+print("1 insert done count: " + t.count());
+
+var c = t.find().sort({ _id: 1 });
+for (var j = 0; j < 400000; j++) {
+    c.next();
+    if (j % 200000 == 0)
+        printjson(c.next());
+}
+printjson(c.next());
+
+print("2");
+
+t.remove({ _id: { $gt: 200000, $lt: 600000} });
+
+print("3");
+
+n = 0;
+last = {};
+printjson(c.next());
+while (c.hasNext()) {
+    n++;
+    last = c.next();
+}
+
+print("4. n:" + n);
+printjson(last);
+
+assert(n > 100000);
+
+print("btreedel.js success");
