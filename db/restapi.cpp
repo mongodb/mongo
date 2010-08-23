61e7998a22245a12ca579406427d345d6a7b8865
dwight <dwight@10gen.com>
2010-08-23 11:24:34 -0400

    clean up diags clientcursor after changes

--- db/restapi.cpp
+++ db/restapi.cpp
@@ -266,8 +266,8 @@ namespace mongo {
             
             ss << "# databases: " << dbHolder.size() << '\n';
             
-//            if( ClientCursor::byLocSize()>500 )
-//                ss << "Cursors byLoc.size(): " << ClientCursor::byLocSize() << '\n';
+            if( ClientCursor::numCursors()>500 )
+                ss << "# Cursors: " << ClientCursor::numCursors() << '\n';
             
             ss << "\nreplication: ";
             if( *replInfo )
