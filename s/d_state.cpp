9aac39189e67a0abcb84a2c311c981f435c3ffbd
Alberto Lerner <alerner@10gen.com>
2010-08-26 15:08:26 -0400

    SERVER-1633 turn logging up to uncover bottleneck

--- s/d_state.cpp
+++ s/d_state.cpp
@@ -336,6 +336,12 @@ namespace mongo {
         virtual LockType locktype() const { return WRITE; } // TODO: figure out how to make this not need to lock
  
         bool run(const string& , BSONObj& cmdObj, string& errmsg, BSONObjBuilder& result, bool){
+
+            // Debugging code for SERVER-1633. Commands have already a coarser timer for
+            // normal operation.
+            Timer timer;
+            vector<int> laps;
+
             lastError.disableForCommand();
             ShardedConnectionInfo* info = ShardedConnectionInfo::get( true );
 
@@ -364,6 +370,9 @@ namespace mongo {
                     configServer.init( configdb );
                 }
             }
+
+            // SERVER-1633
+            laps.push_back( timer.millis() );
             
             if ( cmdObj["shard"].type() == String ){
                 shardingState.gotShardName( cmdObj["shard"].String() );
@@ -387,6 +396,9 @@ namespace mongo {
                     }
                 }
             }
+
+            // SERVER-1633
+            laps.push_back( timer.millis() );
             
             unsigned long long version = extractVersion( cmdObj["version"] , errmsg );
 
@@ -414,6 +426,9 @@ namespace mongo {
                 return 1;
             }
 
+            // SERVER-1633
+            laps.push_back( timer.millis() );
+
             if ( version == 0 && globalVersion > 0 ){
                 if ( ! authoritative ){
                     result.appendBool( "need_authoritative" , true );
@@ -439,6 +454,9 @@ namespace mongo {
                 return false;
             }
             
+            // SERVER-1633
+            laps.push_back( timer.millis() );
+
             if ( version < globalVersion ){
                 while ( shardingState.inCriticalMigrateSection() ){
                     dbtemprelease r;
@@ -459,6 +477,9 @@ namespace mongo {
                 return false;
             }
 
+            // SERVER-1633
+            laps.push_back( timer.millis() );
+
             {
                 dbtemprelease unlock;
                 shardingState.getChunkMatcher( ns );
@@ -467,6 +488,15 @@ namespace mongo {
             result.appendTimestamp( "oldVersion" , oldVersion );
             oldVersion = version;
             globalVersion = version;
+
+            // SERVER-1633
+            ostringstream lapString;
+            lapString << name /* command name */ << " partials: " ;
+            for (size_t i = 1; i<laps.size(); ++i){ 
+                lapString << (laps[i] - laps[i-1]) / 1000 << " ";
+            }
+            lapString << endl;
+            logIfSlow( timer, lapString.str() );
 
             result.append( "ok" , 1 );
             return 1;
