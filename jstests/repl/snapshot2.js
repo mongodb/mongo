d92825979d60d6e05281bc2102608809d66f0c41
Dwight <dwight@10gen.com>
2010-08-26 09:50:22 -0400

    more logging snapshot2.js

--- jstests/repl/snapshot2.js
+++ jstests/repl/snapshot2.js
@@ -1,5 +1,7 @@
 // Test SERVER-623 - starting repl peer from a new snapshot of master
 
+print("snapshot2.js 1 -----------------------------------------------------------");
+
 ports = allocatePorts( 3 );
 
 var baseName = "repl_snapshot2";
@@ -7,21 +9,28 @@ var basePath = "/data/db/" + baseName;
 
 a = new MongodRunner( ports[ 0 ], basePath + "-arbiter" );
 l = new MongodRunner( ports[ 1 ], basePath + "-left", "127.0.0.1:" + ports[ 2 ], "127.0.0.1:" + ports[ 0 ] );
-r = new MongodRunner( ports[ 2 ], basePath + "-right", "127.0.0.1:" + ports[ 1 ], "127.0.0.1:" + ports[ 0 ] ); 
+r = new MongodRunner( ports[ 2 ], basePath + "-right", "127.0.0.1:" + ports[ 1 ], "127.0.0.1:" + ports[ 0 ] );
 
-rp = new ReplPair( l, r, a );
+print("snapshot2.js 2 -----------------------------------------------------------");
+
+rp = new ReplPair(l, r, a);
 rp.start();
+print("snapshot2.js 3 -----------------------------------------------------------");
 rp.waitForSteadyState();
+
+print("snapshot2.js 4 -----------------------------------------------------------");
 
 big = new Array( 2000 ).toString(); // overflow oplog, so test can't pass supriously
 rp.slave().setSlaveOk();
-for( i = 0; i < 500; ++i ) {
+print("snapshot2.js 5 -----------------------------------------------------------");
+for (i = 0; i < 500; ++i) {
     rp.master().getDB( baseName )[ baseName ].save( { _id: new ObjectId(), i: i, b: big } );
     if ( i % 250 == 249 ) {
         assert.soon( function() { return i+1 == rp.slave().getDB( baseName )[ baseName ].count(); } );    
         sleep( 10 ); // give master a chance to grab a sync point - have such small oplogs the master log might overflow otherwise
     }
 }
+print("snapshot2.js 6 -----------------------------------------------------------");
 
 rp.master().getDB( "admin" ).runCommand( {fsync:1,lock:1} );
 leftMaster = ( rp.master().host == rp.left().host );
@@ -48,4 +57,7 @@ rp.master().getDB( baseName )[ baseName ].save( {i:500} );
 assert.soon( function() { return 501 == rp.slave().getDB( baseName )[ baseName ].count(); } );
 
 assert( !rawMongoProgramOutput().match( /resync/ ) );
-assert( !rawMongoProgramOutput().match( /SyncException/ ) );
+assert(!rawMongoProgramOutput().match(/SyncException/));
+
+print("snapshot2.js SUCCESS ----------------");
+
