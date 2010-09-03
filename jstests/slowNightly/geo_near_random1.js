fe341ea9e06d7103cf4efbdaf30844582b02bcb5
Mathias Stearn <mathias@10gen.com>
2010-09-03 13:27:37 -0400

    Adjust times on geo_near_random tests

--- jstests/slowNightly/geo_near_random1.js
+++ jstests/slowNightly/geo_near_random1.js
@@ -3,7 +3,7 @@ load("jstests/libs/geo_near_random.js");
 
 var test = new GeoNearRandomTest("nightly.geo_near_random1");
 
-test.insertPts(1000);
+test.insertPts(200);
 
 test.testPt([0,0]);
 test.testPt(test.mkPt());
--- jstests/slowWeekly/geo_near_random2.js
+++ jstests/slowWeekly/geo_near_random2.js
@@ -1,9 +1,9 @@
 // this tests 1% of all points using $near and $nearSphere
 load("jstests/libs/geo_near_random.js");
 
-var test = new GeoNearRandomTest("nightly.geo_near_random2");
+var test = new GeoNearRandomTest("weekly.geo_near_random2");
 
-test.insertPts(10000);
+test.insertPts(50000);
 
 opts = {sphere:0, nToTest:test.nPts*0.01}; 
 test.testPt([0,0], opts);
