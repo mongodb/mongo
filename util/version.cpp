75a6688cd7821c4a19b48c7954b1dc21a761a0f9
Eliot Horowitz <eliot@10gen.com>
2010-09-28 10:55:37 -0400

    post 1.7.1

--- util/version.cpp
+++ util/version.cpp
@@ -14,7 +14,7 @@ namespace mongo {
     // mongo processes version support
     //
 
-    const char versionString[] = "1.7.1";
+    const char versionString[] = "1.7.2-pre-";
 
     string mongodVersion() {
         stringstream ss;
