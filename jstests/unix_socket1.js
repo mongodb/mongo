4e1692b74ff4b64b4f2180efc090d62f672a1751
Mathias Stearn <redbeard0531@gmail.com>
2010-09-28 21:55:53 -0700

    Fix unix socket parsing and add test SERVER-1867

---
+++ jstests/unix_socket1.js
@@ -0,0 +1,15 @@
+if ( ! _isWindows() ) {
+    hoststring = db.getMongo().host
+    index = hoststring.lastIndexOf(':')
+    if (index == -1){
+        port = '27017'
+    } else {
+        port = hoststring.substr(index + 1)
+    }
+
+    sock = new Mongo('/tmp/mongodb-' + port + '.sock')
+    sockdb = sock.getDB(db.getName())
+    assert( sockdb.runCommand('ping').ok )
+} else {
+    print("Not testing unix sockets on Windows");
+}
