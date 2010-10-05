8e21c18985786352355c89ffef4d4ee2bfabc290
dwight <dwight@10gen.com>
2010-10-04 20:09:41 -0400

    comments and LF fixes

--- db/dur.cpp
+++ db/dur.cpp
@@ -1,5 +1,27 @@
 // @file dur.cpp durability in the storage engine (crash-safeness / journaling)
 
+/* 
+   phases
+
+     PREPLOGBUFFER 
+       we will build an output buffer ourself and then use O_DIRECT
+       we could be in read lock for this
+       for very large objects write directly to redo log in situ?  will be faster.
+     WRITETOREDOLOG 
+       we could be unlocked (the main db lock that is...) for this, with sufficient care, but there is some complexity
+         have to handle falling behind which would use too much ram (going back into a read lock would suffice to stop that).
+         downgrading to (a perhaps upgradable) read lock would be a good start
+     CHECKPOINT
+       apply the writes back to the non-private MMF after they are for certain in redo log
+     REMAPPRIVATEVIEW
+       we could in a write lock quickly flip readers back to the main view, then stay in read lock and do our real 
+         remapping. with many files (e.g., 1000), remapping could be time consuming (several ms), so we don't want 
+         to be too frequent.  tracking time for this step would be wise.
+       there could be a slow down immediately after remapping as fresh copy-on-writes for commonly written pages will 
+         be required.  so doing these remaps more incrementally in the future might make sense - but have to be careful
+         not to introduce bugs.
+*/
+
 #include "pch.h"
 
 #if !defined(_DURABLE)
