8e923e738de38779652fcaaf507a4a692913c8c3
Dwight <dwight@10gen.com>
2010-10-08 14:30:56 -0400

    maxbsonobjectsize in isMaster command return

--- s/commands_admin.cpp
+++ s/commands_admin.cpp
@@ -786,6 +786,7 @@ namespace mongo {
             virtual bool run(const string& , BSONObj& cmdObj, string& errmsg, BSONObjBuilder& result, bool) {
                 result.append("ismaster", 1.0 );
                 result.append("msg", "isdbgrid");
+                result.appendNumber("maxBsonObjectSize", MaxBSONObjectSize);
                 return true;
             }
         } ismaster;
