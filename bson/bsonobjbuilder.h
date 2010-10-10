09d965b418586591c98dbcb9268a0f98b9572b88
Eliot Horowitz <eliot@10gen.com>
2010-10-09 21:57:57 -0400

    initial buffer size for BSONArrayBuilder

--- bson/bsonobjbuilder.h
+++ bson/bsonobjbuilder.h
@@ -643,6 +643,7 @@ namespace mongo {
     public:
         BSONArrayBuilder() : _i(0), _b() {}
         BSONArrayBuilder( BufBuilder &_b ) : _i(0), _b(_b) {}
+        BSONArrayBuilder( int initialSize ) : _i(0), _b(initialSize) {}
 
         template <typename T>
         BSONArrayBuilder& append(const T& x){
