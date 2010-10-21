a652b69e139ac11d3c4a9e0a1a9c5803a03528f7
Kristina Chodorow <kristina@10gen.com>
2010-10-21 15:49:21 -0400

    fix picky test

--- jstests/replsets/twosets.js
+++ jstests/replsets/twosets.js
@@ -5,18 +5,12 @@ doTest = function( signal ) {
 
     var orig = new ReplSetTest( {name: 'testSet', nodes: 3} );
     orig.startSet();
+    orig.initiate();
+    var master = orig.getMaster();
 
     var interloper = new ReplSetTest( {name: 'testSet', nodes: 3, startPort : 31003} );
     interloper.startSet();
-
-    sleep(5000);
-
-    orig.initiate();
     interloper.initiate();
-
-    sleep(5000);
-
-    var master = orig.getMaster();
 
     var conf = master.getDB("local").system.replset.findOne();
  
