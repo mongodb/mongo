7a83a6fb21c38689917581e786f96818291837c2
Eliot Horowitz <eliot@10gen.com>
2010-10-27 10:27:37 -0400

    cleaning cursor test and added assert

--- jstests/cursora.js
+++ jstests/cursora.js
@@ -14,13 +14,15 @@ function run( n , atomic ){
     join = startParallelShell( "sleep(50); db.cursora.remove( {"  + ( atomic ? "$atomic:true" : "" ) + "} ); db.getLastError();" );
     
     start = new Date()
-    num = t.find( function(){ num = 2; for ( var x=0; x<1000; x++ ) num += 2; return num > 0; } ).sort( { _id : -1 } ).limit(n).itcount()
+    ex = t.find( function(){ num = 2; for ( var x=0; x<1000; x++ ) num += 2; return num > 0; } ).sort( { _id : -1 } ).explain()
+    num = ex.n
     end = new Date()
-
+    
     join()
 
     print( "num: " + num + " time:" + ( end.getTime() - start.getTime() ) )
     assert.eq( 0 , t.count() , "after remove" )
+    assert.lt( 0 , ex.nYields , "not enough yields" )
     if ( n == num )
         print( "warning: shouldn't have counted all  n: " + n + " num: " + num );
 }
