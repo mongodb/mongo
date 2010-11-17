fcb5790e7528a101a49c52e442ded0e2b7a043d6
Eliot Horowitz <eliot@10gen.com>
2010-11-16 19:09:43 -0500

    BUMP 1.7.3

--- util/version.cpp
+++ util/version.cpp
@@ -14,7 +14,7 @@ namespace mongo {
     // mongo processes version support
     //
 
-    const char versionString[] = "1.7.3-pre-";
+    const char versionString[] = "1.7.3";
 
     string mongodVersion() {
         stringstream ss;
