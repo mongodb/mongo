cd692f0af1c4a0afe1625e2c91c402e9d298d730
Mathias Stearn <mathias@10gen.com>
2010-12-15 18:10:49 -0500

    indentation and braces

--- util/mmap_posix.cpp
+++ util/mmap_posix.cpp
@@ -115,19 +115,20 @@ namespace mongo {
     
     void* MemoryMappedFile::createPrivateMap() {
         void * x = mmap( /*start*/0 , len , PROT_READ|PROT_WRITE , MAP_PRIVATE , fd , 0 );        
-	    if( x == MAP_FAILED ) {
-	        if ( errno == ENOMEM ) {
-	            if( sizeof(void*) == 4 )
-		        error() << "mmap private failed with out of memory. You are using a 32-bit build and probably need to upgrade to 64" << endl;
-		    else
-  		        error() << "mmap private failed with out of memory. (64 bit build)" << endl;
-	        }
-	        return 0;
-	    }
+        if( x == MAP_FAILED ) {
+            if ( errno == ENOMEM ) {
+                if( sizeof(void*) == 4 ) {
+                    error() << "mmap private failed with out of memory. You are using a 32-bit build and probably need to upgrade to 64" << endl;
+                } else {
+                    error() << "mmap private failed with out of memory. (64 bit build)" << endl;
+                }
+            }
+            return 0;
+        }
         else { 
             views.push_back(x);
         }
-	    return x;
+        return x;
     }
     
     void MemoryMappedFile::flush(bool sync) {
