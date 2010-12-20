d3e7af93ea7924d81e81838a50621ced0f635ceb
Eliot Horowitz <eliot@10gen.com>
2010-12-20 16:25:04 -0500

    finish test for mongos routing slave ok reads to secondaries SERVER-1634

--- jstests/slowNightly/sharding_rs2.js
+++ jstests/slowNightly/sharding_rs2.js
@@ -4,13 +4,14 @@
 s = new ShardingTest( "rs2" , 2 , 1 , 1 , { rs : true , chunksize : 1 } )
 
 db = s.getDB( "test" )
+t = db.foo
 
 // -------------------------------------------------------------------------------------------
 // ---------- test that config server updates when replica set config changes ----------------
 // -------------------------------------------------------------------------------------------
 
 
-db.foo.save( { x : 17 } )
+db.foo.save( { _id : 5 ,x : 17 } )
 assert.eq( 1 , db.foo.count() );
 
 s.config.databases.find().forEach( printjson )
@@ -47,6 +48,13 @@ assert.soon(
         }
     } , "waiting for config server to update" , 180 * 1000 , 1000 );
 
+// cleanup after adding node
+for ( i=0; i<5; i++ ){
+    try {
+        db.foo.findOne();
+    }
+    catch ( e ){}
+}
 
 // -------------------------------------------------------------------------------------------
 // ---------- test routing to slaves ----------------
@@ -55,16 +63,16 @@ assert.soon(
 // --- not sharded ----
 
 m = new Mongo( s.s.name );
-t = m.getDB( "test" ).foo
+ts = m.getDB( "test" ).foo
 
 before = rs.test.getMaster().adminCommand( "serverStatus" ).opcounters
 
 for ( i=0; i<10; i++ )
-    assert.eq( 17 , t.findOne().x , "B1" )
+    assert.eq( 17 , ts.findOne().x , "B1" )
 
 m.setSlaveOk()
 for ( i=0; i<10; i++ )
-    assert.eq( 17 , t.findOne().x , "B2" )
+    assert.eq( 17 , ts.findOne().x , "B2" )
 
 after = rs.test.getMaster().adminCommand( "serverStatus" ).opcounters
 
@@ -75,34 +83,65 @@ assert.eq( before.query + 10 , after.query , "B3" )
 
 // --- sharded ----
 
-t.ensureIndex( { x : 1 } )
+db.foo.ensureIndex( { x : 1 } )
 
 for ( i=0; i<100; i++ ){
     if ( i == 17 ) continue;
-    t.insert( { x : i } )
+    db.foo.insert( { x : i } )
 }
 
-assert.eq( 100 , t.count() , "C1" )
+assert.eq( 100 , db.foo.count() , "C1" )
 
-/*
 s.adminCommand( { enablesharding : "test" } );
 s.adminCommand( { shardcollection : "test.foo" , key : { x : 1 } } );
 
 assert.eq( 100 , t.count() , "C2" )
-try {
-    s.adminCommand( { split : "test.foo" , middle : { x : 50 } } )
-}
-catch ( e ){
-    printjson( e );
-}
+s.adminCommand( { split : "test.foo" , middle : { x : 50 } } )
 
 db.printShardingStatus()
-throw 1
 
-other : s.config.shards.findOne( { _id : { $ne : serverName } } );
-s.adminCommand( { moveChunk : "test.foo" , find : { x : 10 } , to : other } )
+other = s.config.shards.findOne( { _id : { $ne : serverName } } );
+s.adminCommand( { moveChunk : "test.foo" , find : { x : 10 } , to : other._id } )
 assert.eq( 100 , t.count() , "C3" )
 
 assert.eq( 50 , rs.test.getMaster().getDB( "test" ).foo.count() , "C4" )
-*/
+
+// by non-shard key
+
+m = new Mongo( s.s.name );
+ts = m.getDB( "test" ).foo
+
+before = rs.test.getMaster().adminCommand( "serverStatus" ).opcounters
+
+for ( i=0; i<10; i++ )
+    assert.eq( 17 , ts.findOne( { _id : 5 } ).x , "D1" )
+
+m.setSlaveOk()
+for ( i=0; i<10; i++ )
+    assert.eq( 17 , ts.findOne( { _id : 5 } ).x , "D2" )
+
+after = rs.test.getMaster().adminCommand( "serverStatus" ).opcounters
+
+assert.eq( before.query + 10 , after.query , "D3" )
+
+// by shard key
+
+m = new Mongo( s.s.name );
+ts = m.getDB( "test" ).foo
+
+before = rs.test.getMaster().adminCommand( "serverStatus" ).opcounters
+
+for ( i=0; i<10; i++ )
+    assert.eq( 57 , ts.findOne( { x : 57 } ).x , "E1" )
+
+m.setSlaveOk()
+for ( i=0; i<10; i++ )
+    assert.eq( 57 , ts.findOne( { x : 57 } ).x , "E2" )
+
+after = rs.test.getMaster().adminCommand( "serverStatus" ).opcounters
+
+assert.eq( before.query + 10 , after.query , "E3" )
+
+
+
 s.stop()
