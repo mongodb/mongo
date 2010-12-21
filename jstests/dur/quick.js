14f1a87ddcf15fc487d6107a2d5f8ef119d71809
dwight <dwight@10gen.com>
2010-12-21 01:21:22 -0500

    test

--- jstests/dur/quick.js
+++ jstests/dur/quick.js
@@ -47,6 +47,19 @@ stopMongod(30001, /*signal*/9);
 
 // journal file should be present, and non-empty as we killed hard
 
+// we will force removal of a datafile to be sure we can recreate everythign
+// without it being present.
+removeFile(path2 + "/test.0");
+
+// with the file deleted, we MUST start from the beginning of the journal.
+// thus this check to be careful
+var files = listFiles(path2 + "/journal/");
+if (files.some(function (f) { return f.name.indexOf("lsn") >= 0; })) {
+    print(path2);
+    printjson(files);
+    assert(false, "a journal/lsn file is present which will make this test potentially fail.");
+}
+
 // restart and recover
 log();
 var conn = startMongodNoReset("--port", 30002, "--dbpath", path2, "--dur", "--durOptions", 8);
@@ -60,14 +73,18 @@ stopMongod(30002);
 
 // at this point, after clean shutdown, there should be no journal files
 log("check no journal files");
-assert(ls(path2 + "/journal") == null, "journal empty");
+var jfiles = listFiles(path2 + "/journal");
+if (jfiles.length) {
+    printjson(jfiles);
+    assert(jfiles.length == 0, "journal empty");
+}
 
 log("check data matches");
 var diff = run("diff", path1 + "/test.ns", path2 + "/test.ns");
-print(diff);
+print("diff .ns:" + diff);
 assert(diff == "", "error test.ns files differ");
 var diff = run("diff", path1 + "/test.0", path2 + "/test.0");
-print(diff);
+print("diff .0:" + diff);
 assert(diff == "", "error test.0 files differ");
 
 print("quick.js SUCCESS");
