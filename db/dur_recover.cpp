63cbf0d152ebbf26100d5a50c8a927338994a52b
Mathias Stearn <mathias@10gen.com>
2010-12-21 18:11:45 -0500

    move RecoveryJob out to header file

--- db/dur_recover.cpp
+++ db/dur_recover.cpp
@@ -19,6 +19,7 @@
 #include "pch.h"
 
 #include "dur.h"
+#include "dur_recover.h"
 #include "dur_journal.h"
 #include "dur_journalformat.h"
 #include "durop.h"
@@ -159,36 +160,6 @@ namespace mongo {
             const char *_lastDbName; // pointer into mmaped journal file
         };
 
-       /** call go() to execute a recovery from existing journal files.
-        */
-        class RecoveryJob : boost::noncopyable { 
-        public:
-            RecoveryJob() { _lastDataSyncedFromLastRun = 0; }
-            void go(vector<path>& files);
-            ~RecoveryJob();
-        private:
-            void applyEntry(const ParsedJournalEntry& entry, bool apply, bool dump);
-            void applyEntries(const vector<ParsedJournalEntry> &entries);
-            void processSection(const void *, unsigned len);
-            bool processFileBuffer(const void *, unsigned len);
-            bool processFile(path journalfile);
-            void close();
-
-            /** retrieve the mmap pointer for the specified dbName plus file number.
-                open if not yet open.
-                @param fileNo a value of -1 indicates ".ns"
-                @param ofs offset to add to the pointer before returning
-            */
-            void* ptr(const char *dbName, int fileNo, unsigned ofs);
-
-            // fileno,dbname -> map
-            map< pair<int,string>, void* > _fileToPtr;
-
-            // all close at end (destruction) of RecoveryJob
-            list< shared_ptr<MemoryMappedFile> > _files;
-
-            unsigned long long _lastDataSyncedFromLastRun;
-        };
         
         /** retrieve the mmap pointer for the specified dbName plus file number.
             open if not yet open.
---
+++ db/dur_recover.h
@@ -0,0 +1,41 @@
+// @file dur.h durability support
+
+#pragma once
+namespace mongo {
+    class MemoryMappedFile;
+
+    namespace dur {
+        class ParsedJournalEntry;
+
+       /** call go() to execute a recovery from existing journal files.
+        */
+        class RecoveryJob : boost::noncopyable { 
+        public:
+            RecoveryJob() { _lastDataSyncedFromLastRun = 0; }
+            void go(vector<path>& files);
+            ~RecoveryJob();
+        private:
+            void applyEntry(const ParsedJournalEntry& entry, bool apply, bool dump);
+            void applyEntries(const vector<ParsedJournalEntry> &entries);
+            void processSection(const void *, unsigned len);
+            bool processFileBuffer(const void *, unsigned len);
+            bool processFile(path journalfile);
+            void close();
+
+            /** retrieve the mmap pointer for the specified dbName plus file number.
+                open if not yet open.
+                @param fileNo a value of -1 indicates ".ns"
+                @param ofs offset to add to the pointer before returning
+            */
+            void* ptr(const char *dbName, int fileNo, unsigned ofs);
+
+            // fileno,dbname -> map
+            map< pair<int,string>, void* > _fileToPtr;
+
+            // all close at end (destruction) of RecoveryJob
+            list< shared_ptr<MemoryMappedFile> > _files;
+
+            unsigned long long _lastDataSyncedFromLastRun;
+        };
+    }
+}
