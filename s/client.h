54ebaf04cc49e585f5144e4ad7044f69b2afa72f
Eliot Horowitz <eliot@10gen.com>
2010-12-27 16:33:42 -0500

    comments/organization

--- s/client.h
+++ s/client.h
@@ -33,33 +33,59 @@ namespace mongo {
         ClientInfo( int clientId );
         ~ClientInfo();
         
-        string getRemote() const { return _remote; }
+        /** new request from client, adjusts internal state */
+        void newRequest( AbstractMessagingPort* p = 0 );
 
+        /** client disconnected */
+        void disconnect();
+        
+        /**
+         * @return remote socket address of the client
+         */
+        string getRemote() const { return _remote; }
+        
+        /**
+         * notes that this client use this shard 
+         * keeps track of all shards accessed this request
+         */
         void addShard( const string& shard );
+
+        /**
+         * gets shards used on the previous request
+         */
         set<string> * getPrev() const { return _prev; };
         
-        void newRequest( AbstractMessagingPort* p = 0 );
-        void disconnect();
+        /**
+         * gets all shards we've accessed since the last time we called clearSinceLastGetError
+         */
+        const set<string>& sinceLastGetError() const { return _sinceLastGetError; }
+        
+        /**
+         * clears list of shards we've talked to
+         */
+        void clearSinceLastGetError(){ _sinceLastGetError.clear(); }
         
         static ClientInfo * get( int clientId = 0 , bool create = true );
         static void disconnect( int clientId );
-        
-        const set<string>& sinceLastGetError() const { return _sinceLastGetError; }
-        void clearSinceLastGetError(){ 
-            _sinceLastGetError.clear(); 
-        }
 
     private:
         int _id;
         string _remote;
+        
+        // we use _a and _b to store shards we've talked to on the current request and the previous
+        // we use 2 so we can flip for getLastError type operations
 
-        set<string> _a;
-        set<string> _b;
-        set<string> * _cur;
-        set<string> * _prev;
+        set<string> _a; // actual set for _cur or _prev
+        set<string> _b; //   "
+
+        set<string> * _cur; // pointer to _a or _b depending on state
+        set<string> * _prev; //  ""
+        
+        
+        set<string> _sinceLastGetError; // all shards accessed since last getLastError
+        
         int _lastAccess;
         
-        set<string> _sinceLastGetError;
 
         static mongo::mutex _clientsLock;
         static Cache& _clients;
