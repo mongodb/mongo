00a4a0951db5e04b336fb37f797f004b477c0869
dwight <dwight@10gen.com>
2011-01-01 15:57:28 -0500

    try to make diagnosing easier

--- jstests/dur/dur_passthrough.js
+++ jstests/dur/dur_passthrough.js
@@ -12,29 +12,46 @@ files = files.sort(compareOn('name'));
 
 var runnerStart = new Date()
 
+function run(x) {
+    if (/[\/\\]_/.test(x.name) ||
+            !/\.js$/.test(x.name) ||
+            /repair/.test(x.name) ||
+            false // placeholder so all real tests end in ||
+        ) {
+        print("dur_passthrough.js >>>> skipping " + x.name);
+        return;
+    }
+
+    print();
+    print("dur_passthrough.js run " + x.name);
+    print();
+    print("         " + Date.timeFunc(function () { load(x.name); }, 1) + "ms");
+}
+
+// set this variable to debug by skipping to a specific test to start with and go from there
+// var skippingTo = /cursora/;
+var skippingTo = false;
+
 files.forEach(
     function (x) {
-
-        if (/[\/\\]_/.test(x.name) ||
-             !/\.js$/.test(x.name) ||
-             /repair/.test(x.name) ||
-             false // placeholder so all real tests end in ||
-           )
-        {
-            print(" >>>>>>>>>>>>>>> skipping " + x.name);
+        if (skippingTo && !skippingTo.test(x.name)) {
+            print("dur_passthrough.js temp skip " + x.name);
             return;
         }
-
-        print();
-        print(" *******************************************");
-        print("         Test : " + x.name + " ...");
-        print("                " + Date.timeFunc(function () { load(x.name); }, 1) + "ms");
-
+        skippingTo = false;
+        try {
+            run(x);
+        }
+        catch (e) {
+            print("\n\n\n\ndur_passthrough.js FAIL " + x.name + "\n\n\n\n");
+            throw e;
+        }
     }
 );
 
-stopMongod(30100);
+print("dur_passthrough.js stopMongod")
+stopMongod(30100)
 
 var runnerEnd = new Date()
 
-print( "total runner time: " + ( ( runnerEnd.getTime() - runnerStart.getTime() ) / 1000 ) + "secs" )
+print( "dur_passthrough.js total runner time: " + ( ( runnerEnd.getTime() - runnerStart.getTime() ) / 1000 ) + "secs" )
