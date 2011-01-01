72c0520ec95c472f5ea69401e77a1ac716a7f53f
dwight <dwight@10gen.com>
2011-01-01 18:19:40 -0500

    jstest shellkillop was broken

--- jstests/shellkillop.js
+++ jstests/shellkillop.js
@@ -2,17 +2,32 @@ baseName = "jstests_shellkillop";
 
 db[ baseName ].drop();
 
-for( i = 0; i < 100000; ++i ) {
+print("shellkillop.js insert data");
+for (i = 0; i < 100000; ++i) {
     db[ baseName ].save( {i:1} );
 }
 assert.eq( 100000, db[ baseName ].count() );
 
-spawn = startMongoProgramNoConnect( "mongo", "--autokillop", "--port", myPort(), "--eval", "db." + baseName + ".update( {}, {$set:{i:\"abcdefghijkl\"}}, false, true ); db." + baseName + ".count();" );
-sleep( 100 );
-stopMongoProgramByPid( spawn );
-sleep( 100 );
-inprog = db.currentOp().inprog
-printjson( inprog );
-for( i in inprog ) {
-    assert( inprog[ i ].ns != "test." + baseName, "still running op" );
+var evalStr = "print('SKO subtask started'); db." + baseName + ".update( {}, {$set:{i:'abcdefghijkl'}}, false, true ); db." + baseName + ".count();";
+print("shellkillop.js evalStr:" + evalStr);
+spawn = startMongoProgramNoConnect("mongo", "--autokillop", "--port", myPort(), "--eval", evalStr);
+
+sleep(100);
+
+stopMongoProgramByPid(spawn);
+
+sleep(200);
+var inprog = db.currentOp().inprog;
+if (inprog.count) {
+    sleep(2000); // wait more if need be
+    inprog = db.currentOp().inprog;
 }
+for( i in inprog ) {
+    if (inprog[i].ns == "test." + baseName) {
+        print("shellkillop.js FAIL op is still running:");
+        printjson(inprog);
+        assert(false, "still running op");
+    }
+}
+
+print("shellkillop.js SUCCESS");
