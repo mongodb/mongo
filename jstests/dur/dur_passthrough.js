74c81182182f29747a6127ad18ba73ed6866cc45
dwight <dwight@10gen.com>
2011-01-01 17:32:07 -0500

    variable name collisions were breaking dur_passthrough.js

--- jstests/dur/dur_passthrough.js
+++ jstests/dur/dur_passthrough.js
@@ -5,72 +5,78 @@
 conn = startMongodEmpty("--port", 30100, "--dbpath", "/data/db/dur_passthrough", "--dur", "--smallfiles");
 db = conn.getDB("test");
 
-var runnerStart = new Date()
+function durPassThrough() {
 
-var ran = {};
+    var runnerStart = new Date()
 
-/** run a test. won't run more than once. logs if fails and then throws.
-*/
-function run(x) {
-    function _run(x) {
-        if (/[\/\\]_/.test(x.name) ||
-                !/\.js$/.test(x.name) ||
-                /repair/.test(x.name) ||
-                false // placeholder so all real tests end in ||
-            ) {
-            print("dur_passthrough.js >>>> skipping " + x.name);
-            return;
+    var ran = {};
+
+    /** run a test. won't run more than once. logs if fails and then throws.
+    */
+    function run(x) {
+        function _run(x) {
+            if (/[\/\\]_/.test(x.name) ||
+                    !/\.js$/.test(x.name) ||
+                    /repair/.test(x.name) ||
+                    false // placeholder so all real tests end in ||
+                ) {
+                print("dur_passthrough.js >>>> skipping " + x.name);
+                return;
+            }
+            print();
+            print("dur_passthrough.js run " + x.name);
+            print("dur_passthrough.js end " + x.name + ' ' + Date.timeFunc(function () { load(x.name); }, 1) + "ms");
+            print();
         }
-        print();
-        print("dur_passthrough.js run " + x.name);
-        print("dur_passthrough.js end " + x.name + ' ' + Date.timeFunc(function () { load(x.name); }, 1) + "ms");
-        print();
+        if (ran[x.name])
+            return;
+        ran[x.name] = true;
+        try {
+            _run(x);
+        }
+        catch (e) {
+            print("\n\n\n\ndur_passthrough.js FAIL " + x.name + "\n\n\n");
+            throw e;
+        }
     }
-    if (ran[x.name])
-        return;
-    ran[x.name] = true;
-    try {
-        _run(x);
-    }
-    catch (e) {
-        print("\n\n\n\ndur_passthrough.js FAIL " + x.name + "\n\n\n");
-        throw e;
-    }
+
+    var files = listFiles("jstests");
+
+    // run something that will almost surely pass and is fast just to make sure our framework 
+    // here is really working
+    run({ name: 'jstests/basic1.js' });
+
+    // run "suspicious" tests early.  these are tests that have ever failed in buildbot.  we run them 
+    // early and try to get a fail fast
+    run({ name: 'jstests/shellstartparallel.js' });
+    run({ name: 'jstests/cursora.js' });
+
+    // run the shell-oriented tests early. if the shell is broken the other tests aren't meaningful
+    run({ name: 'jstests/run_program1.js' });
+    run({ name: 'jstests/shellspawn.js' });
+    run({ name: 'jstests/shellkillop.js' });
+
+    // set this variable to debug by skipping to a specific test to start with and go from there
+    // var skippingTo = /cursora/;
+    var skippingTo = false;
+
+    files = files.sort(compareOn('name'));
+    files.forEach(
+        function (x) {
+            if (skippingTo && !skippingTo.test(x.name)) {
+                print("dur_passthrough.js temp skip " + x.name);
+                return;
+            }
+            skippingTo = false;
+            run(x);
+        }
+    );
+
+    print("dur_passthrough.js stopMongod");
+    stopMongod(30100);
+    var runnerEnd = new Date();
+    print("dur_passthrough.js total runner time: " + ((runnerEnd.getTime() - runnerStart.getTime()) / 1000) + "secs")
 }
 
-var files = listFiles("jstests");
+durPassThrough();
 
-// run something that will almost surely pass and is fast just to make sure our framework 
-// here is really working
-run({ name: 'jstests/basic1.js' });
-
-// run "suspicious" tests early.  these are tests that have ever failed in buildbot.  we run them 
-// early and try to get a fail fast
-run({ name: 'jstests/shellstartparallel.js' });
-run({ name: 'jstests/cursora.js' });
-
-// run the shell-oriented tests early. if the shell is broken the other tests aren't meaningful
-run({ name: 'jstests/run_program1.js' });
-run({ name: 'jstests/shellspawn.js'});
-run({ name: 'jstests/shellkillop.js'});
-
-// set this variable to debug by skipping to a specific test to start with and go from there
-// var skippingTo = /cursora/;
-var skippingTo = false;
-
-files = files.sort(compareOn('name'));
-files.forEach(
-    function (x) {
-        if (skippingTo && !skippingTo.test(x.name)) {
-            print("dur_passthrough.js temp skip " + x.name);
-            return;
-        }
-        skippingTo = false;
-        run(x);
-    }
-);
-
-print("dur_passthrough.js stopMongod");
-stopMongod(30100);
-var runnerEnd = new Date();
-print( "dur_passthrough.js total runner time: " + ( ( runnerEnd.getTime() - runnerStart.getTime() ) / 1000 ) + "secs" )
