a638ff0d9030f7ca046c744f2e16141f3d7673e0
Aaron <aaron@10gen.com>
2011-01-03 11:42:01 -0800

    more failure debug info for shellstartparallel test

--- jstests/shellstartparallel.js
+++ jstests/shellstartparallel.js
@@ -8,11 +8,11 @@ db.sps.drop();
 join = startParallelShell("sleep(1000); db.sps.insert({x:1});");
 join();
 // print(db.sps.count()); // if it didn't work we might get zero here
-assert(db.sps.count() == 1, "join problem?");
+assert.eq(1, db.sps.count(), "join problem?");
 
 // test with a throw
 join = startParallelShell("db.sps.insert({x:1}); throw 'intentionally_uncaught';");
 join();
-assert(db.sps.count() == 2, "join2 problem?");
+assert.eq(2, db.sps.count(), "join2 problem?");
 
 print("shellstartparallel.js SUCCESS");
