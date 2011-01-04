ffccf20842b834c4a368ffe558cd977993221313
Aaron <aaron@10gen.com>
2011-01-04 14:41:25 -0800

    clear directory in disk/diskfull test, and clean test itself a little

--- jstests/disk/diskfull.js
+++ jstests/disk/diskfull.js
@@ -1,19 +1,25 @@
 doIt = false;
+dbpath = "/data/db/diskfulltest";
+
 files = listFiles( "/data/db" );
 for ( i in files ) {
-    if ( files[ i ].name == "/data/db/diskfulltest" ) {
+    if ( files[ i ].name == dbpath ) {
         doIt = true;
     }
 }
 
 if ( !doIt ) {
-    print( "path /data/db/diskfulltest/ missing, skipping diskfull test" );
+    print( "path " + dbpath + " missing, skipping diskfull test" );
     doIt = false;
 }
 
+// Clear dbpath without removing and recreating diskfulltest directory, as resetDbpath does
+files = listFiles( dbpath );
+files.forEach( function( x ) { removeFile( x.name ) } );
+
 if ( doIt ) {
     port = allocatePorts( 1 )[ 0 ];
-    m = startMongoProgram( "mongod", "--port", port, "--dbpath", "/data/db/diskfulltest", "--nohttpinterface", "--bind_ip", "127.0.0.1" );
+    m = startMongoProgram( "mongod", "--port", port, "--dbpath", dbpath, "--nohttpinterface", "--bind_ip", "127.0.0.1" );
     c = m.getDB( "diskfulltest" ).getCollection( "diskfulltest" )
     c.save( { a: 6 } );
     assert.soon( function() { return rawMongoProgramOutput().match( /file allocation failure/ ); }, "didn't see 'file allocation failure'" );
