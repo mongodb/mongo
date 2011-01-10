7003b4b5b6c3febaa136b1d489a21c32c3d71708
Dwight <dwight@10gen.com>
2011-01-10 12:56:26 -0500

    warning C4099: 'mongo::DbResponse' : type name first seen using 'struct' now seen using 'class'

--- s/d_logic.h
+++ s/d_logic.h
@@ -221,7 +221,7 @@ namespace mongo {
     /**
      * @return true if we took care of the message and nothing else should be done
      */
-    class DbResponse;
+    struct DbResponse;
     bool handlePossibleShardedMessage( Message &m, DbResponse * dbresponse );
 
     void logOpForSharding( const char * opstr , const char * ns , const BSONObj& obj , BSONObj * patt );
