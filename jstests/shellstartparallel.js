c0e021cbe7eeab18f28b02668e7889ee01c2d182
Aaron <aaron@10gen.com>
2011-01-10 10:57:37 -0800

    try to fix shellstartparallel failures

--- jstests/shellstartparallel.js
+++ jstests/shellstartparallel.js
@@ -5,13 +5,12 @@ assert.throws(f);
 
 // verify that join works
 db.sps.drop();
-join = startParallelShell("sleep(1000); db.sps.insert({x:1});");
+join = startParallelShell("sleep(1000); db.sps.insert({x:1}); db.getLastError();");
 join();
-// print(db.sps.count()); // if it didn't work we might get zero here
 assert.eq(1, db.sps.count(), "join problem?");
 
 // test with a throw
-join = startParallelShell("db.sps.insert({x:1}); throw 'intentionally_uncaught';");
+join = startParallelShell("db.sps.insert({x:1}); db.getLastError(); throw 'intentionally_uncaught';");
 join();
 assert.eq(2, db.sps.count(), "join2 problem?");
 
