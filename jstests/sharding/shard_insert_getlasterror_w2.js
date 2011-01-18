fd1addeab9a5e70e3aaf7ed5c66fd9451f512a1b
Eliot Horowitz <eliot@10gen.com>
2011-01-17 23:34:35 -0500

    indent

--- jstests/sharding/shard_insert_getlasterror_w2.js
+++ jstests/sharding/shard_insert_getlasterror_w2.js
@@ -6,70 +6,70 @@ load('jstests/libs/grid.js')
 
 function go() {
 
-var N = 2000
+    var N = 2000
 
-// ~1KB string
-var Text = ''
-for (var i = 0; i < 40; i++)
-    Text += 'abcdefghijklmnopqrstuvwxyz'
+    // ~1KB string
+    var Text = ''
+        for (var i = 0; i < 40; i++)
+            Text += 'abcdefghijklmnopqrstuvwxyz'
 
-// Create replica set with 3 servers
-var repset1 = new ReplicaSet('repset1', 3) .begin()
+    // Create replica set with 3 servers
+    var repset1 = new ReplicaSet('repset1', 3) .begin()
 
-// Add data to it
-var conn1a = repset1.getMaster()
-var db1a = conn1a.getDB('test')
-for (var i = 0; i < N; i++) {
-    db1a['foo'].insert({x: i, text: Text})
-    db1a.getLastError(2)  // wait to be copied to at least one secondary
-}
-
-// Create 3 sharding config servers
-var configsetSpec = new ConfigSet(3)
-var configsetConns = configsetSpec.begin()
-
-// Create sharding router (mongos)
-var routerSpec = new Router(configsetSpec)
-var routerConn = routerSpec.begin()
-var dba = routerConn.getDB('admin')
-var db = routerConn.getDB('test')
-
-// Add repset1 as only shard
-addShard (routerConn, repset1.getURL())
-
-// Enable sharding on test db and its collection foo
-enableSharding (routerConn, 'test')
-db['foo'].ensureIndex({x: 1})
-shardCollection (routerConn, 'test', 'foo', {x: 1})
-
-sleep(30000)
-printjson (db['foo'].stats())
-dba.printShardingStatus()
-printjson (db['foo'].count())
-
-// Add more data
-for (var i = N; i < 2*N; i++) {
-    db['foo'].insert({x: i, text: Text})
-    var x = db.getLastErrorObj(2, 30000)  // wait to be copied to at least one secondary
-    if (i % 30 == 0) print(i)
-    // if config is included, discard its error
-    if (x.shards && x.shards.length == 2) {
-        assert.eq(x.errs.length, 1);
-        assert.eq(x.err, "norepl");
+    // Add data to it
+    var conn1a = repset1.getMaster()
+    var db1a = conn1a.getDB('test')
+    for (var i = 0; i < N; i++) {
+        db1a['foo'].insert({x: i, text: Text})
+        db1a.getLastError(2)  // wait to be copied to at least one secondary
     }
-    else {
-        if (i % 30 == 0 || x.err != null) printjson(x);
-        assert.eq(x.err, null, tojson(x));
+
+    // Create 3 sharding config servers
+    var configsetSpec = new ConfigSet(3)
+    var configsetConns = configsetSpec.begin()
+
+    // Create sharding router (mongos)
+    var routerSpec = new Router(configsetSpec)
+    var routerConn = routerSpec.begin()
+    var dba = routerConn.getDB('admin')
+    var db = routerConn.getDB('test')
+
+    // Add repset1 as only shard
+    addShard (routerConn, repset1.getURL())
+
+    // Enable sharding on test db and its collection foo
+    enableSharding (routerConn, 'test')
+    db['foo'].ensureIndex({x: 1})
+    shardCollection (routerConn, 'test', 'foo', {x: 1})
+
+    sleep(30000)
+    printjson (db['foo'].stats())
+    dba.printShardingStatus()
+    printjson (db['foo'].count())
+
+    // Add more data
+    for (var i = N; i < 2*N; i++) {
+        db['foo'].insert({x: i, text: Text})
+        var x = db.getLastErrorObj(2, 30000)  // wait to be copied to at least one secondary
+            if (i % 30 == 0) print(i)
+        // if config is included, discard its error
+        if (x.shards && x.shards.length == 2) {
+            assert.eq(x.errs.length, 1);
+            assert.eq(x.err, "norepl");
+        }
+        else {
+            if (i % 30 == 0 || x.err != null) printjson(x);
+            assert.eq(x.err, null, tojson(x));
+        }
     }
-}
-// BUG: above getLastError fails on about every 170 inserts
+    // BUG: above getLastError fails on about every 170 inserts
 
-// Done
-routerSpec.end()
-configsetSpec.end()
-repset1.stopSet()
+    // Done
+    routerSpec.end()
+    configsetSpec.end()
+    repset1.stopSet()
 
-print('shard_insert_getlasterror_w2.js SUCCESS')
+    print('shard_insert_getlasterror_w2.js SUCCESS')
 }
 
 //Uncomment below to execute
