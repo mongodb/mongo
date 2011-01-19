19c5748e425fc07b4b724c856c110e07c2098b7a
dwight <dwight@10gen.com>
2011-01-19 10:45:38 -0500

    enhance test

--- jstests/dur/closeall.js
+++ jstests/dur/closeall.js
@@ -3,13 +3,21 @@
 
 function f() {
     var path = "/data/db/closeall";
+    var path2 = "/data/db/closeall_slave";
     var ourdb = "closealltest";
 
     print("closeall.js start mongod");
-    var options = Math.random() < 0.5 ? 24 : 0;
+    var options = (new Date()-0)%2==0 ? 8 : 0;
     print("closeall.js --durOptions " + options);
+    var N = 1000;
+    if (options) 
+        N = 300;
 
-    var conn = startMongodEmpty("--port", 30001, "--dbpath", path, "--dur", "--durOptions", options);
+    // use replication to exercise that code too with a close, and also to test local.sources with a close
+    var conn = startMongodEmpty("--port", 30001, "--dbpath", path, "--dur", "--durOptions", options, "--master", "--oplogSize", 16);
+    var connSlave = startMongodEmpty("--port", 30002, "--dbpath", path2, "--dur", "--durOptions", options, "--slave", "--source", "localhost:30001");
+
+    var slave = connSlave.getDB(ourdb);
 
     // we'll use two connections to make a little parallelism
     var db1 = conn.getDB(ourdb);
@@ -17,17 +25,21 @@ function f() {
 
     print("closeall.js run test");
 
-    for( var i = 0; i < 1000; i++ ) { 
+    for( var i = 0; i < N; i++ ) { 
     	db1.foo.insert({x:1}); // this does wait for a return code so we will get some parallelism
 	    if( i % 7 == 0 )
 	        db1.foo.insert({x:99, y:2});
 	    if( i %     49 == 0 )
-	        db1.foo.update({x:99},{a:1,b:2,c:3,d:4});
+	        db1.foo.update({ x: 99 }, { a: 1, b: 2, c: 3, d: 4 });
+	    if (i % 100 == 0)
+	        db1.foo.find();
 	    if( i == 800 )
 	        db1.foo.ensureIndex({x:1});
 	    var res = db2.adminCommand("closeAllDatabases");
 	    assert( res.ok, "closeAllDatabases res.ok=false");
-    }
+	}
+
+	print(slave.foo.count());
 }
 
 f();
