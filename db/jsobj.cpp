d782a78fc75721a4548e28ca3b2adba36470747e
Eliot Horowitz <eliot@10gen.com>
2011-01-20 17:06:20 -0500

    put Array check back but at 1M instead of 4k

--- db/jsobj.cpp
+++ db/jsobj.cpp
@@ -1402,6 +1402,7 @@ namespace mongo {
             const char *f = e.fieldName();
             try {
                 unsigned u = stringToNum(f);
+                assert( u < 1000000 );
                 if( u >= v.size() )
                     v.resize(u+1);
                 v[u] = e;
