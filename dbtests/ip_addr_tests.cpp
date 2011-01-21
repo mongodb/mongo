ef061ae0de67101fd44af11d5bd17a99f4f5f4a3
Daris Nevil <dnevil@venus.(none)>
2011-01-21 12:46:35 -0600

    Added IPv6 unit tests.

--- dbtests/ip_addr_tests.cpp
+++ dbtests/ip_addr_tests.cpp
@@ -69,6 +69,8 @@ namespace IPAddrTests {
         IP_Addr ip1;
     };
 
+    // -------- IPv4 Tests ---------
+
     class SimpleIPv4 : public Base {
     public:
         void run() {
@@ -171,6 +173,230 @@ namespace IPAddrTests {
     };
 
 
+    // -------- IPv6 Tests ---------
+
+    class SimpleIPv6 : public Base {
+    public:
+        void run() {
+            ASSERT( ip1.parseIPv6("::192.168.1.2") );
+            ASSERT( ip1.getByte(0) == 0 );
+            ASSERT( ip1.getByte(1) == 0 );
+            ASSERT( ip1.getByte(2) == 0 );
+            ASSERT( ip1.getByte(3) == 0 );
+            ASSERT( ip1.getByte(4) == 0 );
+            ASSERT( ip1.getByte(5) == 0 );
+            ASSERT( ip1.getByte(6) == 0 );
+            ASSERT( ip1.getByte(7) == 0 );
+            ASSERT( ip1.getByte(8) == 0 );
+            ASSERT( ip1.getByte(9) == 0 );
+            ASSERT( ip1.getByte(10) == 0 );
+            ASSERT( ip1.getByte(11) == 0 );
+            ASSERT( ip1.getByte(12) == 192 );
+            ASSERT( ip1.getByte(13) == 168 );
+            ASSERT( ip1.getByte(14) == 1 );
+            ASSERT( ip1.getByte(15) == 2 );
+            ASSERT( 128  == ip1.getNetmask() );
+
+            ASSERT( ip1.parseIPv6("1234::192.168.1.2") );
+            ASSERT( ip1.getByte(0) == 0x12 );
+            ASSERT( ip1.getByte(1) == 0x34 );
+            ASSERT( ip1.getByte(2) == 0 );
+            ASSERT( ip1.getByte(3) == 0 );
+            ASSERT( ip1.getByte(4) == 0 );
+            ASSERT( ip1.getByte(5) == 0 );
+            ASSERT( ip1.getByte(6) == 0 );
+            ASSERT( ip1.getByte(7) == 0 );
+            ASSERT( ip1.getByte(8) == 0 );
+            ASSERT( ip1.getByte(9) == 0 );
+            ASSERT( ip1.getByte(10) == 0 );
+            ASSERT( ip1.getByte(11) == 0 );
+            ASSERT( ip1.getByte(12) == 192 );
+            ASSERT( ip1.getByte(13) == 168 );
+            ASSERT( ip1.getByte(14) == 1 );
+            ASSERT( ip1.getByte(15) == 2 );
+        }
+    };
+
+    class MalformedSimpleIPv6 : public Base {
+    public:
+        void run() {
+            ASSERT( !ip1.parseIPv6("1234::192.") );
+            ASSERT( !ip1.parseIPv6("::192.168") );
+            ASSERT( !ip1.parseIPv6("::192.168.") );
+            ASSERT( !ip1.parseIPv6("::192.168.1") );
+            ASSERT( !ip1.parseIPv6("::192.168.1.") );
+        }
+    };
+
+    class OutOfBoundsIPv6 : public Base {
+    public:
+        void run() {
+            ASSERT( !ip1.parseIPv6("::255.1.1.1/129") );
+            ASSERT( !ip1.parseIPv6("::ABCDE") );
+            ASSERT( !ip1.parseIPv6("1:2:3:4:5:6:7:8:9") );
+            ASSERT( !ip1.parseIPv6("1::2::3") );
+        }
+    };
+
+    class ZeroCompressionIPv6 : public Base {
+    public:
+        void run() {
+            ASSERT( ip1.parseIPv6("1234::") );
+            ASSERT( ip1.getByte(0) == 0x12 );
+            ASSERT( ip1.getByte(1) == 0x34 );
+            ASSERT( ip1.getByte(2) == 0 );
+            ASSERT( ip1.getByte(3) == 0 );
+            ASSERT( ip1.getByte(4) == 0 );
+            ASSERT( ip1.getByte(5) == 0 );
+            ASSERT( ip1.getByte(6) == 0 );
+            ASSERT( ip1.getByte(7) == 0 );
+            ASSERT( ip1.getByte(8) == 0 );
+            ASSERT( ip1.getByte(9) == 0 );
+            ASSERT( ip1.getByte(10) == 0 );
+            ASSERT( ip1.getByte(11) == 0 );
+            ASSERT( ip1.getByte(12) == 0 );
+            ASSERT( ip1.getByte(13) == 0 );
+            ASSERT( ip1.getByte(14) == 0 );
+            ASSERT( ip1.getByte(15) == 0 );
+
+            ASSERT( ip1.parseIPv6("::1234") );
+            ASSERT( ip1.getByte(0) == 0 );
+            ASSERT( ip1.getByte(1) == 0 );
+            ASSERT( ip1.getByte(2) == 0 );
+            ASSERT( ip1.getByte(3) == 0 );
+            ASSERT( ip1.getByte(4) == 0 );
+            ASSERT( ip1.getByte(5) == 0 );
+            ASSERT( ip1.getByte(6) == 0 );
+            ASSERT( ip1.getByte(7) == 0 );
+            ASSERT( ip1.getByte(8) == 0 );
+            ASSERT( ip1.getByte(9) == 0 );
+            ASSERT( ip1.getByte(10) == 0 );
+            ASSERT( ip1.getByte(11) == 0 );
+            ASSERT( ip1.getByte(12) == 0 );
+            ASSERT( ip1.getByte(13) == 0 );
+            ASSERT( ip1.getByte(14) == 0x12 );
+            ASSERT( ip1.getByte(15) == 0x34 );
+        }
+    };
+
+    class NetmaskIPv6 : public Base {
+    public:
+        void run() {
+            ASSERT( ip1.parseIPv6("1::/8") );
+            ASSERT( ip1.getByte(0) == 0 );
+            ASSERT( ip1.getByte(1) == 1 );
+            ASSERT( ip1.getByte(2) == 0 );
+            ASSERT( ip1.getByte(3) == 0 );
+            ASSERT( ip1.getByte(4) == 0 );
+            ASSERT( ip1.getByte(5) == 0 );
+            ASSERT( ip1.getByte(6) == 0 );
+            ASSERT( ip1.getByte(7) == 0 );
+            ASSERT( ip1.getByte(8) == 0 );
+            ASSERT( ip1.getByte(9) == 0 );
+            ASSERT( ip1.getByte(10) == 0 );
+            ASSERT( ip1.getByte(11) == 0 );
+            ASSERT( ip1.getByte(12) == 0 );
+            ASSERT( ip1.getByte(13) == 0 );
+            ASSERT( ip1.getByte(14) == 0 );
+            ASSERT( ip1.getByte(15) == 0 );
+            ASSERT( ip1.getNetmask() == 8 );
+
+            ASSERT( ip1.parseIPv6("1234:5678:9abc:4321::/64") );
+            ASSERT( ip1.getByte(0) == 0x12 );
+            ASSERT( ip1.getByte(1) == 0x34 );
+            ASSERT( ip1.getByte(2) == 0x56 );
+            ASSERT( ip1.getByte(3) == 0x78 );
+            ASSERT( ip1.getByte(4) == 0x9a );
+            ASSERT( ip1.getByte(5) == 0xbc );
+            ASSERT( ip1.getByte(6) == 0x43 );
+            ASSERT( ip1.getByte(7) == 0x21 );
+            ASSERT( ip1.getByte(8) == 0 );
+            ASSERT( ip1.getByte(9) == 0 );
+            ASSERT( ip1.getByte(10) == 0 );
+            ASSERT( ip1.getByte(11) == 0 );
+            ASSERT( ip1.getByte(12) == 0 );
+            ASSERT( ip1.getByte(13) == 0 );
+            ASSERT( ip1.getByte(14) == 0 );
+            ASSERT( ip1.getByte(15) == 0 );
+            ASSERT( ip1.getNetmask() == 64 );
+
+            ASSERT( ip1.parseIPv6("1234:5678:9abc:4321:C000::/72") );
+            ASSERT( ip1.getByte(0) == 0x12 );
+            ASSERT( ip1.getByte(1) == 0x34 );
+            ASSERT( ip1.getByte(2) == 0x56 );
+            ASSERT( ip1.getByte(3) == 0x78 );
+            ASSERT( ip1.getByte(4) == 0x9a );
+            ASSERT( ip1.getByte(5) == 0xbc );
+            ASSERT( ip1.getByte(6) == 0x43 );
+            ASSERT( ip1.getByte(7) == 0x21 );
+            ASSERT( ip1.getByte(8) == 0xc0 );
+            ASSERT( ip1.getByte(9) == 0 );
+            ASSERT( ip1.getByte(10) == 0 );
+            ASSERT( ip1.getByte(11) == 0 );
+            ASSERT( ip1.getByte(12) == 0 );
+            ASSERT( ip1.getByte(13) == 0 );
+            ASSERT( ip1.getByte(14) == 0 );
+            ASSERT( ip1.getByte(15) == 0 );
+            ASSERT( ip1.getNetmask() == 72 );
+        }
+    };
+
+    class AllZeroesIPv6 : public Base {
+    public:
+        void run() {
+            ASSERT( ip1.parseIPv6("::") );
+            ASSERT( ip1.getByte(0) == 0 );
+            ASSERT( ip1.getByte(1) == 0 );
+            ASSERT( ip1.getByte(2) == 0 );
+            ASSERT( ip1.getByte(3) == 0 );
+            ASSERT( ip1.getByte(4) == 0 );
+            ASSERT( ip1.getByte(5) == 0 );
+            ASSERT( ip1.getByte(6) == 0 );
+            ASSERT( ip1.getByte(7) == 0 );
+            ASSERT( ip1.getByte(8) == 0 );
+            ASSERT( ip1.getByte(9) == 0 );
+            ASSERT( ip1.getByte(10) == 0 );
+            ASSERT( ip1.getByte(11) == 0 );
+            ASSERT( ip1.getByte(12) == 0 );
+            ASSERT( ip1.getByte(13) == 0 );
+            ASSERT( ip1.getByte(14) == 0 );
+            ASSERT( ip1.getByte(15) == 0 );
+            ASSERT( ip1.getNetmask() == 128 );
+        }
+    };
+
+    class MiddleZeroesIPv6 : public Base {
+    public:
+        void run() {
+            ASSERT( ip1.parseIPv6("fe80::222:19ff:fe5d:a133") );
+            ASSERT( ip1.getByte(0) == 0xfe );
+            ASSERT( ip1.getByte(1) == 0x80 );
+            ASSERT( ip1.getByte(2) == 0 );
+            ASSERT( ip1.getByte(3) == 0 );
+            ASSERT( ip1.getByte(4) == 0 );
+            ASSERT( ip1.getByte(5) == 0 );
+            ASSERT( ip1.getByte(6) == 0 );
+            ASSERT( ip1.getByte(7) == 0 );
+            ASSERT( ip1.getByte(8) == 0x02 );
+            ASSERT( ip1.getByte(9) == 0x22 );
+            ASSERT( ip1.getByte(10) == 0x19 );
+            ASSERT( ip1.getByte(11) == 0xff );
+            ASSERT( ip1.getByte(12) == 0xfe );
+            ASSERT( ip1.getByte(13) == 0x5d );
+            ASSERT( ip1.getByte(14) == 0xa1 );
+            ASSERT( ip1.getByte(15) == 0x33 );
+            ASSERT( ip1.getNetmask() == 128 );
+        }
+    };
+
+    class OtherInvalidIPv6 : public Base {
+    public:
+        void run() {
+            ASSERT( !ip1.parseIPv6("") );
+            ASSERT( !ip1.parseIPv6(" ") );
+        }
+    };
+
     class All : public Suite {
     public:
         All() : Suite( "ip_addr" ) {
@@ -186,6 +412,14 @@ namespace IPAddrTests {
             add< ClassBIPv4 >();
             add< ClassCIPv4 >();
             add< OtherMalformedIPv4 >();
+            add< SimpleIPv6 >();
+            add< MalformedSimpleIPv6 >();
+            add< OutOfBoundsIPv6 >();
+            add< ZeroCompressionIPv6 >();
+            add< NetmaskIPv6 >();
+            add< AllZeroesIPv6 >();
+            add< MiddleZeroesIPv6 >();
+            add< OtherInvalidIPv6 >();
         }
     } myall;
 
