02c68a1647470bf06b0271d6258bf13a22a1872c
Keith Bostic <keith@wiredtiger.com>
2011-01-29 04:11:41 +1100

    Delete the cache in favor of a linked, in-memory tree.  Specifically, move the WT_REF structures into the in-memory pages, and link parent's directly to their children.  This implies parents cannot be evicted while they still have subtrees in memory (including row-leaf pages with off-page duplicate subtrees).   This requires a complete re-write of almost all the "cache" code.
    We no longer cache any overflow items; __wt_bt_ovfl_in does a
    synchronous read in all cases and returns the overflow item without any
    page information.
    
    --HG--
    branch : keith

--- btree/bt_ovfl.c
+++ btree/bt_ovfl.c
@@ -14,22 +14,63 @@
  *	Read an overflow item from the disk.
  */
 int
-__wt_bt_ovfl_in(WT_TOC *toc, WT_OVFL *ovfl, WT_PAGE **pagep, int dsk_verify)
+__wt_bt_ovfl_in(WT_TOC *toc, WT_OVFL *ovfl, DBT *store)
 {
 	DB *db;
-	WT_OFF off;
-	WT_REF ref;
+	ENV *env;
+	WT_PAGE *page, _page;
+	WT_STATS *stats;
 
+	env = toc->env;
 	db = toc->db;
+	stats = env->ienv->cache->stats;
 
 	/*
-	 * Read an overflow page, using an overflow structure on a page for
+	 * Read an overflow page, using an overflow structure from a page for
 	 * which we (better) have a hazard reference.
+	 *
+	 * Overflow reads are synchronous. That may bite me at some point, but
+	 * WiredTiger supports large page sizes, and overflow items should be
+	 * rare.
 	 */
-	__wt_bt_gen_ref_pair(&ref, &off,
-	    ovfl->addr, WT_HDR_BYTES_TO_ALLOC(db, ovfl->size));
-	WT_RET(__wt_bt_page_in(toc, &ref, &off, dsk_verify));
+	WT_VERBOSE(env, WT_VERB_READ, (env,
+	    "overflow read addr/size %lu/%lu",
+	    (u_long)ovfl->addr, (u_long)ovfl->size));
+	WT_STAT_INCR(stats, OVERFLOW_READ);
 
-	*pagep = ref.page;
+	/*
+	 * The only caller that wants a copy of the overflow pages (as opposed
+	 * to the contents of the overflow pages), is the verify code.  For that
+	 * reason, it reads its own overflow pages, it doesn't call this code.
+	 *
+	 * But, we still have to verify the checksum, which means we have to
+	 * read the entire set of pages, then copy the interesting information
+	 * to the beginning of the buffer.   The copy is a shift in a single
+	 * buffer and so should be fast, but it's still not a good thing.  If
+	 * it ever becomes a problem, then we either have to pass the fact that
+	 * it's a "page" back to our caller and let them deal with the offset,
+	 * or add a new field to the DBT that flags the start of the allocated
+	 * buffer, instead of using the "data" field to indicate both the start
+	 * of the data and the start of the allocated memory.
+	 */
+	WT_CLEAR(_page);
+	page = &_page;
+	page->addr = ovfl->addr;
+	page->size = WT_HDR_BYTES_TO_ALLOC(db, ovfl->size);
+
+	/* Re-allocate memory as necessary to hold the overflow pages. */
+	if (store->mem_size < page->size)
+		WT_RET(__wt_realloc(
+		    env, &store->mem_size, page->size, &store->data));
+	page->hdr = store->data;
+
+	/* Read the page. */
+	WT_RET(__wt_page_read(db, page));
+
+	/* Copy the actual data in the DBT down to the start of the data. */
+	(void)memmove(store->data,
+	    (uint8_t *)store->data + sizeof(WT_PAGE_HDR), ovfl->size);
+	store->size = ovfl->size;
+
 	return (0);
 }
