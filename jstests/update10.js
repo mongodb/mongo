5b735fe5787e7f1de90f5a6a00367a825146fced
Paul Sadauskas <psadauskas@gmail.com>
2011-01-29 23:34:26 -0700

    Test min/max for indexed keys, too

--- jstests/update10.js
+++ jstests/update10.js
@@ -20,3 +20,28 @@ f.save( { a: 4 } );
 f.update( { a: 4 }, { $max: { a: 6 } } );
 assert.eq( 6, f.findOne().a );
 
+// again, with indexes
+f.drop();
+f.save( { a: 4 } );
+f.ensureIndex( { a: 1 } );
+f.update( { a: 4 }, { $min: { a: 2 } } );
+assert.eq( 2, f.findOne().a );
+
+f.drop();
+f.save( { a: 4 } );
+f.ensureIndex( { a: 1 } );
+f.update( { a: 4 }, { $min: { a: 6 } } );
+assert.eq( 4, f.findOne().a );
+
+f.drop();
+f.save( { a: 4 } );
+f.ensureIndex( { a: 1 } );
+f.update( { a: 4 }, { $max: { a: 2 } } );
+assert.eq( 4, f.findOne().a );
+
+f.drop();
+f.save( { a: 4 } );
+f.ensureIndex( { a: 1 } );
+f.update( { a: 4 }, { $max: { a: 6 } } );
+assert.eq( 6, f.findOne().a );
+
