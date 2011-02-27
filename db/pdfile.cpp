095948e11e5c3968175273f6d5851e490802ef61
Eliot Horowitz <eliot@10gen.com>
2011-02-26 21:20:08 -0500

    cleaner logging

--- db/pdfile.cpp
+++ db/pdfile.cpp
@@ -830,7 +830,7 @@ namespace mongo {
             }
 
             if ( !ok && logMissing ) {
-                log() << "unindex failed (key too big?) " << id.indexNamespace() << " key: " << j << " obj _id: " << obj["_id"] << endl;
+                log() << "unindex failed (key too big?) " << id.indexNamespace() << " key: " << j << " " << obj["_id"] << endl;
             }
         }
     }
