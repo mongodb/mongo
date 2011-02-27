733134f7da8c67f35096833539a101493968e84f
dwight <dwight@10gen.com>
2011-02-27 11:15:20 -0500

    SERVER-2631 180? 181?

--- util/alignedbuilder.cpp
+++ util/alignedbuilder.cpp
@@ -48,7 +48,7 @@ namespace mongo {
         assert( a );
         while( 1 ) {
             a *= 2;
-            wassert( a <= 128*1024*1024 );
+            wassert( a <= 256*1024*1024 );
             assert( a <= 512*1024*1024 );
             if( _len < a )
                 break;
