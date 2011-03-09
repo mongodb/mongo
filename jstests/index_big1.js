ed2303de56cee2ed08379ef42b83c116fec6f73f
Eliot Horowitz <eliot@10gen.com>
2011-03-09 12:51:29 -0500

    fix test for v8

--- jstests/index_big1.js
+++ jstests/index_big1.js
@@ -10,7 +10,7 @@ for ( i=0; i<N; i++ ) {
     while ( s.length < i )
         s += "x";
 
-    t.insert( { a : i , x : s } )
+    t.insert( { a : i + .5 , x : s } )
 }
 
 t.ensureIndex( { a : 1 , x : 1 } )
@@ -20,7 +20,7 @@ assert.eq( 2 , t.getIndexes().length );
 flip = -1;
 
 for ( i=0; i<N; i++ ) {
-    var c = t.find( { a : i } ).count();
+    var c = t.find( { a : i + .5 } ).count();
     if ( c == 1 ) {
         assert.eq( -1 , flip , "flipping : " + i );
     }
