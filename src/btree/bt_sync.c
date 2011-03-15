7ac86007e33f3ada994e9df6c78a99958a4a4312
Keith Bostic <keith.bostic@wiredtiger.com>
2011-03-15 11:17:59 -0400

    Note that file sync is single-threaded and a depth-first walk.

--- src/btree/bt_sync.c
+++ src/btree/bt_sync.c
@@ -50,7 +50,17 @@ __wt_bt_tree_sync(SESSION *session, WT_PAGE *page, void *arg)
 {
 	WT_UNUSED(arg);
 
-	/* Reconcile any dirty pages. */
+	/*
+	 * Reconcile any dirty pages.
+	 *
+	 * The tree walk is depth first, that is, the worker function is not
+	 * called on internal pages until all children have been visited; so,
+	 * we don't have to worry about reconciling a page that still has a
+	 * child page, or reading a page after we discard it,
+	 *
+	 * We ignore WT_REF connections because file sync is single-threaded
+	 * by the API layer, there's no other threads of control in the system.
+	 */
 	if (WT_PAGE_IS_MODIFIED(page))
 		WT_RET(__wt_page_reconcile(session, page));
 	return (0);
