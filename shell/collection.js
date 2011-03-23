b49e9338db5b3c20996113fcc0e9f1d4ed3b4bf4
Eliot Horowitz <eliot@10gen.com>
2011-03-23 10:43:14 -0400

    collection._distinct helper to get raw info

--- shell/collection.js
+++ shell/collection.js
@@ -530,12 +530,20 @@ DBCollection.prototype.isCapped = function(){
     return ( e && e.options && e.options.capped ) ? true : false;
 }
 
-DBCollection.prototype.distinct = function( keyString , query ){
-    var res = this._dbCommand( { distinct : this._shortName , key : keyString , query : query || {} } );
+DBCollection.prototype._distinct = function( keyString , query ){
+    return this._dbCommand( { distinct : this._shortName , key : keyString , query : query || {} } );
     if ( ! res.ok )
         throw "distinct failed: " + tojson( res );
     return res.values;
 }
+
+DBCollection.prototype.distinct = function( keyString , query ){
+    var res = this._distinct( keyString , query );
+    if ( ! res.ok )
+        throw "distinct failed: " + tojson( res );
+    return res.values;
+}
+
 
 DBCollection.prototype.group = function( params ){
     params.ns = this._shortName;
