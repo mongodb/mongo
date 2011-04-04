2b23df9f52979c20c1548cc01f580f1c413d9c34
Mathias Stearn <redbeard0531@gmail.com>
2011-04-03 21:21:26 -0400

    windows compile

--- bson/bsonobjbuilder.h
+++ bson/bsonobjbuilder.h
@@ -220,7 +220,7 @@ namespace mongo {
             long long x = n;
             if ( x < 0 )
                 x = x * -1;
-            if ( x < ( numeric_limits<int>::max() / 2 ) )
+            if ( x < ( (numeric_limits<int>::max)() / 2 ) ) // extra () to avoid max macro on windows
                 append( fieldName , (int)n );
             else
                 append( fieldName , n );
