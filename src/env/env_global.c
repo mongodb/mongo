1f4fae79bbe3dbf73b6ee964d81cba4a4c5080cb
Keith Bostic <keith.bostic@wiredtiger.com>
2011-04-11 17:17:13 -0400

    Delete __wt_err_stream, we no longer need a global error-stream variable.

--- src/env/env_global.c
+++ src/env/env_global.c
@@ -8,7 +8,6 @@
 #include "wt_internal.h"
 
 void *__wt_addr;				/* Memory flush address. */
-FILE *__wt_err_stream;				/* Error stream from init. */
 
 /*
  * __wt_library_init --
@@ -22,12 +21,6 @@ __wt_library_init(void)
 	 * one we choose.
 	 */
 	__wt_addr = &__wt_addr;
-
-	/*
-	 * We want to be able to redirect error messages from the very first
-	 * instruction.
-	 */
-	__wt_err_stream = stderr;
 
 #ifdef HAVE_DIAGNOSTIC
 	/* Load debug code the compiler might optimize out. */
