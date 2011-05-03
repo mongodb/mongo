b13dff542f40d79d2cc3b1206c768b67b7c6a9ed
Dwight <dwight@10gen.com>
2011-05-03 17:22:11 -0400

    update test for extent alloc changes

--- jstests/disk/newcollection.js
+++ jstests/disk/newcollection.js
@@ -3,11 +3,21 @@
 port = allocatePorts( 1 )[ 0 ]
 var baseName = "jstests_disk_newcollection";
 var m = startMongod( "--noprealloc", "--smallfiles", "--port", port, "--dbpath", "/data/db/" + baseName );
+//var m = db.getMongo();
 db = m.getDB( "test" );
 
-db.createCollection( baseName, {size:15.9*1024*1024} );
-db.baseName.drop();
+var t = db[baseName];
 
-size = m.getDBs().totalSize;
-db.baseName.save( {} );
-assert.eq( size, m.getDBs().totalSize );
+for (var pass = 0; pass <= 1; pass++) {
+
+    db.createCollection(baseName, { size: 15.8 * 1024 * 1024 });
+    if( pass == 0 )
+        t.drop();
+
+    size = m.getDBs().totalSize;
+    t.save({});
+    assert.eq(size, m.getDBs().totalSize);
+    assert(size <= 32 * 1024 * 1024);
+
+    t.drop();
+}
