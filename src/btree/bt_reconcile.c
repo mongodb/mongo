a0786f379bbd0a2de96d25e034908d278ff9ffb3
Michael Cahill <michael.cahill@wiredtiger.com>
2011-05-12 16:23:40 +1000

    Fix split calculation for RLE pages.

--- src/btree/bt_reconcile.c
+++ src/btree/bt_reconcile.c
@@ -1602,7 +1602,7 @@ __rec_col_rle_bulk(SESSION *session, WT_PAGE *page)
 		}
 
 		/* Boundary: allocate, split or write the page. */
-		while (len > r->space_avail)
+		while (len + WT_SIZEOF32(uint16_t) > r->space_avail)
 			WT_RET(__rec_split(session));
 
 		last_data = r->first_free;
