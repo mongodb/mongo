c0864c5c527d80c7f28456c03929ddc39daef17b
Don Anderson <dda@ddanderson.com>
2011-05-12 10:52:41 -0400

    Created WT_TEST.<timestamp>/testxxx.N subdirectories, along with results.txt results.txt contains all the self.pr() output, as well as stack traces. refs #31

--- test/suite/wttest.py
+++ test/suite/wttest.py
@@ -14,18 +14,28 @@ import sys
 import os
 import wiredtiger
 import subprocess
+import traceback
+import time
 
 class WiredTigerTestCase(unittest.TestCase):
-    def setUp(self):
-        self.prhead('started', True)
-        self.testdir = 'WT_TEST'
-        self.origcwd = os.getcwd()
+    _timeStamp = time.strftime('%Y%m%d-%H%M%S', time.localtime())
+    _parentTestdir = 'WT_TEST.' + _timeStamp
+    subprocess.call(["rm", "-rf", _parentTestdir])
+    subprocess.call(["mkdir", "-p", _parentTestdir])
+    _resultfile = open(_parentTestdir + os.sep + 'results.txt', "w", 0)  # unbuffered
 
+    def setUp(self):
+        if not hasattr(self.__class__, 'wt_ntests'):
+            self.__class__.wt_ntests = 0
+        self.__class__.wt_ntests += 1
+        self.testdir = WiredTigerTestCase._parentTestdir + os.sep + self.className() + '.' + str(self.__class__.wt_ntests)
+        self.prhead('started in ' + self.testdir, True)
+        self.origcwd = os.getcwd()
         subprocess.call(["rm", "-rf", self.testdir])
         if os.path.exists(self.testdir):
             raise Exception(self.testdir + ": cannot remove directory");
-        subprocess.call(["mkdir", self.testdir])
-        os.chdir("WT_TEST")
+        subprocess.call(["mkdir", "-p", self.testdir])
+        os.chdir(self.testdir)
 
         self.conn = wiredtiger.wiredtiger_open(".", None, 'create')
         self.pr(`self.conn`)
@@ -34,25 +44,46 @@ class WiredTigerTestCase(unittest.TestCase):
     def tearDown(self):
         self.pr('finishing')
         self.conn.close(None)
+        self.conn = None
         os.chdir(self.origcwd)
+        # Clean up unless there's a failure
+        excinfo = sys.exc_info()
+        if excinfo == (None, None, None):
+            subprocess.call(["rm", "-rf", self.testdir])
+        else:
+            self.pr('FAIL')
+            self.prexception(excinfo)
+            self.pr('preserving directory ' + self.testdir)
         self.prhead('TEST COMPLETED')
 
     def pr(self, s):
         """
         print a progress line for testing
         """
-        print('    ' + self.shortid() + ': ' + s)
+        msg = '    ' + self.shortid() + ': ' + s
+        WiredTigerTestCase._resultfile.write(msg + '\n')
 
     def prhead(self, s, *beginning):
         """
         print a header line for testing, something important
         """
+        msg = ''
         if len(beginning) > 0:
-            print('')
-        print('  ' + self.shortid() + ': ' + s)
+            msg += '\n'
+        msg += '  ' + self.shortid() + ': ' + s
+        print(msg)
+        WiredTigerTestCase._resultfile.write(msg + '\n')
+
+    def prexception(self, excinfo):
+        WiredTigerTestCase._resultfile.write('\n')
+        traceback.print_exception(excinfo[0], excinfo[1], excinfo[2], None, WiredTigerTestCase._resultfile)
+        WiredTigerTestCase._resultfile.write('\n')
 
     def shortid(self):
         return self.id().replace("__main__.","")
+
+    def className(self):
+        return self.__class__.__name__
 
 
 def runsuite(suite):
