93eaf061fad1773ba86fae6eb1e69488e64cbb48
Eliot Horowitz <eliot@10gen.com>
2011-05-18 18:08:21 -0400

    names have to be unique

--- jstests/geo_fiddly_box.js
+++ jstests/geo_fiddly_box.js
@@ -4,20 +4,20 @@
 // "expand" portion of the geo-lookup expands the 2d range in only one
 // direction (so points are required on either side of the expanding range)
 
-db.users.drop();
-db.users.ensureIndex({ loc : "2d" })
+db.geo_fiddly_box.drop();
+db.geo_fiddly_box.ensureIndex({ loc : "2d" })
 
-db.users.insert({ "loc" : [3, 1] })
-db.users.insert({ "loc" : [3, 0.5] })
-db.users.insert({ "loc" : [3, 0.25] })
-db.users.insert({ "loc" : [3, -0.01] })
-db.users.insert({ "loc" : [3, -0.25] })
-db.users.insert({ "loc" : [3, -0.5] })
-db.users.insert({ "loc" : [3, -1] })
+db.geo_fiddly_box.insert({ "loc" : [3, 1] })
+db.geo_fiddly_box.insert({ "loc" : [3, 0.5] })
+db.geo_fiddly_box.insert({ "loc" : [3, 0.25] })
+db.geo_fiddly_box.insert({ "loc" : [3, -0.01] })
+db.geo_fiddly_box.insert({ "loc" : [3, -0.25] })
+db.geo_fiddly_box.insert({ "loc" : [3, -0.5] })
+db.geo_fiddly_box.insert({ "loc" : [3, -1] })
 
 // OK!
-print( db.users.count() )
-assert.eq( 7, db.users.count({ "loc" : { "$within" : { "$box" : [ [2, -2], [46, 2] ] } } }), "Not all locations found!" );
+print( db.geo_fiddly_box.count() )
+assert.eq( 7, db.geo_fiddly_box.count({ "loc" : { "$within" : { "$box" : [ [2, -2], [46, 2] ] } } }), "Not all locations found!" );
 
 
 // Test normal lookup of a small square of points as a sanity check.
@@ -28,17 +28,17 @@ max = 1
 step = 1
 numItems = 0;
 
-db.users2.drop()
-db.users2.ensureIndex({ loc : "2d" }, { max : max + epsilon / 2, min : min - epsilon / 2 })
+db.geo_fiddly_box2.drop()
+db.geo_fiddly_box2.ensureIndex({ loc : "2d" }, { max : max + epsilon / 2, min : min - epsilon / 2 })
 
 for(var x = min; x <= max; x += step){
 	for(var y = min; y <= max; y += step){
-		db.users2.insert({ "loc" : { x : x, y : y } })
+		db.geo_fiddly_box2.insert({ "loc" : { x : x, y : y } })
 		numItems++;
 	}
 }
 
-assert.eq( numItems, db.users2.count({ loc : { $within : { $box : [[min - epsilon / 3,
+assert.eq( numItems, db.geo_fiddly_box2.count({ loc : { $within : { $box : [[min - epsilon / 3,
                                                                     min - epsilon / 3],
                                                                    [max + epsilon / 3,
                                                                     max + epsilon / 3]] } } }), "Not all locations found!");
