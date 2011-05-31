c117c4c2ea62201078f6a46b8bbed173f78746da
Mathias Stearn <mathias@10gen.com>
2011-05-31 15:28:13 -0400

    Make findAndModify work when _id is removed in projection SERVER-2626

--- jstests/find_and_modify2.js
+++ jstests/find_and_modify2.js
@@ -8,3 +8,9 @@ assert.eq(out, {_id:1, i:1});
 
 out = t.findAndModify({update: {$inc: {i:1}}, fields: {i:0}});
 assert.eq(out, {_id:1, j:0});
+
+out = t.findAndModify({update: {$inc: {i:1}}, fields: {_id:0, j:1}});
+assert.eq(out, {j:0});
+
+out = t.findAndModify({update: {$inc: {i:1}}, fields: {_id:0, j:1}, 'new': true});
+assert.eq(out, {j:0});
