ba9daf00db064ef9a284f5b72bb3e43a0ca141bb
Aaron <aaron@10gen.com>
2011-06-02 17:10:23 -0400

    SERVER-1735 add a real field value test case

--- jstests/type2.js
+++ jstests/type2.js
@@ -5,11 +5,12 @@ t.drop();
 
 t.save( {a:null} );
 t.save( {} );
+t.save( {a:'a'} );
 
 function test() {
 	assert.eq( 2, t.count( {a:null} ) );
 	assert.eq( 1, t.count( {a:{$type:10}} ) );
-	assert.eq( 1, t.count( {a:{$exists:true}} ) );
+	assert.eq( 2, t.count( {a:{$exists:true}} ) );
 	assert.eq( 1, t.count( {a:{$exists:false}} ) );
 }
 
