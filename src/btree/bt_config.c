7ea802807585f03d3dba320cfdc0b143ba7f63d6
Michael Cahill <michael.cahill@wiredtiger.com>
2011-06-15 10:02:12 +1000

    Add a newline to the end of config files.

--- src/btree/bt_config.c
+++ src/btree/bt_config.c
@@ -31,6 +31,9 @@ __wt_btconf_read(
 	WT_ERR(__wt_filesize(session, fh, &filesize));
 	WT_ERR(__wt_calloc_def(session, (size_t)filesize, &config));
 	WT_ERR(__wt_read(session, fh, (off_t)0, (uint32_t)filesize, config));
+	
+	/* The file has a newline at the end, replace that with a NUL. */
+	config[(uint32_t)filesize - 1] = '\0';
 
 	/* Verify that the string is NUL-terminated. */
 	if (config[filesize - 1] != '\0') {
@@ -69,8 +72,12 @@ __wt_btconf_write(
 	/* Open the config file. */
 	fh = NULL;
 	WT_ERR(__wt_open(session, fullname, 0666, 1, &fh));
+
+	/* Write the config string and a newline. */
 	WT_ERR(__wt_write(session,
-	    fh, (off_t)0, (uint32_t)strlen(config) + 1, config));
+	    fh, (off_t)0, (uint32_t)strlen(config), config));
+	WT_ERR(__wt_write(session,
+	    fh, (off_t)strlen(config), 1, "\n"));
 
 err:	if (fh != NULL)
 		WT_TRET(__wt_close(session, fh));
