6646b9024f755ea666b5e9a5660e6c55d3705a3a
Aaron <aaron@10gen.com>
2011-06-15 17:24:29 -0700

    SERVER-3192 a few more test cases

--- jstests/orj.js
+++ jstests/orj.js
@@ -3,6 +3,8 @@
 t = db.jstests_orj;
 t.drop();
 
+function check() {
+    
 assert.throws( function() { t.find( { x:0,$or:"a" } ).toArray(); } );
 assert.throws( function() { t.find( { x:0,$or:[] } ).toArray(); } );
 assert.throws( function() { t.find( { x:0,$or:[ "a" ] } ).toArray(); } );
@@ -19,6 +21,7 @@ assert.throws( function() { t.find( { x:0,$nor:[{$nor:"a"}] } ).toArray(); } );
 assert.throws( function() { t.find( { x:0,$nor:[{$nor:[]}] } ).toArray(); } );
 assert.throws( function() { t.find( { x:0,$nor:[{$nor:[ "a" ]}] } ).toArray(); } );
 
+t.remove();
 t.save( {a:1,b:2} );
 assert.eq( 1, t.find( {a:1,b:2} ).itcount() );
 
@@ -26,7 +29,16 @@ assert.eq( 1, t.find( {a:1,$or:[{b:2}]} ).itcount() );
 assert.eq( 0, t.find( {a:1,$or:[{b:3}]} ).itcount() );
 
 assert.eq( 1, t.find( {a:1,$or:[{$or:[{b:2}]}]} ).itcount() );
+assert.eq( 1, t.find( {a:1,$or:[{$or:[{b:2}]}]} ).itcount() );
 assert.eq( 0, t.find( {a:1,$or:[{$or:[{b:3}]}]} ).itcount() );
+
+assert.eq( 1, t.find( {$or:[{$or:[{a:2},{b:2}]}]} ).itcount() );
+assert.eq( 1, t.find( {$or:[{a:2},{$or:[{b:2}]}]} ).itcount() );
+assert.eq( 1, t.find( {$or:[{a:1},{$or:[{b:3}]}]} ).itcount() );
+
+assert.eq( 1, t.find( {$or:[{$or:[{a:1},{a:2}]},{$or:[{b:3},{b:4}]}]} ).itcount() );
+assert.eq( 1, t.find( {$or:[{$or:[{a:0},{a:2}]},{$or:[{b:2},{b:4}]}]} ).itcount() );
+assert.eq( 0, t.find( {$or:[{$or:[{a:0},{a:2}]},{$or:[{b:3},{b:4}]}]} ).itcount() );
 
 assert.eq( 1, t.find( {a:1,$and:[{$or:[{$or:[{b:2}]}]}]} ).itcount() );
 assert.eq( 0, t.find( {a:1,$and:[{$or:[{$or:[{b:3}]}]}]} ).itcount() );
@@ -42,3 +54,24 @@ assert.eq( 1, t.find( {$and:[{$nor:[{a:3},{a:2}]},{$nor:[{b:3},{b:1}]}]} ).itcou
 assert.eq( 1, t.find( {$and:[{$or:[{a:1},{a:2}]},{$nor:[{b:1},{b:3}]}]} ).itcount() );
 assert.eq( 0, t.find( {$and:[{$or:[{a:3},{a:2}]},{$nor:[{b:1},{b:3}]}]} ).itcount() );
 assert.eq( 0, t.find( {$and:[{$or:[{a:1},{a:2}]},{$nor:[{b:1},{b:2}]}]} ).itcount() );
+
+}
+
+check();
+
+t.ensureIndex( {a:1} );
+check();
+t.dropIndexes();
+
+t.ensureIndex( {b:1} );
+check();
+t.dropIndexes();
+
+t.ensureIndex( {a:1} );
+t.ensureIndex( {b:1} );
+check();
+t.dropIndexes();
+
+t.ensureIndex( {a:1,b:1} );
+check();
+t.dropIndexes();
