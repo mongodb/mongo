62ee12935a7fb42236fcc65b279550ea4e703aca
Eliot Horowitz <eliot@10gen.com>
2011-06-20 16:17:32 -0400

    move comment

--- db/query.h
+++ db/query.h
@@ -25,48 +25,6 @@
 #include "diskloc.h"
 #include "projection.h"
 
-/* db request message format
-
-   unsigned opid;         // arbitary; will be echoed back
-   byte operation;
-   int options;
-
-   then for:
-
-   dbInsert:
-      string collection;
-      a series of JSObjects
-   dbDelete:
-      string collection;
-      int flags=0; // 1=DeleteSingle
-      JSObject query;
-   dbUpdate:
-      string collection;
-      int flags; // 1=upsert
-      JSObject query;
-      JSObject objectToUpdate;
-        objectToUpdate may include { $inc: <field> } or { $set: ... }, see struct Mod.
-   dbQuery:
-      string collection;
-      int nToSkip;
-      int nToReturn; // how many you want back as the beginning of the cursor data (0=no limit)
-                     // greater than zero is simply a hint on how many objects to send back per "cursor batch".
-                     // a negative number indicates a hard limit.
-      JSObject query;
-      [JSObject fieldsToReturn]
-   dbGetMore:
-      string collection; // redundant, might use for security.
-      int nToReturn;
-      int64 cursorID;
-   dbKillCursors=2007:
-      int n;
-      int64 cursorIDs[n];
-
-   Note that on Update, there is only one object, which is different
-   from insert where you can pass a list of objects to insert in the db.
-   Note that the update field layout is very similar layout to Query.
-*/
-
 // struct QueryOptions, QueryResult, QueryResultFlags in:
 #include "../client/dbclient.h"
 
