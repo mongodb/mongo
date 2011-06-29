e0fd83b50d121fc9c05e98d4c94d61c18f9608a4
Keith Bostic <keith.bostic@wiredtiger.com>
2011-06-29 16:50:46 -0400

    KNF, then we don't need forward declarations.
    --HG--
    extra : rebase_source : 22ae7ef41d5387e22d8ce2ba41269886319757e7

--- src/include/config.h
+++ src/include/config.h
@@ -5,11 +5,7 @@
  *	All rights reserved.
  */
 
-struct __wt_config;		typedef struct __wt_config WT_CONFIG;
-struct __wt_config_item;	typedef struct __wt_config_item WT_CONFIG_ITEM;
-
-struct __wt_config
-{
+struct __wt_config {
 	WT_SESSION_IMPL *session;
 	const char *orig;
 	const char *end;
@@ -19,8 +15,7 @@ struct __wt_config
 	int8_t *go;
 };
 
-struct __wt_config_item
-{
+struct __wt_config_item {
 	const char *str;
 	size_t len;
 	uint64_t val;
