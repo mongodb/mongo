1c44a0e5b55ef69196fe46b96a7a583292983501
Eliot Horowitz <eliot@10gen.com>
2011-07-05 00:47:04 -0400

    test cleaning

--- jstests/tool/tsv1.js
+++ jstests/tool/tsv1.js
@@ -12,8 +12,9 @@ assert.soon( "2 == c.count()" , "restore 2" );
 a = c.find().sort( { a : 1 } ).toArray();
 delete a[0]._id
 delete a[1]._id
-assert.eq( tojson( { a : "a" , b : "b" , c : "c" , d: "d", e: "e"} ) , tojson( a[1] ) , "tsv parse 1" );
-assert.eq( tojson( base ) , tojson(a[0]) , "tsv parse 0" )
+
+assert.eq( { a : "a" , b : "b" , c : "c" , d: "d", e: "e"} , a[1] , "tsv parse 1" );
+assert.eq( base , a[0] , "tsv parse 0" )
 
 c.drop()
 assert.eq( 0 , c.count() , "after drop 2" )
@@ -24,7 +25,7 @@ assert.eq( 1 , c.count() , "after restore 2" );
 
 x = c.findOne()
 delete x._id;
-assert.eq( tojson( base ) , tojson(x) , "tsv parse 2" )
+assert.eq( base , x , "tsv parse 2" )
 
 
 
