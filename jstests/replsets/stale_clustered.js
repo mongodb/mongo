542e6d18478bfa558c2a2ea13f21b27d9e5669ce
dwight <dwight@10gen.com>
2011-07-16 18:11:35 -0400

    log nicer for test output

--- jstests/replsets/stale_clustered.js
+++ jstests/replsets/stale_clustered.js
@@ -1,6 +1,10 @@
 // this tests that slaveOk'd queries in sharded setups get correctly routed when
 // a slave goes into RECOVERING state, and don't break
 
+function prt(s) {
+    print("\nstale_clustered.js " + s);
+    print();
+}
 
 var shardTest = new ShardingTest( name = "clusteredstale" ,
                                   numShards = 2 ,
@@ -34,16 +38,16 @@ rsB.getMaster().getDB( "test" ).dummy.insert( { x : 1 } )
 rsA.awaitReplication()
 rsB.awaitReplication()
 
-print("1: initial insert")
+prt("1: initial insert")
 
 coll.save({ _id : -1, a : "a", date : new Date() })
 coll.save({ _id : 1, b : "b", date : new Date() })
 
-print("2: shard collection")
+prt("2: shard collection")
 
 shardTest.shardGo( coll, /* shardBy */ { _id : 1 }, /* splitAt */ { _id : 0 } )
 
-print("3: test normal and slaveOk queries")
+prt("3: test normal and slaveOk queries")
 
 // Make shardA and rsA the same
 var shardA = shardTest.getShard(  coll, { _id : -1 } )
@@ -63,7 +67,7 @@ assert.eq( coll.find().itcount(), collSOk.find().itcount() )
 assert.eq( shardAColl.find().itcount(), 1 )
 assert.eq( shardAColl.findOne()._id, -1 )
 
-print("5: overflow oplog");
+prt("5: overflow oplog");
 
 var secs = rsA.getSecondaries()
 var goodSec = secs[0]
@@ -71,28 +75,27 @@ var badSec = secs[1]
 
 rsA.overflow( badSec )
 
-print("6: stop non-overflowed secondary")
+prt("6: stop non-overflowed secondary")
 
 rsA.stop( goodSec, undefined, true )
 
-print("7: check our regular and slaveok query")
+prt("7: check our regular and slaveok query")
 
 assert.eq( coll.find().itcount(), collSOk.find().itcount() )
 
-print("8: restart both our secondaries clean")
+prt("8: restart both our secondaries clean")
 
 rsA.restart( rsA.getSecondaries(), { remember : true, startClean : true }, undefined, 5 * 60 * 1000 )
 
-print("9: wait for recovery")
+prt("9: wait for recovery")
 
 rsA.waitForState( rsA.getSecondaries(), rsA.SECONDARY, 5 * 60 * 1000 )
 
-print("10: check our regular and slaveok query")
+prt("10: check our regular and slaveok query")
 
 assert.eq( coll.find().itcount(), collSOk.find().itcount() )
 
-
-
+prt("DONE\n\n\n");
 
 //shardTest.stop()
 
