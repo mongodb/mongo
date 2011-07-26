af93c5d8771461d85052bcda918179c8259a387c
gregs <greg@10gen.com>
2011-07-26 16:08:49 -0400

    escaping for regex - SERVER-3471

--- s/config.cpp
+++ s/config.cpp
@@ -300,7 +300,7 @@ namespace mongo {
         unserialize( o );
 
         BSONObjBuilder b;
-        b.appendRegex( "_id" , (string)"^" + _name + "." );
+        b.appendRegex( "_id" , (string)"^" + _name + "\\." );
 
         auto_ptr<DBClientCursor> cursor = conn->query( ShardNS::collection ,b.obj() );
         assert( cursor.get() );
