ef7b1b22e54df2da6b3fa434ba25d5e7899c1cb4
dannenberg <dannenberg.matt@gmail.com>
2011-07-27 15:18:06 -0400

    SERVER-1208 config files ignore flags set to =false, warn about fast sync, and skip lines starting with #

--- db/cmdline.h
+++ db/cmdline.h
@@ -106,6 +106,7 @@ namespace mongo {
                                        boost::program_options::options_description& hidden );
 
 
+        static void parseConfigFile( ifstream &f, stringstream &ss);
         /**
          * @return true if should run program, false if should exit
          */
--- jstests/slowNightly/command_line_parsing.js
+++ jstests/slowNightly/command_line_parsing.js
@@ -7,3 +7,15 @@ var baseName = "jstests_slowNightly_command_line_parsing";
 var m = startMongod( "--port", port, "--dbpath", "/data/db/" + baseName, "--notablescan" );
 m.getDB( baseName ).getCollection( baseName ).save( {a:1} );
 assert.throws( function() { m.getDB( baseName ).getCollection( baseName ).find( {a:1} ).toArray() } );
+
+// test config file 
+var m2 = startMongod( "--port", port+2, "--dbpath", "/data/db/" + baseName +"2", "--config", "jstests/libs/testconfig");
+var m2result = {
+    "parsed" : {
+        "config" : "jstests/libs/testconfig",
+        "dbpath" : "/data/db/jstests_slowNightly_command_line_parsing2",
+        "fastsync" : "true",
+        "port" : 31002
+    }
+};
+assert( friendlyEqual(m2result.parsed, m2.getDB("admin").runCommand( "getCmdLineOpts" ).parsed) );
