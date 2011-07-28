724fc134c4a9855b66a8c6f594a354ea888f60a8
Eliot Horowitz <eliot@10gen.com>
2011-07-28 17:23:59 -0400

    make sure to drop the same dups on master and secondaries SERVER-1086

--- db/pdfile.cpp
+++ db/pdfile.cpp
@@ -967,7 +967,7 @@ namespace mongo {
         }
     }
 
-    void DataFileMgr::deleteRecord(const char *ns, Record *todelete, const DiskLoc& dl, bool cappedOK, bool noWarn) {
+    void DataFileMgr::deleteRecord(const char *ns, Record *todelete, const DiskLoc& dl, bool cappedOK, bool noWarn, bool doLog ) {
         dassert( todelete == dl.rec() );
 
         NamespaceDetails* d = nsdetails(ns);
@@ -975,6 +975,14 @@ namespace mongo {
             out() << "failing remove on a capped ns " << ns << endl;
             uassert( 10089 ,  "can't remove from a capped collection" , 0 );
             return;
+        }
+        
+        BSONObj toDelete;
+        if ( doLog ) {
+            BSONElement e = dl.obj()["_id"];
+            if ( e.type() ) {
+                toDelete = e.wrap();
+            }
         }
 
         /* check if any cursors point to us.  if so, advance them. */
@@ -984,6 +992,10 @@ namespace mongo {
 
         _deleteRecord(d, ns, todelete, dl);
         NamespaceDetailsTransient::get_w( ns ).notifyOfWriteOp();
+
+        if ( ! toDelete.isEmpty() ) {
+            logOp( "d" , ns , toDelete );
+        }
     }
 
 
@@ -1276,7 +1288,7 @@ namespace mongo {
         log(1) << "\t fastBuildIndex dupsToDrop:" << dupsToDrop.size() << endl;
 
         for( list<DiskLoc>::iterator i = dupsToDrop.begin(); i != dupsToDrop.end(); i++ ){
-            theDataFileMgr.deleteRecord( ns, i->rec(), *i, false, true );
+            theDataFileMgr.deleteRecord( ns, i->rec(), *i, false, true , true );
             getDur().commitIfNeeded();
         }
 
@@ -1310,10 +1322,11 @@ namespace mongo {
                         throw;
 
                     if ( dropDups ) {
+                        log() << "eliot was here" << endl;
                         DiskLoc toDelete = cc->currLoc();
                         bool ok = cc->advance();
                         cc->updateLocation();
-                        theDataFileMgr.deleteRecord( ns, toDelete.rec(), toDelete, false, true );
+                        theDataFileMgr.deleteRecord( ns, toDelete.rec(), toDelete, false, true , true );
                         if( ClientCursor::find(id, false) == 0 ) {
                             cc.release();
                             if( !ok ) {
---
+++ jstests/repl/drop_dups.js
@@ -0,0 +1,61 @@
+
+var rt = new ReplTest( "drop_dups" );
+
+m = rt.start( true );
+s = rt.start( false );
+
+function block(){
+    am.runCommand( { getlasterror : 1 , w : 2 , wtimeout : 3000 } )
+}
+
+am = m.getDB( "foo" );
+as = s.getDB( "foo" );
+
+function run( createInBackground ) {
+
+    collName = "foo" + ( createInBackground ? "B" : "F" );
+    
+    am[collName].drop();
+    am.blah.insert( { x : 1 } )
+    block();
+    
+    for ( i=0; i<10; i++ ) {
+        am[collName].insert( { _id : i , x : Math.floor( i / 2 ) } )
+    }
+    
+    block();
+    
+    am.runCommand( { "godinsert" : collName , obj : { _id : 100 , x : 20 } } );
+    am.runCommand( { "godinsert" : collName , obj : { _id : 101 , x : 20 } } );
+
+    as.runCommand( { "godinsert" : collName , obj : { _id : 101 , x : 20 } } );
+    as.runCommand( { "godinsert" : collName , obj : { _id : 100 , x : 20 } } );
+    
+    assert.eq( as[collName].count() , am[collName].count() );
+    
+    function mymap(z) {
+        return z._id + ":" + z.x + ",";
+    }
+
+    
+    assert.neq( tojson(am[collName].find().map(mymap)) , 
+                tojson(as[collName].find().map(mymap)) , "order is not supposed to be same on master and slave but it is" );
+    
+    
+    am[collName].ensureIndex( { x : 1 } , { unique : true , dropDups : true , background : createInBackground  } );
+    am.blah.insert( { x : 1 } )
+    block();
+
+    assert.eq( 2 , am[collName].getIndexKeys().length , "A1 : " + createInBackground )
+    assert.eq( 2 , as[collName].getIndexKeys().length , "A2 : " + createInBackground )
+    
+    assert.eq( am[collName].find().sort( { _id : 1 } ).map(mymap) , 
+               as[collName].find().sort( { _id : 1 } ).map(mymap) , "different things dropped on master and slave" );
+    
+    
+}
+
+run( false )
+run( true )
+
+rt.stop()
