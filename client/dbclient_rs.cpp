bb85c0204c705186ca1bd67c6f39e1920ee5711d
Eliot Horowitz <eliot@10gen.com>
2011-08-07 12:49:10 -0400

    backport of SERVER-3462

--- client/dbclient_rs.cpp
+++ client/dbclient_rs.cpp
@@ -463,9 +463,12 @@ namespace mongo {
             if ( ! _slave->isFailed() )
                 return _slave.get();
             _monitor->notifySlaveFailure( _slaveHost );
+            _slaveHost = _monitor->getSlave();
         }
-        
-        _slaveHost = _monitor->getSlave();
+        else {
+            _slaveHost = h;
+        }
+
         _slave.reset( new DBClientConnection( true , this ) );
         _slave->connect( _slaveHost );
         _auth( _slave.get() );
