04261c7d15308d2b20edf61b9c8ee175f6c21ad4
Mathias Stearn <mathias@10gen.com>
2011-08-08 18:56:17 -0400

    Tell glibcxx to use glibc's optimized str functions

--- pch.h
+++ pch.h
@@ -44,7 +44,20 @@
 # include <windows.h>
 #endif
 
+#if defined(__linux__) && defined(MONGO_EXPOSE_MACROS)
+// glibc's optimized versions are better than g++ builtins
+# define __builtin_strcmp strcmp
+# define __builtin_strlen strlen
+# define __builtin_memchr memchr
+# define __builtin_memcmp memcmp
+# define __builtin_memcpy memcpy
+# define __builtin_memset memset
+# define __builtin_memmove memmove
+#endif
+
+
 #include <ctime>
+#include <cstring>
 #include <sstream>
 #include <string>
 #include <memory>
