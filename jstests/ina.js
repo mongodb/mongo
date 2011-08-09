fd441b9d08fc0ffcab3bbee0499c90ca20e2d9fb
Aaron <aaron@10gen.com>
2011-08-09 04:33:24 +0000

    SERVER-3545 fix test typos

--- jstests/ina.js
+++ jstests/ina.js
@@ -4,10 +4,10 @@ t = db.jstests_ina;
 t.drop();
 t.save( {} );
 
-assert.throws( function() { t.( {a:{$in:[{$elemMatch:{b:1}}]}} ).itcount(); } );
+assert.throws( function() { t.find( {a:{$in:[{$elemMatch:{b:1}}]}} ).itcount(); } );
 assert.throws( function() { t.find( {a:{$not:{$in:[{$elemMatch:{b:1}}]}}} ).itcount(); } );
 
-assert.throws( function() { t.( {a:{$nin:[{$elemMatch:{b:1}}]}} ).itcount(); } );
+assert.throws( function() { t.find( {a:{$nin:[{$elemMatch:{b:1}}]}} ).itcount(); } );
 assert.throws( function() { t.find( {a:{$not:{$nin:[{$elemMatch:{b:1}}]}}} ).itcount(); } );
 
 // NOTE Above we don't check cases like {b:2,$elemMatch:{b:3,4}} - generally
