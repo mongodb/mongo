2afc1990522e7409d3be4c4312c55d213bb59ca3
Neil Hooey <nhooey@gmail.com>
2011-08-10 00:50:58 -0400

    Added spacing to an error message.
    This fixes the "Can't modify shard key's value field" message where
    the word "field" would be adjacent to the actual field name.

--- s/strategy_shard.cpp
+++ s/strategy_shard.cpp
@@ -294,7 +294,7 @@ namespace mongo {
                         while(fields.more()) {
                             const string field = fields.next().fieldName();
                             uassert(13123,
-                                    str::stream() << "Can't modify shard key's value field" << field
+                                    str::stream() << "Can't modify shard key's value field: " << field
                                     << " for collection: " << manager->getns(),
                                     ! manager->getShardKey().partOfShardKey(field));
                         }
