f3094a442cf2556cc849ef62aef519c9adabf6ee
Keith Bostic <keith.bostic@wiredtiger.com>
2011-08-15 17:28:57 -0400

    Make this code look like the rest of WiredTiger. The only significant changes are: explicitly ignore the return from conn->add_compressor, get rid of the stderr messages we're not going to keep long-term, and isolate the one big one we are (I don't yet know how to push those errors to the session handle, but that's what we have to do), call out all of the Bzip2 errors the include file lists.
    --HG--
    extra : rebase_source : ee90488809dba4a74d8092facecf136404c1875f

--- ext/compressors/bzip2_compress/bzip2_compress.c
+++ ext/compressors/bzip2_compress/bzip2_compress.c
@@ -1,17 +1,18 @@
-#include <string.h>
-#include <errno.h>
-#include <wiredtiger_ext.h>
 #include <bzlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include <wiredtiger.h>
+#include <wiredtiger_ext.h>
+
+#define	WT_UNUSED(v)	((void)(v))
 
 WT_EXTENSION_API *wt_api;
 
 static int
-bzip2_compress(WT_COMPRESSOR *compressor, WT_SESSION *session,
-    const WT_ITEM *source, WT_ITEM *dest);
-	
+bzip2_compress(WT_COMPRESSOR *, WT_SESSION *, const WT_ITEM *, WT_ITEM *);
 static int
-bzip2_decompress(WT_COMPRESSOR *compressor, WT_SESSION *session,
-    const WT_ITEM *source, WT_ITEM *dest);
+bzip2_decompress(WT_COMPRESSOR *, WT_SESSION *, const WT_ITEM *, WT_ITEM *);
 
 static WT_COMPRESSOR bzip2_compressor = { bzip2_compress, bzip2_decompress };
 
@@ -21,78 +22,91 @@ static const int bz_verbosity = 0;
 /* between 1-9: set the block size to 100k x this number (compression only) */
 static const int bz_blocksize100k = 1;
 
-/* between 0-250: workFactor: see bzip2 manual.  0 is a reasonable default (compression only) */
+/*
+ * between 0-250: workFactor: see bzip2 manual.  0 is a reasonable default
+ * (compression only)
+ */
 static const int bz_workfactor = 0;
 
 /* if nonzero, decompress using less memory, but slower (decompression only) */
 static const int bz_small = 0;
 
-int wiredtiger_extension_init(WT_CONNECTION *conn, WT_EXTENSION_API *api, const char *config)
+int
+wiredtiger_extension_init(
+    WT_CONNECTION *conn, WT_EXTENSION_API *api, const char *config)
 {
 	wt_api = api;
+        WT_UNUSED(config);
 
-        (void)config;
-        /* fprintf(stderr, "bzip2_compress: extension_init called!\n"); */
-	conn->add_compressor(conn, "bzip2_compress", &bzip2_compressor, NULL);
-        return (0);
+	(void)conn->add_compressor(
+	    conn, "bzip2_compress", &bzip2_compressor, NULL);
+	return (0);
 }
 
 /* For OS X */
 __attribute__((destructor))
 static void _fini(void) {
-        /* fprintf(stderr, "bzip2_compress: _fini called!\n"); */
 }
 
 /* Convert a Bzip2 library return code to WT error code */
 static int
-bzip2_convert_error(int bzret)
+bzip2_convert_error(WT_SESSION *wt_session, int bzret)
 {
-	int wtret;
+	const char *msg;
 
-	/* Specific error numbers shown via fprintf can be decoded in bzlib.h */
-	switch (bzret) {
-	case BZ_OUTBUFF_FULL:
-		wtret = WT_TOOSMALL;
-		break;
-	case BZ_CONFIG_ERROR:
-	case BZ_PARAM_ERROR:
-		fprintf(stderr, "bzip2_compress: compress internal config error %d\n", bzret);
-		wtret = WT_ERROR;
-		break;
+	WT_UNUSED(wt_session);
+
+	switch (bzret) {		/* Some errors are anticipated */
 	case BZ_MEM_ERROR:
-		wtret = ENOMEM;
+		return (ENOMEM);
+	case BZ_OUTBUFF_FULL:
+		return (WT_TOOSMALL);
+	}
+
+	switch (bzret) {		/* Some errors are unexpected */
+	case BZ_SEQUENCE_ERROR:
+		msg = "BZ_SEQUENCE_ERROR";
+		break;
+	case BZ_PARAM_ERROR:
+		msg = "BZ_PARAM_ERROR";
 		break;
 	case BZ_DATA_ERROR:
+		msg = "BZ_DATA_ERROR";
+		break;
 	case BZ_DATA_ERROR_MAGIC:
+		msg = "BZ_DATA_ERROR_MAGIC";
+		break;
+	case BZ_IO_ERROR:
+		msg = "BZ_IO_ERROR";
+		break;
 	case BZ_UNEXPECTED_EOF:
-		fprintf(stderr, "bzip2_compress: compress internal config error %d\n", bzret);
-		wtret = WT_ERROR;
+		msg = "BZ_UNEXPECTED_EOF";
+		break;
+	case BZ_CONFIG_ERROR:
+		msg = "BZ_CONFIG_ERROR";
 		break;
 	default:
-		fprintf(stderr, "bzip2_compress: compress error %d\n", bzret);
-		wtret = WT_ERROR;
+		msg = "unknown error";
 		break;
 	}
-	return (wtret);
+
+	/*
+	 * XXX
+	 * This needs to get pushed back to the session handle somehow.
+	 */
+	fprintf(stderr, "bzip2 error: %s: %d\n", msg, bzret);
+	return (WT_ERROR);
 }
 
 /* Implementation of WT_COMPRESSOR for WT_CONNECTION::add_compressor. */
 static int
-bzip2_compress(WT_COMPRESSOR *compressor, WT_SESSION *session,
-    const WT_ITEM *source, WT_ITEM *dest)
+bzip2_compress(WT_COMPRESSOR *compressor,
+    WT_SESSION *wt_session, const WT_ITEM *source, WT_ITEM *dest)
 {
-        /* Unused parameters */
-        (void)compressor;
-        (void)session;
-	int ret;
+	u_int destlen;
 	int bzret;
-	unsigned int destlen;
 
-#ifdef TRACE_BZIP_CALLS
-	static int ncalls = -1;
-        if (++ncalls % 1000 == 0)
-		fprintf(stderr, "bzip2_compress: compress called %d times\n", ncalls);
-#endif
+	WT_UNUSED(compressor);
 
 	destlen = dest->size;
 	bzret = BZ2_bzBuffToBuffCompress((char *)dest->data, &destlen,
@@ -101,43 +115,29 @@ bzip2_compress(WT_COMPRESSOR *compressor, WT_SESSION *session,
 
 	if (bzret == BZ_OK) {
 		dest->size = destlen;
-		ret = 0;
+		return (0);
 	}
-	else
-		ret = bzip2_convert_error(bzret);
-
-        return (ret);
+	return (bzip2_convert_error(wt_session, bzret));
 }
 
 static int
-bzip2_decompress(WT_COMPRESSOR *compressor, WT_SESSION *session,
-    const WT_ITEM *source, WT_ITEM *dest)
+bzip2_decompress(WT_COMPRESSOR *compressor,
+    WT_SESSION *wt_session, const WT_ITEM *source, WT_ITEM *dest)
 {
-        /* Unused parameters */
-        (void)compressor;
-        (void)session;
-	int ret;
 	int bzret;
-	unsigned int destlen;
+	u_int destlen;
 
-#ifdef TRACE_BZIP_CALLS
-	static int ncalls = -1;
-        if (++ncalls % 1000 == 0)
-		fprintf(stderr, "bzip2_compress: decompress called %d times\n", ncalls);
-#endif
+	WT_UNUSED(compressor);
 
 	destlen = dest->size;
 	bzret = BZ2_bzBuffToBuffDecompress((char *)dest->data, &destlen,
-	    (char *)source->data, (unsigned int)source->size,
+	    (char *)source->data, (u_int)source->size,
 	    (int)bz_small, (int)bz_verbosity);
 
 	if (bzret == BZ_OK) {
 		dest->size = destlen;
-		ret = 0;
+		return (0);
 	}
-	else
-		ret = bzip2_convert_error(bzret);
-
-        return (ret);
+	return (bzip2_convert_error(wt_session, bzret));
 }
 /* End implementation of WT_COMPRESSOR. */
--- ext/compressors/nop_compress/nop_compress.c
+++ ext/compressors/nop_compress/nop_compress.c
@@ -1,65 +1,65 @@
-#include <wiredtiger.h>
-#include <string.h>
 #include <errno.h>
+#include <string.h>
+
+#include <wiredtiger.h>
+#include <wiredtiger_ext.h>
+
+#define	WT_UNUSED(v)	((void)(v))
 
 static int
-nop_compress(WT_COMPRESSOR *compressor, WT_SESSION *session,
-    const WT_ITEM *source, WT_ITEM *dest);
-	
+nop_compress(WT_COMPRESSOR *, WT_SESSION *, const WT_ITEM *, WT_ITEM *);
 static int
-nop_decompress(WT_COMPRESSOR *compressor, WT_SESSION *session,
-    const WT_ITEM *source, WT_ITEM *dest);
+nop_decompress(WT_COMPRESSOR *, WT_SESSION *, const WT_ITEM *, WT_ITEM *);
 
 static WT_COMPRESSOR nop_compressor = { nop_compress, nop_decompress };
 
-int wiredtiger_extension_init(WT_CONNECTION *conn,
-    WT_EXTENSION_API *api, const char *config)
+int
+wiredtiger_extension_init(
+    WT_CONNECTION *conn, WT_EXTENSION_API *api, const char *config)
 {
-        (void)api;
-        (void)config;
-        //fprintf(stderr, "nop_compress: extension_init called!\n");
-	conn->add_compressor(conn, "nop_compress", &nop_compressor, NULL);
-        return (0);
+	WT_UNUSED(api);
+	WT_UNUSED(config);
+
+	(void)conn->add_compressor(conn, "nop_compress", &nop_compressor, NULL);
+	return (0);
 }
 
 /* For OS X */
 __attribute__((destructor))
 static void _fini(void) {
-        //fprintf(stderr, "nop_compress: _fini called!\n");
 }
 
 /* Implementation of WT_COMPRESSOR for WT_CONNECTION::add_compressor. */
 static int
-nop_compress(WT_COMPRESSOR *compressor, WT_SESSION *session,
-    const WT_ITEM *source, WT_ITEM *dest)
+nop_compress(WT_COMPRESSOR *compressor,
+    WT_SESSION *session, const WT_ITEM *source, WT_ITEM *dest)
 {
-        /* Unused parameters */
-        (void)compressor;
-        (void)session;
+	WT_UNUSED(compressor);
+	WT_UNUSED(session);
 
-        //fprintf(stderr, "nop_compress: compress called!\n");
         if (dest->size < source->size) {
                 dest->size = source->size;
                 return (ENOMEM);
         }
+
         memcpy((void *)dest->data, source->data, source->size);
         dest->size = source->size;
+
         return (0);
 }
 
 static int
-nop_decompress(WT_COMPRESSOR *compressor, WT_SESSION *session,
-    const WT_ITEM *source, WT_ITEM *dest)
+nop_decompress(WT_COMPRESSOR *compressor,
+    WT_SESSION *session, const WT_ITEM *source, WT_ITEM *dest)
 {
-        /* Unused parameters */
-        (void)compressor;
-        (void)session;
+	WT_UNUSED(compressor);
+	WT_UNUSED(session);
 
-        //fprintf(stderr, "nop_compress: decompress called!\n");
         if (dest->size < source->size) {
                 dest->size = source->size;
                 return (ENOMEM);
         }
+
         memcpy((void *)dest->data, source->data, source->size);
         dest->size = source->size;
         return (0);
