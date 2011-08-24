25c2fbff89d19b86183032b12f9752985ad7a6d1
Spencer T Brody <spencer@10gen.com>
2011-08-24 15:07:30 -0400

    Slight addition to date test.  QA-42.

--- jstests/date3.js
+++ jstests/date3.js
@@ -7,6 +7,7 @@ d1 = new Date(-1000)
 dz = new Date(0)
 d2 = new Date(1000)
 
+t.save( {x: 3, d: dz} )
 t.save( {x: 2, d: d2} )
 t.save( {x: 1, d: d1} )
 
@@ -19,9 +20,10 @@ function test () {
 	assert.eq ( 1, list.size() )
 	assert.eq ( 2, list[0].x )
 	var list = t.find().sort( {d:1} )
-	assert.eq ( 2, list.size() )
+	assert.eq ( 3, list.size() )
 	assert.eq ( 1, list[0].x )
-	assert.eq ( 2, list[1].x )
+	assert.eq ( 3, list[1].x )
+	assert.eq ( 2, list[2].x )
 }
 
 test()
