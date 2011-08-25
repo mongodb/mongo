1450e4ccdf72c77f9e5b8d037bcaff732d863ab3
Spencer T Brody <spencer@10gen.com>
2011-08-25 12:50:25 -0400

    Make date-sorting test deterministic.

--- jstests/sort10.js
+++ jstests/sort10.js
@@ -1,24 +1,24 @@
 // signed dates check
 t = db.sort2;
 
-var opts = {};
-if (Math.random() < 0.3) {
-    opts.background = true;
-    printjson(opts);
-}
-t.drop();
-t.insert({ x: new Date(50000) });
-t.insert({ x: new Date(-50) });
-var d = new Date(-50);
-for (var pass = 0; pass < 2; pass++) {
-    assert(t.find().sort({x:1})[0].x.valueOf() == d.valueOf());
-    t.ensureIndex({ x: 1 }, opts);
-    t.insert({ x: new Date() });
+function checkSorting1(opts) {
+    t.drop();
+    t.insert({ x: new Date(50000) });
+    t.insert({ x: new Date(-50) });
+    var d = new Date(-50);
+    for (var pass = 0; pass < 2; pass++) {
+        assert(t.find().sort({x:1})[0].x.valueOf() == d.valueOf());
+        t.ensureIndex({ x: 1 }, opts);
+        t.insert({ x: new Date() });
+    }
 }
 
+checkSorting1({})
+checkSorting1({"background":true})
 
 
-function checkSorting(dates, sortOrder) {
+
+function checkSorting2(dates, sortOrder) {
     cur = t.find().sort({x:sortOrder});
     assert.eq(dates.length, cur.count(), "Incorrect number of results returned");
     index = 0;
@@ -37,12 +37,12 @@ for (var i = 0; i < dates.length; i++) {
 dates.sort(function(a,b){return a - b});
 reverseDates = dates.slice(0).reverse()
 
-checkSorting(dates, 1)
-checkSorting(reverseDates, -1)
+checkSorting2(dates, 1)
+checkSorting2(reverseDates, -1)
 t.ensureIndex({x:1})
-checkSorting(dates, 1)
-checkSorting(reverseDates, -1)
+checkSorting2(dates, 1)
+checkSorting2(reverseDates, -1)
 t.dropIndexes()
 t.ensureIndex({x:-1})
-checkSorting(dates, 1)
-checkSorting(reverseDates, -1)
+checkSorting2(dates, 1)
+checkSorting2(reverseDates, -1)
