a986c28c283698718724aeed44767d22ff9721e9
Keith Bostic <keith.bostic@wiredtiger.com>
2011-09-07 11:09:54 +0000

    whitespace
    --HG--
    extra : rebase_source : 428b4a98b33b77eb1e020abb44b737fa3b43c6c7

--- src/btree/col_modify.c
+++ src/btree/col_modify.c
@@ -8,7 +8,7 @@
 #include "wt_internal.h"
 
 static int __col_insert_alloc(
-		WT_SESSION_IMPL *, uint64_t, u_int, WT_INSERT **, size_t *);
+	WT_SESSION_IMPL *, uint64_t, u_int, WT_INSERT **, size_t *);
 
 /*
  * __wt_col_modify --
