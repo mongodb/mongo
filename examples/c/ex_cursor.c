d2ae483c6d798122b6313fff83d26e6312e34b38
Keith Bostic <keith.bostic@wiredtiger.com>
2011-09-19 11:55:07 +0000

    Write a bunch more cursor text for the docs.
    --HG--
    extra : rebase_source : 8659c19b4f9099d8038cf1ae7916f9f2c6a1c1b4

--- examples/c/ex_cursor.c
+++ examples/c/ex_cursor.c
@@ -15,12 +15,176 @@
 
 const char *home = "WT_TEST";
 
+static void
+initialize_key(WT_ITEM *key)
+{
+	key->data = "foo";
+	key->size = 3;
+}
+
+static void
+initialize_value(WT_ITEM *val)
+{
+	val->data = "bar";
+	val->size = 3;
+}
+
+static int
+first(WT_CURSOR *cursor)
+{
+	WT_ITEM key, value;
+	int ret;
+
+	while ((ret = cursor->first(cursor)) == 0) {
+		if ((ret = cursor->get_key(cursor, &key)) != 0)
+			return (ret);
+		if ((ret = cursor->get_value(cursor, &value)) != 0)
+			return (ret);
+	}
+	return (ret);
+}
+
+static int
+last(WT_CURSOR *cursor)
+{
+	WT_ITEM key, value;
+	int ret;
+
+	while ((ret = cursor->first(cursor)) == 0) {
+		if ((ret = cursor->get_key(cursor, &key)) != 0)
+			return (ret);
+		if ((ret = cursor->get_value(cursor, &value)) != 0)
+			return (ret);
+	}
+	return (ret);
+}
+
+static int
+forward_traversal(WT_CURSOR *cursor)
+{
+	WT_ITEM key, value;
+	int ret;
+
+	while ((ret = cursor->next(cursor)) == 0) {
+		if ((ret = cursor->get_key(cursor, &key)) != 0)
+			return (ret);
+		if ((ret = cursor->get_value(cursor, &value)) != 0)
+			return (ret);
+	}
+	return (ret);
+}
+
+static int
+backward_traversal(WT_CURSOR *cursor)
+{
+	WT_ITEM key, value;
+	int ret;
+
+	while ((ret = cursor->prev(cursor)) == 0) {
+		if ((ret = cursor->get_key(cursor, &key)) != 0)
+			return (ret);
+		if ((ret = cursor->get_value(cursor, &value)) != 0)
+			return (ret);
+	}
+	return (ret);
+}
+
+static int
+search(WT_CURSOR *cursor)
+{
+	WT_ITEM key, value;
+	int ret;
+
+	initialize_key(&key);
+	cursor->set_key(cursor, &key);
+
+	if ((ret = cursor->search(cursor)) != 0)
+		return (ret);
+
+	if ((ret = cursor->get_value(cursor, &value)) != 0)
+		return (ret);
+
+	return (0);
+}
+
+static int
+search_near(WT_CURSOR *cursor)
+{
+	WT_ITEM key, value;
+	int exact, ret;
+
+	initialize_key(&key);
+	cursor->set_key(cursor, &key);
+
+	if ((ret = cursor->search_near(cursor, &exact)) != 0)
+		return (ret);
+
+	switch (exact) {
+	case -1:		/* Returned key smaller than search key */
+		if ((ret = cursor->get_key(cursor, &key)) != 0)
+			return (ret);
+		break;
+	case 0:			/* Exact match found */
+		break;
+	case 1:			/* Returned key larger than search key */
+		if ((ret = cursor->get_key(cursor, &key)) != 0)
+			return (ret);
+		break;
+	}
+
+	if ((ret = cursor->get_value(cursor, &value)) != 0)
+		return (ret);
+
+	return (0);
+}
+
+static int
+cursor_insert(WT_CURSOR *cursor)
+{
+	WT_ITEM key, value;
+	int ret;
+
+	initialize_key(&key);
+	cursor->set_key(cursor, &key);
+	initialize_value(&value);
+	cursor->set_value(cursor, &value);
+
+	return (cursor->update(cursor));
+}
+
+static int
+cursor_update(WT_CURSOR *cursor)
+{
+	WT_ITEM key, value;
+	int ret;
+
+	initialize_key(&key);
+	cursor->set_key(cursor, &key);
+	initialize_value(&value);
+	cursor->set_value(cursor, &value);
+
+	return (cursor->update(cursor));
+}
+
+static int
+cursor_remove(WT_CURSOR *cursor)
+{
+	WT_ITEM key, value;
+	int ret;
+
+	initialize_key(&key);
+	cursor->set_key(cursor, &key);
+
+	return (cursor->remove(cursor));
+}
+
 int main(void)
 {
-	int ret;
 	WT_CONNECTION *conn;
-	WT_SESSION *session;
 	WT_CURSOR *cursor;
+	WT_ITEM key, value;
+	WT_SESSION *session;
+	int ret;
 
 	/* Open a connection to the database, creating it if necessary. */
 	if ((ret = wiredtiger_open(home, NULL, "create", &conn)) != 0)
@@ -39,8 +203,8 @@ int main(void)
 	ret = session->open_cursor(session, "table:map", NULL, NULL, &cursor);
 	cursor->close(cursor, NULL);
 
-	ret = session->open_cursor(session, "table:map(country,population)",
-	    NULL, NULL, &cursor);
+	ret = session->open_cursor(
+	    session, "table:map(country,population)", NULL, NULL, &cursor);
 	cursor->close(cursor, NULL);
 
 	ret = session->open_cursor(session, "table:", NULL, NULL, &cursor);
--- examples/c/ex_stat.c
+++ examples/c/ex_stat.c
@@ -8,21 +8,68 @@
  *	This is an example demonstrating how to query a database's statistics.
  */
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <wiredtiger.h>
 
 const char *home = "WT_TEST";
 
-int main(void)
+int 
+statistics(WT_SESSION *session)
 {
+	WT_CURSOR *cursor;
+	const char *description, *pvalue;
 	int ret;
+	uint64_t value;
+
+	if ((ret = session->open_cursor(
+	    session, "statistics:", NULL, NULL, &cursor)) != 0)
+		return (ret);
+
+	while ((ret = cursor->next(cursor)) == 0) {
+		if ((ret = cursor->get_value(
+		    cursor, &value, &pvalue, &description)) != 0)
+			return (ret);
+		printf("%s=%s\n", description, pvalue);
+	}
+
+	if (ret == WT_NOTFOUND)
+		ret = 0;
+
+	return (ret);
+}
+
+int 
+statistics_file(WT_SESSION *session, const char *filename)
+{
+	WT_CURSOR *cursor;
+	size_t len;
+	char *object;
+	int ret;
+
+	len = strlen("statistics:") + strlen(filename) + 1;
+	if ((object = malloc(len)) == NULL)
+		return (ENOMEM);
+	(void)snprintf(object, len, "statistics:%s", filename);
+	if ((ret = session->open_cursor(
+	    session, object, NULL, NULL, &cursor)) != 0)
+		return (ret);
+
+	/* Display statistics. */
+
+	return (0);
+}
+
+int
+main(void)
+{
 	WT_CONNECTION *conn;
 	WT_SESSION *session;
-	WT_CURSOR *cursor;
-	const char *key;
-	uint64_t value;
+	const char *f = "foo";
+	int ret;
 
 	if ((ret = wiredtiger_open(home, NULL, "create", &conn)) != 0 ||
 	    (ret = conn->open_session(conn, NULL, NULL, &session)) != 0)
@@ -30,19 +77,7 @@ int main(void)
 		    home, wiredtiger_strerror(ret));
 	/* Note: further error checking omitted for clarity. */
 
-	/* Open a cursor on the (virtual) statistics table. */
-	ret = session->open_cursor(session, "statistics:", NULL, NULL, &cursor);
+	ret = statistics(session);
 
-	while ((ret = cursor->next(cursor)) == 0) {
-		cursor->get_key(cursor, &key);
-		cursor->get_value(cursor, &value);
-
-		printf("Got statistic: %s = %d\n", key, (int)value);
-	}
-
-	ret = conn->close(conn, NULL);
-
-	return (ret);
+	return (conn->close(conn, NULL) == 0 ? ret : EXIT_FAILURE);
 }
-
-
