1329db4856468f2546a03f8947351f636512ef8b
agirbal <antoine@10gen.com>
2011-09-20 15:16:31 -0700

    SERVER-1507: test

--- jstests/shell1.js
+++ jstests/shell1.js
@@ -4,3 +4,12 @@ x = 1;
 shellHelper( "show", "tables;" )
 shellHelper( "show", "tables" )
 shellHelper( "show", "tables ;" )
+
+setVerboseShell();
+var res = db.test.remove({a:1});
+var res2 = db.test.update({a:1}, {b: 1});
+assert(res != undefined && res2 != undefined, "verbose shell 1")
+setVerboseShell(false);
+var res = db.test.remove({a:1});
+assert(res == undefined, "verbose shell 2")
+
