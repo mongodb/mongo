3e599665f80b6e40bffa1ff7e293d7f92a8f5149
gregs <greg@10gen.com>
2011-10-20 13:44:36 -0400

    spelling

--- client/connpool.cpp
+++ client/connpool.cpp
@@ -38,7 +38,7 @@ namespace mongo {
 
     void PoolForHost::done( DBConnectionPool * pool, DBClientBase * c ) {
         if ( _pool.size() >= _maxPerHost ) {
-            pool->onDestory( c );
+            pool->onDestroy( c );
             delete c;
         }
         else {
@@ -55,7 +55,7 @@ namespace mongo {
             _pool.pop();
             
             if ( ! sc.ok( now ) )  {
-                pool->onDestory( sc.conn );
+                pool->onDestroy( sc.conn );
                 delete sc.conn;
                 continue;
             }
@@ -185,7 +185,7 @@ namespace mongo {
 
     void DBConnectionPool::release(const string& host, DBClientBase *c) {
         if ( c->isFailed() ) {
-            onDestory( c );
+            onDestroy( c );
             delete c;
             return;
         }
@@ -228,12 +228,12 @@ namespace mongo {
         }
     }
 
-    void DBConnectionPool::onDestory( DBClientBase * conn ) {
+    void DBConnectionPool::onDestroy( DBClientBase * conn ) {
         if ( _hooks->size() == 0 )
             return;
 
         for ( list<DBConnectionHook*>::iterator i = _hooks->begin(); i != _hooks->end(); i++ ) {
-            (*i)->onDestory( conn );
+            (*i)->onDestroy( conn );
         }
     }
 
@@ -357,7 +357,7 @@ namespace mongo {
 
         for ( size_t i=0; i<toDelete.size(); i++ ) {
             try {
-                onDestory( toDelete[i] );
+                onDestroy( toDelete[i] );
                 delete toDelete[i];
             }
             catch ( ... ) {
