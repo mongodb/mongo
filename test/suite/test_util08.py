0069ea0b5edc13d8420ab9c5a4b2a98ebf794c34
Don Anderson <dda@ddanderson.com>
2011-11-03 15:11:32 -0400

    Added test for copyright refs #16

---
+++ test/suite/test_util08.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+# See the file LICENSE for redistribution information.
+#
+# Copyright (c) 2008-2011 WiredTiger, Inc.
+#	All rights reserved.
+#
+# test_util05.py
+# 	Utilities: wt copyright
+#
+
+import unittest
+from wiredtiger import WiredTigerError
+import wttest
+from suite_subprocess import suite_subprocess
+
+class test_util06(wttest.WiredTigerTestCase, suite_subprocess):
+    def test_copyright(self):
+        """
+        Test copyright in a 'wt' process
+        """
+        outfile = "copyrightout.txt"
+        self.runWt(["copyright"], outfilename=outfile)
+        with open(outfile, 'r') as f:
+            text = f.read(1000)
+            self.assertTrue('Copyright' in text)
+
+
+if __name__ == '__main__':
+    wttest.run()
