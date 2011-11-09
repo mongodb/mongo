d55ed096013710f33ab140bc96bf09737b6896a2
Pedro Larroy <pedro.larroy@nokia.com>
2011-11-09 12:16:26 +0100

    update gch.py with fixes for variant dir builds and compiler flags to avoid an invalid pch depending on the compiler flags

--- gch.py
+++ gch.py
@@ -1,4 +1,3 @@
-# $Id$
 #
 # SCons builder for gcc's precompiled headers
 # Copyright (C) 2006  Tim Blechmann
@@ -18,16 +17,19 @@
 # the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
-# $Revision$
-# $LastChangedRevision$
-# $LastChangedDate$
-# $LastChangedBy$
+# 1.1
+#
+# 09-11-2011 Pedro Larroy: Fixed dependency emitter not working with variant dir
+#
+#
+
 
 import SCons.Action
 import SCons.Builder
 import SCons.Scanner.C
 import SCons.Util
 import SCons.Script
+import os
 
 SCons.Script.EnsureSConsVersion(0,96,92)
 
@@ -37,10 +39,6 @@ GchShAction = SCons.Action.Action('$GCHSHCOM', '$GCHSHCOMSTR')
 def gen_suffix(env, sources):
     return sources[0].get_suffix() + env['GCHSUFFIX']
 
-def header_path(node):
-    path = node.path
-    return path[:-4] # strip final '.gch'
-
 GchShBuilder = SCons.Builder.Builder(action = GchShAction,
                                      source_scanner = SCons.Scanner.C.CScanner(),
                                      suffix = gen_suffix)
@@ -49,15 +47,29 @@ GchBuilder = SCons.Builder.Builder(action = GchAction,
                                    source_scanner = SCons.Scanner.C.CScanner(),
                                    suffix = gen_suffix)
 
+def header_path(node):
+    h_path = node.abspath
+    idx = h_path.rfind('.gch')
+    if idx != -1:
+        h_path = h_path[0:idx]
+        if not os.path.isfile(h_path):
+            raise SCons.Errors.StopError("can't find header file: {0}".format(h_path))
+        return h_path
+
+    else:
+        raise SCons.Errors.StopError("{0} file doesn't have .gch extension".format(h_path))
+
+
 def static_pch_emitter(target,source,env):
     SCons.Defaults.StaticObjectEmitter( target, source, env )
-
     scanner = SCons.Scanner.C.CScanner()
     path = scanner.path(env)
-    deps = scanner(source[0], env, path)
 
-    if env.has_key('Gch') and env['Gch']:
-        if header_path(env['Gch']) in [x.path for x in deps]:
+    deps = scanner(source[0], env, path)
+    if env.get('Gch'):
+        h_path = header_path(env['Gch'])
+        if h_path in [x.abspath for x in deps]:
+            #print 'Found dep. on pch: ', target[0], ' -> ', env['Gch']
             env.Depends(target, env['Gch'])
 
     return (target, source)
@@ -69,9 +81,12 @@ def shared_pch_emitter(target,source,env):
     path = scanner.path(env)
     deps = scanner(source[0], env, path)
 
-    if env.has_key('GchSh') and env['GchSh']:
-        if header_path(env['GchSh']) in [x.path for x in deps]:
+    if env.get('GchSh'):
+        h_path = header_path(env['GchSh'])
+        if h_path in [x.abspath for x in deps]:
+            #print 'Found dep. on pch (shared): ', target[0], ' -> ', env['Gch']
             env.Depends(target, env['GchSh'])
+
     return (target, source)
 
 def generate(env):
@@ -98,8 +113,8 @@ def generate(env):
         env['BUILDERS']['Gch'] = bld
         env['BUILDERS']['GchSh'] = bldsh
 
-    env['GCHCOM']     = '$CXX -o $TARGET -x c++-header -c $CXXFLAGS $_CCCOMCOM $SOURCE'
-    env['GCHSHCOM']   = '$CXX -o $TARGET -x c++-header -c $SHCXXFLAGS $_CCCOMCOM $SOURCE'
+    env['GCHCOM']     = '$CXX -Wall -o $TARGET -x c++-header -c $CXXFLAGS $CCFLAGS $_CCCOMCOM $SOURCE'
+    env['GCHSHCOM']   = '$CXX -o $TARGET -x c++-header -c $SHCXXFLAGS $CCFLAGS $_CCCOMCOM $SOURCE'
     env['GCHSUFFIX']  = '.gch'
 
     for suffix in SCons.Util.Split('.c .C .cc .cxx .cpp .c++'):
