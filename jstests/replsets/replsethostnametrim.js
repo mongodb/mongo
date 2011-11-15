8770fd7de4cf4aae9d4021906300373a932f6b0f
Scott Hernandez <scotthernandez@gmail.com>
2011-11-15 11:48:51 -0500

    SERVER-4244: cleanup test, remove dugging print statements.

--- jstests/replsets/replsethostnametrim.js
+++ jstests/replsets/replsethostnametrim.js
@@ -10,12 +10,12 @@ config.version++;
 var origHost = config.members[0].host;
 config.members[0].host = origHost + " ";
 var result = master.adminCommand({replSetReconfig : config});
-assert.eq(result.ok, 0);
-print("current (bad) config:"); printjson(config);
+assert.eq(result.ok, 1, tojson(result));
+//print("current (bad) config:"); printjson(config);
 
-//check new config
+//check new config to make sure it doesn't have a space in the hostname
 config = master.getDB("local").system.replset.findOne();
 assert.eq(origHost, config.members[0].host);
-print("current (good) config:"); printjson(config);
+//print("current (good) config:"); printjson(config);
 
 replTest.stopSet();
