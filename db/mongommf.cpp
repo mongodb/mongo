ac2ac5d3a4923b13402307aa1f32115cf30b9648
dwight <dwight@10gen.com>
2011-12-01 10:31:36 -0500

    logging and asserts

--- db/mongommf.cpp
+++ db/mongommf.cpp
@@ -267,18 +267,21 @@ namespace mongo {
     }
 
     bool MongoMMF::open(string fname, bool sequentialHint) {
+        LOG(3) << "mmf open " << fname << endl;
         setPath(fname);
         _view_write = mapWithOptions(fname.c_str(), sequentialHint ? SEQUENTIAL : 0);
         return finishOpening();
     }
 
     bool MongoMMF::create(string fname, unsigned long long& len, bool sequentialHint) {
+        LOG(3) << "mmf create " << fname << endl;
         setPath(fname);
         _view_write = map(fname.c_str(), len, sequentialHint ? SEQUENTIAL : 0);
         return finishOpening();
     }
 
     bool MongoMMF::finishOpening() {
+        LOG(3) << "mmf finishOpening " << (void*) _view_write << ' ' << filename() << " len:" << length() << endl;
         if( _view_write ) {
             if( cmdLine.dur ) {
                 _view_private = createPrivateMap();
@@ -300,7 +303,10 @@ namespace mongo {
     }
 
     MongoMMF::~MongoMMF() {
-        close();
+        try { 
+            close();
+        }
+        catch(...) { error() << "exception in ~MongoMMF" << endl; }
     }
 
     namespace dur {
@@ -308,7 +314,10 @@ namespace mongo {
     }
 
     /*virtual*/ void MongoMMF::close() {
+        LOG(3) << "mmf close " << filename() << endl;
+
         if( cmdLine.dur && _view_write/*actually was opened*/ ) {
+            dbMutex.assertWriteLocked();
             dur::closingFileNotification();
         }
 
