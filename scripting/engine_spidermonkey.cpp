9e0a78c91474bc12e17c28dc6d91a972d89db142
Tad Marshall <tad@10gen.com>
2011-12-13 06:17:36 -0500

    SERVER-4432 make "cf_1_f_x) {shellAutocomplete(x)}(" go away
    Test for no function name specified and use a default name if the
    function is unnamed.  Start private internal names with leading
    underscores.  Provide a name for the function anyway.  Run the
    shellAutocomplete function inside try/catch for safety.

--- scripting/engine_spidermonkey.cpp
+++ scripting/engine_spidermonkey.cpp
@@ -440,11 +440,10 @@ namespace mongo {
 
             //cout << "RAW\n---\n" << raw << "\n---" << endl;
 
-            stringstream fname;
-            fname << "cf_";
             static int fnum = 1;
-            fname << "_" << fnum++ << "_";
-            
+            stringstream fname;
+            fname << "__cf__" << fnum++ << "__";
+
             if ( ! hasFunctionIdentifier( raw ) ) {
                 string s = raw;
                 if ( isSimpleStatement( s ) ) {
@@ -460,9 +459,8 @@ namespace mongo {
             size_t start = code.find( '(' );
             assert( start != string::npos );
 
-            string fbase = trim( code.substr( 9 , start - 9 ) );
-
-            fname << "_f_" << fbase;
+            string fbase = ( start > 9 ) ? trim( code.substr( 9 , start - 9 ) ) : "anonymous_function";
+            fname << "f__" << fbase;
 
             code = code.substr( start + 1 );
             size_t end = code.find( ')' );
@@ -488,7 +486,7 @@ namespace mongo {
                 paramArray[i] = params[i].c_str();
             
             // avoid munging previously munged name (kludge; switching to v8 fixes underlying issue)
-            if ( fbase.find("cf__") != 0 && fbase.find("_f_") == string::npos ) {
+            if ( fbase.find("__cf__") != 0 && fbase.find("__f__") == string::npos ) {
                 fbase = fname.str();
             }
 
