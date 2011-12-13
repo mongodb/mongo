82a53933565ba4162f85ef301727054fa774fe37
dwight <dwight@10gen.com>
2011-12-13 00:35:32 -0500

    buildbot count8.js wasnt yielding

--- db/ops/query.cpp
+++ db/ops/query.cpp
@@ -224,7 +224,7 @@ namespace mongo {
        returns -1 on ns does not exist error.
     */
     long long runCount( const char *ns, const BSONObj &cmd, string &err ) {
-        Client::ReadContext cx(ns);
+        Client::Context cx(ns);
         NamespaceDetails *d = nsdetails( ns );
         if ( !d ) {
             err = "ns missing";
