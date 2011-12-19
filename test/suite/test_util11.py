cb26da1f744ea0bb31d6c8261acc5c4eb5a614bd
Don Anderson <dda@ddanderson.com>
2011-12-19 12:15:42 -0500

    Added tests for 'wt list' and 'wt write'.  refs #16

---
+++ test/suite/test_util11.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+#
+# See the file LICENSE for redistribution information.
+#
+# Copyright (c) 2008-2011 WiredTiger, Inc.
+#	All rights reserved.
+#
+# test_util11.py
+#	Utilities: wt list
+#
+
+import unittest
+from wiredtiger import WiredTigerError
+import wttest
+from suite_subprocess import suite_subprocess
+import os
+import struct
+
+class test_util11(wttest.WiredTigerTestCase, suite_subprocess):
+    tablenamepfx = 'test_util11.'
+    session_params = 'key_format=S,value_format=S'
+
+    def populate(self, tablename):
+        """
+        Insert some simple entries into the table
+        """
+        cursor = self.session.open_cursor('table:' + tablename, None, None)
+        cursor.set_key('SOMEKEY')
+        cursor.set_value('SOMEVALUE')
+        cursor.close()
+
+    def test_list_none(self):
+        """
+        Test list in a 'wt' process, with no tables
+        """
+
+        # Construct what we think we'll find
+        filelist = 'file:__schema.wt\n'
+        outfile = "listout.txt"
+        self.KNOWN_FAILURE('"wt list" with empty schema returns WT_NOTFOUND')
+        self.runWt(["list"], outfilename=outfile)
+        self.check_file_content(outfile, filelist)
+
+    def test_list(self):
+        """
+        Test list in a 'wt' process, with a mix of populated and empty tables
+        """
+        pfx = self.tablenamepfx
+        params = self.session_params
+        self.session.create('table:' + pfx + '5', params)
+        self.session.create('table:' + pfx + '3', params)
+        self.session.create('table:' + pfx + '1', params)
+        self.session.create('table:' + pfx + '2', params)
+        self.session.create('table:' + pfx + '4', params)
+        self.populate(pfx + '2')
+        self.populate(pfx + '3')
+
+        # Construct what we think we'll find
+        filelist = 'file:__schema.wt\n'
+        tablelist = ''
+        for i in range(1, 6):
+            filelist += 'file:' + pfx + str(i) + '.wt\n'
+            tablelist += 'table:' + pfx + str(i) + '\n'
+
+        outfile = "listout.txt"
+        self.runWt(["list"], outfilename=outfile)
+        self.check_file_content(outfile, filelist + tablelist)
+
+    def test_list_drop(self):
+        """
+        Test list in a 'wt' process, with a mix of populated and empty tables,
+        after some tables have been dropped.
+        """
+        pfx = self.tablenamepfx
+        params = self.session_params
+        self.session.create('table:' + pfx + '5', params)
+        self.session.create('table:' + pfx + '3', params)
+        self.session.create('table:' + pfx + '1', params)
+        self.session.create('table:' + pfx + '2', params)
+        self.session.create('table:' + pfx + '4', params)
+        self.populate(pfx + '2')
+        self.populate(pfx + '3')
+        self.session.drop('table:' + pfx + '2', None)
+        self.session.drop('table:' + pfx + '4', None)
+
+        # Construct what we think we'll find
+        filelist = 'file:__schema.wt\n'
+        tablelist = ''
+        filelist += 'file:' + pfx + '1.wt\n'
+        tablelist += 'table:' + pfx + '1\n'
+        filelist += 'file:' + pfx + '3.wt\n'
+        tablelist += 'table:' + pfx + '3\n'
+        filelist += 'file:' + pfx + '5.wt\n'
+        tablelist += 'table:' + pfx + '5\n'
+
+        outfile = "listout.txt"
+        self.runWt(["list"], outfilename=outfile)
+        self.check_file_content(outfile, filelist + tablelist)
+
+    def test_list_drop_all(self):
+        """
+        Test list in a 'wt' process, with a mix of populated and empty tables,
+        after all tables have been dropped.
+        """
+        pfx = self.tablenamepfx
+        params = self.session_params
+        self.session.create('table:' + pfx + '5', params)
+        self.session.create('table:' + pfx + '3', params)
+        self.session.create('table:' + pfx + '1', params)
+        self.session.create('table:' + pfx + '2', params)
+        self.session.create('table:' + pfx + '4', params)
+        self.populate(pfx + '2')
+        self.populate(pfx + '3')
+        self.session.drop('table:' + pfx + '5', None)
+        self.session.drop('table:' + pfx + '4', None)
+        self.session.drop('table:' + pfx + '3', None)
+        self.session.drop('table:' + pfx + '2', None)
+        self.session.drop('table:' + pfx + '1', None)
+
+        # Construct what we think we'll find
+        filelist = 'file:__schema.wt\n'
+        outfile = "listout.txt"
+        self.runWt(["list"], outfilename=outfile)
+        self.check_file_content(outfile, filelist)
+
+
+if __name__ == '__main__':
+    wttest.run()
---
+++ test/suite/test_util12.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+#
+# See the file LICENSE for redistribution information.
+#
+# Copyright (c) 2008-2011 WiredTiger, Inc.
+#	All rights reserved.
+#
+# test_util12.py
+#	Utilities: wt write
+#
+
+import unittest
+import wiredtiger
+from wiredtiger import WiredTigerError
+import wttest
+from suite_subprocess import suite_subprocess
+import os
+import struct
+
+class test_util12(wttest.WiredTigerTestCase, suite_subprocess):
+    tablename = 'test_util12.a'
+    nentries = 1000
+    session_params = 'key_format=S,value_format=S'
+
+    def populate(self, tablename):
+        """
+        Insert some simple entries into the table
+        """
+        cursor = self.session.open_cursor('table:' + tablename, None, None)
+        cursor.set_key('SOMEKEY')
+        cursor.set_value('SOMEVALUE')
+        cursor.close()
+
+    def test_write(self):
+        self.session.create('table:' + self.tablename, self.session_params)
+        self.runWt(['write', 'table:' + self.tablename,
+                    'def', '456', 'abc', '123'])
+        cursor = self.session.open_cursor('table:' + self.tablename, None, None)
+        self.assertEqual(cursor.next(), 0)
+        self.assertEqual(cursor.get_key(), 'abc')
+        self.assertEqual(cursor.get_value(), '123')
+        self.assertEqual(cursor.next(), 0)
+        self.assertEqual(cursor.get_key(), 'def')
+        self.assertEqual(cursor.get_value(), '456')
+        self.assertEqual(cursor.next(), wiredtiger.WT_NOTFOUND)
+        cursor.close()
+
+    def test_write_no_keys(self):
+        """
+        Test write in a 'wt' process, with no args
+        """
+        self.session.create('table:' + self.tablename, self.session_params)
+
+        errfile = 'writeerr.txt'
+        self.runWt(['write', 'table:' + self.tablename], errfilename=errfile)
+        self.check_file_contains(errfile, 'usage:')
+
+    def test_write_overwrite(self):
+        self.session.create('table:' + self.tablename, self.session_params)
+        cursor = self.session.open_cursor('table:' + self.tablename, None, None)
+        cursor.set_key('def')
+        cursor.set_value('789')
+        cursor.close()
+        self.runWt(['write', 'table:' + self.tablename,
+                    'def', '456', 'abc', '123'])
+        cursor = self.session.open_cursor('table:' + self.tablename, None, None)
+        self.assertEqual(cursor.next(), 0)
+        self.assertEqual(cursor.get_key(), 'abc')
+        self.assertEqual(cursor.get_value(), '123')
+        self.assertEqual(cursor.next(), 0)
+        self.assertEqual(cursor.get_key(), 'def')
+        self.assertEqual(cursor.get_value(), '456')
+        self.assertEqual(cursor.next(), wiredtiger.WT_NOTFOUND)
+        cursor.close()
+
+    def test_write_bad_args(self):
+        self.session.create('table:' + self.tablename, self.session_params)
+        errfile = 'writeerr.txt'
+        self.runWt(['write', 'table:' + self.tablename,
+                    'def', '456', 'abc'], errfilename=errfile)
+        self.check_file_contains(errfile, 'usage:')
+
+
+if __name__ == '__main__':
+    wttest.run()
