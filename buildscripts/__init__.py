9016d3fd77e079b210e322189952902d45bf7e8d
Eliot Horowitz <eliot@10gen.com>
2011-12-25 12:02:26 -0500

    no more ubuntu hacks

--- buildscripts/__init__.py
+++ buildscripts/__init__.py
@@ -4,7 +4,4 @@ import hacks_ubuntu
 import os;
 
 def findHacks( un ):
-    if un[0] == 'Linux' and (os.path.exists("/etc/debian_version") or
-                             un[3].find("Ubuntu") >= 0):
-        return hacks_ubuntu
     return None
