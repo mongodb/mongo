54b37b38ca683fa6d0da87c06c7798802998a922
Eliot Horowitz <eliot@10gen.com>
2011-12-25 11:58:44 -0500

    old mandriva hack not needed

--- buildscripts/hacks_mandriva.py
+++
@@ -1,9 +0,0 @@
-
-import os
-import glob
-
-def insert( env , options ):
-    jslibPaths = glob.glob('/usr/include/js-*/')
-    if len(jslibPaths) >= 1:
-        jslibPath = jslibPaths.pop()
-        env.Append( CPPPATH=[ jslibPath ] )
