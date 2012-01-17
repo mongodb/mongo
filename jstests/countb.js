3f564bdae6adc11d0cefac87c76764e9eb4ace1e
Aaron <aaron@10gen.com>
2012-01-17 12:18:26 -0800

    add some fast count $where clause checks

--- jstests/countb.js
+++ jstests/countb.js
@@ -7,3 +7,5 @@ t.ensureIndex( {a:1} );
 t.save( {a:['a','b']} );
 assert.eq( 0, t.find( {a:{$in:['a'],$gt:'b'}} ).count() );
 assert.eq( 0, t.find( {$and:[{a:'a'},{a:{$gt:'b'}}]} ).count() );
+assert.eq( 1, t.find( {$and:[{a:'a'},{$where:"this.a[1]=='b'"}]} ).count() );
+assert.eq( 0, t.find( {$and:[{a:'a'},{$where:"this.a[1]!='b'"}]} ).count() );
