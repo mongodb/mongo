511bc4024d473af33da8c07b7e0b036647448295
Keith Bostic <keith.bostic@wiredtiger.com>
2012-01-23 12:03:23 +0000

    Update a comment, WT_BUF -> WT_ITEM.

--- src/support/scratch.c
+++ src/support/scratch.c
@@ -290,12 +290,11 @@ __wt_scr_alloc(WT_SESSION_IMPL *session, uint32_t size, WT_ITEM **scratchp)
 	*scratchp = NULL;
 
 	/*
-	 * There's an array of scratch buffers in each WT_SESSION_IMPL that can
-	 * be used by any function.  We use WT_ITEM structures for scratch
-	 * memory because we already have to have functions that do
-	 * variable-length allocation on WT_BUFs.  Scratch buffers are
-	 * allocated only by a single thread of control, so no locking is
-	 * necessary.
+	 * Each WT_SESSION_IMPL has an array of scratch buffers available for
+	 * use by any function.  We use WT_ITEM structures for scratch memory
+	 * because we already have functions that do variable-length allocation
+	 * on WT_ITEMs.  Scratch buffers are allocated only by a single thread
+	 * of control, so no locking is necessary.
 	 *
 	 * Walk the array, looking for a buffer we can use.
 	 */
