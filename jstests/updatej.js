4ad6e3c6b0e08a877cccf476ffa0c7fa694c2452
Aaron <aaron@10gen.com>
2012-01-25 14:40:46 -0800

    SERVER-4779 update test to characterize expected behavior

--- jstests/updatej.js
+++ jstests/updatej.js
@@ -1,4 +1,4 @@
-// Test that update validation failure doesn't terminate the update without modifying subsequent
+// Test that update validation failure terminates the update without modifying subsequent
 // documents.  SERVER-4779
 
 t = db.jstests_updatej;
@@ -9,6 +9,4 @@ t.save( {a:1} );
 t.save( {a:[]} );
 
 t.update( {}, {$push:{a:2}}, false, true );
-if ( 0 ) { // SERVER-4779
-assert.eq( 2, t.count( {a:2} ) );
-}
+assert.eq( 1, t.count( {a:2} ) );
