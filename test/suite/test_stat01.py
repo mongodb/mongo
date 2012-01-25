274b459466216dd25cab368eca82ef6b19c04044
Don Anderson <dda@ddanderson.com>
2012-01-25 10:54:02 -0500

    Added tests to get a filestat by key as well as stat by key. Added consistency checks to verify that all stats have a string integer value that is equal to the returned integer value. Made the test less verbose.  #refs 16.

--- test/suite/test_stat01.py
+++ test/suite/test_stat01.py
@@ -2,7 +2,7 @@
 #
 # See the file LICENSE for redistribution information.
 #
-# Copyright (c) 2008-2011 WiredTiger, Inc.
+# Copyright (c) 2008-2012 WiredTiger, Inc.
 #	All rights reserved.
 #
 # test_stat01.py
@@ -22,6 +22,14 @@ class test_stat01(wttest.WiredTigerTestCase):
     tablename = 'test_stat01.wt'
     nentries = 25
 
+    def statstr_to_int(self, str):
+        """
+        Convert a statistics value string, which may be in either form:
+        '12345' or '33M (33604836)'
+        """
+        parts = str.rpartition('(')
+        return int(parts[2].rstrip(')'))
+
     def check_stats(self, statcursor, mincount, lookfor):
         """
         Do a quick check of the entries in the the stats cursor,
@@ -37,7 +45,9 @@ class test_stat01(wttest.WiredTigerTestCase):
             self.assertEqual(type(desc), stringclass)
             self.assertEqual(type(valstr), stringclass)
             self.assertEqual(type(val), intclass)
-            print '  stat: \'' + desc + '\', \'' + valstr + '\', ' + str(val)
+            self.assertEqual(val, self.statstr_to_int(valstr))
+            self.printVerbose(2, '  stat: \'' + desc + '\', \'' +
+                              valstr + '\', ' + str(val))
             count += 1
             if desc == lookfor:
                 found = True
@@ -57,14 +67,32 @@ class test_stat01(wttest.WiredTigerTestCase):
             cursor.insert()
         cursor.close()
 
-        print 'overall database stats:'
+        self.printVerbose(2, 'overall database stats:')
         allstat_cursor = self.session.open_cursor('statistics:', None, None)
         self.check_stats(allstat_cursor, 10, 'blocks written to a file')
+
+        # See that we can get a specific stat value by its key,
+        # and verify that its entry is self-consistent
+        allstat_cursor.set_key(wiredtiger.stat.block_write)
+        self.assertEqual(allstat_cursor.search(), 0)
+        values = allstat_cursor.get_values()
+        self.assertEqual(values[0], 'blocks written to a file')
+        val = self.statstr_to_int(values[1])
+        self.assertEqual(val, values[2])
         allstat_cursor.close()
 
-        print 'file specific stats:'
+        self.printVerbose(2, 'file specific stats:')
         filestat_cursor = self.session.open_cursor('statistics:file:' + self.tablename + ".wt", None, None)
         self.check_stats(filestat_cursor, 10, 'overflow pages')
+
+        # See that we can get a specific stat value by its key,
+        # and verify that its entry is self-consistent
+        filestat_cursor.set_key(wiredtiger.filestat.overflow)
+        self.assertEqual(filestat_cursor.search(), 0)
+        values = filestat_cursor.get_values()
+        self.assertEqual(values[0], 'overflow pages')
+        val = self.statstr_to_int(values[1])
+        self.assertEqual(val, values[2])
         filestat_cursor.close()
 
         no_cursor = self.session.open_cursor('statistics:file:DoesNotExist', None, None)
