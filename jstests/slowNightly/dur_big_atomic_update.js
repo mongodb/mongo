27f207582aec390aadebcc4ec0dabf77df78c003
agirbal <antoine@10gen.com>
2012-01-25 19:10:34 -0800

    SERVER-4772: mongo constructor is only local if executed on server

--- jstests/slowNightly/dur_big_atomic_update.js
+++ jstests/slowNightly/dur_big_atomic_update.js
@@ -12,7 +12,7 @@ for (var i=0; i<1024; i++){
     d.foo.insert({_id:i});
 }
 
-big_string = 'x';
+big_string = 'xxxxxxxxxxxxxxxx';
 while (big_string.length < 1024*1024) {
     big_string += big_string;
 }
@@ -31,9 +31,9 @@ for (var i=0; i<1024; i++){
 
 // Do it again but in a db.eval
 d.eval(
-    function(host, big_string) {
-        new Mongo(host).getDB("test").foo.update({}, {$set: {big_string: big_string}}, false, /*multi*/true)
-    }, conn.host, big_string); // Can't pass in connection or DB objects
+    function(big_string) {
+        new Mongo().getDB("test").foo.update({}, {$set: {big_string: big_string}}, false, /*multi*/true)
+    }, big_string); // Can't pass in connection or DB objects
 
 err = d.getLastErrorObj();
 
