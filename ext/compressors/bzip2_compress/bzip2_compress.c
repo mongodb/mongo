174d166569d53a1c0cee43cd54028a3882173863
Michael Cahill <michael.cahill@wiredtiger.com>
2012-02-07 19:40:32 -0500

    bzip2_compress.c:133: error: pointer targets in assignment differ in signedness

--- ext/compressors/bzip2_compress/bzip2_compress.c
+++ ext/compressors/bzip2_compress/bzip2_compress.c
@@ -130,9 +130,9 @@ bzip2_compress(WT_COMPRESSOR *compressor, WT_SESSION *session,
 	    bz_blocksize100k, bz_verbosity, bz_workfactor)) != BZ_OK)
 		return (bzip2_error(session, "BZ2_bzCompressInit", ret));
 
-	bz.next_in = src;
+	bz.next_in = (char *)src;
 	bz.avail_in = src_len;
-	bz.next_out = dst;
+	bz.next_out = (char *)dst;
 	bz.avail_out = dst_len;
 	if ((ret = BZ2_bzCompress(&bz, BZ_FINISH)) == BZ_STREAM_END) {
 		*compression_failed = 0;
@@ -165,9 +165,9 @@ bzip2_decompress(WT_COMPRESSOR *compressor, WT_SESSION *session,
 	if ((ret = BZ2_bzDecompressInit(&bz, bz_small, bz_verbosity)) != BZ_OK)
 		return (bzip2_error(session, "BZ2_bzDecompressInit", ret));
 
-	bz.next_in = src;
+	bz.next_in = (char *)src;
 	bz.avail_in = src_len;
-	bz.next_out = dst;
+	bz.next_out = (char *)dst;
 	bz.avail_out = dst_len;
 	if ((ret = BZ2_bzDecompress(&bz)) == BZ_STREAM_END) {
 		*result_lenp = dst_len - bz.avail_out;
