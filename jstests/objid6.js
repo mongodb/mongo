0e11df0b41db7802dc3565a7a666f6af1a9e4b93
Mathias Stearn <mathias@10gen.com>
2012-02-09 17:35:06 -0500

    Make test less sensitive to differences between time() and gettimeofday()

--- jstests/objid6.js
+++ jstests/objid6.js
@@ -6,9 +6,11 @@ b = new ObjectId("4c17f616a707428966a2801c");
 assert.eq(a.getTimestamp(), b.getTimestamp() , "A" );
 
 x = Math.floor( (new Date()).getTime() / 1000 );
+sleep(10/*ms*/)
 a = new ObjectId();
+sleep(10/*ms*/)
 z = Math.floor( (new Date()).getTime() / 1000 );
 y = a.getTimestamp().getTime() / 1000;
 
-assert( x <= y , "B" );
-assert( y <= z , "C" );
+assert.lte( x , y , "B" );
+assert.lte( y , z , "C" );
