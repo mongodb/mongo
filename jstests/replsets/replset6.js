c94d2c910a2cd185c804abffe4bdbdfa28cc598e
Eric Milkie <milkie@10gen.com>
2012-02-15 18:14:05 -0500

    SERVER-4332 activate test now that everything's working

--- jstests/replsets/replset6.js
+++ jstests/replsets/replset6.js
@@ -1,4 +1,4 @@
-if (0) { // SERVER-4332
+
 // Test replication of collection renaming
 
 baseName = "jstests_replsets_replset6";
@@ -42,7 +42,7 @@ second = baseName + "_second";
 m.getDB( first ).one.save( { a: 1 } );
 assert.soon( function() { return s.getDB( first ).one.findOne() && 1 == s.getDB( first ).one.findOne().a; } );
 
-assert.commandWorked( admin.runCommand( {renameCollection:"jstests_replsets_replset6_first.one", to:"jstests_replset_replset6_second.two"} ) );
+assert.commandWorked( admin.runCommand( {renameCollection:"jstests_replsets_replset6_first.one", to:"jstests_replsets_replset6_second.two"} ) );
 assert.soon( function() {
 	return -1 != s.getDBNames().indexOf( second ) &&
 	    -1 != s.getDB( second ).getCollectionNames().indexOf( "two" ) &&
@@ -50,4 +50,3 @@ assert.soon( function() {
 	    1 == s.getDB( second ).two.findOne().a; } );
 assert.eq( -1, s.getDB( first ).getCollectionNames().indexOf( "one" ) );
 
-}
