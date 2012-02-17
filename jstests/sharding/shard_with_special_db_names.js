c70f2d58ab352997776556d25bb258eea09e1a4d
Ren <renctan@gmail.com>
2012-02-17 12:16:21 -0500

    Changes to address code review comments for SERVER-4954

--- jstests/sharding/shard_with_special_db_names.js
+++ jstests/sharding/shard_with_special_db_names.js
@@ -1,15 +1,14 @@
 (function(){
 
-// SERVER-3702
-
-var s = new ShardingTest( "shard8", 2, 0, 2 );
+var s = new ShardingTest( "shard_with_special_db_names", 2, 0, 2 );
 var specialDB = "[a-z]+";
 var specialNS = specialDB + ".special";
 
 s.adminCommand( { enablesharding : "test" } );
 s.adminCommand( { shardcollection : "test.data" , key : { num : 1 } } );
 
-// Test that the database will not complain "cannot have 2 database names that differs on case"
+// Test that the database will not complain "cannot have 2 database names that
+// differs on case"
 s.adminCommand( { enablesharding : specialDB } );
 s.adminCommand( { shardcollection :  specialNS, key : { num : 1 } } );
 
@@ -21,13 +20,8 @@ s.getDB(specialDB).dropDatabase();
 
 var cursor = s.getDB("config").collections.find( { _id: specialNS } );
 
-if (cursor.hasNext()) {
-  assert.eq( cursor.count(), 1 );
-  assert( cursor.next()["dropped"] );
-}
-else {
-  // It's alright if not found since it should be deleted
-}
+assert.eq( cursor.count(), 1 );
+assert( cursor.next()["dropped"] );
 
 })();
 
