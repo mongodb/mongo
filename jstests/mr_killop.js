fb3712c23886c5c1caba2c4661292b079ac929e4
Aaron <aaron@10gen.com>
2012-02-17 20:11:02 -0800

    clean mr_killop arguments

--- jstests/mr_killop.js
+++ jstests/mr_killop.js
@@ -8,14 +8,14 @@ t2.drop();
     }
 
     /** @return op code for map reduce op created by spawned shell, or that op's child */
-    function op( where ) {
+    function op( childLoop ) {
         p = db.currentOp().inprog;
         debug( p );
         for ( var i in p ) {
             var o = p[ i ];
             // Identify a map/reduce or where distinct operation by its collection, whether or not
             // it is currently active.
-            if ( where ) {
+            if ( childLoop ) {
                 if ( ( o.active || o.waitingForLock ) &&
                     o.query &&
                     o.query.query &&
@@ -40,11 +40,11 @@ t2.drop();
  * Run one map reduce with the specified parameters in a parallel shell, kill the
  * map reduce op or its child op with killOp, and wait for the map reduce op to
  * terminate.
- * @where - if true, a distinct $where op is killed rather than the map reduce op.
+ * @param childLoop - if true, a distinct $where op is killed rather than the map reduce op.
  * This is necessay for a child distinct $where of a map reduce op because child
  * ops currently mask parent ops in currentOp.
  */
-    function testOne( map, reduce, finalize, scope, where, wait ) {
+    function testOne( map, reduce, finalize, scope, childLoop, wait ) {
         t.drop();
         t2.drop();
         // Ensure we have 2 documents for the reduce to run
@@ -79,7 +79,7 @@ t2.drop();
         }
         
         o = null;
-        assert.soon( function() { o = op( where ); return o != -1 } );
+        assert.soon( function() { o = op( childLoop ); return o != -1 } );
 
         res = db.killOp( o );
         debug( "did kill : " + tojson( res ) );
@@ -88,26 +88,26 @@ t2.drop();
         s();
         debug( "parallel shell completed" );
         
-        assert.eq( -1, op( where ) );
+        assert.eq( -1, op( childLoop ) );
     }
 
     /** Test using wait and non wait modes */
-    function test( map, reduce, finalize, scope, where ) {
-        testOne( map, reduce, finalize, scope, where, false );
-        testOne( map, reduce, finalize, scope, where, true );
+    function test( map, reduce, finalize, scope, childLoop ) {
+        testOne( map, reduce, finalize, scope, childLoop, false );
+        testOne( map, reduce, finalize, scope, childLoop, true );
     }
 
     /** Test looping in map and reduce functions */
-    function runMRTests( loop, where ) {
-        test( loop, function( k, v ) { return v[ 0 ]; }, null, null, where );
-        test( function() { emit( this.a, 1 ); }, loop, null, null, where );
-        test( function() { loop(); }, function( k, v ) { return v[ 0 ] }, null, { loop: loop }, where );
+    function runMRTests( loop, childLoop ) {
+        test( loop, function( k, v ) { return v[ 0 ]; }, null, null, childLoop );
+        test( function() { emit( this.a, 1 ); }, loop, null, null, childLoop );
+        test( function() { loop(); }, function( k, v ) { return v[ 0 ] }, null, { loop: loop }, childLoop );
     }
 
     /** Test looping in finalize function */
-    function runFinalizeTests( loop, where ) {
-        test( function() { emit( this.a, 1 ); }, function( k, v ) { return v[ 0 ] }, loop, null, where );
-        test( function() { emit( this.a, 1 ); }, function( k, v ) { return v[ 0 ] }, function( a, b ) { loop() }, { loop: loop }, where );
+    function runFinalizeTests( loop, childLoop ) {
+        test( function() { emit( this.a, 1 ); }, function( k, v ) { return v[ 0 ] }, loop, null, childLoop );
+        test( function() { emit( this.a, 1 ); }, function( k, v ) { return v[ 0 ] }, function( a, b ) { loop() }, { loop: loop }, childLoop );
     }
 
     var loop = function() {
