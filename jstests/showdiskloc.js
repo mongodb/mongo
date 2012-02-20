331fe579b13ccb2205e3e54d40f4dc6c66e782bf
Aaron <aaron@10gen.com>
2012-02-20 14:23:27 -0800

    SERVER-4150 check showDiskLoc mode with a covered index

--- jstests/showdiskloc.js
+++ jstests/showdiskloc.js
@@ -17,3 +17,8 @@ checkResults( t.find()._addSpecial("$showDiskLoc" , true).toArray() );
 t.save( {} );
 t.save( {} );
 checkResults( t.find().batchSize( 2 )._addSpecial("$showDiskLoc" , true).toArray() );
+
+// Check with a covered index.
+t.ensureIndex( { a:1 } );
+checkResults
+( t.find( {}, { _id:0, a:1 } ).hint( { a:1 } )._addSpecial("$showDiskLoc" , true).toArray() );
