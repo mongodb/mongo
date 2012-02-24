00498dc20f070394d9d42e1d40e6dadd0ff54e0a
U-tellus\cwestin <cwestin@10gen.com>
2012-02-24 10:42:34 -0800

    SERVER-4843

--- src/mongo/db/pipeline/accumulator.cpp
+++ src/mongo/db/pipeline/accumulator.cpp
@@ -56,8 +56,6 @@ namespace mongo {
     }
 
     void agg_framework_reservedErrors() {
-        uassert(15956, "reserved error", false);
-
         uassert(16028, "reserved error", false);
         uassert(16029, "reserved error", false);
         uassert(16030, "reserved error", false);
