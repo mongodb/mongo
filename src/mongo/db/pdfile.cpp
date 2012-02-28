9cf0cc2233478ff41b8d32949fa58108071b0af2
Mathias Stearn <mathias@10gen.com>
2012-02-28 16:10:38 -0500

    Try to fix closeAllDatabases SERVER-5101 SERVER-4947 SERVER-5072

--- src/mongo/db/pdfile.cpp
+++ src/mongo/db/pdfile.cpp
@@ -2401,6 +2401,7 @@ namespace mongo {
     bool DatabaseHolder::closeAll( const string& path , BSONObjBuilder& result , bool force ) {
         log() << "DatabaseHolder::closeAll path:" << path << endl;
         d.dbMutex.assertWriteLocked();
+        getDur().commitNow(); // bad things happen if we close a DB with outstanding writes
 
         map<string,Database*>& m = _paths[path];
         _size -= m.size();
