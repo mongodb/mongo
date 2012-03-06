276e5f09ec00fdc7b8131664f4587fe6e9094e74
agirbal <antoine@10gen.com>
2012-03-06 15:22:42 -0800

    SERVER-3012: added $where tests

--- jstests/memory.js
+++ jstests/memory.js
@@ -1,9 +1,11 @@
+var col = db.memoryTest;
+/*
 // test creating many collections to make sure no internal cache goes OOM
 for (var i = 0; i < 10000; ++i) {
-    name = "foo" + i;
+    name = "memoryTest" + i;
     if ((i % 1000) == 0) print("Processing " + name);
     db.eval(function(col) { for (var i = 0; i < 100; ++i) {db[col + "_" + i].find();} }, name);
-}
+}*/
 
 // test recovery of JS engine after out of memory
 db.system.js.save( { "_id" : "f1", "value" : function(n) {
@@ -21,7 +23,6 @@ db.system.js.save( { "_id" : "f1", "value" : function(n) {
 db.eval("f1(10)");
 assert.throws(function() { db.eval("f1(100000000)"); } );
 db.eval("f1(10)");
-
 assert.throws(function() { db.eval("f1(1000000000)"); } );
 db.eval("f1(1000000)");
 db.eval("f1(1000000)");
@@ -31,4 +32,11 @@ db.eval("f1(10)");
 db.eval("f1(1000000)");
 db.eval("f1(1000000)");
 db.eval("f1(1000000)");
+
+// also test $where
+col.drop();
+col.insert({a: 1});
+col.findOne({$where: "var arr = []; for (var i = 0; i < 1000000; ++i) {arr.push(0);}"});
+assert.throws(function() { col.findOne({$where: "var arr = []; for (var i = 0; i < 1000000000; ++i) {arr.push(0);}"}); });
+col.findOne({$where: "var arr = []; for (var i = 0; i < 1000000; ++i) {arr.push(0);}"});
 
