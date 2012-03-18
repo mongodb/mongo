5836affdf8bb007254d77bb6a8beb3d21a2af68b
Dwight <dwight@10gen.com>
2012-03-18 14:24:01 -0400

    defensive code for SimpleRWLock which in debug mode can catch some contract violations

--- src/mongo/util/concurrency/rwlock.h
+++ src/mongo/util/concurrency/rwlock.h
@@ -26,32 +26,9 @@
 #include "mutexdebugger.h"
 #endif
 
-namespace mongo {
+#include "simplerwlock.h"
 
-    /** separated out as later the implementation of this may be different than RWLock, 
-        depending on OS, as there is no upgrade etc. facility herein.
-    */
-    class SimpleRWLock : public RWLockBase { 
-    public:
-        explicit SimpleRWLock(const char *) { }
-        SimpleRWLock() { }
-        void lock() { RWLockBase::lock(); }
-        void unlock() { RWLockBase::unlock(); }
-        void lock_shared() { RWLockBase::lock_shared(); }
-        void unlock_shared() { RWLockBase::unlock_shared(); }
-        class Shared : boost::noncopyable {
-            SimpleRWLock& _r;
-        public:
-            Shared(SimpleRWLock& rwlock) : _r(rwlock) {_r.lock_shared(); }
-            ~Shared() { _r.unlock_shared(); }
-        };
-        class Exclusive : boost::noncopyable {
-            SimpleRWLock& _r;
-        public:
-            Exclusive(SimpleRWLock& rwlock) : _r(rwlock) {_r.lock(); }
-            ~Exclusive() { _r.unlock(); }
-        };
-    };
+namespace mongo {
 
     class RWLock : public RWLockBase { 
         enum { NilState, UpgradableState, Exclusive } x; // only bother to set when doing upgradable related things
--- src/mongo/util/concurrency/rwlockimpl.cpp
+++ src/mongo/util/concurrency/rwlockimpl.cpp
@@ -17,3 +17,49 @@ using namespace std;
 #include "../assert_util.h"
 #include "../time_support.h"
 #include "rwlockimpl.h"
+#include "simplerwlock.h"
+#include "threadlocal.h"
+
+namespace mongo {
+
+#if defined(_WIN32) && defined(_DEBUG)
+    // the code below in _DEBUG build will check that we don't try to recursively lock, 
+    // which is not supported by this class.  also checks that you don't unlock without 
+    // having locked
+    void SimpleRWLock::lock() {
+        unsigned me = GetCurrentThreadId();
+        int& state = s.getRef();
+        dassert( state == 0 );
+        state--;
+        RWLockBase::lock();
+        tid = me; // this is for use in the debugger to see who does have the lock
+    }
+    void SimpleRWLock::unlock() { 
+        int& state = s.getRef();
+        dassert( state == -1 );
+        state++;
+        tid = 0xffffffff;
+        RWLockBase::unlock(); 
+    }
+    void SimpleRWLock::lock_shared() { 
+        int& state = s.getRef();
+        dassert( state == 0 );
+        state++;
+        RWLockBase::lock_shared(); 
+        shares++;
+    }
+    void SimpleRWLock::unlock_shared() { 
+        int& state = s.getRef();
+        dassert( state == 1 );
+        state--;
+        shares--;
+        RWLockBase::unlock_shared(); 
+    }
+#else
+    void SimpleRWLock::lock() { RWLockBase::lock(); }
+    void SimpleRWLock::unlock() { RWLockBase::unlock(); }
+    void SimpleRWLock::lock_shared() { RWLockBase::lock_shared(); }
+    void SimpleRWLock::unlock_shared() { RWLockBase::unlock_shared(); }
+#endif
+
+}
