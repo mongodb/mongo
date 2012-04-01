297f257fc94f18412d9061875f4414f3e559dd99
Eliot Horowitz <eliot@10gen.com>
2012-04-01 02:48:42 -0400

    don't update stats on nmoved until move is complete

--- src/mongo/db/pdfile.cpp
+++ src/mongo/db/pdfile.cpp
@@ -1112,12 +1112,15 @@ namespace mongo {
             // doesn't fit.  reallocate -----------------------------------------------------
             uassert( 10003 , "failing update: objects in a capped ns cannot grow", !(d && d->capped));
             d->paddingTooSmall();
+            deleteRecord(ns, toupdate, dl);
+            DiskLoc res = insert(ns, objNew.objdata(), objNew.objsize(), god);
+
             if (debug.nmoved == -1) // default of -1 rather than 0
                 debug.nmoved = 1;
             else
                 debug.nmoved += 1;
-            deleteRecord(ns, toupdate, dl);
-            return insert(ns, objNew.objdata(), objNew.objsize(), god);
+
+            return res;
         }
 
         nsdt->notifyOfWriteOp();
