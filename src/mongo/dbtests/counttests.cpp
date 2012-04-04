3ae516c69c16be09f5c7dd1c8c1d1069efc0cc00
Aaron <aaron@10gen.com>
2012-04-04 09:37:22 -0700

    SERVER-5428 Add a deterministic count yield validation test to counttests.cpp.

--- src/mongo/dbtests/counttests.cpp
+++ src/mongo/dbtests/counttests.cpp
@@ -20,6 +20,7 @@
 
 #include "../db/cursor.h"
 #include "../db/pdfile.h"
+#include "mongo/db/db.h"
 #include "mongo/db/json.h"
 
 #include "dbtests.h"
@@ -67,6 +68,9 @@ namespace CountTests {
         }
         static void insert( const BSONObj &o ) {
             theDataFileMgr.insert( ns(), o.objdata(), o.objsize() );
+        }
+        static BSONObj countCommand( const BSONObj &query ) {
+            return BSON( "query" << query );
         }
     };
     
@@ -125,6 +129,99 @@ namespace CountTests {
             ASSERT_EQUALS( 1, runCount( ns(), cmd, err ) );
         }
     };
+
+    /** Set a value or await an expected value. */
+    class PendingValue {
+    public:
+        PendingValue( int initialValue ) :
+            _value( initialValue ),
+            _mutex( "CountTests::PendingValue::_mutex" ) {
+        }
+        void set( int newValue ) {
+            scoped_lock lk( _mutex );
+            _value = newValue;
+            _condition.notify_all();
+        }
+        void await( int expectedValue ) const {
+            scoped_lock lk( _mutex );
+            while( _value != expectedValue ) {
+                _condition.wait( lk.boost() );
+            }
+        }
+    private:
+        int _value;
+        mutable mongo::mutex _mutex;
+        mutable boost::condition _condition;
+    };
+
+    /** A writer client will be registered for the lifetime of an object of this class. */
+    class WriterClientScope {
+    public:
+        WriterClientScope() :
+            _state( Initial ),
+            _dummyWriter( boost::bind( &WriterClientScope::runDummyWriter, this ) ) {
+            _state.await( Ready );
+        }
+        ~WriterClientScope() {
+            // Terminate the writer thread even on exception.
+            _state.set( Finished );
+            DESTRUCTOR_GUARD( _dummyWriter.join() );
+        }
+    private:
+        enum State {
+            Initial,
+            Ready,
+            Finished
+        };
+        void runDummyWriter() {
+            Client::initThread( "dummy writer" );
+            // Register a write lock request.
+            cc().curop()->waitingForLock( 'W' );
+            _state.set( Ready );
+            _state.await( Finished );
+            cc().shutdown();
+        }
+        PendingValue _state;
+        boost::thread _dummyWriter;
+    };
+    
+    /**
+     * The runCount() function yields deterministically with sufficient cursor iteration and a
+     * mutually exclusive thread awaiting its mutex.  SERVER-5428
+     */
+    class Yield : public Base {
+    public:
+        void run() {
+            // Insert enough documents that counting them will exceed the iteration threshold
+            // to trigger a yield.
+            for( int i = 0; i < 1000; ++i ) {
+                insert( BSON( "a" << 1 ) );
+            }
+            
+            // Call runCount() under a read lock.
+            dbtemprelease release;
+            Client::ReadContext ctx( ns() );
+
+            int numYieldsBeforeCount = numYields();
+            
+            string err;
+            ASSERT_EQUALS( 1000, runCount( ns(), countCommand( BSON( "a" << 1 ) ), err ) );
+            ASSERT_EQUALS( "", err );
+
+            int numYieldsAfterCount = numYields();
+            int numYieldsDuringCount = numYieldsAfterCount - numYieldsBeforeCount;
+
+            // The runCount() function yieled.
+            ASSERT_NOT_EQUALS( 0, numYieldsDuringCount );
+            ASSERT( 0 < numYieldsDuringCount );
+        }
+    private:
+        int numYields() const {
+            return cc().curop()->infoNoauth()[ "numYields" ].number();
+        }
+        // A writer client is registered while the test runs, causing runCount() to yield.
+        WriterClientScope _writer;
+    };
     
     class All : public Suite {
     public:
@@ -137,6 +234,7 @@ namespace CountTests {
             add<Fields>();
             add<QueryFields>();
             add<IndexedRegex>();
+            add<Yield>();
         }
     } myall;
     
