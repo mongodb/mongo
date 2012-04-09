dca03811cb778279eb4ee02f9e2970858516e66a
Eliot Horowitz <eliot@10gen.com>
2012-04-08 22:33:44 -0400

    make query_yield1 not need a pre-existing db

--- jstests/slowWeekly/query_yield1.js
+++ jstests/slowWeekly/query_yield1.js
@@ -1,3 +1,6 @@
+port = 30201;
+conn = startMongodEmpty("--port", 30201, "--dbpath", "/data/db/query_yield1", "--smallfiles", "--nojournal" );
+db = conn.getDB( "test" );
 
 t = db.query_yield1;
 t.drop()
@@ -51,6 +54,7 @@ print( "start query" );
 
 num = 0;
 start = new Date();
+biggestMe = 0;
 while ( ( (new Date()).getTime() - start ) < ( time * 2 ) ){
     var me = Date.timeFunc( function(){ t.insert( { x : 1 } ); db.getLastError(); } )
     var x = db.currentOp()
@@ -59,6 +63,11 @@ while ( ( (new Date()).getTime() - start ) < ( time * 2 ) ){
         assert.eq( 1 , x.inprog.length , "nothing in prog" );
     }
     
+    if ( me > biggestMe ) {
+        biggestMe = me;
+        print( "biggestMe: " + biggestMe );
+    }
+
     assert.gt( 200 , me , "took too long for me to run" );
 
     if ( x.inprog.length == 0 )
@@ -71,3 +80,4 @@ join();
 var x = db.currentOp()
 assert.eq( 0 , x.inprog.length , "weird 2" );
 
+stopMongod( port )
