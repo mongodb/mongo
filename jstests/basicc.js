82b5219b3a2989f99bd33f3300eae22aeeac947c
Eric Milkie <milkie@10gen.com>
2012-04-11 11:36:21 -0400

    reset db back to 'test' at end of script
    You can't leave the db pointing to something other than 'test', for any test in jstests/.
    Subsequent tests (capped5 in particular) are counting on the db to be 'test'.
    
    Also, I reduced the number of iterations in the test to make it run faster.

--- jstests/basicc.js
+++ jstests/basicc.js
@@ -9,10 +9,11 @@ t2.drop();
 js = "while( 1 ) { db.jstests.basicc1.save( {} ); }";
 pid = startMongoProgramNoConnect( "mongo" , "--eval" , js , db.getMongo().host );
 
-for( var i = 0; i < 100000; ++i ) {
+for( var i = 0; i < 1000; ++i ) {
     t2.save( {} );
 }
 assert.automsg( "!db.getLastError()" );
 stopMongoProgramByPid( pid );
 t1.drop();
 t2.drop();
+db = db.getSisterDB("test");  // put things back the way we found it
