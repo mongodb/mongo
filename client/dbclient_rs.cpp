97d9c1b81567b76b8a1b111a20024386d39343cd
Randolph Tan <randolph@10gen.com>
2012-04-13 14:18:27 -0400

    SERVER-5594 memory corruption in sharding/auth1.js V8 builder
    Signed-off-by: Eric Milkie <milkie@10gen.com>

--- client/dbclient_rs.cpp
+++ client/dbclient_rs.cpp
@@ -550,7 +550,7 @@ namespace mongo {
             errorOccured = true;
         }
 
-        if ( errorOccured ) {
+        if ( errorOccured && nodesOffset >= 0 ) {
             scoped_lock lk( _lock );
             _nodes[nodesOffset].ok = false;
         }
