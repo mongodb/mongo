e45910821fc151e3aeb9bfa1fa16f2315f34a13e
Keith Bostic <keith@wiredtiger.com>
2012-04-23 19:57:22 +0000

    whitespace

--- ext/collators/reverse/reverse_collator.c
+++ ext/collators/reverse/reverse_collator.c
@@ -32,21 +32,21 @@
 
 WT_EXTENSION_API *wt_api;
 
-#define __UNUSED(v)     ((void)(v))
+#define	__UNUSED(v)     ((void)(v))
 
 static int
 collate_reverse(WT_COLLATOR *collator, WT_SESSION *session,
     const WT_ITEM *k1, const WT_ITEM *k2, int *cmp)
 {
-        size_t len;
+	size_t len;
 
-        __UNUSED(collator);
-        __UNUSED(session);
+	__UNUSED(collator);
+	__UNUSED(session);
 
-        len = (k1->size < k2->size) ? k1->size : k2->size;
-        if ((*cmp = memcmp(k2->data, k1->data, len)) == 0)
-                *cmp = ((int)k1->size - (int)k2->size);
-        return (0);
+	len = (k1->size < k2->size) ? k1->size : k2->size;
+	if ((*cmp = memcmp(k2->data, k1->data, len)) == 0)
+		*cmp = ((int)k1->size - (int)k2->size);
+	return (0);
 }
 
 static WT_COLLATOR reverse_collator = { collate_reverse };
@@ -55,12 +55,12 @@ int
 wiredtiger_extension_init(
     WT_SESSION *session, WT_EXTENSION_API *api, const char *config)
 {
-        WT_CONNECTION *conn;
+	WT_CONNECTION *conn;
 
-        __UNUSED(config);
+	__UNUSED(config);
 
-        wt_api = api;
-        conn = session->connection;
+	wt_api = api;
+	conn = session->connection;
 
-        return (conn->add_collator(conn, "reverse", &reverse_collator, NULL));
+	return (conn->add_collator(conn, "reverse", &reverse_collator, NULL));
 }
