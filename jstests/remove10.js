6e12471dad9b863542ff116559261ac94bf9a7ce
Aaron <aaron@10gen.com>
2012-04-24 11:22:23 -0700

    SERVER-2008 Enable and clean test.

--- jstests/remove10.js
+++ jstests/remove10.js
@@ -1,28 +1,38 @@
-// SERVER-2009 Update documents with adjacent indexed keys.
-// This test doesn't fail, it just prints an invalid warning message.
+// Sanity test for removing documents with adjacent index keys.  SERVER-2008
 
-if ( 0 ) { // SERVER-2009
 t = db.jstests_remove10;
 t.drop();
-t.ensureIndex( {i:1} );
+t.ensureIndex( { a:1 } );
 
-function arr( i ) {
+/** @return an array containing a sequence of numbers from i to i + 10. */
+function runStartingWith( i ) {
     ret = [];
- 	for( j = i; j < i + 11; ++j ) {
-        ret.push( j );
+    for( j = 0; j < 11; ++j ) {
+        ret.push( i + j );
     }
     return ret;
 }
 
+// Insert some documents with adjacent index keys.
 for( i = 0; i < 1100; i += 11 ) {
-    t.save( {i:arr( i )} );
+    t.save( { a:runStartingWith( i ) } );
 }
+db.getLastError();
 
-s = startParallelShell( 't = db.jstests_remove10; for( j = 0; j < 1000; ++j ) { o = t.findOne( {i:Random.randInt(1100)} ); t.remove( {_id:o._id} ); t.insert( o ); }' );
+// Remove and then reinsert random documents in the background.
+s = startParallelShell(
+                       't = db.jstests_remove10;' +
+                       'for( j = 0; j < 1000; ++j ) {' +
+                       '    o = t.findOne( { a:Random.randInt( 1100 ) } );' +
+                       '    t.remove( { _id:o._id } );' +
+                       '    t.insert( o );' +
+                       '}'
+                       );
 
+// Find operations are error free.
 for( i = 0; i < 200; ++i ) {
-    t.find( {i:{$gte:0}} ).hint( {i:1} ).itcount();
+    t.find( { a:{ $gte:0 } } ).hint( { a:1 } ).itcount();
+    assert( !db.getLastError() );
 }
 
 s();
-}
