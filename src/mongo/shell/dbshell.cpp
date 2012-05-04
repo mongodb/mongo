mongodb/mongo

commit b118ba77c541b5f952cc9c3e418fee0644ee4c7f

SERVER-5399: Essentially aliasing quit, exit, and function call variations.
Signed-off-by: Andy Schwerin <schwerin@10gen.com>

Previously, `exit` was being implemented as a code in the DB shell,
while `quit` was being implemented as a shell function.  This meant that
both were doing different things, and were called differently--i.e.,
`exit` could be run as is, whereas `quit()` had to be called as a
function.

Additional cleanups / improvements:

Clarifying Control-C/D handling code.
Moving string operations to use boost::algorithm::string libs
Clarifying logic in command parsing
Renaming variable for user input to something more relevant ("code" -> "line").

src/mongo/shell/dbshell.cpp
@@ -21,6 +21,8 @@
 #include <string.h>
 
 #include <boost/filesystem/operations.hpp>
+#include <boost/algorithm/string/predicate.hpp>
+#include <boost/algorithm/string/trim.hpp>
 
 #include "mongo/client/dbclientinterface.h"
 #include "mongo/db/cmdline.h"
@@ -828,99 +830,73 @@ int _main( int argc, char* argv[] ) {
                 prompt = "> ";
             }
 
-            char * line = shellReadline( prompt.c_str() );
+            char * lineCStr = shellReadline( prompt.c_str() );
 
-            char * linePtr = line;  // can't clobber 'line', we need to free() it later
-            if ( linePtr ) {
-                while ( linePtr[0] == ' ' )
-                    ++linePtr;
-                int lineLen = strlen( linePtr );
-                while ( lineLen > 0 && linePtr[lineLen - 1] == ' ' )
-                    linePtr[--lineLen] = 0;
-            }
-
-            if ( ! linePtr || ( strlen( linePtr ) == 4 && strstr( linePtr , "exit" ) ) ) {
-                if ( ! mongo::cmdLine.quiet )
-                    cout << "bye" << endl;
-                if ( line )
-                    free( line );
+            if ( !lineCStr ) {
+                // User must have hit ^C; treat this like an exit.
+                if( !mongo::cmdLine.quiet )
+                    cout << "User interrupt detected; exiting..." << endl;
                 break;
             }
 
-            string code = linePtr;
-            if ( code == "exit" || code == "exit;" ) {
-                free( line );
-                break;
+            string line = lineCStr;
+            boost::algorithm::trim(line);
+
+            // Free line before we forget.
+            free(lineCStr);
+            lineCStr = NULL;
+
+            if ( line == "quit" || line == "exit" ) {
+              // Replace this with the real command that the shell expects
+              line += "(0)";
             }
-            if ( code == "cls" ) {
-                free( line );
+
+            if ( line == "cls" ) {
                 linenoiseClearScreen();
-                continue;
-            }
-
-            if ( code.size() == 0 ) {
-                free( line );
-                continue;
-            }
-
-            if ( startsWith( linePtr, "edit " ) ) {
-                shellHistoryAdd( linePtr );
-
-                const char* s = linePtr + 5; // skip "edit "
-                while( *s && isspace( *s ) )
-                    s++;
-
+            } else if( boost::algorithm::starts_with( line, "edit " ) ) {
+                string s = line.c_str() + 5; // skip "edit "
+                boost::algorithm::trim_left(s);
                 edit( s );
-                free( line );
-                continue;
-            }
+            } else if ( !line.empty() ) {
+                // Whatever we have now will be in the form:
+                //   CMD ARGS
+                bool wasCmd = false;
+                string cmd;
+                size_t strPos = line.find(' ');
+                if ( strPos != string::npos )
+                    cmd = line.substr( 0, strPos );
 
-            gotInterrupted = false;
-            code = finishCode( code );
-            if ( gotInterrupted ) {
-                cout << endl;
-                free( line );
-                continue;
-            }
-
-            if ( code.size() == 0 ) {
-                free( line );
-                break;
-            }
-
-            bool wascmd = false;
-            {
-                string cmd = linePtr;
-                if ( cmd.find( " " ) > 0 )
-                    cmd = cmd.substr( 0 , cmd.find( " " ) );
-
-                if ( cmd.find( "\"" ) == string::npos ) {
+                if ( cmd.find("\"") == string::npos ) {
                     try {
-                        scope->exec( (string)"__iscmd__ = shellHelper[\"" + cmd + "\"];" , "(shellhelp1)" , false , true , true );
+                        scope->exec(
+                                (string) "__iscmd__ = shellHelper[\"" + cmd + "\"];",
+                                "(shellhelp1)" , false , true , true );
                         if ( scope->getBoolean( "__iscmd__" )  ) {
-                            scope->exec( (string)"shellHelper( \"" + cmd + "\" , \"" + code.substr( cmd.size() ) + "\");" , "(shellhelp2)" , false , true , false );
-                            wascmd = true;
+                            scope->exec(
+                                    (string) "shellHelper( \"" + cmd + "\" , \"" + line.substr( cmd.size() ) + "\");",
+                                    "(shellhelp2)" , false , true , false );
+                            wasCmd = true;
                         }
+                    } catch ( std::exception& exc) {
+                        cout << "error2:" << exc.what() << endl;
+                        wasCmd = true;
                     }
-                    catch ( std::exception& e ) {
-                        cout << "error2:" << e.what() << endl;
-                        wascmd = true;
+                }
+
+                if ( !wasCmd ) {
+                    try {
+                        if ( scope->exec( line.c_str() , "(shell)" , false , true , false ) )
+                            scope->exec( "shellPrintHelper( __lastres__ );" , "(shell2)" , true , true , false );
+                    }
+                    catch ( std::exception& exc ) {
+                        cout << "error:" << exc.what() << endl;
                     }
                 }
             }
 
-            if ( ! wascmd ) {
-                try {
-                    if ( scope->exec( code.c_str() , "(shell)" , false , true , false ) )
-                        scope->exec( "shellPrintHelper( __lastres__ );" , "(shell2)" , true , true , false );
-                }
-                catch ( std::exception& e ) {
-                    cout << "error:" << e.what() << endl;
-                }
-            }
-
-            shellHistoryAdd( code.c_str() );
-            free( line );
+            // If the line contained anything, add it to the history.
+            if(!line.empty())
+                shellHistoryAdd( line.c_str() );
         }
 
         shellHistoryDone();

src/mongo/shell/shell_utils.cpp
@@ -126,6 +126,8 @@ namespace mongo {
 
         void installShellUtils( Scope& scope ) {
             scope.injectNative( "quit", Quit );
+            // SERVER-5399: Set `exit` as well, so that quit/exit both resolve.
+            scope.injectNative( "exit", Quit );
             scope.injectNative( "getMemInfo" , JSGetMemInfo );
             scope.injectNative( "_srand" , JSSrand );
             scope.injectNative( "_rand" , JSRand );

commit 1f3ac382c4892f03a0d2c5da076fa5271867fbbc

Rollback b118ba77c541b5f952cc9c3e418fee0644ee4c7f, which broke multi-line.
Effectively reopens SERVER-5399.

src/mongo/shell/dbshell.cpp
@@ -25,8 +25,6 @@
 #endif
 
 #include <boost/filesystem/operations.hpp>
-#include <boost/algorithm/string/predicate.hpp>
-#include <boost/algorithm/string/trim.hpp>
 
 #include "mongo/client/dbclientinterface.h"
 #include "mongo/db/cmdline.h"
@@ -845,73 +843,99 @@ int _main( int argc, char* argv[] ) {
                 prompt = "> ";
             }
 
-            char * lineCStr = shellReadline( prompt.c_str() );
+            char * line = shellReadline( prompt.c_str() );
 
-            if ( !lineCStr ) {
-                // User must have hit ^C; treat this like an exit.
-                if( !mongo::cmdLine.quiet )
-                    cout << "User interrupt detected; exiting..." << endl;
+            char * linePtr = line;  // can't clobber 'line', we need to free() it later
+            if ( linePtr ) {
+                while ( linePtr[0] == ' ' )
+                    ++linePtr;
+                int lineLen = strlen( linePtr );
+                while ( lineLen > 0 && linePtr[lineLen - 1] == ' ' )
+                    linePtr[--lineLen] = 0;
+            }
+
+            if ( ! linePtr || ( strlen( linePtr ) == 4 && strstr( linePtr , "exit" ) ) ) {
+                if ( ! mongo::cmdLine.quiet )
+                    cout << "bye" << endl;
+                if ( line )
+                    free( line );
                 break;
             }
 
-            string line = lineCStr;
-            boost::algorithm::trim(line);
-
-            // Free line before we forget.
-            free(lineCStr);
-            lineCStr = NULL;
-
-            if ( line == "quit" || line == "exit" ) {
-              // Replace this with the real command that the shell expects
-              line += "(0)";
+            string code = linePtr;
+            if ( code == "exit" || code == "exit;" ) {
+                free( line );
+                break;
             }
-
-            if ( line == "cls" ) {
+            if ( code == "cls" ) {
+                free( line );
                 linenoiseClearScreen();
-            } else if( boost::algorithm::starts_with( line, "edit " ) ) {
-                string s = line.c_str() + 5; // skip "edit "
-                boost::algorithm::trim_left(s);
+                continue;
+            }
+
+            if ( code.size() == 0 ) {
+                free( line );
+                continue;
+            }
+
+            if ( startsWith( linePtr, "edit " ) ) {
+                shellHistoryAdd( linePtr );
+
+                const char* s = linePtr + 5; // skip "edit "
+                while( *s && isspace( *s ) )
+                    s++;
+
                 edit( s );
-            } else if ( !line.empty() ) {
-                // Whatever we have now will be in the form:
-                //   CMD ARGS
-                bool wasCmd = false;
-                string cmd;
-                size_t strPos = line.find(' ');
-                if ( strPos != string::npos )
-                    cmd = line.substr( 0, strPos );
+                free( line );
+                continue;
+            }
 
-                if ( cmd.find("\"") == string::npos ) {
+            gotInterrupted = false;
+            code = finishCode( code );
+            if ( gotInterrupted ) {
+                cout << endl;
+                free( line );
+                continue;
+            }
+
+            if ( code.size() == 0 ) {
+                free( line );
+                break;
+            }
+
+            bool wascmd = false;
+            {
+                string cmd = linePtr;
+                if ( cmd.find( " " ) > 0 )
+                    cmd = cmd.substr( 0 , cmd.find( " " ) );
+
+                if ( cmd.find( "\"" ) == string::npos ) {
                     try {
-                        scope->exec(
-                                (string) "__iscmd__ = shellHelper[\"" + cmd + "\"];",
-                                "(shellhelp1)" , false , true , true );
+                        scope->exec( (string)"__iscmd__ = shellHelper[\"" + cmd + "\"];" , "(shellhelp1)" , false , true , true );
                         if ( scope->getBoolean( "__iscmd__" )  ) {
-                            scope->exec(
-                                    (string) "shellHelper( \"" + cmd + "\" , \"" + line.substr( cmd.size() ) + "\");",
-                                    "(shellhelp2)" , false , true , false );
-                            wasCmd = true;
+                            scope->exec( (string)"shellHelper( \"" + cmd + "\" , \"" + code.substr( cmd.size() ) + "\");" , "(shellhelp2)" , false , true , false );
+                            wascmd = true;
                         }
-                    } catch ( std::exception& exc) {
-                        cout << "error2:" << exc.what() << endl;
-                        wasCmd = true;
                     }
-                }
-
-                if ( !wasCmd ) {
-                    try {
-                        if ( scope->exec( line.c_str() , "(shell)" , false , true , false ) )
-                            scope->exec( "shellPrintHelper( __lastres__ );" , "(shell2)" , true , true , false );
-                    }
-                    catch ( std::exception& exc ) {
-                        cout << "error:" << exc.what() << endl;
+                    catch ( std::exception& e ) {
+                        cout << "error2:" << e.what() << endl;
+                        wascmd = true;
                     }
                 }
             }
 
-            // If the line contained anything, add it to the history.
-            if(!line.empty())
-                shellHistoryAdd( line.c_str() );
+            if ( ! wascmd ) {
+                try {
+                    if ( scope->exec( code.c_str() , "(shell)" , false , true , false ) )
+                        scope->exec( "shellPrintHelper( __lastres__ );" , "(shell2)" , true , true , false );
+                }
+                catch ( std::exception& e ) {
+                    cout << "error:" << e.what() << endl;
+                }
+            }
+
+            shellHistoryAdd( code.c_str() );
+            free( line );
         }
 
         shellHistoryDone();

src/mongo/shell/shell_utils.cpp
@@ -126,8 +126,6 @@ namespace mongo {
 
         void installShellUtils( Scope& scope ) {
             scope.injectNative( "quit", Quit );
-            // SERVER-5399: Set `exit` as well, so that quit/exit both resolve.
-            scope.injectNative( "exit", Quit );
             scope.injectNative( "getMemInfo" , JSGetMemInfo );
             scope.injectNative( "_srand" , JSSrand );
             scope.injectNative( "_rand" , JSRand );