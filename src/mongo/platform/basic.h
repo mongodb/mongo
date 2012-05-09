f15931d902441c5d3b881a0ab8222a5ca8e8303c
Eric Milkie <milkie@10gen.com>
2012-05-09 13:25:23 -0400

    fix for non-windows non-linux builds for isnan

--- src/mongo/platform/basic.h
+++ src/mongo/platform/basic.h
@@ -3,19 +3,20 @@
 #pragma once
 
 #ifdef _WIN32
-#include "windows_basic.h"
-#endif
+# include "windows_basic.h"
+#else
 
-#if defined(__linux__)
+# define mongo_isnan(x) isnan(x)
+
+# if defined(__linux__)
 // glibc's optimized versions are better than g++ builtins
-# define __builtin_strcmp strcmp
-# define __builtin_strlen strlen
-# define __builtin_memchr memchr
-# define __builtin_memcmp memcmp
-# define __builtin_memcpy memcpy
-# define __builtin_memset memset
-# define __builtin_memmove memmove
+#  define __builtin_strcmp strcmp
+#  define __builtin_strlen strlen
+#  define __builtin_memchr memchr
+#  define __builtin_memcmp memcmp
+#  define __builtin_memcpy memcpy
+#  define __builtin_memset memset
+#  define __builtin_memmove memmove
+# endif  // if defined(__linux__)
 
-#define mongo_isnan(x) isnan(x)
-#endif
-
+#endif // ifdef _WIN32
