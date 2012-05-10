3e72e558321bfee0c36d886daf298a4074a50c66
Greg Studer <greg@10gen.com>
2012-05-10 11:41:12 -0400

    buildbot make bigMapReduce.js run last, catch errors in simpler tests if possible
    Also added additional logging for future debugging

--- jstests/sharding/zbigMapReduce.js
+++ jstests/sharding/zbigMapReduce.js
@@ -7,13 +7,20 @@ config.settings.save({_id: "chunksize", value: 1});
 s.adminCommand( { enablesharding : "test" } )
 s.adminCommand( { shardcollection : "test.foo", key : { "_id" : 1 } } )
 
+jsTest.log( "Inserting a lot of documents into test.foo" )
+
 db = s.getDB( "test" );
 var idInc = 0;
 var valInc = 0;
 var str=""
 for (i=0;i<4*1024;i++) { str=str+"a"; }
 for (j=0; j<100; j++) for (i=0; i<512; i++){ db.foo.save({ i : idInc++, val: valInc++, y:str})}
+
+jsTest.log( "Documents inserted, waiting for error..." )
+
 db.getLastError();
+
+jsTest.log( "Doing double-checks of insert..." )
 
 // Collect some useful stats to figure out what happened
 if( db.foo.find().itcount() != 51200 ){
@@ -43,14 +50,23 @@ s.printChangeLog();
 function map() { emit('count', 1); } 
 function reduce(key, values) { return Array.sum(values) } 
 
+jsTest.log( "Test basic mapreduce..." )
+
 // Test basic mapReduce
 for ( iter=0; iter<5; iter++ ){
+
+    print( "Test #" + iter )
+    
     out = db.foo.mapReduce(map, reduce,"big_out") 
 }
+
+print( "Testing output to different db..." )
 
 // test output to a different DB
 // do it multiple times so that primary shard changes
 for (iter = 0; iter < 5; iter++) {
+    
+    print( "Test #" + iter )
     
     assert.eq( 51200, db.foo.find().itcount(), "Not all data was found!" )
     
@@ -74,13 +90,26 @@ for (iter = 0; iter < 5; iter++) {
     assert.eq(res.result.db, outDbStr, "Wrong db " + res.result.db);
 }
 
+jsTest.log( "Verifying nonatomic M/R throws..." )
+
 // check nonAtomic output
 assert.throws(function() { db.foo.mapReduce(map, reduce,{out: {replace: "big_out", nonAtomic: true}})});
 
+jsTest.log( )
+
 // add docs with dup "i"
 valInc = 0;
-for (j=0; j<100; j++) for (i=0; i<512; i++){ db.foo.save({ i : idInc++, val: valInc++, y:str})}
-db.getLastError();
+for (j=0; j<100; j++){ 
+    print( "Inserted document: " + (j * 100) );
+    for (i=0; i<512; i++){ db.foo.save({ i : idInc++, val: valInc++, y:str}) }
+}
+
+jsTest.log( "Waiting for errors..." )
+
+assert.eq( null, db.getLastError() )
+
+jsTest.log( "No errors..." )
+
 
 map2 = function() { emit(this.val, 1); }
 reduce2 = function(key, values) { return Array.sum(values); }
@@ -88,11 +117,15 @@ reduce2 = function(key, values) { return Array.sum(values); }
 // test merge
 outcol = "big_out_merge";
 
+jsTestLog( "Test A" )
+
 // mr quarter of the docs
 out = db.foo.mapReduce(map2, reduce2,{ query: {i : {$lt: 25600}}, out: {merge: outcol}});
 printjson(out);
 assert.eq( 25600 , out.counts.emit , "Received wrong result" );
 assert.eq( 25600 , out.counts.output , "Received wrong result" );
+
+jsTestLog( "Test B" )
 
 // mr further docs
 out = db.foo.mapReduce(map2, reduce2,{ query: {i : {$gte: 25600, $lt: 51200}}, out: {merge: outcol}});
@@ -100,12 +133,16 @@ printjson(out);
 assert.eq( 25600 , out.counts.emit , "Received wrong result" );
 assert.eq( 51200 , out.counts.output , "Received wrong result" );
 
+jsTestLog( "Test C" )
+
 // do 2nd half of docs
 out = db.foo.mapReduce(map2, reduce2,{ query: {i : {$gte: 51200}}, out: {merge: outcol, nonAtomic: true}});
 printjson(out);
 assert.eq( 51200 , out.counts.emit , "Received wrong result" );
 assert.eq( 51200 , out.counts.output , "Received wrong result" );
 assert.eq( 1 , db[outcol].findOne().value , "Received wrong result" );
+
+jsTestLog( "Test D" )
 
 // test reduce
 outcol = "big_out_reduce";
@@ -116,11 +153,15 @@ printjson(out);
 assert.eq( 25600 , out.counts.emit , "Received wrong result" );
 assert.eq( 25600 , out.counts.output , "Received wrong result" );
 
+jsTestLog( "Test E" )
+
 // mr further docs
 out = db.foo.mapReduce(map2, reduce2,{ query: {i : {$gte: 25600, $lt: 51200}}, out: {reduce: outcol}});
 printjson(out);
 assert.eq( 25600 , out.counts.emit , "Received wrong result" );
 assert.eq( 51200 , out.counts.output , "Received wrong result" );
+
+jsTestLog( "Test F" )
 
 // do 2nd half of docs
 out = db.foo.mapReduce(map2, reduce2,{ query: {i : {$gte: 51200}}, out: {reduce: outcol, nonAtomic: true}});
@@ -128,6 +169,8 @@ printjson(out);
 assert.eq( 51200 , out.counts.emit , "Received wrong result" );
 assert.eq( 51200 , out.counts.output , "Received wrong result" );
 assert.eq( 2 , db[outcol].findOne().value , "Received wrong result" );
+
+jsTestLog( "Test G" )
 
 // verify that data is also on secondary
 var primary = s._rs[0].test.liveNodes.master
@@ -137,6 +180,8 @@ assert.eq( 51200 , primary.getDB("test")[outcol].count() , "Wrong count" );
 for (var i = 0; i < secondaries.length; ++i) {
 	assert.eq( 51200 , secondaries[i].getDB("test")[outcol].count() , "Wrong count" );
 }
+
+jsTestLog( "DONE" )
 
 s.stop()
 
