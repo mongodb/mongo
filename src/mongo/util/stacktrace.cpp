7cd26291593d76cd5a280b5df4fedf03687a04bd
Eliot Horowitz <eliot@10gen.com>
2012-05-15 14:06:09 -0400

    make follow standards a bit more in preo for more changes

--- src/mongo/util/stacktrace.cpp
+++ src/mongo/util/stacktrace.cpp
@@ -9,29 +9,33 @@
 
 #include <execinfo.h>
 
-static const int maxBackTraceFrames = 20;
-
-void ::mongo::printStackTrace( std::ostream &os ) {
-
-    void *b[maxBackTraceFrames];
-
-    int size = ::backtrace( b, maxBackTraceFrames );
-    for ( int i = 0; i < size; i++ )
-        os << std::hex << b[i] << std::dec << ' ';
-    os << std::endl;
-
-    char **strings;
-
-    strings = ::backtrace_symbols( b, size );
-    for ( int i = 0; i < size; i++ )
-        os << ' ' << strings[i] << '\n';
-    os.flush();
-    ::free( strings );
+namespace mongo {
+    static const int maxBackTraceFrames = 20;
+    
+    void printStackTrace( std::ostream &os ) {
+        
+        void *b[maxBackTraceFrames];
+        
+        int size = ::backtrace( b, maxBackTraceFrames );
+        for ( int i = 0; i < size; i++ )
+            os << std::hex << b[i] << std::dec << ' ';
+        os << std::endl;
+        
+        char **strings;
+        
+        strings = ::backtrace_symbols( b, size );
+        for ( int i = 0; i < size; i++ )
+            os << ' ' << strings[i] << '\n';
+        os.flush();
+        ::free( strings );
+    }
 }
 
 #else
 
-void ::mongo::printStackTrace( std::ostream &os ) {}
+namespace mongo {   
+    void printStackTrace( std::ostream &os ) {}
+}
 
 #endif  // defined(MONGO_HAVE_EXECINFO_BACKTRACE)
 
