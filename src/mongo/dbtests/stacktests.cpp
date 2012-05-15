10d7e8e7166010e41b52a14081572b859d5f84ba
Eliot Horowitz <eliot@10gen.com>
2012-05-15 14:35:56 -0400

    method to check if a call stack has a constrcutor somewhere

---
+++ src/mongo/dbtests/stacktests.cpp
@@ -0,0 +1,79 @@
+
+#include "pch.h"
+
+#include "dbtests.h"
+
+class MyClass {
+public:
+    MyClass() {
+        a = mongo::inConstructorChain();
+        b = thing();
+    }
+
+    MyClass( int  x ) {
+        a = mongo::inConstructorChain();
+        b = thing();
+    }
+
+
+    bool thing() {
+        return mongo::inConstructorChain();
+    }
+
+    bool a;
+    bool b;
+};
+
+namespace foo {
+    class Bar {
+    public:
+        Bar() {
+            a = mongo::inConstructorChain();
+        }
+
+        bool a;
+    };
+};
+
+namespace StackTests {
+
+    class InCons {
+    public:
+        void run() {
+            
+            foo::Bar b;
+            ASSERT( b.a );
+            
+            {
+                MyClass x;
+                ASSERT( x.a );
+                ASSERT( x.b );
+                ASSERT( ! x.thing() );
+            }
+            
+            {
+                MyClass x(5);
+                ASSERT( x.a );
+                ASSERT( x.b );
+                ASSERT( ! x.thing() );
+            }
+            
+            ASSERT( ! mongo::inConstructorChain() );
+        }
+    };
+    
+    class All : public Suite {
+    public:
+        
+        All() : Suite( "stack" ) {
+        }
+
+        void setupTests() {
+            if ( inConstructorChainSupported() ) {
+                DEV add< InCons >(); 
+            }
+        }
+        
+    } myall;
+
+}
--- src/mongo/util/stacktrace.cpp
+++ src/mongo/util/stacktrace.cpp
@@ -4,10 +4,14 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <string>
+
+using std::string;
 
 #ifdef MONGO_HAVE_EXECINFO_BACKTRACE
 
 #include <execinfo.h>
+#include <cxxabi.h>
 
 namespace mongo {
     static const int maxBackTraceFrames = 20;
@@ -29,11 +33,83 @@ namespace mongo {
         os.flush();
         ::free( strings );
     }
+
+   string lastPiece( string& name ) {
+        size_t x = name.rfind( "::" );
+        if ( x == string::npos )
+            return "";
+        
+        string last = name.substr( x + 2 );
+        name = name.substr( 0 , x );
+        return last;
+    }
+
+    bool isNameAConstructor( string name ) {
+        size_t x = name.rfind( '(' );
+        if ( name[name.size()-1] != ')' || x == string::npos )
+            return false;
+        
+        name = name.substr( 0 , x );
+        
+        string method = lastPiece( name );
+        if ( method.size() == 0 )
+            return false;
+
+        string clazz = lastPiece( name );
+        if ( clazz.size() == 0 )
+            clazz = name;
+        
+        return method == clazz;
+    }
+
+    bool inConstructorChain( bool printOffending ){
+        void* b[maxBackTraceFrames];
+        int size = ::backtrace( b, maxBackTraceFrames );
+        char** strings = ::backtrace_symbols( b, size );
+        
+        for ( int i = 0; i < size; i++ ) {
+            string x = strings[i];
+            size_t l = x.find( '(' );
+            size_t r = x.find( '+' );
+            if ( l == string::npos || r == string::npos )
+                continue;
+            string sym = x.substr( l + 1 , r-l-1);
+            if ( sym.size() == 0 )
+                continue;
+
+            int status = -1;
+            char * nice = abi::__cxa_demangle( sym.c_str() , 0 , 0 , &status );
+            if ( status ) 
+                continue;
+            
+            string myNiceCopy = nice;
+            
+            if ( isNameAConstructor( nice ) ) {
+                if ( printOffending ) 
+                    std::cout << "found a constructor in the call tree: " << nice << std::endl;
+                ::free( strings );
+                ::free( nice );
+                return true;
+            }
+
+            ::free( nice );
+
+        }
+        ::free( strings );
+
+        return false;
+    }
+
+    bool inConstructorChainSupported() {
+        return true;
+    }
 }
 
 #else
 
 namespace mongo {   
+    bool inConstructorChain(){ return false; }
+    bool inConstructorChainSupported() { return false; }
     void printStackTrace( std::ostream &os ) {}
 }
 
