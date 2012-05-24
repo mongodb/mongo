e6f9efc3c372f5bc8871df838c6e891117655a88
Eric Milkie <milkie@10gen.com>
2012-05-24 09:41:37 -0400

    SERVER-5852 use libunwind routines for backtrace on freebsd

--- src/mongo/util/stacktrace.cpp
+++ src/mongo/util/stacktrace.cpp
@@ -34,6 +34,53 @@ namespace mongo {
     }
 }
 
+#elif MONGO_HAVE_UNWIND_BACKTRACE
+
+// This makes libunwind only work for within the
+// current process, but that's what we want anyway.
+#define UNW_LOCAL_ONLY
+
+#include <libunwind.h>
+
+namespace mongo {
+    static const int maxBackTraceFrames = 20;
+
+    void printStackTrace( std::ostream &os ) {
+        unw_context_t stack;
+        unw_cursor_t cursor;
+
+        int status = unw_getcontext(&stack);
+        if (status != 0) {
+            os << "unw_getcontext failed: " << status << std::endl;
+            return;
+        }
+        status = unw_init_local(&cursor, &stack);
+        if (status != 0) {
+            os << "unw_init_local failed: " << status << std::endl;
+            return;
+        }
+        for ( int depth = 0; (depth < maxBackTraceFrames && unw_step(&cursor) != 0); ++depth ) {
+            unw_word_t  offset;
+            unw_word_t  pc;
+            char        fname[128];
+
+            status = unw_get_reg(&cursor, UNW_REG_IP,  &pc);
+            if (status != 0) {
+                os << "unw_get_reg failed: " << status << std::endl;
+                return;
+            }
+
+            fname[0] = '\0';
+            status = unw_get_proc_name(&cursor, fname, sizeof(fname), &offset);
+            if (status != 0) {
+                os << "unw_get_proc_name failed: " << status << std::endl;
+                return;
+            }
+            os << fname << "+0x" << offset << " [0x" << std::hex << pc << std::dec << "]" << std::endl;
+        }
+    }
+}
+
 #else
 
 namespace mongo {   
