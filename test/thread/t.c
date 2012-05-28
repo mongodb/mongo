6aeca4a05cbeee1f6bc30ae5cbd9b4676003e349
Keith Bostic <keith@wiredtiger.com>
2012-05-28 11:33:03 +0000

    Now that test/fops is split out of the thread code, there's no need for the error handler to test for ENOENT.

--- test/thread/t.c
+++ test/thread/t.c
@@ -189,10 +189,6 @@ handle_error(WT_EVENT_HANDLER *handler, int error, const char *errmsg)
 	UNUSED(handler);
 	UNUSED(error);
 
-	/* Ignore complaints about truncation of missing files. */
-	if (strcmp(errmsg,
-	    "session.truncate: __wt: No such file or directory") == 0)
-		return (0);
 	return (fprintf(stderr, "%s\n", errmsg) < 0 ? -1 : 0);
 }
 
