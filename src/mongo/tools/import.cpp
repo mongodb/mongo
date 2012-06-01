67964f27dd5268b3c35a45386878c8f655565274
Shannon Weyrick <weyrick@mozek.us>
2012-06-01 15:35:07 -0400

    SERVER-5912 Better error message on long line
    Check correct bitflag for getline() overflow. Verify tool smoke tests run.
    adjust error message to indicate a potential general read error
    
    Signed-off-by: Eric Milkie <milkie@10gen.com>

--- src/mongo/tools/import.cpp
+++ src/mongo/tools/import.cpp
@@ -118,6 +118,8 @@ class Import : public Tool {
         }
         else {
             in->getline( buf , BUF_SIZE );
+            uassert(16329, str::stream() << "read error, or input line too long (max length: "
+                    << BUF_SIZE << ")", !(in->rdstate() & ios_base::failbit));
             log(1) << "got line:" << buf << endl;
         }
         uassert( 10263 ,  "unknown error reading file" ,
