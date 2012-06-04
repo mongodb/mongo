18b5d970338707f2d0e78c2b87610e5f1ee53065
Eliot Horowitz <eliot@10gen.com>
2012-06-04 11:37:56 -0400

    fix formatting in fsync2 test

--- jstests/fsync2.js
+++ jstests/fsync2.js
@@ -1,29 +1,33 @@
-if (!jsTest.options().auth) { // SERVER-4243
 
-db.fsync2.drop();
-
-db.fsync2.save( {x:1} );
-
-d = db.getSisterDB( "admin" );
-
-assert.commandWorked( d.runCommand( {fsync:1, lock: 1 } ) );
-
-for ( i=0; i<200; i++) {
-    assert.eq(1, db.fsync2.count());
-    sleep(1);
+function doTest() {
+    db.fsync2.drop();
+    
+    db.fsync2.save( {x:1} );
+    
+    d = db.getSisterDB( "admin" );
+    
+    assert.commandWorked( d.runCommand( {fsync:1, lock: 1 } ) );
+    
+    for ( i=0; i<200; i++) {
+        assert.eq(1, db.fsync2.count());
+        sleep(1);
+    }
+    
+    db.fsync2.save( {x:1} );
+    
+    m = new Mongo( db.getMongo().host );
+    
+    // Uncomment once SERVER-4243 is fixed
+    //assert.eq(1, m.getDB(db.getName()).fsync2.count());
+    
+    assert( m.getDB("admin").$cmd.sys.unlock.findOne().ok );
+    
+    db.getLastError();
+    
+    assert.eq( 2, db.fsync2.count() );
+    
 }
 
-db.fsync2.save( {x:1} );
-
-m = new Mongo( db.getMongo().host );
-
-// Uncomment once SERVER-4243 is fixed
-//assert.eq(1, m.getDB(db.getName()).fsync2.count());
-
-assert( m.getDB("admin").$cmd.sys.unlock.findOne().ok );
-
-db.getLastError();
-
-assert.eq( 2, db.fsync2.count() );
-
+if (!jsTest.options().auth) { // SERVER-4243
+    doTest();
 }
