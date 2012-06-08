77b4827482d186a2f823267333ae684f9c358af7
Kevin Matulef <matulef@10gen.com>
2012-06-08 15:16:13 -0400

    bye bye medianKey command, was nice knowin' ya

--- src/mongo/s/d_split.cpp
+++ src/mongo/s/d_split.cpp
@@ -43,88 +43,13 @@ namespace mongo {
     public:
         CmdMedianKey() : Command( "medianKey" ) {}
         virtual bool slaveOk() const { return true; }
-        virtual LockType locktype() const { return READ; }
+        virtual LockType locktype() const { return NONE; }
         virtual void help( stringstream &help ) const {
-            help <<
-                 "Internal command.\n"
-                 "example: { medianKey:\"blog.posts\", keyPattern:{x:1}, min:{x:10}, max:{x:55} }\n"
-                 "NOTE: This command may take a while to run";
+            help << "Deprecated internal command. Use splitVector command instead. \n";
         }
         bool run(const string& dbname, BSONObj& jsobj, int, string& errmsg, BSONObjBuilder& result, bool fromRepl ) {
-            const char *ns = jsobj.getStringField( "medianKey" );
-            BSONObj min = jsobj.getObjectField( "min" );
-            BSONObj max = jsobj.getObjectField( "max" );
-            BSONObj keyPattern = jsobj.getObjectField( "keyPattern" );
-
-            if ( keyPattern.isEmpty() ) {
-                errmsg = "no key pattern found in medianKey";
-                return false;
-            }
-
-            Client::Context ctx( ns );
-            NamespaceDetails *d = nsdetails(ns);
-
-            const IndexDetails *id = d->findIndexByPrefix( keyPattern ,
-                                                           true );  /* require single key */
-            if ( id == NULL )
-                return false;
-            min = Helpers::modifiedRangeBound( min , id->keyPattern() , -1 );
-            max = Helpers::modifiedRangeBound( max , id->keyPattern() , 1 );
-
-            Timer timer;
-            int num = 0;
-            int idxNo = d->idxNo(*id);
-
-            // only yielding on first half for now
-            // after this it should be in ram, so 2nd should be fast
-            {
-                shared_ptr<Cursor> c( BtreeCursor::make( d, idxNo, *id, min, max, false, 1 ) );
-                auto_ptr<ClientCursor> cc( new ClientCursor( QueryOption_NoCursorTimeout , c , ns ) );
-                while ( c->ok() ) {
-                    num++;
-                    c->advance();
-                    if ( ! cc->yieldSometimes( ClientCursor::DontNeed ) ) {
-                        cc.release();
-                        break;
-                    }
-                }
-            }
-
-            num /= 2;
-
-            auto_ptr<BtreeCursor> _c( BtreeCursor::make( d, idxNo, *id, min, max, false, 1 ) );
-            BtreeCursor& c = *_c;
-            for( ; num; c.advance(), --num );
-
-            ostringstream os;
-            os << "Finding median for index: " << keyPattern << " between " << min << " and " << max;
-            logIfSlow( timer , os.str() );
-
-            if ( !c.ok() ) {
-                errmsg = "no index entries in the specified range";
-                return false;
-            }
-
-            BSONObj median = c.prettyKey( c.currKey() );
-            result.append( "median", median );
-
-            int x = median.woCompare( min , BSONObj() , false );
-            int y = median.woCompare( max , BSONObj() , false );
-            if ( x == 0 || y == 0 ) {
-                // its on an edge, ok
-            }
-            else if ( x < 0 && y < 0 ) {
-                log( LL_ERROR ) << "median error (1) min: " << min << " max: " << max << " median: " << median << endl;
-                errmsg = "median error 1";
-                return false;
-            }
-            else if ( x > 0 && y > 0 ) {
-                log( LL_ERROR ) << "median error (2) min: " << min << " max: " << max << " median: " << median << endl;
-                errmsg = "median error 2";
-                return false;
-            }
-
-            return true;
+            errmsg = "medianKey command no longer supported. Calling this indicates mismatch between mongo versions.";
+            return false;
         }
     } cmdMedianKey;
 
