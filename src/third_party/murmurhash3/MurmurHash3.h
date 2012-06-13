b058a65565045c5e16ae64b78bf257fb658a2951
Eric Milkie <milkie@10gen.com>
2012-06-13 17:46:08 -0400

    make murmurhash3 compile on Windows

--- src/third_party/murmurhash3/MurmurHash3.h
+++ src/third_party/murmurhash3/MurmurHash3.h
@@ -5,24 +5,7 @@
 #ifndef _MURMURHASH3_H_
 #define _MURMURHASH3_H_
 
-//-----------------------------------------------------------------------------
-// Platform-specific functions and macros
-
-// Microsoft Visual Studio
-
-#if defined(_MSC_VER)
-
-typedef unsigned char uint8_t;
-typedef unsigned long uint32_t;
-typedef unsigned __int64 uint64_t;
-
-// Other compilers
-
-#else	// defined(_MSC_VER)
-
 #include <stdint.h>
-
-#endif // !defined(_MSC_VER)
 
 //-----------------------------------------------------------------------------
 
