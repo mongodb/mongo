db6f9552b6ae54da5e07e9e081002cd1863c42a1
devimplode <requiem.inferna@googlemail.com>
2012-06-14 10:34:49 +0300

    debian/init.d - remove dbpath and logpath from startscript - this belongs into the config file

--- debian/init.d
+++ debian/init.d
@@ -96,8 +96,7 @@ DIETIME=10                   # Time to wait for the server to die, in seconds
                             # 'restart' will not work
 
 DAEMONUSER=${DAEMONUSER:-mongodb}
-DAEMON_OPTS=${DAEMON_OPTS:-"--dbpath $DATA --logpath $LOGFILE run"}
-DAEMON_OPTS="$DAEMON_OPTS --config $CONF"
+DAEMON_OPTS=${DAEMON_OPTS:-"--config $CONF"}
 
 set -e
 
