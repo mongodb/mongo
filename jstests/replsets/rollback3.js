e56b0144be89e9e70358176792731d5bd07ab9b3
Kristina <kristina@10gen.com>
2012-06-20 11:41:56 -0400

    Fix test to wait until server is secondary to query it

--- jstests/replsets/rollback3.js
+++ jstests/replsets/rollback3.js
@@ -233,7 +233,8 @@ doTest = function (signal) {
     assert(A.isMaster().ismaster || A.isMaster().secondary, "A up");
     assert(B.isMaster().ismaster || B.isMaster().secondary, "B up");
     replTest.awaitReplication();
-    waitForAllMembers(B);
+
+    assert.soon(function() { return B.isMaster().secondary; });
 
     assert( dbs_match(a,b), "server data sets do not match after rollback, something is wrong");
 
