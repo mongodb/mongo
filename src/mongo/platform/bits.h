f34e58a18c4c7bc7332b8f426962e09b3258e93d
Eliot Horowitz <eliot@10gen.com>
2012-06-24 20:38:11 -0400

    added MONGO_PLATFORM_(32|64) to make it easier to change behavior at compile time

---
+++ src/mongo/platform/bits.h
@@ -0,0 +1,29 @@
+// bits.h
+
+/*    Copyright 2012 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+// figure out if we're on a 64 or 32 bit system
+
+#if defined(__x86_64__) || defined(__amd64__) || defined(_WIN64)
+#define MONGO_PLATFORM_64
+#elif defined(__i386__) || defined(_WIN32)
+#define MONGO_PLATFORM_32
+#else
+#error "unknown platform"
+#endif
+
---
+++ src/mongo/platform/bits_test.cpp
@@ -0,0 +1,34 @@
+// bits_test.cpp
+
+/*    Copyright 2012 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "mongo/platform/bits.h"
+
+#include "mongo/unittest/unittest.h"
+
+namespace mongo {
+
+    TEST( BitsTest, HeaderCorrect ) {
+#if defined(MONGO_PLATFORM_64)
+        ASSERT_EQUALS( 8, sizeof(char*) );
+#elif defined(MONGO_PLATFORM_32)
+        ASSERT_EQUALS( 4, sizeof(char*) );
+#else
+        ASSERT( false );
+#endif
+    }
+
+}
