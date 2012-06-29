ce91fa9de46717ae0d1bbbf85046558ff02f527f
Mathias Stearn <mathias@10gen.com>
2012-06-29 15:22:44 -0400

    Make test more clear SERVER-4674

--- jstests/aggregation/bugs/server4638.js
+++ jstests/aggregation/bugs/server4638.js
@@ -1,22 +1,16 @@
-
+// SERVER-4638 - this tests explicit undefined values
+// This case is marked as a dup of SERVER-4674
 
 t = db.server4638
 t.drop();
 
-function make_blob(sz) {
-    var x = "";
-    while ( x.length < sz )
-        x+= ".";
-}
-
-small_blob = make_blob( 250 )
-
 for ( i=0; i<2; i++ ){
     //t.insert( { _id : i , x : i } ); // this version works
-    t.insert( { _id : i , x : i , blob : small_blob , a : [] } );
+    t.insert( { _id : i , x : i , undef: undefined } );
 }
+
 db.getLastError();
-res = t.aggregate( { $project : { x : 1 } } )//, { $group : { _id : "$x" , total : { $sum : 1 } } } )
+res = t.aggregate( { $project : { x : 1 } } )
 printjson(res)
 
 assert(res.ok, 'server4638 failed');
