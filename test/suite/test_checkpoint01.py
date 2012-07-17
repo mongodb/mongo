36eb0eae628ee3c49dcea3ac255c0d802ab9e62b
Michael Cahill <michael.cahill@wiredtiger.com>
2012-07-17 21:01:07 +1000

    Add a copyright notice to test_checkpoint01.py.

--- test/suite/test_checkpoint01.py
+++ test/suite/test_checkpoint01.py
@@ -1,9 +1,38 @@
-"""
-Python versino of snapshot tests(http://source.wiredtiger.com/1.2.2/snapshots.html). 
-We define range of data for different snapshots in snapshots dict. First, we set on to 
-build snapsots using build_file_with_snapshots() and then we verify snapshots data inside
-check(). Then we test cursor locks and finally we drop the snapshots.
-"""
+#!/usr/bin/env python
+#
+# Copyright (c) 2008-2012 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# test_checkpoint01.py
+# 	Checkpoint tests
+#
+# We define a range of data for different snapshots in snapshots dict.
+# First, we build the snapsots using build_file_with_snapshots(), then we
+# verify snapshots data inside check(). Then we test cursor locks and finally
+# we drop the snapshots.
+
 import os
 import sys
 import traceback
