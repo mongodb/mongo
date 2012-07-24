076139589fbbb7f55b0b1b7a2c7619ce49af4de6
Randolph Tan <randolph@10gen.com>
2012-07-24 16:05:08 -0400

    SERVER-6463 Mongos sets shard host string to "set/" without any seed members

--- src/mongo/client/dbclient_rs.cpp
+++ src/mongo/client/dbclient_rs.cpp
@@ -602,7 +602,7 @@ namespace mongo {
             break;
         }
 
-        return changed || origHosts != numHosts;
+        return (changed || origHosts != numHosts) && numHosts > 0;
 
     }
 
@@ -726,6 +726,13 @@ namespace mongo {
             log( ! verbose ) << "ReplicaSetMonitor::_checkConnection: " << conn->toString()
                              << ' ' << o << endl;
             
+            if (!o["secondary"].trueValue() && !o["ismaster"].trueValue()) {
+                /* This node is not in steady state so don't trust the host list.
+                 * In the worst case, this node can even be a non member of the set.
+                 */
+                return false;
+            }
+
             // add other nodes
             BSONArrayBuilder b;
             if ( o["hosts"].type() == Array ) {
