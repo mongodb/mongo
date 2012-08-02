f3343c4e1cde8517b63f9e3a0673c7a09ddf232d
Mathias Stearn <mathias@10gen.com>
2012-08-02 13:12:37 -0400

    Add exceptions to test for SERVER-6189 when it is expected to fail

--- jstests/aggregation/bugs/server6189.js
+++ jstests/aggregation/bugs/server6189.js
@@ -17,6 +17,19 @@ function test(date, testSynthetics) {
                                 // $substr will call coerceToString
                                 //, string: {$substr: ['$date', 0,1000]} // server-6679
                                 }} );
+
+    if (date.getUTCFullYear() < 1970 && _isWindows() && result.code == 16422) {
+        // some versions of windows (but not all) fail with dates before 1970
+        print("skipping test of " + date.tojson() + " because system doesn't support old dates");
+        return;
+    }
+
+    if (date.valueOf()/1000 < -2*1024*1024*1024 && result.code == 16421) {
+        // we correctly detected that we are outside of the range of a 32-bit time_t
+        print("skipping test of " + date.tojson() + " because it is outside of time_t range");
+        return;
+    }
+
     assert.commandWorked(res);
     assert.eq(res.result[0], { year: date.getUTCFullYear()
                              , month: date.getUTCMonth() + 1 // jan == 1
@@ -55,8 +68,7 @@ test(ISODate('1960-01-02 03:04:05.001Z'), true); // second = 5
 test(ISODate('1960-01-02 03:04:05.999Z'), true); // second = 5
 
 // Test date before 1900 (negative tm_year values from gmtime)
-// disabled because it fails with 32-bit time_t
-// test(ISODate('1860-01-02 03:04:05.006Z'), false);
+test(ISODate('1860-01-02 03:04:05.006Z'), false);
 
 // Test with time_t == -1 and 0
 test(new Date(-1000), false);
