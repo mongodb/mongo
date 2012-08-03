209bf3b97a416cb74352a8e3fe6e60fc65a10bf3
Gary Murakami <gary.murakami@10gen.com>
2012-08-03 14:48:28 -0400

    git submodule scons build issue fix If you add mongo to a parent project via "git submodule add", you need these fixes to build mongo.

--- buildscripts/utils.py
+++ buildscripts/utils.py
@@ -30,7 +30,7 @@ def getAllSourceFiles( arr=None , prefix="." ):
 
 
 def getGitBranch():
-    if not os.path.exists( ".git" ):
+    if not os.path.exists( ".git" ) or not os.path.isdir(".git"):
         return None
 
     version = open( ".git/HEAD" ,'r' ).read().strip()
@@ -57,7 +57,7 @@ def getGitBranchString( prefix="" , postfix="" ):
     return prefix + b + postfix
 
 def getGitVersion():
-    if not os.path.exists( ".git" ):
+    if not os.path.exists( ".git" ) or not os.path.isdir(".git"):
         return "nogitversion"
 
     version = open( ".git/HEAD" ,'r' ).read().strip()
