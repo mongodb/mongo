mongodb/mongo

commit b3f4d6c4b7aab682376a4503d6f76d698d08d799

SERVER-5007 fixing typos, smp_flags

rpm/mongo.spec
@@ -45,7 +45,7 @@ to develop mongo client software.
 %setup
 
 %build
-scons --prefix=$RPM_BUILD_ROOT/usr all
+scons -%{?_smp_mflags} -prefix=$RPM_BUILD_ROOT/usr all
 # XXX really should have shared library here
 
 %install
@@ -104,6 +104,7 @@ fi
 %{_bindir}/mongofiles
 %{_bindir}/mongoimport
 %{_bindir}/mongorestore
+%{_bindir}/mongosniff
 %{_bindir}/mongostat
 %{_bindir}/bsondump
 %{_bindir}/mongotop
@@ -117,7 +118,7 @@ fi
 %{_mandir}/man1/mongosniff.1*
 %{_mandir}/man1/mongostat.1*
 %{_mandir}/man1/mongorestore.1*
-%{_mandir}/man1/bsondum.1*
+%{_mandir}/man1/bsondump.1*
 
 %files server
 %defattr(-,root,root,-)

commit f52e49da754f4c26255af1323cb34e56a77115e2

Fixing typo introduced in b3f4d6c4b7aab682376a4503d6f76d698d08d799

rpm/mongo.spec
@@ -51,7 +51,7 @@ to develop mongo client software.
 %setup
 
 %build
-scons -%{?_smp_mflags} -prefix=$RPM_BUILD_ROOT/usr all
+scons -%{?_smp_mflags} --prefix=$RPM_BUILD_ROOT/usr all
 # XXX really should have shared library here
 
 %install