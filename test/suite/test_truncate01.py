78cdcd97d54a50d3381185e83121639319ead521
Keith Bostic <keith@wiredtiger.com>
2012-08-26 12:00:51 +0000

    Re-write the primary truncation test to have a set of "layouts" which have implicit, inserted and appended records.  Remove the table test for now, I'll put it back once all of this is working.

--- test/suite/test_truncate01.py
+++ test/suite/test_truncate01.py
@@ -79,9 +79,6 @@ class test_truncate_uri(wttest.WiredTigerTestCase):
             self.session.drop(uri, None)
 
 
-# XXX
-#       Test where the start of the delete is within an append area
-#       we're already testing where it's inside the implicit area.
 # Test session.truncate.
 class test_truncate(wttest.WiredTigerTestCase):
     name = 'test_truncate'
@@ -100,21 +97,18 @@ class test_truncate(wttest.WiredTigerTestCase):
         ('recno', dict(keyfmt='r')),
         ('string', dict(keyfmt='S')),
     ]
-    image = [
-        ('in-memory', dict(reopen=False,append=False,implicit=False)),
-        ('on-disk', dict(reopen=True,append=False,implicit=False)),
-        ('on-disk-append', dict(reopen=True,append=True,implicit=False)),
-        ('on-disk-append-implicit',
-            dict(reopen=True,append=True,implicit=True)),
+    reopen = [
+        ('in-memory', dict(reopen=False)),
+        ('on-disk', dict(reopen=True)),
     ]
     size = [
         ('small', dict(nentries=100,skip=7,search=False)),
-        ('small', dict(nentries=100,skip=7,search=True)),
-        ('big', dict(nentries=1000,skip=37,search=True)),
+        ('small-with-search', dict(nentries=100,skip=7,search=True)),
+        ('big', dict(nentries=1000,skip=37,search=False)),
     ]
 
     scenarios = number_scenarios(
-        multiply_scenarios('.', types, keyfmt, image, size))
+        multiply_scenarios('.', types, keyfmt, size, reopen))
 
     # Set a cursor and optionally search for the item.
     def initCursor(self, uri, key):
@@ -126,7 +120,7 @@ class test_truncate(wttest.WiredTigerTestCase):
         # Test scenarios where we fully instantiate a cursor as well as where we
         # only set the key.  The key may not exist in a column-store so ignore
         # the flag if implicit is set.
-        if self.search and not self.implicit:
+        if self.search and not cursor.key_format == 'r':
             self.assertEqual(cursor.search(), 0)
             self.assertEqual(cursor.get_key(), key_populate(cursor, key))
 
@@ -143,26 +137,26 @@ class test_truncate(wttest.WiredTigerTestCase):
         else:
             cur1.close()
         if not cur2:
-            end = self.nentries - 1
+            end = self.nentries
         else:
             cur2.close()
 
         # If the object should be empty, confirm that.
-        if begin == 1 and end == self.nentries - 1:
+        if begin == 1 and end == self.nentries:
             confirm_empty(self, uri)
             return
 
         # Check the expected values against the object.
         cursor = self.session.open_cursor(uri, None)
         for i in range(begin, end + 1):
-            expected[key_populate(cursor, i)] = 0
+            expected[key_populate(cursor, i)] = [0]
         for k, v in expected.iteritems():
             cursor.set_key(k)
-            if v == 0 and \
+            if v == [0] and \
               cursor.key_format == 'r' and cursor.value_format == '8t':
                 cursor.search()
                 self.assertEqual(cursor.get_values(), [0])
-            elif v == 0:
+            elif v == [0]:
                 self.assertEqual(cursor.search(), wiredtiger.WT_NOTFOUND)
             else:
                 cursor.search()
@@ -170,105 +164,149 @@ class test_truncate(wttest.WiredTigerTestCase):
         cursor.close()
 
     # Test truncation using cursors.
-    #
-    # For begin and end: -1 means pass None for the cursor arg to truncate.
-    # An integer N, with 1 <= N < self.nentries, passes a cursor positioned
-    # at that element.
     def test_truncate_cursor(self):
         uri = self.type + self.name
+
+        # layout:
+        #    the number of initial implicit records
+        #    the number of initial inserted records
+        #    the number of trailing implicit records
+        #    the number of trailing inserted records
+        layout = [
+            # simple set of rows
+            (0, 0, 0, 0),
+
+            # trailing append list, no delete point overlap
+            (0, 0, 0, self.skip - 3),
+
+            # trailing append list, delete point overlap
+            (0, 0, 0, self.skip + 3),
+
+            # trailing implicit list, no delete point overlap
+            (0, 0, self.skip - 3, 1),
+
+            # trailing implicit list, delete point overlap
+            (0, 0, self.skip + 3, 1),
+
+            # leading insert list, no delete point overlap
+            (0, self.skip - 3, 0, 0),
+
+            # leading insert list, delete point overlap
+            (0, self.skip + 3, 0, 0),
+
+            # leading implicit list, no delete point overlap
+            (self.skip - 3, 1, 0, 0),
+
+            # leading implicit list, delete point overlap
+            (self.skip + 3, 1, 0, 0),
+        ]
+
+        # list: truncation patterns applied on top of the layout.
+        #
+        # begin and end: -1 means pass None for the cursor arg to truncate.  An
+        # integer N, with 1 <= N < self.nentries, truncates from/to a cursor
+        # positioned at that row.
         list = [
-            (-1, self.nentries - 1),            # begin to end, begin = None
+            (-1, self.nentries),                # begin to end, begin = None
             (1, -1),                            # begin to end, end = None
-            (1, self.nentries - 1),             # begin to end
+            (1, self.nentries),                 # begin to end
             (-1, self.nentries - self.skip),    # begin to middle, begin = None
             (1, self.nentries - self.skip),     # begin to middle
             (self.skip, -1),                    # middle to end, end = None
-            (self.skip, self.nentries - 1),     # middle to end
+            (self.skip, self.nentries),         # middle to end
             (self.skip,                         # middle to different middle
                 self.nentries - self.skip),
             (1, 1),                             # begin to begin
-            (self.nentries - 1,                 # end to end
-                self.nentries - 1),
+            (self.nentries, self.nentries),     # end to end
             (self.skip, self.skip)              # middle to same middle
             ]
 
-        # Implicit records only apply to column-store.
-        if self.implicit and not self.keyfmt == 'r':
-            return
+        # Build the layout we're going to test
+        total = self.nentries
+        for begin_implicit,begin_insert,end_implicit,end_insert in layout:
 
-        # A simple, one-file file or table object.
-        for begin,end in list:
-            # We want to test cursor transition to the append list: if
-            # self.append is set, append the last rows after we've written
-            # the object and re-read it, so we have a mix of an on-disk
-            # format and an append list.
-            append_count = 0
-            if self.append:
-                append_count = self.skip + 10
-            pop_count = self.nentries - append_count
+            # implicit layouts are only interesting for column-stores
+            if self.keyfmt != 'r' and (begin_implicit or end_implicit):
+                continue;
 
-            # Populate the object.
-            simple_populate(self, uri, self.config + self.keyfmt, pop_count)
+            # implicit records require insert/append records
+            if begin_implicit and not begin_insert or \
+                end_implicit and not end_insert:
+                raise AssertionError('test error: implicit set without insert')
 
-            # Build a dictionary of what the object should look like and
-            # compare against it when we're done.
-            expected = {}
-            cursor = self.session.open_cursor(uri, None)
-            for i in range(1, pop_count):
-                expected[key_populate(cursor, i)] = [value_populate(cursor, i)]
-            cursor.close()
-
-            # Optionally close and re-open the object to get a disk image.
-            if self.reopen:
-                self.reopen_conn()
-
-            # Optionally append rows to the object.
-            if self.append:
-                # Column-store: create implicit records, making sure they span
-                # the end of the delete range, in other words, "12" is larger
-                # than the "10" we used to set append_count.
-                if self.implicit:
-                    cursor = self.session.open_cursor(uri, None)
-                    for i in range(1, 13):
-                        expected[key_populate(cursor, pop_count)] = 0
-                        pop_count += 1
-                    cursor.close()
-
-                cursor = self.session.open_cursor(uri, None)
-                while pop_count < self.nentries:
-                    key = key_populate(cursor, pop_count)
-                    cursor.set_key(key)
-                    val = value_populate(cursor, pop_count)
-                    cursor.set_value(val)
-                    cursor.insert()
-
-                    expected[key] = [val]
-                    pop_count += 1
-                cursor.close()
-
-            self.truncateRangeAndCheck(uri, begin, end, expected)
-            self.session.drop(uri, None)
-
-        # A complex, multi-file table object.
-        if self.type == "table:":
             for begin,end in list:
-                complex_populate(
-                    self, uri, self.config + self.keyfmt, self.nentries)
+                '''
+                print '===== run:'
+                print 'key:', self.keyfmt, 'begin:', begin, 'end:', end
+                print 'total: ', total, \
+                    'begin_implicit:', begin_implicit, \
+                    'begin_insert:', begin_insert, \
+                    'end_implicit:', end_implicit, \
+                    'end_insert:', end_insert
+                '''
 
-                # Build a dictionary of what the object should look like and
-                # compare against it when we're done.
+                # Build a dictionary of what the object should look like for
+                # later comparison
                 expected = {}
+
+                # Create the object.
+                self.session.create(
+                    uri, 'value_format=S,' + self.config + self.keyfmt)
+
+                # Insert the records that aren't implicit or inserted.
+                start = begin_implicit + begin_insert
+                stop = self.nentries - (end_implicit + end_insert)
                 cursor = self.session.open_cursor(uri, None)
-                for i in range(1, self.nentries):
-                    expected[key_populate(cursor, i)] = \
-                        value_populate_complex(i)
+                for i in range(start + 1, stop + 1):
+                    k = key_populate(cursor, i)
+                    v = value_populate(cursor, i)
+                    cursor.set_key(k)
+                    cursor.set_value(v)
+                    cursor.insert()
+                    expected[k] = [v]
                 cursor.close()
 
+                # Optionally close and re-open the object to get a disk image
+                # instead of a big insert list.
                 if self.reopen:
                     self.reopen_conn()
 
-                self.truncateRangeAndCheck(uri, begin, end, expected)
+                # Optionally insert initial implicit records.
+                cursor = self.session.open_cursor(uri, None)
+                start = 0;
+                for i in range(0, begin_implicit):
+                    start += 1
+                    k = key_populate(cursor, start)
+                    expected[k] = [0]
 
+                # Optionally insert leading records.
+                for i in range(0, begin_insert):
+                    start += 1
+                    k = key_populate(cursor, start)
+                    v = value_populate(cursor, start)
+                    cursor.set_key(k)
+                    cursor.set_value(v)
+                    cursor.insert()
+                    expected[k] = [v]
+
+                # Optionally insert trailing implicit records.
+                for i in range(0, end_implicit):
+                    stop += 1
+                    k = key_populate(cursor, stop)
+                    expected[k] = [0]
+
+                # Optionally insert trailing records.
+                for i in range(0, end_insert):
+                    stop += 1
+                    k = key_populate(cursor, stop)
+                    v = value_populate(cursor, stop)
+                    cursor.set_key(k)
+                    cursor.set_value(v)
+                    cursor.insert()
+                    expected[k] = [v]
+                cursor.close()
+
+                self.truncateRangeAndCheck(uri, begin, end, expected)
                 self.session.drop(uri, None)
 
 
