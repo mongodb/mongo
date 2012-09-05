725e52bee717a570e435b1b7eb4acf62ab813f3a
daveh86 <howsdav@gmail.com>
2012-09-06 09:39:01 +1000

    Fix comment in src/mongo/client/gridfs.h
    When loading from a string rather than a file, remoteName is not optional
    
    Signed-off-by: Matt Kangas <matt.kangas@mongodb.com>

--- src/mongo/client/gridfs.h
+++ src/mongo/client/gridfs.h
@@ -85,8 +85,7 @@ namespace mongo {
          * puts the file represented by data into the db
          * @param data pointer to buffer to store in GridFS
          * @param length length of buffer
-         * @param remoteName optional filename to use for file stored in GridFS
-         *                   (default is to use fileName parameter)
+         * @param remoteName filename to use for file stored in GridFS
          * @param contentType optional MIME type for this object.
          *                    (default is to omit)
          * @return the file object
