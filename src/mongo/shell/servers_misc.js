243de0b208795923fd094a7390c0a748d8b5916d
Spencer T Brody <spencer@10gen.com>
2012-09-10 17:12:48 -0400

    SERVER-4237 Only set db in spawned shell process if db is currently defined

--- src/mongo/shell/servers_misc.js
+++ src/mongo/shell/servers_misc.js
@@ -260,7 +260,8 @@ function startParallelShell( jsCode, port ){
 
     if (TestData) {
         jsCode = "TestData = " + tojson(TestData) +
-            ";jsTest.authenticate(db.getMongo());db = db.getSiblingDB('" + db.getName() + "');" +
+            ";jsTest.authenticate(db.getMongo());" +
+            ((typeof(db) != 'undefined') ? "db = db.getSiblingDB('" + db.getName() + "');" : "") +
             jsCode;
     }
 
