ad7db89c325c0b21342f125f9b524a356c143afe
Michael Cahill <michael.cahill@wiredtiger.com>
2012-09-14 12:28:25 +1000

    Allow "lsm:" types to "wt list".

--- src/utilities/util_list.c
+++ src/utilities/util_list.c
@@ -39,8 +39,8 @@ util_list(WT_SESSION *session, int argc, char *argv[])
 	case 0:
 		break;
 	case 1:
-		if ((name = util_name(
-		    *argv, "table", UTIL_FILE_OK | UTIL_TABLE_OK)) == NULL)
+		if ((name = util_name(*argv, "table",
+		    UTIL_FILE_OK | UTIL_LSM_OK | UTIL_TABLE_OK)) == NULL)
 			return (1);
 		break;
 	default:
