4204d9a1125cc6f032195d4871f835e53248b72e
daveh86 <howsdav@gmail.com>
2012-09-17 13:10:33 +1000

    Update src/mongo/client/dbclientinterface.h

--- src/mongo/client/dbclientinterface.h
+++ src/mongo/client/dbclientinterface.h
@@ -841,11 +841,13 @@ namespace mongo {
            @param cache if set to false, the index cache for the connection won't remember this call
            @param background build index in the background (see mongodb docs/wiki for details)
            @param v index version. leave at default value. (unit tests set this parameter.)
+           @param ttl. The value of how many seconds before data should be removed from a collection. To function must be within an index that contains a date field
+
            @return whether or not sent message to db.
              should be true on first call, false on subsequent unless resetIndexCache was called
          */
         virtual bool ensureIndex( const string &ns , BSONObj keys , bool unique = false, const string &name = "",
-                                  bool cache = true, bool background = false, int v = -1 );
+                                  bool cache = true, bool background = false, int v = -1, int ttl = 0 );
 
         /**
            clears the index cache, so the subsequent call to ensureIndex for any index will go to the server
