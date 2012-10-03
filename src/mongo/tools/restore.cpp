e76de3ccd440962bec402d7b47f238e596fccad7
Eric Milkie <milkie@10gen.com>
2012-10-02 22:06:43 -0400

    SERVER-7181 fix build - assert code conflict

--- src/mongo/tools/restore.cpp
+++ src/mongo/tools/restore.cpp
@@ -498,7 +498,7 @@ private:
             ::abort();
         }
 
-        massert(16439, str::stream() << "Error calling getLastError: " << err["errmsg"],
+        massert(16441, str::stream() << "Error calling getLastError: " << err["errmsg"],
                 err["ok"].trueValue());
     }
 };
