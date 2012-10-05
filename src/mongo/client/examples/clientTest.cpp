f82aa2825f93d827f6493a3c58a44125506d48b9
Tad Marshall <tad@10gen.com>
2012-10-05 17:23:22 -0400

    SERVER-7050 Fix Windows compile

--- src/mongo/client/examples/clientTest.cpp
+++ src/mongo/client/examples/clientTest.cpp
@@ -152,7 +152,7 @@ int main( int argc, const char **argv ) {
         conn.ensureIndex(ttlns, BSON("ttltime" << 1), false, "", true, false, -1, 5);
         verify(!conn.findOne(ttlns, BSONObjBuilder().append("name", "foo").obj()).isEmpty());
         // Sleep 66 seconds, 60 seconds for the TTL loop, 5 seconds for the TTL and 1 to ensure
-        sleep(66);
+        sleepsecs(66);
         verify(conn.findOne(ttlns, BSONObjBuilder().append("name", "foo").obj()).isEmpty());
     }
 
