43b0528e1a098dfcdf3753e8fe61ef41af80fd8c
Eliot Horowitz <eliot@10gen.com>
2012-10-10 13:12:59 -0400

    simple Random class to consolidate all random number needs

---
+++ src/mongo/platform/random.h
@@ -0,0 +1,42 @@
+// random.h
+
+/*    Copyright 2012 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#include <cstdlib>
+#include <inttypes.h>
+
+namespace mongo {
+    
+    class PseudoRandom {
+    public:
+        PseudoRandom( unsigned seed ) 
+            : _seed( seed ) {
+        }
+
+        int32_t nextInt32();
+
+        /**
+         * @return a number between 0 and max
+         */
+        int32_t nextInt32( int32_t max ) { return nextInt32() % max; }
+
+    private:
+        unsigned _seed;
+    };
+
+}
