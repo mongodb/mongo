be1898fa0598cce3296d69cb5b760a47de8908bf
Eliot Horowitz <eliot@10gen.com>
2012-10-09 23:12:06 -0400

    fix error code conflict

--- src/mongo/bson/bsonobjiterator.h
+++ src/mongo/bson/bsonobjiterator.h
@@ -70,7 +70,7 @@ namespace mongo {
 
             BSONElement e( _pos, maxLen );
             int esize = e.size( maxLen );
-            massert( 16442, "BSONElement has bad size", esize > 0 );
+            massert( 16446, "BSONElement has bad size", esize > 0 );
             _pos += esize;
 
             return e;
