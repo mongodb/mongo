f7196c1b974a9c5ba1f093bc1749d0fe9f39e661
Eliot Horowitz <eliot@10gen.com>
2012-10-14 08:46:06 -0400

    make memconcept::is string const

--- src/mongo/db/memconcept.h
+++ src/mongo/db/memconcept.h
@@ -37,12 +37,12 @@ namespace mongo {
 
         /** note you can be more than one thing; a datafile header is also the starting pointer
             for a file */
-        void is(void *p, concept c, std::string desc = "", unsigned len=0);
+        void is(void *p, concept c, const std::string desc = "", unsigned len=0);
 
 #if 1
 //#if !defined(_DEBUG)
         inline void invalidate(void *p, unsigned len) { }
-        inline void is(void *p, concept c, std::string, unsigned) { }
+        inline void is(void *p, concept c, const std::string, unsigned) { }
 #endif
 
     }
