1c33a98ebf1a0caa5694fcc9a026bf1949c92f65
Andy Schwerin <schwerin@10gen.com>
2012-10-15 17:51:35 -0400

    Include boost/range/size.hpp, instead of boost/range.hpp in cmdline_test.cpp

--- src/mongo/db/cmdline_test.cpp
+++ src/mongo/db/cmdline_test.cpp
@@ -14,7 +14,7 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <boost/range.hpp>
+#include <boost/range/size.hpp>
 #include <string>
 #include <vector>
 
