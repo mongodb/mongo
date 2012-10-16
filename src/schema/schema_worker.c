08a9eb79bc99320b72391c30c10de79715b71f91
Keith Bostic <keith@wiredtiger.com>
2012-10-16 15:36:48 +0000

    Compaction and checkpoint should both operate on the table indices as well (the latter fixes a bug, we weren't checkpointing indices when a table was specified as the checkpoint target).

--- src/schema/schema_worker.c
+++ src/schema/schema_worker.c
@@ -54,6 +54,19 @@ __wt_schema_worker(WT_SESSION_IMPL *session,
 			WT_TRET(__wt_session_release_btree(session));
 			WT_RET(ret);
 		}
+
+		/* Compaction affects index files as well. */
+		if (func == __wt_compact || func == __wt_checkpoint) {
+			WT_RET(__wt_schema_open_indices(session, table));
+			for (i = 0; i < table->nindices; i++) {
+				WT_RET(__wt_session_get_btree_ckpt(
+				    session, table->indices[i]->source,
+				    cfg, open_flags));
+				ret = func(session, cfg);
+				WT_TRET(__wt_session_release_btree(session));
+				WT_RET(ret);
+			}
+		}
 	} else
 		return (__wt_bad_object_type(session, uri));
 
