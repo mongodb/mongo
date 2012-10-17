61a4ff2a9d44d08ce9c136281bcd46b5657c0ea0
Keith Bostic <keith@wiredtiger.com>
2012-10-17 09:45:09 +0000

    Compaction is also supported on objects of type colgroup and index.

--- src/utilities/util_compact.c
+++ src/utilities/util_compact.c
@@ -29,8 +29,9 @@ util_compact(WT_SESSION *session, int argc, char *argv[])
 	/* The remaining argument is the table name. */
 	if (argc != 1)
 		return (usage());
-	if ((uri = util_name(*argv,
-	    "table", UTIL_FILE_OK | UTIL_LSM_OK | UTIL_TABLE_OK)) == NULL)
+	if ((uri = util_name(*argv, "table",
+	    UTIL_COLGROUP_OK | UTIL_FILE_OK | UTIL_INDEX_OK |
+	    UTIL_LSM_OK | UTIL_TABLE_OK)) == NULL)
 		return (1);
 
 	if ((ret = session->compact(session, uri, NULL)) != 0) {
