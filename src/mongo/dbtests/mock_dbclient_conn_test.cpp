9bd29481d06d953fda6ececf2db447b90778be32
Randolph Tan <randolph@10gen.com>
2012-10-26 10:43:32 -0400

    SERVER-7475 Remove upper bound test on mock dbclient delay
    Set a looser lower bound for delay since it is breaking Windows buildbot.

--- src/mongo/dbtests/mock_dbclient_conn_test.cpp
+++ src/mongo/dbtests/mock_dbclient_conn_test.cpp
@@ -296,7 +296,10 @@ namespace mongo_test {
             mongo::Timer timer;
             conn.query("x.x");
             const int nowInMilliSec = timer.millis();
-            ASSERT_GREATER_THAN_OR_EQUALS(nowInMilliSec, 149);
+            // Use a more lenient lower bound since some platforms like Windows
+            // don't guarantee that sleeps will not wake up earlier (unlike
+            // nanosleep we use for Linux)
+            ASSERT_GREATER_THAN_OR_EQUALS(nowInMilliSec, 130);
         }
 
         {
@@ -304,7 +307,7 @@ namespace mongo_test {
             BSONObj response;
             conn.runCommand("x.x", BSON("serverStatus" << 1), response);
             const int nowInMilliSec = timer.millis();
-            ASSERT_GREATER_THAN_OR_EQUALS(nowInMilliSec, 149);
+            ASSERT_GREATER_THAN_OR_EQUALS(nowInMilliSec, 130);
         }
 
         ASSERT_EQUALS(1U, server.getQueryCount());
