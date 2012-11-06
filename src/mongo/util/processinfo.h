1538f5d076fc15b5736d8535316daed72b26f987
Andrea Lattuada <andrea.lattuada@10gen.com>
2012-11-06 13:37:15 -0500

    SERVER-7570 Initialize ProcessInfo::SystemInfo at startup, add pageSize.
    Signed-off-by: Andy Schwerin <schwerin@10gen.com>

--- src/mongo/util/processinfo.h
+++ src/mongo/util/processinfo.h
@@ -77,6 +77,11 @@ namespace mongo {
         const unsigned getNumCores() const { return sysInfo().numCores; }
 
         /**
+         * Get the system page size in bytes.
+         */
+        static unsigned long long getPageSize() { return systemInfo->pageSize; }
+
+        /**
          * Get the CPU architecture (e.g. x86, x86_64)
          */
         const string& getArch() const { return sysInfo().cpuArch; }
@@ -116,6 +121,7 @@ namespace mongo {
             unsigned addrSize;
             unsigned long long memSize;
             unsigned numCores;
+            unsigned long long pageSize;
             string cpuArch;
             bool hasNuma;
             BSONObj _extraStats;
@@ -123,6 +129,7 @@ namespace mongo {
                     addrSize( 0 ),
                     memSize( 0 ),
                     numCores( 0 ),
+                    pageSize( 0 ),
                     hasNuma( false ) { 
                 // populate SystemInfo during construction
                 collectSystemInfo();
@@ -137,18 +144,14 @@ namespace mongo {
 
         static bool checkNumaEnabled();
 
-        const SystemInfo& sysInfo() const {
-            // initialize and collect sysInfo on first call
-            // TODO: SERVER-5112
-            static ProcessInfo::SystemInfo *initSysInfo = NULL;
-            if ( ! initSysInfo ) {
-                scoped_lock lk( _sysInfoLock );
-                if ( ! initSysInfo ) {
-                    initSysInfo = new SystemInfo();
-                }
-            }
-            return *initSysInfo;
+        static ProcessInfo::SystemInfo* systemInfo;
+
+        inline const SystemInfo& sysInfo() const {
+            return *systemInfo;
         }
+
+    public:
+        static void initializeSystemInfo();
 
     };
 
