8ee10017eed4813af97a0fc82dcc9b3f3d8e3b0c
Andy Schwerin <schwerin@10gen.com>
2012-11-06 17:22:35 -0500

    Fix bson_extract_test -- BSONElement reference outlived containing BSONObj.

--- src/mongo/bson/util/bson_extract_test.cpp
+++ src/mongo/bson/util/bson_extract_test.cpp
@@ -22,94 +22,77 @@
 using namespace mongo;
 
 TEST(ExtractBSON, ExtractField) {
+    BSONObj obj = BSON("a" << 1 << "b" << "hello");
     BSONElement element;
-    ASSERT_OK(bsonExtractField(BSON("a" << 1 << "b" << "hello"), "a", &element));
+    ASSERT_OK(bsonExtractField(obj, "a", &element));
     ASSERT_EQUALS(1, element.Int());
-
-    ASSERT_OK(bsonExtractField(BSON("a" << 1 << "b" << "hello"), "b", &element));
+    ASSERT_OK(bsonExtractField(obj, "b", &element));
     ASSERT_EQUALS(std::string("hello"), element.str());
-
-    ASSERT_EQUALS(ErrorCodes::NoSuchKey,
-                  bsonExtractField(BSON("a" << 1 << "b" << "hello"), "c", &element));
+    ASSERT_EQUALS(ErrorCodes::NoSuchKey, bsonExtractField(obj, "c", &element));
 }
 
 TEST(ExtractBSON, ExtractTypedField) {
+    BSONObj obj = BSON("a" << 1 << "b" << "hello");
     BSONElement element;
-    ASSERT_OK(bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "a", NumberInt, &element));
+    ASSERT_OK(bsonExtractTypedField(obj, "a", NumberInt, &element));
     ASSERT_EQUALS(1, element.Int());
-
-    ASSERT_OK(bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "b", String, &element));
+    ASSERT_OK(bsonExtractTypedField(obj, "b", String, &element));
     ASSERT_EQUALS(std::string("hello"), element.str());
-
-    ASSERT_EQUALS(ErrorCodes::NoSuchKey,
-                  bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "c", String, &element));
-
-    ASSERT_EQUALS(ErrorCodes::TypeMismatch,
-                  bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "a", String, &element));
-
-    ASSERT_EQUALS(ErrorCodes::TypeMismatch,
-                  bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "b", NumberDouble, &element));
+    ASSERT_EQUALS(ErrorCodes::NoSuchKey, bsonExtractTypedField(obj, "c", String, &element));
+    ASSERT_EQUALS(ErrorCodes::TypeMismatch, bsonExtractTypedField(obj, "a", String, &element));
+    ASSERT_EQUALS(ErrorCodes::TypeMismatch, bsonExtractTypedField(obj, "b", NumberDouble, &element));
 }
 
 
 TEST(ExtractBSON, ExtractStringField) {
+    BSONObj obj = BSON("a" << 1 << "b" << "hello");
     std::string s;
-    ASSERT_EQUALS(ErrorCodes::TypeMismatch,
-                  bsonExtractStringField(BSON("a" << 1 << "b" << "hello"), "a", &s));
-
-    ASSERT_OK(bsonExtractStringField(BSON("a" << 1 << "b" << "hello"), "b", &s));
+    ASSERT_EQUALS(ErrorCodes::TypeMismatch, bsonExtractStringField(obj, "a", &s));
+    ASSERT_OK(bsonExtractStringField(obj, "b", &s));
     ASSERT_EQUALS(std::string("hello"), s);
-
-    ASSERT_EQUALS(ErrorCodes::NoSuchKey,
-                  bsonExtractStringField(BSON("a" << 1 << "b" << "hello"), "c", &s));
+    ASSERT_EQUALS(ErrorCodes::NoSuchKey, bsonExtractStringField(obj, "c", &s));
 }
 
 TEST(ExtractBSON, ExtractStringFieldWithDefault) {
+    BSONObj obj = BSON("a" << 1 << "b" << "hello");
     std::string s;
     ASSERT_EQUALS(ErrorCodes::TypeMismatch,
-                  bsonExtractStringFieldWithDefault(BSON("a" << 1 << "b" << "hello"),
-                                                    "a", "default", &s));
+                  bsonExtractStringFieldWithDefault(obj, "a", "default", &s));
 
-    ASSERT_OK(bsonExtractStringFieldWithDefault(BSON("a" << 1 << "b" << "hello"), "b", "default", &s));
+    ASSERT_OK(bsonExtractStringFieldWithDefault(obj, "b", "default", &s));
     ASSERT_EQUALS(std::string("hello"), s);
-
-    ASSERT_OK(bsonExtractStringFieldWithDefault(BSON("a" << 1 << "b" << "hello"), "c", "default", &s));
+    ASSERT_OK(bsonExtractStringFieldWithDefault(obj, "c", "default", &s));
     ASSERT_EQUALS(std::string("default"), s);
 }
 
 TEST(ExtractBSON, ExtractBooleanFieldWithDefault) {
+    BSONObj obj1 = BSON("a" << 1 << "b" << "hello"  << "c" << true);
+    BSONObj obj2 = BSON("a" << 0 << "b" << "hello"  << "c" << false);
     bool b;
     b = false;
-    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 1 << "b" << "hello"  << "c" << true),
-                                                 "a", false, &b));
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(obj1, "a", false, &b));
     ASSERT_EQUALS(true, b);
 
     b = false;
-    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 1 << "b" << "hello"  << "c" << true),
-                                                 "c", false, &b));
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(obj1, "c", false, &b));
     ASSERT_EQUALS(true, b);
 
     b = true;
-    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 0 << "b" << "hello"  << "c" << false),
-                                                 "a", true, &b));
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(obj2, "a", true, &b));
     ASSERT_EQUALS(false, b);
 
     b = true;
-    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 0 << "b" << "hello"  << "c" << false),
-                                                 "c", true, &b));
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(obj2, "c", true, &b));
     ASSERT_EQUALS(false, b);
 
     b = false;
-    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 0 << "b" << "hello"  << "c" << false),
-                                                 "d", true, &b));
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(obj2, "d", true, &b));
     ASSERT_EQUALS(true, b);
 
     b = true;
-    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 0 << "b" << "hello"  << "c" << false),
-                                                 "d", false, &b));
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(obj2, "d", false, &b));
     ASSERT_EQUALS(false, b);
 
     ASSERT_EQUALS(ErrorCodes::TypeMismatch,
-                  bsonExtractBooleanFieldWithDefault(BSON("a" << 1 << "b" << "hello"  << "c" << true),
-                                                     "b", true, &b));
+                  bsonExtractBooleanFieldWithDefault(obj1, "b", true, &b));
 }
