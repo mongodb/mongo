449f8502943557fab1f13e4add93d808e74c68dd
Andy Schwerin <schwerin@10gen.com>
2012-11-06 13:03:56 -0500

    Utility methods for extracting elements from BSON objects, that return Status.
    Code that works with BSON that return Status objects intead of throwing exceptions generate
    a lot of boilerplate when using the regular BSON accessor operations.  These functions
    consolidate that boilerplate in one place, bson_extract.h/cpp.  It also provides a convenient
    location for dropping other generic field extraction methods that return Statuses.

---
+++ src/mongo/bson/util/bson_extract_test.cpp
@@ -0,0 +1,109 @@
+/*    Copyright 2012 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include <string>
+
+#include "mongo/bson/util/bson_extract.h"
+#include "mongo/db/jsobj.h"
+#include "mongo/unittest/unittest.h"
+
+using namespace mongo;
+
+TEST(ExtractBSON, ExtractField) {
+    BSONElement element;
+    ASSERT_OK(bsonExtractField(BSON("a" << 1 << "b" << "hello"), "a", &element));
+    ASSERT_EQUALS(1, element.Int());
+
+    ASSERT_OK(bsonExtractField(BSON("a" << 1 << "b" << "hello"), "b", &element));
+    ASSERT_EQUALS(std::string("hello"), element.str());
+
+    ASSERT_EQUALS(ErrorCodes::NoSuchKey,
+                  bsonExtractField(BSON("a" << 1 << "b" << "hello"), "c", &element));
+}
+
+TEST(ExtractBSON, ExtractTypedField) {
+    BSONElement element;
+    ASSERT_OK(bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "a", NumberInt, &element));
+    ASSERT_EQUALS(1, element.Int());
+
+    ASSERT_OK(bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "b", String, &element));
+    ASSERT_EQUALS(std::string("hello"), element.str());
+
+    ASSERT_EQUALS(ErrorCodes::NoSuchKey,
+                  bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "c", String, &element));
+
+    ASSERT_EQUALS(ErrorCodes::TypeMismatch,
+                  bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "a", String, &element));
+
+    ASSERT_EQUALS(ErrorCodes::TypeMismatch,
+                  bsonExtractTypedField(BSON("a" << 1 << "b" << "hello"), "b", NumberDouble, &element));
+}
+
+
+TEST(ExtractBSON, ExtractStringField) {
+    std::string s;
+    ASSERT_EQUALS(ErrorCodes::TypeMismatch,
+                  bsonExtractStringField(BSON("a" << 1 << "b" << "hello"), "a", &s));
+
+    ASSERT_OK(bsonExtractStringField(BSON("a" << 1 << "b" << "hello"), "b", &s));
+    ASSERT_EQUALS(std::string("hello"), s);
+
+    ASSERT_EQUALS(ErrorCodes::NoSuchKey,
+                  bsonExtractStringField(BSON("a" << 1 << "b" << "hello"), "c", &s));
+}
+
+TEST(ExtractBSON, ExtractStringFieldWithDefault) {
+    std::string s;
+    ASSERT_EQUALS(ErrorCodes::TypeMismatch,
+                  bsonExtractStringFieldWithDefault(BSON("a" << 1 << "b" << "hello"),
+                                                    "a", "default", &s));
+
+    ASSERT_OK(bsonExtractStringFieldWithDefault(BSON("a" << 1 << "b" << "hello"), "b", "default", &s));
+    ASSERT_EQUALS(std::string("hello"), s);
+
+    ASSERT_OK(bsonExtractStringFieldWithDefault(BSON("a" << 1 << "b" << "hello"), "c", "default", &s));
+    ASSERT_EQUALS(std::string("default"), s);
+}
+
+TEST(ExtractBSON, ExtractBooleanFieldWithDefault) {
+    bool b;
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 1 << "b" << "hello"  << "c" << true),
+                                                 "a", false, &b));
+    ASSERT_EQUALS(true, b);
+
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 1 << "b" << "hello"  << "c" << true),
+                                                 "c", false, &b));
+    ASSERT_EQUALS(true, b);
+
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 0 << "b" << "hello"  << "c" << false),
+                                                 "a", true, &b));
+    ASSERT_EQUALS(false, b);
+
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 0 << "b" << "hello"  << "c" << false),
+                                                 "c", true, &b));
+    ASSERT_EQUALS(false, b);
+
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 0 << "b" << "hello"  << "c" << false),
+                                                 "d", true, &b));
+    ASSERT_EQUALS(true, b);
+
+    ASSERT_OK(bsonExtractBooleanFieldWithDefault(BSON("a" << 0 << "b" << "hello"  << "c" << false),
+                                                 "d", false, &b));
+    ASSERT_EQUALS(false, b);
+
+    ASSERT_EQUALS(ErrorCodes::TypeMismatch,
+                  bsonExtractBooleanFieldWithDefault(BSON("a" << 1 << "b" << "hello"  << "c" << true),
+                                                     "b", true, &b));
+}
