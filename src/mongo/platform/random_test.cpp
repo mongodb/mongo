6b5c84d267c283babf22155c956764a78d5ad5d2
Eliot Horowitz <eliot@10gen.com>
2012-11-12 09:46:32 -0500

    a simple unit test for seeded random

---
+++ src/mongo/platform/random_test.cpp
@@ -0,0 +1,32 @@
+// random_test.cpp
+
+
+/*    Copyright 2012 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "mongo/platform/random.h"
+
+#include "mongo/unittest/unittest.h"
+
+namespace mongo {
+    TEST( RandomTest, Seed1 ) {
+        PseudoRandom a( 12 );
+        PseudoRandom b( 12 );
+
+        for ( int i = 0; i < 100; i++ ) {
+            ASSERT_EQUALS( a.nextInt32(), b.nextInt32() );
+        }
+    }
+}
