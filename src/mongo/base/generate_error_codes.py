3120c1143bf96f6807047048a93bc2f572dc8576
Spencer T Brody <spencer@10gen.com>
2012-11-20 15:20:29 -0500

    SERVER-7602 Fix licenses for auto-generated files

--- src/mongo/base/generate_error_codes.py
+++ src/mongo/base/generate_error_codes.py
@@ -141,6 +141,20 @@ def generate_error_class_predicate_definition(class_name, code_names):
 
 header_template = '''// AUTO-GENERATED FILE DO NOT EDIT
 // See src/mongo/base/generate_error_codes.py
+/*    Copyright 2012 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 #pragma once
 
@@ -186,6 +200,20 @@ namespace mongo {
 
 source_template = '''// AUTO-GENERATED FILE DO NOT EDIT
 // See src/mongo/base/generate_error_codes.py
+/*    Copyright 2012 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 #include "mongo/base/error_codes.h"
 
