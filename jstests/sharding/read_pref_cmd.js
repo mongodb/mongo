5ef99388a9c6687e00bdfe399e7d44e77a8d1a08
Randolph Tan <randolph@10gen.com>
2012-11-21 11:54:06 -0500

    Fix test for SERVER-4706
    gle doesn't work with mongos (SERVER-7739) so use awaitReplication instead to synchronize the test.

--- jstests/sharding/read_pref_cmd.js
+++ jstests/sharding/read_pref_cmd.js
@@ -155,7 +155,8 @@ assert.soon(function() {
 doTest(new Mongo(st.rs0.getURL()), st.rs0.nodes, false);
 
 st.s.getDB('test').dropDatabase();
-st.s.getDB('test').runCommand({ getLastError: 1, w: NODE_COUNT });
+// Hack until SERVER-7739 gets fixed
+st.rs0.awaitReplication();
 
 configDB.adminCommand({ enableSharding: 'test' });
 configDB.adminCommand({ shardCollection: 'test.user', key: { x: 1 }});
