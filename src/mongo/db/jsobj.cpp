ae9f99dbc9eb0683147078d290d3f1b8171fe864
Shaun Verch <shaun.verch@10gen.com>
2012-11-21 20:33:16 -0800

    Revert "SERVER-7437 Fixed handling of timestamp object in jsonString"
    This reverts commit 7582d50b18dedef00b605f8a44cd11d267be540c.

--- src/mongo/db/jsobj.cpp
+++ src/mongo/db/jsobj.cpp
@@ -242,12 +242,7 @@ namespace mongo {
             break;
 
         case Timestamp:
-            if ( format == TenGen ) {
-                s << "Timestamp( " << ( timestampTime() / 1000 ) << ", " << timestampInc() << " )";
-            }
-            else {
-                s << "{ \"$timestamp\" : { \"t\" : " << ( timestampTime() / 1000 ) << ", \"i\" : " << timestampInc() << " } }";
-            }
+            s << "{ \"t\" : " << timestampTime() << " , \"i\" : " << timestampInc() << " }";
             break;
 
         case MinKey:
