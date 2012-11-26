4473320aca6097d567cb4f3b2e48ad776dd544b1
Andy Schwerin <schwerin@10gen.com>
2012-11-26 14:49:39 -0500

    SERVER-7428 Regression test.

---
+++ jstests/slowNightly/server7428.js
@@ -0,0 +1,16 @@
+// Regression test for SERVER-7428.
+//
+// Verify that the copyDatabase command works appropriately when the
+// target mongo instance has authentication enabled.
+
+// Setup fromDb with no auth
+var fromDb = MongoRunner.runMongod({ port: 29000 });
+ 
+// Setup toDb with auth
+var toDb = MongoRunner.runMongod({auth : "", port : 31001});
+var admin = toDb.getDB("admin");
+admin.addUser("foo","bar");
+admin.auth("foo","bar");
+  
+admin.copyDatabase('test', 'test', fromDb.host)
+
