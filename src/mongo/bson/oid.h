b8185fc4eac2726d0e850698ac100b9686f8755f
Greg Studer <greg@10gen.com>
2012-11-26 14:09:49 -0500

    SERVER-939 toString() for random part of OID

--- src/mongo/bson/oid.h
+++ src/mongo/bson/oid.h
@@ -44,7 +44,8 @@ namespace mongo {
         OID() : a(0), b(0) { }
 
         enum {
-            kOIDSize = 12
+            kOIDSize = 12,
+            kIncSize = 3
         };
 
         /** init from a 24 char hex string */
@@ -69,6 +70,8 @@ namespace mongo {
         /** @return the object ID output as 24 hex digits */
         std::string str() const { return toHexLower(data, kOIDSize); }
         std::string toString() const { return str(); }
+        /** @return the random/sequential part of the object ID as 6 hex digits */
+        std::string toIncString() const { return toHexLower(_inc, kIncSize); }
 
         static OID gen() { OID o; o.init(); return o; }
 
