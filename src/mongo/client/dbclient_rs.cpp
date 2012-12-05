6c4280c84d4efcef88945c98c3f5d0bc1eef3b38
Randolph Tan <randolph@10gen.com>
2012-12-05 10:31:47 -0500

    Fix linux compiler error caused by SERVER-6754 in buildbot

--- src/mongo/client/dbclient_rs.cpp
+++ src/mongo/client/dbclient_rs.cpp
@@ -1477,7 +1477,7 @@ namespace mongo {
         ConnectionString connStr(_masterHost);
 
         string errmsg;
-        DBClientConnection* newConn;
+        DBClientConnection* newConn = NULL;
 
         try {
             // Needs to perform a dynamic_cast because we need to set the replSet
