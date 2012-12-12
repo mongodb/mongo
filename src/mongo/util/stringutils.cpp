0ee0472878ac373a918f6a0444882f7f6fcf37c8
Eliot Horowitz <eliot@10gen.com>
2012-12-12 10:25:31 -0500

    spacing

--- src/mongo/util/stringutils.cpp
+++ src/mongo/util/stringutils.cpp
@@ -41,18 +41,15 @@ namespace mongo {
             res->append( *it );
         }
     }
-    
+
     LexNumCmp::LexNumCmp( bool lexOnly ) :
     _lexOnly( lexOnly ) {
     }
 
     int LexNumCmp::cmp( const char *s1, const char *s2, bool lexOnly ) {
-        //cout << "START : " << s1 << "\t" << s2 << endl;
-        
         bool startWord = true;
-        
+
         while( *s1 && *s2 ) {
-            
             bool d1 = ( *s1 == '.' );
             bool d2 = ( *s2 == '.' );
             if ( d1 && !d2 )
@@ -64,7 +61,7 @@ namespace mongo {
                 startWord = true;
                 continue;
             }
-            
+
             bool p1 = ( *s1 == (char)255 );
             bool p2 = ( *s2 == (char)255 );
             //cout << "\t\t " << p1 << "\t" << p2 << endl;
@@ -72,30 +69,29 @@ namespace mongo {
                 return 1;
             if ( p2 && !p1 )
                 return -1;
-            
+
             if ( !lexOnly ) {
-                
                 bool n1 = isNumber( *s1 );
                 bool n2 = isNumber( *s2 );
-                
+
                 if ( n1 && n2 ) {
                     // get rid of leading 0s
                     if ( startWord ) {
                         while ( *s1 == '0' ) s1++;
                         while ( *s2 == '0' ) s2++;
                     }
-                    
+
                     char * e1 = (char*)s1;
                     char * e2 = (char*)s2;
-                    
+
                     // find length
                     // if end of string, will break immediately ('\0')
                     while ( isNumber (*e1) ) e1++;
                     while ( isNumber (*e2) ) e2++;
-                    
+
                     int len1 = (int)(e1-s1);
                     int len2 = (int)(e2-s2);
-                    
+
                     int result;
                     // if one is longer than the other, return
                     if ( len1 > len2 ) {
@@ -108,31 +104,31 @@ namespace mongo {
                     else if ( (result = strncmp(s1, s2, len1)) != 0 ) {
                         return result;
                     }
-                    
+
                     // otherwise, the numbers are equal
                     s1 = e1;
                     s2 = e2;
                     startWord = false;
                     continue;
                 }
-                
+
                 if ( n1 )
                     return 1;
-                
+
                 if ( n2 )
                     return -1;
             }
-            
+
             if ( *s1 > *s2 )
                 return 1;
-            
+
             if ( *s2 > *s1 )
                 return -1;
-            
+
             s1++; s2++;
             startWord = false;
         }
-        
+
         if ( *s1 )
             return 1;
         if ( *s2 )
@@ -149,5 +145,5 @@ namespace mongo {
     bool LexNumCmp::operator()( const string &s1, const string &s2 ) const {
         return (*this)( s1.c_str(), s2.c_str() );
     }
-    
+
 } // namespace mongo
