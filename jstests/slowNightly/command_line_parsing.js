26ab30d2bc75af1896f0c3f75541703e351ef839
Shaun Verch <shaun.verch@10gen.com>
2012-12-13 12:27:00 -0500

    SERVER-7925 Fix command line parsing test to account for authentication option

--- jstests/slowNightly/command_line_parsing.js
+++ jstests/slowNightly/command_line_parsing.js
@@ -10,12 +10,22 @@ assert.throws( function() { m.getDB( baseName ).getCollection( baseName ).find( 
 
 // test config file 
 var m2 = startMongod( "--port", port+2, "--dbpath", "/data/db/" + baseName +"2", "--config", "jstests/libs/testconfig");
-var m2result = {
+
+var m2expected = {
     "parsed" : {
         "config" : "jstests/libs/testconfig",
         "dbpath" : "/data/db/jstests_slowNightly_command_line_parsing2",
         "fastsync" : "true",
-        "port" : 31002
+        "port" : 31002,
+        "setParameter" : [
+            "enableTestCommands=1"
+            ]
     }
 };
-assert( friendlyEqual(m2result.parsed, m2.getDB("admin").runCommand( "getCmdLineOpts" ).parsed) );
+var m2result = m2.getDB("admin").runCommand( "getCmdLineOpts" );
+
+print("Expected:");
+printjson(m2expected.parsed);
+print("Actual:");
+printjson(m2result.parsed);
+assert( friendlyEqual(m2expected.parsed, m2result.parsed) );
