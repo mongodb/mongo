913c56c9043ea09a50dab5ec0309385f1d5e3015
Randolph Tan <randolph@10gen.com>
2012-12-17 11:05:17 -0500

    SERVER-7962 read_pref.js failure in RHEL-64bit
    Change the test to check if the server queried is a primary or not.

--- jstests/sharding/read_pref.js
+++ jstests/sharding/read_pref.js
@@ -109,7 +109,8 @@ assert.eq( 1, explain.n );
 
 // Check that mongos will fallback to primary if none of tags given matches
 explain = coll.find().readPref( "secondaryPreferred", [{ z: "3" }, { dc: "ph" }] ).explain();
-assert.eq( primaryNode.name, explain.server );
+// Call getPrimary again since the primary could have changed after the restart.
+assert.eq(replTest.getPrimary().name, explain.server);
 assert.eq( 1, explain.n );
 
 // Kill all members except one
