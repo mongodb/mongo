6889d1658136c753998b4a408dc8d1a3ec28e3b9
Eliot Horowitz <eliot@10gen.com>
2012-12-18 09:40:08 -0500

    SERVER-7769 - fast bson validate

---
+++ src/mongo/bson/bson_validate.cpp
@@ -0,0 +1,229 @@
+// bson_validate.cpp
+
+/*    Copyright 2012 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#include "mongo/bson/bson_validate.h"
+#include "mongo/bson/oid.h"
+
+namespace mongo {
+
+    namespace {
+
+        class Buffer {
+        public:
+            Buffer( const char* buffer, uint64_t maxLength )
+                : _buffer( buffer ), _position( 0 ), _maxLength( maxLength ) {
+            }
+
+            template<typename N>
+            bool readNumber( N* out ) {
+                if ( ( _position + sizeof(N) ) > _maxLength )
+                    return false;
+                if ( out ) {
+                    const N* temp = reinterpret_cast<const N*>(_buffer + _position);
+                    *out = *temp;
+                }
+                _position += sizeof(N);
+                return true;
+            }
+
+            Status readCString( StringData* out ) {
+                const void* x = memchr( _buffer + _position, 0, _maxLength - _position );
+                if ( !x )
+                    return Status( ErrorCodes::InvalidBSON, "no end of c-string" );
+                uint64_t len = static_cast<uint64_t>( static_cast<const char*>(x) - ( _buffer + _position ) );
+
+                StringData data( _buffer + _position, len );
+                _position += len + 1;
+
+                if ( out ) {
+                    *out = data;
+                }
+                return Status::OK();
+            }
+
+            Status readUTF8String( StringData* out ) {
+                int sz;
+                if ( !readNumber<int>( &sz ) )
+                    return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+
+                if ( out ) {
+                    *out = StringData( _buffer + _position, sz );
+                }
+
+                if ( !skip( sz - 1 ) )
+                    return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+
+                char c;
+                if ( !readNumber<char>( &c ) )
+                    return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+
+                if ( c != 0 )
+                    return Status( ErrorCodes::InvalidBSON, "not null terminate string" );
+
+                return Status::OK();
+            }
+
+            bool skip( uint64_t sz ) {
+                _position += sz;
+                return _position < _maxLength;
+            }
+
+            uint64_t position() const {
+                return _position;
+            }
+
+        private:
+            const char* _buffer;
+            uint64_t _position;
+            uint64_t _maxLength;
+        };
+
+        Status validateBSONInternal( Buffer* buffer, int* bsonLength ) {
+            const int start = buffer->position();
+
+            int supposedSize;
+            if ( !buffer->readNumber<int>(&supposedSize) )
+                return Status( ErrorCodes::InvalidBSON, "bson size is larger than buffer size" );
+
+            Status status = Status::OK();
+
+            int sz;
+
+            while ( true ) {
+                char type;
+                if ( !buffer->readNumber<char>(&type) )
+                    return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+
+                if ( type == EOO )
+                    break;
+
+                StringData name;
+                status = buffer->readCString( &name );
+                if ( !status.isOK() )
+                    return status;
+
+                switch ( type ) {
+                case MinKey:
+                case MaxKey:
+                case jstNULL:
+                case Undefined:
+                    break;
+
+                case jstOID:
+                    if ( !buffer->skip( sizeof(OID) ) )
+                        return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+                    break;
+
+                case NumberInt:
+                    if ( !buffer->skip( sizeof(int32_t) ) )
+                        return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+                    break;
+
+                case Bool:
+                    if ( !buffer->skip( sizeof(int8_t) ) )
+                        return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+                    break;
+
+
+                case NumberDouble:
+                case NumberLong:
+                case Timestamp:
+                case Date:
+                    if ( !buffer->skip( sizeof(int64_t) ) )
+                        return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+                    break;
+
+                case DBRef:
+                    status = buffer->readUTF8String( NULL );
+                    if ( !status.isOK() )
+                        return status;
+                    buffer->skip( sizeof(OID) );
+                    break;
+
+                case CodeWScope:
+                    if ( !buffer->readNumber<int>( &sz ) )
+                        return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+
+                    status = buffer->readUTF8String( NULL );
+                    if ( !status.isOK() )
+                        return status;
+
+                    status = validateBSONInternal( buffer, NULL );
+                    if ( !status.isOK() )
+                        return status;
+
+                    break;
+
+                case RegEx:
+                    status = buffer->readCString( NULL );
+                    if ( !status.isOK() )
+                        return status;
+                    status = buffer->readCString( NULL );
+                    if ( !status.isOK() )
+                        return status;
+
+                    break;
+
+                case Code:
+                case Symbol:
+                case String:
+                    status = buffer->readUTF8String( NULL );
+                    if ( !status.isOK() )
+                        return status;
+                    break;
+
+                case BinData:
+                    if ( !buffer->readNumber<int>( &sz ) )
+                        return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+                    if ( !buffer->skip( 1 + sz ) )
+                        return Status( ErrorCodes::InvalidBSON, "invalid bson" );
+                    break;
+
+                case Object:
+                case Array:
+                    status = validateBSONInternal( buffer, NULL );
+                    if ( !status.isOK() )
+                        return status;
+                    break;
+
+                default:
+                    return Status( ErrorCodes::InvalidBSON, "invalid bson type" );
+                }
+            }
+
+            const int end = buffer->position();
+
+            if ( end - start != supposedSize )
+                return Status( ErrorCodes::InvalidBSON, "bson length doesn't match what we found" );
+
+            if ( bsonLength )
+                *bsonLength = supposedSize;
+
+            return Status::OK();
+        }
+    }
+
+    Status validateBSON( const char* originalBuffer, uint64_t maxLength, int* bsonLength ) {
+        if ( maxLength < 5 ) {
+            return Status( ErrorCodes::InvalidBSON, "bson data has to be at least 5 bytes" );
+        }
+
+        Buffer buf( originalBuffer, maxLength );
+        return validateBSONInternal( &buf, bsonLength );
+    }
+
+}
--- src/mongo/bson/bson_validate_test.cpp
+++ src/mongo/bson/bson_validate_test.cpp
@@ -16,21 +16,21 @@
 #include "mongo/db/jsobj.h"
 #include "mongo/unittest/unittest.h"
 #include "mongo/platform/random.h"
+#include "mongo/bson/bson_validate.h"
 
 namespace {
-    
+
     using namespace mongo;
 
     TEST(BSONValidate, Basic) {
         BSONObj x;
         ASSERT_TRUE( x.valid() );
-        
+
         x = BSON( "x" << 1 );
         ASSERT_TRUE( x.valid() );
     }
 
     TEST(BSONValidate, RandomData) {
-        
         PseudoRandom r(17);
 
         int numValid = 0;
@@ -39,15 +39,15 @@ namespace {
 
         for ( int i=0; i<numToRun; i++ ) {
             int size = 1234;
-            
+
             char* x = new char[size];
             int* xx = reinterpret_cast<int*>(x);
             xx[0] = size;
-            
+
             for ( int i=4; i<size; i++ ) {
                 x[i] = r.nextInt32( 255 );
             }
-            
+
             x[size-1] = 0;
 
             BSONObj o( x );
@@ -57,19 +57,24 @@ namespace {
             if ( o.valid() ) {
                 numValid++;
                 jsonSize += o.jsonString().size();
+                ASSERT( validateBSON( o.objdata(), o.objsize() ).isOK() );
+            }
+            else {
+                ASSERT( !validateBSON( o.objdata(), o.objsize() ).isOK() );
             }
 
             delete[] x;
         }
 
-        log() << "RandomData: didn't crash valid/total: " << numValid << "/" << numToRun << " (want few valid ones)" 
+        log() << "RandomData: didn't crash valid/total: " << numValid << "/" << numToRun
+              << " (want few valid ones)"
               << " jsonSize: " << jsonSize << endl;
     }
 
     TEST(BSONValidate, MuckingData1) {
 
         BSONObj theObject;
-        
+
         {
             BSONObjBuilder b;
             b.append( "name" , "eliot was here" );
@@ -80,32 +85,85 @@ namespace {
             }
             a.done();
             b.done();
-            
+
             theObject = b.obj();
         }
 
         int numValid = 0;
         int numToRun = 1000;
         long long jsonSize = 0;
-        
+
         for ( int i=4; i<theObject.objsize()-1; i++ ) {
             BSONObj mine = theObject.copy();
-            
+
             char* data = const_cast<char*>(mine.objdata());
-            
+
             data[ i ] = 200;
 
             numToRun++;
             if ( mine.valid() ) {
                 numValid++;
                 jsonSize += mine.jsonString().size();
+                ASSERT( validateBSON( mine.objdata(), mine.objsize() ).isOK() );
             }
-   
+            else {
+                ASSERT( !validateBSON( mine.objdata(), mine.objsize() ).isOK() );
+            }
+
         }
 
-
-        log() << "MuckingData1: didn't crash valid/total: " << numValid << "/" << numToRun << " (want few valid ones) " 
-              << " jsonSize: " << jsonSize << endl;        
+        log() << "MuckingData1: didn't crash valid/total: " << numValid << "/" << numToRun
+              << " (want few valid ones) "
+              << " jsonSize: " << jsonSize << endl;
     }
+
+    TEST( BSONValidateFast, Empty ) {
+        BSONObj x;
+        ASSERT( validateBSON( x.objdata(), x.objsize() ).isOK() );
+    }
+
+    TEST( BSONValidateFast, RegEx ) {
+        BSONObjBuilder b;
+        b.appendRegex( "foo", "i" );
+        BSONObj x = b.obj();
+        ASSERT( validateBSON( x.objdata(), x.objsize() ).isOK() );
+    }
+
+    TEST(BSONValidateFast, Simple0 ) {
+        BSONObj x;
+        ASSERT( validateBSON( x.objdata(), x.objsize() ).isOK() );
+
+        x = BSON( "foo" << 17 << "bar" << "eliot" );
+        ASSERT( validateBSON( x.objdata(), x.objsize() ).isOK() );
+
+    }
+
+    TEST(BSONValidateFast, Simple2 ) {
+        char buf[64];
+        for ( int i=1; i<=JSTypeMax; i++ ) {
+            BSONObjBuilder b;
+            sprintf( buf, "foo%d", i );
+            b.appendMinForType( buf, i );
+            sprintf( buf, "bar%d", i );
+            b.appendMaxForType( buf, i );
+            BSONObj x = b.obj();
+            ASSERT( validateBSON( x.objdata(), x.objsize() ).isOK() );
+        }
+    }
+
+
+    TEST(BSONValidateFast, Simple3 ) {
+        BSONObjBuilder b;
+        char buf[64];
+        for ( int i=1; i<=JSTypeMax; i++ ) {
+            sprintf( buf, "foo%d", i );
+            b.appendMinForType( buf, i );
+            sprintf( buf, "bar%d", i );
+            b.appendMaxForType( buf, i );
+        }
+        BSONObj x = b.obj();
+        ASSERT( validateBSON( x.objdata(), x.objsize() ).isOK() );
+    }
+
 
 }
