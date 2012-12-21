a7d5f6fa9d14ed29d42f531074e5014fc8dd78a0
Mathias Stearn <mathias@10gen.com>
2012-12-20 20:34:48 -0500

    SERVER-7124 connectionStatus command
    Currently only returns list of users logged in on current connection

---
+++ src/mongo/db/commands/connection_status.cpp
@@ -0,0 +1,60 @@
+/*
+ *    Copyright (C) 2010 10gen Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <mongo/pch.h>
+
+#include "mongo/db/auth/authorization_manager.h"
+#include "mongo/db/commands.h"
+
+namespace mongo {
+    class CmdConnectionStatus : public Command {
+    public:
+        CmdConnectionStatus() : Command("connectionStatus") {}
+        virtual bool requiresAuth() { return false; }
+        virtual bool logTheOp() { return false; }
+        virtual bool slaveOk() const { return true; }
+        virtual LockType locktype() const { return NONE; }
+        virtual void addRequiredPrivileges(const std::string& dbname,
+                                           const BSONObj& cmdObj,
+                                           std::vector<Privilege>* out) {} // No auth required
+
+        void help(stringstream& h) const {
+            h << "Returns connection-specific information such as logged-in users";
+        }
+
+        bool run(const string&, BSONObj& cmdObj, int, string& errmsg,
+                 BSONObjBuilder& result, bool fromRepl) {
+            AuthorizationManager* authMgr = ClientBasic::getCurrent()->getAuthorizationManager();
+
+            BSONObjBuilder authInfo(result.subobjStart("authInfo"));
+            {
+                BSONArrayBuilder authenticatedUsers(authInfo.subarrayStart("authenticatedUsers"));
+
+                PrincipalSet::NameIterator nameIter = authMgr->getAuthenticatedPrincipalNames();
+                for ( ; nameIter.more(); nameIter.next()) {
+                    BSONObjBuilder principal(authenticatedUsers.subobjStart());
+                    principal.append("user", nameIter->getUser());
+                    principal.append("userSource", nameIter->getDB());
+                    principal.doneFast();
+                }
+                authenticatedUsers.doneFast();
+            }
+            authInfo.doneFast();
+
+            return true;
+        }
+    } cmdConnectionStatus;
+}
