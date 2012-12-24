4252beda7ebbadc24c61dabed66a0609e6ac3dae
Eliot Horowitz <eliot@10gen.com>
2012-12-24 03:37:37 -0500

    don't check for db.serverStatus().metrics on mongos

--- jstests/find1.js
+++ jstests/find1.js
@@ -1,7 +1,10 @@
 t = db.find1;
 t.drop();
 
-nscannedStart = db.serverStatus().metrics.document.scanned
+if ( db.serverStatus().metrics ) {
+    // ignore mongos
+    nscannedStart = db.serverStatus().metrics.document.scanned
+}
 
 t.save( { a : 1 , b : "hi" } );
 t.save( { a : 2 , b : "hi" } );
@@ -32,5 +35,8 @@ assert( ! t.findOne( id , { a : 1 } ).b , "I" );
 
 assert(t.validate().valid,"not valid");
 
-nscannedEnd = db.serverStatus().metrics.document.scanned
-assert.lte( nscannedStart + 16, nscannedEnd );
+if ( db.serverStatus().metrics ) {
+    // ignore mongos
+    nscannedEnd = db.serverStatus().metrics.document.scanned
+    assert.lte( nscannedStart + 16, nscannedEnd );
+}
