ea17746c8bada229b5b5dff7e2a5e7dce3c4ab59
Greg Studer <greg@10gen.com>
2013-01-04 18:36:57 -0500

    SERVER-7984 config upgrade timeout for legacy mongos is incorrect

--- src/mongo/s/cluster_client_internal.cpp
+++ src/mongo/s/cluster_client_internal.cpp
@@ -67,16 +67,18 @@ namespace mongo {
 
                 Date_t lastPing = ping.getPing();
 
-                long long quietIntervalMillis = static_cast<long long>(lastPing - jsTime());
-                long long quietIntervalSecs = quietIntervalMillis / 1000;
-                long long quietIntervalMins = quietIntervalSecs / 60;
+                long long quietIntervalMillis = 0;
+                if ((jsTime() - lastPing) >= 0) {
+                    quietIntervalMillis = static_cast<long long>(jsTime() - lastPing);
+                }
+                long long quietIntervalMins = quietIntervalMillis / (60 * 1000);
 
                 // We assume that anything that hasn't pinged in 5 minutes is probably down
                 if (quietIntervalMins >= 5) {
                     log() << "stale mongos detected " << quietIntervalMins << " minutes ago,"
                           << " network location is " << pingDoc["_id"].String()
-                          << ", not checking version";
-                }
+                          << ", not checking version" << endl;
+	}
                 else {
                     if (versionCmp(mongoVersion, minMongoVersion) < 0) {
                         return Status(ErrorCodes::RemoteValidationError,
