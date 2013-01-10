f405c58e098a67158b004f48e121e45146a89afa
Eliot Horowitz <eliot@10gen.com>
2013-01-10 11:37:30 -0500

    BUMP 2.3.2

--- src/mongo/util/version.cpp
+++ src/mongo/util/version.cpp
@@ -47,7 +47,7 @@ namespace mongo {
      *      1.2.3-rc4-pre-
      * If you really need to do something else you'll need to fix _versionArray()
      */
-    const char versionString[] = "2.3.2-pre-";
+    const char versionString[] = "2.3.2";
 
     // See unit test for example outputs
     static BSONArray _versionArray(const char* version){
