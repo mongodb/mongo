d0e873e7acf41b1315e73d67b59531f6339c4119
Eliot Horowitz <eliot@10gen.com>
2013-01-10 23:55:13 -0500

    POST 2.3.2

--- src/mongo/util/version.cpp
+++ src/mongo/util/version.cpp
@@ -47,7 +47,7 @@ namespace mongo {
      *      1.2.3-rc4-pre-
      * If you really need to do something else you'll need to fix _versionArray()
      */
-    const char versionString[] = "2.3.2";
+    const char versionString[] = "2.3.3-pre-";
 
     // See unit test for example outputs
     static BSONArray _versionArray(const char* version){
