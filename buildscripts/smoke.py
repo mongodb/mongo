1a4363cb481ab62f5199eb6f2adf2ffbc04b542d
Eric Milkie <milkie@10gen.com>
2013-01-18 16:28:28 -0500

    SERVER-8222 remove connections_opened.js from small-oplog test

--- buildscripts/smoke.py
+++ buildscripts/smoke.py
@@ -357,7 +357,7 @@ def skipTest(path):
     parentPath = os.path.dirname(path)
     parentDir = os.path.basename(parentPath)
     if small_oplog: # For tests running in parallel
-        if basename in ["cursor8.js", "indexh.js", "dropdb.js"]:
+        if basename in ["cursor8.js", "indexh.js", "dropdb.js", "connections_opened.js"]:
             return True
     if auth or keyFile: # For tests running with auth
         # Skip any tests that run with auth explicitly
