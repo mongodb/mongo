d6d840d3100a7f6292038cc9d6889eeebf6fb7d7
Andrew Morrow <acm@10gen.com>
2013-01-23 23:37:40 -0500

    Actually report times for threaded perftests

--- src/mongo/dbtests/perftests.cpp
+++ src/mongo/dbtests/perftests.cpp
@@ -194,9 +194,12 @@ namespace PerfTests {
 
         virtual void timed() = 0;
 
-        // optional 2nd test phase to be timed separately
-        // return name of it
-        virtual string timed2(DBClientBase&) { return ""; }
+        // optional 2nd test phase to be timed separately. You must provide it with a unique
+        // name in order for it to run by overloading 'name2'.
+        virtual void timed2(DBClientBase&) {}
+
+        // return name of second test.
+        virtual string name2() { return name(); }
 
         virtual void post() { }
 
@@ -289,12 +292,9 @@ namespace PerfTests {
             }
         }
 
-        /** if true runs timed2() again with several threads (8 at time of this writing 
-            shouldn't be just timed2, that is legacy and can be cleaned up one day
+        /** if true runs timed2() again with several threads (8 at time of this writing).
         */
         virtual bool testThreaded() { return false; }
-
-        unsigned long long n;
 
         int howLong() { 
             int hlm = howLongMillis();
@@ -306,6 +306,9 @@ namespace PerfTests {
         }
 
         void run() {
+
+            unsigned long long n = 0;
+
             _ns = string("perftest.") + name();
             client().dropCollection(ns());
             prep();
@@ -314,7 +317,7 @@ namespace PerfTests {
             dur::stats.curr->reset();
             mongo::Timer t;
             n = 0;
-            const unsigned Batch = batchSize();
+            const unsigned int Batch = batchSize();
 
             if( hlm == 0 ) {
                 // means just do once
@@ -322,7 +325,7 @@ namespace PerfTests {
             }
             else {
                 do {
-                    unsigned i;
+                    unsigned int i;
                     for( i = 0; i < Batch; i++ )
                         timed();
                     n += i;
@@ -336,14 +339,14 @@ namespace PerfTests {
 
             post();
 
-            string test2name = timed2(client());
+            string test2name = name2();
             {
-                if( test2name.size() != 0 ) {
+                if( test2name != name() ) {
                     dur::stats.curr->reset();
                     mongo::Timer t;
                     unsigned long long n = 0;
                     while( 1 ) {
-                        unsigned i;
+                        unsigned int i;
                         for( i = 0; i < Batch; i++ )
                             timed2(client());
                         n += i;
@@ -359,40 +362,47 @@ namespace PerfTests {
                 const int nThreads = 8;
                 //cout << "testThreaded nThreads:" << nThreads << endl;
                 mongo::Timer t;
-                launchThreads(nThreads);
-                say(n, t.millis(), test2name+"-threaded");
+                const unsigned long long result = launchThreads(nThreads);
+                const int ms = t.millis();
+                say(result/nThreads, ms, test2name+"-threaded");
             }
         }
 
         bool stop;
 
-        void thread() {
+        void thread(unsigned long long* counter) {
 #if defined(_WIN32)
             static int z;
             srand( ++z ^ (unsigned) time(0));
 #endif
             DBClientType c;
             Client::initThreadIfNotAlready("perftestthr");
+            const unsigned int Batch = batchSize();
             while( 1 ) {
-                for( int i = 0; i < 8; i++ )
+                unsigned int i = 0;
+                for( i = 0; i < Batch; i++ )
                     timed2(c);
+                *counter += i;
                 if( stop ) 
                     break;
             }
             cc().shutdown();
         }
 
-        void launchThreads(int remaining) {
+        unsigned long long launchThreads(int remaining) {
             stop = false;
             if (!remaining) {
                 int hlm = howLong();
                 sleepmillis(hlm);
                 stop = true;
-                return;
+                return 0;
             }
-            boost::thread athread(boost::bind(&B::thread, this));
-            launchThreads(remaining - 1);
+            unsigned long long counter = 0;
+            boost::thread athread(boost::bind(&B::thread, this, &counter));
+            unsigned long long child = launchThreads(remaining - 1);
             athread.join();
+            unsigned long long accum = child + counter;
+            return accum;
         }
     };
 
@@ -983,10 +993,13 @@ namespace PerfTests {
                 return false;
             return true; 
         }
-        string timed2(DBClientBase& c) {
+        string name2() {
+            return "findOne_by_id";
+        }
+
+        void timed2(DBClientBase& c) {
             Query q = QUERY( "_id" << (unsigned) (rand() % i) );
             c.findOne(ns(), q);
-            return "findOne_by_id";
         }
         void post() {
 #if !defined(_DEBUG)
@@ -1051,13 +1064,16 @@ namespace PerfTests {
             client().update(ns(), q, y, /*upsert*/true);
         }
         virtual bool testThreaded() { return true; }
-        virtual string timed2(DBClientBase& c) {
+        virtual string name2() {
+            return name()+"-inc";
+        }
+
+        virtual void timed2(DBClientBase& c) {
             static BSONObj I = BSON( "$inc" << BSON( "y" << 1 ) );
             // test some $inc's
             int x = rand();
             BSONObj q = BSON("x" << x);
             c.update(ns(), q, I);
-            return name()+"-inc";
         }
     };
 
