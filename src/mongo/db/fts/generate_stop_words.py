6981863f1174a3c8d0b25a441ad82085cb11f32c
Eric Milkie <milkie@10gen.com>
2013-01-28 10:29:44 -0500

    speed up stop_word compiles
    Removed the compile hack.
    Removed the hundreds of .insert() calls which were causing the compiler to stall for long periods of time.

--- src/mongo/db/fts/generate_stop_words.py
+++ src/mongo/db/fts/generate_stop_words.py
@@ -39,9 +39,13 @@ namespace fts {
 
         out.write( '  // %s\n' % l_file )
         out.write( '  {\n' )
-        out.write( '   std::set< std::string >& l = (*m)["%s"];\n' % l )
+        out.write( '   const char* const words[] = {\n' )
         for word in open( l_file, "rb" ):
-            out.write( '   l.insert( "%s" );\n' % word.strip() )
+            out.write( '       "%s",\n' % word.strip() )
+        out.write( '   };\n' )
+        out.write( '   const size_t wordcnt = sizeof(words) / sizeof(words[0]);\n' )
+        out.write( '   std::set< std::string >& l = (*m)["%s"];\n' % l )
+        out.write( '   l.insert(&words[0], &words[wordcnt]);\n' )
         out.write( '  }\n' )
     out.write( """
   }
