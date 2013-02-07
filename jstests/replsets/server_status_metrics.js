2e3e2e9957eac59b670d5adbad1483d4615e1845
Scott Hernandez <scotthernandez@gmail.com>
2013-02-07 09:16:30 -0500

    SERVER-8203: cleanup test for failing buildbot

--- jstests/replsets/server_status_metrics.js
+++ jstests/replsets/server_status_metrics.js
@@ -1,14 +1,14 @@
 /**
  * Test replication metrics
  */
-function testSecondaryMetrics(secondary, opCount) {
+function testSecondaryMetrics(secondary, opCount, offset) {
     var ss = secondary.getDB("test").serverStatus()
     printjson(ss.metrics)
 
     assert(ss.metrics.repl.network.readersCreated > 0, "no (oplog) readers created")
     assert(ss.metrics.repl.network.getmores.num > 0, "no getmores")
     assert(ss.metrics.repl.network.getmores.totalMillis > 0, "no getmores time")
-    assert(ss.metrics.repl.network.ops == opCount, "wrong number of ops retrieved")
+    assert.eq(ss.metrics.repl.network.ops, opCount + offset, "wrong number of ops retrieved")
     assert(ss.metrics.repl.network.bytes > 0, "zero or missing network bytes")
 
     assert(ss.metrics.repl.buffer.count >= 0, "buffer count missing")
@@ -22,14 +22,14 @@ function testSecondaryMetrics(secondary, opCount) {
 
     assert(ss.metrics.repl.apply.batches.num > 0, "no batches")
     assert(ss.metrics.repl.apply.batches.totalMillis > 0, "no batch time")
-    assert(ss.metrics.repl.apply.ops == opCount, "wrong number of applied ops")
+    assert.eq(ss.metrics.repl.apply.ops, opCount + offset, "wrong number of applied ops")
 }
 
-function testPrimaryMetrics(primary, opCount) {
+function testPrimaryMetrics(primary, opCount, offset) {
     var ss = primary.getDB("test").serverStatus()
     printjson(ss.metrics)
 
-    assert(ss.metrics.repl.oplog.insert.num === opCount + 1, "wrong oplog insert count")
+    assert.eq(ss.metrics.repl.oplog.insert.num, opCount + offset, "wrong oplog insert count")
     assert(ss.metrics.repl.oplog.insert.totalMillis >= 0, "no oplog inserts time")
     assert(ss.metrics.repl.oplog.insertBytes > 0, "no oplog inserted bytes")
 }
@@ -44,17 +44,23 @@ var secondary = rt.getSecondary();
 var primary = rt.getPrimary();
 var testDB = primary.getDB("test");
 
+var ss = primary.getDB("test").serverStatus();
+var primaryBaseOplogInserts = ss.metrics.repl.oplog.insert.num;
+
+var ss = secondary.getDB("test").serverStatus();
+var secondaryBaseOplogInserts = ss.metrics.repl.oplog.insert.num;
+
 //add test docs
 for(x=0;x<10000;x++){ testDB.a.insert({}) }
 
-testPrimaryMetrics(primary, 10000);
+testPrimaryMetrics(primary, 10000, primaryBaseOplogInserts);
 testDB.getLastError(2);
 
-testSecondaryMetrics(secondary, 10000);
+testSecondaryMetrics(secondary, 10000, secondaryBaseOplogInserts -1);
 
 testDB.a.update({}, {$set:{d:new Date()}},true, true)
 testDB.getLastError(2);
 
-testSecondaryMetrics(secondary, 20000);
+testSecondaryMetrics(secondary, 20000, secondaryBaseOplogInserts -1);
 
 rt.stopSet();
