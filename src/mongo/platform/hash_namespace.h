e23f796526a5158f9e5b5ba0fc9bf3371beb909e
Andrew Morrow <acm@10gen.com>
2013-02-28 15:59:39 -0500

    SERVER-8676 Macroize 'hash' namespace detection in platform, use for s2

---
+++ src/mongo/platform/hash_namespace.h
@@ -0,0 +1,47 @@
+/*    Copyright 2013 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+// NOTE(acm): Before gcc-4.7, __cplusplus is always defined to be 1,
+// so we can't reliably detect C++11 support by exclusively checking
+// the value of __cplusplus.
+#if __cplusplus >= 201103L || defined(__GXX_EXPERIMENTAL_CXX0X__)
+
+#define MONGO_HASH_NAMESPACE_START namespace std {
+#define MONGO_HASH_NAMESPACE_END }
+
+#elif defined(_MSC_VER) && _MSC_VER >= 1500
+
+#if _MSC_VER >= 1600  /* Visual Studio 2010+ */
+
+#define MONGO_HASH_NAMESPACE_START namespace std {
+#define MONGO_HASH_NAMESPACE_END }
+
+#else /* Older Visual Studio */
+
+#define MONGO_HASH_NAMESPACE_START namespace std { namespace tr1 {
+#define MONGO_HASH_NAMESPACE_END }}
+
+#endif
+
+#elif defined(__GNUC__)
+
+#define MONGO_HASH_NAMESPACE_START namespace std { namespace tr1 {
+#define MONGO_HASH_NAMESPACE_END }}
+
+#else
+#error "Cannot determine namespace for 'hash'"
+#endif
