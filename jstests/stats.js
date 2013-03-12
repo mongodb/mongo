3bcf74b73f80e965ccb0acb4f2203ad3e4551b41
Dan Pasette <dan@10gen.com>
2013-03-12 16:43:28 -0400

    SERVER-8920 - fix test to not use the test db

--- jstests/stats.js
+++ jstests/stats.js
@@ -1,6 +1,7 @@
 
-t = db.stats1;
-t.drop();
+var statsDB = db.getSiblingDB( "stats" );
+statsDB.dropDatabase();
+var t = statsDB.stats1;
 
 t.save( { a : 1 } );
 
@@ -8,13 +9,15 @@ assert.lt( 0 , t.dataSize() , "A" );
 assert.lt( t.dataSize() , t.storageSize() , "B" );
 assert.lt( 0 , t.totalIndexSize() , "C" );
 
-var stats = db.stats();
+var stats = statsDB.stats();
 assert.gt( stats.fileSize, 0 );
 assert.eq( stats.dataFileVersion.major, 4 );
 assert.eq( stats.dataFileVersion.minor, 5 );
 
-db.getSiblingDB( "emptydatabase" ).dropDatabase();
-var statsEmptyDB = db.getSiblingDB( "emptydatabase" ).stats();
+// test empty database; should be no dataFileVersion
+statsDB.dropDatabase();
+var statsEmptyDB = statsDB.stats();
 assert.eq( statsEmptyDB.fileSize, 0 );
 assert.eq( {}, statsEmptyDB.dataFileVersion );
 
+statsDB.dropDatabase();
