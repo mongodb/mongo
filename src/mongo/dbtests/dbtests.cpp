dfb307e5716ad789cc2146048853d3a45077056b
Tad Marshall <tad@10gen.com>
2013-03-15 02:55:57 -0400

    SERVER-5099 Convert Unicode text on Windows command line for test.exe
    For the Windows version of test, switch to a Unicode "wmain()" entry
    point and convert Unicode characters on the command line to UTF-8.  Move
    previous main() to a new dbtestsMain() routine so it can be called from
    both main() and wmain().

--- src/mongo/dbtests/dbtests.cpp
+++ src/mongo/dbtests/dbtests.cpp
@@ -25,12 +25,31 @@
 #include "mongo/dbtests/framework.h"
 #include "mongo/util/exception_filter_win32.h"
 #include "mongo/util/startup_test.h"
+#include "mongo/util/text.h"
 
-int main( int argc, char** argv, char** envp ) {
+int dbtestsMain( int argc, char** argv, char** envp ) {
     static StaticObserver StaticObserver;
     setWindowsUnhandledExceptionFilter();
     Command::testCommandsEnabled = 1;
     mongo::runGlobalInitializersOrDie(argc, argv, envp);
     StartupTest::runTests();
-    _exit(mongo::dbtests::runDbTests( argc, argv, "/tmp/unittest" ));
+    return mongo::dbtests::runDbTests( argc, argv, "/tmp/unittest" );
 }
+
+#if defined(_WIN32)
+// In Windows, wmain() is an alternate entry point for main(), and receives the same parameters
+// as main() but encoded in Windows Unicode (UTF-16); "wide" 16-bit wchar_t characters.  The
+// WindowsCommandLine object converts these wide character strings to a UTF-8 coded equivalent
+// and makes them available through the argv() and envp() members.  This enables dbtestsMain()
+// to process UTF-8 encoded arguments and environment variables without regard to platform.
+int wmain(int argc, wchar_t* argvW[], wchar_t* envpW[]) {
+    WindowsCommandLine wcl(argc, argvW, envpW);
+    int exitCode = dbtestsMain(argc, wcl.argv(), wcl.envp());
+    ::_exit(exitCode);
+}
+#else
+int main(int argc, char* argv[], char** envp) {
+    int exitCode = dbtestsMain(argc, argv, envp);
+    ::_exit(exitCode);
+}
+#endif
