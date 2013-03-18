c74d5b6027268219f5f574910967ed7b5320e5be
Keith Bostic <keith@wiredtiger.com>
2013-03-18 12:27:01 -0400

    Minor style change, don't test pointer as if it's a boolean, don't check if a pointer is NULL before freeing it.

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -461,6 +461,6 @@ __wt_txn_global_destroy(WT_CONNECTION_IMPL *conn)
 	session = conn->default_session;
 	txn_global = &conn->txn_global;
 
-	if (txn_global && txn_global->states != NULL)
+	if (txn_global != NULL)
 		__wt_free(session, txn_global->states);
 }
