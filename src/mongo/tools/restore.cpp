afb0f9b2434d53642df88376a30ee519c125d99d
Jason Rassi <rassi@10gen.com>
2013-03-29 18:40:30 -0400

    SERVER-9182 Allow restores of dumps containing edited .metadata.json files

--- src/mongo/tools/restore.cpp
+++ src/mongo/tools/restore.cpp
@@ -458,7 +458,6 @@ private:
         int objSize;
         BSONObj obj;
         obj = fromjson (buf.get(), &objSize);
-        uassert(15934, "JSON object size didn't match file size", objSize == fileSize);
         return obj;
     }
 
