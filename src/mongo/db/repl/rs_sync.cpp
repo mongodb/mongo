d5fad724e291ca8af3b47e4622cc97a3a85b62a0
Eric Milkie <milkie@10gen.com>
2013-04-08 12:44:02 -0400

    SERVER-8420 add a tailcheck to detect cursor deadness

--- src/mongo/db/repl/rs_sync.cpp
+++ src/mongo/db/repl/rs_sync.cpp
@@ -857,6 +857,9 @@ namespace replset {
         }
 
         try {
+            // haveCursor() does not necessarily tell us if we have a non-dead cursor, so we check
+            // tailCheck() as well; see SERVER-8420
+            slave->reader.tailCheck();
             if (!slave->reader.haveCursor()) {
                 if (!slave->reader.connect(id, slave->slave->id(), target->fullName())) {
                     // error message logged in OplogReader::connect
