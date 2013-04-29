bc99c890539b16a7f2d783d40d74bc64e756295d
Eric Milkie <milkie@10gen.com>
2013-04-29 14:52:03 -0400

    SERVER-9452 do not stop queries in god mode
    Prior to this commit, there was no way to do an internal query on a node that was not in PRIMARY or SECONDARY state.
    This ability is necessary for the Index Rebuilder, which runs at startup.

--- src/mongo/db/repl/repl_reads_ok.cpp
+++ src/mongo/db/repl/repl_reads_ok.cpp
@@ -31,6 +31,8 @@ namespace mongo {
             // todo: speed up the secondary case.  as written here there are 2 mutex entries, it
             // can b 1.
             if( isMaster() ) return;
+            if ( cc().isGod() ) return;
+
             uassert(13435, "not master and slaveOk=false",
                     !pq || pq->hasOption(QueryOption_SlaveOk) || pq->hasReadPref());
             uassert(13436,
