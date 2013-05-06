a6dab30a41f5a4a7cb2ed64e534eb2bd6b2a8d97
Alberto Lerner <alerner@10gen.com>
2013-05-06 10:40:27 -0400

    SERVER-7174 Renamed the mods common interface and its calls to be simpler.

--- src/mongo/db/ops/modifier_interface.h
+++ src/mongo/db/ops/modifier_interface.h
@@ -1,5 +1,5 @@
 /**
- *    Copyright (C) 2012 10gen Inc.
+ *    Copyright (C) 2013 10gen Inc.
  *
  *    This program is free software: you can redistribute it and/or  modify
  *    it under the terms of the GNU Affero General Public License, version 3,
@@ -18,8 +18,9 @@
 
 #include "mongo/base/status.h"
 #include "mongo/base/string_data.h"
-#include "mongo/bson/mutable/mutable_bson.h"
-#include "mongo/db/ops/field_ref.h"
+#include "mongo/bson/mutable/element.h"
+#include "mongo/db/field_ref.h"
+#include "mongo/db/jsobj.h"
 
 namespace mongo {
 
@@ -34,11 +35,11 @@ namespace mongo {
      *   + For each document that is being touched on that update, the following methods are
      *     going to be called once for that document and in the order the calls appear here.
      *
-     *     + prepareMod() to check if mod is viable over the document
+     *     + prepare() to check if mod is viable over the document
      *
-     *     + applyMod(), effectively computing the update
+     *     + apply(), effectively computing the update
      *
-     *     + logMod() registering the change in the log for replication purposes
+     *     + log() registering the change in the log for replication purposes
      *
      * Again, a modifier implementation may rely on these last three calls being made and in
      * that particular order and therefore can keep and reuse state between these calls, when
@@ -47,9 +48,9 @@ namespace mongo {
      * TODO:
      * For a reference implementation, see modifier_identity.{h,cpp} used in tests.
      */
-    class ModifierBase {
+    class ModifierInterface {
     public:
-        virtual ~ModifierBase() { }
+        virtual ~ModifierInterface() { }
 
         /**
          * Returns OK and extracts the parameters for this given mod from 'modExpr'. For
@@ -67,7 +68,7 @@ namespace mongo {
          *     time of this mod. Therefore, taking references to elements inside modExpr is
          *     valid.
          */
-        virtual Status init( const BSONElement& modExpr ) = 0;
+        virtual Status init(const BSONElement& modExpr) = 0;
 
         /**
          * Returns OK if it would be correct to apply this mod over the document 'root' (e.g, if
@@ -85,31 +86,28 @@ namespace mongo {
          * If the mod cannot be applied over 'root', returns an error status with a reason
          * description.
          */
-        struct ExecInfo {
-            // The fields of concern to the driver: no other op may modify the fields listed here
-            FieldRef* fieldRef[2]; // not owned here
-            bool inPlace;
-            bool noOp;
-        };
-        virtual Status prepareMod( const mutablebson::Element& root,
-                                  const StringData& matchedField,
-                                  /* IN-OUT */ ExecInfo* execInfo ) = 0;
+        struct ExecInfo;
+        virtual Status prepare(mutablebson::Element root,
+                               const StringData& matchedField,
+                               /* IN-OUT */ ExecInfo* execInfo) = 0;
 
         /**
-         * Returns OK and modifies (or adds) an element (or elements) from 'root'.  This may
-         * act on multiple fields but should only be called once per operator.
+         * Returns OK and modifies (or adds) an element (or elements) from the 'root' passed on
+         * the prepareMod call. This may act on multiple fields but should only be called once
+         * per operator.
          *
          * For this call to be issued, the call to 'prepareElem' must have necessarily turned
-         * off 'ExecMode.noOp', ie this mod over this document is not a no-op.
+         * off 'ExecInfo.noOp', ie this mod over this document is not a no-op.
          *
          * If the mod could not be applied, returns an error status with a reason description.
          */
-        virtual Status applyMod( /* IN-OUT */ mutablebson::Element* root ) = 0;
+        virtual Status apply() const = 0 ;
 
         /**
          * Returns OK and registers the result of this mod in 'logRoot', the document that
          * would eventually become a log entry. The mod must have kept enough state to
-         * be able to produce the log record (see idempotency note below).
+         * be able to produce the log record (see idempotency note below). This call may be
+         * issued even if apply() was not.
          *
          * If the mod could not be logged, returns an error status with a reason description.
          *
@@ -121,8 +119,23 @@ namespace mongo {
          *     for logging purposes.  An array based operator may check the contents of the
          *     array before operating on it.
          */
-        virtual Status logMod( mutablebson::Element* logRoot ) = 0;
+        virtual Status log(mutablebson::Element logRoot) const = 0;
 
+    };
+
+    struct ModifierInterface::ExecInfo {
+        static const int MAX_NUM_FIELDS = 2;
+
+        ExecInfo() : inPlace(false), noOp(false) {
+            for (int i = 0; i < MAX_NUM_FIELDS; i++) {
+                fieldRef[i] = NULL;
+            }
+        }
+
+        // The fields of concern to the driver: no other op may modify the fields listed here.
+        FieldRef* fieldRef[MAX_NUM_FIELDS]; // not owned here
+        bool inPlace;
+        bool noOp;
     };
 
 } // namespace mongo
