9e06e7a9a31a1be892e2b891da86ff02e875c6b2
Tad Marshall <tad@10gen.com>
2013-05-08 11:28:09 -0400

    SERVER-9072 Include function declaration from header file
    Use #include "mongo/s/d_logic.h" to get the declaration of logOpForSharding()
    instead of repeating the declaration.

--- src/mongo/db/repl/oplog.cpp
+++ src/mongo/db/repl/oplog.cpp
@@ -37,18 +37,12 @@
 #include "mongo/db/repl/rs.h"
 #include "mongo/db/repl/write_concern.h"
 #include "mongo/db/stats/counters.h"
+#include "mongo/s/d_logic.h"
 #include "mongo/util/elapsed_tracker.h"
 #include "mongo/util/file.h"
 #include "mongo/util/startup_test.h"
 
 namespace mongo {
-
-    // from d_migrate.cpp
-    void logOpForSharding( const char * opstr,
-                           const char * ns,
-                           const BSONObj& obj,
-                           BSONObj * patt,
-                           const BSONObj* fullObj );
 
     // cached copies of these...so don't rename them, drop them, etc.!!!
     static NamespaceDetails *localOplogMainDetails = 0;
