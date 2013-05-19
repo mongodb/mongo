705872c7e387f65a15d4c24163627f65b50be21d
Eliot Horowitz <eliot@10gen.com>
2013-05-19 19:44:29 -0400

    SERVER-6400: This switches from MatcherOld to Matcher2              This commit also has all semantic changes encompassed in this switch

--- jstests/null.js
+++ jstests/null.js
@@ -12,3 +12,15 @@ t.ensureIndex( { x : 1 } );
 
 assert.eq( 1 , t.find( { x : null } ).count() , "C" );
 assert.eq( 1 , t.find( { x : { $ne : null } } ).count() , "D" );
+
+// -----
+
+assert.eq( 2, t.find( { y : null } ).count(), "E" );
+
+t.ensureIndex( { y : 1 } );
+assert.eq( 2, t.find( { y : null } ).count(), "E" );
+
+t.dropIndex( { y : 1 } );
+
+t.ensureIndex( { y : 1 }, { sparse : true } );
+assert.eq( 2, t.find( { y : null } ).count(), "E" );
