90564b42b9c8fa42d25c8838d751aef57176aff8
Keith Bostic <keith@wiredtiger.com>
2013-06-02 16:48:20 -0400

    Now that the memrata driver is storing the basic configuration (including key and value formats), we don't need to modify the configuration array being passed to the underlying session.open_cursor method.

--- src/cursor/cur_ds.c
+++ src/cursor/cur_ds.c
@@ -286,7 +286,6 @@ __wt_curds_create(WT_SESSION_IMPL *session, const char *uri,
 	WT_CONFIG_ITEM cval;
 	WT_CURSOR *cursor, *dsc;
 	WT_DECL_RET;
-	const char **cfgp, *dscfg[5];
 	const char *metaconf;
 
 	metaconf = NULL;
@@ -307,26 +306,10 @@ __wt_curds_create(WT_SESSION_IMPL *session, const char *uri,
 	WT_ERR(
 	    __wt_strndup(session, cval.str, cval.len, &cursor->value_format));
 
-	/*
-	 * And, we'll need to pass that information down to the underlying
-	 * data-source.
-	 */
-	cfgp = dscfg;
-	if (cfg[0] != NULL) {
-		*cfgp++ = cfg[0];
-		if (cfg[1] != NULL) {
-			*cfgp++ = cfg[1];
-			if (cfg[2] != NULL)
-				*cfgp++ = cfg[2];
-		}
-	}
-	*cfgp++ = metaconf;
-	*cfgp = NULL;
-
 	WT_ERR(__wt_cursor_init(cursor, uri, NULL, cfg, cursorp));
 
 	WT_ERR(dsrc->open_cursor(dsrc,
-	    &session->iface, uri, (WT_CONFIG_ARG *)dscfg, &dsc));
+	    &session->iface, uri, (WT_CONFIG_ARG *)cfg, &dsc));
 	memset(&dsc->q, 0, sizeof(dsc->q));
 	dsc->recno = 0;
 	memset(dsc->raw_recno_buf, 0, sizeof(dsc->raw_recno_buf));
