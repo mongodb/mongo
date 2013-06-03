dda6968a4bf4a90ca58ac94d14c09d8fd8f353bf
Keith Bostic <keith@wiredtiger.com>
2013-06-03 12:21:19 -0400

    We have to remove the data-source entry from the metadata file during the drop.

--- src/schema/schema_drop.c
+++ src/schema/schema_drop.c
@@ -183,10 +183,15 @@ __wt_schema_drop(WT_SESSION_IMPL *session, const char *uri, const char *cfg[])
 		ret = __wt_lsm_tree_drop(session, uri, cfg);
 	else if (WT_PREFIX_MATCH(uri, "table:"))
 		ret = __drop_table(session, uri, force, cfg);
-	else if ((ret = __wt_schema_get_source(session, uri, &dsrc)) == 0)
-		ret = dsrc->drop == NULL ?
-		    __wt_object_unsupported(session, uri) :
-		    dsrc->drop(dsrc, &session->iface, uri, NULL);
+	else if ((ret = __wt_schema_get_source(session, uri, &dsrc)) == 0) {
+		if (dsrc->drop == NULL)
+			ret = __wt_object_unsupported(session, uri);
+		if (ret == 0)
+			WT_TRET_NOTFOUND_OK(__wt_metadata_remove(session, uri));
+		if (ret == 0)
+			ret = dsrc->drop(
+			    dsrc, &session->iface, uri, (WT_CONFIG_ARG *)cfg);
+	}
 
 	/*
 	 * Map WT_NOTFOUND to ENOENT (or to 0 if "force" is set), based on the
