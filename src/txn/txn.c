ea3a8780957d15a5ecea87663042113be9f0dd2a
Michael Cahill <michael.cahill@wiredtiger.com>
2013-06-05 17:45:44 +1000

    Back out one part of the previous change: new snapshot transactions should get an ordinary snapshot.

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -253,7 +253,7 @@ __wt_txn_begin(WT_SESSION_IMPL *session, const char *cfg[])
 
 	F_SET(txn, TXN_RUNNING);
 	if (txn->isolation == TXN_ISO_SNAPSHOT)
-		__wt_txn_refresh(session, txn->id, 1);
+		__wt_txn_refresh(session, WT_TXN_NONE, 1);
 	return (0);
 }
 
