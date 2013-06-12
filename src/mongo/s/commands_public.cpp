2f3af717c00a477d98f8d8ecb7821f04853b3d89
Robert J. Moore <Robert.J.Moore@allanbank.com>
2013-06-11 21:56:49 -0400

    Add support for hashed sharding of the chunks collection with the 'filemd5' command.

--- src/mongo/s/commands_public.cpp
+++ src/mongo/s/commands_public.cpp
@@ -1085,7 +1085,7 @@ namespace mongo {
 
                 ChunkManagerPtr cm = conf->getChunkManager( fullns );
                 massert( 13091 , "how could chunk manager be null!" , cm );
-                if(cm->getShardKey().key() == BSON("files_id" << 1)) {
+                if( cm->getShardKey().key() == BSON("files_id" << 1) || cm->getShardKey().key() == BSON("files_id" << "hashed") ) {
                     BSONObj finder = BSON("files_id" << cmdObj.firstElement());
 
                     map<Shard, BSONObj> resMap;
