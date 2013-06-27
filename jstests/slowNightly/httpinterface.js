d0ea86827f0cf222023ea6fcbe4a5c5314c84d89
Andreas Nilsson <andreas.nilsson@10gen.com>
2013-06-27 16:51:15 +0100

    SERVER-10045 Moved httpinterface.js and fixed issue with small-oplog

--- jstests/slowNightly/httpinterface.js
+++ jstests/slowNightly/httpinterface.js
@@ -1,15 +1,16 @@
 // SERVER-9137 test that the httpinterface parameter enables the web interface
 
-var conn = MongoRunner.runMongod({ smallfiles: ""});
+port = allocatePorts( 1 )[ 0 ];
+var conn = MongoRunner.runMongod({ port:port, smallfiles: ""});
 assert.throws( function(){
                new Mongo('localhost:'+(conn.port+1000))} , 
                null , 
-               "the web interface should not be running on conn.port + 1000" )
+               "the web interface should not be running on " + (conn.port + 1000) )
 MongoRunner.stopMongod(conn);
    
-conn = MongoRunner.runMongod({ smallfiles: "", httpinterface: ""});
+conn = MongoRunner.runMongod({ port:port, smallfiles: "", httpinterface: ""});
 var mongo = new Mongo('localhost:'+(conn.port+1000)) 
 assert.neq(null,
            mongo, 
-           "the web interface should be running on conn.port + 1000");
+           "the web interface should be running on " + (conn.port + 1000));
 MongoRunner.stopMongod(conn);
