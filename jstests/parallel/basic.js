64f066468c5d708d17321a44a1ded85002b4fe30
Spencer T Brody <spencer@10gen.com>
2013-07-01 11:24:57 -0400

    Revert "Fix parallel tests by dropping all databases before starting"
    This reverts commit babd275f88181c3f2cd29585816aab24bc4b421c.

--- jstests/parallel/basic.js
+++ jstests/parallel/basic.js
@@ -1,13 +1,5 @@
 // perform basic js tests in parallel
 
-// Drop all databases before running tests
-var databases = db.adminCommand('listDatabases').databases;
-for (var i = 0; i < databases.length; ++i) {
-    if (databases[i].name != "local")
-        db.getSiblingDB(databases[i].name).dropDatabase();
-}
-
-
 Random.setRandomSeed();
 
 var params = ParallelTester.createJstestsLists( 4 );
