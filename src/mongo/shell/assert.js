b74c169c12b942304b294d2c4b397cb0e5a76641
A. Jesse Jiryu Davis <jesse@10gen.com>
2013-07-09 12:10:06 -0400

    More informative error if assert.throws is misused.

--- src/mongo/shell/assert.js
+++ src/mongo/shell/assert.js
@@ -197,7 +197,7 @@ assert.time = function(f, msg, timeout /*ms*/) {
 assert.throws = function(func, params, msg){
     if (assert._debug && msg) print("in assert for: " + msg);
     if (params && typeof(params) == "string")
-        throw "2nd argument to assert.throws has to be an array"
+        throw ("2nd argument to assert.throws has to be an array, not " + params);
     try {
         func.apply(null, params);
     }
