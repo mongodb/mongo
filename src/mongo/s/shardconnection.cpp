e0d9e65fd3c2dfaec1c82504c310288aacac9ce4
Greg Studer <greg@10gen.com>
2013-07-09 11:59:16 -0400

    SERVER-9861 explicitly forget temporary collections for versioning in M/R

--- src/mongo/s/shardconnection.cpp
+++ src/mongo/s/shardconnection.cpp
@@ -323,6 +323,11 @@ namespace mongo {
             _hosts.clear();
         }
 
+        void forgetNS( const string& ns ) {
+            scoped_spinlock lock( _lock );
+            _seenNS.erase( ns );
+        }
+
         // -----
 
         static thread_specific_ptr<ClientConnections> _perThread;
@@ -487,5 +492,9 @@ namespace mongo {
     void ShardConnection::clearPool() {
         shardConnectionPool.clear();
         ClientConnections::threadInstance()->clearPool();
+    }
+
+    void ShardConnection::forgetNS( const string& ns ) {
+        ClientConnections::threadInstance()->forgetNS( ns );
     }
 }
