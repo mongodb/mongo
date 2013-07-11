af16dc9ef411146bfa97e1d4bfa6a9c955b6127a
Andrew Morrow <acm@10gen.com>
2013-07-11 15:26:09 -0400

    SERVER-10110 Fix flakiness in TimeTBuilder jsobj test
    - Use the same clock source for all measured time points
    - Use sleep interval that necessarily exceeds integer division bounds

--- src/mongo/dbtests/jsobjtests.cpp
+++ src/mongo/dbtests/jsobjtests.cpp
@@ -1504,9 +1504,9 @@ namespace JsobjTests {
     public:
         void run() {
             Date_t before = jsTime();
-            sleepmillis(1);
-            time_t now = time(NULL);
-            sleepmillis(1);
+            sleepmillis(2);
+            time_t now = jsTime().toTimeT();
+            sleepmillis(2);
             Date_t after = jsTime();
 
             BSONObjBuilder b;
