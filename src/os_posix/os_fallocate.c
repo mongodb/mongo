04bba4e5612fd07ce48e02e887f1401798305775
Susan LoVerso <sue@wiredtiger.com>
2013-07-11 16:14:51 -0400

    Add __wt_fallocate.

---
+++ src/os_posix/os_fallocate.c
@@ -0,0 +1,31 @@
+/*-
+ * Copyright (c) 2008-2013 WiredTiger, Inc.
+ *	All rights reserved.
+ *
+ * See the file LICENSE for redistribution information.
+ */
+
+#include "wt_internal.h"
+
+/*
+ * __wt_fallocate --
+ *	Allocate space for a file handle.
+ */
+int
+__wt_fallocate(WT_SESSION_IMPL *session, WT_FH *fh, off_t offset, off_t len)
+{
+	WT_DECL_RET;
+
+	WT_VERBOSE_RET(session, fileops, "%s: fallocate", fh->name);
+
+#if defined(HAVE_POSIX_FALLOCATE)
+	WT_SYSCALL_RETRY(posix_fallocate(fh->fd, offset, len), ret);
+	if (ret != 0)
+		WT_RET_MSG(session, ret, "%s: posix_fallocate", fh->name);
+#elif defined(HAVE_FTRUNCATE)
+	WT_SYSCALL_RETRY(ftruncate(fh->fd, offset + len), ret);
+	if (ret != 0)
+		WT_RET_MSG(session, ret, "%s: ftruncate", fh->name);
+#endif
+	return (0);
+}
