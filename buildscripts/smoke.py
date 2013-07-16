df1725c542b2370d90c2d6a3ab336cba1e0f77d8
Spencer T Brody <spencer@10gen.com>
2013-07-16 12:36:35 -0400

    SERVER-10128 skip killop.js in auth passthrough until auth no longer requires a read lock

--- buildscripts/smoke.py
+++ buildscripts/smoke.py
@@ -386,7 +386,8 @@ def skipTest(path):
                            ("jstests", "bench_test1.js"),
                            ("jstests", "bench_test2.js"),
                            ("jstests", "bench_test3.js"),
-                           ("jstests", "drop2.js") # SERVER-8589
+                           ("jstests", "drop2.js"), # SERVER-8589
+                           ("jstests", "killop.js") # SERVER-10128
                            ]
 
         if os.path.join(parentDir,basename) in [ os.path.join(*test) for test in authTestsToSkip ]:
