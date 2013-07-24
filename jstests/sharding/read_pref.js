2f5f00fecf833d8405226687cf6c743eba349deb
Randolph Tan <randolph@10gen.com>
2013-07-24 14:18:44 -0400

    Increase verbosity for SERVER-10206

--- jstests/sharding/read_pref.js
+++ jstests/sharding/read_pref.js
@@ -13,7 +13,7 @@ var SEC_TAGS = [
 var NODES = SEC_TAGS.length + 1;
 
 var doTest = function(useDollarQuerySyntax) {
-    var st = new ShardingTest({ shards: { rs0: { nodes: NODES, oplogSize: 10,
+    var st = new ShardingTest({ shards: { rs0: { nodes: NODES, oplogSize: 10, verbose: 2,
                     useHostName: true }}});
     var replTest = st.rs0;
     var primaryNode = replTest.getMaster();
