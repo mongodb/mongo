e92fb70b5b66ce607b90e20d5c6c6474fdfa3dcf
Keith Bostic <keith@wiredtiger.com>
2013-07-29 11:04:49 -0400

    Don't set the 'Z' flag to MALLOC_OPTIONS (FreeBSD malloc configuration), we want to get garbage memory back from the allocator.

--- test/bloom/test_bloom.c
+++ test/bloom/test_bloom.c
@@ -75,7 +75,7 @@ main(int argc, char *argv[])
 		++g.progname;
 
 	/* Configure the FreeBSD malloc for debugging. */
-	(void)setenv("MALLOC_OPTIONS", "AJZ", 1);
+	(void)setenv("MALLOC_OPTIONS", "AJ", 1);
 
 	/* Set default configuration values. */
 	g.c_cache = 10;
