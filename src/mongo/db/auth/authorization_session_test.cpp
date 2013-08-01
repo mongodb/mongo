0faf9d4d50319235f0d51be8c62fb9ccd8bd5d3d
Spencer T Brody <spencer@10gen.com>
2013-08-01 16:33:55 -0400

    SERVER-9518 Remove unnecessary mock from authorization_session_test

--- src/mongo/db/auth/authorization_session_test.cpp
+++ src/mongo/db/auth/authorization_session_test.cpp
@@ -33,121 +33,62 @@
 namespace mongo {
 namespace {
 
-    TEST(AuthorizationSessionTest, AcquirePrivilegeAndCheckAuthorization) {
+
+    class AuthorizationSessionTest : public ::mongo::unittest::Test {
+    public:
+        AuthzManagerExternalStateMock* managerState;
+        AuthzSessionExternalStateMock* sessionState;
+        scoped_ptr<AuthorizationSession> authzSession;
+        scoped_ptr<AuthorizationManager> authzManager;
+
+        void setUp() {
+            managerState = new AuthzManagerExternalStateMock();
+            authzManager.reset(new AuthorizationManager(managerState));
+            sessionState = new AuthzSessionExternalStateMock(authzManager.get());
+            authzSession.reset(new AuthorizationSession(sessionState));
+        }
+    };
+
+    TEST_F(AuthorizationSessionTest, AcquirePrivilegeAndCheckAuthorization) {
         Principal* principal = new Principal(UserName("Spencer", "test"));
         ActionSet actions;
         actions.addAction(ActionType::insert);
         Privilege writePrivilege("test", actions);
         Privilege allDBsWritePrivilege("*", actions);
-        AuthzManagerExternalStateMock* managerExternalState = new AuthzManagerExternalStateMock();
-        AuthorizationManager authManager(managerExternalState);
-        AuthzSessionExternalStateMock* sessionExternalState = new AuthzSessionExternalStateMock(
-                &authManager);
-        AuthorizationSession authzSession(sessionExternalState);
 
-        ASSERT_FALSE(authzSession.checkAuthorization("test", ActionType::insert));
-        sessionExternalState->setReturnValueForShouldIgnoreAuthChecks(true);
-        ASSERT_TRUE(authzSession.checkAuthorization("test", ActionType::insert));
-        sessionExternalState->setReturnValueForShouldIgnoreAuthChecks(false);
-        ASSERT_FALSE(authzSession.checkAuthorization("test", ActionType::insert));
+        ASSERT_FALSE(authzSession->checkAuthorization("test", ActionType::insert));
+        sessionState->setReturnValueForShouldIgnoreAuthChecks(true);
+        ASSERT_TRUE(authzSession->checkAuthorization("test", ActionType::insert));
+        sessionState->setReturnValueForShouldIgnoreAuthChecks(false);
+        ASSERT_FALSE(authzSession->checkAuthorization("test", ActionType::insert));
 
         ASSERT_EQUALS(ErrorCodes::UserNotFound,
-                      authzSession.acquirePrivilege(writePrivilege, principal->getName()));
-        authzSession.addAndAuthorizePrincipal(principal);
-        ASSERT_OK(authzSession.acquirePrivilege(writePrivilege, principal->getName()));
-        ASSERT_TRUE(authzSession.checkAuthorization("test", ActionType::insert));
+                      authzSession->acquirePrivilege(writePrivilege, principal->getName()));
+        authzSession->addAndAuthorizePrincipal(principal);
+        ASSERT_OK(authzSession->acquirePrivilege(writePrivilege, principal->getName()));
+        ASSERT_TRUE(authzSession->checkAuthorization("test", ActionType::insert));
 
-        ASSERT_FALSE(authzSession.checkAuthorization("otherDb", ActionType::insert));
-        ASSERT_OK(authzSession.acquirePrivilege(allDBsWritePrivilege, principal->getName()));
-        ASSERT_TRUE(authzSession.checkAuthorization("otherDb", ActionType::insert));
+        ASSERT_FALSE(authzSession->checkAuthorization("otherDb", ActionType::insert));
+        ASSERT_OK(authzSession->acquirePrivilege(allDBsWritePrivilege, principal->getName()));
+        ASSERT_TRUE(authzSession->checkAuthorization("otherDb", ActionType::insert));
         // Auth checks on a collection should be applied to the database name.
-        ASSERT_TRUE(authzSession.checkAuthorization("otherDb.collectionName", ActionType::insert));
+        ASSERT_TRUE(authzSession->checkAuthorization("otherDb.collectionName", ActionType::insert));
 
-        authzSession.logoutDatabase("test");
-        ASSERT_FALSE(authzSession.checkAuthorization("test", ActionType::insert));
+        authzSession->logoutDatabase("test");
+        ASSERT_FALSE(authzSession->checkAuthorization("test", ActionType::insert));
     }
 
-    class AuthManagerExternalStateImplictPriv : public AuthzManagerExternalStateMock {
-    public:
-        AuthManagerExternalStateImplictPriv() : AuthzManagerExternalStateMock() {}
 
-        virtual Status _findUser(const string& usersNamespace,
-                                 const BSONObj& query,
-                                 BSONObj* result) const {
-
-            NamespaceString nsstring(usersNamespace);
-            std::string user = query[AuthorizationManager::USER_NAME_FIELD_NAME].String();
-            std::string userSource;
-            if (!query[AuthorizationManager::USER_SOURCE_FIELD_NAME].trueValue()) {
-                userSource = nsstring.db().toString();
-            }
-            else {
-                userSource = query[AuthorizationManager::USER_SOURCE_FIELD_NAME].String();
-            }
-            *result = mapFindWithDefault(_privilegeDocs,
-                                         std::make_pair(nsstring.db().toString(),
-                                                        UserName(user, userSource)),
-                                         BSON("invalid" << 1));
-            return (*result)["invalid"].trueValue() ?
-                    Status(ErrorCodes::UserNotFound, "User not found") :
-                    Status::OK();
-        }
-
-        void addPrivilegeDocument(const string& dbname,
-                                  const UserName& user,
-                                  const BSONObj& doc) {
-
-            ASSERT(_privilegeDocs.insert(std::make_pair(std::make_pair(dbname, user),
-                                                        doc.getOwned())).second);
-        }
-
-    private:
-        std::map<std::pair<std::string, UserName>, BSONObj > _privilegeDocs;
-    };
-
-    class AuthSessionExternalStateImplictPriv : public AuthzSessionExternalStateMock {
-    public:
-        AuthSessionExternalStateImplictPriv(AuthorizationManager* authzManager,
-                                            AuthManagerExternalStateImplictPriv* managerExternal) :
-            AuthzSessionExternalStateMock(authzManager),
-            _authManagerExternalState(managerExternal) {}
-
-        void addPrivilegeDocument(const string& dbname,
-                                  const UserName& user,
-                                  const BSONObj& doc) {
-            _authManagerExternalState->addPrivilegeDocument(dbname, user, doc);
-        }
-
-    private:
-        AuthManagerExternalStateImplictPriv* _authManagerExternalState;
-    };
-
-    class ImplicitPriviligesTest : public ::mongo::unittest::Test {
-    public:
-        AuthManagerExternalStateImplictPriv* managerState;
-        AuthSessionExternalStateImplictPriv* sessionState;
-        scoped_ptr<AuthorizationSession> authzSession;
-        scoped_ptr<AuthorizationManager> authzManager;
-
-        void setUp() {
-            managerState = new AuthManagerExternalStateImplictPriv();
-            authzManager.reset(new AuthorizationManager(managerState));
-            sessionState = new AuthSessionExternalStateImplictPriv(authzManager.get(),
-                                                                   managerState);
-            authzSession.reset(new AuthorizationSession(sessionState));
-        }
-    };
-
-    TEST_F(ImplicitPriviligesTest, ImplicitAcquireFromSomeDatabases) {
-        sessionState->addPrivilegeDocument("test", UserName("andy", "test"),
+    TEST_F(AuthorizationSessionTest, ImplicitAcquireFromSomeDatabases) {
+        managerState->insertPrivilegeDocument("test",
                                     BSON("user" << "andy" <<
                                          "pwd" << "a" <<
                                          "roles" << BSON_ARRAY("readWrite")));
-        sessionState->addPrivilegeDocument("test2", UserName("andy", "test"),
+        managerState->insertPrivilegeDocument("test2",
                                     BSON("user" << "andy" <<
                                          "userSource" << "test" <<
                                          "roles" <<  BSON_ARRAY("read")));
-        sessionState->addPrivilegeDocument("admin", UserName("andy", "test"),
+        managerState->insertPrivilegeDocument("admin",
                                     BSON("user" << "andy" <<
                                          "userSource" << "test" <<
                                          "roles" << BSON_ARRAY("clusterAdmin") <<
