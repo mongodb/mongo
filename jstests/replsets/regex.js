58f5cb783155df93020c15b81d24515c0fbee488
Scott Hernandez <scotthernandez@gmail.com>
2013-08-09 13:54:22 -0400

    fix test to accept both error codes for update impls

--- jstests/replsets/regex.js
+++ jstests/replsets/regex.js
@@ -1,5 +1,11 @@
 // don't allow regex as _id: SERVER-9502
 
+function assertNotOkForStorage(gleCmd) {
+    var gle = master.getDB("test").runCommand({getLastError : 1, w : 2, wtimeout : 60000});
+    failed = gle.code == 16824 || gle.code == 17013;
+    assert(failed, tojson(gle));
+}
+
 var replTest = new ReplSetTest( {name: "server9502", nodes: 2} );
 var nodes = replTest.startSet();
 replTest.initiate();
@@ -10,10 +16,8 @@ var gle = master.getDB("test").runCommand({getLastError : 1, w : 2, wtimeout : 6
 assert(gle.err === null);
 
 mdb.foo.insert({ _id: /^A/ });
-var gle = master.getDB("test").runCommand({getLastError : 1, w : 2, wtimeout : 60000});
-assert(gle.code === 16824);
+assertNotOkForStorage(master.getDB("test").runCommand({getLastError : 1, w : 2, wtimeout : 60000}));
 
 // _id doesn't have to be first; still disallowed
 mdb.foo.insert({ xxx: "ABCDEF", _id: /ABCDEF/ });
-var gle = master.getDB("test").runCommand({getLastError : 1, w : 2, wtimeout : 60000});
-assert(gle.code === 16824);
+assertNotOkForStorage(master.getDB("test").runCommand({getLastError : 1, w : 2, wtimeout : 60000}));
