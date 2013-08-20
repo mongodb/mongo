0db5ea9c643ca7f5ef9b9d1fd2c0515b612e7bd9
Greg Studer <greg@10gen.com>
2013-08-20 10:01:47 -0400

    SERVER-10482 buildbot movechunk_with_def_paranoia.js make sure test updated with default

--- jstests/sharding/movechunk_with_def_paranoia.js
+++ jstests/sharding/movechunk_with_def_paranoia.js
@@ -5,10 +5,8 @@ var st = new ShardingTest( { shards:2, mongos:1 , other : { chunksize : 1 }});
 load("jstests/sharding/movechunk_include.js")
 setupMoveChunkTest(st);
 
-var shards = [st.shard0, st.shard1];
-for(i in shards) {
-    var dbpath = shards[i].adminCommand("getCmdLineOpts").parsed.dbpath;
-    var hasMoveChunkDir = 0 != ls(dbpath).filter(function(a) {return null != a.match("moveChunk")}).length
-    assert(!hasMoveChunkDir, dbpath + ": has MoveChunk directory + " + ls(dbpath))
-}
+var dbpath = st.shard1.adminCommand("getCmdLineOpts").parsed.dbpath;
+var hasMoveChunkDir = 0 != ls(dbpath).filter(function(a) {return null != a.match("moveChunk")}).length
+assert(hasMoveChunkDir, dbpath + ": does not has MoveChunk directory + " + ls(dbpath))
+
 st.stop()
