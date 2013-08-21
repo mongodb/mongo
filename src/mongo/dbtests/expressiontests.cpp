2b296490d0562ff827a42b2efa38607d6ce0e5d8
matt dannenberg <matt.dannenberg@10gen.com>
2013-08-21 14:07:20 -0400

    SERVER-10086 fix $setDifference to return a set (spec changed)

--- src/mongo/dbtests/expressiontests.cpp
+++ src/mongo/dbtests/expressiontests.cpp
@@ -3019,6 +3019,20 @@ namespace ExpressionTests {
             }
         };
 
+        class SuperWithRedundant : public ExpectedResultBase {
+            Document getSpec() {
+                return DOC("input" << DOC_ARRAY( DOC_ARRAY(1 << 2 << 2)
+                                              << DOC_ARRAY(1) )
+                        << "expected" << DOC("$setIsSubset" << false
+                                          << "$setEquals" << false
+                                          << "$setIntersection" << DOC_ARRAY(1)
+                                          << "$setUnion" << DOC_ARRAY(1 << 2)
+                                          << "$setDifference" << DOC_ARRAY(2) )
+                       );
+
+            }
+        };
+
         class Sub : public ExpectedResultBase {
             Document getSpec() {
                 return DOC("input" << DOC_ARRAY( DOC_ARRAY(1)
