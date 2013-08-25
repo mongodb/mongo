d118be2d095e250eeb2f30ac9feefb3e05ba4e85
Greg Studer <greg@10gen.com>
2013-08-24 21:13:42 -0400

    SERVER-10618 complex_sharding.js -> basic_sharding.js invert verbosity for less verbose rs nodes

--- jstests/sharding/basic_sharding_params.js
+++ jstests/sharding/basic_sharding_params.js
@@ -3,12 +3,12 @@
 function shardingTestUsingObjects() {
     var st = new ShardingTest( {
     
-       mongos : { s0 : { verbose : 1 }, s1 : { verbose : 2 } },
-       config : { c0 : { verbose : 3 } },
-       shards : { d0 : { verbose : 4 }, 
+       mongos : { s0 : { verbose : 6 }, s1 : { verbose : 5 } },
+       config : { c0 : { verbose : 4 } },
+       shards : { d0 : { verbose : 3 }, 
                   rs1 : {
-                      nodes : { d0 : { verbose : 5 }, 
-                                a1 : { verbose : 6 } } }
+                      nodes : { d0 : { verbose : 2 }, 
+                                a1 : { verbose : 1 } } }
        }
     } );
 
@@ -30,21 +30,21 @@ function shardingTestUsingObjects() {
     var rs1_d0 = rs1.nodes[0];
     var rs1_a1 = rs1.nodes[1];
 
-    assert.contains( "-v", s0.commandLine );
-    assert.contains( "-vv", s1.commandLine );
-    assert.contains( "-vvv", c0.commandLine );
-    assert.contains( "-vvvv", d0.commandLine );
-    assert.contains( "-vvvvv", rs1_d0.commandLine );
-    assert.contains( "-vvvvvv", rs1_a1.commandLine );
+    assert.contains( "-vvvvvv", s0.commandLine );
+    assert.contains( "-vvvvv", s1.commandLine );
+    assert.contains( "-vvvv", c0.commandLine );
+    assert.contains( "-vvv", d0.commandLine );
+    assert.contains( "-vv", rs1_d0.commandLine );
+    assert.contains( "-v", rs1_a1.commandLine );
 
     st.stop();
 }
 
 function shardingTestUsingArrays() {
     var st = new ShardingTest( {
-       mongos : [{ verbose : 1 },  { verbose : 2 } ],
+       mongos : [{ verbose : 5 },  { verbose : 4 } ],
        config : [{ verbose : 3 }],
-       shards : [{ verbose : 4 }, { verbose : 5 } ]
+       shards : [{ verbose : 2 }, { verbose : 1 } ]
     });
 
     var s0 = st.s0;
@@ -62,11 +62,11 @@ function shardingTestUsingArrays() {
     var d1 = st.d1;
     assert.eq( d1, st._shardServers[1] );
 
-    assert.contains( "-v", s0.commandLine );
-    assert.contains( "-vv", s1.commandLine );
+    assert.contains( "-vvvvv", s0.commandLine );
+    assert.contains( "-vvvv", s1.commandLine );
     assert.contains( "-vvv", c0.commandLine );
-    assert.contains( "-vvvv", d0.commandLine );
-    assert.contains( "-vvvvv", d1.commandLine );
+    assert.contains( "-vv", d0.commandLine );
+    assert.contains( "-v", d1.commandLine );
 
     st.stop();
 }
