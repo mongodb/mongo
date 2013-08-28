d328dd5227fd0480ff55a5d3b85adcd3e287d1da
Spencer T Brody <spencer@10gen.com>
2013-08-28 15:38:12 -0400

    SERVER-10640 SERVER-10639 Skip sharding/trace_missing_docs_test.js in auth passthrough

--- buildscripts/smoke.py
+++ buildscripts/smoke.py
@@ -400,6 +400,7 @@ def skipTest(path):
         authTestsToSkip = [("jstests", "drop2.js"), # SERVER-8589,
                            ("jstests", "killop.js"), # SERVER-10128
                            ("sharding", "replmonitor_bad_seed.js"), # SERVER-10420
+                           ("sharding", "trace_missing_docs_test.js"), # SERVER-10640
                            ("sharding", "sync3.js"), # SERVER-6388 for this and those below
                            ("sharding", "sync6.js"),
                            ("sharding", "parallel.js"),
