66379f85a208933322a54ff98895392f40d4a7a0
Ian Whalen <ian@mongodb.com>
2013-09-06 16:15:35 -0400

    SERVER-8886 Add OpenSSL exception to files in src/mongo/db

--- src/mongo/db/fts/stop_words.cpp
+++ src/mongo/db/fts/stop_words.cpp
@@ -14,6 +14,18 @@
 *
 *    You should have received a copy of the GNU Affero General Public License
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*    As a special exception, the copyright holders give permission to link the
+*    code of portions of this program with the OpenSSL library under certain
+*    conditions as described in each individual source file and distribute
+*    linked combinations including the program with the OpenSSL library. You
+*    must comply with the GNU Affero General Public License in all respects for
+*    all of the code used other than as permitted herein. If you modify file(s)
+*    with this exception, you may extend this exception to your version of the
+*    file(s), but you are not obligated to do so. If you do not wish to do so,
+*    delete this exception statement from your version. If you delete this
+*    exception statement from all source files in the program, then also delete
+*    it in the license file.
 */
 
 #include <map>
