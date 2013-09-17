5742388238e01474b67c235ec3ecf55548f99939
Matt Dannenberg <matt.dannenberg@10gen.com>
2013-09-17 11:43:44 -0400

    SERVER-9818 fix race in jstests/slowNightly/httpinterface.js

--- jstests/slowNightly/httpinterface.js
+++ jstests/slowNightly/httpinterface.js
@@ -1,16 +1,25 @@
 // SERVER-9137 test that the httpinterface parameter enables the web interface
+port = allocatePorts(1)[0];
+httpPort = port + 1000;
+var conn = MongoRunner.runMongod({port: port, smallfiles: ""});
 
-port = allocatePorts( 1 )[ 0 ];
-var conn = MongoRunner.runMongod({ port:port, smallfiles: ""});
-assert.throws( function(){
-               new Mongo('localhost:'+(conn.port+1000))} , 
-               null , 
-               "the web interface should not be running on " + (conn.port + 1000) )
+tryHttp = function() {
+    try {
+        var mongo = new Mongo('localhost:'+httpPort) ;
+    }
+    catch (e) {
+        return false;
+    }
+    // if we managed to start and connect a new mongo then the web interface is working
+    return true;
+}
+
+assert.throws(function() {assert.soon(tryHttp, "tryHttp failed, like we expected it to")});
+
 MongoRunner.stopMongod(conn);
-   
+
 conn = MongoRunner.runMongod({ port:port, smallfiles: "", httpinterface: ""});
-var mongo = new Mongo('localhost:'+(conn.port+1000)) 
-assert.neq(null,
-           mongo, 
-           "the web interface should be running on " + (conn.port + 1000));
+assert.soon(tryHttp,
+            "the web interface should be running on " + httpPort);
+
 MongoRunner.stopMongod(conn);
