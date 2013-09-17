149614e3232e57ab35c1fc68627b52b3d3195e42
Keith Bostic <keith@wiredtiger.com>
2013-09-17 12:29:02 -0400

    Don't read both the standard and the command-line CONFIG file, use one or the other.

--- test/format/t.c
+++ test/format/t.c
@@ -37,6 +37,9 @@ int
 main(int argc, char *argv[])
 {
 	int ch, reps, ret;
+	const char *config;
+
+	config = NULL;
 
 	if ((g.progname = strrchr(argv[0], '/')) == NULL)
 		g.progname = argv[0];
@@ -52,10 +55,6 @@ main(int argc, char *argv[])
 	(void)setenv("MALLOC_OPTIONS", "AJ", 1);
 #endif
 
-	/* Set values from the "CONFIG" file, if it exists. */
-	if (access("CONFIG", R_OK) == 0)
-		config_file("CONFIG");
-
 	/* Track progress unless we're re-directing output to a file. */
 	g.track = isatty(STDOUT_FILENO) ? 1 : 0;
 
@@ -69,7 +68,7 @@ main(int argc, char *argv[])
 			g.config_open = optarg;
 			break;
 		case 'c':			/* Configuration from a file */
-			config_file(optarg);
+			config = optarg;
 			break;
 		case 'L':			/* Re-direct output to a log */
 			/*
@@ -92,9 +91,22 @@ main(int argc, char *argv[])
 		default:
 			usage();
 		}
-
 	argc -= optind;
 	argv += optind;
+
+	/*
+	 * If we weren't given a configuration file, set values from "CONFIG",
+	 * if it exists.
+	 */
+	if (config == NULL && access("CONFIG", R_OK) == 0)
+		config = "CONFIG";
+	if (config != NULL)
+		config_file(config);
+
+	/*
+	 * The rest of the arguments are individual configurations that modify
+	 * the base configuration.
+	 */
 	for (; *argv != NULL; ++argv)
 		config_single(*argv, 1);
 
