30f4e0801ab576eda3825ca671062f3736ba0f51
Sridhar Nanjundeswaran <sridhar@10gen.com>
2013-09-18 15:04:57 -0400

    SERVER-10792 - Account for enterprise license in enterprise build

--- buildscripts/packaging/msi/buildenterprisemsi.bat
+++ buildscripts/packaging/msi/buildenterprisemsi.bat
@@ -3,6 +3,7 @@ SET VERSION=2.6.0
 SET BINDIR=..\..\..\build\win32\64\dynamic-windows\extrapathdyn_c__Utils_sasl_c__Utils_snmp_c__Utils_ssl\release\ssl\mongo
 SET CLIENTLIBDIR=..\..\..\build\win32\64\dynamic-windows\extrapathdyn_c__Utils_sasl_c__Utils_snmp_c__Utils_ssl\release\ssl\client_build
 SET LICENSEDIR=..\..\..\distsrc
+SET ENTERPRISELICENSEDIR=..\..\..\src\mongo\db\modules\subscription\distsrc
 SET EDITION=Enterprise
 SET FLAVOR=2008R2Plus
 SET SASLDIR=..\..\..\..\..\..\Utils\sasl\bin
@@ -39,10 +40,14 @@ IF NOT "%1"=="" (
         SET CLIENTLIBDIR=%2
         SHIFT
     )
+    IF "%1"=="-enterpriselicensedir" (
+        SET ENTERPRISELICENSEDIR=%2
+        SHIFT
+    )
     SHIFT
     GOTO :loop
 )
 
 ECHO Building enterprise msi for version %VERSION% with binaries from %BINDIR%, sasl from %SASLDIR%, ssl from %OPENSSLDIR%, snmp from %SNMPDIR% and license files from %LICENSEDIR%
 
-%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild /p:Configuration=Release;Version=%VERSION%;License=%LICENSEDIR%;Source=%BINDIR%;SaslSource=%SASLDIR%;SnmpSource=%SNMPDIR%;SslSource=%OPENSSLDIR%;Edition=%EDITION%;Flavor=%FLAVOR%;ClientSource=%CLIENTLIBDIR% MongoDB_64.wixproj
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild /p:Configuration=Release;Version=%VERSION%;License=%LICENSEDIR%;Source=%BINDIR%;SaslSource=%SASLDIR%;SnmpSource=%SNMPDIR%;SslSource=%OPENSSLDIR%;Edition=%EDITION%;Flavor=%FLAVOR%;ClientSource=%CLIENTLIBDIR%;EnterpriseLicense=%ENTERPRISELICENSEDIR% MongoDB_64.wixproj
