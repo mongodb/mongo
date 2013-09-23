fb4944060d8636f37dc400a9ce1339fd20fd96f6
Sridhar Nanjundeswaran <sridhar@10gen.com>
2013-09-23 14:29:52 -0700

    SERVER-10822 - Include generated C++ driver header files folder Build directly using heat, candle and light

--- buildscripts/packaging/msi/build64bit2008R2msi.bat
+++ buildscripts/packaging/msi/build64bit2008R2msi.bat
@@ -5,6 +5,8 @@ SET CLIENTLIBDIR=..\..\..\build\win32\64\client_build
 SET LICENSEDIR=..\..\..\distsrc
 SET EDITION=Standard
 SET FLAVOR=2008R2Plus
+SET CLIENTHEADERDIR=..\..\..\build\win32\normal\client_build\include
+
 :loop
 IF NOT "%1"=="" (
     IF "%1"=="-version" (
@@ -23,9 +25,13 @@ IF NOT "%1"=="" (
         SET CLIENTLIBDIR=%2
         SHIFT
     )
+    IF "%1"=="-clientheaderdir" (
+        SET CLIENTHEADERDIR=%2
+        SHIFT
+    )
     SHIFT
     GOTO :loop
 )
 
 ECHO Building msi for version %VERSION% with binaries from %BINDIR% and license files from %LICENSEDIR%
-%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild /p:Configuration=Release;Version=%VERSION%;License=%LICENSEDIR%;Source=%BINDIR%;Edition=%EDITION%;Flavor=%FLAVOR%;ClientSource=%CLIENTLIBDIR% MongoDB_64.wixproj
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild /p:Configuration=Release;Version=%VERSION%;License=%LICENSEDIR%;Source=%BINDIR%;Edition=%EDITION%;Flavor=%FLAVOR%;ClientSource=%CLIENTLIBDIR%;ClientHeaderSource=%CLIENTHEADERDIR% MongoDB_64.wixproj
