9976d1d8e9627010f0741612cbfee1ac6c1562f6
Eric Milkie <milkie@10gen.com>
2013-09-26 12:58:12 -0400

    SERVER-10261 disable ssl session caching (affects java driver)

--- src/mongo/util/net/ssl_manager.cpp
+++ src/mongo/util/net/ssl_manager.cpp
@@ -150,14 +150,8 @@ namespace mongo {
         // Note: this is for blocking sockets only.
         SSL_CTX_set_mode(_context, SSL_MODE_AUTO_RETRY);
 
-        // Set context within which session can be reused
-        int status = SSL_CTX_set_session_id_context(
-            _context,
-            static_cast<unsigned char*>(static_cast<void*>(&_context)),
-            sizeof(_context));
-        if (!status) {
-            uasserted(16768,"ssl initialization problem");
-        }
+        // Disable session caching (see SERVER-10261)
+        SSL_CTX_set_session_cache_mode(_context, SSL_SESS_CACHE_OFF);
 
         SSLThreadInfo::init();
         SSLThreadInfo::get();
