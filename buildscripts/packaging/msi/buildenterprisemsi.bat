7966d19ab827fadceef4e824fc12d90b7fd968da
Sridhar Nanjundeswaran <sridhar@10gen.com>
2013-09-27 20:11:30 -0700

    SERVER-10870 - Include SNMP configuration files for mongod. Also removed snmp dlls getting installed for non mongod components

--- buildscripts/packaging/msi/buildenterprisemsi.bat
+++ buildscripts/packaging/msi/buildenterprisemsi.bat
@@ -3,11 +3,11 @@ SET VERSION=2.6.0
 SET BINDIR=..\..\..\build\win32\64\dynamic-windows\extrapathdyn_c__Utils_sasl_c__Utils_snmp_c__Utils_ssl\release\ssl\mongo
 SET CLIENTLIBDIR=..\..\..\build\win32\64\dynamic-windows\extrapathdyn_c__Utils_sasl_c__Utils_snmp_c__Utils_ssl\release\ssl\client_build
 SET LICENSEDIR=..\..\..\distsrc
-SET ENTERPRISELICENSEDIR=..\..\..\src\mongo\db\modules\subscription\distsrc
+SET ENTERPRISEBASEDIR=..\..\..\src\mongo\db\modules\subscription
 SET EDITION=Enterprise
 SET FLAVOR=2008R2Plus
 SET SASLDIR=..\..\..\..\..\..\Utils\sasl\bin
-SET OPENSSLDIR=..\..\..\..\..\..\Utils\ssl\bin
+SET OPENSSLDIR=..\..\..\..\..\..\Utils\openssl\bin
 SET SNMPDIR=..\..\..\..\..\..\Utils\snmp\bin
 SET CLIENTHEADERDIR=..\..\..\build\win32\normal\client_build\include
 
@@ -41,8 +41,8 @@ IF NOT "%1"=="" (
         SET CLIENTLIBDIR=%2
         SHIFT
     )
-    IF "%1"=="-enterpriselicensedir" (
-        SET ENTERPRISELICENSEDIR=%2
+    IF "%1"=="-enterprisebasedir" (
+        SET ENTERPRISEBASEDIR=%2
         SHIFT
     )
     IF "%1"=="-clientheaderdir" (
@@ -55,4 +55,4 @@ IF NOT "%1"=="" (
 
 ECHO Building enterprise msi for version %VERSION% with binaries from %BINDIR%, sasl from %SASLDIR%, ssl from %OPENSSLDIR%, snmp from %SNMPDIR% and license files from %LICENSEDIR%
 
-%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild /p:Configuration=Release;Version=%VERSION%;License=%LICENSEDIR%;Source=%BINDIR%;SaslSource=%SASLDIR%;SnmpSource=%SNMPDIR%;SslSource=%OPENSSLDIR%;Edition=%EDITION%;Flavor=%FLAVOR%;ClientSource=%CLIENTLIBDIR%;EnterpriseLicense=%ENTERPRISELICENSEDIR%;ClientHeaderSource=%CLIENTHEADERDIR% MongoDB_64.wixproj
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\msbuild /p:Configuration=Release;Version=%VERSION%;License=%LICENSEDIR%;Source=%BINDIR%;SaslSource=%SASLDIR%;SnmpSource=%SNMPDIR%;SslSource=%OPENSSLDIR%;Edition=%EDITION%;Flavor=%FLAVOR%;ClientSource=%CLIENTLIBDIR%;EnterpriseBase=%ENTERPRISEBASEDIR%;ClientHeaderSource=%CLIENTHEADERDIR% MongoDB_64.wixproj
