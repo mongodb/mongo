55d3d19bf6fd0fae3e11940f0ca1d45bb3afc7a3
Ernie Hershey <ernie.hershey@10gen.com>
2013-09-30 13:33:08 -0400

    SERVER-3579 Change redhat username and group from mongod to mongodb

--- buildscripts/packager-enterprise.py
+++ buildscripts/packager-enterprise.py
@@ -856,12 +856,12 @@ touch $RPM_BUILD_ROOT/var/log/mongodb/mongod.log
 rm -rf $RPM_BUILD_ROOT
 
 %pre server
-if ! /usr/bin/id -g mongod &>/dev/null; then
+if ! /usr/bin/id -g mongodb &>/dev/null; then
     /usr/sbin/groupadd -r mongod
 fi
-if ! /usr/bin/id mongod &>/dev/null; then
-    /usr/sbin/useradd -M -r -g mongod -d /var/lib/mongodb -s /bin/false \
-	-c mongod mongod > /dev/null 2>&1
+if ! /usr/bin/id mongodb &>/dev/null; then
+    /usr/sbin/useradd -M -r -g mongodb -d /var/lib/mongodb -s /bin/false \
+	-c mongodb mongodb > /dev/null 2>&1
 fi
 
 %post server
@@ -926,9 +926,9 @@ fi
 /etc/rc.d/init.d/mongod
 /etc/sysconfig/mongod
 #/etc/rc.d/init.d/mongos
-%attr(0755,mongod,mongod) %dir /var/lib/mongodb
-%attr(0755,mongod,mongod) %dir /var/log/mongodb
-%attr(0640,mongod,mongod) %config(noreplace) %verify(not md5 size mtime) /var/log/mongodb/mongod.log
+%attr(0755,mongodb,mongodb) %dir /var/lib/mongodb
+%attr(0755,mongodb,mongodb) %dir /var/log/mongodb
+%attr(0640,mongodb,mongodb) %config(noreplace) %verify(not md5 size mtime) /var/log/mongodb/mongod.log
 
 %changelog
 * Thu Jan 28 2010 Richard M Kreuter <richard@10gen.com>
