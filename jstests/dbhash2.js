3364db8c55bb72f5b93f42588464dc5a7bb70150
Eliot Horowitz <eliot@10gen.com>
2013-10-03 11:25:00 -0400

    SERVER-11021: cache dbhash results in mongod for config. collections

---
+++ jstests/dbhash2.js
@@ -0,0 +1,22 @@
+
+mydb = db.getSisterDB( "config" );
+
+t = mydb.foo;
+t.drop();
+
+t.insert( { x : 1 } );
+res1 = mydb.runCommand( "dbhash" );
+assert( res1.fromCache.indexOf( "config.foo" ) == -1 );
+
+res2 = mydb.runCommand( "dbhash" );
+assert( res2.fromCache.indexOf( "config.foo" ) >= 0 );
+assert.eq( res1.collections.foo, res2.collections.foo );
+
+t.insert( { x : 2 } );
+res3 = mydb.runCommand( "dbhash" );
+assert( res3.fromCache.indexOf( "config.foo" ) < 0 );
+assert.neq( res1.collections.foo, res3.collections.foo );
+
+
+
+
