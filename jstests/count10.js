b297497910d07493c84eab330d2ed4fcd9e34502
David Storch <david.storch@10gen.com>
2013-10-04 12:58:23 -0400

    SERVER-11035 fix count10.js MCI failures
    Signed-off-by: Ian Whalen <ian@mongodb.com>

--- jstests/count10.js
+++ jstests/count10.js
@@ -9,26 +9,27 @@ for ( i=0; i<100; i++ ){
 // make sure data is written
 db.getLastError();
 
-var thr = new Thread(function () {
+s = startParallelShell(
+    'sleep(1000); ' +
+    'current = db.currentOp({"ns": db.count10.getFullName(), "query.count": db.count10.getName()}); ' +
+    'assert(current); ' +
+    'countOp = current.inprog[0]; ' +
+    'assert(countOp); ' +
+    'db.killOp(countOp.opid); '
+);
+
+function getKilledCount() {
     try {
         db.count10.find("sleep(1000)").count();
-    }
-    catch (e) {
+    } catch (e) {
         return e;
     }
-});
+}
 
-thr.start();
-sleep(1000);
-
-current = db.currentOp({"ns": t.getFullName(), "query.count": t.getName()});
-assert(current);
-countOp = current.inprog[0];
-assert(countOp);
-
-db.killOp(countOp.opid);
-res = thr.returnData();
-
+var res = getKilledCount();
 assert(res);
 assert(res.match(/count failed/) !== null);
 assert(res.match(/\"code\"/) !== null);
+
+s();
+
