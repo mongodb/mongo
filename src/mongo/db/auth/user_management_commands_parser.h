2fd675487ee609e35067021376b18d0ce01da086
Andy Schwerin <schwerin@10gen.com>
2013-10-05 12:59:28 -0400

    SERVER-10670 Expose more parsing functionality from user_management_commands_parser.cpp

--- src/mongo/db/auth/user_management_commands_parser.h
+++ src/mongo/db/auth/user_management_commands_parser.h
@@ -159,6 +159,22 @@ namespace auth {
     Status parseRemoveRolesFromDatabaseCommand(const BSONObj& cmdObj,
                                                const std::string& dbname,
                                                BSONObj* parsedWriteConcern);
+    /**
+     * Parses the privileges described in "privileges" into a vector of Privilege objects.
+     * Returns Status::OK() upon successfully parsing all the elements of "privileges".
+     */
+    Status parseAndValidatePrivilegeArray(const BSONArray& privileges,
+                                          PrivilegeVector* parsedPrivileges);
+
+    /**
+     * Takes a BSONArray of name,source pair documents, parses that array and returns (via the
+     * output param parsedRoleNames) a list of the role names in the input array.
+     * Performs syntactic validation of "rolesArray", only.
+     */
+    Status parseRoleNamesFromBSONArray(const BSONArray& rolesArray,
+                                       const StringData& dbname,
+                                       const StringData& rolesFieldName,
+                                       std::vector<RoleName>* parsedRoleNames);
 
 } // namespace auth
 } // namespace mongo
