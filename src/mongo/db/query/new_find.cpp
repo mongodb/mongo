973f199c11040e3dcdebdf544aaae614367df1b1
Alberto Lerner <alerner@10gen.com>
2013-10-12 17:22:26 -0400

    SERVER-10545 Don't save tailable cursor if only one document was requested.

--- src/mongo/db/query/new_find.cpp
+++ src/mongo/db/query/new_find.cpp
@@ -696,7 +696,7 @@ namespace mongo {
             // collection is empty. Otherwise, the semantics of the tailable cursor is that the
             // client will keep trying to read from it. So we'll keep it around.
             Collection* collection = ctx.ctx().db()->getCollection(cq->ns());
-            if (collection->numRecords() != 0) {
+            if (collection->numRecords() != 0 && pq.getNumToReturn() != 1) {
                 saveClientCursor = true;
             }
         }
