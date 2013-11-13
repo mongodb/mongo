b4d54124ca66f1cdc248ae9dc542fae35f636f83
Greg Studer <greg@10gen.com>
2013-11-13 11:14:11 -0500

    SERVER-11676 buildbot change limit in writeback_bulk_insert.js

--- jstests/sharding/writeback_bulk_insert.js
+++ jstests/sharding/writeback_bulk_insert.js
@@ -26,10 +26,6 @@ var collB = mongosB.getCollection("" + collA)
 collB.insert({hello : "world"})
 assert.eq(null, collB.getDB().getLastError())
 
-var collC = mongosC.getCollection("" + collA)
-collC.insert({hello : "world"})
-assert.eq(null, collC.getDB().getLastError())
-
 jsTest.log("Enabling sharding...")
 
 printjson(mongosA.getDB("admin").runCommand({enableSharding : collA.getDB()
@@ -46,6 +42,7 @@ var data1MB = "x"
 while (data1MB.length < 1024 * 1024)
     data1MB += data1MB;
 
+
 var data7MB = ""
 // Data now at 7MB
 for ( var i = 0; i < 7; i++)
@@ -56,35 +53,12 @@ print("7MB object size is : " + Object.bsonsize({_id : 0,
 
 var dataCloseTo8MB = data7MB;
 // WARNING - MAGIC NUMBERS HERE
-// The idea is to exceed the 16MB limit by just enough so that the message gets
-// passed in the
+// The idea is to just hit the 16MB limit so that the message gets passed in the
 // shell, but adding additional writeback information could fail.
-for ( var i = 0; i < 1031 * 1024 + 862; i++) {
+for ( var i = 0; i < 1024 * 1024 - 70; i++) {
     dataCloseTo8MB += "x"
 }
 
-print("Object size is: " + Object.bsonsize([{_id : 0,
-                                             d : dataCloseTo8MB},
-                                            {_id : 1,
-                                             d : dataCloseTo8MB}]))
-
-jsTest.log("Trigger wbl for mongosB...")
-
-collB.insert([{_id : 0,
-               d : dataCloseTo8MB},
-              {_id : 1,
-               d : dataCloseTo8MB}])
-
-// Will hang if overflow is not detected correctly
-jsTest.log("Waiting for GLE...")
-
-// No error thrown with new batching logic.
-assert.eq(null, collB.getDB().getLastError())
-
-print("GLE correctly returned, error not thrown.")
-
-assert.eq(5, collA.find().itcount())
-assert.eq(5, collB.find().itcount())
 
 var data8MB = "";
 for ( var i = 0; i < 8; i++) {
@@ -92,26 +66,26 @@ for ( var i = 0; i < 8; i++) {
 }
 
 print("Object size is: " + Object.bsonsize([{_id : 0,
-                                             d : data8MB},
+                                             d : dataCloseTo8MB},
                                             {_id : 1,
                                              d : data8MB}]))
 
-jsTest.log("Trigger wbl for mongosC...")
+jsTest.log("Trigger wbl for mongosB...")
 
-collC.insert([{_id : 2,
-               d : data8MB},
-              {_id : 3,
+collB.insert([{_id : 0,
+               d : dataCloseTo8MB},
+              {_id : 1,
                d : data8MB}])
 
 // Should succeed since our insert size is 16MB (plus very small overhead)
 jsTest.log("Waiting for GLE...")
 
-assert.eq(null, collC.getDB().getLastError())
+assert.eq(null, collB.getDB().getLastError())
 
 print("GLE Successful...")
 
-assert.eq(7, collA.find().itcount())
-assert.eq(7, collB.find().itcount())
-assert.eq(7, collC.find().itcount())
+// Check that the counts via both mongoses are the same
+assert.eq(4, collA.find().itcount())
+assert.eq(4, collB.find().itcount())
 
 st.stop()
