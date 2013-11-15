d316ada1bcb8caf21d79b82ff4e424186dca507f
matt dannenberg <matt.dannenberg@10gen.com>
2013-11-15 14:30:49 -0500

    SERVER-11723 a much better fix for compactPreservePadding.js

--- jstests/compactPreservePadding.js
+++ jstests/compactPreservePadding.js
@@ -1,11 +1,11 @@
 // test preservePadding
 collName = "compactPreservePadding";
-db.getSisterDB("compactPreservePaddingDB");
-db.dropDatabase();
-db.createCollection(collName);
+mydb = db.getSisterDB("compactPreservePaddingDB");
+mydb.dropDatabase();
+mydb.createCollection(collName);
 // ensure there is some padding by using power of 2 sizes
-db.runCommand({collMod: collName, usePowerOf2Sizes: true});
-t = db.compactPreservePadding;
+mydb.runCommand({collMod: collName, usePowerOf2Sizes: true});
+t = mydb.compactPreservePadding;
 t.drop();
 // populate db
 for (i = 0; i < 10000; i++) {
@@ -17,7 +17,7 @@ printjson(t.stats());
 originalSize = t.stats().size;
 originalStorage = t.stats().storageSize;
 // compact!
-db.runCommand({compact: collName, preservePadding: true});
+mydb.runCommand({compact: collName, preservePadding: true});
 printjson(t.stats());
 // object sizes ('size') should be the same (unless we hit an edge case involving extents, which
 // this test doesn't) and storage size should shrink
