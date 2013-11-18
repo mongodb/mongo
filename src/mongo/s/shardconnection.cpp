38b3b8f7395d6717efd570a62dc1c2c085d1b049
Greg Studer <greg@10gen.com>
2013-11-18 16:36:30 -0500

    SERVER-5625 don't throw on problem setting shard version on initial connect

--- src/mongo/s/shardconnection.cpp
+++ src/mongo/s/shardconnection.cpp
@@ -33,6 +33,8 @@
 
 namespace mongo {
 
+    MONGO_EXPORT_SERVER_PARAMETER(ignoreInitialVersionFailure, bool, false);
+
     DBConnectionPool shardConnectionPool;
 
     class ClientConnections;
@@ -254,7 +256,14 @@ namespace mongo {
 
                     warning() << "problem while initially checking shard versions on"
                               << " " << shard.getName() << causedBy(e) << endl;
-                    throw;
+                    
+                    if ( !ignoreInitialVersionFailure ) {
+                        throw;
+                    }
+                    else {
+                        // We swallow the error here, checking shard version here is a heuristic to
+                        // prevent later stale config exceptions, not required for correctness.
+                    }
                 }
             }
         }
