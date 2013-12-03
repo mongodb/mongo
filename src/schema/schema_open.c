f7f8ffc2937ec06087c04f2105a626b3ed1c8626
Keith Bostic <keith@wiredtiger.com>
2013-12-03 11:08:00 -0500

    whitespace

--- src/schema/schema_open.c
+++ src/schema/schema_open.c
@@ -131,14 +131,12 @@ __open_index(WT_SESSION_IMPL *session, WT_TABLE *table, WT_INDEX *idx)
 
 	/* Get the data source from the index config. */
 	WT_ERR(__wt_config_getones(session, idx->config, "source", &cval));
-	WT_ERR(__wt_buf_fmt(
-	    session, buf, "%.*s", (int)cval.len, cval.str));
+	WT_ERR(__wt_buf_fmt(session, buf, "%.*s", (int)cval.len, cval.str));
 	idx->source = __wt_buf_steal(session, buf, NULL);
 	idx->need_value = WT_PREFIX_MATCH(idx->source, "lsm:");
 
 	WT_ERR(__wt_config_getones(session, idx->config, "key_format", &cval));
-	WT_ERR(__wt_buf_fmt(
-	    session, buf, "%.*s", (int)cval.len, cval.str));
+	WT_ERR(__wt_buf_fmt(session, buf, "%.*s", (int)cval.len, cval.str));
 	idx->key_format = __wt_buf_steal(session, buf, NULL);
 
 	/*
