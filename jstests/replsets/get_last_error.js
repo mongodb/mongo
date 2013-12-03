6915fce7fa2609b8214c4fc211465d5e005f3017
Eliot Horowitz <eliot@10gen.com>
2013-12-03 11:17:54 -0500

    SERVER-11665: revert change to test for getLastError standalone w handling

--- jstests/replsets/get_last_error.js
+++ jstests/replsets/get_last_error.js
@@ -91,7 +91,14 @@ assert.eq(gle.wtime, null);
 assert.eq(gle.waited, null);
 assert.eq(gle.wtimeout, null);
 
+gle = sdb.getLastErrorObj(2, 10);
 print('Trying standalone server with w=2 and 10ms timeout.');
-assert.throws( function(){ sdb.getLastErrorObj(2, 10); } );
+printjson(gle);
+assert.eq(gle.ok, 1);
+assert.eq(gle.err, "norepl");
+assert.eq(gle.writtenTo, null);
+assert.eq(gle.wtime, null);
+assert.eq(gle.waited, null);
+assert.eq(gle.wtimeout, null);
 
 stopMongod(port);
