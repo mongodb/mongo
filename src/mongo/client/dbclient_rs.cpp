913e08fc92778efadcc0f00d975017f09bf61696
Greg Studer <greg@10gen.com>
2013-12-04 14:28:28 -0500

    SERVER-7246 rename internal dbclient_rs auth helper

--- src/mongo/client/dbclient_rs.cpp
+++ src/mongo/client/dbclient_rs.cpp
@@ -1546,7 +1546,7 @@ namespace mongo {
         return _getMonitor()->isAnyNodeOk();
     }
 
-    static bool isAuthException( const DBException& ex ) {
+    static bool isAuthenticationException( const DBException& ex ) {
         return ex.getCode() == ErrorCodes::AuthenticationFailed;
     }
 
@@ -1594,8 +1594,7 @@ namespace mongo {
             catch ( const DBException &ex ) {
 
                 // We care if we can't authenticate (i.e. bad password) in credential params.
-                // We shouldn't be unauthorized since we aren't doing anything yet.
-                if ( isAuthException( ex ) ) {
+                if ( isAuthenticationException( ex ) ) {
                     throw;
                 }
 
