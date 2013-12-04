1f0b640fb7b0f6de22fe098f12d3b05b23c1caf4
Andy Schwerin <schwerin@10gen.com>
2013-12-04 15:48:37 -0500

    SERVER-3364,SERVER-5650 Move documenation of MONGO_COMPILER macros to compiler.h.
    This deduplicates documentation, hopefully preventing the documentation from diverging.

--- src/mongo/platform/compiler.h
+++ src/mongo/platform/compiler.h
@@ -1,5 +1,3 @@
-// @file mongo/platform/compiler.h
-
 /*
  * Copyright 2012 10gen Inc.
  *
@@ -19,7 +17,60 @@
 #pragma once
 
 /**
- * Include "mongo/platform/compiler.h" to get compiler-specific macro definitions and utilities.
+ * Include "mongo/platform/compiler.h" to get compiler-targeted macro definitions and utilities.
+ *
+ * The following macros are provided in all compiler environments:
+ *
+ *
+ * MONGO_COMPILER_NORETURN
+ *
+ *   Instructs the compiler that the decorated function will not return through the normal return
+ *   path.
+ *
+ *   Correct: void MONGO_COMPILER_NORETURN myAbortFunction();
+ *
+ *
+ * MONGO_COMPILER_VARIABLE_UNUSED
+ *
+ *   Instructs the compiler not to warn if it detects no use of the decorated variable.
+ *   Typically only useful for variables that are always declared but only used in
+ *   conditionally-compiled code.
+ *
+ *   Correct: MONGO_COMPILER_VARIABLE_UNUSED int ignored;
+ *
+ *
+ * MONGO_COMPILER_ALIGN_TYPE(ALIGNMENT)
+ *
+ *   Instructs the compiler to use the given minimum alignment for the decorated type.
+ *
+ *   Alignments should probably always be powers of two.  Also, note that most allocators will not
+ *   be able to guarantee better than 16- or 32-byte alignment.
+ *
+ *   Correct:
+ *     class MONGO_COMPILER_ALIGN_TYPE(16) MyClass {...};
+ *
+ *   Incorrect:
+ *     MONGO_COMPILER_ALIGN_TYPE(16) class MyClass {...};
+ *     class MyClass{...} MONGO_COMPILER_ALIGN_TYPE(16);
+ *
+ *
+ * MONGO_COMPILER_ALIGN_VARIABLE(ALIGNMENT)
+ *
+ *   Instructs the compiler to use the given minimum alignment for the decorated variable.
+ *
+ *   Note that most allocators will not allow heap allocated alignments that are better than 16- or
+ *   32-byte aligned.  Stack allocators may only guarantee up to the natural word length worth of
+ *   alignment.
+ *
+ *   Correct:
+ *     class MyClass {
+ *         MONGO_COMPILER_ALIGN_VARIABLE(8) char a;
+ *     };
+ *
+ *     MONGO_COMPILER_ALIGN_VARIABLE(8) class MyClass {...} singletonInstance;
+ *
+ *   Incorrect:
+ *     int MONGO_COMPILER_ALIGN_VARIABLE(16) a, b;
  */
 
 #if defined(_MSC_VER)
--- src/mongo/platform/compiler_gcc.h
+++ src/mongo/platform/compiler_gcc.h
@@ -1,5 +1,3 @@
-// @file mongo/platform/compiler_gcc.h
-
 /*
  * Copyright 2012 10gen Inc.
  *
@@ -16,56 +14,18 @@
  * limitations under the License.
  */
 
-#pragma once
-
 /**
  * Compiler-specific implementations for gcc.
+ *
+ * Refer to mongo/platform/compiler.h for usage documentation.
  */
 
-/**
- * Use this to decorate the declaration of functions that will not return.
- *
- * Correct:
- *    MONGO_COMPILER_NORETURN void myAbortFunction();
- */
+#pragma once
+
 #define MONGO_COMPILER_NORETURN __attribute__((__noreturn__))
 
-/**
- * Use this to decorate unused variable declarations.
- */
 #define MONGO_COMPILER_VARIABLE_UNUSED __attribute__((__unused__))
 
-/**
- * Use this on a type declaration to specify its minimum alignment.
- *
- * Alignments should probably always be powers of two.  Also, note that most allocators will not be
- * able to guarantee better than 16- or 32-byte alignment.
- *
- * Correct:
- *    class MONGO_COMPILER_ALIGN_TYPE(16) MyClass {...};
- *
- * Incorrect:
- *    MONGO_COMPILER_ALIGN_TYPE(16) class MyClass {...};
- *    class MyClass{...} MONGO_COMPILER_ALIGN_TYPE(16);
- */
 #define MONGO_COMPILER_ALIGN_TYPE(ALIGNMENT) __attribute__(( __aligned__(ALIGNMENT) ))
 
-/**
- * Use this on a global variable or structure field declaration to specify that it must be allocated
- * at a location that is aligned to a multiple of "ALIGNMENT" bytes.
- *
- * Note that most allocators will not allow heap allocated alignments that are better than 16- or
- * 32-byte aligned.  Stack allocators may only guarantee up to the natural word length worth of
- * alignment.
- *
- * Correct:
- *    class MyClass {
- *        MONGO_COMPILER_ALIGN_VARIABLE(8) char a;
- *    };
- *
- *    MONGO_COMPILER_ALIGN_VARIABLE(8) class MyClass {...} singletonInstance;
- *
- * Incorrect:
- *    int MONGO_COMPILER_ALIGN_VARIABLE(16) a, b;
- */
 #define MONGO_COMPILER_ALIGN_VARIABLE(ALIGNMENT) __attribute__(( __aligned__(ALIGNMENT) ))
