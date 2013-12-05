32311b5f0e7a6d00cc2253d7f21397b51f0e126b
Scott Hernandez <scotthernandez@gmail.com>
2013-12-05 14:21:57 -0500

    SERVER-6124: $unset may create a nested field if it doesn't exist

--- jstests/update3.js
+++ jstests/update3.js
@@ -21,3 +21,8 @@ f.drop();
 f.save( {'_id':0} );
 f.update( {}, {$set:{'_id':5}} );
 assert.eq( 0, f.findOne()._id , "D" );
+
+f.drop();
+f.save({_id:1, a:1})
+f.update({}, {$unset:{"a":1, "b.c":1}})
+assert.docEq(f.findOne(), {_id:1}, "E")
