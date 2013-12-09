6ea28a261ff80e27cf0accaca421df3b13e3b20b
Jason Rassi <rassi@10gen.com>
2013-12-09 11:26:11 -0500

    SERVER-10857 Test: text indexes validate documents' language_override

--- jstests/fts_index.js
+++ jstests/fts_index.js
@@ -1,4 +1,6 @@
-// Test that a request to create a text index results in the expected index.
+// Test that:
+// 1. Text indexes properly validate the index spec used to create them.
+// 2. Text indexes properly enforce a schema on the language_override field.
 
 load("jstests/libs/fts.js");
 
@@ -7,26 +9,62 @@ var indexName = "textIndex";
 coll.drop();
 coll.getDB().createCollection(coll.getName());
 
-// Passes text-specific index validation.
+//
+// 1. Text indexes properly validate the index spec used to create them.
+//
+
+// Spec passes text-specific index validation.
 coll.ensureIndex({a: "text"}, {name: indexName, default_language: "spanish"});
 assert(!db.getLastError());
 assert.eq(1, coll.getDB().system.indexes.count({ns: coll.getFullName(), name: indexName}));
 coll.dropIndexes();
 
-// Fails text-specific index validation ("spanglish" unrecognized).
+// Spec fails text-specific index validation ("spanglish" unrecognized).
 coll.ensureIndex({a: "text"}, {name: indexName, default_language: "spanglish"});
 assert(db.getLastError());
 assert.eq(0, coll.system.indexes.count({ns: coll.getFullName(), name: indexName}));
 coll.dropIndexes();
 
-// Passes general index validation.
+// Spec passes general index validation.
 coll.ensureIndex({"$**": "text"}, {name: indexName});
 assert(!db.getLastError());
 assert.eq(1, coll.getDB().system.indexes.count({ns: coll.getFullName(), name: indexName}));
 coll.dropIndexes();
 
-// Fails general index validation ("a.$**" invalid field name for key).
+// Spec fails general index validation ("a.$**" invalid field name for key).
 coll.ensureIndex({"a.$**": "text"}, {name: indexName});
 assert(db.getLastError());
 assert.eq(0, coll.getDB().system.indexes.count({ns: coll.getFullName(), name: indexName}));
 coll.dropIndexes();
+
+//
+// 2. Text indexes properly enforce a schema on the language_override field.
+//
+
+// Can create a text index on a collection where no documents have invalid language_override.
+coll.insert({a: ""});
+coll.insert({a: "", language: "spanish"});
+coll.ensureIndex({a: "text"});
+assert(!db.getLastError());
+coll.drop();
+
+// Can't create a text index on a collection containing document with an invalid language_override.
+coll.insert({a: "", language: "spanglish"});
+coll.ensureIndex({a: "text"});
+assert(db.getLastError());
+coll.drop();
+
+// Can insert documents with valid language_override into text-indexed collection.
+coll.ensureIndex({a: "text"});
+assert(!db.getLastError());
+coll.insert({a: ""});
+coll.insert({a: "", language: "spanish"});
+assert(!db.getLastError());
+coll.drop();
+
+// Can't insert documents with invalid language_override into text-indexed collection.
+coll.ensureIndex({a: "text"});
+assert(!db.getLastError());
+coll.insert({a: "", language: "spanglish"});
+assert(db.getLastError());
+coll.drop();
