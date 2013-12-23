735a759908a3a8e792909eb23160ec29eba83e9c
Randolph Tan <randolph@10gen.com>
2013-12-23 16:02:37 -0500

    SERVER-12178 Make sure to take distributed ns lock when performing orphan cleanup

--- src/mongo/db/commands/cleanup_orphaned_cmd.cpp
+++ src/mongo/db/commands/cleanup_orphaned_cmd.cpp
@@ -213,6 +213,16 @@ namespace mongo {
             ChunkVersion shardVersion;
             Status status = shardingState.refreshMetadataNow( ns, &shardVersion );
             if ( !status.isOK() ) {
+                if ( status.code() == ErrorCodes::RemoteChangeDetected ) {
+                    warning() << "orphaned cleanup needs to be retried, "
+                              << "collection metadata at shard version " << shardVersion
+                              << " changed during reload" << endl;
+                    // Version changed and is not yet in steady state, return the same starting
+                    // key so the client can retry again.
+                    result.append( stoppedAtKeyField(), startingFromKey );
+                    return true;
+                }
+
                 errmsg = str::stream() << "failed to refresh shard metadata: " << status.reason();
                 return false;
             }
