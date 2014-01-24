692155f68924f19cbaae2892e56aa5edbf62fca2
Benety Goh <benety@mongodb.com>
2014-01-24 14:47:55 -0500

    SERVER-8692 additional test cases for queries against null to document existing behavior.

--- src/mongo/db/matcher/expression_leaf_test.cpp
+++ src/mongo/db/matcher/expression_leaf_test.cpp
@@ -101,6 +101,28 @@ namespace mongo {
         ASSERT( eq.matchesBSON( BSON( "b" << 4 ), NULL ) );
     }
 
+    // This test documents how the matcher currently works,
+    // not necessarily how it should work ideally.
+    TEST( EqOp, MatchesNestedNull ) {
+        BSONObj operand = BSON( "a.b" << BSONNULL );
+        EqualityMatchExpression eq;
+        eq.init( "a.b", operand[ "a.b" ] );
+        // null matches any empty object that is on a subpath of a.b
+        ASSERT( eq.matchesBSON( BSONObj(), NULL ) );
+        ASSERT( eq.matchesBSON( BSON( "a" << BSONObj() ), NULL ) );
+        ASSERT( eq.matchesBSON( BSON( "a" << BSON_ARRAY( BSONObj() ) ), NULL ) );
+        ASSERT( eq.matchesBSON( BSON( "a" << BSON( "b" << BSONNULL ) ), NULL ) );
+        // b does not exist as an element in array under a.
+        ASSERT( !eq.matchesBSON( BSON( "a" << BSONArray() ), NULL ) );
+        ASSERT( !eq.matchesBSON( BSON( "a" << BSON_ARRAY( BSONNULL ) ), NULL ) );
+        ASSERT( !eq.matchesBSON( BSON( "a" << BSON_ARRAY( 1 << 2 ) ), NULL ) );
+        // a.b exists but is not null.
+        ASSERT( !eq.matchesBSON( BSON( "a" << BSON( "b" << 4 ) ), NULL ) );
+        ASSERT( !eq.matchesBSON( BSON( "a" << BSON( "b" << BSONObj() ) ), NULL ) );
+        // A non-existent field is treated same way as an empty bson object
+        ASSERT( eq.matchesBSON( BSON( "b" << 4 ), NULL ) );
+    }
+
     TEST( EqOp, MatchesMinKey ) {
         BSONObj operand = BSON( "a" << MinKey );
         EqualityMatchExpression eq;
