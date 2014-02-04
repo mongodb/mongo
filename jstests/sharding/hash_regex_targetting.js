a51798c7d315bf111f53c5b3dd834303df5a0679
Scott Hernandez <scotthernandez@gmail.com>
2014-02-04 11:24:13 -0500

    SERVER-11430: test to keep from regression

---
+++ jstests/sharding/hash_regex_targetting.js
@@ -0,0 +1,21 @@
+// This checks to make sure that the query is untargetted when using a regex
+var st = new ShardingTest({ shards: 3});
+
+st.adminCommand({ enablesharding: "test" });
+st.adminCommand({ shardcollection: "test.server11430", key: { "path" : "hashed" } });
+
+var col = st.s.getDB('test').getCollection('server11430');
+
+var doc1 = { path: "thisisastring", val: true }
+var doc2 = { path: "thisisabigString", val: true }
+
+col.insert([doc1, doc2])
+printjson(col.find({ path : /isa/ }).explain());
+col.update({ path : /isa/ }, { "$set" : { val : false } }, {multi:true});
+var leObj = col.getDB().getLastErrorObj();
+var result  = col.findOne();
+
+assert.eq(false, result.val);
+assert.eq(2, leObj['n']);
+
+st.stop();
