4de0fdb367ee24a1551989815ac8bff58d064118
Ian Whalen <ian@mongodb.com>
2014-02-12 09:30:59 -0500

    Revert "SERVER-12554 Mimic 2.4 behaviour and lazily yield in order to"
    This reverts commit d6873ccb663a07de47738af913fd25ceff23296f.
    
    Reverting because commit broke counttests.cpp in core suite on all platforms

--- src/mongo/db/ops/count.cpp
+++ src/mongo/db/ops/count.cpp
@@ -35,7 +35,6 @@
 #include "mongo/db/catalog/database.h"
 #include "mongo/db/catalog/collection.h"
 #include "mongo/db/query/get_runner.h"
-#include "mongo/util/elapsed_tracker.h"
 
 namespace mongo {
 
@@ -95,16 +94,9 @@ namespace mongo {
         uassertStatusOK(getRunnerCount(collection, query, hintObj, &rawRunner));
         auto_ptr<Runner> runner(rawRunner);
 
-        ElapsedTracker timeToStartYielding(256, 20);
-
         try {
             const ScopedRunnerRegistration safety(runner.get());
-
-            if (timeToStartYielding.intervalHasElapsed()) {
-                // Lazily yield, avoiding a performance regression when
-                // scanning a very small number of documents.
-                runner->setYieldPolicy(Runner::YIELD_AUTO);
-            }
+            runner->setYieldPolicy(Runner::YIELD_AUTO);
 
             long long count = 0;
             Runner::RunnerState state;
