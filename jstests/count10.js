fb34a083e9ac3f50ddbdee72c5f0c029a4ba356e
David Storch <david.storch@10gen.com>
2014-02-19 17:22:42 -0500

    SERVER-12826 count10.js test improvements

--- jstests/count10.js
+++ jstests/count10.js
@@ -1,4 +1,4 @@
-// interrupting a count returns an error code
+// Test that interrupting a count returns an error code.
 
 t = db.count10;
 t.drop();
@@ -6,16 +6,22 @@ t.drop();
 for ( i=0; i<100; i++ ){
     t.save( { x : i } );
 }
-// make sure data is written
+// Make sure data is written.
 db.getLastError();
 
+// Start a parallel shell which repeatedly checks for a count
+// query using db.currentOp(). As soon as the op is found,
+// kill it via db.killOp().
 s = startParallelShell(
-    'sleep(1000); ' +
-    'current = db.currentOp({"ns": db.count10.getFullName(), "query.count": db.count10.getName()}); ' +
-    'assert(current); ' +
-    'countOp = current.inprog[0]; ' +
-    'assert(countOp); ' +
-    'db.killOp(countOp.opid); '
+    'assert.soon(function() {' +
+    '   current = db.currentOp({"ns": db.count10.getFullName(), ' +
+    '                           "query.count": db.count10.getName()}); ' +
+    '   if (!current) return false; ' +
+    '   countOp = current.inprog[0]; ' +
+    '   assert(countOp, "missing countOp"); ' +
+    '   db.killOp(countOp.opid); ' +
+    '   return true; ' +
+    '}, "could not kill count op");'
 );
 
 function getKilledCount() {
@@ -27,6 +33,9 @@ function getKilledCount() {
 }
 
 var res = getKilledCount();
+jsTest.log("killed count output start");
+printjson(res);
+jsTest.log("killed count output end");
 assert(res);
 assert(res.match(/count failed/) !== null);
 assert(res.match(/\"code\"/) !== null);
