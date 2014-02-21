5708c7a2308817e3bc775802daf1ecbd1b80f777
Ernie Hershey <ernie.hershey@10gen.com>
2014-02-21 10:34:00 -0500

    post 2.6.0-rc0

--- src/mongo/util/version.cpp
+++ src/mongo/util/version.cpp
@@ -31,7 +31,7 @@ namespace mongo {
      *      1.2.3-rc4-pre-
      * If you really need to do something else you'll need to fix _versionArray()
      */
-    const char versionString[] = "2.6.0-rc0";
+    const char versionString[] = "2.6.0-rc1-pre-";
 
     // See unit test for example outputs
     BSONArray toVersionArray(const char* version){
