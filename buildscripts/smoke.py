9d15c2e8cae042055f9d5814f792ab9b9cecad7c
Eric Milkie <milkie@10gen.com>
2014-02-25 11:33:03 -0500

    omit dbadmin.js from smalloplog suite; calls CloseAllDatabases

--- buildscripts/smoke.py
+++ buildscripts/smoke.py
@@ -201,7 +201,7 @@ class mongod(object):
         if self.kwargs.get('small_oplog_rs'):
             argv += ["--replSet", "foo", "--oplogSize", "511"]
         if self.slave:
-            argv += ['--slave', '--autoresync', '--source', 'localhost:' + str(srcport)]
+            argv += ['--slave', '--source', 'localhost:' + str(srcport)]
         if self.kwargs.get('no_journal'):
             argv += ['--nojournal']
         if self.kwargs.get('no_preallocj'):
@@ -380,7 +380,7 @@ def skipTest(path):
     parentPath = os.path.dirname(path)
     parentDir = os.path.basename(parentPath)
     if small_oplog: # For tests running in parallel
-        if basename in ["cursor8.js", "indexh.js", "dropdb.js", "connections_opened.js", "opcounters.js"]:
+        if basename in ["cursor8.js", "indexh.js", "dropdb.js", "connections_opened.js", "opcounters.js", "dbadmin.js"]:
             return True
     if use_ssl:
         # Skip tests using mongobridge since it does not support SSL
