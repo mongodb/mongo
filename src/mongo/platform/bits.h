388524c02bd76b1d3af79b8622503f87a391d2fb
Corentin Baron <corentin.baron@ext.bull.net>
2014-03-07 15:57:45 +0100

    Added PPC64 as it works as is in little endian
    Signed-off-by: Matt Kangas <matt.kangas@mongodb.com>

--- src/mongo/platform/bits.h
+++ src/mongo/platform/bits.h
@@ -19,7 +19,7 @@
 
 // figure out if we're on a 64 or 32 bit system
 
-#if defined(__x86_64__) || defined(__amd64__) || defined(_WIN64) || defined(__aarch64__)
+#if defined(__x86_64__) || defined(__amd64__) || defined(_WIN64) || defined(__aarch64__) || defined(__powerpc64__)
 #define MONGO_PLATFORM_64
 #elif defined(__i386__) || defined(_WIN32) || defined(__arm__)
 #define MONGO_PLATFORM_32
