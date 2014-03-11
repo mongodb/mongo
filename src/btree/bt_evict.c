2bad6fba854f95dc63da312396d976bd2a20ad42
Michael Cahill <michael.cahill@wiredtiger.com>
2014-03-11 22:02:04 +1100

    If a page is awaiting forced eviction, and LRU eviction fails, don't bump the read generation.

--- src/btree/bt_evict.c
+++ src/btree/bt_evict.c
@@ -1241,7 +1241,8 @@ __wt_evict_lru_page(WT_SESSION_IMPL *session, int is_app)
 	 * the page and some other thread may have evicted it by the time we
 	 * look at it.
 	 */
-	page->read_gen = __wt_cache_read_gen_set(session);
+	if (page->read_gen != WT_READGEN_OLDEST)
+		page->read_gen = __wt_cache_read_gen_set(session);
 
 	WT_WITH_BTREE(session, btree, ret = __wt_evict_page(session, page));
 
