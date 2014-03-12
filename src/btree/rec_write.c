80aba6386b3c792ace13812a4b25db4617b44cb3
Keith Bostic <keith@wiredtiger.com>
2014-03-12 09:44:17 -0400

    add a comment about why only child-modified is the expected state for column-store child pages.

--- src/btree/rec_write.c
+++ src/btree/rec_write.c
@@ -2523,7 +2523,14 @@ __rec_col_int(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_PAGE *page)
 		addr = NULL;
 		child = ref->page;
 		if (state != 0) {
+			/*
+			 * Currently the only non-zero returned stated possible
+			 * for a column-store page is child-modified (all other
+			 * states are part of the fast-truncate support, which
+			 * is row-store only).
+			 */
 			WT_ASSERT(session, state == WT_CHILD_MODIFIED);
+
 			switch (F_ISSET(child->modify, WT_PM_REC_MASK)) {
 			case WT_PM_REC_EMPTY:
 				/*
