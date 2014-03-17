925d3c57e1c7af4566b989a760d6cfa15bdf3437
Randolph Tan <randolph@10gen.com>
2014-03-17 14:18:40 -0400

    SERVER-13234 Remove timeout from cloneDb.js test

--- jstests/replsets/cloneDb.js
+++ jstests/replsets/cloneDb.js
@@ -17,10 +17,11 @@ doTest = function( signal ) {
     var db1 = master.getDB('test')
     
     print("Insert data");
+    var bulk = db1['foo'].initializeUnorderedBulkOp();
     for (var i = 0; i < N; i++) {
-        var option = { writeConcern: { w: 2, wtimeout: 1000}};
-        assert.writeOK(db1['foo'].insert({ x: i, text: Text }, option));
+        bulk.insert({ x: i, text: Text });
     }
+    assert.writeOK(bulk.execute());
     
     print("Create single server");
     var solo = new Server ('singleTarget')
@@ -38,9 +39,12 @@ doTest = function( signal ) {
     print("Now test the reverse direction");
     db1 = master.getDB('test2')
     db2 = soloConn.getDB('test2')
+
+    bulk = db2['foo'].initializeUnorderedBulkOp();
     for (var i = 0; i < N; i++) {
-        db2['foo'].insert({x: i, text: Text})
+        bulk.insert({ x: i, text: Text });
     }
+    assert.writeOK(bulk.execute());
     db1.cloneDatabase (solo.host())
     assert.eq (Text, db2['foo'] .findOne({x: N-1}) ['text'], 'cloneDatabase failed (test2)')
 
