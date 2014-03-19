4e6f037f56707a8a7a81e556ae95cc4ff9ad3782
Wisdom Omuya <deafgoat@gmail.com>
2014-03-19 18:22:01 -0400

    MCI-13287: delegate to shell when compressing tar archives

--- buildscripts/make_archive.py
+++ buildscripts/make_archive.py
@@ -29,16 +29,86 @@ For a detailed usage example, see src/SConscript.client or src/mongo/SConscript.
 import optparse
 import os
 import sys
+import shutil
+import zipfile
+from subprocess import (Popen,
+                        PIPE,
+                        STDOUT)
 
 def main(argv):
     opts = parse_options(argv[1:])
-    archive = open_archive_for_write(opts.output_filename, opts.archive_format)
+    if opts.archive_format in ('tar', 'tgz'):
+        make_tar_archive(opts)
+    elif opts.archive_format in ('zip'):
+        make_zip_archive(opts)
+    else:
+        raise ValueError('Unsupported archive format "%s"' % opts.archive_format)
+
+def make_tar_archive(opts):
+    '''Given the parsed options, generates the 'opt.output_filename'
+    tarball containing all the files in 'opt.input_filename' renamed
+    according to the mappings in 'opts.transformations'.
+
+    e.g. for an input file named "a/mongo/build/DISTSRC", and an
+    existing transformation {"a/mongo/build": "release"}, the input
+    file will be written to the tarball as "release/DISTSRC"
+
+    All files to be compressed are copied into new directories as
+    required by 'opts.transformations'. Once the tarball has been
+    created, all temporary directory structures created for the
+    purposes of compressing, are removed.
+    '''
+    tar_options = "cvf"
+    if opts.archive_format is 'tgz':
+        tar_options += "z"
+    created_directories = []
+    tar_command = ["tar", tar_options, opts.output_filename]
+    for input_filename in opts.input_filenames:
+        preferred_filename = get_preferred_filename(input_filename, opts.transformations)
+        tar_command.append(preferred_filename)
+        print "copying %s => %s" % (input_filename, preferred_filename)
+        enclosing_directory = os.path.dirname(preferred_filename)
+        if not os.path.exists(enclosing_directory):
+            os.makedirs(enclosing_directory)
+            created_directories.append(enclosing_directory)
+        shutil.copyfile(input_filename, preferred_filename)
+
+    # execute the full tar command
+    proc = Popen(tar_command, stdout=PIPE, stderr=STDOUT, bufsize=0)
+    proc.wait()
+
+    # clean up temp directories/files created
+    for input_filename in opts.input_filenames:
+        preferred_filename = get_preferred_filename(input_filename, opts.transformations)
+        # don't delete the original file
+        if input_filename is preferred_filename:
+            continue
+        enclosing_directory = os.path.dirname(preferred_filename)
+        try:
+            # only delete enclosing directory if we created it
+            if enclosing_directory in created_directories:
+                shutil.rmtree(enclosing_directory)
+            else:
+                os.remove(preferred_filename)
+        except Exception:
+            pass
+
+def make_zip_archive(opts):
+    '''Given the parsed options, generates the 'opt.output_filename'
+    zipfile containing all the files in 'opt.input_filename' renamed
+    according to the mappings in 'opts.transformations'.
+
+    All files in 'opt.output_filename' are renamed before being
+    written into the zipfile.
+    '''
+    archive = open_zip_archive_for_write(opts.output_filename)
     try:
         for input_filename in opts.input_filenames:
             archive.add(input_filename, arcname=get_preferred_filename(input_filename,
-                                                                       opts.transformations))
+            opts.transformations))
     finally:
         archive.close()
+
 
 def parse_options(args):
     parser = optparse.OptionParser()
@@ -82,30 +152,21 @@ def parse_options(args):
 
     return opts
 
-def open_archive_for_write(filename, archive_format):
-    '''Open a tar or zip archive for write, with the given format, and return it.
-
-    The type of archive is determined by the "archive_format" parameter, which should be
-    "tar", "tgz" (for gzipped tar) or "zip".
+def open_zip_archive_for_write(filename):
+    '''Open a zip archive for writing and return it.
     '''
-
-    if archive_format in ('tar', 'tgz'):
-        import tarfile
-        mode = 'w'
-        if archive_format is 'tgz':
-            mode += '|gz'
-        return tarfile.open(filename, mode)
-    if archive_format is 'zip':
-        import zipfile
-        # Infuriatingly, Zipfile calls the "add" method "write", but they're otherwise identical,
-        # for our purposes.  WrappedZipFile is a minimal adapter class.
-        class WrappedZipFile(zipfile.ZipFile):
-            def add(self, filename, arcname):
-                return self.write(filename, arcname)
-        return WrappedZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
-    raise ValueError('Unsupported archive format "%s"' % archive_format)
+    # Infuriatingly, Zipfile calls the "add" method "write", but they're otherwise identical,
+    # for our purposes.  WrappedZipFile is a minimal adapter class.
+    class WrappedZipFile(zipfile.ZipFile):
+        def add(self, filename, arcname):
+            return self.write(filename, arcname)
+    return WrappedZipFile(filename, 'w', zipfile.ZIP_DEFLATED)
 
 def get_preferred_filename(input_filename, transformations):
+    '''Does a prefix subsitution on 'input_filename' for the
+    first matching transformation in 'transformations' and
+    returns the substituted string
+    '''
     for match, replace in transformations:
         if input_filename.startswith(match):
             return replace + input_filename[len(match):]
