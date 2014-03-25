57f97f213346b7a0305c81fd70894a77ca35a886
Scott Hernandez <scotthernandez@gmail.com>
2014-03-25 15:29:54 -0400

    SERVER-13297: remove dead jstests

--- jstests/set_param1.js
+++
@@ -1,9 +0,0 @@
-
-old = db.adminCommand( { "getParameter" : "*" } )
-tmp1 = db.adminCommand( { "setParameter" : 1 , "logLevel" : 5 } )
-tmp2 = db.adminCommand( { "setParameter" : 1 , "logLevel" : old.logLevel } )
-now = db.adminCommand( { "getParameter" : "*" } )
-
-assert.eq( old , now , "A" )
-assert.eq( old.logLevel , tmp1.was , "B" )
-assert.eq( 5 , tmp2.was , "C" )
