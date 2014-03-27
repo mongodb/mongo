7f6bf41feeccd2a2ba4f191277a64221231c73e2
Shaun Verch <shaun.verch@mongodb.com>
2014-03-27 18:03:45 -0400

    SERVER-13379 Do not mark Environment as valid before canonicalization is done in mongos (cherry picked from commit 9265798fae9f784bff50755260c9536e15ceb7d7)

--- src/mongo/s/mongos_options_init.cpp
+++ src/mongo/s/mongos_options_init.cpp
@@ -40,7 +40,9 @@ namespace mongo {
         if (!handlePreValidationMongosOptions(moe::startupOptionsParsed, context->args())) {
             ::_exit(EXIT_SUCCESS);
         }
-        Status ret = moe::startupOptionsParsed.validate();
+        // Run validation, but tell the Environment that we don't want it to be set as "valid",
+        // since we may be making it invalid in the canonicalization process.
+        Status ret = moe::startupOptionsParsed.validate(false/*setValid*/);
         if (!ret.isOK()) {
             return ret;
         }
@@ -49,6 +51,10 @@ namespace mongo {
             return ret;
         }
         ret = canonicalizeMongosOptions(&moe::startupOptionsParsed);
+        if (!ret.isOK()) {
+            return ret;
+        }
+        ret = moe::startupOptionsParsed.validate();
         if (!ret.isOK()) {
             return ret;
         }
