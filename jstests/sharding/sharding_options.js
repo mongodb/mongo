ce4afbc1c211559b33f7fd2145cd7b4120ce9113
Shaun Verch <shaun.verch@mongodb.com>
2014-03-28 09:41:15 -0400

    SERVER-13379 Canonicalize moveParanoia and noMoveParanoia as sharding.archiveMovedChunks (cherry picked from commit 3973a76ece2b85b8258831ae7dae97e8e3da8eec)

---
+++ jstests/sharding/sharding_options.js
@@ -0,0 +1,71 @@
+var baseName = "jstests_sharding_sharding_options";
+
+function removeOptionsAddedByFramework(getCmdLineOptsResult) {
+    // Remove options that we are not interested in checking, but that get set by the test
+    delete getCmdLineOptsResult.parsed.setParameter
+    delete getCmdLineOptsResult.parsed.storage
+    delete getCmdLineOptsResult.parsed.net
+    delete getCmdLineOptsResult.parsed.fastsync
+    delete getCmdLineOptsResult.parsed.security
+    return getCmdLineOptsResult;
+}
+
+function testGetCmdLineOpts(mongoRunnerConfig, expectedResult) {
+
+    // Start mongod with options
+    var mongod = MongoRunner.runMongod(mongoRunnerConfig);
+
+    // Get the parsed options
+    var getCmdLineOptsResult = mongod.adminCommand("getCmdLineOpts");
+    printjson(getCmdLineOptsResult);
+
+    // Remove options added by the test framework
+    getCmdLineOptsResult = removeOptionsAddedByFramework(getCmdLineOptsResult);
+
+    // Make sure the options are equal to what we expect
+    assert.docEq(getCmdLineOptsResult.parsed, expectedResult.parsed);
+
+    // Cleanup
+    MongoRunner.stopMongod(mongod.port);
+}
+
+jsTest.log("Testing \"moveParanoia\" command line option");
+var expectedResult = {
+    "parsed" : {
+        "sharding" : {
+            "archiveMovedChunks" : true
+        }
+    }
+};
+testGetCmdLineOpts({ moveParanoia : "" }, expectedResult);
+
+jsTest.log("Testing \"noMoveParanoia\" command line option");
+expectedResult = {
+    "parsed" : {
+        "sharding" : {
+            "archiveMovedChunks" : false
+        }
+    }
+};
+testGetCmdLineOpts({ noMoveParanoia : "" }, expectedResult);
+
+jsTest.log("Testing \"sharding.archiveMovedChunks\" config file option");
+expectedResult = {
+    "parsed" : {
+        "config" : "jstests/libs/config_files/enable_paranoia.json",
+        "sharding" : {
+            "archiveMovedChunks" : true
+        }
+    }
+};
+testGetCmdLineOpts({ config : "jstests/libs/config_files/enable_paranoia.json" }, expectedResult);
+
+jsTest.log("Testing with no explicit sharding option setting");
+expectedResult = {
+    "parsed" : { }
+};
+testGetCmdLineOpts({}, expectedResult);
+
+
+
+print(baseName + " succeeded.");
