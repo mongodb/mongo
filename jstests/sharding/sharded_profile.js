7ccb298ff2790f4d4b5e844e4124f9051a4ec437
Greg Studer <greg@10gen.com>
2014-03-31 11:10:12 -0400

    SERVER-13413 profiling should not do version checks against op namespace

---
+++ jstests/sharding/sharded_profile.js
@@ -0,0 +1,39 @@
+//
+// Tests whether profiling can trigger stale config errors and interfere with write batches
+// SERVER-13413
+//
+
+var options = { separateConfig : true };
+
+var st = new ShardingTest({ shards : 1, mongos : 2, other : options });
+st.stopBalancer();
+
+var mongos = st.s0;
+var admin = mongos.getDB( "admin" );
+var shards = mongos.getCollection( "config.shards" ).find().toArray();
+var coll = mongos.getCollection( "foo.bar" );
+
+assert( admin.runCommand({ enableSharding : coll.getDB() + "" }).ok );
+assert( admin.runCommand({ shardCollection : coll + "", key : { _id : 1 } }).ok );
+
+st.printShardingStatus();
+
+jsTest.log( "Turning on profiling..." );
+
+st.shard0.getDB(coll.getDB().toString()).setProfilingLevel(2);
+var profileColl = st.shard0.getDB(coll.getDB().toString()).system.profile;
+
+var inserts = [{ _id : 0 }, { _id : 1 }, { _id : 2 }];
+var staleColl = st.s1.getCollection(coll.toString());
+
+staleColl.insert(inserts);
+assert.gleOK(staleColl.getDB().getLastErrorObj());
+
+printjson(profileColl.find().toArray());
+
+for (var i = 0; i < inserts.length; i++) {
+    assert.neq(null, profileColl.findOne({ 'query._id' : i }));
+}
+
+jsTest.log( "DONE!" );
+st.stop();
