32fb2dc80747aaf2de1bf315493d8d8a5d3fc99b
Mathias Stearn <mathias@10gen.com>
2014-04-23 12:14:53 -0400

    SERVER-13630 Enhance OwnedPointerVector (cherry picked from commit daa7222fe86201e487f7b7d0c74bc97ecfd664cb)

--- src/mongo/base/owned_pointer_vector.h
+++ src/mongo/base/owned_pointer_vector.h
@@ -15,6 +15,7 @@
 
 #pragma once
 
+#include <cstring>
 #include <vector>
 
 #include "mongo/base/disallow_copying.h"
@@ -31,8 +32,24 @@ namespace mongo {
         MONGO_DISALLOW_COPYING(OwnedPointerVector);
 
     public:
-        OwnedPointerVector();
-        ~OwnedPointerVector();
+        OwnedPointerVector() {}
+        ~OwnedPointerVector() { clear(); }
+
+        /**
+         * Takes ownership of all pointers contained in 'other'.
+         * NOTE: argument is intentionally taken by value.
+         */
+        OwnedPointerVector(std::vector<T*> other) { _vector.swap(other); }
+
+        /**
+         * Takes ownership of all pointers contained in 'other'.
+         * NOTE: argument is intentionally taken by value.
+         */
+        OwnedPointerVector& operator=(std::vector<T*> other) {
+            clear();
+            _vector.swap(other);
+            return *this;
+        }
 
         typedef typename std::vector<T*>::const_iterator const_iterator;
         typedef typename std::vector<T*>::const_reverse_iterator const_reverse_iterator;
@@ -49,23 +66,69 @@ namespace mongo {
         const_iterator end() const { return _vector.end(); }
         const_reverse_iterator rend() const { return _vector.rend(); }
 
+        T* operator[] (size_t i) const { return _vector[i]; }
+        T* back() const { return _vector.back(); }
+        T* front() const { return _vector.front(); }
+
+        void push_back(T* ptr) { _vector.push_back(ptr); }
+
+        /**
+         * Deletes all pointers in the vector, then sets its size to 0.
+         */
         void clear();
+
+        /**
+         * Deletes the pointer at 'it', then erases it from the vector.
+         */
+        void erase(const_iterator it) {
+            delete *it;
+            // vector::erase(const_iterator) is new in c++11, so converting to non-const iterator.
+            _vector.erase(_vector.begin() + (it - begin()));
+        }
+
+        //
+        // extensions
+        //
+
+        /**
+         * Releases the entire vector to allow you to transfer ownership.
+         *
+         * Leaves the OwnedPointerVector empty.
+         * Named after the similar method and pattern in std::auto_ptr.
+         */
+        std::vector<T*> release() {
+            std::vector<T*> out;
+            out.swap(_vector);
+            return out;
+        }
+
+        /**
+         * Releases ownership of a single element.
+         *
+         * Sets that element to NULL and does not change size().
+         */
+        T* releaseAt(size_t i) {
+            T* out = _vector[i];
+            _vector[i] = NULL;
+            return out;
+        }
+
+        T* popAndReleaseBack() {
+            T* out = _vector.back();
+            _vector.pop_back();
+            return out;
+        }
+
+        void popAndDeleteBack() {
+            delete popAndReleaseBack();
+        }
 
     private:
         std::vector<T*> _vector;
     };
 
     template<class T>
-    OwnedPointerVector<T>::OwnedPointerVector() {
-    }
-
-    template<class T>
-    OwnedPointerVector<T>::~OwnedPointerVector() {
-        clear();
-    }
-
-    template<class T>
-    void OwnedPointerVector<T>::clear() {
+    inline void OwnedPointerVector<T>::clear() {
         for( typename std::vector<T*>::iterator i = _vector.begin(); i != _vector.end(); ++i ) {
             delete *i;
         }
