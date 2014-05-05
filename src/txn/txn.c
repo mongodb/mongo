5accae2eabc58826be6ea59460a81a7a4c541d29
Michael Cahill <michael.cahill@wiredtiger.com>
2014-05-05 17:53:13 +1000

    Make sure changes performed by the checkpoint transaction are logged.
    refs #972, #979

--- src/txn/txn.c
+++ src/txn/txn.c
@@ -267,7 +267,7 @@ __wt_txn_release(WT_SESSION_IMPL *session)
 	WT_TXN_STATE *txn_state;
 
 	txn = &session->txn;
-	txn->mod_count = 0;
+	WT_ASSERT(session, txn->mod_count == 0);
 	txn->notify = NULL;
 
 	txn_global = &S2C(session)->txn_global;
@@ -341,6 +341,7 @@ __wt_txn_commit(WT_SESSION_IMPL *session, const char *cfg[])
 	/* Free memory associated with updates. */
 	for (i = 0, op = txn->mod; i < txn->mod_count; i++, op++)
 		__wt_txn_op_free(session, op);
+	txn->mod_count = 0;
 
 	/*
 	 * Auto-commit transactions need a new transaction snapshot so that the
@@ -350,7 +351,9 @@ __wt_txn_commit(WT_SESSION_IMPL *session, const char *cfg[])
 	 * the cursor.  Get the new snapshot before releasing the ID for the
 	 * commit.
 	 */
-	if (session->ncursors > 0 && txn->isolation != TXN_ISO_READ_UNCOMMITTED)
+	if (session->ncursors > 0 &&
+	    F_ISSET(txn, TXN_HAS_ID) &&
+	    txn->isolation != TXN_ISO_READ_UNCOMMITTED)
 		__wt_txn_refresh(session, txn->id + 1, 1);
 	__wt_txn_release(session);
 	return (0);
@@ -403,6 +406,7 @@ __wt_txn_rollback(WT_SESSION_IMPL *session, const char *cfg[])
 		/* Free any memory allocated for the operation. */
 		__wt_txn_op_free(session, op);
 	}
+	txn->mod_count = 0;
 
 	__wt_txn_release(session);
 	return (ret);
