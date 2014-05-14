7642a99ca74d9375fdda93d9f87f2216ce99253e
Susan LoVerso <sue@wiredtiger.com>
2014-05-14 13:51:05 -0400

    Typo.

--- dist/api_data.py
+++ dist/api_data.py
@@ -481,7 +481,7 @@ methods = {
 	    configure the cursor for dump format inputs and outputs: "hex"
 	    selects a simple hexadecimal format, "json" selects a JSON format
 	    with each record formats as fields named by column names if
-	    availabe, and "print" selects a format where only non-printing
+	    available, and "print" selects a format where only non-printing
 	    characters are hexadecimal encoded, and "json" produces a JSON
 	    encoding of the data.  The "hex" and "print" dump format are
 	    compatible with the @ref util_dump and @ref util_load commands''',
