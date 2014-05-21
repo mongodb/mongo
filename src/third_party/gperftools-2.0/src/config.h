34a354ecca4b14cd6b3048f77a79e6b81bc7ddd3
Benety Goh <benety@mongodb.com>
2014-05-21 13:56:48 -0400

    SERVER-8995 removed gperftools-2.0 from src/third_party

--- src/third_party/gperftools-2.0/src/config.h
+++
@@ -1,7 +0,0 @@
-#if defined(__linux__)
-#include "config-10gen-linux.h"
-#elif defined(__APPLE__)
-#include "config-10gen-macos.h"
-#else
-#error "In tree version of TCMalloc not supported on this platform!"
-#endif
