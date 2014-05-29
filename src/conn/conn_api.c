8efc48e5b9f567635c355f79edbd55ac11f393ee
Susan LoVerso <sue@wiredtiger.com>
2014-05-29 12:00:45 -0400

    Rename back to .basecfg.  Update ordering in docs. #1010

--- src/conn/conn_api.c
+++ src/conn/conn_api.c
@@ -7,7 +7,7 @@
 
 #include "wt_internal.h"
 
-#define	WT_BASECONFIG	"WiredTiger.createcfg"
+#define	WT_BASECONFIG	"WiredTiger.basecfg"
 #define	WT_USERCONFIG	"WiredTiger.config"
 
 static int __conn_statistics_config(WT_SESSION_IMPL *, const char *[]);
