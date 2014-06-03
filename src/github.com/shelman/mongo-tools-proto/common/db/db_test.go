ed2f83c0163ad22853c8742e730e00f90559663c
Sam Helman <sam.helman@10gen.com>
2014-06-03 14:58:47 -0400

    Tests for session provider
    Former-commit-id: 595317379ec2dd6b8a14de6455cadac979544d18

--- src/github.com/shelman/mongo-tools-proto/common/db/db_test.go
+++ src/github.com/shelman/mongo-tools-proto/common/db/db_test.go
@@ -1,9 +1,56 @@
 package db
 
 import (
+	"github.com/shelman/mongo-tools-proto/common/options"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
 )
+
+func TestInitSessionProvider(t *testing.T) {
+
+	Convey("When initializing a session provider", t, func() {
+
+		Convey("the host and port specified should propagate to the"+
+			" provider's dial info", func() {
+
+			opts := options.New("", "", "")
+			opts.Connection = &options.Connection{
+				Host: "host1,host2",
+				Port: "20000",
+			}
+			provider, err := InitSessionProvider(opts)
+			So(err, ShouldBeNil)
+			So(provider.dialInfo.Addrs, ShouldResemble, []string{"host1:20000",
+				"host2:20000"})
+
+		})
+
+		Convey("if ssl is not specified, no dialer function should be"+
+			" created", func() {
+
+			opts := options.New("", "", "")
+			provider, err := InitSessionProvider(opts)
+			So(err, ShouldBeNil)
+			So(provider.dialInfo.DialServer, ShouldBeNil)
+
+		})
+
+		Convey("if ssl is specified, a dialer function to use ssl should"+
+			" be created and used by the provider", func() {
+
+			opts := options.New("", "", "")
+			opts.SSL = &options.SSL{
+				UseSSL: true,
+			}
+			provider, err := InitSessionProvider(opts)
+			So(err, ShouldBeNil)
+			So(provider.dialInfo.DialServer, ShouldNotBeNil)
+
+		})
+
+	})
+
+}
 
 func TestCreateConnectionAddrs(t *testing.T) {
 
