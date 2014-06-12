a6793da36b0026c9b0ef50228f40472b31b5c76d
Jason Rassi <rassi@10gen.com>
2014-06-12 17:13:43 -0400

    SERVER-14048 mongodump can't use exhaust mode with mongos

--- src/mongo/tools/dump.cpp
+++ src/mongo/tools/dump.cpp
@@ -381,7 +381,7 @@ public:
     }
 
     int run() {
-        bool usingMongos = false;
+        bool usingMongos = isMongos();
         int serverAuthzVersion = 0;
         BSONObj dumpQuery;
 
@@ -449,8 +449,6 @@ public:
                 return -1;
             }
         }
-
-        usingMongos = isMongos();
 
         boost::filesystem::path root(mongoDumpGlobalParams.outputDirectory);
 
