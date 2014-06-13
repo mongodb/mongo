a8a4412dcd0602b35744713ff0c8bb4d2b012e93
Sam Helman <sam.helman@10gen.com>
2014-06-13 11:06:19 -0400

    Added test for math.go
    Former-commit-id: ad0163c9cd89930f34e4c2bc36a39abb8ac60b3c

---
+++ src/github.com/shelman/mongo-tools-proto/common/util/math_test.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"github.com/shelman/mongo-tools-proto/common/testutil"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestMaxInt(t *testing.T) {
+
+	testutil.VerifyTestType(t, "unit")
+
+	Convey("When finding the maximum of two ints", t, func() {
+
+		Convey("the larger int should be returned", func() {
+
+			So(MaxInt(1, 2), ShouldEqual, 2)
+			So(MaxInt(2, 1), ShouldEqual, 2)
+
+		})
+
+	})
+}
