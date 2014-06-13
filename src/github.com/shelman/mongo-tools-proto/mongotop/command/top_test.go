a76dd9209e831620ed20ead48c85bca9ba4df60b
Sam Helman <sam.helman@10gen.com>
2014-06-13 11:49:53 -0400

    Added tests for top diffing
    Former-commit-id: bf5220641fad41806a7b7fe4eb7a640e84d0a335

---
+++ src/github.com/shelman/mongo-tools-proto/mongotop/command/top_test.go
@@ -0,0 +1,113 @@
+package command
+
+import (
+	"github.com/shelman/mongo-tools-proto/common/testutil"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestTopDiffToRows(t *testing.T) {
+
+	testutil.VerifyTestType(t, "unit")
+
+	Convey("When converting a TopDiff to rows to be printed", t, func() {
+
+		var diff *TopDiff
+
+		Convey("the first row should contain the appropriate column"+
+			" headers", func() {
+
+			diff = &TopDiff{
+				Totals: map[string][]int{},
+			}
+
+			rows := diff.ToRows()
+			So(len(rows), ShouldEqual, 1)
+			headerRow := rows[0]
+			So(len(headerRow), ShouldEqual, 5)
+			So(headerRow[0], ShouldEqual, "ns")
+			So(headerRow[1], ShouldEqual, "total")
+			So(headerRow[2], ShouldEqual, "read")
+			So(headerRow[3], ShouldEqual, "write")
+
+		})
+
+		Convey("the subsequent rows should contain the correct totals from"+
+			" the diff", func() {
+
+			diff = &TopDiff{
+				Totals: map[string][]int{
+					"a.b": []int{0, 1000, 2000},
+					"c.d": []int{2000, 1000, 0},
+				},
+			}
+
+			rows := diff.ToRows()
+			So(len(rows), ShouldEqual, 3)
+			So(rows[1][0], ShouldEqual, "a.b")
+			So(rows[1][1], ShouldEqual, "0ms")
+			So(rows[1][2], ShouldEqual, "1ms")
+			So(rows[1][3], ShouldEqual, "2ms")
+			So(rows[2][0], ShouldEqual, "c.d")
+			So(rows[2][1], ShouldEqual, "2ms")
+			So(rows[2][2], ShouldEqual, "1ms")
+			So(rows[2][3], ShouldEqual, "0ms")
+
+		})
+
+		Convey("the namespaces should appear in alphabetical order", func() {
+
+			diff = &TopDiff{
+				Totals: map[string][]int{
+					"a.b": []int{},
+					"a.c": []int{},
+					"a.a": []int{},
+				},
+			}
+
+			rows := diff.ToRows()
+			So(len(rows), ShouldEqual, 4)
+			So(rows[1][0], ShouldEqual, "a.a")
+			So(rows[2][0], ShouldEqual, "a.b")
+			So(rows[3][0], ShouldEqual, "a.c")
+
+		})
+
+		Convey("any negative values should be capped to 0", func() {
+
+			diff = &TopDiff{
+				Totals: map[string][]int{
+					"a.b": []int{-1000, 5000, -3000},
+				},
+			}
+
+			rows := diff.ToRows()
+			So(len(rows), ShouldEqual, 2)
+			So(rows[1][1], ShouldEqual, "0ms")
+			So(rows[1][2], ShouldEqual, "5ms")
+			So(rows[1][3], ShouldEqual, "0ms")
+
+		})
+
+		Convey("any namespaces that are just a database, are from the local"+
+			"database, or are a collection of namespaces should be"+
+			" skipped", func() {
+
+			diff := &TopDiff{
+				Totals: map[string][]int{
+					"a.b":          []int{},
+					"local.b":      []int{},
+					"a.namespaces": []int{},
+					"a":            []int{},
+				},
+			}
+
+			rows := diff.ToRows()
+			So(len(rows), ShouldEqual, 2)
+			So(rows[1][0], ShouldEqual, "a.b")
+
+		})
+
+	})
+
+}
