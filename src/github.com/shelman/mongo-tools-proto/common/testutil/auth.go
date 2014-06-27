f6ff5a3611c74a902fc4b36ab14b00c238b0937e
Sam Helman <sam.helman@10gen.com>
2014-06-27 10:45:43 -0400

    Update comment
    Former-commit-id: c973d3cb021392ba2fc89abdf11c686e535a84c5

--- src/github.com/shelman/mongo-tools-proto/common/testutil/auth.go
+++ src/github.com/shelman/mongo-tools-proto/common/testutil/auth.go
@@ -11,13 +11,18 @@ var (
 	UserAdminPassword = "password"
 )
 
-// Initialize a user admin for the specified server.  Assumes that there are
-// no existing users, otherwise will fail with a permissions issue.
+// Initialize a user admin, using the already-connected session passed in.
+// Assumes that there are no existing users, otherwise will fail with a
+// permissions issue.
 func CreateUserAdmin(t *testing.T, session *mgo.Session) {
 	CreateUserWithRole(t, session, UserAdmin, UserAdminPassword,
 		mgo.RoleUserAdminAny, false)
 }
 
+// Create a user with the specified password and role, using the
+// already-connected session passed in.  If needsLogin is true, then the
+// default user admin and password will be used to log in to the admin
+// db before creating the user.
 func CreateUserWithRole(t *testing.T, session *mgo.Session, user,
 	password string, role mgo.Role, needsLogin bool) {
 
