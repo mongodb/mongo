8f071ee21b4763f0c88832a422865ec078940631
Spencer T Brody <spencer@mongodb.com>
2014-07-03 15:53:17 -0400

    SERVER-14355 Allow dbAdmin role to create system.profile collection with custom size

--- jstests/auth/profile_access.js
+++ jstests/auth/profile_access.js
@@ -9,7 +9,16 @@ adminDb.createUser({user:'admin',
 
 adminDb.auth('admin','password');
 testDb.createUser({user:'readUser',pwd:'password',roles:['read']});
+testDb.createUser({user:'dbAdminUser',pwd:'password',roles:['dbAdmin']});
 testDb.setProfilingLevel(2);
+testDb.foo.findOne();
 adminDb.logout();
 testDb.auth('readUser','password');
 assert.throws(function() { testDb.system.profile.findOne(); });
+testDb.logout();
+
+// SERVER-14355
+testDb.auth('dbAdminUser','password');
+testDb.setProfilingLevel(0);
+testDb.system.profile.drop();
+assert.commandWorked(testDb.createCollection("system.profile", {capped: true, size: 1024}));
