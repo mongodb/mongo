580ca36cdbf205a86400479ad0219e0cccd01a6d
Ernie Hershey <ernie.hershey@10gen.com>
2014-07-25 23:00:12 -0400

    post 2.7.4

--- rpm/mongo.mdv.spec
+++ rpm/mongo.mdv.spec
@@ -1,5 +1,5 @@
 %define name    mongodb
-%define version 2.7.4
+%define version 2.7.5-pre-
 %define release %mkrel 1
 
 Name:    %{name}
--- src/mongo/util/version.cpp
+++ src/mongo/util/version.cpp
@@ -43,7 +43,7 @@ namespace mongo {
      *      1.2.3-rc4-pre-
      * If you really need to do something else you'll need to fix _versionArray()
      */
-    const char versionString[] = "2.7.4";
+    const char versionString[] = "2.7.5-pre-";
 
     // See unit test for example outputs
     BSONArray toVersionArray(const char* version){
