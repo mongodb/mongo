02948ba65855df43bfbdee5c4e0f834c678532b7
Eliot Horowitz <eliot@10gen.com>
2014-07-30 12:53:14 -0400

    SERVER-13635: make some capped test not use system.

--- jstests/core/capped5.js
+++ jstests/core/capped5.js
@@ -20,7 +20,7 @@ t.drop();
 db.createCollection( tn , {capped: true, size: 1024 * 1024 * 1 } );
 t.insert( { _id : 5 , x : 11 } );
 t.insert( { _id : 5 , x : 12 } );
-assert.eq( 1, db.system.indexes.count( {ns:"test."+tn} ) ); //now we assume _id index
+assert.eq( 1, t.getIndexes().length ); //now we assume _id index
 assert.eq( 1, t.find().toArray().length ); //_id index unique, so second insert fails
 
 t.drop();
@@ -28,7 +28,7 @@ db.createCollection( tn , {capped: true, size: 1024 * 1024 * 1 } );
 t.insert( { _id : 5 , x : 11 } );
 t.insert( { _id : 6 , x : 12 } );
 t.ensureIndex( { x:1 }, {unique:true} );
-assert.eq( 2, db.system.indexes.count( {ns:"test."+tn} ) ); //now we assume _id index
+assert.eq( 2, t.getIndexes().length ); //now we assume _id index
 assert.eq( 2, t.find().hint( {x:1} ).toArray().length );
 
 // SERVER-525 (closed) unique indexes in capped collection
