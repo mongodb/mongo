bb887b35b38f73de05ed96dd39d97841a501029a
Eliot Horowitz <eliot@10gen.com>
2014-08-01 11:14:43 -0400

    SERVER-13635: make drop test work for generic storage engine

--- jstests/mmap_v1/drop.js
+++ jstests/mmap_v1/drop.js
@@ -6,19 +6,19 @@ res = coll.runCommand("drop");
 assert( !res.ok, tojson( res ) );
 
 
-assert.eq(0, coll.getIndexes().length, "A");
+assert.eq(0, db.system.indexes.find({ns : coll + ""}).count(), "A");
 coll.save({});
-assert.eq(1, coll.getIndexes().length, "B");
+assert.eq(1, db.system.indexes.find({ns : coll + ""}).count(), "B");
 coll.ensureIndex({a : 1});
-assert.eq(2, coll.getIndexes().length, "C");
+assert.eq(2, db.system.indexes.find({ns : coll + ""}).count(), "C");
 assert.commandWorked(db.runCommand({drop : coll.getName()}));
-assert.eq(0, coll.getIndexes().length, "D");
+assert.eq(0, db.system.indexes.find({ns : coll + ""}).count(), "D");
 
 coll.ensureIndex({a : 1});
-assert.eq(2, coll.getIndexes().length, "E");
+assert.eq(2, db.system.indexes.find({ns : coll + ""}).count(), "E");
 assert.commandWorked(db.runCommand({deleteIndexes : coll.getName(), index : "*"}),
                      "delete indexes A");
-assert.eq(1, coll.getIndexes().length, "G");
+assert.eq(1, db.system.indexes.find({ns : coll + ""}).count(), "G");
 
 // make sure we can still use it
 coll.save({});
