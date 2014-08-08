50601136fc8b9399a1832b84396e74e7bc9011c5
Randolph Tan <randolph@10gen.com>
2014-08-08 09:52:10 -0400

    Revert "Revert "SERVER-14728 Shard depends on string comparison of replica set connection string""
    This reverts commit d261864e9156af7ecc3d0491111dffa0161e9d0d.

--- src/mongo/s/shard.cpp
+++ src/mongo/s/shard.cpp
@@ -155,24 +155,48 @@ namespace mongo {
             return ShardPtr();
         }
 
-        ShardPtr find( const string& ident ) {
-            string mykey = ident;
+        ShardPtr find(const string& ident) {
+            string errmsg;
+            ConnectionString connStr = ConnectionString::parse(ident, errmsg);
 
-            {
-                scoped_lock lk( _mutex );
-                ShardMap::iterator i = _lookup.find( mykey );
+            uassert(18642, str::stream() << "Error parsing connection string: " << ident,
+                    errmsg.empty());
 
-                if ( i != _lookup.end() )
-                    return i->second;
+            if (connStr.type() == ConnectionString::SET) {
+                scoped_lock lk(_rsMutex);
+                ShardMap::iterator iter = _rsLookup.find(connStr.getSetName());
+
+                if (iter == _rsLookup.end()) {
+                    return ShardPtr();
+                }
+
+                return iter->second;
+            }
+            else {
+                scoped_lock lk(_mutex);
+                ShardMap::iterator iter = _lookup.find(ident);
+
+                if (iter == _rsLookup.end()) {
+                    return ShardPtr();
+                }
+
+                return iter->second;
+            }
+        }
+
+        ShardPtr findWithRetry(const string& ident) {
+            ShardPtr shard(find(ident));
+
+            if (shard != NULL) {
+                return shard;
             }
 
             // not in our maps, re-load all
             reload();
 
-            scoped_lock lk( _mutex );
-            ShardMap::iterator i = _lookup.find( mykey );
-            massert( 13129 , (string)"can't find shard for: " + mykey , i != _lookup.end() );
-            return i->second;
+            shard = find(ident);
+            massert(13129 , str::stream() << "can't find shard for: " << ident, shard != NULL);
+            return shard;
         }
 
         // Lookup shard by replica set name. Returns Shard::EMTPY if the name can't be found.
@@ -182,12 +206,12 @@ namespace mongo {
             scoped_lock lk( _rsMutex );
             ShardMap::iterator i = _rsLookup.find( name );
 
-            return (i == _rsLookup.end()) ? Shard::EMPTY : i->second.get();
+            return (i == _rsLookup.end()) ? Shard::EMPTY : *(i->second.get());
         }
 
         // Useful for ensuring our shard data will not be modified while we use it
         Shard findCopy( const string& ident ){
-            ShardPtr found = find( ident );
+            ShardPtr found = findWithRetry(ident);
             scoped_lock lk( _mutex );
             massert( 13128 , (string)"can't find shard for: " + ident , found.get() );
             return *found.get();
