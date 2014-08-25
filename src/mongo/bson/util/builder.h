a9f9040c76e2927aa801728578b86541336c2fa2
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2014-08-25 15:27:43 -0400

    SERVER-14668 Add function to StringBuilder for formatting addresses

--- src/mongo/bson/util/builder.h
+++ src/mongo/bson/util/builder.h
@@ -269,12 +269,14 @@ namespace mongo {
     template <typename Allocator>
     class StringBuilderImpl {
     public:
-        static const size_t MONGO_DBL_SIZE = 3 + DBL_MANT_DIG - DBL_MIN_EXP;
+        // Sizes are determined based on the number of characters in 64-bit + the trailing '\0'
+        static const size_t MONGO_DBL_SIZE = 3 + DBL_MANT_DIG - DBL_MIN_EXP + 1;
         static const size_t MONGO_S32_SIZE = 12;
         static const size_t MONGO_U32_SIZE = 11;
         static const size_t MONGO_S64_SIZE = 23;
         static const size_t MONGO_U64_SIZE = 22;
         static const size_t MONGO_S16_SIZE = 7;
+        static const size_t MONGO_PTR_SIZE = 19;    // Accounts for the 0x prefix
 
         StringBuilderImpl() { }
 
@@ -302,8 +304,23 @@ namespace mongo {
         StringBuilderImpl& operator<<( short x ) {
             return SBNUM( x , MONGO_S16_SIZE , "%hd" );
         }
+        StringBuilderImpl& operator<<(const void* x) {
+            if (sizeof(x) == 8) {
+                return SBNUM(x, MONGO_PTR_SIZE, "0x%llX");
+            }
+            else {
+                return SBNUM(x, MONGO_PTR_SIZE, "0x%lX");
+            }
+        }
         StringBuilderImpl& operator<<( char c ) {
             _buf.grow( 1 )[0] = c;
+            return *this;
+        }
+        StringBuilderImpl& operator<<(const char* str) {
+            return *this << StringData(str);
+        }
+        StringBuilderImpl& operator<<(const StringData& str) {
+            append(str);
             return *this;
         }
 
@@ -323,11 +340,6 @@ namespace mongo {
         void write( const char* buf, int len) { memcpy( _buf.grow( len ) , buf , len ); }
 
         void append( const StringData& str ) { str.copyTo( _buf.grow( str.size() ), false ); }
-
-        StringBuilderImpl& operator<<( const StringData& str ) {
-            append( str );
-            return *this;
-        }
 
         void reset( int maxSize = 0 ) { _buf.reset( maxSize ); }
 
