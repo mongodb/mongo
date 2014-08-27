aac8ba84d8fac7dc531a03c63224fdfc8cd3baa3
David Storch <david.storch@10gen.com>
2014-08-27 10:09:04 -0400

    SERVER-13104 enumerate all possibilties for $or below an $and

--- src/mongo/db/query/query_planner_test.cpp
+++ src/mongo/db/query/query_planner_test.cpp
@@ -4651,6 +4651,61 @@ namespace {
         assertNumSolutions(internalQueryEnumerationMaxOrSolutions);
     }
 
+    // SERVER-13104: test that we properly enumerate all solutions for nested $or.
+    TEST_F(QueryPlannerTest, EnumerateNestedOr) {
+        params.options = QueryPlannerParams::NO_TABLE_SCAN;
+        addIndex(BSON("a" << 1));
+        addIndex(BSON("b" << 1));
+        addIndex(BSON("c" << 1));
+
+        runQuery(fromjson("{d: 1, $or: [{a: 1, b: 1}, {c: 1}]}"));
+
+        assertNumSolutions(2U);
+        assertSolutionExists("{fetch: {filter: {d: 1}, node: {or: {nodes: ["
+                                "{fetch: {filter: {b: 1}, node: {ixscan: {pattern: {a: 1}}}}},"
+                                "{ixscan: {pattern: {c: 1}}}]}}}}");
+        assertSolutionExists("{fetch: {filter: {d: 1}, node: {or: {nodes: ["
+                                "{fetch: {filter: {a: 1}, node: {ixscan: {pattern: {b: 1}}}}},"
+                                "{ixscan: {pattern: {c: 1}}}]}}}}");
+    }
+
+    // SERVER-13104: test that we properly enumerate all solutions for nested $or.
+    TEST_F(QueryPlannerTest, EnumerateNestedOr2) {
+        params.options = QueryPlannerParams::NO_TABLE_SCAN;
+        addIndex(BSON("a" << 1));
+        addIndex(BSON("b" << 1));
+        addIndex(BSON("c" << 1));
+        addIndex(BSON("d" << 1));
+        addIndex(BSON("e" << 1));
+        addIndex(BSON("f" << 1));
+
+        runQuery(fromjson("{a: 1, b: 1, $or: [{c: 1, d: 1}, {e: 1, f: 1}]}"));
+
+        assertNumSolutions(6U);
+
+        // Four possibilities from indexing the $or.
+        assertSolutionExists("{fetch: {filter: {a: 1, b: 1}, node: {or: {nodes: ["
+                                "{fetch: {filter: {d: 1}, node: {ixscan: {pattern: {c: 1}}}}},"
+                                "{fetch: {filter: {f: 1}, node: {ixscan: {pattern: {e: 1}}}}}"
+                             "]}}}}");
+        assertSolutionExists("{fetch: {filter: {a: 1, b: 1}, node: {or: {nodes: ["
+                                "{fetch: {filter: {c: 1}, node: {ixscan: {pattern: {d: 1}}}}},"
+                                "{fetch: {filter: {f: 1}, node: {ixscan: {pattern: {e: 1}}}}}"
+                             "]}}}}");
+        assertSolutionExists("{fetch: {filter: {a: 1, b: 1}, node: {or: {nodes: ["
+                                "{fetch: {filter: {d: 1}, node: {ixscan: {pattern: {c: 1}}}}},"
+                                "{fetch: {filter: {e: 1}, node: {ixscan: {pattern: {f: 1}}}}}"
+                             "]}}}}");
+        assertSolutionExists("{fetch: {filter: {a: 1, b: 1}, node: {or: {nodes: ["
+                                "{fetch: {filter: {c: 1}, node: {ixscan: {pattern: {d: 1}}}}},"
+                                "{fetch: {filter: {e: 1}, node: {ixscan: {pattern: {f: 1}}}}}"
+                             "]}}}}");
+
+        // Two possibilties from outside the $or.
+        assertSolutionExists("{fetch: {node: {ixscan: {pattern: {a: 1}}}}}");
+        assertSolutionExists("{fetch: {node: {ixscan: {pattern: {b: 1}}}}}");
+    }
+
     //
     // Test the "split limited sort stages" hack.
     //
