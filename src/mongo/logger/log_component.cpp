1d46ac81bb65d8065f7667103d25abcef6fbf897
Benety Goh <benety@mongodb.com>
2014-08-28 10:09:46 -0400

    SERVER-4217 use hyphen to display default component in log message

--- src/mongo/logger/log_component.cpp
+++ src/mongo/logger/log_component.cpp
@@ -134,7 +134,7 @@ std::string _dottedNames[LogComponent::kNumLogComponents+1];
 
     StringData LogComponent::getNameForLog() const {
         switch (_value) {
-        case kDefault:              return createStringData("        ");
+        case kDefault:              return createStringData("-       ");
         case kAccessControl:        return createStringData("ACCESS  ");
         case kCommands:             return createStringData("COMMANDS");
         case kIndexing:             return createStringData("INDEXING");
