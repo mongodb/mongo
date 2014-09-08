05c32130a6b38ef600ffea3b8739096fdf018f00
mike o'brien <mpobrien005@gmail.com>
2014-09-08 16:21:08 -0400

    use ReadAtLeast()
    Former-commit-id: 57ea9240a248389719295a5678d1f69284a04172

--- src/github.com/shelman/mongo-tools-proto/common/db/shim.go
+++ src/github.com/shelman/mongo-tools-proto/common/db/shim.go
@@ -11,14 +11,14 @@ import (
 const MaxBSONSize = 16 * 1024 * 1024
 
 type StorageShim struct {
-	DBPath     string
-	Database   string
-	Collection string
-	Skip       int
-	Limit      int
-	ShimPath   string
-	Query      string
-	process    *exec.Cmd
+	DBPath      string
+	Database    string
+	Collection  string
+	Skip        int
+	Limit       int
+	ShimPath    string
+	Query       string
+	shimProcess *exec.Cmd
 }
 
 //BSONStream wraps a stream
@@ -99,52 +99,26 @@ func (shim *StorageShim) Open() (*BSONStream, error) {
 	return &BSONStream{stdOut, nil}, nil
 }
 
-//readBytes is a helper that reads 'length' bytes from the Reader, making
-//multiple calls to Read() if necessary.
-//Returns an error if the underlying reader hits an error on Read(), or if
-//the reader encounters EOF. The caller should check that bytesRead == length
-//in cases where the returned error is EOF.
-func readBytes(r io.Reader, into []byte, offset, length int) (int, error) {
-	if length > len(into) {
-		panic("Length for read is greater than buffer size")
-	}
-	bytesRead := 0
-	for bytesRead < length {
-		n, err := r.Read(into[offset+bytesRead : offset+length])
-		bytesRead += n
-		if err != nil {
-			return bytesRead, err
-		}
-	}
-	//The correct number of bytes has been read and we haven't hit EOF
-	return bytesRead, nil
-}
-
 func (shim *BSONStream) LoadNextInto(into []byte) (bool, int32) {
 	//Read the bson object size (a 4 byte integer)
-	n, err := readBytes(shim.Stream, into, 0, 4)
+	_, err := io.ReadAtLeast(shim.Stream, into[0:4], 4)
 	if err != nil {
 		if err != io.EOF {
 			shim.err = err
+			return false, 0
 		} else {
-			//Hit end of stream , no more docs to iterate.
-			if n == 0 {
-				shim.err = nil
-				return false, 0
-			} else {
-				//This case means we hit EOF but read a partial document,
-				//so there's a broken doc in the stream. Treat this as error.
-				shim.err = fmt.Errorf("Invalid bson")
-				return false, 0
-			}
+			//We hit EOF right away, so we're at the end of the stream.
+			shim.err = nil
+			return false, 0
 		}
-		return false, 0
 	}
 
-	bsonSize := int32((uint32(into[0]) << 0) |
-		(uint32(into[1]) << 8) |
-		(uint32(into[2]) << 16) |
-		uint32(into[3])<<24)
+	bsonSize := int32(
+		(uint32(into[0]) << 0) |
+			(uint32(into[1]) << 8) |
+			(uint32(into[2]) << 16) |
+			(uint32(into[3]) << 24),
+	)
 
 	//Verify that the size of the BSON object we are about to read can
 	//actually fit into the buffer that was provided. If not, either the BSON is
@@ -153,24 +127,19 @@ func (shim *BSONStream) LoadNextInto(into []byte) (bool, int32) {
 		shim.err = fmt.Errorf("Invalid BSONSize: %v bytes", bsonSize)
 		return false, 0
 	}
-	n, err = readBytes(shim.Stream, into, 4, int(bsonSize-4))
+	_, err = readBytes(shim.Stream, into, 4, int(bsonSize-4))
 	if err != nil {
-		if err == io.EOF {
-			//Hit end of stream , no more docs to iterate.
-			if n == 0 {
-				shim.err = nil
-				return false, 0
-			} else {
-				//This case means we hit EOF but read a partial document,
-				//so there's a broken doc in the stream. Treat this as error.
-				shim.err = fmt.Errorf("Invalid bson")
-				return false, 0
-			}
-		} else {
+		if err != io.EOF {
 			shim.err = err
+			return false, 0
+		} else {
+			//This case means we hit EOF but read a partial document,
+			//so there's a broken doc in the stream. Treat this as error.
+			shim.err = fmt.Errorf("Invalid bson: %v", err)
 			return false, 0
 		}
 	}
+
 	shim.err = nil
 	return true, bsonSize
 }
@@ -178,10 +147,3 @@ func (shim *BSONStream) LoadNextInto(into []byte) (bool, int32) {
 func (shim *BSONStream) Err() error {
 	return shim.err
 }
-
-/*
-
-func (shim *BSONStream) Next(into []byte) {
-	shim.
-}
-*/
