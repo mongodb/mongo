06d9d5ca346f9cb075773224dec93fe296ccdcae
Alex Gorrod <alexg@wiredtiger.com>
2014-09-11 12:40:50 +1000

    Fixup cache pool reconfigure now that options are being folded together.
    Try to make the code more obvious as well.

--- src/conn/conn_api.c
+++ src/conn/conn_api.c
@@ -660,11 +660,6 @@ __conn_reconfigure(WT_CONNECTION *wt_conn, const char *config)
 	WT_ERR(__wt_statlog_create(session, config_cfg));
 	WT_ERR(__wt_verbose_config(session, config_cfg));
 
-	/* Wake up the cache pool server so any changes are noticed. */
-	if (F_ISSET(conn, WT_CONN_CACHE_POOL))
-		WT_ERR(__wt_cond_signal(
-		    session, __wt_process.cache_pool->cache_pool_cond));
-
 	WT_ERR(__wt_config_merge(session, config_cfg, &p));
 	__wt_free(session, conn->cfg);
 	conn->cfg = p;
