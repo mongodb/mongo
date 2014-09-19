3fe2d4fb37cce8259991f7af8b58d67b357af84e
Tyler Brock <tyler.brock@gmail.com>
2014-09-19 09:26:41 -0400

    SERVER-15183 suppress warning about fabs value truncation

--- src/third_party/s2/util/math/mathutil.cc
+++ src/third_party/s2/util/math/mathutil.cc
@@ -117,8 +117,16 @@ bool MathUtil::RealRootsForCubic(long double const a,
     return true;
   }
 
+// Disable error about fabs causing truncation of value because
+// it takes a double instead of a long double (Clang 3.5+)
+// See SERVER-15183
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wabsolute-value"
+
   long double const A =
     -sgn(R) * pow(fabs(R) + sqrt(R_squared - Q_cubed), 1.0/3.0L);
+
+#pragma clang diagnostic pop
 
   if (A != 0.0) {  // in which case, B from NR is zero
     *r1 = A + Q / A - a_third;
