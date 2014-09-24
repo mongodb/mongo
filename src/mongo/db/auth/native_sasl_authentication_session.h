f23e92530e7356fa1959aac0f15cac526e46109f
Amalia Hawkins <amalia.hawkins@10gen.com>
2014-09-24 17:51:12 -0400

    SERVER-7596 Clean up license headers to remove extraneous sentence.

--- src/mongo/db/auth/native_sasl_authentication_session.h
+++ src/mongo/db/auth/native_sasl_authentication_session.h
@@ -1,5 +1,5 @@
 /*
- *    Copyright (C) 2014 10gen, Inc.  All Rights Reserved.
+ *    Copyright (C) 2014 10gen, Inc.
  *
  *    This program is free software: you can redistribute it and/or  modify
  *    it under the terms of the GNU Affero General Public License, version 3,
