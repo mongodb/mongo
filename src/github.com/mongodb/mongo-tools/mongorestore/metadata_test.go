6a697bf4b44024d9f5644431c2d02190fd9b1bf2
Wisdom Omuya <deafgoat@gmail.com>
2014-09-26 13:10:58 -0400

    remove unused TestIndexDocumentConversion
    Former-commit-id: ffb8b47211020ad29a66e1388321e5e455de936c

--- src/github.com/mongodb/mongo-tools/mongorestore/metadata_test.go
+++ src/github.com/mongodb/mongo-tools/mongorestore/metadata_test.go
@@ -18,29 +18,3 @@ func TestCollectionInfoFromOptions(t *testing.T) {
 	})
 
 }
-
-func TestIndexDocumentConversion(t *testing.T) {
-
-	testutil.VerifyTestType(t, "unit")
-
-	Convey("With some test index doc bson examples", t, func() {
-
-		Convey("simple index {a:1, b:-1}", func() {
-			idx := IndexDocument{
-				Name: "test1",
-				Key:  bson.D{{"a", 1.0}, {"b", -1.0}},
-			}
-
-			Convey("converts properly", func() {
-				mgoIdx, err := idx.ToMgoFormat()
-				So(err, ShouldBeNil)
-				So(mgoIdx.Name, ShouldEqual, "test1")
-				So(len(mgoIdx.Key), ShouldEqual, 2)
-				So(mgoIdx.Key[0], ShouldEqual, "+a")
-				So(mgoIdx.Key[1], ShouldEqual, "-b")
-			})
-		})
-
-		//TODO more of these after change to not use mgo
-	})
-}
