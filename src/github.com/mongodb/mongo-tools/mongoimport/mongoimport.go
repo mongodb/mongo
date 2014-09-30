791ecdea2e7b8f956085a6141dc04fafc2aab1a5
Wisdom Omuya <deafgoat@gmail.com>
2014-09-29 20:08:35 -0400

    lint fix
    Former-commit-id: 7baf7e20fd562739a7b94648c6588c5c3df49924

--- src/github.com/mongodb/mongo-tools/mongoimport/mongoimport.go
+++ src/github.com/mongodb/mongo-tools/mongoimport/mongoimport.go
@@ -141,7 +141,7 @@ func (mongoImport *MongoImport) ValidateSettings(args []string) error {
 	// ensure we have a valid string to use for the collection
 	if mongoImport.ToolOptions.Namespace.Collection == "" {
 		if fileBaseName == "" {
-			return fmt.Errorf("no collection specified!")
+			return fmt.Errorf("no collection specified")
 		}
 		fileBaseName = filepath.Base(fileBaseName)
 		if lastDotIndex := strings.LastIndex(fileBaseName, "."); lastDotIndex != -1 {
@@ -151,7 +151,7 @@ func (mongoImport *MongoImport) ValidateSettings(args []string) error {
 			return err
 		}
 		mongoImport.ToolOptions.Namespace.Collection = fileBaseName
-		util.PrintlnTimeStamped("no collection specified!")
+		util.PrintlnTimeStamped("no collection specified")
 		util.PrintfTimeStamped("using filename '%v' as collection\n",
 			mongoImport.ToolOptions.Namespace.Collection)
 	}
