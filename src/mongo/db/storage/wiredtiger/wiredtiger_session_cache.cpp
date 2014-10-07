a08a73cf69a268aa4043bf1f3ee800778684a882
Michael Cahill <michael.cahill@wiredtiger.com>
2014-10-07 17:00:53 +1100

    Switch to read-uncommitted for MongoDB sessions: higher-level locking avoids conflicts, and we always want to see the results of other sessions.

--- src/mongo/db/storage/wiredtiger/wiredtiger_session_cache.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_session_cache.cpp
@@ -8,7 +8,7 @@ namespace mongo {
 
     WiredTigerSession::WiredTigerSession( WT_CONNECTION* conn ) {
         _session = NULL;
-        int ret = conn->open_session(conn, NULL, NULL, &_session);
+        int ret = conn->open_session(conn, NULL, "isolation=read-uncommitted", &_session);
         invariantWTOK(ret);
     }
 
