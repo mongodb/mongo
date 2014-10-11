0dab1d5ab65aa1d7f27bb2d5c51bf9360ca38a4f
Eliot Horowitz <eliot@10gen.com>
2014-10-11 19:46:08 -0400

    SERVER-13635: abstract storagesize per db for listDatabases

--- src/mongo/db/storage/kv/kv_collection_catalog_entry.h
+++ src/mongo/db/storage/kv/kv_collection_catalog_entry.h
@@ -77,6 +77,7 @@ namespace mongo {
                                        long long newExpireSeconds );
 
         RecordStore* getRecordStore() { return _recrodStore.get(); }
+        const RecordStore* getRecordStore() const { return _recrodStore.get(); }
 
     protected:
         virtual MetaData _getMetaData( OperationContext* txn ) const;
