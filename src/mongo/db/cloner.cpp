bc21c23cbe79344034081bb8e4910fb0711b4ecc
Eric Milkie <milkie@10gen.com>
2014-10-15 13:29:21 -0400

    SERVER-12061 fix assert code
    (cherry picked from commit 525b5ef94f72c376c4d831539be3988b8ff0a0ec)

--- src/mongo/db/cloner.cpp
+++ src/mongo/db/cloner.cpp
@@ -173,7 +173,7 @@ namespace mongo {
                         warning() << ss.ss.str() << "; skipping";
                         continue;
                     }
-                    uasserted(28530, ss);
+                    uasserted(28531, ss);
                 }
 
                 ++numSeen;
