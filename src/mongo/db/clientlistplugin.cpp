c2c33f5cbf6285f29c2e81dc3c67816079b0e79f
Eliot Horowitz <eliot@10gen.com>
2014-10-16 10:37:39 -0400

    SERVER-15683: add command to introspect OperationContext and RecoveryUnit

--- src/mongo/db/clientlistplugin.cpp
+++ src/mongo/db/clientlistplugin.cpp
@@ -28,7 +28,9 @@
 
 #include "mongo/platform/basic.h"
 
+#include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/db/client.h"
+#include "mongo/db/commands.h"
 #include "mongo/db/curop.h"
 #include "mongo/db/global_environment_experiment.h"
 #include "mongo/db/operation_context.h"
@@ -116,5 +118,49 @@ namespace {
         }
 
     } clientListPlugin;
+
+    class CommandHelper : public GlobalEnvironmentExperiment::ProcessOperationContext {
+    public:
+        virtual void processOpContext(OperationContext* txn) {
+            BSONObjBuilder b;
+            if ( txn->getClient() )
+                txn->getClient()->reportState( b );
+            if ( txn->getCurOp() )
+                txn->getCurOp()->reportState( &b );
+            if ( txn->lockState() )
+                b.append( "lockState", txn->lockState()->reportState() );
+            if ( txn->recoveryUnit() )
+                txn->recoveryUnit()->reportState( &b );
+            array.append( b.obj() );
+        }
+
+        BSONArrayBuilder array;
+    };
+
+    class CurrentOpContexts : public Command {
+    public:
+        CurrentOpContexts() : Command( "currentOpCtx" ) { }
+
+        virtual bool isWriteCommandForConfigServer() const { return false; }
+
+        virtual bool slaveOk() const { return true; }
+
+        bool run( OperationContext* txn,
+                  const string& dbname,
+                  BSONObj& cmdObj,
+                  int,
+                  string& errmsg,
+                  BSONObjBuilder& result,
+                  bool fromRepl) {
+
+            CommandHelper helper;
+            getGlobalEnvironment()->forEachOperationContext(&helper);
+
+            result.appendArray( "operations", helper.array.arr() );
+
+            return true;
+        }
+    } currentOpContexts;
+
 }  // namespace
 }  // namespace mongo
