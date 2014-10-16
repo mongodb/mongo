d6125b358d8d467ecd930fb5fdb235943b8f3a33
Michael Cahill <michael.cahill@wiredtiger.com>
2014-10-16 21:42:04 +1100

    Strip metadata from JSON dumps if if cannot be passed to WT_SESSION::create.

--- src/schema/schema_open.c
+++ src/schema/schema_open.c
@@ -421,7 +421,7 @@ __wt_schema_open_table(WT_SESSION_IMPL *session,
 		goto err;
 
 	if (table->ncolgroups > 0 && table->is_simple)
-		WT_RET_MSG(session, EINVAL,
+		WT_ERR_MSG(session, EINVAL,
 		    "%s requires a table with named columns", tablename);
 
 	WT_ERR(__wt_calloc_def(session, WT_COLGROUPS(table), &table->cgroups));
