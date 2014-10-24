b2b8ad756016707265aa0b0be3f21b2205d577ed
Keith Bostic <keith@wiredtiger.com>
2014-10-24 17:38:58 -0400

    Prefix the WT_ATOMIC_XXX macros with underscores, it's a mistake for code outside of those include files to use them.

--- src/include/gcc.h
+++ src/include/gcc.h
@@ -79,43 +79,43 @@
  * atomic state changes, we rely on the WT_ATOMIC_ADD and WT_ATOMIC_CAS
  * (compare and swap) operations.
  */
-#define	WT_ATOMIC_ADD(v, val, n)					\
+#define	__WT_ATOMIC_ADD(v, val, n)					\
 	(WT_STATIC_ASSERT(sizeof(v) == (n)), __sync_add_and_fetch(&(v), val))
-#define	WT_ATOMIC_CAS(v, old, new, n)					\
+#define	__WT_ATOMIC_CAS(v, old, new, n)					\
 	(WT_STATIC_ASSERT(sizeof(v) == (n)),				\
 	__sync_bool_compare_and_swap(&(v), old, new))
-#define	WT_ATOMIC_CAS_VAL(v, old, new, n)				\
+#define	__WT_ATOMIC_CAS_VAL(v, old, new, n)				\
 	(WT_STATIC_ASSERT(sizeof(v) == (n)),				\
 	__sync_val_compare_and_swap(&(v), old, new))
-#define	WT_ATOMIC_STORE(v, val, n)					\
+#define	__WT_ATOMIC_STORE(v, val, n)					\
 	(WT_STATIC_ASSERT(sizeof(v) == (n)),				\
 	__sync_lock_test_and_set(&(v), val))
-#define	WT_ATOMIC_SUB(v, val, n)					\
+#define	__WT_ATOMIC_SUB(v, val, n)					\
 	(WT_STATIC_ASSERT(sizeof(v) == (n)), __sync_sub_and_fetch(&(v), val))
 
-#define	WT_ATOMIC_ADD1(v, val)		WT_ATOMIC_ADD(v, val, 1)
-#define	WT_ATOMIC_CAS1(v, old, new)	WT_ATOMIC_CAS(v, old, new, 1)
-#define	WT_ATOMIC_CAS_VAL1(v, old, new)	WT_ATOMIC_CAS_VAL(v, old, new, 1)
-#define	WT_ATOMIC_STORE1(v, val)	WT_ATOMIC_STORE(v, val, 1)
-#define	WT_ATOMIC_SUB1(v, val)		WT_ATOMIC_SUB(v, val, 1)
+#define	WT_ATOMIC_ADD1(v, val)		__WT_ATOMIC_ADD(v, val, 1)
+#define	WT_ATOMIC_CAS1(v, old, new)	__WT_ATOMIC_CAS(v, old, new, 1)
+#define	WT_ATOMIC_CAS_VAL1(v, old, new)	__WT_ATOMIC_CAS_VAL(v, old, new, 1)
+#define	WT_ATOMIC_STORE1(v, val)	__WT_ATOMIC_STORE(v, val, 1)
+#define	WT_ATOMIC_SUB1(v, val)		__WT_ATOMIC_SUB(v, val, 1)
 
-#define	WT_ATOMIC_ADD2(v, val)		WT_ATOMIC_ADD(v, val, 2)
-#define	WT_ATOMIC_CAS2(v, old, new)	WT_ATOMIC_CAS(v, old, new, 2)
-#define	WT_ATOMIC_CAS_VAL2(v, old, new)	WT_ATOMIC_CAS_VAL(v, old, new, 2)
-#define	WT_ATOMIC_STORE2(v, val)	WT_ATOMIC_STORE(v, val, 2)
-#define	WT_ATOMIC_SUB2(v, val)		WT_ATOMIC_SUB(v, val, 2)
+#define	WT_ATOMIC_ADD2(v, val)		__WT_ATOMIC_ADD(v, val, 2)
+#define	WT_ATOMIC_CAS2(v, old, new)	__WT_ATOMIC_CAS(v, old, new, 2)
+#define	WT_ATOMIC_CAS_VAL2(v, old, new)	__WT_ATOMIC_CAS_VAL(v, old, new, 2)
+#define	WT_ATOMIC_STORE2(v, val)	__WT_ATOMIC_STORE(v, val, 2)
+#define	WT_ATOMIC_SUB2(v, val)		__WT_ATOMIC_SUB(v, val, 2)
 
-#define	WT_ATOMIC_ADD4(v, val)		WT_ATOMIC_ADD(v, val, 4)
-#define	WT_ATOMIC_CAS4(v, old, new)	WT_ATOMIC_CAS(v, old, new, 4)
-#define	WT_ATOMIC_CAS_VAL4(v, old, new)	WT_ATOMIC_CAS_VAL(v, old, new, 4)
-#define	WT_ATOMIC_STORE4(v, val)	WT_ATOMIC_STORE(v, val, 4)
-#define	WT_ATOMIC_SUB4(v, val)		WT_ATOMIC_SUB(v, val, 4)
+#define	WT_ATOMIC_ADD4(v, val)		__WT_ATOMIC_ADD(v, val, 4)
+#define	WT_ATOMIC_CAS4(v, old, new)	__WT_ATOMIC_CAS(v, old, new, 4)
+#define	WT_ATOMIC_CAS_VAL4(v, old, new)	__WT_ATOMIC_CAS_VAL(v, old, new, 4)
+#define	WT_ATOMIC_STORE4(v, val)	__WT_ATOMIC_STORE(v, val, 4)
+#define	WT_ATOMIC_SUB4(v, val)		__WT_ATOMIC_SUB(v, val, 4)
 
-#define	WT_ATOMIC_ADD8(v, val)		WT_ATOMIC_ADD(v, val, 8)
-#define	WT_ATOMIC_CAS8(v, old, new)	WT_ATOMIC_CAS(v, old, new, 8)
-#define	WT_ATOMIC_CAS_VAL8(v, old, new)	WT_ATOMIC_CAS_VAL(v, old, new, 8)
-#define	WT_ATOMIC_STORE8(v, val)	WT_ATOMIC_STORE(v, val, 8)
-#define	WT_ATOMIC_SUB8(v, val)		WT_ATOMIC_SUB(v, val, 8)
+#define	WT_ATOMIC_ADD8(v, val)		__WT_ATOMIC_ADD(v, val, 8)
+#define	WT_ATOMIC_CAS8(v, old, new)	__WT_ATOMIC_CAS(v, old, new, 8)
+#define	WT_ATOMIC_CAS_VAL8(v, old, new)	__WT_ATOMIC_CAS_VAL(v, old, new, 8)
+#define	WT_ATOMIC_STORE8(v, val)	__WT_ATOMIC_STORE(v, val, 8)
+#define	WT_ATOMIC_SUB8(v, val)		__WT_ATOMIC_SUB(v, val, 8)
 
 /* Compile read-write barrier */
 #define	WT_BARRIER() asm volatile("" ::: "memory")
