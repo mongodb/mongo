197358e4cf4cdb99d94ea668243a4cd4791bd523
Jason Rassi <rassi@10gen.com>
2014-10-24 09:41:18 -0400

    SERVER-15797 doTTLForIndex shouldn't explicitly create WriteUnitOfWork

--- src/mongo/db/ttl.cpp
+++ src/mongo/db/ttl.cpp
@@ -235,10 +235,8 @@ namespace mongo {
                     return true;
                 }
 
-                WriteUnitOfWork uow( txn );
                 n = deleteObjects( txn, db, ns, query, PlanExecutor::YIELD_AUTO, false, true );
                 ttlDeletedDocuments.increment( n );
-                uow.commit();
             }
 
             LOG(1) << "\tTTL deleted: " << n << endl;
