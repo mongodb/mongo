fe676f44c0a53d3c83d085b84c928f705948f3f1
Lauren Hecht <laurenhecht@robotlake.com>
2014-10-27 09:04:17 -0400

    Revert "Clear WiredTiger stats each time they are gathered."
    This reverts commit 07c7c91d52dd66fe2b15d14be75e33b747188452.

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
@@ -75,7 +75,7 @@ namespace mongo {
         ss << "cache_size=" << cacheSizeGB << "G,";
         ss << "session_max=20000,";
         ss << "extensions=[local=(entry=index_collator_extension)],";
-        ss << "statistics=(all,clear),";
+        ss << "statistics=(all),";
         ss << "log=(enabled),";
         ss << extraOpenOptions;
         string config = ss.str();
