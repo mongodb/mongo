1b9f7d763c75fb5c6f13a2c619381494edd81898
Eliot Horowitz <eliot@10gen.com>
2014-10-29 18:40:05 -0400

    SERVER-13635: kv engine can pass through flushAllFiles

--- src/mongo/db/storage/kv/kv_engine.h
+++ src/mongo/db/storage/kv/kv_engine.h
@@ -88,6 +88,9 @@ namespace mongo {
         virtual Status dropSortedDataInterface( OperationContext* opCtx,
                                                 const StringData& ident ) = 0;
 
+        // optional
+        virtual int flushAllFiles( bool sync ) { return 0; }
+
         /**
          * This must not change over the lifetime of the engine.
          */
