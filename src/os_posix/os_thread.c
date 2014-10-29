c6400f76e75e3d3f177fa18887fb3758518a18bc
Keith Bostic <keith@wiredtiger.com>
2014-10-29 15:18:40 -0400

    os_thread.c:55:45: error: cast from function call of type 'pthread_t' to non-matching type 'void *' [-Werror=bad-function-cast]

--- src/os_posix/os_thread.c
+++ src/os_posix/os_thread.c
@@ -45,12 +45,15 @@ __wt_thread_join(WT_SESSION_IMPL *session, wt_thread_t tid)
 void
 __wt_thread_id(char *buf, size_t buflen)
 {
+	pthread_t self;
+
 	/*
 	 * POSIX 1003.1 allows pthread_t to be an opaque type, but on systems
 	 * where it's a pointer, we'd rather print out the pointer and match
 	 * gdb output. Since we don't yet run on any systems where pthread_t
 	 * is not a pointer, do it that way for now.
 	 */
+	self = pthread_self();
 	(void)snprintf(buf, buflen,
-	    "%" PRIu64 ":%p", (uint64_t)getpid(), (void *)pthread_self());
+	    "%" PRIu64 ":%p", (uint64_t)getpid(), (void *)self);
 }
