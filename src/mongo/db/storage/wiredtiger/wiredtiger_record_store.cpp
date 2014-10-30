cf9bc81eccbac434d0d953b99a49dbdf96391103
Lauren Hecht <laurenhecht@robotlake.com>
2014-10-30 14:06:24 -0400

    better defaults

--- src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
@@ -63,7 +63,8 @@ namespace mongo {
         // override values in the prefix, but not values in the suffix.
         str::stream ss;
         ss << "type=file,";
-        ss << "memory_page_max=10m,";
+        ss << "leaf_page_max=2m,";
+        ss << "memory_page_max=100m,";
 
         ss << extraStrings << ",";
 
