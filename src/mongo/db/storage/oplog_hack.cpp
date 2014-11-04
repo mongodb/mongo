4731939510292ac460decd503e7b0e984c09a569
Mathias Stearn <mathias@10gen.com>
2014-11-04 14:37:29 -0500

    SERVER-15948 add missing files

---
+++ src/mongo/db/storage/oplog_hack.cpp
@@ -0,0 +1,80 @@
+/**
+ *    Copyright (C) 2014 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kStorage
+
+#include "mongo/db/storage/oplog_hack.h"
+
+#include <limits>
+
+#include "mongo/bson/bson_validate.h"
+#include "mongo/bson/optime.h"
+#include "mongo/db/diskloc.h"
+#include "mongo/db/jsobj.h"
+
+namespace mongo {
+namespace oploghack {
+
+    StatusWith<DiskLoc> keyForOptime(const OpTime& opTime) {
+        // Make sure secs and inc wouldn't be negative if treated as signed. This ensures that they
+        // don't sort differently when put in a DiskLoc. It also avoids issues with Null/Invalid
+        // DiskLocs
+        if (opTime.getSecs() > uint32_t(std::numeric_limits<int32_t>::max()))
+            return StatusWith<DiskLoc>(ErrorCodes::BadValue, "ts secs too high");
+
+        if (opTime.getInc() > uint32_t(std::numeric_limits<int32_t>::max()))
+            return StatusWith<DiskLoc>(ErrorCodes::BadValue, "ts inc too high");
+
+        const DiskLoc out = DiskLoc(opTime.getSecs(), opTime.getInc());
+        if (out <= minDiskLoc)
+            return StatusWith<DiskLoc>(ErrorCodes::BadValue, "ts too low");
+        if (out >= maxDiskLoc)
+            return StatusWith<DiskLoc>(ErrorCodes::BadValue, "ts too high");
+
+        return StatusWith<DiskLoc>(out);
+    }
+
+    /**
+     * data and len must be the arguments from RecordStore::insert() on an oplog collection.
+     */
+    StatusWith<DiskLoc> extractKey(const char* data, int len) {
+        DEV invariant(validateBSON(data, len).isOK());
+
+        const BSONObj obj(data);
+        const BSONElement elem = obj["ts"];
+        if (elem.eoo())
+            return StatusWith<DiskLoc>(ErrorCodes::BadValue, "no ts field");
+        if (elem.type() != Timestamp)
+            return StatusWith<DiskLoc>(ErrorCodes::BadValue, "ts must be a Timestamp");
+
+        return keyForOptime(elem._opTime());
+    }
+
+}  // namespace oploghack
+} // namespace mongo
