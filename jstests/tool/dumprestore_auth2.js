26c14e90548cdac77817c22513fc560e592a088c
Adam Midvidy <amidvidy@gmail.com>
2014-11-06 11:08:53 -0500

    SERVER-15992 dumprestore_auth{2,3} js rely on W=0

--- jstests/tool/dumprestore_auth2.js
+++ jstests/tool/dumprestore_auth2.js
@@ -54,7 +54,9 @@ assert.eq(2, admindb.system.users.count(), "didn't drop backup and test users");
 assert.eq(0, admindb.system.roles.count(), "didn't drop roles");
 assert.eq(0, coll.count(), "didn't drop foo coll");
 
-t.runTool("restore", "--dir", t.ext, "--username", "restore", "--password", "pass");
+// This test depends on W=0 to mask unique index violations.
+// This should be fixed once we implement TOOLS-341
+t.runTool("restore", "--dir", t.ext, "--username", "restore", "--password", "pass", "--w", "0");
 
 assert.soon("admindb.system.users.findOne()", "no data after restore");
 assert.eq(4, admindb.system.users.count(), "didn't restore users");
@@ -78,7 +80,7 @@ admindb.createRole({role: "customRole2", roles: [], privileges:[]});
 admindb.dropUser("root");
 admindb.logout();
 
-t.runTool("restore", "--dir", t.ext, "--username", "restore", "--password", "pass", "--drop");
+t.runTool("restore", "--dir", t.ext, "--username", "restore", "--password", "pass", "--drop", "--w", "0");
 
 admindb.auth("root", "pass");
 assert.soon("1 == admindb.system.users.find({user:'root'}).count()", "didn't restore users 2");
