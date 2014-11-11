6c0c1df12e3c6366f410f4b3137de7b16854bda1
matt dannenberg <matt.dannenberg@10gen.com>
2014-11-11 09:45:55 -0500

    SERVER-16093 improve replsets/replsetarb2.js

--- jstests/replsets/replsetarb2.js
+++ jstests/replsets/replsetarb2.js
@@ -1,27 +1,28 @@
 // Election when master fails and remaining nodes are an arbiter and a slave.
 
-doTest = function( signal ) {
+(function() {
+    "use strict";
 
     var replTest = new ReplSetTest( {name: 'unicomplex', nodes: 3} );
     var nodes = replTest.nodeList();
 
-    print(tojson(nodes));
-
     var conns = replTest.startSet();
     var r = replTest.initiate({"_id" : "unicomplex",
-                "members" : [
-                    {"_id" : 0, "host" : nodes[0] },
-                    {"_id" : 1, "host" : nodes[1], "arbiterOnly" : true, "votes": 1, "priority" : 0},
-                    {"_id" : 2, "host" : nodes[2] }]});
+                               "members" : [
+                                   {"_id" : 0, "host" : nodes[0], "priority" : 2},
+                                   {"_id" : 1, "host" : nodes[1], "arbiterOnly" : true, "votes": 1},
+                                   {"_id" : 2, "host" : nodes[2]}
+                                ]});
 
     // Make sure we have a master
     var master = replTest.getMaster();
+    assert.eq(master, conns[0], "wrong node became master");
 
     // Make sure we have an arbiter
     assert.soon(function() {
-        res = conns[1].getDB("admin").runCommand({replSetGetStatus: 1});
+        var res = conns[1].getDB("admin").runCommand({replSetGetStatus: 1});
         printjson(res);
-        return res.myState == 7;
+        return res.myState === 7;
     }, "Aribiter failed to initialize.");
 
     var result = conns[1].getDB("admin").runCommand({isMaster : 1});
@@ -32,19 +33,15 @@ doTest = function( signal ) {
     master.getDB("foo").foo.insert({a: "foo"});
     replTest.awaitReplication();
 
-    assert( ! conns[1].getDB( "admin" ).runCommand( "ismaster" ).secondary , "arbiter shouldn't be secondary" )
-
     // Now kill the original master
-    mId = replTest.getNodeId( master );
-    replTest.stop( mId );
+    var mId = replTest.getNodeId(master);
+    replTest.stop(mId);
 
     // And make sure that the slave is promoted
-    new_master = replTest.getMaster();
+    var new_master = replTest.getMaster();
 
-    newMasterId = replTest.getNodeId( new_master );
-    assert( newMasterId == 2, "Slave wasn't promoted to new master");
+    var newMasterId = replTest.getNodeId(new_master);
+    assert.eq(newMasterId, 2, "Secondary wasn't promoted to new primary");
 
-    replTest.stopSet( signal );
-}
-
-doTest( 15 );
+    replTest.stopSet(15);
+}());
