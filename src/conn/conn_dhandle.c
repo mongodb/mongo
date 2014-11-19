486fb534ed3bf2b93720d583325212e513d41cd4
Michael Cahill <michael.cahill@wiredtiger.com>
2014-11-20 06:30:37 +1100

    Now that we increment / decrement handle reference counts in the session layer, they should start at zero.  Otherwise, we could never sweep handles.

--- src/conn/conn_dhandle.c
+++ src/conn/conn_dhandle.c
@@ -189,7 +189,6 @@ __conn_dhandle_get(WT_SESSION_IMPL *session,
 	WT_RET(__wt_calloc_def(session, 1, &dhandle));
 
 	WT_ERR(__wt_rwlock_alloc(session, &dhandle->rwlock, "data handle"));
-	dhandle->session_ref = 1;
 
 	dhandle->name_hash = __wt_hash_city64(name, strlen(name));
 	WT_ERR(__wt_strdup(session, name, &dhandle->name));
