2c0c9e9ae2efeffbfaf6508660430d440b27564a
Valeri Karpov <valeri.karpov@10gen.com>
2014-11-24 17:28:13 -0500

    Move newly added qa test configs to new directory
    Former-commit-id: 667d599c8e6f55b9a93a651aa6b28830970eb6de

--- test/qa-tests/configs/kerberos.linux.sh
+++
@@ -1,4 +0,0 @@
-echo "107.23.89.149 kdc.10gen.me" | sudo tee -a /etc/hosts
-sudo hostname "kdc.10gen.me"
-sudo cp legacy/jstests/libs/mockkrb5.conf /etc/krb5.conf
-kinit -p mockuser@10GEN.ME -k -t legacy/jstests/libs/mockuser.keytab
