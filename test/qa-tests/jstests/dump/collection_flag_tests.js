b0c07f641c25b50bbf36b1d2110b5c678f80120e
Valeri Karpov <valeri.karpov@10gen.com>
2014-12-01 10:34:40 -0500

    TOOLS-202 tests for the --collection flag for mongodump
    Former-commit-id: 73229ca88f9e4dd2ad15d478710d02bfd8319355

---
+++ test/qa-tests/jstests/dump/collection_flag_tests.js
@@ -0,0 +1,43 @@
+if (typeof getToolTest === 'undefined') {
+  load('jstests/configs/plain_28.config.js');
+}
+
+(function() {
+  var toolTest = getToolTest('collectionFlagTest');
+  var commonToolArgs = getCommonToolArguments();
+  var db = toolTest.db.getSiblingDB('foo');
+
+  db.dropDatabase();
+  assert.eq(0, db.bar.count());
+  db.getSiblingDB('baz').dropDatabase();
+  assert.eq(0, db.getSiblingDB('baz').bar.count());
+
+  // Insert into the 'foo' database
+  db.bar.insert({ x: 1 });
+  // and into the 'baz' database
+  db.getSiblingDB('baz').bar.insert({ x: 2 });
+
+  // Running mongodump with `--collection bar` and no '--db' flag should throw
+  // an error
+  var dumpArgs = ['dump', '--collection', 'bar'].concat(commonToolArgs);
+  assert(toolTest.runTool.apply(toolTest, dumpArgs) !== 0,
+    'mongodump should exit with a non-zero status when --collection is ' +
+    'specified but --db isn\'t');
+
+  // Running mongodump with `--collection bar --db foo` should only dump
+  // the 'foo' database and ignore the 'baz' database
+  var dumpArgs = ['dump', '--collection', 'bar', '--db', 'foo'].
+    concat(commonToolArgs);
+  toolTest.runTool.apply(toolTest, dumpArgs);
+  db.dropDatabase();
+  db.getSiblingDB('baz').dropDatabase();
+  assert.eq(0, db.bar.count());
+  assert.eq(0, db.getSiblingDB('baz').bar.count());
+
+  var restoreArgs = ['restore'].concat(commonToolArgs);
+  toolTest.runTool.apply(toolTest, restoreArgs);
+  assert.eq(1, db.bar.count());
+  assert.eq(0, db.getSiblingDB('baz').bar.count());
+
+  toolTest.stop();
+})();
