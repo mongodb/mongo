6ddbf4719898588b4b3a727635c7b7212e416928
Valeri Karpov <valeri.karpov@10gen.com>
2014-12-02 15:13:23 -0500

    TOOLS-435 add forceTableScan flag test
    Former-commit-id: 3e4f3723b5fc2552a568c8a9a58687dd42cca2e2

---
+++ test/qa-tests/jstests/dump/force_table_scan_tests.js
@@ -0,0 +1,52 @@
+if (typeof getToolTest === 'undefined') {
+  load('jstests/configs/plain_28.config.js');
+}
+
+(function() {
+  resetDbpath('dump');
+  var toolTest = getToolTest('forceTableScanTest');
+  var commonToolArgs = getCommonToolArguments();
+
+  // IMPORTANT: make sure global `db` object is equal to this db, because
+  // startParallelShell gives you no way of overriding db object.
+  db = toolTest.db.getSiblingDB('foo');
+
+  db.dropDatabase();
+  assert.eq(0, db.bar.count());
+
+  // Run parallel shell that inserts every millisecond
+  var insertsShell = startParallelShell(
+    'print(\'starting insert\'); ' +
+    'for (var i = 0; i < 1000; ++i) { ' +
+    '  db.getSiblingDB(\'foo\').bar.insert({ x: i }); ' +
+    '  sleep(1); ' +
+    '}');
+
+  // Give some time for inserts to actually start before dumping
+  sleep(100);
+
+  var countBeforeMongodump = db.bar.count();
+  // Crash if parallel shell hasn't started inserting yet
+  assert.gt(countBeforeMongodump, 0);
+
+  var dumpArgs = ['dump', '--forceTableScan'].concat(commonToolArgs);
+  assert.eq(toolTest.runTool.apply(toolTest, dumpArgs), 0,
+    'mongodump --forceTableScan should succeed');
+
+  // Wait for inserts to finish so we can then drop the database
+  insertsShell();
+  db.dropDatabase();
+  assert.eq(0, db.bar.count());
+
+  // --batchSize is necessary because config servers don't allow
+  // batch writes, so if you've dumped the config DB you should
+  // be careful to set this.
+  var restoreArgs = ['restore', /*'--batchSize', '1',*/ '--w', '1',
+    '--drop'].concat(commonToolArgs);
+  assert.eq(toolTest.runTool.apply(toolTest, restoreArgs), 0,
+    'mongorestore should succeed');
+  assert.gte(db.bar.count(), countBeforeMongodump);
+  assert.lt(db.bar.count(), 1000);
+
+  toolTest.stop();
+})();
