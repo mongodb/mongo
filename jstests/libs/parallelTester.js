e1baf8419c0df083322e6f989b95770d8e0c0816
Jason Rassi <rassi@10gen.com>
2014-12-03 15:17:08 -0500

    SERVER-16408 Disable max_time_ms.js in parallel suite
    max_time_ms.js is (by design) sensitive to the execution time of the
    queries that it runs. As such, it should not be run in the parallel
    suite.

--- jstests/libs/parallelTester.js
+++ jstests/libs/parallelTester.js
@@ -151,6 +151,7 @@ if ( typeof _threadInject != "undefined" ){
                                    "set_param1.js", // changes global state
                                    "geo_update_btree2.js", // SERVER-11132 test disables table scans
                                    "update_setOnInsert.js", // SERVER-9982
+                                   "max_time_ms.js", // Sensitive to query execution time, by design
                                ] );
         
         var parallelFilesDir = "jstests/core";
