b3a65dda5b7ad16a6215508c6c59496f7f14fbf3
Keith Bostic <keith@wiredtiger.com>
2014-12-03 17:08:51 -0500

    The initial byte comparison has to be case-independent as well.

--- src/include/misc.h
+++ src/include/misc.h
@@ -179,7 +179,8 @@
 	(((const char *)str)[0] == ((const char *)bytes)[0] &&		\
 	    strncmp(str, bytes, len) == 0 && (str)[(len)] == '\0')
 #define	WT_STRING_CASE_MATCH(str, bytes, len)				\
-	(((const char *)str)[0] == ((const char *)bytes)[0] &&		\
+	(tolower(((const char *)str)[0]) ==				\
+	    tolower(((const char *)bytes)[0]) &&			\
 	    strncasecmp(str, bytes, len) == 0 && (str)[(len)] == '\0')
 
 /*
