9aeffc1281fe3b0e5eeca3b17c25019d2041dcf4
Sam Helman <sam.helman@10gen.com>
2014-12-04 15:13:19 -0500

    TOOLS-196: js test for restoring with nonempty admin.tempusers
    Former-commit-id: a6df8591252a9ccd0e786fb2ddd4fcc681762152

---
+++ test/qa-tests/jstests/restore/nonempty_temp_users.js
@@ -0,0 +1,45 @@
+(function() {
+
+    // Tests running mongorestore and restoring users with a nonempty temp 
+    // users collection.
+
+    jsTest.log('Testing restoring users with a nonempty temp users collection.'+
+        ' The restore should fail');
+
+    var toolTest = new ToolTest('nonempty_temp_users');
+    toolTest.startDB('foo');
+
+    // where we'll put the dump
+    var dumpTarget = 'nonempty_temp_users_dump';
+
+    // the admin db
+    var adminDB = toolTest.db.getSiblingDB('admin');
+
+    // create a user on the admin database
+    adminDB.createUser(
+        {
+            user: 'adminUser',
+            pwd: 'password',
+            roles: [
+                { role: 'read', db: 'admin' },
+            ],
+        }
+    );
+
+    // dump the data
+    var ret = toolTest.runTool('dump', '--out', dumpTarget);
+
+    // clear out the user
+    adminDB.dropAllUsers();
+
+    // insert into the tempusers collection
+    adminDB.tempusers.insert({ _id: 'corruption' });
+
+    // restore the data. it should fail
+    ret = toolTest.runTool('restore', dumpTarget);
+    assert.neq(0, ret);
+
+    // success
+    toolTest.stop();
+
+}());
