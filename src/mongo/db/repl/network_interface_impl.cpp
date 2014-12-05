4c8eab7305bddbf544c6c9c62fe871486d012af3
Andy Schwerin <schwerin@mongodb.com>
2014-12-05 16:21:20 -0500

    SERVER-15424 Dynamically size replication network thread pool.

--- src/mongo/db/repl/network_interface_impl.cpp
+++ src/mongo/db/repl/network_interface_impl.cpp
@@ -54,8 +54,11 @@ namespace mongo {
 namespace repl {
 
 namespace {
-    const size_t kNumThreads = 8;
-    Seconds kDefaultMaxConnectionAge(30);
+
+    const int kMinThreads = 1;
+    const int kMaxThreads = 51;  // Set to 1 + max repl set size, for heartbeat + wiggle room.
+    const Seconds kMaxIdleThreadAge(30);
+    const Seconds kDefaultMaxConnectionAge(30);
 
 }  // namespace
 
@@ -364,6 +367,9 @@ namespace {
     }
 
     NetworkInterfaceImpl::NetworkInterfaceImpl() :
+        _numIdleThreads(0),
+        _nextThreadId(0),
+        _lastFullUtilizationDate(),
         _isExecutorRunnable(false),
         _inShutdown(false),
         _numActiveNetworkRequests(0) {
@@ -386,6 +392,32 @@ namespace {
 
     }
 
+    void NetworkInterfaceImpl::_startNewNetworkThread_inlock() {
+        if (_inShutdown) {
+            LOG(1) <<
+                "Not starting new replication networking thread while shutting down replication.";
+            return;
+        }
+        if (_threads.size() >= kMaxThreads) {
+            LOG(1) << "Not starting new replication networking thread because " << kMaxThreads <<
+                " are already running; " << _numIdleThreads << " threads are idle and " <<
+                _pending.size() << " network requests are waiting for a thread to serve them.";
+            return;
+        }
+        const std::string threadName(str::stream() << "ReplExecNetThread-" << _nextThreadId++);
+        try {
+            _threads.push_back(
+                    boost::make_shared<boost::thread>(
+                            stdx::bind(&NetworkInterfaceImpl::_requestProcessorThreadBody,
+                                       this,
+                                       threadName)));
+            ++_numIdleThreads;
+        }
+        catch (const std::exception& ex) {
+            error() << "Failed to start " << threadName << "; " << _threads.size() <<
+                " other network threads still running; caught exception: " << ex.what();
+        }
+    }
 
     void NetworkInterfaceImpl::startup() {
         boost::lock_guard<boost::mutex> lk(_mutex);
@@ -393,26 +425,22 @@ namespace {
         if (!_threads.empty()) {
             return;
         }
-        for (size_t i = 0; i < kNumThreads; ++i) {
-            const std::string threadName(str::stream() << "ReplExecNetThread-" << i);
-            _threads.push_back(
-                    boost::shared_ptr<boost::thread>(
-                            new boost::thread(
-                                    stdx::bind(&NetworkInterfaceImpl::_consumeNetworkRequests,
-                                               this,
-                                               threadName
-                                              ))));
+        for (size_t i = 0; i < kMinThreads; ++i) {
+            _startNewNetworkThread_inlock();
         }
     }
 
     void NetworkInterfaceImpl::shutdown() {
+        using std::swap;
         boost::unique_lock<boost::mutex> lk(_mutex);
         _inShutdown = true;
         _hasPending.notify_all();
+        ThreadList threadsToJoin;
+        swap(threadsToJoin, _threads);
         lk.unlock();
         _connPool->closeAllInUseConnections();
-        std::for_each(_threads.begin(),
-                      _threads.end(),
+        std::for_each(threadsToJoin.begin(),
+                      threadsToJoin.end(),
                       stdx::bind(&boost::thread::join, stdx::placeholders::_1));
     }
 
@@ -448,17 +476,39 @@ namespace {
         _isExecutorRunnable = false;
     }
 
-    void NetworkInterfaceImpl::_consumeNetworkRequests(const std::string& threadName) {
+    void NetworkInterfaceImpl::_requestProcessorThreadBody(
+            NetworkInterfaceImpl* net,
+            const std::string& threadName) {
         setThreadName(threadName);
+        LOG(1) << "thread starting";
+        net->_consumeNetworkRequests();
+
+        // At this point, another thread may have destroyed "net", if this thread chose to detach
+        // itself and remove itself from net->_threads before releasing net->_mutex.  Do not access
+        // member variables of "net" from here, on.
+        LOG(1) << "thread shutting down";
+    }
+
+    void NetworkInterfaceImpl::_consumeNetworkRequests() {
         boost::unique_lock<boost::mutex> lk(_mutex);
         while (!_inShutdown) {
             if (_pending.empty()) {
-                _hasPending.wait(lk);
+                if (_threads.size() > kMinThreads) {
+                    const Date_t nowDate = now();
+                    const Date_t nextThreadRetirementDate =
+                        _lastFullUtilizationDate + kMaxIdleThreadAge.total_milliseconds();
+                    if (nowDate > nextThreadRetirementDate) {
+                        _lastFullUtilizationDate = nowDate;
+                        break;
+                    }
+                }
+                _hasPending.timed_wait(lk, kMaxIdleThreadAge);
                 continue;
             }
             CommandData todo = _pending.front();
             _pending.pop_front();
             ++_numActiveNetworkRequests;
+            --_numIdleThreads;
             lk.unlock();
             ResponseStatus result = _runCommand(todo.request);
             LOG(2) << "Network status of sending " << todo.request.cmdObj.firstElementFieldName() <<
@@ -466,8 +516,27 @@ namespace {
             todo.onFinish(result);
             lk.lock();
             --_numActiveNetworkRequests;
+            ++_numIdleThreads;
             _signalWorkAvailable_inlock();
         }
+        --_numIdleThreads;
+        if (_inShutdown) {
+            return;
+        }
+        // This thread is ending because it was idle for too long.
+        // Find self in _threads, remove self from _threads, detach self.
+        for (size_t i = 0; i < _threads.size(); ++i) {
+            if (_threads[i]->get_id() != boost::this_thread::get_id()) {
+                continue;
+            }
+            _threads[i]->detach();
+            _threads[i].swap(_threads.back());
+            _threads.pop_back();
+            return;
+        }
+        severe().stream() << "Could not find this thread, with id " <<
+            boost::this_thread::get_id() << " in the replication networking thread pool";
+        fassertFailedNoTrace(28581);
     }
 
     void NetworkInterfaceImpl::startCommand(
@@ -482,6 +551,12 @@ namespace {
         cd.cbHandle = cbHandle;
         cd.request = request;
         cd.onFinish = onFinish;
+        if (_numIdleThreads < _pending.size()) {
+            _startNewNetworkThread_inlock();
+        }
+        if (_numIdleThreads <= _pending.size()) {
+            _lastFullUtilizationDate = curTimeMillis64();
+        }
         _hasPending.notify_one();
     }
 
