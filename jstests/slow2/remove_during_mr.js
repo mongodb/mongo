mongodb/mongo

commit 29a700ada85ff1a11ca428bbf71d337ece2f2c5a

SERVER-13922 remove dbtemprelease from non-repl remaining spots

src/mongo/db/commands/mr.cpp
@@ -948,7 +948,7 @@ namespace mongo {
                 verify( foundIndex );
             }
 
-            Client::ReadContext ctx( _config.incLong );
+            scoped_ptr<Client::ReadContext> ctx(new Client::ReadContext(_config.incLong));
 
             BSONObj prev;
             BSONList all;
@@ -969,7 +969,7 @@ namespace mongo {
                                                 whereCallback).isOK());
 
             Runner* rawRunner;
-            verify(getRunner(ctx.ctx().db()->getCollection(_config.incLong),
+            verify(getRunner(ctx->ctx().db()->getCollection(_config.incLong),
                              cq, &rawRunner, QueryPlannerParams::NO_TABLE_SCAN).isOK());
 
             auto_ptr<Runner> runner(rawRunner);
@@ -992,18 +992,12 @@ namespace mongo {
 
                 runner->saveState();
 
-                // can't be a smart pointer since it needs throw annotation on destructor
-                dbtempreleasecond* yield = new dbtempreleasecond();
+                ctx.reset();
 
-                try {
-                    // reduce a finalize array
-                    finalReduce( all );
-                }
-                catch (...) {
-                    delete yield; // if throws, replaces current exception rather than terminating.
-                    throw;
-                }
-                delete yield;
+                // reduce a finalize array
+                finalReduce( all );
+
+                ctx.reset(new Client::ReadContext(_config.incLong));
 
                 all.clear();
                 prev = o;
@@ -1016,13 +1010,10 @@ namespace mongo {
                 _txn->checkForInterrupt();
             }
 
-            {
-                dbtempreleasecond tl;
-                if ( ! tl.unlocked() )
-                    warning() << "map/reduce can't temp release" << endl;
-                // reduce and finalize last array
-                finalReduce( all );
-            }
+            ctx.reset();
+            // reduce and finalize last array
+            finalReduce( all );
+            ctx.reset(new Client::ReadContext(_config.incLong));
 
             pm.finished();
         }
@@ -1296,11 +1287,11 @@ namespace mongo {
                         // We've got a cursor preventing migrations off, now re-establish our useful cursor
 
                         // Need lock and context to use it
-                        Lock::DBRead lock( config.ns );
+                        scoped_ptr<Lock::DBRead> lock(new Lock::DBRead(config.ns));
 
                         // This context does no version check, safe b/c we checked earlier and have an
                         // open cursor
-                        Client::Context ctx(config.ns, storageGlobalParams.dbpath, false);
+                        scoped_ptr<Client::Context> ctx(new Client::Context(config.ns, storageGlobalParams.dbpath, false));
 
                         const NamespaceString nss(config.ns);
                         const WhereCallbackReal whereCallback(nss.db());
@@ -1317,7 +1308,7 @@ namespace mongo {
                         }
 
                         Runner* rawRunner;
-                        if (!getRunner(ctx.db()->getCollection( config.ns), cq, &rawRunner).isOK()) {
+                        if (!getRunner(ctx->db()->getCollection( config.ns), cq, &rawRunner).isOK()) {
                             uasserted(17239, "Can't get runner for query " + config.filter.toString());
                             return 0;
                         }
@@ -1352,16 +1343,14 @@ namespace mongo {
                             if (numInputs % 100 == 0) {
                                 Timer t;
 
-                                // TODO: As an optimization, we might want to do the save/restore 
-                                // state and yield inside the reduceAndSpillInMemoryState method,
-                                // so it only happens if necessary.
-                                //
-                                runner->saveState();
-                                {
-                                    dbtemprelease unlock;
-                                    state.reduceAndSpillInMemoryStateIfNeeded();
-                                }
-                                runner->restoreState();
+                                // TODO: As an optimization, we might want to do the save/restore
+                                // state and yield inside the reduceAndSpillInMemoryState method, so
+                                // it only happens if necessary.
+                                ctx.reset();
+                                lock.reset();
+                                state.reduceAndSpillInMemoryStateIfNeeded();
+                                lock.reset(new Lock::DBRead(config.ns));
+                                ctx.reset(new Client::Context(config.ns, storageGlobalParams.dbpath, false));
 
                                 reduceTime += t.micros();
 

src/mongo/db/cloner.cpp
@@ -59,26 +59,6 @@ namespace mongo {
 
     BSONElement getErrField(const BSONObj& o);
 
-    /** Selectively release the mutex based on a parameter. */
-    class dbtempreleaseif {
-        MONGO_DISALLOW_COPYING(dbtempreleaseif);
-    public:
-        dbtempreleaseif( bool release ) : _impl( release ? new dbtemprelease() : 0 ) {}
-        ~dbtempreleaseif() throw(DBException) {
-            if (_impl)
-                delete _impl;
-        }
-    private:
-        // can't use a smart pointer because we need throw annotation on destructor
-        dbtemprelease* _impl;
-    };
-
-    void mayInterrupt( bool mayBeInterrupted ) {
-        if ( mayBeInterrupted ) {
-            killCurrentOp.checkForInterrupt( false );
-        }
-    }
-
     /* for index info object:
          { "name" : "name_1" , "ns" : "foo.index3" , "key" :  { "name" : 1.0 } }
        we need to fix up the value in the "ns" parameter so that the name prefix is correct on a
@@ -107,14 +87,8 @@ namespace mongo {
             else
                 b.append(e);
         }
-        BSONObj res= b.obj();
 
-        /*    if( mod ) {
-            out() << "before: " << o.toString() << endl;
-            o.dump();
-            out() << "after:  " << res.toString() << endl;
-            res.dump();
-            }*/
+        BSONObj res= b.obj();
 
         return res;
     }
@@ -129,37 +103,35 @@ namespace mongo {
         {}
 
         void operator()( DBClientCursorBatchIterator &i ) {
+            // XXX: can probably take dblock instead
             Lock::GlobalWrite lk;
             context.relocked();
 
             bool createdCollection = false;
             Collection* collection = NULL;
 
+            if ( isindex == false ) {
+                collection = context.db()->getCollection( to_collection );
+                if ( !collection ) {
+                    massert( 17321,
+                             str::stream()
+                             << "collection dropped during clone ["
+                             << to_collection << "]",
+                             !createdCollection );
+                    createdCollection = true;
+                    collection = context.db()->createCollection( txn, to_collection );
+                    verify( collection );
+                }
+            }
+
             while( i.moreInCurrentBatch() ) {
-                if ( numSeen % 128 == 127 /*yield some*/ ) {
-                    collection = NULL;
+                if ( numSeen % 128 == 127 ) {
                     time_t now = time(0);
                     if( now - lastLog >= 60 ) {
                         // report progress
                         if( lastLog )
                             log() << "clone " << to_collection << ' ' << numSeen << endl;
                         lastLog = now;
-                    }
-                    mayInterrupt( _mayBeInterrupted );
-                    dbtempreleaseif t( _mayYield );
-                }
-
-                if ( isindex == false && collection == NULL ) {
-                    collection = context.db()->getCollection( to_collection );
-                    if ( !collection ) {
-                        massert( 17321,
-                                 str::stream()
-                                 << "collection dropped during clone ["
-                                 << to_collection << "]",
-                                 !createdCollection );
-                        createdCollection = true;
-                        collection = context.db()->createCollection( txn, to_collection );
-                        verify( collection );
                     }
                 }
 
@@ -249,8 +221,7 @@ namespace mongo {
 
         int options = QueryOption_NoCursorTimeout | ( slaveOk ? QueryOption_SlaveOk : 0 );
         {
-            mayInterrupt( mayBeInterrupted );
-            dbtempreleaseif r( mayYield );
+            dbtemprelease r;
             _conn->query(boost::function<void(DBClientCursorBatchIterator &)>(f), from_collection,
                          query, 0, options);
         }
@@ -417,8 +388,7 @@ namespace mongo {
             /* todo: we can put these releases inside dbclient or a dbclient specialization.
                or just wait until we get rid of global lock anyway.
                */
-            mayInterrupt( opts.mayBeInterrupted );
-            dbtempreleaseif r( opts.mayYield );
+            dbtemprelease r;
 
             // just using exhaust for collection copying right now
 
@@ -477,10 +447,6 @@ namespace mongo {
         }
 
         for ( list<BSONObj>::iterator i=toClone.begin(); i != toClone.end(); i++ ) {
-            {
-                mayInterrupt( opts.mayBeInterrupted );
-                dbtempreleaseif r( opts.mayYield );
-            }
             BSONObj collection = *i;
             LOG(2) << "  really will clone: " << collection << endl;
             const char * from_name = collection["name"].valuestr();

src/mongo/db/db.h
@@ -71,33 +71,6 @@ namespace mongo {
         }
     };
 
-    /**
-     * only does a temp release if we're not nested and have a lock
-     *
-     * WARNING: do not put in a smart pointer or any other class. If you absolutely must, you need
-     * to add the throw(DBException) annotation to it's destructor.
-     */
-    class dbtempreleasecond : boost::noncopyable {
-        dbtemprelease * real;
-    public:
-        dbtempreleasecond() {
-            real = 0;
-            if( Lock::isLocked() ) {
-                // if nested don't temprelease, and we don't complain either for this class
-                if( !Lock::nested() ) {
-                    real = new dbtemprelease();
-                }
-            }
-        }
-        ~dbtempreleasecond() throw(DBException) {
-            if ( real ) {
-                delete real;
-                real = 0;
-            }
-        }
-        bool unlocked() const { return real != 0; }
-    };
-
     extern void (*snmpInit)();
 
 } // namespace mongo

src/mongo/db/pdfile.cpp
@@ -52,7 +52,6 @@ _ disallow system* manipulations from the database.
 #include "mongo/db/pdfile_private.h"
 #include "mongo/db/background.h"
 #include "mongo/db/clientcursor.h"
-#include "mongo/db/cloner.h"
 #include "mongo/db/commands/server_status.h"
 #include "mongo/db/curop-inl.h"
 #include "mongo/db/db.h"

src/mongo/db/repair_database.cpp
@@ -38,7 +38,6 @@
 #include "mongo/db/catalog/database_holder.h"
 #include "mongo/db/catalog/index_create.h"
 #include "mongo/db/client.h"
-#include "mongo/db/cloner.h"
 #include "mongo/db/index/index_descriptor.h"
 #include "mongo/util/file.h"
 #include "mongo/util/file_allocator.h"

commit 23fb35b7a52d0d50829a6bb9a47954374ab2b61f

SERVER-15539: Properly save/restore state when yielding in M/R
This fixes a regression introduced in commit 29a700ad.

jstests/slow2/remove_during_mr.js
@@ -1,0 +1,57 @@
+// SERVER-15539
+'use strict';
+
+load('jstests/libs/parallelTester.js');
+
+function client1() {
+    for(var i = 0; i < 1000; i++) {
+        db.remove_during_mr.remove({rand: {$gte: Random.rand()}}, {justOne: true});
+    }
+}
+
+function client2() {
+
+    function mapper() {
+        emit(this.key, 1);
+    }
+
+    function reducer() {
+        return {};
+    }
+
+    for(var i = 0; i < 1000; i++) {
+        var options = {
+            out: {replace: 'bar'},
+            sort: {_id: -1}
+        };
+
+        db.remove_during_mr.mapReduce(mapper, reducer, options);
+    }
+}
+
+// prepare some basic data for the collection
+db.remove_during_mr.drop();
+
+var bulk = db.remove_during_mr.initializeUnorderedBulkOp();
+for (var i = 0; i < 3000; i++) {
+    bulk.insert({i: i, key: Random.randInt(), rand: Random.rand()});
+}
+bulk.execute();
+
+var threads = [];
+for (var i = 0; i < 20; i++) {
+    var t;
+
+    if (i % 2 === 0) {
+        t = new ScopedThread(client1);
+    } else {
+        t = new ScopedThread(client2);
+    }
+
+    threads.push(t);
+    t.start();
+}
+
+threads.forEach(function(t) {
+    t.join();
+});

src/mongo/db/commands/mr.cpp
@@ -1404,9 +1404,11 @@ namespace mongo {
                                 // TODO: As an optimization, we might want to do the save/restore
                                 // state and yield inside the reduceAndSpillInMemoryState method, so
                                 // it only happens if necessary.
+                                exec->saveState();
                                 lock.reset();
                                 state.reduceAndSpillInMemoryStateIfNeeded();
                                 lock.reset(new Lock::DBRead(txn->lockState(), nss.db()));
+                                exec->restoreState(txn);
 
                                 // Need to reload the database, in case it was dropped after we
                                 // released the lock