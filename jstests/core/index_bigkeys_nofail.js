f2f48b749ded0c5585c798c302f6162f19336670
Geert Bosch <geert@mongodb.com>
2014-12-11 18:36:57 -0500

    SERVER-16497: Fix minor test case nits

--- jstests/core/index_bigkeys_nofail.js
+++ jstests/core/index_bigkeys_nofail.js
@@ -1,22 +1,25 @@
-// SERVER-16497
-var t=db.index_bigkeys_nofail;
-t.drop();
-var res=db.getSiblingDB('admin').runCommand( { setParameter: 1, failIndexKeyTooLong: true } );
-var was=res.was;
-assert.commandWorked(res);
+// SERVER-16497: Check that failIndexKeyTooLong setting works
+(function() {
+    "use strict";
 
+    var t=db.index_bigkeys_nofail;
+    t.drop();
+    var res=db.getSiblingDB('admin').runCommand( { setParameter: 1, failIndexKeyTooLong: true } );
+    var was=res.was;
+    assert.commandWorked(res);
 
-var x = new Array(1025).join('x');
-assert.commandWorked(t.ensureIndex({name:1}));
-assert.writeError(t.insert({name:x}));
-assert.commandWorked(t.dropIndex({name:1}));
-assert.writeOK(t.insert({name:x}));
-assert.commandFailed(t.ensureIndex({name:1}));
+    var x = new Array(1025).join('x');
+    assert.commandWorked(t.ensureIndex({name:1}));
+    assert.writeError(t.insert({name:x}));
+    assert.commandWorked(t.dropIndex({name:1}));
+    assert.writeOK(t.insert({name:x}));
+    assert.commandFailed(t.ensureIndex({name:1}));
 
-t.drop();
-db.getSiblingDB('admin').runCommand( { setParameter: 1, failIndexKeyTooLong: false } );
-assert.writeOK(t.insert({name:x}));
-assert.commandWorked(t.ensureIndex({name:1}));
-assert.writeOK(t.insert({name:x}));
+    t.drop();
+    db.getSiblingDB('admin').runCommand( { setParameter: 1, failIndexKeyTooLong: false } );
+    assert.writeOK(t.insert({name:x}));
+    assert.commandWorked(t.ensureIndex({name:1}));
+    assert.writeOK(t.insert({name:x}));
 
-db.getSiblingDB('admin').runCommand( { setParameter: 1, failIndexKeyTooLong: was } );
+    db.getSiblingDB('admin').runCommand( { setParameter: 1, failIndexKeyTooLong: was } );
+}());
