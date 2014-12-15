b90bdb092764c846ba46a553f1bf73adff145019
Kyle Erf <erf@mongodb.com>
2014-12-15 13:43:32 -0600

    TOOLS-513 correctly detect auth version 1
    Former-commit-id: cceac3f178aec7da72c3d193e826fc6e806b85a9

--- common/auth/auth_info.go
+++ common/auth/auth_info.go
@@ -3,11 +3,12 @@ package auth
 import (
 	"fmt"
 	"github.com/mongodb/mongo-tools/common/db"
-	"github.com/mongodb/mongo-tools/common/util"
 	"gopkg.in/mgo.v2/bson"
 	"strings"
 )
 
+// GetAuthVersion gets the auth version of the connected server and
+// returns the value as an int along with any errors that occured.
 func GetAuthVersion(commander db.CommandRunner) (int, error) {
 	results := bson.M{}
 	err := commander.Run(
@@ -18,34 +19,27 @@ func GetAuthVersion(commander db.CommandRunner) (int, error) {
 		&results,
 		"admin",
 	)
+
 	if err != nil {
+		errMessage := err.Error()
+		// as a necessary hack, if the error message takes a certain form,
+		// we can infer version 1. This is because early versions of mongodb
+		// had no concept of an "auth schema version", so asking for the
+		// authSchemaVersion value will return a "no option found" or "no such cmd"
+		if errMessage == "no option found to get" ||
+			strings.HasPrefix(errMessage, "no such cmd") {
+			return 1, nil
+		}
+		// otherwise it's a connection error, so bubble it up
 		return 0, err
 	}
 
-	if util.IsTruthy(results["ok"]) {
-		version, ok := results["authSchemaVersion"].(int)
-		if !ok {
-			// very unlikely this will ever happen
-			return 0, fmt.Errorf(
-				"getParameter command returned non-numeric result: %v",
-				results["authSchemaVersion"])
-		}
-		return version, nil
-	}
-	errMessage, ok := results["errmsg"].(string)
+	version, ok := results["authSchemaVersion"].(int)
 	if !ok {
-		// errmsg will always be returned on error unless the command API changes
+		// very unlikely this will ever happen
 		return 0, fmt.Errorf(
-			"getParameter command returned non-string errmsg: %v",
+			"getParameter command returned non-numeric result: %v",
 			results["authSchemaVersion"])
 	}
-	// as a necessary hack, if the error message takes a certain form,
-	// we can infer version 1. This is because early versions of mongodb
-	// had no concept of an "auth schema version", so asking for the
-	// authSchemaVersion value will return a "no option found" or "no such cmd"
-	if errMessage == "no option found to get" ||
-		strings.HasPrefix(errMessage, "no such cmd") {
-		return 1, nil
-	}
-	return 0, fmt.Errorf(errMessage)
+	return version, nil
 }
