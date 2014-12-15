9dff2ecab4059123f9d092e366a109805190d07b
Keith Bostic <keith@wiredtiger.com>
2014-12-15 11:30:08 -0500

    Clean up run reporting.

--- test/format/recover.sh
+++ test/format/recover.sh
@@ -12,10 +12,14 @@ config=
 count=0
 while true; do
 	count=`expr $count + 1`
-	if test $runs -ne 0 -a $count -gt $runs; then
-		exit 0
+	if test $runs -eq 0; then
+		echo "recovery test: $count"
+	else
+		if test $count -gt $runs; then
+			exit 0
+		fi
+		echo "recovery test: $count of $runs"
 	fi
-	echo "recovery test: $count of $runs"
 
 	./t $config -q abort=1 timer=$timer
 
