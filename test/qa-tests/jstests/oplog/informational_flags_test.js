e46753de12773f9a46423f03559bc4b711da211a
Valeri Karpov <valeri.karpov@10gen.com>
2014-12-16 16:59:58 -0500

    TOOLS-238 add informational flag tests for mongooplog
    Former-commit-id: a81ced873965190ed9c67a11d26a07b85a95e541

---
+++ test/qa-tests/jstests/oplog/informational_flags_test.js
@@ -0,0 +1,27 @@
+if (typeof getToolTest === 'undefined') {
+  load('jstests/configs/plain_28.config.js');
+}
+
+(function() {
+  var CURRENT_MONGOD_RELEASE = '2.8';
+
+  var toolTest = getToolTest('oplogInformationalFlagTest');
+  var commonToolArgs = getCommonToolArguments();
+
+  var verifyFlagOutput = function(flag, expected) {
+    var args = ['oplog'].concat(commonToolArgs).concat(flag);
+    assert.eq(toolTest.runTool.apply(toolTest, args), 0,
+      'mongooplog should succeed with ' + flag);
+
+    var output = rawMongoProgramOutput();
+    
+    assert(output.indexOf(expected) !== -1,
+      'mongooplog ' + flag + ' should produce output that contains "' +
+      expected + "'");
+  };
+
+  verifyFlagOutput('--version', CURRENT_MONGOD_RELEASE);
+  verifyFlagOutput('--help', 'Usage:');
+
+  toolTest.stop();
+})();
