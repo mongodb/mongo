235f747e2df80d9899497595a2b649e7d6df8601
Mark Benvenuto <mark.benvenuto@mongodb.com>
2014-12-17 14:11:41 -0500

    snprintf - Implement a custom version of snprintf match the truncation behavior of C99 standard snprintf until MSVC supports snprintf.

--- src/os_win/os_vsnprintf.c
+++ src/os_win/os_vsnprintf.c
@@ -7,8 +7,6 @@
 
 #include "wt_internal.h"
 
-#undef vsnprintf
-
 _Check_return_opt_ int __cdecl _wt_vsnprintf(
     _Out_writes_(_MaxCount) char * _DstBuf,
     _In_ size_t _MaxCount,
@@ -17,12 +15,23 @@ _Check_return_opt_ int __cdecl _wt_vsnprintf(
 {
 	int len;
 
-	len = (size_t)vsnprintf(_DstBuf, _MaxCount, _Format, _ArgList);
+	/*
+	 * WiredTiger will call with length 0 to get the needed buffer size
+	 * We call the count only version in this case since vsnprintf_s assumes
+	 * length is greater than zero or else it triggers the invalid_parameter
+	 * handler.
+	 */
+	if (_MaxCount == 0) {
+		return _vscprintf(_Format, _ArgList);
+	}
+
+	len = (size_t)_vsnprintf_s(
+		_DstBuf, _MaxCount, _TRUNCATE, _Format, _ArgList);
 
 	/*
 	 * The MSVC implementation returns -1 on truncation instead of what
-	 * it would have written.  We could iteratively grow the buffer, or
-	 * just ask us how big a buffer they would like.
+	 * it would have written.  We could let callers iteratively grow the
+	 * buffer, or just ask us how big a buffer they would like.
 	 */
 	if (len == -1)
 		len = _vscprintf(_Format, _ArgList) + 1;
