633eb21e02a3d0faa9fdd574cd96e1e30eede238
mike o'brien <mpobrien005@gmail.com>
2014-12-18 14:31:26 -0500

    TOOLS-521 bsondump tests should run without SSL options
    Former-commit-id: 348d1cc84e5e53972c4263d6b171dc20d1a125e9

--- test/qa-tests/jstests/bson/bsondump_options.js
+++ test/qa-tests/jstests/bson/bsondump_options.js
@@ -2,41 +2,41 @@
 
 (function(){
     var sampleFilepath = "jstests/bson/testdata/sample.bson"
-    var x = runMongoProgram( "bsondump", "--type=fake", sampleFilepath);
+    var x = _runMongoProgram( "bsondump", "--type=fake", sampleFilepath);
     assert.neq(x, 0, "bsondump should exit with failure when given a non-existent type");
 
-    x = runMongoProgram( "bsondump", "jstests/bson/testdata/asdfasdfasdf");
+    x = _runMongoProgram( "bsondump", "jstests/bson/testdata/asdfasdfasdf");
     assert.neq(x, 0, "bsondump should exit with failure when given a non-existent file");
 
-    x = runMongoProgram( "bsondump", "--noobjcheck", sampleFilepath);
+    x = _runMongoProgram( "bsondump", "--noobjcheck", sampleFilepath);
     assert.neq(x, 0, "bsondump should exit with failure when given --noobjcheck");
 
-    x = runMongoProgram( "bsondump", "--collection", sampleFilepath);
+    x = _runMongoProgram( "bsondump", "--collection", sampleFilepath);
     assert.neq(x, 0, "bsondump should exit with failure when given --collection");
 
-    x = runMongoProgram( "bsondump", sampleFilepath, sampleFilepath);
+    x = _runMongoProgram( "bsondump", sampleFilepath, sampleFilepath);
     assert.neq(x, 0, "bsondump should exit with failure when given multiple files");
    
-    x = runMongoProgram( "bsondump", "-vvvv", sampleFilepath);
+    x = _runMongoProgram( "bsondump", "-vvvv", sampleFilepath);
     assert.eq(x, 0, "bsondump should exit with success when given verbosity");
-    x = runMongoProgram( "bsondump", "--verbose", sampleFilepath);
+    x = _runMongoProgram( "bsondump", "--verbose", sampleFilepath);
     assert.eq(x, 0, "bsondump should exit with success when given verbosity");
 
     clearRawMongoProgramOutput()
-    x = runMongoProgram( "bsondump", "--quiet", sampleFilepath);
+    x = _runMongoProgram( "bsondump", "--quiet", sampleFilepath);
     assert.eq(x, 0, "bsondump should exit with success when given --quiet");
     var results = rawMongoProgramOutput();
     assert.eq(results.search("found"), -1, "only the found docs should be printed when --quiet is used");
     assert.gt(results.search("I am a string"), -1, "found docs should still be printed when --quiet is used");
 
     clearRawMongoProgramOutput()
-    x = runMongoProgram( "bsondump", "--help");
+    x = _runMongoProgram( "bsondump", "--help");
     assert.eq(x, 0, "bsondump should exit with success when given --help");
     var results = rawMongoProgramOutput();
     assert.gt(results.search("Usage"), -1, "help text should be printed when given --help");
 
     clearRawMongoProgramOutput()
-    x = runMongoProgram( "bsondump", "--version");
+    x = _runMongoProgram( "bsondump", "--version");
     assert.eq(x, 0, "bsondump should exit with success when given --version");
     var results = rawMongoProgramOutput();
     assert.gt(results.search("version"), -1, "version info should be printed when given --version");
--- test/qa-tests/jstests/bson/deep_nested.js
+++ test/qa-tests/jstests/bson/deep_nested.js
@@ -1,8 +1,8 @@
 // This test checks that bsondump can handle a deeply nested document without breaking
 
 (function(){
-  var x = runMongoProgram( "bsondump", "--type=json", "jstests/bson/testdata/deep_nested.bson");
+  var x = _runMongoProgram( "bsondump", "--type=json", "jstests/bson/testdata/deep_nested.bson");
   assert.eq(x, 0, "bsondump should exit successfully with 0");
-  var x = runMongoProgram( "bsondump", "--type=debug", "jstests/bson/testdata/deep_nested.bson");
+  var x = _runMongoProgram( "bsondump", "--type=debug", "jstests/bson/testdata/deep_nested.bson");
   assert.eq(x, 0, "bsondump should exit successfully with 0");
 })();
