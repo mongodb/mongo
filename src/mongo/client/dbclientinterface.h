357cf5e9029db85ce36cd6c7ef181edcc142f493
Spencer T Brody <spencer@mongodb.com>
2014-12-19 15:16:02 -0500

    SERVER-16237 Don't check shard version if the replica set monitor knows the primary is down

--- src/mongo/client/dbclientinterface.h
+++ src/mongo/client/dbclientinterface.h
@@ -1309,6 +1309,7 @@ namespace mongo {
         }
 
         string getServerAddress() const { return _serverString; }
+        const HostAndPort& getServerHostAndPort() const { return _server; }
 
         virtual void killCursor( long long cursorID );
         virtual bool callRead( Message& toSend , Message& response ) { return call( toSend , response ); }
