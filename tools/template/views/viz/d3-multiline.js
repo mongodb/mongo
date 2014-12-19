3ef84182e9676faa10171b634b6e0c6291325da3
Thomas Rueckstiess <thomas@rueckstiess.net>
2014-12-19 16:08:32 +1100

    added crosshair and focus.

--- tools/template/views/viz/d3-multiline.js
+++ tools/template/views/viz/d3-multiline.js
@@ -91,10 +91,52 @@ module.exports = function(opts) {
 
   } // redraw
 
+  function findClosest(mx, series) {
+    var mxi = x.invert(mx);
+    var i = bisect(series.data, mxi);
+    var d0 = series.data[i - 1];
+    var d1 = series.data[i];
+    return (mxi - d0.x > d1.x - mxi) ? d1 : d0;
+  }
+
+  function mousemove() {
+    var mouse = d3.mouse(this);
+    var mx = mouse[0], 
+        my = mouse[1];
+
+    var dArr = series.map(function (serie) { return findClosest(mx, serie); });
+    var dists = dArr.map(function (d) {
+      return Math.sqrt(Math.pow(mx-x(d.x), 2) + Math.pow(my-y(d.y), 2));
+    })
+    var i = dists.indexOf(Math.min.apply(Math, dists));
+    var serie = series[i];
+    var d = dArr[i];
+
+    focus.attr("transform", "translate(" + x(d.x) + "," + y(d.y) + ")");
+
+    focus.select("circle")
+      .attr('stroke', serie.color);
+
+    focus.select("text.name")
+      .text(serie.name)
+      .attr('fill', serie.color);
+    
+    focus.select("text.value")
+      .text(d3.format(",")(d.y))
+      .attr('fill', serie.color);
+
+    crosshairX
+      .attr("y1", y(d.y))
+      .attr("y2", y(d.y));
+
+    crosshairY
+      .attr("x1", x(d.x))
+      .attr("x2", x(d.x));
+  }
 
   // --- initial setup (only called once) ---
   var margin = {
-      top: 20,
+      top: 80,
       right: 380,
       bottom: 60,
       left: 100
@@ -105,6 +147,8 @@ module.exports = function(opts) {
     el = opts.el;
 
   var options, series; // initialized in redraw
+
+  var bisect = d3.bisector(function(d) { return d.x; }).left;
 
   // // no data to plot    
   // if (series.length === 0) return;
@@ -135,6 +179,58 @@ module.exports = function(opts) {
 
   svg.append("g")
     .attr("class", "y axis");
+
+  var crosshairX = svg.append("line")
+    .style("stroke", "#ddd")
+    .style("display", "none")
+    .attr("class", 'x')
+    .attr("x1", 0)
+    .attr("x2", width)
+
+  var crosshairY = svg.append("line")
+    .style("stroke", "#ddd")
+    .style("display", "none")
+    .attr("class", 'x')
+    .attr("y1", 0)
+    .attr("y2", height);
+
+  // focus
+  var focus = svg.append("g")
+    .attr("class", "focus")
+    .style("display", "none");
+
+  focus.append("circle")
+    .attr("r", 5)
+    .attr("fill", "none")
+    .attr("stroke-width", 1);
+
+  focus.append("text")
+    .attr("class", "name")
+    .attr("x", 9)
+    .attr("dy", "-.8em");
+  
+  focus.append("text")
+    .attr("class", "value")
+    .attr("font-size", "20px")
+    .attr("x", 9)
+    .attr("dy", "-1.2em");
+
+  // rect for mouseover
+  svg.append("rect")
+    .attr("width", width)
+    .attr("height", height)
+    .style("opacity", 0.)
+    .on("mouseover", function() { 
+      focus.style("display", null); 
+      crosshairX.style("display", null); 
+      crosshairY.style("display", null); 
+    })
+    .on("mouseout", function() { 
+      focus.style("display", "none"); 
+      crosshairX.style("display", "none"); 
+      crosshairY.style("display", "none"); 
+    })
+    .on("mousemove", mousemove);
 
   var paths, circles;  // initialized in redraw
 
