ff8697f4e848052b7fb139abbae6f5a42ae0a9ec
Thomas Rueckstiess <thomas@rueckstiess.net>
2014-12-28 10:51:29 +1100

    made menu toggleable for mobile devices

--- tools/template/views/app.js
+++ tools/template/views/app.js
@@ -7,15 +7,25 @@ var AmpersandView = require('ampersand-view'),
 var AppView = module.exports = AmpersandView.extend({
   template: require('./templates/app.jade'),
   props: {
-    chartView: 'object'
+    chartView: 'object',
+    menuOpen: {
+      type: 'boolean',
+      default: false
+    }
   },
   events: {
-    'click .navbar-right label': 'navButtonClicked'
+    'click .navbar-right label': 'navButtonClicked',
+    'click [data-hook=menu-toggle]': 'menuToggled'
   },
   bindings: {
     'model.chart.subSampled': {
       type: 'toggle',
       hook: 'sample-warning'
+    },
+    'menuOpen': {
+      type: 'booleanClass',
+      selector: '#wrapper',
+      name: 'active'
     }
   },
   subviews: {
@@ -55,26 +65,9 @@ var AppView = module.exports = AmpersandView.extend({
     this.model.chart[name] = value;
     this.model.chart.recalcXDomain = (name === 'xSetting');
     this.chartView.render();
+  },
+  menuToggled: function (event) {
+    this.toggle('menuOpen');
   }
-
-  // navButtonClicked: function (event) {
-  //   var $target = $(event.target);
-  //   var text = $target.text();
-
-  //   if (text === 'linear' || text === 'log-scale') {
-  //     this.model.yLinear = (text === 'linear');
-  //     $(this.queryByHook('linear')).removeClass('active');
-  //     $(this.queryByHook('absolute')).removeClass('active');
-
-  //   }
-
-
-  //   switch (text) {
-  //     case 'linear': this.model.yLinear = true; break;
-  //     case 'log-scale': this.model.yLinear = false; break;
-  //     case 'relative': this.model.xRelative = true; break;
-  //     case 'absolute': this.model.xRelative = false; break;
-  //   }
-  // }
 
 });
