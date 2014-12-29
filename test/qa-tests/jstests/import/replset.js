09944bdabf12891cc02b102c47d9297e1acefca4
Wisdom Omuya <deafgoat@gmail.com>
2014-12-29 13:01:48 -0500

    TOOLS-243: update replset.js test

--- test/qa-tests/jstests/import/replset.js
+++ test/qa-tests/jstests/import/replset.js
@@ -3,39 +3,31 @@
 
     var toolTest = new ToolTest('import_repl');
 
-    var replset1 = new ReplSetTest({nodes:3, name:"importtest"})
-    replset1.startSet()
-    replset1.initiate()
+    var replset1 = new ReplSetTest({nodes:3, name:'importtest'});
+    replset1.startSet();
+    replset1.initiate();
 
-    var primary = replset1.getPrimary()
-    var secondary = replset1.getURL()
+    var primary = replset1.getPrimary();
+    var secondary = replset1.getSecondary();
 
-    var db = primary.getDB("import_repl_test")
-    var ret = toolTest.runTool("import", "--file",
-      "jstests/import/testdata/basic.json",
-      "--db", db.getName(),
-      "--collection", db.c.getName(), 
-      "--host", secondary.host + ":" + secondary.port)
+    var db = primary.getDB('import_repl_test');
 
     // trying to write to the secondary should fail
-    assert.neq(ret, 0, "writing to secondary should fail")
+    assert.neq(runMongoProgram.apply(this, ['mongoimport', '--file', 'jstests/import/testdata/basic.json', '--db', db.getName(), '--collection', db.c.getName(), '--host', secondary.host]), 0, "writing to secondary should fail");
+
+    assert.eq(db.c.count(), 0, 'database not empty');
 
     // now import using the primary
-    var ret = toolTest.runTool("import", "--file",
-      "jstests/import/testdata/basic.json",
-      "--db", db.getName(),
-      "--collection", db.c.getName(), 
-      "--host", primary.host + ":" + primary.port)
-    assert.eq(ret, 0, "writing to primary should succeed")
-    db.dropDatabase()
+    assert.eq(runMongoProgram.apply(this, ['mongoimport', '--file', 'jstests/import/testdata/basic.json', '--db', db.getName(), '--collection', db.c.getName(), '--host', primary.host]), 0, "writing to primary should succeed");
+
+    assert.neq(db.c.count(), 0, 'database unexpectedly empty on primary');
+
+    db.dropDatabase();
 
     // import using the secondary but include replset name, should succeed
-    var ret = toolTest.runTool("import", "--file",
-      "jstests/import/testdata/basic.json",
-      "--db", db.getName(),
-      "--collection", db.c.getName(), 
-      "--host", replset1.name + "/" + secondary.host + ":" + secondary.port)
-    assert.eq(ret, 0, "writing to secondary with replset name should succeed")
+    assert.eq(runMongoProgram.apply(this, ['mongoimport', '--file', 'jstests/import/testdata/basic.json', '--db', db.getName(), '--collection', db.c.getName(), '--host', replset1.name + "/" + secondary.host]), 0, "writing to secondary with replset name should succeed");
+
+    assert.neq(db.c.count(), 0, 'database unexpectedly empty on secondary');
 
     toolTest.stop();
 }());
