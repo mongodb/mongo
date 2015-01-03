21e1811a4e92c72b095d93e5cadb1d3428605d42
Andrew Morrow <acm@mongodb.com>
2015-01-03 15:14:28 -0500

    SERVER-13256 Move observer to mutex.cpp and remove util.cpp

--- src/mongo/util/concurrency/mutex.cpp
+++ src/mongo/util/concurrency/mutex.cpp
@@ -1,6 +1,5 @@
-// @file util.cpp
-
-/*    Copyright 2009 10gen Inc.
+/**
+ *    Copyright (C) 2015 MongoDB Inc.
  *
  *    This program is free software: you can redistribute it and/or  modify
  *    it under the terms of the GNU Affero General Public License, version 3,
@@ -18,27 +17,18 @@
  *    code of portions of this program with the OpenSSL library under certain
  *    conditions as described in each individual source file and distribute
  *    linked combinations including the program with the OpenSSL library. You
- *    must comply with the GNU Affero General Public License in all respects
- *    for all of the code used other than as permitted herein. If you modify
- *    file(s) with this exception, you may extend this exception to your
- *    version of the file(s), but you are not obligated to do so. If you do not
- *    wish to do so, delete this exception statement from your version. If you
- *    delete this exception statement from all source files in the program,
- *    then also delete it in the license file.
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
  */
 
-#include "mongo/pch.h"
+#include "mongo/platform/basic.h"
 
-#include <iomanip>
-
-#include "mongo/platform/atomic_word.h"
 #include "mongo/util/concurrency/mutex.h"
-#include "mongo/util/goodies.h"
-#include "mongo/util/mongoutils/str.h"
-#include "mongo/util/stacktrace.h"
-#include "mongo/util/startup_test.h"
-#include "mongo/util/time_support.h"
-#include "mongo/util/timer.h"
 
 namespace mongo {
 
