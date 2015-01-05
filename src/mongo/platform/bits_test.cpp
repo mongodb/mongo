e136ca081381d7f0c52bd12f69552454771b497c
Mathias Stearn <mathias@10gen.com>
2015-01-05 11:42:10 -0500

    Add countLeadingZeros and countTrailingZeros to bits.h

--- src/mongo/platform/bits_test.cpp
+++ src/mongo/platform/bits_test.cpp
@@ -43,17 +43,25 @@ namespace mongo {
 #endif
     }
 
-#if defined(MONGO_SYSTEM_FFS)
-    TEST( BitsTest, FIRST_BIT_SET ) {
-        ASSERT_EQUALS( firstBitSet(0), mongo_firstBitSet(0) );
-        ASSERT_EQUALS( firstBitSet(0x1234), mongo_firstBitSet(0x1234) );
+    TEST( BitsTest_CountZeros, Constants ) {
+        ASSERT_EQUALS( countLeadingZeros64(0ull), 64 );
+        ASSERT_EQUALS( countTrailingZeros64(0ull), 64 );
+
+        ASSERT_EQUALS( countLeadingZeros64(0x1234ull), 64 - 13);
+        ASSERT_EQUALS( countTrailingZeros64(0x1234ull), 2);
+
+        ASSERT_EQUALS( countLeadingZeros64(0x1234ull << 32), 32 - 13);
+        ASSERT_EQUALS( countTrailingZeros64(0x1234ull << 32), 2 + 32);
+
+        ASSERT_EQUALS( countLeadingZeros64((0x1234ull << 32) | 0x1234ull), 32 - 13);
+        ASSERT_EQUALS( countTrailingZeros64((0x1234ull << 32) | 0x1234ull), 2);
+    }
         
+    TEST( BitsTest_CountZeros, EachBit ) {
         for ( int i = 0; i < 64; i++ ) {
             unsigned long long x = 1ULL << i;
-            ASSERT_EQUALS( firstBitSet(x), mongo_firstBitSet(x) );
-            x &= 0x5;
-            ASSERT_EQUALS( firstBitSet(x), mongo_firstBitSet(x) );
+            ASSERT_EQUALS( countLeadingZeros64(x), 64 - 1 - i );
+            ASSERT_EQUALS( countTrailingZeros64(x), i );
         }
     }
-#endif
 }
