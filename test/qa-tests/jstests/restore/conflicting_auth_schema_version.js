81216882d1d819c158759356814b0261c98b9f3b
mike o'brien <mpobrien005@gmail.com>
2015-01-06 16:55:47 -0500

    TOOLS-440 fix conflicting auth schema version test to use correct auth mechanisms

--- test/qa-tests/jstests/restore/conflicting_auth_schema_version.js
+++ test/qa-tests/jstests/restore/conflicting_auth_schema_version.js
@@ -7,12 +7,12 @@
     jsTest.log('Testing restoring a dump with a potentially conflicting'+
             ' authSchemaVersion in the database');
 
-    var runTest = function(sourceDBVersion, dumpVersion, restoreVersion, destDBVersion) {
+    var runTest = function(sourceDBVersion, dumpVersion, restoreVersion, destDBVersion, shouldSucceed) {
 
         jsTest.log('Running with sourceDBVersion=' + (sourceDBVersion || 'latest') +
                 ', dumpVersion=' + (dumpVersion || 'latest') + ', restoreVersion=' +
                 (restoreVersion || 'latest') + ', and destDBVersion=' + 
-                (destDBVersion || 'latest'));
+                (destDBVersion || 'latest') + ', expected to pass=' + shouldSucceed);
 
         var toolTest = new ToolTest('conflicting_auth_schema_version', 
             { binVersion: sourceDBVersion, auth: '' });
@@ -38,7 +38,12 @@
                 ],
             }
         );
-        adminDB.auth('admin', 'password');
+        var authInfo ={user:'admin', pwd:'password'}
+        if(sourceDBVersion == "2.6"){
+          authInfo.mechanism = "MONGODB-CR"
+        }
+        assert.eq(1, adminDB.auth(authInfo));
+
 
         // add some data
         for (var i = 0; i < 10; i++) {
@@ -48,9 +53,13 @@
         assert.eq(10, testDB.data.count());
 
         // dump all the data
-        var ret = toolTest.runTool('dump' + (dumpVersion ? ('-'+dumpVersion) : ''),
+        args = ['dump' + (dumpVersion ? ('-'+dumpVersion) : ''),
                 '--out', dumpTarget, '--username', 'admin',
-                '--password', 'password');
+                '--password', 'password']
+        if(sourceDBVersion == "2.6") {
+          args.push("--authenticationMechanism=MONGODB-CR")
+        }
+        var ret = toolTest.runTool.apply(toolTest, args)
         assert.eq(0, ret);
 
         // restart the mongod, with a clean db path
@@ -77,23 +86,31 @@
                 ],
             }
         );
-        adminDB.auth('admin28', 'password');
+
+        var authInfoDest ={user:'admin28', pwd:'password'}
+        if(destDBVersion == "2.6"){
+          authInfoDest.mechanism = "MONGODB-CR"
+        }
+        assert.eq(1, adminDB.auth(authInfoDest));
 
         // do a full restore
         ret = toolTest.runTool('restore' + (restoreVersion ? ('-'+restoreVersion) : ''), 
             '--username', 'admin28', '--password', 
             'password', '--stopOnError', dumpTarget);
-        assert.eq(0, ret);
-
-        // make sure the data and users are all there
-        assert.eq(10, testDB.data.count());
-        for (var i = 0; i < 10; i++) {
-            assert.eq(1, testDB.data.count({ _id: i }));
+        if(shouldSucceed){
+          assert.eq(0, ret);
+          // make sure the data and users are all there
+          assert.eq(10, testDB.data.count());
+          for (var i = 0; i < 10; i++) {
+              assert.eq(1, testDB.data.count({ _id: i }));
+          }
+          var users = adminDB.getUsers();
+          assert.eq(2, users.length);
+          assert(users[0].user === 'admin' || users[1].user === 'admin');
+          assert(users[0].user === 'admin28' || users[1].user === 'admin28');
+        }else{
+          assert.neq(0, ret);
         }
-        var users = adminDB.getUsers();
-        assert.eq(2, users.length);
-        assert(users[0].user === 'admin' || users[1].user === 'admin');
-        assert(users[0].user === 'admin28' || users[1].user === 'admin28');
 
         // success
         toolTest.stop();
@@ -101,11 +118,11 @@
     };
 
     // 'undefined' triggers latest
-    runTest('2.6', '2.6', undefined, '2.6');
-    runTest('2.6', '2.6', undefined, undefined);
-    runTest('2.6', undefined, undefined, undefined);
-    runTest(undefined, '2.6', undefined, '2.6');
-    runTest(undefined, undefined, undefined, '2.6');
-    runTest(undefined, undefined, undefined, undefined);
+    runTest('2.6', '2.6', undefined, '2.6', true);
+    runTest('2.6', '2.6', undefined, undefined, true);
+    runTest('2.6', undefined, undefined, undefined, true);
+    //runTest(undefined, '2.6', undefined, '2.6');
+    runTest(undefined, undefined, undefined, '2.6', false);
+    runTest(undefined, undefined, undefined, undefined, true);
 
 }());
