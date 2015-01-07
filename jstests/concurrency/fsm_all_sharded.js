7c7060d9e301de69a65d33f5f771454f9d807353
alabid <alabidan@gmail.com>
2015-01-07 17:49:47 -0500

    SERVER-16648 Added blacklists to fsm_*js runners due to MongoDB restrictions or known bugs

--- jstests/concurrency/fsm_all_sharded.js
+++ jstests/concurrency/fsm_all_sharded.js
@@ -1,9 +1,45 @@
+'use strict';
+
 load('jstests/concurrency/fsm_libs/runner.js');
 
 var dir = 'jstests/concurrency/fsm_workloads';
 
 var blacklist = [
-    'indexed_insert_multikey.js' // SERVER-16143
+    // Disabled due to known bugs
+    'agg_match.js', // SERVER-3645 .count() can be wrong on sharded collections
+
+    // Disabled due to MongoDB restrictions and/or workload restrictions
+
+    // These workloads sometimes trigger 'Could not lock auth data update lock'
+    // errors because the AuthorizationManager currently waits for only five
+    // seconds to acquire the lock for authorization documents
+    'auth_create_role.js',
+    'auth_create_user.js',
+    'auth_drop_role.js',
+    'auth_drop_user.js', // SERVER-16739 OpenSSL libcrypto crash
+
+    // These workloads are disabled because of recent changes in capped
+    // collection behavior with wiredTiger (see: SERVER-16235)
+    'create_capped_collection.js',
+    'create_capped_collection_maxdocs.js',
+
+    'agg_group_external.js', // uses >100MB of data, and is flaky
+    'agg_sort_external.js', // uses >100MB of data, and is flaky
+    'findAndModify_remove.js', // our findAndModify queries lack shard keys
+    'findAndModify_update.js', // our findAndModify queries lack shard keys
+    'findAndModify_update_collscan.js', // our findAndModify queries lack shard keys
+    'findAndModify_update_grow.js', // our findAndModify queries lack shard keys
+    'findAndModify_upsert.js', // our findAndModify queries lack shard keys
+    'findAndModify_upsert_collscan.js', // our findAndModify queries lack shard keys
+    'group.js', // the group command cannot be issued against a sharded cluster
+    'group_cond.js', // the group command cannot be issued against a sharded cluster
+    'indexed_insert_eval.js', // eval doesn't work with sharded collections
+    'indexed_insert_eval_nolock.js', // eval doesn't work with sharded collections
+    'remove_single_document.js', // our .remove(query, {justOne: true}) calls lack shard keys
+    'remove_single_document_eval.js', // eval doesn't work with sharded collections
+    'update_simple_eval.js', // eval doesn't work with sharded collections
+    'update_simple_eval_nolock.js', // eval doesn't work with sharded collections
+    'update_upsert_multi.js', // our update queries lack shard keys
 ].map(function(file) { return dir + '/' + file; });
 
 // SERVER-16196 re-enable executing workloads against sharded clusters
