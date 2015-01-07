cd1b99e67478a7328e17ba18e08ee932eb94da1d
Wisdom Omuya <deafgoat@gmail.com>
2015-01-07 12:25:01 -0500

    TOOLS-378: clearly separate xcopy flags

--- set_gopath.bat
+++ set_gopath.bat
@@ -1,6 +1,6 @@
 @echo off
 set TOOLSPKG=%cd%\.gopath\src\github.com\mongodb\mongo-tools
-for %%t in (bsondump, common, mongostat, mongofiles, mongoexport, mongoimport, mongorestore, mongodump, mongotop, mongooplog) do echo d | xcopy %cd%\%%t %TOOLSPKG%\%%t /yes
+for %%t in (bsondump, common, mongostat, mongofiles, mongoexport, mongoimport, mongorestore, mongodump, mongotop, mongooplog) do echo d | xcopy %cd%\%%t %TOOLSPKG%\%%t /Y /E /S
 REM copy vendored libraries to GOPATH
-for /f %%v in ('dir /b /a:d "%cd%\vendor\src\*"') do echo d | xcopy %cd%\vendor\src\%%v %cd%\.gopath\src\%%v /yes
+for /f %%v in ('dir /b /a:d "%cd%\vendor\src\*"') do echo d | xcopy %cd%\vendor\src\%%v %cd%\.gopath\src\%%v /Y /E /S
 set GOPATH=%cd%\.gopath;%cd%\vendor
