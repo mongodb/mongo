5c9eb2d631798f8ac913511ffccbd4a4bf8027a0
Benety Goh <benety@mongodb.com>
2015-01-14 13:56:16 -0500

    SERVER-16243 db.serverStatus().dur should be generated only for mmapv1 when journaling is enabled

--- jstests/noPassthrough/server_status.js
+++ jstests/noPassthrough/server_status.js
@@ -22,4 +22,27 @@
                tojson(serverStatus));
     }
     stopMongod(port);
+
+    // 'dur' is mmapv1-specific and should only be generated when journaling is enabled.
+    mongo = startMongodEmpty('--port', port, '--dbpath', dbpath, '--smallfiles', '--journal');
+    testDB = mongo.getDB('test');
+    serverStatus = assert.commandWorked(testDB.serverStatus());
+    if (serverStatus.storageEngine.name == 'mmapv1') {
+        assert(serverStatus.dur,
+               'mmapv1 db.serverStatus() result must contain "dur" document: ' +
+               tojson(serverStatus));
+    }
+    else {
+        assert(!serverStatus.dur,
+               'Unexpected "dur" document in non-mmapv1 db.serverStatus() result: ' +
+               tojson(serverStatus));
+    }
+    stopMongod(port);
+    mongo = startMongodEmpty('--port', port, '--dbpath', dbpath, '--smallfiles', '--nojournal');
+    testDB = mongo.getDB('test');
+    serverStatus = assert.commandWorked(testDB.serverStatus());
+    assert(!serverStatus.dur,
+           'Unexpected "dur" document in db.serverStatus() result when journaling is disabled: ' +
+           tojson(serverStatus));
+    stopMongod(port);
 }());
