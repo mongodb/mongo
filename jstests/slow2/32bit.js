b42f74998bd3d4aa3bfc940999d0d3d2648e0e6b
Matt Kangas <matt.kangas@mongodb.com>
2015-01-23 18:20:11 -0500

    SERVER-17233 32bit.js fix whitespace
    (cherry picked from commit 56fa6f8021da4a3eba4f494643fe33cdafd5dc78)

--- jstests/slow2/32bit.js
+++ jstests/slow2/32bit.js
@@ -13,9 +13,10 @@ function f() {
     var mydb = db.getSisterDB( "test_32bit" );
     mydb.dropDatabase();
 
-    while( 1 ) {
-        if( pass >= 2 )
-        break;
+    while (1) {
+        if (pass >= 2) {
+            break;
+        }
         print("32bit.js PASS #" + pass);
         pass++;
 
@@ -27,8 +28,9 @@ function f() {
         t.ensureIndex({a:1});
         t.ensureIndex({b:1}, true);
         t.ensureIndex({x:1});
-        if( Math.random() < 0.3 )
-        t.ensureIndex({c:1});
+        if (Math.random() < 0.3) {
+            t.ensureIndex({c:1});
+        }
         t.ensureIndex({d:1});
         t.ensureIndex({e:1});
         t.ensureIndex({f:1});
@@ -43,65 +45,67 @@ function f() {
         a = 0;
         c = 'kkk';
         var start = new Date();
-        while( 1 ) {
-        b = Math.random(seed);
-        d = c + -a;
+
+        while (1) {
+            b = Math.random(seed);
+            d = c + -a;
             f = Math.random(seed) + a;
             a++;
-        cc = big;
-            if( Math.random(seed) < .1 )
-        cc = null;
-        var res = t.insert({ a: a, b: b, c: cc, d: d, f: f });
-        if( Math.random(seed) < 0.01 ) {
-            if (res.hasWriteError()) {
-                // Presumably we have mmap error on 32 bit. try a few more manipulations
-                // attempting to break things.
-                t.insert({a:33,b:44,c:55,d:66,f:66});
-                t.insert({a:33,b:44000,c:55,d:66});
-                t.insert({a:33,b:440000,c:55});
-                t.insert({a:33,b:4400000});
-                t.update({a:20},{'$set':{c:'abc'}});
-                t.update({a:21},{'$set':{c:'aadsfbc'}});
-                t.update({a:22},{'$set':{c:'c'}});
-                t.update({a:23},{'$set':{b:cc}});
-                t.remove({a:22});
-                break;
+            cc = big;
+            if (Math.random(seed) < .1) {
+                cc = null;
             }
+            var res = t.insert({ a: a, b: b, c: cc, d: d, f: f });
+            if (Math.random(seed) < 0.01) {
+                if (res.hasWriteError()) {
+                    // Presumably we have mmap error on 32 bit. try a few more manipulations
+                    // attempting to break things.
+                    t.insert({a:33,b:44,c:55,d:66,f:66});
+                    t.insert({a:33,b:44000,c:55,d:66});
+                    t.insert({a:33,b:440000,c:55});
+                    t.insert({a:33,b:4400000});
+                    t.update({a:20},{'$set':{c:'abc'}});
+                    t.update({a:21},{'$set':{c:'aadsfbc'}});
+                    t.update({a:22},{'$set':{c:'c'}});
+                    t.update({a:23},{'$set':{b:cc}});
+                    t.remove({a:22});
+                    break;
+                }
 
-            t.remove({a:a});
-            t.remove({b:Math.random(seed)});
-            t.insert({e:1});
-            t.insert({f:'aaaaaaaaaa'});
+                t.remove({a:a});
+                t.remove({b:Math.random(seed)});
+                t.insert({e:1});
+                t.insert({f:'aaaaaaaaaa'});
 
-                if( Math.random() < 0.00001 ) { print("remove cc"); t.remove({c:cc}); }
-                if( Math.random() < 0.0001 ) { print("update cc"); t.update({c:cc},{'$set':{c:1}},false,true); }
-                if( Math.random() < 0.00001 ) { print("remove e"); t.remove({e:1}); }
-        }
-        if (a == 20000 ) {
-            var delta_ms = (new Date())-start;
-            // 2MM / 20000 = 100.  1000ms/sec.
-            var eta_secs = delta_ms * (100 / 1000);
-            print("32bit.js eta_secs:" + eta_secs);
-            if( eta_secs > 1000 ) {
-                print("32bit.js machine is slow, stopping early. a:" + a);
-                mydb.dropDatabase();
-                return;
+                if (Math.random() < 0.00001) { print("remove cc"); t.remove({c:cc}); }
+                if (Math.random() < 0.0001) { print("update cc"); t.update({c:cc},{'$set':{c:1}},false,true); }
+                if (Math.random() < 0.00001) { print("remove e"); t.remove({e:1}); }
             }
-        }
-        if( a % 100000 == 0 ) {
-            print(a);
-            // on 64 bit we won't error out, so artificially stop.  on 32 bit we will hit
-            // mmap limit ~1.6MM but may vary by a factor of 2x by platform
-            if( a >= 2200000 ) {
+            if (a == 20000 ) {
+                var delta_ms = (new Date())-start;
+                // 2MM / 20000 = 100.  1000ms/sec.
+                var eta_secs = delta_ms * (100 / 1000);
+                print("32bit.js eta_secs:" + eta_secs);
+                if (eta_secs > 1000) {
+                    print("32bit.js machine is slow, stopping early. a:" + a);
                     mydb.dropDatabase();
-            return;
-        }
+                    return;
+                }
+            }
+            if (a % 100000 == 0) {
+                print(a);
+                // on 64 bit we won't error out, so artificially stop.  on 32 bit we will hit
+                // mmap limit ~1.6MM but may vary by a factor of 2x by platform
+                if (a >= 2200000) {
+                    mydb.dropDatabase();
+                    return;
+                }
             }
         }
         print("count: " + t.count());
 
         var res = t.validate();
-        if( !res.valid ) {
+        if (!res.valid) {
             print("32bit.js FAIL validating");
             print(res.result);
             printjson(res);
