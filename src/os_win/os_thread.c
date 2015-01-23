3abb99d58aaa46b0b3fcd338293a668422e3fcaf
Mark Benvenuto <mark.benvenuto@mongodb.com>
2015-01-23 15:05:16 -0500

    Close Thread Handle after thread join on Windows

--- src/os_win/os_thread.c
+++ src/os_win/os_thread.c
@@ -33,10 +33,15 @@ __wt_thread_join(WT_SESSION_IMPL *session, wt_thread_t tid)
 {
 	WT_DECL_RET;
 
-	if ((ret = WaitForSingleObject(tid, INFINITE)) == WAIT_OBJECT_0)
-		return (0);
+	if ((ret = WaitForSingleObject(tid, INFINITE)) != WAIT_OBJECT_0)
+		/*
+		 * If we fail to wait, we will leak handles so do not continue
+		 */
+		WT_PANIC_RET(session, WT_PANIC, "Wait for thread join failed");
 
-	WT_RET_MSG(session, ret, "WaitForSingleObject");
+	CloseHandle(tid);
+
+	return (0);
 }
 
 /*
