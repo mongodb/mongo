7acd20177afbf9de0ecf5fd6942ff77d2c5f12e0
Gevorg Voskanyan <v_gevorg@yahoo.com>
2015-02-07 14:34:01 +0400

    fixed compile error

--- src/mongo/db/catalog/collection.cpp
+++ src/mongo/db/catalog/collection.cpp
@@ -355,7 +355,7 @@ namespace mongo {
         // object is removed from all indexes.
         StatusWith<RecordId> newLocation = _recordStore->updateRecord( txn,
                                                                       oldLocation,
-                                                                      RecordData(objOld.objdata(), objOld.objsize()),
+                                                                      RecordData(objOld.value().objdata(), objOld.value().objsize()),
                                                                       objNew.objdata(),
                                                                       objNew.objsize(),
                                                                       _enforceQuota( enforceQuota ),
