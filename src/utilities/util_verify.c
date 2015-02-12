c9ee93cbb7200623fa150e56de0fc324a1591ae0
Keith Bostic <keith@wiredtiger.com>
2015-02-12 16:20:34 -0500

    Another try at removing HAVE_DIAGNOSTIC requirement from the verify code (and this time, using a branch before merging, because not doing so was really, really stupid).

--- src/utilities/util_verify.c
+++ src/utilities/util_verify.c
@@ -10,18 +10,6 @@
 
 static int usage(void);
 
-#undef OPT_ARGS
-#undef USAGE_ARGS
-#ifdef HAVE_DIAGNOSTIC
-#define	OPT_ARGS	"d:"
-#define	USAGE_ARGS	\
-	"[-d dump_address | dump_blocks | dump_offsets=#,# | "\
-	"dump_pages | dump_shape] uri"
-#else
-#define	OPT_ARGS	""
-#define	USAGE_ARGS	"uri"
-#endif
-
 int
 util_verify(WT_SESSION *session, int argc, char *argv[])
 {
@@ -32,7 +20,7 @@ util_verify(WT_SESSION *session, int argc, char *argv[])
 
 	dump_address = dump_blocks = dump_pages = dump_shape = 0;
 	config = dump_offsets = name = NULL;
-	while ((ch = __wt_getopt(progname, argc, argv, OPT_ARGS)) != EOF)
+	while ((ch = __wt_getopt(progname, argc, argv, "d:")) != EOF)
 		switch (ch) {
 		case 'd':
 			if (strcmp(__wt_optarg, "dump_address") == 0)
@@ -121,6 +109,8 @@ usage(void)
 	(void)fprintf(stderr,
 	    "usage: %s %s "
 	    "verify %s\n",
-	    progname, usage_prefix, USAGE_ARGS);
+	    progname, usage_prefix,
+	    "[-d dump_address | dump_blocks | "
+	    "dump_offsets=#,# | dump_pages | dump_shape] uri");
 	return (1);
 }
