ac7d4eac9eb895886086eb6f0b763147ec7f3b90
Spencer Jackson <spencer.jackson@mongodb.com>
2015-02-13 11:49:49 -0500

    SERVER-17278: Enforce BSON BinData length
    (cherry picked from commit 8ef2743189617343c5c4888aca34a9886d21e783)

--- src/mongo/bson/bson_validate.cpp
+++ src/mongo/bson/bson_validate.cpp
@@ -29,6 +29,7 @@
 
 #include <cstring>
 #include <deque>
+#include <limits>
 
 #include "mongo/base/data_view.h"
 #include "mongo/bson/bson_validate.h"
@@ -254,6 +255,8 @@ namespace mongo {
                 int sz;
                 if ( !buffer->readNumber<int>( &sz ) )
                     return makeError("invalid bson", idElem);
+                if ( sz < 0 || sz == std::numeric_limits<int>::max() )
+                    return makeError("invalid size in bson", idElem);
                 if ( !buffer->skip( 1 + sz ) )
                     return makeError("invalid bson", idElem);
                 return Status::OK();
