7e911dbd1227ebfde692b680a498835798d6e88d
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2015-02-19 10:25:09 -0500

    SERVER-17323 Instantiate storage engine before the Locker
    (cherry picked from commit 9fa0565f2b45cf1faebf3cb62c6777b7373dee08)
    
    Conflicts:
    	src/mongo/db/client.cpp

--- src/mongo/db/client.cpp
+++ src/mongo/db/client.cpp
@@ -130,7 +130,6 @@ namespace {
           _connectionId(p ? p->connectionId() : 0),
           _god(0),
           _txn(NULL),
-          _locker(newLocker()),
           _lastOp(0),
           _shutdown(false) {
 
@@ -294,6 +293,7 @@ namespace {
         }
     }
 
+
     void Client::Context::checkNotStale() const { 
         switch ( _client->_curOp->getOp() ) {
         case dbGetMore: // getMore's are special and should be handled else where
@@ -323,12 +323,20 @@ namespace {
     }
     
     Client::Context::~Context() {
-        DEV verify( _client == currentClient.get() );
+        DEV verify(_client == currentClient.get());
 
         // Lock must still be held
         invariant(_txn->lockState()->isLocked());
 
         _client->_curOp->recordGlobalTime(_txn->lockState()->isWriteLocked(), _timer.micros());
+    }
+
+    Locker* Client::getLocker() {
+        if (!_locker) {
+            _locker.reset(newLocker());
+        }
+
+        return _locker.get();
     }
 
     void Client::appendLastOp( BSONObjBuilder& b ) const {
