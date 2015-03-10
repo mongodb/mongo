300a825f5e59ac7bf9e6273caba3b59b603e9431
matt dannenberg <matt.dannenberg@10gen.com>
2015-03-10 11:53:54 -0400

    SERVER-5522 improve replHandshake log message

--- src/mongo/db/repl/master_slave.cpp
+++ src/mongo/db/repl/master_slave.cpp
@@ -395,7 +395,7 @@ namespace repl {
         BSONObj res;
         bool ok = conn->runCommand( "admin" , cmd.obj() , res );
         // ignoring for now on purpose for older versions
-        LOG( ok ? 1 : 0 ) << "replHandshake res not: " << ok << " res: " << res << endl;
+        LOG( ok ? 1 : 0 ) << "replHandshake result: " << res << endl;
         return true;
     }
 
