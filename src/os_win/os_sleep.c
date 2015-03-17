cb90547f82fe92975d215d066fed4ed2126c6eda
Susan LoVerso <sue@wiredtiger.com>
2015-03-17 12:09:11 -0400

    Fix microsecond to millisecond conversion if small value given. #1764

--- src/os_win/os_sleep.c
+++ src/os_win/os_sleep.c
@@ -15,5 +15,12 @@
 void
 __wt_sleep(uint64_t seconds, uint64_t micro_seconds)
 {
+	/*
+	 * If the caller wants a small pause, set to our
+	 * smallest granularity.
+	 */
+	if (seconds == 0 &&
+	    micro_seconds != 0 && micro_seconds < 1000)
+		micro_seconds = 1000;
 	Sleep(seconds * 1000 + micro_seconds / 1000);
 }
