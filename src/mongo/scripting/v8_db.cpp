b2ac8c83361c32a1ac9dc2dd6079f869ce752030
Spencer Jackson <spencer.jackson@mongodb.com>
2015-03-23 11:03:47 -0400

    SERVER-17647 Compute BinData length

--- src/mongo/scripting/v8_db.cpp
+++ src/mongo/scripting/v8_db.cpp
@@ -882,8 +882,9 @@ namespace mongo {
         // uassert if invalid base64 string
         string tmpBase64 = base64::decode(*utf);
         // length property stores the decoded length
-        it->ForceSet(scope->v8StringData("len"), v8::Number::New(tmpBase64.length()));
-        it->ForceSet(scope->v8StringData("type"), type);
+        it->ForceSet(scope->v8StringData("len"), v8::Number::New(tmpBase64.length()),
+                     v8::PropertyAttribute::ReadOnly);
+        it->ForceSet(scope->v8StringData("type"), type, v8::PropertyAttribute::ReadOnly);
         it->SetInternalField(0, args[1]);
 
         return it;
@@ -910,15 +911,14 @@ namespace mongo {
     v8::Handle<v8::Value> binDataToHex(V8Scope* scope, const v8::Arguments& args) {
         v8::Handle<v8::Object> it = args.This();
         verify(scope->BinDataFT()->HasInstance(it));
-        int len = v8::Handle<v8::Number>::Cast(it->Get(v8::String::New("len")))->Int32Value();
         verify(it->InternalFieldCount() == 1);
         string data = base64::decode(toSTLString(it->GetInternalField(0)));
         stringstream ss;
         ss.setf (ios_base::hex, ios_base::basefield);
         ss.fill ('0');
         ss.setf (ios_base::right, ios_base::adjustfield);
-        for(int i = 0; i < len; i++) {
-            unsigned v = (unsigned char) data[i];
+        for(std::string::iterator it = data.begin(); it != data.end(); ++it) {
+            unsigned v = (unsigned char) *it;
             ss << setw(2) << v;
         }
         return v8::String::New(ss.str().c_str());
