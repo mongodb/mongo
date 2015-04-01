cbc6bc3c72bf7472b112a6782c76ba2ea3fa1ad7
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2015-04-01 15:55:38 -0400

    SERVER-17723 Make the move_primary_ignore_sharded.js test more readable

--- jstests/sharding/moveprimary_ignore_sharded.js
+++ jstests/sharding/moveprimary_ignore_sharded.js
@@ -1,5 +1,4 @@
 // Checks that movePrimary doesn't move collections detected as sharded when it begins moving
-
 var st = new ShardingTest({ shards : 2, mongos : 2, verbose : 1 })
 
 // Stop balancer, otherwise mongosB may load information about the database non-deterministically
@@ -8,47 +7,36 @@ st.stopBalancer();
 var mongosA = st.s0
 var mongosB = st.s1
 
-st.shard0.getDB( "admin" ).runCommand({ setParameter : 1, logLevel : 2 })
-st.shard1.getDB( "admin" ).runCommand({ setParameter : 1, logLevel : 2 })
-
 var adminA = mongosA.getDB( "admin" )
 var adminB = mongosB.getDB( "admin" )
 
 var configA = mongosA.getDB( "config" )
+var configB = mongosB.getDB( "config" )
+
+// Populate some data
+assert.writeOK(mongosA.getCollection("foo.coll0").insert({ hello : "world" }));
+assert.writeOK(mongosA.getCollection("bar.coll0").insert({ hello : "world" }));
+assert.writeOK(mongosA.getCollection("foo.coll1").insert({ hello : "world" }));
+assert.writeOK(mongosA.getCollection("bar.coll1").insert({ hello : "world" }));
+assert.writeOK(mongosA.getCollection("foo.coll2").insert({ hello : "world" }));
+assert.writeOK(mongosA.getCollection("bar.coll2").insert({ hello : "world" }));
+
+// Enable sharding
+printjson( adminA.runCommand({ enableSharding : "foo" }) );
+printjson( adminA.runCommand({ enableSharding : "bar" }) );
 
 // Setup three collections per-db
 // 0 : not sharded
 // 1 : sharded
 // 2 : sharded but not seen as sharded by mongosB
-
-var collsFooA = []
-var collsFooB = []
-var collsBarA = []
-var collsBarB = []
-
-for( var i = 0; i < 3; i++ ){
-    
-    collsFooA.push( mongosA.getCollection( "foo.coll" + i ) )
-    collsFooB.push( mongosB.getCollection( "foo.coll" + i ) )
-    collsBarA.push( mongosA.getCollection( "bar.coll" + i ) )
-    collsBarB.push( mongosB.getCollection( "bar.coll" + i ) )
-
-    assert.writeOK(collsFooA[i].insert({ hello : "world" }));
-    assert.writeOK(collsBarA[i].insert({ hello : "world" }));
-}
-
-// Enable sharding
-printjson( adminA.runCommand({ enableSharding : collsFooA[0].getDB() + "" }) )
-printjson( adminA.runCommand({ enableSharding : collsBarA[0].getDB() + "" }) )
-
-printjson( adminA.runCommand({ shardCollection : collsFooA[1] + "", key : { _id : 1 } }) )
-printjson( adminA.runCommand({ shardCollection : collsFooA[2] + "", key : { _id : 1 } }) )
-printjson( adminA.runCommand({ shardCollection : collsBarA[1] + "", key : { _id : 1 } }) )
-printjson( adminA.runCommand({ shardCollection : collsBarA[2] + "", key : { _id : 1 } }) )
+printjson( adminA.runCommand({ shardCollection : "foo.coll1", key : { _id : 1 } }) );
+printjson( adminA.runCommand({ shardCollection : "foo.coll2", key : { _id : 1 } }) );
+printjson( adminA.runCommand({ shardCollection : "bar.coll1", key : { _id : 1 } }) );
+printjson( adminA.runCommand({ shardCollection : "bar.coll2", key : { _id : 1 } }) );
 
 // All collections are now on primary shard
-var fooPrimaryShard = configA.databases.findOne({ _id : collsFooA[0].getDB() + "" }).primary
-var barPrimaryShard = configA.databases.findOne({ _id : collsBarA[0].getDB() + "" }).primary
+var fooPrimaryShard = configA.databases.findOne({ _id : "foo" }).primary
+var barPrimaryShard = configA.databases.findOne({ _id : "bar" }).primary
 
 var shards = configA.shards.find().toArray()
 var fooPrimaryShard = fooPrimaryShard == shards[0]._id ? shards[0]  : shards[1]
@@ -56,25 +44,21 @@ var fooOtherShard = fooPrimaryShard._id == shards[0]._id ? shards[1]  : shards[0
 var barPrimaryShard = barPrimaryShard == shards[0]._id  ? shards[0] : shards[1] 
 var barOtherShard = barPrimaryShard._id == shards[0]._id  ? shards[1] : shards[0] 
 
-jsTest.log( "Setup collections for moveprimary test..." )
-st.printShardingStatus()
+st.printShardingStatus();
 
 jsTest.log( "Running movePrimary for foo through mongosA ..." )
 
 // MongosA should already know about all the collection states
-
-printjson( adminA.runCommand({ movePrimary : collsFooA[0].getDB() + "", to : fooOtherShard._id }) )
-
-jsTest.log( "Run!" )
+printjson( adminA.runCommand({ movePrimary : "foo", to : fooOtherShard._id }) )
 
 // All collections still correctly sharded / unsharded
-assert.neq( null, collsFooA[0].findOne() )
-assert.neq( null, collsFooA[1].findOne() )
-assert.neq( null, collsFooA[2].findOne() )
+assert.neq( null, mongosA.getCollection("foo.coll0").findOne() );
+assert.neq( null, mongosA.getCollection("foo.coll1").findOne() );
+assert.neq( null, mongosA.getCollection("foo.coll2").findOne() );
 
-assert.neq( null, collsFooB[0].findOne() )
-assert.neq( null, collsFooB[1].findOne() )
-assert.neq( null, collsFooB[2].findOne() )
+assert.neq( null, mongosB.getCollection("foo.coll0").findOne() );
+assert.neq( null, mongosB.getCollection("foo.coll1").findOne() );
+assert.neq( null, mongosB.getCollection("foo.coll2").findOne() );
 
 function realCollectionCount( mydb ) {
     var num = 0;
@@ -86,35 +70,23 @@ function realCollectionCount( mydb ) {
 }
 
 // All collections sane
-assert.eq( 2, realCollectionCount( new Mongo( fooPrimaryShard.host ).getDB( collsFooA[0].getDB() ) ) );
-assert.eq( 1, realCollectionCount( new Mongo( fooOtherShard.host ).getDB( collsFooA[0].getDB() ) ) );
+assert.eq( 2, realCollectionCount( new Mongo( fooPrimaryShard.host ).getDB( "foo" ) ) );
+assert.eq( 1, realCollectionCount( new Mongo( fooOtherShard.host ).getDB( "foo" ) ) );
 
-jsTest.log( "Running movePrimary for bar through mongosB ..." )
-
-// Make mongosB detect the first two collections in bar, but not third, to make sure 
-// we refresh our config state before doing dangerous things
-collsBarB[0].findOne()
-collsBarB[1].findOne()
-
-printjson( adminB.runCommand({ movePrimary : collsBarA[0].getDB() + "", to : barOtherShard._id }) )
-
-jsTest.log( "Run!" )
+jsTest.log( "Running movePrimary for bar through mongosB ..." );
+printjson( adminB.runCommand({ movePrimary : "bar", to : barOtherShard._id }) );
 
 // All collections still correctly sharded / unsharded
-assert.neq( null, collsBarA[0].findOne() )
-assert.neq( null, collsBarA[1].findOne() )
-assert.neq( null, collsBarA[2].findOne() )
+assert.neq( null, mongosA.getCollection("bar.coll0").findOne() );
+assert.neq( null, mongosA.getCollection("bar.coll1").findOne() );
+assert.neq( null, mongosA.getCollection("bar.coll2").findOne() );
 
-assert.neq( null, collsBarB[0].findOne() )
-assert.neq( null, collsBarB[1].findOne() )
-assert.neq( null, collsBarB[2].findOne() )
+assert.neq( null, mongosB.getCollection("bar.coll0").findOne() );
+assert.neq( null, mongosB.getCollection("bar.coll1").findOne() );
+assert.neq( null, mongosB.getCollection("bar.coll2").findOne() );
 
 // All collections sane
-assert.eq( 2, realCollectionCount( new Mongo( barPrimaryShard.host ).getDB( collsBarA[0].getDB() ) ) );
-assert.eq( 1, realCollectionCount( new Mongo( barOtherShard.host ).getDB( collsBarA[0].getDB() ) ) );
+assert.eq( 2, realCollectionCount( new Mongo( barPrimaryShard.host ).getDB( "bar" ) ) );
+assert.eq( 1, realCollectionCount( new Mongo( barOtherShard.host ).getDB( "bar" ) ) );
 
-st.stop()
-
-
-
-
+st.stop();
