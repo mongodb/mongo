3936e6af141666a9c6a29ce14acfd4dff3954321
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2015-04-09 17:41:31 -0400

    SERVER-17972 Fix basic.js/basicPlus.js and durability
    When run in parallel, the tests that this commit changes cause the 'db'
    variable to be modified and impact other tests.

--- jstests/core/indexes_on_indexes.js
+++ jstests/core/indexes_on_indexes.js
@@ -1,25 +1,27 @@
-// ensure an index cannot be created on system.indexes
-var t = db.getSiblingDB("indexes_on_indexes");
-t.dropDatabase();
-t.coll.insert({});
+// Ensure an index cannot be created on system.indexes
+(function() {
+    var t = db.getSiblingDB("indexes_on_indexes");
+    t.dropDatabase();
+    t.coll.insert({});
 
-printjson(t.system.indexes.getIndexes());
-assert.eq(t.system.indexes.getIndexes().length, 0);
+    printjson(t.system.indexes.getIndexes());
+    assert.eq(t.system.indexes.getIndexes().length, 0);
 
-print("trying via ensureIndex");
-assert.commandFailed(t.system.indexes.ensureIndex({_id:1}));
-printjson(t.system.indexes.getIndexes());
-assert.eq(t.system.indexes.getIndexes().length, 0);
+    print("trying via ensureIndex");
+    assert.commandFailed(t.system.indexes.ensureIndex({_id:1}));
+    printjson(t.system.indexes.getIndexes());
+    assert.eq(t.system.indexes.getIndexes().length, 0);
 
-print("trying via createIndex");
-assert.throws(t.system.indexes.createIndex({_id:1}));
-printjson(t.system.indexes.getIndexes());
-assert.eq(t.system.indexes.getIndexes().length, 0);
+    print("trying via createIndex");
+    assert.throws(t.system.indexes.createIndex({_id:1}));
+    printjson(t.system.indexes.getIndexes());
+    assert.eq(t.system.indexes.getIndexes().length, 0);
 
-print("trying via direct insertion");
-assert.throws(t.system.indexes.insert({ v:1,
-                                        key:{_id:1},
-                                        ns: "indexes_on_indexes.system.indexes",
-                                        name:"wontwork"}));
-printjson(t.system.indexes.getIndexes());
-assert.eq(t.system.indexes.getIndexes().length, 0);
+    print("trying via direct insertion");
+    assert.throws(t.system.indexes.insert({ v:1,
+                                            key:{_id:1},
+                                            ns: "indexes_on_indexes.system.indexes",
+                                            name:"wontwork"}));
+    printjson(t.system.indexes.getIndexes());
+    assert.eq(t.system.indexes.getIndexes().length, 0);
+}());
--- jstests/core/list_indexes_non_existent_ns.js
+++ jstests/core/list_indexes_non_existent_ns.js
@@ -1,17 +1,18 @@
 // Test the listIndexes command on non-existent collection.
+(function() {
+    var dbTest = db.getSiblingDB("list_indexes_non_existent_db");
+    assert.commandWorked(dbTest.dropDatabase());
 
-var db = db.getSiblingDB("list_indexes_non_existent_db");
-assert.commandWorked(db.dropDatabase());
+    var coll;
 
-var coll;
+    // Non-existent database
+    coll = dbTest.getCollection("list_indexes_non_existent_db");
+    assert.commandFailed(coll.runCommand("listIndexes"));
 
-// Non-existent database
-coll = db.getCollection("list_indexes_non_existent_db");
-assert.commandFailed(coll.runCommand("listIndexes"));
+    // Creates the actual database that did not exist till now
+    coll.insert({});
 
-// Creates the db
-coll.insert({});
-
-// Non-existent collection
-coll = db.getCollection("list_indexes_non_existent_collection");
-assert.commandFailed(coll.runCommand("listIndexes"));
+    // Non-existent collection
+    coll = dbTest.getCollection("list_indexes_non_existent_collection");
+    assert.commandFailed(coll.runCommand("listIndexes"));
+}());
