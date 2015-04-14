5ed5981b93dcdbd1164746d0e23f0a95bc02f8b3
Keith Bostic <keith@wiredtiger.com>
2015-04-14 08:13:07 -0400

    Fix the usage statement (options without arguments go first).

--- test/thread/t.c
+++ test/thread/t.c
@@ -299,8 +299,8 @@ usage(void)
 {
 	fprintf(stderr,
 	    "usage: %s "
-	    "[-FLS] [-C wiredtiger-config] [-k keys] [-l log]\n\t"
-	    "[-n ops] [-R readers] [-r runs] [-t f|r|v] [-v] [-W writers]\n",
+	    "[-FLSv] [-C wiredtiger-config] [-k keys] [-l log]\n\t"
+	    "[-n ops] [-R readers] [-r runs] [-t f|r|v] [-W writers]\n",
 	    progname);
 	fprintf(stderr, "%s",
 	    "\t-C specify wiredtiger_open configuration arguments\n"
