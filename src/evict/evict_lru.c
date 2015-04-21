6155c465a519612e16cec5feb9fcf79fd0121d7f
Alex Gorrod <alexg@wiredtiger.com>
2015-04-21 13:15:06 +1000

    Revert a change to split large in-memory pages.
    Refs WT-1890 WT-1896

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -666,7 +666,7 @@ __evict_clear_all_walks(WT_SESSION_IMPL *session)
  *	Evict a given page.
  */
 int
-__wt_evict_page(WT_SESSION_IMPL *session, WT_REF *ref, int inmem_split)
+__wt_evict_page(WT_SESSION_IMPL *session, WT_REF *ref)
 {
 	WT_DECL_RET;
 	WT_TXN *txn;
@@ -693,7 +693,7 @@ __wt_evict_page(WT_SESSION_IMPL *session, WT_REF *ref, int inmem_split)
 	WT_ASSERT(session,
 	    !F_ISSET(txn, TXN_HAS_ID) || !__wt_txn_visible(session, txn->id));
 
-	ret = __wt_evict(session, ref, inmem_split ? WT_EVICT_FORCE_SPLIT : 0);
+	ret = __wt_evict(session, ref, 0);
 	txn->isolation = saved_iso;
 
 	return (ret);
@@ -1232,8 +1232,7 @@ __evict_walk_file(WT_SESSION_IMPL *session, u_int *slotp, uint32_t flags)
 		}
 
 fast:		/* If the page can't be evicted, give up. */
-		if (!__wt_page_can_evict(session,
-		    page, WT_EVICT_CHECK_SPLITS | WT_EVICT_FORCE_SPLIT))
+		if (!__wt_page_can_evict(session, page, 1))
 			continue;
 
 		/*
@@ -1425,7 +1424,7 @@ __wt_evict_lru_page(WT_SESSION_IMPL *session, int is_server)
 	if (page->read_gen != WT_READGEN_OLDEST)
 		page->read_gen = __wt_cache_read_gen_set(session);
 
-	WT_WITH_BTREE(session, btree, ret = __wt_evict_page(session, ref, 0));
+	WT_WITH_BTREE(session, btree, ret = __wt_evict_page(session, ref));
 
 	(void)WT_ATOMIC_SUB4(btree->evict_busy, 1);
 
