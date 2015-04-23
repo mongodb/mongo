3e254079484ce35a3cb70c48478c69defdb8f012
Alex Gorrod <alexg@wiredtiger.com>
2015-04-23 05:42:08 +0000

    Fix a deadlock related to LSM. There are cases where closing a file with an existing checkpoint could self deadlock.
    Check in the meta tracking whether we've already visited a checkpoint handle.
    
    Refs WT-716

--- src/include/extern.h
+++ src/include/extern.h
@@ -427,6 +427,7 @@ extern int __wt_metadata_remove(WT_SESSION_IMPL *session, const char *key);
 extern int __wt_metadata_search( WT_SESSION_IMPL *session, const char *key, char **valuep);
 extern void __wt_meta_track_discard(WT_SESSION_IMPL *session);
 extern int __wt_meta_track_on(WT_SESSION_IMPL *session);
+extern int __wt_meta_track_find_handle( WT_SESSION_IMPL *session, const char *name, const char *checkpoint);
 extern int __wt_meta_track_off(WT_SESSION_IMPL *session, int need_sync, int unroll);
 extern int __wt_meta_track_sub_on(WT_SESSION_IMPL *session);
 extern int __wt_meta_track_sub_off(WT_SESSION_IMPL *session);
