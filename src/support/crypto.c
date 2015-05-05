42ce740f0e817db51e69edfa537001cd528512a6
Keith Bostic <keith@wiredtiger.com>
2015-05-05 10:17:28 -0400

    No need for error labels, they just return. Right-fill some comments, KNF

--- src/support/crypto.c
+++ src/support/crypto.c
@@ -14,10 +14,9 @@
  *	WT_ITEM and return the decrypted buffer.
  */
 int
-__wt_decrypt(WT_SESSION_IMPL *session, WT_ENCRYPTOR *encryptor,
-    size_t skip, WT_ITEM *in, WT_ITEM **out)
+__wt_decrypt(WT_SESSION_IMPL *session,
+    WT_ENCRYPTOR *encryptor, size_t skip, WT_ITEM *in, WT_ITEM **out)
 {
-	WT_DECL_RET;
 	size_t encryptor_data_len, result_len;
 	uint32_t encrypt_len;
 	uint8_t *dst, *src;
@@ -25,37 +24,36 @@ __wt_decrypt(WT_SESSION_IMPL *session, WT_ENCRYPTOR *encryptor,
 	encrypt_len = WT_STORE_SIZE(*((uint32_t *)
 	    ((uint8_t *)in->data + skip)));
 	if (encrypt_len > in->size)
-		WT_ERR_MSG(session, WT_ERROR,
+		WT_RET_MSG(session, WT_ERROR,
 		    "corrupted encrypted item: padded size less than "
 		    "actual size");
 	/*
 	 * We're allocating the number of bytes we're expecting
 	 * from decryption plus the unencrypted header.
 	 */
-	WT_ERR(__wt_buf_initsize(session, *out, encrypt_len));
+	WT_RET(__wt_buf_initsize(session, *out, encrypt_len));
 
 	src = (uint8_t *)in->data + skip + WT_ENCRYPT_LEN_SIZE;
 	encryptor_data_len = encrypt_len - (skip + WT_ENCRYPT_LEN_SIZE);
 	dst = (uint8_t *)(*out)->mem + skip;
 
-	WT_ERR(encryptor->decrypt(encryptor, &session->iface,
-	    src, encryptor_data_len,
-	    dst, encryptor_data_len, &result_len));
+	WT_RET(encryptor->decrypt(encryptor, &session->iface,
+	    src, encryptor_data_len, dst, encryptor_data_len, &result_len));
 
 	/*
 	 * Copy in the skipped header bytes.
 	 */
 	memcpy((*out)->mem, in->data, skip);
+
 	/*
-	 * Set the real result length in the output buffer including
-	 * the skipped header size.  The encryptor may have done its
-	 * own padding so the returned result length is the real data
-	 * length after decryption removes any of its padding.
+	 * Set the real result length in the output buffer including the skipped
+	 * header size. The encryptor may have done its own padding so the
+	 * returned result length is the real data length after decryption
+	 * removes any of its padding.
 	 */
-	result_len += skip;
-	WT_ERR(__wt_buf_initsize(session, *out, result_len));
-err:
-	return (ret);
+	(*out)->size = result_len + skip;
+
+	return (0);
 }
 
 /*
@@ -66,7 +64,6 @@ int
 __wt_encrypt(WT_SESSION_IMPL *session,
     WT_KEYED_ENCRYPTOR *kencryptor, size_t skip, WT_ITEM *in, WT_ITEM *out)
 {
-	WT_DECL_RET;
 	size_t dst_len, result_len, src_len;
 	uint32_t *unpadded_lenp;
 	uint8_t *dst, *src;
@@ -82,10 +79,10 @@ __wt_encrypt(WT_SESSION_IMPL *session,
 	dst_len = src_len + kencryptor->size_const;
 
 	/*
-	 * Send in the temporary source address and length that
-	 * includes the checksum we injected.
+	 * Send in the temporary source address and length that includes the
+	 * checksum we injected.
 	 */
-	WT_ERR(kencryptor->encryptor->encrypt(kencryptor->encryptor,
+	WT_RET(kencryptor->encryptor->encrypt(kencryptor->encryptor,
 	    &session->iface, src, src_len, dst, dst_len, &result_len));
 
 	/*
@@ -93,18 +90,16 @@ __wt_encrypt(WT_SESSION_IMPL *session,
 	 */
 	result_len += skip + WT_ENCRYPT_LEN_SIZE;
 	/*
-	 * Store original size so we know how much space is needed
-	 * on the decryption side.
+	 * Store original size so we know how much space is needed on the
+	 * decryption side.
 	 */
 	*unpadded_lenp = WT_STORE_SIZE(result_len);
 	/*
-	 * Copy in the skipped header bytes, set the final data
-	 * size.
+	 * Copy in the skipped header bytes, set the final data size.
 	 */
 	memcpy(out->mem, in->mem, skip);
 	out->size = result_len;
-err:
-	return (ret);
+	return (0);
 }
 
 /*
@@ -112,13 +107,14 @@ err:
  *	Return the size needed for the destination buffer.
  */
 void
-__wt_encrypt_size(WT_SESSION_IMPL *session, WT_KEYED_ENCRYPTOR *kencryptor,
-    size_t incoming_size, size_t *sizep)
+__wt_encrypt_size(WT_SESSION_IMPL *session,
+    WT_KEYED_ENCRYPTOR *kencryptor, size_t incoming_size, size_t *sizep)
 {
 	WT_UNUSED(session);
+
 	if (sizep == NULL)
 		return;
-	*sizep = incoming_size + kencryptor->size_const +
-	    WT_ENCRYPT_LEN_SIZE;
+
+	*sizep = incoming_size + kencryptor->size_const + WT_ENCRYPT_LEN_SIZE;
 	return;
 }
