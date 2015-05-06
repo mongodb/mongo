564140a0aae77d6c5a959e7a2656f2fc32047930
Dan Pasette <dan@10gen.com>
2015-05-06 17:59:05 -0400

    SERVER-18286 Increase the value of the WiredTiger close_idle_time
    Set the value of close_idle_time to ~28 hours to prevent idle
    collections and indexes from being forced out of cache

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
@@ -105,6 +105,7 @@ namespace mongo {
             ss << "log=(enabled=true,archive=true,path=journal,compressor=";
             ss << wiredTigerGlobalOptions.journalCompressor << "),";
         }
+        ss << "file_manager=(close_idle_time=100000),"; //~28 hours, will put better fix in 3.1.x
         ss << "checkpoint=(wait=" << wiredTigerGlobalOptions.checkpointDelaySecs;
         ss << ",log_size=2GB),";
         ss << "statistics_log=(wait=" << wiredTigerGlobalOptions.statisticsLogDelaySecs << "),";
