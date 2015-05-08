d4b2d888e55202a7e593753606d1e60974613f06
Pat Gunn <pgunn@mongodb.com>
2015-05-08 00:05:05 -0400

    dist/s_all plus refactor fprintf(stdout to printf(

--- examples/c/ex_backup.c
+++ examples/c/ex_backup.c
@@ -98,12 +98,10 @@ compare_backups(int i)
 	else
 		snprintf(msg, sizeof(msg), "%d", i);
 	if (ret == 0)
-		fprintf(stdout,
-		    "Iteration %s: Tables %s.%d and %s.%d identical\n",
+		printf("Iteration %s: Tables %s.%d and %s.%d identical\n",
 		    msg, full_out, i, incr_out, i);
 	else {
-		fprintf(stdout,
-		    "Iteration %s: Tables %s.%d and %s.%d differ\n",
+		printf("Iteration %s: Tables %s.%d and %s.%d differ\n",
 		    msg, full_out, i, incr_out, i);
 		exit(1);
 	}
@@ -289,16 +287,16 @@ main(void)
 	ret = setup_directories();
 	ret = wt_conn->open_session(wt_conn, NULL, NULL, &session);
 	ret = session->create(session, uri, "key_format=S,value_format=S");
-	fprintf(stdout, "Adding initial data\n");
+	printf("Adding initial data\n");
 	ret = add_work(session, 0);
 
-	fprintf(stdout, "Taking initial backup\n");
+	printf("Taking initial backup\n");
 	ret = take_full_backup(session, 0);
 
 	ret = session->checkpoint(session, NULL);
 
 	for (i = 1; i < MAX_ITERATIONS; i++) {
-		fprintf(stdout, "Iteration %d: adding data\n", i);
+		printf("Iteration %d: adding data\n", i);
 		ret = add_work(session, i);
 		ret = session->checkpoint(session, NULL);
 		/*
@@ -306,17 +304,17 @@ main(void)
 		 * comparison purposes.  A normal incremental backup
 		 * procedure would not include this.
 		 */
-		fprintf(stdout, "Iteration %d: taking full backup\n", i);
+		printf("Iteration %d: taking full backup\n", i);
 		ret = take_full_backup(session, i);
 		/*
 		 * Taking the incremental backup also calls truncate
 		 * to archive the log files, if the copies were successful.
 		 * See that function for details on that call.
 		 */
-		fprintf(stdout, "Iteration %d: taking incremental backup\n", i); 
+		printf("Iteration %d: taking incremental backup\n", i);
 		ret = take_incr_backup(session, i);
 
-		fprintf(stdout, "Iteration %d: dumping and comparing data\n", i);
+		printf("Iteration %d: dumping and comparing data\n", i);
 		ret = compare_backups(i);
 	}
 
@@ -325,7 +323,7 @@ main(void)
 	 * comparison between the incremental and original.
 	 */
 	ret = wt_conn->close(wt_conn, NULL);
-	fprintf(stdout, "Final comparison: dumping and comparing data\n", i);
+	printf("Final comparison: dumping and comparing data\n", i);
 	ret = compare_backups(0);
 	return (ret);
 }
