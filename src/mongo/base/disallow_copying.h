bb3c301d62a8c5601fb0bed176013ef7ac0a3720
Adam Midvidy <amidvidy@gmail.com>
2015-05-18 12:47:16 -0400

    Revert "SERVER-18486 modernize MONGO_DISALLOW_COPYING"
    This reverts commit d31bf41e161177d933bcac782a3fce3ef61f190a.

--- src/mongo/base/disallow_copying.h
+++ src/mongo/base/disallow_copying.h
@@ -32,9 +32,6 @@
  * for class "CLASS".  Must be the _first_ or _last_ line of the class declaration.  Prefer
  * to use it as the first line.
  *
- * If you use this macro and wish to make the class movable, you must define the move assignment
- * operator and move constructor.
- *
  * Usage:
  *    class Foo {
  *        MONGO_DISALLOW_COPYING(Foo);
@@ -42,6 +39,7 @@
  *        ...
  *    };
  */
-#define MONGO_DISALLOW_COPYING(CLASS)        \
-    CLASS(const CLASS&) = delete;            \
-    CLASS& operator=(const CLASS&) = delete
+#define MONGO_DISALLOW_COPYING(CLASS) \
+    private:                                    \
+    CLASS(const CLASS&);                        \
+    CLASS& operator=(const CLASS&)
