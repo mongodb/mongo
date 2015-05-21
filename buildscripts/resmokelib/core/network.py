cbad13858229d7dafb41d1e6bf3ba05aa1f29503
Max Hirschhorn <max.hirschhorn@mongodb.com>
2015-05-21 19:50:54 -0400

    SERVER-18600 Avoid giving the same port to multiple processes.

--- buildscripts/resmokelib/core/network.py
+++ buildscripts/resmokelib/core/network.py
@@ -4,26 +4,44 @@ Helper to reserve a network port.
 
 from __future__ import absolute_import
 
-import socket
+import socket as _socket
+import threading
 
 
 class UnusedPort(object):
     """
-    Acquires a unused port.
+    Acquires an unused port from the OS.
     """
+
+    # Use a set to keep track of ports that are acquired from the OS to avoid returning duplicates.
+    # We do not remove ports from this set because they are used throughout the lifetime of
+    # resmoke.py for started mongod/mongos processes.
+    _ALLOCATED_PORTS = set()
+    _ALLOCATED_PORTS_LOCK = threading.Lock()
 
     def __init__(self):
         self.num = None
+        self.__socket = None
 
     def __enter__(self):
-        self.__socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.__socket.bind(("0.0.0.0", 0))
+        while True:
+            socket = _socket.socket(_socket.AF_INET, _socket.SOCK_STREAM)
+            socket.setsockopt(_socket.SOL_SOCKET, _socket.SO_REUSEADDR, 1)
+            socket.bind(("0.0.0.0", 0))
 
-        addr, port = self.__socket.getsockname()
-        self.num = port
+            port = socket.getsockname()[1]
+            with UnusedPort._ALLOCATED_PORTS_LOCK:
+                # Check whether the OS has already given us 'port'.
+                if port in UnusedPort._ALLOCATED_PORTS:
+                    socket.close()
+                    continue
 
-        return self
+                UnusedPort._ALLOCATED_PORTS.add(port)
+
+            self.num = port
+            self.__socket = socket
+            return self
 
     def __exit__(self, *exc_info):
-        self.__socket.close()
+        if self.__socket is not None:
+            self.__socket.close()
