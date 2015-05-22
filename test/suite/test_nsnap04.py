c1fe0823534b0b693b96f7e85fd2298ccfaf2700
Alex Gorrod <alexg@wiredtiger.com>
2015-05-22 02:55:33 +0000

    Add broken test case.

---
+++ test/suite/test_nsnap04.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#
+# Public Domain 2014-2015 MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# test_nsnap04.py
+#   Named snapshots: Create snapshot from running transaction
+
+from suite_subprocess import suite_subprocess
+from wtscenario import multiply_scenarios, number_scenarios
+from helper import simple_populate
+import wiredtiger, wttest
+
+class test_nsnap04(wttest.WiredTigerTestCase, suite_subprocess):
+    tablename = 'test_nsnap04'
+    uri = 'table:' + tablename
+    nrows_per_itr = 10
+
+    def check_named_snapshot(self, session, snapshot, expected):
+        c = session.open_cursor(self.uri)
+        session.begin_transaction("snapshot=" + str(snapshot))
+        count = 0
+        for row in c:
+            count += 1
+        self.session.commit_transaction()
+        # print "Checking snapshot %d, expect %d, found %d" % (snapshot, expected, count)
+        self.assertEqual(count, expected)
+
+    def test_named_snapshots(self):
+        # Populate a table
+        end = start = 0
+        simple_populate(self, self.uri, 'key_format=i', 0)
+
+        snapshots = []
+        c = self.session.open_cursor(self.uri)
+        for i in xrange(self.nrows_per_itr):
+            c[i] = "some value"
+        self.session.begin_transaction("isolation=snapshot")
+        # Insert some more content.
+        for i in xrange(self.nrows_per_itr):
+            c[self.nrows_per_itr + i] = "some value"
+
+        self.session.snapshot("name=0")
+        # Insert some more content.
+        for i in xrange(self.nrows_per_itr):
+            c[2 * self.nrows_per_itr + i] = "some value"
+
+        new_session = self.conn.open_session()
+        self.check_named_snapshot(new_session, 0, 3 * self.nrows_per_itr)
+        new_session.close()
+
+        # Insert some more content. Should this be an error?
+        for i in xrange(self.nrows_per_itr):
+            c[2 * self.nrows_per_itr + i] = "some value"
+
+        self.session.commit_transaction()
+
+        new_session = self.conn.open_session()
+        self.check_named_snapshot(new_session, 0, 4 * self.nrows_per_itr)
+        new_session.close()
+
+if __name__ == '__main__':
+    wttest.run()
