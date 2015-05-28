eb1ab6f69ac464c1456d1cde5555e1d83f5e377d
Keith Bostic <keith@wiredtiger.com>
2015-05-28 08:24:03 -0400

    Strip out the "log=(recover=)" configuration from the base configuration file, we don't want wt to create a database and have that be part of the base configuration. Reference WT-1943.

--- src/conn/conn_api.c
+++ src/conn/conn_api.c
@@ -1721,8 +1721,8 @@ __conn_write_base_config(WT_SESSION_IMPL *session, const char *cfg[])
 	 * to be stripped out from the base configuration file; do that now, and
 	 * merge the rest to be written.
 	 */
-	WT_ERR(__wt_config_merge(session,
-	    cfg + 1, "create=,encryption=(secretkey=)", &base_config));
+	WT_ERR(__wt_config_merge(session, cfg + 1,
+	    "create=,encryption=(secretkey=),log=(recover=)", &base_config));
 	WT_ERR(__wt_config_init(session, &parser, base_config));
 	while ((ret = __wt_config_next(&parser, &k, &v)) == 0) {
 		/* Fix quoting for non-trivial settings. */
