xiexiaoy/eloqdoc

SERVER-17689: handle wce better in initial sync

src/mongo/db/cloner.cpp
@@ -49,6 +49,7 @@
 #include "mongo/db/commands.h"
 #include "mongo/db/commands/copydb.h"
 #include "mongo/db/commands/rename_collection.h"
+#include "mongo/db/concurrency/write_conflict_exception.h"
 #include "mongo/db/dbdirectclient.h"
 #include "mongo/db/dbhelpers.h"
 #include "mongo/db/service_context.h"
@@ -147,11 +148,12 @@ namespace mongo {
                          << "collection dropped during clone ["
                          << to_collection.ns() << "]",
                          !createdCollection );
-                WriteUnitOfWork wunit(txn);
-                collection = db->createCollection(txn, to_collection.ns(), CollectionOptions());
-                verify(collection);
-
-                wunit.commit();
+                MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+                    WriteUnitOfWork wunit(txn);
+                    collection = db->createCollection(txn, to_collection.ns(), CollectionOptions());
+                    verify(collection);
+                    wunit.commit();
+                } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "createCollection", to_collection.ns());
             }
 
             while( i.moreInCurrentBatch() ) {
@@ -217,19 +219,18 @@ namespace mongo {
                 }
 
                 ++numSeen;
-                WriteUnitOfWork wunit(txn);
+                MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+                    WriteUnitOfWork wunit(txn);
 
-                BSONObj js = tmp;
-
-                StatusWith<RecordId> loc = collection->insertDocument( txn, js, true );
-                if ( !loc.isOK() ) {
-                    error() << "error: exception cloning object in " << from_collection
-                            << ' ' << loc.getStatus() << " obj:" << js;
-                }
-                uassertStatusOK( loc.getStatus() );
-
-                wunit.commit();
-
+                    BSONObj doc = tmp;
+                    StatusWith<RecordId> loc = collection->insertDocument( txn, doc, true );
+                    if ( !loc.isOK() ) {
+                        error() << "error: exception cloning object in " << from_collection
+                                << ' ' << loc.getStatus() << " obj:" << doc;
+                    }
+                    uassertStatusOK( loc.getStatus() );
+                    wunit.commit();
+                } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "cloner insert", to_collection.ns());
                 RARELY if ( time( 0 ) - saveLast > 60 ) {
                     log() << numSeen << " objects cloned so far from collection " << from_collection;
                     saveLast = time( 0 );
@@ -325,10 +326,12 @@ namespace mongo {
 
         Collection* collection = db->getCollection( to_collection );
         if ( !collection ) {
-            WriteUnitOfWork wunit(txn);
-            collection = db->createCollection(txn, to_collection.ns(), CollectionOptions());
-            invariant(collection);
-            wunit.commit();
+            MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+                WriteUnitOfWork wunit(txn);
+                collection = db->createCollection(txn, to_collection.ns(), CollectionOptions());
+                invariant(collection);
+                wunit.commit();
+            } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "createCollection", to_collection.ns());
         }
 
         // TODO pass the MultiIndexBlock when inserting into the collection rather than building the
@@ -389,13 +392,15 @@ namespace mongo {
             invariant(collList.size() <= 1);
             BSONObj col = collList.front();
             if (col["options"].isABSONObj()) {
-                WriteUnitOfWork wunit(txn);
-                Status status = userCreateNS(txn, db, ns, col["options"].Obj(), 0);
-                if ( !status.isOK() ) {
-                    errmsg = status.toString();
-                    return false;
-                }
-                wunit.commit();
+                MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+                    WriteUnitOfWork wunit(txn);
+                    Status status = userCreateNS(txn, db, ns, col["options"].Obj(), 0);
+                    if ( !status.isOK() ) {
+                        errmsg = status.toString();
+                        return false;
+                    }
+                    wunit.commit();
+                } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "createUser", ns);
             }
         }
 
@@ -569,19 +574,20 @@ namespace mongo {
                 Database* db = dbHolder().openDb(txn, toDBName);
 
                 {
-                    WriteUnitOfWork wunit(txn);
+                    MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+                        WriteUnitOfWork wunit(txn);
 
-                    // we defer building id index for performance - building it in batch is much
-                    // faster
-                    Status createStatus = userCreateNS(txn, db, to_name.ns(), options, false);
-                    if ( !createStatus.isOK() ) {
-                        errmsg = str::stream() << "failed to create collection \""
-                                               << to_name.ns() << "\": "
-                                               << createStatus.reason();
-                        return false;
-                    }
-
-                    wunit.commit();
+                        // we defer building id index for performance - building it in batch is much
+                        // faster
+                        Status createStatus = userCreateNS(txn, db, to_name.ns(), options, false);
+                        if ( !createStatus.isOK() ) {
+                            errmsg = str::stream() << "failed to create collection \""
+                                                   << to_name.ns() << "\": "
+                                                   << createStatus.reason();
+                            return false;
+                        }
+                        wunit.commit();
+                    } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "createUser", to_name.ns());
                 }
 
                 LOG(1) << "\t\t cloning " << from_name << " -> " << to_name << endl;

src/mongo/db/repl/minvalid.cpp
@@ -34,6 +34,7 @@
 
 #include "mongo/bson/timestamp.h"
 #include "mongo/db/concurrency/d_concurrency.h"
+#include "mongo/db/concurrency/write_conflict_exception.h"
 #include "mongo/db/dbhelpers.h"
 #include "mongo/db/jsobj.h"
 #include "mongo/db/operation_context.h"
@@ -51,49 +52,60 @@ namespace {
 
     // Writes
     void clearInitialSyncFlag(OperationContext* txn) {
-        ScopedTransaction transaction(txn, MODE_IX);
-        // TODO: Investigate correctness of taking MODE_IX for DB/Collection locks
-        Lock::DBLock dblk(txn->lockState(), "local", MODE_X);
-        Helpers::putSingleton(txn, minvalidNS, BSON("$unset" << initialSyncFlag));
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(txn, MODE_IX);
+            // TODO: Investigate correctness of taking MODE_IX for DB/Collection locks
+            Lock::DBLock dblk(txn->lockState(), "local", MODE_X);
+            Helpers::putSingleton(txn, minvalidNS, BSON("$unset" << initialSyncFlag));
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "clearInitialSyncFlags", minvalidNS);
+
     }
 
     void setInitialSyncFlag(OperationContext* txn) {
-        ScopedTransaction transaction(txn, MODE_IX);
-        Lock::DBLock dblk(txn->lockState(), "local", MODE_X);
-        Helpers::putSingleton(txn, minvalidNS, BSON("$set" << initialSyncFlag));
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(txn, MODE_IX);
+            Lock::DBLock dblk(txn->lockState(), "local", MODE_X);
+            Helpers::putSingleton(txn, minvalidNS, BSON("$set" << initialSyncFlag));
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "setInitialSyncFlags", minvalidNS);
     }
 
     void setMinValid(OperationContext* ctx, Timestamp ts) {
-        ScopedTransaction transaction(ctx, MODE_IX);
-        Lock::DBLock dblk(ctx->lockState(), "local", MODE_X);
-        Helpers::putSingleton(ctx, minvalidNS, BSON("$set" << BSON("ts" << ts)));
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(ctx, MODE_IX);
+            Lock::DBLock dblk(ctx->lockState(), "local", MODE_X);
+            Helpers::putSingleton(ctx, minvalidNS, BSON("$set" << BSON("ts" << ts)));
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(ctx, "setMinValid", minvalidNS);
     }
 
     // Reads
     bool getInitialSyncFlag() {
         OperationContextImpl txn;
-        ScopedTransaction transaction(&txn, MODE_IS);
-        Lock::DBLock dblk(txn.lockState(), "local", MODE_IS);
-        Lock::CollectionLock lk(txn.lockState(), minvalidNS, MODE_IS);
-        BSONObj mv;
-        bool found = Helpers::getSingleton( &txn, minvalidNS, mv);
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(&txn, MODE_IS);
+            Lock::DBLock dblk(txn.lockState(), "local", MODE_IS);
+            Lock::CollectionLock lk(txn.lockState(), minvalidNS, MODE_IS);
+            BSONObj mv;
+            bool found = Helpers::getSingleton( &txn, minvalidNS, mv);
 
-        if (found) {
-            return mv[initialSyncFlagString].trueValue();
-        }
-        return false;
+            if (found) {
+                return mv[initialSyncFlagString].trueValue();
+            }
+            return false;
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(&txn, "getInitialSyncFlags", minvalidNS);
     }
 
     Timestamp getMinValid(OperationContext* txn) {
-        ScopedTransaction transaction(txn, MODE_IS);
-        Lock::DBLock dblk(txn->lockState(), "local", MODE_IS);
-        Lock::CollectionLock lk(txn->lockState(), minvalidNS, MODE_IS);
-        BSONObj mv;
-        bool found = Helpers::getSingleton(txn, minvalidNS, mv);
-        if (found) {
-            return mv["ts"].timestamp();
-        }
-        return Timestamp();
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(txn, MODE_IS);
+            Lock::DBLock dblk(txn->lockState(), "local", MODE_IS);
+            Lock::CollectionLock lk(txn->lockState(), minvalidNS, MODE_IS);
+            BSONObj mv;
+            bool found = Helpers::getSingleton(txn, minvalidNS, mv);
+            if (found) {
+                return mv["ts"].timestamp();
+            }
+            return Timestamp();
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "getMinValid", minvalidNS);
     }
 
 }

mongodb/mongo

(cherry picked from commit 57232d88b869b0741d95998f8ed09a96b2e7a1bc)

src/mongo/db/cloner.cpp
@@ -48,6 +48,7 @@
 #include "mongo/db/commands.h"
 #include "mongo/db/commands/copydb.h"
 #include "mongo/db/commands/rename_collection.h"
+#include "mongo/db/concurrency/write_conflict_exception.h"
 #include "mongo/db/dbdirectclient.h"
 #include "mongo/db/dbhelpers.h"
 #include "mongo/db/index_builder.h"
@@ -145,17 +146,20 @@ namespace mongo {
                          << "collection dropped during clone ["
                          << to_collection.ns() << "]",
                          !createdCollection );
-                WriteUnitOfWork wunit(txn);
-                collection = db->createCollection(txn, to_collection.ns());
-                verify(collection);
+                MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
 
-                if (logForRepl) {
-                    repl::logOp(txn,
-                                "c",
-                                (_dbName + ".$cmd").c_str(),
-                                BSON("create" << to_collection.coll()));
-                }
-                wunit.commit();
+                    WriteUnitOfWork wunit(txn);
+                    collection = db->createCollection(txn, to_collection.ns());
+                    verify(collection);
+
+                    if (logForRepl) {
+                        repl::logOp(txn,
+                                    "c",
+                                    (_dbName + ".$cmd").c_str(),
+                                    BSON("create" << to_collection.coll()));
+                    }
+                    wunit.commit();
+                } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "createCollection", to_collection.ns());
             }
 
             while( i.moreInCurrentBatch() ) {
@@ -221,21 +225,22 @@ namespace mongo {
                 }
 
                 ++numSeen;
-                WriteUnitOfWork wunit(txn);
+                MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+                    WriteUnitOfWork wunit(txn);
 
-                BSONObj js = tmp;
+                    BSONObj js = tmp;
 
-                StatusWith<RecordId> loc = collection->insertDocument( txn, js, true );
-                if ( !loc.isOK() ) {
-                    error() << "error: exception cloning object in " << from_collection
-                            << ' ' << loc.toString() << " obj:" << js;
-                }
-                uassertStatusOK( loc.getStatus() );
-                if (logForRepl)
-                    repl::logOp(txn, "i", to_collection.ns().c_str(), js);
+                    StatusWith<RecordId> loc = collection->insertDocument( txn, js, true );
+                    if ( !loc.isOK() ) {
+                        error() << "error: exception cloning object in " << from_collection
+                                << ' ' << loc.toString() << " obj:" << js;
+                    }
+                    uassertStatusOK( loc.getStatus() );
+                    if (logForRepl)
+                        repl::logOp(txn, "i", to_collection.ns().c_str(), js);
 
-                wunit.commit();
-
+                    wunit.commit();
+                } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "cloner insert", to_collection.ns());
                 RARELY if ( time( 0 ) - saveLast > 60 ) {
                     log() << numSeen << " objects cloned so far from collection " << from_collection;
                     saveLast = time( 0 );
@@ -335,16 +340,18 @@ namespace mongo {
 
         Collection* collection = db->getCollection( to_collection );
         if ( !collection ) {
-            WriteUnitOfWork wunit(txn);
-            collection = db->createCollection( txn, to_collection.ns() );
-            invariant(collection);
-            if (logForRepl) {
-                repl::logOp(txn,
-                            "c",
-                            (toDBName + ".$cmd").c_str(),
-                            BSON("create" << to_collection.coll()));
-            }
-            wunit.commit();
+            MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+                WriteUnitOfWork wunit(txn);
+                collection = db->createCollection( txn, to_collection.ns() );
+                invariant(collection);
+                if (logForRepl) {
+                    repl::logOp(txn,
+                                "c",
+                                (toDBName + ".$cmd").c_str(),
+                                BSON("create" << to_collection.coll()));
+                }
+                wunit.commit();
+            } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "createCollection", to_collection.ns());
         }
 
         // TODO pass the MultiIndexBlock when inserting into the collection rather than building the
@@ -406,13 +413,15 @@ namespace mongo {
             invariant(collList.size() <= 1);
             BSONObj col = collList.front();
             if (col["options"].isABSONObj()) {
-                WriteUnitOfWork wunit(txn);
-                Status status = userCreateNS(txn, db, ns, col["options"].Obj(), logForRepl, 0);
-                if ( !status.isOK() ) {
-                    errmsg = status.toString();
-                    return false;
-                }
-                wunit.commit();
+                MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+                    WriteUnitOfWork wunit(txn);
+                    Status status = userCreateNS(txn, db, ns, col["options"].Obj(), logForRepl, 0);
+                    if ( !status.isOK() ) {
+                        errmsg = status.toString();
+                        return false;
+                    }
+                    wunit.commit();
+                } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "createUser", ns);
             }
         }
 
@@ -583,7 +592,7 @@ namespace mongo {
 
                 Database* db = dbHolder().openDb(txn, toDBName);
 
-                {
+                MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
                     WriteUnitOfWork wunit(txn);
 
                     // we defer building id index for performance - building it in batch is much
@@ -600,9 +609,7 @@ namespace mongo {
                                                << createStatus.reason();
                         return false;
                     }
-
-                    wunit.commit();
-                }
+                } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "createUser", to_name.ns());
 
                 LOG(1) << "\t\t cloning " << from_name << " -> " << to_name << endl;
                 Query q;

src/mongo/db/repl/minvalid.cpp
@@ -34,6 +34,7 @@
 
 #include "mongo/bson/optime.h"
 #include "mongo/db/concurrency/d_concurrency.h"
+#include "mongo/db/concurrency/write_conflict_exception.h"
 #include "mongo/db/dbhelpers.h"
 #include "mongo/db/jsobj.h"
 #include "mongo/db/operation_context.h"
@@ -50,45 +51,54 @@ namespace {
 } // namespace
 
     void clearInitialSyncFlag(OperationContext* txn) {
-        ScopedTransaction transaction(txn, MODE_IX);
-        Lock::DBLock lk(txn->lockState(), "local", MODE_X);
-        Helpers::putSingleton(txn, minvalidNS, BSON("$unset" << initialSyncFlag));
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(txn, MODE_IX);
+            Lock::DBLock lk(txn->lockState(), "local", MODE_X);
+            Helpers::putSingleton(txn, minvalidNS, BSON("$unset" << initialSyncFlag));
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "clearInitialSyncFlags", minvalidNS);
     }
 
     void setInitialSyncFlag(OperationContext* txn) {
-        ScopedTransaction transaction(txn, MODE_IX);
-        Lock::DBLock lk(txn->lockState(), "local", MODE_X);
-        Helpers::putSingleton(txn, minvalidNS, BSON("$set" << initialSyncFlag));
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(txn, MODE_IX);
+            Lock::DBLock lk(txn->lockState(), "local", MODE_X);
+            Helpers::putSingleton(txn, minvalidNS, BSON("$set" << initialSyncFlag));
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "setInitialSyncFlags", minvalidNS);
     }
 
     bool getInitialSyncFlag() {
         OperationContextImpl txn;
-        ScopedTransaction transaction(&txn, MODE_IX);
-        Lock::DBLock lk(txn.lockState(), "local", MODE_X);
-        BSONObj mv;
-        bool found = Helpers::getSingleton( &txn, minvalidNS, mv);
-
-        if (found) {
-            return mv[initialSyncFlagString].trueValue();
-        }
-        return false;
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(&txn, MODE_IX);
+            Lock::DBLock lk(txn.lockState(), "local", MODE_X);
+            BSONObj mv;
+            bool found = Helpers::getSingleton( &txn, minvalidNS, mv);
+            if (found) {
+                return mv[initialSyncFlagString].trueValue();
+            }
+            return false;
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(&txn, "getInitialSyncFlags", minvalidNS);
     }
 
     void setMinValid(OperationContext* ctx, OpTime ts) {
-        ScopedTransaction transaction(ctx, MODE_IX);
-        Lock::DBLock lk(ctx->lockState(), "local", MODE_X);
-        Helpers::putSingleton(ctx, minvalidNS, BSON("$set" << BSON("ts" << ts)));
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(ctx, MODE_IX);
+            Lock::DBLock lk(ctx->lockState(), "local", MODE_X);
+            Helpers::putSingleton(ctx, minvalidNS, BSON("$set" << BSON("ts" << ts)));
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(ctx, "setMinValid", minvalidNS);
     }
 
     OpTime getMinValid(OperationContext* txn) {
-        ScopedTransaction transaction(txn, MODE_IS);
-        Lock::DBLock lk(txn->lockState(), "local", MODE_S);
-        BSONObj mv;
-        bool found = Helpers::getSingleton(txn, minvalidNS, mv);
-        if (found) {
-            return mv["ts"]._opTime();
-        }
-        return OpTime();
+        MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
+            ScopedTransaction transaction(txn, MODE_IS);
+            Lock::DBLock lk(txn->lockState(), "local", MODE_S);
+            BSONObj mv;
+            bool found = Helpers::getSingleton(txn, minvalidNS, mv);
+            if (found) {
+                return mv["ts"]._opTime();
+            }
+            return OpTime();
+        } MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "getMinValid", minvalidNS);
     }
 
 }