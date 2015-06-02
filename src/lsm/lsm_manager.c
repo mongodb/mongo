0840cee04aed3d90770dd6789947ff687f34022b
Alex Gorrod <alexg@wiredtiger.com>
2015-06-02 05:02:06 +0000

    Split LSM verbose into two categories.
    Messages from the work queue management can be overwhelming.

--- src/lsm/lsm_manager.c
+++ src/lsm/lsm_manager.c
@@ -429,7 +429,8 @@ __lsm_manager_run_server(WT_SESSION_IMPL *session)
 				    session, WT_LSM_WORK_FLUSH, 0, lsm_tree));
 				WT_ERR(__wt_lsm_manager_push_entry(
 				    session, WT_LSM_WORK_BLOOM, 0, lsm_tree));
-				WT_ERR(__wt_verbose(session, WT_VERB_LSM,
+				WT_ERR(__wt_verbose(session,
+				    WT_VERB_LSM_MANAGER,
 				    "MGR %s: queue %d mod %d nchunks %d"
 				    " flags 0x%x aggressive %d pushms %" PRIu64
 				    " fillms %" PRIu64,
