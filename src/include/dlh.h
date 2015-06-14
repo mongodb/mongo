1175ca8d34672c0413a00d32eda5f68eea52b704
Keith Bostic <keith@wiredtiger.com>
2015-06-14 10:02:21 -0400

    Revert a test line I shouldn't have committed.

--- src/include/dlh.h
+++ src/include/dlh.h
@@ -1,6 +1,6 @@
 /*-
  * Copyright (c) 2014-2015 MongoDB, Inc.
- * Copyright (c) 2008-2014 WiredTiger, Inc.    foo.
+ * Copyright (c) 2008-2014 WiredTiger, Inc.
  *	All rights reserved.
  *
  * See the file LICENSE for redistribution information.
