faa81cb8d59b46e0a1c336b7f5fa90bf266a6555
Daniel Alabi <alabidan@gmail.com>
2015-06-29 15:34:40 -0400

    SERVER-19195 SyncClusterConnection should return error code obtained from config servers when write \$cmd fails

--- src/mongo/client/syncclusterconnection.cpp
+++ src/mongo/client/syncclusterconnection.cpp
@@ -38,6 +38,7 @@
 #include "mongo/client/dbclientinterface.h"
 #include "mongo/db/dbmessage.h"
 #include "mongo/db/namespace_string.h"
+#include "mongo/rpc/get_status_from_command_result.h"
 #include "mongo/util/log.h"
 
 // error codes 8000-8009
@@ -267,15 +268,17 @@ BSONObj SyncClusterConnection::findOne(const string& ns,
             _checkLast();
 
             for (size_t i = 0; i < all.size(); i++) {
-                BSONObj temp = all[i];
-                if (isOk(temp))
+                Status status = getStatusFromCommandResult(all[i]);
+                if (status.isOK()) {
                     continue;
+                }
+
                 stringstream ss;
-                ss << "write $cmd failed on a node: " << temp.jsonString();
+                ss << "write $cmd failed on a node: " << status.toString();
                 ss << " " << _conns[i]->toString();
                 ss << " ns: " << ns;
                 ss << " cmd: " << query.toString();
-                throw UserException(13105, ss.str());
+                throw UserException(status.code(), ss.str());
             }
 
             return all[0];
