83f5d68e98d012db0846425ec66a72e328c79a52
David Hatch <david.hatch@mongodb.com>
2015-07-15 11:32:37 -0400

    SERVER-19629 Add Decimal128 roundtrip and CRUD jstests

---
+++ jstests/decimal/decimal_constructors.js
@@ -0,0 +1,25 @@
+// Tests constructing NumberDecimal with various types
+
+(function() {
+    "use strict";
+    var col = db.decimal_constructors;
+    col.drop();
+
+    // Insert some sample data.
+
+    assert.writeOK(col.insert([
+        { "decimal" : NumberDecimal("1") },
+        { "decimal" : NumberDecimal(1) },
+        { "decimal" : NumberDecimal(NumberLong("1")) },
+        { "decimal" : NumberDecimal(NumberInt("1")) },
+    ]), "Initial insertion of decimals failed");
+
+    // Find values with various types and NumberDecimal constructed types
+    assert.eq(col.find({ "decimal" : NumberDecimal("1") }).count(), "4");
+    assert.eq(col.find({ "decimal" : NumberDecimal(1) }).count(), "4");
+    assert.eq(col.find({ "decimal" : NumberDecimal(NumberLong(1)) }).count(), "4");
+    assert.eq(col.find({ "decimal" : NumberDecimal(NumberInt(1)) }).count(), "4");
+    assert.eq(col.find({ "decimal" : 1 }).count(), "4");
+    assert.eq(col.find({ "decimal" : NumberLong(1) }).count(), "4");
+    assert.eq(col.find({ "decimal" : NumberInt(1) }).count(), "4");
+}());
--- jstests/decimal/placeholder.js
+++
@@ -1,2 +0,0 @@
-// TODO: Replace this file with decimal JS tests
-// when experimental decimal support is available
