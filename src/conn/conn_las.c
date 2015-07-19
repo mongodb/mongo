6d614d9b2d1789daeb777239e0fcf24111698dfe
Keith Bostic <keith@wiredtiger.com>
2015-07-19 11:56:00 -0400

    Test suite fixes.

--- src/conn/conn_las.c
+++ src/conn/conn_las.c
@@ -43,7 +43,7 @@ __wt_las_create(WT_SESSION_IMPL *session)
 		return (0);
 	}
 
-	/* Open an internal session, used for all lookaside operations. */
+	/* Open an internal session, used for lookaside cursors. */
 	WT_ERR(__wt_open_internal_session(
 	    conn, "lookaside table", 1, 1, &conn->las_session));
 	session = conn->las_session;
@@ -63,9 +63,6 @@ __wt_las_create(WT_SESSION_IMPL *session)
 	    session, WT_LASFILE_URI, NULL, open_cursor_cfg, &conn->las_cursor));
 
 	/*
-	 * Handle sweep does enough I/O it may be called upon to perform slow
-	 * operations for the block manager.
-	 *
 	 * No cache checks.
 	 * No lookaside records during reconciliation.
 	 * No checkpoints or logging.
@@ -87,21 +84,28 @@ int
 __wt_las_destroy(WT_SESSION_IMPL *session)
 {
 	WT_CONNECTION_IMPL *conn;
+	WT_CURSOR *cursor;
 	WT_DECL_RET;
 	WT_SESSION *wt_session;
 
 	conn = S2C(session);
 
-	/* Close any enclosing session. */
-	if (conn->las_session != NULL) {
-		wt_session = &conn->las_session->iface;
-		WT_TRET(wt_session->close(wt_session, NULL));
+	if (conn->las_session == NULL)
+		return (0);
 
-		conn->sweep_session = NULL;
-	}
+	/* Close open cursors. */
+	if ((cursor = conn->las_cursor) != NULL)
+		WT_TRET(cursor->close(cursor));
 
-	/* Discard any previous incarnation of the file. */
-	return (__las_drop(session));
+	/* Discard any incarnation of the file. */
+	WT_TRET(__las_drop(conn->las_session));
+
+	/* Close the session. */
+	wt_session = &conn->las_session->iface;
+	WT_TRET(wt_session->close(wt_session, NULL));
+	conn->las_session = NULL;
+
+	return (ret);
 }
 
 /*
