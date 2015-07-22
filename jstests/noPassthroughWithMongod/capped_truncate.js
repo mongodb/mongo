c1c1e095ba0b813b811d8eb2587b42583192c030
Dan Pasette <dan@10gen.com>
2015-07-22 18:43:12 -0400

    SERVER-19513 move functional test to noPassthroughWithMongod
    (cherry picked from commit 162fb82bed75ede539bbb960f44d232f8b8e5848)

--- jstests/noPassthroughWithMongod/capped_truncate.js
+++ jstests/noPassthroughWithMongod/capped_truncate.js
@@ -4,18 +4,18 @@
 (function() {
     'use strict';
 
-    db.cappedb.drop();
-    assert.commandWorked(db.runCommand({ create: "cappedb",
+    db.capped_truncate.drop();
+    assert.commandWorked(db.runCommand({ create: "capped_truncate",
                                          capped: true,
                                          size: 1000,
                                          autoIndexId: true }));
-    var t = db.cappedb;
+    var t = db.capped_truncate;
 
     for (var j = 1; j <= 10; j++) {
         assert.writeOK(t.insert({x:j}));
     }
 
-    assert.commandWorked(db.runCommand({ captrunc: "cappedb", n: 5, inc: false }));
+    assert.commandWorked(db.runCommand({ captrunc: "capped_truncate", n: 5, inc: false }));
     assert.eq(5, t.count(), "wrong number of documents in capped collection after truncate");
     assert.eq(5, t.distinct("_id").length, "wrong number of entries in _id index after truncate");
 
