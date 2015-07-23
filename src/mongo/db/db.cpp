0e5875d237ee4f113c63470521a9991dcf0807c7
Mathias Stearn <mathias@10gen.com>
2015-07-23 13:24:14 -0400

    SERVER-19553 Make mongod not use sayPiggyBack for KillCursor messages
    (cherry picked from commit 7cdd8036184934ef5ef42907f890b6603cd891a7)

--- src/mongo/db/db.cpp
+++ src/mongo/db/db.cpp
@@ -993,6 +993,9 @@ static int mongoDbMain(int argc, char* argv[], char **envp) {
 
     setupSignalHandlers();
 
+    // Mongod shouldn't lazily kill cursors. It doesn't mix well with connection pooling.
+    DBClientConnection::setLazyKillCursor(false);
+
     dbExecCommand = argv[0];
 
     srand(curTimeMicros());
