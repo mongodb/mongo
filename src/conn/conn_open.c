24c469e69b885a012295fde938b5adf8200f0dcd
Keith Bostic <keith@wiredtiger.com>
2015-08-03 07:37:17 -0400

    Move the __wt_sweep_destroy call to shut down the sweep server to before we assert WT_CONN_CLOSING; that flag is used in __wt_conn_btree_open to assert no further files will be opened, and the sweep server now opens btree files as part of its sweeping of the LAS file.

--- src/conn/conn_open.c
+++ src/conn/conn_open.c
@@ -110,12 +110,12 @@ __wt_connection_close(WT_CONNECTION_IMPL *conn)
 	F_CLR(conn, WT_CONN_SERVER_RUN);
 	WT_TRET(__wt_async_destroy(session));
 	WT_TRET(__wt_lsm_manager_destroy(session));
+	WT_TRET(__wt_sweep_destroy(session));
 
 	F_SET(conn, WT_CONN_CLOSING);
 
 	WT_TRET(__wt_checkpoint_server_destroy(session));
 	WT_TRET(__wt_statlog_destroy(session, 1));
-	WT_TRET(__wt_sweep_destroy(session));
 	WT_TRET(__wt_evict_destroy(session));
 
 	/* Close the lookaside file cursors, after all eviction is complete. */
