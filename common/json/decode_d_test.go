61bfea23b0bc95c0859d893f80b3b48ed82567f4
Kyle Erf <erf@mongodb.com>
2015-08-04 15:54:54 -0400

    TOOLS-767 fix decoding 'null' JSON into bson.D

--- common/json/decode_d_test.go
+++ common/json/decode_d_test.go
@@ -23,6 +23,24 @@ func TestDecodeBsonD(t *testing.T) {
 
 		})
 
+		Convey("A nested bson.D should be parsed", func() {
+			data := `{"a": 17, "b":{"foo":"bar", "baz":"boo"}, c:"wow" }`
+			out := struct {
+				A int    `json:"a"`
+				B bson.D `json:"b"`
+				C string `json:"c"`
+			}{}
+			err := Unmarshal([]byte(data), &out)
+			So(err, ShouldBeNil)
+			So(out.A, ShouldEqual, 17)
+			So(out.C, ShouldEqual, "wow")
+			So(len(out.B), ShouldEqual, 2)
+			So(out.B[0].Name, ShouldEqual, "foo")
+			So(out.B[0].Value, ShouldEqual, "bar")
+			So(out.B[1].Name, ShouldEqual, "baz")
+			So(out.B[1].Value, ShouldEqual, "boo")
+		})
+
 		Convey("Objects nested within DocElems should still be parsed", func() {
 			data := `{"a":["x", "y","z"], "b":{"foo":"bar", "baz":"boo"}}`
 			out := bson.D{}
@@ -34,6 +52,21 @@ func TestDecodeBsonD(t *testing.T) {
 			So(out[0].Value, ShouldResemble, []interface{}{"x", "y", "z"})
 			So(out[1].Value, ShouldResemble, map[string]interface{}{"foo": "bar", "baz": "boo"})
 		})
+
+		Convey("null should be a valid value", func() {
+			data := `{"a":true, "b":null, "c": 5}`
+			out := bson.D{}
+			err := Unmarshal([]byte(data), &out)
+			So(err, ShouldBeNil)
+			So(len(out), ShouldEqual, 3)
+			So(out[0].Name, ShouldEqual, "a")
+			So(out[0].Value, ShouldEqual, true)
+			So(out[1].Name, ShouldEqual, "b")
+			So(out[1].Value, ShouldBeNil)
+			So(out[2].Name, ShouldEqual, "c")
+			So(out[2].Value, ShouldEqual, 5)
+		})
+
 	})
 	Convey("Unmarshalling to a non-bson.D slice types should fail", t, func() {
 		data := `{"a":["x", "y","z"], "b":{"foo":"bar", "baz":"boo"}}`
