3041da9fc2127a3193a769c2a708261700e690bb
Kamran Khan <kamran.khan@mongodb.com>
2015-08-03 23:57:03 -0400

    SERVER-19694 Disable mapReduce FSM workload that triggers dupe key exception
    The workload will be re-enabled when the underlying server bug is fixed.

--- jstests/concurrency/fsm_all_sharded.js
+++ jstests/concurrency/fsm_all_sharded.js
@@ -14,6 +14,7 @@ var blacklist = [
     'distinct.js', // SERVER-13116 distinct isn't sharding aware
     'distinct_noindex.js', // SERVER-13116 distinct isn't sharding aware
     'distinct_projection.js', // SERVER-13116 distinct isn't sharding aware
+    'map_reduce_drop.js', // SERVER-19694 duplicate key exception on config.locks collection
     'yield_sort.js', // SERVER-17011 Cursor can return objects out of order if updated during query
     'yield_sort_merge.js', // SERVER-17011 also applies, since this query uses SORT stage,
                            // not SORT_MERGE stage in sharded environment
