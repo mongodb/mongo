9f71be47d8c35563e90587d01bd0e48f23beca17
Jason Rassi <rassi@10gen.com>
2015-08-05 13:25:37 -0400

    SERVER-18766 Add ClusterClientCursorMock

---
+++ src/mongo/s/query/cluster_client_cursor_mock.cpp
@@ -0,0 +1,77 @@
+/**
+ *    Copyright (C) 2015 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kQuery
+
+#include "mongo/platform/basic.h"
+
+#include "mongo/s/query/cluster_client_cursor_mock.h"
+
+namespace mongo {
+
+ClusterClientCursorMock::ClusterClientCursorMock(stdx::function<void(void)> killCallback)
+    : _killCallback(std::move(killCallback)) {}
+
+ClusterClientCursorMock::~ClusterClientCursorMock() {
+    invariant(_exhausted || _killed);
+}
+
+StatusWith<boost::optional<BSONObj>> ClusterClientCursorMock::next() {
+    invariant(!_killed);
+
+    if (_resultsQueue.empty()) {
+        _exhausted = true;
+        return {boost::none};
+    }
+
+    auto out = _resultsQueue.front();
+    _resultsQueue.pop();
+
+    if (!out.isOK()) {
+        return out.getStatus();
+    }
+
+    return boost::optional<BSONObj>(out.getValue());
+}
+
+void ClusterClientCursorMock::kill() {
+    _killed = true;
+    if (_killCallback) {
+        _killCallback();
+    }
+}
+
+void ClusterClientCursorMock::queueResult(BSONObj obj) {
+    _resultsQueue.push({obj});
+}
+
+void ClusterClientCursorMock::queueError(Status status) {
+    _resultsQueue.push({status});
+}
+
+}  // namespace mongo
---
+++ src/mongo/s/query/cluster_client_cursor_mock.h
@@ -0,0 +1,67 @@
+/**
+ *    Copyright (C) 2015 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#pragma once
+
+#include <queue>
+
+#include "mongo/s/query/cluster_client_cursor.h"
+#include "mongo/stdx/functional.h"
+
+namespace mongo {
+
+class ClusterClientCursorMock final : public ClusterClientCursor {
+    MONGO_DISALLOW_COPYING(ClusterClientCursorMock);
+
+public:
+    ClusterClientCursorMock(stdx::function<void(void)> killCallback = stdx::function<void(void)>());
+
+    ~ClusterClientCursorMock();
+
+    StatusWith<boost::optional<BSONObj>> next() final;
+
+    void kill() final;
+
+    /**
+     * Queues a BSONObj to be returned.
+     */
+    void queueResult(BSONObj obj);
+
+    /**
+     * Queues an error response.
+     */
+    void queueError(Status status);
+
+private:
+    bool _killed = false;
+    bool _exhausted = false;
+    std::queue<StatusWith<BSONObj>> _resultsQueue;
+    stdx::function<void(void)> _killCallback;
+};
+
+}  // namespace mongo
