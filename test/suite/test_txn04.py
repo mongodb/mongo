be237028149594899c83d7aefb4e86fe84a6733c
Susan LoVerso <sue@wiredtiger.com>
2015-08-06 13:25:29 -0400

    WT-2031 Restore test_txn04.py.  Accidentally committed a debug version.

--- test/suite/test_txn04.py
+++ test/suite/test_txn04.py
@@ -46,9 +46,6 @@ class test_txn04(wttest.WiredTigerTestCase, suite_subprocess):
         '(method=none,enabled)',
         '(enabled=false)'
     ]
-    sync_list = [
-        '(method=dsync,enabled)',
-    ]
 
     types = [
         ('row', dict(tabletype='row',
@@ -58,18 +55,11 @@ class test_txn04(wttest.WiredTigerTestCase, suite_subprocess):
         ('fix', dict(tabletype='fix',
                     create_params = 'key_format=r,value_format=8t')),
     ]
-    types = [
-        ('row', dict(tabletype='row',
-                    create_params = 'key_format=i,value_format=i')),
-    ]
     op1s = [
         ('insert', dict(op1=('insert', 6))),
         ('update', dict(op1=('update', 2))),
         ('remove', dict(op1=('remove', 2))),
         ('trunc-stop', dict(op1=('stop', 2))),
-    ]
-    op1s = [
-        ('insert', dict(op1=('insert', 6))),
     ]
     txn1s = [('t1c', dict(txn1='commit')), ('t1r', dict(txn1='rollback'))]
 
@@ -132,10 +122,8 @@ class test_txn04(wttest.WiredTigerTestCase, suite_subprocess):
             os.mkdir(self.backup_dir)
 
         cmd += self.backup_dir
-        print "WT command" + cmd
         self.runWt(cmd.split())
         self.exception='false'
-        print "Open backup dir"
         backup_conn_params = 'log=(enabled,file_max=%s)' % self.logmax
         backup_conn = wiredtiger_open(self.backup_dir, backup_conn_params)
         try:
