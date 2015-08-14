9309a888c136521e3e415ca8df5c42f63f1c2ebd
Michael Cahill <michael.cahill@mongodb.com>
2015-08-14 16:20:41 +1000

    WT-2038 Immediately discard temporary handles from session caches.
    This should mean that checkpoint locks don't accumulate waiting for a sweep.

--- src/include/dhandle.h
+++ src/include/dhandle.h
@@ -28,6 +28,11 @@
  */
 #define	WT_SAVE_DHANDLE(s, e) WT_WITH_DHANDLE(s, (s)->dhandle, e)
 
+/* Check if a handle is inactive. */
+#define	WT_DHANDLE_INACTIVE(dhandle)					\
+	(F_ISSET(dhandle, WT_DHANDLE_DEAD) ||				\
+	!F_ISSET(dhandle, WT_DHANDLE_EXCLUSIVE | WT_DHANDLE_OPEN))
+
 /*
  * WT_DATA_HANDLE --
  *	A handle for a generic named data source.
@@ -45,7 +50,6 @@ struct __wt_data_handle {
 	uint32_t session_ref;		/* Sessions referencing this handle */
 	int32_t	 session_inuse;		/* Sessions using this handle */
 	time_t	 timeofdeath;		/* Use count went to 0 */
-	time_t	 timeofdiscard;		/* Time of last failed discard */
 
 	uint64_t name_hash;		/* Hash of name */
 	const char *name;		/* Object name as a URI */
