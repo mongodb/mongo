278338486d13816a0e2d5dbe3b9f2d8ab2df80cb
Alex Gorrod <alexg@wiredtiger.com>
2015-09-10 09:47:34 +1000

    SERVER-20322 Use a separate session when creating new indexes in WiredTiger.

--- src/mongo/db/storage/wiredtiger/wiredtiger_index.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_index.cpp
@@ -203,7 +203,9 @@ StatusWith<std::string> WiredTigerIndex::generateCreateString(const std::string&
 int WiredTigerIndex::Create(OperationContext* txn,
                             const std::string& uri,
                             const std::string& config) {
-    WT_SESSION* s = WiredTigerRecoveryUnit::get(txn)->getSession(txn)->getSession();
+    // Don't use the session from the recovery unit: create should not be used in a transaction
+    WiredTigerSession session(WiredTigerRecoveryUnit::get(txn)->getSessionCache()->conn());
+    WT_SESSION *s = session.getSession();
     LOG(1) << "create uri: " << uri << " config: " << config;
     return s->create(s, uri.c_str(), config.c_str());
 }
