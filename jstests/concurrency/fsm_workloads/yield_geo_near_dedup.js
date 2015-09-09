da43f0452e9f93788c19dbee4e541cfe4593628a
Max Hirschhorn <max.hirschhorn@mongodb.com>
2015-09-09 09:26:09 -0400

    SERVER-20145 Fix yield_geo_near_dedup.js to work with re-inserted docs.

--- jstests/concurrency/fsm_workloads/yield_geo_near_dedup.js
+++ jstests/concurrency/fsm_workloads/yield_geo_near_dedup.js
@@ -10,6 +10,21 @@ load('jstests/concurrency/fsm_workloads/yield_geo_near.js'); // for $config
 
 var $config = extendWorkload($config, function($config, $super) {
 
+    $config.states.remove = function remove(db, collName) {
+        var id = Random.randInt(this.nDocs);
+        var doc = db[collName].findOne({ _id: id });
+        if (doc !== null) {
+            var res = db[collName].remove({ _id: id });
+            assertAlways.writeOK(res);
+            if (res.nRemoved > 0) {
+                // Re-insert the document with the same '_id', but an incremented 'timesInserted' to
+                // distinguish it from the deleted document.
+                doc.timesInserted++;
+                assertAlways.writeOK(db[collName].insert(doc));
+            }
+        }
+    };
+
     /*
      * Use geo $nearSphere query to find points near the origin. Note this should be done using the
      * geoNear command, rather than a $nearSphere query, as the $nearSphere query doesn't work in a
@@ -34,29 +49,35 @@ var $config = extendWorkload($config, function($config, $super) {
         assertWhenOwnColl.commandWorked(res);
         assertWhenOwnColl(function verifyResults() {
             var results = res.results;
-            // TODO: Support non-primitive _id
-            var ids = [];
+            var seenObjs = [];
             for (var i = 0; i < results.length; i++) {
-                var id = results[i].obj._id;
-                assertWhenOwnColl.eq(-1, ids.indexOf(id),
-                    'geoNear command returned a duplicate id of [ ' + id + ' ]: ' + tojson(ids));
-                ids.push(id);
+                var doc = results[i].obj;
+
+                // The pair (_id, timesInserted) is the smallest set of attributes that uniquely
+                // identifies a document.
+                var objToSearchFor = { _id: doc._id, timesInserted: doc.timesInserted };
+                var found = seenObjs.some(function(obj) {
+                    return bsonWoCompare(obj, objToSearchFor) === 0;
+                });
+                assertWhenOwnColl(!found, 'geoNear command returned the document ' + tojson(doc) +
+                                  ' multiple times: ' + tojson(seenObjs));
+                seenObjs.push(objToSearchFor);
             }
         });
     };
 
     $config.data.genUpdateDoc = function genUpdateDoc() {
         // Attempts to perform an in-place update to trigger an invalidation on MMAP v1.
-        return { $inc: { num: 1 } };
+        return { $inc: { timesUpdated: 1 } };
     };
 
     $config.data.getIndexSpec = function getIndexSpec() {
         return { geo: '2dsphere' };
-    }
+    };
 
     $config.data.getReplaceSpec = function getReplaceSpec(i, coords) {
-        return { _id: i, geo: coords, num: 0 };
-    }
+        return { _id: i, geo: coords, timesUpdated: 0, timesInserted: 0 };
+    };
 
     return $config;
 });
