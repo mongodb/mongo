ef78d1b66f3f88ce4b68ea7c93496c2287341f98
Yunhe (John) Wang <yunhe.wang@mongodb.com>
2015-09-11 17:09:45 -0400

    SERVER-19361 Prevent inserting of doc with more than one _id field
    (cherry picked from commit 61baf779dacca5ae17ac3ea1c3338b039824c280)

--- src/mongo/db/ops/insert.cpp
+++ src/mongo/db/ops/insert.cpp
@@ -47,6 +47,7 @@ StatusWith<BSONObj> fixDocumentForInsert(const BSONObj& doc) {
 
     bool firstElementIsId = doc.firstElement().fieldNameStringData() == "_id";
     bool hasTimestampToFix = false;
+    bool hadId = false;
     {
         BSONObjIterator i(doc);
         while (i.more()) {
@@ -69,6 +70,7 @@ StatusWith<BSONObj> fixDocumentForInsert(const BSONObj& doc) {
 
             // check no regexp for _id (SERVER-9502)
             // also, disallow undefined and arrays
+            // Make sure _id isn't duplicated (SERVER-19361).
             if (str::equals(fieldName, "_id")) {
                 if (e.type() == RegEx) {
                     return StatusWith<BSONObj>(ErrorCodes::BadValue, "can't use a regex for _id");
@@ -86,14 +88,18 @@ StatusWith<BSONObj> fixDocumentForInsert(const BSONObj& doc) {
                     if (!s.isOK())
                         return StatusWith<BSONObj>(s);
                 }
+                if (hadId) {
+                    return StatusWith<BSONObj>(ErrorCodes::BadValue,
+                                               "can't have multiple _id fields in one document");
+                } else {
+                    hadId = true;
+                }
             }
         }
     }
 
     if (firstElementIsId && !hasTimestampToFix)
         return StatusWith<BSONObj>(BSONObj());
-
-    bool hadId = firstElementIsId;
 
     BSONObjIterator i(doc);
 
@@ -105,7 +111,6 @@ StatusWith<BSONObj> fixDocumentForInsert(const BSONObj& doc) {
         BSONElement e = doc["_id"];
         if (e.type()) {
             b.append(e);
-            hadId = true;
         } else {
             b.appendOID("_id", NULL, true);
         }
