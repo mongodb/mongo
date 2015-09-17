3be238f557a8c7167b4e5b5a19b9953aba461e57
David Storch <david.storch@10gen.com>
2015-09-17 17:06:47 -0400

    SERVER-20194 modify read_pref.js sharding test to accommodate explain format unification
    A side effect of the new mongos read path is that the explain format will be the
    same, regardless of whether explain is specified using $explain or the explain
    command.

--- jstests/sharding/read_pref.js
+++ jstests/sharding/read_pref.js
@@ -99,7 +99,6 @@ var doTest = function(useDollarQuerySyntax) {
         return false;
     });
 
-
     var getExplain = function(readPrefMode, readPrefTags) {
         if (useDollarQuerySyntax) {
             var readPrefObj = {
@@ -121,7 +120,19 @@ var doTest = function(useDollarQuerySyntax) {
     var getExplainServer = function(explain) {
         var serverInfo;
 
-        if (useDollarQuerySyntax) {
+        // TODO: Remove this when SERVER-20194 is resolved. The resolution of this ticket will unify
+        // the formats returned by queries routed through mongos with the $explain flag and explain
+        // commands routed through mongos.
+        var cmdRes = coll.getDB("test").adminCommand({
+            getParameter: 1,
+            useClusterClientCursor: 1
+        });
+        assert.commandWorked(cmdRes);
+        var useCCC = cmdRes.useClusterClientCursor;
+
+        // TODO: Remove the if block and unconditionally use the code in the else block under
+        // SERVER-20194.
+        if (useDollarQuerySyntax && !useCCC) {
             serverInfo = explain.serverInfo;
         }
         else {
