ed51568a5d7351538248323324fdc63a26c60213
Scott Hernandez <scotthernandez@gmail.com>
2015-09-22 11:40:43 -0400

    cleanup replsets/single_server_majority.js test

--- jstests/replsets/single_server_majority.js
+++ jstests/replsets/single_server_majority.js
@@ -1,11 +1,10 @@
 // This test checks that w:"majority" works correctly on a lone mongod
 
-// set up a mongod and connect a mongo
+// set up a mongod and connect
 var mongod = MongoRunner.runMongod({});
-var mongo = startMongoProgram("mongo", "--port", mongod.port);
 
 // get db and collection, then preform a trivial insert 
-db = mongo.getDB("test")
+db = mongod.getDB("test")
 col = db.getCollection("single_server_majority");
 col.drop();
 // see if we can get a majority write on this single server
