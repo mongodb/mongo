dd9ffe930af98cb757516e151674958b1fe0a1d4
Gabriel Russell <gabriel.russell@mongodb.com>
2015-09-23 11:42:27 -0400

    TOOLS-847 stop handling SIGHUP

--- common/signals/signals_unix.go
+++ common/signals/signals_unix.go
@@ -12,7 +12,7 @@ import (
 func Handle() {
 	// make the chan buffered to avoid a race where the signal comes in after we start notifying but before we start listening
 	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
 	<-sigChan
 	os.Exit(util.ExitKill)
 }
