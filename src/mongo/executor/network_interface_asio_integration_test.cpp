5dd9557b643cc4fd3031425cd001b3e526939db5
Adam Midvidy <amidvidy@gmail.com>
2015-09-22 21:16:32 -0400

    SERVER-20562 implement ASIO stress test

--- src/mongo/executor/network_interface_asio_integration_test.cpp
+++ src/mongo/executor/network_interface_asio_integration_test.cpp
@@ -30,6 +30,7 @@
 
 #include "mongo/platform/basic.h"
 
+#include <algorithm>
 #include <exception>
 
 #include "mongo/client/connection_string.h"
@@ -39,12 +40,14 @@
 #include "mongo/executor/network_interface_asio.h"
 #include "mongo/executor/network_interface_asio_test_utils.h"
 #include "mongo/executor/task_executor.h"
+#include "mongo/platform/random.h"
 #include "mongo/rpc/get_status_from_command_result.h"
 #include "mongo/stdx/future.h"
 #include "mongo/stdx/memory.h"
 #include "mongo/unittest/integration_test.h"
 #include "mongo/unittest/unittest.h"
 #include "mongo/util/assert_util.h"
+#include "mongo/util/concurrency/thread_pool.h"
 #include "mongo/util/log.h"
 #include "mongo/util/scopeguard.h"
 
@@ -58,6 +61,7 @@ public:
         NetworkInterfaceASIO::Options options{};
         options.streamFactory = stdx::make_unique<AsyncStreamFactory>();
         options.timerFactory = stdx::make_unique<AsyncTimerFactoryASIO>();
+        options.connectionPoolOptions.maxConnections = 256u;
         _net = stdx::make_unique<NetworkInterfaceASIO>(std::move(options));
         _net->startup();
     }
@@ -76,17 +80,19 @@ public:
         return unittest::getFixtureConnectionString();
     }
 
-    StatusWith<RemoteCommandResponse> runCommand(const RemoteCommandRequest& request) {
-        auto cb = makeCallbackHandle();
+    Deferred<StatusWith<RemoteCommandResponse>> runCommand(
+        const TaskExecutor::CallbackHandle& cbHandle, const RemoteCommandRequest& request) {
         Deferred<StatusWith<RemoteCommandResponse>> deferred;
-        log() << "running command: " << request.toString();
-
-        net().startCommand(cb,
+        net().startCommand(cbHandle,
                            request,
-                           [&deferred](StatusWith<RemoteCommandResponse> resp) {
+                           [deferred](StatusWith<RemoteCommandResponse> resp) mutable {
                                deferred.emplace(std::move(resp));
                            });
+        return deferred;
+    }
 
+    StatusWith<RemoteCommandResponse> runCommandSync(const RemoteCommandRequest& request) {
+        auto deferred = runCommand(makeCallbackHandle(), request);
         auto& res = deferred.get();
         if (res.isOK()) {
             log() << "got command result: " << res.getValue().toString();
@@ -99,8 +105,8 @@ public:
     void assertCommandOK(StringData db,
                          const BSONObj& cmd,
                          Milliseconds timeoutMillis = Milliseconds(-1)) {
-        auto res = unittest::assertGet(
-            runCommand({fixture().getServers()[0], db.toString(), cmd, BSONObj(), timeoutMillis}));
+        auto res = unittest::assertGet(runCommandSync(
+            {fixture().getServers()[0], db.toString(), cmd, BSONObj(), timeoutMillis}));
         ASSERT_OK(getStatusFromCommandResult(res.data));
     }
 
@@ -108,8 +114,8 @@ public:
                                     const BSONObj& cmd,
                                     Milliseconds timeoutMillis,
                                     ErrorCodes::Error reason) {
-        auto clientStatus =
-            runCommand({fixture().getServers()[0], db.toString(), cmd, BSONObj(), timeoutMillis});
+        auto clientStatus = runCommandSync(
+            {fixture().getServers()[0], db.toString(), cmd, BSONObj(), timeoutMillis});
         ASSERT_TRUE(clientStatus == reason);
     }
 
@@ -117,8 +123,8 @@ public:
                                     const BSONObj& cmd,
                                     Milliseconds timeoutMillis,
                                     ErrorCodes::Error reason) {
-        auto res = unittest::assertGet(
-            runCommand({fixture().getServers()[0], db.toString(), cmd, BSONObj(), timeoutMillis}));
+        auto res = unittest::assertGet(runCommandSync(
+            {fixture().getServers()[0], db.toString(), cmd, BSONObj(), timeoutMillis}));
         auto serverStatus = getStatusFromCommandResult(res.data);
         ASSERT_TRUE(serverStatus == reason);
     }
@@ -160,12 +166,139 @@ TEST_F(NetworkInterfaceASIOIntegrationTest, Timeouts) {
                                Milliseconds(10000000000),  // big, big timeout.
                                ErrorCodes::JSInterpreterFailure);
 
-    // Run a find command with a big timeout. It should return before we hit the ASIO timeout.
+    // Run a find command with a big timeout.It should return before we hit the ASIO
+    // timeout
     assertCommandOK("foo",
                     BSON("find"
                          << "bar"
                          << "limit" << 1),
                     Milliseconds(10000000));
+}
+
+class StressTestOp {
+public:
+    using Fixture = NetworkInterfaceASIOIntegrationTest;
+    using Pool = ThreadPoolInterface;
+
+    Deferred<Status> run(Fixture* fixture, Pool* pool) {
+        auto cb = makeCallbackHandle();
+        auto self = *this;
+        auto out =
+            fixture->runCommand(cb,
+                                {unittest::getFixtureConnectionString().getServers()[0],
+                                 "foo",
+                                 _command,
+                                 Seconds(5)})
+                .then(pool,
+                      [self](StatusWith<RemoteCommandResponse> resp) -> Status {
+                          auto status = resp.isOK()
+                              ? getStatusFromCommandResult(resp.getValue().data)
+                              : resp.getStatus();
+
+                          return status == self._expected
+                              ? Status::OK()
+                              : Status{ErrorCodes::BadValue,
+                                       str::stream() << "Expected "
+                                                     << ErrorCodes::errorString(self._expected)
+                                                     << " but got " << status.toString()};
+                      });
+        if (_cancel) {
+            // TODO: have this happen at some random time in the future.
+            fixture->net().cancelCommand(cb);
+        }
+        return out;
+    }
+
+    static Deferred<Status> runTimeoutOp(Fixture* fixture, Pool* pool) {
+        return StressTestOp(BSON("find"
+                                 << "bar"
+                                 << "filter" << BSON("$where"
+                                                     << "while(true) { sleep(1); }")),
+                            ErrorCodes::ExceededTimeLimit,
+                            false).run(fixture, pool);
+    }
+
+    static Deferred<Status> runCompleteOp(Fixture* fixture, Pool* pool) {
+        return StressTestOp(BSON("find"
+                                 << "baz"
+                                 << "limit" << 1),
+                            ErrorCodes::OK,
+                            false).run(fixture, pool);
+    }
+
+    static Deferred<Status> runCancelOp(Fixture* fixture, Pool* pool) {
+        return StressTestOp(BSON("find"
+                                 << "bar"
+                                 << "filter" << BSON("$where"
+                                                     << "while(true) { sleep(1); }")),
+                            ErrorCodes::CallbackCanceled,
+                            true).run(fixture, pool);
+    }
+
+private:
+    StressTestOp(const BSONObj& command, ErrorCodes::Error expected, bool cancel)
+        : _command(command), _expected(expected), _cancel(cancel) {}
+
+    BSONObj _command;
+    ErrorCodes::Error _expected;
+    bool _cancel;
+};
+
+TEST_F(NetworkInterfaceASIOIntegrationTest, StressTest) {
+    const std::size_t numOps = 10000;
+    std::vector<Deferred<Status>> ops;
+
+    std::unique_ptr<SecureRandom> seedSource{SecureRandom::create()};
+    auto seed = seedSource->nextInt64();
+
+    log() << "Random seed is " << seed;
+    auto rng = PseudoRandom(seed);  // TODO: read from command line
+    log() << "Starting stress test...";
+
+    ThreadPool::Options threadPoolOpts;
+    threadPoolOpts.poolName = "StressTestPool";
+    threadPoolOpts.maxThreads = 8;
+    ThreadPool pool(threadPoolOpts);
+    pool.startup();
+
+    auto poolGuard = MakeGuard([&pool] {
+        pool.schedule([&pool] { pool.shutdown(); });
+        pool.join();
+    });
+
+    std::generate_n(std::back_inserter(ops),
+                    numOps,
+                    [&rng, &pool, this] {
+                        switch (rng.nextInt32(3)) {
+                            case 0:
+                                return StressTestOp::runCancelOp(this, &pool);
+                            case 1:
+                                return StressTestOp::runCompleteOp(this, &pool);
+
+                            case 2:
+                                // TODO: Reenable runTimeoutOp after we fix whatever bug causes it
+                                // to hang.
+                                // return StressTestOp::runTimeoutOp(this, &pool);
+                                return StressTestOp::runCompleteOp(this, &pool);
+                            default:
+
+                                MONGO_UNREACHABLE;
+                        }
+                    });
+
+    log() << "running ops";
+    auto res = helpers::collect(ops, &pool)
+                   .then(&pool,
+                         [](std::vector<Status> opResults) -> Status {
+                             for (const auto& opResult : opResults) {
+                                 if (!opResult.isOK()) {
+                                     return opResult;
+                                 }
+                             }
+                             return Status::OK();
+                         })
+                   .get();
+    ASSERT_OK(res);
 }
 
 }  // namespace
--- src/mongo/executor/network_interface_asio_test_utils.h
+++ src/mongo/executor/network_interface_asio_test_utils.h
@@ -29,10 +29,14 @@
 #include <memory>
 
 #include <boost/optional.hpp>
+#include <utility>
+#include <vector>
 
 #include "mongo/executor/task_executor.h"
 #include "mongo/stdx/condition_variable.h"
+#include "mongo/stdx/future.h"
 #include "mongo/stdx/mutex.h"
+#include "mongo/util/concurrency/thread_pool_interface.h"
 
 namespace mongo {
 namespace executor {
@@ -59,23 +63,92 @@ class Deferred {
 public:
     template <typename... Args>
     void emplace(Args&&... args) {
-        stdx::unique_lock<stdx::mutex> lk(_mtx);
-        _thing.emplace(std::forward<T>(args)...);
-        _cv.notify_one();
+        _emplace(_state.get(), std::forward<Args>(args)...);
     }
 
     T& get() {
-        stdx::unique_lock<stdx::mutex> lk(_mtx);
-        _cv.wait(lk, [this] { return _thing.is_initialized(); });
-        return *_thing;
+        return _get(_state.get());
+    }
+
+    template <typename Continuation>
+    auto then(ThreadPoolInterface* pool, Continuation&& continuation)
+        -> Deferred<decltype(continuation(std::declval<Deferred<T>>().get()))> {
+        // XXX: The ugliness of the above type signature is because you can't refer to 'this' in
+        // a template parameter, at least on g++-4.8.2.
+        auto state = _state;
+        Deferred<decltype(continuation(get()))> thenDeferred;
+
+        pool->schedule([this, thenDeferred, continuation, state]() mutable {
+            thenDeferred.emplace(continuation(_get(state.get())));
+        });
+        return thenDeferred;
     }
 
 private:
-    stdx::mutex _mtx;
-    stdx::condition_variable _cv;
-    boost::optional<T> _thing;
+    struct State {
+        stdx::mutex mtx;
+        stdx::condition_variable cv;
+        boost::optional<T> thing;
+    };
+
+    template <typename... Args>
+    void _emplace(State* state, Args&&... args) {
+        stdx::lock_guard<stdx::mutex> lk(_state->mtx);
+        invariant(!state->thing.is_initialized());
+        state->thing.emplace(std::forward<T>(args)...);
+        state->cv.notify_one();
+    }
+
+    T& _get(State* state) {
+        stdx::unique_lock<stdx::mutex> lk(state->mtx);
+        state->cv.wait(lk, [state] { return state->thing.is_initialized(); });
+        return *state->thing;
+    }
+
+private:
+    std::shared_ptr<State> _state = std::make_shared<State>();
 };
 
+namespace helpers {
+
+template <typename T>
+static Deferred<std::vector<T>> collect(std::vector<Deferred<T>>& ds, ThreadPoolInterface* pool) {
+    Deferred<std::vector<T>> out;
+    struct CollectState {
+        // hack to avoid requiring U to be default constructible.
+        std::vector<boost::optional<T>> mem{};
+        std::size_t numFinished = 0;
+        std::size_t goal = 0;
+        stdx::mutex mtx;
+    };
+
+    auto collectState = std::make_shared<CollectState>();
+    collectState->goal = ds.size();
+    collectState->mem.resize(collectState->goal);
+
+    for (std::size_t i = 0; i < ds.size(); ++i) {
+        ds[i].then(pool,
+                   [collectState, out, i](T res) mutable {
+                       // The bool return is unused.
+                       stdx::lock_guard<stdx::mutex> lk(collectState->mtx);
+                       collectState->mem[i] = std::move(res);
+
+                       // If we're done.
+                       if (collectState->goal == ++collectState->numFinished) {
+                           std::vector<T> outInitialized;
+                           outInitialized.reserve(collectState->mem.size());
+                           for (auto&& mem_entry : collectState->mem) {
+                               outInitialized.emplace_back(std::move(*mem_entry));
+                           }
+                           out.emplace(outInitialized);
+                       }
+                       return true;
+                   });
+    }
+    return out;
+}
+
+}  // namespace helpers
 
 }  // namespace executor
 }  // namespace mongo
