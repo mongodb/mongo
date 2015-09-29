257845475d4fb578a98e428260d889f0876f7e35
Andy Schwerin <schwerin@mongodb.com>
2015-09-29 16:54:50 -0400

    SERVER-19418 Work around MSVC2013 function static initializer bug in ShardedConnectionInfo::addHook.
    MSVC2013 doesn't provide thread-safe initialization of function
    statics,so this patch converts ShardedConnectionHook::addHook's function-static
    mutex to a file-static mutex, which will be initialized before main().

--- src/mongo/db/s/sharded_connection_info.cpp
+++ src/mongo/db/s/sharded_connection_info.cpp
@@ -38,6 +38,7 @@
 #include "mongo/client/global_conn_pool.h"
 #include "mongo/db/client.h"
 #include "mongo/db/operation_context.h"
+#include "mongo/platform/atomic_word.h"
 #include "mongo/s/chunk_version.h"
 #include "mongo/s/client/shard_connection.h"
 #include "mongo/s/client/sharding_connection_hook.h"
@@ -85,20 +86,26 @@ void ShardedConnectionInfo::setVersion(const std::string& ns, const ChunkVersion
     _versions[ns] = version;
 }
 
+namespace {
+stdx::mutex addHookMutex;
+AtomicUInt32 alreadyAddedHook{0};
+}  // namespace
+
 void ShardedConnectionInfo::addHook() {
-    static stdx::mutex lock;
-    static bool done = false;
-
-    stdx::lock_guard<stdx::mutex> lk(lock);
-    if (!done) {
-        log() << "first cluster operation detected, adding sharding hook to enable versioning "
-                 "and authentication to remote servers";
-
-        globalConnPool.addHook(new ShardingConnectionHook(false));
-        shardConnectionPool.addHook(new ShardingConnectionHook(true));
-
-        done = true;
+    if (alreadyAddedHook.loadRelaxed()) {
+        return;
     }
+    stdx::lock_guard<stdx::mutex> lk{addHookMutex};
+    if (alreadyAddedHook.load()) {
+        return;
+    }
+    log() << "first cluster operation detected, adding sharding hook to enable versioning "
+             "and authentication to remote servers";
+
+    globalConnPool.addHook(new ShardingConnectionHook(false));
+    shardConnectionPool.addHook(new ShardingConnectionHook(true));
+
+    alreadyAddedHook.store(1);
 }
 
 }  // namespace mongo
