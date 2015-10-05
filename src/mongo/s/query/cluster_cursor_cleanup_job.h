01dead3f2c56292f08faf5461e60e79f5ac3ba3a
Jason Rassi <rassi@10gen.com>
2015-10-05 13:32:09 -0400

    SERVER-20194 SERVER-20758 Update ticket number in s/query/ comments

--- src/mongo/s/query/cluster_cursor_cleanup_job.h
+++ src/mongo/s/query/cluster_cursor_cleanup_job.h
@@ -48,7 +48,7 @@ public:
      * Period of time after which mortal cursors are killed for inactivity. Configurable with
      * server parameter "cursorTimeoutMillis".
      *
-     * TODO: Move declaration to cpp file once CursorCache class is deleted. See SERVER-20194 for
+     * TODO: Move declaration to cpp file once CursorCache class is deleted. See SERVER-20758 for
      * more details.
      */
     static std::atomic<long long> cursorTimeoutMillis;
