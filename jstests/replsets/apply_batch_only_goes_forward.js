7f62960716b37662070ba6fada747c60f5968c09
Scott Hernandez <scotthernandez@gmail.com>
2015-10-06 12:45:40 -0400

    SERVER-20326: Make sure to update the existing minvalid doc if it exists

--- jstests/replsets/apply_batch_only_goes_forward.js
+++ jstests/replsets/apply_batch_only_goes_forward.js
@@ -30,19 +30,30 @@
     var sLocal = slave.getDB("local");
     var sMinvalid = sLocal["replset.minvalid"];
     var stepDownSecs = 30;
-    var stepDownCmd = {replSetStepDown:stepDownSecs, force:true};
+    var stepDownCmd = {replSetStepDown: stepDownSecs, force: true};
 
     // Write op
-    assert.writeOK(mTest.foo.save({}, {writeConcern:{w:3}}));
+    assert.writeOK(mTest.foo.save({}, {writeConcern: {w: 3}}));
     replTest.waitForState(slave, replTest.SECONDARY, 30000);
-    assert.writeOK(mTest.foo.save({}, {writeConcern:{w:3}}));
+    assert.writeOK(mTest.foo.save({}, {writeConcern: {w: 3}}));
 
     // Set minvalid to something far in the future for the current primary, to simulate recovery.
     // Note: This is so far in the future (5 days) that it will never become secondary.
     var farFutureTS = new Timestamp(Math.floor(new Date().getTime()/1000) +
                                                (60 * 60 * 24 * 5 /* in five days*/), 0);
-    print("future TS: " + tojson(farFutureTS) + ", date:" + tsToDate(farFutureTS));
-    assert.writeOK(mMinvalid.save({_id:1, ts: farFutureTS, t:NumberLong(-1)}));
+    var rsgs = assert.commandWorked(mLocal.adminCommand("replSetGetStatus"));
+    var primaryOpTime = rsgs.members.filter( function (member) {
+                                                        return member.self;}
+                                            )[0].optime;
+    jsTest.log("future TS: " + tojson(farFutureTS) + ", date:" + tsToDate(farFutureTS));
+    // We do an update in case there is a minvalid document on the primary already.
+    // If the doc doesn't exist then upsert:true will create it, and the writeConcern ensures
+    // that update returns details of the write, like whether an update or insert was performed.
+    printjson(assert.writeOK(mMinvalid.update({},
+                                              { ts: farFutureTS,
+                                                t: NumberLong(-1),
+                                                begin: primaryOpTime},
+                                              { upsert: true, writeConcern: {w: 1}})));
 
     jsTest.log("restart primary");
     replTest.restart(master);
@@ -50,7 +61,7 @@
     replTest.waitForState(master, replTest.RECOVERING, 90000);
 
     // Slave is now master... so do a write to get a minvalid entry on the secondary.
-    assert.writeOK(replTest.getMaster().getDB("test").foo.save({}, {writeConcern:{w:3}}));
+    assert.writeOK(replTest.getMaster().getDB("test").foo.save({}, {writeConcern: {w: 3}}));
 
     assert.soon(function() {
         var mv;
