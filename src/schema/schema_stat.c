9601694d41b4c1812cb6246ca98e4707c633291c
Alex Gorrod <alexg@wiredtiger.com>
2015-10-16 17:13:09 +1100

    WT-2174 Avoid the table list lock when creating a size only statistics cursor.
    When there are lots of tables being created and dropped it can take
    a long time to get the table lock.

--- src/schema/schema_stat.c
+++ src/schema/schema_stat.c
@@ -53,6 +53,61 @@ err:	__wt_scr_free(session, &buf);
 }
 
 /*
+ * __curstat_size_only --
+ *	 For very simple tables we can avoid getting table handles if
+ *	 configured to only retrieve the size. It's worthwhile because
+ *	 workloads that create and drop a lot of tables can put a lot of
+ *	 pressure on the table list lock.
+ */
+static int
+__curstat_size_only(WT_SESSION_IMPL *session,
+    const char *uri, bool *was_fast,WT_CURSOR_STAT *cst)
+{
+	WT_CONFIG cparser;
+	WT_CONFIG_ITEM ckey, colconf, cval;
+	WT_DECL_RET;
+	WT_ITEM namebuf;
+	wt_off_t filesize;
+	char *tableconf;
+
+	WT_CLEAR(namebuf);
+	*was_fast = false;
+
+	if (!F_ISSET(cst, WT_CONN_STAT_SIZE))
+		return (0);
+
+	WT_RET(__wt_metadata_search(session, uri, &tableconf));
+	WT_ERR(__wt_config_getones(session, tableconf, "columns", &colconf));
+
+	WT_ERR(__wt_config_subinit(session, &cparser, &colconf));
+	if ((ret = __wt_config_next(&cparser, &ckey, &cval)) == 0)
+		WT_ERR(WT_NOTFOUND);
+
+	/* Build up a file name from a table URI. */
+	WT_ERR(__wt_buf_fmt(
+	    session, &namebuf, "%s.wt", uri + strlen("table:")));
+	/*
+	 * Get the size of the underlying file.
+	 * TODO: It's possible that we will race with a table drop here, if
+	 * we do then there will be an error message generated.
+	 */
+	WT_ERR(__wt_filesize_name(session, namebuf.data, &filesize));
+
+	/* Setup the statistics structure */
+	__wt_stat_dsrc_init_single(&cst->u.dsrc_stats);
+	cst->u.dsrc_stats.block_size = filesize;
+	__wt_curstat_dsrc_final(cst);
+
+err:	__wt_free(session, tableconf);
+	__wt_buf_free(session, &namebuf);
+
+	if (ret == 0)
+		*was_fast = true;
+
+	return (ret);
+}
+
+/*
  * __wt_curstat_table_init --
  *	Initialize the statistics for a table.
  */
@@ -67,6 +122,11 @@ __wt_curstat_table_init(WT_SESSION_IMPL *session,
 	WT_TABLE *table;
 	u_int i;
 	const char *name;
+	bool was_fast;
+
+	WT_RET(__curstat_size_only(session, uri, &was_fast, cst));
+	if (was_fast)
+		return (0);
 
 	name = uri + strlen("table:");
 	WT_RET(__wt_schema_get_table(
