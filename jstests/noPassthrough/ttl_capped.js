18ed396f69f27476456c7564c9b73a223b5cea16
Max Hirschhorn <max.hirschhorn@mongodb.com>
2015-10-16 19:38:29 -0400

    SERVER-20920 Wait longer for the TTL monitor to finish in ttl_capped.js.
    Use db.serverStatus().metrics.ttl.passes to know when the TTL monitor
    has finished processing all of the TTL indexes.
    
    Create many collections with a TTL index to increase the odds that the
    TTL monitor would process a non-capped collection after a capped
    collection.

--- jstests/noPassthrough/ttl_capped.js
+++ jstests/noPassthrough/ttl_capped.js
@@ -1,52 +1,82 @@
 /**
- * Test that a capped collection ttl index doesn't cause the server to shut down, 
- * nor stop processing collections in the db
- * TODO: Change this test to show that you can't create capped collection ttl index 
- *       Will need to figure out how to test that failed index ttl processing doesn't block others
+ * Test that a TTL index on a capped collection doesn't crash the server or cause the TTL monitor
+ * to skip processing other (non-capped) collections on the database.
  */
 (function() {
     "use strict";
 
-    var baseDir = "jstests_ttl_capped";
-    var dbpath = MongoRunner.dataPath + baseDir + "/";
-    
-    var m = MongoRunner.runMongod({
-                                    dbpath: dbpath,
-                                    setParameter:"ttlMonitorSleepSecs=1"});
-    var db = m.getDB( "test" );
-    
-    // Make sure we have collections before and after the capped one, so we can check they work.
-    var bc = db.ttl_before;
-    var t = db.ttl_capped;
-    var ac = db.ttl_zafter;
-    
-    t.drop();
-    bc.drop();
-    ac.drop();
-    var dt = (new Date()).getTime();
-    jsTest.log("using date on inserted docs: " + tojson(new Date(dt)));
-    
-    // increase logging
-    assert.commandWorked(db.adminCommand({setParameter:1, logLevel:1}));
+    var dbpath = MongoRunner.dataPath + "ttl_capped";
+    resetDbpath(dbpath);
 
-    bc.ensureIndex( { x : 1 } , { expireAfterSeconds : -1 } );
-    assert.commandWorked(t.runCommand( "create", { capped:true, size:100, maxCount:100} ));
-    t.ensureIndex( { x : 1 } , { expireAfterSeconds : -1 } );
-    ac.ensureIndex( { x : 1 } , { expireAfterSeconds : -1 } );
-    
-    assert.writeOK(bc.insert({x: new Date(dt)}));
-    assert.writeOK(t.insert({x: new Date(dt)}));
-    assert.writeOK(ac.insert({x: new Date(dt)}));
-    
-    assert.eq(bc.count(), 1);
-    assert.eq(t.count(), 1);
-    assert.eq(ac.count(), 1);
+    var conn = MongoRunner.runMongod({
+        dbpath: dbpath,
+        noCleanData: true,
+        setParameter: "ttlMonitorSleepSecs=1",
+    });
+    assert.neq(null, conn, "mongod was unable to start up");
 
-    sleep(1.1 * 1000); // 2 second sleep
-    jsTest.log("TTL work should be done.")
-    assert.eq(bc.count(), 0);
-    assert.eq(t.count(), 1);
-    assert.eq(ac.count(), 0);
-    
-    MongoRunner.stopMongod(m.port);
+    var testDB = conn.getDB("test");
+
+    assert.commandWorked(testDB.adminCommand({setParameter: 1, ttlMonitorEnabled: false}));
+
+    var now = Date.now();
+    var expireAfterSeconds = 10;
+
+    var numCollectionsToCreate = 20;
+    var width = numCollectionsToCreate.toString().length;
+
+    // Create 'numCollectionsToCreate' collections with a TTL index, where every third collection is
+    // capped. We create many collections with a TTL index to increase the odds that the TTL monitor
+    // would process a non-capped collection after a capped collection. This allows us to verify
+    // that the TTL monitor continues processing the remaining collections after encountering an
+    // error processing a capped collection.
+    for (var i = 0; i < numCollectionsToCreate; i++) {
+        var collName = "ttl" + i.zeroPad(width);
+        if (i % 3 === 1) {
+            assert.commandWorked(testDB.createCollection(collName, {capped: true, size: 4096}));
+        }
+
+        // Create a TTL index on the 'date' field of the collection.
+        var res = testDB[collName].ensureIndex({date: 1}, {expireAfterSeconds: expireAfterSeconds});
+        assert.commandWorked(res);
+
+        // Insert a single document with a 'date' field that is already expired according to the
+        // index definition.
+        assert.writeOK(testDB[collName].insert({date: new Date(now - expireAfterSeconds * 1000)}));
+    }
+
+    // Increase the verbosity of the TTL monitor's output.
+    assert.commandWorked(testDB.adminCommand({setParameter: 1, logComponentVerbosity: {index: 1}}));
+
+    // Enable the TTL monitor and wait for it to run.
+    var ttlPasses = testDB.serverStatus().metrics.ttl.passes;
+    assert.commandWorked(testDB.adminCommand({setParameter: 1, ttlMonitorEnabled: true}));
+
+    var timeoutSeconds = 5;
+    assert.soon(
+        function checkIfTTLMonitorRan() {
+            // The 'ttl.passes' metric is incremented when the TTL monitor starts processing the
+            // indexes, so we wait for it to be incremented twice to know that the TTL monitor
+            // finished processing the indexes at least once.
+            return testDB.serverStatus().metrics.ttl.passes >= ttlPasses + 2;
+        },
+        function msg() {
+            return "TTL monitor didn't run within " + timeoutSeconds + " seconds";
+        },
+        timeoutSeconds * 1000
+    );
+
+    for (var i = 0; i < numCollectionsToCreate; i++) {
+        var coll = testDB["ttl" + i.zeroPad(width)];
+        var count = coll.count();
+        if (i % 3 === 1) {
+            assert.eq(1, count, "the TTL monitor shouldn't have removed expired documents from" +
+                      " the capped collection '" + coll.getFullName() + "'");
+        } else {
+            assert.eq(0, count, "the TTL monitor didn't removed expired documents from the" +
+                      " collection '" + coll.getFullName() + "'");
+        }
+    }
+
+    MongoRunner.stopMongod(conn);
 })();
