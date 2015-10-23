8f7a125ab719157863fc7fc90f783d647a75deeb
Mark Benvenuto <mark.benvenuto@mongodb.com>
2015-10-23 16:10:27 -0400

    SERVER-21104 WiredTiger no longer compiles on non-x86 platforms

--- src/third_party/wiredtiger/build_darwin/wiredtiger_config.h
+++ src/third_party/wiredtiger/build_darwin/wiredtiger_config.h
@@ -119,7 +119,7 @@
 /* #undef HAVE_VERBOSE */
 
 /* Define to 1 if you have the <x86intrin.h> header file. */
-#define HAVE_X86INTRIN_H 1
+/* #undef HAVE_X86INTRIN_H */
 
 /* Spinlock type from mutex.h. */
 #define SPINLOCK_TYPE SPINLOCK_PTHREAD_MUTEX
