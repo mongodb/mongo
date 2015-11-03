2c20582a12dd40eb7ff6a0125d1b34393133d09a
Benety Goh <benety@mongodb.com>
2015-11-03 11:42:51 -0500

    SERVER-21248 additional failover test cases per test plan
    (cherry picked from commit b0a6137a79d64441f1f45790d9302d779366fdfd)

---
+++ jstests/replsets/priority_takeover_cascading_priorities.js
@@ -0,0 +1,43 @@
+// Multiple voting members with varying levels of priority.
+// 5-node replica set with priorities 5, 4, 3, 2 and 1 (default).
+// Start replica set. Ensure that highest priority node becomes primary eventually.
+// Shut down the primary and confirm that the next highest priority node becomes primary.
+// Repeat until 3 nodes are left standing.
+(function () {
+    'use strict';
+    load('jstests/replsets/rslib.js');
+
+    var name = 'priority_takeover_cascading_priorities';
+    var replSet = new ReplSetTest({name: name, nodes: [
+        {rsConfig: {priority: 5}},
+        {rsConfig: {priority: 4}},
+        {rsConfig: {priority: 3}},
+        {rsConfig: {priority: 2}},
+        {rsConfig: {priority: 1}},
+    ]});
+    replSet.startSet();
+    replSet.initiate();
+
+    var waitForPrimary = function(i) {
+        // Refresh connections to nodes.
+        replSet.status();
+        assert.commandWorked(
+            replSet.nodes[i].adminCommand({
+                replSetTest: 1,
+                waitForMemberState: replSet.PRIMARY,
+                timeoutMillis: 60 * 1000,
+            }),
+            'node ' + i + ' ' + replSet.nodes[i].host + ' failed to become primary'
+        );
+    };
+
+    waitForPrimary(0);
+    replSet.stop(0);
+
+    waitForPrimary(1);
+    replSet.stop(1);
+
+    waitForPrimary(2);
+
+    // Cannot stop any more nodes because we will not have a majority.
+})();
