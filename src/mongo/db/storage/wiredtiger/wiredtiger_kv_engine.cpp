884644ac56de2edc2223b75ceabe9e1a6fef6dab
Mark Benvenuto <mark.benvenuto@mongodb.com>
2015-11-05 13:04:22 -0500

    SERVER-20991 Change WiredTiger caching strategy on Windows

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
@@ -169,6 +169,9 @@ WiredTigerKVEngine::WiredTigerKVEngine(const std::string& path,
     ss << ",log_size=2GB),";
     ss << "statistics_log=(wait=" << wiredTigerGlobalOptions.statisticsLogDelaySecs << "),";
     ss << WiredTigerCustomizationHooks::get(getGlobalServiceContext())->getOpenConfig("system");
+#ifdef _WIN32
+    ss << "direct_io=(data),";
+#endif
     ss << extraOpenOptions;
     if (!_durable) {
         // If we started without the journal, but previously used the journal then open with the
