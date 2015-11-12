1b8a8957dc7adc2bba893a6d7953e5256849dca6
Spencer T Brody <spencer@mongodb.com>
2015-11-12 14:50:53 -0500

    SERVER-21435 Use a new shard instead of re-using an existing one in addshard5.js test

--- jstests/sharding/addshard5.js
+++ jstests/sharding/addshard5.js
@@ -5,7 +5,7 @@
 
 'use strict';
 
-var st = new ShardingTest({ shards: 3, mongos: 1 });
+var st = new ShardingTest({ shards: 2, mongos: 1 });
 
 var mongos = st.s;
 var admin = mongos.getDB('admin');
@@ -18,42 +18,39 @@ config.shards.find().sort({ _id: 1 }).forEach(function(doc) {
     shards.push(Object.merge(doc, { conn: new Mongo(doc.host) }));
 });
 
-// Remove the last shard so we can use it later
-assert.commandWorked(mongos.adminCommand({ removeShard: shards[2]._id }));
-assert.commandWorked(mongos.adminCommand({ removeShard: shards[2]._id }));
-
 // Shard collection
 assert.commandWorked(mongos.adminCommand({ enableSharding: coll.getDB() + ''}));
 
 // Just to be sure what primary we start from
-printjson(mongos.adminCommand({ movePrimary: coll.getDB() + '', to: shards[0]._id }));
+st.ensurePrimaryShard(coll.getDB().getName(), shards[0]._id);
 assert.commandWorked(mongos.adminCommand({ shardCollection: coll + '', key: { _id: 1 } }));
 
 // Insert one document
 assert.writeOK(coll.insert({ hello: 'world'}));
 
-// Migrate the collection to and from shard2 so shard1 loads the shard2 host
+// Migrate the collection to and from shard1 so shard0 loads the shard1 host
 assert.commandWorked(mongos.adminCommand(
     { moveChunk: coll + '', find: { _id: 0 }, to: shards[1]._id, _waitForDelete: true }));
 assert.commandWorked(mongos.adminCommand(
     { moveChunk: coll + '', find: { _id: 0 }, to: shards[0]._id, _waitForDelete: true }));
 
-// Drop and re-add shard with last shard's host
+// Drop and re-add shard with the same name but a new host.
 assert.commandWorked(mongos.adminCommand({ removeShard: shards[1]._id }));
 assert.commandWorked(mongos.adminCommand({ removeShard: shards[1]._id }));
 
-assert.commandWorked(mongos.adminCommand({ addShard: shards[2].host, name: shards[1]._id }));
+var shard2 = MongoRunner.runMongod({});
+assert.commandWorked(mongos.adminCommand({ addShard: shard2.host, name: shards[1]._id }));
 
 jsTest.log('Shard was dropped and re-added with same name...');
 st.printShardingStatus();
 
 shards[0].conn.getDB('admin').runCommand({ setParameter: 1, traceExceptions: true });
-shards[2].conn.getDB('admin').runCommand({ setParameter: 1, traceExceptions: true });
+shard2.getDB('admin').runCommand({ setParameter: 1, traceExceptions: true });
 
 // Try a migration
 assert.commandWorked(mongos.adminCommand({ moveChunk: coll + '', find: { _id: 0 }, to: shards[1]._id }));
 
-assert.neq(null, shards[2].conn.getCollection(coll + '').findOne());
+assert.eq('world', shard2.getCollection(coll + '').findOne().hello);
 
 st.stop();
 
