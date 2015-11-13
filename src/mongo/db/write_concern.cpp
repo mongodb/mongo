9e431f4fd9a6af65292aff3729a3f6d281784ac1
matt dannenberg <matt.dannenberg@10gen.com>
2015-11-13 05:28:41 -0500

    SERVER-21362 journal sync in PV0

--- src/mongo/db/write_concern.cpp
+++ src/mongo/db/write_concern.cpp
@@ -67,8 +67,7 @@ void setupSynchronousCommit(OperationContext* txn) {
 namespace {
 // The consensus protocol requires that w: majority implies j: true on all nodes.
 void addJournalSyncForWMajority(WriteConcernOptions* writeConcern) {
-    if (repl::getGlobalReplicationCoordinator()->isV1ElectionProtocol() &&
-        writeConcern->wMode == WriteConcernOptions::kMajority &&
+    if (writeConcern->wMode == WriteConcernOptions::kMajority &&
         writeConcern->syncMode == WriteConcernOptions::NONE &&
         getGlobalServiceContext()->getGlobalStorageEngine()->isDurable()) {
         writeConcern->syncMode = WriteConcernOptions::JOURNAL;
