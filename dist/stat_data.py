33e0955a2d4f648951647bdd0b4749be60636da7
Keith Bostic <keith@wiredtiger.com>
2015-11-29 15:10:57 -0500

    The no_clear flag doesn't imply no_scale (at least, it doesn't imply it at this level, no_clear doesn't cause an entry to be output in the no_scale list), and all entries with no_clear also list no_scale.

--- dist/stat_data.py
+++ dist/stat_data.py
@@ -16,12 +16,8 @@
 #       no_clear        Value not cleared when statistics cleared
 #       no_scale        Don't scale value per second in the logging tool script
 #
-# The no_clear flag is a little complicated: it means we don't clear the values
-# when resetting statistics after each run (necessary when the WiredTiger engine
-# is updating values that persist over multiple runs, for example the count of
-# cursors), but it also causes the underlying display routines to not treat the
-# change between displays as relative to the number of seconds, that is, it's an
-# absolute value. The no_clear flag should be set in either case.
+# The no_clear and no_scale flags are normally always set together (values that
+# are maintained over time are normally not scaled per second).
 
 from operator import attrgetter
 import sys
