8cb4ecce11762679a07f632c1ff3d7a7732ef023
Michael Cahill <michael.cahill@mongodb.com>
2015-12-01 12:31:36 +1100

    SERVER-21691 Avoid blocking in-memory splits.
    Only trylock the page's reconciliation lock (so in-memory splits aren't blocked by slow splits of siblings), don't give up as soon as a checkpoint starts.

--- src/btree/bt_split.c
+++ src/btree/bt_split.c
@@ -1182,8 +1182,8 @@ err:	/*
  *	Lock an internal page.
  */
 static int
-__split_internal_lock(
-    WT_SESSION_IMPL *session, WT_REF *ref, WT_PAGE **parentp, bool *hazardp)
+__split_internal_lock(WT_SESSION_IMPL *session, WT_REF *ref, bool trylock,
+    WT_PAGE **parentp, bool *hazardp)
 {
 	WT_DECL_RET;
 	WT_PAGE *parent;
@@ -1201,7 +1201,7 @@ __split_internal_lock(
 	 * loop until the exclusive lock is resolved). If we want to split
 	 * the parent, give up to avoid that deadlock.
 	 */
-	if (S2BT(session)->checkpointing != WT_CKPT_OFF)
+	if (!trylock && S2BT(session)->checkpointing != WT_CKPT_OFF)
 		return (EBUSY);
 
 	/*
@@ -1226,7 +1226,10 @@ __split_internal_lock(
 		if (F_ISSET_ATOMIC(parent, WT_PAGE_SPLIT_BLOCK))
 			return (EBUSY);
 
-		WT_RET(__wt_fair_lock(session, &parent->page_lock));
+		if (trylock)
+			WT_RET(__wt_fair_trylock(session, &parent->page_lock));
+		else
+			WT_RET(__wt_fair_lock(session, &parent->page_lock));
 		if (parent == ref->home)
 			break;
 		WT_RET(__wt_fair_unlock(session, &parent->page_lock));
@@ -1370,7 +1373,7 @@ __split_parent_climb(WT_SESSION_IMPL *session, WT_PAGE *page, bool page_hazard)
 		 * locks, lock-coupling up the tree.
 		 */
 		WT_ERR(__split_internal_lock(
-		    session, ref, &parent, &parent_hazard));
+		    session, ref, true, &parent, &parent_hazard));
 		ret = __split_internal(session, parent, page);
 		WT_TRET(__split_internal_unlock(session, page, page_hazard));
 
@@ -1883,7 +1886,7 @@ __wt_split_insert(WT_SESSION_IMPL *session, WT_REF *ref)
 	WT_RET(__wt_verbose(
 	    session, WT_VERB_SPLIT, "%p: split-insert", ref->page));
 
-	WT_RET(__split_internal_lock(session, ref, &parent, &hazard));
+	WT_RET(__split_internal_lock(session, ref, true, &parent, &hazard));
 	if ((ret = __split_insert(session, ref)) != 0) {
 		WT_TRET(__split_internal_unlock(session, parent, hazard));
 		return (ret);
@@ -1975,7 +1978,7 @@ __wt_split_multi(WT_SESSION_IMPL *session, WT_REF *ref, int closing)
 	WT_RET(__wt_verbose(
 	    session, WT_VERB_SPLIT, "%p: split-multi", ref->page));
 
-	WT_RET(__split_internal_lock(session, ref, &parent, &hazard));
+	WT_RET(__split_internal_lock(session, ref, false, &parent, &hazard));
 	if ((ret = __split_multi(session, ref, closing)) != 0 || closing) {
 		WT_TRET(__split_internal_unlock(session, parent, hazard));
 		return (ret);
@@ -2004,7 +2007,7 @@ __wt_split_reverse(WT_SESSION_IMPL *session, WT_REF *ref)
 	WT_RET(__wt_verbose(
 	    session, WT_VERB_SPLIT, "%p: reverse-split", ref->page));
 
-	WT_RET(__split_internal_lock(session, ref, &parent, &hazard));
+	WT_RET(__split_internal_lock(session, ref, false, &parent, &hazard));
 	ret = __split_parent(session, ref, NULL, 0, 0, false, true);
 	WT_TRET(__split_internal_unlock(session, parent, hazard));
 	return (ret);
