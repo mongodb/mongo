0bc4f8f2dc7b51a86d6e8c120f48264af1e500af
Michael Cahill <michael.cahill@mongodb.com>
2015-12-02 15:06:19 +1100

    Don't try to test log compression from test/format on mongodb-3.0.
    Appararently the changes for test/format to configure log compression
    were not backported to the mongodb-3.0 branch.

--- test/format/smoke.sh
+++ test/format/smoke.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # Smoke-test format as part of running "make check".
-args="-1 -c "." data_source=table ops=100000 rows=10000 threads=4 compression=none logging_compression=none"
+args="-1 -c "." data_source=table ops=100000 rows=10000 threads=4 compression=none"
 
 ./t $args file_type=fix || exit 1
 ./t $args file_type=row || exit 1
