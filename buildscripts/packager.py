676a6ade885b0950a52538e64033611dcd640b4d
Ernie Hershey <ernie.hershey@10gen.com>
2015-12-07 07:36:43 -0500

    SERVER-21781 Improve packaging nightly logic

--- buildscripts/packager.py
+++ buildscripts/packager.py
@@ -54,8 +54,10 @@ class Spec(object):
         self.gitspec = gitspec
         self.rel = rel
 
+    # Nightly version numbers can be in the form: 3.0.7-pre-, or 3.0.7-5-g3b67ac
+    #
     def is_nightly(self):
-        return bool(re.search("-$", self.version()))
+        return bool(re.search("-$", self.version())) or bool(re.search("\d-\d+-g[0-9a-f]+$", self.version()))
 
     def is_rc(self):
         return bool(re.search("-rc\d+$", self.version()))
@@ -93,10 +95,10 @@ class Spec(object):
       else:
         corenum = 1
       # RC's
-      if re.search("-rc\d+$", self.version()):
+      if self.is_rc():
         return "0.%s.%s" % (corenum, re.sub('.*-','',self.version()))
       # Nightlies
-      elif re.search("-$", self.version()):
+      elif self.is_nightly():
         return "0.%s.%s" % (corenum, time.strftime("%Y%m%d"))
       else:
         return str(corenum)
