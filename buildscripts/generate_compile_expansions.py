4e39f9a466fa488f722a0307370abebf972c5f2f
Sam Kleinman <samk@10gen.com>
2015-12-08 10:29:43 -0500

    SERVER-21793: update evergreen configuration for new 3.2 branch

--- buildscripts/generate_compile_expansions.py
+++ buildscripts/generate_compile_expansions.py
@@ -50,8 +50,8 @@ if not version_parts:
     exit(1)
 
 if version_parts[0]:
-    print "suffix: latest"
-    print "src_suffix: latest"
+    print "suffix: v3.0-latest"
+    print "src_suffix: v3.0-latest"
 else:
     print "suffix: {0}".format(version_line)
     print "src_suffix: r{0}".format(version_line)
