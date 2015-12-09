2cc1025948dc378a3e1e69c4147e50a81457a781
Keith Bostic <keith@wiredtiger.com>
2015-12-09 11:13:38 -0500

    WT-2275: the created cursor isn't being closed on error.

--- src/cursor/cur_table.c
+++ src/cursor/cur_table.c
@@ -968,8 +968,11 @@ __wt_curtable_open(WT_SESSION_IMPL *session,
 	WT_ERR(__wt_strdup(session, tmp->data, &ctable->cfg[1]));
 
 	if (0) {
-err:		WT_TRET(__curtable_close(cursor));
-		*cursorp = NULL;
+err:		if (*cursorp != NULL) {
+			WT_TRET(__wt_cursor_close(*cursorp));
+			*cursorp = NULL;
+		}
+		WT_TRET(__curtable_close(cursor));
 	}
 
 	__wt_scr_free(session, &tmp);
