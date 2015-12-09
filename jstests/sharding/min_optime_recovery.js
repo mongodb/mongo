2a39a1d5b173e8281852018af43f236f0673b49e
Randolph Tan <randolph@10gen.com>
2015-12-09 15:19:23 -0500

    SERVER-20824 Test for sharding state recovery

--- jstests/sharding/min_optime_recovery.js
+++ jstests/sharding/min_optime_recovery.js
@@ -1,32 +1,83 @@
 /**
- * Tests that the minOpTimeRecovery document will be created after a migration only
- * if the config server is a replica set.
+ * Basic testing for minOpTimeRecovery document. Tests that it will be created after a migration
+ * only if the config server is a replica set and recovery will not run when disabled.
  */
 (function() {
 "use strict";
 
-var st = new ShardingTest({ shards: 2 });
+var runTest = function(withRecovery) {
+    var st = new ShardingTest({ shards: 2 });
 
-var testDB = st.s.getDB('test');
-testDB.adminCommand({ enableSharding: 'test' });
-st.ensurePrimaryShard('test', 'shard0000');
-testDB.adminCommand({ shardCollection: 'test.user', key: { x: 1 }});
-testDB.adminCommand({ moveChunk: 'test.user', find: { x: 0 }, to: 'shard0001' });
+    var testDB = st.s.getDB('test');
+    testDB.adminCommand({ enableSharding: 'test' });
+    st.ensurePrimaryShard('test', 'shard0000');
+    testDB.adminCommand({ shardCollection: 'test.user', key: { x: 1 }});
 
-var shardAdmin = st.d0.getDB('admin');
-var doc = shardAdmin.system.version.findOne();
+    var opTimeBeforeMigrate = null;
+    if (st.configRS) {
+        var priConn = st.configRS.getPrimary();
+        var replStatus = priConn.getDB('admin').runCommand({ replSetGetStatus: 1 });
+        replStatus.members.forEach(function(memberState) {
+            if (memberState.state == 1) { // if primary
+                opTimeBeforeMigrate = memberState.optime;
 
-if (st.configRS) {
-    assert.neq(null, doc);
-    assert.eq('minOpTimeRecovery', doc._id);
-    assert.eq(st.configRS.getURL(), doc.configsvrConnectionString);
-    assert.eq('shard0000', doc.shardName);
-    assert.gt(doc.minOpTime.ts.getTime(), 0);
-}
-else {
-    assert.eq(null, doc);
-}
+                assert.neq(null, opTimeBeforeMigrate);
+                assert.neq(null, opTimeBeforeMigrate.ts);
+                assert.neq(null, opTimeBeforeMigrate.t);
+            }
+        });
+    }
 
-st.stop();
+    testDB.adminCommand({ moveChunk: 'test.user', find: { x: 0 }, to: 'shard0001' });
+
+    var shardAdmin = st.d0.getDB('admin');
+    var doc = shardAdmin.system.version.findOne();
+
+    if (st.configRS) {
+        assert.neq(null, doc);
+        assert.eq('minOpTimeRecovery', doc._id);
+        assert.eq(st.configRS.getURL(), doc.configsvrConnectionString);
+        assert.eq('shard0000', doc.shardName);
+        assert.gt(doc.minOpTime.ts.getTime(), 0);
+    }
+    else {
+        assert.eq(null, doc);
+    }
+
+    var restartCmdLineOptions = Object.merge(st.d0.commandLine, {
+        setParameter: 'recoverShardingState=' + (withRecovery? 'true' : 'false'),
+        restart: true
+    });
+
+    // Restart the shard that donated a chunk to trigger the optime recovery logic.
+    st.stopMongod(0);
+    var newMongod = MongoRunner.runMongod(restartCmdLineOptions);
+    var shardingSection = newMongod.getDB('admin').runCommand({ serverStatus: 1 }).sharding;
+
+    if (st.configRS && withRecovery) {
+        assert.neq(null, shardingSection);
+
+        // Confirm that the config server string points to an actual config server replica set.
+        var configConnStr = shardingSection.configsvrConnectionString;
+        var configConn = new Mongo(configConnStr);
+        var configIsMaster = configConn.getDB('admin').runCommand({ isMaster: 1 });
+        assert.gt(configConnStr.indexOf('/'), 0);
+        assert.eq(1, configIsMaster.configsvr); // If it's a shard, this field won't exist.
+
+        var configOpTimeObj = shardingSection.lastSeenConfigServerOpTime;
+        assert.neq(null, configOpTimeObj);
+        assert.gte(configOpTimeObj.ts, opTimeBeforeMigrate.ts);
+        assert.gte(configOpTimeObj.t, opTimeBeforeMigrate.t);
+    }
+    else {
+        assert.eq(null, shardingSection);
+    }
+
+    MongoRunner.stopMongod(newMongod.port);
+    st.stop();
+};
+
+runTest(true);
+runTest(false);
 
 })();
