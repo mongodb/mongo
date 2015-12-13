1d431f95bb55365b9379f98e33803f68d114d701
Keith Bostic <keith@wiredtiger.com>
2015-12-13 13:08:18 -0500

    Fix moving the huffman code to a BSD 3-clause copyright, it's a MongoDB copyright, add a BSD 3-clause check to the dist script.

--- src/support/huffman.c
+++ src/support/huffman.c
@@ -11,11 +11,11 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 4. Neither the name of the University nor the names of its contributors
+ * 4. Neither the name MongoDB or the name WiredTiger
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * THIS SOFTWARE IS PROVIDED BY MONGODB INC. ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
  * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
