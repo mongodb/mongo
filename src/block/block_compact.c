mongodb/mongo

commit 769dc5976fe0d2448fb4fde511c5bc29eea39bfb

Change checkpoints to do first-fit allocation when we start writing the actual checkpoint blocks, that way if we delete significant space, the checkpoint blocks won't prevent file truncation.

src/block/block_compact.c
@@ -19,15 +19,10 @@ __wt_block_compact_start(WT_SESSION_IMPL *session, WT_BLOCK *block)
 {
 	WT_UNUSED(session);
 
-	/*
-	 * Save the current allocation plan, switch to first-fit allocation.
-	 * We don't need the lock, but it's not a performance question and
-	 * might avoid bugs in the future.
-	 */
-	__wt_spin_lock(session, &block->live_lock);
-	block->allocfirst_save = block->allocfirst;
-	block->allocfirst = 1;
-	__wt_spin_unlock(session, &block->live_lock);
+	/* Switch to first-fit allocation. */
+	__wt_block_configure_first_fit(block, 1);
+
+	block->compact_pct_tenths = 0;
 
 	return (0);
 }
@@ -41,14 +36,8 @@ __wt_block_compact_end(WT_SESSION_IMPL *session, WT_BLOCK *block)
 {
 	WT_UNUSED(session);
 
-	/*
-	 * Restore the previous allocation plan.
-	 * We don't need the lock, but it's not a performance question and
-	 * might avoid bugs in the future.
-	 */
-	__wt_spin_lock(session, &block->live_lock);
-	block->allocfirst = block->allocfirst_save;
-	__wt_spin_unlock(session, &block->live_lock);
+	/* Restore the original allocation plan. */
+	__wt_block_configure_first_fit(block, 0);
 
 	block->compact_pct_tenths = 0;
 

src/block/block_ckpt.c
@@ -224,6 +224,9 @@ __wt_block_checkpoint(WT_SESSION_IMPL *session,
 
 	ci = &block->live;
 
+	/* Switch to first-fit allocation. */
+	__wt_block_configure_first_fit(block, 1);
+
 	/*
 	 * Write the root page: it's possible for there to be a checkpoint of
 	 * an empty tree, in which case, we store an illegal root offset.
@@ -237,7 +240,7 @@ __wt_block_checkpoint(WT_SESSION_IMPL *session,
 		ci->root_offset = WT_BLOCK_INVALID_OFFSET;
 		ci->root_size = ci->root_cksum = 0;
 	} else
-		WT_RET(__wt_block_write_off(session, block, buf,
+		WT_ERR(__wt_block_write_off(session, block, buf,
 		    &ci->root_offset, &ci->root_size, &ci->root_cksum,
 		    data_cksum, 0));
 
@@ -245,13 +248,16 @@ __wt_block_checkpoint(WT_SESSION_IMPL *session,
 	 * Checkpoints are potentially reading/writing/merging lots of blocks,
 	 * pre-allocate structures for this thread's use.
 	 */
-	WT_RET(__wt_block_ext_prealloc(session, 250));
+	WT_ERR(__wt_block_ext_prealloc(session, 250));
 
 	/* Process the checkpoint list, deleting and updating as required. */
 	ret = __ckpt_process(session, block, ckptbase);
 
 	/* Discard any excessive memory we've allocated. */
 	WT_TRET(__wt_block_ext_discard(session, 250));
+
+	/* Restore the original allocation plan. */
+err:	__wt_block_configure_first_fit(block, 0);
 
 	return (ret);
 }

src/block/block_open.c
@@ -115,6 +115,25 @@ __block_destroy(WT_SESSION_IMPL *session, WT_BLOCK *block)
 }
 
 /*
+ * __wt_block_configure_first_fit --
+ *	Configure first-fit allocation.
+ */
+void
+__wt_block_configure_first_fit(WT_BLOCK *block, int on)
+{
+	/*
+	 * Switch to first-fit allocation so we rewrite blocks at the start of
+	 * the file; use atomic instructions because checkpoints also configure
+	 * first-fit allocation, and this way we stay on first-fit allocation
+	 * as long as any operation wants it.
+	 */
+	if (on)
+		(void)WT_ATOMIC_ADD4(block->allocfirst, 1);
+	else
+		(void)WT_ATOMIC_SUB4(block->allocfirst, 1);
+}
+
+/*
  * __wt_block_open --
  *	Open a block handle.
  */

src/include/block.h
@@ -218,7 +218,7 @@ struct __wt_block {
 	SLIST_ENTRY(__wt_block) hashl;	/* Hashed list of handles */
 
 	/* Configuration information, set when the file is opened. */
-	int	 allocfirst;		/* Allocation is first-fit */
+	uint32_t allocfirst;		/* Allocation is first-fit */
 	uint32_t allocsize;		/* Allocation size */
 	size_t	 os_cache;		/* System buffer cache flush max */
 	size_t	 os_cache_max;
@@ -239,7 +239,6 @@ struct __wt_block {
 	int		ckpt_inprogress;/* Live checkpoint in progress */
 
 				/* Compaction support */
-	int	allocfirst_save;	/* Saved: allocation is first-fit */
 	int	compact_pct_tenths;	/* Percent to compact */
 
 				/* Salvage support */

src/include/extern.h
@@ -45,6 +45,7 @@ extern int __wt_block_unmap( WT_SESSION_IMPL *session, WT_BLOCK *block, void *ma
 extern int __wt_block_manager_open(WT_SESSION_IMPL *session, const char *filename, const char *cfg[], int forced_salvage, int readonly, uint32_t allocsize, WT_BM **bmp);
 extern int __wt_block_manager_truncate( WT_SESSION_IMPL *session, const char *filename, uint32_t allocsize);
 extern int __wt_block_manager_create( WT_SESSION_IMPL *session, const char *filename, uint32_t allocsize);
+extern void __wt_block_configure_first_fit(WT_BLOCK *block, int on);
 extern int __wt_block_open(WT_SESSION_IMPL *session, const char *filename, const char *cfg[], int forced_salvage, int readonly, uint32_t allocsize, WT_BLOCK **blockp);
 extern int __wt_block_close(WT_SESSION_IMPL *session, WT_BLOCK *block);
 extern int __wt_desc_init(WT_SESSION_IMPL *session, WT_FH *fh, uint32_t allocsize);

commit ad33acecd0d537c8adcc8a941f0dfda13541a06c

SERVER-21833: Fix a bug introduced in 769dc59 (March, 2015), when we started clearing WT_BLOCK.compact_pct_tenths in __wt_block_compact_start. The problem is __wt_block_compact_skip is the first block compaction routine called, it it sets WT_BLOCK.compact_pct_tenths and then returns if any compaction is to be done. The __wt_block_compact_start function is called after that, when the compaction run is starting. This bug effectively prevents compaction from ever happening, the size checked in __wt_block_compact_page_skip to decide if a page should be rewritten or skipped is the file's total size, and every page is skipped.

src/block/block_compact.c
@@ -22,8 +22,6 @@ __wt_block_compact_start(WT_SESSION_IMPL *session, WT_BLOCK *block)
 	/* Switch to first-fit allocation. */
 	__wt_block_configure_first_fit(block, true);
 
-	block->compact_pct_tenths = 0;
-
 	return (0);
 }
 
@@ -38,8 +36,6 @@ __wt_block_compact_end(WT_SESSION_IMPL *session, WT_BLOCK *block)
 
 	/* Restore the original allocation plan. */
 	__wt_block_configure_first_fit(block, false);
-
-	block->compact_pct_tenths = 0;
 
 	return (0);
 }