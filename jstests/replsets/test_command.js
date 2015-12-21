01c81a8a1e1635d4161b62b8ee445e16a24c5b09
matt dannenberg <matt.dannenberg@10gen.com>
2015-12-21 08:42:09 -0500

    SERVER-21975 wait for secondary to become secondary before starting tests in replsets/test_command.js
    change waitForMemberState to handle a potential network hangup in test_commands.js
    
    Format JS code with approved style.
    
    (cherry picked from commit adbe3cd2afe3aeda46c4576027eaa3482216f851)
    (cherry picked from commit b8b3c3f5962aea2931df2b8807de35d2a9ff87aa)

--- jstests/replsets/test_command.js
+++ jstests/replsets/test_command.js
@@ -26,6 +26,24 @@
     }),
                          'node 0' + replSet.nodes[0].host + ' failed to become primary');
 
+    // We need the try/catch to handle that the node may have hung up the connection due
+    // to a state change.
+    try {
+        assert.commandWorked(replSet.nodes[1].adminCommand({
+            replSetTest: 1,
+            waitForMemberState: ReplSetTest.State.SECONDARY,
+            timeoutMillis: 60 * 1000,
+        }));
+    } catch (e) {
+        jsTestLog(e);
+        assert.commandWorked(replSet.nodes[1].adminCommand({
+            replSetTest: 1,
+            waitForMemberState: ReplSetTest.State.SECONDARY,
+            timeoutMillis: 60 * 1000,
+        }),
+                             'node 1' + replSet.nodes[1].host + ' failed to become secondary');
+    }
+
     var primary = replSet.getPrimary();
     var secondary = replSet.getSecondary();
 
