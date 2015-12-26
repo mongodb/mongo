9b5463a508c9c4db49ceb4f628e912bb54c72ddd
Keith Bostic <keith@wiredtiger.com>
2015-12-26 13:58:35 -0500

    The test_compact02.test_compact02.test_compact02(128KB.1mb.file) keeps failing every now and then, drop the maximum leaf-page size back to 64KB.

--- test/suite/test_compact02.py
+++ test/suite/test_compact02.py
@@ -49,11 +49,13 @@ class test_compact02(wttest.WiredTigerTestCase):
     # There's a balance between the pages we create and the size of the records
     # being stored: compaction doesn't work on tables with many overflow items
     # because we don't rewrite them. Experimentally, 8KB is as small as the test
-    # can go.
+    # can go. Additionally, we can't set the maximum page size too large because
+    # there won't be enough pages to rewrite. Experimentally, 64KB is as large
+    # as the test can go.
     fileConfig = [
         ('default', dict(fileConfig='')),
         ('8KB', dict(fileConfig='leaf_page_max=8kb')),
-        ('128KB', dict(fileConfig='leaf_page_max=128kb')),
+        ('64KB', dict(fileConfig='leaf_page_max=64KB')),
     ]
     scenarios = \
         number_scenarios(multiply_scenarios('.', types, cacheSize, fileConfig))
