9287affdb7e7c80061450fed87671311672b55d8
Andy Schwerin <schwerin@mongodb.com>
2015-12-30 12:09:42 -0500

    SERVER-22007 Disable last-error tracking for help commands.
    (cherry picked from commit 8ae053a4d9c5bd2672140964e7f09bd6b734dbdc)

--- src/mongo/db/commands/get_last_error.cpp
+++ src/mongo/db/commands/get_last_error.cpp
@@ -93,7 +93,6 @@ public:
                                        const BSONObj& cmdObj,
                                        std::vector<Privilege>* out) {}  // No auth required
     virtual void help(stringstream& help) const {
-        LastError::get(cc()).disable();  // SERVER-11492
         help << "return error status of the last operation on this connection\n"
              << "options:\n"
              << "  { fsync:true } - fsync before returning, or wait for journal commit if running "
