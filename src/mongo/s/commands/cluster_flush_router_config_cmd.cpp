4d59d95e81a634fbe719ac67f109157edc8a9666
Misha Tyulenev <misha@mongodb.com>
2016-01-06 10:27:36 -0500

    SERVER-21914: Make flushRouterConfig call ShardRegistry::reload()

--- src/mongo/s/commands/cluster_flush_router_config_cmd.cpp
+++ src/mongo/s/commands/cluster_flush_router_config_cmd.cpp
@@ -30,6 +30,7 @@
 
 #include "mongo/db/commands.h"
 #include "mongo/s/catalog/catalog_cache.h"
+#include "mongo/s/client/shard_registry.h"
 #include "mongo/s/config.h"
 #include "mongo/s/grid.h"
 
@@ -70,6 +71,7 @@ public:
                      int options,
                      std::string& errmsg,
                      BSONObjBuilder& result) {
+        grid.shardRegistry()->reload(txn);
         grid.catalogCache()->invalidateAll();
 
         result.appendBool("flushed", true);
