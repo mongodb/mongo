30f331f0e6930489c6ad0ae30d6670c0e57e0baa
Spencer T Brody <spencer@mongodb.com>
2016-01-06 15:21:55 -0500

    SERVER-20580 Re-enable csrs_upgrade_during_migrate.js test
    (cherry picked from commit 43209263a40af233dd1fae25aa719c7b2b304480)

--- jstests/sharding/csrs_upgrade_during_migrate.js
+++ jstests/sharding/csrs_upgrade_during_migrate.js
@@ -120,10 +120,7 @@ var st;
     jsTest.log("Adding non-voting members to csrs set: " + tojson(csrsConfig));
     assert.commandWorked(csrs[0].adminCommand({replSetReconfig: csrsConfig}));
 
-    // This write is an easy way to wait for all members of the CSRS set to have
-    // replicated all of the documents.
-    assert.writeOK(csrs[0].getCollection('config.tmp').insert({},
-                                                              { writeConcern: { w:csrs.length }}));
+    waitUntilAllNodesCaughtUp(csrs);
 
     jsTest.log("Starting long-running chunk migration");
     // Turn on fail point to confirm that moveChunk aborts before getting to the critical section.
@@ -145,8 +142,9 @@ var st;
                     return st.s0.getDB('config').changelog.findOne({what: 'moveChunk.start'});
                 });
 
-    jsTest.log("Shutting down second and third SCCC config server nodes");
-    MongoRunner.stopMongod(st.c1);
+    // Only shut down one of the SCCC config servers to avoid any period without any config servers
+    // online.
+    jsTest.log("Shutting down third SCCC config server node");
     MongoRunner.stopMongod(st.c2);
 
     csrsConfig.members.forEach(function (member) { member.votes = 1; member.priority = 1});
