097d7922feba1be3ccbcd421b3abf114ac4463ae
Keith Bostic <keith@wiredtiger.com>
2016-01-14 14:50:17 -0500

    WT-60: big endian support
    Add a check, based on WORDS_BIGENDIAN, that the build endian-ness
    matches the run environment.

--- src/support/global.c
+++ src/support/global.c
@@ -12,6 +12,35 @@ WT_PROCESS __wt_process;			/* Per-process structure */
 static int __wt_pthread_once_failed;		/* If initialization failed */
 
 /*
+ * __wt_endian_check --
+ *	Check the build matches the machine.
+ */
+static int
+__wt_endian_check(void)
+{
+	uint64_t v;
+	bool big;
+	const char *e;
+
+	v = 1;
+	big = *((uint8_t *)&v) == 0;
+
+#ifdef WORDS_BIGENDIAN
+	if (big)
+		return (0);
+	e = "big-endian";
+#else
+	if (!big)
+		return (0);
+	e = "little-endian";
+#endif
+	fprintf(stderr,
+	    "This is a %s build of the WiredTiger data engine, incompatible "
+	    "with this system\n", e);
+	return (EINVAL);
+}
+
+/*
  * __wt_global_once --
  *	Global initialization, run once.
  */
@@ -49,6 +78,9 @@ __wt_library_init(void)
 {
 	static bool first = true;
 	WT_DECL_RET;
+
+	/* Check the build matches the machine. */
+	WT_RET(__wt_endian_check());
 
 	/*
 	 * Do per-process initialization once, before anything else, but only
