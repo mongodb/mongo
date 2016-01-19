a7209dc5ebc402bfa4bc1dfb85f6be8056549943
Jonathan Reams <jbreams@mongodb.com>
2016-01-19 10:50:06 -0500

    SERVER-22176 auto_rebalance.js should ignore fasserts in sharding_legacy_op_query_WT

--- jstests/sharding/auto_rebalance.js
+++ jstests/sharding/auto_rebalance.js
@@ -50,6 +50,8 @@ assert.soon(function() {
 st.rs0.awaitReplication(120000);
 st.rs1.awaitReplication(120000);
 
-st.stop();
+// TODO: mongod only exits with MongoRunner.EXIT_ABRUPT in sharding_legacy_op_query_WT
+// this should be fixed by SERVER-22176
+st.stop({ allowedExitCodes: [ MongoRunner.EXIT_ABRUPT ] });
 
 })();
