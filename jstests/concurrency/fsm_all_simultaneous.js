50d09c53bed8185dbe268d1e6d6fa1e68e9161c5
Max Hirschhorn <max.hirschhorn@mongodb.com>
2016-01-26 17:27:33 -0500

    SERVER-21499 Enable fsm_all_simultaneous.js FSM runner.
    fsm_all_simultaneous.js runs different FSM workloads simultaneously. By
    default, these workloads still run with their own collection and
    database.
    
    Defined concurrency_simultaneous and concurrency_simultaneous_WT tasks
    to run this mode of the concurrency framework in Evergreen.

--- jstests/concurrency/fsm_all_simultaneous.js
+++ jstests/concurrency/fsm_all_simultaneous.js
@@ -5,9 +5,17 @@ load('jstests/concurrency/fsm_libs/runner.js');
 var dir = 'jstests/concurrency/fsm_workloads';
 
 var blacklist = [
+    // Disabled due to MongoDB restrictions and/or workload restrictions
+
+    // These workloads implicitly assume that their tid ranges are [0, $config.threadCount). This
+    // isn't guaranteed to be true when they are run in parallel with other workloads.
+    'list_indexes.js',
+    'update_inc_capped.js',
+
+    'agg_group_external.js', // uses >100MB of data, which can overwhelm test hosts
+    'agg_sort_external.js', // uses >100MB of data, which can overwhelm test hosts
 ].map(function(file) { return dir + '/' + file; });
 
-// SERVER-16196 re-enable executing workloads
-// runWorkloadsInParallel(ls(dir).filter(function(file) {
-//     return !Array.contains(blacklist, file);
-// }));
+runWorkloadsInParallel(ls(dir).filter(function(file) {
+    return !Array.contains(blacklist, file);
+}));
