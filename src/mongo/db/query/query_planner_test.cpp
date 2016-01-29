ecfac2be1f855c19ef6b50eef712fd3b6b9631f7
Tess Avitabile <tess.avitabile@mongodb.com>
2016-01-29 13:55:48 -0500

    SERVER-18115 Avoid unnecessary in-memory sort stage for .min()/.max() queries

--- src/mongo/db/query/query_planner_test.cpp
+++ src/mongo/db/query/query_planner_test.cpp
@@ -1089,6 +1089,45 @@ TEST_F(QueryPlannerTest, MaxMinSort) {
     assertSolutionExists("{fetch: {node: {ixscan: {filter: null, pattern: {a: 1}}}}}");
 }
 
+TEST_F(QueryPlannerTest, MaxMinSortEqualityFirstSortSecond) {
+    addIndex(BSON("a" << 1 << "b" << 1));
+
+    // Run an empty query, sort {b: 1}, max/min arguments.
+    runQueryFull(BSONObj(),
+                 fromjson("{b: 1}"),
+                 BSONObj(),
+                 0,
+                 0,
+                 BSONObj(),
+                 fromjson("{a: 1, b: 1}"),
+                 fromjson("{a: 1, b: 2}"),
+                 false);
+
+    assertNumSolutions(1);
+    assertSolutionExists("{fetch: {node: {ixscan: {filter: null, pattern: {a: 1, b: 1}}}}}");
+}
+
+TEST_F(QueryPlannerTest, MaxMinSortInequalityFirstSortSecond) {
+    addIndex(BSON("a" << 1 << "b" << 1));
+
+    // Run an empty query, sort {b: 1}, max/min arguments.
+    runQueryFull(BSONObj(),
+                 fromjson("{b: 1}"),
+                 BSONObj(),
+                 0,
+                 0,
+                 BSONObj(),
+                 fromjson("{a: 1, b: 1}"),
+                 fromjson("{a: 2, b: 2}"),
+                 false);
+
+    assertNumSolutions(1);
+    assertSolutionExists(
+        "{sort: {pattern: {b: 1}, limit: 0, node: {sortKeyGen: {node: "
+        "{fetch: {node: "
+        "{ixscan: {filter: null, pattern: {a: 1, b: 1}}}}}}}}}");
+}
+
 TEST_F(QueryPlannerTest, MaxMinReverseSort) {
     addIndex(BSON("a" << 1));
 
