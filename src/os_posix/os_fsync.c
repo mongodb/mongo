64fd83193f254aaea3149b34d796b57a80c31fa7
Susan LoVerso <sue@mongodb.com>
2016-02-08 17:05:13 -0500

    WT-2349  Fix misplaced parens on assert.

--- src/os_posix/os_fsync.c
+++ src/os_posix/os_fsync.c
@@ -136,9 +136,9 @@ __wt_fsync(WT_SESSION_IMPL *session, WT_FH *fh)
 
 	WT_RET(__wt_verbose(session, WT_VERB_FILEOPS, "%s: fsync", fh->name));
 
-	WT_ASSERT(session, !F_ISSET(S2C(session), WT_CONN_READONLY ||
+	WT_ASSERT(session, !F_ISSET(S2C(session), WT_CONN_READONLY) ||
 	    WT_STRING_MATCH(fh->name, WT_SINGLETHREAD,
-	    strlen(WT_SINGLETHREAD))));
+	    strlen(WT_SINGLETHREAD)));
 	if ((ret = __wt_handle_sync(fh->fd)) == 0)
 		return (0);
 	WT_RET_MSG(session, ret, "%s fsync error", fh->name);
