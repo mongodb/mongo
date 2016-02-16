62ebc18691ef982d15506d8e50204c5a32897e5f
Adam Midvidy <amidvidy@gmail.com>
2016-02-16 13:53:45 -0500

    SERVER-22354 add more coverage of basic functionality to readOnly suite

--- jstests/readonly/lib/read_only_test.js
+++ jstests/readonly/lib/read_only_test.js
@@ -2,9 +2,11 @@
 
 function runReadOnlyTest(test) {
 
-    assert.eq(typeof(test.exec), "function");
-    assert.eq(typeof(test.load), "function");
-    assert.eq(typeof(test.name), "string");
+    printjson(test);
+
+    assert.eq(typeof(test.exec), 'function');
+    assert.eq(typeof(test.load), 'function');
+    assert.eq(typeof(test.name), 'string');
 
     // TODO: read storageEngine from testData when read-only mode is supported in WiredTiger.
     var options = {
@@ -15,8 +17,8 @@ function runReadOnlyTest(test) {
     var writableMongod = MongoRunner.runMongod(options);
     var dbpath = writableMongod.dbpath;
 
-    jsTest.log("starting load phase for test: " + test.name);
-    test.load(writableMongod.getDB("test")[test.name]);
+    jsTest.log('starting load phase for test: ' + test.name);
+    test.load(writableMongod.getDB('test')[test.name]);
 
     MongoRunner.stopMongod(writableMongod);
 
@@ -28,8 +30,32 @@ function runReadOnlyTest(test) {
 
     var readOnlyMongod = MongoRunner.runMongod(readOnlyOptions);
 
-    jsTest.log("starting execution phase for test: " + test.name);
-    test.exec(readOnlyMongod.getDB("test")[test.name]);
+    jsTest.log('starting execution phase for test: ' + test.name);
+    test.exec(readOnlyMongod.getDB('test')[test.name]);
 
     MongoRunner.stopMongod(readOnlyMongod);
 }
+
+function* cycleN(arr, N) {
+    for (var i = 0; i < N; ++i) {
+        yield arr[i % arr.length];
+    }
+};
+
+function* zip2(iter1, iter2)  {
+    var n1 = iter1.next();
+    var n2 = iter2.next();
+    while (!n1.done || !n2.done) {
+        var res = [];
+        if (!n1.done) {
+            res.push(n1.value);
+            n1 = iter1.next();
+        }
+        if (!n2.done) {
+            res.push(n2.value);
+            n2 = iter2.next();
+        }
+
+        yield res;
+    }
+};
---
+++ jstests/readonly/server_status.js
@@ -0,0 +1,15 @@
+load('jstests/readonly/lib/read_only_test.js');
+
+runReadOnlyTest(function() {
+    'use strict';
+
+    return {
+        name: 'server_status',
+
+        load: function(writableCollection) {
+        },
+        exec: function(readableCollection) {
+            assert.commandWorked(readableCollection.getDB().serverStatus());
+        }
+    };
+}());
