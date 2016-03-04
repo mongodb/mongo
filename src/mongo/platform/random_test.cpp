ec270ef20b801d249b17d2ea9a115350ddd64de9
Mathias Stearn <redbeard0531@gmail.com>
2016-03-04 13:29:11 -0500

    SERVER-20121 Use unsigned arithmetic in PsuedoRandom
    (cherry picked from commit 1c5fdf8)
    Fully manual cherry pick.

--- src/mongo/platform/random_test.cpp
+++ src/mongo/platform/random_test.cpp
@@ -17,6 +17,7 @@
  */
 
 #include <set>
+#include <vector>
 
 #include "mongo/platform/random.h"
 
@@ -84,6 +85,72 @@ namespace mongo {
             s.insert( a.nextInt64() );
         }
         ASSERT_EQUALS( 100U, s.size() );
+    }
+
+    TEST(RandomTest, NextInt32SanityCheck) {
+        // Generate 1000 int32s and assert that each bit is set between 40% and 60% of the time.
+        // This is a bare minimum sanity check, not an attempt to ensure quality random numbers.
+
+        PseudoRandom a(11);
+        std::vector<int32_t> nums;
+        for (int i = 0; i < 1000; i++) {
+            nums.push_back(a.nextInt32());
+        }
+
+        for (int bit = 0; bit < 32; bit++) {
+            int onesCount = 0;
+            for (size_t i=0; i < nums.size(); i++) {
+                bool isSet = (nums[i] >> bit) & 1;
+                if (isSet)
+                    onesCount++;
+            }
+
+            if (onesCount < 400 || onesCount > 600)
+                FAIL(str::stream() << "bit " << bit << " was set " << (onesCount / 10.)
+                                   << "% of the time.");
+        }
+    }
+
+    TEST(RandomTest, NextInt64SanityCheck) {
+        // Generate 1000 int64s and assert that each bit is set between 40% and 60% of the time.
+        // This is a bare minimum sanity check, not an attempt to ensure quality random numbers.
+
+        PseudoRandom a(11);
+        std::vector<int64_t> nums;
+        for (int i = 0; i < 1000; i++) {
+            nums.push_back(a.nextInt64());
+        }
+
+        for (int bit = 0; bit < 64; bit++) {
+            int onesCount = 0;
+            for (size_t i=0; i < nums.size(); i++) {
+                bool isSet = (nums[i] >> bit) & 1;
+                if (isSet)
+                    onesCount++;
+            }
+
+            if (onesCount < 400 || onesCount > 600)
+                FAIL(str::stream() << "bit " << bit << " was set " << (onesCount / 10.)
+                                   << "% of the time.");
+        }
+    }
+
+    TEST(RandomTest, NextInt32InRange) {
+        PseudoRandom a(11);
+        for (int i = 0; i < 1000; i++) {
+            int32_t res = a.nextInt32(10);
+            ASSERT_GREATER_THAN_OR_EQUALS(res, 0);
+            ASSERT_LESS_THAN(res, 10);
+        }
+    }
+
+    TEST(RandomTest, NextInt64InRange) {
+        PseudoRandom a(11);
+        for (int i = 0; i < 1000; i++) {
+            int64_t res = a.nextInt64(10);
+            ASSERT_GREATER_THAN_OR_EQUALS(res, 0);
+            ASSERT_LESS_THAN(res, 10);
+        }
     }
 
 
