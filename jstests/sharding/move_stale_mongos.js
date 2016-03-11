77fb3dd6e3a640e559bf44a67bf3e694884cf279
Judah Schvimer <judah@mongodb.com>
2016-03-11 10:29:02 -0500

    SERVER-23037 added _waitFoDelete: true to move_stale_mongos.js

--- jstests/sharding/move_stale_mongos.js
+++ jstests/sharding/move_stale_mongos.js
@@ -17,7 +17,7 @@ for (var i = 0; i < 100; i += 10) {
     assert.commandWorked(st.s0.getDB('admin').runCommand({split: testNs, middle: {_id: i}}));
     var nextShardIndex = (curShardIndex + 1) % shards.length;
     assert.commandWorked(st.s1.getDB('admin').runCommand(
-        {moveChunk: testNs, find: {_id: i + 5}, to: shards[nextShardIndex]}));
+        {moveChunk: testNs, find: {_id: i + 5}, to: shards[nextShardIndex], _waitForDelete: true}));
     curShardIndex = nextShardIndex;
 }
 
