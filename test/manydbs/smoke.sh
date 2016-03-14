541480893952e25030820d518a4c9be921cb5cd3
Susan LoVerso <sue@mongodb.com>
2016-03-14 11:11:14 -0400

    WT-2463 Reinstate smoke.sh tests.

--- test/manydbs/smoke.sh
+++ test/manydbs/smoke.sh
@@ -14,9 +14,9 @@ set -e
 # 
 echo "manydbs: default with operations turned on"
 $TEST_WRAPPER ./t
-#echo "manydbs: totally idle databases"
-#$TEST_WRAPPER ./t -I
-#echo "manydbs: 40 databases with operations"
-#$TEST_WRAPPER ./t -D 40
-#echo "manydbs: 40 idle databases"
-#$TEST_WRAPPER ./t -I -D 40
+echo "manydbs: totally idle databases"
+$TEST_WRAPPER ./t -I
+echo "manydbs: 40 databases with operations"
+$TEST_WRAPPER ./t -D 40
+echo "manydbs: 40 idle databases"
+$TEST_WRAPPER ./t -I -D 40
