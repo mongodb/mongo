5d1262cc394d685b59ae3185d7315227085e897d
Geert Bosch <geert@mongodb.com>
2016-03-16 09:47:40 -0400

    SERVER-22599 uassert on cache full error

--- src/mongo/db/storage/wiredtiger/wiredtiger_util.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_util.cpp
@@ -70,6 +70,8 @@ Status wtRCToStatus_slow(int retCode, const char* prefix) {
         return Status(ErrorCodes::BadValue, s);
     }
 
+    uassert(ErrorCodes::ExceededMemoryLimit, s, retCode != WT_CACHE_FULL);
+
     // TODO convert specific codes rather than just using UNKNOWN_ERROR for everything.
     return Status(ErrorCodes::UnknownError, s);
 }
