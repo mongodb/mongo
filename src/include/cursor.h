689f3f95534c9f1f67478f8c702303274d988594
Don Anderson <dda@ddanderson.com>
2016-03-16 15:08:00 -0400

    WT-2414 When iterating cursor joins, take advantage of the fact that we have the index key for the first index available.  We don't need to extract it again.

--- src/include/cursor.h
+++ src/include/cursor.h
@@ -289,7 +289,8 @@ struct __wt_cursor_join_iter {
 	WT_CURSOR_JOIN_ENTRY	*entry;
 	WT_CURSOR		*cursor;	/* has null projection */
 	WT_CURSOR		*main;		/* main table with projection */
-	WT_ITEM			*curkey;
+	WT_ITEM			*curkey;	/* primary key */
+	WT_ITEM			 idxkey;
 	bool			 positioned;
 	bool			 isequal;	/* advancing means we're done */
 };
