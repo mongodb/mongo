2feb9792dca6815dcc2f4317755216994b76b936
Susan LoVerso <sue@mongodb.com>
2016-03-16 15:43:44 -0400

    WT-2461 Look explicitly for the final number of expected files.

--- test/suite/test_sweep01.py
+++ test/suite/test_sweep01.py
@@ -106,24 +106,21 @@ class test_sweep01(wttest.WiredTigerTestCase, suite_subprocess):
         k = 0
         sleep = 0
         max = 60
-        last_nfile = nfile1
+        final_nfile = 4
         while sleep < max:
             self.session.checkpoint()
             k = k+1
             c[k] = 1
             sleep += 2
             time.sleep(2)
-            # Give slow machines time to process files.  As long as progress
-            # is made, keep going.
+            # Give slow machines time to process files.
             stat_cursor = self.session.open_cursor('statistics:', None, None)
             this_nfile = stat_cursor[stat.conn.file_open][2]
             stat_cursor.close()
             self.pr("==== loop " + str(sleep))
-            self.pr("last_nfile " + str(last_nfile))
             self.pr("this_nfile " + str(this_nfile))
-            if this_nfile == last_nfile and this_nfile < nfile1:
+            if this_nfile == final_nfile:
                 break
-            last_nfile = this_nfile
         c.close()
         self.pr("Sweep loop took " + str(sleep))
 
@@ -191,7 +188,7 @@ class test_sweep01(wttest.WiredTigerTestCase, suite_subprocess):
         self.assertEqual(nfile2 < nfile1, True)
         # The only files that should be left are the metadata, the lookaside
         # file, the lock file, and the active file.
-        if (nfile2 != 4):
+        if (nfile2 != final_nfile):
             print "close1: " + str(close1) + " close2: " + str(close2)
             print "remove1: " + str(remove1) + " remove2: " + str(remove2)
             print "sweep1: " + str(sweep1) + " sweep2: " + str(sweep2)
@@ -200,7 +197,7 @@ class test_sweep01(wttest.WiredTigerTestCase, suite_subprocess):
             print "tod1: " + str(tod1) + " tod2: " + str(tod2)
             print "ref1: " + str(ref1) + " ref2: " + str(ref2)
             print "XX2: nfile1: " + str(nfile1) + " nfile2: " + str(nfile2)
-        self.assertEqual(nfile2 == 4, True)
+        self.assertEqual(nfile2 == final_nfile, True)
 
 if __name__ == '__main__':
     wttest.run()
