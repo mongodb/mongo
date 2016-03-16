c2af7abae8d09d290d7457ab77f5a7529806b75a
Randolph Tan <randolph@10gen.com>
2016-03-16 17:30:40 -0400

    SERVER-22661 Write a type for ShardIdentity

---
+++ src/mongo/db/s/type_shard_identity.cpp
@@ -0,0 +1,174 @@
+/**
+ *    Copyright (C) 2016 MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects
+ *    for all of the code used other than as permitted herein. If you modify
+ *    file(s) with this exception, you may extend this exception to your
+ *    version of the file(s), but you are not obligated to do so. If you do not
+ *    wish to do so, delete this exception statement from your version. If you
+ *    delete this exception statement from all source files in the program,
+ *    then also delete it in the license file.
+ */
+
+#include "mongo/db/s/type_shard_identity.h"
+
+#include "mongo/bson/util/bson_extract.h"
+
+namespace mongo {
+
+const std::string ShardIdentityType::ConfigNS = "admin.system.version";
+const std::string id("shardIdentity");
+const BSONField<std::string> configsvrConnString("configsvrConnectionString");
+const BSONField<std::string> shardName("shardName");
+const BSONField<OID> clusterId("clusterId");
+
+StatusWith<ShardIdentityType> ShardIdentityType::fromBSON(const BSONObj& source) {
+    if (!source.hasField("_id")) {
+        return {ErrorCodes::NoSuchKey, str::stream() << "missing _id field"};
+    }
+
+    ShardIdentityType shardIdentity;
+
+    {
+        std::string docId;
+        Status status = bsonExtractStringField(source, "_id", &docId);
+        if (!status.isOK()) {
+            return status;
+        }
+
+        if (docId != id) {
+            return {ErrorCodes::FailedToParse,
+                    str::stream() << "got _id: " << docId << " instead of " << id};
+        }
+    }
+
+    {
+        std::string connString;
+        Status status = bsonExtractStringField(source, configsvrConnString(), &connString);
+        if (!status.isOK()) {
+            return status;
+        }
+
+        shardIdentity.setConfigsvrConnString(connString);
+    }
+
+    {
+        std::string name;
+        Status status = bsonExtractStringField(source, shardName(), &name);
+        if (!status.isOK()) {
+            return status;
+        }
+
+        shardIdentity.setShardName(name);
+    }
+
+    {
+        OID oid;
+        Status status = bsonExtractOIDField(source, clusterId(), &oid);
+        if (!status.isOK()) {
+            return status;
+        }
+
+        shardIdentity.setClusterId(oid);
+    }
+
+    return shardIdentity;
+}
+
+Status ShardIdentityType::validate() const {
+    if (!_configsvrConnString || _configsvrConnString->empty()) {
+        return {ErrorCodes::NoSuchKey,
+                str::stream() << "missing " << configsvrConnString() << " field"};
+    }
+
+    if (!_shardName || _shardName->empty()) {
+        return {ErrorCodes::NoSuchKey, str::stream() << "missing " << shardName() << " field"};
+    }
+
+    if (!_clusterId || _clusterId->isSet()) {
+        return {ErrorCodes::NoSuchKey, str::stream() << "missing " << clusterId() << " field"};
+    }
+
+    return Status::OK();
+}
+
+BSONObj ShardIdentityType::toBSON() const {
+    BSONObjBuilder builder;
+
+    builder.append("_id", id);
+
+    if (_configsvrConnString) {
+        builder << configsvrConnString(_configsvrConnString.get());
+    }
+
+    if (_shardName) {
+        builder << shardName(_shardName.get());
+    }
+
+    if (_clusterId) {
+        builder << clusterId(_clusterId.get());
+    }
+
+    return builder.obj();
+}
+
+std::string ShardIdentityType::toString() const {
+    return toBSON().toString();
+}
+
+bool ShardIdentityType::isConfigsvrConnStringSet() const {
+    return _configsvrConnString.is_initialized();
+}
+
+const std::string& ShardIdentityType::getConfigsvrConnString() const {
+    invariant(_configsvrConnString);
+    return _configsvrConnString.get();
+}
+
+void ShardIdentityType::setConfigsvrConnString(std::string connString) {
+    _configsvrConnString = std::move(connString);
+}
+
+bool ShardIdentityType::isShardNameSet() const {
+    return _shardName.is_initialized();
+}
+
+const std::string& ShardIdentityType::getShardName() const {
+    invariant(_shardName);
+    return _shardName.get();
+}
+
+void ShardIdentityType::setShardName(std::string shardName) {
+    _shardName = std::move(shardName);
+}
+
+bool ShardIdentityType::isClusterIdSet() const {
+    return _clusterId.is_initialized();
+}
+
+const OID& ShardIdentityType::getClusterId() const {
+    invariant(_clusterId);
+    return _clusterId.get();
+}
+
+void ShardIdentityType::setClusterId(OID clusterId) {
+    _clusterId = std::move(clusterId);
+}
+
+}  // namespace mongo
