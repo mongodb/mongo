25dcef8ac90ed23a552cae817026502850ba7bbf
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2016-03-18 10:40:23 -0400

    SERVER-23223 Sharded planCache[List/Clear/Set]Filter commands should check collection name

--- src/mongo/s/commands/cluster_index_filter_cmd.cpp
+++ src/mongo/s/commands/cluster_index_filter_cmd.cpp
@@ -26,14 +26,12 @@
  *    it in the license file.
  */
 
-#include "mongo/base/init.h"
+#include "mongo/platform/basic.h"
+
 #include "mongo/base/error_codes.h"
 #include "mongo/db/auth/authorization_session.h"
-#include "mongo/db/client_basic.h"
+#include "mongo/db/client.h"
 #include "mongo/db/commands.h"
-#include "mongo/s/config.h"
-#include "mongo/s/grid.h"
-#include "mongo/s/stale_exception.h"
 #include "mongo/s/strategy.h"
 
 namespace mongo {
@@ -41,6 +39,8 @@ namespace mongo {
 using std::string;
 using std::stringstream;
 using std::vector;
+
+namespace {
 
 /**
  * Base class for mongos index filter commands.
@@ -51,6 +51,13 @@ class ClusterIndexFilterCmd : public Command {
     MONGO_DISALLOW_COPYING(ClusterIndexFilterCmd);
 
 public:
+    /**
+     * Instantiates a command that can be invoked by "name", which will be described by
+     * "helpText", and will require privilege "actionType" to run.
+     */
+    ClusterIndexFilterCmd(const std::string& name, const std::string& helpText)
+        : Command(name), _helpText(helpText) {}
+
     virtual ~ClusterIndexFilterCmd() {}
 
     bool slaveOk() const {
@@ -86,84 +93,57 @@ public:
              BSONObj& cmdObj,
              int options,
              std::string& errmsg,
-             BSONObjBuilder& result);
+             BSONObjBuilder& result) {
+        const NamespaceString nss(parseNs(dbname, cmdObj));
+        uassert(ErrorCodes::InvalidNamespace,
+                str::stream() << nss.ns() << " is not a valid namespace",
+                nss.isValid());
 
-public:
-    /**
-     * Instantiates a command that can be invoked by "name", which will be described by
-     * "helpText", and will require privilege "actionType" to run.
-     */
-    ClusterIndexFilterCmd(const std::string& name, const std::string& helpText)
-        : Command(name), _helpText(helpText) {}
+        // Dispatch command to all the shards.
+        // Targeted shard commands are generally data-dependent but index filter
+        // commands are tied to query shape (data has no effect on query shape).
+        vector<Strategy::CommandResult> results;
+        Strategy::commandOp(txn, dbname, cmdObj, options, nss.ns(), BSONObj(), &results);
 
-private:
-    std::string _helpText;
-};
+        // Set value of first shard result's "ok" field.
+        bool clusterCmdResult = true;
 
-//
-// Cluster index filter command implementation(s) below
-//
+        for (vector<Strategy::CommandResult>::const_iterator i = results.begin();
+             i != results.end();
+             ++i) {
+            const Strategy::CommandResult& cmdResult = *i;
 
-bool ClusterIndexFilterCmd::run(OperationContext* txn,
-                                const std::string& dbName,
-                                BSONObj& cmdObj,
-                                int options,
-                                std::string& errMsg,
-                                BSONObjBuilder& result) {
-    const std::string fullns = parseNs(dbName, cmdObj);
-    NamespaceString nss(fullns);
+            // XXX: In absence of sensible aggregation strategy,
+            //      promote first shard's result to top level.
+            if (i == results.begin()) {
+                result.appendElements(cmdResult.result);
+                clusterCmdResult = cmdResult.result["ok"].trueValue();
+            }
 
-    // Dispatch command to all the shards.
-    // Targeted shard commands are generally data-dependent but index filter
-    // commands are tied to query shape (data has no effect on query shape).
-    vector<Strategy::CommandResult> results;
-    Strategy::commandOp(txn, dbName, cmdObj, options, nss.ns(), BSONObj(), &results);
-
-    // Set value of first shard result's "ok" field.
-    bool clusterCmdResult = true;
-
-    for (vector<Strategy::CommandResult>::const_iterator i = results.begin(); i != results.end();
-         ++i) {
-        const Strategy::CommandResult& cmdResult = *i;
-
-        // XXX: In absence of sensible aggregation strategy,
-        //      promote first shard's result to top level.
-        if (i == results.begin()) {
-            result.appendElements(cmdResult.result);
-            clusterCmdResult = cmdResult.result["ok"].trueValue();
+            // Append shard result as a sub object.
+            // Name the field after the shard.
+            result.append(cmdResult.shardTargetId, cmdResult.result);
         }
 
-        // Append shard result as a sub object.
-        // Name the field after the shard.
-        result.append(cmdResult.shardTargetId, cmdResult.result);
+        return clusterCmdResult;
     }
 
-    return clusterCmdResult;
-}
+private:
+    const std::string _helpText;
+};
 
-//
 // Register index filter commands at startup
-//
+ClusterIndexFilterCmd clusterPlanCacheListFiltersCmd(
+    "planCacheListFilters", "Displays index filters for all query shapes in a collection.");
 
-namespace {
+ClusterIndexFilterCmd clusterPlanCacheClearFiltersCmd(
+    "planCacheClearFilters",
+    "Clears index filter for a single query shape or, "
+    "if the query shape is omitted, all filters for the collection.");
 
-MONGO_INITIALIZER(RegisterIndexFilterCommands)(InitializerContext* context) {
-    // Leaked intentionally: a Command registers itself when constructed.
+ClusterIndexFilterCmd clusterPlanCacheSetFilterCmd(
+    "planCacheSetFilter", "Sets index filter for a query shape. Overrides existing index filter.");
 
-    new ClusterIndexFilterCmd("planCacheListFilters",
-                              "Displays index filters for all query shapes in a collection.");
-
-    new ClusterIndexFilterCmd("planCacheClearFilters",
-                              "Clears index filter for a single query shape or, "
-                              "if the query shape is omitted, all filters for the collection.");
-
-    new ClusterIndexFilterCmd(
-        "planCacheSetFilter",
-        "Sets index filter for a query shape. Overrides existing index filter.");
-
-    return Status::OK();
-}
 
 }  // namespace
-
 }  // namespace mongo
