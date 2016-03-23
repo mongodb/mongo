c2b1e28fd15a4400cbb311057ef33a2120f1e6e2
Jason Carey <jcarey@argv.me>
2016-03-23 11:27:44 -0400

    SERVER-22391 Handle race in connpool hostTimeout
    If a new request comes in at the same moment a hostTimeout triggers we
    can race in shutdown (and hit an invariant).  An additional check fixes
    the misbehavior.

--- src/mongo/executor/connection_pool_asio_integration_test.cpp
+++ src/mongo/executor/connection_pool_asio_integration_test.cpp
@@ -41,6 +41,7 @@
 #include "mongo/unittest/integration_test.h"
 #include "mongo/unittest/unittest.h"
 #include "mongo/util/scopeguard.h"
+#include "mongo/util/time_support.h"
 
 namespace mongo {
 namespace executor {
@@ -118,6 +119,34 @@ TEST(ConnectionPoolASIO, TestPing) {
     }
 
     ASSERT_LTE(MyNetworkConnectionHook::count(), 10);
+}
+
+/**
+ * This test verifies a fix for SERVER-22391, where we raced if a new request
+ * came in at the same time a host timeout was triggering.
+ */
+TEST(ConnectionPoolASIO, TestHostTimeoutRace) {
+    auto fixture = unittest::getFixtureConnectionString();
+
+    NetworkInterfaceASIO::Options options;
+    options.streamFactory = stdx::make_unique<AsyncStreamFactory>();
+    options.connectionPoolOptions.hostTimeout = Milliseconds(1);
+    options.timerFactory = stdx::make_unique<AsyncTimerFactoryASIO>();
+    NetworkInterfaceASIO net{std::move(options)};
+
+    net.startup();
+    auto guard = MakeGuard([&] { net.shutdown(); });
+
+    for (int i = 0; i < 1000; i++) {
+        Deferred<StatusWith<RemoteCommandResponse>> deferred;
+        net.startCommand(
+            makeCallbackHandle(),
+            RemoteCommandRequest{fixture.getServers()[0], "admin", BSON("ping" << 1), BSONObj()},
+            [&](StatusWith<RemoteCommandResponse> resp) { deferred.emplace(std::move(resp)); });
+
+        ASSERT_OK(deferred.get().getStatus());
+        sleepmillis(1);
+    }
 }
 
 }  // namespace
