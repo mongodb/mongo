bc59fbb5a6aa562c010eff525bd3a0b905392d97
Ramon Fernandez <ramon@mongodb.com>
2016-03-29 18:42:46 -0400

    SERVER-21863, SERVER-22767: fix lint errors from recent backports.

--- jstests/noPassthrough/server22767.js
+++ jstests/noPassthrough/server22767.js
@@ -1,14 +1,15 @@
 // test that the mongos doesn't segfault when it receives malformed BSON
-var st = new ShardingTest({shards:1});
+var st = new ShardingTest({shards: 1});
 var testDB = st.getDB('test');
-testDB.test.insert({a:1});
+testDB.test.insert({a: 1});
 
 try {
     testDB.test.find({key: {$regex: 'abcd\0xyz'}}).explain();
 } catch (e) {
     /*
      * if the mongos segfaults, the error is the msg:
-     * "Error: error doing query: failed: network error while attempting to run command 'explain' on host '127.0.0.1:20014'"
+     * "Error: error doing query: failed: network error while attempting to run command 'explain' on
+     *host '127.0.0.1:20014'"
      *
      * if the mongos doesn't segfault, the error is the object:
      * "Error: explain failed: {
