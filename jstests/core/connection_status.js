e266e7e354ae4f2a34c7a3c5dc754ec236d82334
Shane Harvey <shane.harvey@mongodb.com>
2016-03-30 13:37:17 -0400

    SERVER-23184 Reduce listCollections privileges
    (cherry picked from commit 0b490582031c2be63239ac0885801739946a2a78)

--- jstests/core/connection_status.js
+++ jstests/core/connection_status.js
@@ -1,67 +1,84 @@
 // Tests the connectionStatus command
+(function() {
+    var dbName = 'connection_status';
+    var myDB = db.getSiblingDB(dbName);
+    myDB.dropAllUsers();
 
-var dbName = 'connection_status';
-var myDB = db.getSiblingDB(dbName);
-myDB.dropAllUsers();
+    function test(userName) {
+        myDB.createUser(
+            {user: userName, pwd: "weak password", roles: [{db: "admin", role: "root"}]});
+        myDB.auth(userName, "weak password");
 
-function test(userName) {
-    myDB.createUser({user: userName, pwd: "weak password", roles: [{db: "admin", role: "root"}]});
-    myDB.auth(userName, "weak password");
+        var output = myDB.runCommand("connectionStatus");
+        assert.commandWorked(output);
 
-    var output = myDB.runCommand("connectionStatus");
-    assert.commandWorked(output);
+        // Test that authenticated users are properly returned.
+        var users = output.authInfo.authenticatedUsers;
 
-    // Test that authenticated users are properly returned.
-    var users = output.authInfo.authenticatedUsers;
+        var matches = 0;
+        for (var i = 0; i < users.length; i++) {
+            if (users[i].db != dbName)
+                continue;
 
-    var matches = 0;
-    for (var i = 0; i < users.length; i++) {
-        if (users[i].db != dbName)
-            continue;
-
-        assert.eq(users[i].user, userName);
-        matches++;
-    }
-    assert.eq(matches, 1);
-
-    // Test that authenticated roles are properly returned.
-    var roles = output.authInfo.authenticatedUserRoles;
-
-    matches = 0;
-    for (var i = 0; i < roles.length; i++) {
-        if (roles[i].db != "admin")
-            continue;
-
-        assert.eq(roles[i].role, "root");
-        matches++;
-    }
-    assert(matches >= 1);
-
-    // Test roles/ privileges for a non-root user.
-    myDB.createUser({user: "foo", pwd: "weak password", roles: [{db: "foo", role: "read"}]});
-    myDB.logout();
-    myDB.auth("foo", "weak password");
-
-    output = myDB.runCommand({"connectionStatus": 1, "showPrivileges": 1});
-    assert.commandWorked(output);
-
-    var privileges = output.authInfo.authenticatedUserPrivileges;
-
-    matches = 0;
-    for (var i = 0; i < privileges.length; i++) {
-        if (privileges[i].resource.anyResource) {
+            assert.eq(users[i].user, userName);
             matches++;
         }
+        assert.eq(matches, 1);
+
+        // Test that authenticated roles are properly returned.
+        var roles = output.authInfo.authenticatedUserRoles;
+
+        matches = 0;
+        for (var i = 0; i < roles.length; i++) {
+            if (roles[i].db != "admin")
+                continue;
+
+            assert.eq(roles[i].role, "root");
+            matches++;
+        }
+        assert(matches >= 1);
+
+        // Test roles/ privileges for a non-root user.
+        myDB.createUser({user: "foo", pwd: "weak password", roles: [{db: "foo", role: "read"}]});
+        myDB.logout();
+        myDB.auth("foo", "weak password");
+
+        output = myDB.runCommand({"connectionStatus": 1, "showPrivileges": 1});
+        assert.commandWorked(output);
+
+        var users = output.authInfo.authenticatedUsers;
+        var authedAsSystem = false;
+        for (var i = 0; i < users.length; i++) {
+            var authed = users[i];
+            if (authed.user === "__system" && authed.db === "local") {
+                authedAsSystem = true;
+            }
+        }
+
+        var privileges = output.authInfo.authenticatedUserPrivileges;
+
+        for (var i = 0; i < privileges.length; i++) {
+            if (privileges[i].resource.anyResource) {
+                if (authedAsSystem) {
+                    assert.eq(["anyAction"],
+                              privileges[i].actions,
+                              "__system user should only have anyResource/anyAction privilege:" +
+                                  tojson(output));
+                } else {
+                    assert(false,
+                           "read role should not have anyResource privileges:" + tojson(output));
+                }
+            }
+        }
+
+        myDB.logout();
+
+        // Clean up.
+        myDB.auth(userName, "weak password");
+        myDB.dropAllUsers();
+        myDB.logout();
     }
-    assert(matches >= 1);
 
-    myDB.logout();
-
-    // Clean up.
-    myDB.auth(userName, "weak password");
-    myDB.dropAllUsers();
-    myDB.logout();
-}
-
-test("someone");
-test("someone else");
+    test("someone");
+    test("someone else");
+})();
