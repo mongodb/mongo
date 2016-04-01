5abdeae4aaac2a6a4c781f4a218fb45e923a76d3
Scott Hernandez <scotthernandez@gmail.com>
2016-04-01 16:44:13 -0400

    add more diagnostic logging to test

--- jstests/core/update_multi5.js
+++ jstests/core/update_multi5.js
@@ -1,13 +1,18 @@
+// tests that $addToSet works in a multi-update.
+(function() {
+    "use strict";
+    var t = db.update_multi5;
+    t.drop();
 
-t = db.update_multi5;
+    assert.writeOK(t.insert({path: 'r1', subscribers: [1, 2]}));
+    assert.writeOK(t.insert({path: 'r2', subscribers: [3, 4]}));
 
-t.drop();
+    var res = assert.writeOK(t.update(
+        {}, {$addToSet: {subscribers: 5}}, {upsert: false, multi: true, writeConcern: {w: 1}}));
 
-t.insert({path: 'r1', subscribers: [1, 2]});
-t.insert({path: 'r2', subscribers: [3, 4]});
+    assert.eq(res.nMatched, 2, tojson(res));
 
-t.update({}, {$addToSet: {subscribers: 5}}, false, true);
-
-t.find().forEach(function(z) {
-    assert.eq(3, z.subscribers.length, z);
-});
+    t.find().forEach(function(z) {
+        assert.eq(3, z.subscribers.length, tojson(z));
+    });
+})();
