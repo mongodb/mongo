aa0fe8d687acd6b6250eeb52ba166b93e9d547e6
Keith Bostic <keith@wiredtiger.com>
2016-04-04 11:03:02 -0400

    WT-2532: WT_STREAM_APPEND and WT_STREAM_LINE_BUFFER flag overlap.
    The WT_STREAM_APPEND and WT_STREAM_LINE_BUFFER flags have the same
    value.  This means any stream opened for appending will also be
    line-buffered, and could possibly cause failure by appearing to set
    WT_STREAM_APPEND plus WT_STREAM_READ or WT_STREAM_WRITE, when only
    one can legally be specified.

--- src/include/os.h
+++ src/include/os.h
@@ -75,9 +75,9 @@
 #define	WT_OPEN_FIXED		0x004	/* Path isn't relative to home */
 #define	WT_OPEN_READONLY	0x008	/* Readonly open */
 #define	WT_STREAM_APPEND	0x010	/* Open a stream: append */
-#define	WT_STREAM_LINE_BUFFER	0x010	/* Line buffer the stream */
-#define	WT_STREAM_READ		0x020	/* Open a stream: read */
-#define	WT_STREAM_WRITE		0x040	/* Open a stream: write */
+#define	WT_STREAM_LINE_BUFFER	0x020	/* Line buffer the stream */
+#define	WT_STREAM_READ		0x040	/* Open a stream: read */
+#define	WT_STREAM_WRITE		0x080	/* Open a stream: write */
 
 struct __wt_fh {
 	const char *name;			/* File name */
