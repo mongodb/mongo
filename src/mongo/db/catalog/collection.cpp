ce39e32d01e3a909948f850360040809ac7c8a8d
Robert Guo <robert.guo@10gen.com>
2016-04-18 17:14:02 -0400

    SERVER-23762 validate() should fail when encountering invalid BSON

--- src/mongo/db/catalog/collection.cpp
+++ src/mongo/db/catalog/collection.cpp
@@ -931,7 +931,7 @@ public:
         const Status status = validateBSON(obj.objdata(), obj.objsize());
         if (status.isOK())
             *dataSize = obj.objsize();
-        return Status::OK();
+        return status;
     }
 };
 
