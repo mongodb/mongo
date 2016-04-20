7e733ecfe71bce04199cf010b113f176b5210a27
Geert Bosch <geert@mongodb.com>
2016-04-20 10:33:45 -0400

    SERVER-23766 Remove beta warning for inMemory storage engine

--- src/mongo/db/startup_warnings_mongod.cpp
+++ src/mongo/db/startup_warnings_mongod.cpp
@@ -366,15 +366,6 @@ void logMongodStartupWarnings(const StorageGlobalParams& storageParams,
         warned = true;
     }
 
-#if MONGO_ENTERPRISE_VERSION
-    if (storageParams.engine == "inMemory") {
-        log() << startupWarningsLog;
-        log() << "** NOTE: The inMemory storage engine is in beta. " << startupWarningsLog;
-        log() << "**       Do not use in production." << startupWarningsLog;
-        warned = true;
-    }
-#endif
-
     if (warned) {
         log() << startupWarningsLog;
     }
