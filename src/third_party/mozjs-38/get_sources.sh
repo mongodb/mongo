136689d28727416d1770358e72f0a86020aa09e0
Mark Benvenuto <mark.benvenuto@mongodb.com>
2016-04-21 10:41:27 -0400

    SERVER-23358 Remove SpiderMonkey 38

--- src/third_party/mozjs-38/get_sources.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# how we got the last firefox sources
-
-wget "https://ftp.mozilla.org/pub/mozilla.org/firefox/releases/38.6.1esr/source/firefox-38.6.1esr.source.tar.bz2"
-
-tar -jxf firefox-38.6.1esr.source.tar.bz2
-
-mv mozilla-esr38 mozilla-release
