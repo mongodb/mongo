a878b57a5f5e45fb68560e8a9eb2247cf1e0d4b8
David Storch <david.storch@10gen.com>
2016-04-21 11:21:24 -0400

    SERVER-7005 reject embedded null bytes in $regex
    (cherry picked from commit 33471d4424dd81e5310b27867ecb3647c60cf7a4)
    
    Conflicts:
    	jstests/core/regex.js

--- src/mongo/db/matcher/expression_leaf.cpp
+++ src/mongo/db/matcher/expression_leaf.cpp
@@ -269,6 +269,16 @@ Status RegexMatchExpression::init(const StringData& path,
         return Status(ErrorCodes::BadValue, "Regular expression is too long");
     }
 
+    if (regex.find('\0') != std::string::npos) {
+        return Status(ErrorCodes::BadValue,
+                      "Regular expression cannot contain an embedded null byte");
+    }
+
+    if (options.find('\0') != std::string::npos) {
+        return Status(ErrorCodes::BadValue,
+                      "Regular expression options string cannot contain an embedded null byte");
+    }
+
     _regex = regex.toString();
     _flags = options.toString();
     _re.reset(new pcrecpp::RE(_regex.c_str(), flags2options(_flags.c_str())));
