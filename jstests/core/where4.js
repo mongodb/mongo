5751b5417080e4f10f25ade1a6b6f58efc263fbb
Tess Avitabile <tess.avitabile@mongodb.com>
2016-04-22 10:19:17 -0400

    SERVER-23872 Check all writes succeed in where4.js

--- jstests/core/where4.js
+++ jstests/core/where4.js
@@ -1,12 +1,11 @@
+var myDB = db.getSiblingDB("where4");
 
-db.where4.drop();
+myDB.dropDatabase();
 
-db.system.js.insert({_id: "w4", value: "5"});
+assert.writeOK(myDB.where4.insert({x: 1, y: 1}));
+assert.writeOK(myDB.where4.insert({x: 2, y: 1}));
 
-db.where4.insert({x: 1, y: 1});
-db.where4.insert({x: 2, y: 1});
-
-db.where4.update(
+assert.writeOK(myDB.where4.update(
     {
       $where: function() {
           return this.x == 1;
@@ -14,24 +13,21 @@ db.where4.update(
     },
     {$inc: {y: 1}},
     false,
-    true);
+    true));
 
-assert.eq(2, db.where4.findOne({x: 1}).y);
-assert.eq(1, db.where4.findOne({x: 2}).y);
+assert.eq(2, myDB.where4.findOne({x: 1}).y);
+assert.eq(1, myDB.where4.findOne({x: 2}).y);
 
 // Test that where queries work with stored javascript
-db.system.js.save({
+assert.writeOK(myDB.system.js.save({
     _id: "where4_addOne",
     value: function(x) {
         return x + 1;
     }
-});
+}));
 
-db.where4.update({$where: "where4_addOne(this.x) == 2"}, {$inc: {y: 1}}, false, true);
+assert.writeOK(
+    myDB.where4.update({$where: "where4_addOne(this.x) == 2"}, {$inc: {y: 1}}, false, true));
 
-assert.eq(3, db.where4.findOne({x: 1}).y);
-assert.eq(1, db.where4.findOne({x: 2}).y);
-
-db.system.js.remove({_id: "where4_equalsOne"});
-
-db.system.js.remove({_id: "w4"});
+assert.eq(3, myDB.where4.findOne({x: 1}).y);
+assert.eq(1, myDB.where4.findOne({x: 2}).y);
