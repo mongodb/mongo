5da455876cdfda2fe7ff7ad1d6d4ab61fb3cf149
David Storch <david.storch@10gen.com>
2016-04-22 11:32:04 -0400

    SERVER-7005 reject embedded null bytes in $regex
    Manually cherry-picked from 33471d4424d

--- src/mongo/db/matcher/expression_leaf.cpp
+++ src/mongo/db/matcher/expression_leaf.cpp
@@ -204,6 +204,16 @@ namespace mongo {
             return Status( ErrorCodes::BadValue, "Regular expression is too long" );
         }
 
+        if (regex.find('\0') != std::string::npos) {
+            return Status(ErrorCodes::BadValue,
+                          "Regular expression cannot contain an embedded null byte");
+        }
+
+        if (options.find('\0') != std::string::npos) {
+            return Status(ErrorCodes::BadValue,
+                          "Regular expression options string cannot contain an embedded null byte");
+        }
+
         _regex = regex.toString();
         _flags = options.toString();
         _re.reset( new pcrecpp::RE( _regex.c_str(), flags2options( _flags.c_str() ) ) );
