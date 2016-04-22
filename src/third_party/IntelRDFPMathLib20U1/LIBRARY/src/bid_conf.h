3cdac3063cc57ca85a394eb2ba01c422b9f7d30e
Geert Bosch <geert@mongodb.com>
2016-04-22 09:42:15 -0400

    SERVER-23553 Enable compile of intel_decimal128 library on s390

--- src/third_party/IntelRDFPMathLib20U1/LIBRARY/src/bid_conf.h
+++ src/third_party/IntelRDFPMathLib20U1/LIBRARY/src/bid_conf.h
@@ -873,7 +873,8 @@
 #if defined(WINDOWS)
 #define BID_SIZE_LONG 4
 #else
-#if defined(__x86_64__) || defined (__ia64__)  || defined(HPUX_OS_64) || defined(__powerpc64__)
+#if defined(__x86_64__) || defined (__ia64__)  || defined(HPUX_OS_64) || defined(__powerpc64__) \
+      || defined(__s390x__)
 #define BID_SIZE_LONG 8
 #else
 #define BID_SIZE_LONG 4
