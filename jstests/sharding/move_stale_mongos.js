c783668b896fbadff1a2f799bd150409794847d1
Spencer T Brody <spencer@mongodb.com>
2016-04-25 15:34:43 -0400

    SERVER-23915 Make split_stale_mongos.js wait for split to propagate to all config servers
    (cherry picked from commit d30ab0aa759d75cabc0a88ea5f2fd2b9c1a6c9a8)
    
    SERVER-23915 Make move_stale_mongos.js wait for split and move to propagate to all config servers
    
    (cherry picked from commit a8ebe7c54cde9b2f8effe055df29e6bbc8be663d)

--- jstests/sharding/move_stale_mongos.js
+++ jstests/sharding/move_stale_mongos.js
@@ -15,10 +15,16 @@ var curShardIndex = 0;
 
 for (var i = 0; i < 100; i += 10) {
     assert.commandWorked(st.s0.getDB('admin').runCommand({split: testNs, middle: {_id: i}}));
+    if (st.configRS) {
+        st.configRS.awaitLastOpCommitted();  // Ensure that other mongos sees the split
+    }
     var nextShardIndex = (curShardIndex + 1) % shards.length;
     assert.commandWorked(st.s1.getDB('admin').runCommand(
         {moveChunk: testNs, find: {_id: i + 5}, to: shards[nextShardIndex]}));
     curShardIndex = nextShardIndex;
+    if (st.configRS) {
+        st.configRS.awaitLastOpCommitted();  // Ensure that other mongos sees the move
+    }
 }
 
 st.stop();
