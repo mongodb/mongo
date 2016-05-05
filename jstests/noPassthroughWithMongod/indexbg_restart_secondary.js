ddd5b1de8048e2af238dea2d62e925c5a2f6e283
Vincent Do <do.vincent@live.com>
2016-05-05 14:29:31 -0400

    SERVER-13204 Make indexbg tests not timing dependent

--- jstests/noPassthroughWithMongod/indexbg_restart_secondary.js
+++ jstests/noPassthroughWithMongod/indexbg_restart_secondary.js
@@ -1,76 +1,78 @@
-// TODO: SERVER-13215 move test back to replSets suite.
-
 /**
- * TODO: SERVER-13204
- * This  tests inserts a huge number of documents, initiates a background index build
- * and tries to perform another task in parallel while the background index task is
- * active. The problem is that this is timing dependent and the current test setup
- * tries to achieve this by inserting insane amount of documents.
+ * Starts a replica set with arbiter, builds an index in background.
+ * Kills the secondary once the index build starts with a failpoint.
+ * The index build should resume when the secondary is restarted.
  */
 
-/**
- * Starts a replica set with arbiter, builds an index in background
- * restart secondary once it starts building index, secondary should
- * restart when index build after it restarts
- */
+// @tags: [requires_persistence, requires_journaling]
+(function() {
+    'use strict';
 
-// Set up replica set
-var replTest = new ReplSetTest({name: 'bgIndex', nodes: 3});
-var nodes = replTest.nodeList();
+    // Set up replica set
+    var replTest = new ReplSetTest({name: 'bgIndex', nodes: 3});
+    var nodes = replTest.nodeList();
 
-// We need an arbiter to ensure that the primary doesn't step down when we restart the secondary
-replTest.startSet();
-replTest.initiate({
-    "_id": "bgIndex",
-    "members": [
-        {"_id": 0, "host": nodes[0]},
-        {"_id": 1, "host": nodes[1]},
-        {"_id": 2, "host": nodes[2], "arbiterOnly": true}
-    ]
-});
+    // We need an arbiter to ensure that the primary doesn't step down
+    // when we restart the secondary.
+    replTest.startSet();
+    replTest.initiate({
+        "_id": "bgIndex",
+        "members": [
+            {"_id": 0, "host": nodes[0]},
+            {"_id": 1, "host": nodes[1]},
+            {"_id": 2, "host": nodes[2], "arbiterOnly": true}
+        ]
+    });
 
-var master = replTest.getPrimary();
-var second = replTest.getSecondary();
+    var master = replTest.getPrimary();
+    var second = replTest.getSecondary();
 
-var secondId = replTest.getNodeId(second);
+    var secondId = replTest.getNodeId(second);
 
-var masterDB = master.getDB('bgIndexSec');
-var secondDB = second.getDB('bgIndexSec');
+    var masterDB = master.getDB('bgIndexSec');
+    var secondDB = second.getDB('bgIndexSec');
 
-var size = 500000;
+    var collectionName = 'jstests_bgsec';
 
-jsTest.log("creating test data " + size + " documents");
-var bulk = masterDB.jstests_bgsec.initializeUnorderedBulkOp();
-for (var i = 0; i < size; ++i) {
-    bulk.insert({i: i});
-}
-assert.writeOK(bulk.execute());
+    var coll = masterDB.getCollection(collectionName);
 
-jsTest.log("Starting background indexing");
-masterDB.jstests_bgsec.ensureIndex({i: 1}, {background: true});
-assert.eq(2, masterDB.jstests_bgsec.getIndexes().length);
+    var size = 100;
 
-// Wait for the secondary to get the index entry
-assert.soon(function() {
-    return 2 == secondDB.jstests_bgsec.getIndexes().length;
-}, "index not created on secondary (prior to restart)", 5 * 60 * 1000);
-
-// restart secondary and reconnect
-jsTest.log("Restarting secondary");
-replTest.restart(secondId, {}, /*wait=*/true);
-
-// Make sure secondary comes back
-assert.soon(function() {
-    try {
-        secondDB.jstests_bgsec.getIndexes().length;  // trigger a reconnect if needed
-        return true;
-    } catch (e) {
-        return false;
+    var bulk = masterDB.jstests_bgsec.initializeUnorderedBulkOp();
+    for (var i = 0; i < size; ++i) {
+        bulk.insert({i: i});
     }
-}, "secondary didn't restart", 30000, 1000);
+    assert.writeOK(bulk.execute({writeConcern: {j: true}}));
+    assert.eq(size, coll.count(), 'unexpected number of documents after bulk insert.');
 
-assert.soon(function() {
-    return 2 == secondDB.jstests_bgsec.getIndexes().length;
-}, "Index build not resumed after restart", 30000, 50);
+    // Make sure the documents make it to the secondary.
+    replTest.awaitReplication();
 
-jsTest.log("indexbg-restart-secondary.js complete");
+    assert.commandWorked(secondDB.adminCommand(
+        {configureFailPoint: 'crashAfterStartingIndexBuild', mode: 'alwaysOn'}));
+    coll.createIndex({i: 1}, {background: true});
+    assert.eq(2, coll.getIndexes().length);
+
+    assert.eq(waitProgram(second.pid),
+              MongoRunner.EXIT_TEST,
+              "secondary should have crashed due to the 'crashAfterStartingIndexBuild' " +
+                  "failpoint being set.");
+
+    // Restart the secondary. Not using the restart() function here
+    // since the server is already killed by the fail point.
+    replTest.start(secondId, {}, /*restart=*/true, /*wait=*/true);
+
+    // Make sure secondary comes back.
+    assert.soon(function() {
+        try {
+            secondDB.isMaster();  // trigger a reconnect if needed
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }, "secondary didn't restart", 30000, 1000);
+
+    assert.soon(function() {
+        return 2 == secondDB.getCollection(collectionName).getIndexes().length;
+    }, "Index build not resumed after restart", 30000, 50);
+}());
---
+++ jstests/noPassthroughWithMongod/indexbg_restart_secondary_noretry.js
@@ -0,0 +1,77 @@
+/**
+ * Starts a replica set, builds an index in background.
+ * Kills the secondary with a failpoint once the index build starts.
+ * It should *not* build an index on the secondary on restart.
+ */
+
+// @tags: [requires_persistence, requires_journaling]
+(function() {
+    'use strict';
+
+    // Set up replica set.
+    var replTest = new ReplSetTest(
+        {name: 'bgIndexNoRetry', nodes: 3, nodeOptions: {noIndexBuildRetry: "", syncdelay: 1}});
+    var nodenames = replTest.nodeList();
+
+    var nodes = replTest.startSet();
+    replTest.initiate({
+        "_id": "bgIndexNoRetry",
+        "members": [
+            {"_id": 0, "host": nodenames[0]},
+            {"_id": 1, "host": nodenames[1]},
+            {"_id": 2, "host": nodenames[2], arbiterOnly: true}
+        ]
+    });
+
+    var master = replTest.getPrimary();
+    var second = replTest.getSecondary();
+
+    var secondId = replTest.getNodeId(second);
+
+    var masterDB = master.getDB('bgIndexNoRetrySec');
+    var secondDB = second.getDB('bgIndexNoRetrySec');
+
+    var collectionName = 'jstests_bgsec';
+
+    var size = 100;
+
+    var masterColl = masterDB.getCollection(collectionName);
+    var bulk = masterColl.initializeUnorderedBulkOp();
+    for (var i = 0; i < size; ++i) {
+        bulk.insert({i: i});
+    }
+    assert.writeOK(bulk.execute({writeConcern: {j: true}}));
+    assert.eq(size, masterColl.count(), 'unexpected number of documents after bulk insert.');
+
+    // Make sure the documents get replicated to the secondary.
+    replTest.awaitReplication();
+
+    assert.commandWorked(secondDB.adminCommand(
+        {configureFailPoint: 'crashAfterStartingIndexBuild', mode: 'alwaysOn'}));
+    masterColl.createIndex({i: 1}, {background: true});
+    assert.eq(2, masterColl.getIndexes().length);
+
+    assert.eq(waitProgram(second.pid),
+              MongoRunner.EXIT_TEST,
+              "secondary should have crashed due to the 'crashAfterStartingIndexBuild' " +
+                  "failpoint being set.");
+
+    // Restart the secondary. Not using the restart() function here
+    // since the server is already killed by the fail point.
+    replTest.start(secondId, {}, /*restart=*/true, /*wait=*/true);
+
+    // Make sure secondary comes back.
+    assert.soon(function() {
+        try {
+            secondDB.isMaster();  // trigger a reconnect if needed
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }, "secondary didn't restart", 60000, 1000);
+
+    var secondaryColl = secondDB.getCollection(collectionName);
+
+    assert.neq(2, secondaryColl.getIndexes().length);
+    replTest.stopSet();
+}());
