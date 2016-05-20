0b5cbbadf49da830f20fba6e779b7278f211e394
Jason Carey <jcarey@argv.me>
2016-05-20 17:35:55 -0400

    SERVER-23902 non-throwing stdx::thread
    Instead of aliasing std::thread for stdx::thread, we wrap std::thread in
    an api compatible version which std::terminates if std::thread's
    constructor throws.  Given that we don't have a good strategy for
    re-trying thread creation failures, this improves our stack traces, as
    we can see which part of the code failed to create a thread, rather than
    getting a generic error at the top of the stack.

--- src/mongo/stdx/thread.h
+++ src/mongo/stdx/thread.h
@@ -28,12 +28,83 @@
 
 #pragma once
 
+#include <exception>
 #include <thread>
+#include <type_traits>
 
 namespace mongo {
 namespace stdx {
 
-using thread = ::std::thread;                // NOLINT
+/**
+ * We're wrapping std::thread here, rather than aliasing it, because we'd like
+ * a std::thread that's identical in all ways to the original, but terminates
+ * if a new thread cannot be allocated.  We'd like this behavior because we
+ * rarely if ever try/catch thread creation, and don't have a strategy for
+ * retrying.  Therefore, all throwing does is remove context as to which part
+ * of the system failed thread creation (as the exception itself is caught at
+ * the top of the stack).
+ *
+ * We're putting this in stdx, rather than having it as some kind of
+ * mongo::Thread, because the signature and use of the type is otherwise
+ * completely identical.  Rather than migrate all callers, it was deemed
+ * simpler to make the in place adjustment and retain it in stdx.
+ *
+ * We implement this with private inheritance to minimize the overhead of our
+ * wrapping and to simplify the implementation.
+ */
+class thread : private ::std::thread {  // NOLINT
+public:
+    using ::std::thread::native_handle_type;  // NOLINT
+    using ::std::thread::id;                  // NOLINT
+
+    thread() noexcept : ::std::thread::thread() {}  // NOLINT
+
+    thread(const thread&) = delete;
+
+    thread(thread&& other) noexcept
+        : ::std::thread::thread(static_cast<::std::thread&&>(std::move(other))) {}  // NOLINT
+
+    /**
+     * As of C++14, the Function overload for std::thread requires that this constructor only
+     * participate in overload resolution if std::decay_t<Function> is not the same type as thread.
+     * That prevents this overload from intercepting calls that might generate implicit conversions
+     * before binding to other constructors (specifically move/copy constructors).
+     */
+    template <
+        class Function,
+        class... Args,
+        typename std::enable_if<!std::is_same<thread, typename std::decay<Function>::type>::value,
+                                int>::type = 0>
+    explicit thread(Function&& f, Args&&... args) try:
+        ::std::thread::thread(std::forward<Function>(f), std::forward<Args>(args)...) {}  // NOLINT
+    catch (...) {
+        std::terminate();
+    }
+
+    thread& operator=(const thread&) = delete;
+
+    thread& operator=(thread&& other) noexcept {
+        return static_cast<thread&>(
+            ::std::thread::operator=(static_cast<::std::thread&&>(std::move(other))));  // NOLINT
+    };
+
+    using ::std::thread::joinable;              // NOLINT
+    using ::std::thread::get_id;                // NOLINT
+    using ::std::thread::native_handle;         // NOLINT
+    using ::std::thread::hardware_concurrency;  // NOLINT
+
+    using ::std::thread::join;    // NOLINT
+    using ::std::thread::detach;  // NOLINT
+
+    void swap(thread& other) noexcept {
+        ::std::thread::swap(static_cast<::std::thread&>(other));  // NOLINT
+    }
+};
+
+inline void swap(thread& lhs, thread& rhs) noexcept {
+    lhs.swap(rhs);
+}
+
 namespace this_thread = ::std::this_thread;  // NOLINT
 
 }  // namespace stdx
