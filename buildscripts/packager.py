fd49250793c337939278f3f8b4958e9561228650
Sam Kleinman <samk@10gen.com>
2016-05-24 11:42:45 -0400

    SERVER-23043: support for ubuntu1604 community and enterprise builds
    (cherry picked from commit 9811b7af764d0a38734d00cafd6397b4f32183f5)

--- buildscripts/packager.py
+++ buildscripts/packager.py
@@ -209,8 +209,8 @@ class Distro(object):
                 return "precise"
             elif build_os == 'ubuntu1404':
                 return "trusty"
-            elif build_os == 'ubuntu1504':
-                return "vivid"
+            elif build_os == 'ubuntu1604':
+                return "xenial"
             else:
                 raise Exception("unsupported build_os: %s" % build_os)
         elif self.n == 'debian':
@@ -244,7 +244,7 @@ class Distro(object):
         elif self.n == 'amazon':
             return [ "amazon" ]
         elif self.n == 'ubuntu':
-            return [ "ubuntu1204", "ubuntu1404" ]
+            return [ "ubuntu1204", "ubuntu1404", "ubuntu1604", ]
         elif self.n == 'debian':
             return [ "debian71" ]
         else:
