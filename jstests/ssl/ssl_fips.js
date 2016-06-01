edf2aa684c3f0ec9a88a9a50453dc4f3a0f26b2a
Spencer Jackson <spencer.jackson@mongodb.com>
2016-06-01 15:49:10 -0400

    SERVER-24369: Accept OpenSSL FIPS error message from Ubuntu 16.04

--- jstests/ssl/ssl_fips.js
+++ jstests/ssl/ssl_fips.js
@@ -24,7 +24,10 @@ if (mongo != 0) {
     print("mongod failed to start, checking for FIPS support");
     mongoOutput = rawMongoProgramOutput();
     assert(mongoOutput.match(/this version of mongodb was not compiled with FIPS support/) ||
-           mongoOutput.match(/FIPS_mode_set:fips mode not supported/));
+           mongoOutput.match(/FIPS_mode_set:fips mode not supported/) ||
+           // Ubuntu 16.04's OpenSSL produces an unexpected error message, remove this check when
+           // SERVER-24350 is resolved
+           mongoOutput.match(/error:00000000:lib\(0\):func\(0\):reason\(0\)/));
 } else {
     // verify that auth works, SERVER-18051
     md.getDB("admin").createUser({user: "root", pwd: "root", roles: ["root"]});
