ccbcb85c68bf03f6ecc024a32a2b6c6a35c23c33
Mathias Stearn <mathias@10gen.com>
2016-06-03 15:01:20 -0400

    SERVER-23974 Don't use attribute((cold)) with clang

--- src/mongo/platform/compiler_gcc.h
+++ src/mongo/platform/compiler_gcc.h
@@ -35,9 +35,17 @@
 #pragma once
 
 
+#ifdef __clang__
+// Our minimum clang version (3.4) doesn't support the "cold" attribute. We could try to use it with
+// clang versions that support the attribute, but since Apple uses weird version numbers on clang
+// and the main goal with the attribute is to improve our production builds with gcc, it didn't seem
+// worth it.
+#define MONGO_COMPILER_COLD_FUNCTION
+#define MONGO_COMPILER_NORETURN __attribute__((__noreturn__))
+#else
 #define MONGO_COMPILER_COLD_FUNCTION __attribute__((__cold__))
-
 #define MONGO_COMPILER_NORETURN __attribute__((__noreturn__, __cold__))
+#endif
 
 #define MONGO_COMPILER_VARIABLE_UNUSED __attribute__((__unused__))
 
