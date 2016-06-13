8878d6e30d6542929debd989045384cd93ba50a1
iwysiu <isabella.siu@10gen.com>
2016-06-13 13:49:31 -0400

    SERVER-24535 modify testcases.py to work with concurrent fuzzing

--- buildscripts/resmokelib/testing/testcases.py
+++ buildscripts/resmokelib/testing/testcases.py
@@ -312,6 +312,9 @@ class JSTestCase(TestCase):
         test_data = global_vars.get("TestData", {}).copy()
         test_data["minPort"] = core.network.PortAllocator.min_test_port(fixture.job_num)
         test_data["maxPort"] = core.network.PortAllocator.max_test_port(fixture.job_num)
+        # Marks the main test when multiple test clients are run concurrently, to notify the test
+        # of any code that should only be run once.
+        test_data["isMainTest"] = True
 
         global_vars["TestData"] = test_data
         self.shell_options["global_vars"] = global_vars
