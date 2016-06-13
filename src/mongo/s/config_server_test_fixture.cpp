a819d49dfe94abcc0556ca9e96321cf16508b9d7
Randolph Tan <randolph@10gen.com>
2016-06-13 18:20:40 -0400

    SERVER-24567 Implement Config Server Test fixture

---
+++ src/mongo/s/config_server_test_fixture.cpp
@@ -0,0 +1,307 @@
+/**
+ *    Copyright (C) 2016 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/platform/basic.h"
+
+#include "mongo/s/config_server_test_fixture.h"
+
+#include <algorithm>
+#include <vector>
+
+#include "mongo/base/status_with.h"
+#include "mongo/client/remote_command_targeter_factory_mock.h"
+#include "mongo/client/remote_command_targeter_mock.h"
+#include "mongo/db/client.h"
+#include "mongo/db/commands.h"
+#include "mongo/db/namespace_string.h"
+#include "mongo/db/query/query_request.h"
+#include "mongo/db/repl/read_concern_args.h"
+#include "mongo/db/repl/repl_settings.h"
+#include "mongo/db/repl/replication_coordinator_mock.h"
+#include "mongo/db/service_context_noop.h"
+#include "mongo/executor/network_interface_mock.h"
+#include "mongo/executor/task_executor_pool.h"
+#include "mongo/executor/thread_pool_task_executor_test_fixture.h"
+#include "mongo/rpc/metadata/repl_set_metadata.h"
+#include "mongo/rpc/metadata/server_selection_metadata.h"
+#include "mongo/s/balancer/balancer_configuration.h"
+#include "mongo/s/catalog/catalog_cache.h"
+#include "mongo/s/catalog/dist_lock_manager_mock.h"
+#include "mongo/s/catalog/replset/sharding_catalog_client_impl.h"
+#include "mongo/s/catalog/replset/sharding_catalog_manager_impl.h"
+#include "mongo/s/catalog/type_changelog.h"
+#include "mongo/s/catalog/type_collection.h"
+#include "mongo/s/catalog/type_shard.h"
+#include "mongo/s/client/shard_factory.h"
+#include "mongo/s/client/shard_local.h"
+#include "mongo/s/client/shard_registry.h"
+#include "mongo/s/client/shard_remote.h"
+#include "mongo/s/grid.h"
+#include "mongo/s/query/cluster_cursor_manager.h"
+#include "mongo/s/set_shard_version_request.h"
+#include "mongo/s/sharding_egress_metadata_hook_for_mongod.h"
+#include "mongo/s/write_ops/batched_command_request.h"
+#include "mongo/s/write_ops/batched_command_response.h"
+#include "mongo/stdx/memory.h"
+#include "mongo/util/clock_source_mock.h"
+#include "mongo/util/tick_source_mock.h"
+
+namespace mongo {
+
+using executor::NetworkInterfaceMock;
+using executor::NetworkTestEnv;
+using executor::RemoteCommandRequest;
+using executor::RemoteCommandResponse;
+using rpc::ShardingEgressMetadataHookForMongod;
+using unittest::assertGet;
+
+using std::string;
+using std::vector;
+using unittest::assertGet;
+
+namespace {
+ReadPreferenceSetting kReadPref(ReadPreference::PrimaryOnly);
+}
+
+ConfigServerTestFixture::ConfigServerTestFixture() = default;
+
+ConfigServerTestFixture::~ConfigServerTestFixture() = default;
+
+const Seconds ConfigServerTestFixture::kFutureTimeout{5};
+
+void ConfigServerTestFixture::setUp() {
+    ServiceContextMongoDTest::setUp();
+
+    auto serviceContext = getGlobalServiceContext();
+    _messagePort = stdx::make_unique<MessagingPortMock>();
+    Client::initThreadIfNotAlready("ConfigServerTestFixture");
+    _opCtx = cc().makeOperationContext();
+
+    repl::ReplSettings replSettings;
+    repl::ReplicationCoordinator::set(
+        serviceContext, stdx::make_unique<repl::ReplicationCoordinatorMock>(replSettings));
+
+    // Set up executor pool used for most operations.
+    auto fixedNet = stdx::make_unique<executor::NetworkInterfaceMock>();
+    fixedNet->setEgressMetadataHook(stdx::make_unique<ShardingEgressMetadataHookForMongod>());
+    _mockNetwork = fixedNet.get();
+    auto fixedExec = makeThreadPoolTestExecutor(std::move(fixedNet));
+    _networkTestEnv = stdx::make_unique<NetworkTestEnv>(fixedExec.get(), _mockNetwork);
+    _executor = fixedExec.get();
+
+    auto netForPool = stdx::make_unique<executor::NetworkInterfaceMock>();
+    netForPool->setEgressMetadataHook(stdx::make_unique<ShardingEgressMetadataHookForMongod>());
+    auto execForPool = makeThreadPoolTestExecutor(std::move(netForPool));
+    std::vector<std::unique_ptr<executor::TaskExecutor>> executorsForPool;
+    executorsForPool.emplace_back(std::move(execForPool));
+
+    auto executorPool = stdx::make_unique<executor::TaskExecutorPool>();
+    executorPool->addExecutors(std::move(executorsForPool), std::move(fixedExec));
+
+    // Set up executor used for a few special operations during addShard.
+    auto specialNet(stdx::make_unique<executor::NetworkInterfaceMock>());
+    auto specialMockNet = specialNet.get();
+    auto specialExec = makeThreadPoolTestExecutor(std::move(specialNet));
+    _addShardNetworkTestEnv = stdx::make_unique<NetworkTestEnv>(specialExec.get(), specialMockNet);
+    _executorForAddShard = specialExec.get();
+
+    auto uniqueDistLockManager = stdx::make_unique<DistLockManagerMock>();
+    _distLockManager = uniqueDistLockManager.get();
+    std::unique_ptr<ShardingCatalogClientImpl> catalogClient(
+        stdx::make_unique<ShardingCatalogClientImpl>(std::move(uniqueDistLockManager)));
+    _catalogClient = catalogClient.get();
+    catalogClient->startup();
+
+    std::unique_ptr<ShardingCatalogManagerImpl> catalogManager(
+        stdx::make_unique<ShardingCatalogManagerImpl>(_catalogClient, std::move(specialExec)));
+    _catalogManager = catalogManager.get();
+    catalogManager->startup();
+
+    auto targeterFactory(stdx::make_unique<RemoteCommandTargeterFactoryMock>());
+    auto targeterFactoryPtr = targeterFactory.get();
+    _targeterFactory = targeterFactoryPtr;
+
+    ShardFactory::BuilderCallable setBuilder =
+        [targeterFactoryPtr](const ShardId& shardId, const ConnectionString& connStr) {
+            return stdx::make_unique<ShardRemote>(
+                shardId, connStr, targeterFactoryPtr->create(connStr));
+        };
+
+    ShardFactory::BuilderCallable masterBuilder =
+        [targeterFactoryPtr](const ShardId& shardId, const ConnectionString& connStr) {
+            return stdx::make_unique<ShardRemote>(
+                shardId, connStr, targeterFactoryPtr->create(connStr));
+        };
+
+    ShardFactory::BuilderCallable localBuilder = [](const ShardId& shardId,
+                                                    const ConnectionString& connStr) {
+        return stdx::make_unique<ShardLocal>(shardId);
+    };
+
+    ShardFactory::BuildersMap buildersMap{
+        {ConnectionString::SET, std::move(setBuilder)},
+        {ConnectionString::MASTER, std::move(masterBuilder)},
+        {ConnectionString::LOCAL, std::move(localBuilder)},
+    };
+
+    auto shardFactory =
+        stdx::make_unique<ShardFactory>(std::move(buildersMap), std::move(targeterFactory));
+
+    auto shardRegistry(
+        stdx::make_unique<ShardRegistry>(std::move(shardFactory), ConnectionString::forLocal()));
+    executorPool->startup();
+
+    // For now initialize the global grid object. All sharding objects will be accessible from there
+    // until we get rid of it.
+    grid.init(std::move(catalogClient),
+              std::move(catalogManager),
+              stdx::make_unique<CatalogCache>(),
+              std::move(shardRegistry),
+              stdx::make_unique<ClusterCursorManager>(serviceContext->getPreciseClockSource()),
+              stdx::make_unique<BalancerConfiguration>(),
+              std::move(executorPool),
+              _mockNetwork);
+}
+
+void ConfigServerTestFixture::tearDown() {
+    grid.getExecutorPool()->shutdownAndJoin();
+    grid.catalogManager()->shutDown(_opCtx.get());
+    grid.catalogClient(_opCtx.get())->shutDown(_opCtx.get());
+    grid.clearForUnitTests();
+
+    _opCtx.reset();
+    _client.reset();
+
+    ServiceContextMongoDTest::tearDown();
+}
+
+void ConfigServerTestFixture::shutdownExecutor() {
+    if (_executor) {
+        _executor->shutdown();
+        _executorForAddShard->shutdown();
+    }
+}
+
+ShardingCatalogClient* ConfigServerTestFixture::catalogClient() const {
+    return grid.catalogClient(_opCtx.get());
+}
+
+ShardingCatalogManager* ConfigServerTestFixture::catalogManager() const {
+    return grid.catalogManager();
+}
+
+ShardingCatalogClientImpl* ConfigServerTestFixture::getCatalogClient() const {
+    return _catalogClient;
+}
+
+ShardRegistry* ConfigServerTestFixture::shardRegistry() const {
+    invariant(grid.shardRegistry());
+
+    return grid.shardRegistry();
+}
+
+RemoteCommandTargeterFactoryMock* ConfigServerTestFixture::targeterFactory() const {
+    invariant(_targeterFactory);
+
+    return _targeterFactory;
+}
+
+std::shared_ptr<Shard> ConfigServerTestFixture::getConfigShard() const {
+    auto shardRegistry = grid.shardRegistry();
+    invariant(shardRegistry);
+
+    return shardRegistry->getConfigShard();
+}
+
+executor::NetworkInterfaceMock* ConfigServerTestFixture::network() const {
+    invariant(_mockNetwork);
+
+    return _mockNetwork;
+}
+
+executor::TaskExecutor* ConfigServerTestFixture::executor() const {
+    invariant(_executor);
+
+    return _executor;
+}
+
+MessagingPortMock* ConfigServerTestFixture::getMessagingPort() const {
+    return _messagePort.get();
+}
+
+DistLockManagerMock* ConfigServerTestFixture::distLock() const {
+    invariant(_distLockManager);
+    return _distLockManager;
+}
+
+OperationContext* ConfigServerTestFixture::operationContext() const {
+    invariant(_opCtx);
+
+    return _opCtx.get();
+}
+
+void ConfigServerTestFixture::onCommand(NetworkTestEnv::OnCommandFunction func) {
+    _networkTestEnv->onCommand(func);
+}
+
+void ConfigServerTestFixture::onCommandForAddShard(NetworkTestEnv::OnCommandFunction func) {
+    _addShardNetworkTestEnv->onCommand(func);
+}
+
+void ConfigServerTestFixture::onCommandWithMetadata(
+    NetworkTestEnv::OnCommandWithMetadataFunction func) {
+    _networkTestEnv->onCommandWithMetadata(func);
+}
+
+void ConfigServerTestFixture::onFindCommand(NetworkTestEnv::OnFindCommandFunction func) {
+    _networkTestEnv->onFindCommand(func);
+}
+
+void ConfigServerTestFixture::onFindWithMetadataCommand(
+    NetworkTestEnv::OnFindCommandWithMetadataFunction func) {
+    _networkTestEnv->onFindWithMetadataCommand(func);
+}
+
+Status ConfigServerTestFixture::insertToConfigCollection(OperationContext* txn,
+                                                         const NamespaceString& ns,
+                                                         const BSONObj& doc) {
+    auto insert(stdx::make_unique<BatchedInsertRequest>());
+    insert->addToDocuments(doc);
+
+    BatchedCommandRequest request(insert.release());
+    request.setNS(ns);
+
+    auto config = getConfigShard();
+    invariant(config);
+
+    auto insertStatus = config->runCommand(
+        txn, kReadPref, ns.db().toString(), request.toBSON(), Shard::RetryPolicy::kNoRetry);
+    return insertStatus.getStatus();
+}
+
+}  // namespace mongo
---
+++ src/mongo/s/config_server_test_fixture.h
@@ -0,0 +1,157 @@
+/**
+ *    Copyright (C) 2016 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#pragma once
+
+#include <utility>
+
+#include "mongo/db/service_context.h"
+#include "mongo/db/service_context_d_test_fixture.h"
+#include "mongo/executor/network_test_env.h"
+#include "mongo/unittest/unittest.h"
+#include "mongo/util/net/message_port_mock.h"
+
+namespace mongo {
+
+class BSONObj;
+class CatalogCache;
+struct ChunkVersion;
+class CollectionType;
+class DistLockManagerMock;
+class NamespaceString;
+class Shard;
+class ShardFactoryMock;
+class ShardingCatalogClient;
+class ShardingCatalogClientImpl;
+class ShardingCatalogManager;
+class ShardingCatalogManagerImpl;
+class RemoteCommandTargeterFactoryMock;
+class RemoteCommandTargeterMock;
+class ShardRegistry;
+class ShardType;
+template <typename T>
+class StatusWith;
+
+namespace executor {
+class NetworkInterfaceMock;
+class TaskExecutor;
+}  // namespace executor
+
+/**
+ * Sets up the mocked out objects for testing the catalog manager and catalog client with the
+ * remote interface backed by the NetworkTestEnv and config server as the local storage engine.
+ */
+class ConfigServerTestFixture : public ServiceContextMongoDTest {
+public:
+    ConfigServerTestFixture();
+    ~ConfigServerTestFixture();
+
+    static const Seconds kFutureTimeout;
+
+    template <typename Lambda>
+    executor::NetworkTestEnv::FutureHandle<typename std::result_of<Lambda()>::type> launchAsync(
+        Lambda&& func) const {
+        return _networkTestEnv->launchAsync(std::forward<Lambda>(func));
+    }
+
+    ShardingCatalogClient* catalogClient() const;
+
+    ShardingCatalogManager* catalogManager() const;
+
+    /**
+     * Prefer catalogClient() method over this as much as possible.
+     */
+    ShardingCatalogClientImpl* getCatalogClient() const;
+
+    ShardRegistry* shardRegistry() const;
+
+    RemoteCommandTargeterFactoryMock* targeterFactory() const;
+
+    std::shared_ptr<Shard> getConfigShard() const;
+
+    executor::NetworkInterfaceMock* network() const;
+
+    executor::TaskExecutor* executor() const;
+
+    MessagingPortMock* getMessagingPort() const;
+
+    DistLockManagerMock* distLock() const;
+
+    OperationContext* operationContext() const;
+
+    /**
+     * Insert a document to this config server to the specified namespace.
+     */
+    Status insertToConfigCollection(OperationContext* txn,
+                                    const NamespaceString& ns,
+                                    const BSONObj& doc);
+
+    /**
+     * Blocking methods, which receive one message from the network and respond using the
+     * responses returned from the input function. This is a syntactic sugar for simple,
+     * single request + response or find tests.
+     */
+    void onCommand(executor::NetworkTestEnv::OnCommandFunction func);
+    // Same as onCommand but run against _addShardNetworkTestEnv
+    void onCommandForAddShard(executor::NetworkTestEnv::OnCommandFunction func);
+    void onCommandWithMetadata(executor::NetworkTestEnv::OnCommandWithMetadataFunction func);
+    void onFindCommand(executor::NetworkTestEnv::OnFindCommandFunction func);
+    void onFindWithMetadataCommand(
+        executor::NetworkTestEnv::OnFindCommandWithMetadataFunction func);
+
+    void setUp() override;
+
+    void tearDown() override;
+
+    void shutdownExecutor();
+
+    /**
+     * Checks that the given command has the expected settings for read after opTime.
+     */
+    void checkReadConcern(const BSONObj& cmdObj,
+                          const Timestamp& expectedTS,
+                          long long expectedTerm) const;
+
+private:
+    ServiceContext::UniqueClient _client;
+    ServiceContext::UniqueOperationContext _opCtx;
+    std::unique_ptr<MessagingPortMock> _messagePort;
+
+    RemoteCommandTargeterFactoryMock* _targeterFactory;
+
+    executor::NetworkInterfaceMock* _mockNetwork;
+    executor::TaskExecutor* _executor;
+    executor::TaskExecutor* _executorForAddShard;
+    std::unique_ptr<executor::NetworkTestEnv> _networkTestEnv;
+    std::unique_ptr<executor::NetworkTestEnv> _addShardNetworkTestEnv;
+    DistLockManagerMock* _distLockManager = nullptr;
+    ShardingCatalogClientImpl* _catalogClient = nullptr;
+    ShardingCatalogManagerImpl* _catalogManager = nullptr;
+};
+
+}  // namespace mongo
