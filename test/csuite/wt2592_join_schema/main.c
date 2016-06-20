55aa7926f29c141431ac4f23bfb5678044c40488
David Hows <howsdav@gmail.com>
2016-06-21 00:01:58 +1000

    WT-2605 Fix valgrind (#2813)

--- test/csuite/wt2592_join_schema/main.c
+++ test/csuite/wt2592_join_schema/main.c
@@ -83,6 +83,7 @@ main(int argc, char *argv[])
 	int count, ret;
 
 	opts = &_opts;
+	recno = 0;
 	memset(opts, 0, sizeof(*opts));
 	testutil_check(testutil_parse_opts(argc, argv, opts));
 	testutil_make_work_dir(opts->home);
