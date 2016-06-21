01dcc1b450d4448ecc6f9d431bf59c137aab36f1
David Storch <david.storch@10gen.com>
2016-06-21 15:41:08 -0400

    SERVER-24405 make count command forward collation to the shards

--- src/mongo/s/commands/cluster_count_cmd.cpp
+++ src/mongo/s/commands/cluster_count_cmd.cpp
@@ -144,7 +144,7 @@ public:
         }
 
         const std::initializer_list<StringData> passthroughFields = {
-            "hint", "$queryOptions", "readConcern", QueryRequest::cmdOptionMaxTimeMS,
+            "$queryOptions", "collation", "hint", "readConcern", QueryRequest::cmdOptionMaxTimeMS,
         };
         for (auto name : passthroughFields) {
             if (auto field = cmdObj[name]) {
