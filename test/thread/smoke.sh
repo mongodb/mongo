fb715af91441e1b14b7ec894d55f1811fe97fd7a
Sulabh Mahajan <sulabh.mahajan@mongodb.com>
2016-07-01 14:27:07 +1000

    WT-2486 Update make check so that it runs faster (#2837)
    Do this by reducing how much testing is done by manydbs, format, recovery and thread. Increase the amount of testing done with those test cases in Jenkins instead.

--- test/thread/smoke.sh
+++ test/thread/smoke.sh
@@ -4,10 +4,10 @@ set -e
 
 # Smoke-test format as part of running "make check".
 $TEST_WRAPPER ./t -t f
-$TEST_WRAPPER ./t -S -F -t f
+$TEST_WRAPPER ./t -S -F -n 1000 -t f
 
 $TEST_WRAPPER ./t -t r
-$TEST_WRAPPER ./t -S -F -t r
+$TEST_WRAPPER ./t -S -F -n 1000 -t r
 
 $TEST_WRAPPER ./t -t v
-$TEST_WRAPPER ./t -S -F -t v
+$TEST_WRAPPER ./t -S -F -n 1000 -t v
