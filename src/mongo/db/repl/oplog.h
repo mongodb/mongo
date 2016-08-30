3d56ecc5637be7b017289b91904bef323196bdcf
Benety Goh <benety@mongodb.com>
2016-08-30 14:10:21 -0400

    SERVER-25519 repl::checkForCappedOplog accepts db pointer to local database
    (cherry picked from commit d740809efc076f6c48cc904a0d68aa0115ba3ab5)

--- src/mongo/db/repl/oplog.h
+++ src/mongo/db/repl/oplog.h
@@ -168,7 +168,7 @@ void signalOplogWaiters();
 /**
  * Check that the oplog is capped, and abort the process if it is not.
  */
-void checkForCappedOplog(OperationContext* txn);
+void checkForCappedOplog(OperationContext* txn, Database* db);
 
 }  // namespace repl
 }  // namespace mongo
