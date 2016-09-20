afdca9e2978e55b29a2b01e4cad55c794afca65e
Waley Chen <waleycz@gmail.com>
2016-09-20 16:37:31 -0400

    SERVER-17358 Add flag to customize maxLogLine when logging large lines

--- jstests/core/loglong.js
+++ jstests/core/loglong.js
@@ -1,34 +1,56 @@
 // test for SERVER-5013
 // make sure very long long lines get truncated
 
-t = db.loglong;
-t.drop();
+(function() {
+    "use strict";
 
-t.insert({x: 1});
-
-n = 0;
-query = {
-    x: []
-};
-while (Object.bsonsize(query) < 30000) {
-    query.x.push(n++);
-}
-
-before = db.adminCommand({setParameter: 1, logLevel: 1});
-
-t.findOne(query);
-
-x = db.adminCommand({setParameter: 1, logLevel: before.was});
-assert.eq(1, x.was, tojson(x));
-
-log = db.adminCommand({getLog: "global"}).log;
-
-found = false;
-for (i = log.length - 1; i >= 0; i--) {
-    if (log[i].indexOf("warning: log line attempted (16kB)") >= 0) {
-        found = true;
-        break;
+    const options = {setParameter: "maxLogSizeKB=9"};
+    if (jsTestOptions().storageEngine == "wiredTiger") {
+        options.enableMajorityReadConcern = "";
     }
-}
+    const conn = MongoRunner.runMongod(options);
 
-assert(found, tojson(log));
+    var db = conn.getDB('db');
+    var res = db.adminCommand({getParameter: 1, maxLogSizeKB: 1});
+    assert.eq(9, res.maxLogSizeKB);
+
+    var t = db.loglong;
+    t.drop();
+
+    t.insert({x: 1});
+
+    var n = 0;
+    var query = {x: []};
+    while (Object.bsonsize(query) < 30000) {
+        query.x.push(n++);
+    }
+
+    assertLogTruncated(db, t, 9);
+
+    var res = db.adminCommand({setParameter: 1, maxLogSizeKB: 8});
+    assert.eq(res.ok, 1);
+
+    assertLogTruncated(db, t, 8);
+
+    function assertLogTruncated(db, t, maxLogSize) {
+        var before = db.adminCommand({setParameter: 1, logLevel: 1});
+
+        t.findOne(query);
+
+        var x = db.adminCommand({setParameter: 1, logLevel: before.was});
+        assert.eq(1, x.was, tojson(x));
+
+        var log = db.adminCommand({getLog: "global"}).log;
+
+        var found = false;
+        var toFind = "warning: log line attempted (16kB) over max size (" + maxLogSize + "kB)";
+        for (var i = log.length - 1; i >= 0; i--) {
+            if (log[i].indexOf(toFind) >= 0) {
+                found = true;
+                break;
+            }
+        }
+
+        assert(found, tojson(log));
+    }
+})();
