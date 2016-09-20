fb992867f9f277dd949fdce455bb073975737bab
Max Hirschhorn <max.hirschhorn@mongodb.com>
2016-09-20 19:07:54 -0400

    SERVER-24564 Remove test cases that mix 3.0 and latest in a replica set.

--- jstests/multiVersion/partial_index_upgrade.js
+++ jstests/multiVersion/partial_index_upgrade.js
@@ -1,8 +1,6 @@
 /**
  * Test the upgrade process for 3.0 ~~> the latest version involving partial indexes:
  *   - An invalid usage of partialFilterExpression should cause the mongod to fail to start up.
- *   - A request to build an invalid partial index should cause a secondary running the latest
- *     version and syncing off a primary running 3.0 to fassert.
  */
 (function() {
     'use strict';
@@ -53,53 +51,5 @@
                   conn,
                   'mongod should not have been able to start up when an index with' +
                       ' options ' + tojson(indexOptions) + ' exists');
-    });
-
-    // Create a replica set with a primary running 3.0 and a secondary running the latest version.
-    // The secondary should terminate when the command to build an invalid partial index replicates.
-    testCases.forEach(function(indexOptions) {
-        var replSetName = 'partial_index_replset';
-        var nodes = [
-            {binVersion: '3.0'},
-            {binVersion: 'latest'},
-        ];
-
-        var rst = new ReplSetTest({name: replSetName, nodes: nodes});
-
-        var conns = rst.startSet({startClean: true});
-
-        // Use write commands in order to make assertions about success of operations based on the
-        // response.
-        conns.forEach(function(conn) {
-            conn.forceWriteMode('commands');
-        });
-
-        // Rig the election so that the 3.0 node becomes the primary.
-        var replSetConfig = rst.getReplSetConfig();
-        replSetConfig.members[1].priority = 0;
-
-        rst.initiate(replSetConfig);
-
-        var primary30 = conns[0];
-        var secondaryLatest = conns[1];
-
-        assert.commandWorked(primary30.getDB('test').coll.createIndex({a: 1}, indexOptions),
-                             'failed to create index with options ' + tojson(indexOptions));
-
-        // Verify that the secondary running the latest version terminates when the command to build
-        // an invalid partial index replicates.
-        assert.soon(
-            function() {
-                try {
-                    secondaryLatest.getDB('test').runCommand({ping: 1});
-                } catch (e) {
-                    return true;
-                }
-                return false;
-            },
-            'secondary should have terminated due to request to build an invalid partial index' +
-                ' with options ' + tojson(indexOptions));
-
-        rst.stopSet(undefined, undefined, {allowedExitCodes: [MongoRunner.EXIT_ABRUPT]});
     });
 })();
