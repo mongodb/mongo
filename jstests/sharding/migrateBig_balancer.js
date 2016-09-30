ba344084775139e211e6d8904a2282c1cec1ad83
Spencer T Brody <spencer@mongodb.com>
2016-09-30 13:23:55 -0400

    SERVER-26414 Don't preemptively abort test due to perceived machine slowness in migrateBig_balancer.js

--- jstests/sharding/migrateBig_balancer.js
+++ jstests/sharding/migrateBig_balancer.js
@@ -1,11 +1,12 @@
 (function() {
+    "use strict";
 
     var st =
         new ShardingTest({name: 'migrateBig_balancer', shards: 2, other: {enableBalancer: true}});
     var mongos = st.s;
 
     var admin = mongos.getDB("admin");
-    db = mongos.getDB("test");
+    var db = mongos.getDB("test");
     var coll = db.getCollection("stuff");
 
     assert.commandWorked(admin.runCommand({enablesharding: coll.getDB().getName()}));
@@ -18,7 +19,7 @@
     for (var i = 0; i < nsq; i++)
         data += data;
 
-    dataObj = {};
+    var dataObj = {};
     for (var i = 0; i < n; i++)
         dataObj["data-" + i] = data;
 
@@ -40,20 +41,7 @@
         5, mongos.getDB("config").chunks.find({ns: "test.stuff"}).count(), "not enough chunks");
 
     assert.soon(function() {
-        // On *extremely* slow or variable systems, we've seen migrations fail in the critical
-        // section and
-        // kill the server.  Do an explicit check for this. SERVER-8781
-        // TODO: Remove once we can better specify what systems to run what tests on.
-        try {
-            assert.commandWorked(st.shard0.getDB("admin").runCommand({ping: 1}));
-            assert.commandWorked(st.shard1.getDB("admin").runCommand({ping: 1}));
-        } catch (e) {
-            print("An error occurred contacting a shard during balancing," +
-                  " this may be due to slow disk I/O, aborting test.");
-            throw e;
-        }
-
-        res = mongos.getDB("config").chunks.group({
+        var res = mongos.getDB("config").chunks.group({
             cond: {ns: "test.stuff"},
             key: {shard: 1},
             reduce: function(doc, out) {
