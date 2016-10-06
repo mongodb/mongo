32bd5fcc91071b45173d7fbbdb541f562c767f5f
Robert Guo <robert.guo@10gen.com>
2016-10-06 16:34:21 -0400

    SERVER-26503 fix incorrect uses of assert.throws()

--- jstests/core/ord.js
+++ jstests/core/ord.js
@@ -5,30 +5,34 @@
 // the $or implementation may need minor adjustments (memory ownership) if this
 // behavior is changed.
 
-t = db.jstests_ord;
-t.drop();
+(function() {
+    const t = db.jstests_ord;
+    t.drop();
 
-t.ensureIndex({a: 1});
-t.ensureIndex({b: 1});
+    t.ensureIndex({a: 1});
+    t.ensureIndex({b: 1});
 
-for (i = 0; i < 80; ++i) {
-    t.save({a: 1});
-}
+    for (i = 0; i < 80; ++i) {
+        t.save({a: 1});
+    }
 
-for (i = 0; i < 100; ++i) {
-    t.save({b: 1});
-}
+    for (i = 0; i < 100; ++i) {
+        t.save({b: 1});
+    }
 
-c = t.find({$or: [{a: 1}, {b: 1}]}).batchSize(100);
-for (i = 0; i < 90; ++i) {
-    c.next();
-}
-// At this point, our initial query has ended and there is a client cursor waiting
-// to read additional documents from index {b:1}.  Deduping is performed against
-// the index key {a:1}.
+    const c = t.find({$or: [{a: 1}, {b: 1}]}).batchSize(100);
+    for (i = 0; i < 100; ++i) {
+        c.next();
+    }
+    // At this point, our initial query has ended and there is a client cursor waiting
+    // to read additional documents from index {b:1}.  Deduping is performed against
+    // the index key {a:1}.
 
-t.dropIndex({a: 1});
+    t.dropIndex({a: 1});
 
-// Dropping an index kills all cursors on the indexed namespace, not just those
-// cursors using the dropped index.
-assert.throws(c.next());
+    // Dropping an index kills all cursors on the indexed namespace, not just those
+    // cursors using the dropped index.
+    assert.throws(function() {
+        c.next();
+    });
+})();
--- jstests/ssl/x509_client.js
+++ jstests/ssl/x509_client.js
@@ -29,22 +29,16 @@ function authAndTest(mongo) {
     test = mongo.getDB("test");
 
     // It should be impossible to create users with the same name as the server's subject
-    assert.throws(
-        function() {
-            external.createUser(
-                {user: SERVER_USER, roles: [{'role': 'userAdminAnyDatabase', 'db': 'admin'}]});
-        },
-        {},
-        "Created user with same name as the server's x.509 subject");
+    assert.throws(function() {
+        external.createUser(
+            {user: SERVER_USER, roles: [{'role': 'userAdminAnyDatabase', 'db': 'admin'}]});
+    }, [], "Created user with same name as the server's x.509 subject");
 
     // It should be impossible to create users with names recognized as cluster members
-    assert.throws(
-        function() {
-            external.createUser(
-                {user: INTERNAL_USER, roles: [{'role': 'userAdminAnyDatabase', 'db': 'admin'}]});
-        },
-        {},
-        "Created user which would be recognized as a cluster member");
+    assert.throws(function() {
+        external.createUser(
+            {user: INTERNAL_USER, roles: [{'role': 'userAdminAnyDatabase', 'db': 'admin'}]});
+    }, [], "Created user which would be recognized as a cluster member");
 
     // Add user using localhost exception
     external.createUser({
@@ -62,12 +56,9 @@ function authAndTest(mongo) {
     });
 
     // Localhost exception should not be in place anymore
-    assert.throws(
-        function() {
-            test.foo.findOne();
-        },
-        {},
-        "read without login");
+    assert.throws(function() {
+        test.foo.findOne();
+    }, [], "read without login");
 
     assert(!external.auth({user: INVALID_CLIENT_USER, mechanism: 'MONGODB-X509'}),
            "authentication with invalid user should fail");
@@ -86,12 +77,9 @@ function authAndTest(mongo) {
     test.foo.findOne();
 
     external.logout();
-    assert.throws(
-        function() {
-            test.foo.findOne();
-        },
-        {},
-        "read after logout");
+    assert.throws(function() {
+        test.foo.findOne();
+    }, [], "read after logout");
 }
 
 print("1. Testing x.509 auth to mongod");
