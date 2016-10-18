59bfd84718bc931f3871a8d53e53edb004434788
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2016-10-18 17:58:00 -0400

    SERVER-26365 Forward port changes to merge_chunks_test.js to master

--- jstests/sharding/merge_chunks_test.js
+++ jstests/sharding/merge_chunks_test.js
@@ -9,33 +9,35 @@
     var mongos = st.s0;
     var staleMongos = st.s1;
     var admin = mongos.getDB("admin");
-    var shards = mongos.getCollection("config.shards").find().toArray();
     var coll = mongos.getCollection("foo.bar");
 
-    assert(admin.runCommand({enableSharding: coll.getDB() + ""}).ok);
-    printjson(admin.runCommand({movePrimary: coll.getDB() + "", to: shards[0]._id}));
-    assert(admin.runCommand({shardCollection: coll + "", key: {_id: 1}}).ok);
+    assert.commandWorked(admin.runCommand({enableSharding: coll.getDB() + ""}));
+    st.ensurePrimaryShard('foo', st.shard0.shardName);
+    assert.commandWorked(admin.runCommand({shardCollection: coll + "", key: {_id: 1}}));
 
     // Create ranges MIN->0,0->10,(hole),20->40,40->50,50->90,(hole),100->110,110->MAX on first
     // shard
     jsTest.log("Creating ranges...");
 
-    assert(admin.runCommand({split: coll + "", middle: {_id: 0}}).ok);
-    assert(admin.runCommand({split: coll + "", middle: {_id: 10}}).ok);
-    assert(admin.runCommand({split: coll + "", middle: {_id: 20}}).ok);
-    assert(admin.runCommand({split: coll + "", middle: {_id: 40}}).ok);
-    assert(admin.runCommand({split: coll + "", middle: {_id: 50}}).ok);
-    assert(admin.runCommand({split: coll + "", middle: {_id: 90}}).ok);
-    assert(admin.runCommand({split: coll + "", middle: {_id: 100}}).ok);
-    assert(admin.runCommand({split: coll + "", middle: {_id: 110}}).ok);
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 0}}));
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 10}}));
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 20}}));
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 40}}));
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 50}}));
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 90}}));
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 100}}));
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 110}}));
 
-    assert(admin.runCommand({moveChunk: coll + "", find: {_id: 10}, to: shards[1]._id}).ok);
-    assert(admin.runCommand({moveChunk: coll + "", find: {_id: 90}, to: shards[1]._id}).ok);
+    assert.commandWorked(
+        admin.runCommand({moveChunk: coll + "", find: {_id: 10}, to: st.shard1.shardName}));
+    assert.commandWorked(
+        admin.runCommand({moveChunk: coll + "", find: {_id: 90}, to: st.shard1.shardName}));
 
     st.printShardingStatus();
 
     // Insert some data into each of the consolidated ranges
     assert.writeOK(coll.insert({_id: 0}));
+    assert.writeOK(coll.insert({_id: 10}));
     assert.writeOK(coll.insert({_id: 40}));
     assert.writeOK(coll.insert({_id: 110}));
 
@@ -47,60 +49,94 @@
     // S1: 10->20, 90->100
 
     // Make sure merging non-exact chunks is invalid
-
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: MinKey}, {_id: 5}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 5}, {_id: 10}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 15}, {_id: 50}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 20}, {_id: 55}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 105}, {_id: MaxKey}]}).ok);
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: MinKey}, {_id: 5}]}));
+    assert.commandFailed(admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 5}, {_id: 10}]}));
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 15}, {_id: 50}]}));
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 20}, {_id: 55}]}));
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 105}, {_id: MaxKey}]}));
 
     // Make sure merging single chunks is invalid
-
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: MinKey}, {_id: 0}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 20}, {_id: 40}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 110}, {_id: MaxKey}]}).ok);
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: MinKey}, {_id: 0}]}));
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 20}, {_id: 40}]}));
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 110}, {_id: MaxKey}]}));
 
     // Make sure merging over holes is invalid
-
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 0}, {_id: 40}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 40}, {_id: 110}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 50}, {_id: 110}]}).ok);
+    assert.commandFailed(admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 0}, {_id: 40}]}));
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 40}, {_id: 110}]}));
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 50}, {_id: 110}]}));
 
     // Make sure merging between shards is invalid
-
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 0}, {_id: 20}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 10}, {_id: 40}]}).ok);
-    assert(!admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 40}, {_id: 100}]}).ok);
-
-    assert.eq(3, staleCollection.find().itcount());
+    assert.commandFailed(admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 0}, {_id: 20}]}));
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 10}, {_id: 40}]}));
+    assert.commandFailed(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 40}, {_id: 100}]}));
+    assert.eq(4, staleCollection.find().itcount());
 
     jsTest.log("Trying merges that should succeed...");
 
-    assert(admin.runCommand({mergeChunks: coll + "", bounds: [{_id: MinKey}, {_id: 10}]}).ok);
-
-    assert.eq(3, staleCollection.find().itcount());
-
+    // Make sure merge including the MinKey works
+    assert.commandWorked(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: MinKey}, {_id: 10}]}));
+    assert.eq(4, staleCollection.find().itcount());
     // S0: min->10, 20->40, 40->50, 50->90, 100->110, 110->max
     // S1: 10->20, 90->100
 
-    // Make sure merging three chunks is valid.
-
-    jsTest.log(tojson(admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 20}, {_id: 90}]})));
-
+    // Make sure merging three chunks in the middle works
+    assert.commandWorked(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 20}, {_id: 90}]}));
+    assert.eq(4, staleCollection.find().itcount());
     // S0: min->10, 20->90, 100->110, 110->max
     // S1: 10->20, 90->100
 
-    assert.eq(3, staleCollection.find().itcount());
-
-    assert(admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 100}, {_id: MaxKey}]}).ok);
-
-    assert.eq(3, staleCollection.find().itcount());
-
+    // Make sure merge including the MaxKey works
+    assert.commandWorked(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 100}, {_id: MaxKey}]}));
+    assert.eq(4, staleCollection.find().itcount());
     // S0: min->10, 20->90, 100->max
     // S1: 10->20, 90->100
 
-    st.printShardingStatus();
+    // Make sure merging chunks after a chunk has been moved out of a shard succeeds
+    assert.commandWorked(
+        admin.runCommand({moveChunk: coll + "", find: {_id: 110}, to: st.shard1.shardName}));
+    assert.commandWorked(
+        admin.runCommand({moveChunk: coll + "", find: {_id: 10}, to: st.shard0.shardName}));
+    assert.eq(4, staleCollection.find().itcount());
+    // S0: min->10, 10->20, 20->90
+    // S1: 90->100, 100->max
+
+    assert.commandWorked(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: 90}, {_id: MaxKey}]}));
+    assert.eq(4, staleCollection.find().itcount());
+    // S0: min->10, 10->20, 20->90
+    // S1: 90->max
+
+    // Make sure merge on the other shard after a chunk has been merged succeeds
+    assert.commandWorked(
+        admin.runCommand({mergeChunks: coll + "", bounds: [{_id: MinKey}, {_id: 90}]}));
+    // S0: min->90
+    // S1: 90->max
+
+    st.printShardingStatus(true);
+
+    assert.eq(2, st.s0.getDB('config').chunks.find({}).itcount());
+    assert.eq(1,
+              st.s0.getDB('config')
+                  .chunks.find({'min._id': MinKey, 'max._id': 90, shard: st.shard0.shardName})
+                  .itcount());
+    assert.eq(1,
+              st.s0.getDB('config')
+                  .chunks.find({'min._id': 90, 'max._id': MaxKey, shard: st.shard1.shardName})
+                  .itcount());
 
     st.stop();
-
 })();
