66edc250f46519ea7c6b65f628d201ffbfd0dfbf
Jonathan Abrahams <jonathan@mongodb.com>
2016-11-21 17:14:24 -0500

    SERVER-27145 Shard name from a single node mongod not used as shard index in getSerializedCluster concurrency suite file cluster.js
    (cherry picked from commit 9bb919e988166f5e08aeb0f394842c8afe5ae03f)

--- jstests/concurrency/fsm_libs/cluster.js
+++ jstests/concurrency/fsm_libs/cluster.js
@@ -451,7 +451,7 @@ var Cluster = function(options) {
             } else {
                 // If the shard is a standalone mongod, the format of st.shard0.name in ShardingTest
                 // is "localhost:20006".
-                cluster.shards[i] = [shard.name];
+                cluster.shards[shard.shardName] = [shard.name];
             }
             ++i;
             shard = st['shard' + i];
