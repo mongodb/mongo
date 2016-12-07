77147629b714b1e062c1b406e0aef193cfca36a8
Misha Tyulenev <misha@mongodb.com>
2016-12-07 11:34:34 -0500

    SERVER-27078 fix race in ShardRegistry initialization

--- src/mongo/s/client/shard_registry.h
+++ src/mongo/s/client/shard_registry.h
@@ -97,12 +97,6 @@ public:
      */
     void rebuildShardIfExists(const ConnectionString& newConnString, ShardFactory* factory);
 
-    /**
-     * Rebuilds config shard. The result is to recreate a ReplicaSetMonitor in the case it does
-     * not exist.
-     */
-    void rebuildConfigShard(ShardFactory* factory);
-
 private:
     /**
      * Reads shards docs from the catalog client and fills in maps.
@@ -171,14 +165,6 @@ public:
      * returned false.
      */
     bool reload(OperationContext* txn);
-
-    /**
-     * Throws out and reconstructs the config shard.  This has the effect that if replica set
-     * monitoring of the config server replica set has stopped (because the set was down for too
-     * long), this will cause the ReplicaSetMonitor to be rebuilt, which will re-trigger monitoring
-     * of the config replica set to resume.
-     */
-    void rebuildConfigShard();
 
     /**
      * Takes a connection string describing either a shard or config server replica set, looks
