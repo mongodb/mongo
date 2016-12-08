f3e719cd327d3e2a50f36723e71e08b2729436c8
Nathan Myers <ncm@asperasoft.com>
2016-12-07 19:53:52 -0500

    SERVER-26668 backport fix chunk_manipulation_util.js fix

--- jstests/libs/chunk_manipulation_util.js
+++ jstests/libs/chunk_manipulation_util.js
@@ -101,7 +101,8 @@ function configureMoveChunkFailPoint(shardConnection, stepNumber, mode) {
     assert(stepNumber >= 1);
     assert(stepNumber <= 6);
     var admin = shardConnection.getDB('admin');
-    admin.runCommand({configureFailPoint: 'moveChunkHangAtStep' + stepNumber, mode: mode});
+    assert.commandWorked(
+        admin.runCommand({configureFailPoint: 'moveChunkHangAtStep' + stepNumber, mode: mode}));
 }
 
 //
@@ -166,7 +167,8 @@ function configureMigrateFailPoint(shardConnection, stepNumber, mode) {
     assert(stepNumber >= 1);
     assert(stepNumber <= 5);
     var admin = shardConnection.getDB('admin');
-    admin.runCommand({configureFailPoint: 'migrateThreadHangAtStep' + stepNumber, mode: mode});
+    assert.commandWorked(
+        admin.runCommand({configureFailPoint: 'migrateThreadHangAtStep' + stepNumber, mode: mode}));
 }
 
 //
