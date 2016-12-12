57b9e3b61fa08b51743d13ab5f8275534ddcf3ba
Kyle Suarez <kyle.suarez@mongodb.com>
2016-12-12 11:11:03 -0500

    SERVER-25904 include expected error code in read-only write ops test

--- jstests/readonly/write_ops.js
+++ jstests/readonly/write_ops.js
@@ -5,17 +5,26 @@ runReadOnlyTest(function() {
     return {
         name: 'write_ops',
         load: function(writableCollection) {
-            assert.writeOK(writableCollection.insert({x: 1}));
+            assert.writeOK(writableCollection.insert({_id: 0, x: 1}));
         },
         exec: function(readableCollection) {
             // Test that insert fails.
-            assert.writeError(readableCollection.insert({x: 2}));
+            assert.writeErrorWithCode(
+                readableCollection.insert({x: 2}),
+                ErrorCodes.IllegalOperation,
+                "Expected insert to fail because database is in read-only mode");
 
             // Test that delete fails.
-            assert.writeError(readableCollection.remove({x: 1}));
+            assert.writeErrorWithCode(
+                readableCollection.remove({x: 1}),
+                ErrorCodes.IllegalOperation,
+                "Expected remove to fail because database is in read-only mode");
 
             // Test that update fails.
-            assert.writeError(readableCollection.update({x: 1}, {$inc: {x: 1}}));
+            assert.writeErrorWithCode(
+                readableCollection.update({_id: 0}, {$inc: {x: 1}}),
+                ErrorCodes.IllegalOperation,
+                "Expected update to fail because database is in read-only mode");
         }
     };
 }());
