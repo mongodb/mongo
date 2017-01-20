cbe53c003584cae8efad1715aeac181c8f8d2eb7
Judah Schvimer <judah@mongodb.com>
2017-01-20 10:08:50 -0500

    SERVER-27543 send OplogQueryMetadata with OplogFetcher queries
    (cherry picked from commit 859dfb093328ae9129f18952df4f25b123977a38)

--- src/mongo/db/repl/replication_coordinator_impl.h
+++ src/mongo/db/repl/replication_coordinator_impl.h
@@ -65,6 +65,7 @@ struct ConnectionPoolStats;
 }  // namespace executor
 
 namespace rpc {
+class OplogQueryMetadata;
 class ReplSetMetadata;
 }  // namespace rpc
 
@@ -276,7 +277,8 @@ public:
                                               const ReplSetRequestVotesArgs& args,
                                               ReplSetRequestVotesResponse* response) override;
 
-    virtual void prepareReplMetadata(const OpTime& lastOpTimeFromClient,
+    virtual void prepareReplMetadata(const BSONObj& metadataRequestObj,
+                                     const OpTime& lastOpTimeFromClient,
                                      BSONObjBuilder* builder) const override;
 
     virtual Status processHeartbeatV1(const ReplSetHeartbeatArgsV1& args,
@@ -574,6 +576,11 @@ private:
      * Returns the _writeConcernMajorityJournalDefault of our current _rsConfig.
      */
     bool getWriteConcernMajorityShouldJournal_inlock() const;
+
+    /**
+     * Returns the OpTime of the current committed snapshot, if one exists.
+     */
+    OpTime _getCurrentCommittedSnapshotOpTime_inlock() const;
 
     /**
      * Helper method that removes entries from _slaveInfo if they correspond to a node
@@ -1004,6 +1011,17 @@ private:
      */
     EventHandle _processReplSetMetadata_incallback(const rpc::ReplSetMetadata& replMetadata,
                                                    bool advanceCommitPoint);
+
+    /**
+     * Prepares a metadata object for ReplSetMetadata.
+     */
+    void _prepareReplSetMetadata_inlock(const OpTime& lastOpTimeFromClient,
+                                        BSONObjBuilder* builder) const;
+
+    /**
+     * Prepares a metadata object for OplogQueryMetadata.
+     */
+    void _prepareOplogQueryMetadata_inlock(BSONObjBuilder* builder) const;
 
     /**
      * Blesses a snapshot to be used for new committed reads.
