c4c75ecf2e797e8c11279e411df952dd69c72b98
Matthew Russotto <matthew.russotto@10gen.com>
2017-02-14 15:34:43 -0500

    SERVER-23225 Create a jstest to ensure readConcern "majority" is disallowed with protocol version 0.

---
+++ jstests/replsets/read_concern_majority_pv0_disallowed.js
@@ -0,0 +1,25 @@
+// Ensuring that readConcern: "majority" is not allowed with protocolVersion: 0.
+
+(function() {
+    "use strict";
+    load("jstests/replsets/rslib.js");  // For startSetIfSupportsReadMajority.
+
+    var replTest = new ReplSetTest(
+        {"nodes": 1, "nodeOptions": {"enableMajorityReadConcern": ""}, "protocolVersion": 0});
+    if (!startSetIfSupportsReadMajority(replTest)) {
+        jsTest.log("skipping test since storage engine doesn't support committed reads");
+        return;
+    }
+    replTest.initiate();
+
+    var primary = replTest.getPrimary();
+    var coll = primary.getCollection("test.test_coll");
+    assert.writeOK(coll.insert({_id: 1, data: "read_concern_majority_pv0_disallowed"},
+                               {"w": "majority", "wtimeout": replTest.kDefaultTimeoutMS}));
+
+    // A read concern of majority should be disallowed because protocol version is 0.
+    assert.commandFailedWithCode(coll.runCommand("find", {"readConcern": {"level": "majority"}}),
+                                 ErrorCodes.ReadConcernMajorityNotEnabled);
+
+    replTest.stopSet();
+}());
