89cc7047e7c5a7df0f2179821bf38b5ced2fd6c4
Eddie Louie <eddie.louie@mongodb.com>
2017-02-16 12:57:36 -0500

    SERVER-27876 Exclude stacks that are duplicates when dumping stacks on Windows in hang_analyzer.py

--- buildscripts/hang_analyzer.py
+++ buildscripts/hang_analyzer.py
@@ -128,7 +128,7 @@ class WindowsDumper(object):
             ".reload",  # Reload symbols
             "!peb",     # Dump current exe, & environment variables
             "lm",       # Dump loaded modules
-            "~* kp 100", # Dump All Threads with function arguments
+            "!uniqstack -pn", # Dump All unique Threads with function arguments
             "!cs -l",   # Dump all locked critical sections
             ".dump /ma /u dump_" + process_name + "." + str(pid) + "." + self.get_dump_ext() if take_dump else "",
                         # Dump to file, dump_<process name>_<time stamp>_<pid in hex>.<pid>.mdmp
