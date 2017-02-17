e52c6669b07d2ab8057fe1eacf86335c29281e79
Benety Goh <benety@mongodb.com>
2017-02-17 11:47:15 -0500

    SERVER-28037 Raise default writeConcern wtimeout in db.js to 10 minutes
    (cherry picked from commit e95a52865cfc9c0700e284104ec0443cc63b3408)

--- src/mongo/shell/db.js
+++ src/mongo/shell/db.js
@@ -1228,7 +1228,7 @@ var DB;
     /////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    var _defaultWriteConcern = {w: 'majority', wtimeout: 5 * 60 * 1000};
+    var _defaultWriteConcern = {w: 'majority', wtimeout: 10 * 60 * 1000};
 
     function getUserObjString(userObj) {
         var pwd = userObj.pwd;
