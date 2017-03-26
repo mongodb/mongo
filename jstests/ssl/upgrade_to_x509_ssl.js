710cefca98db7c8f7736a79cebca855b99509235
Benety Goh <benety@mongodb.com>
2017-03-26 07:12:52 -0400

    SERVER-28376 fixed jstests/ssl and jstests/sslSpecial tests to not require node 0 as primary

--- jstests/ssl/upgrade_to_x509_ssl.js
+++ jstests/ssl/upgrade_to_x509_ssl.js
@@ -46,8 +46,11 @@ var rst = new ReplSetTest({
     nodeOptions: opts,
 });
 rst.startSet();
-rst.initiate(Object.extend(rst.getReplSetConfig(),
-                           {writeConcernMajorityJournalDefault: wcMajorityJournalDefault}));
+
+// ReplSetTest.initiate() requires all nodes to be to be authorized to run replSetGetStatus.
+// TODO(SERVER-14017): Remove this in favor of using initiate() everywhere.
+rst.initiateWithAnyNodeAsPrimary(Object.extend(
+    rst.getReplSetConfig(), {writeConcernMajorityJournalDefault: wcMajorityJournalDefault}));
 
 // Connect to master and do some basic operations
 var rstConn1 = rst.getPrimary();
