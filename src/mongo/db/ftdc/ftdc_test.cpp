145be8c08243566275b86eefa8939f05eb823173
Mark Benvenuto <mark.benvenuto@mongodb.com>
2017-04-17 17:59:05 -0400

    SERVER-25868 Incomplete FTDC files on Windows
    (cherry picked from commit b5277068ff66e7fe2b2a98144aa6993ca6e17f5c)

--- src/mongo/db/ftdc/ftdc_test.cpp
+++ src/mongo/db/ftdc/ftdc_test.cpp
@@ -58,6 +58,8 @@ void ValidateDocumentList(const boost::filesystem::path& p, const std::vector<BS
         sw = reader.hasNext();
     }
 
+    ASSERT_OK(sw);
+
     ValidateDocumentList(list, docs);
 }
 
