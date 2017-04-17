36c7a372530f98932c905d6cf6bbba913675a24f
Mark Benvenuto <mark.benvenuto@mongodb.com>
2017-04-17 17:59:04 -0400

    SERVER-24610 Refactor System Stats Collectors
    (cherry picked from commit aca68d91a7226a14cfdc73171eb97c4922559940)

--- src/mongo/db/ftdc/ftdc_system_stats_windows.cpp
+++ src/mongo/db/ftdc/ftdc_system_stats_windows.cpp
@@ -26,39 +26,12 @@
  * then also delete it in the license file.
  */
 
-#include <string>
+#include "mongo/platform/basic.h"
 
-#include "mongo/base/status.h"
-#include "mongo/bson/bsonobjbuilder.h"
-#include "mongo/db/ftdc/controller.h"
-#include "mongo/db/ftdc/controller.h"
+#include "mongo/db/ftdc/ftdc_system_stats.h"
 
 namespace mongo {
 
-class FTDCController;
-
-/**
- * Base class for system metrics collectors. Sets collector name to a common name all system metrics
- * collectors to use.
- */
-class SystemMetricsCollector : public FTDCCollectorInterface {
-public:
-    std::string name() const final;
-
-protected:
-    /**
-     * Convert any errors we see into BSON for the user to see in the final FTDC document. It is
-     * acceptable for the collector to fail, but we do not want to shutdown the FTDC loop because
-     * of it. We assume that the BSONBuilder is not corrupt on non-OK Status but nothing else with
-     * regards to the final document output.
-     */
-    static void processStatusErrors(Status s, BSONObjBuilder* builder);
-};
-
-
-/**
- * Install a system metrics collector if it exists as a periodic collector.
- */
-void installSystemMetricsCollector(FTDCController* controller);
+void installSystemMetricsCollector(FTDCController* controller) {}
 
 }  // namespace mongo
