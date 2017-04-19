dff28119fc6dec90eae1de08ee8a8d76cf1c1be6
Keith Bostic <keith.bostic@mongodb.com>
2017-04-19 02:49:39 -0400

    WT-3288 fix error codes for event_handler to be consistent in file operations (#3393)
    `__wt_formatmessage()` takes a raw Windows error, `__wt_err()` takes the mapped Windows error and we want to return the mapped Windows error. Use a new pattern: `windows_error` is the raw Windows error, `ret` is the mapped Windows error.

--- src/support/err.c
+++ src/support/err.c
@@ -144,6 +144,8 @@ __wt_event_handler_set(WT_SESSION_IMPL *session, WT_EVENT_HANDLER *handler)
 			handler->handle_message = __handle_message_default;
 		if (handler->handle_progress == NULL)
 			handler->handle_progress = __handle_progress_default;
+		if (handler->handle_close == NULL)
+			handler->handle_close = __handle_close_default;
 	}
 
 	session->event_handler = handler;
