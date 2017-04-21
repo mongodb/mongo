4171efb989ba2b7d5d1d1d77a76840a7a155d571
Spencer T Brody <spencer@mongodb.com>
2017-04-21 14:56:49 -0400

    SERVER-28186 Increase slaveDelay in repl10.js test

--- jstests/master_slave/repl10.js
+++ jstests/master_slave/repl10.js
@@ -1,40 +1,43 @@
 // Test slave delay
+(function() {
+    "use strict";
 
-var baseName = "jstests_repl10test";
+    var baseName = "jstests_repl10test";
 
-soonCount = function(count) {
-    assert.soon(function() {
-        //                print( "check count" );
-        //                print( "count: " + s.getDB( baseName ).z.find().count() );
-        return s.getDB(baseName).a.find().count() == count;
-    });
-};
+    var soonCount = function(s, count) {
+        assert.soon(function() {
+            //                print( "check count" );
+            //                print( "count: " + s.getDB( baseName ).z.find().count() );
+            return s.getDB(baseName).a.find().count() == count;
+        });
+    };
 
-doTest = function(signal) {
+    var doTest = function(signal) {
 
-    rt = new ReplTest("repl10tests");
+        var rt = new ReplTest("repl10tests");
 
-    m = rt.start(true);
-    s = rt.start(false, {"slavedelay": "10"});
+        var m = rt.start(true);
+        var s = rt.start(false, {"slavedelay": "30"});
 
-    am = m.getDB(baseName).a;
+        var am = m.getDB(baseName).a;
 
-    am.save({i: 1});
+        am.save({i: 1});
 
-    soonCount(1);
+        soonCount(s, 1);
 
-    am.save({i: 2});
-    assert.eq(2, am.count());
-    sleep(3000);
-    assert.eq(1, s.getDB(baseName).a.count());
+        am.save({i: 2});
+        assert.eq(2, am.count());
+        sleep(2000);
+        assert.eq(1, s.getDB(baseName).a.count());
 
-    soonCount(2);
+        soonCount(s, 2);
 
-    rt.stop();
-};
+        rt.stop();
+    };
 
-print("repl10.js dotest(15)");
-doTest(15);  // SIGTERM
-print("repl10.js dotest(15)");
-doTest(9);  // SIGKILL
-print("repl10.js SUCCESS");
+    print("repl10.js dotest(15)");
+    doTest(15);  // SIGTERM
+    print("repl10.js dotest(15)");
+    doTest(9);  // SIGKILL
+    print("repl10.js SUCCESS");
+}());
