xiexiaoy/eloqdoc

SERVER-28357 Do not enforce quota on starting MMAPv1 compact

src/mongo/db/storage/mmap_v1/record_store_v1_simple.cpp
@@ -453,7 +453,8 @@ Status SimpleRecordStoreV1::compact(OperationContext* opCtx,
         _details->setLastExtentSize(opCtx, 0);
 
         // create a new extent so new records go there
-        increaseStorageSize(opCtx, _details->lastExtentSize(opCtx), true);
+        const bool enforceQuota = false;
+        increaseStorageSize(opCtx, _details->lastExtentSize(opCtx), enforceQuota);
         wunit.commit();
     }
 

mongodb/mongo

(cherry picked from commit 914bbbd26a686e032fdddec964b109ea78c6e6f6)
Conflicts:
	src/mongo/db/storage/mmap_v1/record_store_v1_simple.cpp

src/mongo/db/storage/mmap_v1/record_store_v1_simple.cpp
@@ -453,7 +453,8 @@ Status SimpleRecordStoreV1::compact(OperationContext* txn,
         _details->setLastExtentSize(txn, 0);
 
         // create a new extent so new records go there
-        increaseStorageSize(txn, _details->lastExtentSize(txn), true);
+        const bool enforceQuota = false;
+        increaseStorageSize(txn, _details->lastExtentSize(txn), enforceQuota);
         wunit.commit();
     }
 