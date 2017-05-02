fff318ff19aa01656d33fbe079b7dbb1d98c9f87
Andrew Morrow <acm@mongodb.com>
2017-05-01 22:33:35 -0400

    SERVER-29036 The IDL compiler invocation must depend on the IDL compiler sources

--- site_scons/site_tools/idl_tool.py
+++ site_scons/site_tools/idl_tool.py
@@ -53,6 +53,8 @@ def idl_scanner(node, env, path):
     deps_list = deps_str.splitlines()
 
     nodes_deps_list = [ env.File(d) for d in deps_list]
+    nodes_deps_list.extend(env.Glob('#buildscripts/idl/*.py'))
+    nodes_deps_list.extend(env.Glob('#buildscripts/idl/idl/*.py'))
 
     return nodes_deps_list
 
