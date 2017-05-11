32d9aecda006bac401645679c04fcaa6d855e213
Keith Bostic <keith.bostic@mongodb.com>
2017-05-11 07:52:20 -0400

    WT-3312 encryption btree configuration test (#3413)
    * WT-3312 encryption btree configuration test
    
    Don't check for the metadata file explicitly (it implies we should check
    other special files too, like the LAS file). If there's no explicit
    encryption argument, default to the connection's value, that includes
    the metadata file and the LAS file.
    
    * Remove my added line in the comment, it didn't add anything useful.

--- src/btree/bt_handle.c
+++ src/btree/bt_handle.c
@@ -418,15 +418,13 @@ __btree_conf(WT_SESSION_IMPL *session, WT_CKPT *ckpt)
 	WT_RET(__wt_compressor_config(session, &cval, &btree->compressor));
 
 	/*
-	 * We do not use __wt_config_gets_none here because "none"
-	 * and the empty string have different meanings.  The
-	 * empty string means inherit the system encryption setting
-	 * and "none" means this table is in the clear even if the
-	 * database is encrypted.  If this is the metadata handle
-	 * always inherit from the connection.
+	 * We do not use __wt_config_gets_none here because "none" and the empty
+	 * string have different meanings. The empty string means inherit the
+	 * system encryption setting and "none" means this table is in the clear
+	 * even if the database is encrypted.
 	 */
 	WT_RET(__wt_config_gets(session, cfg, "encryption.name", &cval));
-	if (WT_IS_METADATA(btree->dhandle) || cval.len == 0)
+	if (cval.len == 0)
 		btree->kencryptor = conn->kencryptor;
 	else if (WT_STRING_MATCH("none", cval.str, cval.len))
 		btree->kencryptor = NULL;
