1055dafd8d1693d48200fb492ef0912e33b60bbc
Spencer Jackson <spencer.jackson@mongodb.com>
2017-05-18 12:30:58 -0400

    SERVER-29259: Ensure creation of authorization indexes
    (cherry picked from commit 763396a466b74548bcf76b23482247b668d04db5)

---
+++ jstests/auth/system_authorization_indexes.js
@@ -0,0 +1,63 @@
+/** Ensure that authorization system collections' indexes are correctly generated.
+ */
+
+(function() {
+    let conn = MongoRunner.runMongod({smallfiles: ""});
+    let db = conn.getDB("admin");
+
+    // TEST: User and role collections start off with no indexes
+    assert.eq(0, db.system.users.getIndexes().length);
+    assert.eq(0, db.system.roles.getIndexes().length);
+
+    // TEST: User and role creation generates indexes
+    db.createUser({user: "user", pwd: "pwd", roles: []});
+    assert.eq(2, db.system.users.getIndexes().length);
+
+    db.createRole({role: "role", privileges: [], roles: []});
+    assert.eq(2, db.system.roles.getIndexes().length);
+
+    // TEST: Destroying admin.system.users index and restarting will recreate it
+    assert.commandWorked(db.system.users.dropIndexes());
+    MongoRunner.stopMongod(conn);
+    conn = MongoRunner.runMongod({restart: conn, cleanData: false});
+    db = conn.getDB("admin");
+    assert.eq(2, db.system.users.getIndexes().length);
+    assert.eq(2, db.system.roles.getIndexes().length);
+
+    // TEST: Destroying admin.system.roles index and restarting will recreate it
+    assert.commandWorked(db.system.roles.dropIndexes());
+    MongoRunner.stopMongod(conn);
+    conn = MongoRunner.runMongod({restart: conn, cleanData: false});
+    db = conn.getDB("admin");
+    assert.eq(2, db.system.users.getIndexes().length);
+    assert.eq(2, db.system.roles.getIndexes().length);
+
+    // TEST: Destroying both authorization indexes and restarting will recreate them
+    assert.commandWorked(db.system.users.dropIndexes());
+    assert.commandWorked(db.system.roles.dropIndexes());
+    MongoRunner.stopMongod(conn);
+    conn = MongoRunner.runMongod({restart: conn, cleanData: false});
+    db = conn.getDB("admin");
+    assert.eq(2, db.system.users.getIndexes().length);
+    assert.eq(2, db.system.roles.getIndexes().length);
+
+    // TEST: Destroying the admin.system.users index and restarting will recreate it, even if
+    // admin.system.roles does not exist
+    db.dropDatabase();
+    db.createUser({user: "user", pwd: "pwd", roles: []});
+    assert.commandWorked(db.system.users.dropIndexes());
+    MongoRunner.stopMongod(conn);
+    conn = MongoRunner.runMongod({restart: conn, cleanData: false});
+    db = conn.getDB("admin");
+    assert.eq(2, db.system.users.getIndexes().length);
+
+    // TEST: Destroying the admin.system.roles index and restarting will recreate it, even if
+    // admin.system.users does not exist
+    db.dropDatabase();
+    db.createRole({role: "role", privileges: [], roles: []});
+    assert.commandWorked(db.system.roles.dropIndexes());
+    MongoRunner.stopMongod(conn);
+    conn = MongoRunner.runMongod({restart: conn, cleanData: false});
+    db = conn.getDB("admin");
+    assert.eq(2, db.system.roles.getIndexes().length);
+})();
