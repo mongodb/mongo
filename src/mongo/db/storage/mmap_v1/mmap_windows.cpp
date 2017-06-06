6f7fd7318d61bd145bb75a9a0a5d35387d2a6b9f
Kim Tao <kim.tao@mongodb.com>
2017-06-06 12:56:42 -0400

    SERVER-29003 Fix unsigned int warning

--- src/mongo/db/storage/mmap_v1/mmap_windows.cpp
+++ src/mongo/db/storage/mmap_v1/mmap_windows.cpp
@@ -194,7 +194,7 @@ void* MemoryMappedFile::map(OperationContext* opCtx,
     filename[255] = 0;
     {
         size_t len = strlen(filename);
-        for (size_t i = len - 1; i >= 0; i--) {
+        for (int i = len - 1; i >= 0; i--) {
             if (filename[i] == '/' || filename[i] == '\\')
                 break;
 
