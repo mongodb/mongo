50fedc523a9b55714d58617ca290d3cef3560dd6
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2017-06-07 10:11:51 -0400

    SERVER-24735 Correct logger to use STORAGE instead of INDEX

--- src/mongo/db/storage/mmap_v1/catalog/namespace_index.cpp
+++ src/mongo/db/storage/mmap_v1/catalog/namespace_index.cpp
@@ -28,7 +28,7 @@
  *    it in the license file.
  */
 
-#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kIndex
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kStorage
 
 #include "mongo/platform/basic.h"
 
