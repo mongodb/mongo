xiexiaoy/eloqdoc

SERVER-29011 Don't use side sessions during compacts in the WT KV Engine

src/mongo/db/storage/wiredtiger/wiredtiger_index.cpp
@@ -463,8 +463,8 @@ Status WiredTigerIndex::initAsEmpty(OperationContext* opCtx) {
 Status WiredTigerIndex::compact(OperationContext* opCtx) {
     WiredTigerSessionCache* cache = WiredTigerRecoveryUnit::get(opCtx)->getSessionCache();
     if (!cache->isEphemeral()) {
-        UniqueWiredTigerSession session = cache->getSession();
-        WT_SESSION* s = session->getSession();
+        WT_SESSION* s = WiredTigerRecoveryUnit::get(opCtx)->getSession(opCtx)->getSession();
+        opCtx->recoveryUnit()->abandonSnapshot();
         int ret = s->compact(s, uri().c_str(), "timeout=0");
         invariantWTOK(ret);
     }

src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
@@ -1530,8 +1530,8 @@ Status WiredTigerRecordStore::compact(OperationContext* opCtx,
                                       CompactStats* stats) {
     WiredTigerSessionCache* cache = WiredTigerRecoveryUnit::get(opCtx)->getSessionCache();
     if (!cache->isEphemeral()) {
-        UniqueWiredTigerSession session = cache->getSession();
-        WT_SESSION* s = session->getSession();
+        WT_SESSION* s = WiredTigerRecoveryUnit::get(opCtx)->getSession(opCtx)->getSession();
+        opCtx->recoveryUnit()->abandonSnapshot();
         int ret = s->compact(s, getURI().c_str(), "timeout=0");
         invariantWTOK(ret);
     }

mongodb/mongo

(cherry picked from commit 584d4a6a25ce56b07f13247b3ce7fe298b4a111e)

src/mongo/db/storage/wiredtiger/wiredtiger_index.cpp
@@ -457,8 +457,8 @@ Status WiredTigerIndex::initAsEmpty(OperationContext* txn) {
 Status WiredTigerIndex::compact(OperationContext* txn) {
     WiredTigerSessionCache* cache = WiredTigerRecoveryUnit::get(txn)->getSessionCache();
     if (!cache->isEphemeral()) {
-        UniqueWiredTigerSession session = cache->getSession();
-        WT_SESSION* s = session->getSession();
+        WT_SESSION* s = WiredTigerRecoveryUnit::get(txn)->getSession(txn)->getSession();
+        txn->recoveryUnit()->abandonSnapshot();
         int ret = s->compact(s, uri().c_str(), "timeout=0");
         invariantWTOK(ret);
     }

src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
@@ -1524,8 +1524,8 @@ Status WiredTigerRecordStore::compact(OperationContext* txn,
                                       CompactStats* stats) {
     WiredTigerSessionCache* cache = WiredTigerRecoveryUnit::get(txn)->getSessionCache();
     if (!cache->isEphemeral()) {
-        UniqueWiredTigerSession session = cache->getSession();
-        WT_SESSION* s = session->getSession();
+        WT_SESSION* s = WiredTigerRecoveryUnit::get(txn)->getSession(txn)->getSession();
+        txn->recoveryUnit()->abandonSnapshot();
         int ret = s->compact(s, getURI().c_str(), "timeout=0");
         invariantWTOK(ret);
     }