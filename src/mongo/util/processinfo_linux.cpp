2c895be9d7f506432b8377bbbde357452a92e9d2
Tyler Kaye <tyler.kaye@mongodb.com>
2017-06-30 11:00:07 -0400

    SERVER-28693 Use getrusage() to report the number of major page faults on linux

--- src/mongo/util/processinfo_linux.cpp
+++ src/mongo/util/processinfo_linux.cpp
@@ -38,6 +38,8 @@
 #include <sched.h>
 #include <stdio.h>
 #include <sys/mman.h>
+#include <sys/resource.h>
+#include <sys/time.h>
 #include <sys/utsname.h>
 #include <unistd.h>
 #ifdef __UCLIBC__
@@ -451,11 +453,12 @@ double ProcessInfo::getSystemMemoryPressurePercentage() {
 }
 
 void ProcessInfo::getExtraInfo(BSONObjBuilder& info) {
-    LinuxProc p(_pid);
-    if (p._maj_flt <= std::numeric_limits<long long>::max())
-        info.appendNumber("page_faults", static_cast<long long>(p._maj_flt));
+    struct rusage ru;
+    getrusage(RUSAGE_SELF, &ru);
+    if (ru.ru_majflt <= std::numeric_limits<long long>::max())
+        info.appendNumber("page_faults", static_cast<long long>(ru.ru_majflt));
     else
-        info.appendNumber("page_faults", static_cast<double>(p._maj_flt));
+        info.appendNumber("page_faults", static_cast<double>(ru.ru_majflt));
 }
 
 /**
