3579adf6c8b96d26898af9fe3cc5eb9677f75632
David Hows <howsdav@gmail.com>
2017-07-19 17:37:59 +1000

    WT-3438 Don't tune eviction thread count when the count is fixed (#3519)
    (cherry picked from commit 6173a98979715ed727c432c1a31da64ea8a37048)
    Signed-off-by: Alex Gorrod <alexander.gorrod@mongodb.com>

--- src/evict/evict_lru.c
+++ src/evict/evict_lru.c
@@ -941,6 +941,13 @@ __evict_tune_workers(WT_SESSION_IMPL *session)
 	conn = S2C(session);
 	cache = conn->cache;
 
+	/*
+	 * If we have a fixed number of eviction threads, there is no value in
+	 * calculating if we should do any tuning.
+	 */
+       if (conn->evict_threads_max == conn->evict_threads_min)
+		return (0);
+
 	WT_ASSERT(session, conn->evict_threads.threads[0]->session == session);
 	pgs_evicted_cur = pgs_evicted_persec_cur = 0;
 
