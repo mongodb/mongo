645172dfe76a2a25b6c19f3ba6086c8605c617ab
Yves Duhem <yves.duhem@mongodb.com>
2017-07-25 16:08:31 -0400

    SERVER-30308 Handle absent test_flags in combined_resmoke_args()

--- buildscripts/ciconfig/evergreen.py
+++ buildscripts/ciconfig/evergreen.py
@@ -185,7 +185,11 @@ class VariantTask(Task):
     def combined_resmoke_args(self):
         """Return the combined resmoke arguments resulting from the concatenation of the task's
          resmoke_args parameter and the variant's test_flags parameter."""
-        return "{} {}".format(self.resmoke_args, self.variant.test_flags)
+        test_flags = self.variant.test_flags
+        if test_flags:
+            return "{} {}".format(self.resmoke_args, test_flags)
+        else:
+            return self.resmoke_args
 
 
 class ResmokeArgs(object):
