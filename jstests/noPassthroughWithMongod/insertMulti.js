00cf113dc5994b610e72607909fca9f839717e6c
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2017-07-27 16:30:45 -0400

    SERVER-28752 Lower the number of inserted entries in insertMulti.js

--- jstests/noPassthroughWithMongod/insertMulti.js
+++ jstests/noPassthroughWithMongod/insertMulti.js
@@ -43,8 +43,11 @@
     t.drop();
     var doc = makeDocument(16 * 1024);
     var docs = [];
-    for (var i = 0; i < 1500; i++)
+    for (var i = 0; i < 1000; i++)
         docs.push(Object.extend({}, doc));
     t.insert(docs);
+    assert.eq(null, t.getDB().getLastError());
     assert.eq(t.count(), docs.length);
+
+    t.drop();
 })();
