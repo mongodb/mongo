95e05a742e588987aab6e4cef65817c1c3fb5cdc
Nathan Myers <nathan.myers@10gen.com>
2017-08-07 11:56:20 -0400

    SERVER-30531 Remove blacklisting of nonexistent tests

--- jstests/concurrency/fsm_all_sharded_replication.js
+++ jstests/concurrency/fsm_all_sharded_replication.js
@@ -17,7 +17,6 @@ var blacklist = [
     'agg_match.js',
     'count.js',
     'count_limit_skip.js',
-    'count_noindex.js',
 
     // $lookup and $graphLookup are not supported on sharded collections.
     'agg_graph_lookup.js',
@@ -51,7 +50,6 @@ var blacklist = [
     'compact_simultaneous_padding_bytes.js',  // compact can only be run against a mongod
     'convert_to_capped_collection.js',        // convertToCapped can't be run on mongos processes
     'convert_to_capped_collection_index.js',  // convertToCapped can't be run on mongos processes
-    'findAndModify_mixed_queue.js',           // findAndModify requires a shard key
     'findAndModify_mixed_queue_unindexed.js',   // findAndModify requires a shard key
     'findAndModify_remove_queue.js',            // remove cannot be {} for findAndModify
     'findAndModify_remove_queue_unindexed.js',  // findAndModify requires a shard key
