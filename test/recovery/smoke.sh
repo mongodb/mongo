7ed46d495384165a01aeefc8127bf30198ed2384
sueloverso <sue@mongodb.com>
2017-08-09 02:08:21 -0400

    WT-3452 Add timestamp-based recovery test. (#3554)

--- test/recovery/smoke.sh
+++ test/recovery/smoke.sh
@@ -8,4 +8,8 @@ $TEST_WRAPPER ./random-abort -t 10 -T 5
 $TEST_WRAPPER ./random-abort -m -t 10 -T 5
 $TEST_WRAPPER ./random-abort -C -t 10 -T 5
 $TEST_WRAPPER ./random-abort -C -m -t 10 -T 5
+$TEST_WRAPPER ./timestamp-abort -t 10 -T 5
+$TEST_WRAPPER ./timestamp-abort -m -t 10 -T 5
+$TEST_WRAPPER ./timestamp-abort -C -t 10 -T 5
+$TEST_WRAPPER ./timestamp-abort -C -m -t 10 -T 5
 $TEST_WRAPPER ./truncated-log
