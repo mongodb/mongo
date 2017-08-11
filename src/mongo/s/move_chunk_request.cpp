ac278086e705b289a784e4f40fe7b851b69a7b57
Esha Maharishi <esha.maharishi@mongodb.com>
2017-08-11 12:29:24 -0400

    SERVER-30183 ensure a moveChunk that joins the active moveChunk has the same waitForDelete option

--- src/mongo/s/move_chunk_request.cpp
+++ src/mongo/s/move_chunk_request.cpp
@@ -175,7 +175,8 @@ bool MoveChunkRequest::operator==(const MoveChunkRequest& other) const {
         return false;
     if (_range != other._range)
         return false;
-
+    if (_waitForDelete != other._waitForDelete)
+        return false;
     return true;
 }
 
