03055999749ed577af14d5d9cc969f616b4c3196
Max Hirschhorn <max.hirschhorn@mongodb.com>
2017-08-30 11:15:13 -0400

    SERVER-30686 Add retryable_writes_jscore_passthrough_WT task.

--- jstests/core/currentop.js
+++ jstests/core/currentop.js
@@ -16,11 +16,20 @@
     // Wait until the write appears in the currentOp output reporting that it is waiting for a lock.
     assert.soon(
         function() {
-            return db.currentOp({
-                         ns: coll.getFullName(),
-                         "locks.Global": "w",
-                         "waitingForLock": true,
-                     }).inprog.length === 1;
+            const ops = db.currentOp({
+                $and: [
+                    {"locks.Global": "w", waitingForLock: true},
+                    // Depending on whether CurOp::setNS_inlock() has been called, the "ns" field
+                    // may either be the full collection name or the command namespace.
+                    {
+                      $or: [
+                          {ns: coll.getFullName()},
+                          {ns: db.$cmd.getFullName(), "command.insert": coll.getName()}
+                      ]
+                    }
+                ]
+            });
+            return ops.inprog.length === 1;
         },
         function() {
             return "Failed to find blocked insert in currentOp() output: " + tojson(db.currentOp());
