191c6c43650c5655a530bdd0228cd4370975416e
Spencer Jackson <spencer.jackson@mongodb.com>
2017-09-06 18:36:10 -0400

    SERVER-30971: Own "scope" BSONObj in mapReduce
    (cherry picked from commit f668265318237dc75708a20254e695869487d884)
    (cherry picked from commit 876ebee8c7dd0e2d992f36a848ff4dc50ee6603e)

--- jstests/core/mr4.js
+++ jstests/core/mr4.js
@@ -9,7 +9,7 @@ t.save({x: 4, tags: ["b", "c"]});
 
 m = function() {
     this.tags.forEach(function(z) {
-        emit(z, {count: xx});
+        emit(z, {count: xx.val});
     });
 };
 
@@ -23,7 +23,7 @@ r = function(key, values) {
     };
 };
 
-res = t.mapReduce(m, r, {out: "mr4_out", scope: {xx: 1}});
+res = t.mapReduce(m, r, {out: "mr4_out", scope: {xx: {val: 1}}});
 z = res.convertToSingleObject();
 
 assert.eq(3, Object.keySet(z).length, "A1");
@@ -33,7 +33,7 @@ assert.eq(3, z.c.count, "A4");
 
 res.drop();
 
-res = t.mapReduce(m, r, {scope: {xx: 2}, out: "mr4_out"});
+res = t.mapReduce(m, r, {scope: {xx: {val: 2}}, out: "mr4_out"});
 z = res.convertToSingleObject();
 
 assert.eq(3, Object.keySet(z).length, "A1");
