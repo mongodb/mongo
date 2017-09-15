577f6c2677eb1384e5a50b6829bfa2ad7665e3a1
Henrik Edin <henrik.edin@mongodb.com>
2017-09-15 17:08:10 -0400

    SERVER-30482 Verify and enforce good ordering for the values for the sharding task executor refresh and host timeouts.
    (cherry picked from commit 8c50e87a82afdb7852fbac32e9bb4dbe7b6f8030)

--- src/mongo/s/sharding_initialization.cpp
+++ src/mongo/s/sharding_initialization.cpp
@@ -193,6 +193,30 @@ Status initializeGlobalShardingState(OperationContext* txn,
         ? Milliseconds(ShardingTaskExecutorPoolRefreshTimeoutMS)
         : ConnectionPool::kDefaultRefreshTimeout;
 
+    if (connPoolOptions.refreshRequirement <= connPoolOptions.refreshTimeout) {
+        auto newRefreshTimeout = connPoolOptions.refreshRequirement - Milliseconds(1);
+        warning() << "ShardingTaskExecutorPoolRefreshRequirementMS ("
+                  << connPoolOptions.refreshRequirement
+                  << ") set below ShardingTaskExecutorPoolRefreshTimeoutMS ("
+                  << connPoolOptions.refreshTimeout
+                  << "). Adjusting ShardingTaskExecutorPoolRefreshTimeoutMS to "
+                  << newRefreshTimeout;
+        connPoolOptions.refreshTimeout = newRefreshTimeout;
+    }
+
+    if (connPoolOptions.hostTimeout <=
+        connPoolOptions.refreshRequirement + connPoolOptions.refreshTimeout) {
+        auto newHostTimeout =
+            connPoolOptions.refreshRequirement + connPoolOptions.refreshTimeout + Milliseconds(1);
+        warning() << "ShardingTaskExecutorPoolHostTimeoutMS (" << connPoolOptions.hostTimeout
+                  << ") set below ShardingTaskExecutorPoolRefreshRequirementMS ("
+                  << connPoolOptions.refreshRequirement
+                  << ") + ShardingTaskExecutorPoolRefreshTimeoutMS ("
+                  << connPoolOptions.refreshTimeout
+                  << "). Adjusting ShardingTaskExecutorPoolHostTimeoutMS to " << newHostTimeout;
+        connPoolOptions.hostTimeout = newHostTimeout;
+    }
+
     auto network =
         executor::makeNetworkInterface("NetworkInterfaceASIO-ShardRegistry",
                                        stdx::make_unique<ShardingNetworkConnectionHook>(),
