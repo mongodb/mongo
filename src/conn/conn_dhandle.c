3061e3e6e3a28b52eda60f010f8caa2374c988ce
sueloverso <sue@mongodb.com>
2017-09-20 02:16:38 -0400

    WT-3590 Keep data consistent if writes fail during a clean shutdown (#3666)
    * Add a regular checkpoint during close.
    
    * Add a test case showing trees out of sync when writes fail during close.
    
    * Make sure the metadata is flushed during clean shutdown.  A database-wide checkpoint can be complex, flush the metadata again afterwards to make it easy to detect that recovery can be skipped.
    
    * Do a "soft panic" if anything goes wrong during close.  Avoid aborting diagnostic builds, but make sure there are no further writes to disk (e.g., if the checkpoint fails).

--- src/conn/conn_dhandle.c
+++ src/conn/conn_dhandle.c
@@ -223,15 +223,14 @@ __wt_conn_dhandle_close(
 
 		/*
 		 * Mark the handle dead (letting the tree be discarded later) if
-		 * it's not already marked dead, our caller allows it, it's not
-		 * a final close, and it's not a memory-mapped tree. (We can't
-		 * mark memory-mapped tree handles dead because we close the
-		 * underlying file handle to allow the file to be removed and
-		 * memory-mapped trees contain pointers into memory that become
-		 * invalid if the mapping is closed.)
+		 * it's not already marked dead, and it's not a memory-mapped
+		 * tree. (We can't mark memory-mapped tree handles dead because
+		 * we close the underlying file handle to allow the file to be
+		 * removed and memory-mapped trees contain pointers into memory
+		 * that become invalid if the mapping is closed.)
 		 */
 		bm = btree->bm;
-		if (!discard && mark_dead && !final &&
+		if (!discard && mark_dead &&
 		    (bm == NULL || !bm->is_mapped(bm, session)))
 			marked_dead = true;
 
@@ -786,7 +785,7 @@ restart:
 
 		WT_WITH_DHANDLE(session, dhandle,
 		    WT_TRET(__wt_conn_dhandle_discard_single(
-		    session, true, false)));
+		    session, true, F_ISSET(conn, WT_CONN_PANIC))));
 		goto restart;
 	}
 
@@ -812,7 +811,7 @@ restart:
 	WT_TAILQ_SAFE_REMOVE_BEGIN(dhandle, &conn->dhqh, q, dhandle_tmp) {
 		WT_WITH_DHANDLE(session, dhandle,
 		    WT_TRET(__wt_conn_dhandle_discard_single(
-		    session, true, false)));
+		    session, true, F_ISSET(conn, WT_CONN_PANIC))));
 	} WT_TAILQ_SAFE_REMOVE_END
 
 	return (ret);
