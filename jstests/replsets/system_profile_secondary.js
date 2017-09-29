87b23ef60c1d3c32d98e5c38c598664223434a80
Geert Bosch <geert@mongodb.com>
2017-09-29 17:08:46 -0400

    SERVER-31018 Fix lint

--- jstests/replsets/system_profile_secondary.js
+++ jstests/replsets/system_profile_secondary.js
@@ -8,19 +8,18 @@
 
     var secondaryDB = rst.getSecondary().getDB('test');
 
-    jsTestLog('Enable profiling on the secondary')
-        assert.commandWorked(secondaryDB.runCommand({profile: 2}));
+    jsTestLog('Enable profiling on the secondary');
+    assert.commandWorked(secondaryDB.runCommand({profile: 2}));
 
-    jsTestLog('Perform a query that returns no results, but will get profiled.')
-        secondaryDB.doesntexist.find({})
-            .itcount();
+    jsTestLog('Perform a query that returns no results, but will get profiled.');
+    secondaryDB.doesntexist.find({}).itcount();
 
     let numProfileEntries = (coll) =>
         coll.getDB()
             .system.profile.find({op: 'query', ns: coll.getFullName(), nreturned: 0})
             .itcount();
 
-    jsTestLog('Check the query is in the profile and turn profiling off.')
-        assert.eq(numProfileEntries(secondaryDB.doesntexist), 1, 'expected a single profile entry');
+    jsTestLog('Check the query is in the profile and turn profiling off.');
+    assert.eq(numProfileEntries(secondaryDB.doesntexist), 1, 'expected a single profile entry');
     assert.commandWorked(secondaryDB.runCommand({profile: 0}));
 })();
