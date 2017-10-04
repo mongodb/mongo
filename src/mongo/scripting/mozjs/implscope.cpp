xiexiaoy/eloqdoc

SERVER-29651 Interrupts are caught in implscope functions or by the deadline monitor if neccessary.

src/mongo/scripting/mozjs/implscope.cpp
@@ -490,82 +490,81 @@ void MozJSImplScope::init(const BSONObj* data) {
     }
 }
 
-void MozJSImplScope::setNumber(const char* field, double val) {
-    MozJSEntry entry(this);
+template <typename ImplScopeFunction>
+auto MozJSImplScope::_runSafely(ImplScopeFunction&& functionToRun) -> decltype(functionToRun()) {
+    try {
+        MozJSEntry entry(this);
+        return functionToRun();
+    } catch (...) {
+        _error = _status.reason();
 
-    ObjectWrapper(_context, _global).setNumber(field, val);
+        // Clear the status state
+        auto status = std::move(_status);
+        uassertStatusOK(status);
+        throw;
+    }
+}
+
+void MozJSImplScope::setNumber(const char* field, double val) {
+    _runSafely([this, &field, &val] { ObjectWrapper(_context, _global).setNumber(field, val); });
 }
 
 void MozJSImplScope::setString(const char* field, StringData val) {
-    MozJSEntry entry(this);
-
-    ObjectWrapper(_context, _global).setString(field, val);
+    _runSafely([this, &field, &val] { ObjectWrapper(_context, _global).setString(field, val); });
 }
 
 void MozJSImplScope::setBoolean(const char* field, bool val) {
-    MozJSEntry entry(this);
-
-    ObjectWrapper(_context, _global).setBoolean(field, val);
+    _runSafely([this, &field, &val] { ObjectWrapper(_context, _global).setBoolean(field, val); });
 }
 
 void MozJSImplScope::setElement(const char* field, const BSONElement& e, const BSONObj& parent) {
-    MozJSEntry entry(this);
+    _runSafely([this, &field, &e, &parent] {
 
-    ObjectWrapper(_context, _global).setBSONElement(field, e, parent, false);
+        ObjectWrapper(_context, _global).setBSONElement(field, e, parent, false);
+    });
 }
 
 void MozJSImplScope::setObject(const char* field, const BSONObj& obj, bool readOnly) {
-    MozJSEntry entry(this);
+    _runSafely([this, &field, &obj, &readOnly] {
 
-    ObjectWrapper(_context, _global).setBSON(field, obj, readOnly);
+        ObjectWrapper(_context, _global).setBSON(field, obj, readOnly);
+    });
 }
 
 int MozJSImplScope::type(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).type(field);
+    return _runSafely([this, &field] { return ObjectWrapper(_context, _global).type(field); });
 }
 
 double MozJSImplScope::getNumber(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getNumber(field);
+    return _runSafely([this, &field] { return ObjectWrapper(_context, _global).getNumber(field); });
 }
 
 int MozJSImplScope::getNumberInt(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getNumberInt(field);
+    return _runSafely(
+        [this, &field] { return ObjectWrapper(_context, _global).getNumberInt(field); });
 }
 
 long long MozJSImplScope::getNumberLongLong(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getNumberLongLong(field);
+    return _runSafely(
+        [this, &field] { return ObjectWrapper(_context, _global).getNumberLongLong(field); });
 }
 
 Decimal128 MozJSImplScope::getNumberDecimal(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getNumberDecimal(field);
+    return _runSafely(
+        [this, &field] { return ObjectWrapper(_context, _global).getNumberDecimal(field); });
 }
 
 std::string MozJSImplScope::getString(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getString(field);
+    return _runSafely([this, &field] { return ObjectWrapper(_context, _global).getString(field); });
 }
 
 bool MozJSImplScope::getBoolean(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getBoolean(field);
+    return _runSafely(
+        [this, &field] { return ObjectWrapper(_context, _global).getBoolean(field); });
 }
 
 BSONObj MozJSImplScope::getObject(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getObject(field);
+    return _runSafely([this, &field] { return ObjectWrapper(_context, _global).getObject(field); });
 }
 
 void MozJSImplScope::newFunction(StringData raw, JS::MutableHandleValue out) {
@@ -641,17 +640,15 @@ ScriptingFunction MozJSImplScope::_createFunction(const char* raw) {
 }
 
 void MozJSImplScope::setFunction(const char* field, const char* code) {
-    MozJSEntry entry(this);
-
-    JS::RootedValue fun(_context);
-    _MozJSCreateFunction(code, &fun);
-    ObjectWrapper(_context, _global).setValue(field, fun);
+    _runSafely([this, &field, &code] {
+        JS::RootedValue fun(_context);
+        _MozJSCreateFunction(code, &fun);
+        ObjectWrapper(_context, _global).setValue(field, fun);
+    });
 }
 
 void MozJSImplScope::rename(const char* from, const char* to) {
-    MozJSEntry entry(this);
-
-    ObjectWrapper(_context, _global).rename(from, to);
+    _runSafely([this, &from, &to] { ObjectWrapper(_context, _global).rename(from, to); });
 }
 
 int MozJSImplScope::invoke(ScriptingFunction func,
@@ -763,15 +760,15 @@ bool MozJSImplScope::exec(StringData code,
 }
 
 void MozJSImplScope::injectNative(const char* field, NativeFunction func, void* data) {
-    MozJSEntry entry(this);
+    _runSafely([this, &field, &func, &data] {
+        JS::RootedObject obj(_context);
 
-    JS::RootedObject obj(_context);
+        NativeFunctionInfo::make(_context, &obj, func, data);
 
-    NativeFunctionInfo::make(_context, &obj, func, data);
-
-    JS::RootedValue value(_context);
-    value.setObjectOrNull(obj);
-    ObjectWrapper(_context, _global).setValue(field, value);
+        JS::RootedValue value(_context);
+        value.setObjectOrNull(obj);
+        ObjectWrapper(_context, _global).setValue(field, value);
+    });
 }
 
 void MozJSImplScope::gc() {
@@ -780,57 +777,59 @@ void MozJSImplScope::gc() {
 }
 
 void MozJSImplScope::localConnectForDbEval(OperationContext* opCtx, const char* dbName) {
-    MozJSEntry entry(this);
 
-    if (_connectState == ConnectState::External)
-        uasserted(12510, "externalSetup already called, can't call localConnect");
-    if (_connectState == ConnectState::Local) {
-        if (_localDBName == dbName)
-            return;
-        uasserted(12511,
-                  str::stream() << "localConnect previously called with name " << _localDBName);
-    }
+    _runSafely([this, &opCtx, &dbName] {
+        if (_connectState == ConnectState::External)
+            uasserted(12510, "externalSetup already called, can't call localConnect");
+        if (_connectState == ConnectState::Local) {
+            if (_localDBName == dbName)
+                return;
+            uasserted(12511,
+                      str::stream() << "localConnect previously called with name " << _localDBName);
+        }
 
-    // NOTE: order is important here.  the following methods must be called after
-    //       the above conditional statements.
+        // NOTE: order is important here.  the following methods must be called after
+        //       the above conditional statements.
 
-    _connectState = ConnectState::Local;
-    _localDBName = dbName;
+        _connectState = ConnectState::Local;
+        _localDBName = dbName;
 
-    loadStored(opCtx);
+        loadStored(opCtx);
 
-    // install db access functions in the global object
-    installDBAccess();
+        // install db access functions in the global object
+        installDBAccess();
 
-    // install the Mongo function object and instantiate the 'db' global
-    _mongoLocalProto.install(_global);
-    execCoreFiles();
+        // install the Mongo function object and instantiate the 'db' global
+        _mongoLocalProto.install(_global);
+        execCoreFiles();
 
-    const char* const makeMongo = "const _mongo = new Mongo()";
-    exec(makeMongo, "local connect 2", false, true, true, 0);
+        const char* const makeMongo = "const _mongo = new Mongo()";
+        exec(makeMongo, "local connect 2", false, true, true, 0);
 
-    std::string makeDB = str::stream() << "const db = _mongo.getDB(\"" << dbName << "\");";
-    exec(makeDB, "local connect 3", false, true, true, 0);
+        std::string makeDB = str::stream() << "const db = _mongo.getDB(\"" << dbName << "\");";
+        exec(makeDB, "local connect 3", false, true, true, 0);
+    });
 }
 
 void MozJSImplScope::externalSetup() {
-    MozJSEntry entry(this);
 
-    if (_connectState == ConnectState::External)
-        return;
-    if (_connectState == ConnectState::Local)
-        uasserted(12512, "localConnect already called, can't call externalSetup");
+    _runSafely([&] {
+        if (_connectState == ConnectState::External)
+            return;
+        if (_connectState == ConnectState::Local)
+            uasserted(12512, "localConnect already called, can't call externalSetup");
 
-    // install db access functions in the global object
-    installDBAccess();
+        // install db access functions in the global object
+        installDBAccess();
 
-    // install thread-related functions (e.g. _threadInject)
-    installFork();
+        // install thread-related functions (e.g. _threadInject)
+        installFork();
 
-    // install the Mongo function object
-    _mongoExternalProto.install(_global);
-    execCoreFiles();
-    _connectState = ConnectState::External;
+        // install the Mongo function object
+        _mongoExternalProto.install(_global);
+        execCoreFiles();
+        _connectState = ConnectState::External;
+    });
 }
 
 void MozJSImplScope::reset() {

mongodb/mongo

(cherry picked from commit ce5e5fdfcad9ee0b3b08954982ded2a4bdfd8ac2)

src/mongo/scripting/mozjs/implscope.cpp
@@ -407,82 +407,81 @@ void MozJSImplScope::init(const BSONObj* data) {
     }
 }
 
-void MozJSImplScope::setNumber(const char* field, double val) {
-    MozJSEntry entry(this);
+template <typename ImplScopeFunction>
+auto MozJSImplScope::_runSafely(ImplScopeFunction&& functionToRun) -> decltype(functionToRun()) {
+    try {
+        MozJSEntry entry(this);
+        return functionToRun();
+    } catch (...) {
+        _error = _status.reason();
 
-    ObjectWrapper(_context, _global).setNumber(field, val);
+        // Clear the status state
+        auto status = std::move(_status);
+        uassertStatusOK(status);
+        throw;
+    }
+}
+
+void MozJSImplScope::setNumber(const char* field, double val) {
+    _runSafely([this, &field, &val] { ObjectWrapper(_context, _global).setNumber(field, val); });
 }
 
 void MozJSImplScope::setString(const char* field, StringData val) {
-    MozJSEntry entry(this);
-
-    ObjectWrapper(_context, _global).setString(field, val);
+    _runSafely([this, &field, &val] { ObjectWrapper(_context, _global).setString(field, val); });
 }
 
 void MozJSImplScope::setBoolean(const char* field, bool val) {
-    MozJSEntry entry(this);
-
-    ObjectWrapper(_context, _global).setBoolean(field, val);
+    _runSafely([this, &field, &val] { ObjectWrapper(_context, _global).setBoolean(field, val); });
 }
 
 void MozJSImplScope::setElement(const char* field, const BSONElement& e, const BSONObj& parent) {
-    MozJSEntry entry(this);
+    _runSafely([this, &field, &e, &parent] {
 
-    ObjectWrapper(_context, _global).setBSONElement(field, e, parent, false);
+        ObjectWrapper(_context, _global).setBSONElement(field, e, parent, false);
+    });
 }
 
 void MozJSImplScope::setObject(const char* field, const BSONObj& obj, bool readOnly) {
-    MozJSEntry entry(this);
+    _runSafely([this, &field, &obj, &readOnly] {
 
-    ObjectWrapper(_context, _global).setBSON(field, obj, readOnly);
+        ObjectWrapper(_context, _global).setBSON(field, obj, readOnly);
+    });
 }
 
 int MozJSImplScope::type(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).type(field);
+    return _runSafely([this, &field] { return ObjectWrapper(_context, _global).type(field); });
 }
 
 double MozJSImplScope::getNumber(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getNumber(field);
+    return _runSafely([this, &field] { return ObjectWrapper(_context, _global).getNumber(field); });
 }
 
 int MozJSImplScope::getNumberInt(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getNumberInt(field);
+    return _runSafely(
+        [this, &field] { return ObjectWrapper(_context, _global).getNumberInt(field); });
 }
 
 long long MozJSImplScope::getNumberLongLong(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getNumberLongLong(field);
+    return _runSafely(
+        [this, &field] { return ObjectWrapper(_context, _global).getNumberLongLong(field); });
 }
 
 Decimal128 MozJSImplScope::getNumberDecimal(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getNumberDecimal(field);
+    return _runSafely(
+        [this, &field] { return ObjectWrapper(_context, _global).getNumberDecimal(field); });
 }
 
 std::string MozJSImplScope::getString(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getString(field);
+    return _runSafely([this, &field] { return ObjectWrapper(_context, _global).getString(field); });
 }
 
 bool MozJSImplScope::getBoolean(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getBoolean(field);
+    return _runSafely(
+        [this, &field] { return ObjectWrapper(_context, _global).getBoolean(field); });
 }
 
 BSONObj MozJSImplScope::getObject(const char* field) {
-    MozJSEntry entry(this);
-
-    return ObjectWrapper(_context, _global).getObject(field);
+    return _runSafely([this, &field] { return ObjectWrapper(_context, _global).getObject(field); });
 }
 
 void MozJSImplScope::newFunction(StringData raw, JS::MutableHandleValue out) {
@@ -552,17 +551,15 @@ ScriptingFunction MozJSImplScope::_createFunction(const char* raw) {
 }
 
 void MozJSImplScope::setFunction(const char* field, const char* code) {
-    MozJSEntry entry(this);
-
-    JS::RootedValue fun(_context);
-    _MozJSCreateFunction(code, &fun);
-    ObjectWrapper(_context, _global).setValue(field, fun);
+    _runSafely([this, &field, &code] {
+        JS::RootedValue fun(_context);
+        _MozJSCreateFunction(code, &fun);
+        ObjectWrapper(_context, _global).setValue(field, fun);
+    });
 }
 
 void MozJSImplScope::rename(const char* from, const char* to) {
-    MozJSEntry entry(this);
-
-    ObjectWrapper(_context, _global).rename(from, to);
+    _runSafely([this, &from, &to] { ObjectWrapper(_context, _global).rename(from, to); });
 }
 
 int MozJSImplScope::invoke(ScriptingFunction func,
@@ -674,15 +671,15 @@ bool MozJSImplScope::exec(StringData code,
 }
 
 void MozJSImplScope::injectNative(const char* field, NativeFunction func, void* data) {
-    MozJSEntry entry(this);
+    _runSafely([this, &field, &func, &data] {
+        JS::RootedObject obj(_context);
 
-    JS::RootedObject obj(_context);
+        NativeFunctionInfo::make(_context, &obj, func, data);
 
-    NativeFunctionInfo::make(_context, &obj, func, data);
-
-    JS::RootedValue value(_context);
-    value.setObjectOrNull(obj);
-    ObjectWrapper(_context, _global).setValue(field, value);
+        JS::RootedValue value(_context);
+        value.setObjectOrNull(obj);
+        ObjectWrapper(_context, _global).setValue(field, value);
+    });
 }
 
 void MozJSImplScope::gc() {
@@ -691,59 +688,59 @@ void MozJSImplScope::gc() {
 }
 
 void MozJSImplScope::localConnectForDbEval(OperationContext* txn, const char* dbName) {
-    MozJSEntry entry(this);
+    _runSafely([this, &txn, &dbName] {
+        if (_connectState == ConnectState::External)
+            uasserted(12510, "externalSetup already called, can't call localConnect");
+        if (_connectState == ConnectState::Local) {
+            if (_localDBName == dbName)
+                return;
+            uasserted(12511,
+                      str::stream() << "localConnect previously called with name " << _localDBName);
+        }
 
-    if (_connectState == ConnectState::External)
-        uasserted(12510, "externalSetup already called, can't call localConnect");
-    if (_connectState == ConnectState::Local) {
-        if (_localDBName == dbName)
-            return;
-        uasserted(12511,
-                  str::stream() << "localConnect previously called with name " << _localDBName);
-    }
+        // NOTE: order is important here.  the following methods must be called after
+        //       the above conditional statements.
 
-    // NOTE: order is important here.  the following methods must be called after
-    //       the above conditional statements.
+        _connectState = ConnectState::Local;
+        _localDBName = dbName;
 
-    _connectState = ConnectState::Local;
-    _localDBName = dbName;
+        loadStored(txn);
 
-    loadStored(txn);
+        // install db access functions in the global object
+        installDBAccess();
 
-    // install db access functions in the global object
-    installDBAccess();
+        // install the Mongo function object and instantiate the 'db' global
+        _mongoLocalProto.install(_global);
+        execCoreFiles();
 
-    // install the Mongo function object and instantiate the 'db' global
-    _mongoLocalProto.install(_global);
-    execCoreFiles();
+        const char* const makeMongo = "const _mongo = new Mongo()";
+        exec(makeMongo, "local connect 2", false, true, true, 0);
 
-    const char* const makeMongo = "const _mongo = new Mongo()";
-    exec(makeMongo, "local connect 2", false, true, true, 0);
-
-    std::string makeDB = str::stream() << "const db = _mongo.getDB(\"" << dbName << "\");";
-    exec(makeDB, "local connect 3", false, true, true, 0);
+        std::string makeDB = str::stream() << "const db = _mongo.getDB(\"" << dbName << "\");";
+        exec(makeDB, "local connect 3", false, true, true, 0);
+    });
 }
 
 void MozJSImplScope::externalSetup() {
-    MozJSEntry entry(this);
+    _runSafely([&] {
+        if (_connectState == ConnectState::External)
+            return;
+        if (_connectState == ConnectState::Local)
+            uasserted(12512, "localConnect already called, can't call externalSetup");
 
-    if (_connectState == ConnectState::External)
-        return;
-    if (_connectState == ConnectState::Local)
-        uasserted(12512, "localConnect already called, can't call externalSetup");
+        mongo::sm::reset(0);
 
-    mongo::sm::reset(0);
+        // install db access functions in the global object
+        installDBAccess();
 
-    // install db access functions in the global object
-    installDBAccess();
+        // install thread-related functions (e.g. _threadInject)
+        installFork();
 
-    // install thread-related functions (e.g. _threadInject)
-    installFork();
-
-    // install the Mongo function object
-    _mongoExternalProto.install(_global);
-    execCoreFiles();
-    _connectState = ConnectState::External;
+        // install the Mongo function object
+        _mongoExternalProto.install(_global);
+        execCoreFiles();
+        _connectState = ConnectState::External;
+    });
 }
 
 void MozJSImplScope::reset() {