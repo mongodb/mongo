8c35aac5f109639d6b6d942db100acc0f709cd10
Jonathan Reams <jbreams@mongodb.com>
2017-10-09 14:51:56 -0400

    SERVER-31468 replSets/startParallelShell.js should wait for its parallel shells to exit

--- jstests/replsets/startParallelShell.js
+++ jstests/replsets/startParallelShell.js
@@ -18,16 +18,18 @@ var db;
     assert.eq(url, mongo.host, "replSet.getURL() should match active connection string");
 
     print("* Starting parallel shell on --host " + db.getMongo().host);
-    startParallelShell('db.coll0.insert({test: "connString only"});');
+    var awaitShell = startParallelShell('db.coll0.insert({test: "connString only"});');
     assert.soon(function() {
         return db.coll0.find({test: "connString only"}).count() === 1;
     });
+    awaitShell();
 
     const uri = new MongoURI(url);
     const port0 = uri.servers[0].port;
     print("* Starting parallel shell w/ --port " + port0);
-    startParallelShell('db.coll0.insert({test: "explicit port"});', port0);
+    awaitShell = startParallelShell('db.coll0.insert({test: "explicit port"});', port0);
     assert.soon(function() {
         return db.coll0.find({test: "explicit port"}).count() === 1;
     });
+    awaitShell();
 })();
