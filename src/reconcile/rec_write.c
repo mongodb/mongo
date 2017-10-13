6c0ebaed7b65079e2a07a418ba805a4b8b802839
Keith Bostic <keith.bostic@mongodb.com>
2017-10-13 12:02:03 -0400

    WT-3657 Timestamp and lookaside related automated test failures (#3736)
    Don't include update-associated memory in the split decision unless
    there's either entries to put on the page or updates that will be
    associated with the image. Based on no data at all, I'm bounding the
    total number at 10 before we'll consider update information as part
    of a split.

--- src/reconcile/rec_write.c
+++ src/reconcile/rec_write.c
@@ -2014,19 +2014,29 @@ __rec_leaf_page_max(WT_SESSION_IMPL *session, WT_RECONCILE *r)
 /*
  * __rec_need_split --
  *	Check whether adding some bytes to the page requires a split.
- *
- *	This takes into account the disk image growing across a boundary, and
- *	also triggers a split for row store leaf pages when a threshold number
- *	of saved updates is reached.  This allows pages to split for update /
- *	restore and lookaside eviction when there is no visible data that
- *	causes the disk image to grow.
  */
 static bool
 __rec_need_split(WT_RECONCILE *r, size_t len)
 {
-	if (r->page->type == WT_PAGE_ROW_LEAF)
+	/*
+	 * In the case of a row-store leaf page, trigger a split if a threshold
+	 * number of saved updates is reached. This allows pages to split for
+	 * update/restore and lookaside eviction when there is no visible data
+	 * causing the disk image to grow.
+	 *
+	 * In the case of small pages or large keys, we might try to split when
+	 * a page has no updates or entries, which isn't possible. To consider
+	 * update/restore or lookaside information, require either page entries
+	 * or updates that will be attached to the image. The limit is one of
+	 * either, but it doesn't make sense to create pages or images with few
+	 * entries or updates, even where page sizes are small (especially as
+	 * updates that will eventually become overflow items can throw off our
+	 * calculations). Bound the combination at something reasonable.
+	 */
+	if (r->page->type == WT_PAGE_ROW_LEAF && r->entries + r->supd_next > 10)
 		len += r->supd_memsize;
 
+	/* Check for the disk image crossing a boundary. */
 	return (r->raw_compression ?
 	    len > r->space_avail : WT_CHECK_CROSSING_BND(r, len));
 }
