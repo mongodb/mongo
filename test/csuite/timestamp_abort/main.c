7a702f125909a58035625e664affa625c1f88049
sueloverso <sue@mongodb.com>
2017-11-09 21:57:53 -0500

    WT-3648 Read the thread's timestamp information once. (#3782)

--- test/csuite/timestamp_abort/main.c
+++ test/csuite/timestamp_abort/main.c
@@ -75,7 +75,7 @@ static const char * const ckpt_file = "checkpoint_done";
 
 static bool compat, inmem, use_ts;
 static volatile uint64_t global_ts = 1;
-static uint64_t th_ts[MAX_TH];
+static volatile uint64_t th_ts[MAX_TH];
 
 #define	ENV_CONFIG_COMPAT	",compatibility=(release=\"2.9\")"
 #define	ENV_CONFIG_DEF						\
@@ -121,7 +121,7 @@ thread_ts_run(void *arg)
 	WT_CURSOR *cur_stable;
 	WT_SESSION *session;
 	THREAD_DATA *td;
-	uint64_t i, last_ts, oldest_ts;
+	uint64_t i, last_ts, oldest_ts, this_ts;
 	char tscfg[64];
 
 	td = (THREAD_DATA *)arg;
@@ -148,10 +148,11 @@ thread_ts_run(void *arg)
 			 * any thread still with a zero timestamp we go to
 			 * sleep.
 			 */
-			if (th_ts[i] == 0)
+			this_ts = th_ts[i];
+			if (this_ts == 0)
 				goto ts_wait;
-			if (th_ts[i] != 0 && th_ts[i] < oldest_ts)
-				oldest_ts = th_ts[i];
+			else if (this_ts < oldest_ts)
+				oldest_ts = this_ts;
 		}
 
 		if (oldest_ts != UINT64_MAX &&
