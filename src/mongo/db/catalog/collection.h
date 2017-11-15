b79e5f04ffc79b5892f89c22b9e5f26a297b1185
Martin Neupauer <martin.neupauer@mongodb.com>
2017-11-15 15:38:14 -0500

    SERVER-31684 Fix unexpected "operation exceeded time limit" errors
    The changestream queries used an operation context deadline to track
    a wait time before returning EOF. This occasionaly interfered with
    normal operation deadlines leading to unexpected errors.

--- src/mongo/db/catalog/collection.h
+++ src/mongo/db/catalog/collection.h
@@ -112,17 +112,12 @@ public:
     void notifyAll();
 
     /**
-     * Waits for 'timeout' microseconds, or until notifyAll() is called to indicate that new
+     * Waits until 'deadline', or until notifyAll() is called to indicate that new
      * data is available in the capped collection.
      *
      * NOTE: Waiting threads can be signaled by calling kill or notify* methods.
      */
-    void wait(Microseconds timeout) const;
-
-    /**
-     * Same as above but also ensures that if the version has changed, it also returns.
-     */
-    void wait(uint64_t prevVersion, Microseconds timeout) const;
+    void waitUntil(uint64_t prevVersion, Date_t deadline) const;
 
     /**
      * Returns the version for use as an additional wake condition when used above.
@@ -130,11 +125,6 @@ public:
     uint64_t getVersion() const {
         return _version;
     }
-
-    /**
-     * Same as above but without a timeout.
-     */
-    void wait() const;
 
     /**
      * Cancels the notifier if the collection is dropped/invalidated, and wakes all waiting.
@@ -147,11 +137,6 @@ public:
     bool isDead();
 
 private:
-    // Helper for wait impls.
-    void _wait(stdx::unique_lock<stdx::mutex>& lk,
-               uint64_t prevVersion,
-               Microseconds timeout) const;
-
     // Signalled when a successful insert is made into a capped collection.
     mutable stdx::condition_variable _notifier;
 
