xiexiaoy/eloqdoc

SERVER-31827 Wait for Secondary after stepdown.
The primary being told to step down will kill all incoming connections
during that stepdown process.  When this happens, connections which
are incoming may be closed before they can enter a state which
"reserves" them from closure.  To facilitate correct semantics in this
test, we should wait for the primary to completely step down to a
secondary, before trying to authenticate to it again.

jstests/multiVersion/libs/multi_rs.js
@@ -43,6 +43,7 @@ ReplSetTest.prototype.upgradePrimary = function(primary, options, user, pwd) {
     }
 
     let oldPrimary = this.stepdown(primary);
+    this.waitForState(oldPrimary, ReplSetTest.State.SECONDARY);
 
     // stepping down the node can close the connection and lose the authentication state, so
     // re-authenticate here before calling awaitNodesAgreeOnPrimary().

mongodb/mongo

(cherry picked from commit 29769e9514980334d72b77bd5af618b7a69e7e72)

jstests/multiVersion/libs/multi_rs.js
@@ -25,6 +25,7 @@ ReplSetTest.prototype.upgradeSet = function(options, user, pwd) {
         var node = nodesToUpgrade[i];
         if (node == primary) {
             node = this.stepdown(node);
+            this.waitForState(node, ReplSetTest.State.SECONDARY);
             primary = this.getPrimary();
         }
 