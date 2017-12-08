xiexiaoy/eloqdoc

SERVER-27724 Minimize split chunk command routing table refreshes

jstests/sharding/dump_coll_metadata.js
@@ -33,14 +33,18 @@
     assert(shardAdmin.runCommand({getShardVersion: coll + "xyz", fullMetadata: true}).metadata !=
            undefined);
 
-    // Make sure we get multiple chunks after a split
-    assert(admin.runCommand({split: coll + "", middle: {_id: 0}}).ok);
+    // Make sure we get multiple chunks after a split and refresh -- splits by themselves do not
+    // cause the shard to refresh.
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 0}}));
+    assert.commandWorked(
+        st.shard0.getDB('admin').runCommand({forceRoutingTableRefresh: coll + ""}));
 
-    assert(shardAdmin.runCommand({getShardVersion: coll + ""}).ok);
+    assert.commandWorked(shardAdmin.runCommand({getShardVersion: coll + ""}));
     printjson(shardAdmin.runCommand({getShardVersion: coll + "", fullMetadata: true}));
 
     // Make sure we have chunks info
     result = shardAdmin.runCommand({getShardVersion: coll + "", fullMetadata: true});
+    assert.commandWorked(result);
     metadata = result.metadata;
 
     assert.eq(metadata.chunks.length, 2);

mongodb/mongo

(cherry picked from commit c9a30dfc4dcf383131ae059b154968a302fbe17c)

jstests/sharding/dump_coll_metadata.js
@@ -33,14 +33,18 @@
     assert(shardAdmin.runCommand({getShardVersion: coll + "xyz", fullMetadata: true}).metadata !=
            undefined);
 
-    // Make sure we get multiple chunks after a split
-    assert(admin.runCommand({split: coll + "", middle: {_id: 0}}).ok);
+    // Make sure we get multiple chunks after a split and refresh -- splits by themselves do not
+    // cause the shard to refresh.
+    assert.commandWorked(admin.runCommand({split: coll + "", middle: {_id: 0}}));
+    assert.commandWorked(
+        st.shard0.getDB('admin').runCommand({_flushRoutingTableCacheUpdates: coll + ""}));
 
-    assert(shardAdmin.runCommand({getShardVersion: coll + ""}).ok);
+    assert.commandWorked(shardAdmin.runCommand({getShardVersion: coll + ""}));
     printjson(shardAdmin.runCommand({getShardVersion: coll + "", fullMetadata: true}));
 
     // Make sure we have chunks info
     result = shardAdmin.runCommand({getShardVersion: coll + "", fullMetadata: true});
+    assert.commandWorked(result);
     metadata = result.metadata;
 
     assert.eq(metadata.chunks.length, 2);