42a8f21be68549c31cbe80b6f15faff6d4eb6eda
Geert Bosch <geert@mongodb.com>
2017-12-13 14:03:42 -0500

    SERVER-32315 Remove IndexObserver
    (cherry picked from commit 715cedf3377220a5c5e774b2ad5034996336f36f)

--- src/mongo/db/catalog/collection.h
+++ src/mongo/db/catalog/collection.h
@@ -324,11 +324,6 @@ public:
         virtual void notifyCappedWaitersIfNeeded() = 0;
 
         virtual const CollatorInterface* getDefaultCollator() const = 0;
-
-        virtual void informIndexObserver(OperationContext* opCtx,
-                                         const IndexDescriptor* descriptor,
-                                         const IndexKeyEntry& indexEntry,
-                                         const ValidationOperation operation) const = 0;
     };
 
 private:
@@ -733,15 +728,6 @@ public:
         return this->_impl().getDefaultCollator();
     }
 
-    /**
-     * Calls the Inforn function in the IndexObserver if it's hooked.
-     */
-    inline void informIndexObserver(OperationContext* opCtx,
-                                    const IndexDescriptor* descriptor,
-                                    const IndexKeyEntry& indexEntry,
-                                    const ValidationOperation operation) const {
-        return this->_impl().informIndexObserver(opCtx, descriptor, indexEntry, operation);
-    }
 
 private:
     inline DatabaseCatalogEntry* dbce() const {
