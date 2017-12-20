xiexiaoy/eloqdoc

SERVER-31930: Have ephemeralForTest capped collections unindex deleted documents in rollback.

src/mongo/db/storage/ephemeral_for_test/ephemeral_for_test_record_store.cpp
@@ -561,9 +561,15 @@ void EphemeralForTestRecordStore::cappedTruncateAfter(OperationContext* opCtx,
     Records::iterator it =
         inclusive ? _data->records.lower_bound(end) : _data->records.upper_bound(end);
     while (it != _data->records.end()) {
-        opCtx->recoveryUnit()->registerChange(
-            new RemoveChange(opCtx, _data, it->first, it->second));
-        _data->dataSize -= it->second.size;
+        RecordId id = it->first;
+        EphemeralForTestRecord record = it->second;
+
+        if (_cappedCallback) {
+            uassertStatusOK(_cappedCallback->aboutToDeleteCapped(opCtx, id, record.toRecordData()));
+        }
+
+        opCtx->recoveryUnit()->registerChange(new RemoveChange(opCtx, _data, id, record));
+        _data->dataSize -= record.size;
         _data->records.erase(it++);
     }
 }

mongodb/mongo

(cherry picked from commit 6ad1c6cea2e516ac8800265ed7358edaea9382d8)

src/mongo/db/storage/ephemeral_for_test/ephemeral_for_test_record_store.cpp
@@ -556,8 +556,15 @@ void EphemeralForTestRecordStore::temp_cappedTruncateAfter(OperationContext* txn
     Records::iterator it =
         inclusive ? _data->records.lower_bound(end) : _data->records.upper_bound(end);
     while (it != _data->records.end()) {
-        txn->recoveryUnit()->registerChange(new RemoveChange(txn, _data, it->first, it->second));
-        _data->dataSize -= it->second.size;
+        RecordId id = it->first;
+        EphemeralForTestRecord record = it->second;
+
+        if (_cappedCallback) {
+            uassertStatusOK(_cappedCallback->aboutToDeleteCapped(txn, id, record.toRecordData()));
+        }
+
+        txn->recoveryUnit()->registerChange(new RemoveChange(txn, _data, id, record));
+        _data->dataSize -= record.size;
         _data->records.erase(it++);
     }
 }