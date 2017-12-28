f6ee7e6f975f386a654d17768f4f7ac04946118b
Benety Goh <benety@mongodb.com>
2017-12-28 18:04:01 -0500

    SERVER-32402 reset Database drop pending state upon step down after dropping collections
    (cherry picked from commit afc4b85e1c9023548d6eba7b121a07172e0708ba)

--- src/mongo/db/catalog/drop_database.cpp
+++ src/mongo/db/catalog/drop_database.cpp
@@ -226,6 +226,10 @@ Status dropDatabase(OperationContext* opCtx, const std::string& dbName) {
             opCtx->writesAreReplicated() && !replCoord->canAcceptWritesForDatabase(opCtx, dbName);
 
         if (userInitiatedWritesAndNotPrimary) {
+            AutoGetDb autoDB(opCtx, dbName, MODE_X);
+            if (auto db = autoDB.getDb()) {
+                db->setDropPending(opCtx, false);
+            }
             return Status(ErrorCodes::PrimarySteppedDown,
                           str::stream() << "Could not drop database " << dbName
                                         << " because we transitioned from PRIMARY to "
