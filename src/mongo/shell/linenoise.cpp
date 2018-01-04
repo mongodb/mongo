06002a6b47800a57dafdafad5e0343876f979b7b
Kevin Pulo <kevin.pulo@mongodb.com>
2018-01-04 06:34:45 +0000

    SERVER-32473 don't output an error on shell startup if no history file
    (cherry picked from commit 4e51bb975ba8b1d2e5faa396543f16eff2ecdee5)

--- src/mongo/shell/linenoise.cpp
+++ src/mongo/shell/linenoise.cpp
@@ -2815,6 +2815,11 @@ mongo::Status linenoiseHistorySave(const char* filename) {
 mongo::Status linenoiseHistoryLoad(const char* filename) {
     FILE* fp = fopen(filename, "rt");
     if (fp == NULL) {
+        if (errno == ENOENT) {
+            // Not having a history file isn't an error condition.
+            // For example, it's always the case when the shell is run for the first time.
+            return mongo::Status::OK();
+        }
         return linenoiseFileError(mongo::ErrorCodes::FileOpenFailed, "fopen()", filename);
     }
 
