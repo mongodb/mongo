d2b96a097a0891ab1dd45551fb0d728dab1fe5a5
Kevin Albertson <kevin.albertson@10gen.com>
2018-01-04 16:27:22 -0500

    SERVER-32065 support retryWrites URI param in shell
    (cherry picked from commit 28c18ba63785d6cbf0ae27c4d5031255b6b79b2c)

--- src/mongo/client/mongo_uri.cpp
+++ src/mongo/client/mongo_uri.cpp
@@ -422,16 +422,30 @@ MongoURI MongoURI::parseImpl(const std::string& url) {
         addTXTOptions(parseOptions(connectionOptions, url), canonicalHost, url, isSeedlist);
 
     // If a replica set option was specified, store it in the 'setName' field.
-    const auto optIter = options.find("replicaSet");
+    auto optIter = options.find("replicaSet");
     std::string setName;
     if (optIter != end(options)) {
         setName = optIter->second;
         invariant(!setName.empty());
     }
 
+    boost::optional<bool> retryWrites = boost::none;
+    optIter = options.find("retryWrites");
+    if (optIter != end(options)) {
+        if (optIter->second == "true") {
+            retryWrites.reset(true);
+        } else if (optIter->second == "false") {
+            retryWrites.reset(false);
+        } else {
+            uasserted(ErrorCodes::FailedToParse,
+                      str::stream() << "retryWrites must be either \"true\" or \"false\"");
+        }
+    }
+
     ConnectionString cs(
         setName.empty() ? ConnectionString::MASTER : ConnectionString::SET, servers, setName);
-    return MongoURI(std::move(cs), username, password, database, std::move(options));
+    return MongoURI(
+        std::move(cs), username, password, database, std::move(retryWrites), std::move(options));
 }
 
 StatusWith<MongoURI> MongoURI::parse(const std::string& url) try {
