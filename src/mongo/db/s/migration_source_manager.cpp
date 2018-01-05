9f36a4b903401c8d2f1f7e248cf96eaa64ff99ce
Jack Mulrow <jack.mulrow@mongodb.com>
2018-01-05 12:44:25 -0500

    SERVER-32554 Ensure migration cloner is cancelled in cleanup

--- src/mongo/db/s/migration_source_manager.cpp
+++ src/mongo/db/s/migration_source_manager.cpp
@@ -671,6 +671,10 @@ void MigrationSourceManager::_cleanup(OperationContext* opCtx) {
     // The cleanup operations below are potentially blocking or acquire other locks, so perform them
     // outside of the collection X lock
 
+    if (cloneDriver) {
+        cloneDriver->cancelClone(opCtx);
+    }
+
     if (_state == kCriticalSection || _state == kCloneCompleted) {
         // NOTE: The order of the operations below is important and the comments explain the
         // reasoning behind it
@@ -689,10 +693,6 @@ void MigrationSourceManager::_cleanup(OperationContext* opCtx) {
         // Clear the 'minOpTime recovery' document so that the next time a node from this shard
         // becomes a primary, it won't have to recover the config server optime.
         ShardingStateRecovery::endMetadataOp(opCtx);
-    }
-
-    if (cloneDriver) {
-        cloneDriver->cancelClone(opCtx);
     }
 
     _state = kDone;
