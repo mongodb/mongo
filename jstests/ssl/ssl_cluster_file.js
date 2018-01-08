d34d2ba2b34cc18f8c853ecaa5a9cc59f587282b
Spencer Jackson <spencer.jackson@mongodb.com>
2018-01-08 18:38:43 -0500

    SERVER-32551: Ensure Transport Layer doesn't use clusterFile as server cert

---
+++ jstests/ssl/ssl_cluster_file.js
@@ -0,0 +1,35 @@
+(function() {
+    "use strict";
+
+    var CA_CERT = "jstests/libs/ca.pem";
+    var SERVER_CERT = "jstests/libs/server.pem";
+    var CLIENT_CERT = "jstests/libs/client.pem";
+    var BAD_SAN_CERT = "jstests/libs/badSAN.pem";
+
+    var mongod = MongoRunner.runMongod({
+        sslMode: "requireSSL",
+        sslPEMKeyFile: SERVER_CERT,
+        sslCAFile: CA_CERT,
+        sslClusterFile: BAD_SAN_CERT
+    });
+
+    var mongo = runMongoProgram("mongo",
+                                "--host",
+                                "localhost",
+                                "--port",
+                                mongod.port,
+                                "--ssl",
+                                "--sslCAFile",
+                                CA_CERT,
+                                "--sslPEMKeyFile",
+                                CLIENT_CERT,
+                                "--eval",
+                                ";");
+
+    // runMongoProgram returns 0 on success
+    assert.eq(
+        0,
+        mongo,
+        "Connection attempt failed when an irrelevant sslClusterFile was provided to the server!");
+
+}());
