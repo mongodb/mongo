4f6b79d6528a035ddc69e7eaef5fe1a972e657f5
Bernard Gorman <bernard.gorman@gmail.com>
2018-01-23 11:08:54 +0000

    SERVER-32175 change_stream_collation.js should always use assert.soon() when checking cursor.hasNext()

--- jstests/change_streams/change_stream_collation.js
+++ jstests/change_streams/change_stream_collation.js
@@ -197,7 +197,7 @@
         assert.writeOK(noCollationCollection.insert({_id: 1, text: "abc"}));
         assert.soon(() => cursor.hasNext());
         assert.docEq(cursor.next(), {docId: 0});
-        assert(cursor.hasNext());
+        assert.soon(() => cursor.hasNext());
         assert.docEq(cursor.next(), {docId: 1});
         assert(!cursor.hasNext());
     }());
