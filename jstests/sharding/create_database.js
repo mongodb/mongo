1f3e2875a1c7a922c2454f28fcc4a7ba22b431dd
Esha Maharishi <esha.maharishi@mongodb.com>
2018-02-06 16:27:27 -0500

    SERVER-33022 remove testing for fcv=3.4

--- jstests/sharding/create_database.js
+++ jstests/sharding/create_database.js
@@ -79,11 +79,4 @@
     assert.commandWorked(st.s.adminCommand({setFeatureCompatibilityVersion: "3.6"}));
     assertDbVersionNotAssigned(st.s, dbName);
 
-    //
-    // FCV 3.4 (This section can be deleted once FCV 3.4 is removed).
-    //
-
-    assert.commandWorked(st.s.adminCommand({setFeatureCompatibilityVersion: "3.4"}));
-    assertDbVersionNotAssigned(st.s, dbName);
-
 })();
