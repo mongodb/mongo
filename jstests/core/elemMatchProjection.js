xiexiaoy/eloqdoc

SERVER-33092 Fix elemMatchProjection.js to always have monotonically increasing _ids.

jstests/core/elemMatchProjection.js
@@ -9,20 +9,31 @@
 
     const date1 = new Date();
 
+    // Generate monotonically increasing _id values. ObjectIds generated by the shell are not
+    // guaranteed to be monotically increasing, and we will depend on the _id sort order later in
+    // the test.
+    let currentId = 0;
+    function nextId() {
+        return ++currentId;
+    }
+
     // Insert various styles of arrays.
     const bulk = coll.initializeUnorderedBulkOp();
     for (let i = 0; i < 100; i++) {
-        bulk.insert({group: 1, x: [1, 2, 3, 4, 5]});
-        bulk.insert({group: 2, x: [{a: 1, b: 2}, {a: 2, c: 3}, {a: 1, d: 5}]});
+        bulk.insert({_id: nextId(), group: 1, x: [1, 2, 3, 4, 5]});
+        bulk.insert({_id: nextId(), group: 2, x: [{a: 1, b: 2}, {a: 2, c: 3}, {a: 1, d: 5}]});
         bulk.insert({
+            _id: nextId(),
             group: 3,
             x: [{a: 1, b: 2}, {a: 2, c: 3}, {a: 1, d: 5}],
             y: [{aa: 1, bb: 2}, {aa: 2, cc: 3}, {aa: 1, dd: 5}]
         });
-        bulk.insert({group: 3, x: [{a: 1, b: 3}, {a: -6, c: 3}]});
-        bulk.insert({group: 4, x: [{a: 1, b: 4}, {a: -6, c: 3}]});
-        bulk.insert({group: 5, x: [new Date(), 5, 10, 'string', new ObjectId(), 123.456]});
+        bulk.insert({_id: nextId(), group: 3, x: [{a: 1, b: 3}, {a: -6, c: 3}]});
+        bulk.insert({_id: nextId(), group: 4, x: [{a: 1, b: 4}, {a: -6, c: 3}]});
+        bulk.insert(
+            {_id: nextId(), group: 5, x: [new Date(), 5, 10, 'string', new ObjectId(), 123.456]});
         bulk.insert({
+            _id: nextId(),
             group: 6,
             x: [
                 {a: 'string', b: date1},
@@ -30,20 +41,34 @@
                 {a: 'string2', b: date1}
             ]
         });
-        bulk.insert({group: 7, x: [{y: [1, 2, 3, 4]}]});
-        bulk.insert({group: 8, x: [{y: [{a: 1, b: 2}, {a: 3, b: 4}]}]});
-        bulk.insert(
-            {group: 9, x: [{y: [{a: 1, b: 2}, {a: 3, b: 4}]}, {z: [{a: 1, b: 2}, {a: 3, b: 4}]}]});
-        bulk.insert({group: 10, x: [{a: 1, b: 2}, {a: 3, b: 4}], y: [{c: 1, d: 2}, {c: 3, d: 4}]});
-        bulk.insert({group: 10, x: [{a: 1, b: 2}, {a: 3, b: 4}], y: [{c: 1, d: 2}, {c: 3, d: 4}]});
+        bulk.insert({_id: nextId(), group: 7, x: [{y: [1, 2, 3, 4]}]});
+        bulk.insert({_id: nextId(), group: 8, x: [{y: [{a: 1, b: 2}, {a: 3, b: 4}]}]});
         bulk.insert({
+            _id: nextId(),
+            group: 9,
+            x: [{y: [{a: 1, b: 2}, {a: 3, b: 4}]}, {z: [{a: 1, b: 2}, {a: 3, b: 4}]}]
+        });
+        bulk.insert({
+            _id: nextId(),
+            group: 10,
+            x: [{a: 1, b: 2}, {a: 3, b: 4}],
+            y: [{c: 1, d: 2}, {c: 3, d: 4}]
+        });
+        bulk.insert({
+            _id: nextId(),
+            group: 10,
+            x: [{a: 1, b: 2}, {a: 3, b: 4}],
+            y: [{c: 1, d: 2}, {c: 3, d: 4}]
+        });
+        bulk.insert({
+            _id: nextId(),
             group: 11,
             x: [{a: 1, b: 2}, {a: 2, c: 3}, {a: 1, d: 5}],
             covered: [{aa: 1, bb: 2}, {aa: 2, cc: 3}, {aa: 1, dd: 5}]
         });
-        bulk.insert({group: 12, x: {y: [{a: 1, b: 1}, {a: 1, b: 2}]}});
-        bulk.insert({group: 13, x: [{a: 1, b: 1}, {a: 1, b: 2}]});
-        bulk.insert({group: 13, x: [{a: 1, b: 2}, {a: 1, b: 1}]});
+        bulk.insert({_id: nextId(), group: 12, x: {y: [{a: 1, b: 1}, {a: 1, b: 2}]}});
+        bulk.insert({_id: nextId(), group: 13, x: [{a: 1, b: 1}, {a: 1, b: 2}]});
+        bulk.insert({_id: nextId(), group: 13, x: [{a: 1, b: 2}, {a: 1, b: 1}]});
     }
     assert.writeOK(bulk.execute());
 

mongodb/mongo

(cherry picked from commit ab9fbc6a90c65d228dd15973aa6706563c086106)
Conflicts:
      jstests/core/elemMatchProjection.js

jstests/core/elemMatchProjection.js
@@ -4,35 +4,59 @@ t.drop();
 
 date1 = new Date();
 
+// Generate monotonically increasing _id values. ObjectIds generated by the shell are not guaranteed
+// to be monotically increasing, and we will depend on the _id sort order later in the test.
+var currentId = 0;
+function nextId() {
+    return ++currentId;
+}
+
 // Insert various styles of arrays
 for (i = 0; i < 100; i++) {
-    t.insert({group: 1, x: [1, 2, 3, 4, 5]});
-    t.insert({group: 2, x: [{a: 1, b: 2}, {a: 2, c: 3}, {a: 1, d: 5}]});
+    t.insert({_id: nextId(), group: 1, x: [1, 2, 3, 4, 5]});
+    t.insert({_id: nextId(), group: 2, x: [{a: 1, b: 2}, {a: 2, c: 3}, {a: 1, d: 5}]});
     t.insert({
+        _id: nextId(),
         group: 3,
         x: [{a: 1, b: 2}, {a: 2, c: 3}, {a: 1, d: 5}],
         y: [{aa: 1, bb: 2}, {aa: 2, cc: 3}, {aa: 1, dd: 5}]
     });
-    t.insert({group: 3, x: [{a: 1, b: 3}, {a: -6, c: 3}]});
-    t.insert({group: 4, x: [{a: 1, b: 4}, {a: -6, c: 3}]});
-    t.insert({group: 5, x: [new Date(), 5, 10, 'string', new ObjectId(), 123.456]});
+    t.insert({_id: nextId(), group: 3, x: [{a: 1, b: 3}, {a: -6, c: 3}]});
+    t.insert({_id: nextId(), group: 4, x: [{a: 1, b: 4}, {a: -6, c: 3}]});
+    t.insert({_id: nextId(), group: 5, x: [new Date(), 5, 10, 'string', new ObjectId(), 123.456]});
     t.insert({
+        _id: nextId(),
         group: 6,
         x: [{a: 'string', b: date1}, {a: new ObjectId(), b: 1.2345}, {a: 'string2', b: date1}]
     });
-    t.insert({group: 7, x: [{y: [1, 2, 3, 4]}]});
-    t.insert({group: 8, x: [{y: [{a: 1, b: 2}, {a: 3, b: 4}]}]});
-    t.insert({group: 9, x: [{y: [{a: 1, b: 2}, {a: 3, b: 4}]}, {z: [{a: 1, b: 2}, {a: 3, b: 4}]}]});
-    t.insert({group: 10, x: [{a: 1, b: 2}, {a: 3, b: 4}], y: [{c: 1, d: 2}, {c: 3, d: 4}]});
-    t.insert({group: 10, x: [{a: 1, b: 2}, {a: 3, b: 4}], y: [{c: 1, d: 2}, {c: 3, d: 4}]});
+    t.insert({_id: nextId(), group: 7, x: [{y: [1, 2, 3, 4]}]});
+    t.insert({_id: nextId(), group: 8, x: [{y: [{a: 1, b: 2}, {a: 3, b: 4}]}]});
     t.insert({
+        _id: nextId(),
+        group: 9,
+        x: [{y: [{a: 1, b: 2}, {a: 3, b: 4}]}, {z: [{a: 1, b: 2}, {a: 3, b: 4}]}]
+    });
+    t.insert({
+        _id: nextId(),
+        group: 10,
+        x: [{a: 1, b: 2}, {a: 3, b: 4}],
+        y: [{c: 1, d: 2}, {c: 3, d: 4}]
+    });
+    t.insert({
+        _id: nextId(),
+        group: 10,
+        x: [{a: 1, b: 2}, {a: 3, b: 4}],
+        y: [{c: 1, d: 2}, {c: 3, d: 4}]
+    });
+    t.insert({
+        _id: nextId(),
         group: 11,
         x: [{a: 1, b: 2}, {a: 2, c: 3}, {a: 1, d: 5}],
         covered: [{aa: 1, bb: 2}, {aa: 2, cc: 3}, {aa: 1, dd: 5}]
     });
-    t.insert({group: 12, x: {y: [{a: 1, b: 1}, {a: 1, b: 2}]}});
-    t.insert({group: 13, x: [{a: 1, b: 1}, {a: 1, b: 2}]});
-    t.insert({group: 13, x: [{a: 1, b: 2}, {a: 1, b: 1}]});
+    t.insert({_id: nextId(), group: 12, x: {y: [{a: 1, b: 1}, {a: 1, b: 2}]}});
+    t.insert({_id: nextId(), group: 13, x: [{a: 1, b: 1}, {a: 1, b: 2}]});
+    t.insert({_id: nextId(), group: 13, x: [{a: 1, b: 2}, {a: 1, b: 1}]});
 }
 t.ensureIndex({
     group: 1,