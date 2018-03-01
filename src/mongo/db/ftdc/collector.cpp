xiexiaoy/eloqdoc

SERVER-33233 Don't stall ftdc due to running out of tickets

src/mongo/db/ftdc/collector.cpp
@@ -67,6 +67,7 @@ std::tuple<BSONObj, Date_t> FTDCCollectorCollection::collect(Client* client) {
     // batches that are taking a long time.
     auto opCtx = client->makeOperationContext();
     opCtx->lockState()->setShouldConflictWithSecondaryBatchApplication(false);
+    opCtx->lockState()->setShouldAcquireTicket(false);
 
     for (auto& collector : _collectors) {
         BSONObjBuilder subObjBuilder(builder.subobjStart(collector->name()));

mongodb/mongo

(cherry picked from commit 5436c851b8322a0cb18611be0672f6a76d6e2f38)

src/mongo/db/ftdc/collector.cpp
@@ -67,6 +67,7 @@ std::tuple<BSONObj, Date_t> FTDCCollectorCollection::collect(Client* client) {
     // batches that are taking a long time.
     auto txn = client->makeOperationContext();
     txn->lockState()->setShouldConflictWithSecondaryBatchApplication(false);
+    txn->lockState()->setShouldAcquireTicket(false);
 
     for (auto& collector : _collectors) {
         BSONObjBuilder subObjBuilder(builder.subobjStart(collector->name()));