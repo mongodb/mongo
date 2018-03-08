929a4a26e1e1c073a4e9ea805ddc8e574caefe4b
Jonathan Abrahams <jonathan@mongodb.com>
2018-03-08 08:50:08 -0500

    SERVER-33691 Remote EC2 instances should have a log disk. Also increase the /data disk to 100GB.
    (cherry picked from commit cf31435ae65fdb8aaf0311793c0441e6d2172bcb)

--- buildscripts/launch_evergreen_ec2_instance.sh
+++ buildscripts/launch_evergreen_ec2_instance.sh
@@ -13,17 +13,17 @@ function _usage_ {
   cat << EOF
 usage: $0 options
 This script supports the following parameters for Windows & Linux platforms:
-  -k <ssh_key_id>,     [REQUIRED] The ssh key id used to access the new AWS EC2 instance.
-  -y <aws_ec2_yml>,    [REQUIRED] YAML file name where to store the new AWS EC2 instance
-                       information. This file will be used in etc/evergreen.yml for
-                       macro expansion of variables used in other functions.
-  -e <expire_hours>,   [OPTIONAL] Number of hours to expire the AWS EC2 instance.
+  -k <ssh_key_id>,        [REQUIRED] The ssh key id used to access the new AWS EC2 instance.
+  -y <aws_ec2_yml>,       [REQUIRED] YAML file name where to store the new AWS EC2 instance
+                          information. This file will be used in etc/evergreen.yml for
+                          macro expansion of variables used in other functions.
+  -e <expire_hours>,      [OPTIONAL] Number of hours to expire the AWS EC2 instance.
   -g <security_group_id>, [OPTIONAL] The security group id to be used for the new AWS EC2 instance.
                           To specify more than one group, invoke this option each time.
-  -n <subnet_id>,      [OPTIONAL] The subnet id in which to launch the AWS EC2 instance.
-  -s <security_group>, [OPTIONAL] The security group to be used for the new AWS EC2 instance.
-                       To specify more than one group, invoke this option each time.
-  -t <tag_name>,       [OPTIONAL] The tag name of the new AWS EC2 instance.
+  -n <subnet_id>,         [OPTIONAL] The subnet id in which to launch the AWS EC2 instance.
+  -s <security_group>,    [OPTIONAL] The security group to be used for the new AWS EC2 instance.
+                          To specify more than one group, invoke this option each time.
+  -t <tag_name>,          [OPTIONAL] The tag name of the new AWS EC2 instance.
 EOF
 }
 
@@ -95,36 +95,36 @@ instance_type=$(curl -s $aws_metadata_url/instance-type)
 echo "AMI EC2 info: $ami $instance_type"
 
 data_df=$(df -BG -T /data | tail -n1 | tr -s ' ')
-base_device_name=$(echo $data_df | cut -f1 -d ' ' | sed -e 's/.*\///g')
+base_data_device_name=$(echo $data_df | cut -f1 -d ' ' | sed -e 's/.*\///g')
 fstype=$(echo $data_df | cut -f2 -d ' ')
-device_size=$(echo $data_df | cut -f3 -d ' ' | cut -f1 -d "G" | cut -f1 -d ".")
-if [[ -z "$base_device_name" || -z "$fstype" || -z "$device_size" ]]; then
+# Set the size for /data to 100GB.
+let data_device_size=100
+if [[ -z "$base_data_device_name" || -z "$fstype" ]]; then
   echo "Could not detect /data mount point, one of the following are not detected:"
-  echo "base_device_name: '$base_device_name' fstype: '$fstype' device_size: '$device_size'"
+  echo "base_device_name: '$base_data_device_name' fstype: '$fstype' data_device_size: '$data_device_size'"
   exit 1
 fi
 
 # Linux variants have block device mappings, so discover the mounted drives.
 if [ $(uname) = "Linux" ]; then
-  devices_info="$base_device_name;$fstype;$device_size"
+  data_devices_info="$base_data_device_name;$fstype;$data_device_size"
 
-  is_raid_device=$(grep -s active /proc/mdstat | grep $base_device_name) || true
+  is_raid_device=$(grep -s active /proc/mdstat | grep $base_data_device_name) || true
   if [ ! -z "$is_raid_device" ]; then
-    raid_device_name=$base_device_name
-    raid_devices=$(lsblk | grep $raid_device_name -B1 | cut -f1 -d ' ' | grep -v $raid_device_name | tr '\n\r' ' ')
-    devices_info=
-    for device in $raid_devices
+    raid_data_device_name=$base_data_device_name
+    raid_data_devices=$(lsblk | grep $raid_data_device_name -B1 | cut -f1 -d ' ' | grep -v $raid_data_device_name | tr '\n\r' ' ')
+    data_devices_info=
+    for data_device in $raid_data_devices
     do
-      data_device=$(lsblk -o NAME,SIZE | grep $device | tr -s ' ')
-      device_size=$(echo $data_device | cut -f2 -d ' ' | cut -f1 -d "G" | cut -f1 -d ".")
-      devices_info="$devices_info $device;$fstype;$device_size"
+      data_device=$(lsblk -o NAME,SIZE | grep $data_device | tr -s ' ')
+      data_devices_info="$data_devices_info $data_device;$fstype;$data_device_size"
     done
   fi
 
   # Discover FS options on device, for now, only supports:
   #  - XFS options: crc finobt
   if [ $fstype = "xfs" ]; then
-    xfs_info=$(xfs_info /dev/$base_device_name)
+    xfs_info=$(xfs_info /dev/$base_data_device_name)
     xfs_option_choices="crc finobt"
     opt_num=0
     opt_sep=""
@@ -132,7 +132,7 @@ if [ $(uname) = "Linux" ]; then
     do
       opt_grep=$(echo $xfs_info | grep $xfs_option_choice) || true
       if [ ! -z "$opt_grep" ]; then
-        let "opt_num=opt_num+1"
+        let opt_num=opt_num+1
         if [ $opt_num -gt 1 ]; then
           opt_sep=","
         fi
@@ -145,23 +145,36 @@ if [ $(uname) = "Linux" ]; then
     fi
   fi
   # Specify the Block devices and sizes to be attached to the EC2 instance.
-  for info in $devices_info
+  for info in $data_devices_info
   do
-    device=$(echo $info | cut -f1 -d ';')
-    size=$(echo $info | cut -f3 -d ';')
-    device_names=$(echo "$device_names $device" | sed -e 's/^ *//; s/ *$//')
-    block_device_option="$block_device_option --blockDevice $device $size"
+    data_device=$(echo $info | cut -f1 -d ';')
+    data_size=$(echo $info | cut -f3 -d ';')
+    data_device_names=$(echo "$data_device_names $data_device" | sed -e 's/^ *//; s/ *$//')
+    block_data_device_option="$block_data_device_option --blockDevice $data_device $data_size"
   done
+  # We use next available device for the logging EBS volume
+  last_data_device=$(echo ${data_device_names##* })
+  next_drive_letter=$(echo -n $last_data_device | tail -c 1 | tr "0-9a-z" "1-9a-z_")
+  log_device_name="${last_data_device%?}$next_drive_letter"
+  log_device_size=50
+  log_devices_info="$log_device_name;$fstype;$log_device_size"
+  block_log_device_option="--blockDevice $log_device_name $log_device_size"
 elif [ "Windows_NT" = "$OS" ]; then
-  # We use 'xvdf' as the first available device for the EBS volume, which is mounted on 'd:'
+  # We use 'xvdf' as the first available device for the logging EBS volume, which is mounted on 'd:'
   # See http://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/device_naming.html
-  device="xvdf"
-  device_names="d"
-  devices_info="$device;$fstype;$device_size"
-  block_device_option="--blockDevice $device $device_size"
+  data_device="xvdf"
+  data_device_names="d"
+  data_devices_info="$data_device;$fstype;$data_device_size"
+  block_data_device_option="--blockDevice $data_device $data_device_size"
+  # We use 'xvdg' as the next available device for the logging EBS volume, which is mounted on 'e:'
+  log_device="xvdg"
+  log_device_name="e"
+  log_device_size=50
+  log_devices_info="$log_device;$fstype;$log_device_size"
+  block_log_device_option="--blockDevice $log_device $log_device_size"
 fi
 
-echo "Devices: $devices_info"
+echo "Data Devices: $data_devices_info"
 
 # Launch a new instance.
 aws_ec2=$(python buildscripts/aws_ec2.py \
@@ -175,7 +188,8 @@ aws_ec2=$(python buildscripts/aws_ec2.py \
           --tagName "$tag_name"          \
           --tagOwner "$USER"             \
           $expire_tag                    \
-          $block_device_option | tr -cd "[:print:]\n")
+          $block_log_device_option       \
+          $block_data_device_option | tr -cd "[:print:]\n")
 echo "Spawned new AMI EC2 instance: $aws_ec2"
 
 # Get new instance ID & ip_address
@@ -187,7 +201,8 @@ echo "instance_id: $instance_id" > $aws_ec2_yml
 echo "ami: $ami" >> $aws_ec2_yml
 echo "instance_type: $instance_type" >> $aws_ec2_yml
 echo "ip_address: $ip_address" >> $aws_ec2_yml
-echo "device_names: $device_names" >> $aws_ec2_yml
-echo "raid_device_name: $raid_device_name" >> $aws_ec2_yml
+echo "data_device_names: $data_device_names" >> $aws_ec2_yml
+echo "raid_data_device_name: $raid_data_device_name" >> $aws_ec2_yml
+echo "log_device_name: $log_device_name" >> $aws_ec2_yml
 echo "fstype: $fstype" >> $aws_ec2_yml
 echo "fs_options: $fs_options" >> $aws_ec2_yml
