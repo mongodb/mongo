a51fb6e222787f9968c3eb6d311fc74b3e571a24
KevinCybura <KevinCybura@gmail.com>
2018-03-08 17:58:49 -0500

    slice

--- src/mongo/db/pipeline/expression.cpp
+++ src/mongo/db/pipeline/expression.cpp
@@ -3678,40 +3678,119 @@ const char* ExpressionIsArray::getOpName() const {
 
 /* ----------------------- ExpressionSlice ---------------------------- */
 
+// Value ExpressionSlice::evaluate(const Document& root) const {
+//     const size_t n = vpOperand.size();
+
+//     Value arrayVal = vpOperand[0]->evaluate(root);
+//     // Could be either a start index or the length from 0.
+//     Value arg2 = vpOperand[1]->evaluate(root);
+
+//     if (arrayVal.nullish() || arg2.nullish()) {
+//         return Value(BSONNULL);
+//     }
+
+//     uassert(28724,
+//             str::stream() << "First argument to $slice must be an array, but is"
+//                           << " of type: "
+//                           << typeName(arrayVal.getType()),
+//             arrayVal.isArray());
+//     uassert(28725,
+//             str::stream() << "Second argument to $slice must be a numeric value,"
+//                           << " but is of type: "
+//                           << typeName(arg2.getType()),
+//             arg2.numeric());
+//     uassert(28726,
+//             str::stream() << "Second argument to $slice can't be represented as"
+//                           << " a 32-bit integer: "
+//                           << arg2.coerceToDouble(),
+//             arg2.integral());
+
+//     const auto& array = arrayVal.getArray();
+//     size_t start;
+//     size_t end;
+
+//     if (n == 2) {
+//         // Only count given.
+//         int count = arg2.coerceToInt();
+//         start = 0;
+//         end = array.size();
+//         if (count >= 0) {
+//             end = std::min(end, size_t(count));
+//         } else {
+//             // Negative count's start from the back. If a abs(count) is greater
+//             // than the
+//             // length of the array, return the whole array.
+//             start = std::max(0, static_cast<int>(array.size()) + count);
+//         }
+//     } else {
+//         // We have both a start index and a count.
+//         int startInt = arg2.coerceToInt();
+//         if (startInt < 0) {
+//             // Negative values start from the back. If a abs(start) is greater
+//             // than the length
+//             // of the array, start from 0.
+//             start = std::max(0, static_cast<int>(array.size()) + startInt);
+//         } else {
+//             start = std::min(array.size(), size_t(startInt));
+//         }
+
+//         Value countVal = vpOperand[2]->evaluate(root);
+
+//         if (countVal.nullish()) {
+//             return Value(BSONNULL);
+//         }
+
+//         uassert(28727,
+//                 str::stream() << "Third argument to $slice must be numeric, but "
+//                               << "is of type: "
+//                               << typeName(countVal.getType()),
+//                 countVal.numeric());
+//         uassert(28728,
+//                 str::stream() << "Third argument to $slice can't be represented"
+//                               << " as a 32-bit integer: "
+//                               << countVal.coerceToDouble(),
+//                 countVal.integral());
+//         uassert(28729,
+//                 str::stream() << "Third argument to $slice must be positive: "
+//                               << countVal.coerceToInt(),
+//                 countVal.coerceToInt() > 0);
+
+//         size_t count = size_t(countVal.coerceToInt());
+//         end = std::min(start + count, array.size());
+//     }
+
+//     return Value(vector<Value>(array.begin() + start, array.begin() + end));
+// }
 Value ExpressionSlice::evaluate(const Document& root) const {
     const size_t n = vpOperand.size();
-
-    Value arrayVal = vpOperand[0]->evaluate(root);
     // Could be either a start index or the length from 0.
     Value arg2 = vpOperand[1]->evaluate(root);
 
-    if (arrayVal.nullish() || arg2.nullish()) {
-        return Value(BSONNULL);
-    }
-
-    uassert(28724,
-            str::stream() << "First argument to $slice must be an array, but is"
-                          << " of type: "
-                          << typeName(arrayVal.getType()),
-            arrayVal.isArray());
-    uassert(28725,
-            str::stream() << "Second argument to $slice must be a numeric value,"
-                          << " but is of type: "
-                          << typeName(arg2.getType()),
-            arg2.numeric());
-    uassert(28726,
-            str::stream() << "Second argument to $slice can't be represented as"
-                          << " a 32-bit integer: "
-                          << arg2.coerceToDouble(),
-            arg2.integral());
-
-    const auto& array = arrayVal.getArray();
     size_t start;
     size_t end;
+    if (n == 2) {
+        start = 0;
+        end = arg2.coerceToInt();
+    } else {
+        Value arg3 = vpOperand[2]->evaluate(root);
 
+        if (arg3.nullish()) {
+            return Value(BSONNULL);
+        }
+        start = arg2.coerceToInt();
+        end = arg3.coerceToInt();
+    }
+
+    // If array is a filter expression call computeNthFilterdValue to return an array of size "end".
+    const Value arrayVal = dynamic_cast<ExpressionFilter*>(vpOperand[0].get())
+        ? dynamic_cast<ExpressionFilter*>(vpOperand[0].get())
+              ->computeNthFilteredValue(root, static_cast<long long>(end) + 1)
+        : vpOperand[0]->evaluate(root);
+
+    const auto& array = arrayVal.getArray();
     if (n == 2) {
         // Only count given.
-        int count = arg2.coerceToInt();
+        int count = end;
         start = 0;
         end = array.size();
         if (count >= 0) {
@@ -3724,7 +3803,7 @@ Value ExpressionSlice::evaluate(const Document& root) const {
         }
     } else {
         // We have both a start index and a count.
-        int startInt = arg2.coerceToInt();
+        int startInt = start;
         if (startInt < 0) {
             // Negative values start from the back. If a abs(start) is greater
             // than the length
@@ -3734,33 +3813,12 @@ Value ExpressionSlice::evaluate(const Document& root) const {
             start = std::min(array.size(), size_t(startInt));
         }
 
-        Value countVal = vpOperand[2]->evaluate(root);
-
-        if (countVal.nullish()) {
-            return Value(BSONNULL);
-        }
-
-        uassert(28727,
-                str::stream() << "Third argument to $slice must be numeric, but "
-                              << "is of type: "
-                              << typeName(countVal.getType()),
-                countVal.numeric());
-        uassert(28728,
-                str::stream() << "Third argument to $slice can't be represented"
-                              << " as a 32-bit integer: "
-                              << countVal.coerceToDouble(),
-                countVal.integral());
-        uassert(28729,
-                str::stream() << "Third argument to $slice must be positive: "
-                              << countVal.coerceToInt(),
-                countVal.coerceToInt() > 0);
-
-        size_t count = size_t(countVal.coerceToInt());
-        end = std::min(start + count, array.size());
+        end = std::min(start + end, array.size());
     }
 
     return Value(vector<Value>(array.begin() + start, array.begin() + end));
 }
+
 
 REGISTER_EXPRESSION(slice, ExpressionSlice::parse);
 const char* ExpressionSlice::getOpName() const {
