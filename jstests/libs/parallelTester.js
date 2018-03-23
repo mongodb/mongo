6ace5353e20086ce02202b8c2c684cadcfac2b47
Nick Zolnierz <nicholas.zolnierz@mongodb.com>
2018-03-23 16:04:21 -0400

    SERVER-34097: Prevent compact_keep_indexes.js from running in parallel with awaitdata_getmore_cmd.js

--- jstests/libs/parallelTester.js
+++ jstests/libs/parallelTester.js
@@ -272,6 +272,13 @@ if (typeof _threadInject != "undefined") {
             parallelFilesDir + "/profile_repair_cursor.js",
             parallelFilesDir + "/profile_sampling.js",
             parallelFilesDir + "/profile_update.js",
+
+            // These tests can't be run in parallel because they expect an awaitData cursor to
+            // return after maxTimeMS, however this doesn't work if a long running blocking
+            // operation is running in parallel.
+            // TODO: Remove this restriction as part of SERVER-33942.
+            parallelFilesDir + "/compact_keeps_indexes.js",
+            parallelFilesDir + "/awaitdata_getmore_cmd.js",
         ];
         var serialTests = makeKeys(serialTestsArr);
 
