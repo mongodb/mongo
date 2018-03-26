f3acd4a1e259b1f1fcdd51d23ad2519369257dbf
Robert Guo <robert.guo@10gen.com>
2018-03-26 13:37:45 -0400

    SERVER-32920 Avoid overriding read preference for the config server in passthrough tests
    (cherry picked from commit fe88a7402503f76b43c7ee71ef45b4ac0b9e3db7)

--- jstests/libs/override_methods/enable_causal_consistency.js
+++ jstests/libs/override_methods/enable_causal_consistency.js
@@ -5,9 +5,9 @@
     "use strict";
 
     load("jstests/libs/override_methods/override_helpers.js");
+    load('jstests/libs/override_methods/set_read_preference_secondary.js');
 
     db.getMongo().setCausalConsistency();
-    db.getMongo().setReadPref("secondary");
 
     OverrideHelpers.prependOverrideInParallelShell(
         "jstests/libs/override_methods/enable_causal_consistency.js");
