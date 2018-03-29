xiexiaoy/eloqdoc

SERVER-28670 Add sharding CatalogCache and donor metrics to serverStatus
Includes metrics for refresh, clone and migration critical section
duration.

src/mongo/db/s/sharding_state.cpp
@@ -49,6 +49,7 @@
 #include "mongo/db/s/operation_sharding_state.h"
 #include "mongo/db/s/sharded_connection_info.h"
 #include "mongo/db/s/sharding_initialization_mongod.h"
+#include "mongo/db/s/sharding_statistics.h"
 #include "mongo/db/s/type_shard_identity.h"
 #include "mongo/executor/network_interface_factory.h"
 #include "mongo/executor/network_interface_thread_pool.h"
@@ -224,6 +225,8 @@ Status ShardingState::onStaleShardVersion(OperationContext* opCtx,
 
     LOG(2) << "metadata refresh requested for " << nss.ns() << " at shard version "
            << expectedVersion;
+
+    ShardingStatistics::get(opCtx).countStaleConfigErrors.addAndFetch(1);
 
     // Ensure any ongoing migrations have completed
     auto& oss = OperationShardingState::get(opCtx);

src/mongo/s/catalog_cache.cpp
@@ -34,6 +34,7 @@
 
 #include "mongo/base/status.h"
 #include "mongo/base/status_with.h"
+#include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/db/query/collation/collator_factory_interface.h"
 #include "mongo/db/repl/optime_with.h"
 #include "mongo/platform/unordered_set.h"
@@ -44,6 +45,7 @@
 #include "mongo/s/grid.h"
 #include "mongo/util/concurrency/with_lock.h"
 #include "mongo/util/log.h"
+#include "mongo/util/scopeguard.h"
 #include "mongo/util/timer.h"
 
 namespace mongo {
@@ -175,7 +177,16 @@ StatusWith<CachedCollectionRoutingInfo> CatalogCache::getCollectionRoutingInfo(
             ul.unlock();
 
             auto refreshStatus = [&]() {
+                Timer t;
+                ON_BLOCK_EXIT([&] { _stats.totalRefreshWaitTimeMicros.addAndFetch(t.micros()); });
+
                 try {
+                    const Milliseconds kReportingInterval{250};
+                    while (!refreshNotification->waitFor(opCtx, kReportingInterval)) {
+                        _stats.totalRefreshWaitTimeMicros.addAndFetch(t.micros());
+                        t.reset();
+                    }
+
                     return refreshNotification->get(opCtx);
                 } catch (const DBException& ex) {
                     return ex.toStatus();
@@ -214,6 +225,8 @@ StatusWith<CachedCollectionRoutingInfo> CatalogCache::getShardedCollectionRoutin
 }
 
 void CatalogCache::onStaleConfigError(CachedCollectionRoutingInfo&& ccriToInvalidate) {
+    _stats.countStaleConfigErrors.addAndFetch(1);
+
     // Ensure the move constructor of CachedCollectionRoutingInfo is invoked in order to clear the
     // input argument so it can't be used anymore
     auto ccri(ccriToInvalidate);
@@ -275,6 +288,25 @@ void CatalogCache::purgeAllDatabases() {
     _databases.clear();
 }
 
+void CatalogCache::report(BSONObjBuilder* builder) const {
+    BSONObjBuilder cacheStatsBuilder(builder->subobjStart("catalogCache"));
+
+    size_t numDatabaseEntries;
+    size_t numCollectionEntries{0};
+    {
+        stdx::lock_guard<stdx::mutex> ul(_mutex);
+        numDatabaseEntries = _databases.size();
+        for (const auto& entry : _databases) {
+            numCollectionEntries += entry.second->collections.size();
+        }
+    }
+
+    cacheStatsBuilder.append("numDatabaseEntries", static_cast<long long>(numDatabaseEntries));
+    cacheStatsBuilder.append("numCollectionEntries", static_cast<long long>(numCollectionEntries));
+
+    _stats.report(&cacheStatsBuilder);
+}
+
 std::shared_ptr<CatalogCache::DatabaseInfoEntry> CatalogCache::_getDatabase(OperationContext* opCtx,
                                                                             StringData dbName) {
     stdx::lock_guard<stdx::mutex> lg(_mutex);
@@ -316,15 +348,45 @@ void CatalogCache::_scheduleCollectionRefresh(WithLock lk,
                                               std::shared_ptr<ChunkManager> existingRoutingInfo,
                                               NamespaceString const& nss,
                                               int refreshAttempt) {
-    Timer t;
+    // If we have an existing chunk manager, the refresh is considered "incremental", regardless of
+    // how many chunks are in the differential
+    const bool isIncremental(existingRoutingInfo);
 
-    const ChunkVersion startingCollectionVersion =
-        (existingRoutingInfo ? existingRoutingInfo->getVersion() : ChunkVersion::UNSHARDED());
+    if (isIncremental) {
+        _stats.numActiveIncrementalRefreshes.addAndFetch(1);
+        _stats.countIncrementalRefreshesStarted.addAndFetch(1);
+    } else {
+        _stats.numActiveFullRefreshes.addAndFetch(1);
+        _stats.countFullRefreshesStarted.addAndFetch(1);
+    }
 
-    const auto refreshFailed =
-        [ this, t, dbEntry, nss, refreshAttempt ](WithLock lk, const Status& status) noexcept {
-        log() << "Refresh for collection " << nss << " took " << t.millis() << " ms and failed"
-              << causedBy(redact(status));
+    // Invoked when one iteration of getChunksSince has completed, whether with success or error
+    const auto onRefreshCompleted = [ this, t = Timer(), nss, isIncremental ](
+        const Status& status, ChunkManager* routingInfoAfterRefresh) {
+        if (isIncremental) {
+            _stats.numActiveIncrementalRefreshes.subtractAndFetch(1);
+        } else {
+            _stats.numActiveFullRefreshes.subtractAndFetch(1);
+        }
+
+        if (!status.isOK()) {
+            _stats.countFailedRefreshes.addAndFetch(1);
+
+            log() << "Refresh for collection " << nss << " took " << t.millis() << " ms and failed"
+                  << causedBy(redact(status));
+        } else if (routingInfoAfterRefresh) {
+            log() << "Refresh for collection " << nss << " took " << t.millis()
+                  << " ms and found version " << routingInfoAfterRefresh->getVersion();
+        } else {
+            log() << "Refresh for collection " << nss << " took " << t.millis()
+                  << " ms and found the collection is not sharded";
+        }
+    };
+
+    // Invoked if getChunksSince resulted in error
+    const auto onRefreshFailed = [ this, dbEntry, nss, refreshAttempt, onRefreshCompleted ](
+        WithLock lk, const Status& status) noexcept {
+        onRefreshCompleted(status, nullptr);
 
         auto& collections = dbEntry->collections;
         auto it = collections.find(nss.ns());
@@ -344,20 +406,24 @@ void CatalogCache::_scheduleCollectionRefresh(WithLock lk,
         }
     };
 
-    const auto refreshCallback = [ this, t, dbEntry, nss, existingRoutingInfo, refreshFailed ](
-        OperationContext * opCtx,
-        StatusWith<CatalogCacheLoader::CollectionAndChangedChunks> swCollAndChunks) noexcept {
+    const auto refreshCallback =
+        [ this, dbEntry, nss, existingRoutingInfo, onRefreshFailed, onRefreshCompleted ](
+            OperationContext * opCtx,
+            StatusWith<CatalogCacheLoader::CollectionAndChangedChunks> swCollAndChunks) noexcept {
         std::shared_ptr<ChunkManager> newRoutingInfo;
         try {
             newRoutingInfo = refreshCollectionRoutingInfo(
                 opCtx, nss, std::move(existingRoutingInfo), std::move(swCollAndChunks));
+
+            onRefreshCompleted(Status::OK(), newRoutingInfo.get());
         } catch (const DBException& ex) {
             stdx::lock_guard<stdx::mutex> lg(_mutex);
-            refreshFailed(lg, ex.toStatus());
+            onRefreshFailed(lg, ex.toStatus());
             return;
         }
 
         stdx::lock_guard<stdx::mutex> lg(_mutex);
+
         auto& collections = dbEntry->collections;
         auto it = collections.find(nss.ns());
         invariant(it != collections.end());
@@ -368,17 +434,14 @@ void CatalogCache::_scheduleCollectionRefresh(WithLock lk,
         collEntry.refreshCompletionNotification = nullptr;
 
         if (!newRoutingInfo) {
-            log() << "Refresh for collection " << nss << " took " << t.millis()
-                  << " and found the collection is not sharded";
-
             collections.erase(it);
         } else {
-            log() << "Refresh for collection " << nss << " took " << t.millis()
-                  << " ms and found version " << newRoutingInfo->getVersion();
-
             collEntry.routingInfo = std::move(newRoutingInfo);
         }
     };
+
+    const ChunkVersion startingCollectionVersion =
+        (existingRoutingInfo ? existingRoutingInfo->getVersion() : ChunkVersion::UNSHARDED());
 
     log() << "Refreshing chunks for collection " << nss << " based on version "
           << startingCollectionVersion;
@@ -394,8 +457,22 @@ void CatalogCache::_scheduleCollectionRefresh(WithLock lk,
         invariant(status != ErrorCodes::ConflictingOperationInProgress);
 
         stdx::lock_guard<stdx::mutex> lg(_mutex);
-        refreshFailed(lg, status);
+        onRefreshFailed(lg, status);
     }
+}
+
+void CatalogCache::Stats::report(BSONObjBuilder* builder) const {
+    builder->append("countStaleConfigErrors", countStaleConfigErrors.load());
+
+    builder->append("totalRefreshWaitTimeMicros", totalRefreshWaitTimeMicros.load());
+
+    builder->append("numActiveIncrementalRefreshes", numActiveIncrementalRefreshes.load());
+    builder->append("countIncrementalRefreshesStarted", countIncrementalRefreshesStarted.load());
+
+    builder->append("numActiveFullRefreshes", numActiveFullRefreshes.load());
+    builder->append("countFullRefreshesStarted", countFullRefreshesStarted.load());
+
+    builder->append("countFailedRefreshes", countFailedRefreshes.load());
 }
 
 CachedDatabaseInfo::CachedDatabaseInfo(std::shared_ptr<CatalogCache::DatabaseInfoEntry> db)

src/mongo/s/s_sharding_server_status.cpp
@@ -28,10 +28,10 @@
 
 #include "mongo/platform/basic.h"
 
-#include "mongo/base/init.h"
 #include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/db/commands/server_status.h"
 #include "mongo/s/balancer_configuration.h"
+#include "mongo/s/catalog_cache.h"
 #include "mongo/s/client/shard_registry.h"
 #include "mongo/s/grid.h"
 
@@ -48,28 +48,44 @@ public:
 
     BSONObj generateSection(OperationContext* opCtx,
                             const BSONElement& configElement) const override {
-        auto shardRegistry = Grid::get(opCtx)->shardRegistry();
-        invariant(shardRegistry);
+        auto const grid = Grid::get(opCtx);
+        auto const shardRegistry = grid->shardRegistry();
 
         BSONObjBuilder result;
+
         result.append("configsvrConnectionString",
                       shardRegistry->getConfigServerConnectionString().toString());
 
-        Grid::get(opCtx)->configOpTime().append(&result, "lastSeenConfigServerOpTime");
+        grid->configOpTime().append(&result, "lastSeenConfigServerOpTime");
 
-        long long maxChunkSizeInBytes =
-            Grid::get(opCtx)->getBalancerConfiguration()->getMaxChunkSizeBytes();
+        const long long maxChunkSizeInBytes =
+            grid->getBalancerConfiguration()->getMaxChunkSizeBytes();
         result.append("maxChunkSizeInBytes", maxChunkSizeInBytes);
 
         return result.obj();
     }
-};
 
-MONGO_INITIALIZER(ShardingServerStatusSection)(InitializerContext* context) {
-    new ShardingServerStatus();
+} shardingServerStatus;
 
-    return Status::OK();
-}
+class ShardingStatisticsServerStatus final : public ServerStatusSection {
+public:
+    ShardingStatisticsServerStatus() : ServerStatusSection("shardingStatistics") {}
+
+    bool includeByDefault() const override {
+        return true;
+    }
+
+    BSONObj generateSection(OperationContext* opCtx,
+                            const BSONElement& configElement) const override {
+        auto const grid = Grid::get(opCtx);
+        auto const catalogCache = grid->catalogCache();
+
+        BSONObjBuilder result;
+        catalogCache->report(&result);
+        return result.obj();
+    }
+
+} shardingStatisticsServerStatus;
 
 }  // namespace
 }  // namespace mongo

mongodb/mongo

(cherry picked from commit c4142a8e0b486f3642b700c9efb208f909e3ff1d)

src/mongo/db/s/sharding_state.cpp
@@ -49,6 +49,7 @@
 #include "mongo/db/s/operation_sharding_state.h"
 #include "mongo/db/s/sharded_connection_info.h"
 #include "mongo/db/s/sharding_initialization_mongod.h"
+#include "mongo/db/s/sharding_statistics.h"
 #include "mongo/db/s/type_shard_identity.h"
 #include "mongo/executor/network_interface_factory.h"
 #include "mongo/executor/network_interface_thread_pool.h"
@@ -229,6 +230,8 @@ Status ShardingState::onStaleShardVersion(OperationContext* txn,
 
     LOG(2) << "metadata refresh requested for " << nss.ns() << " at shard version "
            << expectedVersion;
+
+    ShardingStatistics::get(txn).countStaleConfigErrors.addAndFetch(1);
 
     // Ensure any ongoing migrations have completed
     auto& oss = OperationShardingState::get(txn);

src/mongo/s/catalog_cache.cpp
@@ -34,6 +34,7 @@
 
 #include "mongo/base/status.h"
 #include "mongo/base/status_with.h"
+#include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/db/operation_context.h"
 #include "mongo/db/query/collation/collator_factory_interface.h"
 #include "mongo/db/repl/optime_with.h"
@@ -45,6 +46,7 @@
 #include "mongo/s/grid.h"
 #include "mongo/stdx/memory.h"
 #include "mongo/util/log.h"
+#include "mongo/util/scopeguard.h"
 #include "mongo/util/timer.h"
 
 namespace mongo {
@@ -213,7 +215,16 @@ StatusWith<CachedCollectionRoutingInfo> CatalogCache::getCollectionRoutingInfo(
             ul.unlock();
 
             auto refreshStatus = [&]() {
+                Timer t;
+                ON_BLOCK_EXIT([&] { _stats.totalRefreshWaitTimeMicros.addAndFetch(t.micros()); });
+
                 try {
+                    const Milliseconds kReportingInterval{250};
+                    while (!refreshNotification->waitFor(opCtx, kReportingInterval)) {
+                        _stats.totalRefreshWaitTimeMicros.addAndFetch(t.micros());
+                        t.reset();
+                    }
+
                     return refreshNotification->get(opCtx);
                 } catch (const DBException& ex) {
                     return ex.toStatus();
@@ -256,6 +267,8 @@ StatusWith<CachedCollectionRoutingInfo> CatalogCache::getShardedCollectionRoutin
 }
 
 void CatalogCache::onStaleConfigError(CachedCollectionRoutingInfo&& ccriToInvalidate) {
+    _stats.countStaleConfigErrors.addAndFetch(1);
+
     // Ensure the move constructor of CachedCollectionRoutingInfo is invoked in order to clear the
     // input argument so it can't be used anymore
     auto ccri(ccriToInvalidate);
@@ -327,6 +340,25 @@ void CatalogCache::purgeAllDatabases() {
     _databases.clear();
 }
 
+void CatalogCache::report(BSONObjBuilder* builder) const {
+    BSONObjBuilder cacheStatsBuilder(builder->subobjStart("catalogCache"));
+
+    size_t numDatabaseEntries;
+    size_t numCollectionEntries{0};
+    {
+        stdx::lock_guard<stdx::mutex> ul(_mutex);
+        numDatabaseEntries = _databases.size();
+        for (const auto& entry : _databases) {
+            numCollectionEntries += entry.second->collections.size();
+        }
+    }
+
+    cacheStatsBuilder.append("numDatabaseEntries", static_cast<long long>(numDatabaseEntries));
+    cacheStatsBuilder.append("numCollectionEntries", static_cast<long long>(numCollectionEntries));
+
+    _stats.report(&cacheStatsBuilder);
+}
+
 std::shared_ptr<CatalogCache::DatabaseInfoEntry> CatalogCache::_getDatabase(OperationContext* opCtx,
                                                                             StringData dbName) {
     stdx::lock_guard<stdx::mutex> lg(_mutex);
@@ -368,15 +400,47 @@ void CatalogCache::_scheduleCollectionRefresh_inlock(
     std::shared_ptr<ChunkManager> existingRoutingInfo,
     const NamespaceString& nss,
     int refreshAttempt) {
+
+    // If we have an existing chunk manager, the refresh is considered "incremental", regardless of
+    // how many chunks are in the differential
+    const bool isIncremental(existingRoutingInfo);
+
+    if (isIncremental) {
+        _stats.numActiveIncrementalRefreshes.addAndFetch(1);
+        _stats.countIncrementalRefreshesStarted.addAndFetch(1);
+    } else {
+        _stats.numActiveFullRefreshes.addAndFetch(1);
+        _stats.countFullRefreshesStarted.addAndFetch(1);
+    }
+
     Timer t;
+    // Invoked when one iteration of getChunksSince has completed, whether with success or error
+    const auto onRefreshCompleted =
+        [this, t, nss, isIncremental](const Status& status, ChunkManager* routingInfoAfterRefresh) {
+            if (isIncremental) {
+                _stats.numActiveIncrementalRefreshes.subtractAndFetch(1);
+            } else {
+                _stats.numActiveFullRefreshes.subtractAndFetch(1);
+            }
 
-    const ChunkVersion startingCollectionVersion =
-        (existingRoutingInfo ? existingRoutingInfo->getVersion() : ChunkVersion::UNSHARDED());
+            if (!status.isOK()) {
+                _stats.countFailedRefreshes.addAndFetch(1);
 
+                log() << "Refresh for collection " << nss << " took " << t.millis()
+                      << " ms and failed" << causedBy(redact(status));
+            } else if (routingInfoAfterRefresh) {
+                log() << "Refresh for collection " << nss << " took " << t.millis()
+                      << " ms and found version " << routingInfoAfterRefresh->getVersion();
+            } else {
+                log() << "Refresh for collection " << nss << " took " << t.millis()
+                      << " ms and found the collection is not sharded";
+            }
+        };
+
+    // Invoked if getChunksSince resulted in error
     const auto refreshFailed_inlock =
-        [ this, t, dbEntry, nss, refreshAttempt ](const Status& status) noexcept {
-        log() << "Refresh for collection " << nss << " took " << t.millis() << " ms and failed"
-              << causedBy(redact(status));
+        [ this, dbEntry, nss, refreshAttempt, onRefreshCompleted ](const Status& status) noexcept {
+        onRefreshCompleted(status, nullptr);
 
         auto& collections = dbEntry->collections;
         auto it = collections.find(nss.ns());
@@ -397,13 +461,15 @@ void CatalogCache::_scheduleCollectionRefresh_inlock(
     };
 
     const auto refreshCallback =
-        [ this, t, dbEntry, nss, existingRoutingInfo, refreshFailed_inlock ](
+        [ this, dbEntry, nss, existingRoutingInfo, refreshFailed_inlock, onRefreshCompleted ](
             OperationContext * opCtx,
             StatusWith<CatalogCacheLoader::CollectionAndChangedChunks> swCollAndChunks) noexcept {
         std::shared_ptr<ChunkManager> newRoutingInfo;
         try {
             newRoutingInfo = refreshCollectionRoutingInfo(
                 opCtx, nss, std::move(existingRoutingInfo), std::move(swCollAndChunks));
+
+            onRefreshCompleted(Status::OK(), newRoutingInfo.get());
         } catch (const DBException& ex) {
             stdx::lock_guard<stdx::mutex> lg(_mutex);
             refreshFailed_inlock(ex.toStatus());
@@ -411,6 +477,7 @@ void CatalogCache::_scheduleCollectionRefresh_inlock(
         }
 
         stdx::lock_guard<stdx::mutex> lg(_mutex);
+
         auto& collections = dbEntry->collections;
         auto it = collections.find(nss.ns());
         invariant(it != collections.end());
@@ -421,17 +488,14 @@ void CatalogCache::_scheduleCollectionRefresh_inlock(
         collEntry.refreshCompletionNotification = nullptr;
 
         if (!newRoutingInfo) {
-            log() << "Refresh for collection " << nss << " took " << t.millis()
-                  << " and found the collection is not sharded";
-
             collections.erase(it);
         } else {
-            log() << "Refresh for collection " << nss << " took " << t.millis()
-                  << " ms and found version " << newRoutingInfo->getVersion();
-
             collEntry.routingInfo = std::move(newRoutingInfo);
         }
     };
+
+    const ChunkVersion startingCollectionVersion =
+        (existingRoutingInfo ? existingRoutingInfo->getVersion() : ChunkVersion::UNSHARDED());
 
     log() << "Refreshing chunks for collection " << nss << " based on version "
           << startingCollectionVersion;
@@ -449,6 +513,20 @@ void CatalogCache::_scheduleCollectionRefresh_inlock(
         stdx::lock_guard<stdx::mutex> lg(_mutex);
         refreshFailed_inlock(status);
     }
+}
+
+void CatalogCache::Stats::report(BSONObjBuilder* builder) const {
+    builder->append("countStaleConfigErrors", countStaleConfigErrors.load());
+
+    builder->append("totalRefreshWaitTimeMicros", totalRefreshWaitTimeMicros.load());
+
+    builder->append("numActiveIncrementalRefreshes", numActiveIncrementalRefreshes.load());
+    builder->append("countIncrementalRefreshesStarted", countIncrementalRefreshesStarted.load());
+
+    builder->append("numActiveFullRefreshes", numActiveFullRefreshes.load());
+    builder->append("countFullRefreshesStarted", countFullRefreshesStarted.load());
+
+    builder->append("countFailedRefreshes", countFailedRefreshes.load());
 }
 
 CachedDatabaseInfo::CachedDatabaseInfo(std::shared_ptr<CatalogCache::DatabaseInfoEntry> db)

src/mongo/s/s_sharding_server_status.cpp
@@ -30,6 +30,8 @@
 
 #include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/db/commands/server_status.h"
+#include "mongo/s/balancer_configuration.h"
+#include "mongo/s/catalog_cache.h"
 #include "mongo/s/client/shard_registry.h"
 #include "mongo/s/grid.h"
 
@@ -39,35 +41,52 @@ namespace {
 
 class ShardingServerStatus : public ServerStatusSection {
 public:
-    ShardingServerStatus();
+    ShardingServerStatus() : ServerStatusSection("sharding") {}
 
-    bool includeByDefault() const final;
+    bool includeByDefault() const override {
+        return true;
+    }
 
-    BSONObj generateSection(OperationContext* txn, const BSONElement& configElement) const final;
-};
+    BSONObj generateSection(OperationContext* txn,
+                            const BSONElement& configElement) const override {
+        auto const grid = Grid::get(txn);
+        auto const shardRegistry = grid->shardRegistry();
+
+        BSONObjBuilder result;
+
+        result.append("configsvrConnectionString",
+                      shardRegistry->getConfigServerConnectionString().toString());
+
+        grid->configOpTime().append(&result, "lastSeenConfigServerOpTime");
+
+        const long long maxChunkSizeInBytes =
+            grid->getBalancerConfiguration()->getMaxChunkSizeBytes();
+        result.append("maxChunkSizeInBytes", maxChunkSizeInBytes);
+
+        return result.obj();
+    }
+
+} shardingServerStatus;
+
+class ShardingStatisticsServerStatus final : public ServerStatusSection {
+public:
+    ShardingStatisticsServerStatus() : ServerStatusSection("shardingStatistics") {}
+
+    bool includeByDefault() const override {
+        return true;
+    }
+
+    BSONObj generateSection(OperationContext* txn,
+                            const BSONElement& configElement) const override {
+        auto const grid = Grid::get(txn);
+        auto const catalogCache = grid->catalogCache();
+
+        BSONObjBuilder result;
+        catalogCache->report(&result);
+        return result.obj();
+    }
+
+} shardingStatisticsServerStatus;
 
 }  // namespace
-
-ShardingServerStatus shardingServerStatus;
-
-ShardingServerStatus::ShardingServerStatus() : ServerStatusSection("sharding") {}
-
-bool ShardingServerStatus::includeByDefault() const {
-    return true;
-}
-
-// This implementation runs on mongoS.
-BSONObj ShardingServerStatus::generateSection(OperationContext* txn,
-                                              const BSONElement& configElement) const {
-    invariant(grid.shardRegistry());
-
-    BSONObjBuilder result;
-    result.append("configsvrConnectionString",
-                  grid.shardRegistry()->getConfigServerConnectionString().toString());
-
-    grid.configOpTime().append(&result, "lastSeenConfigServerOpTime");
-
-    return result.obj();
-}
-
 }  // namespace mongo