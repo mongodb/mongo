c948a158d2ba4362906cc2b3cae9fa07b5e57ff4
Misha Tyulenev <misha@mongodb.com>
2018-04-04 20:20:14 -0400

    SERVER-33767 verify targeted shard for global snapshot reads with multi-version routing table

--- src/mongo/s/commands/cluster_commands_helpers.cpp
+++ src/mongo/s/commands/cluster_commands_helpers.cpp
@@ -557,8 +557,31 @@ BSONObj appendAtClusterTimeToReadConcern(BSONObj readConcernObj, LogicalTime atC
     return readConcernBob.obj();
 }
 
-LogicalTime computeAtClusterTimeForShards(OperationContext* opCtx,
-                                          const std::set<ShardId>& shardIds) {
+boost::optional<LogicalTime> computeAtClusterTimeForOneShard(OperationContext* opCtx,
+                                                             const ShardId& shardId) {
+
+    if (repl::ReadConcernArgs::get(opCtx).getLevel() !=
+        repl::ReadConcernLevel::kSnapshotReadConcern) {
+        return boost::none;
+    }
+
+    auto shardRegistry = Grid::get(opCtx)->shardRegistry();
+    invariant(shardRegistry);
+    return shardRegistry->getShardNoReload(shardId)->getLastCommittedOpTime();
+}
+
+boost::optional<LogicalTime> computeAtClusterTime(OperationContext* opCtx,
+                                                  bool mustRunOnAll,
+                                                  const std::set<ShardId>& shardIds,
+                                                  const NamespaceString& nss,
+                                                  const BSONObj query,
+                                                  const BSONObj collation) {
+
+    if (repl::ReadConcernArgs::get(opCtx).getLevel() !=
+        repl::ReadConcernLevel::kSnapshotReadConcern) {
+        return boost::none;
+    }
+
     auto shardRegistry = Grid::get(opCtx)->shardRegistry();
     invariant(shardRegistry);
     LogicalTime highestTime;
@@ -567,9 +590,36 @@ LogicalTime computeAtClusterTimeForShards(OperationContext* opCtx,
             shardRegistry->getShardNoReload(shardId)->getLastCommittedOpTime();
         if (lastCommittedOpTime > highestTime) {
             highestTime = lastCommittedOpTime;
+        } else if (lastCommittedOpTime == LogicalTime::kUninitialized) {
+            // Use current cluster time if at least one of targeted shards does not have the
+            // accurate lastCommittedOpTime.
+            highestTime = LogicalClock::get(opCtx)->getClusterTime();
         }
     }
-    return highestTime;
+
+    // mustRunOnAll is also used in tests to bypass getting the routing table when it is not a part
+    // of the fixture.
+    if (mustRunOnAll) {
+        return highestTime;
+    }
+
+    auto routingInfo = uassertStatusOK(Grid::get(opCtx)->catalogCache()->getCollectionRoutingInfoAt(
+        opCtx, nss, highestTime.asTimestamp()));
+
+    const auto cm = routingInfo.cm();
+    if (!cm) {  // The collection is unsharded and received the primary shardId.
+        return highestTime;
+    }
+
+    // Verify that the at the highestTime the shardIds were the same. If any of the targeted
+    // chunks have been migrated since the highestTime the query may return false empty results.
+    std::set<ShardId> shardIdsAt = {};
+    cm->getShardIdsForQuery(opCtx, query, collation, &shardIdsAt);
+    if (shardIds == shardIdsAt) {
+        return highestTime;
+    }
+
+    return LogicalClock::get(opCtx)->getClusterTime();
 }
 
 std::set<ShardId> getTargetedShardsForQuery(OperationContext* opCtx,
@@ -586,20 +636,6 @@ std::set<ShardId> getTargetedShardsForQuery(OperationContext* opCtx,
 
     // The collection is unsharded. Target only the primary shard for the database.
     return {routingInfo.db().primaryId()};
-}
-
-boost::optional<LogicalTime> computeAtClusterTime(OperationContext* opCtx,
-                                                  const CachedCollectionRoutingInfo& routingInfo,
-                                                  const std::set<ShardId>& shardIds,
-                                                  const BSONObj& query,
-                                                  const BSONObj& collation) {
-    if (repl::ReadConcernArgs::get(opCtx).getLevel() !=
-        repl::ReadConcernLevel::kSnapshotReadConcern) {
-        return boost::none;
-    }
-
-    // TODO SERVER-33767: Integrate multi-versioned routing table into atClusterTime selection.
-    return LogicalClock::get(opCtx)->getClusterTime();
 }
 
 }  // namespace mongo
