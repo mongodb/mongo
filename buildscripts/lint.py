4500059d1af9cfc732eeb2e78d865e2e9d9d9e95
Jonathan Abrahams <jonathan@mongodb.com>
2018-04-09 12:18:58 -0400

    SERVER-23312 Fix running buildscripts/lint.py as a script

--- buildscripts/lint.py
+++ buildscripts/lint.py
@@ -1,12 +1,18 @@
 """Lint module."""
 
+from __future__ import absolute_import
 from __future__ import print_function
 
-import sys
 import codecs
+import os
+import sys
 
-import buildscripts.cpplint as cpplint
-import buildscripts.utils as utils
+# Get relative imports to work when the package is not installed on the PYTHONPATH.
+if __name__ == "__main__" and __package__ is None:
+    sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import buildscripts.cpplint as cpplint  # pylint: disable=wrong-import-position
+import buildscripts.utils as utils  # pylint: disable=wrong-import-position
 
 
 class CheckForConfigH(object):
