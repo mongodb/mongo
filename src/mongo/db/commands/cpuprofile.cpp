c30e4d2225120479077e8a78f3648d284215d9aa
Ben Caimano <ben.caimano@10gen.com>
2018-04-12 15:03:50 -0400

    SERVER-35181 Corrected function signatures for CpuProfilerCommand
    Note that this also adds a RHEL 7.0 variant specifically for Dagger
    and gperftools

--- src/mongo/db/commands/cpuprofile.cpp
+++ src/mongo/db/commands/cpuprofile.cpp
@@ -71,7 +71,7 @@ namespace {
 class CpuProfilerCommand : public ErrmsgCommandDeprecated {
 public:
     CpuProfilerCommand(char const* name) : ErrmsgCommandDeprecated(name) {}
-    AllowedOnSecondary secondaryAllowed() const override {
+    AllowedOnSecondary secondaryAllowed(ServiceContext* context) const override {
         return AllowedOnSecondary::kAlways;
     }
     bool adminOnly() const override {
@@ -82,7 +82,7 @@ public:
     }
     void addRequiredPrivileges(const std::string& dbname,
                                const BSONObj& cmdObj,
-                               std::vector<Privilege>* out) override {
+                               std::vector<Privilege>* out) const override {
         ActionSet actions;
         actions.addAction(ActionType::cpuProfiler);
         out->push_back(Privilege(ResourcePattern::forClusterResource(), actions));
