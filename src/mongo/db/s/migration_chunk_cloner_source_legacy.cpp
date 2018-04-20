2dff7f1f50540af8aa1b982b41b4dd752068c89e
Matthew Saltz <matthew.saltz@mongodb.com>
2018-04-20 12:30:42 -0400

    SERVER-34571 Ensure _deleteNotifyExec in MigrationChunkClonerSourceLegacy always reset on cleanup
    (cherry picked from commit 179985c786cea234b65946ff647debfdfdbed511)

--- src/mongo/db/s/migration_chunk_cloner_source_legacy.cpp
+++ src/mongo/db/s/migration_chunk_cloner_source_legacy.cpp
@@ -533,13 +533,17 @@ void MigrationChunkClonerSourceLegacy::_cleanup(OperationContext* opCtx) {
         _reload.clear();
         _deleted.clear();
     }
+    // Implicitly resets _deleteNotifyExec to avoid possible invariant failure
+    // in on destruction of MigrationChunkClonerSourceLegacy, and will always
+    // call deleteNotifyExec destructor on scope exit even if something in the
+    // below if statement fails
+    auto deleteNotifyExec = std::move(_deleteNotifyExec);
 
-    if (_deleteNotifyExec) {
+    if (deleteNotifyExec) {
         AutoGetCollection autoColl(opCtx, _args.getNss(), MODE_IS);
         const auto cursorManager =
             autoColl.getCollection() ? autoColl.getCollection()->getCursorManager() : nullptr;
-        _deleteNotifyExec->dispose(opCtx, cursorManager);
-        _deleteNotifyExec.reset();
+        deleteNotifyExec->dispose(opCtx, cursorManager);
     }
 }
 
