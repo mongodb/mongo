5e118de9148bf95e7ca2de6169fcf20a071d59b4
William Schultz <william.schultz@mongodb.com>
2018-04-24 11:47:16 -0400

    SERVER-34259 Convert start_transaction_with_read.js to use transactions shell API

--- jstests/core/txns/start_transaction_with_read.js
+++ jstests/core/txns/start_transaction_with_read.js
@@ -12,7 +12,6 @@
     testDB.runCommand({drop: collName, writeConcern: {w: "majority"}});
 
     testDB.runCommand({create: coll.getName(), writeConcern: {w: "majority"}});
-    let txnNumber = 0;
 
     const sessionOptions = {causalConsistency: false};
     const session = testDB.getMongo().startSession(sessionOptions);
@@ -24,46 +23,25 @@
     assert.writeOK(sessionColl.insert(initialDoc, {writeConcern: {w: "majority"}}));
 
     jsTest.log("Start a transaction with a read");
-    let res = assert.commandWorked(sessionDb.runCommand({
-        find: collName,
-        batchSize: 10,
-        txnNumber: NumberLong(txnNumber),
-        readConcern: {level: "snapshot"},
-        startTransaction: true,
-        autocommit: false
-    }));
-    assert.eq(res.cursor.firstBatch, [initialDoc]);
+
+    session.startTransaction();
+
+    let docs = sessionColl.find({}).toArray();
+    assert.eq(docs, [initialDoc]);
 
     jsTest.log("Insert two documents in a transaction");
+
     // Insert a doc within the transaction.
-    assert.commandWorked(sessionDb.runCommand({
-        insert: collName,
-        documents: [{_id: "insert-1"}],
-        txnNumber: NumberLong(txnNumber),
-        autocommit: false
-    }));
+    assert.commandWorked(sessionColl.insert({_id: "insert-1"}));
 
     // Read in the same transaction returns the doc.
-    res = sessionDb.runCommand({
-        find: collName,
-        filter: {_id: "insert-1"},
-        txnNumber: NumberLong(txnNumber),
-        autocommit: false
-    });
-    assert.commandWorked(res);
-    assert.docEq([{_id: "insert-1"}], res.cursor.firstBatch);
+    docs = sessionColl.find({_id: "insert-1"}).toArray();
+    assert.docEq(docs, [{_id: "insert-1"}]);
 
     // Insert a doc within a transaction.
-    assert.commandWorked(sessionDb.runCommand({
-        insert: collName,
-        documents: [{_id: "insert-2"}],
-        txnNumber: NumberLong(txnNumber),
-        autocommit: false
-    }));
+    assert.commandWorked(sessionColl.insert({_id: "insert-2"}));
 
-    // commitTransaction can only be run on the admin database.
-    assert.commandWorked(sessionDb.adminCommand(
-        {commitTransaction: 1, txnNumber: NumberLong(txnNumber), autocommit: false}));
+    session.commitTransaction();
 
     // Read with default read concern sees the committed transaction.
     assert.eq({_id: "insert-1"}, coll.findOne({_id: "insert-1"}));
