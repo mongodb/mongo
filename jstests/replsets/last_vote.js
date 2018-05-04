381acc4baa6d5f730fb77be5acc39f8473a16b88
William Schultz <william.schultz@mongodb.com>
2018-05-03 22:05:05 -0400

    SERVER-34557 last_vote.js needs to wait for stable node before reading oplog

--- jstests/replsets/last_vote.js
+++ jstests/replsets/last_vote.js
@@ -113,6 +113,8 @@
 
     jsTestLog("Restarting node 0 in replica set mode");
     node0 = rst.restart(0);  // Restart in replSet mode again.
+    rst.waitForState(node0, ReplSetTest.State.SECONDARY);
+
     assert.soonNoExcept(function() {
         assertCurrentTerm(node0, term);
         return true;
