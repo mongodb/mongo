4129d16840b66f42b3027df485c026fa7a80b40e
Jonathan Abrahams <jonathan@mongodb.com>
2018-05-08 16:19:40 -0400

    SERVER-34680 - Add mongoe to bypass_compile artifacts tarball

--- buildscripts/bypass_compile_and_fetch_binaries.py
+++ buildscripts/bypass_compile_and_fetch_binaries.py
@@ -269,6 +269,7 @@ def main():  # pylint: disable=too-many-locals,too-many-statements
                 extract_files = [
                     executable_name("dbtest"),
                     executable_name("mongobridge"),
+                    executable_name("mongoe"),
                     "build/integration_tests.txt",
                 ]
                 with tarfile.open(filename, "r:gz") as tar:
