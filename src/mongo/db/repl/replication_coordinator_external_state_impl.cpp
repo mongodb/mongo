4f9bdd18edb24c66fbb587833bff8c1bcba91c80
Matthew Russotto <matthew.russotto@10gen.com>
2018-05-09 13:51:25 -0400

    SERVER-34362 ReplicaSetCoordinatorExternalStateImpl::_dropAllTempCollections() needs to log failed db.

--- src/mongo/db/repl/replication_coordinator_external_state_impl.cpp
+++ src/mongo/db/repl/replication_coordinator_external_state_impl.cpp
@@ -796,7 +796,7 @@ void ReplicationCoordinatorExternalStateImpl::_dropAllTempCollections(OperationC
         // Since we must be holding the global lock during this function, if listDatabases
         // returned this dbname, we should be able to get a reference to it - it can't have
         // been dropped.
-        invariant(db);
+        invariant(db, str::stream() << "Unable to get reference to database " << *it);
         db->clearTmpCollections(opCtx);
     }
 }
