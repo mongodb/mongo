ada1fe5ab9c62910fd811f44955bbf921b899676
Suganthi Mani <suganthi.mani@mongodb.com>
2018-05-21 17:43:02 -0400

    SERVER-35087 Disabling read_concern_uninitated_set.js for storage engines (ephemeralForTest & mmapv1) which do not support majority read concern.
    (cherry picked from commit da70d5b25f0807cd03bc74ff16c4377a22f4605a)
    (cherry picked from commit 6ee59a9d5b49d4ca81102b99063a30328f7f9b4c)

--- jstests/replsets/read_concern_uninitated_set.js
+++ jstests/replsets/read_concern_uninitated_set.js
@@ -4,6 +4,13 @@
  */
 (function() {
     "use strict";
+    // For supportsMajorityReadConcern().
+    load("jstests/multiVersion/libs/causal_consistency_helpers.js");
+
+    if (!supportsMajorityReadConcern()) {
+        jsTestLog("Skipping test since storage engine doesn't support majority read concern.");
+        return;
+    }
 
     const rst = new ReplSetTest({nodes: 1});
     rst.startSet();
