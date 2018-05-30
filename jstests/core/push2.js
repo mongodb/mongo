c29e88c561a449ed4a6e2638fec5b0e1c96e02c4
Louis Williams <louis.williams@mongodb.com>
2018-05-30 14:29:04 -0400

    SERVER-35228 Use an immediately-invoked function with an early return instead of using quit()

--- jstests/core/push2.js
+++ jstests/core/push2.js
@@ -1,26 +1,28 @@
-// TODO: Remove this for SERVER-35237 to be resolved.
-if (jsTest.options().storageEngine === 'wiredTiger') {
-    jsTest.log('not running test on wiredTiger storage engine');
-    quit();
-}
+(function() {
+    // TODO: Remove this for SERVER-35237 to be resolved.
+    if (jsTest.options().storageEngine === 'wiredTiger') {
+        jsTest.log('not running test on wiredTiger storage engine');
+        return;
+    }
 
-t = db.push2;
-t.drop();
+    t = db.push2;
+    t.drop();
 
-t.save({_id: 1, a: []});
+    t.save({_id: 1, a: []});
 
-s = new Array(700000).toString();
+    s = new Array(700000).toString();
 
-gotError = null;
+    gotError = null;
 
-for (x = 0; x < 100; x++) {
-    print(x + " pushes");
-    var res = t.update({}, {$push: {a: s}});
-    gotError = res.hasWriteError();
-    if (gotError)
-        break;
-}
+    for (x = 0; x < 100; x++) {
+        print(x + " pushes");
+        var res = t.update({}, {$push: {a: s}});
+        gotError = res.hasWriteError();
+        if (gotError)
+            break;
+    }
 
-assert(gotError, "should have gotten error");
+    assert(gotError, "should have gotten error");
 
-t.drop();
+    t.drop();
+})();
