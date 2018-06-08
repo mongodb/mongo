5445067793ec31b5dbdf7076fe710586f9a63414
Ben Caimano <ben.caimano@10gen.com>
2018-06-08 18:53:14 -0400

    SERVER-35517 Add failpoint mechanism to the mongo shell
    (cherry picked from commit d1ef07466b2da86ad1933c1e607ef3f3364ba100)

--- src/mongo/util/fail_point_service.cpp
+++ src/mongo/util/fail_point_service.cpp
@@ -26,9 +26,14 @@
  *    then also delete it in the license file.
  */
 
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kCommand
+
 #include "mongo/platform/basic.h"
 
 #include "mongo/util/fail_point_service.h"
+
+#include "mongo/util/assert_util.h"
+#include "mongo/util/log.h"
 
 namespace mongo {
 
@@ -52,6 +57,22 @@ MONGO_INITIALIZER_GENERAL(AllFailPointsRegistered, MONGO_NO_PREREQUISITES, MONGO
 
 FailPointRegistry* getGlobalFailPointRegistry() {
     return _fpRegistry.get();
+}
+
+void setGlobalFailPoint(const std::string& failPointName, const BSONObj& cmdObj) {
+    FailPointRegistry* registry = getGlobalFailPointRegistry();
+    FailPoint* failPoint = registry->getFailPoint(failPointName);
+
+    if (failPoint == nullptr)
+        uasserted(ErrorCodes::FailPointSetFailed, failPointName + " not found");
+
+    FailPoint::Mode mode;
+    FailPoint::ValType val;
+    BSONObj data;
+    std::tie(mode, val, data) = uassertStatusOK(FailPoint::parseBSON(cmdObj));
+
+    failPoint->setMode(mode, val, data);
+    warning() << "failpoint: " << failPointName << " set to: " << failPoint->toBSON();
 }
 
 FailPointEnableBlock::FailPointEnableBlock(const std::string& failPointName) {
