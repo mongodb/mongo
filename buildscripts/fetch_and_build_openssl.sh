4e614564d032bc9133ac46319b588b4b62cee304
Billy Donahue <billy.donahue@mongodb.com>
2018-06-15 13:12:46 -0400

    SERVER-35620 Use 'shasum -a 256' for tarball integrity.
    Instead of using in-house Python.
    Refactor fetch_and_build_openssl.sh
    
    workaround for old shasum stdin bug

--- buildscripts/fetch_and_build_openssl.sh
+++ buildscripts/fetch_and_build_openssl.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 set -o errexit
-set -o verbose
+#set -o verbose
 
 if [ $# -ne 3 ]; then
-    echo "Arguments: <python command> <make flags> <config flags>"
+    echo "Arguments: <python command> <make flags> <config flags>" >&2
     exit 3
 fi
 
-PYTHON=$1
-OPENSSL_MAKE_FLAGS=$2
-OPENSSL_CONFIG_FLAGS=$3
+PYTHON="$1"   # not needed anymore
+OPENSSL_MAKE_FLAGS="$2"
+OPENSSL_CONFIG_FLAGS="$3"
 
 OPENSSL_VERSION=1.1.0h
 OPENSSL_NAME=openssl-${OPENSSL_VERSION}
 OPENSSL_TARBALL=${OPENSSL_NAME}.tar.gz
+
+basedir="$(pwd)"
+
 mkdir -p openssl
-curl -L -o openssl/${OPENSSL_TARBALL} https://s3.amazonaws.com/boxes.10gen.com/build/${OPENSSL_TARBALL}
-LOCAL_HASH="$(${PYTHON} buildscripts/sha256sum.py openssl/${OPENSSL_TARBALL})"
-echo ${LOCAL_HASH}
+pushd openssl
+curl -L -o ${OPENSSL_TARBALL} \
+    "https://s3.amazonaws.com/boxes.10gen.com/build/${OPENSSL_TARBALL}"
 
-if [ "$LOCAL_HASH" != "5835626cde9e99656585fc7aaa2302a73a7e1340bf8c14fd635a62c66802a517" ]
-then
-    exit 3
-fi
+# To regenerate: shasum -a 256 -b $OPENSSL_TARBALL
+shasum -c /dev/fd/0 <<EOF || exit 3
+5835626cde9e99656585fc7aaa2302a73a7e1340bf8c14fd635a62c66802a517 *$OPENSSL_TARBALL
+EOF
 
-cd openssl
-tar -xvzf ${OPENSSL_TARBALL} --strip-components 1
-./config no-shared --prefix=${PWD}/../openssl_install_dir ${OPENSSL_CONFIG_FLAGS}
+tar -xzf ${OPENSSL_TARBALL}
+
+mkdir -p build
+pushd build
+../$OPENSSL_NAME/config no-shared --prefix="$basedir/openssl_install_dir" ${OPENSSL_CONFIG_FLAGS}
 make ${OPENSSL_MAKE_FLAGS}
 make install_sw
+popd # build
+
+popd # openssl
