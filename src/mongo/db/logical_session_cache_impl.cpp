3940964838cd54c302edb37c49a981c5547c4ae8
Jason Carey <jcarey@argv.me>
2018-06-20 14:08:35 -0400

    SERVER-35706 ctor race in LogicalSessionCacheImpl
    The constructor for LogicalSessionCacheImpl writes to fields after it
    has kicked off periodic jobs that also write to those fields.  It
    doesn't do it for any good reason, so re-order to fix the race.
    
    (cherry picked from commit af7c50e0ac57f50ba258177f0fbd48b8a837001e)

--- src/mongo/db/logical_session_cache_impl.cpp
+++ src/mongo/db/logical_session_cache_impl.cpp
@@ -66,6 +66,9 @@ LogicalSessionCacheImpl::LogicalSessionCacheImpl(
       _service(std::move(service)),
       _sessionsColl(std::move(collection)),
       _transactionReaper(std::move(transactionReaper)) {
+    _stats.setLastSessionsCollectionJobTimestamp(now());
+    _stats.setLastTransactionReaperJobTimestamp(now());
+
     if (!disableLogicalSessionCacheRefresh) {
         _service->scheduleJob({"LogicalSessionCacheRefresh",
                                [this](Client* client) { _periodicRefresh(client); },
@@ -76,8 +79,6 @@ LogicalSessionCacheImpl::LogicalSessionCacheImpl(
                                    _refreshInterval});
         }
     }
-    _stats.setLastSessionsCollectionJobTimestamp(now());
-    _stats.setLastTransactionReaperJobTimestamp(now());
 }
 
 LogicalSessionCacheImpl::~LogicalSessionCacheImpl() {
