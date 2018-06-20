97f19510c79eb405cb00b8b811804a2f9f31e295
Samy Lanka <samy.lanka@gmail.com>
2018-06-20 11:25:17 -0400

    SERVER-35239 AwaitData cursor must handle getMore from client with higher lastKnownCommittedOpTime
    (cherry picked from commit 2c6e1b2fa22cbee1129409635cceedd7f99bfea1)

--- src/mongo/db/query/plan_executor.h
+++ src/mongo/db/query/plan_executor.h
@@ -469,6 +469,12 @@ public:
 
 private:
     /**
+     * Returns true if the PlanExecutor should listen for inserts, which is when a getMore is called
+     * on a tailable and awaitData cursor that still has time left and hasn't been interrupted.
+     */
+    bool shouldListenForInserts();
+
+    /**
      * Returns true if the PlanExecutor should wait for data to be inserted, which is when a getMore
      * is called on a tailable and awaitData cursor on a capped collection.  Returns false if an EOF
      * should be returned immediately.
