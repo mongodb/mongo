a6eae4759b792f86284006d475b00559fe96953c
David Storch <david.storch@10gen.com>
2018-06-20 14:17:47 -0400

    SERVER-35693 Pre-sort $in elements to avoid O(n^2) boost::flat_set() complexity.

--- src/mongo/db/matcher/expression_leaf.cpp
+++ src/mongo/db/matcher/expression_leaf.cpp
@@ -477,6 +477,11 @@ void InMatchExpression::_doSetCollator(const CollatorInterface* collator) {
     _collator = collator;
     _eltCmp = BSONElementComparator(BSONElementComparator::FieldNamesMode::kIgnore, _collator);
 
+    // Re-sort the list of equalities according to our current comparator. This is necessary to work
+    // around https://svn.boost.org/trac10/ticket/13140.
+    std::sort(
+        _originalEqualityVector.begin(), _originalEqualityVector.end(), _eltCmp.makeLessThan());
+
     // We need to re-compute '_equalitySet', since our set comparator has changed.
     _equalitySet = _eltCmp.makeBSONEltFlatSet(_originalEqualityVector);
 }
@@ -496,7 +501,12 @@ Status InMatchExpression::setEqualities(std::vector<BSONElement> equalities) {
             _hasEmptyArray = true;
         }
     }
+
     _originalEqualityVector = std::move(equalities);
+
+    // Sort the list of equalities to work around https://svn.boost.org/trac10/ticket/13140.
+    std::sort(
+        _originalEqualityVector.begin(), _originalEqualityVector.end(), _eltCmp.makeLessThan());
 
     _equalitySet = _eltCmp.makeBSONEltFlatSet(_originalEqualityVector);
 
--- src/mongo/db/matcher/expression_leaf.h
+++ src/mongo/db/matcher/expression_leaf.h
@@ -457,6 +457,12 @@ private:
 
     // Original container of equality elements, including duplicates. Needed for re-computing
     // '_equalitySet' in case '_collator' changes after elements have been added.
+    //
+    // We keep the equalities in sorted order according to the current BSON element comparator. This
+    // list of equalities will be used to construct a boost::flat_set, which maintains the set of
+    // elements in sorted order within a contiguous region of memory. Sorting and then constructing
+    // a flat_set is O(n log n), whereas the boost::flat_set constructor is O(n ^ 2) due to
+    // https://svn.boost.org/trac10/ticket/13140.
     std::vector<BSONElement> _originalEqualityVector;
 
     // Set of equality elements associated with this expression. '_eltCmp' is used as a comparator
