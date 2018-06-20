286fdc12c0d37f5c614875eae9ab541638103b10
Esha Maharishi <esha.maharishi@mongodb.com>
2018-06-20 15:38:59 -0400

    SERVER-35670 Add test that if a shard already has a cache entry for a database without a version, refreshing causes the entry to be updated with the version

--- jstests/sharding/database_versioning_cache_entry_without_version_updated_with_version.js
+++ jstests/sharding/database_versioning_cache_entry_without_version_updated_with_version.js
@@ -4,7 +4,7 @@
 // its on-disk cache so that the version can also be picked up by secondaries.
 (function() {
 
-    const st = new ShardingTest({shards: 1});
+    const st = new ShardingTest({shards: 1, rs: {nodes: 2}, other: {verbose: 2}});
 
     assert.commandWorked(st.s.getDB("test").getCollection("foo").insert({x: 1}));
 
@@ -24,6 +24,9 @@
         st.shard0.getDB("config").getCollection("cache.databases").findOne({_id: "test"}).version;
     assert.docEq(versionOnConfig, versionOnShard);
 
+    // The shard primary's in-memory version matches the on-disk version.
+    assert.eq(versionOnShard, st.shard0.adminCommand({getDatabaseVersion: "test"}).dbVersion);
+
     jsTest.log("Remove the database version from the shard's cache entry");
     assert.commandWorked(
         st.shard0.getDB("config").getCollection("cache.databases").update({_id: "test"}, {
@@ -33,12 +36,25 @@
         null,
         st.shard0.getDB("config").getCollection("cache.databases").findOne({_id: "test"}).version);
 
-    // After the shard refreshes, it should have updated the cache entry with a version, even though
-    // it already had an entry cached.
-    assert.commandWorked(st.shard0.adminCommand({_flushDatabaseCacheUpdates: "test"}));
+    // Deleting the version field from the on-disk entry did not affect the in-memory version.
+    assert.eq(versionOnShard, st.shard0.adminCommand({getDatabaseVersion: "test"}).dbVersion);
+
+    // The shard secondary does not have a version cached in memory.
+    assert.eq({}, st.rs0.getSecondary().adminCommand({getDatabaseVersion: "test"}).dbVersion);
+
+    // A versioned request against the shard secondary makes the shard primary refresh and update
+    // the on-disk cache entry with a version, even though it already had an on-disk cache entry and
+    // had the up-to-date version cached in memory.
+    // Use readConcern 'local' because the default on secondaries is 'available'.
+    assert.commandWorked(st.s.getDB("test").runCommand(
+        {listCollections: 1, $readPreference: {mode: "secondary"}, readConcern: {level: "local"}}));
     const versionOnShard2 =
         st.shard0.getDB("config").getCollection("cache.databases").findOne({_id: "test"}).version;
     assert.docEq(versionOnConfig, versionOnShard2);
+
+    // The shard secondary's in-memory version now matches the on-disk version.
+    assert.eq(versionOnShard,
+              st.rs0.getSecondary().adminCommand({getDatabaseVersion: "test"}).dbVersion);
 
     st.stop();
 
