9c59460c4ceecb2e127ea682f2a024ecfd55ef53
jannaerin <golden.janna@gmail.com>
2018-06-22 16:00:40 -0400

    SERVER-35723 Make the collection critical section into an RAII class
    (cherry picked from commit 019264f0277a8ea675a97ed88370bda064923651)

--- src/mongo/db/s/collection_sharding_state.cpp
+++ src/mongo/db/s/collection_sharding_state.cpp
@@ -415,4 +415,21 @@ bool CollectionShardingState::_checkShardVersionOk(OperationContext* opCtx,
     MONGO_UNREACHABLE;
 }
 
+CollectionCriticalSection::CollectionCriticalSection(OperationContext* opCtx, NamespaceString ns)
+    : _nss(std::move(ns)), _opCtx(opCtx) {
+    AutoGetCollection autoColl(_opCtx, _nss, MODE_IX, MODE_X);
+    CollectionShardingState::get(opCtx, _nss)->enterCriticalSectionCatchUpPhase(_opCtx);
+}
+
+CollectionCriticalSection::~CollectionCriticalSection() {
+    UninterruptibleLockGuard noInterrupt(_opCtx->lockState());
+    AutoGetCollection autoColl(_opCtx, _nss, MODE_IX, MODE_X);
+    CollectionShardingState::get(_opCtx, _nss)->exitCriticalSection(_opCtx);
+}
+
+void CollectionCriticalSection::enterCommitPhase() {
+    AutoGetCollection autoColl(_opCtx, _nss, MODE_IX, MODE_X);
+    CollectionShardingState::get(_opCtx, _nss)->enterCriticalSectionCommitPhase(_opCtx);
+}
+
 }  // namespace mongo
