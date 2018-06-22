d1d58971adb648aa5f2750f206bb0fba88a8214e
Siyuan Zhou <siyuan.zhou@mongodb.com>
2018-06-22 16:54:24 -0400

    SERVER-35758 Mongo shell prompt errors when running transactions after overriding 'db'
    (cherry picked from commit bb9f225e47faacb95ae9e5658313a5ebf56f37bf)

---
+++ jstests/core/txns/shell_prompt_in_transaction.js
@@ -0,0 +1,43 @@
+// Test shell prompt doesn't run in the session of the global 'db'.
+// @tags: [uses_transactions]
+
+(function() {
+    "use strict";
+
+    const collName = "shell_prompt_in_transaction";
+
+    db.getCollection(collName).drop();
+    assert.commandWorked(db.runCommand({create: collName, writeConcern: {w: "majority"}}));
+
+    // Override the global "db".
+    const session = db.getMongo().startSession();
+    db = session.getDatabase(db.getName());
+    const coll = db.getCollection(collName);
+
+    function simulatePrompt() {
+        __promptWrapper__(defaultPrompt);
+    }
+
+    // Start a transaction, so the session will attach txn info to the commands running on it.
+    session.startTransaction();
+    jsTestLog("Run shell prompt to simulate a user hitting enter.");
+    simulatePrompt();
+    const doc = {_id: "shell-write"};
+    assert.commandWorked(coll.insert(doc));
+    assert.docEq(doc, coll.findOne());
+    simulatePrompt();
+    session.abortTransaction();
+    assert.docEq(null, coll.findOne());
+
+    // Start a transaction, so the session has a running transaction now.
+    simulatePrompt();
+    session.startTransaction();
+    jsTestLog("Run shell prompt to simulate a user hitting enter.");
+    simulatePrompt();
+    assert.commandWorked(coll.insert(doc));
+    simulatePrompt();
+    session.commitTransaction();
+    assert.docEq(doc, coll.findOne());
+
+    coll.drop();
+})();
