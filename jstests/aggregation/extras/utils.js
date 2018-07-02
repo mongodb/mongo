b5b50044fc550351e0677234e55fadd0992bbe53
Charlie Swanson <charlie.swanson@mongodb.com>
2018-07-02 16:00:10 -0400

    SERVER-35765 Clean up utils.js.
    This is in preparation for expanded functionality to allow for a custom
    comparator.

--- jstests/aggregation/extras/utils.js
+++ jstests/aggregation/extras/utils.js
@@ -11,152 +11,135 @@ function testExpression(coll, expression, result) {
  * as the collation spec. Replaces the contents of 'coll' with a single empty document.
  */
 function testExpressionWithCollation(coll, expression, result, collationSpec) {
-    coll.remove({});
-    coll.insert({});
+    assert.commandWorked(coll.remove({}));
+    assert.commandWorked(coll.insert({}));
 
-    var options = collationSpec !== undefined ? {collation: collationSpec} : undefined;
+    const options = collationSpec !== undefined ? {collation: collationSpec} : undefined;
 
-    var res = coll.aggregate([{$project: {output: expression}}], options).toArray();
+    const res = coll.aggregate([{$project: {output: expression}}], options).toArray();
 
     assert.eq(res.length, 1, tojson(res));
     assert.eq(res[0].output, result, tojson(res));
 }
 
-/*
-  Utility functions used to test aggregation
-*/
-
-function anyEq(al, ar, v) {
-    if (typeof(v) == 'undefined')
-        v = false;
+/**
+ * Returns true if 'al' is the same as 'ar'. If the two are arrays, the arrays can be in any order.
+ * Objects (either 'al' and 'ar' themselves, or embedded objects) must have all the same properties,
+ * with the exception of '_id'. If 'al' and 'ar' are neither object nor arrays, they must be the
+ * same value.
+ */
+function anyEq(al, ar, verbose = false) {
+    const debug = msg => verbose ? print(msg) : null;  // Helper to log 'msg' iff 'verbose' is true.
 
     if (al instanceof Array) {
         if (!(ar instanceof Array)) {
-            if (v)
-                print('anyEq: ar is not an array ' + tojson(ar));
+            debug('anyEq: ar is not an array ' + tojson(ar));
             return false;
         }
 
-        if (!arrayEq(al, ar, v)) {
-            if (v)
-                print(`anyEq: arrayEq(al, ar): false; al=${tojson(al)}, ar=${tojson(ar)}`);
+        if (!arrayEq(al, ar, verbose)) {
+            debug(`anyEq: arrayEq(al, ar): false; al=${tojson(al)}, ar=${tojson(ar)}`);
             return false;
         }
     } else if (al instanceof Object) {
         // Be sure to explicitly check for Arrays, since Arrays are considered instances of Objects,
         // and we do not want to consider [] to be equal to {}.
         if (!(ar instanceof Object) || (ar instanceof Array)) {
-            if (v)
-                print('anyEq: ar is not an object ' + tojson(ar));
+            debug('anyEq: ar is not an object ' + tojson(ar));
             return false;
         }
 
-        if (!documentEq(al, ar, v)) {
-            if (v)
-                print(`anyEq: documentEq(al, ar): false; al=${tojson(al)}, ar=${tojson(ar)}`);
+        if (!documentEq(al, ar, verbose)) {
+            debug(`anyEq: documentEq(al, ar): false; al=${tojson(al)}, ar=${tojson(ar)}`);
             return false;
         }
     } else if (al != ar) {
-        if (v)
-            print(`anyEq: (al != ar): false; al=${tojson(al)}, ar=${tojson(ar)}`);
+        debug(`anyEq: (al != ar): false; al=${tojson(al)}, ar=${tojson(ar)}`);
         return false;
     }
 
-    /* if we got here, they matched */
-    if (v)
-        print(`anyEq: these are equal: ${tojson(al)} == ${tojson(ar)}`);
+    debug(`anyEq: these are equal: ${tojson(al)} == ${tojson(ar)}`);
     return true;
 }
 
-/*
-  Compare two documents for equality.  Returns true or false.  Only equal if
-  they have the exact same set of properties, and all the properties' values
-  match.
-*/
-function documentEq(dl, dr, v) {
-    if (typeof(v) == 'undefined')
-        v = false;
+/**
+ * Compare two documents for equality. Returns true or false. Only equal if they have the exact same
+ * set of properties, and all the properties' values match.
+ */
+function documentEq(dl, dr, verbose = false) {
+    const debug = msg => verbose ? print(msg) : null;  // Helper to log 'msg' iff 'verbose' is true.
 
-    /* make sure these are both objects */
+    // Make sure these are both objects.
     if (!(dl instanceof Object)) {
-        if (v)
-            print('documentEq:  dl is not an object ' + tojson(dl));
+        debug('documentEq:  dl is not an object ' + tojson(dl));
         return false;
     }
     if (!(dr instanceof Object)) {
-        if (v)
-            print('documentEq:  dr is not an object ' + tojson(dr));
+        debug('documentEq:  dr is not an object ' + tojson(dr));
         return false;
     }
 
-    /* start by checking for all of dl's properties in dr */
-    for (var propertyName in dl) {
-        /* skip inherited properties */
+    // Start by checking for all of dl's properties in dr.
+    for (let propertyName in dl) {
+        // Skip inherited properties.
         if (!dl.hasOwnProperty(propertyName))
             continue;
 
-        /* the documents aren't equal if they don't both have the property */
+        // The documents aren't equal if they don't both have the property.
         if (!dr.hasOwnProperty(propertyName)) {
-            if (v)
-                print('documentEq: dr doesn\'t have property ' + propertyName);
+            debug('documentEq: dr doesn\'t have property ' + propertyName);
             return false;
         }
 
-        /* if the property is the _id, they don't have to be equal */
+        // If the property is the _id, they don't have to be equal.
         if (propertyName == '_id')
             continue;
 
-        if (!anyEq(dl[propertyName], dr[propertyName], v)) {
+        if (!anyEq(dl[propertyName], dr[propertyName], verbose)) {
             return false;
         }
     }
 
-    /* now make sure that dr doesn't have any extras that dl doesn't have */
-    for (var propertyName in dr) {
+    // Now make sure that dr doesn't have any extras that dl doesn't have.
+    for (let propertyName in dr) {
         if (!dr.hasOwnProperty(propertyName))
             continue;
 
-        /* if dl doesn't have this complain; if it does, we compared it above */
+        // If dl doesn't have this they are not equal; if it does, we compared it above and know it
+        // to be equal.
         if (!dl.hasOwnProperty(propertyName)) {
-            if (v)
-                print('documentEq: dl is missing property ' + propertyName);
+            debug('documentEq: dl is missing property ' + propertyName);
             return false;
         }
     }
 
-    /* if we got here, the two documents are an exact match */
-    if (v)
-        print(`documentEq: these are equal: ${tojson(dl)} == ${tojson(dr)}`);
+    debug(`documentEq: these are equal: ${tojson(dl)} == ${tojson(dr)}`);
     return true;
 }
 
-function arrayEq(al, ar, v) {
-    if (typeof(v) == 'undefined')
-        v = false;
+function arrayEq(al, ar, verbose = false) {
+    const debug = msg => verbose ? print(msg) : null;  // Helper to log 'msg' iff 'verbose' is true.
 
-    /* check that these are both arrays */
+    // Check that these are both arrays.
     if (!(al instanceof Array)) {
-        if (v)
-            print('arrayEq: al is not an array: ' + tojson(al));
+        debug('arrayEq: al is not an array: ' + tojson(al));
         return false;
     }
 
     if (!(ar instanceof Array)) {
-        if (v)
-            print('arrayEq: ar is not an array: ' + tojson(ar));
+        debug('arrayEq: ar is not an array: ' + tojson(ar));
         return false;
     }
 
     if (al.length != ar.length) {
-        if (v)
-            print(`arrayEq:  array lengths do not match ${tojson(al)}, ${tojson(ar)}`);
+        debug(`arrayEq:  array lengths do not match ${tojson(al)}, ${tojson(ar)}`);
         return false;
     }
 
-    var i = 0;
-    var j = 0;
+    let i = 0;
+    let j = 0;
     while (i < al.length) {
-        if (anyEq(al[i], ar[j], v)) {
+        if (anyEq(al[i], ar[j], verbose)) {
             j = 0;
             i++;
         } else if (j < ar.length) {
@@ -166,115 +149,95 @@ function arrayEq(al, ar, v) {
         }
     }
 
-    /* if we got here, the two objects matched */
     return true;
 }
 
-/*
-  Make a shallow copy of an array.
-*/
+/**
+ * Makes a shallow copy of 'a'.
+ */
 function arrayShallowCopy(a) {
     assert(a instanceof Array, 'arrayShallowCopy: argument is not an array');
-
-    var c = [];
-    for (var i = 0; i < a.length; ++i)
-        c.push(a[i]);
-
-    return c;
+    return a.slice();  // Makes a copy.
 }
 
-/*
-  Compare two sets of documents (expressed as arrays) to see if they match.
-  The two sets must have the same documents, although the order need not
-  match.
-
-  Are non-scalar values references?
+/**
+ * Compare two sets of documents (expressed as arrays) to see if they match. The two sets must have
+ * the same documents, although the order need not match and the _id values need not match.
+ *
+ * Are non-scalar values references?
 */
-function resultsEq(rl, rr, v) {
-    if (typeof(v) == 'undefined')
-        v = false;
+function resultsEq(rl, rr, verbose = false) {
+    const debug = msg => verbose ? print(msg) : null;  // Helper to log 'msg' iff 'verbose' is true.
 
-    /* make clones of the arguments so that we don't damage them */
+    // Make clones of the arguments so that we don't damage them.
     rl = arrayShallowCopy(rl);
     rr = arrayShallowCopy(rr);
 
     if (rl.length != rr.length) {
-        if (v)
-            print(`resultsEq:  array lengths do not match ${tojson(rl)}, ${tojson(rr)}`);
+        debug(`resultsEq:  array lengths do not match ${tojson(rl)}, ${tojson(rr)}`);
         return false;
     }
 
-    for (var i = 0; i < rl.length; ++i) {
-        var foundIt = false;
+    for (let i = 0; i < rl.length; ++i) {
+        let foundIt = false;
 
-        /* find a match in the other array */
-        for (var j = 0; j < rr.length; ++j) {
-            if (!anyEq(rl[i], rr[j], v))
+        // Find a match in the other array.
+        for (let j = 0; j < rr.length; ++j) {
+            if (!anyEq(rl[i], rr[j], verbose))
                 continue;
 
-            /*
-              Because we made the copies above, we can edit these out of the
-              arrays so we don't check on them anymore.
-
-              For the inner loop, we're going to be skipping out, so we don't
-              need to be too careful.
-            */
+            // Because we made the copies above, we can edit these out of the arrays so we don't
+            // check on them anymore.
+            // For the inner loop, we're going to be skipping out, so we don't need to be too
+            // careful.
             rr.splice(j, 1);
             foundIt = true;
             break;
-            /* TODO */
         }
 
         if (!foundIt) {
-            /* if we got here, we didn't find this item */
-            if (v)
-                print(`resultsEq: search target missing index ${i} (${tojson(rl[i])})`);
+            // If we got here, we didn't find this item.
+            debug(`resultsEq: search target missing index ${i} (${tojson(rl[i])})`);
             return false;
         }
     }
 
-    /* if we got here, everything matched */
     assert(!rr.length);
     return true;
 }
 
-function orderedArrayEq(al, ar, v) {
+function orderedArrayEq(al, ar, verbose = false) {
     if (al.length != ar.length) {
-        if (v)
+        if (verbose)
             print(`orderedArrayEq:  array lengths do not match ${tojson(al)}, ${tojson(ar)}`);
         return false;
     }
 
-    /* check the elements in the array */
-    for (var i = 0; i < al.length; ++i) {
-        if (!anyEq(al[i], ar[i], v))
+    for (let i = 0; i < al.length; ++i) {
+        if (!anyEq(al[i], ar[i], verbose))
             return false;
     }
 
-    /* if we got here, everything matched */
     return true;
 }
 
-/*
-  Assert that aggregation fails with a specific code.
-  Error message is optional.
-*/
-
+/**
+ * Asserts that the given aggregation fails with a specific code. Error message is optional.
+ */
 function assertErrorCode(coll, pipe, code, errmsg) {
     if (!Array.isArray(pipe)) {
         pipe = [pipe];
     }
 
-    var cmd = {pipeline: pipe};
-    // cmd.cursor = {};
+    let cmd = {pipeline: pipe};
     cmd.cursor = {batchSize: 0};
 
-    var cursorRes = coll.runCommand("aggregate", cmd);
+    let cursorRes = coll.runCommand("aggregate", cmd);
     if (cursorRes.ok) {
-        var followupBatchSize = 0;  // default
-        var cursor = new DBCommandCursor(coll.getDB(), cursorRes, followupBatchSize);
+        let followupBatchSize = 0;  // default
+        let cursor = new DBCommandCursor(coll.getDB(), cursorRes, followupBatchSize);
 
-        var error = assert.throws(function() {
+        let error = assert.throws(function() {
             cursor.itcount();
         }, [], "expected error: " + code);
 
