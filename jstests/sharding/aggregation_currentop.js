a9ae61e976af1fecbf0779a03f400d685e6e58f6
jinichu <jinnybyun@gmail.com>
2018-07-05 15:39:07 -0400

    SERVER-35173 Added autocommit value to currentOp's transaction sub-document
    (cherry picked from commit 1447252f5f44e4a2df4b7e38d4bdef0d88e526c4)

--- jstests/sharding/aggregation_currentop.js
+++ jstests/sharding/aggregation_currentop.js
@@ -625,7 +625,8 @@ TestData.skipAwaitingReplicationOnShardsBeforeCheckingUUIDs = true;
             opid: {$exists: false},
             desc: "inactive transaction",
             "lsid.id": {$in: sessions.map((session) => session.getSessionId().id)},
-            "transaction.parameters.txnNumber": {$gte: 0, $lt: sessions.length}
+            "transaction.parameters.txnNumber": {$gte: 0, $lt: sessions.length},
+            "transaction.parameters.autocommit": false
         };
     }
 
