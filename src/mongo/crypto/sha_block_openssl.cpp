74a69092cdc8dfb0d804aba9846e3b656a66205b
Michael Gmelin <mg@grem.de>
2018-07-13 11:17:32 -0400

    SERVER-36039 Fix build for LibreSSL 2.7 (adopted parts of OpenSSL 1.1 API).
    Closes #1252
    
    Signed-off-by: Mark Benvenuto <mark.benvenuto@mongodb.com>
    (cherry picked from commit a1fad21d12d53d755634740e6b6fe1bc970494c8)

--- src/mongo/crypto/sha_block_openssl.cpp
+++ src/mongo/crypto/sha_block_openssl.cpp
@@ -44,7 +44,8 @@
 #include <openssl/hmac.h>
 #include <openssl/sha.h>
 
-#if OPENSSL_VERSION_NUMBER < 0x10100000L || defined(LIBRESSL_VERSION_NUMBER)
+#if OPENSSL_VERSION_NUMBER < 0x10100000L || \
+    (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x2070000fL)
 namespace {
 // Copies of OpenSSL after 1.1.0 define new EVP digest routines. We must
 // polyfill used definitions to interact with older OpenSSL versions.
