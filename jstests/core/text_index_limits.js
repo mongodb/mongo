ab5cf1f214277536f1d4d2d1dc1fa319af911bbe
Xiangyu Yao <xiangyu.yao@mongodb.com>
2018-08-03 14:22:13 -0400

    SERVER-22078 Remove term list limits for text index in FCV 4.2

---
+++ jstests/core/text_index_limits.js
@@ -0,0 +1,42 @@
+// There is no limit for the total size or the number of unique terms for text index.
+(function() {
+    "use strict";
+
+    var t = db.text_index_limits;
+    t.drop();
+
+    assert.commandWorked(t.createIndex({comments: "text"}));
+
+    // 1. Test number of unique terms exceeds 400,000
+    let commentsWithALotOfUniqueWords = "";
+    // 26^4 = 456,976 > 400,000
+    for (let ch1 = 97; ch1 < 123; ch1++) {
+        for (let ch2 = 97; ch2 < 123; ch2++) {
+            for (let ch3 = 97; ch3 < 123; ch3++) {
+                for (let ch4 = 97; ch4 < 123; ch4++) {
+                    let word = String.fromCharCode(ch1, ch2, ch3, ch4);
+                    commentsWithALotOfUniqueWords += word + " ";
+                }
+            }
+        }
+    }
+    assert.commandWorked(db.runCommand(
+        {insert: t.getName(), documents: [{_id: 1, comments: commentsWithALotOfUniqueWords}]}));
+
+    // 2. Test total size of index keys for unique terms exceeds 4MB
+
+    // 26^3 = 17576 < 400,000
+    let prefix = "a".repeat(400);
+    let commentsWithWordsOfLargeSize = "";
+    for (let ch1 = 97; ch1 < 123; ch1++) {
+        for (let ch2 = 97; ch2 < 123; ch2++) {
+            for (let ch3 = 97; ch3 < 123; ch3++) {
+                let word = String.fromCharCode(ch1, ch2, ch3);
+                commentsWithWordsOfLargeSize += prefix + word + " ";
+            }
+        }
+    }
+    assert.commandWorked(db.runCommand(
+        {insert: t.getName(), documents: [{_id: 2, comments: commentsWithWordsOfLargeSize}]}));
+
+}());
