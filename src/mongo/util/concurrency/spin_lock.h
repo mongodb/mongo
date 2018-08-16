57be1f7097ceed41b156a826c541aa276f35dd61
Siyuan Zhou <siyuan.zhou@mongodb.com>
2018-08-16 18:52:36 -0400

    Revert "SERVER-36676 spinlock as mutex in dbg builds linux"
    This reverts commit db1535d7e9094fd444609ade9bf0fb725d611f85.

--- src/mongo/util/concurrency/spin_lock.h
+++ src/mongo/util/concurrency/spin_lock.h
@@ -37,7 +37,6 @@
 #endif
 
 #include "mongo/base/disallow_copying.h"
-#include "mongo/config.h"
 #include "mongo/platform/compiler.h"
 #include "mongo/stdx/mutex.h"
 
@@ -70,27 +69,6 @@ private:
 
 #else
 
-#if MONGO_CONFIG_DEBUG_BUILD
-class SpinLock {
-    MONGO_DISALLOW_COPYING(SpinLock);
-
-public:
-    SpinLock() = default;
-
-    void lock() {
-        _mutex.lock();
-    }
-
-    void unlock() {
-        _mutex.unlock();
-    }
-
-private:
-    stdx::mutex _mutex;
-};
-
-#else
-
 class SpinLock {
     MONGO_DISALLOW_COPYING(SpinLock);
 
@@ -118,9 +96,6 @@ private:
     // Initializes to the cleared state.
     std::atomic_flag _locked = ATOMIC_FLAG_INIT;  // NOLINT
 };
-
-#endif
-
 #endif
 
 using scoped_spinlock = stdx::lock_guard<SpinLock>;
