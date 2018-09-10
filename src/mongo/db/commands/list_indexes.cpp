xiexiaoy/eloqdoc

SERVER-25175 listIndexes command only includes ready indexes

src/mongo/db/commands/list_indexes.cpp
@@ -154,7 +154,7 @@ public:
         vector<string> indexNames;
         writeConflictRetry(opCtx, "listIndexes", ns.ns(), [&indexNames, &cce, &opCtx] {
             indexNames.clear();
-            cce->getAllIndexes(opCtx, &indexNames);
+            cce->getReadyIndexes(opCtx, &indexNames);
         });
 
         auto ws = make_unique<WorkingSet>();

src/mongo/db/storage/bson_collection_catalog_entry.cpp
@@ -144,6 +144,16 @@ void BSONCollectionCatalogEntry::getAllIndexes(OperationContext* opCtx,
     }
 }
 
+void BSONCollectionCatalogEntry::getReadyIndexes(OperationContext* opCtx,
+                                                 std::vector<std::string>* names) const {
+    MetaData md = _getMetaData(opCtx);
+
+    for (unsigned i = 0; i < md.indexes.size(); i++) {
+        if (md.indexes[i].ready)
+            names->push_back(md.indexes[i].spec["name"].String());
+    }
+}
+
 bool BSONCollectionCatalogEntry::isIndexMultikey(OperationContext* opCtx,
                                                  StringData indexName,
                                                  MultikeyPaths* multikeyPaths) const {

src/mongo/db/storage/bson_collection_catalog_entry.h
@@ -59,6 +59,8 @@ public:
 
     virtual void getAllIndexes(OperationContext* opCtx, std::vector<std::string>* names) const;
 
+    virtual void getReadyIndexes(OperationContext* opCtx, std::vector<std::string>* names) const;
+
     virtual bool isIndexMultikey(OperationContext* opCtx,
                                  StringData indexName,
                                  MultikeyPaths* multikeyPaths) const;

src/mongo/db/storage/mmap_v1/catalog/namespace_details_collection_entry.cpp
@@ -107,6 +107,19 @@ void NamespaceDetailsCollectionCatalogEntry::getAllIndexes(OperationContext* opC
     }
 }
 
+void NamespaceDetailsCollectionCatalogEntry::getReadyIndexes(
+    OperationContext* opCtx, std::vector<std::string>* names) const {
+    NamespaceDetails::IndexIterator i = _details->ii(true);
+    while (i.more()) {
+        const IndexDetails& id = i.next();
+        const BSONObj obj = _indexRecordStore->dataFor(opCtx, id.info.toRecordId()).toBson();
+        const char* idxName = obj.getStringField("name");
+        if (isIndexReady(opCtx, StringData(idxName))) {
+            names->push_back(idxName);
+        }
+    }
+}
+
 bool NamespaceDetailsCollectionCatalogEntry::isIndexMultikey(OperationContext* opCtx,
                                                              StringData idxName,
                                                              MultikeyPaths* multikeyPaths) const {

src/mongo/db/storage/mmap_v1/catalog/namespace_details_collection_entry.h
@@ -66,6 +66,8 @@ public:
 
     void getAllIndexes(OperationContext* opCtx, std::vector<std::string>* names) const final;
 
+    void getReadyIndexes(OperationContext* opCtx, std::vector<std::string>* names) const final;
+
     BSONObj getIndexSpec(OperationContext* opCtx, StringData idxName) const final;
 
     bool isIndexMultikey(OperationContext* opCtx,

mongodb/mongo

(cherry picked from commit 2cc9396586e771a0484a4017bf3f126098818d2b)

src/mongo/db/commands/list_indexes.cpp
@@ -152,7 +152,7 @@ public:
         vector<string> indexNames;
         MONGO_WRITE_CONFLICT_RETRY_LOOP_BEGIN {
             indexNames.clear();
-            cce->getAllIndexes(txn, &indexNames);
+            cce->getReadyIndexes(txn, &indexNames);
         }
         MONGO_WRITE_CONFLICT_RETRY_LOOP_END(txn, "listIndexes", ns.ns());
 

src/mongo/db/storage/bson_collection_catalog_entry.cpp
@@ -143,6 +143,16 @@ void BSONCollectionCatalogEntry::getAllIndexes(OperationContext* txn,
     }
 }
 
+void BSONCollectionCatalogEntry::getReadyIndexes(OperationContext* txn,
+                                                 std::vector<std::string>* names) const {
+    MetaData md = _getMetaData(txn);
+
+    for (unsigned i = 0; i < md.indexes.size(); i++) {
+        if (md.indexes[i].ready)
+            names->push_back(md.indexes[i].spec["name"].String());
+    }
+}
+
 bool BSONCollectionCatalogEntry::isIndexMultikey(OperationContext* txn,
                                                  StringData indexName,
                                                  MultikeyPaths* multikeyPaths) const {

src/mongo/db/storage/bson_collection_catalog_entry.h
@@ -58,6 +58,8 @@ public:
 
     virtual void getAllIndexes(OperationContext* txn, std::vector<std::string>* names) const;
 
+    virtual void getReadyIndexes(OperationContext* txn, std::vector<std::string>* names) const;
+
     virtual bool isIndexMultikey(OperationContext* txn,
                                  StringData indexName,
                                  MultikeyPaths* multikeyPaths) const;

src/mongo/db/storage/mmap_v1/catalog/namespace_details_collection_entry.cpp
@@ -106,6 +106,19 @@ void NamespaceDetailsCollectionCatalogEntry::getAllIndexes(OperationContext* txn
     }
 }
 
+void NamespaceDetailsCollectionCatalogEntry::getReadyIndexes(
+    OperationContext* txn, std::vector<std::string>* names) const {
+    NamespaceDetails::IndexIterator i = _details->ii(true);
+    while (i.more()) {
+        const IndexDetails& id = i.next();
+        const BSONObj obj = _indexRecordStore->dataFor(txn, id.info.toRecordId()).toBson();
+        const char* idxName = obj.getStringField("name");
+        if (isIndexReady(txn, StringData(idxName))) {
+            names->push_back(idxName);
+        }
+    }
+}
+
 bool NamespaceDetailsCollectionCatalogEntry::isIndexMultikey(OperationContext* txn,
                                                              StringData idxName,
                                                              MultikeyPaths* multikeyPaths) const {

src/mongo/db/storage/mmap_v1/catalog/namespace_details_collection_entry.h
@@ -67,6 +67,8 @@ public:
 
     BSONObj getIndexSpec(OperationContext* txn, StringData idxName) const final;
 
+    void getReadyIndexes(OperationContext* txn, std::vector<std::string>* names) const final;
+
     bool isIndexMultikey(OperationContext* txn,
                          StringData indexName,
                          MultikeyPaths* multikeyPaths) const final;