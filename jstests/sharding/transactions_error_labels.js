e9c9b87b155f3c50c71bbb15a989349152386530
Randolph Tan <randolph@10gen.com>
2018-09-27 14:52:30 -0400

    SERVER-36580 Write test for verifying that mongos preserves the error label from shards when executing write commands.

---
+++ jstests/sharding/transactions_error_labels.js
@@ -0,0 +1,31 @@
+// Test TransientTransactionErrors error label in mongos write commands.
+// @tags: [uses_transactions, uses_single_shard_transaction]
+(function() {
+    "use strict";
+
+    let st = new ShardingTest({shards: 1});
+
+    let session = st.s.startSession();
+    let sessionDB = session.getDatabase('test');
+
+    st.rs0.nodes.forEach(function(node) {
+        assert.commandWorked(node.getDB('admin').runCommand({
+            configureFailPoint: "failCommand",
+            mode: "alwaysOn",
+            data: {errorCode: ErrorCodes.WriteConflict, failCommands: ["insert"]}
+        }));
+    });
+
+    session.startTransaction();
+
+    let res = sessionDB.runCommand({
+        insert: 'foo',
+        documents: [{_id: "insert-1"}],
+        readConcern: {level: "snapshot"},
+    });
+
+    assert.commandFailedWithCode(res, ErrorCodes.WriteConflict);
+    assert.eq(res.errorLabels, ["TransientTransactionError"]);
+
+    st.stop();
+}());
