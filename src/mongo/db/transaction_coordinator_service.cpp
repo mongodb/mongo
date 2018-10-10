fd7819f662796a99e8db589bd29dc1a326331ae7
Esha Maharishi <esha.maharishi@mongodb.com>
2018-10-10 14:24:28 -0400

    SERVER-37547 Use kTransaction log component for log messages from the TransactionCoordinatorService

--- src/mongo/db/transaction_coordinator_service.cpp
+++ src/mongo/db/transaction_coordinator_service.cpp
@@ -26,7 +26,7 @@
  *    it in the license file.
  */
 
-#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kSharding
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kTransaction
 
 #include "mongo/platform/basic.h"
 
