fc3a452a6ecb35914b9df98b19309b03de644fd6
Shreyas Kalyan <shreyaskalyan@gmail.com>
2018-10-19 11:49:43 -0400

    SERVER-37599 Log exit code of shell-spawned processes
    (cherry picked from commit 10934947f4a0e9fd83c256ed297f3a0a73c4d971)

--- src/mongo/shell/servers.js
+++ src/mongo/shell/servers.js
@@ -1187,7 +1187,8 @@ var MongoRunner, _startMongod, startMongoProgram, runMongoProgram, startMongoPro
             } catch (e) {
                 var res = checkProgram(pid);
                 if (!res.alive) {
-                    print("Could not start mongo program at " + port + ", process ended");
+                    print("Could not start mongo program at " + port +
+                          ", process ended with exit code: " + res.exitCode);
                     serverExitCodeMap[port] = res.exitCode;
                     return true;
                 }
@@ -1223,9 +1224,10 @@ var MongoRunner, _startMongod, startMongoProgram, runMongoProgram, startMongoPro
                 m.pid = pid;
                 return true;
             } catch (e) {
-                if (!checkProgram(pid).alive) {
-                    print("Could not start mongo program at " + port + ", process ended");
-
+                var res = checkProgram(pid);
+                if (!res.alive) {
+                    print("Could not start mongo program at " + port +
+                          ", process ended with exit code: " + res.exitCode);
                     // Break out
                     m = null;
                     return true;
