ac1bba79e1f0aeeb654b4c11b8dcf6e1b0962ed6
Jack Mulrow <jack.mulrow@mongodb.com>
2018-11-05 10:49:40 -0500

    SERVER-37902 recovering_slaveok.js should assert all writes it makes succeed
    (cherry picked from commit 72a0bc78c11f8684a47e0ba135fe25c536cd240e)

--- jstests/sharding/recovering_slaveok.js
+++ jstests/sharding/recovering_slaveok.js
@@ -42,8 +42,8 @@ TestData.skipCheckingUUIDsConsistentAcrossCluster = true;
 
     print("1: initial insert");
 
-    coll.save({_id: -1, a: "a", date: new Date()});
-    coll.save({_id: 1, b: "b", date: new Date()});
+    assert.writeOK(coll.save({_id: -1, a: "a", date: new Date()}));
+    assert.writeOK(coll.save({_id: 1, b: "b", date: new Date()}));
 
     print("2: shard collection");
 
