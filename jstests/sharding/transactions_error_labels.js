bc8bfc6b8ad5ebf05090ae49f8fa8bf35d028d28
Jack Mulrow <jack.mulrow@mongodb.com>
2018-11-09 13:37:54 -0500

    SERVER-38057 Unset failCommand fail point in transactions_error_labels.js

--- jstests/sharding/transactions_error_labels.js
+++ jstests/sharding/transactions_error_labels.js
@@ -6,10 +6,10 @@
     let st = new ShardingTest({shards: 1});
 
     let session = st.s.startSession();
-    let sessionDB = session.getDatabase('test');
+    let sessionDB = session.getDatabase("test");
 
     st.rs0.nodes.forEach(function(node) {
-        assert.commandWorked(node.getDB('admin').runCommand({
+        assert.commandWorked(node.getDB("admin").runCommand({
             configureFailPoint: "failCommand",
             mode: "alwaysOn",
             data: {errorCode: ErrorCodes.WriteConflict, failCommands: ["insert"]}
@@ -19,13 +19,18 @@
     session.startTransaction();
 
     let res = sessionDB.runCommand({
-        insert: 'foo',
+        insert: "foo",
         documents: [{_id: "insert-1"}],
         readConcern: {level: "snapshot"},
     });
 
     assert.commandFailedWithCode(res, ErrorCodes.WriteConflict);
     assert.eq(res.errorLabels, ["TransientTransactionError"]);
+
+    st.rs0.nodes.forEach(function(node) {
+        assert.commandWorked(
+            node.getDB("admin").runCommand({configureFailPoint: "failCommand", mode: "off"}));
+    });
 
     st.stop();
 }());
