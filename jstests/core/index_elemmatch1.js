910a0bda4c434571600e67d78afb2424dc94eaaa
James Wahlin <james@mongodb.com>
2018-11-16 14:00:37 -0500

    SERVER-38181 modernize jstests/core/index_elemmatch1.js and reduce data size

--- jstests/core/index_elemmatch1.js
+++ jstests/core/index_elemmatch1.js
@@ -1,48 +1,31 @@
+/**
+ * Tests find with $elemMatch when supporting indexes are in place.
+ */
+(function() {
+    "use strict";
 
-t = db.index_elemmatch1;
-t.drop();
+    const coll = db.index_elemmatch1;
+    coll.drop();
 
-x = 0;
-y = 0;
-var bulk = t.initializeUnorderedBulkOp();
-for (a = 0; a < 100; a++) {
-    for (b = 0; b < 100; b++) {
-        bulk.insert({a: a, b: b % 10, arr: [{x: x++ % 10, y: y++ % 10}]});
-    }
-}
-assert.writeOK(bulk.execute());
-
-t.ensureIndex({a: 1, b: 1});
-t.ensureIndex({"arr.x": 1, a: 1});
-
-assert.eq(100, t.find({a: 55}).itcount(), "A1");
-assert.eq(10, t.find({a: 55, b: 7}).itcount(), "A2");
-
-q = {
-    a: 55,
-    b: {$in: [1, 5, 8]}
-};
-assert.eq(30, t.find(q).itcount(), "A3");
-
-q.arr = {
-    $elemMatch: {x: 5, y: 5}
-};
-assert.eq(10, t.find(q).itcount(), "A4");
-
-function nscannedForCursor(explain, cursor) {
-    plans = explain.allPlans;
-    for (i in plans) {
-        if (plans[i].cursor == cursor) {
-            return plans[i].nscanned;
+    let x = 0;
+    let y = 0;
+    const bulk = coll.initializeUnorderedBulkOp();
+    for (let a = 0; a < 10; a++) {
+        for (let b = 0; b < 10; b++) {
+            bulk.insert({a: a, b: b % 10, arr: [{x: x++ % 10, y: y++ % 10}]});
         }
     }
-    return -1;
-}
+    assert.commandWorked(bulk.execute());
 
-var explain = t.find(q).hint({"arr.x": 1, a: 1}).explain("executionStats");
-assert.eq(t.find(q).itcount(), explain.executionStats.totalKeysExamined);
+    assert.commandWorked(coll.createIndex({a: 1, b: 1}));
+    assert.commandWorked(coll.createIndex({"arr.x": 1, a: 1}));
 
-printjson(t.find(q).explain());
-print("Num results:");
-assert.eq(10, t.find(q).itcount());
-printjson(t.find(q).itcount());
+    const query = {a: 5, b: {$in: [1, 3, 5]}, arr: {$elemMatch: {x: 5, y: 5}}};
+
+    const count = coll.find(query).itcount();
+    assert.eq(count, 1);
+
+    const explain = coll.find(query).hint({"arr.x": 1, a: 1}).explain("executionStats");
+    assert.commandWorked(explain);
+    assert.eq(count, explain.executionStats.totalKeysExamined, explain);
+})();
