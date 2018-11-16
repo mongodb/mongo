c5f382a9aae82c2e87362fb0da3cc1d08e9edbb5
Andrew Morrow <acm@mongodb.com>
2018-11-16 12:30:38 -0500

    SERVER-34873 Revert "SERVER-33125 Work around codegen error in s390x exception unwinding."
    This reverts commit 85b39d411987431a8c37b1de267b167a384ea9b3.

--- src/mongo/db/pipeline/parsed_aggregation_projection.cpp
+++ src/mongo/db/pipeline/parsed_aggregation_projection.cpp
@@ -182,23 +182,13 @@ private:
         : _rawObj(spec), _policies(policies) {}
 
     /**
-     * Parses a single BSONElement, with 'fieldName' representing the path used for projection
-     * inclusion or exclusion. This code was broken out into a separate function as a workaround for
-     * SERVER-33125.
-     */
-    void parseElementWithFieldName(BSONElement elem, StringData fieldName) {
-        FieldPath elemPath(fieldName);
-        parseElement(elem, elemPath);
-    }
-
-    /**
      * Traverses '_rawObj' to determine the type of projection, populating '_parsedType' in the
      * process.
      */
     void parse() {
         size_t nFields = 0;
         for (auto&& elem : _rawObj) {
-            parseElementWithFieldName(elem, elem.fieldName());
+            parseElement(elem, FieldPath(elem.fieldName()));
             nFields++;
         }
 
