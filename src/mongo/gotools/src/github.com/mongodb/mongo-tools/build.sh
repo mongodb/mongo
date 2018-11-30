1c7e9f40da962b38151e6bdbc1443d7b55366718
David Golden <xdg@xdg.me>
2018-11-30 09:50:32 -0500

    Import tools: ff4c48505122337b6b38c82bdd00b19c17c2a775 from branch v4.0
    ref: 0f0d8665c1..ff4c485051
    for: 4.0.5
    
    TOOLS-1566    Should not include "ssl" tag for Linux 64 build
    TOOLS-1709    Set build version and git revision using -ldflags
    TOOLS-1742    import cycle between util and testutil
    TOOLS-1996    Allow building tools from inside an ordinary GOPATH
    TOOLS-2149    Configure build outside Evergreen

---
+++ src/mongo/gotools/src/github.com/mongodb/mongo-tools/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -o errexit
+tags=""
+if [ ! -z "$1" ]
+  then
+  	tags="$@"
+fi
+
+# make sure we're in the directory where the script lives
+SCRIPT_DIR="$(cd "$(dirname ${BASH_SOURCE[0]})" && pwd)"
+cd $SCRIPT_DIR
+
+. ./set_goenv.sh
+set_goenv || exit
+
+# remove stale packages
+rm -rf vendor/pkg
+
+mkdir -p bin
+
+ec=0
+for i in bsondump mongostat mongofiles mongoexport mongoimport mongorestore mongodump mongotop mongoreplay; do
+        echo "Building ${i}..."
+        go build -o "bin/$i" -ldflags "$(print_ldflags)" -tags "$(print_tags $tags)" "$i/main/$i.go" || { echo "Error building $i"; ec=1; break; }
+        ./bin/$i --version | head -1
+done
+
+if [ -t /dev/stdin ]; then
+    stty sane
+fi
+
+exit $ec
