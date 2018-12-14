061c7c342649589e6dcfde282f643623a172c23f
Jonathan Reams <jbreams@mongodb.com>
2018-12-13 21:59:58 -0500

    SERVER-38649 Add generic AlarmScheduler and AlarmRunner
    (cherry picked from commit 764396a48f5a31b548eab9092967cac610c24b73)

---
+++ src/mongo/util/alarm.cpp
@@ -0,0 +1,172 @@
+/**
+ *    Copyright (C) 2018-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/platform/basic.h"
+
+#include "mongo/util/alarm.h"
+
+#include <vector>
+
+namespace mongo {
+
+class AlarmSchedulerPrecise::HandleImpl final
+    : public AlarmScheduler::Handle,
+      public std::enable_shared_from_this<AlarmSchedulerPrecise::HandleImpl> {
+public:
+    HandleImpl(std::weak_ptr<AlarmSchedulerPrecise> service, AlarmSchedulerPrecise::AlarmMapIt it)
+        : _service(std::move(service)), _myIt(std::move(it)) {}
+
+    struct MakeEmptyHandle {};
+    explicit HandleImpl(MakeEmptyHandle)
+        : _service(std::shared_ptr<AlarmSchedulerPrecise>(nullptr)), _myIt(), _done(true) {}
+
+    Status cancel() override {
+        auto service = _service.lock();
+        if (!service) {
+            return {ErrorCodes::ShutdownInProgress, "The alarm scheduler was shutdown"};
+        }
+
+        stdx::unique_lock<stdx::mutex> lk(service->_mutex);
+        if (_done) {
+            return {ErrorCodes::AlarmAlreadyFulfilled, "The alarm has already been canceled"};
+        }
+
+        auto state = std::move(_myIt->second);
+        service->_alarms.erase(_myIt);
+        lk.unlock();
+
+        std::move(state.promise)
+            .setError({ErrorCodes::CallbackCanceled,
+                       "The alarm was canceled before it expired or could be processed"});
+        return Status::OK();
+    }
+
+    void setDone() {
+        _done = true;
+    }
+
+private:
+    std::weak_ptr<AlarmSchedulerPrecise> const _service;
+    AlarmSchedulerPrecise::AlarmMapIt _myIt;
+    bool _done = false;
+};
+
+AlarmSchedulerPrecise::~AlarmSchedulerPrecise() {
+    clearAllAlarms();
+}
+
+AlarmScheduler::Alarm AlarmSchedulerPrecise::alarmAt(Date_t date) {
+    stdx::unique_lock<stdx::mutex> lk(_mutex);
+    if (_shutdown) {
+        Alarm ret;
+        ret.future = Future<void>::makeReady(
+            Status(ErrorCodes::ShutdownInProgress, "Alarm scheduler has been shut down."));
+        ret.handle = std::make_shared<HandleImpl>(HandleImpl::MakeEmptyHandle{});
+        return ret;
+    }
+
+    auto pf = makePromiseFuture<void>();
+    auto it = _alarms.emplace(date, AlarmData(std::move(pf.promise)));
+    auto nextAlarm = _alarms.begin()->first;
+    auto ret = std::make_shared<HandleImpl>(shared_from_this(), it);
+    it->second.handle = ret;
+    lk.unlock();
+
+    callRegisterHook(nextAlarm, shared_from_this());
+    return {std::move(pf.future), std::move(ret)};
+}
+
+void AlarmSchedulerPrecise::processExpiredAlarms(
+    boost::optional<AlarmScheduler::AlarmExpireHook> hook) {
+    AlarmCount processed = 0;
+    auto now = clockSource()->now();
+    std::vector<Promise<void>> toExpire;
+    AlarmMapIt it;
+
+    stdx::unique_lock<stdx::mutex> lk(_mutex);
+    for (it = _alarms.begin(); it != _alarms.end();) {
+        if (hook && !(*hook)(processed + 1)) {
+            break;
+        }
+
+        if (it->first > now) {
+            break;
+        }
+
+        processed++;
+        toExpire.push_back(std::move(it->second.promise));
+        auto handle = it->second.handle.lock();
+        if (handle) {
+            handle->setDone();
+        }
+
+        it = _alarms.erase(it);
+    }
+
+    lk.unlock();
+
+    for (auto& promise : toExpire) {
+        promise.emplaceValue();
+    }
+}
+
+Date_t AlarmSchedulerPrecise::nextAlarm() {
+    stdx::lock_guard<stdx::mutex> lk(_mutex);
+    return (_alarms.empty()) ? Date_t::max() : _alarms.begin()->first;
+}
+
+void AlarmSchedulerPrecise::clearAllAlarms() {
+    stdx::unique_lock<stdx::mutex> lk(_mutex);
+    _clearAllAlarmsImpl(lk);
+}
+
+void AlarmSchedulerPrecise::clearAllAlarmsAndShutdown() {
+    stdx::unique_lock<stdx::mutex> lk(_mutex);
+    _shutdown = true;
+    _clearAllAlarmsImpl(lk);
+}
+
+void AlarmSchedulerPrecise::_clearAllAlarmsImpl(stdx::unique_lock<stdx::mutex>& lk) {
+    std::vector<Promise<void>> toExpire;
+    for (AlarmMapIt it = _alarms.begin(); it != _alarms.end();) {
+        toExpire.push_back(std::move(it->second.promise));
+        auto handle = it->second.handle.lock();
+        if (handle) {
+            handle->setDone();
+        }
+        it = _alarms.erase(it);
+    }
+
+    lk.unlock();
+    for (auto& alarm : toExpire) {
+        alarm.setError({ErrorCodes::CallbackCanceled, "Alarm scheduler was cleared"});
+    }
+}
+
+}  // namespace mongo
---
+++ src/mongo/util/alarm_test.cpp
@@ -0,0 +1,168 @@
+/**
+ *    Copyright (C) 2018-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kDefault
+
+#include "mongo/platform/basic.h"
+
+#include "mongo/stdx/chrono.h"
+#include "mongo/unittest/unittest.h"
+#include "mongo/util/alarm.h"
+#include "mongo/util/alarm_runner_background_thread.h"
+#include "mongo/util/clock_source_mock.h"
+#include "mongo/util/log.h"
+
+namespace mongo {
+namespace {
+TEST(AlarmScheduler, BasicSingleThread) {
+    auto clockSource = std::make_unique<ClockSourceMock>();
+
+    std::shared_ptr<AlarmScheduler> scheduler =
+        std::make_shared<AlarmSchedulerPrecise>(clockSource.get());
+
+    auto testStart = clockSource->now();
+    auto alarm = scheduler->alarmAt(testStart + Milliseconds(10));
+    bool firstTimerExpired = false;
+    std::move(alarm.future).getAsync([&](Status status) {
+        log() << "Timer expired: " << status;
+        firstTimerExpired = true;
+    });
+
+    alarm = scheduler->alarmAt(testStart + Milliseconds(500));
+    bool secondTimerExpired = false;
+    std::move(alarm.future).getAsync([&](Status status) {
+        log() << "Second timer expired: " << status;
+        secondTimerExpired = true;
+    });
+
+    alarm = scheduler->alarmAt(testStart + Milliseconds(515));
+    bool thirdTimerExpired = false;
+    std::move(alarm.future).getAsync([&](Status status) {
+        log() << "third timer expired: " << status;
+        thirdTimerExpired = true;
+    });
+    auto missingEvent = alarm.handle;
+
+    alarm = scheduler->alarmAt(testStart + Milliseconds(250));
+
+    clockSource->advance(Milliseconds(11));
+    scheduler->processExpiredAlarms();
+    ASSERT_TRUE(firstTimerExpired);
+    ASSERT_FALSE(secondTimerExpired);
+
+    ASSERT_OK(alarm.handle->cancel());
+    auto cancelledStatus = std::move(alarm.future).getNoThrow();
+    ASSERT_EQ(cancelledStatus.code(), ErrorCodes::CallbackCanceled);
+
+
+    clockSource->advance(Milliseconds(501));
+    scheduler->processExpiredAlarms();
+    ASSERT_TRUE(secondTimerExpired);
+    ASSERT_FALSE(thirdTimerExpired);
+
+    clockSource->advance(Milliseconds(64));
+    scheduler->processExpiredAlarms();
+    ASSERT_TRUE(thirdTimerExpired);
+
+    cancelledStatus = missingEvent->cancel();
+    ASSERT_EQ(cancelledStatus.code(), ErrorCodes::AlarmAlreadyFulfilled);
+    alarm = scheduler->alarmAt(testStart + Hours(5));
+    scheduler->clearAllAlarmsAndShutdown();
+    cancelledStatus = std::move(alarm.future).getNoThrow();
+    ASSERT_EQ(cancelledStatus.code(), ErrorCodes::CallbackCanceled);
+
+    alarm = scheduler->alarmFromNow(Milliseconds{50});
+    auto shutdownStatus = alarm.future.getNoThrow();
+    ASSERT_EQ(shutdownStatus.code(), ErrorCodes::ShutdownInProgress);
+}
+
+TEST(AlarmRunner, BasicTest) {
+    auto clockSource = std::make_unique<ClockSourceMock>();
+    auto scheduler = std::make_shared<AlarmSchedulerPrecise>(clockSource.get());
+    AlarmRunnerBackgroundThread runner({scheduler});
+    runner.start();
+
+    auto alarm1 = scheduler->alarmFromNow(Milliseconds(10));
+    auto alarm2 = scheduler->alarmFromNow(Milliseconds(20));
+
+    AtomicWord<bool> future2Filled{false};
+    auto pf = makePromiseFuture<void>();
+    std::move(alarm2.future).getAsync([&future2Filled,
+                                       promise = pf.promise.share() ](Status status) mutable {
+        ASSERT_OK(status);
+        future2Filled.store(true);
+        promise.emplaceValue();
+    });
+
+    clockSource->advance(Milliseconds(11));
+
+    ASSERT_OK(alarm1.future.getNoThrow());
+    ASSERT_FALSE(future2Filled.load());
+
+    clockSource->advance(Milliseconds(21));
+    ASSERT_OK(pf.future.getNoThrow());
+
+    auto alarm3 = scheduler->alarmFromNow(Milliseconds(10));
+
+    ASSERT_OK(alarm3.handle->cancel());
+
+    ASSERT_EQ(alarm3.future.getNoThrow().code(), ErrorCodes::CallbackCanceled);
+
+    auto alarm4 = scheduler->alarmFromNow(Milliseconds(10));
+    runner.shutdown();
+
+    ASSERT_EQ(alarm4.future.getNoThrow().code(), ErrorCodes::CallbackCanceled);
+
+    auto alarm5 = scheduler->alarmFromNow(Milliseconds(50));
+    ASSERT_EQ(alarm5.future.getNoThrow().code(), ErrorCodes::ShutdownInProgress);
+}
+
+TEST(AlarmRunner, SeveralSchedulers) {
+    auto clockSource = std::make_unique<ClockSourceMock>();
+    auto scheduler1 = std::make_shared<AlarmSchedulerPrecise>(clockSource.get());
+    auto scheduler2 = std::make_shared<AlarmSchedulerPrecise>(clockSource.get());
+
+    AlarmRunnerBackgroundThread runner({scheduler1, scheduler2});
+    runner.start();
+
+    scheduler1->alarmAt(Date_t::max());
+    // Schedule two alarms, the first is just to wake up the runner so that it can try to decide
+    // which scheduler to wait for next. The second is the one we actually want to wait for.
+    auto alarm1 = scheduler2->alarmFromNow(Milliseconds(1));
+    auto alarm2 = scheduler2->alarmFromNow(Milliseconds(20));
+    clockSource->advance(Milliseconds(2));
+    ASSERT_OK(alarm1.future.getNoThrow());
+
+    // If everything goes well then we should be able to wait on this future without blocking.
+    clockSource->advance(Milliseconds(20));
+    ASSERT_OK(alarm2.future.getNoThrow());
+
+    runner.shutdown();
+}
+}  // namespace
+}  // namespace mongo
