05ebfef11161b96ba0e8374f34c359a403097f60
Andrew Morrow <acm@mongodb.com>
2018-12-15 18:53:04 -0500

    SERVER-38450 Convert global conn pool options to IDL

--- src/mongo/client/global_conn_pool.cpp
+++ src/mongo/client/global_conn_pool.cpp
@@ -33,33 +33,13 @@
 #include "mongo/client/global_conn_pool.h"
 
 #include "mongo/base/init.h"
-#include "mongo/db/server_parameters.h"
+#include "mongo/client/global_conn_pool_gen.h"
 
 namespace mongo {
 namespace {
 
-// Maximum connections per host the connection pool should store
-int maxConnsPerHost(200);
-ExportedServerParameter<int, ServerParameterType::kStartupOnly>  //
-    maxConnsPerHostParameter(ServerParameterSet::getGlobal(),
-                             "connPoolMaxConnsPerHost",
-                             &maxConnsPerHost);
-
-// Maximum in-use connections per host in the global connection pool
-int maxInUseConnsPerHost(std::numeric_limits<int>::max());
-ExportedServerParameter<int, ServerParameterType::kStartupOnly>  //
-    maxInUseConnsPerHostParameter(ServerParameterSet::getGlobal(),
-                                  "connPoolMaxInUseConnsPerHost",
-                                  &maxInUseConnsPerHost);
-
-// Amount of time, in minutes, to keep idle connections in the global connection pool
-int globalConnPoolIdleTimeout(std::numeric_limits<int>::max());
-ExportedServerParameter<int, ServerParameterType::kStartupOnly>  //
-    globalConnPoolIdleTimeoutParameter(ServerParameterSet::getGlobal(),
-                                       "globalConnPoolIdleTimeoutMinutes",
-                                       &globalConnPoolIdleTimeout);
-
-MONGO_INITIALIZER(InitializeGlobalConnectionPool)(InitializerContext* context) {
+MONGO_INITIALIZER_WITH_PREREQUISITES(InitializeGlobalConnectionPool, ("EndStartupOptionStorage"))
+(InitializerContext* context) {
     globalConnPool.setName("connection pool");
     globalConnPool.setMaxPoolSize(maxConnsPerHost);
     globalConnPool.setMaxInUse(maxInUseConnsPerHost);
