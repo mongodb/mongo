6734c12d17dd4c0e2738a47feb7114221d6ba66d
Robert Guo <robert.guo@10gen.com>
2018-12-26 16:14:59 -0500

    SERVER-38775 allow gennylib_test accept arbitrary cmdline args

--- buildscripts/resmokelib/testing/testcases/gennylib_test.py
+++ buildscripts/resmokelib/testing/testcases/gennylib_test.py
@@ -15,14 +15,20 @@ class GennyLibTestCase(interface.ProcessTestCase):
 
     REGISTERED_NAME = "gennylib_test"
 
-    def __init__(self, logger, test_name, program_executable=None, tags=None):
-        """Initialize the GennyLibTestCase with the executable to run."""
+    def __init__(self, logger, test_name, program_executable=None, verbatim_arguments=None):
+        """
+        Initialize the GennyLibTestCase with the executable to run.
+
+        @param verbatim_arguments: list of arguments that are passed through without
+            processing by resmoke. This is necessary because Catch2 requires positional arguments
+            and CTest can't use resmoke's version of "=" separated keyword arguments.
+        """
 
         interface.ProcessTestCase.__init__(self, logger, "gennylib test", program_executable)
 
         self.program_executable = program_executable
         self.program_options = {}
-        self.tags = tags
+        self.verbatim_arguments = utils.default_if_none(verbatim_arguments, [])[:]
 
     def configure(self, fixture, *args, **kwargs):
         """Configure the test case."""
@@ -33,5 +39,6 @@ class GennyLibTestCase(interface.ProcessTestCase):
         self.program_options["process_kwargs"] = process_kwargs
 
     def _make_process(self):
-        return core.programs.generic_program(self.logger, [self.program_executable, self.tags],
+        return core.programs.generic_program(self.logger,
+                                             [self.program_executable] + self.verbatim_arguments,
                                              **self.program_options)
