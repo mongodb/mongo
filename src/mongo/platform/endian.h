6216c1f39ca10f148a82cb2b9bba0bd2b61c8793
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2018-12-30 18:16:23 -0500

    SERVER-38730 fix struct ByteOrderConverter<Decimal128::Value> to return the converted struct

--- src/mongo/platform/endian.h
+++ src/mongo/platform/endian.h
@@ -422,26 +422,26 @@ struct ByteOrderConverter<Decimal128::Value> {
     typedef Decimal128::Value T;
 
     inline static T nativeToBig(T t) {
-        ByteOrderConverter<uint64_t>::nativeToBig(t.low64);
-        ByteOrderConverter<uint64_t>::nativeToBig(t.high64);
+        t.low64 = ByteOrderConverter<uint64_t>::nativeToBig(t.low64);
+        t.high64 = ByteOrderConverter<uint64_t>::nativeToBig(t.high64);
         return t;
     }
 
     inline static T bigToNative(T t) {
-        ByteOrderConverter<uint64_t>::bigToNative(t.low64);
-        ByteOrderConverter<uint64_t>::bigToNative(t.high64);
+        t.low64 = ByteOrderConverter<uint64_t>::bigToNative(t.low64);
+        t.high64 = ByteOrderConverter<uint64_t>::bigToNative(t.high64);
         return t;
     }
 
     inline static T nativeToLittle(T t) {
-        ByteOrderConverter<uint64_t>::nativeToLittle(t.low64);
-        ByteOrderConverter<uint64_t>::nativeToLittle(t.high64);
+        t.low64 = ByteOrderConverter<uint64_t>::nativeToLittle(t.low64);
+        t.high64 = ByteOrderConverter<uint64_t>::nativeToLittle(t.high64);
         return t;
     }
 
     inline static T littleToNative(T t) {
-        ByteOrderConverter<uint64_t>::littleToNative(t.low64);
-        ByteOrderConverter<uint64_t>::littleToNative(t.high64);
+        t.low64 = ByteOrderConverter<uint64_t>::littleToNative(t.low64);
+        t.high64 = ByteOrderConverter<uint64_t>::littleToNative(t.high64);
         return t;
     }
 };
