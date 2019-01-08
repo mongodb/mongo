8ce634946a837a925382c519ddacdba5dba1f3af
Mathew Robinson <chasinglogic@gmail.com>
2019-01-08 13:13:09 -0500

    SERVER-33469 Fix lint errors

--- src/mongo/db/initialize_server_global_state.cpp
+++ src/mongo/db/initialize_server_global_state.cpp
@@ -1,30 +1,30 @@
 /**
-*    Copyright (C) 2008 10gen Inc.
-*
-*    This program is free software: you can redistribute it and/or  modify
-*    it under the terms of the GNU Affero General Public License, version 3,
-*    as published by the Free Software Foundation.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Affero General Public License for more details.
-*
-*    You should have received a copy of the GNU Affero General Public License
-*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*
-*    As a special exception, the copyright holders give permission to link the
-*    code of portions of this program with the OpenSSL library under certain
-*    conditions as described in each individual source file and distribute
-*    linked combinations including the program with the OpenSSL library. You
-*    must comply with the GNU Affero General Public License in all respects for
-*    all of the code used other than as permitted herein. If you modify file(s)
-*    with this exception, you may extend this exception to your version of the
-*    file(s), but you are not obligated to do so. If you do not wish to do so,
-*    delete this exception statement from your version. If you delete this
-*    exception statement from all source files in the program, then also delete
-*    it in the license file.
-*/
+ *    Copyright (C) 2008 10gen Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
 
 #define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kControl
 
@@ -203,8 +203,8 @@ MONGO_INITIALIZER_GENERAL(ServerLogRedirection,
                           ("default"))
 (InitializerContext*) {
     using logger::LogManager;
-    using logger::MessageEventEphemeral;
     using logger::MessageEventDetailsEncoder;
+    using logger::MessageEventEphemeral;
     using logger::MessageEventWithContextEncoder;
     using logger::MessageLogDomain;
     using logger::RotatableFileAppender;
@@ -225,9 +225,8 @@ MONGO_INITIALIZER_GENERAL(ServerLogRedirection,
         manager->getGlobalDomain()->attachAppender(MessageLogDomain::AppenderAutoPtr(
             new SyslogAppender<MessageEventEphemeral>(new logger::MessageEventDetailsEncoder)));
         manager->getNamedDomain("javascriptOutput")
-            ->attachAppender(
-                MessageLogDomain::AppenderAutoPtr(new SyslogAppender<MessageEventEphemeral>(
-                    new logger::MessageEventDetailsEncoder)));
+            ->attachAppender(MessageLogDomain::AppenderAutoPtr(
+                new SyslogAppender<MessageEventEphemeral>(new logger::MessageEventDetailsEncoder)));
 #endif  // defined(_WIN32)
     } else if (!serverGlobalParams.logpath.empty()) {
         fassert(16448, !serverGlobalParams.logWithSyslog);
