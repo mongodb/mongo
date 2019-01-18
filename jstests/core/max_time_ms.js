161446183ef54319533aefeb2540556c72d0cb16
vrachev <vlad.rachev@mongodb.com>
2019-01-18 13:41:08 -0500

    SERVER-38954: Increase query execution time for the test case in max_time_ms.js that expects it to hit the time limit

--- jstests/core/max_time_ms.js
+++ jstests/core/max_time_ms.js
@@ -15,11 +15,12 @@ var res;
 var error;
 
 //
-// Simple positive test for query: a ~300ms query with a 100ms time limit should be aborted.
+// Simple positive test for query: a ~100 second query with a 100ms time limit should be aborted.
 //
 
 t.drop();
-t.insert([{}, {}, {}]);
+assert.writeOK(t.insert(Array.from({length: 1000}, _ => ({}))));
+
 cursor = t.find({
     $where: function() {
         sleep(100);
