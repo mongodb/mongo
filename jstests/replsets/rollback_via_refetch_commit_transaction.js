9fe77b9f6f61bc0eda2a51a98d2238017cb5f4d5
A. Jesse Jiryu Davis <jesse@mongodb.com>
2019-02-15 18:05:23 -0500

    SERVER-39623 Fix race in rollback_via_refetch_commit_transaction.js

--- jstests/replsets/rollback_via_refetch_commit_transaction.js
+++ jstests/replsets/rollback_via_refetch_commit_transaction.js
@@ -7,50 +7,41 @@
  * @tags: [uses_transactions, uses_prepare_transaction]
  */
 
+TestData.skipCheckDBHashes = true;
+
 (function() {
     "use strict";
-    load("jstests/libs/write_concern_util.js");
     load("jstests/core/txns/libs/prepare_helpers.js");
+    load("jstests/replsets/libs/rollback_test.js");
 
     const dbName = "test";
     const collName = "rollback_via_refetch_commit_transaction";
 
+    // Provide RollbackTest with custom ReplSetTest so we can set forceRollbackViaRefetch.
     const rst = new ReplSetTest({
         name: collName,
         nodes: 3,
         useBridge: true,
         nodeOptions: {setParameter: "forceRollbackViaRefetch=true"}
     });
-    let nodes = rst.startSet();
 
-    let config = rst.getReplSetConfig();
+    rst.startSet();
+    const config = rst.getReplSetConfig();
     config.members[2].priority = 0;
+    config.settings = {chainingAllowed: false};
     rst.initiate(config);
 
-    const rollbackNode = nodes[0];
-    const syncSource = nodes[1];
-    const tiebreakerNode = nodes[2];
+    const rollbackTest = new RollbackTest(collName, rst);
 
-    // Wait for primary to be up and ready.
-    let primary = rst.getPrimary();
-    assert.eq(rollbackNode, primary);
+    // Create collection that exists on the sync source and rollback node.
+    assert.commandWorked(rollbackTest.getPrimary().getDB(dbName).runCommand(
+        {create: collName, writeConcern: {w: 3}}));
 
-    // Also wait for the secondaries.
-    rst.awaitSecondaryNodes();
-
-    // Create the collection we're using beforehand.
-    assert.commandWorked(
-        primary.getDB(dbName).runCommand({create: collName, writeConcern: {w: 3}}));
-
-    // Stop replication on the tiebreaker node, but keep it connected to the primary.
-    stopServerReplication(tiebreakerNode);
-
-    // Partition out the future primary so it doesn't receive the oplog entry.
-    syncSource.disconnect(rollbackNode);
-    syncSource.disconnect(tiebreakerNode);
+    // Stop replication from the current primary ("rollbackNode").
+    const rollbackNode = rollbackTest.transitionToRollbackOperations();
 
     // Issue a 'prepareTransaction' command just to the current primary.
-    const session = primary.getDB(dbName).getMongo().startSession({causalConsistency: false});
+    const session = rollbackNode.getDB(dbName).getMongo().startSession({causalConsistency: false});
     const sessionDB = session.getDatabase(dbName);
     const sessionColl = sessionDB.getCollection(collName);
     session.startTransaction();
@@ -59,43 +50,15 @@
         session.getDatabase('admin').adminCommand({prepareTransaction: 1, writeConcern: {w: 1}}));
     assert(result.prepareTimestamp,
            "prepareTransaction did not return a 'prepareTimestamp': " + tojson(result));
-
     PrepareHelpers.commitTransactionAfterPrepareTS(session, result.prepareTimestamp);
 
-    // Shift the partition to only isolate the rollback node (current primary) instead.
-    rollbackNode.disconnect(tiebreakerNode);
+    // Step down current primary and elect a node that lacks the commit.
+    rollbackTest.transitionToSyncSourceOperationsBeforeRollback();
 
-    // Issue an explicit stepdown command to save us some of the wait.
-    try {
-        assert.commandWorked(rollbackNode.adminCommand({replSetStepDown: 60, force: true}));
-    } catch (e) {
-        // Stepdown may fail if the node has already started stepping down.
-        if (!ErrorCodes.isNotMasterError(e))
-            throw e;
-        print('Caught exception from replSetStepDown: ' + e);
-    }
-
-    // Wait for the old primary to finish stepping down and become a secondary.
-    rst.waitForState(rollbackNode, ReplSetTest.State.SECONDARY);
-
-    // Let the sync source step up in its place by giving it the third node's vote.
-    syncSource.reconnect(tiebreakerNode);
-    primary = rst.getPrimary();
-    assert.eq(syncSource, primary);
-
-    // Make sure the sync source has something new on it. We let the tiebreaker node replicate
-    // that for some added test robustness.
-    restartServerReplication(tiebreakerNode);
-    assert.writeOK(
-        syncSource.getDB(dbName)["syncSourceOnly"].insert({e: 1, writeConcern: {w: "majority"}}));
-
+    // Verify the old primary crashes trying to roll back.
     clearRawMongoProgramOutput();
-
-    // Reconnect the old primary to the rest of the set. We expect it to go into rollback.
-    rollbackNode.reconnect(syncSource);
-    rollbackNode.reconnect(tiebreakerNode);
-
-    // Verify that it crashed because it failed to roll back.
+    rollbackTest.transitionToSyncSourceOperationsDuringRollback();
+    jsTestLog("Waiting for crash");
     assert.soon(function() {
         try {
             rollbackNode.getDB("local").runCommand({ping: 1});
@@ -105,9 +68,10 @@
         return false;
     }, "Node did not fassert", ReplSetTest.kDefaultTimeoutMS);
 
+    // Let the ReplSetTest know the old primary is down.
     rst.stop(rst.getNodeId(rollbackNode), undefined, {allowedExitCode: MongoRunner.EXIT_ABRUPT});
 
-    var msg = RegExp("Can't roll back this command yet: ");
+    const msg = RegExp("Can't roll back this command yet: ");
     assert.soon(function() {
         return rawMongoProgramOutput().match(msg);
     }, "Node did not fail to roll back entry.");
