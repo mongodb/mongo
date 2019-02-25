698410ce76f2b802227dfe633d7065ecd1f62678
Xiangyu Yao <xiangyu.yao@mongodb.com>
2019-02-25 14:38:18 -0500

    SERVER-35732 Test dbStats.fsUsedSize behavior for database with hyphenated names
    (cherry picked from commit 2e206b90bde9e41263d4073522248091c49437fa)

---
+++ jstests/noPassthrough/hyphenated_database_name.js
@@ -0,0 +1,24 @@
+/**
+ * Test that hyphenated database name can work with dbStats when directoryperdb is turned on.
+ *
+ * @tags: [requires_persistence]
+ */
+(function() {
+    "use strict";
+    var isDirectoryPerDBSupported = jsTest.options().storageEngine == "wiredTiger" ||
+        jsTest.options().storageEngine == "mmapv1" || !jsTest.options().storageEngine;
+    if (!isDirectoryPerDBSupported)
+        return;
+
+    const dbName = "test-hyphen";
+    let conn = MongoRunner.runMongod({directoryperdb: ''});
+
+    conn.getDB(dbName).a.insert({x: 1});
+    let res = conn.getDB(dbName).runCommand({dbStats: 1, scale: 1});
+    jsTestLog("dbStats: " + tojson(res));
+    assert(res.db == "test-hyphen");
+    assert(res.fsUsedSize > 0);
+    assert(res.fsTotalSize > 0);
+
+    MongoRunner.stopMongod(conn);
+})();
