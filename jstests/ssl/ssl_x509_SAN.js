51c8325894cf148d0f44d41b570f5238cc18a9a6
Mark Benvenuto <mark.benvenuto@mongodb.com>
2019-02-28 13:16:43 -0500

    SERVER-39592 Disable IPv6 connection in jstests\ssl\ssl_x509_SAN.js on machines that lack ipv6

--- jstests/ssl/ssl_x509_SAN.js
+++ jstests/ssl/ssl_x509_SAN.js
@@ -8,32 +8,62 @@ load('jstests/ssl/libs/ssl_helpers.js');
     const CA_CERT = "jstests/libs/ca.pem";
     const CLIENT_CERT = "jstests/libs/client.pem";
 
+    // Some test machines lack ipv6 so test for by starting a mongod that needs to bind to an ipv6
+    // address.
+    var hasIpv6 = true;
+    const mongodHasIpv6 = MongoRunner.runMongod({
+        sslMode: "requireSSL",
+        sslPEMKeyFile: SERVER1_CERT,
+        sslCAFile: CA_CERT,
+        ipv6: "",
+        bind_ip: "::1,127.0.0.1"
+    });
+    if (mongodHasIpv6 == null) {
+        jsTest.log("Unable to run all tests because ipv6 is not on machine, see BF-10990");
+        hasIpv6 = false;
+    } else {
+        MongoRunner.stopMongod(mongodHasIpv6);
+    }
+
     function authAndTest(cert_option) {
         function test_host(host, port) {
-            const mongo = runMongoProgram("mongo",
-                                          "--host",
-                                          host,
-                                          "--port",
-                                          port,
-                                          "--ipv6",
-                                          "--ssl",
-                                          "--sslCAFile",
-                                          CA_CERT,
-                                          "--sslPEMKeyFile",
-                                          CLIENT_CERT,
-                                          "--eval",
-                                          ";");
+            let args = [
+                "mongo",
+                "--host",
+                host,
+                "--port",
+                port,
+                "--ssl",
+                "--sslCAFile",
+                CA_CERT,
+                "--sslPEMKeyFile",
+                CLIENT_CERT,
+                "--eval",
+                ";"
+            ];
+
+            if (hasIpv6) {
+                args.push("--ipv6");
+            }
+
+            const mongo = runMongoProgram.apply(null, args);
 
             assert.eq(0, mongo, "Connection succeeded");
         }
 
-        const x509_options = {sslMode: "requireSSL", sslCAFile: CA_CERT, ipv6: "", bind_ip_all: ""};
+        const x509_options = {sslMode: "requireSSL", sslCAFile: CA_CERT, bind_ip_all: ""};
+
+        if (hasIpv6) {
+            Object.extend(x509_options, {ipv6: ""});
+        }
 
         let mongod = MongoRunner.runMongod(Object.merge(x509_options, cert_option));
 
         test_host("localhost", mongod.port);
         test_host("127.0.0.1", mongod.port);
-        test_host("::1", mongod.port);
+        if (hasIpv6) {
+            test_host("::1", mongod.port);
+        }
 
         MongoRunner.stopMongod(mongod);
     }
