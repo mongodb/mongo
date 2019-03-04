0e782a2bfd418df3b991bccb1716c1f1dd6b5163
Nick Zolnierz <nicholas.zolnierz@mongodb.com>
2019-03-04 12:06:22 -0500

    SERVER-39904 In out_max_time_ms.js, ensure that mongos does not prematurely time out before a command reaches the shard(s)

--- jstests/noPassthrough/out_max_time_ms.js
+++ jstests/noPassthrough/out_max_time_ms.js
@@ -5,6 +5,7 @@
 (function() {
     load("jstests/aggregation/extras/out_helpers.js");  // For withEachOutMode().
     load("jstests/libs/fixture_helpers.js");            // For isMongos().
+    load("jstests/libs/profiler.js");  // For profilerHasSingleMatchingEntryOrThrow.
 
     const kDBName = "test";
     const kSourceCollName = "out_max_time_ms_source";
@@ -36,11 +37,10 @@
     }
 
     /**
-     * Given a $out mode and two connections, run a $out aggregation against 'connToQuery', force
-     * 'connToHang' to hang on the given failpoint, and ensure that the $out maxTimeMS expires.
+     * Given a $out mode and a mongod connection, run a $out aggregation against 'conn' which hangs
+     * on the given failpoint and ensure that the $out maxTimeMS expires.
      */
-    function forceAggregationToHangAndCheckMaxTimeMsExpires(
-        mode, connToQuery, connToHang, failPointName) {
+    function forceAggregationToHangAndCheckMaxTimeMsExpires(mode, conn, failPointName) {
         // Use a short maxTimeMS so that the test completes in a reasonable amount of time. We will
         // use the 'maxTimeNeverTimeOut' failpoint to ensure that the operation does not
         // prematurely time out.
@@ -58,10 +58,10 @@
         if (mode == "replaceCollection")
             delete failpointCommand.data;
 
-        assert.commandWorked(connToHang.getDB("admin").runCommand(failpointCommand));
+        assert.commandWorked(conn.getDB("admin").runCommand(failpointCommand));
 
         // Make sure we don't run out of time before the failpoint is hit.
-        assert.commandWorked(connToHang.getDB("admin").runCommand(
+        assert.commandWorked(conn.getDB("admin").runCommand(
             {configureFailPoint: "maxTimeNeverTimeOut", mode: "alwaysOn"}));
 
         // Build the parallel shell function.
@@ -75,11 +75,11 @@
             assert.eq(err.code, ErrorCodes.MaxTimeMSExpired, "expected aggregation to fail");
         };
         shellStr += `(${runAggregate.toString()})();`;
-        const awaitShell = startParallelShell(shellStr, connToQuery.port);
+        const awaitShell = startParallelShell(shellStr, conn.port);
 
-        waitUntilServerHangsOnFailPoint(connToHang, failPointName);
+        waitUntilServerHangsOnFailPoint(conn, failPointName);
 
-        assert.commandWorked(connToHang.getDB("admin").runCommand(
+        assert.commandWorked(conn.getDB("admin").runCommand(
             {configureFailPoint: "maxTimeNeverTimeOut", mode: "off"}));
 
         // The aggregation running in the parallel shell will hang on the failpoint, burning
@@ -89,20 +89,17 @@
         // Now drop the failpoint, allowing the aggregation to proceed. It should hit an
         // interrupt check and terminate immediately.
         assert.commandWorked(
-            connToHang.getDB("admin").runCommand({configureFailPoint: failPointName, mode: "off"}));
+            conn.getDB("admin").runCommand({configureFailPoint: failPointName, mode: "off"}));
 
         // Wait for the parallel shell to finish.
         assert.eq(awaitShell(), 0);
     }
 
-    function runTest(mode, connToQuery, connToHang, setupCollections) {
+    function runUnshardedTest(mode, conn) {
         jsTestLog("Running test in mode " + mode);
-        if (mode == "replaceCollection" && FixtureHelpers.isMongos(connToQuery.getDB("admin"))) {
-            return;
-        }
 
-        const sourceColl = connToQuery.getDB(kDBName)[kSourceCollName];
-        const destColl = connToQuery.getDB(kDBName)[kDestCollName];
+        const sourceColl = conn.getDB(kDBName)[kSourceCollName];
+        const destColl = conn.getDB(kDBName)[kDestCollName];
         assert.commandWorked(destColl.remove({}));
 
         // Be sure we're able to read from a cursor with a maxTimeMS set on it.
@@ -120,14 +117,13 @@
         // Force the aggregation to hang while the batch is being written.
         const kFailPointName =
             mode == "replaceDocuments" ? "hangDuringBatchUpdate" : "hangDuringBatchInsert";
-        forceAggregationToHangAndCheckMaxTimeMsExpires(
-            mode, connToQuery, connToHang, kFailPointName);
+        forceAggregationToHangAndCheckMaxTimeMsExpires(mode, conn, kFailPointName);
 
         assert.commandWorked(destColl.remove({}));
 
         // Force the aggregation to hang while the batch is being built.
         forceAggregationToHangAndCheckMaxTimeMsExpires(
-            mode, connToQuery, connToHang, "hangWhileBuildingDocumentSourceOutBatch");
+            mode, conn, "hangWhileBuildingDocumentSourceOutBatch");
     }
 
     // Run on a standalone.
@@ -135,9 +131,53 @@
         const conn = MongoRunner.runMongod({});
         assert.neq(null, conn, 'mongod was unable to start up');
         insertDocs(conn.getDB(kDBName)[kSourceCollName]);
-        withEachOutMode((mode) => runTest(mode, conn, conn));
+        withEachOutMode((mode) => runUnshardedTest(mode, conn));
         MongoRunner.stopMongod(conn);
     })();
+
+    // Runs a $out against 'mongosConn' and verifies that the maxTimeMS value is included in the
+    // command sent to mongod. Since the actual timeout can unreliably happen in mongos before even
+    // reaching the shard, we instead set a very large timeout and verify that the command sent to
+    // mongod includes the maxTimeMS.
+    function runShardedTest(mode, mongosConn, mongodConn, comment) {
+        jsTestLog("Running sharded test in mode " + mode);
+        if (mode == "replaceCollection") {
+            return;
+        }
+
+        // Set a large timeout since we expect the command to finish.
+        const maxTimeMS = 1000 * 20;
+
+        const sourceColl = mongosConn.getDB(kDBName)[kSourceCollName];
+        const destColl = mongosConn.getDB(kDBName)[kDestCollName];
+        assert.commandWorked(destColl.remove({}));
+
+        // Make sure we don't timeout in mongos before even reaching the shards.
+        assert.commandWorked(mongosConn.getDB("admin").runCommand(
+            {configureFailPoint: "maxTimeNeverTimeOut", mode: "alwaysOn"}));
+
+        const cursor = sourceColl.aggregate([{$out: {to: destColl.getName(), mode: mode}}],
+                                            {maxTimeMS: maxTimeMS, comment: comment});
+        assert(!cursor.hasNext());
+
+        // Filter the profiler entries on the existence of $out, since aggregations through mongos
+        // will include an extra aggregation with an empty pipeline to establish cursors on the
+        // shards.
+        assert.soon(function() {
+            return mongodConn.getDB(kDBName)
+                       .system.profile
+                       .find({
+                           "command.aggregate": kSourceCollName,
+                           "command.pipeline.$out": {"$exists": true},
+                           "command.comment": comment,
+                           "command.maxTimeMS": maxTimeMS,
+                       })
+                       .itcount() == 1;
+        });
+
+        assert.commandWorked(mongosConn.getDB("admin").runCommand(
+            {configureFailPoint: "maxTimeNeverTimeOut", mode: "off"}));
+    }
 
     // Run on a sharded cluster.
     (function() {
@@ -159,11 +199,12 @@
                      kDBName);
         insertDocs(st.s.getDB(kDBName)[kSourceCollName]);
 
-        // Run the test with 'destColl' unsharded.
-        withEachOutMode((mode) => runTest(mode,
-                                          st.s,      // connection to query
-                                          st.shard0  // connection to force a hang
-                                          ));
+        // Start the profiler on each shard so that we can examine the $out's maxTimeMS.
+        assert.commandWorked(st.shard0.getDB(kDBName).setProfilingLevel(2));
+        assert.commandWorked(st.shard1.getDB(kDBName).setProfilingLevel(2));
+
+        // // Run the test with 'destColl' unsharded.
+        withEachOutMode((mode) => runShardedTest(mode, st.s, st.shard0, mode + "_unshardedDest"));
 
         // Run the test with 'destColl' sharded. This means that writes will be sent to both
         // shards, and if either one hangs, the MaxTimeMS will expire.
@@ -174,16 +215,13 @@
                      {_id: 6},  // move
                      kDBName);
 
-        function runTestMakingShardHang(shard) {
-            jsTestLog("Running test forcing shard " + shard.name + " to hang");
-            withEachOutMode((mode) => runTest(mode,
-                                              st.s,  // connection to query
-                                              shard  // connection to force a hang
-                                              ));
-        }
+        jsTestLog("Running test forcing shard " + st.shard0.name + " to hang");
+        withEachOutMode((mode) => runShardedTest(
+                            mode, st.s, st.shard0, mode + "_shardedDest_" + st.shard0.name));
 
-        runTestMakingShardHang(st.shard0);
-        runTestMakingShardHang(st.shard1);
+        jsTestLog("Running test forcing shard " + st.shard1.name + " to hang");
+        withEachOutMode((mode) => runShardedTest(
+                            mode, st.s, st.shard1, mode + "_shardedDest_" + st.shard1.name));
 
         st.stop();
     })();
