bc276668e8d992ff833fdbfd0f4280419d11eda1
Esha Maharishi <esha.maharishi@mongodb.com>
2019-03-08 12:53:28 -0500

    Revert "SERVER-37886 Make prepareTransaction set Client last OpTime to the greater of the prepare OpTime and system last OpTime if the Client's last OpTime is less than the prepare OpTime"
    This reverts commit 8f3ad3eab4631a393cb4c2bfff69015baf63ebc9.

--- src/mongo/db/s/txn_two_phase_commit_cmds.cpp
+++ src/mongo/db/s/txn_two_phase_commit_cmds.cpp
@@ -111,14 +111,11 @@ public:
                 auto& replClient = repl::ReplClientInfo::forClient(opCtx->getClient());
                 auto prepareOpTime = txnParticipant.getPrepareOpTime();
 
-                // Ensure waiting for writeConcern of the prepare OpTime.
+                // Set the client optime to be prepareOpTime if it's not already later than
+                // prepareOpTime. This ensures that we wait for writeConcern and that prepareOpTime
+                // will be committed.
                 if (prepareOpTime > replClient.getLastOp()) {
-                    // In case this node has failed over, in which case the term will have
-                    // increased, set the Client's last OpTime to the larger of the system last
-                    // OpTime and the prepare OpTime.
-                    const auto systemLastOpTime =
-                        repl::ReplicationCoordinator::get(opCtx)->getMyLastAppliedOpTime();
-                    replClient.setLastOp(std::max(prepareOpTime, systemLastOpTime));
+                    replClient.setLastOp(prepareOpTime);
                 }
 
                 invariant(opCtx->recoveryUnit()->getPrepareTimestamp() ==
