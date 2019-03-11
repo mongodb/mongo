58ff5248610305a35403020077a4dbdcd0691ff0
A. Jesse Jiryu Davis <jesse@mongodb.com>
2019-03-11 07:37:01 -0400

    SERVER-40060 Add retry in ReplSetTest.initiate

--- src/mongo/shell/replsettest.js
+++ src/mongo/shell/replsettest.js
@@ -921,6 +921,23 @@ var ReplSetTest = function(opts) {
         }
     };
 
+    function replSetCommandWithRetry(master, cmd) {
+        printjson(cmd);
+        const cmdName = Object.keys(cmd)[0];
+        const errorMsg = `${cmdName} during initiate failed`;
+        assert.retry(() => {
+            assert.commandWorkedOrFailedWithCode(
+                master.runCommand(cmd),
+                [
+                  ErrorCodes.NodeNotFound,
+                  ErrorCodes.NewReplicaSetConfigurationIncompatible,
+                  ErrorCodes.InterruptedDueToStepDown
+                ],
+                errorMsg);
+            return true;
+        }, errorMsg, 3, 5 * 1000);
+    }
+
     /**
      * Runs replSetInitiate on the first node of the replica set.
      * Ensures that a primary is elected (not necessarily node 0).
@@ -953,9 +970,13 @@ var ReplSetTest = function(opts) {
         this._setDefaultConfigOptions(config);
 
         cmd[cmdKey] = config;
-        printjson(cmd);
 
-        assert.commandWorked(master.runCommand(cmd), tojson(cmd));
+        // replSetInitiate and replSetReconfig commands can fail with a NodeNotFound error if a
+        // heartbeat times out during the quorum check. They may also fail with
+        // NewReplicaSetConfigurationIncompatible on similar timeout during the config validation
+        // stage while deducing isSelf(). This can fail with an InterruptedDueToStepDown error when
+        // interrupted. We try several times, to reduce the chance of failing this way.
+        replSetCommandWithRetry(master, cmd);
         this.getPrimary();  // Blocks until there is a primary.
 
         // Reconfigure the set to contain the correct number of nodes (if necessary).
@@ -974,40 +995,7 @@ var ReplSetTest = function(opts) {
 
             cmd = {replSetReconfig: config};
             print("Reconfiguring replica set to add in other nodes");
-            printjson(cmd);
-
-            // replSetInitiate and replSetReconfig commands can fail with a NodeNotFound error
-            // if a heartbeat times out during the quorum check.
-            // They may also fail with NewReplicaSetConfigurationIncompatible on similar timeout
-            // during the config validation stage while deducing isSelf().
-            // This can fail with an InterruptedDueToStepDown error when interrupted.
-            // We retry three times to reduce the chance of failing this way.
-            assert.retry(() => {
-                var res;
-                try {
-                    res = master.runCommand(cmd);
-                    if (res.ok === 1) {
-                        return true;
-                    }
-                } catch (e) {
-                    // reconfig can lead to a stepdown if the primary looks for a majority before
-                    // a majority of nodes have successfully joined the set. If there is a stepdown
-                    // then the reconfig request will be killed and respond with a network error.
-                    if (isNetworkError(e)) {
-                        return true;
-                    }
-                    throw e;
-                }
-
-                assert.commandFailedWithCode(res,
-                                             [
-                                               ErrorCodes.NodeNotFound,
-                                               ErrorCodes.NewReplicaSetConfigurationIncompatible,
-                                               ErrorCodes.InterruptedDueToStepDown
-                                             ],
-                                             "replSetReconfig during initiate failed");
-                return false;
-            }, "replSetReconfig during initiate failed", 3, 5 * 1000);
+            replSetCommandWithRetry(master, cmd);
         }
 
         // Setup authentication if running test with authentication
