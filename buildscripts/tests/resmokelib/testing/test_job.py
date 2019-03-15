bba439e61b06656c2f1efa4ba11995e3afae4173
Jonathan Abrahams <jonathan@mongodb.com>
2019-03-15 10:31:49 -0400

    SERVER-39857 Refactor test_job.py to use a better mechanism to determine the number of jobs on the queue

--- buildscripts/tests/resmokelib/testing/test_job.py
+++ buildscripts/tests/resmokelib/testing/test_job.py
@@ -50,122 +50,146 @@ class TestJob(unittest.TestCase):
 
     @staticmethod
     def expected_run_num(time_repeat_tests_secs, test_time_secs):
-        """Return the number of times a test is expected to run.
-
-        Note this result should be used as an approximation as the test_time_secs relies on
-        time.sleep(), which does not guarantee an exact pause, plus the overhead of other functions
-        running for each test.
-        """
-        return (time_repeat_tests_secs / test_time_secs) - 1
+        """Return the number of times a test is expected to run."""
+        return time_repeat_tests_secs / test_time_secs
 
     def test__run_num_repeat(self):
         num_repeat_tests = 3
         queue = _queue.Queue()
         suite_options = self.get_suite_options(num_repeat_tests=num_repeat_tests)
-        job_object = UnitJob(suite_options, {})
+        mock_time = MockTime(1)
+        job_object = UnitJob(suite_options)
         self.queue_tests(self.TESTS, queue, queue_element.QueueElemRepeatNum, suite_options)
-        job_object._run(queue, self.mock_interrupt_flag())
+        with mock.patch("time.time", side_effect=mock_time.time):
+            job_object._run(queue, self.mock_interrupt_flag())
         self.assertEqual(job_object.total_test_num, num_repeat_tests * len(self.TESTS))
         for test in self.TESTS:
             self.assertEqual(job_object.tests[test], num_repeat_tests)
 
     def test__run_time_repeat_time_no_min_max(self):
-        delay = 0.1
-        time_repeat_tests_secs = 1
-        expected_tests_run = self.expected_run_num(time_repeat_tests_secs, delay)
+        increment = 1
+        time_repeat_tests_secs = 10
+        expected_tests_run = self.expected_run_num(time_repeat_tests_secs, increment)
         queue = _queue.Queue()
         suite_options = self.get_suite_options(time_repeat_tests_secs=time_repeat_tests_secs)
-        job_object = UnitJob(suite_options, {}, delay=delay)
+        mock_time = MockTime(increment)
+        job_object = UnitJob(suite_options)
         self.queue_tests(self.TESTS, queue, queue_element.QueueElemRepeatTime, suite_options)
-        job_object._run(queue, self.mock_interrupt_flag())
-        self.assertGreaterEqual(job_object.total_test_num, expected_tests_run * len(self.TESTS))
+        with mock.patch("time.time", side_effect=mock_time.time):
+            job_object._run(queue, self.mock_interrupt_flag())
+        self.assertEqual(job_object.total_test_num, expected_tests_run * len(self.TESTS))
         for test in self.TESTS:
-            self.assertGreaterEqual(job_object.tests[test], expected_tests_run)
+            self.assertEqual(job_object.tests[test], expected_tests_run)
 
     def test__run_time_repeat_time_no_min(self):
-        delay = 0.1
-        time_repeat_tests_secs = 1
+        increment = 1
+        time_repeat_tests_secs = 10
         num_repeat_tests_max = 100
-        expected_tests_run = self.expected_run_num(time_repeat_tests_secs, delay)
+        expected_tests_run = self.expected_run_num(time_repeat_tests_secs, increment)
         queue = _queue.Queue()
         suite_options = self.get_suite_options(time_repeat_tests_secs=time_repeat_tests_secs)
-        job_object = UnitJob(suite_options, {}, delay=delay)
+        mock_time = MockTime(increment)
+        job_object = UnitJob(suite_options)
         self.queue_tests(self.TESTS, queue, queue_element.QueueElemRepeatTime, suite_options)
-        job_object._run(queue, self.mock_interrupt_flag())
+        with mock.patch("time.time", side_effect=mock_time.time):
+            job_object._run(queue, self.mock_interrupt_flag())
         self.assertLess(job_object.total_test_num, num_repeat_tests_max * len(self.TESTS))
         for test in self.TESTS:
-            self.assertGreaterEqual(job_object.tests[test], expected_tests_run)
+            self.assertEqual(job_object.tests[test], expected_tests_run)
 
     def test__run_time_repeat_time_no_max(self):
-        delay = 0.1
-        time_repeat_tests_secs = 1
+        increment = 1
+        time_repeat_tests_secs = 10
         num_repeat_tests_min = 1
-        expected_tests_run = self.expected_run_num(time_repeat_tests_secs, delay)
+        expected_tests_run = self.expected_run_num(time_repeat_tests_secs, increment)
         queue = _queue.Queue()
         suite_options = self.get_suite_options(time_repeat_tests_secs=time_repeat_tests_secs,
                                                num_repeat_tests_min=num_repeat_tests_min)
-        job_object = UnitJob(suite_options, {}, delay=delay)
+        mock_time = MockTime(increment)
+        job_object = UnitJob(suite_options)
         self.queue_tests(self.TESTS, queue, queue_element.QueueElemRepeatTime, suite_options)
-        job_object._run(queue, self.mock_interrupt_flag())
+        with mock.patch("time.time", side_effect=mock_time.time):
+            job_object._run(queue, self.mock_interrupt_flag())
         self.assertGreater(job_object.total_test_num, num_repeat_tests_min * len(self.TESTS))
         for test in self.TESTS:
-            self.assertGreaterEqual(job_object.tests[test], expected_tests_run)
+            self.assertEqual(job_object.tests[test], expected_tests_run)
 
     def test__run_time_repeat_time(self):
-        delay = 0.1
-        time_repeat_tests_secs = 1
+        increment = 1
+        time_repeat_tests_secs = 10
         num_repeat_tests_min = 1
         num_repeat_tests_max = 100
-        expected_tests_run = self.expected_run_num(time_repeat_tests_secs, delay)
+        expected_tests_run = self.expected_run_num(time_repeat_tests_secs, increment)
         queue = _queue.Queue()
         suite_options = self.get_suite_options(time_repeat_tests_secs=time_repeat_tests_secs,
                                                num_repeat_tests_min=num_repeat_tests_min,
                                                num_repeat_tests_max=num_repeat_tests_max)
-        job_object = UnitJob(suite_options, {}, delay=delay)
+        mock_time = MockTime(increment)
+        job_object = UnitJob(suite_options)
         self.queue_tests(self.TESTS, queue, queue_element.QueueElemRepeatTime, suite_options)
-        job_object._run(queue, self.mock_interrupt_flag())
+        with mock.patch("time.time", side_effect=mock_time.time):
+            job_object._run(queue, self.mock_interrupt_flag())
         self.assertGreater(job_object.total_test_num, num_repeat_tests_min * len(self.TESTS))
         self.assertLess(job_object.total_test_num, num_repeat_tests_max * len(self.TESTS))
         for test in self.TESTS:
-            self.assertGreaterEqual(job_object.tests[test], expected_tests_run)
+            self.assertEqual(job_object.tests[test], expected_tests_run)
 
     def test__run_time_repeat_min(self):
-        delay = 0.1
-        time_repeat_tests_secs = 0.05
+        increment = 1
+        time_repeat_tests_secs = 2
         num_repeat_tests_min = 3
         num_repeat_tests_max = 100
         queue = _queue.Queue()
         suite_options = self.get_suite_options(time_repeat_tests_secs=time_repeat_tests_secs,
                                                num_repeat_tests_min=num_repeat_tests_min,
                                                num_repeat_tests_max=num_repeat_tests_max)
-        job_object = UnitJob(suite_options, {}, delay=delay)
+        mock_time = MockTime(increment)
+        job_object = UnitJob(suite_options)
         self.queue_tests(self.TESTS, queue, queue_element.QueueElemRepeatTime, suite_options)
-        job_object._run(queue, self.mock_interrupt_flag())
+        with mock.patch("time.time", side_effect=mock_time.time):
+            job_object._run(queue, self.mock_interrupt_flag())
         self.assertEqual(job_object.total_test_num, num_repeat_tests_min * len(self.TESTS))
         for test in self.TESTS:
             self.assertEqual(job_object.tests[test], num_repeat_tests_min)
 
     def test__run_time_repeat_max(self):
-        delay = 0.1
+        increment = 1
         time_repeat_tests_secs = 30
         num_repeat_tests_min = 1
         num_repeat_tests_max = 10
-        expected_time_repeat_tests = self.expected_run_num(time_repeat_tests_secs, delay)
+        expected_time_repeat_tests = self.expected_run_num(time_repeat_tests_secs, increment)
         queue = _queue.Queue()
         suite_options = self.get_suite_options(time_repeat_tests_secs=time_repeat_tests_secs,
                                                num_repeat_tests_min=num_repeat_tests_min,
                                                num_repeat_tests_max=num_repeat_tests_max)
-        job_object = UnitJob(suite_options, {}, delay=delay)
+        mock_time = MockTime(increment)
+        job_object = UnitJob(suite_options)
         self.queue_tests(self.TESTS, queue, queue_element.QueueElemRepeatTime, suite_options)
-        job_object._run(queue, self.mock_interrupt_flag())
+        with mock.patch("time.time", side_effect=mock_time.time):
+            job_object._run(queue, self.mock_interrupt_flag())
         self.assertEqual(job_object.total_test_num, num_repeat_tests_max * len(self.TESTS))
         for test in self.TESTS:
             self.assertEqual(job_object.tests[test], num_repeat_tests_max)
             self.assertLess(job_object.tests[test], expected_time_repeat_tests)
 
 
+class MockTime(object):
+    """Class to mock time.time."""
+
+    def __init__(self, increment):
+        """Initialize with an increment which simulates a time increment."""
+        self._time = 0
+        self._increment = increment
+
+    def time(self):
+        """Simulate time.time by incrementing for every invocation."""
+        cur_time = self._time
+        self._time += self._increment
+        return cur_time
+
+
 class UnitJob(job.Job):  # pylint: disable=too-many-instance-attributes
-    def __init__(self, suite_options, _, delay=0):  #pylint: disable=super-init-not-called
+    def __init__(self, suite_options):  #pylint: disable=super-init-not-called
         self.job_num = 0
         self.logger = logging.getLogger("job_unittest")
         self.fixture = None
@@ -175,7 +199,6 @@ class UnitJob(job.Job):  # pylint: disable=too-many-instance-attributes
         self.suite_options = suite_options
         self.test_queue_logger = logging.getLogger("job_unittest")
         self.total_test_num = 0
-        self.delay = delay
         self.tests = {}
 
     def _execute_test(self, test):
@@ -183,4 +206,3 @@ class UnitJob(job.Job):  # pylint: disable=too-many-instance-attributes
         if test.test_name not in self.tests:
             self.tests[test.test_name] = 0
         self.tests[test.test_name] += 1
-        time.sleep(self.delay)
