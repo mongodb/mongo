4309c3c77f28ccc27760e4781797b1d2afd8cb06
Geert Bosch <geert@mongodb.com>
2019-05-13 14:17:07 -0400

    SERVER-41117 Blacklist autocomplete.js from parallel testing

--- jstests/libs/parallelTester.js
+++ jstests/libs/parallelTester.js
@@ -192,6 +192,7 @@ if (typeof _threadInject != "undefined") {
             "geo_update_btree2.js",   // SERVER-11132 test disables table scans
             "update_setOnInsert.js",  // SERVER-9982
             "max_time_ms.js",         // Sensitive to query execution time, by design
+            "autocomplete.js",        // Likewise.
 
             // This overwrites MinKey/MaxKey's singleton which breaks
             // any other test that uses MinKey/MaxKey
