02f1d3c5196953588c7a2d3e9aa0114eb7e1c643
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2019-05-14 08:02:49 -0400

    SERVER-37235 Adding missing header file

--- src/mongo/s/transaction_router.cpp
+++ src/mongo/s/transaction_router.cpp
@@ -41,6 +41,7 @@
 #include "mongo/db/logical_session_id.h"
 #include "mongo/db/logical_session_id.h"
 #include "mongo/db/repl/read_concern_args.h"
+#include "mongo/db/session_catalog.h"
 #include "mongo/executor/task_executor_pool.h"
 #include "mongo/rpc/get_status_from_command_result.h"
 #include "mongo/s/async_requests_sender.h"
