26e3184399b3e9ed88c632715266eb4b6b21b5a9
Benety Goh <benety@mongodb.com>
2019-05-16 19:36:04 -0400

    Revert "SERVER-40041 block prepared transactions behind index builds during initial sync"
    This reverts commit 17603c3b095703521268824b41eca24fa528c1c5.

--- src/mongo/db/repl/transaction_oplog_application.cpp
+++ src/mongo/db/repl/transaction_oplog_application.cpp
@@ -296,7 +296,7 @@ Status _applyPrepareTransaction(OperationContext* opCtx,
         opCtx->recoveryUnit()->setRoundUpPreparedTimestamps(true);
     }
 
-    // Block application of prepare oplog entries on secondaries when a concurrent background index
+    // Block application of prepare oplog entry on secondaries when a concurrent background index
     // build is running.
     // This will prevent hybrid index builds from corrupting an index on secondary nodes if a
     // prepared transaction becomes prepared during a build but commits after the index build
@@ -304,12 +304,8 @@ Status _applyPrepareTransaction(OperationContext* opCtx,
     for (const auto& op : ops) {
         auto ns = op.getNss();
         auto uuid = *op.getUuid();
-        if (BackgroundOperation::inProgForNs(ns)) {
-            warning() << "blocking replication until index builds are finished on "
-                      << redact(ns.toString()) << ", due to prepared transaction";
-            BackgroundOperation::awaitNoBgOpInProgForNs(ns);
-            IndexBuildsCoordinator::get(opCtx)->awaitNoIndexBuildInProgressForCollection(uuid);
-        }
+        BackgroundOperation::awaitNoBgOpInProgForNs(ns);
+        IndexBuildsCoordinator::get(opCtx)->awaitNoIndexBuildInProgressForCollection(uuid);
     }
 
     // Transaction operations are in their own batch, so we can modify their opCtx.
