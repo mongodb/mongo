9f833a5af0e66a88d5473468826064648fdc5031
Esha Maharishi <esha.maharishi@mongodb.com>
2019-05-17 10:00:38 -0400

    SERVER-40979 Create class for tracking server-wide two phase commit metrics on mongod

---
+++ src/mongo/db/s/server_transaction_coordinators_metrics.cpp
@@ -0,0 +1,107 @@
+/**
+ *    Copyright (C) 2019-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/platform/basic.h"
+
+#include "mongo/db/s/server_transaction_coordinators_metrics.h"
+
+#include "mongo/db/operation_context.h"
+#include "mongo/db/service_context.h"
+
+namespace mongo {
+namespace {
+const auto ServerTransactionCoordinatorsMetricsDecoration =
+    ServiceContext::declareDecoration<ServerTransactionCoordinatorsMetrics>();
+}  // namespace
+
+ServerTransactionCoordinatorsMetrics* ServerTransactionCoordinatorsMetrics::get(
+    ServiceContext* service) {
+    return &ServerTransactionCoordinatorsMetricsDecoration(service);
+}
+
+ServerTransactionCoordinatorsMetrics* ServerTransactionCoordinatorsMetrics::get(
+    OperationContext* opCtx) {
+    return get(opCtx->getServiceContext());
+}
+
+std::int64_t ServerTransactionCoordinatorsMetrics::getTotalCreated() {
+    return _totalCreated.load();
+}
+void ServerTransactionCoordinatorsMetrics::incrementTotalCreated() {
+    _totalCreated.fetchAndAdd(1);
+}
+
+std::int64_t ServerTransactionCoordinatorsMetrics::getTotalStartedTwoPhaseCommit() {
+    return _totalStartedTwoPhaseCommit.load();
+}
+void ServerTransactionCoordinatorsMetrics::incrementTotalStartedTwoPhaseCommit() {
+    _totalStartedTwoPhaseCommit.fetchAndAdd(1);
+}
+
+std::int64_t ServerTransactionCoordinatorsMetrics::getCurrentWritingParticipantList() {
+    return _totalWritingParticipantList.load();
+}
+void ServerTransactionCoordinatorsMetrics::incrementCurrentWritingParticipantList() {
+    _totalWritingParticipantList.fetchAndAdd(1);
+}
+void ServerTransactionCoordinatorsMetrics::decrementCurrentWritingParticipantList() {
+    _totalWritingParticipantList.fetchAndSubtract(1);
+}
+
+std::int64_t ServerTransactionCoordinatorsMetrics::getCurrentWaitingForVotes() {
+    return _totalWaitingForVotes.load();
+}
+void ServerTransactionCoordinatorsMetrics::incrementCurrentWaitingForVotes() {
+    _totalWaitingForVotes.fetchAndAdd(1);
+}
+void ServerTransactionCoordinatorsMetrics::decrementCurrentWaitingForVotes() {
+    _totalWaitingForVotes.fetchAndSubtract(1);
+}
+
+std::int64_t ServerTransactionCoordinatorsMetrics::getCurrentWritingDecision() {
+    return _totalWritingDecision.load();
+}
+void ServerTransactionCoordinatorsMetrics::incrementCurrentWritingDecision() {
+    _totalWritingDecision.fetchAndAdd(1);
+}
+void ServerTransactionCoordinatorsMetrics::decrementCurrentWritingDecision() {
+    _totalWritingDecision.fetchAndSubtract(1);
+}
+
+std::int64_t ServerTransactionCoordinatorsMetrics::getCurrentWaitingForDecisionAcks() {
+    return _totalWaitingForDecisionAcks.load();
+}
+void ServerTransactionCoordinatorsMetrics::incrementCurrentWaitingForDecisionAcks() {
+    _totalWaitingForDecisionAcks.fetchAndAdd(1);
+}
+void ServerTransactionCoordinatorsMetrics::decrementCurrentWaitingForDecisionAcks() {
+    _totalWaitingForDecisionAcks.fetchAndSubtract(1);
+}
+
+}  // namespace mongo
---
+++ src/mongo/db/s/server_transaction_coordinators_metrics.h
@@ -0,0 +1,95 @@
+/**
+ *    Copyright (C) 2019-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#pragma once
+
+#include "mongo/db/operation_context.h"
+#include "mongo/db/service_context.h"
+
+namespace mongo {
+
+/**
+ * Container for server-wide metrics about transaction two-phase commits.
+ */
+class ServerTransactionCoordinatorsMetrics {
+    ServerTransactionCoordinatorsMetrics(const ServerTransactionCoordinatorsMetrics&) = delete;
+    ServerTransactionCoordinatorsMetrics& operator=(const ServerTransactionCoordinatorsMetrics&) =
+        delete;
+
+public:
+    ServerTransactionCoordinatorsMetrics() = default;
+
+    static ServerTransactionCoordinatorsMetrics* get(ServiceContext* service);
+    static ServerTransactionCoordinatorsMetrics* get(OperationContext* opCtx);
+
+    std::int64_t getTotalCreated();
+    void incrementTotalCreated();
+
+    std::int64_t getTotalStartedTwoPhaseCommit();
+    void incrementTotalStartedTwoPhaseCommit();
+
+    std::int64_t getCurrentWritingParticipantList();
+    void incrementCurrentWritingParticipantList();
+    void decrementCurrentWritingParticipantList();
+
+    std::int64_t getCurrentWaitingForVotes();
+    void incrementCurrentWaitingForVotes();
+    void decrementCurrentWaitingForVotes();
+
+    std::int64_t getCurrentWritingDecision();
+    void incrementCurrentWritingDecision();
+    void decrementCurrentWritingDecision();
+
+    std::int64_t getCurrentWaitingForDecisionAcks();
+    void incrementCurrentWaitingForDecisionAcks();
+    void decrementCurrentWaitingForDecisionAcks();
+
+private:
+    // The total number of transaction coordinators created on this process since the process's
+    // inception.
+    AtomicWord<std::int64_t> _totalCreated{0};
+
+    // The total number of transaction coordinators on this process that started a two-phase commit
+    // since the process's inception.
+    AtomicWord<std::int64_t> _totalStartedTwoPhaseCommit{0};
+
+    // The number of transaction coordinators currently in the "writing participant list" phase.
+    AtomicWord<std::int64_t> _totalWritingParticipantList{0};
+
+    // The number of transaction coordinators currently in the "waiting for votes" phase.
+    AtomicWord<std::int64_t> _totalWaitingForVotes{0};
+
+    // The number of transaction coordinators currently in the "writing decision" phase.
+    AtomicWord<std::int64_t> _totalWritingDecision{0};
+
+    // The number of transaction coordinators currently in the "waiting for decision acks" phase.
+    AtomicWord<std::int64_t> _totalWaitingForDecisionAcks{0};
+};
+
+}  // namespace mongo
