f88af354d7c59c1eda232a10a7781a181f5c3aaa
Louis Williams <louis.williams@mongodb.com>
2019-05-21 16:23:03 -0400

    SERVER-41254 Turn on write-ahead logging for hybrid index side tables

--- src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
@@ -620,9 +620,9 @@ StatusWith<std::string> WiredTigerRecordStore::generateCreateString(
     bool replicatedWrites = getGlobalReplSettings().usingReplSets() ||
         repl::ReplSettings::shouldRecoverFromOplogAsStandalone();
 
-    // Do not journal writes when 'ns' is an empty string, which is the case for internal-only
-    // temporary tables.
-    if (ns.size() && WiredTigerUtil::useTableLogging(NamespaceString(ns), replicatedWrites)) {
+    // Journal writes when 'ns' is an empty string, which is the case for internal-only temporary
+    // tables. This is for improved diagnostics.
+    if (!ns.size() || WiredTigerUtil::useTableLogging(NamespaceString(ns), replicatedWrites)) {
         ss << ",log=(enabled=true)";
     } else {
         ss << ",log=(enabled=false)";
@@ -675,11 +675,12 @@ WiredTigerRecordStore::WiredTigerRecordStore(WiredTigerKVEngine* kvEngine,
         invariant(_cappedMaxDocs == -1);
     }
 
-    if (!params.isReadOnly && !isTemp()) {
+    if (!params.isReadOnly) {
         bool replicatedWrites = getGlobalReplSettings().usingReplSets() ||
             repl::ReplSettings::shouldRecoverFromOplogAsStandalone();
-        uassertStatusOK(WiredTigerUtil::setTableLogging(
-            ctx, _uri, WiredTigerUtil::useTableLogging(NamespaceString(ns()), replicatedWrites)));
+        bool useLogging =
+            isTemp() || WiredTigerUtil::useTableLogging(NamespaceString(ns()), replicatedWrites);
+        uassertStatusOK(WiredTigerUtil::setTableLogging(ctx, _uri, useLogging));
     }
 
     if (_isOplog) {
