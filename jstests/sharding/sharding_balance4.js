be431419dbc96abbfb08acc0578a040aba16eba1
Matthew Saltz <matthew.saltz@mongodb.com>
2019-05-28 14:07:28 -0400

    Revert "SERVER-37674 Increase logging verbosity for sharding_balance4.js"
    This reverts commit 5b2179fb8f852c956d85a10d01dcae5c4ae4b0c2.

--- jstests/sharding/sharding_balance4.js
+++ jstests/sharding/sharding_balance4.js
@@ -7,12 +7,7 @@
  */
 (function() {
 
-    // TODO (SERVER-37698): Lower logging level.
-    var s = new ShardingTest({
-        shards: [{verbose: 3}, {verbose: 3}],
-        mongos: 1,
-        other: {chunkSize: 1, enableAutoSplit: true, mongosOptions: {verbose: 3}}
-    });
+    var s = new ShardingTest({shards: 2, mongos: 1, other: {chunkSize: 1, enableAutoSplit: true}});
 
     assert.commandWorked(s.s0.adminCommand({enablesharding: "test"}));
     s.ensurePrimaryShard('test', s.shard1.shardName);
@@ -93,6 +88,8 @@
                 print("not asserting for key failure: " + x + " want: " + e + " got: " + tojson(z));
                 return false;
             }
+
+            s.s.getDB("admin").runCommand({setParameter: 1, logLevel: 2});
 
             printjson(db.foo.findOne({_id: parseInt(x)}));
 
