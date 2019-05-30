a7e5069c5a5caae4d64efa9086b0c5f0766ab508
Xiangyu Yao <xiangyu.yao@mongodb.com>
2019-05-30 14:26:42 -0400

    SERVER-41398 Create IndexAccessMethodFactory
    (cherry picked from commit a54a1e4edcd48ee4aa5085e9067656196f73308f)

--- src/mongo/dbtests/framework.cpp
+++ src/mongo/dbtests/framework.cpp
@@ -42,6 +42,7 @@
 #include "mongo/db/client.h"
 #include "mongo/db/concurrency/lock_state.h"
 #include "mongo/db/dbdirectclient.h"
+#include "mongo/db/index/index_access_method_factory_impl.h"
 #include "mongo/db/index_builds_coordinator_mongod.h"
 #include "mongo/db/op_observer_registry.h"
 #include "mongo/db/s/sharding_state.h"
@@ -106,6 +107,8 @@ int runDbTests(int argc, char** argv) {
 
     initializeStorageEngine(globalServiceContext, StorageEngineInitFlags::kNone);
     DatabaseHolder::set(globalServiceContext, std::make_unique<DatabaseHolderImpl>());
+    IndexAccessMethodFactory::set(globalServiceContext,
+                                  std::make_unique<IndexAccessMethodFactoryImpl>());
     IndexBuildsCoordinator::set(globalServiceContext,
                                 std::make_unique<IndexBuildsCoordinatorMongod>());
     auto registry = stdx::make_unique<OpObserverRegistry>();
