99185ab9685a51466e56ef0fd862aaf1318a79d6
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2019-05-31 08:10:49 -0400

    SERVER-41141 Remove DB X lock acquisition for secondary index builds

--- src/mongo/db/index_builds_coordinator.h
+++ src/mongo/db/index_builds_coordinator.h
@@ -379,7 +379,7 @@ protected:
                      Collection* collection,
                      const NamespaceString& nss,
                      std::shared_ptr<ReplIndexBuildState> replState,
-                     Lock::DBLock* dbLock);
+                     boost::optional<Lock::CollectionLock>* collLock);
     /**
      * Returns total number of indexes in collection, including unfinished/in-progress indexes.
      *
