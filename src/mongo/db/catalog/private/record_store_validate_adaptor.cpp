8738b2b1586a016ded63d9396b99792d9e8379b9
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2019-06-04 16:29:41 -0400

    SERVER-41540 Fix _indexNsResultsMap usage

--- src/mongo/db/catalog/private/record_store_validate_adaptor.cpp
+++ src/mongo/db/catalog/private/record_store_validate_adaptor.cpp
@@ -87,14 +87,11 @@ Status RecordStoreValidateAdaptor::validate(const RecordId& recordId,
     for (auto& it : _indexConsistency->getIndexInfo()) {
         IndexInfo& indexInfo = it.second;
         const IndexDescriptor* descriptor = indexInfo.descriptor;
-        ValidateResults curRecordResults;
-
         const IndexAccessMethod* iam = _indexCatalog->getEntry(descriptor)->accessMethod();
 
         if (descriptor->isPartial()) {
             const IndexCatalogEntry* ice = _indexCatalog->getEntry(descriptor);
             if (!ice->getFilterExpression()->matchesBSON(recordBson)) {
-                (*_indexNsResultsMap)[descriptor->indexName()] = curRecordResults;
                 continue;
             }
         }
@@ -114,8 +111,9 @@ Status RecordStoreValidateAdaptor::validate(const RecordId& recordId,
                 {multikeyMetadataKeys.begin(), multikeyMetadataKeys.end()},
                 multikeyPaths)) {
             std::string msg = str::stream() << "Index " << descriptor->indexName()
-                                            << " is not multi-key, but a multikey path "
-                                            << " is present in document " << recordId;
+                                            << " is not multi-key but has more than one"
+                                            << " key in document " << recordId;
+            ValidateResults& curRecordResults = (*_indexNsResultsMap)[descriptor->indexName()];
             curRecordResults.errors.push_back(msg);
             curRecordResults.valid = false;
         }
@@ -140,7 +138,6 @@ Status RecordStoreValidateAdaptor::validate(const RecordId& recordId,
             ks.resetToKey(key, indexInfo.ord, recordId);
             _indexConsistency->addDocKey(ks, &indexInfo, recordId, key);
         }
-        (*_indexNsResultsMap)[descriptor->indexName()] = curRecordResults;
     }
     return status;
 }
