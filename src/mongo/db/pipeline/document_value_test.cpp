9069ee03843ed66cd7745da5c0ce72fb54c030ed
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2019-06-05 19:29:20 -0400

    Revert "SERVER-40869 Error when coercing out of bounds double to long"
    This reverts commit bcad0d20e517e10f0dab71a0cfabe0a9e25c401b.

--- src/mongo/db/pipeline/document_value_test.cpp
+++ src/mongo/db/pipeline/document_value_test.cpp
@@ -27,8 +27,6 @@
  *    it in the license file.
  */
 
-#include <math.h>
-
 #include "mongo/platform/basic.h"
 
 #include "mongo/bson/bson_depth.h"
@@ -1170,8 +1168,8 @@ class LongToInt : public ToIntBase {
     Value value() {
         return Value(0xff00000007LL);
     }
-    bool asserts() {
-        return true;
+    int expected() {
+        return 7;
     }
 };
 
@@ -1210,46 +1208,6 @@ class StringToInt {
 public:
     void run() {
         ASSERT_THROWS(Value(StringData()).coerceToInt(), AssertionException);
-    }
-};
-
-/** Coerce maxInt to int */
-class MaxIntToInt : public ToIntBase {
-    Value value() {
-        return Value((double)std::numeric_limits<int>::max());
-    }
-    int expected() {
-        return std::numeric_limits<int>::max();
-    }
-};
-
-/** Coerce minInt to int */
-class MinIntToInt : public ToIntBase {
-    Value value() {
-        return Value((double)std::numeric_limits<int>::min());
-    }
-    int expected() {
-        return std::numeric_limits<int>::min();
-    }
-};
-
-/** Coerce maxInt + 1 to int */
-class TooLargeToInt : public ToIntBase {
-    Value value() {
-        return Value((double)std::numeric_limits<int>::max() + 1);
-    }
-    bool asserts() {
-        return true;
-    }
-};
-
-/** Coerce minInt - 1 to int */
-class TooLargeNegativeToInt : public ToIntBase {
-    Value value() {
-        return Value((double)std::numeric_limits<int>::min() - 1);
-    }
-    bool asserts() {
-        return true;
     }
 };
 
@@ -1300,57 +1258,6 @@ class DoubleToLong : public ToLongBase {
     }
     long long expected() {
         return 9;
-    }
-};
-
-/** Coerce infinity to long. */
-class InfToLong : public ToLongBase {
-    Value value() {
-        return Value(std::numeric_limits<double>::infinity());
-    }
-    bool asserts() {
-        return true;
-    }
-};
-
-/** Coerce negative infinity to long. **/
-class NegInfToLong : public ToLongBase {
-    Value value() {
-        return Value(std::numeric_limits<double>::infinity() * -1);
-    }
-    bool asserts() {
-        return true;
-    }
-};
-
-/** Coerce large to long. **/
-class InvalidLargeToLong : public ToLongBase {
-    Value value() {
-        return Value(pow(2, 63));
-    }
-    bool asserts() {
-        return true;
-    }
-};
-
-/** Coerce lowest double to long. **/
-class LowestDoubleToLong : public ToLongBase {
-    Value value() {
-        return Value(static_cast<double>(std::numeric_limits<long long>::lowest()));
-    }
-    long long expected() {
-        return std::numeric_limits<long long>::lowest();
-    }
-};
-
-/** Coerce 'towards infinity' to long **/
-class TowardsInfinityToLong : public ToLongBase {
-    Value value() {
-        return Value(static_cast<double>(std::nextafter(std::numeric_limits<long long>::lowest(),
-                                                        std::numeric_limits<double>::lowest())));
-    }
-    bool asserts() {
-        return true;
     }
 };
 
@@ -2042,21 +1949,12 @@ public:
         add<Value::Coerce::NullToInt>();
         add<Value::Coerce::UndefinedToInt>();
         add<Value::Coerce::StringToInt>();
-        add<Value::Coerce::MaxIntToInt>();
-        add<Value::Coerce::MinIntToInt>();
-        add<Value::Coerce::TooLargeToInt>();
-        add<Value::Coerce::TooLargeNegativeToInt>();
         add<Value::Coerce::IntToLong>();
         add<Value::Coerce::LongToLong>();
         add<Value::Coerce::DoubleToLong>();
         add<Value::Coerce::NullToLong>();
         add<Value::Coerce::UndefinedToLong>();
         add<Value::Coerce::StringToLong>();
-        add<Value::Coerce::InfToLong>();
-        add<Value::Coerce::NegInfToLong>();
-        add<Value::Coerce::InvalidLargeToLong>();
-        add<Value::Coerce::LowestDoubleToLong>();
-        add<Value::Coerce::TowardsInfinityToLong>();
         add<Value::Coerce::IntToDouble>();
         add<Value::Coerce::LongToDouble>();
         add<Value::Coerce::DoubleToDouble>();
