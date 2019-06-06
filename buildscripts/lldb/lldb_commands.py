cd8fc73729417a11fe65a1c5b1b540bcaba95420
Mathew Robinson <chasinglogic@gmail.com>
2019-06-06 17:34:05 -0400

    SERVER-41168 Port mongodb-specific debugging commands in gdb to lldb

---
+++ buildscripts/lldb/lldb_commands.py
@@ -0,0 +1,26 @@
+import lldb
+
+def __lldb_init_module(debugger, dict):
+    ############################
+    # register custom commands #
+    ############################
+    debugger.HandleCommand("command script add -f lldb_commands.PrintGlobalServiceContext mongodb-service-context")
+    debugger.HandleCommand("command script add -f lldb_commands.PrintGlobalServiceContext mongodb-dump-locks")
+    debugger.HandleCommand("command script alias mongodb-help help")
+
+
+#######################
+# Command Definitions #
+#######################
+
+def PrintGlobalServiceContext(debugger, *args):
+    """
+    Provides the same convenience command available in GDB
+    integrations to print the globalServiceContext.
+    """
+    debugger.HandleCommand("print *globalServiceContext")
+
+
+def MongoDBDumpLocks(debugger, *args):
+    """Dump locks in the mongod process."""
+    debugger.HandleCommand("call globalLockManager.dump()")
