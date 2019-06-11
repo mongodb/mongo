b6f1010451de1a10e12c4b6d865476cce284afce
Alex Cameron <alex.cameron@10gen.com>
2019-06-11 08:45:32 +0000

    SERVER-39004 Introduce a quota mechanism for the overflow file
    (cherry picked from commit b29c22ad4b89dccca63bd2a279c48f47f76093d1)

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
@@ -542,6 +542,7 @@ WiredTigerKVEngine::WiredTigerKVEngine(const std::string& canonicalName,
                                        ClockSource* cs,
                                        const std::string& extraOpenOptions,
                                        size_t cacheSizeMB,
+                                       size_t maxCacheOverflowFileSizeMB,
                                        bool durable,
                                        bool ephemeral,
                                        bool repair,
@@ -574,6 +575,7 @@ WiredTigerKVEngine::WiredTigerKVEngine(const std::string& canonicalName,
     std::stringstream ss;
     ss << "create,";
     ss << "cache_size=" << cacheSizeMB << "M,";
+    ss << "cache_overflow=(file_max=" << maxCacheOverflowFileSizeMB << "M),";
     ss << "session_max=33000,";
     ss << "eviction=(threads_min=4,threads_max=4),";
     ss << "config_base=false,";
--- src/mongo/db/storage/wiredtiger/wiredtiger_prefixed_record_store_test.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_prefixed_record_store_test.cpp
@@ -78,6 +78,7 @@ public:
                                          _cs.get(),
                                          "",
                                          1,
+                                         0,
                                          false,
                                          false,
                                          false,
