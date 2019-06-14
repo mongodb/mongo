20d327d3e415b9b4b4d869bb09bd136c3b6d6b22
William Schultz <william.schultz@mongodb.com>
2019-06-14 17:59:58 -0400

    SERVER-41377 Add JS tests that the rollback data files are generated correctly for prepared and unprepared transactions

---
+++ jstests/replsets/libs/rollback_files.js
@@ -0,0 +1,72 @@
+/**
+ * Verifies that the rollback file for a given database path and namespace exists and contains the
+ * 'expectedDocs', in any order. If there are multiple rollback files for the given collection,
+ * chooses one of those files arbitrarily to read data from. Note that a rollback file is simply a
+ * sequence of concatenated BSON objects, which is a format that can be read by the bsondump tool.
+ */
+function checkRollbackFiles(dbPath, nss, expectedDocs) {
+    // Check the path of the rollback directory.
+    const rollbackDir = dbPath + '/rollback';
+    assert(pathExists(rollbackDir), 'directory for rollback files does not exist: ' + rollbackDir);
+
+    // We try to handle both possible rollback file layouts here. The first layout, used by the
+    // older 'rollbackViaRefetch' algorithm, puts rollback files directly inside the /rollback
+    // directory with a naming scheme of '<db>.<collection>.<timestamp>.bson'. The newer layout,
+    // used by recover-to-timestamp (RTT) rollback, places them inside a
+    // 'rollback/<db>.<collection>' directory with a file naming scheme of
+    // 'removed.<timestamp>.bson'. The data formats of the files themselves should be the same in
+    // both cases, though. These file layouts are documented here:
+    // https://docs.mongodb.com/manual/core/replica-set-rollbacks/#collect-rollback-data.
+
+    function getRollbackViaRefetchRollbackFile() {
+        let files = listFiles(rollbackDir);
+        let rollbackFiles = files.filter(f => !f.isDirectory && f.baseName.startsWith(nss));
+        assert.gte(rollbackFiles.length,
+                   1,
+                   "No rollbackViaRefetch rollback files found for namespace: " + nss);
+        return rollbackFiles[0].name;
+    }
+
+    function getRTTRollbackFile() {
+        let rollbackFiles = listFiles(rollbackDir + "/" + nss);
+        assert.gte(rollbackFiles.length, 1, "No RTT rollback files found for namespace: " + nss);
+        return rollbackFiles[0].name;
+    }
+
+    // If all the objects in the rollback directory are files, not directories, then this implies
+    // the rollback files have been written using the rollbackViaRefetch mechanism. Otherwise, we
+    // assume the files are written using the RTT mechanism.
+    let rollbackFile;
+    if (listFiles(rollbackDir).every(f => !f.isDirectory)) {
+        print("Assuming rollback files written using the 'rollbackViaRefetch' layout.");
+        rollbackFile = getRollbackViaRefetchRollbackFile();
+    } else {
+        print("Assuming rollback files written using the 'RTT' layout.");
+        rollbackFile = getRTTRollbackFile();
+    }
+
+    print("Found rollback file: " + rollbackFile);
+
+    // If the rollback BSON file is encrypted, don't try to check the data contents. Checking its
+    // existence is sufficient.
+    if (rollbackFile.endsWith(".enc")) {
+        print("Bypassing check of rollback file data since it is encrypted.");
+        return;
+    }
+
+    // Windows doesn't always play nice with the bsondump tool and the way we pass arguments to it.
+    // Checking the existence of the rollback directory above should be sufficient on Windows.
+    if (_isWindows()) {
+        print("Bypassing check of rollback file data on Windows.");
+        return;
+    }
+
+    // Parse the BSON rollback file and check for the right documents. The documents may be written
+    // out in an arbitrary order so we just check the document set.
+    let tmpJSONFile = "rollback_tmp.json";
+    let exitCode =
+        MongoRunner.runMongoTool("bsondump", {outFile: tmpJSONFile, bsonFile: rollbackFile});
+    assert.eq(exitCode, 0, "bsondump failed to parse the rollback file");
+    let docs = cat(tmpJSONFile).split("\n").filter(l => l.length).map(JSON.parse);
+    assert.sameMembers(docs, expectedDocs);
+}
--- jstests/replsets/rollback_prepare_transaction.js
+++ jstests/replsets/rollback_prepare_transaction.js
@@ -1,5 +1,5 @@
 /**
- * Tests that a prepared transactions are correctly rolled-back.
+ * Tests that prepared transactions are correctly rolled-back.
  *
  * @tags: [uses_transactions, uses_prepare_transaction]
  */
@@ -8,6 +8,7 @@
 
     load("jstests/core/txns/libs/prepare_helpers.js");
     load("jstests/replsets/libs/rollback_test.js");
+    load("jstests/replsets/libs/rollback_files.js");
 
     const rollbackTest = new RollbackTest();
     const rollbackNode = rollbackTest.getPrimary();
@@ -15,41 +16,73 @@
     const testDB = rollbackNode.getDB("test");
     const collName = "rollback_prepare_transaction";
     const testColl = testDB.getCollection(collName);
-    const txnDoc = {_id: 42};
 
     // We perform some operations on the collection aside from starting and preparing a transaction
     // in order to cause the count diff computed by replication to be non-zero.
-    assert.commandWorked(testColl.insert({_id: 1}));
+    assert.commandWorked(testColl.insert({_id: "a"}));
+
+    // Start two separate sessions for running transactions. On 'session1', we will run a prepared
+    // transaction whose commit operation gets rolled back, and on 'session2', we will run a
+    // prepared transaction whose prepare operation gets rolled back.
+    const session1 = rollbackNode.startSession();
+    const session1DB = session1.getDatabase(testDB.getName());
+    const session1Coll = session1DB.getCollection(collName);
+
+    const session2 = rollbackNode.startSession();
+    const session2DB = session2.getDatabase(testDB.getName());
+    const session2Coll = session2DB.getCollection(collName);
+
+    // Prepare a transaction whose commit operation will be rolled back.
+    session1.startTransaction();
+    assert.commandWorked(session1Coll.insert({_id: "t2_a"}));
+    assert.commandWorked(session1Coll.insert({_id: "t2_b"}));
+    assert.commandWorked(session1Coll.insert({_id: "t2_c"}));
+    let prepareTs = PrepareHelpers.prepareTransaction(session1);
 
     rollbackTest.transitionToRollbackOperations();
 
     // The following operations will be rolled-back.
-    assert.commandWorked(testColl.insert({_id: 2}));
+    assert.commandWorked(testColl.insert({_id: "b"}));
 
-    const session = rollbackNode.startSession();
-    const sessionDB = session.getDatabase(testDB.getName());
-    const sessionColl = sessionDB.getCollection(collName);
-
-    session.startTransaction();
-    assert.commandWorked(sessionColl.insert(txnDoc));
+    session2.startTransaction();
+    assert.commandWorked(session2Coll.insert({_id: "t1"}));
 
     // Use w: 1 to simulate a prepare that will not become majority-committed.
-    PrepareHelpers.prepareTransaction(session, {w: 1});
+    PrepareHelpers.prepareTransaction(session2, {w: 1});
+
+    // Commit the transaction that was prepared before the common point.
+    PrepareHelpers.commitTransaction(session1, prepareTs);
 
     // This is not exactly correct, but characterizes the current behavior of fastcount, which
     // includes the prepared but uncommitted transaction in the collection count.
-    assert.eq(3, testColl.count());
+    assert.eq(6, testColl.count());
 
-    // Only two documents are visible.
-    arrayEq([{_id: 1}, {_id: 2}], testColl.find().toArray());
+    // Check the visible documents.
+    arrayEq([{_id: "a"}, {_id: "b"}, {_id: "t2_a"}, {_id: "t2_b"}, {_id: "t2_c"}],
+            testColl.find().toArray());
 
     rollbackTest.transitionToSyncSourceOperationsBeforeRollback();
     rollbackTest.transitionToSyncSourceOperationsDuringRollback();
-    rollbackTest.transitionToSteadyStateOperations();
+    // Skip consistency checks so they don't conflict with the prepared transaction.
+    rollbackTest.transitionToSteadyStateOperations({skipDataConsistencyChecks: true});
 
     // Both the regular insert and prepared insert should be rolled-back.
-    assert.eq(1, testColl.count());
-    assert.eq({_id: 1}, testColl.findOne());
+    assert.sameMembers([{_id: "a"}], testColl.find().toArray());
+
+    // Confirm that the rollback wrote deleted documents to a file.
+    const replTest = rollbackTest.getTestFixture();
+    const expectedDocs = [{_id: "b"}, {_id: "t2_a"}, {_id: "t2_b"}, {_id: "t2_c"}];
+    checkRollbackFiles(replTest.getDbPath(rollbackNode), testColl.getFullName(), expectedDocs);
+
+    // Allow the test to complete by aborting the left over prepared transaction.
+    jsTestLog("Aborting the prepared transaction on session " + tojson(session1.getSessionId()));
+    let adminDB = rollbackTest.getPrimary().getDB("admin");
+    assert.commandWorked(adminDB.adminCommand({
+        abortTransaction: 1,
+        lsid: session1.getSessionId(),
+        txnNumber: session1.getTxnNumber_forTesting(),
+        autocommit: false
+    }));
 
     rollbackTest.stop();
 })();
