732734397e624fb5b8682fef4b08f3ad149d7d84
Blake Oler <blake.oler@mongodb.com>
2019-06-17 15:23:04 -0400

    SERVER-41004 Allow CursorNotFound errors to count as a successful kill in the killSessions command
    (cherry picked from commit a0c6dc635efc4f14144fa292a626bde63656fbf8)

--- src/mongo/db/kill_sessions_common.h
+++ src/mongo/db/kill_sessions_common.h
@@ -92,7 +92,7 @@ public:
      * which this adaptor was constructed.
      */
     template <typename Mgr>
-    void operator()(Mgr& mgr) {
+    void operator()(Mgr& mgr) noexcept {
         LogicalSessionIdSet activeSessions;
         mgr.appendActiveSessions(&activeSessions);
 
@@ -105,8 +105,13 @@ public:
                     try {
                         _eraser(mgr, id);
                         _cursorsKilled++;
-                    } catch (...) {
-                        _failures.push_back(exceptionToStatus());
+                    } catch (const ExceptionFor<ErrorCodes::CursorNotFound>&) {
+                        // Cursor was killed separately after this command was initiated. Still
+                        // count the cursor as killed here, since the user's request is
+                        // technically satisfied.
+                        _cursorsKilled++;
+                    } catch (const DBException& ex) {
+                        _failures.push_back(ex.toStatus());
                     }
                 }
             }
