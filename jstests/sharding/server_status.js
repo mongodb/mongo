874f1e2c64c1500e17c79b86fb61d7777541cf39
Arun Banala <arun.banala@mongodb.com>
2019-06-19 17:26:09 +0100

    SERVER-41184 Add metrics to indicate the amount of sharded updates done with only _id in query

--- jstests/sharding/server_status.js
+++ jstests/sharding/server_status.js
@@ -6,24 +6,29 @@
 (function() {
     "use strict";
 
-    var st = new ShardingTest({shards: 1});
+    const st = new ShardingTest({shards: 2});
+    const testDB = st.s.getDB("test");
+    const testColl = testDB.coll;
 
-    var testDB = st.s.getDB('test');
-    testDB.adminCommand({enableSharding: 'test'});
-    testDB.adminCommand({shardCollection: 'test.user', key: {_id: 1}});
+    assert.commandWorked(st.s0.adminCommand({enableSharding: testDB.getName()}));
+    st.ensurePrimaryShard(testDB.getName(), st.shard0.shardName);
 
-    // Initialize shard metadata in shards
-    testDB.user.insert({x: 1});
+    // Shard testColl on {x:1}, split it at {x:0}, and move chunk {x:1} to shard1.
+    st.shardColl(testColl, {x: 1}, {x: 0}, {x: 1});
 
-    var checkShardingServerStatus = function(doc) {
-        var shardingSection = doc.sharding;
+    // Insert one document on each shard.
+    assert.commandWorked(testColl.insert({x: 1, _id: 1}));
+    assert.commandWorked(testColl.insert({x: -1, _id: 0}));
+
+    let checkShardingServerStatus = function(doc) {
+        let shardingSection = doc.sharding;
         assert.neq(shardingSection, null);
 
-        var configConnStr = shardingSection.configsvrConnectionString;
-        var configConn = new Mongo(configConnStr);
-        var configIsMaster = configConn.getDB('admin').runCommand({isMaster: 1});
+        let configConnStr = shardingSection.configsvrConnectionString;
+        let configConn = new Mongo(configConnStr);
+        let configIsMaster = configConn.getDB('admin').runCommand({isMaster: 1});
 
-        var configOpTimeObj = shardingSection.lastSeenConfigServerOpTime;
+        let configOpTimeObj = shardingSection.lastSeenConfigServerOpTime;
 
         assert.gt(configConnStr.indexOf('/'), 0);
         assert.gte(configIsMaster.configsvr, 1);  // If it's a shard, this field won't exist.
@@ -34,10 +39,40 @@
         assert.neq(null, shardingSection.maxChunkSizeInBytes);
     };
 
-    var mongosServerStatus = testDB.adminCommand({serverStatus: 1});
+    // Update documents using _id in query so that we publish
+    // 'updateOneOpStyleBroadcastWithExactIDCount' metric.
+
+    // Should increment the metric as the update cannot target single shard and are {multi:false}.
+    assert.commandWorked(testDB.coll.update({_id: "missing"}, {$set: {a: 1}}, {multi: false}));
+    assert.commandWorked(testDB.coll.update({_id: 1}, {$set: {a: 2}}, {multi: false}));
+
+    // Should increment the metric because we broadcast by _id, even though the update subsequently
+    // fails on the shard when it attempts an invalid modification of the shard key.
+    assert.commandFailedWithCode(testDB.coll.update({_id: 1}, {$set: {x: 2}}, {multi: false}),
+                                 [ErrorCodes.ImmutableField, 31025]);
+
+    // Shouldn't increment the metric when {multi:true}.
+    assert.commandWorked(testDB.coll.update({_id: 1}, {$set: {a: 3}}, {multi: true}));
+    assert.commandWorked(testDB.coll.update({}, {$set: {a: 3}}, {multi: true}));
+
+    // Shouldn't increment the metric when update can target single shard.
+    assert.commandWorked(testDB.coll.update({x: 11}, {$set: {a: 2}}, {multi: false}));
+    assert.commandWorked(testDB.coll.update({x: 1}, {$set: {a: 2}}, {multi: false}));
+
+    // Shouldn't increment the metric when routing fails.
+    assert.commandFailedWithCode(testDB.coll.update({}, {$set: {x: 2}}, {multi: false}),
+                                 ErrorCodes.InvalidOptions);
+    assert.commandFailedWithCode(testDB.coll.update({_id: 1}, {$set: {x: 2}}, {upsert: true}),
+                                 ErrorCodes.ShardKeyNotFound);
+
+    const mongosServerStatus = testDB.adminCommand({serverStatus: 1});
+
+    // Verify that only the first three updates incremented the metric counter.
+    assert.eq(3, mongosServerStatus.metrics.query.updateOneOpStyleBroadcastWithExactIDCount);
+
     checkShardingServerStatus(mongosServerStatus);
 
-    var mongodServerStatus = st.rs0.getPrimary().getDB('admin').runCommand({serverStatus: 1});
+    const mongodServerStatus = st.rs0.getPrimary().getDB('admin').runCommand({serverStatus: 1});
     checkShardingServerStatus(mongodServerStatus);
 
     st.stop();
