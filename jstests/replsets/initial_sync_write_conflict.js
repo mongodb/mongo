a54ca7e453bf0f291d5f1a825a0c92efab0b0b7b
Allison Easton <allison.easton@mongodb.com>
2019-06-25 09:11:09 -0400

    SERVER-41529 Prevent dangling index records by calling _addDocumentToIndexBlock outside of writeConflictRetry block.
    (cherry picked from commit 54ca8a7112746c7637a295b6d57b6f2c3b4df9b7)

---
+++ jstests/replsets/initial_sync_write_conflict.js
@@ -0,0 +1,34 @@
+/*
+ *  Tests that initial sync is successfully able to clone the collection and build
+ *  index without any orphan index entries even if a WriteConflictException is
+ *  thrown while inserting documents into collections.
+ */
+
+const testName = "write_conflict_exception";
+
+// Start a 2 node replica set.
+const replSet = new ReplSetTest({name: testName, nodes: [{}, {rsConfig: {priority: 0}}]});
+
+jsTest.log("Starting test");
+replSet.startSet();
+replSet.initiate();
+
+var secondary = replSet.getSecondary();
+
+// Start and restart secondary with fail point that throws exception enabled.
+jsTest.log("Stopping secondary");
+replSet.stop(secondary);
+jsTest.log("Re-starting secondary ");
+secondary = replSet.start(secondary, {
+    startClean: true,
+    setParameter: {"failpoint.failAfterBulkLoadDocInsert": "{'mode': {'times': 1}}"}
+});
+
+// Wait for everything to be synced.
+jsTest.log("Waiting for initial sync to succeed");
+replSet.waitForState(secondary, ReplSetTest.State.SECONDARY);
+
+// If the index table contains any entries pointing to invalid document(RecordID), then
+// validateCollections called during replica stopSet will capture the index corruption and throw
+// error.
+replSet.stopSet();
