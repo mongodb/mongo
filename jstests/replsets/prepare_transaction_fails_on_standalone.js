89a719b5fcac9bbe5ce9369939cba2bc24735b26
Pavi Vetriselvan <pvselvan@umich.edu>
2019-06-27 11:10:30 -0400

    SERVER-36824 prevent prepareTransaction command on standalone
    (cherry picked from commit 78f534fa92809fd3b9366a046fa5d24988b32944)

---
+++ jstests/replsets/prepare_transaction_fails_on_standalone.js
@@ -0,0 +1,20 @@
+/**
+ * Tests that the 'prepareTransaction' command fails against a standalone node.
+ *
+ * @tags: [uses_transactions, uses_prepare_transaction]
+ */
+(function() {
+    "use strict";
+
+    const standalone = MongoRunner.runMongod();
+
+    const collName = "prepare_transaction_fails_on_standalone";
+    const dbName = "test";
+    const testDB = standalone.getDB(dbName);
+
+    assert.commandWorked(testDB.runCommand({create: collName}));
+
+    assert.commandFailedWithCode(testDB.adminCommand({prepareTransaction: 1}), 51239);
+
+    MongoRunner.stopMongod(standalone);
+}());
