6e81decdf230de5a6e9fc1dd9b2e5a54e78d6d7c
Benety Goh <benety@mongodb.com>
2019-07-06 07:48:18 -0400

    SERVER-41968 fix db.currentOp() query in index build killop tests
    This affects mostly index builds that use the IndexBuildsCoordinator because there
    could be multiple db.currentOp() entries that match the createIndexes command.
    
    (cherry picked from commit 82177ac1ee32044a733be742b8244eff9389a9fc)

--- jstests/noPassthrough/index_killop_standalone.js
+++ jstests/noPassthrough/index_killop_standalone.js
@@ -21,7 +21,7 @@ function testAbortIndexBuild(options) {
     const createIdx = IndexBuildTest.startIndexBuild(conn, coll.getFullName(), {a: 1}, options);
 
     // When the index build starts, find its op id.
-    const opId = IndexBuildTest.waitForIndexBuildToStart(testDB);
+    const opId = IndexBuildTest.waitForIndexBuildToScanCollection(testDB, coll.getName(), 'a_1');
 
     // Kill the index build.
     assert.commandWorked(testDB.killOp(opId));
