030148e81b4eb7073d18266fe868c286a74cf0e4
Xiangyu Yao <xiangyu.yao@mongodb.com>
2019-07-10 10:39:27 -0400

    Revert "SERVER-36547 After 4.2 check_unique_indexes.js shouldn't rely on a specific FCV version"
    This reverts commit 264994554c4e298fb151386f33251d171bbbb613.

--- jstests/libs/check_unique_indexes.js
+++ jstests/libs/check_unique_indexes.js
@@ -3,6 +3,9 @@
 /**
  * Verifies that all unique indexes belonging to all collections on all databases on the server
  * are in correct data format version.
+ *
+ * TODO: SERVER-36547: Once we are past 4.2 release, make this script FCV independent.
+ * After 4.2 we can just assert if we do not see the correct index versions.
  */
 function checkUniqueIndexFormatVersion(adminDB) {
     // Data format version is WiredTiger specific and not required to be tested for other
@@ -11,6 +14,15 @@ function checkUniqueIndexFormatVersion(adminDB) {
         assert.commandWorked(adminDB.serverStatus()).storageEngine.name === "wiredTiger";
     if (!isWiredTiger)
         return;
+
+    // Unique indexes got an upgrade in 4.2, their WiredTiger specific internal version has changed
+    // since then
+    const newUniqIdxFCV = "4.2";
+
+    // Obtain the current FCV version
+    let res =
+        assert.commandWorked(adminDB.runCommand({getParameter: 1, featureCompatibilityVersion: 1}));
+    let currentFCV = res["featureCompatibilityVersion"]["version"];
 
     res = assert.commandWorked(adminDB.runCommand({"listDatabases": 1}));
     let databaseList = res.databases;
@@ -30,16 +42,30 @@ function checkUniqueIndexFormatVersion(adminDB) {
                                   .next()
                                   .storageStats.indexDetails[index.name]
                                   .metadata.formatVersion;
+                    // Unique indexes are expected to have new format version only post
+                    // newUniqIdxFCV
                     if (index.v === 2) {
-                        assert.eq(
-                            ifv,
-                            12,
-                            "Expected index format version 12 for unique index: " + tojson(index));
+                        if (currentFCV === newUniqIdxFCV)
+                            assert.eq(ifv,
+                                      12,
+                                      "Expected index format version 12 for unique index: " +
+                                          tojson(index));
+                        else
+                            assert.eq(ifv,
+                                      8,
+                                      "Expected index format version 8 for unique index: " +
+                                          tojson(index));
                     } else {
-                        assert.eq(
-                            ifv,
-                            11,
-                            "Expected index format version 11 for unique index: " + tojson(index));
+                        if (currentFCV === newUniqIdxFCV)
+                            assert.eq(ifv,
+                                      11,
+                                      "Expected index format version 11 for unique index: " +
+                                          tojson(index));
+                        else
+                            assert.eq(ifv,
+                                      6,
+                                      "Expected index format version 6 for unique index: " +
+                                          tojson(index));
                     }
                 }
             });
