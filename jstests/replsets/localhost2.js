c952e2305daba32a0e72ac452bb552a69c24204a
William Schultz <william.schultz@mongodb.com>
2019-07-17 10:13:47 -0400

    SERVER-42004 localhost JS tests should wait for an election to complete before shutting down
    (cherry picked from commit 19d0d47fb83d01edb18e0bcb235d6c39bbe16bbd)

--- jstests/replsets/localhost2.js
+++ jstests/replsets/localhost2.js
@@ -4,7 +4,7 @@
     'use strict';
 
     // Select localhost when binding to localhost
-    const rt = new ReplSetTest({name: "rsLocal", nodes: 2});
+    const rt = new ReplSetTest({name: "rsLocal", nodes: 1});
     const primary = rt.startSet({bind_ip: "0.0.0.0"})[0];
     const db = primary.getDB('admin');
     const resp = assert.commandWorked(db.adminCommand({replSetInitiate: undefined}));
@@ -13,5 +13,8 @@
     assert(!resp.me.startsWith('0.0.0.0:'), tojson(resp.me) + " should not start with 0.0.0.0:");
     assert(!resp.me.startsWith('localhost:'),
            tojson(resp.me) + " should not start with localhost:");
+
+    // Wait for the primary to complete its election before shutting down the set.
+    assert.soon(() => db.runCommand({ismaster: 1}).ismaster);
     rt.stopSet();
 })();
