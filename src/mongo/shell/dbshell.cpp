0789a935ccee8e402cce8c7cdf52a41b9cc17010
Andrew Morrow <acm@mongodb.com>
2019-07-21 17:20:33 -0400

    SERVER-42257 Announce known emulations

--- src/mongo/shell/dbshell.cpp
+++ src/mongo/shell/dbshell.cpp
@@ -914,6 +914,12 @@ int _main(int argc, char* argv[], char** envp) {
                         false,
                         true,
                         false);
+
+            scope->exec("shellHelper( 'show', 'nonGenuineMongoDBCheck' )",
+                        "(nonGenuineMongoDBCheck)",
+                        false,
+                        true,
+                        false);
         }
 
         shellHistoryInit();
