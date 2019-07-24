966f51fa210c9db0e4cfff18fe400c0b2433b263
Suganthi Mani <suganthi.mani@mongodb.com>
2019-07-24 13:10:02 -0400

    SERVER-42129 modifies test to account for ephemeralForTest engine's missing oplog after restart.
    (cherry picked from commit 45ff40ff0f2a2d7664a38acd06bfb4f4fe235267)

--- jstests/libs/storage_engine_utils.js
+++ jstests/libs/storage_engine_utils.js
@@ -4,3 +4,9 @@ function storageEngineIsWiredTigerOrInMemory() {
     return !jsTest.options().storageEngine || jsTest.options().storageEngine === "wiredTiger" ||
         jsTest.options().storageEngine === "inMemory";
 }
+
+function storageEngineIsWiredTiger() {
+    // We assume that WiredTiger is the default storage engine, if the storage engine is
+    // unspecified in the test options.
+    return !jsTest.options().storageEngine || jsTest.options().storageEngine === "wiredTiger";
+}
--- jstests/replsets/drop_oplog_should_fail_if_storage_engine_supports_replSetResizeOplog_command.js
+++ jstests/replsets/drop_oplog_should_fail_if_storage_engine_supports_replSetResizeOplog_command.js
@@ -1,11 +1,15 @@
-// Tests that dropping the oplog is forbidden on standalone nodes with storage engines
-// that support the command "replSetResizeOplog". The support for this command is
-// provided only by the WiredTiger storage engine.
-// Therefore, attempts to drop the oplog when using these storage engines should fail.
-// Also, nodes running in a replica set will forbid dropping the oplog, but
-// for a different reason.
-// Note: We detect whether a storage engine supports the replSetResizeOplog command
-// by checking whether it supportsRecoveryTimestamp().
+/* Tests that dropping the oplog is forbidden on standalone nodes with storage engines
+ * that support the command "replSetResizeOplog". The support for this command is
+ * provided only by the WiredTiger storage engine.
+ * Therefore, attempts to drop the oplog when using these storage engines should fail.
+ * Also, nodes running in a replica set will forbid dropping the oplog, but
+ * for a different reason.
+ * Note: We detect whether a storage engine supports the replSetResizeOplog command
+ * by checking whether it supportsRecoveryTimestamp().
+ *
+ * @tags: [requires_persistence]
+ */
+
 (function() {
 "use strict";
 
@@ -16,17 +20,18 @@ const rt = new ReplSetTest({
     nodes: 1
 });
 
-rt.startSet();
-
-rt.initiate();
-
-// Restart as a standalone node.
-rt.restart(0, {noReplSet: true});
+// Start as a standalone node.
+rt.start(0, {noReplSet: true});
 
 let master = rt.getPrimary();
 let localDB = master.getDB('local');
 
-if (storageEngineIsWiredTigerOrInMemory()) {
+// Standalone nodes don't start with an oplog; create one. The size of the oplog doesn't
+// matter. We are capping the oplog because some storage engines do not allow the creation
+// of uncapped oplog collections.
+assert.commandWorked(localDB.runCommand({create: 'oplog.rs', capped: true, size: 1000}));
+
+if (storageEngineIsWiredTiger()) {
     const ret = assert.commandFailed(localDB.runCommand({drop: 'oplog.rs'}));
     assert.eq("can't drop oplog on storage engines that support replSetResizeOplog command",
               ret.errmsg);
