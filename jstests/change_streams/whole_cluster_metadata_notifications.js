2f3f86e9d81dffe2e35c8afabac33b9db3b7ca40
Davis Haupt <davis.haupt@mongodb.com>
2019-07-26 16:24:24 -0400

    SERVER-42419 add requires_majority_read_concern tag to whole cluster metadata changestream test
    (cherry picked from commit a220e3043fbfe5c300048364fe062a4864dc5182)

--- jstests/change_streams/whole_cluster_metadata_notifications.js
+++ jstests/change_streams/whole_cluster_metadata_notifications.js
@@ -1,4 +1,8 @@
 // Tests of metadata notifications for a $changeStream on a whole cluster.
+// When run in sharded+transaction passthrough suites, it is possible that the two unsharded
+// collections will live on different shards. Majority read concern cannot be off with multi-shard
+// transactions, which is why this test needs the tag below.
+// @tags: [requires_majority_read_concern]
 (function() {
 "use strict";
 
