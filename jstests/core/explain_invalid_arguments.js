b167082924bace8ee0e56ea4678d528bfef21c5e
krk <keremkat@gmail.com>
2019-08-04 11:58:56 +0200

    Wrap test in an IIFE.

--- jstests/core/explain_invalid_arguments.js
+++ jstests/core/explain_invalid_arguments.js
@@ -1,52 +1,55 @@
-// Tests for validating arguments of the explain command.
+/**
+ * Tests for validating arguments of the explain command:
+ *  -- SERVER-27976 Do not accept unknown argument for the explain command.
+ */
+(function() {
+    "use strict";
 
-var collName = "jstests_explain_invalid_arguments";
-var t = db[collName];
-t.drop();
-t.insert({});
+    var collName = "jstests_explain_invalid_arguments";
 
-var explain = db.runCommand({
-  explain: { find: collName },
-  verbosity: "executionStats"
-});
-assert.commandWorked(explain);
+    var explain = db.runCommand({
+        explain: { find: collName },
+        verbosity: "executionStats"
+    });
+    assert.commandWorked(explain);
 
-explain = db.runCommand({
-  explain: { find: collName },
-  verosity: "executionStats"
-});
-assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
-assert.eq("explain does not support 'verosity' argument.", explain.errmsg);
+    explain = db.runCommand({
+        explain: { find: collName },
+        verosity: "executionStats"
+    });
+    assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
+    assert.eq("explain does not support 'verosity' argument.", explain.errmsg);
 
-explain = db.runCommand({
-  explain: { find: collName },
-  verbosity: "executionStats",
-  foo: 1
-});
-assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
-assert.eq("explain does not support 'foo' argument.", explain.errmsg);
+    explain = db.runCommand({
+        explain: { find: collName },
+        verbosity: "executionStats",
+        foo: 1
+    });
+    assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
+    assert.eq("explain does not support 'foo' argument.", explain.errmsg);
 
-explain = db.runCommand({
-  explain: { find: collName },
-  foo: 1,
-  bar: 1
-});
-assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
-assert.eq("explain does not support 'foo' argument.", explain.errmsg);
+    explain = db.runCommand({
+        explain: { find: collName },
+        foo: 1,
+        bar: 1
+    });
+    assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
+    assert.eq("explain does not support 'foo' argument.", explain.errmsg);
 
-explain = db.runCommand({
-  explain: { find: collName },
-  foo: 1,
-  verbosity: "executionStats",
-  bar: 1
-});
-assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
-assert.eq("explain does not support 'foo' argument.", explain.errmsg);
+    explain = db.runCommand({
+        explain: { find: collName },
+        foo: 1,
+        verbosity: "executionStats",
+        bar: 1
+    });
+    assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
+    assert.eq("explain does not support 'foo' argument.", explain.errmsg);
 
-explain = db.runCommand({
-  explain: { find: collName },
-  explain: {},
-  bar: 1
-});
-assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
-assert.eq("explain does not support 'bar' argument.", explain.errmsg);
+    explain = db.runCommand({
+        explain: { find: collName },
+        explain: {},
+        bar: 1
+    });
+    assert.commandFailedWithCode(explain, ErrorCodes.InvalidOptions);
+    assert.eq("explain does not support 'bar' argument.", explain.errmsg);
+})();
