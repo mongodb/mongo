b81e081faab34d7deae06b4b837f2a1ba5b8360d
Max Hirschhorn <max.hirschhorn@mongodb.com>
2019-08-13 16:02:18 -0400

    SERVER-42623 Remove events from scheduler using `is` operator.
    (cherry picked from commit 6d38158e6b81d23072dfaf2baefa52cce27043ef)

--- buildscripts/resmokelib/logging/flush.py
+++ buildscripts/resmokelib/logging/flush.py
@@ -4,9 +4,10 @@ These instances are used to send logs to buildlogger.
 """
 
 import logging
-import sched
 import threading
 import time
+
+from ..utils import scheduler
 
 _FLUSH_THREAD_LOCK = threading.Lock()
 _FLUSH_THREAD = None
@@ -93,7 +94,7 @@ class _FlushThread(threading.Thread):
             self.__schedule_updated.wait(secs)
             self.__schedule_updated.clear()
 
-        self.__scheduler = sched.scheduler(time.time, interruptible_sleep)
+        self.__scheduler = scheduler.Scheduler(time.monotonic, interruptible_sleep)
         self.__schedule_updated = threading.Event()
         self.__should_stop = threading.Event()
         self.__terminated = threading.Event()
---
+++ buildscripts/tests/resmokelib/utils/test_scheduler.py
@@ -0,0 +1,43 @@
+"""Unit tests for buildscripts/resmokelib/utils/scheduler.py."""
+
+import sched
+import unittest
+
+from buildscripts.resmokelib.utils import scheduler as _scheduler
+
+# pylint: disable=missing-docstring
+
+
+def noop():
+    pass
+
+
+class TestScheduler(unittest.TestCase):
+    """Unit tests for the Scheduler class."""
+    scheduler = _scheduler.Scheduler
+
+    def setUp(self):
+        self.__scheduler = self.scheduler()
+
+    def test_cancel_with_identical_time_and_priority(self):
+        event1 = self.__scheduler.enterabs(time=0, priority=0, action=noop)
+        event2 = self.__scheduler.enterabs(time=0, priority=0, action=noop)
+
+        self.__scheduler.cancel(event1)
+        self.assertIs(self.__scheduler.queue[0], event2)
+
+        # Attempting to cancel the same event should fail because it has already been removed.
+        with self.assertRaises(ValueError):
+            self.__scheduler.cancel(event1)
+
+        self.__scheduler.cancel(event2)
+        self.assertEqual(self.__scheduler.queue, [])
+
+
+class TestBuiltinScheduler(TestScheduler):
+    """Unit tests for the sched.scheduler class."""
+    scheduler = sched.scheduler
+
+    def test_cancel_with_identical_time_and_priority(self):
+        with self.assertRaises(AssertionError):
+            super().test_cancel_with_identical_time_and_priority()
