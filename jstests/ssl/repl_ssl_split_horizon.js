c8641e0d403d1033f55effea9c2e921fabadf260
Justin Seyster <justin.seyster@mongodb.com>
2019-08-13 18:52:23 -0400

    SERVER-42178 Lint fixup
    (cherry picked from commit 191d27d5e6b74fbc4f7651b31ab9562d2b34efd6)
    
     Conflicts:
    	jstests/ssl/repl_ssl_split_horizon.js

--- jstests/ssl/repl_ssl_split_horizon.js
+++ jstests/ssl/repl_ssl_split_horizon.js
@@ -41,7 +41,7 @@
                 version is ${glibc_version}, skipping this test.`);
             return;
         } else {
-            var verCheck = runProgram("grep", "/etc/os-release", "\"SUSE Linux Enterprise Server\"")
+            var verCheck = runProgram("grep", "/etc/os-release", "\"SUSE Linux Enterprise Server\"");
             if (verCheck == 0) {
                 jsTestLog(`HOSTALIASES does not seem to work as expected but we detected SLES. GLIBC
                     version is ${glibc_version}, skipping this test.`);
