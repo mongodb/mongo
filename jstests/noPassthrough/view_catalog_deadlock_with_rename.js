3fe21bd5cbc750b6123ab89374cffd77db03c4d6
Xiangyu Yao <xiangyu.yao@mongodb.com>
2019-08-19 18:12:29 -0400

    Revert "SERVER-41947 Disallow using the system.views collection name as the source or target names in the rename command"
    This reverts commit ab5e8175f64c33606861c8b659e3708944177871.

---
+++ jstests/noPassthrough/view_catalog_deadlock_with_rename.js
@@ -0,0 +1,36 @@
+/**
+ * This test demonstrates the deadlock we found in SERVER-40994: Rename locks
+ * the source and target based on their resourceId order, and a delete op on an
+ * non-existent collection (which also triggers a view catalog reload) locks the
+ * same two namespaces in different order.
+ *
+ * The fix is to always lock 'system.views' collection in the end.
+ */
+(function() {
+'use strict';
+
+const conn = MongoRunner.runMongod();
+const db = conn.getDB('test');
+
+assert.commandWorked(db.runCommand({insert: 'a', documents: [{x: 1}]}));
+assert.commandWorked(db.runCommand({insert: 'b', documents: [{x: 1}]}));
+
+assert.commandWorked(db.createView('viewA', 'a', []));
+
+// Will cause a view catalog reload.
+assert.commandWorked(db.runCommand(
+    {insert: 'system.views', documents: [{_id: 'test.viewB', viewOn: 'b', pipeline: []}]}));
+
+const renameSystemViews = startParallelShell(function() {
+    // This used to first lock 'test.system.views' and then 'test.aaabb' in X mode.
+    assert.commandWorked(
+        db.adminCommand({renameCollection: 'test.system.views', to: 'test.aaabb'}));
+}, conn.port);
+
+// This triggers view catalog reload. Therefore it first locked 'test.aaabb' in IX mode and then
+// 'test.system.views' in IS mode.
+assert.commandWorked(db.runCommand({delete: 'aaabb', deletes: [{q: {x: 2}, limit: 1}]}));
+
+renameSystemViews();
+MongoRunner.stopMongod(conn);
+})();
