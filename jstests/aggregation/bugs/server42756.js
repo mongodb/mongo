6729eaa16ca2794425fd90f034506e8d30a0cb5f
Anton Korshunov <anton.korshunov@mongodb.com>
2019-08-28 22:30:42 +0000

    SERVER-42756 Ensure that pipeline optimizations are re-enabled after test finishes

--- jstests/aggregation/bugs/server42756.js
+++ jstests/aggregation/bugs/server42756.js
@@ -10,43 +10,54 @@ const specials = [{val: NaN, path: "$nan"}, {val: Infinity, path: "$inf"}];
 
 assert.commandWorked(coll.insert({inf: Infinity, nan: NaN}));
 
-["off", "alwaysOn"].forEach((mode) => {
-    assert.commandWorked(
-        db.adminCommand({configureFailPoint: 'disablePipelineOptimization', mode: mode}));
+try {
+    ["alwaysOn", "off"].forEach((mode) => {
+        assert.commandWorked(
+            db.adminCommand({configureFailPoint: 'disablePipelineOptimization', mode: mode}));
 
-    // TODO SERVER-43034: include $add and $sum.
-    ["$multiply"].forEach((op) => {
-        (function testCommutativityWithConstArguments() {
-            specials.forEach((special) => {
-                numbers.forEach((num) => {
-                    const expected = [{
-                        a: (num instanceof NumberDecimal ? NumberDecimal(special.val) : special.val)
-                    }];
-                    assert.eq(expected,
-                              coll.aggregate([{$project: {a: {[op]: [special.val, num]}, _id: 0}}])
-                                  .toArray());
-                    assert.eq(expected,
-                              coll.aggregate([{$project: {a: {[op]: [num, special.val]}, _id: 0}}])
-                                  .toArray());
+        // TODO SERVER-43034: include $add and $sum.
+        ["$multiply"].forEach((op) => {
+            (function testCommutativityWithConstArguments() {
+                specials.forEach((special) => {
+                    numbers.forEach((num) => {
+                        const expected = [{
+                            a: (num instanceof NumberDecimal ? NumberDecimal(special.val)
+                                                             : special.val)
+                        }];
+                        assert.eq(
+                            expected,
+                            coll.aggregate([{$project: {a: {[op]: [special.val, num]}, _id: 0}}])
+                                .toArray());
+                        assert.eq(
+                            expected,
+                            coll.aggregate([{$project: {a: {[op]: [num, special.val]}, _id: 0}}])
+                                .toArray());
+                    });
                 });
-            });
-        })();
+            })();
 
-        (function testCommutativityWithNonConstArgument() {
-            specials.forEach((special) => {
-                numbers.forEach((num) => {
-                    const expected = [{
-                        a: (num instanceof NumberDecimal ? NumberDecimal(special.val) : special.val)
-                    }];
-                    assert.eq(expected,
-                              coll.aggregate([{$project: {a: {[op]: [special.path, num]}, _id: 0}}])
-                                  .toArray());
-                    assert.eq(expected,
-                              coll.aggregate([{$project: {a: {[op]: [num, special.path]}, _id: 0}}])
-                                  .toArray());
+            (function testCommutativityWithNonConstArgument() {
+                specials.forEach((special) => {
+                    numbers.forEach((num) => {
+                        const expected = [{
+                            a: (num instanceof NumberDecimal ? NumberDecimal(special.val)
+                                                             : special.val)
+                        }];
+                        assert.eq(
+                            expected,
+                            coll.aggregate([{$project: {a: {[op]: [special.path, num]}, _id: 0}}])
+                                .toArray());
+                        assert.eq(
+                            expected,
+                            coll.aggregate([{$project: {a: {[op]: [num, special.path]}, _id: 0}}])
+                                .toArray());
+                    });
                 });
-            });
-        })();
+            })();
+        });
     });
-});
+} finally {
+    assert.commandWorked(
+        db.adminCommand({configureFailPoint: 'disablePipelineOptimization', mode: 'off'}));
+}
 })();
