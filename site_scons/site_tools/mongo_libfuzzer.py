d4480b94410fa3a7624f3f79ca1d56d0f10b3479
Mathew Robinson <mathew.robinson@mongodb.com>
2019-09-10 13:48:09 +0000

    SERVER-41973 Integrate mongo_libfuzzer.py with Hygienic

--- site_scons/site_tools/mongo_libfuzzer.py
+++ site_scons/site_tools/mongo_libfuzzer.py
@@ -34,6 +34,18 @@ def build_cpp_libfuzzer_test(env, target, source, **kwargs):
     sanitizer_option = '-fsanitize=fuzzer'
     myenv.Prepend(LINKFLAGS=[sanitizer_option])
 
+    libfuzzer_test_components = {'tests', 'fuzzertests'}
+    if (
+            'AIB_COMPONENT' in kwargs
+            and not kwargs['AIB_COMPONENTS'].endswith('-fuzzertest')
+    ):
+        kwargs['AIB_COMPONENT'] += '-fuzzertest'
+
+    if 'AIB_COMPONENTS_EXTRA' in kwargs:
+        libfuzzer_test_components = set(kwargs['AIB_COMPONENTS_EXTRA']).union(libfuzzer_test_components)
+
+    kwargs['AIB_COMPONENTS_EXTRA'] = libfuzzer_test_components
+
     result = myenv.Program(target, source, **kwargs)
     myenv.RegisterLibfuzzerTest(result[0])
     hygienic = myenv.GetOption('install-mode') == 'hygienic'
