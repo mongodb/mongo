cfb01eec7cea6ec2663e8f218ed1e07bb1209d52
Benety Goh <benety@mongodb.com>
2019-09-28 12:31:59 +0000

    SERVER-43301 disable backup_restore_*.js tests under two phase index builds

--- jstests/noPassthrough/backup_restore_stop_start.js
+++ jstests/noPassthrough/backup_restore_stop_start.js
@@ -11,7 +11,11 @@
  *
  * Some methods for backup used in this test checkpoint the files in the dbpath. This technique will
  * not work for ephemeral storage engines, as they do not store any data in the dbpath.
- * @tags: [requires_persistence, requires_replication]
+ * @tags: [
+ *     requires_persistence,
+ *     requires_replication,
+ *     two_phase_index_builds_unsupported,
+ * ]
  */
 
 load("jstests/noPassthrough/libs/backup_restore.js");
