bcec27cddbc82def0eb09e7abe047b14f6ccb995
Adam Cooper <adam.cooper@mongodb.com>
2019-10-07 22:01:54 +0000

    SERVER-43751 Recompute compressor manager message parameters
    (cherry picked from commit 1411cf602a21e45a5ef42b6869c480eb420976ee)

--- src/mongo/transport/message_compressor_manager_test.cpp
+++ src/mongo/transport/message_compressor_manager_test.cpp
@@ -317,6 +317,27 @@ TEST(MessageCompressorManager, MessageSizeTooLarge) {
     ASSERT_NOT_OK(status);
 }
 
+TEST(MessageCompressorManager, MessageSizeTooSmall) {
+    auto registry = buildRegistry();
+    MessageCompressorManager compManager(&registry);
+
+    auto badMessageBuffer = SharedBuffer::allocate(128);
+    MsgData::View badMessage(badMessageBuffer.get());
+    badMessage.setId(1);
+    badMessage.setResponseToMsgId(0);
+    badMessage.setOperation(dbCompressed);
+    badMessage.setLen(128);
+
+    DataRangeCursor cursor(badMessage.data(), badMessage.data() + badMessage.dataLen());
+    uassertStatusOK(cursor.writeAndAdvance<LittleEndian<int32_t>>(dbQuery));
+    uassertStatusOK(cursor.writeAndAdvance<LittleEndian<int32_t>>(-1));
+    uassertStatusOK(
+        cursor.writeAndAdvance<LittleEndian<uint8_t>>(registry.getCompressor("noop")->getId()));
+
+    auto status = compManager.decompressMessage(Message(badMessageBuffer), nullptr).getStatus();
+    ASSERT_NOT_OK(status);
+}
+
 TEST(MessageCompressorManager, RuntMessage) {
     auto registry = buildRegistry();
     MessageCompressorManager compManager(&registry);
