cf4c944977a348494d81eeaf7eddb96ef0457876
Benety Goh <benety@mongodb.com>
2019-10-11 19:19:38 +0000

    SERVER-43910 lockInfo command obtains lock to client info mapping from LockManager

--- src/mongo/db/concurrency/lock_manager.cpp
+++ src/mongo/db/concurrency/lock_manager.cpp
@@ -400,6 +400,32 @@ const unsigned LockManager::_numLockBuckets(128);
 // The exact value doesn't appear very important, but should be power of two
 const unsigned LockManager::_numPartitions = 32;
 
+// static
+std::map<LockerId, BSONObj> LockManager::getLockToClientMap(ServiceContext* serviceContext) {
+    std::map<LockerId, BSONObj> lockToClientMap;
+
+    for (ServiceContext::LockedClientsCursor cursor(serviceContext);
+         Client* client = cursor.next();) {
+        invariant(client);
+
+        stdx::lock_guard<Client> lk(*client);
+        const OperationContext* clientOpCtx = client->getOperationContext();
+
+        // Operation context specific information
+        if (clientOpCtx) {
+            BSONObjBuilder infoBuilder;
+            // The client information
+            client->reportState(infoBuilder);
+
+            infoBuilder.append("opid", static_cast<int>(clientOpCtx->getOpID()));
+            LockerId lockerId = clientOpCtx->lockState()->getId();
+            lockToClientMap.insert({lockerId, infoBuilder.obj()});
+        }
+    }
+
+    return lockToClientMap;
+}
+
 LockManager::LockManager() {
     _lockBuckets = new LockBucket[_numLockBuckets];
     _partitions = new Partition[_numPartitions];
