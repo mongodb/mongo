dfc9b994a471b289a9fa8583471e27696b869771
Shreyas Kalyan <shreyas.kalyan@mongodb.com>
2019-10-15 16:04:49 +0000

    SERVER-42886 Fix bypassAutoDecrypt settings in shell

--- jstests/client_encrypt/fle_auto_decrypt.js
+++ jstests/client_encrypt/fle_auto_decrypt.js
@@ -15,6 +15,8 @@ const x509_options = {
 
 const conn = MongoRunner.runMongod(x509_options);
 
+const deterministicAlgorithm = "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic";
+
 let localKMS = {
     key: BinData(
         0,
@@ -29,29 +31,45 @@ const clientSideFLEOptions = {
     schemaMap: {}
 };
 
-const deterministicAlgorithm = "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic";
-
 const shell = Mongo(conn.host, clientSideFLEOptions);
 const keyVault = shell.getKeyVault();
 
 const keyId = keyVault.createKey("local", "arn:aws:kms:us-east-1:fake:fake:fake", ['mongoKey']);
 
-const clientEncrypt = shell.getClientEncryption();
-const encryptedStr = clientEncrypt.encrypt(keyId, "mongodb", deterministicAlgorithm);
+var test = function(conn, clientSideFLEOptions, keyId) {
+    const test_shell = Mongo(conn.host, clientSideFLEOptions);
 
-// Insert encrypted string into database
-const collection = conn.getDB("test").getCollection("collection");
+    const clientEncrypt = test_shell.getClientEncryption();
+    const encryptedStr = clientEncrypt.encrypt(keyId, "mongodb", deterministicAlgorithm);
 
-for (var i = 0; i < 150; i++) {
-    assert.commandWorked(collection.insert({string: encryptedStr, id: 1}));
-}
+    // Insert encrypted string into database
+    const collection = conn.getDB("test").getCollection("collection");
 
-// Ensure string is auto decrypted
-const encryptedCollection = shell.getDB("test").getCollection("collection");
-const result = encryptedCollection.find({id: 1}).toArray();
-result.forEach(function(entry) {
-    assert.eq(entry.string, "mongodb");
-});
+    for (var i = 0; i < 150; i++) {
+        assert.commandWorked(collection.insert({string: encryptedStr, id: 1}));
+    }
+
+    // Ensure string is auto decrypted
+    const encryptedCollection = test_shell.getDB("test").getCollection("collection");
+    const result = encryptedCollection.find({id: 1}).toArray();
+    result.forEach(function(entry) {
+        assert.eq(entry.string, "mongodb");
+    });
+};
+
+test(conn, clientSideFLEOptions, keyId);
+
+// Test that auto decrypt still works with bypassAutoEncryption
+const clientSideFLEOptionsBypassAutoEncrypt = {
+    kmsProviders: {
+        local: localKMS,
+    },
+    keyVaultNamespace: "test.coll",
+    bypassAutoEncryption: true,
+    schemaMap: {}
+};
+
+test(conn, clientSideFLEOptionsBypassAutoEncrypt, keyId);
 
 MongoRunner.stopMongod(conn);
 }());
