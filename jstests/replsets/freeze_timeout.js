854c92f3c9319f588fdbbcce125451d64d895a22
Samyukta Lanka <samy.lanka@mongodb.com>
2019-10-16 23:12:40 +0000

    SERVER-42534 Step down primary for less time in freeze_timeout.js and increase electionTimeoutMillis
    (cherry picked from commit 488adcaea8526492411d433229a765d5bd7dd5db)

--- jstests/replsets/freeze_timeout.js
+++ jstests/replsets/freeze_timeout.js
@@ -8,16 +8,21 @@
 
     jsTestLog('1: initialize single node replica set');
     const replSet = new ReplSetTest({name: 'freeze_timeout', nodes: 1});
-    const nodes = replSet.startSet();
-    const config = replSet.getReplSetConfig();
-    replSet.initiate(config);
+    replSet.startSet();
+
+    var conf = replSet.getReplSetConfig();
+    // Increase the election timeout to 24 hours so that elections are not called due to election
+    // timeouts, since we need them to be called for the kSingleNodePromptElection election reason.
+    conf.settings = {electionTimeoutMillis: 24 * 60 * 60 * 1000};
+    replSet.initiate(conf);
     replSet.awaitReplication();
+
     let primary = replSet.getPrimary();
     const initialPrimaryStatus = assert.commandWorked(primary.adminCommand({serverStatus: 1}));
 
     jsTestLog('2: step down primary');
     assert.throws(function() {
-        primary.getDB("admin").runCommand({replSetStepDown: 10, force: 1});
+        primary.getDB("admin").runCommand({replSetStepDown: 1, force: 1});
     });
 
     jsTestLog('3: wait for stepped down node to become primary again');
@@ -33,17 +38,13 @@
 
     jsTestLog('4: step down primary again');
     assert.throws(function() {
-        primary.getDB("admin").runCommand({replSetStepDown: 10, force: 1});
+        primary.getDB("admin").runCommand({replSetStepDown: 30, force: 1});
     });
 
-    jsTestLog('5: freeze stepped down primary for 30 seconds');
-    primary.getDB("admin").runCommand({replSetFreeze: 30});
-    sleep(1000);
-
-    jsTestLog('6: unfreeze stepped down primary after waiting for 1 second');
+    jsTestLog('5: unfreeze stepped down primary');
     primary.getDB("admin").runCommand({replSetFreeze: 0});
 
-    jsTestLog('7: wait for unfrozen node to become primary again');
+    jsTestLog('6: wait for unfrozen node to become primary again');
     primary = replSet.getPrimary();
 
     // Check that both the 'called' and 'successful' fields of the 'freezeTimeout' election reason
