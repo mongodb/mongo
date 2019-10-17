abe90fb2090ead3fdbf9f62d5db50c88eb31d527
Anton Korshunov <anton.korshunov@mongodb.com>
2019-10-17 10:05:56 +0000

    SERVER-43764 Add more testing for special values in DoubleDoubleSummation

--- src/mongo/db/pipeline/accumulator_sum.cpp
+++ src/mongo/db/pipeline/accumulator_sum.cpp
@@ -120,15 +120,7 @@ Value AccumulatorSum::getValue(bool toBeMerged) {
         case NumberDouble:
             return Value(nonDecimalTotal.getDouble());
         case NumberDecimal: {
-            double sum, error;
-            std::tie(sum, error) = nonDecimalTotal.getDoubleDouble();
-            Decimal128 total;  // zero
-            if (sum != 0) {
-                total = total.add(Decimal128(sum, Decimal128::kRoundTo34Digits));
-                total = total.add(Decimal128(error, Decimal128::kRoundTo34Digits));
-            }
-            total = total.add(decimalTotal);
-            return Value(total);
+            return Value(decimalTotal.add(nonDecimalTotal.getDecimal()));
         }
         default:
             MONGO_UNREACHABLE;
