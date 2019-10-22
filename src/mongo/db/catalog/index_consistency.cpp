30d9f90fdd2a2fcf197bdee465a55d63eb9cec5e
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2019-10-22 11:19:39 +0000

    SERVER-43908 Modify IndexConsistency hash-map keys to avoid hitting an invariant on duplicate index keys in KeyString form for different indexes

--- src/mongo/db/catalog/index_consistency.cpp
+++ src/mongo/db/catalog/index_consistency.cpp
@@ -53,6 +53,24 @@ namespace {
 // The number of items we can scan before we must yield.
 static const int kScanLimit = 1000;
 static const size_t kNumHashBuckets = 1U << 16;
+
+/**
+ * Returns a key for the '_extraIndexEntries' and '_missingIndexEntries' maps. The key is a pair
+ * of index name and the index key represented in KeyString form.
+ * Using the index name is required as the index keys are passed in as KeyStrings which do not
+ * contain field names.
+ *
+ * If we had the following document: { a: 1, b: 1 } with two indexes on keys "a" and "b", then
+ * the KeyStrings for the index keys of the document would be identical as the field name in the
+ * KeyString is not present. The BSON representation of this would look like: { : 1 } for both.
+ * To distinguish these as different index keys, return a pair of index name and index key.
+ */
+std::pair<std::string, std::string> _generateKeyForMap(const IndexInfo& indexInfo,
+                                                       const KeyString& ks) {
+    return std::make_pair(indexInfo.descriptor->indexName(),
+                          std::string(ks.getBuffer(), ks.getSize()));
+}
+
 }  // namespace
 
 IndexInfo::IndexInfo(const IndexDescriptor* descriptor)
@@ -225,10 +243,10 @@ void IndexConsistency::addDocKey(const KeyString& ks,
             idKey = data["_id"];
         }
 
-        std::string key = std::string(ks.getBuffer(), ks.getSize());
         BSONObj info = _generateInfo(*indexInfo, recordId, indexKey, idKey);
 
-        // Cannot have duplicate KeyStrings during the document scan phase.
+        // Cannot have duplicate KeyStrings during the document scan phase for the same index.
+        IndexKey key = _generateKeyForMap(*indexInfo, ks);
         invariant(_missingIndexEntries.count(key) == 0);
         _missingIndexEntries.insert(std::make_pair(key, info));
     }
@@ -250,10 +268,8 @@ void IndexConsistency::addIndexKey(const KeyString& ks,
         // If there is a corresponding document key for the index entry key, we remove the key from
         // the '_missingIndexEntries' map. However if there was no document key for the index entry
         // key, we add the key to the '_extraIndexEntries' map.
-
-        std::string key = std::string(ks.getBuffer(), ks.getSize());
+        IndexKey key = _generateKeyForMap(*indexInfo, ks);
         BSONObj info = _generateInfo(*indexInfo, recordId, indexKey, boost::none);
-
         if (_missingIndexEntries.count(key) == 0) {
             // We may have multiple extra index entries for a given KeyString.
             auto search = _extraIndexEntries.find(key);
--- src/mongo/dbtests/validate_tests.cpp
+++ src/mongo/dbtests/validate_tests.cpp
@@ -1227,6 +1227,126 @@ public:
     }
 };
 
+class ValidateDuplicateDocumentIndexKeySet : public ValidateBase {
+public:
+    ValidateDuplicateDocumentIndexKeySet() : ValidateBase(/*full=*/false, /*background=*/false) {}
+
+    void run() {
+        // Create a new collection.
+        lockDb(MODE_X);
+        Collection* coll;
+        {
+            WriteUnitOfWork wunit(&_opCtx);
+            ASSERT_OK(_db->dropCollection(&_opCtx, _ns));
+            coll = _db->createCollection(&_opCtx, _ns);
+            wunit.commit();
+        }
+
+        // Create two identical indexes only differing by key pattern and name.
+        {
+            const auto indexName = "a";
+            const auto indexKey = BSON("a" << 1);
+            auto status =
+                dbtests::createIndexFromSpec(&_opCtx,
+                                             coll->ns().ns(),
+                                             BSON("name" << indexName << "key" << indexKey << "v"
+                                                         << static_cast<int>(kIndexVersion)
+                                                         << "background"
+                                                         << false
+                                                         << "ns"
+                                                         << _ns));
+            ASSERT_OK(status);
+        }
+
+        {
+            const auto indexName = "b";
+            const auto indexKey = BSON("b" << 1);
+            auto status =
+                dbtests::createIndexFromSpec(&_opCtx,
+                                             coll->ns().ns(),
+                                             BSON("name" << indexName << "key" << indexKey << "v"
+                                                         << static_cast<int>(kIndexVersion)
+                                                         << "background"
+                                                         << false
+                                                         << "ns"
+                                                         << _ns));
+            ASSERT_OK(status);
+        }
+
+        // Insert a document.
+        OpDebug* const nullOpDebug = nullptr;
+        RecordId rid = RecordId::min();
+        lockDb(MODE_X);
+        {
+            WriteUnitOfWork wunit(&_opCtx);
+            ASSERT_OK(
+                coll->insertDocument(&_opCtx,
+                                     InsertStatement(BSON("_id" << 1 << "a" << 1 << "b" << 1)),
+                                     nullOpDebug,
+                                     true));
+            rid = coll->getCursor(&_opCtx)->next()->id;
+            wunit.commit();
+        }
+        releaseDb();
+        ASSERT_TRUE(checkValid());
+
+        // Remove the index entry for index "a".
+        {
+            lockDb(MODE_X);
+
+            IndexCatalog* indexCatalog = coll->getIndexCatalog();
+            const std::string indexName = "a";
+            auto descriptor = indexCatalog->findIndexByName(&_opCtx, indexName);
+            auto iam =
+                const_cast<IndexAccessMethod*>(indexCatalog->getEntry(descriptor)->accessMethod());
+
+            WriteUnitOfWork wunit(&_opCtx);
+            int64_t numDeleted;
+            const BSONObj actualKey = BSON("a" << 1);
+            InsertDeleteOptions options;
+            options.logIfError = true;
+            options.dupsAllowed = true;
+
+            ASSERT_OK(iam->remove(&_opCtx, actualKey, RecordId(1), options, &numDeleted));
+
+            ASSERT_EQUALS(numDeleted, 1);
+            wunit.commit();
+
+            releaseDb();
+        }
+
+        // Remove the index entry for index "b".
+        {
+            lockDb(MODE_X);
+
+            IndexCatalog* indexCatalog = coll->getIndexCatalog();
+            const std::string indexName = "b";
+            auto descriptor = indexCatalog->findIndexByName(&_opCtx, indexName);
+            auto iam =
+                const_cast<IndexAccessMethod*>(indexCatalog->getEntry(descriptor)->accessMethod());
+
+            WriteUnitOfWork wunit(&_opCtx);
+            int64_t numDeleted;
+            const BSONObj actualKey = BSON("b" << 1);
+            InsertDeleteOptions options;
+            options.logIfError = true;
+            options.dupsAllowed = true;
+
+            ASSERT_OK(iam->remove(&_opCtx, actualKey, RecordId(1), options, &numDeleted));
+            ASSERT_EQUALS(numDeleted, 1);
+            wunit.commit();
+
+            releaseDb();
+        }
+
+        {
+            // Now we have two missing index entries with the keys { : 1 } since the KeyStrings
+            // aren't hydrated with their field names.
+            ASSERT_TRUE(!checkValid());
+        }
+    }
+};
+
 class ValidateTests : public Suite {
 public:
     ValidateTests() : Suite("validate_tests") {}
@@ -1267,6 +1387,8 @@ public:
         add<ValidateMissingAndExtraIndexEntryResults<false, false>>();
         add<ValidateMissingIndexEntryResults<false, false>>();
         add<ValidateExtraIndexEntryResults<false, false>>();
+
+        add<ValidateDuplicateDocumentIndexKeySet>();
     }
 } validateTests;
 }  // namespace ValidateTests
