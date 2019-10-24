a9bfbf6b7a465d9981e4054587a76479f5df8d18
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2019-10-24 13:05:25 +0000

    SERVER-44188 validate_tests should remove index entries by RecordId obtained from the cursor

--- src/mongo/dbtests/validate_tests.cpp
+++ src/mongo/dbtests/validate_tests.cpp
@@ -1307,7 +1307,7 @@ public:
             options.logIfError = true;
             options.dupsAllowed = true;
 
-            ASSERT_OK(iam->remove(&_opCtx, actualKey, RecordId(1), options, &numDeleted));
+            ASSERT_OK(iam->remove(&_opCtx, actualKey, rid, options, &numDeleted));
 
             ASSERT_EQUALS(numDeleted, 1);
             wunit.commit();
@@ -1332,7 +1332,7 @@ public:
             options.logIfError = true;
             options.dupsAllowed = true;
 
-            ASSERT_OK(iam->remove(&_opCtx, actualKey, RecordId(1), options, &numDeleted));
+            ASSERT_OK(iam->remove(&_opCtx, actualKey, rid, options, &numDeleted));
             ASSERT_EQUALS(numDeleted, 1);
             wunit.commit();
 
