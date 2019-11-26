96faf66a31507e714d2255116c10f6bbda11c8e8
Adam Cooper <adam.cooper@mongodb.com>
2019-11-26 21:53:52 +0000

    SERVER-43726 Make ssl_alert_reporting.js tolerate LEGACY crypto policy on RHEL8
    (cherry picked from commit 1b3fe4d3a7f136e888647a1de605dc8030f71583)

--- jstests/ssl/libs/ssl_helpers.js
+++ jstests/ssl/libs/ssl_helpers.js
@@ -275,7 +275,8 @@ function isRHEL8() {
 
 function sslProviderSupportsTLS1_0() {
     if (isRHEL8()) {
-        return false;
+        const cryptoPolicy = cat("/etc/crypto-policies/config");
+        return cryptoPolicy.includes("LEGACY");
     }
 
     return true;
@@ -283,7 +284,8 @@ function sslProviderSupportsTLS1_0() {
 
 function sslProviderSupportsTLS1_1() {
     if (isRHEL8()) {
-        return false;
+        const cryptoPolicy = cat("/etc/crypto-policies/config");
+        return cryptoPolicy.includes("LEGACY");
     }
 
     return true;
