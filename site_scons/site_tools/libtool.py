8591df844932e573aa8badd80db1e9c95f6d1320
Mathew Robinson <mathew.robinson@mongodb.com>
2019-12-11 20:54:37 +0000

    SERVER-44970 Remove noop_action from site_scons/site_tools/libtool.py

--- site_scons/site_tools/libtool.py
+++ site_scons/site_tools/libtool.py
@@ -7,11 +7,8 @@ def generate(env):
     env['ARCOM'] = '$AR -static -o $TARGET $ARFLAGS $SOURCES'
     env['ARFLAGS'] = ["-s", "-no_warning_for_no_symbols"]
 
-    def noop_action(env, target, source):
-        pass
-
     # Disable running ranlib, since we added 's' above
-    env['RANLIBCOM'] = noop_action
+    env['RANLIBCOM'] = ''
     env['RANLIBCOMSTR'] = 'Skipping ranlib for libtool generated target $TARGET'
 
 
