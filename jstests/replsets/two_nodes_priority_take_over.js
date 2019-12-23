e351d69ef98529aaf91c584d8c68919340082b81
Siyuan Zhou <siyuan.zhou@mongodb.com>
2019-12-23 21:03:36 +0000

    SERVER-33801 Reenable two_nodes_priority_take_over.js

--- jstests/replsets/two_nodes_priority_take_over.js
+++ jstests/replsets/two_nodes_priority_take_over.js
@@ -2,57 +2,51 @@
 // because of stepping down. In a two node replset, this rejection will prevent
 // smooth priority takeover.
 
-// TODO: We have to disable this test until SERVER-21456 is fixed, due to the
-// race of tagging and closing connections on stepdown.
-if (false) {
-    load("jstests/replsets/rslib.js");
+(function() {
+"use strict";
 
-    (function() {
-        "use strict";
-        var name = "two_nodes_priority_take_over";
-        var rst = new ReplSetTest({name: name, nodes: 2});
+load("jstests/replsets/rslib.js");
 
-        rst.startSet();
-        var conf = rst.getReplSetConfig();
-        conf.members[0].priority = 2;
-        conf.members[1].priority = 1;
-        rst.initiate(conf);
-        rst.awaitSecondaryNodes();
-        // Set verbosity for replication on all nodes.
-        var verbosity = {
-            "setParameter": 1,
-            "logComponentVerbosity": {"verbosity": 4, "storage": {"verbosity": 1}}
-        };
-        rst.nodes.forEach(function(node) {
-            node.adminCommand(verbosity);
-        });
+var name = "two_nodes_priority_take_over";
+var rst = new ReplSetTest({name: name, nodes: 2});
 
-        // The first node will be the primary at the beginning.
-        rst.waitForState(rst.nodes[0], ReplSetTest.State.PRIMARY);
+rst.startSet();
+var conf = rst.getReplSetConfig();
+conf.members[0].priority = 2;
+conf.members[1].priority = 1;
+rst.initiate(conf);
+rst.awaitSecondaryNodes();
 
-        // Get the term when replset is stable.
-        var res = rst.getPrimary().adminCommand("replSetGetStatus");
-        assert.commandWorked(res);
-        var stableTerm = res.term;
+// Increase the verbosity for everything except storage on all nodes.
+setLogVerbosity(rst.nodes, {"verbosity": 4, "storage": {"verbosity": 1}});
 
-        // Reconfig to change priorities. The current primary remains the same until
-        // the higher priority node takes over.
-        var conf = rst.getReplSetConfig();
-        conf.members[0].priority = 1;
-        conf.members[1].priority = 2;
-        conf.version = 2;
-        reconfig(rst, conf);
+// The first node will be the primary at the beginning.
+rst.waitForState(rst.nodes[0], ReplSetTest.State.PRIMARY);
 
-        // The second node will take over the primary.
-        rst.waitForState(rst.nodes[1], ReplSetTest.State.PRIMARY, 60 * 1000);
+// Get the term when replset is stable.
+var res = rst.getPrimary().adminCommand("replSetGetStatus");
+assert.commandWorked(res);
+var stableTerm = res.term;
 
-        res = rst.getPrimary().adminCommand("replSetGetStatus");
-        assert.commandWorked(res);
-        var newTerm = res.term;
+// Reconfig to change priorities. The current primary remains the same until
+// the higher priority node takes over.
+var conf = rst.getReplSetConfigFromNode();
+conf.members[0].priority = 1;
+conf.members[1].priority = 2;
+conf.version = conf.version + 1;
+reconfig(rst, conf);
 
-        // Priority takeover should happen smoothly without failed election as there is
-        // no current candidate. If vote requests failed (wrongly) for some reason,
-        // nodes have to start new elections, which increase the term unnecessarily.
-        assert.eq(newTerm, stableTerm + 1);
-    })();
-}
+// The second node will take over the primary.
+rst.waitForState(rst.nodes[1], ReplSetTest.State.PRIMARY, 60 * 1000);
+
+res = rst.getPrimary().adminCommand("replSetGetStatus");
+assert.commandWorked(res);
+var newTerm = res.term;
+
+// Priority takeover should happen smoothly without failed election as there is
+// no current candidate. If vote requests failed (wrongly) for some reason,
+// nodes have to start new elections, which increase the term unnecessarily.
+assert.eq(newTerm, stableTerm + 1);
+
+rst.stopSet();
+})();
