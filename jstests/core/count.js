dbb9af6eb32692a2a0e4603340cc227a5d9735b8
Katherine Wu <katherine.wu@mongodb.com>
2019-12-24 00:48:39 +0000

    SERVER-42410 mongo shell count helper command throws errors on invalid options

--- jstests/core/count.js
+++ jstests/core/count.js
@@ -1,26 +1,37 @@
-// @tags: [requires_fastcount]
+// @tags: [requires_fastcount, assumes_against_mongod_not_mongos]
 
-t = db.jstests_count;
+(function() {
+"use strict";
 
-t.drop();
-t.save({i: 1});
-t.save({i: 2});
-assert.eq(1, t.find({i: 1}).count(), "A");
-assert.eq(1, t.count({i: 1}), "B");
-assert.eq(2, t.find().count(), "C");
-assert.eq(2, t.find(undefined).count(), "D");
-assert.eq(2, t.find(null).count(), "E");
-assert.eq(2, t.count(), "F");
+const coll = db.jstests_count;
 
-t.drop();
-t.save({a: true, b: false});
-t.ensureIndex({b: 1, a: 1});
-assert.eq(1, t.find({a: true, b: false}).count(), "G");
-assert.eq(1, t.find({b: false, a: true}).count(), "H");
+coll.drop();
+assert.commandWorked(coll.insert({i: 1}));
+assert.commandWorked(coll.insert({i: 2}));
+assert.eq(1, coll.find({i: 1}).count());
+assert.eq(1, coll.count({i: 1}));
+assert.eq(2, coll.find().count());
+assert.eq(2, coll.find(undefined).count());
+assert.eq(2, coll.find(null).count());
+assert.eq(2, coll.count());
 
-t.drop();
-t.save({a: true, b: false});
-t.ensureIndex({b: 1, a: 1, c: 1});
+coll.drop();
+assert.commandWorked(coll.insert({a: true, b: false}));
+assert.commandWorked(coll.ensureIndex({b: 1, a: 1}));
+assert.eq(1, coll.find({a: true, b: false}).count());
+assert.eq(1, coll.find({b: false, a: true}).count());
 
-assert.eq(1, t.find({a: true, b: false}).count(), "I");
-assert.eq(1, t.find({b: false, a: true}).count(), "J");
+coll.drop();
+assert.commandWorked(coll.insert({a: true, b: false}));
+assert.commandWorked(coll.ensureIndex({b: 1, a: 1, c: 1}));
+
+assert.eq(1, coll.find({a: true, b: false}).count());
+assert.eq(1, coll.find({b: false, a: true}).count());
+
+// Make sure that invalid options passed into the shell count helper generate errors.
+assert.eq(assert.throws(() => coll.count({}, {random: true})).code, 40415);
+
+// Test that a string passed into count will be converted into a $where clause.
+assert.eq(1, coll.count("this.a"));
+assert.eq(0, coll.count("this.b"));
+})();
