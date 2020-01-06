d6f3842709c9759ad87a31529ee3f05cbdba56d7
James Wahlin <james.wahlin@mongodb.com>
2020-01-06 16:49:46 +0000

    SERVER-45369 Add requires_fcv_44 tag to map_reduce_validation.js

--- jstests/core/map_reduce_validation.js
+++ jstests/core/map_reduce_validation.js
@@ -2,7 +2,8 @@
 // @tags: [
 //   assumes_no_implicit_collection_creation_after_drop,
 //   uses_map_reduce_with_temp_collections,
-//   does_not_support_stepdowns
+//   does_not_support_stepdowns,
+//   requires_fcv_44
 // ]
 (function() {
 "use strict";
