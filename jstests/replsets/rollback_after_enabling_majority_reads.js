6f0db2a804fc4b07346459b5302ffc95356e3fd2
Lingzhi Deng <lingzhi.deng@mongodb.com>
2020-01-11 00:00:51 +0000

    SERVER-45484: Blacklist rollback_after_enabling_majority_reads.js from multiversion suite

--- jstests/replsets/rollback_after_enabling_majority_reads.js
+++ jstests/replsets/rollback_after_enabling_majority_reads.js
@@ -5,7 +5,7 @@
  * enableMajorityReadConcern=true.
  * Rollback after restarting with enableMajorityReadConcern=true succeeds if the common point is at
  * least the stable timestamp, i.e. we do not attempt to roll back operations that were included in
- * @tags: [requires_persistence]
+ * @tags: [requires_persistence, requires_fcv_44]
  */
 (function() {
 "use strict";
