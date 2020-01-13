92f2eb0263feb59adea80eb8115614623a2e213a
Alexander Costas <alexander.costas@mongodb.com>
2020-01-13 20:10:40 +0000

    SERVER-45488: detect_outliers doesn't appear to be running

--- buildscripts/signal_processing_setup.sh
+++ buildscripts/signal_processing_setup.sh
@@ -1,22 +1,13 @@
 #!/usr/bin/env bash
-if [[ -d "signal_processing_venv" ]]; then
-    source ./signal_processing_venv/bin/activate
-    exit 0
-fi
+rm -rf ./signal_processing_venv
+rm -f ./analysis.yml
+rm -f ./.evergreen.yml
 
 # Configure signal processing
 cat > ./analysis.yml << EOF
-mongo_uri: mongodb+srv://\${analysis_user}:\${analysis_password}@performancedata-g6tsc.mongodb.net/perf
+mongo_uri: mongodb+srv://${analysis_user}:${analysis_password}@performancedata-g6tsc.mongodb.net/perf
 is_patch: ${is_patch}
 task_id: ${task_id}
-EOF
-virtualenv -p /opt/mongodbtoolchain/v3/bin/python3 signal_processing_venv
-
-# Setup pip to use our internal PyPI
-cat > ./signal_processing_venv/pip.conf << EOF
-[global]
-index-url = https://pypi.org/simple
-extra-index-url = https://${perf_jira_user}:${perf_jira_pw}@artifactory.corp.mongodb.com/artifactory/api/pypi/mongodb-dag-local/simple
 EOF
 
 # Create the Evergreen API credentials
@@ -26,8 +17,15 @@ api_key: "${evergreen_api_key}"
 user: "${evergreen_api_user}"
 END_OF_CREDS
 
+# need to create virtualenv here to configure it below
+virtualenv -p /opt/mongodbtoolchain/v3/bin/python3 signal_processing_venv
+
+# Setup pip to use our internal PyPI
+cat > ./signal_processing_venv/pip.conf << EOF
+[global]
+index-url = https://pypi.org/simple
+extra-index-url = https://${perf_jira_user}:${perf_jira_pw}@artifactory.corp.mongodb.com/artifactory/api/pypi/mongodb-dag-local/simple
+EOF
+
 source ./signal_processing_venv/bin/activate
 pip install dag-signal-processing~=1.0.14
-
-export analysis_user="${dsi_analysis_atlas_user}"
-export analysis_password="${dsi_analysis_atlas_pw}"
