8eff91ffbeb4f346fc5edbb6b5cec96127a1e424
Marcos José Grillo Ramírez <marcos.grillo@10gen.com>
2020-01-13 13:14:22 +0000

    SERVER-45483 Stale Shard Version errors are not retried if another error is returned
    Temporarily allow MultipleErrorsOccurred on read_only tests

--- jstests/readonly/write_ops.js
+++ jstests/readonly/write_ops.js
@@ -1,29 +1,32 @@
 load("jstests/readonly/lib/read_only_test.js");
-
+/*
+ * TODO SERVER-45483: The write command should only be returning an
+ * IlegalOperation
+ */
 runReadOnlyTest(function() {
     'use strict';
     return {
         name: 'write_ops',
         load: function(writableCollection) {
-            assert.commandWorked(writableCollection.insert({_id: 0, x: 1}, {ordered: false}));
+            assert.commandWorked(writableCollection.insert({_id: 0, x: 1}));
         },
         exec: function(readableCollection) {
             // Test that insert fails.
             assert.writeErrorWithCode(
-                readableCollection.insert({x: 2}, {ordered: false}),
-                ErrorCodes.IllegalOperation,
+                readableCollection.insert({x: 2}),
+                [ErrorCodes.IllegalOperation, ErrorCodes.MultipleErrorsOccurred],
                 "Expected insert to fail because database is in read-only mode");
 
             // Test that delete fails.
             assert.writeErrorWithCode(
-                readableCollection.remove({x: 1}, {ordered: false}),
-                ErrorCodes.IllegalOperation,
+                readableCollection.remove({x: 1}),
+                [ErrorCodes.IllegalOperation, ErrorCodes.MultipleErrorsOccurred],
                 "Expected remove to fail because database is in read-only mode");
 
             // Test that update fails.
             assert.writeErrorWithCode(
-                readableCollection.update({_id: 0}, {$inc: {x: 1}}, {ordered: false}),
-                ErrorCodes.IllegalOperation,
+                readableCollection.update({_id: 0}, {$inc: {x: 1}}),
+                [ErrorCodes.IllegalOperation, ErrorCodes.MultipleErrorsOccurred],
                 "Expected update to fail because database is in read-only mode");
         }
     };
