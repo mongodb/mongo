7ba6fede8aab0ab8a77515d7c11a017b213d8f93
Amirsaman Memaripour <amirsaman.memaripour@10gen.com>
2020-01-16 20:44:59 +0000

    SERVER-45212 Add try_lock() to SpinLock

--- src/mongo/db/client.h
+++ src/mongo/db/client.h
@@ -152,6 +152,9 @@ public:
     void unlock() {
         _lock.unlock();
     }
+    bool try_lock() {
+        return _lock.try_lock();
+    }
 
     /**
      * Makes a new operation context representing an operation on this client.  At most
