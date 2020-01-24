a7aecc1ff0af7822c38b5f5da2bc0fd27e3f7778
Benety Goh <benety@mongodb.com>
2020-01-24 13:16:09 -0500

    SERVER-45681 fix list_indexes_with_build_uuids.js to work with storage engine without snapshot read support

--- jstests/noPassthrough/list_indexes_with_build_uuids.js
+++ jstests/noPassthrough/list_indexes_with_build_uuids.js
@@ -57,11 +57,20 @@ replSet.waitForAllIndexBuildsToFinish(dbName, collName);
 // Start hanging index builds on the secondary.
 IndexBuildTest.pauseIndexBuilds(secondary);
 
-// Build and hang on the second index.
-assert.commandWorked(primaryDB.runCommand({
-    createIndexes: collName,
-    indexes: [{key: {j: 1}, name: secondIndexName, background: true}],
-}));
+// With storage engines that do not support snapshot reads, the commitIndexBuild oplog entry may
+// block the listIndexes command on the secondary during oplog application because it will hold the
+// PBWM while waiting for the index build to complete in the backgroud. Therefore, we get the
+// primary to hold off on writing the commitIndexBuild oplog entry until we are ready to resume
+// index builds on the secondary.
+if (IndexBuildTest.supportsTwoPhaseIndexBuild(primary)) {
+    IndexBuildTest.pauseIndexBuilds(primary);
+}
+
+// Build and hang on the second index. This should be run in the background if we pause index
+// builds on the primary because the createIndexes command will block.
+const coll = primaryDB.getCollection(collName);
+const createIdx =
+    IndexBuildTest.startIndexBuild(primary, coll.getFullName(), {j: 1}, {name: secondIndexName});
 
 // Wait for index builds to start on the secondary.
 const opId = IndexBuildTest.waitForIndexBuildToStart(secondaryDB);
@@ -81,8 +90,12 @@ assert.eq(indexes[1].name, "first");
 assert.eq(indexes[2].spec.name, "second");
 assert(indexes[2].hasOwnProperty("buildUUID"));
 
-// Allow the secondary to finish the index build.
+// Allow the replica set to finish the index build.
 IndexBuildTest.resumeIndexBuilds(secondary);
+
+// Wait for the index build to complete on the primary if we paused it.
+IndexBuildTest.resumeIndexBuilds(primary);
+createIdx();
 
 replSet.stopSet();
 }());
