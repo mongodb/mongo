4433187014909dc7e83849d7f32c082042b1b2bf
Siyuan Zhou <visualzhou@gmail.com>
2020-02-07 18:48:09 -0500

    SERVER-35437 Wait for secondary state after stepdown command in multi_rs.js

--- jstests/multiVersion/libs/multi_rs.js
+++ jstests/multiVersion/libs/multi_rs.js
@@ -117,6 +117,9 @@ ReplSetTest.prototype.stepdown = function(nodeId) {
         return true;
     });
 
+    // Wait for the state to be secondary to avoid the race of closing new connections on stepdown
+    // on 3.2 and 3.4.
+    this.waitForState(node, ReplSetTest.State.SECONDARY);
     return this.reconnect(node);
 };
 
