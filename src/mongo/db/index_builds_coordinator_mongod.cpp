5f3625880188f9ca76bacb02c541abc2bd2d0f96
Eric Milkie <milkie@10gen.com>
2020-02-12 12:54:17 -0500

    SERVER-44984 reduce index build thread pool default and index build external sort memory limit default
    This reverts commit b48cb5d8e2a8ccab0f4be401cc2533e5c2b650ae.

--- src/mongo/db/index_builds_coordinator_mongod.cpp
+++ src/mongo/db/index_builds_coordinator_mongod.cpp
@@ -70,7 +70,7 @@ ThreadPool::Options makeDefaultThreadPoolOptions() {
     // We depend on thread pool sizes being equal between primaries and secondaries. If a secondary
     // has fewer resources than a primary, index build oplog entries can replicate in an order that
     // the secondary is unable to fulfill, leading to deadlocks. See SERVER-44250.
-    options.maxThreads = 10;
+    options.maxThreads = 3;
 
     // Ensure all threads have a client.
     options.onCreateThread = [](const std::string& threadName) {
@@ -84,6 +84,10 @@ ThreadPool::Options makeDefaultThreadPoolOptions() {
 
 IndexBuildsCoordinatorMongod::IndexBuildsCoordinatorMongod()
     : _threadPool(makeDefaultThreadPoolOptions()) {
+    _threadPool.startup();
+}
+IndexBuildsCoordinatorMongod::IndexBuildsCoordinatorMongod(ThreadPool::Options options)
+    : _threadPool(std::move(options)) {
     _threadPool.startup();
 }
 
