20f37580bdf89817db398c477d619627ce1feef7
Alex Taskov <alex.taskov@mongodb.com>
2020-02-12 09:45:35 -0500

    SERVER-46088 Exclude sharding auth tests from ephemeralForTest variant

--- jstests/auth/upgrade_noauth_to_keyfile_with_sharding.js
+++ jstests/auth/upgrade_noauth_to_keyfile_with_sharding.js
@@ -1,6 +1,6 @@
 // Tests access control upgrade on a sharded cluster
 // The purpose is to verify the connectivity between mongos, config server, and the shards
-// @tags: [requires_sharding]
+// @tags: [requires_sharding, requires_document_locking]
 
 load('jstests/ssl/libs/ssl_helpers.js');
 
