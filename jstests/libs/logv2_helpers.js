40296a983c2ace467788ea931229aa9d91311cd1
Henrik Edin <henrik.edin@mongodb.com>
2020-02-19 09:18:30 -0500

    SERVER-45138 Fix logv2 text builder

--- jstests/libs/logv2_helpers.js
+++ jstests/libs/logv2_helpers.js
@@ -3,7 +3,8 @@
 function isJsonLogNoConn() {
     const textDefault =
         getBuildInfo().buildEnvironment.cppdefines.indexOf("MONGO_CONFIG_TEXT_LOG_DEFAULT") >= 0;
-    if (typeof TestData !== 'undefined' && typeof TestData.logFormat !== 'undefined') {
+    if (typeof TestData !== 'undefined' && typeof TestData.logFormat !== 'undefined' &&
+        TestData["logFormat"] != "") {
         return TestData["logFormat"] != "text" || (textDefault && TestData["logFormat"] == "json");
     }
 
@@ -13,14 +14,15 @@ function isJsonLogNoConn() {
 function isJsonLog(conn) {
     const textDefault =
         getBuildInfo().buildEnvironment.cppdefines.indexOf("MONGO_CONFIG_TEXT_LOG_DEFAULT") >= 0;
-
-    if (typeof TestData !== 'undefined' && typeof TestData.logFormat !== 'undefined') {
+    if (typeof TestData !== 'undefined' && typeof TestData.logFormat !== 'undefined' &&
+        TestData["logFormat"] != "") {
         return TestData["logFormat"] != "text" || (textDefault && TestData["logFormat"] == "json");
     }
 
     const opts = assert.commandWorked(conn.getDB("admin").runCommand({"getCmdLineOpts": 1}));
     const parsed = opts["parsed"];
-    if (parsed.hasOwnProperty("systemLog") && parsed["systemLog"].hasOwnProperty("logFormat")) {
+    if (parsed.hasOwnProperty("systemLog") && parsed["systemLog"].hasOwnProperty("logFormat") &&
+        parsed["systemLog"]["logFormat"] != "") {
         return parsed["systemLog"]["logFormat"] != "text" ||
             (textDefault && parsed["systemLog"]["logFormat"] == "json");
     }
