39c6f724daaaf2f0d0f16d815f9fe281dd7dbba9
Randolph Tan <randolph@10gen.com>
2020-02-21 12:59:25 -0500

    SERVER-46301 Add requires_persistence tag to SERVER-26369.js

--- jstests/sslSpecial/SERVER-26369.js
+++ jstests/sslSpecial/SERVER-26369.js
@@ -1,3 +1,7 @@
+// Test restarts a single repl set node and it will never have repl config when it restarts
+// so it won't be able to transition to primary.
+// @tags: [requires_persistence]
+
 // Validate the shardsrvr does not crash when enabling SSL with encrypted PEM for a cluster
 // Checking UUID consistency involves talking to a shard node, which in this test is shutdown
 TestData.skipCheckingUUIDsConsistentAcrossCluster = true;
