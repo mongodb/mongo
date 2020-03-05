ef959bbc8e0b3601323a162eb99d69716b07e548
A. Jesse Jiryu Davis <jesse@mongodb.com>
2020-03-05 16:28:07 -0500

    SERVER-46089 Test downgrade with arbiter

--- jstests/multiVersion/remove_config_term_on_replset_downgrade.js
+++ jstests/multiVersion/remove_config_term_on_replset_downgrade.js
@@ -9,73 +9,91 @@
 load('jstests/multiVersion/libs/multi_rs.js');
 load('jstests/libs/test_background_ops.js');
 
-let newVersion = "latest";
-let oldVersion = "last-stable";
+const newVersion = "latest";
+const oldVersion = "last-stable";
 
-let nodes = {
-    n1: {binVersion: newVersion},
-    n2: {binVersion: newVersion},
-    n3: {binVersion: newVersion}
-};
+function testDowngrade(useArbiter) {
+    let nodes = {
+        n1: {binVersion: newVersion},
+        n2: {binVersion: newVersion},
+        n3: {binVersion: newVersion}
+    };
 
-let rst = new ReplSetTest({nodes: nodes});
-rst.startSet();
-rst.initiate();
+    let replicaSetName = `testDowngrade${useArbiter ? "With" : "Without"}Arbiter`;
+    let rst = new ReplSetTest({name: replicaSetName, nodes: nodes});
+    let nodenames = rst.nodeList();
+    rst.startSet();
+    rst.initiate({
+        "_id": replicaSetName,
+        "members": [
+            {"_id": 0, "host": nodenames[0]},
+            {"_id": 1, "host": nodenames[1]},
+            {"_id": 2, "host": nodenames[2], arbiterOnly: useArbiter}
+        ]
+    });
 
-let primary = rst.getPrimary();
+    let primary = rst.getPrimary();
 
-// The default FCV is latestFCV for non-shard replica sets.
-let primaryAdminDB = rst.getPrimary().getDB("admin");
-checkFCV(primaryAdminDB, latestFCV);
+    // The default FCV is latestFCV for non-shard replica sets.
+    let primaryAdminDB = rst.getPrimary().getDB("admin");
+    checkFCV(primaryAdminDB, latestFCV);
 
-// Reconfig in FCV 4.4.
-let originalConfig = rst.getReplSetConfigFromNode();
-originalConfig.version++;
-reconfig(rst, originalConfig);
-rst.awaitNodesAgreeOnConfigVersion();
+    // Reconfig in FCV 4.4.
+    let originalConfig = rst.getReplSetConfigFromNode();
+    originalConfig.version++;
+    reconfig(rst, originalConfig);
+    rst.awaitNodesAgreeOnConfigVersion();
 
-// Check that the term field exists in the config document on all nodes.
-rst.nodes.forEach(function(node) {
-    jsTestLog("Checking the config term on node " + tojson(node.host) + " before downgrade.");
-    let config = node.getDB("local").getCollection("system.replset").findOne();
-    assert(config.hasOwnProperty("term"));
-});
+    // Check that the term field exists in the config document on all nodes.
+    rst.nodes.forEach(function(node) {
+        jsTestLog("Checking the config term on node " + tojson(node.host) + " before downgrade.");
+        let config = node.getDB("local").getCollection("system.replset").findOne();
+        assert(config.hasOwnProperty("term"));
+    });
 
-// Remember the config from the primary before the downgrade.
-let configInNewVersion = rst.getReplSetConfigFromNode();
+    // Remember the config from the primary before the downgrade.
+    let configInNewVersion = rst.getReplSetConfigFromNode();
 
-jsTest.log("Downgrading FCV to 4.2");
-assert.commandWorked(primary.adminCommand({setFeatureCompatibilityVersion: lastStableFCV}));
-rst.awaitReplication();
-// Check that the term field doesn't exist in the config document on all nodes.
-rst.nodes.forEach(function(node) {
-    jsTestLog("Checking the config term on node " + tojson(node.host) + " after FCV downgrade.");
-    let config = node.getDB("local").getCollection("system.replset").findOne();
-    assert(!config.hasOwnProperty("term"), tojson(config));
-    // The configs can only differ in config versions and terms.
-    config.term = configInNewVersion.term;
-    // The versions differ because of the force reconfig on downgrade.
-    assert.eq(config.version, configInNewVersion.version + 1);
-    config.version = configInNewVersion.version;
-    assert.docEq(configInNewVersion, config);
-});
+    jsTest.log("Downgrading FCV to 4.2");
+    assert.commandWorked(primary.adminCommand({setFeatureCompatibilityVersion: lastStableFCV}));
+    rst.awaitReplication();
+    // Check that the term field doesn't exist in the config document on all nodes.
+    rst.nodes.forEach(function(node) {
+        jsTestLog("Checking the config term on node " + tojson(node.host) +
+                  " after FCV downgrade.");
+        let config = node.getDB("local").getCollection("system.replset").findOne();
+        assert(!config.hasOwnProperty("term"), tojson(config));
+        // The configs can only differ in config versions and terms.
+        config.term = configInNewVersion.term;
+        // The versions differ because of the force reconfig on downgrade.
+        assert.eq(config.version, configInNewVersion.version + 1);
+        config.version = configInNewVersion.version;
+        assert.docEq(configInNewVersion, config);
+    });
 
-jsTest.log("Downgrading replica set..");
-rst.upgradeSet({binVersion: oldVersion});
-jsTest.log("Downgrade complete.");
+    jsTest.log("Downgrading replica set..");
+    rst.upgradeSet({binVersion: oldVersion});
+    jsTest.log("Downgrade complete.");
 
-// Check that the term field doesn't exist in the config document on all nodes.
-rst.nodes.forEach(function(node) {
-    reconnect(node);
-    jsTestLog("Checking the config term on node " + tojson(node.host) + " after binary downgrade.");
-    let config = node.getDB("local").getCollection("system.replset").findOne();
-    assert(!config.hasOwnProperty("term"), tojson(config));
-    // The configs can only differ in config versions and terms.
-    config.term = configInNewVersion.term;
-    // The versions differ because of the force reconfig on downgrade.
-    assert.eq(config.version, configInNewVersion.version + 1);
-    config.version = configInNewVersion.version;
-    assert.docEq(configInNewVersion, config);
-});
-rst.stopSet();
+    // Check that the term field doesn't exist in the config document on all nodes.
+    rst.nodes.forEach(function(node) {
+        reconnect(node);
+        jsTestLog("Checking the config term on node " + tojson(node.host) +
+                  " after binary downgrade.");
+        let config = node.getDB("local").getCollection("system.replset").findOne();
+        assert(!config.hasOwnProperty("term"), tojson(config));
+        // The configs can only differ in config versions and terms.
+        config.term = configInNewVersion.term;
+        // The versions differ because of the force reconfig on downgrade.
+        assert.eq(config.version, configInNewVersion.version + 1);
+        config.version = configInNewVersion.version;
+        assert.docEq(configInNewVersion, config);
+    });
+    rst.stopSet();
+}
+
+jsTestLog('Test downgrading WITHOUT an arbiter');
+testDowngrade(false);
+jsTestLog('Test downgrading WITH an arbiter');
+testDowngrade(true);
 })();
