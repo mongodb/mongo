3293adbef235bf86c6efd2c2b47a352d48d8434e
Billy Donahue <billy.donahue@mongodb.com>
2020-03-06 15:25:57 -0500

    SERVER-46763 structure the VersionInfoInterface tuples
    (cherry picked from commit 157f1221f6e2d896fb5968714d84b3da52eab504)

--- site_scons/mongo/generators.py
+++ site_scons/mongo/generators.py
@@ -4,17 +4,17 @@ import hashlib
 
 # Default and alternative generator definitions go here.
 
-# This is the tuple that will be returned by the buildInfo command and
+# This is the key/value mapping that will be returned by the buildInfo command and
 # printed by the --version command-line option to mongod.
-# Each tuple consists of:
-#   key (string)
-#   value (string)
-#   should be included in buildInfo output (bool)
-#   should be included in --version output (bool)
-# The values will be passed through env.subst, so you can use any SCons variables you
+# Each mapped value is in turn a dict consisting of:
+#   key: <string>
+#   value: <string>
+#   inBuildInfo: <bool> : should it be included in buildInfo output
+#   inVersion: <bool> : should it be included in --version output
+# The `value` field will be passed through env.subst, so you can use any SCons variables you
 # want to define them.
 def default_buildinfo_environment_data():
-    return (
+    data = (
         (
             'distmod',
             '$MONGO_DISTMOD',
@@ -76,12 +76,16 @@ def default_buildinfo_environment_data():
             False,
         ),
     )
+    return {
+        k:{'key': k, 'value': v, 'inBuildInfo': ibi, 'inVersion': iv}
+        for k, v, ibi, iv in data
+    }
 
 
 # If you want buildInfo and --version to be relatively empty, set
 # MONGO_BUILDINFO_ENVIRONMENT_DATA = empty_buildinfo_environment_data()
 def empty_buildinfo_environment_data():
-    return ()
+    return {}
 
 
 def default_variant_dir_generator(target, source, env, for_signature):
