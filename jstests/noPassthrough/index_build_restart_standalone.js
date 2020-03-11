cb002a42a593f80ee5062ee044667b46b93e354b
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2020-03-11 18:59:04 -0400

    SERVER-46310 index_build_restart_standalone.js should startup standalone nodes with 'recoverFromOplogAsStandalone=true'
    (cherry picked from commit 2dba93df686147f88bc13486365b1cae86958c7f)

--- jstests/noPassthrough/index_build_restart_standalone.js
+++ jstests/noPassthrough/index_build_restart_standalone.js
@@ -60,7 +60,22 @@ rst.stopSet(/*signal=*/null, /*forRestart=*/true);
 TestData.skipCheckDBHashes = false;
 
 function restartStandalone(node) {
-    return rst.restart(node, {noReplSet: true});
+    // Startup a mongod process on the nodes data files with recoverFromOplogAsStandalone=true. This
+    // parameter ensures that when the standalone starts up, it applies all unapplied oplog entries
+    // since the last shutdown.
+    const recoveryMongod = MongoRunner.runMongod({
+        dbpath: node.dbpath,
+        noReplSet: true,
+        noCleanData: true,
+        setParameter: 'recoverFromOplogAsStandalone=true'
+    });
+
+    // We need to shutdown this instance of mongod as using the recoverFromOplogAsStandalone=true
+    // parameter puts the server into read-only mode, but we need to be able to perform writes for
+    // this test.
+    MongoRunner.stopMongod(recoveryMongod);
+
+    return MongoRunner.runMongod({dbpath: node.dbpath, noReplSet: true, noCleanData: true});
 }
 
 (function restartPrimaryAsStandaloneAndCreate() {
