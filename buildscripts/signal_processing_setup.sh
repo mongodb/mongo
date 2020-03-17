f759d504ce7af142fedcffddcd95bfd02db642ed
Jeff Zambory <jeff.zambory@mongodb.com>
2020-03-17 10:39:52 -0400

    SERVER-45867 Use latest version of signal-processing
    (cherry picked from commit 889af79abadc442ad9277fa87cb4340654bdf8c0)
    
    # Conflicts:
    #	buildscripts/signal_processing_setup.sh
    #	etc/perf.yml
    #	etc/system_perf.yml

--- buildscripts/signal_processing_setup.sh
+++ buildscripts/signal_processing_setup.sh
@@ -5,10 +5,14 @@ if [[ -d "signal_processing_venv" ]]; then
 fi
 
 # Configure signal processing
-cat > ./analysis.yml << EOF
-mongo_uri: mongodb+srv://\${analysis_user}:\${analysis_password}@performancedata-g6tsc.mongodb.net/perf
-is_patch: ${is_patch}
+cat > .signal-processing.yml << EOF
 task_id: ${task_id}
+is_patch: ${is_patch}
+mongo_uri: mongodb+srv://${dsi_analysis_atlas_user}:${dsi_analysis_atlas_pw}@performancedata-g6tsc.mongodb.net/perf
+evergreen:
+  api_key: "${evergreen_api_key}"
+  user: "${evergreen_api_user}"
+  api_server_host: https://evergreen.mongodb.com
 EOF
 virtualenv -p /opt/mongodbtoolchain/v3/bin/python3 signal_processing_venv
 
@@ -19,15 +23,8 @@ index-url = https://pypi.org/simple
 extra-index-url = https://${perf_jira_user}:${perf_jira_pw}@artifactory.corp.mongodb.com/artifactory/api/pypi/mongodb-dag-local/simple
 EOF
 
-# Create the Evergreen API credentials
-cat > .evergreen.yml <<END_OF_CREDS
-api_server_host: https://evergreen.mongodb.com/api
-api_key: "${evergreen_api_key}"
-user: "${evergreen_api_user}"
-END_OF_CREDS
-
 source ./signal_processing_venv/bin/activate
-pip install dag-signal-processing~=1.0.14
+pip install dag-signal-processing~=2.0.0
 
 export analysis_user="${dsi_analysis_atlas_user}"
 export analysis_password="${dsi_analysis_atlas_pw}"
