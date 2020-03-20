55549b3383e36b6b5646354c3e0a7519add48e5d
Lamont Nelson <lamont.nelson@mongodb.com>
2020-03-20 02:57:22 -0400

    SERVER-47029: add only confirmed nodes when publishing onConfirmedSet; include best effort guess when nothing has been confirmed yet.

--- src/mongo/client/streamable_replica_set_monitor.h
+++ src/mongo/client/streamable_replica_set_monitor.h
@@ -131,6 +131,14 @@ private:
     };
     using HostQueryPtr = std::shared_ptr<HostQuery>;
 
+    // Information collected from the primary ServerDescription to be published via the
+    // ReplicaSetChangeNotifier
+    struct ChangeNotifierState {
+        HostAndPort primaryAddress;
+        std::set<HostAndPort> passives;
+        ConnectionString connectionString;
+    };
+
     SemiFuture<std::vector<HostAndPort>> _enqueueOutstandingQuery(
         WithLock, const ReadPreferenceSetting& criteria, const Date_t& deadline);
 
@@ -174,6 +182,7 @@ private:
     void _failOutstandingWithStatus(WithLock, Status status);
     bool _hasMembershipChange(sdam::TopologyDescriptionPtr oldDescription,
                               sdam::TopologyDescriptionPtr newDescription);
+    void _setConfirmedNotifierState(WithLock, const ServerDescriptionPtr& primaryDescription);
 
     Status _makeUnsatisfiedReadPrefError(const ReadPreferenceSetting& criteria) const;
     Status _makeReplicaSetMonitorRemovedError() const;
@@ -199,6 +208,7 @@ private:
 
     mutable Mutex _mutex = MONGO_MAKE_LATCH("ReplicaSetMonitor");
     std::vector<HostQueryPtr> _outstandingQueries;
+    boost::optional<ChangeNotifierState> _confirmedNotifierState;
     mutable PseudoRandom _random;
 
     static inline const auto kServerSelectionConfig =
