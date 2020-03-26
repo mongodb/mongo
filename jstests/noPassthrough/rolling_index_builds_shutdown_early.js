945c75a806e0c509c1282db236afd74106e73255
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2020-03-26 09:58:33 -0400

    SERVER-47131 Do not run 'rolling_index_builds_shutdown_early.js' on the commit quorum off builder
    (cherry picked from commit 8564f6263c90980d7d37c9c95f96238bb8d9f015)

--- jstests/noPassthrough/rolling_index_builds_shutdown_early.js
+++ jstests/noPassthrough/rolling_index_builds_shutdown_early.js
@@ -47,8 +47,9 @@ let primary = replTest.getPrimary();
 let primaryDB = primary.getDB(dbName);
 let coll = primaryDB.getCollection(collName);
 
-if (!IndexBuildTest.supportsTwoPhaseIndexBuild(primary)) {
-    jsTestLog('Two phase index builds not supported, skipping test.');
+if (!IndexBuildTest.supportsTwoPhaseIndexBuild(primary) ||
+    !IndexBuildTest.indexBuildCommitQuorumEnabled(primary)) {
+    jsTestLog('Two phase index builds or commit quorum are not supported, skipping test.');
     replTest.stopSet();
     return;
 }
