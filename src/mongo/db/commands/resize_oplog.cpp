e4ad538053ed7914575227de2b6cb596352b239a
Gregory Noma <gregory.noma@gmail.com>
2020-03-27 10:30:12 -0400

    Revert "SERVER-46779 Make replSetResizeOplog not require size parameter"
    This reverts commit 5862665e24170eb9fe9d0977fba1e6dc56a7a257.

--- src/mongo/db/commands/resize_oplog.cpp
+++ src/mongo/db/commands/resize_oplog.cpp
@@ -93,15 +93,13 @@ public:
         auto params =
             ReplSetResizeOplogRequest::parse(IDLParserErrorContext("replSetResizeOplog"), jsobj);
 
+        auto sizeMB = params.getSize();
+        const long long size = sizeMB * 1024 * 1024;  // sizeInMB * numByteInMB
+
         return writeConflictRetry(opCtx, "replSetResizeOplog", coll->ns().ns(), [&] {
             WriteUnitOfWork wunit(opCtx);
-
-            if (auto sizeMB = params.getSize()) {
-                const long long sizeBytes = *sizeMB * 1024 * 1024;
-                uassertStatusOK(coll->getRecordStore()->updateCappedSize(opCtx, sizeBytes));
-                DurableCatalog::get(opCtx)->updateCappedSize(
-                    opCtx, coll->getCatalogId(), sizeBytes);
-            }
+            uassertStatusOK(coll->getRecordStore()->updateCappedSize(opCtx, size));
+            DurableCatalog::get(opCtx)->updateCappedSize(opCtx, coll->getCatalogId(), size);
 
             if (auto minRetentionHoursOpt = params.getMinRetentionHours()) {
                 storageGlobalParams.oplogMinRetentionHours.store(*minRetentionHoursOpt);
@@ -109,11 +107,9 @@ public:
             wunit.commit();
 
             LOGV2(20497,
-                  "replSetResizeOplog success.",
-                  "size"_attr = DurableCatalog::get(opCtx)
-                                    ->getCollectionOptions(opCtx, coll->getCatalogId())
-                                    .cappedSize,
-                  "minRetentionHours"_attr = storageGlobalParams.oplogMinRetentionHours.load());
+                  "replSetResizeOplog success. Size: {size}, minRetentionHours: {retention}",
+                  "size"_attr = size,
+                  "retention"_attr = storageGlobalParams.oplogMinRetentionHours.load());
             return true;
         });
     }
