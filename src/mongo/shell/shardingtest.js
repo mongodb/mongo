6883bdfb8b8cff32176b1fd176df04da9165fd67
Jack Mulrow <jack.mulrow@mongodb.com>
2020-04-02 14:20:42 -0400

    SERVER-29153 Wait for nodes to agree on primary before writes in ShardingTest setup
    (cherry picked from commit 85a915d3b49c0cd0b106f40df55a68a2f6779de1)

--- src/mongo/shell/shardingtest.js
+++ src/mongo/shell/shardingtest.js
@@ -1354,6 +1354,7 @@ var ShardingTest = function(params) {
         }
 
         var rs = this._rs[i].test;
+        rs.awaitNodesAgreeOnPrimary();
         rs.getPrimary().getDB("admin").foo.save({x: 1});
 
         if (keyFile) {
@@ -1422,6 +1423,7 @@ var ShardingTest = function(params) {
     this.configRS.initiateWithAnyNodeAsPrimary(config);
 
     // Wait for master to be elected before starting mongos
+    this.configRS.awaitNodesAgreeOnPrimary();
     var csrsPrimary = this.configRS.getPrimary();
 
     // If 'otherParams.mongosOptions.binVersion' is an array value, then we'll end up constructing a
