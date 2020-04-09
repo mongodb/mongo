437b5f31521e39615553803324fae2b1ddb3b2b8
Judah Schvimer <judah@mongodb.com>
2020-04-09 10:28:22 -0400

    SERVER-47369 doReplSetReconfig should fail during primary drain mode

--- src/mongo/db/repl/replication_coordinator_impl_reconfig_test.cpp
+++ src/mongo/db/repl/replication_coordinator_impl_reconfig_test.cpp
@@ -93,6 +93,38 @@ TEST_F(ReplCoordTest, NodeReturnsNotMasterWhenReconfigReceivedWhileSecondary) {
     ASSERT_TRUE(result.obj().isEmpty());
 }
 
+TEST_F(ReplCoordTest, NodeReturnsNotMasterWhenReconfigReceivedWhileInDrainMode) {
+    init();
+
+    assertStartSuccess(BSON("_id"
+                            << "mySet"
+                            << "version" << 1 << "members"
+                            << BSON_ARRAY(BSON("_id" << 0 << "host"
+                                                     << "test1:1234")
+                                          << BSON("_id" << 1 << "host"
+                                                        << "test2:1234"))
+                            << "protocolVersion" << 1),
+                       HostAndPort("test1", 1234));
+    replCoordSetMyLastAppliedOpTime(OpTime(Timestamp(100, 1), 0), Date_t() + Seconds(100));
+    replCoordSetMyLastDurableOpTime(OpTime(Timestamp(100, 1), 0), Date_t() + Seconds(100));
+    ASSERT_OK(getReplCoord()->setFollowerMode(MemberState::RS_SECONDARY));
+    ASSERT_TRUE(getReplCoord()->getMemberState().secondary());
+
+    const auto opCtx = makeOperationContext();
+    simulateSuccessfulV1ElectionWithoutExitingDrainMode(
+        getReplCoord()->getElectionTimeout_forTest(), opCtx.get());
+
+    ASSERT_EQUALS(1, getReplCoord()->getTerm());
+    ASSERT_TRUE(getReplCoord()->getMemberState().primary());
+
+    BSONObjBuilder result;
+    ReplSetReconfigArgs args;
+    args.force = false;
+    ASSERT_EQUALS(ErrorCodes::NotMaster,
+                  getReplCoord()->processReplSetReconfig(opCtx.get(), args, &result));
+    ASSERT_TRUE(result.obj().isEmpty());
+}
+
 TEST_F(ReplCoordTest, NodeReturnsInvalidReplicaSetConfigWhenReconfigReceivedWithInvalidConfig) {
     // start up, become primary, receive uninitializable config
     assertStartSuccess(BSON("_id"
