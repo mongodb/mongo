3abfe3f8aed2b6e15ecb7c80bc77db0449d5bea5
Kevin Pulo <kevin.pulo@mongodb.com>
2020-04-09 15:36:14 +1000

    SERVER-44051 ensure getShardDistribution correctly checks for sharded collections
    (cherry picked from commit f32f2f906f8c37145ed2bf64cd8db99d35671a41)

--- src/mongo/shell/collection.js
+++ src/mongo/shell/collection.js
@@ -1113,7 +1113,8 @@ DBCollection.autocomplete = function(obj) {
  * @return {boolean}
  */
 DBCollection.prototype._isSharded = function() {
-    return !!this._db.getSiblingDB("config").collections.countDocuments({_id: this._fullName});
+    return !!this._db.getSiblingDB("config").collections.countDocuments(
+        {_id: this._fullName, dropped: false});
 };
 
 /**
