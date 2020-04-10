b263a8f810e2ff7c47262ecd53c1b919348f0196
Blake Oler <blake.oler@mongodb.com>
2020-04-10 14:00:59 -0400

    SERVER-41777 Call awaitLastOpCommitted() in test with stale mongos
    (cherry picked from commit bfe2e00fabc343c527f4eb3ab62be844ac4c02d3)

--- jstests/sharding/multi_mongos2.js
+++ jstests/sharding/multi_mongos2.js
@@ -6,6 +6,7 @@ var st = new ShardingTest({shards: 2, mongos: 2});
 
 assert.commandWorked(st.s0.adminCommand({enablesharding: "test"}));
 st.ensurePrimaryShard('test', st.shard1.shardName);
+st.configRS.awaitLastOpCommitted();
 
 // "test.foo" - sharded (by mongos 0)
 assert.commandWorked(st.s0.adminCommand({shardcollection: "test.foo", key: {num: 1}}));
@@ -17,6 +18,7 @@ assert.eq(1, st.s1.getDB('test').existing.count({_id: 1}));
 
 // "test.existing" - unsharded to sharded (by mongos 1)
 assert.commandWorked(st.s1.adminCommand({shardcollection: "test.existing", key: {_id: 1}}));
+st.configRS.awaitLastOpCommitted();
 assert.commandWorked(st.s1.adminCommand({split: "test.existing", middle: {_id: 5}}));
 assert.commandWorked(
     st.s1.adminCommand({moveChunk: "test.existing", find: {_id: 1}, to: st.shard0.shardName}));
@@ -30,6 +32,7 @@ assert.eq(1, st.s0.getDB('test').existing2.count({_id: 1}));
 assert.eq(1, st.s1.getDB('test').existing2.count({_id: 1}));
 
 assert.commandWorked(st.s1.adminCommand({shardcollection: "test.existing2", key: {_id: 1}}));
+st.configRS.awaitLastOpCommitted();
 assert.eq(true, st.s1.getDB('test').existing2.stats().sharded);
 
 assert.commandWorked(st.s1.adminCommand({split: "test.existing2", middle: {_id: 5}}));
@@ -45,6 +48,8 @@ assert.eq(1, st.s0.getDB('test').existing3.count({_id: 1}));
 assert.eq(1, st.s1.getDB('test').existing3.count({_id: 1}));
 
 assert.writeOK(st.s1.adminCommand({shardcollection: "test.existing3", key: {_id: 1}}));
+st.configRS.awaitLastOpCommitted();
+
 assert.commandWorked(st.s1.adminCommand({split: "test.existing3", middle: {_id: 5}}));
 assert.commandWorked(
     st.s0.adminCommand({moveChunk: "test.existing3", find: {_id: 1}, to: st.shard0.shardName}));
