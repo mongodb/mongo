d6ed492d525276cc4e33414a94f3a19b75852a4e
Adam Cooper <adam.cooper@mongodb.com>
2020-04-10 10:15:55 -0400

    Revert "SERVER-47268 retry sharded transaction in noop__createIndexes_not_blocked_by_txn.js on snapshot errors"
    This reverts commit d3da6de558fc4fbd7bb20617ed5d1f34c2c58bb3.

--- jstests/core/txns/noop_createIndexes_not_blocked_by_txn.js
+++ jstests/core/txns/noop_createIndexes_not_blocked_by_txn.js
@@ -3,10 +3,6 @@
 (function() {
 "use strict";
 
-// TODO(SERVER-39704): Remove the following load after SERVER-39704 is completed
-// For withTxnAndAutoRetryOnMongos.
-load('jstests/libs/auto_retry_transaction_in_sharding.js');
-
 const dbName = 'noop_createIndexes_not_blocked';
 const collName = 'test';
 const testDB = db.getSiblingDB(dbName);
@@ -28,43 +24,33 @@ const createIndexesCommand = {
 };
 assert.commandWorked(sessionDB.runCommand(createIndexesCommand));
 
-// Default read concern level to use for transactions. Snapshot read concern is not supported in
-// sharded transactions when majority reads are disabled.
-if (!TestData.hasOwnProperty('defaultTransactionReadConcernLevel')) {
-    TestData.defaultTransactionReadConcernLevel =
-        TestData.enableMajorityReadConcern !== false ? 'snapshot' : 'local';
-}
+session.startTransaction();
+assert.commandWorked(sessionDB[collName].insert({a: 5, b: 6}));
 
-// TODO(SERVER-39704): We use the withTxnAndAutoRetryOnMongos
-// function to handle how MongoS will propagate a StaleShardVersion error as a
-// TransientTransactionError. After SERVER-39704 is completed the
-// withTxnAndAutoRetryOnMongos function can be removed
-withTxnAndAutoRetryOnMongos(session, () => {
-    assert.commandWorked(sessionDB[collName].insert({a: 5, b: 6}));
+// This should not block because an identical index exists.
+let res = testDB.runCommand(createIndexesCommand);
+assert.commandWorked(res);
+assert.eq(res.numIndexesBefore, res.numIndexesAfter);
 
-    // This should not block because an identical index exists.
-    let res = testDB.runCommand(createIndexesCommand);
-    assert.commandWorked(res);
-    assert.eq(res.numIndexesBefore, res.numIndexesAfter);
-
-    // This should not block but return an error because the index exists with different options.
-    res = testDB.runCommand({
-        createIndexes: collName,
-        indexes: [{key: {a: 1}, name: "unique_a_1", unique: true}],
-    });
-    assert.commandFailedWithCode(res, ErrorCodes.IndexOptionsConflict);
-
-    // This should block and time out because the index does not already exist.
-    res = testDB.runCommand(
-        {createIndexes: collName, indexes: [{key: {b: 1}, name: "b_1"}], maxTimeMS: 500});
-    assert.commandFailedWithCode(res, ErrorCodes.MaxTimeMSExpired);
-
-    // This should block and time out because one of the indexes does not already exist.
-    res = testDB.runCommand({
-        createIndexes: collName,
-        indexes: [{key: {a: 1}, name: "a_1"}, {key: {b: 1}, name: "b_1"}],
-        maxTimeMS: 500
-    });
-    assert.commandFailedWithCode(res, ErrorCodes.MaxTimeMSExpired);
+// This should not block but return an error because the index exists with different options.
+res = testDB.runCommand({
+    createIndexes: collName,
+    indexes: [{key: {a: 1}, name: "unique_a_1", unique: true}],
 });
+assert.commandFailedWithCode(res, ErrorCodes.IndexOptionsConflict);
+
+// This should block and time out because the index does not already exist.
+res = testDB.runCommand(
+    {createIndexes: collName, indexes: [{key: {b: 1}, name: "b_1"}], maxTimeMS: 500});
+assert.commandFailedWithCode(res, ErrorCodes.MaxTimeMSExpired);
+
+// This should block and time out because one of the indexes does not already exist.
+res = testDB.runCommand({
+    createIndexes: collName,
+    indexes: [{key: {a: 1}, name: "a_1"}, {key: {b: 1}, name: "b_1"}],
+    maxTimeMS: 500
+});
+assert.commandFailedWithCode(res, ErrorCodes.MaxTimeMSExpired);
+
+assert.commandWorked(session.commitTransaction_forTesting());
 }());
