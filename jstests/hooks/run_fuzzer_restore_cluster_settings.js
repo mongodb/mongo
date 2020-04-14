709fecfc02190395b73849d5a902eae8c170e421
Gregory Noma <gregory.noma@gmail.com>
2020-04-14 14:37:21 -0400

    SERVER-47500 Specify writeConcern: {w: 1} in run_fuzzer_restore_cluster_settings.js
    (cherry picked from commit 3755ee11d537fe42781c88031790b2bbb34b1d1c)

--- jstests/hooks/run_fuzzer_restore_cluster_settings.js
+++ jstests/hooks/run_fuzzer_restore_cluster_settings.js
@@ -3,6 +3,10 @@
 
 // Unsetting read/write settings. This command will also cause the server to refresh and get
 // the new settings.
-assert.commandWorked(
-    db.adminCommand({setDefaultRWConcern: 1, defaultReadConcern: {}, defaultWriteConcern: {}}));
+assert.commandWorked(db.adminCommand({
+    setDefaultRWConcern: 1,
+    defaultReadConcern: {},
+    defaultWriteConcern: {},
+    writeConcern: {w: 1}
+}));
 })();
