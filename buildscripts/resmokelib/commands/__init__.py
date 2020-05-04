aee854c10ec766b0af71f02675ce6ce31bd19d47
Carl Raiden Worley <carl.worley@10gen.com>
2020-05-04 12:02:26 -0400

    SERVER-46684 Repackage the hang-analyzer as a resmoke subcommand

--- buildscripts/resmokelib/commands/__init__.py
+++ buildscripts/resmokelib/commands/__init__.py
@@ -2,3 +2,4 @@
 
 from . import interface
 from . import run
+from . import hang_analyzer
---
+++ buildscripts/resmokelib/hang_analyzer/__init__.py
@@ -0,0 +1,5 @@
+"""Utilities for the hang analyzer subcommand."""
+
+from . import process_list
+from . import dumper
+from . import process
