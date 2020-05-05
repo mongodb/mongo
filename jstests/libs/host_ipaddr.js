840799ea16600d18131c5b18c0a678d00f31b329
Mark Benvenuto <mark.benvenuto@mongodb.com>
2020-05-05 15:50:20 -0400

    SERVER-47927 Ubuntu 20.04 - ifconfig replaced with ip command

--- jstests/libs/host_ipaddr.js
+++ jstests/libs/host_ipaddr.js
@@ -15,7 +15,8 @@ function get_ipaddr() {
 
     var ipFile = path + "ipaddr-" + Random.srand() + ".log";
     var windowsCmd = "ipconfig > " + ipFile;
-    var unixCmd = "/sbin/ifconfig | grep inet | grep -v '127.0.0.1' > " + ipFile;
+    var unixCmd =
+        "(/sbin/ifconfig || /usr/sbin/ip addr) | grep 'inet ' | grep -v '127.0.0.1' > " + ipFile;
     var ipAddr = null;
     var hostType = null;
 
@@ -27,8 +28,9 @@ function get_ipaddr() {
             runProgram('cmd.exe', '/c', windowsCmd);
             ipAddr = cat(ipFile).match(/IPv4.*: (.*)/)[1];
         } else {
-            runProgram('bash', '-c', unixCmd);
-            ipAddr = cat(ipFile).replace(/addr:/g, "").match(/inet (.[^ ]*) /)[1];
+            runProgram('/bin/sh', '-c', unixCmd);
+            ipAddr =
+                cat(ipFile).replace(/addr:/g, "").match(/inet ([\d]+\.[\d]+\.[\d]+\.[\d]+)/)[1];
         }
     } finally {
         removeFile(ipFile);
