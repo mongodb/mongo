bc8faeeeefe9e6663598b161ced59d41be71f2b8
vrachev <vlad.rachev@mongodb.com>
2020-05-06 09:46:35 -0400

    SERVER-47611 Re-work to_local_args function using argparse
    (cherry picked from commit ece056a4cd1eba70a47f88d6d5fd2ba38e7d4df0)

--- buildscripts/resmokelib/commands/run.py
+++ buildscripts/resmokelib/commands/run.py
@@ -22,6 +22,7 @@ except ImportError:
 from buildscripts.resmokelib import config
 from buildscripts.resmokelib import errors
 from buildscripts.resmokelib import logging
+from buildscripts.resmokelib import parser
 from buildscripts.resmokelib import reportfile
 from buildscripts.resmokelib import sighandler
 from buildscripts.resmokelib import suitesconfig
@@ -158,12 +159,11 @@ class TestRunner(interface.Subcommand):  # pylint: disable=too-many-instance-att
         """Run the suite and tests specified."""
         self._resmoke_logger.info("verbatim resmoke.py invocation: %s", " ".join(sys.argv))
 
-        # TODO: SERVER-47611
-        # if config.EVERGREEN_TASK_ID:
-        #     local_args = parser.to_local_args()
-        #     self._resmoke_logger.info("resmoke.py invocation for local usage: %s %s",
-        #                               os.path.join("buildscripts", "resmoke.py"),
-        #                               " ".join(local_args))
+        if config.EVERGREEN_TASK_ID:
+            local_args = parser.to_local_args()
+            self._resmoke_logger.info("resmoke.py invocation for local usage: %s %s",
+                                      os.path.join("buildscripts", "resmoke.py"),
+                                      " ".join(local_args))
 
         suites = None
         try:
