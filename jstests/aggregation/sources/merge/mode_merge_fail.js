78fad397e4861f5c0cf735589be1334f8dd2b174
Mihai Andrei <mihai.andrei@10gen.com>
2020-05-08 11:33:45 -0400

    SERVER-48043 mode_merge_fail.js and mode_pipeline_fail.js should not assume ordering of writes from $merge
    (cherry picked from commit 649790b0ba9df895f168ac061941eacb633792f9)

--- jstests/aggregation/sources/merge/mode_merge_fail.js
+++ jstests/aggregation/sources/merge/mode_merge_fail.js
@@ -26,15 +26,37 @@ const pipeline = [mergeStage];
     assert.commandWorked(target.insert([{_id: 1, b: 1}, {_id: 3, b: 3}]));
     let error = assert.throws(() => source.aggregate(pipeline));
     assert.commandFailedWithCode(error, ErrorCodes.MergeStageNoMatchingDocument);
-    assertArrayEq(
-        {actual: target.find().toArray(), expected: [{_id: 1, a: 1, b: 1}, {_id: 3, a: 3, b: 3}]});
+    // Since there is no way to guarantee the ordering of the writes performed by $merge, it
+    // follows that the contents of the target collection will depend on when the write which
+    // triggers the MergeStageNoMatchingDocument error executes. As such, we test that the
+    // target collection contains some combination of its original documents and expected
+    // updates. In particular, it should be the case that each document has fields '_id' and
+    // 'b', but may or not have field 'a'. All present fields must share the same value as _id.
+    let checkOutputDocument = function(document) {
+        const hasB = document.hasOwnProperty("b");
+        const hasA = document.hasOwnProperty("a");
+        assert(hasB, document);
+        const bValue = document["b"];
+        assert.eq(bValue, document["_id"], document);
+        if (hasA) {
+            assert.eq(bValue, document["a"], document);
+        }
+    };
+
+    let result = target.find().toArray();
+    assert.eq(result.length, 2, result);
+    for (let elem of result) {
+        checkOutputDocument(elem);
+    }
 
     // Multiple documents without a match.
     assert(target.drop());
     assert.commandWorked(target.insert([{_id: 1, b: 1}]));
     error = assert.throws(() => source.aggregate(pipeline));
     assert.commandFailedWithCode(error, ErrorCodes.MergeStageNoMatchingDocument);
-    assertArrayEq({actual: target.find().toArray(), expected: [{_id: 1, a: 1, b: 1}]});
+    result = target.find().toArray();
+    assert.eq(result.length, 1, result);
+    checkOutputDocument(result[0]);
 })();
 
 // Test $merge when all documents in the source collection have a matching document in the
