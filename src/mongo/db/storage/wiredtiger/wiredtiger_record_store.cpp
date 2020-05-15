903e5c1fc671614575b54481e67f12219cbb8d74
Daniel Gottlieb <daniel.gottlieb@mongodb.com>
2020-05-14 21:24:34 -0400

    SERVER-48230: Release cursor resources for special oplog functions.
    (cherry picked from commit 0553eb750667692d25959c3b080156a6117789cf)

--- src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
@@ -1526,9 +1526,8 @@ StatusWith<Timestamp> WiredTigerRecordStore::getLatestOplogTimestamp(
 
     WiredTigerSessionCache* cache = WiredTigerRecoveryUnit::get(opCtx)->getSessionCache();
     auto sessRaii = cache->getSession();
-    WT_SESSION* sess = sessRaii->getSession();
-    WT_CURSOR* cursor;
-    invariantWTOK(sess->open_cursor(sess, _uri.c_str(), nullptr, nullptr, &cursor));
+    WT_CURSOR* cursor = sessRaii->getCachedCursor(_uri, _tableId, nullptr);
+    ON_BLOCK_EXIT([&] { sessRaii->releaseCursor(_tableId, cursor); });
     int ret = cursor->prev(cursor);
     if (ret == WT_NOTFOUND) {
         return Status(ErrorCodes::CollectionIsEmpty, "oplog is empty");
@@ -1536,7 +1535,6 @@ StatusWith<Timestamp> WiredTigerRecordStore::getLatestOplogTimestamp(
     invariantWTOK(ret);
 
     RecordId recordId = getKey(cursor);
-    invariantWTOK(sess->reset(sess));
 
     return {Timestamp(static_cast<unsigned long long>(recordId.repr()))};
 }
@@ -1550,9 +1548,8 @@ StatusWith<Timestamp> WiredTigerRecordStore::getEarliestOplogTimestamp(Operation
     if (_cappedFirstRecord == RecordId()) {
         WiredTigerSessionCache* cache = WiredTigerRecoveryUnit::get(opCtx)->getSessionCache();
         auto sessRaii = cache->getSession();
-        WT_SESSION* sess = sessRaii->getSession();
-        WT_CURSOR* cursor;
-        invariantWTOK(sess->open_cursor(sess, _uri.c_str(), nullptr, nullptr, &cursor));
+        WT_CURSOR* cursor = sessRaii->getCachedCursor(_uri, _tableId, nullptr);
+        ON_BLOCK_EXIT([&] { sessRaii->releaseCursor(_tableId, cursor); });
         auto ret = cursor->next(cursor);
         if (ret == WT_NOTFOUND) {
             return Status(ErrorCodes::CollectionIsEmpty, "oplog is empty");
@@ -1560,7 +1557,6 @@ StatusWith<Timestamp> WiredTigerRecordStore::getEarliestOplogTimestamp(Operation
         invariantWTOK(ret);
 
         _cappedFirstRecord = getKey(cursor);
-        invariantWTOK(sess->reset(sess));
     }
 
     return {Timestamp(static_cast<unsigned long long>(_cappedFirstRecord.repr()))};
