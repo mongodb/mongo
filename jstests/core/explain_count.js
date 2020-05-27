ca388ac2c93d785260aafdcc05299e0a7c3a41c0
Mihai Andrei <mihai.andrei@10gen.com>
2020-05-27 17:12:47 -0400

    SERVER-48343 Wrap runCommand calls with 'assert.commandWorked' in explain_count.js

--- jstests/core/explain_count.js
+++ jstests/core/explain_count.js
@@ -61,30 +61,39 @@ function checkIndexedCountWithPred(db, explain, keyName, bounds) {
 
 // Collection does not exist.
 assert.eq(0, t.count());
-var explain = db.runCommand({explain: {count: collName}, verbosity: "executionStats"});
+var explain =
+    assert.commandWorked(db.runCommand({explain: {count: collName}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 0});
 
 // Collection does not exist with skip, limit, and/or query.
-assert.eq(0, db.runCommand({count: collName, skip: 3}).n);
-explain = db.runCommand({explain: {count: collName, skip: 3}, verbosity: "executionStats"});
+var result = assert.commandWorked(db.runCommand({count: collName, skip: 3}));
+assert.eq(0, result.n);
+explain = assert.commandWorked(
+    db.runCommand({explain: {count: collName, skip: 3}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 0});
 
-assert.eq(0, db.runCommand({count: collName, limit: 3}).n);
-explain = db.runCommand({explain: {count: collName, limit: 3}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, limit: 3}));
+assert.eq(0, result.n);
+explain = assert.commandWorked(
+    db.runCommand({explain: {count: collName, limit: 3}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 0});
 
-assert.eq(0, db.runCommand({count: collName, limit: -3}).n);
-explain = db.runCommand({explain: {count: collName, limit: -3}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, limit: -3}));
+assert.eq(0, result.n);
+explain = assert.commandWorked(
+    db.runCommand({explain: {count: collName, limit: -3}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 0});
 
-assert.eq(0, db.runCommand({count: collName, limit: -3, skip: 4}).n);
-explain =
-    db.runCommand({explain: {count: collName, limit: -3, skip: 4}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, limit: -3, skip: 4}));
+assert.eq(0, result.n);
+explain = assert.commandWorked(
+    db.runCommand({explain: {count: collName, limit: -3, skip: 4}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 0});
 
-assert.eq(0, db.runCommand({count: collName, query: {a: 1}, limit: -3, skip: 4}).n);
-explain = db.runCommand(
-    {explain: {count: collName, query: {a: 1}, limit: -3, skip: 4}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, query: {a: 1}, limit: -3, skip: 4}));
+assert.eq(0, result.n);
+explain = assert.commandWorked(db.runCommand(
+    {explain: {count: collName, query: {a: 1}, limit: -3, skip: 4}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 0});
 
 // Now add a bit of data to the collection.
@@ -97,47 +106,59 @@ for (var i = 0; i < 10; i++) {
 
 // Trivial count with no skip, limit, or query.
 assert.eq(10, t.count());
-explain = db.runCommand({explain: {count: collName}, verbosity: "executionStats"});
+explain =
+    assert.commandWorked(db.runCommand({explain: {count: collName}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 10});
 
 // Trivial count with skip.
-assert.eq(7, db.runCommand({count: collName, skip: 3}).n);
-explain = db.runCommand({explain: {count: collName, skip: 3}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, skip: 3}));
+assert.eq(7, result.n);
+explain = assert.commandWorked(
+    db.runCommand({explain: {count: collName, skip: 3}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 7});
 
 // Trivial count with limit.
-assert.eq(3, db.runCommand({count: collName, limit: 3}).n);
-explain = db.runCommand({explain: {count: collName, limit: 3}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, limit: 3}));
+assert.eq(3, result.n);
+explain = assert.commandWorked(
+    db.runCommand({explain: {count: collName, limit: 3}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 3});
 
 // Trivial count with negative limit.
-assert.eq(3, db.runCommand({count: collName, limit: -3}).n);
-explain = db.runCommand({explain: {count: collName, limit: -3}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, limit: -3}));
+assert.eq(3, result.n);
+explain = assert.commandWorked(
+    db.runCommand({explain: {count: collName, limit: -3}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 3});
 
 // Trivial count with both limit and skip.
-assert.eq(3, db.runCommand({count: collName, limit: -3, skip: 4}).n);
-explain =
-    db.runCommand({explain: {count: collName, limit: -3, skip: 4}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, limit: -3, skip: 4}));
+assert.eq(3, result.n);
+explain = assert.commandWorked(
+    db.runCommand({explain: {count: collName, limit: -3, skip: 4}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 3});
 
 // With a query.
-assert.eq(10, db.runCommand({count: collName, query: {a: 1}}).n);
-explain = db.runCommand({explain: {count: collName, query: {a: 1}}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, query: {a: 1}}));
+assert.eq(10, result.n);
+explain = assert.commandWorked(
+    db.runCommand({explain: {count: collName, query: {a: 1}}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 10});
 checkIndexedCountWithPred(db, explain, "a", [1.0, 1.0]);
 
 // With a query and skip.
-assert.eq(7, db.runCommand({count: collName, query: {a: 1}, skip: 3}).n);
-explain = db.runCommand(
-    {explain: {count: collName, query: {a: 1}, skip: 3}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, query: {a: 1}, skip: 3}));
+assert.eq(7, result.n);
+explain = assert.commandWorked(db.runCommand(
+    {explain: {count: collName, query: {a: 1}, skip: 3}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 7});
 checkIndexedCountWithPred(db, explain, "a", [1.0, 1.0]);
 
 // With a query and limit.
-assert.eq(3, db.runCommand({count: collName, query: {a: 1}, limit: 3}).n);
-explain = db.runCommand(
-    {explain: {count: collName, query: {a: 1}, limit: 3}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, query: {a: 1}, limit: 3}));
+assert.eq(3, result.n);
+explain = assert.commandWorked(db.runCommand(
+    {explain: {count: collName, query: {a: 1}, limit: 3}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 3});
 checkIndexedCountWithPred(db, explain, "a", [1.0, 1.0]);
 
@@ -145,15 +166,17 @@ checkIndexedCountWithPred(db, explain, "a", [1.0, 1.0]);
 t.insert({a: 2});
 
 // Case where all results are skipped.
-assert.eq(0, db.runCommand({count: collName, query: {a: 2}, skip: 2}).n);
-explain = db.runCommand(
-    {explain: {count: collName, query: {a: 2}, skip: 2}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, query: {a: 2}, skip: 2}));
+assert.eq(0, result.n);
+explain = assert.commandWorked(db.runCommand(
+    {explain: {count: collName, query: {a: 2}, skip: 2}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 0});
 checkIndexedCountWithPred(db, explain, "a", [2, 2]);
 
 // Case where we have a limit, but we don't hit it.
-assert.eq(1, db.runCommand({count: collName, query: {a: 2}, limit: 2}).n);
-explain = db.runCommand(
-    {explain: {count: collName, query: {a: 2}, limit: 2}, verbosity: "executionStats"});
+result = assert.commandWorked(db.runCommand({count: collName, query: {a: 2}, limit: 2}));
+assert.eq(1, result.n);
+explain = assert.commandWorked(db.runCommand(
+    {explain: {count: collName, query: {a: 2}, limit: 2}, verbosity: "executionStats"}));
 assertExplainCount({explainResults: explain, expectedCount: 1});
 checkIndexedCountWithPred(db, explain, "a", [2, 2]);
