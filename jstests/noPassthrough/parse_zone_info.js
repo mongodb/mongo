c7bb043a38360dddfe983e7d887a768888c4f34d
James Wahlin <james@mongodb.com>
2020-05-29 11:06:32 -0400

    SERVER-44273 Fix timeZoneInfo file load on Windows
    (cherry picked from commit 56ef6a8f586e58f2d23911226c7880a7b678e0af)

--- jstests/noPassthrough/parse_zone_info.js
+++ jstests/noPassthrough/parse_zone_info.js
@@ -1,4 +1,4 @@
-// Tests the parsing of the timeZoneInfo parameter.
+// Tests the parsing of the timeZoneInfo parameter and file use.
 (function() {
 // Test that a bad file causes startup to fail.
 let conn = MongoRunner.runMongod({timeZoneInfo: "jstests/libs/config_files/bad_timezone_info"});
@@ -16,5 +16,20 @@ assert(rawMongoProgramOutput().indexOf("Failed to create service context") != -1
 // Test that startup can succeed with a good file.
 conn = MongoRunner.runMongod({timeZoneInfo: "jstests/libs/config_files/good_timezone_info"});
 assert.neq(conn, null, "expected launching mongod with good timezone rules to succeed");
+
+// Test that can use file-provided timezones in an expression.
+const testDB = conn.getDB("test");
+const coll = testDB.parse_zone_info;
+assert.commandWorked(coll.insert({x: new Date()}));
+assert.doesNotThrow(
+    () => coll.aggregate([{$set: {x_parts: {$dateToParts: {date: "$x", timezone: "GMT"}}}}]));
+assert.doesNotThrow(
+    () => coll.aggregate(
+        [{$set: {x_parts: {$dateToParts: {date: "$x", timezone: "America/Sao_Paulo"}}}}]));
+
+// Confirm that attempt to use a non-existent timezone in an expression fails.
+const err = assert.throws(
+    () => coll.aggregate([{$set: {x_parts: {$dateToParts: {date: "$x", timezone: "Unknown"}}}}]));
+assert.eq(err.code, 40485);
 MongoRunner.stopMongod(conn);
 }());
