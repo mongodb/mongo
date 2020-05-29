445a62507a009c093385b1ddceb1ea33a107e006
Benety Goh <benety@mongodb.com>
2020-05-29 11:19:22 -0400

    SERVER-48407 Blacklist create_index_background_unique.js from running on inMemory variant

--- jstests/concurrency/fsm_workloads/create_index_background_unique.js
+++ jstests/concurrency/fsm_workloads/create_index_background_unique.js
@@ -5,7 +5,14 @@
  *
  * Creates multiple unique background indexes in parallel.
  *
- * @tags: [creates_background_indexes]
+ * Marked as 'requires_persistence' to prevent the test from running on 'inMemory' variant, because
+ * the test generates a large oplog and 'inMemory' instances have limited resources to accommodate
+ * all nodes in the replica set (which all run in the same instance), so it may fail with the OOM
+ * error.
+ * @tags: [
+ *     creates_background_indexes,
+ *     requires_persistence,
+ * ]
  */
 load("jstests/concurrency/fsm_workload_helpers/assert_handle_fail_in_transaction.js");
 var $config = (function() {
