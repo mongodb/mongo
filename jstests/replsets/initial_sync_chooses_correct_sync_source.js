311febb021dcb7a710d9e063a8d80a99be71da16
Matthew Russotto <matthew.russotto@10gen.com>
2020-06-09 11:06:46 -0400

    SERVER-48591 Remove 'allowChaining' parameter from test files.

--- jstests/replsets/initial_sync_chooses_correct_sync_source.js
+++ jstests/replsets/initial_sync_chooses_correct_sync_source.js
@@ -13,8 +13,8 @@ load("jstests/libs/fail_point_util.js");
 
 const delayMillis = 50;  // Adds a delay long enough to make a node not the "nearest" sync source.
 const testName = "initial_sync_chooses_correct_sync_source";
-const rst = new ReplSetTest(
-    {name: testName, nodes: [{}, {rsConfig: {priority: 0}}], allowChaining: true, useBridge: true});
+const rst =
+    new ReplSetTest({name: testName, nodes: [{}, {rsConfig: {priority: 0}}], useBridge: true});
 const nodes = rst.startSet();
 rst.initiate();
 
