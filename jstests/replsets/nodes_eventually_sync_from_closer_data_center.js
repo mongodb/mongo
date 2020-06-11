b491225f92290b2d593e8b5f2411f1fe9f93088c
XueruiFa <xuerui.fa@mongodb.com>
2020-06-11 14:57:36 +0000

    SERVER-47457: Test that a node syncing from a far away data center will eventually sync from a closer data center

--- jstests/replsets/nodes_eventually_sync_from_closer_data_center.js
+++ jstests/replsets/nodes_eventually_sync_from_closer_data_center.js
@@ -1,9 +1,9 @@
-/*
- * Tests that nodes in the same region as the primary eventually do not sync across data centers. We
- * do this with a three-node replica set (P, S1, and S2). P and S1 are in the same data center,
- * while S2 is in its own data center. Initially, S1 syncs from S2, and S2 syncs from P. We verify
- * that eventually S1 will decide to sync from P, because it is in the same datacenter as P and thus
- * has a lower ping time.
+/**
+ * Tests that nodes that are syncing from a node in a far away data center will eventually choose to
+ * sync from a node in a closer data center. We do this with a three node replica set (P, S1, and
+ * S2). We create a data center for each node and configure delays such that P's data center is
+ * significantly farther away from S2 than S1's data center. Finally, we verify that S2 will decide
+ * to sync from S1, since S1's data center is closer.
  *
  * @tags: [requires_fcv_46]
  */
@@ -39,8 +39,8 @@ rst.initiateWithHighElectionTimeout();
 rst.awaitReplication();
 
 const primary = rst.getPrimary();
-const testNode = rst.getSecondaries()[0];
-const secondary = rst.getSecondaries()[1];
+const centralSecondary = rst.getSecondaries()[0];
+const testNode = rst.getSecondaries()[1];
 
 const primaryDB = primary.getDB(name);
 const primaryColl = primaryDB["testColl"];
@@ -55,13 +55,19 @@ let serverStatus = assert.commandWorked(testNode.adminCommand({serverStatus: 1})
 assert.eq(0, serverStatus.syncSource.numSyncSourceChangesDueToSignificantlyCloserNode);
 
 jsTestLog("Forcing sync sources for the secondaries");
-const secondaryForceSyncSource = forceSyncSource(rst, secondary, primary);
-const testNodeForceSyncSource = forceSyncSource(rst, testNode, secondary);
+forceSyncSource(rst, centralSecondary, primary);
+// Force the test node to sync from the primary in the far away data center.
+const testNodeForceSyncSource = forceSyncSource(rst, testNode, primary);
 
-// Partition the nodes so that 'testNode' is in the same data center as the primary,
-const westDC = new DataCenter("westDC", [primary, testNode]);
-const eastDC = new DataCenter("eastDC", [secondary]);
-delayMessagesBetweenDataCenters(westDC, eastDC, 50 /* delayMillis */);
+const westDC = new DataCenter("west", [primary]);
+const centralDC = new DataCenter("centralDC", [centralSecondary]);
+const eastDC = new DataCenter("east", [testNode]);
+
+// Set the delay for adjacent data centers to 50 ms. Since the west data center and east data center
+// are further apart, we delay messages between them by 300 ms.
+delayMessagesBetweenDataCenters(westDC, centralDC, 50 /* delayMillis */);
+delayMessagesBetweenDataCenters(centralDC, eastDC, 50 /* delayMillis */);
+delayMessagesBetweenDataCenters(westDC, eastDC, 300 /* delayMillis */);
 
 // Hang 'testNode' in the oplog fetcher to ensure that sync source candidates are ahead of us.
 const hangOplogFetcherBeforeAdvancingLastFetched =
@@ -71,33 +77,30 @@ const hangOplogFetcherBeforeAdvancingLastFetched =
 assert.commandWorked(primaryColl.insert({"make": "batch"}, {writeConcern: {w: 2}}));
 
 hangOplogFetcherBeforeAdvancingLastFetched.wait();
-
-secondaryForceSyncSource.off();
 testNodeForceSyncSource.off();
 
 const advancedTimestamp =
     assert.commandWorked(primaryColl.runCommand("insert", {documents: [{"advance": "timestamp"}]}))
         .operationTime;
 jsTestLog(
-    `Waiting for 'testNode' to receive heartbeats. The primary should have advanced its optime to ${
+    `Waiting for 'testNode' to receive heartbeats. The target sync source should have advanced its optime to ${
         advancedTimestamp}`);
 assert.soon(() => {
     const replSetGetStatus = assert.commandWorked(testNode.adminCommand({replSetGetStatus: 1}));
 
-    // Wait for a heartbeat from the primary that shows that the primary's last timestamp is at
-    // least 'advancedTimestamp'. This ensures the test node sees that the primary is ahead of
-    // itself, and as a result, it can choose the primary as a sync source.
-    const primaryTimestamp = replSetGetStatus.members[0].optime.ts;
-    const receivedPrimaryHb = (bsonWoCompare(primaryTimestamp, advancedTimestamp) >= 0);
+    // Wait for a heartbeat from the target sync source that shows that the target sync source's
+    // last timestamp is at least 'advancedTimestamp'. This ensures the test node sees that the
+    // target sync source is ahead of itself, and as a result, it can decide to sync from the target
+    // sync source.
+    const centralTimestamp = replSetGetStatus.members[1].optime.ts;
+    const receivedCentralHb = (bsonWoCompare(centralTimestamp, advancedTimestamp) >= 0);
 
     // Wait for enough heartbeats from the test node's current sync source so that our understanding
-    // of the ping time is over 10 ms. This makes it likely to re-evaluate the sync source, since
-    // the latency is most likely greater than 'changeSyncSourceThresholdMillis' over the primary's
-    // ping time.
-    const syncSourcePingTime = replSetGetStatus.members[2].pingMs;
-    const receivedSyncSourceHb = (syncSourcePingTime > 10);
+    // of the ping time is over 60 ms. This makes it likely to re-evaluate the sync source.
+    const syncSourcePingTime = replSetGetStatus.members[0].pingMs;
+    const receivedSyncSourceHb = (syncSourcePingTime > 60);
 
-    return (receivedPrimaryHb && receivedSyncSourceHb);
+    return (receivedCentralHb && receivedSyncSourceHb);
 });
 
 const replSetGetStatus = assert.commandWorked(testNode.adminCommand({replSetGetStatus: 1}));
@@ -105,8 +108,8 @@ jsTestLog(replSetGetStatus);
 
 hangOplogFetcherBeforeAdvancingLastFetched.off();
 
-jsTestLog("Verifying that the node eventually syncs from the primary");
-rst.awaitSyncSource(testNode, primary);
+jsTestLog("Verifying that the node eventually syncs from centralSecondary");
+rst.awaitSyncSource(testNode, centralSecondary);
 
 // Verify that the metric was incremented correctly.
 serverStatus = assert.commandWorked(testNode.adminCommand({serverStatus: 1})).metrics.repl;
