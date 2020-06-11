e52733a000b23cde7dcd2b2fc765a8a1f1012165
Matthew Russotto <matthew.russotto@10gen.com>
2020-06-11 10:39:12 -0400

    SERVER-48695 setAppliedThrough must set orderedCommit=false on the recovery unit

--- src/mongo/db/repl/storage_interface_impl.cpp
+++ src/mongo/db/repl/storage_interface_impl.cpp
@@ -898,6 +898,7 @@ Status _updateWithQuery(OperationContext* opCtx,
         WriteUnitOfWork wuow(opCtx);
         if (!ts.isNull()) {
             uassertStatusOK(opCtx->recoveryUnit()->setTimestamp(ts));
+            opCtx->recoveryUnit()->setOrderedCommit(false);
         }
 
         auto planExecutorResult = mongo::getExecutorUpdate(
