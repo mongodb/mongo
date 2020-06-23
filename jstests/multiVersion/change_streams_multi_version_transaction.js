14c48c81acb35168931473979ef6437f12b43e80
Jenny Peshansky <jenny.peshansky@mongodb.com>
2020-06-23 19:59:27 +0000

    SERVER-48568 Changed nopeOptions to nodeOptions when configuring the replica set
    (cherry picked from commit 9ebadf67aa20ded58dbda3db71ca3165007729e9)
    (cherry picked from commit 27f5c1ee9f513f29fe30b8ebefed99581428c6e1)

--- jstests/multiVersion/change_streams_multi_version_transaction.js
+++ jstests/multiVersion/change_streams_multi_version_transaction.js
@@ -12,7 +12,7 @@ load("jstests/replsets/rslib.js");              // For startSetIfSupportsReadMaj
 
 const rst = new ReplSetTest({
     nodes: 2,
-    nopeOptions: {binVersion: "last-stable"},
+    nodeOptions: {binVersion: "last-stable"},
 });
 
 if (!startSetIfSupportsReadMajority(rst)) {
