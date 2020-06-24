2b01612eceb90dee3681e86ec2cea90229ed5fe3
Hirday Gupta <hirday.gupta@mongodb.com>
2020-06-24 16:49:07 +0000

    SERVER-48244 remove shell-side explain string validation
    (cherry picked from commit c76a5544eb4423c7dbca6862fddf9371ce999f6f)

--- jstests/core/explain_find.js
+++ jstests/core/explain_find.js
@@ -1,32 +1,48 @@
 // Tests for explaining find through the explain command.
 
-var collName = "jstests_explain_find";
-var t = db[collName];
-t.drop();
+(function() {
+    "use strict";
 
-t.ensureIndex({a: 1});
+    var collName = "jstests_explain_find";
+    var t = db[collName];
+    t.drop();
 
-for (var i = 0; i < 10; i++) {
-    t.insert({_id: i, a: i});
-}
+    t.ensureIndex({a: 1});
 
-var explain =
-    db.runCommand({explain: {find: collName, filter: {a: {$lte: 2}}}, verbosity: "executionStats"});
-printjson(explain);
-assert.commandWorked(explain);
-assert.eq(3, explain.executionStats.nReturned);
+    for (var i = 0; i < 10; i++) {
+        t.insert({_id: i, a: i});
+    }
 
-explain = db.runCommand(
-    {explain: {find: collName, min: {a: 4}, max: {a: 6}}, verbosity: "executionStats"});
-printjson(explain);
-assert.commandWorked(explain);
-assert.eq(2, explain.executionStats.nReturned);
+    var explain = db.runCommand(
+        {explain: {find: collName, filter: {a: {$lte: 2}}}, verbosity: "executionStats"});
+    printjson(explain);
+    assert.commandWorked(explain);
+    assert.eq(3, explain.executionStats.nReturned);
 
-// Compatibility test for the $explain OP_QUERY flag. This can only run if find command is disabled.
-if (!db.getMongo().useReadCommands()) {
-    var explain = t.find({$query: {a: 4}, $explain: true}).limit(-1).next();
-    assert("queryPlanner" in explain);
-    assert("executionStats" in explain);
-    assert.eq(1, explain.executionStats.nReturned);
-    assert("allPlansExecution" in explain.executionStats);
-}
+    explain = db.runCommand(
+        {explain: {find: collName, min: {a: 4}, max: {a: 6}}, verbosity: "executionStats"});
+    printjson(explain);
+    assert.commandWorked(explain);
+    assert.eq(2, explain.executionStats.nReturned);
+
+    // Compatibility test for the $explain OP_QUERY flag. This can only run if find command is
+    // disabled.
+    if (!db.getMongo().useReadCommands()) {
+        var explain = t.find({$query: {a: 4}, $explain: true}).limit(-1).next();
+        assert("queryPlanner" in explain);
+        assert("executionStats" in explain);
+        assert.eq(1, explain.executionStats.nReturned);
+        assert("allPlansExecution" in explain.executionStats);
+    }
+
+    // Invalid verbosity string.
+    let error = assert.throws(function() {
+        t.explain("foobar").find().finish();
+    });
+    assert.commandFailedWithCode(error, ErrorCodes.FailedToParse);
+
+    error = assert.throws(function() {
+        t.find().explain("foobar");
+    });
+    assert.commandFailedWithCode(error, ErrorCodes.FailedToParse);
+}());
