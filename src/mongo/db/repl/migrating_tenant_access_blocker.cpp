41c8ff7850cc56480827f57811c4b45555f3fdf2
Esha Maharishi <esha.maharishi@mongodb.com>
2020-06-24 10:47:04 -0400

    SERVER-48938 Create MigratingTenantAccessBlocker class

---
+++ src/mongo/db/repl/migrating_tenant_access_blocker.cpp
@@ -0,0 +1,239 @@
+/**
+ *    Copyright (C) 2020-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/platform/basic.h"
+
+#include "mongo/db/client.h"
+#include "mongo/db/repl/migrating_tenant_access_blocker.h"
+#include "mongo/db/repl/replication_coordinator.h"
+
+namespace mongo {
+
+MigratingTenantAccessBlocker::MigratingTenantAccessBlocker(ServiceContext* serviceContext,
+                                                           executor::TaskExecutor* executor)
+    : _serviceContext(serviceContext), _executor(executor) {}
+
+void MigratingTenantAccessBlocker::checkIfCanWriteOrThrow() {
+    stdx::lock_guard<Latch> lg(_mutex);
+
+    switch (_access) {
+        case Access::kAllow:
+            return;
+        case Access::kBlockWrites:
+        case Access::kBlockWritesAndReads:
+            uasserted(ErrorCodes::TenantMigrationConflict,
+                      "Write must block until this tenant migration commits or aborts");
+        case Access::kReject:
+            uasserted(ErrorCodes::TenantMigrationCommitted,
+                      "Write must be re-routed to the new owner of this tenant");
+        default:
+            MONGO_UNREACHABLE;
+    }
+}
+
+void MigratingTenantAccessBlocker::checkIfCanWriteOrBlock(OperationContext* opCtx) {
+    stdx::unique_lock<Latch> ul(_mutex);
+
+    opCtx->waitForConditionOrInterrupt(_transitionOutOfBlockingCV, ul, [&]() {
+        return _access == Access::kAllow || _access == Access::kReject;
+    });
+
+    uassert(ErrorCodes::TenantMigrationCommitted,
+            "Write must be re-routed to the new owner of this database",
+            _access == Access::kAllow);
+}
+
+void MigratingTenantAccessBlocker::checkIfCanReadOrBlock(OperationContext* opCtx,
+                                                         const Timestamp& readTimestamp) {
+    stdx::unique_lock<Latch> ul(_mutex);
+
+    auto canRead = [&]() {
+        return _access == Access::kAllow || _access == Access::kBlockWrites ||
+            readTimestamp < *_blockTimestamp;
+    };
+
+    opCtx->waitForConditionOrInterrupt(
+        _transitionOutOfBlockingCV, ul, [&]() { return canRead() || _access == Access::kReject; });
+
+    uassert(ErrorCodes::TenantMigrationCommitted,
+            "Read must be re-routed to the new owner of this database",
+            canRead());
+}
+
+void MigratingTenantAccessBlocker::startBlockingWrites() {
+    stdx::lock_guard<Latch> lg(_mutex);
+
+    invariant(_access == Access::kAllow);
+    invariant(!_blockTimestamp);
+    invariant(!_commitOrAbortOpTime);
+    invariant(!_waitForCommitOrAbortToMajorityCommitOpCtx);
+
+    _access = Access::kBlockWrites;
+}
+
+void MigratingTenantAccessBlocker::startBlockingReadsAfter(const Timestamp& blockTimestamp) {
+    stdx::lock_guard<Latch> lg(_mutex);
+
+    invariant(_access == Access::kBlockWrites);
+    invariant(!_blockTimestamp);
+    invariant(!_commitOrAbortOpTime);
+    invariant(!_waitForCommitOrAbortToMajorityCommitOpCtx);
+
+    _access = Access::kBlockWritesAndReads;
+    _blockTimestamp = blockTimestamp;
+}
+
+void MigratingTenantAccessBlocker::rollBackStartBlocking() {
+    stdx::lock_guard<Latch> lg(_mutex);
+
+    invariant(_access == Access::kBlockWrites || _access == Access::kBlockWritesAndReads);
+    invariant(!_commitOrAbortOpTime);
+    invariant(!_waitForCommitOrAbortToMajorityCommitOpCtx);
+
+    _access = Access::kAllow;
+    _blockTimestamp.reset();
+    _transitionOutOfBlockingCV.notify_all();
+}
+
+void MigratingTenantAccessBlocker::commit(repl::OpTime commitOpTime) {
+    stdx::lock_guard<Latch> lg(_mutex);
+
+    invariant(_access == Access::kBlockWritesAndReads);
+    invariant(_blockTimestamp);
+    invariant(!_commitOrAbortOpTime);
+    invariant(!_waitForCommitOrAbortToMajorityCommitOpCtx);
+
+    _commitOrAbortOpTime = commitOpTime;
+
+    const auto callbackFn = [this, commitOpTime]() {
+        stdx::lock_guard<Latch> lg(_mutex);
+
+        invariant(_access == Access::kBlockWritesAndReads);
+        invariant(_blockTimestamp);
+        invariant(_commitOrAbortOpTime == commitOpTime);
+        invariant(_waitForCommitOrAbortToMajorityCommitOpCtx);
+
+        _access = Access::kReject;
+        _transitionOutOfBlockingCV.notify_all();
+    };
+
+    _waitForOpTimeToMajorityCommit(commitOpTime, callbackFn);
+}
+
+void MigratingTenantAccessBlocker::abort(repl::OpTime abortOpTime) {
+    stdx::lock_guard<Latch> lg(_mutex);
+
+    invariant(_access == Access::kBlockWritesAndReads);
+    invariant(_blockTimestamp);
+    invariant(!_commitOrAbortOpTime);
+    invariant(!_waitForCommitOrAbortToMajorityCommitOpCtx);
+
+    _commitOrAbortOpTime = abortOpTime;
+
+    const auto callbackFn = [this, abortOpTime]() {
+        stdx::lock_guard<Latch> lg(_mutex);
+
+        invariant(_access == Access::kBlockWritesAndReads);
+        invariant(_blockTimestamp);
+        invariant(_commitOrAbortOpTime == abortOpTime);
+        invariant(_waitForCommitOrAbortToMajorityCommitOpCtx);
+
+        _access = Access::kAllow;
+        _blockTimestamp.reset();
+        _commitOrAbortOpTime.reset();
+        _waitForCommitOrAbortToMajorityCommitOpCtx = nullptr;
+        _transitionOutOfBlockingCV.notify_all();
+    };
+
+    _waitForOpTimeToMajorityCommit(abortOpTime, callbackFn);
+}
+
+void MigratingTenantAccessBlocker::rollBackCommitOrAbort() {
+    stdx::lock_guard<Latch> lg(_mutex);
+
+    invariant(_access == Access::kBlockWritesAndReads);
+    invariant(_blockTimestamp);
+    invariant(_commitOrAbortOpTime);
+
+    _commitOrAbortOpTime.reset();
+    if (_waitForCommitOrAbortToMajorityCommitOpCtx) {
+        stdx::lock_guard<Client> lk(*_waitForCommitOrAbortToMajorityCommitOpCtx->getClient());
+        _waitForCommitOrAbortToMajorityCommitOpCtx->markKilled();
+    }
+    _waitForCommitOrAbortToMajorityCommitOpCtx = nullptr;
+}
+
+void MigratingTenantAccessBlocker::_waitForOpTimeToMajorityCommit(
+    repl::OpTime opTime, std::function<void()> callbackFn) {
+    uassertStatusOK(
+        _executor->scheduleWork([&](const executor::TaskExecutor::CallbackArgs& cbData) {
+            if (!cbData.status.isOK()) {
+                return;
+            }
+
+            // Keep waiting for 'opTime' to become majority committed until the node is shutting
+            // down or 'opTime' in particular has rolled back.
+            Status status(ErrorCodes::InternalError, "Not Set");
+            while (!status.isOK()) {
+                if (ErrorCodes::isA<ErrorCategory::ShutdownError>(status.code())) {
+                    return;
+                }
+
+                ThreadClient tc("MigratingTenantAccessBlocker::commit", _serviceContext);
+                const auto opCtxHolder = tc->makeOperationContext();
+                const auto opCtx = opCtxHolder.get();
+
+                {
+                    stdx::lock_guard<Latch> lg(_mutex);
+
+                    // Check if 'opTime' has rolled back.
+                    if (!_commitOrAbortOpTime || _commitOrAbortOpTime != opTime) {
+                        return;
+                    }
+
+                    // Save 'opCtx' so that if 'opTime' rolls back after this point, 'opCtx' will be
+                    // interrupted and 'waitUntilSnapshotCommitted' will return an interrupt error.
+                    _waitForCommitOrAbortToMajorityCommitOpCtx = opCtx;
+                }
+
+                try {
+                    repl::ReplicationCoordinator::get(opCtx)->waitUntilSnapshotCommitted(
+                        opCtx, opTime.getTimestamp());
+                    status = Status::OK();
+                } catch (const DBException& ex) {
+                    status = ex.toStatus();
+                }
+            }
+
+            // 'opTime' became majority committed.
+            callbackFn();
+        }));
+}
+
+}  // namespace mongo
---
+++ src/mongo/db/repl/migrating_tenant_access_blocker.h
@@ -0,0 +1,156 @@
+/**
+ *    Copyright (C) 2020-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#pragma once
+
+#include <boost/optional.hpp>
+
+#include "mongo/bson/timestamp.h"
+#include "mongo/db/operation_context.h"
+#include "mongo/db/repl/optime.h"
+#include "mongo/executor/task_executor.h"
+
+namespace mongo {
+
+/**
+ * The MigratingTenantAccessBlocker is used to block and eventually reject reads and writes to a
+ * database while the Atlas Serverless tenant that owns the database is being migrated from this
+ * replica set to another replica set.
+ *
+ * In order to preserve causal consistency across the migration, this replica set, the "donor",
+ * blocks writes and reads as of a particular "blockTimestamp". The donor then advances the
+ * recipient's clusterTime to "blockTimestamp" before committing the migration.
+ *
+ * Client writes are run inside a new loop, similar to writeConflictRetry:
+ *
+ * template <typename F>
+ * auto migrationConflictRetry(OperationContext* opCtx, const Database* db, F&& f) {
+ *     while (true) {
+ *         try {
+ *             return f();
+ *         } catch (const MigrationConflictException&) {
+ *             MigratingTenantAccessBlocker::get(db).checkIfCanWriteOrBlock(opCtx);
+ *         }
+ *     }
+ * }
+ *
+ * Writes call checkIfCanWriteOrThrow after being assigned an OpTime but before committing. The
+ * method throws MigratingTenantConflict if writes are being blocked, which is caught in the loop.
+ * The write then blocks until the migration either commits (in which case checkIfCanWriteOrBlock
+ * throws an error that causes the write to be rejected) or aborts (in which case
+ * checkIfCanWriteOrBlock returns successfully and the write is retried in the loop). This loop is
+ * used because writes must not block after being assigned an OpTime but before committing.
+ *
+ * Reads with afterClusterTime or atClusterTime call checkIfCanReadOrBlock at some point after
+ * waiting for readConcern, that is, after waiting to reach their clusterTime, which includes
+ * waiting for all earlier oplog holes to be filled.
+ *
+ * Given this, the donor uses this class's API in the following way:
+ *
+ * 1. The donor primary creates a WriteUnitOfWork to do a write, call it the "start blocking" write.
+ * The donor primary calls startBlockingWrites before the write is assigned an OpTime. This write's
+ * Timestamp will be the "blockTimestamp".
+ *
+ * At this point:
+ * - Writes that have already passed checkIfCanWriteOrThrow must have been assigned an OpTime before
+ *   the blockTimestamp, since the blockTimestamp hasn't been assigned yet, and OpTimes are handed
+ *   out in monotonically increasing order.
+ * - Writes that have not yet passed checkIfCanWriteOrThrow will end up blocking. Some of these
+ *   writes may have already been assigned an OpTime, or may end up being assigned an OpTime that is
+ *   before the blockTimestamp, and so will end up blocking unnecessarily, but not incorrectly.
+ *
+ * 2. In the op observer after the "start blocking" write's OpTime is set, primaries and secondaries
+ * of the donor replica set call startBlockingReadsAfter with the write's Timestamp as
+ * "blockTimestamp".
+ *
+ * At this point:
+ * - Reads on the node that have already passed checkIfCanReadOrBlock must have a clusterTime before
+ *   the blockTimestamp, since the write at blockTimestamp hasn't committed yet (i.e., there's still
+ *   an oplog hole at blockTimestamp).
+ * - Reads on the node that have not yet passed checkIfCanReadOrBlock will end up blocking.
+ *
+ * If the "start blocking" write aborts or the write rolls back via replication rollback, the node
+ * calls rollBackStartBlocking.
+ *
+ * 4a. The donor primary commits the migration by doing another write, call it the "commit" write.
+ * The op observer for the "commit" write on primaries and secondaries calls commit, which
+ * asynchronously waits for the "commit" write's OpTime to become majority committed, then
+ * transitions the class to reject writes and reads.
+ *
+ * 4b. The donor primary can instead abort the migration by doing a write, call it the "abort"
+ * write. The op observer for the "abort" write on primaries and secondaries calls abort, which
+ * asynchronously waits for the "abort" write's OpTime to become majority committed, then
+ * transitions the class back to allowing reads and writes.
+ *
+ * If the "commit" or "abort" write aborts or rolls back via replication rollback, the node calls
+ * rollBackCommitOrAbort, which cancels the asynchronous task.
+ */
+class MigratingTenantAccessBlocker {
+public:
+    MigratingTenantAccessBlocker(ServiceContext* serviceContext, executor::TaskExecutor* executor);
+
+    //
+    // Called by all writes and reads against the database.
+    //
+
+    void checkIfCanWriteOrThrow();
+    void checkIfCanWriteOrBlock(OperationContext* opCtx);
+    void checkIfCanReadOrBlock(OperationContext* opCtx, const Timestamp& readTimestamp);
+
+    //
+    // Called while donating this database.
+    //
+
+    void startBlockingWrites();
+    void startBlockingReadsAfter(const Timestamp& timestamp);
+    void rollBackStartBlocking();
+
+    void commit(repl::OpTime opTime);
+    void abort(repl::OpTime opTime);
+    void rollBackCommitOrAbort();
+
+private:
+    void _waitForOpTimeToMajorityCommit(repl::OpTime opTime, std::function<void()> callbackFn);
+
+    enum class Access { kAllow, kBlockWrites, kBlockWritesAndReads, kReject };
+
+    ServiceContext* _serviceContext;
+    executor::TaskExecutor* _executor;
+
+    // Protects the state below.
+    mutable Mutex _mutex = MONGO_MAKE_LATCH("MigratingTenantAccessBlocker::_mutex");
+
+    Access _access{Access::kAllow};
+    boost::optional<Timestamp> _blockTimestamp;
+    boost::optional<repl::OpTime> _commitOrAbortOpTime;
+    OperationContext* _waitForCommitOrAbortToMajorityCommitOpCtx{nullptr};
+    stdx::condition_variable _transitionOutOfBlockingCV;
+};
+
+}  // namespace mongo
