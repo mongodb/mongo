6d650280c2d21c24dc2c45d1d41e915f4d244ffa
Eric Cox <eric.cox@mongodb.com>
2020-06-30 21:45:29 +0000

    Revert SERVER-48742 Log changes to profiler settings

--- src/mongo/db/commands/profile_common.cpp
+++ src/mongo/db/commands/profile_common.cpp
@@ -26,16 +26,13 @@
  *    exception statement from all source files in the program, then also delete
  *    it in the license file.
  */
-#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kCommand
 
 #include "mongo/platform/basic.h"
 
 #include "mongo/db/auth/authorization_session.h"
 #include "mongo/db/commands/profile_common.h"
 #include "mongo/db/commands/profile_gen.h"
-#include "mongo/db/jsobj.h"
 #include "mongo/idl/idl_parser.h"
-#include "mongo/logv2/log.h"
 
 namespace mongo {
 
@@ -72,12 +69,10 @@ bool ProfileCmdBase::run(OperationContext* opCtx,
     // Delegate to _applyProfilingLevel to set the profiling level appropriately whether we are on
     // mongoD or mongoS.
     int oldLevel = _applyProfilingLevel(opCtx, dbName, profilingLevel);
-    auto oldSlowMS = serverGlobalParams.slowMS;
-    auto oldSampleRate = serverGlobalParams.sampleRate;
 
     result.append("was", oldLevel);
-    result.append("slowms", oldSlowMS);
-    result.append("sampleRate", oldSampleRate);
+    result.append("slowms", serverGlobalParams.slowMS);
+    result.append("sampleRate", serverGlobalParams.sampleRate);
 
     if (auto slowms = request.getSlowms()) {
         serverGlobalParams.slowMS = *slowms;
@@ -87,27 +82,6 @@ bool ProfileCmdBase::run(OperationContext* opCtx,
                 "'sampleRate' must be between 0.0 and 1.0 inclusive",
                 *sampleRate >= 0.0 && *sampleRate <= 1.0);
         serverGlobalParams.sampleRate = *sampleRate;
-    }
-
-    // Log the change made to server's profiling settings, unless the request was to get the current
-    // value.
-    if (profilingLevel != -1) {
-        logv2::DynamicAttributes attrs;
-
-        BSONObjBuilder oldState;
-        BSONObjBuilder newState;
-
-        oldState.append("level"_sd, oldLevel);
-        oldState.append("slowms"_sd, oldSlowMS);
-        oldState.append("sampleRate"_sd, oldSampleRate);
-        attrs.add("from", oldState.obj());
-
-        newState.append("level"_sd, profilingLevel);
-        newState.append("slowms"_sd, serverGlobalParams.slowMS);
-        newState.append("sampleRate"_sd, serverGlobalParams.sampleRate);
-        attrs.add("to", newState.obj());
-
-        LOGV2(48742, "Profiler settings changed", attrs);
     }
 
     return true;
