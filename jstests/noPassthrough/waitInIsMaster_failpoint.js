9bb04b49efb804ae92d142a1190214070cac841f
Tess Avitabile <tess.avitabile@mongodb.com>
2020-07-02 18:25:15 +0000

    SERVER-49294 waitInIsMaster_failpoint.js should ensure the shell is connected before enabling failpoint
    (cherry picked from commit 9d4f0f264a6e0035abd3d541583a615bc8085a86)

--- jstests/noPassthrough/waitInIsMaster_failpoint.js
+++ jstests/noPassthrough/waitInIsMaster_failpoint.js
@@ -12,7 +12,11 @@ function runTest(conn) {
         assert.gte(isMasterDuration, 100);
     }
 
-    // Use a skip of 1, since the parallel shell runs isMaster when it starts.
+    // Do a find to make sure that the shell has finished running isMaster while establishing its
+    // initial connection.
+    assert.eq(0, conn.getDB("test").c.find().itcount());
+
+    // Use a skip of 1, since the parallel shell runs isMaster/hello when it starts.
     const helloFailpoint = configureFailPoint(conn, "waitInHello", {}, {skip: 1});
     const awaitIsMaster = startParallelShell(runIsMasterCommand, conn.port);
     helloFailpoint.wait();
