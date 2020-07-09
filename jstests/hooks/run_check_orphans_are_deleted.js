102b3dd7fb50c0220148e5272c00c9bfb418d6c3
Haley Connelly <haley.connelly@mongodb.com>
2020-07-09 16:45:32 +0000

    SERVER-47900 Disable checkOrphansDeleted helper in 4.4 multiversion testing

--- jstests/hooks/run_check_orphans_are_deleted.js
+++ jstests/hooks/run_check_orphans_are_deleted.js
@@ -6,6 +6,15 @@
  * Note: This hook won't find documents which don't have the full shard key.
  */
 (function() {
+
+if (jsTest.options().mixedBinVersions) {
+    // The resumable range deleter can only be enabled in FCV 4.4. We skip running this hook when
+    // the cluster is in FCV 4.2 because there's otherwise no guarantee orphan documents are
+    // eventually cleaned up.
+    jsTestLog("CheckOrphansAreDeletedHelpers are disabled for 4.4 multiversion testing.");
+    return;
+}
+
 load('jstests/libs/check_orphans_are_deleted_helpers.js');  // For CheckOrphansAreDeletedHelpers.
 load('jstests/libs/discover_topology.js');                  // For Topology and DiscoverTopology.
 
