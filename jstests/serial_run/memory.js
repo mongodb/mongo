fd109f08f97117237dae3a9428c3d2d66f14e221
Katherine Wu <katherine.wu@mongodb.com>
2020-07-10 14:15:01 -0400

    SERVER-49438 Allow memory.js tests to accept $where timeout interruption

--- jstests/serial_run/memory.js
+++ jstests/serial_run/memory.js
@@ -38,7 +38,8 @@ function assertMemoryError(func) {
     try {
         func();
     } catch (e) {
-        if (e.message.includes("Out of memory")) {
+        if (e.message.includes("Out of memory") ||
+            e.message.includes("JavaScript execution interrupted")) {
             return;
         }
         throw e;
