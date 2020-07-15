ca39dac5d6a8316787141d13bcd05261042f6540
Judah Schvimer <judah@mongodb.com>
2020-07-15 17:23:47 +0000

    SERVER-49924 SERVER-49527 recoverFromOplogAsStandalone does not relax index constraints
    (cherry picked from commit e0cde43310e5dab3fcf6e93bb115259e70a165e8)
    (cherry picked from commit b855c38dc4eaba412df8c195c6e00a569cf47d2a)

--- src/mongo/db/repl/replication_coordinator_impl.cpp
+++ src/mongo/db/repl/replication_coordinator_impl.cpp
@@ -2903,6 +2903,9 @@ bool ReplicationCoordinatorImpl::isInPrimaryOrSecondaryState_UNSAFE() const {
 
 bool ReplicationCoordinatorImpl::shouldRelaxIndexConstraints(OperationContext* opCtx,
                                                              const NamespaceString& ns) {
+    if (ReplSettings::shouldRecoverFromOplogAsStandalone()) {
+        return true;
+    }
     return !canAcceptWritesFor(opCtx, ns);
 }
 
