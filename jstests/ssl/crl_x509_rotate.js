4027b89d97e1e33d975b1c603f08b2aaf6ca3284
Gabriel Marks <gabriel.marks@mongodb.com>
2020-07-20 21:50:29 +0000

    SERVER-49749 Test rotate functionality in various cases

---
+++ jstests/ssl/crl_x509_rotate.js
@@ -0,0 +1,71 @@
+// Check that rotation works for the CRL
+
+(function() {
+"use strict";
+
+load('jstests/ssl/libs/ssl_helpers.js');
+
+if (determineSSLProvider() === "openssl" || determineSSLProvider() === "apple") {
+    return;
+}
+
+const dbPath = MongoRunner.toRealDir("$dataDir/cluster_x509_rotate_test/");
+mkdir(dbPath);
+
+copyCertificateFile("jstests/libs/crl.pem", dbPath + "/crl-test.pem");
+
+const mongod = MongoRunner.runMongod({
+    sslMode: "requireSSL",
+    sslPEMKeyFile: "jstests/libs/server.pem",
+    sslCAFile: "jstests/libs/ca.pem",
+    sslCRLFile: dbPath + "/crl-test.pem"
+});
+
+const host = "localhost:" + mongod.port;
+
+// Make sure that client-revoked can connect at first
+let out = runMongoProgram("mongo",
+                          "--host",
+                          host,
+                          "--ssl",
+                          "--sslPEMKeyFile",
+                          "jstests/libs/client_revoked.pem",
+                          "--sslCAFile",
+                          "jstests/libs/ca.pem",
+                          "--eval",
+                          ";");
+assert.eq(out, 0, "Initial mongo invocation failed");
+
+// Rotate in new CRL
+copyCertificateFile("jstests/libs/crl_client_revoked.pem", dbPath + "/crl-test.pem");
+
+assert.commandWorked(mongod.adminCommand({rotateCertificates: 1}));
+
+// Make sure client-revoked can't connect
+out = runMongoProgram("mongo",
+                      "--host",
+                      host,
+                      "--ssl",
+                      "--sslPEMKeyFile",
+                      "jstests/libs/client_revoked.pem",
+                      "--sslCAFile",
+                      "jstests/libs/ca.pem",
+                      "--eval",
+                      ";");
+assert.neq(out, 0, "Mongo invocation did not fail");
+
+// Make sure client can still connect
+out = runMongoProgram("mongo",
+                      "--host",
+                      host,
+                      "--ssl",
+                      "--sslPEMKeyFile",
+                      "jstests/libs/client.pem",
+                      "--sslCAFile",
+                      "jstests/libs/ca.pem",
+                      "--eval",
+                      ";");
+assert.eq(out, 0, "Mongo invocation failed");
+
+MongoRunner.stopMongod(mongod);
+}());
---
+++ jstests/ssl/server_x509_rotate.js
@@ -0,0 +1,61 @@
+// Check that rotation works for the server certificate
+
+(function() {
+"use strict";
+
+load('jstests/ssl/libs/ssl_helpers.js');
+
+if (determineSSLProvider() === "openssl") {
+    return;
+}
+
+const dbPath = MongoRunner.toRealDir("$dataDir/cluster_x509_rotate_test/");
+mkdir(dbPath);
+
+copyCertificateFile("jstests/libs/ca.pem", dbPath + "/ca-test.pem");
+copyCertificateFile("jstests/libs/server.pem", dbPath + "/server-test.pem");
+
+const mongod = MongoRunner.runMongod({
+    sslMode: "requireSSL",
+    sslPEMKeyFile: dbPath + "/server-test.pem",
+    sslCAFile: dbPath + "/ca-test.pem"
+});
+
+// Rotate in new certificates
+copyCertificateFile("jstests/libs/trusted-ca.pem", dbPath + "/ca-test.pem");
+copyCertificateFile("jstests/libs/trusted-server.pem", dbPath + "/server-test.pem");
+
+assert.commandWorked(mongod.adminCommand({rotateCertificates: 1}));
+// make sure that mongo is still connected after rotation
+assert.commandWorked(mongod.adminCommand({connectionStatus: 1}));
+
+const host = "localhost:" + mongod.port;
+
+// Start shell with old certificates and make sure it can't connect
+let out = runMongoProgram("mongo",
+                          "--host",
+                          host,
+                          "--ssl",
+                          "--sslPEMKeyFile",
+                          "jstests/libs/client.pem",
+                          "--sslCAFile",
+                          "jstests/libs/ca.pem",
+                          "--eval",
+                          ";");
+assert.neq(out, 0, "Mongo invocation did not fail");
+
+// Start shell with new certificates and make sure it can connect
+out = runMongoProgram("mongo",
+                      "--host",
+                      host,
+                      "--ssl",
+                      "--sslPEMKeyFile",
+                      "jstests/libs/trusted-client.pem",
+                      "--sslCAFile",
+                      "jstests/libs/trusted-ca.pem",
+                      "--eval",
+                      ";");
+assert.eq(out, 0, "Mongo invocation failed");
+
+MongoRunner.stopMongod(mongod);
+}());
