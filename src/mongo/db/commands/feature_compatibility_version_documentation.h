bbe2ef4b43290421b406684f204d0ef7100c5d57
Jason Chan <jason.chan@mongodb.com>
2020-07-21 15:32:41 +0000

    SERVER-49733 Parameterize the URLs referenced in feature_compatibility_version_documentation.h

--- src/mongo/db/commands/feature_compatibility_version_documentation.h
+++ src/mongo/db/commands/feature_compatibility_version_documentation.h
@@ -29,11 +29,19 @@
 
 #pragma once
 
+#include <fmt/format.h>
+
+#include "mongo/db/commands/feature_compatibility_version_parser.h"
+
 namespace mongo {
 namespace feature_compatibility_version_documentation {
-constexpr StringData kCompatibilityLink =
-    "https://docs.mongodb.com/master/release-notes/4.4-compatibility/#feature-compatibility"_sd;
-constexpr StringData kUpgradeLink =
-    "https://docs.mongodb.com/master/release-notes/4.4/#upgrade-procedures"_sd;
+using namespace fmt::literals;
+
+const std::string kCompatibilityLink =
+    "https://docs.mongodb.com/master/release-notes/{}-compatibility/#feature-compatibility"_format(
+        FeatureCompatibilityVersionParser::kLastLTS);
+const std::string kUpgradeLink =
+    "https://docs.mongodb.com/master/release-notes/{}/#upgrade-procedures"_format(
+        FeatureCompatibilityVersionParser::kLastLTS);
 }  // namespace feature_compatibility_version_documentation
 }  // namespace mongo
