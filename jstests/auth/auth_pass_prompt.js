e02ce7d7fad15b9d40dd79d6002ddb53fbbe74d3
Gabriel Marks <gabriel.marks@mongodb.com>
2020-08-05 16:26:50 +0000

    SERVER-49495 Alleviate race in auth_pass_prompt.js
    (cherry picked from commit b2c8bcd33f08e2d540a1d43b61628f82980b66b6)

--- jstests/auth/auth_pass_prompt.js
+++ jstests/auth/auth_pass_prompt.js
@@ -20,9 +20,10 @@ if (!_isWindows()) {
     const ret = runProgram(
         binshell, '-c', `echo password | ${mongo} --host ${host} --port ${port} --eval ${auth}`);
 
-    const output = rawMongoProgramOutput();
-    assert.eq(output.includes("Enter password:"), true, output);
-    assert.eq(output.includes("Successful authentication"), true, output);
+    assert.soon(() => {
+        const output = rawMongoProgramOutput();
+        return output.includes("Enter password:") && output.includes("Successful authentication");
+    });
 
     MongoRunner.stopMongod(conn);
 }
