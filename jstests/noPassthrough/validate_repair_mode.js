2f4aecb91daae89c7f8eabe24ba1682ff1088186
Benety Goh <benety@mongodb.com>
2020-08-09 12:33:13 -0400

    SERVER-49687 disable validate_during_repair under windows

--- jstests/noPassthrough/validate_repair_mode.js
+++ jstests/noPassthrough/validate_repair_mode.js
@@ -5,6 +5,12 @@
 
 (function() {
 
+if (_isWindows()) {
+    // TODO(SERVER-50205): Re-enable under Windows.
+    jsTestLog('Skipping test under Windows.');
+    return;
+}
+
 let conn = MongoRunner.runMongod({});
 db = conn.getDB("validate_repair_mode");
 
