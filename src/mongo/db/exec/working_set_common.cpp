9c58a5c69e9f78b7e0b218c798d35421e8de5e34
Milena Ivanova <milena.ivanova@mongodb.com>
2020-08-11 16:30:12 +0000

    SERVER-49718 Log message reporting corruption doesn't mention namespace

--- src/mongo/db/exec/working_set_common.cpp
+++ src/mongo/db/exec/working_set_common.cpp
@@ -99,11 +99,14 @@ bool WorkingSetCommon::fetch(OperationContext* opCtx,
             LOGV2_ERROR_OPTIONS(
                 4615603,
                 {logv2::UserAssertAfterLog(ErrorCodes::DataCorruptionDetected)},
-                "Erroneous index key found with reference to non-existent record id "
-                "{recordId}: "
-                "{indexKeyData}. Consider dropping and then re-creating the index with key "
-                "pattern "
-                "{indexKeyPattern} and then running the validate command on the collection.",
+                "Erroneous index key found in {namespace} with reference to non-existent "
+                "record id {recordId}: {indexKeyData}. Consider dropping and then re-creating "
+                "the index with key pattern {indexKeyPattern} and then running the validate "
+                "command on the collection.",
+                "Erroneous index key found with reference to non-existent record id. Consider "
+                "dropping and then re-creating the index and then running the validate command "
+                "on the collection.",
+                "namespace"_attr = ns,
                 "recordId"_attr = member->recordId,
                 "indexKeyData"_attr = logv2::seqLog(
                     boost::make_transform_iterator(member->keyData.begin(), indexKeyEntryToObjFn),
