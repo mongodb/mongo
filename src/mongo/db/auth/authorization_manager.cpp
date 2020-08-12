d8c7c2c514fb8a44b7b2a731105582b66ed7b253
Mark Benvenuto <mark.benvenuto@mongodb.com>
2020-08-12 15:02:29 -0400

    SERVER-50180 Fix User lifetime management in AuthorizationManager::acquireUserForSessionRefresh
    (cherry picked from commit 0a1b7e768ca29e2cb232c1af70beb55485fc96cb)

--- src/mongo/db/auth/authorization_manager.cpp
+++ src/mongo/db/auth/authorization_manager.cpp
@@ -595,6 +595,7 @@ Status AuthorizationManager::acquireUserForSessionRefresh(OperationContext* opCt
     }
 
     if (uid != (*user)->getID()) {
+        releaseUser(*user);
         *user = nullptr;
         return {ErrorCodes::UserNotFound,
                 str::stream() << "User id from privilege document '" << userName.toString()
