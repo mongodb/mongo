4a95b7ebc88499c2b3b9aaa11fabbed895f40f18
Bernard Gorman <bernard.gorman@gmail.com>
2020-08-21 11:25:52 +0100

    SERVER-50402 Expose original assertion in change_streams_unsharded_update_resume.js

--- jstests/sharding/change_streams_unsharded_update_resume.js
+++ jstests/sharding/change_streams_unsharded_update_resume.js
@@ -56,7 +56,7 @@ assert.soon(() => {
         assert.eq(csCur.hasNext(), false, () => tojson(csCur.next()));
         return false;
     } catch (ex) {
-        assert.eq(ex.code, ErrorCodes.ChangeStreamFatalError);
+        assert.eq(ex.code, ErrorCodes.ChangeStreamFatalError, () => tojson({originalError: ex}));
         return true;
     }
 });
