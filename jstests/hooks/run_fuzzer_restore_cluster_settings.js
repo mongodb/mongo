c132aec751112ac8f4507ae6b694477b19badc53
A. Jesse Jiryu Davis <jesse@mongodb.com>
2020-08-25 10:20:38 -0400

    SERVER-50533 Unset "requireApiVersion" after fuzz tests

--- jstests/hooks/run_fuzzer_restore_cluster_settings.js
+++ jstests/hooks/run_fuzzer_restore_cluster_settings.js
@@ -1,6 +1,8 @@
 (function() {
 'use strict';
 
+assert.commandWorked(db.adminCommand({setParameter: 1, requireApiVersion: false, apiVersion: "1"}));
+
 // Unsetting read/write settings. This command will also cause the server to refresh and get
 // the new settings.
 assert.commandWorked(db.adminCommand({
