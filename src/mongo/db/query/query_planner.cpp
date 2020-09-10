b39aecbdf43f463832739d7415d59958619b8cff
Charlie Swanson <charlie.swanson@mongodb.com>
2020-09-10 18:17:42 -0400

    SERVER-50764 Fix bad merge conflict - add break;

--- src/mongo/db/query/query_planner.cpp
+++ src/mongo/db/query/query_planner.cpp
@@ -175,6 +175,7 @@ string optionString(size_t options) {
                 break;
             case QueryPlannerParams::ASSERT_MIN_TS_HAS_NOT_FALLEN_OFF_OPLOG:
                 ss << "ASSERT_MIN_TS_HAS_NOT_FALLEN_OFF_OPLOG ";
+                break;
             case QueryPlannerParams::ENUMERATE_OR_CHILDREN_LOCKSTEP:
                 ss << "ENUMERATE_OR_CHILDREN_LOCKSTEP ";
                 break;
