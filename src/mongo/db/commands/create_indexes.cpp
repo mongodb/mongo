8a70f5128af22e6f9da80e47a9484ea50332d064
Benety Goh <benety@mongodb.com>
2020-09-15 20:00:04 -0400

    Revert "SERVER-48428 do not acquire IX locks in createIndexes if not doing any writes; this will avoid it being killed by stepDown"
    This reverts commit 81d5259fb84897191e01114f623050c29b760523.

--- src/mongo/db/commands/create_indexes.cpp
+++ src/mongo/db/commands/create_indexes.cpp
@@ -541,7 +541,7 @@ bool runCreateIndexesWithCoordinator(OperationContext* opCtx,
     // 3) Check if we can create the index without handing control to the IndexBuildsCoordinator.
     OptionalCollectionUUID collectionUUID;
     {
-        Lock::DBLock dbLock(opCtx, ns.db(), MODE_IS);
+        Lock::DBLock dbLock(opCtx, ns.db(), MODE_IX);
         checkDatabaseShardingState(opCtx, ns);
         if (!repl::ReplicationCoordinator::get(opCtx)->canAcceptWritesFor(opCtx, ns)) {
             uasserted(ErrorCodes::NotMaster,
@@ -549,14 +549,12 @@ bool runCreateIndexesWithCoordinator(OperationContext* opCtx,
         }
 
         bool indexExists = writeConflictRetry(opCtx, "createCollectionWithIndexes", ns.ns(), [&] {
-            AutoGetCollection autoColl(opCtx, ns, MODE_IS);
+            AutoGetCollection autoColl(opCtx, ns, MODE_IX);
             auto collection = autoColl.getCollection();
 
             // Before potentially taking an exclusive collection lock, check if all indexes already
             // exist while holding an intent lock.
             if (collection && indexesAlreadyExist(opCtx, collection, specs, &result)) {
-                repl::ReplClientInfo::forClient(opCtx->getClient())
-                    .setLastOpToSystemLastOpTime(opCtx);
                 return true;
             }
 
