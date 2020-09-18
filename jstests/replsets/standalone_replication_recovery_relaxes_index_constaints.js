7f3ba55f33669e7540a5c42db5200ad06d6b8477
Pavi Vetriselvan <pavithra.vetriselvan@mongodb.com>
2020-09-18 11:48:43 -0400

    SERVER-51047 standalone_replication_recovery_relaxes_index_constaints.js should use multiversion_incompatible tag

--- jstests/replsets/standalone_replication_recovery_relaxes_index_constaints.js
+++ jstests/replsets/standalone_replication_recovery_relaxes_index_constaints.js
@@ -5,7 +5,9 @@
  *
  * This test only makes sense for storage engines that support recover to stable timestamp.
  * @tags: [requires_wiredtiger, requires_persistence, requires_journaling, requires_replication,
- * requires_majority_read_concern]
+ * requires_majority_read_concern,
+ * # Restarting as a standalone is not supported in multiversion tests.
+ * multiversion_incompatible]
  */
 
 (function() {
