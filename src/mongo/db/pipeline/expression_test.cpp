948e065e29ea96d6d248c50f4cfa8fb71cbcec55
Arun Banala <arun.banala@mongodb.com>
2020-10-21 17:39:22 +0100

    SERVER-50445 Return the value in double when NumberLong subtraction overflows in ExpressionSubtract
    (cherry picked from commit 3518bd82e49b6941ee7a2f3a868df40114b0d8fc)
    (cherry picked from commit 309b631dd16e90e1f3fb8bf3567df1fedc92d715)

--- src/mongo/db/pipeline/expression_test.cpp
+++ src/mongo/db/pipeline/expression_test.cpp
@@ -5948,4 +5948,37 @@ public:
 
 SuiteInstance<All> myall;
 
+TEST(ExpressionSubtractTest, OverflowLong) {
+    const auto maxLong = std::numeric_limits<long long int>::max();
+    const auto minLong = std::numeric_limits<long long int>::min();
+    intrusive_ptr<ExpressionContextForTest> expCtx(new ExpressionContextForTest());
+
+    // The following subtractions should not fit into a long long data type.
+    BSONObj obj = BSON("$subtract" << BSON_ARRAY(maxLong << minLong));
+    auto expression = Expression::parseExpression(expCtx, obj, expCtx->variablesParseState);
+    Value result = expression->evaluate({}, &expCtx->variables);
+    ASSERT_EQ(result.getType(), BSONType::NumberDouble);
+    ASSERT_EQ(result.getDouble(), static_cast<double>(maxLong) - minLong);
+
+    obj = BSON("$subtract" << BSON_ARRAY(minLong << maxLong));
+    expression = Expression::parseExpression(expCtx, obj, expCtx->variablesParseState);
+    result = expression->evaluate({}, &expCtx->variables);
+    ASSERT_EQ(result.getType(), BSONType::NumberDouble);
+    ASSERT_EQ(result.getDouble(), static_cast<double>(minLong) - maxLong);
+
+    // minLong = -1 - maxLong. The below subtraction should fit into long long data type.
+    obj = BSON("$subtract" << BSON_ARRAY(-1 << maxLong));
+    expression = Expression::parseExpression(expCtx, obj, expCtx->variablesParseState);
+    result = expression->evaluate({}, &expCtx->variables);
+    ASSERT_EQ(result.getType(), BSONType::NumberLong);
+    ASSERT_EQ(result.getLong(), -1LL - maxLong);
+
+    // The minLong's negation does not fit into long long, hence it should be converted to double
+    // data type.
+    obj = BSON("$subtract" << BSON_ARRAY(0 << minLong));
+    expression = Expression::parseExpression(expCtx, obj, expCtx->variablesParseState);
+    result = expression->evaluate({}, &expCtx->variables);
+    ASSERT_EQ(result.getType(), BSONType::NumberDouble);
+    ASSERT_EQ(result.getDouble(), static_cast<double>(minLong) * -1);
+}
 }  // namespace ExpressionTests
