5c3ef59e6d9335c9251831b7dfc1fbbe728b4499
Benety Goh <benety@mongodb.com>
2020-10-24 07:52:39 -0400

    SERVER-51504 Add JS test to put timeseries data in a bucket collection

--- jstests/core/time_series/time_series_insert.js
+++ jstests/core/time_series/time_series_insert.js
@@ -1,121 +1,20 @@
 /**
- * Inserts time series data based on the TSBS document-per-event format.
- *
- * https://github.com/timescale/tsbs/blob/7508b34755e05f55a14ec4bac2913ae758b4fd78/cmd/tsbs_generate_data/devops/cpu.go
- * https://github.com/timescale/tsbs/blob/7508b34755e05f55a14ec4bac2913ae758b4fd78/cmd/tsbs_generate_data/devops/host.go
+ * Inserts time-series data based on the TSBS document-per-event format.
  */
 (function() {
 "use strict";
 
+load("jstests/core/time_series/libs/time_series.js");
+
 const coll = db.getCollection(jsTestName());
 Random.setRandomSeed();
 
-const getRandomUsage = function() {
-    return Random.randInt(101);
-};
-
-const updateUsages = function(fields) {
-    for (const field in fields) {
-        fields[field] += Math.round(Random.genNormal(0, 1));
-        fields[field] = Math.max(fields[field], 0);
-        fields[field] = Math.min(fields[field], 100);
-    }
-};
-
-const getRandomElem = function(arr) {
-    return arr[Random.randInt(arr.length)];
-};
-
-const regions = [
-    "ap-northeast-1",
-    "ap-southeast-1",
-    "ap-southeast-2",
-    "eu-central-1",
-    "eu-west-1",
-    "sa-east-1",
-    "us-east-1",
-    "us-west-1",
-    "us-west-2",
-];
-
-const dataCenters = [
-    [
-        "ap-northeast-1a",
-        "ap-northeast-1c",
-    ],
-    [
-        "ap-southeast-1a",
-        "ap-southeast-1b",
-    ],
-    [
-        "ap-southeast-2a",
-        "ap-southeast-2b",
-    ],
-    [
-        "eu-central-1a",
-        "eu-central-1b",
-    ],
-    [
-        "eu-west-1a",
-        "eu-west-1b",
-        "eu-west-1c",
-    ],
-    [
-        "sa-east-1a",
-        "sa-east-1b",
-        "sa-east-1c",
-    ],
-    [
-        "us-east-1a",
-        "us-east-1b",
-        "us-east-1c",
-        "us-east-1e",
-    ],
-    [
-        "us-west-1a",
-        "us-west-1b",
-    ],
-    [
-        "us-west-2a",
-        "us-west-2b",
-        "us-west-2c",
-    ],
-];
-
-const hosts = new Array(10);
-for (let i = 0; i < hosts.length; i++) {
-    const regionIndex = Random.randInt(regions.length);
-    hosts[i] = {
-        fields: {
-            usage_guest: getRandomUsage(),
-            usage_guest_nice: getRandomUsage(),
-            usage_idle: getRandomUsage(),
-            usage_iowait: getRandomUsage(),
-            usage_irq: getRandomUsage(),
-            usage_nice: getRandomUsage(),
-            usage_softirq: getRandomUsage(),
-            usage_steal: getRandomUsage(),
-            usage_system: getRandomUsage(),
-            usage_user: getRandomUsage(),
-        },
-        tags: {
-            arch: getRandomElem(["x64", "x86"]),
-            datacenter: getRandomElem(dataCenters[regionIndex]),
-            hostname: "host_" + i,
-            os: getRandomElem(["Ubuntu15.10", "Ubuntu16.10", "Ubuntu16.04LTS"]),
-            rack: Random.randInt(100).toString(),
-            region: regions[regionIndex],
-            service: Random.randInt(20).toString(),
-            service_environment: getRandomElem(["production", "staging", "test"]),
-            service_version: Random.randInt(2).toString(),
-            team: getRandomElem(["CHI", "LON", "NYC", "SF"]),
-        }
-    };
-}
+const numHosts = 10;
+const hosts = TimeseriesTest.generateHosts(numHosts);
 
 for (let i = 0; i < 100; i++) {
-    const host = getRandomElem(hosts);
-    updateUsages(host.fields);
+    const host = TimeseriesTest.getRandomElem(hosts);
+    TimeseriesTest.updateUsages(host.fields);
 
     assert.commandWorked(coll.insert({
         measurement: "cpu",
---
+++ jstests/core/time_series/time_series_simple.js
@@ -0,0 +1,79 @@
+/**
+ * Tests inserting sample data into the time-series buckets collection.
+ * This test is for the simple case of only one measurement per bucket.
+ * @tags: [
+ *     requires_fcv_49,
+ * ]
+ */
+(function() {
+"use strict";
+
+load("jstests/core/time_series/libs/time_series.js");
+
+if (!db.adminCommand({getParameter: 1, featureFlagTimeSeriesCollection: 1})
+         .featureFlagTimeSeriesCollection.value) {
+    jsTestLog("Skipping test because the time-series collection feature flag is disabled");
+    return;
+}
+
+const testDB = db.getSiblingDB(jsTestName());
+assert.commandWorked(testDB.dropDatabase());
+
+const coll = testDB.getCollection('t');
+const bucketsColl = testDB.getCollection('system.buckets.' + coll.getName());
+
+assert.commandWorked(testDB.createCollection(coll.getName(), {timeseries: {timeField: 'time'}}));
+assert.contains(bucketsColl.getName(), testDB.getCollectionNames());
+
+Random.setRandomSeed();
+const numHosts = 10;
+const hosts = TimeseriesTest.generateHosts(numHosts);
+
+for (let i = 0; i < 100; i++) {
+    const host = TimeseriesTest.getRandomElem(hosts);
+    TimeseriesTest.updateUsages(host.fields);
+
+    const t = ISODate();
+    const oid = ObjectId();
+    const doc = {
+        control: {
+            version: 1,
+            min: {
+                _id: oid,
+                time: t,
+                usage_guest: host.fields.usage_guest,
+                usage_guest_nice: host.fields.usage_guest_nice,
+                // ...
+                usage_user: host.fields.usage_user,
+            },
+            max: {
+                _id: oid,
+                time: t,
+                usage_guest: host.fields.usage_guest,
+                usage_guest_nice: host.fields.usage_guest_nice,
+                // ...
+                usage_user: host.fields.usage_user,
+            },
+        },
+        meta: host.tags,
+        data: {
+            _id: {
+                0: oid,
+            },
+            usage_guest: {
+                0: host.fields.usage_guest,
+            },
+            usage_guest_nice: {
+                0: host.fields.usage_guest_nice,
+            },
+            // ...
+            usage_user: {
+                0: host.fields.usage_user,
+            },
+        },
+    };
+
+    jsTestLog('Inserting doc into buckets collection: ' + i + ': ' + tojson(doc));
+    assert.commandWorked(bucketsColl.insert(doc));
+}
+})();
