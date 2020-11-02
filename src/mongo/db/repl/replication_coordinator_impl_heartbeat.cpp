3ed7e17fb826c2069b5572486a9c09d41997dc8a
A. Jesse Jiryu Davis <jesse@mongodb.com>
2020-11-02 17:05:22 -0500

    SERVER-33747 Fix crash when arbiter restarts and enters REMOVED
    (cherry picked from commit 72aacd4ffaf6500777a8a51f87b0797f8ea8ad0b)

--- src/mongo/db/repl/replication_coordinator_impl_heartbeat.cpp
+++ src/mongo/db/repl/replication_coordinator_impl_heartbeat.cpp
@@ -568,7 +568,7 @@ void ReplicationCoordinatorImpl::_heartbeatReconfigStore(
             }
         }
 
-        if (!isArbiter && isFirstConfig) {
+        if (!isArbiter && myIndex.isOK() && myIndex.getValue() != -1) {
             shouldStartDataReplication = true;
         }
 
