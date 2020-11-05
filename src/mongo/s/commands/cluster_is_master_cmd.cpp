fc1b099c92ea269875e52c07f8f02a94bbb3a906
Pavi Vetriselvan <pavithra.vetriselvan@mongodb.com>
2020-11-05 11:32:11 -0500

    SERVER-50412 implement helloOk protocol negotiation on mongos

--- src/mongo/s/commands/cluster_is_master_cmd.cpp
+++ src/mongo/s/commands/cluster_is_master_cmd.cpp
@@ -144,6 +144,19 @@ public:
         // The isMaster response always includes a topologyVersion.
         auto currentMongosTopologyVersion = mongosIsMasterResponse->getTopologyVersion();
 
+        // Try to parse the optional 'helloOk' field. On mongos, if we see this field, we will
+        // respond with helloOk: true so the client knows that it can continue to send the hello
+        // command to mongos.
+        bool helloOk;
+        Status status = bsonExtractBooleanField(cmdObj, "helloOk", &helloOk);
+        if (status.isOK()) {
+            // Attach helloOk: true to the response so that the client knows the server supports
+            // the hello command.
+            result.append("helloOk", true);
+        } else if (status.code() != ErrorCodes::NoSuchKey) {
+            uassertStatusOK(status);
+        }
+
         result.appendNumber("maxBsonObjectSize", BSONObjMaxUserSize);
         result.appendNumber("maxMessageSizeBytes", MaxMessageSizeBytes);
         result.appendNumber("maxWriteBatchSize", write_ops::kMaxWriteBatchSize);
