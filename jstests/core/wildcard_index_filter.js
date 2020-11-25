862a896b02b55f6ab597d08365b4af7a37e553c7
Anton Korshunov <anton.korshunov@mongodb.com>
2020-11-25 22:36:55 +0000

    SERVER-53087 Exclude wildcard_index_filter.js from core_sbe suite due to incompatibility

--- jstests/core/wildcard_index_filter.js
+++ jstests/core/wildcard_index_filter.js
@@ -5,6 +5,7 @@
  * that may return different values after a failover; in this case, 'planCacheClearFilters'.
  * @tags: [
  *   does_not_support_stepdowns,
+ *   sbe_incompatible,
  * ]
  */
 (function() {
