2a0e76082be0f2aca82830bcaf91f6d737b842ac
Ryan Egesdahl <ryan.egesdahl@mongodb.com>
2020-11-25 13:05:53 -0800

    SERVER-48291 Ensure runtime is dynamically linked in dynamic builds
    Prior to this point, a dynamic build might have resulted in some runtime
    libraries being statically linked into shared objects and executables in
    cases where "shared" runtime libraries were actually linker scripts that
    linked static versions. This was the case with the MongoDB toolchain and
    some distro toolchains, including those installed as updated compiler
    versions in RHEL.
    
    The effect of having runtime libraries statically linked was that
    symbols from those libraries would end up scattered over the compiled
    objects, increasing object sizes and slowing down server startup. Now,
    whenever a dynamic build is selected, the user can choose whether to
    create "shim" runtime libraries that wrap the static ones.
    
    The default behavior on Linux is that dynamic builds will detect whether
    runtime libraries are linker scripts and create shim libraries if any are
    found. On Windows, the default is to always use a dynamic runtime library
    with dynamic builds. For other platforms, the prior behavior remains
    unchanged.

---
+++ src/shim_crt.cpp
@@ -0,0 +1,3 @@
+// This file intentionally blank. shim_crt.cpp rolls together the compiler
+// runtime libraries to provide a single source of those symbols to other
+// compiled objects in a dynamically linked build.
