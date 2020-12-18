c4d86a6bc12b343d262c68c78f5abf0019048d74
Cheahuychou Mao <mao.cheahuychou@gmail.com>
2020-12-18 20:19:13 +0000

    SERVER-53444 Make tests that run removeShard in assert.soon to wait for the state to become completed not error on ShardNotFound
    (cherry picked from commit 03637b5614c1a29983cdac9a1f9ab2d3f7060f15)

--- jstests/sharding/move_jumbo_chunk.js
+++ jstests/sharding/move_jumbo_chunk.js
@@ -142,6 +142,15 @@ let res = st.s.adminCommand({removeShard: st.shard1.shardName});
 assert.commandWorked(res);
 assert.soon(function() {
     res = st.s.adminCommand({removeShard: st.shard1.shardName});
+    if (!res.ok && res.code === ErrorCodes.ShardNotFound) {
+        // If the config server primary steps down right after removing the config.shards doc
+        // for the shard but before responding with "state": "completed", the mongos would retry
+        // the _configsvrRemoveShard command against the new config server primary, which would
+        // not find the removed shard in its ShardRegistry if it has done a ShardRegistry reload
+        // after the config.shards doc for the shard was removed. This would cause the command
+        // to fail with ShardNotFound.
+        return true;
+    }
     assert.commandWorked(res);
     return ("completed" == res.state);
 }, "failed to remove shard");
