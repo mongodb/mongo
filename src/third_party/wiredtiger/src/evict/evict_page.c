de69ba3a224f5ced65cc70352759fa98319d9063
Luke Chen <luke.chen@mongodb.com>
2020-12-18 16:17:23 +1100

    Import wiredtiger: 0ad8b18ea1c5ef64133a6914ba73801f6b884e8a from branch mongodb-5.0
    ref: 34b5932693..0ad8b18ea1
    for: 4.9.0
    
    WT-6740       Fix unintentionally releasing the snapshot by mistreating the recovery session as an eviction session

--- src/third_party/wiredtiger/src/evict/evict_page.c
+++ src/third_party/wiredtiger/src/evict/evict_page.c
@@ -659,7 +659,7 @@ __evict_review(WT_SESSION_IMPL *session, WT_REF *ref, uint32_t evict_flags, bool
     use_snapshot_for_app_thread = !F_ISSET(session, WT_SESSION_INTERNAL) &&
       !WT_IS_METADATA(session->dhandle) && WT_SESSION_TXN_SHARED(session)->id != WT_TXN_NONE &&
       F_ISSET(session->txn, WT_TXN_HAS_SNAPSHOT);
-    is_eviction_thread = FLD_ISSET(evict_flags, WT_REC_EVICTION_THREAD);
+    is_eviction_thread = F_ISSET(session, WT_SESSION_EVICTION);
 
     /* Make sure that both conditions above are not true at the same time. */
     WT_ASSERT(session, !use_snapshot_for_app_thread || !is_eviction_thread);
