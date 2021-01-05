c0a5622e48f42195c4f8f754487a72947378a857
Dianna Hohensee <dianna.hohensee@mongodb.com>
2021-01-05 16:58:41 -0500

    SERVER-53324 remove extra development only logging for lock-free reads and update test depending upon it

--- jstests/noPassthrough/disable_lock_free_reads_server_parameter.js
+++ jstests/noPassthrough/disable_lock_free_reads_server_parameter.js
@@ -20,7 +20,69 @@
 (function() {
 "use strict";
 
+load("jstests/libs/parallelTester.js");
+load("jstests/libs/wait_for_command.js");
+
+/**
+ * Set a 'lockMode' collection lock on 'collectionNs' to be held for 1 hour. This will ensure that
+ * the lock will not be released before desired. The operation can be killed later to release the
+ * lock.
+ *
+ * 'sleepComment' adds a comment so that the operation is can be identified for waitForCommand().
+ */
+function sleepFunction(host, collectionNs, lockMode, sleepComment) {
+    const mongo = new Mongo(host);
+    // Set a MODE_IS collection lock to be held for 1 hour.
+    // Holding this lock for 1 hour will trigger a test timeout.
+    assert.commandFailedWithCode(mongo.adminCommand({
+        sleep: 1,
+        secs: 3600,
+        lockTarget: collectionNs,
+        lock: lockMode,
+        $comment: sleepComment
+    }),
+                                 ErrorCodes.Interrupted);
+}
+
+function runReadAgainstLock(host, dbName, collName, expectingLFR) {
+    // Set up a document to later try to read.
+    const mongo = new Mongo(host);
+    const db = mongo.getDB(dbName);
+    const coll = db.getCollection(collName);
+    coll.drop();
+    assert.commandWorked(coll.insert({a: 1}));
+
+    const sleepComment = "Lock sleep";
+    const sleepCommand =
+        new Thread(sleepFunction, host, dbName + "." + collName, "w", sleepComment);
+    sleepCommand.start();
+
+    // Wait for the sleep command to start.
+    const sleepID = waitForCommand(
+        "sleepCmd",
+        op => (op["ns"] == "admin.$cmd" && op["command"]["$comment"] == sleepComment),
+        mongo.getDB("admin"));
+
+    try {
+        const findResult = coll.find({});
+        if (expectingLFR) {
+            assert.eq(1, coll.find({}).itcount());
+        } else {
+            const failureTimeoutMS = 1 * 1000;
+            assert.commandFailedWithCode(
+                db.runCommand({find: collName, maxTimeMS: failureTimeoutMS}),
+                ErrorCodes.MaxTimeMSExpired);
+        }
+    } finally {
+        // Kill the sleep command in order to release the collection lock.
+        assert.commandWorked(mongo.getDB("admin").killOp(sleepID));
+        sleepCommand.join();
+    }
+}
+
 const replSetName = 'disable_lock_free_reads_server_parameter';
+const dbName = "TestDB";
+const collName = "TestColl";
 
 jsTest.log(
     "Starting server with featureFlagLockFreeReads=true in standalone mode: this should turn " +
@@ -28,7 +90,7 @@ jsTest.log(
 
 let conn = MongoRunner.runMongod({setParameter: "featureFlagLockFreeReads=true"});
 assert(conn);
-checkLog.containsJson(conn, 4788403);  // Logging that lock-free reads is enabled.
+runReadAgainstLock(conn.host, dbName, collName, true);
 MongoRunner.stopMongod(conn);
 
 jsTest.log("Starting server with featureFlagLockFreeReads=false in standalone mode: this is the " +
@@ -36,40 +98,41 @@ jsTest.log("Starting server with featureFlagLockFreeReads=false in standalone mo
 
 conn = MongoRunner.runMongod({setParameter: "featureFlagLockFreeReads=false"});
 assert(conn);
-assert(!checkLog.checkContainsOnce(conn, "disabling lock-free reads"));
-checkLog.containsJson(conn, 4788402);  // Logging that lock-free reads is disabled.
+runReadAgainstLock(conn.host, dbName, collName, false);
 MongoRunner.stopMongod(conn);
 
 jsTest.log(
     "Starting server with featureFlagLockFreeReads=true and enableMajorityReadConcern=false: " +
-    "this should override the setting to true.");
+    "this should override the setting to false.");
 
-conn = MongoRunner.runMongod({
-    replSet: replSetName,
-    enableMajorityReadConcern: false,
-    setParameter: "featureFlagLockFreeReads=true"
+let rst = new ReplSetTest({
+    name: replSetName,
+    nodes: 1,
+    nodeOptions: {enableMajorityReadConcern: "false", setParameter: "featureFlagLockFreeReads=true"}
 });
-assert(conn);
-checkLog.containsJson(conn, 4788401);  // Logging eMRCf disables lock-free reads.
-checkLog.containsJson(conn, 4788402);  // Logging that lock-free reads is disabled.
-MongoRunner.stopMongod(conn);
+rst.startSet();
+rst.initiate();
+runReadAgainstLock(rst.getPrimary().host, dbName, collName, false);
+rst.stopSet();
 
 jsTest.log("Starting server as a replica set member with featureFlagLockFreeReads=true: this " +
            "should turn on lock-free reads.");
 
-conn = MongoRunner.runMongod({replSet: replSetName, setParameter: "featureFlagLockFreeReads=true"});
-assert(conn);
-checkLog.containsJson(conn, 4788403);  // Logging that lock-free reads is enabled.
-MongoRunner.stopMongod(conn);
+rst = new ReplSetTest(
+    {name: replSetName, nodes: 1, nodeOptions: {setParameter: "featureFlagLockFreeReads=true"}});
+rst.startSet();
+rst.initiate();
+runReadAgainstLock(rst.getPrimary().host, dbName, collName, true);
+rst.stopSet();
 
 jsTest.log(
     "Starting server as a replica set member with featureFlagLockFreeReads=false: this is the " +
     "default and nothing should happen.");
 
-conn =
-    MongoRunner.runMongod({replSet: replSetName, setParameter: "featureFlagLockFreeReads=false"});
-assert(conn);
-assert(!checkLog.checkContainsOnce(conn, "disabling lock-free reads"));
-checkLog.containsJson(conn, 4788402);  // Logging that lock-free reads is disabled.
-MongoRunner.stopMongod(conn);
+rst = new ReplSetTest(
+    {name: replSetName, nodes: 1, nodeOptions: {setParameter: "featureFlagLockFreeReads=false"}});
+rst.startSet();
+rst.initiate();
+runReadAgainstLock(rst.getPrimary().host, dbName, collName, false);
+rst.stopSet();
 }());
