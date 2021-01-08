8d9dd063d94b4f17d1adfa331dfc2c27a4180085
Jason Chan <jason.chan@mongodb.com>
2021-01-08 21:45:57 +0000

    SERVER-53646 Catch exception if waiting for failpoint throws on close connections in awaitable_hello_on_nodes_with_invalid_configs.js

--- jstests/replsets/awaitable_hello_on_nodes_with_invalid_configs.js
+++ jstests/replsets/awaitable_hello_on_nodes_with_invalid_configs.js
@@ -127,7 +127,13 @@ awaitPrimaryHelloBeforeNodeRemoval();
 assert.soonNoExcept(
     () => assert.commandFailedWithCode(secondaryDB.adminCommand({replSetGetStatus: 1}),
                                        ErrorCodes.InvalidReplicaSetConfig));
-connectionsClosedAfterRemoved.wait();
+
+// It is possible that 'waitForFailpoint' is called as connections are being closed, triggering
+// an exception. In this case, retry until we are sure connections are finished closing.
+assert.soonNoExcept(function() {
+    connectionsClosedAfterRemoved.wait();
+    return true;
+});
 
 const primaryRespAfterRemoval = assert.commandWorked(primaryDB.runCommand({hello: 1}));
 const secondaryRespAfterRemoval = assert.commandWorked(secondaryDB.runCommand({hello: 1}));
