0659e83d7d6855c6b2739f2c959d456607300c6b
Samy Lanka <samy.lanka@mongodb.com>
2021-01-15 20:57:06 +0000

    SERVER-53666 Make sure curSecondary is in SECONDARY state after restarting in rollback test fixture

--- jstests/replsets/libs/rollback_test.js
+++ jstests/replsets/libs/rollback_test.js
@@ -558,6 +558,8 @@ function RollbackTest(name = "RollbackTest", replSet) {
 
         curSecondary = rst.getSecondary();
         assert.neq(curPrimary, curSecondary);
+
+        waitForState(curSecondary, ReplSetTest.State.SECONDARY);
     };
 
     /**
