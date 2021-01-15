a930e29a1c8f655ef6a5d4e8157c58479b2ce96b
Jason Chan <jason.chan@mongodb.com>
2021-01-15 18:25:15 +0000

    SERVER-53835 Add requires_fcv_49 to dropdb.js

--- jstests/core/dropdb.js
+++ jstests/core/dropdb.js
@@ -1,6 +1,7 @@
 // Test that a db does not exist after it is dropped.
 // Disabled in the small oplog suite because the secondary may create a primary db
 // with the same name as the dropped db when requesting a clone.
+// @tags: [requires_fcv_49]
 
 m = db.getMongo();
 baseName = "jstests_dropdb";
