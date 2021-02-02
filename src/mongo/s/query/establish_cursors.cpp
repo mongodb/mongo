622ba6fa36050f5a3f9d4decc4aca61eca840147
Ben Caimano <ben.caimano@10gen.com>
2021-02-02 15:35:06 +0000

    SERVER-46740 Add missing reference character

--- src/mongo/s/query/establish_cursors.cpp
+++ src/mongo/s/query/establish_cursors.cpp
@@ -68,7 +68,7 @@ public:
     /**
      * Make a RequestSender and thus send requests.
      */
-    void sendRequests(const ReadPreferenceSetting readPref,
+    void sendRequests(const ReadPreferenceSetting& readPref,
                       const std::vector<std::pair<ShardId, BSONObj>>& remotes,
                       Shard::RetryPolicy retryPolicy);
 
@@ -113,7 +113,7 @@ private:
     std::vector<RemoteCursor> _remoteCursors;
 };
 
-void CursorEstablisher::sendRequests(const ReadPreferenceSetting readPref,
+void CursorEstablisher::sendRequests(const ReadPreferenceSetting& readPref,
                                      const std::vector<std::pair<ShardId, BSONObj>>& remotes,
                                      Shard::RetryPolicy retryPolicy) {
     // Construct the requests
