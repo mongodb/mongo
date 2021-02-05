5ee73f447aaa1e1b9503aff6ff031f240708c711
Cheahuychou Mao <mao.cheahuychou@gmail.com>
2021-02-05 17:04:30 +0000

    SERVER-54349 Run st.configRS.awaitReplication() before running replSetStepUp in sharded_index_consistency_metrics.js

--- jstests/noPassthrough/sharded_index_consistency_metrics.js
+++ jstests/noPassthrough/sharded_index_consistency_metrics.js
@@ -152,7 +152,7 @@ checkServerStatus(configPrimaryConn, configSecondaryConn, connsWithoutIndexConsi
 
 // Force the secondary that tracks inconsistent indexes to step up to primary and update the
 // appropriate test variables.
-assert.commandWorked(configSecondaryConn.adminCommand({replSetStepUp: 1}));
+st.configRS.stepUp(configSecondaryConn);
 st.configRS.waitForState(configSecondaryConn, ReplSetTest.State.PRIMARY);
 st.configRS.waitForState(configPrimaryConn, ReplSetTest.State.SECONDARY);
 st.configRS.awaitNodesAgreeOnPrimary();
