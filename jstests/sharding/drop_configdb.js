ade18a70ec691e327b7efa800ad5ea56d51c9ac5
Tommaso Tocci <tommaso.tocci@mongodb.com>
2021-02-05 11:49:41 +0100

    SERVER-54331 Extend dropCollection test coverage

--- jstests/sharding/drop_configdb.js
+++ jstests/sharding/drop_configdb.js
@@ -9,6 +9,14 @@ var st = new ShardingTest({shards: 1});
 var mongos = st.s;
 var config = st.configRS.getPrimary().getDB('config');
 
+jsTest.log("Dropping a collection in admin/config DB is illegal");
+{
+    assert.commandFailedWithCode(st.s.getDB('admin').runCommand({drop: 'secrets'}),
+                                 ErrorCodes.IllegalOperation);
+    assert.commandFailedWithCode(st.s.getDB('config').runCommand({drop: 'settings'}),
+                                 ErrorCodes.IllegalOperation);
+}
+
 // Try to drop config db via configsvr
 
 print("1: Try to drop config database via configsvr");
