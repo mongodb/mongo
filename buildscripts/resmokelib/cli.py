eb4a50d0456826678b5bd57209ecfadde3c15dc0
Siran Wang <siran.wang@mongodb.com>
2021-02-18 17:18:35 -0500

    SERVER-54293 add instruction and example for resmoke help message

--- buildscripts/resmokelib/cli.py
+++ buildscripts/resmokelib/cli.py
@@ -13,5 +13,9 @@ def main(argv):
     :return: None
     """
     __start_time = time.time()
-    subcommand = parser.parse_command_line(argv[1:], start_time=__start_time)
+    subcommand = parser.parse_command_line(
+        argv[1:], start_time=__start_time,
+        usage="Resmoke is MongoDB's correctness testing orchestrator.\n"
+        "For more information, see the help message for each subcommand.\n"
+        "For example: resmoke.py run -h\n")
     subcommand.execute()
