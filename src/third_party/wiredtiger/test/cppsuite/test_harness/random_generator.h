e5d76cde416a3f9d886f82f87e6fba6c5c8f4135
Luke Chen <luke.chen@mongodb.com>
2021-02-22 14:44:32 +1100

    Import wiredtiger: bcbb6c3c52b719f6c108edd6594c1e1561ef0fe1 from branch mongodb-5.0
    ref: 86d3d12e8a..bcbb6c3c52
    for: 4.9.0
    
    WT-7206       Update test framework to align with C++ style

--- src/third_party/wiredtiger/test/cppsuite/test_harness/random_generator.h
+++ src/third_party/wiredtiger/test/cppsuite/test_harness/random_generator.h
@@ -1,4 +1,3 @@
-/* Include guard. */
 #ifndef RANDOM_GENERATOR_H
 #define RANDOM_GENERATOR_H
 
@@ -12,21 +11,21 @@ namespace test_harness {
 /* Helper class to generate random values. */
 class random_generator {
     public:
-    static random_generator *
+    /* No copies of the singleton allowed. */
+    random_generator(random_generator const &) = delete;
+    void operator=(random_generator const &) = delete;
+
+    static random_generator &
     get_instance()
     {
-        if (!_instance)
-            _instance = new random_generator;
-        return (_instance);
+        static random_generator _instance;
+        return _instance;
     }
 
     std::string
     generate_string(std::size_t length)
     {
         std::string random_string;
-
-        if (length == 0)
-            throw std::invalid_argument("random_generator.generate_string: 0 is an invalid length");
 
         for (std::size_t i = 0; i < length; ++i)
             random_string += _characters[_distribution(_generator)];
@@ -37,18 +36,15 @@ class random_generator {
     private:
     random_generator()
     {
-        _generator = std::mt19937(_random_device());
+        _generator = std::mt19937(std::random_device{}());
         _distribution = std::uniform_int_distribution<>(0, _characters.size() - 1);
     }
 
-    static random_generator *_instance;
     std::mt19937 _generator;
-    std::random_device _random_device;
     std::uniform_int_distribution<> _distribution;
     const std::string _characters =
       "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 };
-random_generator *random_generator::_instance = 0;
 } // namespace test_harness
 
 #endif
