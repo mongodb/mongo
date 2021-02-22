f155a5dec7ed077bc7e0526365c98b830364d998
Luke Chen <luke.chen@mongodb.com>
2021-02-22 14:44:26 +1100

    Import wiredtiger: e932b903d726bc6202d981d8a759fad3262c8c71 from branch mongodb-5.0
    ref: 27b1bc5713..e932b903d7
    for: 4.9.0
    
    WT-7099       Fix the assert not considering reinsert to history store after a prepared rollback

--- src/third_party/wiredtiger/src/btree/row_modify.c
+++ src/third_party/wiredtiger/src/btree/row_modify.c
@@ -119,13 +119,17 @@ __wt_row_modify(WT_CURSOR_BTREE *cbt, const WT_ITEM *key, const WT_ITEM *value, 
             __wt_upd_value_assign(cbt->modify_update, upd);
         } else {
             /*
-             * We only update history store records in two cases:
+             * We only update history store records in three cases:
              *  1) Delete the record with a tombstone with WT_TS_NONE.
              *  2) Update the record's stop time point if the prepared update written to the data
              * store is committed.
+             *  3) Reinsert an update that has been deleted by a prepared rollback.
              */
             WT_ASSERT(session,
               !WT_IS_HS(S2BT(session)->dhandle) ||
+                (*upd_entry == NULL ||
+                  ((*upd_entry)->type == WT_UPDATE_TOMBSTONE && (*upd_entry)->txnid == WT_TS_NONE &&
+                    (*upd_entry)->start_ts == WT_TS_NONE)) ||
                 (upd_arg->type == WT_UPDATE_TOMBSTONE && upd_arg->start_ts == WT_TS_NONE &&
                   upd_arg->next == NULL) ||
                 (upd_arg->type == WT_UPDATE_TOMBSTONE && upd_arg->next != NULL &&
