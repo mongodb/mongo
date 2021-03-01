6601c787b7c4853be8bd54bcc1dbf481f2ee6286
Andrii Dobroshynski <andrii.dobroshynski@mongodb.com>
2021-03-01 18:52:33 +0000

    SERVER-54562 [SBE] Fix jstests/core/index_filter_commands.js failure due to explain format change in SBE

---
+++ jstests/libs/sbe_explain_helpers.js
@@ -0,0 +1,38 @@
+/**
+ * Helpers for checking correctness of generated SBE plans when expected explain() output differs.
+ */
+
+// Include helpers for analyzing explain output.
+load("jstests/libs/analyze_plan.js");
+
+const isSBEEnabled = (() => {
+    const getParam = db.adminCommand({getParameter: 1, featureFlagSBE: 1});
+    return getParam.hasOwnProperty("featureFlagSBE") && getParam.featureFlagSBE.value;
+})();
+
+// SBE does not support building IDHACK plans. As such, if SBE is being used, we assert that the
+// generated plan is an 'expectedParentStageForIxScan' + IXSCAN over the _id index.
+function assertIdHackPlan(db, root, expectedParentStageForIxScan) {
+    if (isSBEEnabled) {
+        const parentStage = getPlanStage(root, expectedParentStageForIxScan);
+        assert.neq(parentStage, null, root);
+
+        const ixscan = parentStage.inputStage;
+        assert.neq(ixscan, null, root);
+        assert.eq(ixscan.stage, "IXSCAN", root);
+        assert(!ixscan.hasOwnProperty("filter"), root);
+        assert.eq(ixscan.indexName, "_id_", root);
+    } else {
+        assert(isIdhack(db, root), root);
+    }
+}
+
+// When SBE is enabled, we assert that the generated plan used an IXSCAN. Otherwise, we assert that
+// 'isIdhack' returns false.
+function assertNonIdHackPlan(db, root) {
+    if (isSBEEnabled) {
+        assert(isIxscan(db, root), root);
+    } else {
+        assert(!isIdhack(db, root), root);
+    }
+}
