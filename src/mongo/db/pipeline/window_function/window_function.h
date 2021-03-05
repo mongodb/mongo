a8f799fd3c6f3e7f00bd83064dfea49699133cbc
Hartek Sabharwal <hartek.sabharwal@mongodb.com>
2021-03-05 17:23:04 +0000

    SERVER-54234 Implement removable $stddev window function

--- src/mongo/db/pipeline/window_function/window_function.h
+++ src/mongo/db/pipeline/window_function/window_function.h
@@ -206,4 +206,92 @@ private:
     // insertion.
     std::list<Value> _list;
 };
+
+class WindowFunctionStdDev : public WindowFunctionState {
+protected:
+    explicit WindowFunctionStdDev(ExpressionContext* const expCtx, bool isSamp)
+        : WindowFunctionState(expCtx),
+          _sum(AccumulatorSum::create(expCtx)),
+          _m2(AccumulatorSum::create(expCtx)),
+          _isSamp(isSamp),
+          _count(0),
+          _nonfiniteValueCount(0) {}
+
+public:
+    static Value getDefault() {
+        return Value(BSONNULL);
+    }
+
+    void add(Value value) {
+        update(std::move(value), +1);
+    }
+
+    void remove(Value value) {
+        update(std::move(value), -1);
+    }
+
+    Value getValue() const final {
+        if (_nonfiniteValueCount > 0)
+            return Value(std::numeric_limits<double>::quiet_NaN());
+        const long long adjustedCount = _isSamp ? _count - 1 : _count;
+        if (adjustedCount == 0)
+            return getDefault();
+        return Value(sqrt(_m2->getValue(false).coerceToDouble() / adjustedCount));
+    }
+
+    void reset() {
+        _m2->reset();
+        _sum->reset();
+        _count = 0;
+        _nonfiniteValueCount = 0;
+    }
+
+private:
+    void update(Value value, int quantity) {
+        // quantity should be 1 if adding value, -1 if removing value
+        if (!value.numeric())
+            return;
+        if ((value.getType() == NumberDouble && !std::isfinite(value.getDouble())) ||
+            (value.getType() == NumberDecimal && !value.getDecimal().isFinite())) {
+            _nonfiniteValueCount += quantity;
+            _count += quantity;
+            return;
+        }
+
+        if (_count == 0) {  // Assuming we are adding value if _count == 0.
+            _count++;
+            _sum->process(value, false);
+            return;
+        } else if (_count + quantity == 0) {  // Empty the window.
+            reset();
+            return;
+        }
+        double x = _count * value.coerceToDouble() - _sum->getValue(false).coerceToDouble();
+        _count += quantity;
+        _sum->process(Value{value.coerceToDouble() * quantity}, false);
+        _m2->process(Value{x * x * quantity / (_count * (_count - quantity))}, false);
+    }
+
+    // Std dev cannot make use of RemovableSum because of its specific handling of non-finite
+    // values. Adding a NaN or +/-inf makes the result NaN. Additionally, the consistent and
+    // exclusive use of doubles in std dev calculations makes the type handling in RemovableSum
+    // unnecessary.
+    boost::intrusive_ptr<AccumulatorState> _sum;
+    boost::intrusive_ptr<AccumulatorState> _m2;
+    bool _isSamp;
+    long long _count;
+    int _nonfiniteValueCount;
+};
+
+class WindowFunctionStdDevPop final : public WindowFunctionStdDev {
+public:
+    explicit WindowFunctionStdDevPop(ExpressionContext* const expCtx)
+        : WindowFunctionStdDev(expCtx, false) {}
+};
+
+class WindowFunctionStdDevSamp final : public WindowFunctionStdDev {
+public:
+    explicit WindowFunctionStdDevSamp(ExpressionContext* const expCtx)
+        : WindowFunctionStdDev(expCtx, true) {}
+};
 }  // namespace mongo
