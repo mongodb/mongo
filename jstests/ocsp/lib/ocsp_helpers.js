531a6ebdbd15b30d9b08964a9b4f371e0aaa9b12
Shreyas Kalyan <shreyas.kalyan@10gen.com>
2021-03-16 11:02:28 -0700

    SERVER-55122 Fix OCSP to allow intermediate certificates in tlsCertificateKeyFile

--- jstests/ocsp/lib/ocsp_helpers.js
+++ jstests/ocsp/lib/ocsp_helpers.js
@@ -13,10 +13,13 @@ const OCSP_SERVER_MUSTSTAPLE_CERT = "jstests/libs/ocsp/server_ocsp_mustStaple.pe
 const OCSP_SERVER_CERT_REVOKED = "jstests/libs/ocsp/server_ocsp_revoked.pem";
 const OCSP_RESPONDER_CERT = "jstests/libs/ocsp/ocsp_responder.crt";
 const OCSP_RESPONDER_KEY = "jstests/libs/ocsp/ocsp_responder.key";
-const OCSP_INTERMEDIATE_CA_PEM = "jstests/libs/ocsp/intermediate_ca_ocsp.pem";
-const OCSP_INTERMEDIATE_CA_CERT = "jstests/libs/ocsp/intermediate_ca_ocsp.crt";
-const OCSP_INTERMEDIATE_CA_KEY = "jstests/libs/ocsp/intermediate_ca_ocsp.key";
-const OCSP_SERVER_INTERMEDIATE_CA_CERT = "jstests/libs/ocsp/server_intermediate_ca_ocsp.pem";
+const OCSP_INTERMEDIATE_CA_WITH_ROOT_PEM = "jstests/libs/ocsp/intermediate_ca_with_root_ocsp.pem";
+const OCSP_INTERMEDIATE_CA_ONLY_CERT = "jstests/libs/ocsp/intermediate_ca_only_ocsp.crt";
+const OCSP_INTERMEDIATE_CA_ONLY_KEY = "jstests/libs/ocsp/intermediate_ca_only_ocsp.key";
+const OCSP_SERVER_SIGNED_BY_INTERMEDIATE_CA_PEM =
+    "jstests/libs/ocsp/server_signed_by_intermediate_ca_ocsp.pem";
+const OCSP_SERVER_AND_INTERMEDIATE_APPENDED_PEM =
+    "jstests/libs/ocsp/server_and_intermediate_ca_appended_ocsp.pem";
 
 var clearOCSPCache = function() {
     let provider = determineSSLProvider();
@@ -60,8 +63,8 @@ var supportsStapling = function() {
     if (determineSSLProvider() !== "openssl") {
         return false;
     }
-
     if (isUbuntu1804() === true) {
         return false;
     }
+    return true;
 };
---
+++ jstests/ocsp/ocsp_unable_to_staple_log.js
@@ -0,0 +1,35 @@
+// Check that log messages for OCSP stapling work
+// @tags: [requires_http_client, requires_ocsp_stapling]
+
+load("jstests/ocsp/lib/mock_ocsp.js");
+
+(function() {
+"use strict";
+
+if (!supportsStapling()) {
+    return;
+}
+
+const logPath = MongoRunner.dataPath + "mongod.log";
+
+const ocsp_options = {
+    logpath: logPath,
+    sslMode: "requireSSL",
+    sslPEMKeyFile: OCSP_SERVER_SIGNED_BY_INTERMEDIATE_CA_PEM,
+    sslCAFile: OCSP_CA_PEM,
+    sslAllowInvalidHostnames: "",
+    waitForConnect: false,
+};
+
+// Because waitForConnect is off, we need to wait for the process to create the
+// mongod logfile, hence the sleep.
+const conn = MongoRunner.runMongod(ocsp_options);
+sleep(5000);
+
+const failedToStapleID = 5512202;
+assert.soon(() => {
+    return cat(logPath).trim().split("\n").some((line) => JSON.parse(line).id === failedToStapleID);
+});
+
+MongoRunner.stopMongod(conn);
+})();
