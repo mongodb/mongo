f299c612b32ce174b6c9f43428dde3161b60d5cb
Luke Chen <luke.chen@mongodb.com>
2021-03-16 16:28:42 +1100

    Import wiredtiger: eb71b5604e50d526ed0dee82dccf5030fddc0784 from branch mongodb-5.0
    ref: 8b41afa190..eb71b5604e
    for: 4.9.0
    
    WT-7223       WT_CALL_FUNCTION should not print out a message

--- src/third_party/wiredtiger/src/os_posix/os_fallocate.c
+++ src/third_party/wiredtiger/src/os_posix/os_fallocate.c
@@ -20,30 +20,28 @@
  */
 #if defined(HAVE_FALLOCATE) || (defined(__linux__) && defined(SYS_fallocate)) || \
   defined(HAVE_POSIX_FALLOCATE)
-#define WT_CALL_FUNCTION(op)                                                   \
-    do {                                                                       \
-        WT_DECL_RET;                                                           \
-        WT_FILE_HANDLE_POSIX *pfh;                                             \
-        WT_SESSION_IMPL *session;                                              \
-        bool remap;                                                            \
-                                                                               \
-        session = (WT_SESSION_IMPL *)wt_session;                               \
-        pfh = (WT_FILE_HANDLE_POSIX *)file_handle;                             \
-                                                                               \
-        remap = (offset != pfh->mmap_size);                                    \
-        if (remap)                                                             \
-            __wt_prepare_remap_resize_file(file_handle, wt_session);           \
-                                                                               \
-        WT_SYSCALL_RETRY(op, ret);                                             \
-        if (remap) {                                                           \
-            if (ret == 0)                                                      \
-                __wt_remap_resize_file(file_handle, wt_session);               \
-            else {                                                             \
-                __wt_release_without_remap(file_handle);                       \
-                WT_RET_MSG(session, ret, "%s: fallocate:", file_handle->name); \
-            }                                                                  \
-        }                                                                      \
-        return (0);                                                            \
+#define WT_CALL_FUNCTION(op)                                         \
+    do {                                                             \
+        WT_DECL_RET;                                                 \
+        WT_FILE_HANDLE_POSIX *pfh;                                   \
+        bool remap;                                                  \
+                                                                     \
+        pfh = (WT_FILE_HANDLE_POSIX *)file_handle;                   \
+                                                                     \
+        remap = (offset != pfh->mmap_size);                          \
+        if (remap)                                                   \
+            __wt_prepare_remap_resize_file(file_handle, wt_session); \
+                                                                     \
+        WT_SYSCALL_RETRY(op, ret);                                   \
+        if (remap) {                                                 \
+            if (ret == 0)                                            \
+                __wt_remap_resize_file(file_handle, wt_session);     \
+            else {                                                   \
+                __wt_release_without_remap(file_handle);             \
+                WT_RET(ret);                                         \
+            }                                                        \
+        }                                                            \
+        return (0);                                                  \
     } while (0)
 #endif
 
