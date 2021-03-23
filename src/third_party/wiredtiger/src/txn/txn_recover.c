576975d110c45c821a955742629ddd9d2d341f5a
Luke Chen <luke.chen@mongodb.com>
2021-03-23 16:11:17 +1100

    Import wiredtiger: 87e9ff913a14fb7f4c28444a153cb566e3aa2481 from branch mongodb-5.0
    ref: d9105d6af2..87e9ff913a
    for: 5.0.0
    
    WT-7302       Use last connection base write generation as a minimum base write generation for a btree

--- src/third_party/wiredtiger/src/txn/txn_recover.c
+++ src/third_party/wiredtiger/src/txn/txn_recover.c
@@ -792,7 +792,6 @@ __wt_txn_recover(WT_SESSION_IMPL *session, const char *cfg[])
     conn->txn_global.recovery_timestamp = conn->txn_global.meta_ckpt_timestamp = WT_TS_NONE;
 
     F_SET(conn, WT_CONN_RECOVERING);
-    WT_ERR(__recovery_set_ckpt_base_write_gen(&r));
     WT_ERR(__wt_metadata_search(session, WT_METAFILE_URI, &config));
     WT_ERR(__recovery_setup_file(&r, WT_METAFILE_URI, config));
     WT_ERR(__wt_metadata_cursor_open(session, NULL, &metac));
@@ -960,6 +959,7 @@ done:
     WT_ERR(__recovery_set_checkpoint_timestamp(&r));
     WT_ERR(__recovery_set_oldest_timestamp(&r));
     WT_ERR(__recovery_set_checkpoint_snapshot(session));
+    WT_ERR(__recovery_set_ckpt_base_write_gen(&r));
 
     /*
      * Perform rollback to stable only when the following conditions met.
