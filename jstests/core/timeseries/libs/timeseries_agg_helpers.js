fa9e51dd3791efb8c45121dee1974debd4d9b5fc
Yoonsoo Kim <yoonsoo.kim@mongodb.com>
2021-03-26 21:31:25 +0000

    SERVER-54069 Added $merge/$out test suites for time-series

---
+++ jstests/core/timeseries/libs/timeseries_agg_helpers.js
@@ -0,0 +1,110 @@
+load("jstests/core/timeseries/libs/timeseries.js");
+
+/**
+ * Helper class for aggregate tests with time-series collection.
+ */
+class TimeseriesAggTests {
+    /**
+     * Gets a test db object based on the test suite name.
+     */
+    static getTestDb() {
+        return db.getSiblingDB(jsTestName());
+    }
+
+    /**
+     * Prepares two input collections with exactly the same data set. One is a time-series
+     * collection and the other one is a non time-series collection.
+     *
+     * @param numHosts Number of different hosts to generate.
+     * @param numIterations Number of measurement generation loop iterations.
+     * @returns An array of a time-series collection and a non time-series collection,
+     *     respectively in this order.
+     */
+    static prepareInputCollections(numHosts, numIterations) {
+        const timeseriesCollOption = {timeseries: {timeField: "time", metaField: "hostid"}};
+
+        Random.setRandomSeed();
+        const hosts = TimeseriesTest.generateHosts(numHosts);
+
+        const testDB = TimeseriesAggTests.getTestDb();
+
+        // Creates a time-series input collection.
+        const inColl = testDB.getCollection("in");
+        inColl.drop();
+        assert.commandWorked(testDB.createCollection(inColl.getName()), timeseriesCollOption);
+
+        // Creates a non time-series observer input collection.
+        const observerInColl = testDB.getCollection("observer_in");
+        observerInColl.drop();
+        assert.commandWorked(testDB.createCollection(observerInColl.getName()));
+
+        // Inserts exactly the same random measurement to both inColl and observerInColl.
+        for (let i = 0; i < numIterations; i++) {
+            let host = TimeseriesTest.getRandomElem(hosts);
+            let usage = TimeseriesTest.getRandomUsage();
+            let newMeasurement = {"cpu": usage, "hostid": host.tags.hostid, "time": ISODate()};
+            assert.commandWorked(inColl.insert(newMeasurement));
+            assert.commandWorked(observerInColl.insert(newMeasurement));
+
+            if (i % 2) {
+                let idleMeasurement = {
+                    "idle": 100 - usage,
+                    "hostid": host.tags.hostid,
+                    "time": ISODate()
+                };
+                assert.commandWorked(inColl.insert(idleMeasurement));
+                assert.commandWorked(observerInColl.insert(idleMeasurement));
+            }
+        }
+
+        return [inColl, observerInColl];
+    }
+
+    /**
+     * Gets an output collection object with the name 'outCollname'.
+     */
+    static getOutputCollection(outCollName) {
+        const testDB = TimeseriesAggTests.getTestDb();
+
+        let outColl = testDB.getCollection(outCollName);
+        outColl.drop();
+
+        return outColl;
+    }
+
+    /**
+     * Gets the aggregation results from the 'outColl' which the last stage of the 'pipeline' on the
+     * 'inColl' will output the results to.
+     *
+     * Assumes that the last stage of the 'pipeline' is either $out or $merge.
+     *
+     * Executes 'prepareAction' before executing 'pipeline'. 'prepareAction' takes a collection
+     * parameter and returns nothing.
+     *
+     * Returns sorted data by "time" field. The sorted result data will help simplify comparison
+     * logic.
+     */
+    static getOutputAggregateResults(inColl, pipeline, prepareAction = null) {
+        // Figures out the output collection name from the last pipeline stage.
+        var outCollName = "out";
+        if (pipeline[pipeline.length - 1]["$out"] != undefined) {
+            // If the last stage is "$out", gets the output collection name from it.
+            outCollName = pipeline[pipeline.length - 1]["$out"];
+        } else if (pipeline[pipeline.length - 1]["$merge"] != undefined) {
+            // If the last stage is "$merge", gets the output collection name from it.
+            outCollName = pipeline[pipeline.length - 1]["$merge"].into;
+        }
+
+        let outColl = TimeseriesAggTests.getOutputCollection(outCollName);
+        if (prepareAction != null) {
+            prepareAction(outColl);
+        }
+
+        // Assumes that the pipeline's last stage outputs result into 'outColl'.
+        assert.doesNotThrow(() => inColl.aggregate(pipeline));
+
+        return outColl.find({}, {"_id": 0, "time": 1, "hostid": 1, "cpu": 1, "idle": 1})
+            .sort({"time": 1})
+            .toArray();
+    }
+}
---
+++ jstests/core/timeseries/timeseries_merge.js
@@ -0,0 +1,115 @@
+/**
+ * Verifies that time-series collections work as expected with $merge.
+ *
+ *
+ * @tags: [
+ *     assumes_unsharded_collection,
+ *     requires_timeseries,
+ *     requires_fcv_49,
+ * ]
+ */
+(function() {
+"use strict";
+
+load("jstests/core/timeseries/libs/timeseries_agg_helpers.js");
+
+if (!TimeseriesTest.timeseriesCollectionsEnabled(db.getMongo())) {
+    jsTestLog("Skipping test because the time-series collection feature flag is disabled");
+    return;
+}
+
+const testDB = TimeseriesAggTests.getTestDb();
+assert.commandWorked(testDB.dropDatabase());
+const numHosts = 10;
+const numIterations = 20;
+
+let [inColl, observerInColl] = TimeseriesAggTests.prepareInputCollections(numHosts, numIterations);
+
+// $merge on field requires the output collection to have an unique index on key(s) for "on" field.
+function prepareOutputCollectionForMergeOn(outColl) {
+    outColl.drop();
+    assert.commandWorked(testDB.createCollection(outColl.getName()));
+
+    assert.commandWorked(outColl.createIndex({"hostid": 1}, {unique: true}));
+}
+
+/**
+ * Verifies that simple $merge works as expected with time-series source collection.
+ */
+let runSimpleMergeTestCase = () => {
+    // Gets the expected results from non time-series observer input collection.
+    let expectedResults = TimeseriesAggTests.getOutputAggregateResults(
+        observerInColl, [{$merge: {into: "observer_out"}}]);
+
+    // Gets the actual results from time-series input collection.
+    let actualResults =
+        TimeseriesAggTests.getOutputAggregateResults(inColl, [{$merge: {into: "out"}}]);
+
+    // Verifies that the number of measurements is same as expected.
+    assert.eq(actualResults.length, expectedResults.length, actualResults);
+
+    // Verifies that every measurement is same as expected.
+    for (var i = 0; i < expectedResults.length; ++i) {
+        assert.eq(actualResults[i], expectedResults[i], actualResults);
+    }
+};
+
+let runMergeOnErrorTestCase = () => {
+    var outColl = TimeseriesAggTests.getOutputCollection("out");
+    prepareOutputCollectionForMergeOn(outColl);
+
+    // This must fail because source '_id' field will try to replace target '_id' field which is
+    // immutable. This verifies that source '_id' is materialized.
+    jsTestLog("'ImmutableField' error expected below!");
+    var err = assert.throws(() => inColl.aggregate([{
+        $merge: {
+            into: outColl.getName(),
+            on: "hostid",
+            whenMatched: "replace",
+        }
+    }]));
+    assert.commandFailedWithCode(err, ErrorCodes.ImmutableField);
+};
+
+/**
+ * Verifies that $merge "on" field works as expected with time-series source collection.
+ */
+let runMergeOnTestCase = () => {
+    var mergePipeline = [
+        {$project: {_id: 0, cpu: 1, idle: 1, hostid: 1, time: 1}},
+        {$sort: {time: 1}},
+        {
+            $merge: {
+                into: "observer_out",
+                on: "hostid",
+                whenMatched: "merge",
+            }
+        }
+    ];
+
+    // Gets the expected results from non time-series observer input collection.
+    let expectedResults = TimeseriesAggTests.getOutputAggregateResults(
+        observerInColl,
+        mergePipeline,
+        /*prepareAction*/ (outColl) => prepareOutputCollectionForMergeOn(outColl));
+
+    // Gets the actual results from time-series input collection.
+    mergePipeline[mergePipeline.length - 1]["$merge"].into = "out";
+    let actualResults = TimeseriesAggTests.getOutputAggregateResults(
+        inColl,
+        mergePipeline,
+        /*prepareAction*/ (outColl) => prepareOutputCollectionForMergeOn(outColl));
+
+    // Verifies that the number of measurements is same as expected.
+    assert.eq(actualResults.length, expectedResults.length, actualResults);
+
+    // Verifies that every measurement is same as expected.
+    for (var i = 0; i < expectedResults.length; ++i) {
+        assert.eq(actualResults[i], expectedResults[i], actualResults);
+    }
+};
+
+runSimpleMergeTestCase();
+runMergeOnErrorTestCase();
+runMergeOnTestCase();
+})();
