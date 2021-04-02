e306bd36283d0682842cf3fc2ddbc47363254a81
XueruiFa <xuerui.fa@mongodb.com>
2021-04-02 16:10:39 +0000

    SERVER-55751: Increase election timeout in initial_sync_nodes_contribute_to_liveness_majority.js

--- jstests/replsets/initial_sync_nodes_contribute_to_liveness_majorities.js
+++ jstests/replsets/initial_sync_nodes_contribute_to_liveness_majorities.js
@@ -18,7 +18,7 @@ const name = jsTestName();
 const rst = new ReplSetTest({
     name,
     nodes: [{}, {rsConfig: {priority: 0}}],
-    settings: {electionTimeoutMillis: 1000, heartbeatIntervalMillis: 250},
+    settings: {electionTimeoutMillis: 3000, heartbeatIntervalMillis: 250},
     useBridge: true
 });
 rst.startSet();
@@ -49,9 +49,9 @@ assert.commandWorked(initialSyncSecondary.adminCommand({
 secondary.disconnect(primary);
 
 // Verify that the primary should not step down due to not seeing a quorum. This is because the
-// primary should be receiving heartbeats from the initial sync node.
-assert.throws(() => checkLog.contains(
-                  primary, "can't see a majority of the set, relinquishing primary", 3000));
+// primary should be receiving heartbeats from the initial sync node. We specifically look for the
+// 'Can't see a majority of the set, relinquishing primary' log message.
+assert.throws(() => checkLog.containsJson(primary, 21809, {} /* attrs */, 3000));
 
 assert.eq(primary, rst.getPrimary());
 
