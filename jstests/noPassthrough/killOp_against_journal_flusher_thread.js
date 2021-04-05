05021e1f07eef3b39f7f1a6fa04cc540f94766d0
Dianna Hohensee <dianna.hohensee@mongodb.com>
2021-04-05 21:25:14 +0000

    SERVER-55745 The Fuzzer can run killOp on the JournalFlusher thread and cause it to throw an unexpected error

---
+++ jstests/noPassthrough/killOp_against_journal_flusher_thread.js
@@ -0,0 +1,41 @@
+/**
+ * Tests that killOp is ineffectual against the journal flusher thread.
+ *
+ * @tags: [requires_journaling]
+ */
+
+(function() {
+"use strict";
+
+const rst = new ReplSetTest({nodes: 1});
+rst.startSet({journal: "", journalCommitInterval: 500});
+rst.initiate();
+
+const primary = rst.getPrimary();
+const adminDB = primary.getDB("admin");
+const testColl = primary.getDB("testDB").getCollection("testColl");
+
+// Find the JournalFlusher thread's opID.
+const currentOpResults = adminDB.currentOp();
+const journalFlusherOp = currentOpResults.inprog.filter(function(op) {
+    if (op.desc && op.desc == "JournalFlusher") {
+        jsTestLog("Found JournalFlusher operation: " + tojson(op));
+        return true;
+    }
+});
+assert.eq(1,
+          journalFlusherOp.length,
+          "Unexpectedly found multiple JournalFlusher operations: " + tojson(journalFlusherOp));
+
+// Try to kill the JournalFlusher thread.
+assert.commandWorked(adminDB.killOp(journalFlusherOp[0].opid));
+
+// Do a write with {j:true} writeConcern to hurry up the next journal flush.
+assert.commandWorked(testColl.insert({}, {writeConcern: {j: true}}));
+
+// Whenever the journal flusher tries to run, it should encounter the killOp Interrupt error.
+checkLog.containsJson(primary, 55745001);
+
+rst.stopSet();
+// MongoRunner.stopMongod(conn);
+})();
