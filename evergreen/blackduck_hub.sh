0388d08d440a365e1540cf07b3fbd00dcd98a2c7
Richard Samuels <richard.l.samuels@gmail.com>
2021-04-06 10:26:34 -0400

    SERVER-55626 Extract shell scripts from evergreen.yml part 1

---
+++ evergreen/blackduck_hub.sh
@@ -0,0 +1,12 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+. "$DIR/prelude.sh"
+
+cd src
+
+set -o errexit
+set -o verbose
+
+# activate the virtualenv if it has been set up
+activate_venv
+
+python buildscripts/blackduck_hub.py -v scan_and_report --build_logger=mci.buildlogger --build_logger_task_id=${task_id} --report_file=report.json
---
+++ evergreen/package.sh
@@ -0,0 +1,12 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+. "$DIR/prelude.sh"
+
+cd src
+
+set -o errexit
+set -o verbose
+
+if [ $(find . -name mongocryptd${exe} | wc -l) -eq 1 ]; then
+  # Validate that this build_variant is listed as a known enterprise task for mongocryptd
+  eval PATH=$PATH:$HOME $python ../buildscripts/validate_mongocryptd.py --variant "${build_variant}" ../etc/evergreen.yml
+fi
