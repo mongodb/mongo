ea2a42d6f59108774e2f5564e4abe8de50f0260c
Richard Samuels <richard.l.samuels@gmail.com>
2021-04-06 10:26:34 -0400

    SERVER-55626 Extract shell scripts from evergreen.yml part 1

---
+++ evergreen/gen_feature_flags.sh
@@ -0,0 +1,9 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+. "$DIR/prelude.sh"
+
+cd src
+
+set -o errexit
+set -o verbose
+activate_venv
+$python buildscripts/idl/gen_all_feature_flag_list.py --import-dir src --import-dir src/mongo/db/modules/enterprise/src
---
+++ evergreen/libdeps_run.sh
@@ -0,0 +1,13 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+. "$DIR/prelude.sh"
+
+cd src
+
+set -o errexit
+set -o verbose
+
+activate_venv
+GRAPH_FILE=$(find build -name "libdeps.graphml")
+python buildscripts/libdeps/gacli.py --graph-file $GRAPH_FILE >results.txt
+gzip $GRAPH_FILE
+mv $GRAPH_FILE.gz .
