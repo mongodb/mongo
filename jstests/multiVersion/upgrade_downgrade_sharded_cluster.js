727c948598d69e5b8be555721582bfc50a00457c
Jordi Serra Torrens <jordi.serra-torrens@mongodb.com>
2021-04-06 09:51:04 +0000

    SERVER-55811: upgrade_downgrade_sharded_cluster.js does not properly check the upgrade from lastContinuous (4.9) to latest (5.0)

--- jstests/multiVersion/upgrade_downgrade_sharded_cluster.js
+++ jstests/multiVersion/upgrade_downgrade_sharded_cluster.js
@@ -21,13 +21,13 @@ load("jstests/sharding/libs/find_chunks_util.js");
 // - Entries on config.collections doesn't have the 'distributionMode' and the 'dropped' fields
 //
 // This test must be removed once 5.0 is defined as the lastLTS (SERVER-52630)
-function testDroppedAndDistributionModeFieldsSetup() {
+function testDroppedAndDistributionModeFieldsSetup(oldVersion) {
     let configDB = st.s.getDB('config');
     // Setup sharded collections
     assert.commandWorked(st.s.adminCommand({shardCollection: 'sharded.foo', key: {x: 1}}));
     assert.commandWorked(st.s.adminCommand({shardCollection: 'sharded.bar', key: {x: 1}}));
 
-    {
+    if (oldVersion == lastLTSFCV) {
         var collFoo = configDB.collections.findOne({_id: 'sharded.foo'});
         assert.eq('sharded', collFoo.distributionMode);
         assert.eq(false, collFoo.dropped);
@@ -37,11 +37,13 @@ function testDroppedAndDistributionModeFieldsSetup() {
         assert.eq(false, collBar.dropped);
     }
 
-    // Drop a collection so that it's metadata is left over on the config server's
+    // Drop a collection so that in a 4.4 binary, its metadata is left over on the config server's
     // config.collections as 'dropped: true'
     st.s.getDB('sharded').foo.drop();
-    assert.eq(true, configDB.collections.findOne({_id: 'sharded.foo'}).dropped);
-    assert.neq(null, configDB.collections.findOne({_id: 'sharded.bar'}));
+    if (oldVersion == lastLTSFCV) {
+        assert.eq(true, configDB.collections.findOne({_id: 'sharded.foo'}).dropped);
+        assert.neq(null, configDB.collections.findOne({_id: 'sharded.bar'}));
+    }
 }
 
 function testDroppedAndDistributionModeFieldsChecksAfterUpgrade() {
@@ -207,10 +209,10 @@ function testChunkCollectionUuidFieldChecksAfterFCVDowngrade() {
     } while (cursor.hasNext());
 }
 
-function setupInitialStateOnOldVersion() {
+function setupInitialStateOnOldVersion(oldVersion) {
     assert.commandWorked(st.s.adminCommand({enableSharding: 'sharded'}));
 
-    testDroppedAndDistributionModeFieldsSetup();
+    testDroppedAndDistributionModeFieldsSetup(oldVersion);
     testTimestampFieldSetup();
     testChunkCollectionUuidFieldSetup();
 }
@@ -271,10 +273,12 @@ for (let oldVersion of [lastLTSFCV, lastContinuousFCV]) {
         }
     });
 
+    jsTest.log('oldVersion: ' + oldVersion);
+
     st.configRS.awaitReplication();
 
     // Setup initial conditions
-    setupInitialStateOnOldVersion();
+    setupInitialStateOnOldVersion(oldVersion);
 
     // Upgrade the entire cluster to the latest version.
     jsTest.log('upgrading cluster');
