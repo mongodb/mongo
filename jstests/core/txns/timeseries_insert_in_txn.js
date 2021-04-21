1eadaf115d098198845026aef15b0f24578b46b8
Benety Goh <benety@mongodb.com>
2021-04-20 21:06:31 -0400

    SERVER-54955 add js test for inserting into a time-series collection within a multi-document transaction

---
+++ jstests/core/txns/timeseries_insert_in_txn.js
@@ -0,0 +1,34 @@
+/**
+ * Tests that it is illegal to insert documents into a time-series collection within a transaction.
+ * @tags: [
+ *     assumes_no_implicit_collection_creation_after_drop,
+ *     uses_transactions,
+ *     requires_fcv_49,
+ * ]
+ */
+(function() {
+"use strict";
+
+load("jstests/core/timeseries/libs/timeseries.js");
+
+if (!TimeseriesTest.timeseriesCollectionsEnabled(db.getMongo())) {
+    jsTestLog("Skipping test because the time-series collection feature flag is disabled");
+    return;
+}
+
+// Use a custom database, to avoid conflict with other tests that use the system.js collection.
+const session = db.getMongo().startSession();
+const testDB = session.getDatabase(jsTestName());
+
+// Create time-series collection outside transaction.
+const collForCreate = testDB.getCollection('t');
+collForCreate.drop();
+const timeFieldName = 'time';
+assert.commandWorked(
+    testDB.createCollection(collForCreate.getName(), {timeseries: {timeField: timeFieldName}}));
+
+session.startTransaction();
+const coll = testDB.getCollection(collForCreate.getName());
+assert.commandWorked(coll.insert({_id: 0, [timeFieldName]: ISODate()}));
+assert.commandWorked(session.abortTransaction_forTesting());
+})();
