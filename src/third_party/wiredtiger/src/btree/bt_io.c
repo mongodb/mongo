a83b7f8120c16b346e45a47a39cdf2543f94838a
Luke Chen <luke.chen@mongodb.com>
2021-04-28 16:27:05 +1000

    Import wiredtiger: 38e948b9eb4ab532372ea62d0bc18b927007b079 from branch mongodb-4.4
    ref: 885403077c..38e948b9eb
    for: 4.4.6
    
    WT-7426       Set write generation number when the page image gets created

--- src/third_party/wiredtiger/src/btree/bt_io.c
+++ src/third_party/wiredtiger/src/btree/bt_io.c
@@ -303,20 +303,7 @@ __wt_bt_write(WT_SESSION_IMPL *session, WT_ITEM *buf, uint8_t *addr, size_t *add
     if (encrypted)
         F_SET(dsk, WT_PAGE_ENCRYPTED);
 
-    /*
-     * We increment the block's write generation so it's easy to identify newer versions of blocks
-     * during salvage. (It's common in WiredTiger, at least for the default block manager, for
-     * multiple blocks to be internally consistent with identical first and last keys, so we need a
-     * way to know the most recent state of the block. We could check which leaf is referenced by a
-     * valid internal page, but that implies salvaging internal pages, which I don't want to do, and
-     * it's not as good anyway, because the internal page may not have been written after the leaf
-     * page was updated. So, write generations it is.
-     *
-     * Nothing is locked at this point but two versions of a page with the same generation is pretty
-     * unlikely, and if we did, they're going to be roughly identical for the purposes of salvage,
-     * anyway.
-     */
-    dsk->write_gen = ++btree->write_gen;
+    WT_ASSERT(session, (dsk->write_gen != 0 && dsk->write_gen > btree->base_write_gen));
 
     /*
      * Checksum the data if the buffer isn't compressed or checksums are configured.
