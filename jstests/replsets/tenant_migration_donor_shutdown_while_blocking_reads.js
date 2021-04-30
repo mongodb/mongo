202937fa78084e21b17af47d1dc2af1b9be5a38f
Cheahuychou Mao <mao.cheahuychou@gmail.com>
2021-04-30 17:53:58 +0000

    SERVER-56354 Ensure that the future chain in checkIfCanReadOrBlock gets interrupted when the command's opCtx is marked as killed
    (cherry picked from commit 589020d961aadba1d38ea88d0b3f4b1878bf3be0)

---
+++ jstests/replsets/tenant_migration_donor_shutdown_while_blocking_reads.js
@@ -0,0 +1,67 @@
+/**
+ * Tests that tenant migration donor can peacefully shut down when there are reads being blocked due
+ * to an in-progress migration.
+ *
+ * @tags: [requires_fcv_47, requires_majority_read_concern, incompatible_with_eft,
+ * incompatible_with_windows_tls, incompatible_with_macos, requires_persistence]
+ */
+
+(function() {
+"use strict";
+
+load("jstests/libs/parallelTester.js");
+load("jstests/libs/fail_point_util.js");
+load("jstests/libs/uuid_util.js");
+load("jstests/replsets/libs/tenant_migration_test.js");
+load("jstests/replsets/libs/tenant_migration_util.js");
+
+const tenantMigrationTest = new TenantMigrationTest({name: jsTestName()});
+if (!tenantMigrationTest.isFeatureFlagEnabled()) {
+    jsTestLog("Skipping test because the tenant migrations feature flag is disabled");
+    return;
+}
+
+const kTenantId = "testTenantId";
+const kDbName = kTenantId + "_testDb";
+const kCollName = "testColl";
+
+const donorRst = tenantMigrationTest.getDonorRst();
+const donorPrimary = tenantMigrationTest.getDonorPrimary();
+const testDb = donorPrimary.getDB(kDbName);
+
+assert.commandWorked(testDb.runCommand({insert: kCollName, documents: [{_id: 0}]}));
+
+const migrationId = UUID();
+const migrationOpts = {
+    migrationIdString: extractUUIDFromObject(migrationId),
+    tenantId: kTenantId,
+};
+
+let fp = configureFailPoint(donorPrimary, "pauseTenantMigrationBeforeLeavingBlockingState");
+assert.commandWorked(tenantMigrationTest.startMigration(
+    migrationOpts, false /* retryOnRetryableErrors */, false /* automaticForgetMigration */));
+
+fp.wait();
+const donorDoc =
+    donorPrimary.getCollection(TenantMigrationTest.kConfigDonorsNS).findOne({tenantId: kTenantId});
+assert.neq(null, donorDoc);
+
+let readThread = new Thread((host, dbName, collName, afterClusterTime) => {
+    const node = new Mongo(host);
+    const db = node.getDB(dbName);
+    const res = db.runCommand({
+        find: collName,
+        readConcern: {afterClusterTime: Timestamp(afterClusterTime.t, afterClusterTime.i)}
+    });
+    assert.commandFailedWithCode(res, ErrorCodes.InterruptedAtShutdown);
+}, donorPrimary.host, kDbName, kCollName, donorDoc.blockTimestamp);
+readThread.start();
+
+// Shut down the donor after the read starts blocking.
+assert.soon(() => TenantMigrationUtil.getNumBlockedReads(donorPrimary, kTenantId) == 1);
+donorRst.stop(donorPrimary);
+readThread.join();
+
+donorRst.stopSet();
+tenantMigrationTest.stop();
+})();
