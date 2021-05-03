d7757d755daa3f02295e2d69ad81f5faf193f27c
Luke Chen <luke.chen@mongodb.com>
2021-05-04 09:58:16 +1000

    Import wiredtiger: bcb90a271ce721d53ce49f1d805f52111f67a5e2 from branch mongodb-5.0
    ref: e3efbee8a3..bcb90a271c
    for: 5.0.0
    
    WT-7345       Update incorrect copyright notices format

--- src/third_party/wiredtiger/build_cmake/configs/compile_test/pthread_cond_monotonic_test.c
+++ src/third_party/wiredtiger/build_cmake/configs/compile_test/pthread_cond_monotonic_test.c
@@ -1,3 +1,11 @@
+/* 
+ * Copyright (c) 2014-present MongoDB, Inc.
+ * Copyright (c) 2008-2014 WiredTiger, Inc.
+ *	All rights reserved.
+ *
+ * See the file LICENSE for redistribution information.
+ */
+
 #include <errno.h>
 #include <pthread.h>
 #include <stdlib.h>
