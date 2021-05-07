bf895e6c5d415e1687bfd879e073b231cd2200e8
Luke Chen <luke.chen@mongodb.com>
2021-05-07 12:24:26 +1000

    Import wiredtiger: 073ad6d27fd91e061a19f987c4167e9c09239a66 from branch mongodb-5.0
    ref: c866a61e2f..073ad6d27f
    for: 5.0.0
    
    WT-7485       Coverity analysis defect 120018: Resource leak

--- src/third_party/wiredtiger/src/tiered/tiered_config.c
+++ src/third_party/wiredtiger/src/tiered/tiered_config.c
@@ -141,7 +141,14 @@ __wt_tiered_bucket_config(
     *bstoragep = new;
 
 done:
+    if (0) {
 err:
+        if (new != NULL) {
+            __wt_free(session, new->bucket);
+            __wt_free(session, new->bucket_prefix);
+        }
+        __wt_free(session, new);
+    }
     __wt_spin_unlock(session, &conn->storage_lock);
     return (ret);
 }
