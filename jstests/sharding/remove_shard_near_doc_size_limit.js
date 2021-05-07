78d5ecce93e88f754ef1bbe11e4c5f9367c2983a
Randolph Tan <randolph@10gen.com>
2021-05-07 15:17:47 +0000

    SERVER-56760 Exclude remove_shard_near_doc_size_limit.js from config stepdown suites

--- jstests/sharding/remove_shard_near_doc_size_limit.js
+++ jstests/sharding/remove_shard_near_doc_size_limit.js
@@ -4,7 +4,7 @@
  * consider adding them together in a batch. It ensures that the proper document size is considered
  * and that we can still migrate when calling removeShard.
  *
- * @tags: [requires_fcv_44, multiversion_incompatible]
+ * @tags: [requires_fcv_44, multiversion_incompatible, does_not_support_config_stepdowns]
  */
 
 (function() {
