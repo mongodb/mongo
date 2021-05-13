541015db8073ba776b122223ffd7f402247ad37b
Richard Samuels <richard.l.samuels@gmail.com>
2021-05-13 12:09:40 -0400

    SERVER-55626 Fix serial_run

--- evergreen/resmoke_tests_execute.sh
+++ evergreen/resmoke_tests_execute.sh
@@ -89,7 +89,7 @@ if [[ ${disable_unit_tests} = "false" && ! -f ${skip_tests} ]]; then
 
   # Reduce the JSHeapLimit for the serial_run task task on Code Coverage builder variant.
   if [[ "${build_variant}" = "enterprise-rhel-80-64-bit-coverage" && "${task_name}" = "serial_run" ]]; then
-    extra_args="$extra_args --mongodSetParameter {'jsHeapLimitMB':10}"
+    extra_args="$extra_args --mongodSetParameter \"{'jsHeapLimitMB':10}\""
   fi
 
   path_value="$PATH"
