5c7e3e96ba5a42ea66be971a58cbe3706b1baf1a
Chenhao Qu <chenhao.qu@mongodb.com>
2021-05-13 00:42:22 +0000

    Import wiredtiger: 675444e2930b6d1a2327bbbcb6075d150123105e from branch mongodb-5.0
    ref: 712bd4d2eb..675444e293
    for: 5.0.0
    
    WT-7486       Coverity explcit NULL dereferenced

--- src/third_party/wiredtiger/src/btree/bt_curnext.c
+++ src/third_party/wiredtiger/src/btree/bt_curnext.c
@@ -679,6 +679,8 @@ __wt_btcur_next_prefix(WT_CURSOR_BTREE *cbt, WT_ITEM *prefix, bool truncating)
         page = cbt->ref == NULL ? NULL : cbt->ref->page;
 
         if (F_ISSET(cbt, WT_CBT_ITERATE_APPEND)) {
+            /* The page cannot be NULL if the above flag is set. */
+            WT_ASSERT(session, page != NULL);
             switch (page->type) {
             case WT_PAGE_COL_FIX:
                 ret = __cursor_fix_append_next(cbt, newpage, restart);
