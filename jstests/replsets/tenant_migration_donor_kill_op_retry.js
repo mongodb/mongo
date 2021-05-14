faefb37b303f11e68bee1b0eb08fef907a237fbe
Vishnu Kaushik <vishnu.kaushik@mongodb.com>
2021-05-14 15:10:12 +0000

    SERVER-52343 add incompatible_with_macos tag to failing tests

--- jstests/replsets/tenant_migration_donor_kill_op_retry.js
+++ jstests/replsets/tenant_migration_donor_kill_op_retry.js
@@ -2,7 +2,7 @@
  * Tests that the donor will retry its steps if its OperationContext is interrupted by a killOp.
  *
  * @tags: [requires_fcv_47, requires_majority_read_concern, incompatible_with_eft,
- * incompatible_with_windows_tls]
+ * incompatible_with_windows_tls, incompatible_with_macos, requires_persistence]
  */
 
 (function() {
