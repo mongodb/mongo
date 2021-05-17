32e142329ce15c2e923dbe285f34d75d9285e771
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2021-05-17 15:53:41 +0000

    SERVER-53841 Additional logging for oplog_rollover.js

--- jstests/replsets/oplog_rollover.js
+++ jstests/replsets/oplog_rollover.js
@@ -17,7 +17,7 @@ function doTest(storageEngine) {
         nodeOptions: {
             syncdelay: 1,
             setParameter: {
-                logComponentVerbosity: tojson({storage: 1}),
+                logComponentVerbosity: tojson({storage: 2}),
                 'failpoint.hangOplogCapMaintainerThread': tojson({mode: 'alwaysOn'})
             }
         },
