2af7ba48290bc0cbeecde0540282dbc7a8d4daab
Siran Wang <siran.wang@mongodb.com>
2021-05-21 02:43:34 -0400

    SERVER-56911 improve lint_fuzzer_sanity_patch to check generated tests

--- evergreen/functions/added_and_modified_patch_files_get.sh
+++ evergreen/functions/added_and_modified_patch_files_get.sh
@@ -6,7 +6,7 @@ cd src
 set -o verbose
 set -o errexit
 
-git diff HEAD --name-only --line-prefix="${workdir}/src/" --diff-filter=d >>modified_and_created_patch_files.txt
+git diff --name-only origin/${branch_name}... --line-prefix="${workdir}/src/" --diff-filter=d >>modified_and_created_patch_files.txt
 if [ -d src/mongo/db/modules/enterprise ]; then
   pushd src/mongo/db/modules/enterprise
   git diff HEAD --name-only --line-prefix="${workdir}/src/src/mongo/db/modules/enterprise/" --diff-filter=d >>~1/modified_and_created_patch_files.txt
--- evergreen/lint_fuzzer_sanity_patch.sh
+++ evergreen/lint_fuzzer_sanity_patch.sh
@@ -8,6 +8,25 @@ set -o verbose
 
 add_nodejs_to_path
 
-# Run parse-jsfiles on 50 files at a time with 32 processes in parallel.
-# Grep returns 1 if it fails to find a match.
-(grep -v "\.tpl\.js$" modified_and_created_patch_files.txt | grep "\.js$" || true) | xargs -P 32 -L 50 npm run --prefix jstestfuzz parse-jsfiles --
+mkdir -p jstestfuzzinput jstestfuzzoutput
+
+indir="$(pwd)/jstestfuzzinput"
+outdir="$(pwd)/jstestfuzzoutput"
+
+# Grep all the js files from modified_and_created_patch_files.txt and put them into $indir.
+(grep -v "\.tpl\.js$" modified_and_created_patch_files.txt | grep "\.js$" | xargs -I {} cp {} $indir || true)
+
+# Count the number of files in $indir.
+if [[ "$(ls -A $indir)" ]]; then
+  num_files=$(ls -A $indir | wc -l)
+
+  # Only fetch 50 files to generate jsfuzz testing files.
+  if [[ $num_files -gt 50 ]]; then
+    num_files=50
+  fi
+
+  npm run --prefix jstestfuzz jstestfuzz -- --jsTestsDir $indir --out $outdir --numSourceFiles $num_files --numGeneratedFiles 50
+
+  # Run parse-jsfiles on 50 files at a time with 32 processes in parallel.
+  ls -1 -d $outdir/* | xargs -P 32 -L 50 npm run --prefix jstestfuzz parse-jsfiles --
+fi
