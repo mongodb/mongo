cb27ca055e55224523bb73b48c83bf4fb4e236f1
Maddie Zechar <mez2113@columbia.edu>
2021-05-24 16:37:46 +0000

    SERVER-57258: Add deprecation msg for mapReduce()

--- src/mongo/s/commands/cluster_map_reduce_agg.cpp
+++ src/mongo/s/commands/cluster_map_reduce_agg.cpp
@@ -26,7 +26,7 @@
  *    exception statement from all source files in the program, then also delete
  *    it in the license file.
  */
-
+#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kCommand
 #include "mongo/platform/basic.h"
 
 #include "mongo/bson/bsonobj.h"
@@ -46,6 +46,7 @@
 #include "mongo/db/query/cursor_response.h"
 #include "mongo/db/query/explain_common.h"
 #include "mongo/db/query/map_reduce_output_format.h"
+#include "mongo/logv2/log.h"
 #include "mongo/s/catalog_cache.h"
 #include "mongo/s/cluster_commands_helpers.h"
 #include "mongo/s/commands/cluster_map_reduce_agg.h"
@@ -54,7 +55,7 @@
 
 namespace mongo {
 namespace {
-
+Rarely _sampler;
 auto makeExpressionContext(OperationContext* opCtx,
                            const MapReduceCommandRequest& parsedMr,
                            const ChunkManager& cm,
@@ -150,6 +151,12 @@ bool runAggregationMapReduce(OperationContext* opCtx,
     auto hasOutDB = parsedMr.getOutOptions().getDatabaseName();
     auto resolvedOutNss = NamespaceString{hasOutDB ? *hasOutDB : parsedMr.getNamespace().db(),
                                           parsedMr.getOutOptions().getCollectionName()};
+
+    if (_sampler.tick()) {
+        LOGV2_WARNING(5725800,
+                      "The map reduce command is deprecated. For more information, see "
+                      "https://docs.mongodb.com/manual/core/map-reduce/");
+    }
 
     if (parsedMr.getOutOptions().getOutputType() != OutputType::InMemory) {
         involvedNamespaces.insert(resolvedOutNss);
