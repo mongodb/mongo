382f72294154f55ff944f7cc5a1aeff3dfe55346
Mark Benvenuto <mark.benvenuto@mongodb.com>
2021-05-24 12:12:26 -0400

    SERVER-56965 blackduck_scanner should only check for security vulnerabilties on non-master branches
    (cherry picked from commit dab9cd94768cf8f3caa4fecf8820f6f1029317c5)

--- evergreen/blackduck_hub.sh
+++ evergreen/blackduck_hub.sh
@@ -9,4 +9,9 @@ set -o verbose
 # activate the virtualenv if it has been set up
 activate_venv
 
-python buildscripts/blackduck_hub.py -v scan_and_report --build_logger=mci.buildlogger --build_logger_task_id=${task_id} --report_file=report.json
+additional_args=
+if [ "$branch_name" != "master" ]; then
+  additional_args="--vulnerabilities_only"
+fi
+
+python buildscripts/blackduck_hub.py -v scan_and_report --build_logger=mci.buildlogger --build_logger_task_id=${task_id} --report_file=report.json $additional_args
