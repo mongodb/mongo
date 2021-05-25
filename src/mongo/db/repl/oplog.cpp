49c2229a09ae8eca27e93c8424dff80a445b830a
Arun Banala <arun.banala@mongodb.com>
2021-05-25 10:57:38 +0100

    SERVER-57170 Fix FCV check for applying $v:2 oplog entries

--- src/mongo/db/repl/oplog.cpp
+++ src/mongo/db/repl/oplog.cpp
@@ -1418,7 +1418,10 @@ Status applyOperation_inlock(OperationContext* opCtx,
             if (updateMod.type() == write_ops::UpdateModification::Type::kDelta) {
                 // If we are validating features as primary, only allow $v:2 delta entries if we are
                 // at FCV 4.7 or newer to prevent them from being written to the oplog.
-                if (serverGlobalParams.validateFeaturesAsPrimary.load()) {
+                //
+                // TODO SERVER-57176: Remove the check on 'kRecovering'.
+                if (serverGlobalParams.validateFeaturesAsPrimary.load() &&
+                    mode != OplogApplication::Mode::kRecovering) {
                     uassert(4773100,
                             "Delta oplog entries may not be used in FCV below 4.7",
                             serverGlobalParams.featureCompatibility.isGreaterThanOrEqualTo(
