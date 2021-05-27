09617a6bca8ae249435b904155b721cf293877e4
Jack Mulrow <jack.mulrow@mongodb.com>
2021-05-27 15:21:06 +0000

    SERVER-48711 Remove FCV guard from transient_txn_error_labels_with_write_concern.js

--- jstests/sharding/transient_txn_error_labels_with_write_concern.js
+++ jstests/sharding/transient_txn_error_labels_with_write_concern.js
@@ -11,8 +11,6 @@ load("jstests/libs/fail_point_util.js");
 load("jstests/libs/write_concern_util.js");
 load("jstests/replsets/rslib.js");
 
-const clusterInFCV44 = jsTestOptions().mongosBinVersion != 'last-lts';
-
 const dbName = "test";
 const collName = "transient_txn_error_labels_with_write_concern";
 
@@ -82,30 +80,26 @@ function runNoSuchTransactionTests(cmd, cmdName) {
     assert(!res.hasOwnProperty("writeConcernError"), res);
     assert.eq(res["errorLabels"], ["TransientTransactionError"], res);
 
-    // Failpoint failTransactionNoopWrite doesn't exist in mongo versions <= 4.4
-    if (clusterInFCV44) {
-        jsTest.log(
-            "If the noop write for NoSuchTransaction cannot occur, the error is not transient");
+    jsTest.log("If the noop write for NoSuchTransaction cannot occur, the error is not transient");
 
-        const failpoint = configureFailPoint(primary, "failTransactionNoopWrite");
+    const failpoint = configureFailPoint(primary, "failTransactionNoopWrite");
 
-        // The server will attempt to perform a noop write, since the command returns
-        // NoSuchTransaction. The noop write will time out because of the failpoint.
-        // This should not be a TransientTransactionError, since the server has not successfully
-        // replicated a write to confirm that it is primary.
-        // Use a txnNumber that is one higher than the server has tracked.
-        res = sessionDb.adminCommand(Object.assign(Object.assign({}, cmd), {
-            txnNumber: NumberLong(session.getTxnNumber_forTesting() + 1),
-            autocommit: false,
-            writeConcern: writeConcernMajority,
-            maxTimeMS: 1000
-        }));
+    // The server will attempt to perform a noop write, since the command returns
+    // NoSuchTransaction. The noop write will time out because of the failpoint.
+    // This should not be a TransientTransactionError, since the server has not successfully
+    // replicated a write to confirm that it is primary.
+    // Use a txnNumber that is one higher than the server has tracked.
+    res = sessionDb.adminCommand(Object.assign(Object.assign({}, cmd), {
+        txnNumber: NumberLong(session.getTxnNumber_forTesting() + 1),
+        autocommit: false,
+        writeConcern: writeConcernMajority,
+        maxTimeMS: 1000
+    }));
 
-        failpoint.off();
+    failpoint.off();
 
-        assert.commandFailedWithCode(res, ErrorCodes.MaxTimeMSExpired);
-        assert(!res.hasOwnProperty("errorLabels"));
-    }
+    assert.commandFailedWithCode(res, ErrorCodes.MaxTimeMSExpired);
+    assert(!res.hasOwnProperty("errorLabels"));
 
     rst.awaitReplication();
 }
