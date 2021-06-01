b17e0a99632872e44684700978d55c01be1a7895
Richard Samuels <richard.l.samuels@gmail.com>
2021-06-01 17:49:24 -0400

    SERVER-57398 Extract evergreen yaml linter into its own repo

--- buildscripts/evglint/model.py
+++
@@ -1,5 +0,0 @@
-"""Type annotations for evglint."""
-from typing import Callable, List
-
-LintRule = Callable[[dict], List["LintError"]]
-LintError = str
--- buildscripts/yamllinters.sh
+++ buildscripts/yamllinters.sh
@@ -4,4 +4,4 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR/../"
 
 find buildscripts etc jstests -name '*.y*ml' -exec yamllint -c etc/yamllint_config.yml {} +
-python3 buildscripts/evglint
+python -m evergreen_lint -c ./etc/evergreen_lint.yml lint
