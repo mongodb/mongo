081ac5bb3550d83168ae19e74d0eb900e209151d
Luis Osta <luis.osta@mongodb.com>
2021-06-02 20:37:23 +0000

    SERVER-56902 Remove _abortReason member variable from ReshardingRecipientService
    (cherry picked from commit e812f2356243c2a20e2869ca9d06869ba8129ee4)

--- src/mongo/db/s/resharding/resharding_recipient_service.h
+++ src/mongo/db/s/resharding/resharding_recipient_service.h
@@ -209,7 +209,7 @@ private:
                                   boost::optional<mongo::Date_t> configStartTime);
 
     // Removes the local recipient document from disk.
-    void _removeRecipientDocument();
+    void _removeRecipientDocument(bool aborted);
 
     std::unique_ptr<ReshardingDataReplicationInterface> _makeDataReplication(
         OperationContext* opCtx, bool cloningDone);
@@ -262,10 +262,6 @@ private:
 
     // Canceled when there is an unrecoverable error or stepdown.
     boost::optional<CancellationSource> _abortSource;
-
-    // Contains the status with which the operation was aborted.
-    // TODO SERVER-56902: Remove the _abortReason completely.
-    boost::optional<Status> _abortReason;
 
     // The identifier associated to the recoverable critical section.
     const BSONObj _critSecReason;
