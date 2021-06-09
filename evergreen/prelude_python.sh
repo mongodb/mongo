f623f9426b90077f16bdb5e1cd33ba9b8a102ae5
Daniel Moody <daniel.moody@mongodb.com>
2021-06-09 09:25:23 -0500

    SERVER-55446 added rosetta and arm64 macos builders to evergreen
    (cherry picked from commit 02feb4df5a0e4bf9ac915aaafd8842ce9c738d73)

--- evergreen/prelude_python.sh
+++ evergreen/prelude_python.sh
@@ -1,5 +1,13 @@
 if [ "Windows_NT" = "$OS" ]; then
   python='/cygdrive/c/python/python37/python.exe'
 else
-  python="/opt/mongodbtoolchain/v3/bin/python3"
+  if [ -f /opt/mongodbtoolchain/v3/bin/python3 ]; then
+    python="/opt/mongodbtoolchain/v3/bin/python3"
+  elif [ -f "$(which python3)" ]; then
+    echo "Could not find mongodbtoolchain python, using system python $(which python3)" >2
+    python=$(which python3)
+  else
+    echo "Could not find python3." >2
+    return 1
+  fi
 fi
