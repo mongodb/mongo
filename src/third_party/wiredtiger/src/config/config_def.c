ba289562f3e97028cbff7e466f1c5e7a4d3129c5
Luke Chen <luke.chen@mongodb.com>
2021-06-09 17:19:03 +1000

    Import wiredtiger: 8a3206b68bd7ef048c5805ef3396c408791d668b from branch mongodb-5.0
    ref: 92379db4f8..8a3206b68b
    for: 5.0.0-rc1
    
    WT-7651       Add synchronization for flush_tier calls

--- src/third_party/wiredtiger/src/config/config_def.c
+++ src/third_party/wiredtiger/src/config/config_def.c
@@ -325,6 +325,7 @@ static const WT_CONFIG_CHECK confchk_WT_SESSION_drop[] = {
 
 static const WT_CONFIG_CHECK confchk_WT_SESSION_flush_tier[] = {
   {"flush_timestamp", "string", NULL, NULL, NULL, 0}, {"force", "boolean", NULL, NULL, NULL, 0},
+  {"lock_wait", "boolean", NULL, NULL, NULL, 0},
   {"sync", "string", NULL, "choices=[\"off\",\"on\"]", NULL, 0}, {NULL, NULL, NULL, NULL, NULL, 0}};
 
 static const WT_CONFIG_CHECK confchk_WT_SESSION_join[] = {
@@ -1185,8 +1186,8 @@ static const WT_CONFIG_ENTRY config_entries[] = {{"WT_CONNECTION.add_collator", 
     "checkpoint_wait=true,force=false,lock_wait=true,"
     "remove_files=true",
     confchk_WT_SESSION_drop, 4},
-  {"WT_SESSION.flush_tier", "flush_timestamp=,force=false,sync=on", confchk_WT_SESSION_flush_tier,
-    3},
+  {"WT_SESSION.flush_tier", "flush_timestamp=,force=false,lock_wait=true,sync=on",
+    confchk_WT_SESSION_flush_tier, 4},
   {"WT_SESSION.join",
     "bloom_bit_count=16,bloom_false_positives=false,"
     "bloom_hash_count=8,compare=\"eq\",count=,operation=\"and\","
