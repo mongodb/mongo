ba7766f8184a756c8339ce973d19ee6dc8ceef72
Luke Chen <luke.chen@mongodb.com>
2021-06-09 17:19:07 +1000

    Import wiredtiger: ccb1fc26d51419846e3185a7c41e5c3052b5bb07 from branch mongodb-5.0
    ref: 8a3206b68b..ccb1fc26d5
    for: 5.0.0-rc1
    
    WT-7656       Destroy tiered condvar after thread is joined

--- src/third_party/wiredtiger/src/conn/conn_tiered.c
+++ src/third_party/wiredtiger/src/conn/conn_tiered.c
@@ -658,7 +658,6 @@ __wt_tiered_storage_destroy(WT_SESSION_IMPL *session)
             __wt_tiered_work_free(session, entry);
         }
     }
-    __wt_cond_destroy(session, &conn->tiered_cond);
     if (conn->tiered_session != NULL) {
         WT_TRET(__wt_session_close_internal(conn->tiered_session));
         conn->tiered_session = NULL;
@@ -671,8 +670,10 @@ __wt_tiered_storage_destroy(WT_SESSION_IMPL *session)
         WT_TRET(__wt_thread_join(session, &conn->tiered_mgr_tid));
         conn->tiered_mgr_tid_set = false;
     }
+    /* Destroy all condition variables after threads have stopped. */
+    __wt_cond_destroy(session, &conn->tiered_cond);
     __wt_cond_destroy(session, &conn->tiered_mgr_cond);
-    /* This condition variable is last because any internal thread could be using it.  */
+    /* The flush condition variable must be last because any internal thread could be using it.  */
     __wt_cond_destroy(session, &conn->flush_cond);
 
     if (conn->tiered_mgr_session != NULL) {
