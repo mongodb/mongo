a6b69570fe28c9fd71f7be3976d5cbeb78be9243
Luke Chen <luke.chen@mongodb.com>
2021-06-10 16:47:03 +1000

    Import wiredtiger: fd77581d2be7553e5ddc025143bc12e2b0ab035c from branch mongodb-5.0
    ref: 5904d74bfd..fd77581d2b
    for: 5.1.0
    
    WT-7231       Add CMake build and tests into Evergreen

---
+++ src/third_party/wiredtiger/build_cmake/toolchains/cl.cmake
@@ -0,0 +1,13 @@
+#
+# Public Domain 2014-present MongoDB, Inc.
+# Public Domain 2008-2014 WiredTiger, Inc.
+#  All rights reserved.
+#
+# See the file LICENSE for redistribution information.
+#
+
+cmake_minimum_required(VERSION 3.10.0)
+
+set(CMAKE_C_COMPILER "cl.exe")
+set(CMAKE_CXX_COMPILER "cl.exe")
+set(CMAKE_ASM_COMPILER "cl.exe")
