51bfdee7fae85ad03fcb1f6b17d24c8e8ff93ad5
Sergi Mateo Bellido <sergi.mateo-bellido@mongodb.com>
2021-06-10 19:17:22 +0000

    SERVER-57606 Define the default read concern to available in sharding_rs2.js
    (cherry picked from commit b962214f9061c94078f6195e80b7109e8f2552f8)

--- jstests/sharding/sharding_rs2.js
+++ jstests/sharding/sharding_rs2.js
@@ -38,10 +38,15 @@ var db = s.getDB("test");
 var t = db.foo;
 
 assert.commandWorked(s.s0.adminCommand({enablesharding: "test"}));
-// The default WC is majority and fsyncLock will prevent satisfying any majority writes.
-assert.commandWorked(s.s.adminCommand(
-    {setDefaultRWConcern: 1, defaultWriteConcern: {w: 1}, writeConcern: {w: "majority"}}));
-
+// The default WC is 'majority' and fsyncLock will prevent satisfying any majority writes.
+// The default RC is 'local' but fsync will block any refresh on secondaries, that's why RC is
+// defaulted to 'available'.
+assert.commandWorked(s.s.adminCommand({
+    setDefaultRWConcern: 1,
+    defaultReadConcern: {level: "available"},
+    defaultWriteConcern: {w: 1},
+    writeConcern: {w: "majority"}
+}));
 s.ensurePrimaryShard('test', s.shard0.shardName);
 
 // -------------------------------------------------------------------------------------------
