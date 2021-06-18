c855011857e09e31498acf8a45fab630589aaf7c
Paolo Polato <paolo.polato@mongodb.com>
2021-06-18 07:49:12 +0000

    SERVER-56307 fix the convergence criteria to end the catchup phase
    (cherry picked from commit 49209fc34e19bbb15405b0927e38ff3e7d9e9dc5)

--- src/mongo/db/s/migration_destination_manager.cpp
+++ src/mongo/db/s/migration_destination_manager.cpp
@@ -286,7 +286,7 @@ MigrationDestinationManager::State MigrationDestinationManager::getState() const
     return _state;
 }
 
-void MigrationDestinationManager::setState(State newState) {
+void MigrationDestinationManager::_setState(State newState) {
     stdx::lock_guard<Latch> sl(_mutex);
     _state = newState;
     _stateChangedCV.notify_all();
@@ -1065,7 +1065,7 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* outerOpCtx) {
     repl::OpTime lastOpApplied;
     {
         // 3. Initial bulk clone
-        setState(CLONE);
+        _setState(CLONE);
 
         _sessionMigration->start(opCtx->getServiceContext());
 
@@ -1175,7 +1175,7 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* outerOpCtx) {
 
     {
         // 4. Do bulk of mods
-        setState(CATCHUP);
+        _setState(CATCHUP);
 
         while (true) {
             auto res = uassertStatusOKWithContext(
@@ -1272,7 +1272,7 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* outerOpCtx) {
 
     {
         // 5. Wait for commit
-        setState(STEADY);
+        _setState(STEADY);
 
         bool transferAfterCommit = false;
         while (getState() == STEADY || getState() == COMMIT_START) {
@@ -1341,7 +1341,7 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* outerOpCtx) {
         return;
     }
 
-    setState(DONE);
+    _setState(DONE);
 
     timing.done(6);
     migrateThreadHangAtStep6.pauseWhileSet();
