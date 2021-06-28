b65e1b4c7c4a6bb08ce6643aeb7b04476bff4f57
Luis Osta <luis.osta@mongodb.com>
2021-06-28 13:08:55 +0000

    SERVER-57665 Remove the unused disallowWritesForResharding() method

--- src/mongo/db/s/collection_metadata.h
+++ src/mongo/db/s/collection_metadata.h
@@ -83,14 +83,6 @@ public:
     void throwIfReshardingInProgress(NamespaceString const& nss) const;
 
     /**
-     * The caller should disallow writes when
-     *      1. The coordinator is in the mirroring state, OR
-     *      2. The coordinator is in the decision persisted state, but the UUID is still the
-     *         original UUID.
-     */
-    bool disallowWritesForResharding(const UUID& currentCollectionUUID) const;
-
-    /**
      * Returns the current shard version for the collection or UNSHARDED if it is not sharded.
      *
      * Will throw ShardInvalidatedForTargeting if _thisShardId is marked as stale by
