0ef0c929cd583f5039a1d13d3b212a18eda27c2c
Hana Pearlman <hana.pearlman@mongodb.com>
2021-07-07 11:52:03 +0000

    SERVER-58220: Do a local read when the collection stores local resharding oplog buffer

--- src/mongo/db/pipeline/sharded_agg_helpers.cpp
+++ src/mongo/db/pipeline/sharded_agg_helpers.cpp
@@ -1274,9 +1274,11 @@ std::unique_ptr<Pipeline, PipelineDeleter> attachCursorToPipeline(Pipeline* owne
     return shardVersionRetry(
         expCtx->opCtx, catalogCache, expCtx->ns, "targeting pipeline to attach cursors"_sd, [&]() {
             auto pipelineToTarget = pipeline->clone();
-            if (!allowTargetingShards || expCtx->ns.db() == "local") {
+            if (!allowTargetingShards || expCtx->ns.db() == "local" ||
+                expCtx->ns.isReshardingLocalOplogBufferCollection()) {
                 // If the db is local, this may be a change stream examining the oplog. We know the
-                // oplog (and any other local collections) will not be sharded.
+                // oplog, other local collections, and collections that store the local resharding
+                // oplog buffer won't be sharded.
                 return expCtx->mongoProcessInterface->attachCursorSourceToPipelineForLocalRead(
                     pipelineToTarget.release());
             }
