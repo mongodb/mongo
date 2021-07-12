a0bf25607170084eca1452cc429d47e7e7c20275
Luke Chen <luke.chen@mongodb.com>
2021-07-12 18:03:17 +1000

    Import wiredtiger: ac3cda66d4a88e608bacef3872168f9db800cd34 from branch mongodb-master
    ref: 36982f9b75..ac3cda66d4
    for: 5.1.0
    
    WT-7813       Stop inserting to history store if we see a prepared update

--- src/third_party/wiredtiger/src/history/hs_rec.c
+++ src/third_party/wiredtiger/src/history/hs_rec.c
@@ -523,13 +523,13 @@ __wt_hs_insert_updates(WT_SESSION_IMPL *session, WT_PAGE *page, WT_MULTI *multi,
         hs_inserted = squashed = false;
 
         /*
-         * Flush the updates on stack. Stopping once we run out or we reach the onpage upd start
-         * time point, we can squash updates with the same start time point as the onpage update
-         * away.
+         * Flush the updates on stack. Stopping once we run out or we reach the onpage update or we
+         * encounter a prepared update.
          */
         modify_cnt = 0;
-        for (; updates.size > 0; tmp = full_value, full_value = prev_full_value,
-                                 prev_full_value = tmp, upd = prev_upd) {
+        for (; updates.size > 0 && upd->prepare_state != WT_PREPARE_INPROGRESS;
+             tmp = full_value, full_value = prev_full_value, prev_full_value = tmp,
+             upd = prev_upd) {
             /* We should never insert the onpage value to the history store. */
             WT_ASSERT(session, upd != list->onpage_upd);
             WT_ASSERT(session, upd->type == WT_UPDATE_STANDARD || upd->type == WT_UPDATE_MODIFY);
