5ca12ef49c2bb07199cc236f019cd889775cee24
Luke Chen <luke.chen@mongodb.com>
2021-07-16 15:30:37 +1000

    Import wiredtiger: c85ab8d5c2ea820f709c5750b7c92ca2e62f3adc from branch mongodb-master
    ref: 2f0c065d26..c85ab8d5c2
    for: 5.1.0
    
    WT-7778       Fix null dereferencing, and return of incorrect allocation size

--- src/third_party/wiredtiger/src/meta/meta_ckpt.c
+++ src/third_party/wiredtiger/src/meta/meta_ckpt.c
@@ -705,6 +705,9 @@ __wt_meta_ckptlist_get(
 
     *ckptbasep = NULL;
 
+    if (allocated != NULL)
+        *allocated = 0;
+
     btree = S2BT(session);
     config = NULL;
 
@@ -717,9 +720,11 @@ __wt_meta_ckptlist_get(
      */
     if (!WT_IS_METADATA(session->dhandle) && btree->ckpt != NULL) {
         *ckptbasep = btree->ckpt;
-        *allocated = btree->ckpt_bytes_allocated;
         if (update)
-            WT_ERR(__meta_ckptlist_allocate_new_ckpt(session, ckptbasep, allocated, NULL));
+            WT_ERR(__meta_ckptlist_allocate_new_ckpt(
+              session, ckptbasep, &btree->ckpt_bytes_allocated, NULL));
+        if (allocated != NULL)
+            *allocated = btree->ckpt_bytes_allocated;
 #ifdef HAVE_DIAGNOSTIC
         /*
          * Sanity check: Let's compare to a list generated from metadata. There should be no
