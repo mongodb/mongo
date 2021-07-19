e7e03817009d1b921e21b34cf9bb10a0ea692dd9
Luke Chen <luke.chen@mongodb.com>
2021-07-19 14:42:51 +1000

    Import wiredtiger: 038e0a6839a1d4eefb6b9d8ef3f2fa03620ffb91 from branch mongodb-master
    ref: e6d16f8d03..038e0a6839
    for: 5.1.0
    
    WT-6729       Quiesce eviction prior running rollback to stable's active transaction check

--- src/third_party/wiredtiger/src/txn/txn_rollback_to_stable.c
+++ src/third_party/wiredtiger/src/txn/txn_rollback_to_stable.c
@@ -1603,14 +1603,77 @@ err:
 static int
 __rollback_to_stable(WT_SESSION_IMPL *session, bool no_ckpt)
 {
+    WT_CACHE *cache;
     WT_CONNECTION_IMPL *conn;
     WT_DECL_RET;
     WT_TXN_GLOBAL *txn_global;
     wt_timestamp_t rollback_timestamp;
+    size_t retries;
+    uint32_t cache_flags;
     char ts_string[2][WT_TS_INT_STRING_SIZE];
 
     conn = S2C(session);
+    cache = conn->cache;
     txn_global = &conn->txn_global;
+
+    /*
+     * We're about to run a check for active transactions in the system to stop users from shooting
+     * themselves in the foot. Eviction threads may interfere with this check if they involve writes
+     * to the history store so we need to wait until the system is no longer evicting content.
+     *
+     * If we detect active evictions, we should wait a millisecond and check again. If we're waiting
+     * for evictions to quiesce for more than 2 minutes, we should give up on waiting and proceed
+     * with the transaction check anyway.
+     */
+#define WT_RTS_EVICT_MAX_RETRIES (2 * WT_MINUTE * WT_THOUSAND)
+    /*
+     * These are the types of evictions that can result in a history store operation. Since we want
+     * to avoid these happening concurrently with our check, we need to look for these flags.
+     */
+#define WT_CACHE_EVICT_HS_FLAGS \
+    (WT_CACHE_EVICT_DIRTY | WT_CACHE_EVICT_UPDATES | WT_CACHE_EVICT_URGENT)
+    for (retries = 0; retries < WT_RTS_EVICT_MAX_RETRIES; ++retries) {
+        /*
+         * If we're shutting down or running with an in-memory configuration, we aren't at risk of
+         * racing with history store transactions.
+         */
+        if (F_ISSET(conn, WT_CONN_CLOSING_TIMESTAMP | WT_CONN_IN_MEMORY))
+            break;
+
+        /* Check whether eviction has quiesced. */
+        WT_ORDERED_READ(cache_flags, cache->flags);
+        if (!FLD_ISSET(cache_flags, WT_CACHE_EVICT_HS_FLAGS)) {
+            /*
+             * If we we find that the eviction flags are unset, interrupt the eviction server and
+             * acquire the pass lock to stop the server from setting the eviction flags AFTER this
+             * point and racing with our check.
+             */
+            (void)__wt_atomic_addv32(&cache->pass_intr, 1);
+            __wt_spin_lock(session, &cache->evict_pass_lock);
+            (void)__wt_atomic_subv32(&cache->pass_intr, 1);
+            FLD_SET(session->lock_flags, WT_SESSION_LOCKED_PASS);
+
+            /*
+             * Check that the flags didn't get set in between when we checked and when we acquired
+             * the server lock. If it did get set, release the locks and keep trying. If they're
+             * still unset, break out of this loop and commence our check.
+             */
+            WT_ORDERED_READ(cache_flags, cache->flags);
+            if (!FLD_ISSET(cache_flags, WT_CACHE_EVICT_HS_FLAGS))
+                break;
+            else {
+                __wt_spin_unlock(session, &cache->evict_pass_lock);
+                FLD_CLR(session->lock_flags, WT_SESSION_LOCKED_PASS);
+            }
+        }
+        /* If we're retrying, pause for a millisecond and let eviction make some progress. */
+        __wt_sleep(0, WT_THOUSAND);
+    }
+    if (retries == WT_RTS_EVICT_MAX_RETRIES) {
+        WT_ERR(__wt_msg(
+          session, "timed out waiting for eviction to quiesce, running rollback to stable"));
+        WT_ASSERT(session, false && "Timed out waiting for eviction to quiesce prior to rts");
+    }
 
     /*
      * Rollback to stable should ignore tombstones in the history store since it needs to scan the
@@ -1619,6 +1682,11 @@ __rollback_to_stable(WT_SESSION_IMPL *session, bool no_ckpt)
     F_SET(session, WT_SESSION_ROLLBACK_TO_STABLE);
 
     WT_ERR(__rollback_to_stable_check(session));
+
+    if (FLD_ISSET(session->lock_flags, WT_SESSION_LOCKED_PASS)) {
+        __wt_spin_unlock(session, &cache->evict_pass_lock);
+        FLD_CLR(session->lock_flags, WT_SESSION_LOCKED_PASS);
+    }
 
     /*
      * Copy the stable timestamp, otherwise we'd need to lock it each time it's accessed. Even
@@ -1653,6 +1721,10 @@ __rollback_to_stable(WT_SESSION_IMPL *session, bool no_ckpt)
         WT_ERR(session->iface.checkpoint(&session->iface, "force=1"));
 
 err:
+    if (FLD_ISSET(session->lock_flags, WT_SESSION_LOCKED_PASS)) {
+        __wt_spin_unlock(session, &cache->evict_pass_lock);
+        FLD_CLR(session->lock_flags, WT_SESSION_LOCKED_PASS);
+    }
     F_CLR(session, WT_SESSION_ROLLBACK_TO_STABLE);
     return (ret);
 }
