dbebb20241af73384093b6169c9b4e9b307bbb00
Mihai Andrei <mihai.andrei@10gen.com>
2021-07-20 13:35:02 +0000

    SERVER-56980 Robustify jstests/core/query_hash_stability.js
    (cherry picked from commit15096505deaa04cbae308f6cd276747c6b02db45)

--- jstests/core/query_hash_stability.js
+++ jstests/core/query_hash_stability.js
@@ -12,21 +12,55 @@ coll.drop();
 // Be sure the collection exists.
 assert.commandWorked(coll.insert({x: 5}));
 
-function getPlanCacheKeyFromExplain(explainRes) {
-    const hash = FixtureHelpers.isMongos(db)
-        ? explainRes.queryPlanner.winningPlan.shards[0].planCacheKey
-        : explainRes.queryPlanner.planCacheKey;
-    assert.eq(typeof (hash), "string");
-    return hash;
-}
+/**
+ * Given two explain plans (firstExplain, secondExplain), this function makes assertions about their
+ * 'planCacheField' values (in particular, whether they are 'expectedToMatch').
+ */
+let assertPlanCacheField = function(
+    {firstExplain, secondExplain, planCacheField, expectedToMatch}) {
+    let compareFn = function(first, second) {
+        assert.eq(typeof (first), "string");
+        assert.eq(typeof (second), "string");
+        assert.eq(first === second,
+                  expectedToMatch,
+                  "Mismatch for field " + planCacheField + " when comparing " +
+                      tojson(firstExplain) + " with " + tojson(secondExplain));
+    };
 
-function getQueryHashFromExplain(explainRes) {
-    const hash = FixtureHelpers.isMongos(db)
-        ? explainRes.queryPlanner.winningPlan.shards[0].queryHash
-        : explainRes.queryPlanner.queryHash;
-    assert.eq(typeof (hash), "string");
-    return hash;
-}
+    // SERVER-56980: When running in a sharded environment, we group the values for 'planCacheField'
+    // by shard. This is because in a multi-version environment, we want to ensure that we are
+    // comparing the results produced by the same shard in the event that the planCacheKey format
+    // changed in between versions.
+    if (FixtureHelpers.isMongos(db)) {
+        let buildShardMap = function(shardedPlan) {
+            let explainMap = {};
+            for (const shard of shardedPlan.queryPlanner.winningPlan.shards) {
+                explainMap[shard.shardName] = shard[planCacheField];
+            }
+            return explainMap;
+        };
+
+        const firstExplainMap = buildShardMap(firstExplain);
+        const secondExplainMap = buildShardMap(secondExplain);
+
+        // Should have the same number of elements.
+        assert.eq(Object.keys(firstExplainMap).length,
+                  Object.keys(secondExplainMap).length,
+                  "Expected " + tojson(firstExplainMap) + " and " + tojson(secondExplainMap) +
+                      " to have the same number of elements");
+
+        // Match the values for 'planCacheField' for each shard.
+        for (const shardName of Object.keys(firstExplainMap)) {
+            const firstPlanCacheValue = firstExplainMap[shardName];
+            const secondPlanCacheValue = secondExplainMap[shardName];
+            compareFn(firstPlanCacheValue, secondPlanCacheValue);
+        }
+    } else {
+        const first = firstExplain['queryPlanner'][planCacheField];
+        const second = secondExplain['queryPlanner'][planCacheField];
+        compareFn(first, second);
+    }
+};
 
 const query = {
     x: 3
@@ -40,18 +74,38 @@ assert.commandWorked(coll.createIndex({x: 1}, {sparse: true}));
 const withIndexExplain = coll.find(query).explain();
 
 // 'queryHash' shouldn't change across catalog changes.
-assert.eq(getQueryHashFromExplain(initialExplain), getQueryHashFromExplain(withIndexExplain));
+assertPlanCacheField({
+    firstExplain: initialExplain,
+    secondExplain: withIndexExplain,
+    planCacheField: 'queryHash',
+    expectedToMatch: true
+});
+
 // We added an index so the plan cache key changed.
-assert.neq(getPlanCacheKeyFromExplain(initialExplain),
-           getPlanCacheKeyFromExplain(withIndexExplain));
+assertPlanCacheField({
+    firstExplain: initialExplain,
+    secondExplain: withIndexExplain,
+    planCacheField: 'planCacheKey',
+    expectedToMatch: false
+});
 
 // Drop the index.
 assert.commandWorked(coll.dropIndex({x: 1}));
 const postDropExplain = coll.find(query).explain();
 
 // 'queryHash' shouldn't change across catalog changes.
-assert.eq(getQueryHashFromExplain(initialExplain), getQueryHashFromExplain(postDropExplain));
+assertPlanCacheField({
+    firstExplain: initialExplain,
+    secondExplain: postDropExplain,
+    planCacheField: 'queryHash',
+    expectedToMatch: true
+});
 
 // The 'planCacheKey' should be the same as what it was before we dropped the index.
-assert.eq(getPlanCacheKeyFromExplain(initialExplain), getPlanCacheKeyFromExplain(postDropExplain));
+assertPlanCacheField({
+    firstExplain: initialExplain,
+    secondExplain: postDropExplain,
+    planCacheField: 'planCacheKey',
+    expectedToMatch: true
+});
 })();
