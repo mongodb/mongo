b3a8b4525fa66b5f6eed74f327564831f5fd44a1
Yoonsoo Kim <yoonsoo.kim@mongodb.com>
2021-08-02 21:42:39 +0000

    SERVER-58432 Include dochub link in legacy op deprecation log messages

--- src/mongo/rpc/warn_deprecated_wire_ops.cpp
+++ src/mongo/rpc/warn_deprecated_wire_ops.cpp
@@ -69,7 +69,8 @@ void warnDeprecation(Client& client, StringData op) {
 
     LOGV2_DEBUG(5578800,
                 (*bumpedSeverity)(clientKey).toInt(),
-                "Deprecated operation requested",
+                "Deprecated operation requested. For more details see "
+                "https://dochub.mongodb.org/core/legacy-opcode-compatibility",
                 "op"_attr = op,
                 "clientInfo"_attr = clientInfo);
 }
