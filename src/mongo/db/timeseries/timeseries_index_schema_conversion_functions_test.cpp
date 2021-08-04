1ebe18c6bac75047ef7e66dacb4ba9c1bac62964
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2021-08-04 13:05:32 +0000

    SERVER-58779 Add explicit reverse mapping tests for time-series measurement indexes

--- src/mongo/db/timeseries/timeseries_index_schema_conversion_functions_test.cpp
+++ src/mongo/db/timeseries/timeseries_index_schema_conversion_functions_test.cpp
@@ -322,5 +322,54 @@ TEST(TimeseriesIndexSchemaConversionTest, 2dsphereMetadataIndexSpecConversion) {
     testBothWaysIndexSpecConversion(timeseriesOptions, timeseriesIndexSpec, bucketsIndexSpec);
 }
 
+// {a: 1} <=> {control.max.a: 1, control.min.a: 1}
+TEST(TimeseriesIndexSchemaConversionTest, AscendingMeasurementIndexSpecConversion) {
+    RAIIServerParameterControllerForTest controller("featureFlagTimeseriesMetricIndexes", true);
+    TimeseriesOptions timeseriesOptions = makeTimeseriesOptions();
+    BSONObj timeseriesIndexSpec = BSON("a" << 1);
+    BSONObj bucketsIndexSpec = BSON("control.max.a" << 1 << "control.min.a" << 1);
+
+    testBothWaysIndexSpecConversion(timeseriesOptions, timeseriesIndexSpec, bucketsIndexSpec);
+}
+
+// {a: -1} <=> {control.min.a: -1, control.max.a: -1}
+TEST(TimeseriesIndexSchemaConversionTest, DescendingMeasurementIndexSpecConversion) {
+    RAIIServerParameterControllerForTest controller("featureFlagTimeseriesMetricIndexes", true);
+    TimeseriesOptions timeseriesOptions = makeTimeseriesOptions();
+    BSONObj timeseriesIndexSpec = BSON("a" << -1);
+    BSONObj bucketsIndexSpec = BSON("control.min.a" << -1 << "control.max.a" << -1);
+
+    testBothWaysIndexSpecConversion(timeseriesOptions, timeseriesIndexSpec, bucketsIndexSpec);
+}
+
+// {a: 1, b: -1, c: 1, d: "2dsphere"} <=> {control.max.a: 1, control.min.a: 1,
+//                                         control.min.b: -1, control.max.b: -1,
+//                                         control.max.c: 1, control.min.c: 1,
+//                                         data.d: "2dsphere_bucket"}
+TEST(TimeseriesIndexSchemaConversionTest, MixedCompoundMeasurementIndexSpecConversion) {
+    RAIIServerParameterControllerForTest controller("featureFlagTimeseriesMetricIndexes", true);
+    TimeseriesOptions timeseriesOptions = makeTimeseriesOptions();
+    BSONObj timeseriesIndexSpec = BSON("a" << 1 << "b" << -1 << "c" << 1 << "d"
+                                           << "2dsphere");
+    BSONObj bucketsIndexSpec = BSON(
+        "control.max.a" << 1 << "control.min.a" << 1 << "control.min.b" << -1 << "control.max.b"
+                        << -1 << "control.max.c" << 1 << "control.min.c" << 1 << "data.d"
+                        << "2dsphere_bucket");
+
+    testBothWaysIndexSpecConversion(timeseriesOptions, timeseriesIndexSpec, bucketsIndexSpec);
+}
+
+// {a: "2sphere"} <=> {data.a: "2dsphere_bucket"}
+TEST(TimeseriesIndexSchemaConversionTest, 2dsphereMeasurementIndexSpecConversion) {
+    RAIIServerParameterControllerForTest controller("featureFlagTimeseriesMetricIndexes", true);
+    TimeseriesOptions timeseriesOptions = makeTimeseriesOptions();
+    BSONObj timeseriesIndexSpec = BSON("a"
+                                       << "2dsphere");
+    BSONObj bucketsIndexSpec = BSON("data.a"
+                                    << "2dsphere_bucket");
+
+    testBothWaysIndexSpecConversion(timeseriesOptions, timeseriesIndexSpec, bucketsIndexSpec);
+}
+
 }  // namespace
 }  // namespace mongo
