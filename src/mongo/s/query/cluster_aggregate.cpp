e830129078cb6c6d7c90b50ce62ab7f6669b12ec
Tyler Seip <Tyler.Seip@mongodb.com>
2021-08-09 16:25:08 +0000

    SERVER-55581: Return ScopedDbConnection to connection pool in cluster aggregate

--- src/mongo/s/query/cluster_aggregate.cpp
+++ src/mongo/s/query/cluster_aggregate.cpp
@@ -450,6 +450,7 @@ BSONObj getUnshardedCollInfo(const Shard* primaryShard, const NamespaceString& n
     ScopedDbConnection conn(primaryShard->getConnString());
     std::list<BSONObj> all =
         conn->getCollectionInfos(nss.db().toString(), BSON("name" << nss.coll()));
+    conn.done();
     if (all.empty()) {
         // Collection does not exist, return an empty object.
         return BSONObj();
