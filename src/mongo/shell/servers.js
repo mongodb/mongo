dd072345dfb74e2321c1ffc268ce791dfad42b93
Daniel Gottlieb <daniel.gottlieb@mongodb.com>
2021-08-16 12:27:38 -0400

    SERVER-59353: Strip featureFlagRetryableFindAndModify for mongod binaries prior to 5.0

--- src/mongo/shell/servers.js
+++ src/mongo/shell/servers.js
@@ -697,6 +697,7 @@ MongoRunner.mongodOptions = function(opts = {}) {
     _removeSetParameterIfBeforeVersion(
         opts, "enableDefaultWriteConcernUpdatesForInitiate", "5.0.0");
     _removeSetParameterIfBeforeVersion(opts, "enableReconfigRollbackCommittedWritesCheck", "5.0.0");
+    _removeSetParameterIfBeforeVersion(opts, "featureFlagRetryableFindAndModify", "5.0.0");
 
     if (!opts.logFile && opts.useLogFiles) {
         opts.logFile = opts.dbpath + "/mongod.log";
