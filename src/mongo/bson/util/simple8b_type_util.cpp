7d7048d88b4047b733d23cc0d2af92383d4ef6d9
Henrik Edin <henrik.edin@mongodb.com>
2021-08-24 10:48:35 -0400

    SERVER-59524 Correctly handle endianness in Simple8bTypeUtil::decodeBinary

--- src/mongo/bson/util/simple8b_type_util.cpp
+++ src/mongo/bson/util/simple8b_type_util.cpp
@@ -199,8 +199,8 @@ int128_t Simple8bTypeUtil::encodeBinary(const char* val, size_t size) {
 }
 
 void Simple8bTypeUtil::decodeBinary(int128_t val, char* result, size_t size) {
-    uint64_t low = absl::Int128Low64(val);
-    uint64_t high = absl::Int128High64(val);
+    uint64_t low = LittleEndian<uint64_t>::store(absl::Int128Low64(val));
+    uint64_t high = LittleEndian<uint64_t>::store(absl::Int128High64(val));
     if (size > 8) {
         memcpy(result, &low, 8);
         memcpy(result + 8, &high, size - 8);
