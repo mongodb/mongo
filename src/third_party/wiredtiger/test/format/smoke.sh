bd4618516699fa24d90601985e18dc45772daae9
Alison Felizzi <alison.felizzi@mongodb.com>
2021-09-02 06:33:36 +0000

    Import wiredtiger: 2fab3ae25129e7810de7a290e82289422d7e85d7 from branch mongodb-master
    ref: 7b4995aa52..2fab3ae251
    for: 5.1.0
    
    WT-6193       Re-enable VLCS testing in format-test

--- src/third_party/wiredtiger/test/format/smoke.sh
+++ src/third_party/wiredtiger/test/format/smoke.sh
@@ -5,15 +5,16 @@ set -e
 # Smoke-test format as part of running "make check".
 args="-1 -c . "
 args="$args btree.compression=none "
+args="$args cache.minimum=40 "
 args="$args logging_compression=none"
-args="$args runs.ops=50000 "
-args="$args runs.rows=10000 "
+args="$args runs.ops=500000 "
+args="$args runs.rows=100000 "
 args="$args runs.source=table "
 args="$args runs.threads=4 "
 
-# Temporarily disabled
+# Temporarily disable LSM and FLCS.
 # $TEST_WRAPPER ./t $args runs.type=fix
 # $TEST_WRAPPER ./t $args runs.type=row runs.source=lsm
-# $TEST_WRAPPER ./t $args runs.type=var
 
 $TEST_WRAPPER ./t $args runs.type=row
+$TEST_WRAPPER ./t $args runs.type=var
