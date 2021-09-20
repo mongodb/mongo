ebc5f8d832fa3a144370a2775a46191512393831
Henrik Edin <henrik.edin@mongodb.com>
2021-09-20 17:34:07 +0000

    SERVER-60058 Add helper library to check for feature flags in jstests

---
+++ jstests/libs/feature_flag_util.js
@@ -0,0 +1,19 @@
+/**
+ * Utilities for feature flags.
+ */
+var FeatureFlagUtil = class {
+    /**
+     * Returns true if feature flag is enabled, false otherwise.
+     */
+    static isEnabled(db, featureFlag) {
+        return eval(
+            `const admin = db.getSiblingDB("admin");
+            const flagDoc = admin.runCommand({getParameter: 1, featureFlag${featureFlag}: 1});
+            const fcvDoc = admin.runCommand({getParameter: 1, featureCompatibilityVersion: 1});
+            flagDoc.hasOwnProperty("featureFlag${featureFlag}") &&
+                flagDoc.featureFlag${featureFlag}.value &&
+                MongoRunner.compareBinVersions(fcvDoc.featureCompatibilityVersion.version,
+                                            flagDoc.featureFlag${featureFlag}.fcv) >= 0;`
+        );
+    }
+};
