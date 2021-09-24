9573b5aa54b59ac21c98a4967218c70790588af6
David Bradford <david.bradford@mongodb.com>
2021-09-24 16:20:22 -0400

    SERVER-60223: Look in root folder for evergreen binary
    (cherry picked from commit de1be8d2198c9a4772fea8d9ecd2a5c0c36ee819)

--- evergreen/generate_build_variant.sh
+++ evergreen/generate_build_variant.sh
@@ -6,7 +6,7 @@ cd src
 set -o errexit
 
 activate_venv
-PATH=$PATH:$HOME $python buildscripts/evergreen_gen_build_variant.py \
+PATH=$PATH:$HOME:/ $python buildscripts/evergreen_gen_build_variant.py \
   --expansion-file ../expansions.yml \
   --evg-api-config ./.evergreen.yml \
   --output-file ${build_variant}.json \
