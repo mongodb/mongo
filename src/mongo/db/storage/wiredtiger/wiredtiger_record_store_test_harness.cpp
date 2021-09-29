mongodb/mongo

commit 6b3acc72af58963a772213cc882e34b6db5bf144

SERVER-59618 add LockerNoopClientObserver and register this ClientObserver in unit tests with Service Context

src/mongo/db/storage/wiredtiger/wiredtiger_record_store_test_harness.cpp
@@ -29,6 +29,7 @@
 
 #include "mongo/db/storage/wiredtiger/wiredtiger_record_store_test_harness.h"
 
+#include "mongo/db/concurrency/locker_noop_client_observer.h"
 #include "mongo/db/operation_context_noop.h"
 #include "mongo/db/repl/replication_coordinator_mock.h"
 
@@ -46,6 +47,9 @@ WiredTigerHarnessHelper::WiredTigerHarnessHelper(StringData extraStrings)
               false,
               false,
               false) {
+    auto service = getServiceContext();
+    service->registerClientObserver(std::make_unique<LockerNoopClientObserver>());
+
     repl::ReplicationCoordinator::set(
         serviceContext(),
         std::make_unique<repl::ReplicationCoordinatorMock>(serviceContext(), repl::ReplSettings()));

src/mongo/db/concurrency/locker_noop_client_observer.h
@@ -1,0 +1,57 @@
+/**
+ *    Copyright (C) 2021-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#pragma once
+
+#include "mongo/db/concurrency/locker_noop.h"
+#include "mongo/db/service_context.h"
+
+namespace mongo {
+
+/**
+ * ServiceContext hook that ensures OperationContexts are created with a valid
+ * Locker instance. Intended for use in tests that do not require a storage engine.
+ */
+class LockerNoopClientObserver : public ServiceContext::ClientObserver {
+public:
+    LockerNoopClientObserver() = default;
+    ~LockerNoopClientObserver() = default;
+
+    void onCreateClient(Client* client) final {}
+
+    void onDestroyClient(Client* client) final {}
+
+    void onCreateOperationContext(OperationContext* opCtx) final {
+        opCtx->setLockState(std::make_unique<LockerNoop>());
+    }
+
+    void onDestroyOperationContext(OperationContext* opCtx) final {}
+};
+
+}  // namespace mongo

src/mongo/db/concurrency/locker_noop_service_context_test_fixture.h
@@ -1,0 +1,51 @@
+/**
+ *    Copyright (C) 2021-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#pragma once
+
+#include "mongo/db/concurrency/locker_noop_client_observer.h"
+#include "mongo/db/service_context_test_fixture.h"
+
+namespace mongo {
+
+/**
+ * Test fixture for tests that require a properly initialized global service context
+ * and a stub Locker implementation whenever an OperationContext is requested.
+ */
+class LockerNoopServiceContextTest : public ServiceContextTest {
+protected:
+    LockerNoopServiceContextTest() {
+        auto service = getServiceContext();
+        service->registerClientObserver(std::make_unique<LockerNoopClientObserver>());
+    }
+
+    ~LockerNoopServiceContextTest() = default;
+};
+
+}  // namespace mongo

src/mongo/db/exec/sbe/SConscript
@@ -116,6 +116,9 @@ env.Library(
         '$BUILD_DIR/mongo/unittest/unittest',
         'query_sbe',
     ],
+    LIBDEPS_PRIVATE=[
+        '$BUILD_DIR/mongo/db/service_context_test_fixture',
+    ],
 )
 
 env.CppUnitTest(

src/mongo/db/ftdc/ftdc_test.h
@@ -30,12 +30,12 @@
 #include <boost/filesystem.hpp>
 #include <vector>
 
+#include "mongo/db/concurrency/locker_noop_service_context_test_fixture.h"
 #include "mongo/db/jsobj.h"
-#include "mongo/db/service_context_test_fixture.h"
 
 namespace mongo {
 
-class FTDCTest : public ServiceContextTest {
+class FTDCTest : public LockerNoopServiceContextTest {
 public:
     FTDCTest();
 };

src/mongo/db/stats/resource_consumption_metrics_test.cpp
@@ -29,8 +29,8 @@
 
 #include "mongo/platform/basic.h"
 
+#include "mongo/db/concurrency/locker_noop_service_context_test_fixture.h"
 #include "mongo/db/repl/replication_coordinator_mock.h"
-#include "mongo/db/service_context_test_fixture.h"
 #include "mongo/db/stats/operation_resource_consumption_gen.h"
 #include "mongo/db/stats/resource_consumption_metrics.h"
 #include "mongo/unittest/unittest.h"
@@ -49,7 +49,7 @@ ServerParameter* getServerParameter(const std::string& name) {
 }
 }  // namespace
 
-class ResourceConsumptionMetricsTest : public ServiceContextTest {
+class ResourceConsumptionMetricsTest : public LockerNoopServiceContextTest {
 public:
     void setUp() {
         _opCtx = makeOperationContext();

src/mongo/db/storage/wiredtiger/wiredtiger_standard_index_test.cpp
@@ -35,6 +35,7 @@
 #include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/db/catalog/collection_mock.h"
 #include "mongo/db/catalog/index_catalog_entry.h"
+#include "mongo/db/concurrency/locker_noop_client_observer.h"
 #include "mongo/db/index/index_descriptor.h"
 #include "mongo/db/json.h"
 #include "mongo/db/operation_context_noop.h"
@@ -56,6 +57,9 @@ using std::string;
 class WiredTigerIndexHarnessHelper final : public SortedDataInterfaceHarnessHelper {
 public:
     WiredTigerIndexHarnessHelper() : _dbpath("wt_test"), _conn(nullptr) {
+        auto service = getServiceContext();
+        service->registerClientObserver(std::make_unique<LockerNoopClientObserver>());
+
         const char* config = "create,cache_size=1G,";
         int ret = wiredtiger_open(_dbpath.path().c_str(), nullptr, config, &_conn);
         invariantWTOK(ret);

src/mongo/db/update/update_node_test_fixture.h
@@ -29,8 +29,8 @@
 
 #pragma once
 
+#include "mongo/db/concurrency/locker_noop_service_context_test_fixture.h"
 #include "mongo/db/service_context.h"
-#include "mongo/db/service_context_test_fixture.h"
 #include "mongo/db/update/update_node.h"
 #include "mongo/db/update/v1_log_builder.h"
 #include "mongo/db/update/v2_log_builder.h"
@@ -38,7 +38,7 @@
 
 namespace mongo {
 
-class UpdateNodeTest : public ServiceContextTest {
+class UpdateNodeTest : public LockerNoopServiceContextTest {
 public:
     ~UpdateNodeTest() override = default;
 

commit 45041b9e5aece67df50f549d67df2ff8f5fcce3e

SERVER-60275 WiredTigerHarnessHelper registers client observer before initializing storage engine
This fixes a TSAN data race introduced in commit 6b3acc72af58963a772213cc882e34b6db5bf14.
(suppressed by partial revert in commit 505bd6a60bf925e01b4ef0b7eb501708b87c21b6).

src/mongo/db/storage/wiredtiger/wiredtiger_record_store_test_harness.cpp
@@ -36,6 +36,7 @@ namespace mongo {
 
 WiredTigerHarnessHelper::WiredTigerHarnessHelper(StringData extraStrings)
     : _dbpath("wt_test"),
+      _lockerNoopClientObserverRegisterer(getServiceContext()),
       _engine(kWiredTigerEngineName,
               _dbpath.path(),
               &_cs,

src/mongo/db/storage/wiredtiger/wiredtiger_record_store_test_harness.h
@@ -29,6 +29,7 @@
 
 #include "mongo/platform/basic.h"
 
+#include "mongo/db/concurrency/locker_noop_service_context_test_fixture.h"
 #include "mongo/db/storage/record_store_test_harness.h"
 #include "mongo/db/storage/wiredtiger/wiredtiger_record_store.h"
 #include "mongo/unittest/temp_dir.h"
@@ -75,6 +76,10 @@ private:
     unittest::TempDir _dbpath;
     ClockSourceMock _cs;
 
+    // Since WTKVEngine starts threads that require the global service context, we load
+    // the client observer for LockerNoop before creating the storage engine to avoid a
+    // potential data race (that might be reported by a tool like TSAN).
+    LockerNoopClientObserverRegisterer _lockerNoopClientObserverRegisterer;
     WiredTigerKVEngine _engine;
 };
 }  // namespace mongo