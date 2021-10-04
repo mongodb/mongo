91944199b9bb27ff2864c3ee6b0c18f1448e121c
Gregory Noma <gregory.noma@gmail.com>
2021-10-04 19:50:08 +0000

    SERVER-60322 Do not re-use `Collection*` when renaming collection between databases
    (cherry picked from commit 5a7aade7b5939c5946f84b337886f861a33bab1a)

--- src/mongo/db/catalog/rename_collection.cpp
+++ src/mongo/db/catalog/rename_collection.cpp
@@ -552,22 +552,20 @@ Status renameBetweenDBs(OperationContext* opCtx,
     const auto& tmpName = tmpNameResult.getValue();
 
     LOGV2(20398,
-          "Attempting to create temporary collection: {tmpName} with the contents of collection: "
-          "{source}",
           "Attempting to create temporary collection",
           "temporaryCollection"_attr = tmpName,
           "sourceCollection"_attr = source);
 
-    Collection* tmpColl = nullptr;
+    // Renaming across databases will result in a new UUID.
+    NamespaceStringOrUUID tmpCollUUID{tmpName.db().toString(), UUID::gen()};
+
     {
         auto collectionOptions = sourceColl->getCollectionOptions();
-
-        // Renaming across databases will result in a new UUID.
-        collectionOptions.uuid = UUID::gen();
+        collectionOptions.uuid = tmpCollUUID.uuid();
 
         writeConflictRetry(opCtx, "renameCollection", tmpName.ns(), [&] {
             WriteUnitOfWork wunit(opCtx);
-            tmpColl = targetDB->createCollection(opCtx, tmpName, collectionOptions);
+            targetDB->createCollection(opCtx, tmpName, collectionOptions);
             wunit.commit();
         });
     }
@@ -623,7 +621,7 @@ Status renameBetweenDBs(OperationContext* opCtx,
             WriteUnitOfWork wunit(opCtx);
             auto fromMigrate = false;
             try {
-                CollectionWriter tmpCollWriter(tmpColl);
+                CollectionWriter tmpCollWriter(opCtx, *tmpCollUUID.uuid());
                 IndexBuildsCoordinator::get(opCtx)->createIndexesOnEmptyCollection(
                     opCtx, tmpCollWriter, indexesToCopy, fromMigrate);
             } catch (DBException& ex) {
@@ -638,9 +636,6 @@ Status renameBetweenDBs(OperationContext* opCtx,
     }
 
     {
-        NamespaceStringOrUUID tmpCollUUID =
-            NamespaceStringOrUUID(std::string(tmpName.db()), tmpColl->uuid());
-        tmpColl = nullptr;
         statsTracker.reset();
 
         // Copy over all the data from source collection to temporary collection. For this we can
