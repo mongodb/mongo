41480a13a723f82ea1132dab8bde2892bdc6b103
Richard Samuels <richard.l.samuels@gmail.com>
2021-10-05 17:07:19 +0000

    SERVER-58836 Increase jepsen list-append time limit

--- evergreen/jepsen_docker/list-append.sh
+++ evergreen/jepsen_docker/list-append.sh
@@ -7,7 +7,7 @@ cd jepsen/docker
 
 # actually run the tests
 start_time=$(date +%s)
-sudo docker exec jepsen-control bash --login -c "cd /jepsen/mongodb && lein run test-all -w list-append -n n1 -n n2 -n n3 -n n4 -n n5 -n n6 -n n7 -n n8 -n n9 -r 1000 --concurrency 3n --time-limit 120 --max-writes-per-key 128 --read-concern majority --write-concern majority --txn-read-concern snapshot --txn-write-concern majority --nemesis-interval 1 --nemesis partition --test-count 30" | tee jepsen_${task_name}_${execution}.log
+sudo docker exec jepsen-control bash --login -c "cd /jepsen/mongodb && lein run test-all -w list-append -n n1 -n n2 -n n3 -n n4 -n n5 -n n6 -n n7 -n n8 -n n9 -r 1000 --concurrency 3n --time-limit 240 --max-writes-per-key 128 --read-concern majority --write-concern majority --txn-read-concern snapshot --txn-write-concern majority --nemesis-interval 1 --nemesis partition --test-count 30" | tee jepsen_${task_name}_${execution}.log
 end_time=$(date +%s)
 elapsed_secs=$((end_time - start_time))
 
