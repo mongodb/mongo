143bf0610de209b4922d05facd980447cc108247
sergey.galtsev <sergey.galtsev@mongodb.com>
2021-10-07 21:57:26 +0000

    SERVER-57727 race conditions in x509_invalid.js

--- jstests/ssl/x509_invalid.js
+++ jstests/ssl/x509_invalid.js
@@ -3,19 +3,54 @@
 (function() {
     'use strict';
 
-    const CLIENT_NAME = 'CN=client,OU=KernelUser,O=MongoDB,L=New York City,ST=New York,C=US';
+    const CLIENT_NAME = "CN=client,OU=KernelUser,O=MongoDB,L=New York City,ST=New York,C=US";
     const CLIENT_CERT = 'jstests/libs/client.pem';
     const SERVER_CERT = 'jstests/libs/server.pem';
     const CA_CERT = 'jstests/libs/ca.pem';
     const SELF_SIGNED_CERT = 'jstests/libs/client-self-signed.pem';
 
-    function testClient(conn, cert, name, shouldSucceed) {
+    function hasX509AuthSucceeded(conn) {
+        if (checkLog.checkContainsOnce(conn,
+                                       'Successfully authenticated as principal ' + CLIENT_NAME)) {
+            return true;
+        }
+        if (checkLog.checkContainsOnce(conn, 'No verified subject name available from client')) {
+            return false;
+        }
+        print("Not yet clear what was the result...");
+        return null;
+    }
+
+    function testClient(cert, name, shouldSucceed) {
+        print("Starting mongod...");
+        const conn = MongoRunner.runMongod({
+            auth: '',
+            sslMode: 'requireSSL',
+            sslPEMKeyFile: SERVER_CERT,
+            sslCAFile: CA_CERT,
+            sslAllowInvalidCertificates: '',
+        });
+
+        print("Creating admin user...");
+        const admin = conn.getDB('admin');
+        admin.createUser({user: "admin", pwd: "admin", roles: ["root"]});
+        admin.auth('admin', 'admin');
+
+        print("Creating external user...");
+        const external = conn.getDB('$external');
+        external.createUser({user: CLIENT_NAME, roles: [{'role': 'readWrite', 'db': 'test'}]});
+
         let auth = {mechanism: 'MONGODB-X509'};
         if (name !== null) {
             auth.user = name;
         }
+
+        print("Running mongo shell script...");
+        if (!shouldSucceed) {
+            print("Note: following shell command is expected to fail");
+        }
+
         const script = 'assert(db.getSiblingDB(\'$external\').auth(' + tojson(auth) + '));';
-        clearRawMongoProgramOutput();
         const exitCode = runMongoProgram('mongo',
                                          '--ssl',
                                          '--sslAllowInvalidHostnames',
@@ -28,34 +63,19 @@
                                          '--eval',
                                          script);
 
+        print("Analyzing results...");
         assert.eq(shouldSucceed, exitCode === 0, "exitCode = " + tojson(exitCode));
-        assert.eq(
-            !shouldSucceed,
-            rawMongoProgramOutput().includes('No verified subject name available from client'));
+        assert.soon(() => hasX509AuthSucceeded(admin) !== null,
+                    "can not find in mongod logs whether it succeeded to authenticate",
+                    15000);
+        assert.eq(shouldSucceed, hasX509AuthSucceeded(admin));
+
+        print("Stopping mongod...");
+        MongoRunner.stopMongod(conn);
     }
 
-    function runTest(conn) {
-        const admin = conn.getDB('admin');
-        admin.createUser({user: "admin", pwd: "admin", roles: ["root"]});
-        admin.auth('admin', 'admin');
-
-        const external = conn.getDB('$external');
-        external.createUser({user: CLIENT_NAME, roles: [{'role': 'readWrite', 'db': 'test'}]});
-
-        testClient(conn, CLIENT_CERT, CLIENT_NAME, true);
-        testClient(conn, SELF_SIGNED_CERT, CLIENT_NAME, false);
-        testClient(conn, CLIENT_CERT, null, true);
-        testClient(conn, SELF_SIGNED_CERT, null, false);
-    }
-
-    // Standalone.
-    const mongod = MongoRunner.runMongod({
-        auth: '',
-        sslMode: 'requireSSL',
-        sslPEMKeyFile: SERVER_CERT,
-        sslCAFile: CA_CERT,
-        sslAllowInvalidCertificates: '',
-    });
-    runTest(mongod);
-    MongoRunner.stopMongod(mongod);
+    testClient(CLIENT_CERT, CLIENT_NAME, true);
+    testClient(SELF_SIGNED_CERT, CLIENT_NAME, false);
+    testClient(CLIENT_CERT, null, true);
+    testClient(SELF_SIGNED_CERT, null, false);
 })();
