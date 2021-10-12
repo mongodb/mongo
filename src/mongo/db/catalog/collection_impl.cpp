3953546c3cb0afa6dcafbc02f9c732d26a3166ec
Jason Chan <jason.chan@mongodb.com>
2021-10-12 14:13:29 +0000

    SERVER-60469 Fix bug where two optimes for being reserved for retryable deletes despite not capturing a preImage

--- src/mongo/db/catalog/collection_impl.cpp
+++ src/mongo/db/catalog/collection_impl.cpp
@@ -1157,7 +1157,10 @@ void CollectionImpl::deleteDocument(OperationContext* opCtx,
         uasserted(10089, "cannot remove from a capped collection");
     }
 
-    const auto oplogSlot = reserveOplogSlotsForRetryableFindAndModify(opCtx);
+    boost::optional<OplogSlot> oplogSlot = boost::none;
+    if (storeDeletedDoc == Collection::StoreDeletedDoc::On) {
+        oplogSlot = reserveOplogSlotsForRetryableFindAndModify(opCtx);
+    }
     OpObserver::OplogDeleteEntryArgs deleteArgs{
         nullptr, fromMigrate, getRecordPreImages(), oplogSlot, oplogSlot != boost::none};
 
