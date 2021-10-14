10d9f10ea2c88a99a18ab7261501a24708b878cf
Benety Goh <benety@mongodb.com>
2021-10-14 07:06:04 -0400

    SERVER-60388 benchmark_main registers LockerNoopClientObserver with ServiceContext

--- src/mongo/unittest/benchmark_main.cpp
+++ src/mongo/unittest/benchmark_main.cpp
@@ -35,6 +35,7 @@
 
 #include "mongo/base/initializer.h"
 #include "mongo/config.h"
+#include "mongo/db/concurrency/locker_noop_client_observer.h"
 #include "mongo/db/service_context.h"
 #include "mongo/logv2/log.h"
 #include "mongo/util/signal_handlers_synchronous.h"
@@ -45,7 +46,9 @@ int main(int argc, char** argv) {
     ::mongo::setupSynchronousSignalHandlers();
 
     ::mongo::runGlobalInitializersOrDie(std::vector<std::string>(argv, argv + argc));
-    ::mongo::setGlobalServiceContext(::mongo::ServiceContext::make());
+    auto serviceContext = ::mongo::ServiceContext::make();
+    serviceContext->registerClientObserver(std::make_unique<::mongo::LockerNoopClientObserver>());
+    ::mongo::setGlobalServiceContext(std::move(serviceContext));
 
     // Copied from the BENCHMARK_MAIN macro.
     ::benchmark::Initialize(&argc, argv);
