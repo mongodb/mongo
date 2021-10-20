e4b53ea718e9c698186e7f1ca1007ab8afad9077
Benety Goh <benety@mongodb.com>
2021-10-20 09:54:06 -0400

    SERVER-60812 clean up explain5.js

--- jstests/core/explain5.js
+++ jstests/core/explain5.js
@@ -4,30 +4,34 @@
 //   assumes_read_concern_local,
 // ]
 
-t = db.jstests_explain5;
+(function() {
+'use strict';
+
+const t = db.jstests_explain5;
 t.drop();
 
-t.createIndex({a: 1});
-t.createIndex({b: 1});
+assert.commandWorked(t.createIndex({a: 1}));
+assert.commandWorked(t.createIndex({b: 1}));
 
-for (i = 0; i < 1000; ++i) {
-    t.save({a: i, b: i % 3});
+for (let i = 0; i < 1000; ++i) {
+    assert.commandWorked(t.insert({_id: i, a: i, b: i % 3}));
 }
 
 // Query with an initial set of documents.
-var explain1 = t.find({a: {$gte: 0}, b: 2}).sort({a: 1}).hint({a: 1}).explain("executionStats");
-printjson(explain1);
-var stats1 = explain1.executionStats;
+const explain1 = t.find({a: {$gte: 0}, b: 2}).sort({a: 1}).hint({a: 1}).explain("executionStats");
+jsTestLog('explain5 explain output after initial documents: ' + tojson(explain1));
+const stats1 = explain1.executionStats;
 assert.eq(333, stats1.nReturned, 'wrong nReturned for explain1');
 assert.eq(1000, stats1.totalKeysExamined, 'wrong totalKeysExamined for explain1');
 
-for (i = 1000; i < 2000; ++i) {
-    t.save({a: i, b: i % 3});
+for (let i = 1000; i < 2000; ++i) {
+    assert.commandWorked(t.insert({_id: i, a: i, b: i % 3}));
 }
 
 // Query with some additional documents.
-var explain2 = t.find({a: {$gte: 0}, b: 2}).sort({a: 1}).hint({a: 1}).explain("executionStats");
-printjson(explain2);
-var stats2 = explain2.executionStats;
+const explain2 = t.find({a: {$gte: 0}, b: 2}).sort({a: 1}).hint({a: 1}).explain("executionStats");
+jsTestLog('explain5 explain output after additional documents: ' + tojson(explain2));
+const stats2 = explain2.executionStats;
 assert.eq(666, stats2.nReturned, 'wrong nReturned for explain2');
 assert.eq(2000, stats2.totalKeysExamined, 'wrong totalKeysExamined for explain2');
+})();
