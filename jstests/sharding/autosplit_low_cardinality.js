f9f9302a5254b6ec976228c5d8cdf70b39ef96eb
Pierlauro Sciarelli <pierlauro.sciarelli@mongodb.com>
2021-10-20 08:40:18 +0000

    SERVER-59882 Autosplit tests may get one less chunk than expected due to wuow commit delay

--- jstests/sharding/autosplit_low_cardinality.js
+++ jstests/sharding/autosplit_low_cardinality.js
@@ -41,13 +41,40 @@ insertBigDocsWithKey(-10, 4);
 insertBigDocsWithKey(10, 4);
 waitForOngoingChunkSplits(st);
 
-// At least one split should have been performed
-assert.gte(numChunks(), 2, "Number of chunks is less then 2, no split have been perfomed");
+let expectedNumChunks = 2;
+try {
+    // At least one split should have been performed
+    assert.gte(numChunks(),
+               expectedNumChunks,
+               "Number of chunks is less than 2, no split have been perfomed");
+} catch (e) {
+    // (SERVER-59882) split may not have happened due to commit delay of the inserted documents
+    print("Retrying performing one insert after catching exception " + e);
+    insertBigDocsWithKey(10, 1);
+    waitForOngoingChunkSplits(st);
+    assert.gte(
+        numChunks(),
+        expectedNumChunks,
+        "Number of chunks is less than " + expectedNumChunks + ", no split has been perfomed");
+}
+
+expectedNumChunks++;
 
 insertBigDocsWithKey(20, 4);
 waitForOngoingChunkSplits(st);
 // An additional split should have been performed
-assert.gte(numChunks(), 3, "Number of chunks must be at least 3");
+try {
+    assert.gte(numChunks(), expectedNumChunks, "Number of chunks must be at least 3");
+} catch (e) {
+    // (SERVER-59882) split may not have happened due to commit delay of the inserted documents
+    print("Retrying performing one insert after catching exception " + e);
+    insertBigDocsWithKey(20, 1);
+    waitForOngoingChunkSplits(st);
+    assert.gte(
+        numChunks(),
+        expectedNumChunks,
+        "Number of chunks is less than " + 3 + ", not all expected splits have been perfomed");
+}
 
 st.stop();
 })();
