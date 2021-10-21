5500c2b6e8a0fad7e779d003feb0d3c9f92f1c77
Max Hirschhorn <max.hirschhorn@mongodb.com>
2021-10-21 14:40:40 +0000

    SERVER-57557 Skip FCV server parameter check in checkFCV() for mongos.
    (cherry picked from commit f830b5201069f86d1e638f9dcd3294b799e5dfb4)

--- jstests/libs/feature_compatibility_version.js
+++ jstests/libs/feature_compatibility_version.js
@@ -20,10 +20,13 @@ var lastStableFCV = "4.0";
  * {featureCompatibilityVersion: {version: <required>, targetVersion: <optional>}, ok: 1}.
  */
 function checkFCV(adminDB, version, targetVersion) {
-    let res = adminDB.runCommand({getParameter: 1, featureCompatibilityVersion: 1});
-    assert.commandWorked(res);
-    assert.eq(res.featureCompatibilityVersion.version, version, tojson(res));
-    assert.eq(res.featureCompatibilityVersion.targetVersion, targetVersion, tojson(res));
+    const isMongod = !adminDB.getMongo().isMongos();
+    if (isMongod) {
+        let res = adminDB.runCommand({getParameter: 1, featureCompatibilityVersion: 1});
+        assert.commandWorked(res);
+        assert.eq(res.featureCompatibilityVersion.version, version, tojson(res));
+        assert.eq(res.featureCompatibilityVersion.targetVersion, targetVersion, tojson(res));
+    }
 
     let doc = adminDB.system.version.findOne({_id: "featureCompatibilityVersion"});
     assert.eq(doc.version, version, tojson(doc));
