aa3c6eb2c7b4f76521004461e41b4a8b2022586e
Dan Larkin-York <dan.larkin-york@mongodb.com>
2021-10-29 22:30:05 +0000

    SERVER-61106 Use woCompare to generate timeseries summaries

--- src/mongo/db/timeseries/minmax.cpp
+++ src/mongo/db/timeseries/minmax.cpp
@@ -488,7 +488,7 @@ std::pair<MinMaxStore::Iterator, MinMaxStore::Iterator> MinMax::_update(
             (data.type() == MinMaxStore::Type::kObject && comp(typeComp(Object), 0)) ||
             (data.type() == MinMaxStore::Type::kArray && comp(typeComp(Array), 0)) ||
             (data.type() == MinMaxStore::Type::kValue &&
-             elem.compare(data.value(), comp, false, stringComparator))) {
+             comp(elem.woCompare(data.value(), false, stringComparator), 0))) {
             data.setValue(elem);
         }
     };
