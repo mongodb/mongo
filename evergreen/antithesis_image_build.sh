14cd3f8d8d89a2bdc33374f80ed04d1e326f8fc6
Richard Samuels <richard.l.samuels@gmail.com>
2021-11-03 12:09:10 +0000

    SERVER-61161 Prevent antithesis images from being overwritten when older tasks are rerun

--- evergreen/antithesis_image_build.sh
+++ evergreen/antithesis_image_build.sh
@@ -3,6 +3,20 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 
 set -euo pipefail
 
+cd src
+commit_date=$(date -d "$(git log -1 -s --format=%ci)" "+%s")
+last_run_date=$(cat ../antithesis_last_push.txt || echo 0)
+if [ "${is_patch}" != "true" && $last_run_date -gt $commit_date ]; then
+  echo -e "Refusing to push new antithesis images because this commit is older\nthan the last pushed commit"
+  exit 0
+fi
+cd ..
+
+# check that the binaries in dist-test are linked to libvoidstar
+ldd src/dist-test/bin/mongod | grep libvoidstar
+ldd src/dist-test/bin/mongos | grep libvoidstar
+ldd src/dist-test/bin/mongo | grep libvoidstar
+
 # prepare the image building environment
 cp -rf src/buildscripts/antithesis/ antithesis
 # due to gitignore, we can't commit a folder called logs, so make it here
@@ -27,10 +41,19 @@ tag="evergreen-latest-${branch_name}"
 if [ "${is_patch}" = "true" ]; then
   tag="evergreen-patch"
 fi
+
+if [ -n "${antithesis_image_tag}" ]; then
+  echo "Using provided tag: '$antithesis_image_tag' for docker pushes"
+  tag=$antithesis_image_tag
+fi
+
 docker build . -t workload:$tag
 cd ../database
 docker build . -t database:$tag
 cd ..
+# ensure that the embedded image references actually point to the images we're
+# pushing here
+sed -i s/evergreen-latest-master/$tag/ docker-compose.yml
 docker build . -t config:$tag
 
 # login, push, and logout
@@ -39,13 +62,17 @@ cat mongodb.key.json | docker login -u _json_key https://us-central1-docker.pkg.
 rm mongodb.key.json
 
 # tag and push to the registry
-docker tag workload:$tag us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/workload:$tag
-docker push us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/workload:$tag
+docker tag "workload:$tag" "us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/workload:$tag"
+docker push "us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/workload:$tag"
 
-docker tag database:$tag us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/database:$tag
-docker push us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/database:$tag
+docker tag "database:$tag" "us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/database:$tag"
+docker push "us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/database:$tag"
 
-docker tag config:$tag us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/config:$tag
-docker push us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/config:$tag
+docker tag "config:$tag" "us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/config:$tag"
+docker push "us-central1-docker.pkg.dev/molten-verve-216720/mongodb-repository/config:$tag"
 
 docker logout https://us-central1-docker.pkg.dev
+
+if [ "${is_patch}" != "true" ]; then
+  echo "$commit_date" > antithesis_next_push.txt
+fi
