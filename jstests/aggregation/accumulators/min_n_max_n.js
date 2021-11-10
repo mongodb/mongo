7836a39feba905c3c4596669b79c05fd93a7f424
Steve La <steve.la@mongodb.com>
2021-11-10 21:52:34 +0000

    SERVER-59590 Add min_n_max_n limit n test

--- jstests/aggregation/accumulators/min_n_max_n.js
+++ jstests/aggregation/accumulators/min_n_max_n.js
@@ -24,14 +24,36 @@ if (!isExactTopNEnabled) {
 // Basic correctness tests.
 let docs = [];
 const n = 4;
-const states = [{state: 'CA', sales: 10}, {state: 'NY', sales: 7}, {state: 'TX', sales: 4}];
+
+const largestInt =
+    NumberDecimal("9223372036854775807");  // This is max int64 which is supported as N.
+const largestIntPlus1 = NumberDecimal("9223372036854775808");  // Adding 1 puts it over the edge.
+
+// Some big number that will allow us to test big without running this testcase into next decade.
+const bigN = 10000;
+
+const states = [
+    {state: 'CA', sales: 10},
+    {state: 'NY', sales: 7},
+    {state: 'TX', sales: 4},
+    {state: 'WY', sales: bigN}
+];
 let expectedMinNResults = [];
 let expectedMaxNResults = [];
+let expectedLargestIntMinNResults = [];
+let expectedLargestIntMaxNResults = [];
+let expectedBigNMinNResults = [];
+let expectedBigNMaxNResults = [];
+
 for (const stateDoc of states) {
     const state = stateDoc['state'];
     const sales = stateDoc['sales'];
     let minArr = [];
     let maxArr = [];
+    let minArrForLargestInt = [];
+    let maxArrForLargestInt = [];
+    let minArrForBigN = [];
+    let maxArrForBigN = [];
     for (let i = 1; i <= sales; ++i) {
         const amount = i * 100;
         docs.push({state: state, sales: amount});
@@ -43,32 +65,56 @@ for (const stateDoc of states) {
         if (sales - n < i) {
             maxArr.push(amount);
         }
+
+        // Always push into the arrays for the largestInt
+        minArrForLargestInt.push(amount);
+        maxArrForLargestInt.push(amount);
+
+        // For the bigInt array, record the lowest/highest 'n-1' values.
+        // This is because we will do a bigN-1 in the actual query to properly validate the result.
+        if (i < bigN) {
+            minArrForBigN.push(amount);
+        }
+        if (sales - bigN < i - 1) {
+            maxArrForBigN.push(amount);
+        }
     }
-    expectedMinNResults.push({_id: state, minSales: minArr});
+    expectedMinNResults.push({_id: state, sales: minArr});
+    expectedLargestIntMinNResults.push({_id: state, sales: minArrForLargestInt});
+    expectedBigNMinNResults.push({_id: state, sales: minArrForBigN});
 
     // Reverse 'maxArr' results since $maxN outputs results in descending order.
-    expectedMaxNResults.push({_id: state, maxSales: maxArr.reverse()});
+    expectedMaxNResults.push({_id: state, sales: maxArr.reverse()});
+    expectedLargestIntMaxNResults.push({_id: state, sales: maxArrForLargestInt.reverse()});
+    expectedBigNMaxNResults.push({_id: state, sales: maxArrForBigN.reverse()});
 }
 
 assert.commandWorked(coll.insert(docs));
 
+// Run a minN/maxN query and compare it against the expectedResult.
 // Note that the output documents are sorted by '_id' so that we can compare actual groups against
 // expected groups (we cannot perform unordered comparison because order matters for $minN/$maxN).
-const actualMinNResults =
-    coll.aggregate([
-            {$group: {_id: '$state', minSales: {$minN: {input: '$sales', n: n}}}},
-            {$sort: {_id: 1}}
-        ])
-        .toArray();
-assert.eq(expectedMinNResults, actualMinNResults);
+function runAndCompareMinMaxN(nFunction, n, expectedResults) {
+    const actualResults =
+        coll.aggregate([
+                {$group: {_id: '$state', sales: {[nFunction]: {input: '$sales', n: n}}}},
+                {$sort: {_id: 1}}
+            ])
+            .toArray();
+    assert.eq(expectedResults, actualResults);
+}
 
-const actualMaxNResults =
-    coll.aggregate([
-            {$group: {_id: '$state', maxSales: {$maxN: {input: '$sales', n: n}}}},
-            {$sort: {_id: 1}}
-        ])
-        .toArray();
-assert.eq(expectedMaxNResults, actualMaxNResults);
+// Test for regular N.
+runAndCompareMinMaxN("$minN", n, expectedMinNResults);
+runAndCompareMinMaxN("$maxN", n, expectedMaxNResults);
+
+// Verify N can be as large as the largest integer.
+runAndCompareMinMaxN("$minN", largestInt, expectedLargestIntMinNResults);
+runAndCompareMinMaxN("$maxN", largestInt, expectedLargestIntMaxNResults);
+
+// Verify a large value for N, but still do not return all of all values.
+runAndCompareMinMaxN("$minN", bigN - 1, expectedBigNMinNResults);
+runAndCompareMinMaxN("$maxN", bigN - 1, expectedBigNMaxNResults);
 
 // Verify that we can dynamically compute 'n' based on the group key for $group.
 const groupKeyNExpr = {
@@ -129,6 +175,20 @@ assert.commandFailedWithCode(coll.runCommand("aggregate", {
     cursor: {}
 }),
                              5787908);
+
+assert.commandFailedWithCode(coll.runCommand("aggregate", {
+    pipeline: [{$group: {_id: {'st': '$state'}, minSales: {$minN: {input: '$sales', n: 0}}}}],
+    cursor: {}
+}),
+                             5787908);
+
+assert.commandFailedWithCode(coll.runCommand("aggregate", {
+    pipeline: [
+        {$group: {_id: {'st': '$state'}, minSales: {$minN: {input: '$sales', n: largestIntPlus1}}}}
+    ],
+    cursor: {}
+}),
+                             5787903);
 
 // Reject invalid specifications.
 
