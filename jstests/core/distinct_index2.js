ca2d3881fd96febfbbcd8dba641ff2e283583929
Benety Goh <benety@mongodb.com>
2021-11-11 05:42:03 -0500

    SERVER-61279 clean up distinct_index2.js

--- jstests/core/distinct_index2.js
+++ jstests/core/distinct_index2.js
@@ -1,41 +1,47 @@
-t = db.distinct_index2;
-t.drop();
-
-t.createIndex({a: 1, b: 1});
-t.createIndex({c: 1});
+(function() {
+'use strict';
 
 // Uniformly distributed dataset.
 // If we use a randomly generated dataset, we might not
-// generate all the distinct values in the range [0, 10).
-for (var a = 0; a < 10; a++) {
-    for (var b = 0; b < 10; b++) {
-        for (var c = 0; c < 10; c++) {
-            t.insert({a: a, b: b, c: c});
+// generate all the distinct values in the range [0, k).
+const k = 10;
+let docs = [];
+let docId = 0;
+for (let a = 0; a < k; a++) {
+    for (let b = 0; b < k; b++) {
+        for (let c = 0; c < k; c++) {
+            docs.push({_id: docId++, a: a, b: b, c: c});
         }
     }
 }
 
-correct = [];
-for (i = 0; i < 10; i++)
-    correct.push(i);
+const collNamePrefix = 'distinct_index2_';
+let collCount = 0;
 
+const correct = Array.from({length: k}, (_, i) => i);
 function check(field) {
-    res = t.distinct(field);
-    res = res.sort();
-    assert.eq(correct, res, "check: " + field);
+    let res = t.distinct(field);
+    assert.sameMembers(correct, res, "check: " + field + "; coll: " + t.getFullName());
 
     if (field != "a") {
         res = t.distinct(field, {a: 1});
-        res = res.sort();
-        assert.eq(correct, res, "check 2: " + field);
+        assert.sameMembers(correct, res, "check 2: " + field + "; coll: " + t.getFullName());
     }
 }
+
+let t = db.getCollection(collNamePrefix + collCount++);
+t.drop();
+assert.commandWorked(t.createIndexes([{a: 1, b: 1}, {c: 1}]));
+assert.commandWorked(t.insert(docs));
 
 check("a");
 check("b");
 check("c");
 
 // hashed index should produce same results.
-t.dropIndexes();
-t.createIndex({a: "hashed"});
+t = db.getCollection(collNamePrefix + collCount++);
+t.drop();
+assert.commandWorked(t.createIndex({a: "hashed"}));
+assert.commandWorked(t.insert(docs));
 check("a");
+})();
