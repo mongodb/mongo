15a4a05bbab92423f6a7796ba980499c8dc1853b
Josef Ahmad <josef.ahmad@mongodb.com>
2021-11-12 09:07:07 +0000

    SERVER-60806 Validate collMod for collections clustered by arbitrary key

--- jstests/noPassthrough/clustered_coll_mod.js
+++ jstests/noPassthrough/clustered_coll_mod.js
@@ -10,75 +10,78 @@
 "use strict";
 
 load("jstests/libs/clustered_collection_util.js");
+load("jstests/libs/collection_drop_recreate.js");
 
 // Run TTL monitor constantly to speed up this test.
 const conn = MongoRunner.runMongod({setParameter: 'ttlMonitorSleepSecs=1'});
 
-const clusteredIndexesEnabled =
-    assert.commandWorked(conn.adminCommand({getParameter: 1, featureFlagClusteredIndexes: 1}))
-        .featureFlagClusteredIndexes.value;
-
-if (!clusteredIndexesEnabled) {
+if (ClusteredCollectionUtil.areClusteredIndexesEnabled(conn) == false) {
     jsTestLog('Skipping test because the clustered indexes feature flag is disabled');
     MongoRunner.stopMongod(conn);
     return;
 }
 
-const dbName = jsTestName();
-const testDB = conn.getDB(dbName);
-const collName = "coll";
+function testCollMod(coll, clusterKey, clusterKeyName) {
+    const collName = coll.getName();
+    const clusterKeyFieldName = Object.keys(clusterKey)[0];
+    // Set the original expireAfterSeconds to a day.
+    const expireAfterSeconds = 60 * 60 * 24;
 
-// Set the original expireAfterSeconds to a day.
-const expireAfterSeconds = 60 * 60 * 24;
+    assertDropCollection(coll.getDB(), collName);
+    assert.commandWorked(coll.getDB().createCollection(
+        coll.getName(), {clusteredIndex: {key: clusterKey, unique: true}, expireAfterSeconds}));
 
-assert.commandWorked(testDB.createCollection(
-    collName, {clusteredIndex: {key: {_id: 1}, unique: true}, expireAfterSeconds}));
+    // Insert documents less than a day old so they don't automatically expire.
+    const batchSize = 10;
+    const now = new Date();
+    let docs = [];
+    for (let i = 0; i < batchSize; i++) {
+        // Make them 5 minutes expired.
+        const fiveMinutesPastMS = 5 * 60 * 1000;
+        const recentDate = new Date(now - fiveMinutesPastMS - i);
+        docs.push({
+            [clusterKeyFieldName]: recentDate,
+            info: "unexpired",
+        });
+    }
+    assert.commandWorked(coll.insertMany(docs, {ordered: false}));
+    assert.eq(coll.find().itcount(), batchSize);
 
-// Insert documents less than a day old so they don't automatically expire.
-const batchSize = 10;
-const now = new Date();
-let docs = [];
-for (let i = 0; i < batchSize; i++) {
-    // Make them 5 minutes expired.
-    const fiveMinutesPastMS = 5 * 60 * 1000;
-    const recentDate = new Date(now - fiveMinutesPastMS - i);
-    docs.push({
-        _id: recentDate,
-        info: "unexpired",
-    });
+    ClusteredCollectionUtil.waitForTTL(coll.getDB());
+    assert.eq(coll.find().itcount(), batchSize);
+
+    // Shorten the expireAfterSeconds so all the documents in the collection are expired.
+    assert.commandWorked(coll.getDB().runCommand({collMod: collName, expireAfterSeconds: 1}));
+
+    ClusteredCollectionUtil.waitForTTL(coll.getDB());
+
+    // Confirm all documents were deleted once the expireAfterSeconds was shortened.
+    assert.eq(coll.find().itcount(), 0);
+
+    // Turn TTL off.
+    assert.commandWorked(coll.getDB().runCommand({collMod: collName, expireAfterSeconds: "off"}));
+
+    // Ensure there is no outstanding TTL pass in progress that will still remove entries.
+    ClusteredCollectionUtil.waitForTTL(coll.getDB());
+
+    assert.commandWorked(coll.insert({[clusterKeyFieldName]: now, info: "unexpired"}));
+
+    ClusteredCollectionUtil.waitForTTL(coll.getDB());
+
+    assert.eq(coll.find().itcount(), 1);
+
+    assert.commandFailedWithCode(
+        coll.getDB().runCommand(
+            {collMod: collName, index: {keyPattern: {[clusterKeyFieldName]: 1}, hidden: true}}),
+        6011800);
+
+    assert.commandFailedWithCode(
+        coll.getDB().runCommand({collMod: collName, index: {name: clusterKeyName, hidden: true}}),
+        6011800);
 }
-const coll = testDB[collName];
-assert.commandWorked(coll.insertMany(docs, {ordered: false}));
-assert.eq(coll.find().itcount(), batchSize);
 
-ClusteredCollectionUtil.waitForTTL(testDB);
-assert.eq(coll.find().itcount(), batchSize);
-
-// Shorten the expireAfterSeconds so all the documents in the collection are expired.
-assert.commandWorked(testDB.runCommand({collMod: collName, expireAfterSeconds: 1}));
-
-ClusteredCollectionUtil.waitForTTL(testDB);
-
-// Confirm all documents were deleted once the expireAfterSeconds was shortened.
-assert.eq(coll.find().itcount(), 0);
-
-// Turn TTL off.
-assert.commandWorked(testDB.runCommand({collMod: collName, expireAfterSeconds: "off"}));
-
-// Ensure there is no outstanding TTL pass in progress that will still remove entries.
-ClusteredCollectionUtil.waitForTTL(testDB);
-
-assert.commandWorked(coll.insert({_id: now, info: "unexpired"}));
-
-ClusteredCollectionUtil.waitForTTL(testDB);
-
-assert.eq(coll.find().itcount(), 1);
-
-assert.commandFailedWithCode(
-    testDB.runCommand({collMod: collName, index: {keyPattern: {_id: 1}, hidden: true}}), 6011800);
-
-assert.commandFailedWithCode(
-    testDB.runCommand({collMod: collName, index: {name: "_id_", hidden: true}}), 6011800);
+testCollMod(conn.getDB(jsTestName())["coll"], {_id: 1}, "_id_");
+testCollMod(conn.getDB("local")["coll"], {ts: 1}, "ts_1");
 
 MongoRunner.stopMongod(conn);
 })();
