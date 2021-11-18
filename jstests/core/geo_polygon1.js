42db290dae003db1e77b9971f6c6bbd7f090e2c7
Benety Goh <benety@mongodb.com>
2021-11-18 20:32:42 +0000

    SERVER-61512 clean up geo_polygon1.js

--- jstests/core/geo_polygon1.js
+++ jstests/core/geo_polygon1.js
@@ -10,38 +10,42 @@ let collCount = 0;
 let t = db.getCollection(collNamePrefix + collCount++);
 t.drop();
 
-let num = 0;
+assert.commandWorked(t.createIndex({loc: "2d"}));
+
+let docs = [];
+let docId = 0;
 for (let x = 1; x < 9; x++) {
     for (let y = 1; y < 9; y++) {
-        const o = {_id: num++, loc: [x, y]};
-        t.save(o);
+        docs.push({_id: docId++, loc: [x, y]});
     }
 }
-
-t.createIndex({loc: "2d"});
+assert.commandWorked(t.insert(docs));
 
 const triangle = [[0, 0], [1, 1], [0, 2]];
 
 // Look at only a small slice of the data within a triangle
-assert.eq(1, t.find({loc: {"$within": {"$polygon": triangle}}}).count(), "Triangle Test");
+assert.eq(1, t.countDocuments({loc: {"$within": {"$polygon": triangle}}}), "Triangle Test");
 
 let boxBounds = [[0, 0], [0, 10], [10, 10], [10, 0]];
 
-assert.eq(num, t.find({loc: {"$within": {"$polygon": boxBounds}}}).count(), "Bounding Box Test");
+assert.eq(
+    docs.length, t.find({loc: {"$within": {"$polygon": boxBounds}}}).count(), "Bounding Box Test");
 
 // Make sure we can add object-based polygons
-assert.eq(num,
-          t.find({
-               loc: {$within: {$polygon: {a: [-10, -10], b: [-10, 10], c: [10, 10], d: [10, -10]}}}
-           }).count());
+assert.eq(docs.length, t.countDocuments({
+    loc: {$within: {$polygon: {a: [-10, -10], b: [-10, 10], c: [10, 10], d: [10, -10]}}}
+}));
 
 // Look in a box much bigger than the one we have data in
 boxBounds = [[-100, -100], [-100, 100], [100, 100], [100, -100]];
-assert.eq(
-    num, t.find({loc: {"$within": {"$polygon": boxBounds}}}).count(), "Big Bounding Box Test");
+assert.eq(docs.length,
+          t.countDocuments({loc: {"$within": {"$polygon": boxBounds}}}),
+          "Big Bounding Box Test");
 
 t = db.getCollection(collNamePrefix + collCount++);
 t.drop();
+
+assert.commandWorked(t.createIndex({loc: "2d"}));
 
 const pacman = [
     [0, 2],
@@ -55,35 +59,23 @@ const pacman = [
     [2, 0]  // Bottom
 ];
 
-t.save({loc: [1, 3]});  // Add a point that's in
-assert.commandWorked(t.createIndex({loc: "2d"}));
+assert.commandWorked(t.insert({_id: docId++, loc: [1, 3]}));  // Add a point that's in
 
-assert.eq(1, t.find({loc: {$within: {$polygon: pacman}}}).count(), "Pacman single point");
+assert.eq(1, t.countDocuments({loc: {$within: {$polygon: pacman}}}), "Pacman single point");
 
-t.save({loc: [5, 3]});   // Add a point that's out right in the mouth opening
-t.save({loc: [3, 7]});   // Add a point above the center of the head
-t.save({loc: [3, -1]});  // Add a point below the center of the bottom
+assert.commandWorked(t.insert([
+    {_id: docId++, loc: [5, 3]},   // Add a point that's out right in the mouth opening
+    {_id: docId++, loc: [3, 7]},   // Add a point above the center of the head
+    {_id: docId++, loc: [3, -1]},  // Add a point below the center of the bottom
+]));
 
-assert.eq(1, t.find({loc: {$within: {$polygon: pacman}}}).count(), "Pacman double point");
+assert.eq(1, t.countDocuments({loc: {$within: {$polygon: pacman}}}), "Pacman double point");
 
 // Make sure we can't add bad polygons
-let okay = true;
-try {
-    t.find({loc: {$within: {$polygon: [1, 2]}}}).toArray();
-    okay = false;
-} catch (e) {
-}
-assert(okay);
-try {
-    t.find({loc: {$within: {$polygon: [[1, 2]]}}}).toArray();
-    okay = false;
-} catch (e) {
-}
-assert(okay);
-try {
-    t.find({loc: {$within: {$polygon: [[1, 2], [2, 3]]}}}).toArray();
-    okay = false;
-} catch (e) {
-}
-assert(okay);
+assert.throwsWithCode(() => t.find({loc: {$within: {$polygon: [1, 2]}}}).toArray(),
+                      ErrorCodes.BadValue);
+assert.throwsWithCode(() => t.find({loc: {$within: {$polygon: [[1, 2]]}}}).toArray(),
+                      ErrorCodes.BadValue);
+assert.throwsWithCode(() => t.find({loc: {$within: {$polygon: [[1, 2], [2, 3]]}}}).toArray(),
+                      ErrorCodes.BadValue);
 })();
