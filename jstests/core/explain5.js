8c41f7ab1793621f8068e474807b92a59c1ca8d5
Benety Goh <benety@mongodb.com>
2021-11-18 14:01:48 +0000

    SERVER-61586 clean up explain5.js to use bulk inserts

--- jstests/core/explain5.js
+++ jstests/core/explain5.js
@@ -2,6 +2,7 @@
 // @tags: [
 //   assumes_balancer_off,
 //   assumes_read_concern_local,
+//   operations_longer_than_stepdown_interval,  // large bulk inserts
 // ]
 
 (function() {
@@ -10,12 +11,16 @@
 const t = db.jstests_explain5;
 t.drop();
 
-assert.commandWorked(t.createIndex({a: 1}));
-assert.commandWorked(t.createIndex({b: 1}));
+assert.commandWorked(t.createIndexes([
+    {a: 1},
+    {b: 1},
+]));
 
+let docs = [];
 for (let i = 0; i < 1000; ++i) {
-    assert.commandWorked(t.insert({_id: i, a: i, b: i % 3}));
+    docs.push({_id: i, a: i, b: i % 3});
 }
+assert.commandWorked(t.insert(docs));
 
 // Query with an initial set of documents.
 const explain1 = t.find({a: {$gte: 0}, b: 2}).sort({a: 1}).hint({a: 1}).explain("executionStats");
@@ -24,9 +29,11 @@ const stats1 = explain1.executionStats;
 assert.eq(333, stats1.nReturned, 'wrong nReturned for explain1');
 assert.eq(1000, stats1.totalKeysExamined, 'wrong totalKeysExamined for explain1');
 
+docs = [];
 for (let i = 1000; i < 2000; ++i) {
-    assert.commandWorked(t.insert({_id: i, a: i, b: i % 3}));
+    docs.push({_id: i, a: i, b: i % 3});
 }
+assert.commandWorked(t.insert(docs));
 
 // Query with some additional documents.
 const explain2 = t.find({a: {$gte: 0}, b: 2}).sort({a: 1}).hint({a: 1}).explain("executionStats");
