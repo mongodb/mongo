5b6df52bd38e223fb186c6ede035de92abbeb82b
Reo Kimura <reo.kimura@mongodb.com>
2021-11-22 18:56:43 +0000

    SERVER-58533 Log FCV at initialization and on log rotation

--- src/mongo/db/commands/feature_compatibility_version.cpp
+++ src/mongo/db/commands/feature_compatibility_version.cpp
@@ -421,7 +421,7 @@ void FeatureCompatibilityVersion::initializeForStartup(OperationContext* opCtx) 
     invariant(opCtx->lockState()->isW());
     auto featureCompatibilityVersion = findFeatureCompatibilityVersionDocument(opCtx);
     if (!featureCompatibilityVersion) {
-        LOGV2(5853303, "featureCompatibilityVersion document missing at startup");
+        FeatureCompatibilityVersion::logFCVWithContext("startup"_sd);
         return;
     }
 
@@ -448,10 +448,7 @@ void FeatureCompatibilityVersion::initializeForStartup(OperationContext* opCtx) 
     serverGlobalParams.mutableFeatureCompatibility.setVersion(version);
     FeatureCompatibilityVersion::updateMinWireVersion();
 
-    LOGV2(5853300,
-          "Intializing featureCompatibilityVersion at startup",
-          "featureCompatibilityVersion"_attr =
-              multiversion::toString(serverGlobalParams.featureCompatibility.getVersion()));
+    FeatureCompatibilityVersion::logFCVWithContext("startup"_sd);
 
     // On startup, if the version is in an upgrading or downgrading state, print a warning.
     if (serverGlobalParams.featureCompatibility.isUpgradingOrDowngrading()) {
@@ -518,6 +515,19 @@ void FeatureCompatibilityVersion::advanceLastFCVUpdateTimestamp(Timestamp fcvUpd
 void FeatureCompatibilityVersion::clearLastFCVUpdateTimestamp() {
     stdx::lock_guard lk(lastFCVUpdateTimestampMutex);
     lastFCVUpdateTimestamp = Timestamp();
+}
+
+void FeatureCompatibilityVersion::logFCVWithContext(StringData context) {
+    LOGV2_OPTIONS(
+        5853300,
+        {logv2::LogComponent::kReplication},
+        "current featureCompatibilityVersion value",
+        "featureCompatibilityVersion"_attr =
+            serverGlobalParams.featureCompatibility.isVersionInitialized()
+            ? multiversion::toString(serverGlobalParams.featureCompatibility.getVersion())
+            : multiversion::toString(
+                  multiversion::FeatureCompatibilityVersion::kUnsetDefaultLastLTSBehavior),
+        "context"_attr = context);
 }
 
 
--- src/mongo/db/log_process_details.cpp
+++ src/mongo/db/log_process_details.cpp
@@ -38,6 +38,7 @@
 #include "mongo/bson/bsonobj.h"
 #include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/bson/json.h"
+#include "mongo/db/commands/feature_compatibility_version.h"
 #include "mongo/db/repl/repl_set_config.h"
 #include "mongo/db/repl/replication_coordinator.h"
 #include "mongo/db/server_options.h"
@@ -103,14 +104,7 @@ void logProcessDetailsForLogRotate(ServiceContext* serviceContext) {
         }
     }
 
-    LOGV2(5853301,
-          "featureCompatibilityVersion on log rotation",
-          "featureCompatibilityVersion"_attr =
-              serverGlobalParams.featureCompatibility.isVersionInitialized()
-              ? multiversion::toString(serverGlobalParams.featureCompatibility.getVersion())
-              : multiversion::toString(
-                    multiversion::FeatureCompatibilityVersion::kUnsetDefaultLastLTSBehavior));
-
+    FeatureCompatibilityVersion::logFCVWithContext("log rotation"_sd);
     logProcessDetails(nullptr);
 }
 
