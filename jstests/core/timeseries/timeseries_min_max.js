5020d2f2bf05d427943f7e78857ff0b235fc3027
Dan Larkin-York <dan.larkin-york@mongodb.com>
2021-11-24 00:24:31 +0000

    SERVER-61602 timeseries_min_max.js does not assume measurements are returned in insertion order

--- jstests/core/timeseries/timeseries_min_max.js
+++ jstests/core/timeseries/timeseries_min_max.js
@@ -42,15 +42,28 @@ TimeseriesTest.run((insert) => {
         doc[timeFieldName] = ISODate();
         assert.commandWorked(insert(coll, doc));
 
+        // Find the _id value of the measurement just inserted.
+        const matchingMeasurements = coll.find(doc).toArray();
+        assert.eq(matchingMeasurements.length, 1);
+        const measurementId = matchingMeasurements[0]._id;
+
+        // Find the bucket the measurement belongs to.
         const bucketDocs = bucketsColl
-                               .find({}, {
-                                   'control.min._id': 0,
-                                   'control.max._id': 0,
-                                   ['control.min.' + timeFieldName]: 0,
-                                   ['control.max.' + timeFieldName]: 0
-                               })
+                               .find({
+                                   $and: [
+                                       {"control.min._id": {$lte: measurementId}},
+                                       {"control.max._id": {$gte: measurementId}}
+                                   ]
+                               },
+                                     {
+                                         'control.min._id': 0,
+                                         'control.max._id': 0,
+                                         ['control.min.' + timeFieldName]: 0,
+                                         ['control.max.' + timeFieldName]: 0
+                                     })
                                .toArray();
-        assert.eq(1, bucketDocs.length, bucketDocs);
+        assert.eq(bucketDocs.length, 1);
+
         const bucketDoc = bucketDocs[0];
         jsTestLog('Bucket collection document: ' + tojson(bucketDoc));
 
