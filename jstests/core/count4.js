be431ed2158ab6cbd133492ac152753c90478be6
Benety Goh <benety@mongodb.com>
2021-12-01 06:45:07 -0500

    SERVER-61798 clean up count4.js

--- jstests/core/count4.js
+++ jstests/core/count4.js
@@ -1,19 +1,26 @@
+(function() {
+'use strict';
 
-t = db.count4;
+const t = db.count4;
 t.drop();
 
-for (i = 0; i < 100; i++) {
-    t.save({x: i});
+let docs = [];
+for (let i = 0; i < 100; i++) {
+    docs.push({_id: i, x: i});
 }
+assert.commandWorked(t.insert(docs));
 
-q = {
+const q = {
     x: {$gt: 25, $lte: 75}
 };
 
-assert.eq(50, t.find(q).count(), "A");
-assert.eq(50, t.find(q).itcount(), "B");
+assert.eq(50, t.find(q).count(), 'collection scan fast count: ' + tojson(q));
+assert.eq(50, t.find(q).itcount(), 'collection scan find iterator count: ' + tojson(q));
+assert.eq(50, t.countDocuments(q), 'collection scan aggregation count: ' + tojson(q));
 
-t.createIndex({x: 1});
+assert.commandWorked(t.createIndex({x: 1}));
 
-assert.eq(50, t.find(q).count(), "C");
-assert.eq(50, t.find(q).itcount(), "D");
+assert.eq(50, t.find(q).count(), 'index scan fast count: ' + tojson(q));
+assert.eq(50, t.find(q).itcount(), 'index scan find iterator count: ' + tojson(q));
+assert.eq(50, t.countDocuments(q), 'index scan aggregation count: ' + tojson(q));
+})();
