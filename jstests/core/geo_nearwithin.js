07eff86a1d0f925f0785e2f87d36e2fba1e9d489
Benety Goh <benety@mongodb.com>
2021-12-15 21:56:16 +0000

    SERVER-62070 clean up geo_nearwithin.js

--- jstests/core/geo_nearwithin.js
+++ jstests/core/geo_nearwithin.js
@@ -1,31 +1,36 @@
 // Test $near + $within.
 (function() {
-t = db.geo_nearwithin;
+'use strict';
+
+const t = db.geo_nearwithin;
 t.drop();
 
-points = 10;
+assert.commandWorked(t.createIndex({geo: "2d"}));
+
+let docs = [];
+let docId = 0;
+const points = 10;
 for (var x = -points; x < points; x += 1) {
     for (var y = -points; y < points; y += 1) {
-        assert.commandWorked(t.insert({geo: [x, y]}));
+        docs.push({_id: docId++, geo: [x, y]});
     }
 }
-
-assert.commandWorked(t.createIndex({geo: "2d"}));
+assert.commandWorked(t.insert(docs));
 
 const runQuery = (center) =>
     t.find({$and: [{geo: {$near: [0, 0]}}, {geo: {$within: {$center: center}}}]}).toArray();
 
-resNear = runQuery([[0, 0], 1]);
-assert.eq(resNear.length, 5);
+let resNear = runQuery([[0, 0], 1]);
+assert.eq(resNear.length, 5, tojson(resNear));
 
 resNear = runQuery([[0, 0], 0]);
-assert.eq(resNear.length, 1);
+assert.eq(resNear.length, 1, tojson(resNear));
 
 resNear = runQuery([[1, 0], 0.5]);
-assert.eq(resNear.length, 1);
+assert.eq(resNear.length, 1, tojson(resNear));
 
 resNear = runQuery([[1, 0], 1.5]);
-assert.eq(resNear.length, 9);
+assert.eq(resNear.length, 9, tojson(resNear));
 
 // We want everything distance >1 from us but <1.5
 // These points are (-+1, -+1)
@@ -36,5 +41,5 @@ resNear = t.find({
                    {geo: {$not: {$within: {$center: [[0, 0], 1]}}}}
                ]
            }).toArray();
-assert.eq(resNear.length, 4);
+assert.eq(resNear.length, 4, tojson(resNear));
 }());
