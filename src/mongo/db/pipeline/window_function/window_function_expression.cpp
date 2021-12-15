c07540fefab04df035434955230f95a1d6297ed6
Mihai Andrei <mihai.andrei@10gen.com>
2021-12-15 16:04:08 +0000

    SERVER-61951 Delete TODO referencing SERVER-52247

--- src/mongo/db/pipeline/window_function/window_function_expression.cpp
+++ src/mongo/db/pipeline/window_function/window_function_expression.cpp
@@ -61,8 +61,6 @@ REGISTER_WINDOW_FUNCTION_CONDITIONALLY(linearFill,
                                        ExpressionLinearFill::parse,
                                        multiversion::FeatureCompatibilityVersion::kVersion_5_2,
                                        feature_flags::gFlagFill.isEnabledAndIgnoreFCV());
-// TODO SERVER-52247 Replace boost::none with 'gFeatureFlagExactTopNAccumulator.getVersion()' below
-// once 'gFeatureFlagExactTopNAccumulator' is set to true by default and is configured with an FCV.
 REGISTER_WINDOW_FUNCTION_CONDITIONALLY(
     minN,
     (ExpressionN<WindowFunctionMinN, AccumulatorMinN>::parse),
