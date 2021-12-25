d3b2c6b751133636f8b7622c6fa3ed805be8facd
Andrew Shuvalov <andrew.shuvalov@mongodb.com>
2021-12-25 21:40:04 +0000

    SERVER-62273 fix health monitor integration tests

--- jstests/sharding/health_monitor/server_status_health.js
+++ jstests/sharding/health_monitor/server_status_health.js
@@ -6,10 +6,13 @@
 var st = new ShardingTest({
     mongos: 1,
     shards: 1,
+    other: {mongosOptions: {setParameter: {featureFlagHealthMonitoring: true}}}
 });
 
 const result = assert.commandWorked(st.s0.adminCommand({serverStatus: 1})).health;
 print(tojson(result));
 
 assert(result.state == "StartupCheck" || result.state == "Ok");
+
+st.stop();
 })();
--- jstests/sharding/health_monitor/set_parameter_health_monitor_intensity.js
+++ jstests/sharding/health_monitor/set_parameter_health_monitor_intensity.js
@@ -7,6 +7,7 @@ var st = new ShardingTest({
     mongos: [{
         setParameter: {
             healthMonitoring: tojson({dns: "off", ldap: "critical"}),
+            featureFlagHealthMonitoring: true
         }
     }],
     shards: 1,
@@ -22,8 +23,10 @@ assert.commandFailed(st.s0.adminCommand({"setParameter": 1, healthMonitoring: {i
 
 assert.commandWorked(
     st.s0.adminCommand({"setParameter": 1, healthMonitoring: {dns: 'non-critical', ldap: 'off'}}));
-var result = assert.commandWorked(st.adminCommand({"getParameter": 1, healthMonitoring: 1}));
+var result = st.s0.adminCommand({"getParameter": 1, healthMonitoring: 1});
 print(tojson(result));
 assert.eq(result.healthMonitoring.dns, "non-critical");
 assert.eq(result.healthMonitoring.ldap, "off");
+
+st.stop();
 }());
