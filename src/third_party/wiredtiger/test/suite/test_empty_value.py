022728055c0a6a1994e49a33f6426be4b8494a2a
Luke Chen <luke.chen@mongodb.com>
2021-12-30 14:14:04 +1100

    Import wiredtiger: 22d1d1ebe7f449d8971874968c5384d35683f9fd from branch mongodb-5.2
    ref: e29560bb09..22d1d1ebe7
    for: 5.2.0-rc4
    
    WT-8293       Re-enabling empty_value python test

--- src/third_party/wiredtiger/test/suite/test_empty_value.py
+++ src/third_party/wiredtiger/test/suite/test_empty_value.py
@@ -37,7 +37,6 @@ class test_row_store_empty_values(wttest.WiredTigerTestCase):
     conn_config = 'statistics=(all)'
 
     # Smoke test empty row-store values.
-    @unittest.skip("Skipping, to be removed or updated in future ticket")
     def test_row_store_empty_values(self):
         nentries = 25000
         uri = 'file:test_empty_values'          # This is a btree layer test.
