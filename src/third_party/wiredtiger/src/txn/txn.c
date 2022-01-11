407ec649e0cfaee34883a398db5d8975954f519a
Luke Chen <luke.chen@mongodb.com>
2022-01-11 16:21:59 +1100

    Import wiredtiger: e1427e4a69fa834ea3630523526379eaaef18c6c from branch mongodb-5.2
    ref: 02329fa1d9..e1427e4a69
    for: 5.2.0-rc5
    
    WT-8598       Avoid checkpoint cleanup always on shutdown

--- src/third_party/wiredtiger/src/txn/txn.c
+++ src/third_party/wiredtiger/src/txn/txn.c
@@ -2369,8 +2369,10 @@ __wt_txn_global_shutdown(WT_SESSION_IMPL *session, const char **cfg)
     WT_SESSION_IMPL *s;
     char ts_string[WT_TS_INT_STRING_SIZE];
     const char *ckpt_cfg;
+    bool use_timestamp;
 
     conn = S2C(session);
+    use_timestamp = false;
 
     /*
      * Perform a system-wide checkpoint so that all tables are consistent with each other. All
@@ -2378,19 +2380,20 @@ __wt_txn_global_shutdown(WT_SESSION_IMPL *session, const char **cfg)
      * before shutting down all the subsystems. We have shut down all user sessions, but send in
      * true for waiting for internal races.
      */
+    F_SET(conn, WT_CONN_CLOSING_CHECKPOINT);
     WT_TRET(__wt_config_gets(session, cfg, "use_timestamp", &cval));
     ckpt_cfg = "use_timestamp=false";
     if (cval.val != 0) {
         ckpt_cfg = "use_timestamp=true";
         if (conn->txn_global.has_stable_timestamp)
-            F_SET(conn, WT_CONN_CLOSING_TIMESTAMP);
+            use_timestamp = true;
     }
     if (!F_ISSET(conn, WT_CONN_IN_MEMORY | WT_CONN_READONLY | WT_CONN_PANIC)) {
         /*
          * Perform rollback to stable to ensure that the stable version is written to disk on a
          * clean shutdown.
          */
-        if (F_ISSET(conn, WT_CONN_CLOSING_TIMESTAMP)) {
+        if (use_timestamp) {
             __wt_verbose(session, WT_VERB_RTS,
               "performing shutdown rollback to stable with stable timestamp: %s",
               __wt_timestamp_to_string(conn->txn_global.stable_timestamp, ts_string));
