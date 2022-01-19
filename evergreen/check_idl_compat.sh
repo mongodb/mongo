661d4df5bff6b1305c1f316cdd1c661dcbbcd938
Huayu Ouyang <huayu.ouyang@mongodb.com>
2022-01-19 15:38:02 +0000

    SERVER-61287 Remove IDL compatibility checker script TODO

--- evergreen/check_idl_compat.sh
+++ evergreen/check_idl_compat.sh
@@ -11,6 +11,5 @@ $python buildscripts/idl/check_versioned_api_commands_have_idl_definitions.py -v
 $python buildscripts/idl/checkout_idl_files_from_past_releases.py -v idls
 find idls -maxdepth 1 -mindepth 1 -type d | while read dir; do
   echo "Performing idl check compatibility with release: $dir:"
-  # TODO (SERVER-61287): We haven't actually checked out old enterprise IDLs
   $python buildscripts/idl/idl_check_compatibility.py -v --old-include "$dir/src" --old-include "$dir/src/mongo/db/modules/enterprise/src" --new-include src --new-include src/mongo/db/modules/enterprise/src "$dir/src" src
 done
