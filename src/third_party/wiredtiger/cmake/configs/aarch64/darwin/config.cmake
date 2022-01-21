1f15a513cddc07b77328dea84861775878ece876
Luke Chen <luke.chen@mongodb.com>
2022-01-21 16:03:27 +1100

    Import wiredtiger: 1dea0087c18f7edd182da370ed1762692815dd59 from branch mongodb-master
    ref: aab32463d8..1dea0087c1
    for: 5.3.0
    
    WT-8594       Compile with Apple M1 chip

---
+++ src/third_party/wiredtiger/cmake/configs/aarch64/darwin/config.cmake
@@ -0,0 +1,19 @@
+include(CheckCCompilerFlag)
+
+set(WT_ARCH "aarch64" CACHE STRING "")
+set(WT_OS "darwin" CACHE STRING "")
+set(WT_POSIX ON CACHE BOOL "")
+
+# ARMv8-A is the 64-bit ARM architecture, turn on the optional CRC instructions.
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -march=armv8-a+crc" CACHE STRING "" FORCE)
+
+check_c_compiler_flag("-moutline-atomics" has_moutline_atomics)
+
+# moutline-atomics preserves backwards compatibility with Arm v8.0 systems but also supports
+# using Arm v8.1 atomics. The latter can massively improve performance on larger Arm systems.
+# The flag was back ported to gcc8, 9 and is the default in gcc10+. See if the compiler supports
+# the flag.
+if(has_moutline_atomics)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -moutline-atomics" CACHE STRING "" FORCE)
+endif()
+unset(has_moutline_atomics CACHE)
