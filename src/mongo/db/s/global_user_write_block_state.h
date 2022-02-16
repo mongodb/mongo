46ff463ec5608f17b6243899eb12535ec98b5dc8
Jordi Serra Torrens <jordi.serra-torrens@mongodb.com>
2022-02-16 12:18:57 +0000

    SERVER-63611 Create in-memory state holder for global user write blocking

---
+++ src/mongo/db/s/global_user_write_block_state.h
@@ -0,0 +1,63 @@
+/**
+ *    Copyright (C) 2022-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#pragma once
+
+#include "mongo/db/operation_context.h"
+#include "mongo/db/service_context.h"
+
+namespace mongo {
+
+class GlobalUserWriteBlockState {
+public:
+    GlobalUserWriteBlockState() = default;
+
+    static GlobalUserWriteBlockState* get(ServiceContext* serviceContext);
+    static GlobalUserWriteBlockState* get(OperationContext* opCtx);
+
+    /**
+     * Methods to control the global user write blocking state. Callers must be hold the GlobalLock
+     * in MODE_X.
+     */
+    void enableUserWriteBlocking(OperationContext* opCtx);
+    void disableUserWriteBlocking(OperationContext* opCtx);
+
+    /**
+     * Checks that user writes are allowed on the specified namespace. Callers must hold the
+     * GlobalLock in any mode.
+     */
+    void checkUserWritesAllowed(OperationContext* opCtx, const NamespaceString& nss) const;
+
+private:
+    // Modifying the state below requires holding the GlobalLock in X mode; holding the DBLock in
+    // any mode is acceptable for reading it.
+    bool _globalUserWritesBlocked{false};
+};
+
+}  // namespace mongo
