687a404519f8c83e494aa3523aed7ef9d0784bff
David Percy <david.percy@mongodb.com>
2022-02-18 19:04:56 +0000

    SERVER-63841 Disable time-series metric indexes
    This reverts commit f44d4a644c49e2370d4dfe1792dda5855d5d67ad.
    This reverts commit 0d6e7a4a1a035b57178458c341578260f956977c.
    
    (cherry picked from commit 239052f6e878dcf0892a8ae33b9e69c30a6ffc6d)

--- src/mongo/db/catalog/multi_index_block.cpp
+++ src/mongo/db/catalog/multi_index_block.cpp
@@ -52,7 +52,6 @@
 #include "mongo/db/repl/replication_coordinator.h"
 #include "mongo/db/repl/tenant_migration_conflict_info.h"
 #include "mongo/db/storage/storage_options.h"
-#include "mongo/db/storage/storage_parameters_gen.h"
 #include "mongo/db/storage/write_unit_of_work.h"
 #include "mongo/db/timeseries/timeseries_constants.h"
 #include "mongo/db/timeseries/timeseries_index_schema_conversion_functions.h"
@@ -272,10 +271,10 @@ StatusWith<std::vector<BSONObj>> MultiIndexBlock::init(
             info = statusWithInfo.getValue();
             indexInfoObjs.push_back(info);
 
+            // TODO SERVER-54592: Remove FCV check once feature flag is enabled.
             boost::optional<TimeseriesOptions> options = collection->getTimeseriesOptions();
             if (options &&
-                feature_flags::gTimeseriesMetricIndexes.isEnabled(
-                    serverGlobalParams.featureCompatibility) &&
+                serverGlobalParams.featureCompatibility.isFCVUpgradingToOrAlreadyLatest() &&
                 timeseries::doesBucketsIndexIncludeMeasurement(
                     opCtx, collection->ns(), *options, info)) {
                 invariant(collection->getTimeseriesBucketsMayHaveMixedSchemaData());
