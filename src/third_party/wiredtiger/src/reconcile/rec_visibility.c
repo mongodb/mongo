d9cbe79974a9dd0596c72cd4033fc9d8ceb1eb5b
Luke Chen <luke.chen@mongodb.com>
2022-02-23 11:44:18 +1100

    Import wiredtiger: 9698fc6dd74a57d0281d7d27ecc5311d433441c7 from branch mongodb-4.4
    ref: 57d44e1a1f..9698fc6dd7
    for: 4.4.13
    
    WT-8004       Create a read order for the architecture guide
    WT-8320       Select updates restored from history store irrespective of visibility
    WT-8424       Use consistent toolchain in little-endian
    WT-8477       Enforce the use of Pymongo 3.12.2 in our Evergreen tests
    WT-8605       Disable the perf tests for non-develop branches in Evergreen

--- src/third_party/wiredtiger/src/reconcile/rec_visibility.c
+++ src/third_party/wiredtiger/src/reconcile/rec_visibility.c
@@ -463,12 +463,14 @@ __wt_rec_upd_select(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_INSERT *ins, W
          * update chain.
          *
          * Also, if an earlier reconciliation performed an update-restore eviction and this update
-         * was restored from disk, we can select this update irrespective of visibility. This
+         * was restored from disk, or, we rolled back a prepared transaction and restored an update
+         * from the history store, we can select this update irrespective of visibility. This
          * scenario can happen if the current reconciliation has a limited visibility of updates
          * compared to one of the previous reconciliations.
          */
         if (!F_ISSET(upd,
-              WT_UPDATE_DS | WT_UPDATE_PREPARE_RESTORED_FROM_DS | WT_UPDATE_RESTORED_FROM_DS) &&
+              WT_UPDATE_DS | WT_UPDATE_PREPARE_RESTORED_FROM_DS | WT_UPDATE_RESTORED_FROM_DS |
+                WT_UPDATE_RESTORED_FROM_HS) &&
           !is_hs_page &&
           (F_ISSET(r, WT_REC_VISIBLE_ALL) ? WT_TXNID_LE(r->last_running, txnid) :
                                             !__txn_visible_id(session, txnid))) {
