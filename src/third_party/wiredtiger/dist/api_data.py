1284ff6c5580d59423ccc59a4f33f172d604bbc4
Etienne Petrel <etienne.petrel@mongodb.com>
2022-02-25 04:58:23 +0000

    Import wiredtiger: 074ba0f7b5f2b73de49da3ae352b83b8d33b6f80 from branch mongodb-master
    ref: a69db0c7f3..074ba0f7b5
    for: 6.0.0
    
    WT-6422       Document expectation around ignore_prepare

--- src/third_party/wiredtiger/dist/api_data.py
+++ src/third_party/wiredtiger/dist/api_data.py
@@ -1733,9 +1733,8 @@ methods = {
         whether to ignore the updates by other prepared transactions as part of
         read operations of this transaction.  When \c true, forces the
         transaction to be read-only.  Use \c force to ignore prepared updates
-        and permit writes (which can cause lost updates unless the application
-        knows something about the relationship between prepared transactions
-        and the updates that are ignoring them)''',
+        and permit writes (see @ref timestamp_prepare_ignore_prepare for
+        more information)''',
         choices=['false', 'force', 'true']),
     Config('isolation', '', r'''
         the isolation level for this transaction; defaults to the
