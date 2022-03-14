8b9df93c9e47f54e725dfff2f6bda5944e59a459
Ruoxin Xu <ruoxin.xu@mongodb.com>
2022-03-14 13:12:48 +0000

    SERVER-64463 Robustify count1.js test

--- jstests/sharding/count1.js
+++ jstests/sharding/count1.js
@@ -146,11 +146,12 @@ assert.commandFailedWithCode(db.runCommand({count: 'foo', query: {$c: {$abc: 3}}
                              ErrorCodes.BadValue);
 
 // ii. Negative skip values should return error.
-assert.commandFailedWithCode(db.runCommand({count: 'foo', skip: -2}), ErrorCodes.FailedToParse);
+assert.commandFailedWithCode(db.runCommand({count: 'foo', skip: -2}),
+                             [ErrorCodes.FailedToParse, 51024]);
 
 // iii. Negative skip values with positive limit should return error.
 assert.commandFailedWithCode(db.runCommand({count: 'foo', skip: -2, limit: 1}),
-                             ErrorCodes.FailedToParse);
+                             [ErrorCodes.FailedToParse, 51024]);
 
 // iv. Unknown options should return error.
 assert.commandFailedWithCode(db.runCommand({count: 'foo', random: true}), 40415);
