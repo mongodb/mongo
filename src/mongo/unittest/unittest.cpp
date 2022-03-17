3ec4631a7f16b76f7490ec8b68fc778c8c7416c3
auto-revert-processor <dev-prod-dag@mongodb.com>
2022-03-17 09:14:00 +0000

    Revert "SERVER-61860 exec in most DEATH_TESTs"
    This reverts commit b372b25622b9ec4dd7ae5b8e3424218b8eaa774a.

--- src/mongo/unittest/unittest.cpp
+++ src/mongo/unittest/unittest.cpp
@@ -388,20 +388,13 @@ std::unique_ptr<Result> Suite::run(const std::string& filter,
     Timer timer;
     auto r = std::make_unique<Result>(_name);
 
-    boost::optional<pcrecpp::RE> filterRe;
-    boost::optional<pcrecpp::RE> fileNameFilterRe;
-    if (!filter.empty())
-        filterRe.emplace(filter);
-    if (!fileNameFilter.empty())
-        fileNameFilterRe.emplace(fileNameFilter);
-
     for (const auto& tc : _tests) {
-        if (filterRe && !filterRe->PartialMatch(tc.name)) {
+        if (filter.size() && tc.name.find(filter) == std::string::npos) {
             LOGV2_DEBUG(23057, 1, "skipped due to filter", "test"_attr = tc.name);
             continue;
         }
 
-        if (fileNameFilterRe && !fileNameFilterRe->PartialMatch(tc.fileName)) {
+        if (fileNameFilter.size() && tc.fileName.find(fileNameFilter) == std::string::npos) {
             LOGV2_DEBUG(23058, 1, "skipped due to fileNameFilter", "testFile"_attr = tc.fileName);
             continue;
         }
@@ -655,12 +648,6 @@ ComparisonAssertion<op> ComparisonAssertion<op>::make(const char* theFile,
 
 // Provide definitions for common instantiations of ComparisonAssertion.
 INSTANTIATE_COMPARISON_ASSERTION_CTORS();
-
-
-SpawnInfo& getSpawnInfo() {
-    static auto v = new SpawnInfo{};
-    return *v;
-}
 
 namespace {
 // At startup, teach the terminate handler how to print TestAssertionFailureException.
