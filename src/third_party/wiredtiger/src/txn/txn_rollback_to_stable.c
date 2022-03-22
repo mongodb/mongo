3802a06137db53dfba0976405104ec1db11dec68
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2022-03-22 13:44:04 +0000

    Revert "Import wiredtiger: d641b5a3c086d76556690b8f96da71a674c02030 from branch mongodb-master"
    This reverts commit bb0a88a3a1cc2d97935cd4adb6e3733604e1623c.

--- src/third_party/wiredtiger/src/txn/txn_rollback_to_stable.c
+++ src/third_party/wiredtiger/src/txn/txn_rollback_to_stable.c
@@ -1681,8 +1681,7 @@ __rollback_to_stable_btree_apply(
 
     if (perform_rts || max_durable_ts > rollback_timestamp || prepared_updates ||
       !durable_ts_found || has_txn_updates_gt_than_ckpt_snap) {
-        ret = __wt_session_get_dhandle(
-          session, uri, NULL, NULL, WT_DHANDLE_DISCARD | WT_DHANDLE_EXCLUSIVE);
+        ret = __wt_session_get_dhandle(session, uri, NULL, NULL, 0);
         if (ret != 0)
             WT_ERR_MSG(session, ret, "%s: unable to open handle%s", uri,
               ret == EBUSY ? ", error indicates handle is unavailable due to concurrent use" : "");
