f88d0d5e7dce55613fdd7085118731de4a92ba91
Sophia Tan <sophia_tll@hotmail.com>
2022-03-25 20:35:56 +0000

    SERVER-64233 shard split jstest case should not depend on maxTimeMS to prove a tenant access blocker is installed

--- jstests/serverless/shard_split_tenant_access_blocking.js
+++ jstests/serverless/shard_split_tenant_access_blocking.js
@@ -5,6 +5,7 @@
  */
 
 load("jstests/libs/fail_point_util.js");
+load("jstests/libs/parallelTester.js");
 load('jstests/libs/parallel_shell_helpers.js');
 load("jstests/serverless/libs/basic_serverless_test.js");
 
@@ -23,17 +24,16 @@ test.addRecipientNodes();
 const donorPrimary = test.donor.getPrimary();
 const migrationId = UUID();
 const tenantIds = ["tenant1", "tenant2"];
-const maxTimeMS = 1 * 2000;  // 2 seconds
 
 jsTestLog("Asserting no state document exist before command");
 assert.isnull(findMigration(donorPrimary, migrationId));
 
 jsTestLog("Asserting we can write before the migration");
 tenantIds.forEach(id => {
-    const tenantDB = donorPrimary.getDB(id + "_data");
-    let insertedObj = {name: id + "1", payload: "testing_data"};
+    const tenantDB = donorPrimary.getDB(`${id}_data`);
+    const insertedObj = {name: `${id}_1`, payload: "testing_data"};
     assert.commandWorked(
-        tenantDB.runCommand({insert: "testing_collection", documents: [insertedObj], maxTimeMS}));
+        tenantDB.runCommand({insert: "testing_collection", documents: [insertedObj]}));
 });
 
 // configure failpoints
@@ -56,17 +56,35 @@ jsTestLog("Asserting state document is in blocking state");
 assertMigrationState(donorPrimary, migrationId, "blocking");
 
 jsTestLog("Asserting we cannot write in blocking state");
-tenantIds.forEach(id => {
-    const tenantDB = donorPrimary.getDB(id + "_data");
-    let insertedObj = {name: id + "2", payload: "testing_data2"};
-    let res =
-        tenantDB.runCommand({insert: "testing_collection", documents: [insertedObj], maxTimeMS});
-    assert.commandFailedWithCode(res, ErrorCodes.MaxTimeMSExpired);
+let writeThreads = [];
+tenantIds.forEach((id, index) => {
+    // Send write commands in an individual thread
+    writeThreads[index] = new Thread((host, id) => {
+        const donorPrimary = new Mongo(host);
+        const tenantDB = donorPrimary.getDB(`${id}_data`);
+        const insertedObj = {name: `${id}_2`, payload: "testing_data2"};
+        const res = tenantDB.runCommand({insert: "testing_collection", documents: [insertedObj]});
+        jsTestLog("Get response for write command: " + tojson(res));
+    }, donorPrimary.host, id);
+    writeThreads[index].start();
 });
+// Poll the numBlockedWrites of tenant migration access blocker from donor and expect it's
+// increased by the blocked write command.
+assert.soon(function() {
+    return tenantIds.every(id => {
+        const mtab = donorPrimary.getDB('admin')
+                         .adminCommand({serverStatus: 1})
+                         .tenantMigrationAccessBlocker[id]
+                         .donor;
+        return mtab.numBlockedWrites > 0;
+    });
+}, "no blocked writes found", 10 * 1000, 1 * 1000);
 
 jsTestLog("Disabling failpoints and waiting for command to complete");
 blockingFailPoint.off();
 awaitCommand();
+
+writeThreads.forEach(thread => thread.join());
 
 jsTestLog("Asserting state document exist after command");
 assertMigrationState(donorPrimary, migrationId, "committed");
