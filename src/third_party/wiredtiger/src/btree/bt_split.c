6bfde2b76777665f2f7196dda846fe5c4685efda
Etienne Petrel <etienne.petrel@mongodb.com>
2022-03-29 01:02:39 +0000

    Import wiredtiger: 07627d4b3ef3040c7538ce3e76924f77402dc49a from branch mongodb-master
    ref: 74b2591686..07627d4b3e
    for: 6.0.0
    
    WT-9004       Fix memory leak in update restore eviction

--- src/third_party/wiredtiger/src/btree/bt_split.c
+++ src/third_party/wiredtiger/src/btree/bt_split.c
@@ -1547,6 +1547,7 @@ static void
 __split_multi_inmem_final(WT_SESSION_IMPL *session, WT_PAGE *orig, WT_MULTI *multi)
 {
     WT_SAVE_UPD *supd;
+    WT_UPDATE **tmp;
     uint32_t i, slot;
 
     /* If we have saved updates, we must have decided to restore them to the new page. */
@@ -1570,9 +1571,17 @@ __split_multi_inmem_final(WT_SESSION_IMPL *session, WT_PAGE *orig, WT_MULTI *mul
         } else
             supd->ins->upd = NULL;
 
-        /* Free the updates written to the data store and the history store. */
-        if (supd->onpage_upd != NULL && !F_ISSET(S2C(session), WT_CONN_IN_MEMORY))
-            __wt_free_update_list(session, &supd->onpage_upd);
+        /*
+         * Free the updates written to the data store and the history store when there exists an
+         * onpage value. It is possible that there can be an onpage tombstone without an onpage
+         * value when the tombstone is globally visible. Do not free them here as it is possible
+         * that the globally visible tombstone is already freed as part of update obsolete check.
+         */
+        if (supd->onpage_upd != NULL && !F_ISSET(S2C(session), WT_CONN_IN_MEMORY)) {
+            tmp = supd->onpage_tombstone != NULL ? &supd->onpage_tombstone : &supd->onpage_upd;
+            __wt_free_update_list(session, tmp);
+            supd->onpage_tombstone = supd->onpage_upd = NULL;
+        }
     }
 }
 
@@ -1585,7 +1594,7 @@ static void
 __split_multi_inmem_fail(WT_SESSION_IMPL *session, WT_PAGE *orig, WT_MULTI *multi, WT_REF *ref)
 {
     WT_SAVE_UPD *supd;
-    WT_UPDATE *upd;
+    WT_UPDATE *upd, *tmp;
     uint32_t i, slot;
 
     if (!F_ISSET(S2C(session), WT_CONN_IN_MEMORY))
@@ -1606,11 +1615,11 @@ __split_multi_inmem_fail(WT_SESSION_IMPL *session, WT_PAGE *orig, WT_MULTI *mult
                 upd = supd->ins->upd;
 
             WT_ASSERT(session, upd != NULL);
-
-            for (; upd->next != NULL && upd->next != supd->onpage_upd; upd = upd->next)
+            tmp = supd->onpage_tombstone != NULL ? supd->onpage_tombstone : supd->onpage_upd;
+            for (; upd->next != NULL && upd->next != tmp; upd = upd->next)
                 ;
             if (upd->next == NULL)
-                upd->next = supd->onpage_upd;
+                upd->next = tmp;
         }
 
     /*
