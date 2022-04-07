a65ad556b00f746059340f783f0a0dcce93e6cc7
sergey.galtsev <sergey.galtsev@mongodb.com>
2022-04-07 23:48:41 +0000

    SERVER-64065 disable user_management_helpers on selinux

--- jstests/core/connection_status.js
+++ jstests/core/connection_status.js
@@ -2,6 +2,7 @@
 //   assumes_superuser_permissions,
 //   assumes_write_concern_unchanged,
 //   creates_and_authenticates_user,
+//   no_selinux,
 //   requires_auth,
 //   requires_non_retryable_commands,
 // ]
