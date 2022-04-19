31864e3866ce9cc54c08463019846ded2ad9e6e5
Henrik Edin <henrik.edin@mongodb.com>
2022-04-19 12:55:37 +0000

    SERVER-65501 Re-start interleaved object compression when scalar field is encountered as object
    This makes the behavior the same as when going from object to scalar.

--- src/mongo/bson/util/bsoncolumnbuilder.cpp
+++ src/mongo/bson/util/bsoncolumnbuilder.cpp
@@ -140,6 +140,10 @@ std::pair<BSONObj::iterator, bool> _traverseLockStep(const BSONObj& reference,
                 }
             }
         } else {
+            // Going from scalar to object is not allowed, this would compress inefficiently
+            if (it->type() == Object || it->type() == Array) {
+                return {it, false};
+            }
             // Non-object, call provided function with the two elements
             elemFunc(elem,
                      it != end && elem.fieldNameStringData() == it->fieldNameStringData()
@@ -356,6 +360,10 @@ std::pair<BSONObj::iterator, bool> _traverseLockStepLegacy(const BSONObj& refere
                 }
             }
         } else {
+            // Going from scalar to object is not allowed, this would compress inefficiently
+            if (it->type() == Object) {
+                return {it, false};
+            }
             // Non-object, call provided function with the two elements
             elemFunc(elem,
                      it != end && elem.fieldNameStringData() == it->fieldNameStringData()
