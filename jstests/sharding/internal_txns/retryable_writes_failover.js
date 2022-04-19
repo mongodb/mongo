c8dea9f922ce046e0ef3962a7384a5a6d964ab04
Sanika Phanse <sanika.phanse@mongodb.com>
2022-04-19 17:20:09 +0000

    SERVER-65635 Tests running large txns do not have correct oplog entry sizes
    (cherry picked from commit 6d5c93d1ada87cfdf55f1384508fe0f6d96dc86c)

--- jstests/sharding/internal_txns/retryable_writes_failover.js
+++ jstests/sharding/internal_txns/retryable_writes_failover.js
@@ -2,7 +2,11 @@
  * Tests that retryable internal transactions for insert, update and delete are retryable across
  * failover.
  *
- * @tags: [requires_fcv_60, uses_transactions]
+ * Exclude this test from large_txn variants because the variant enforces that the max transaction
+ * oplog entry length is 2 operations, and the oplog length assertions in this test do not account
+ * for this. We are not losing test coverage as this test inherently tests large transactions.
+ *
+ * @tags: [requires_fcv_60, uses_transactions, exclude_from_large_txns]
  */
 (function() {
 'use strict';
