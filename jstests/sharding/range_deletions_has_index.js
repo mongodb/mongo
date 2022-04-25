e6ee576307ef5325c35891313db8d9aa2516aee3
Allison Easton <allison.easton@mongodb.com>
2022-04-25 06:19:23 +0000

    SERVER-64285 Create indexes on `config.rangeDeletions`
    (cherry picked from commit 0d1f974d015f3e527f66d0dd6d1e623dc43571e6)

---
+++ jstests/sharding/range_deletions_has_index.js
@@ -0,0 +1,32 @@
+/**
+ * Tests that the _id and uuid indexes are created on config.rangeDeletions on primary step up.
+ *
+ * @tags: [
+ *  requires_fcv_60,
+ * ]
+ */
+
+(function() {
+'use strict';
+
+const st = new ShardingTest({shards: {rs0: {nodes: 2}}});
+
+// Test index gets created with an empty range deletions collection
+let newPrimary = st.rs0.getSecondaries()[0];
+assert.commandWorked(newPrimary.adminCommand({replSetStepUp: 1}));
+st.rs0.waitForPrimary();
+const rangeDeletionColl = newPrimary.getDB("config").getCollection("rangeDeletions");
+let res = rangeDeletionColl.runCommand({listIndexes: rangeDeletionColl.getName()});
+assert.commandWorked(res);
+let indexes = res.cursor.firstBatch;
+
+assert.eq(indexes.length, 2);
+indexes.forEach((index) => {
+    if (bsonWoCompare(index.key, {"_id": 1}) !== 0) {
+        assert.eq(bsonWoCompare(index.key, {"collectionUuid": 1, "range.min": 1, "range.max": 1}),
+                  0);
+    }
+});
+
+st.stop();
+})();
