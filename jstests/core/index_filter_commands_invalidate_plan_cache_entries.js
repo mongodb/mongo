279da2ed4129f4d4a4afa633d9d2546c047619bc
Ruoxin Xu <ruoxin.xu@mongodb.com>
2022-04-27 10:09:28 +0000

    SERVER-65897 Remove SBE plan cache entries from the corresponding collection when an index filter is set or cleared
    (cherry picked from commit 4ecf46e108a3a3aed8a262fe79cc0213fa605a01)

--- jstests/core/index_filter_commands_invalidate_plan_cache_entries.js
+++ jstests/core/index_filter_commands_invalidate_plan_cache_entries.js
@@ -1,6 +1,6 @@
 /**
- * Test that index filter commands (planCacheSetFilter, planCacheClear) invalidate the corresponding
- * plan cache entries.
+ * Test that index filter commands (planCacheSetFilter, planCacheClearFilters) invalidate the
+ * corresponding plan cache entries.
  * @tags: [
  *   # This test attempts to perform queries with plan cache filters set up. The index filter
  *   # commands and the queries to which those index filters apply could be routed to different
@@ -23,29 +23,33 @@ const isSbePlanCacheEnabled = checkSBEEnabled(db, ["featureFlagSbePlanCache"]) ?
 
 const collName = "index_filter_commands_invalidate_plan_cache_entries";
 const coll = db[collName];
-coll.drop();
 
-// We need multiple indexes so that the multi-planner is executed.
-assert.commandWorked(coll.createIndex({a: 1}));
-assert.commandWorked(coll.createIndex({b: 1}));
-assert.commandWorked(coll.createIndex({a: 1, b: 1}));
-assert.commandWorked(coll.insert({a: 1, b: 1, c: 1}));
+function initCollection(collection) {
+    collection.drop();
 
-function existsInPlanCache(query, sort, projection) {
+    // We need multiple indexes so that the multi-planner is executed.
+    assert.commandWorked(collection.createIndex({a: 1}));
+    assert.commandWorked(collection.createIndex({b: 1}));
+    assert.commandWorked(collection.createIndex({a: 1, b: 1}));
+    assert.commandWorked(collection.insert({a: 1, b: 1, c: 1}));
+}
+initCollection(coll);
+
+function existsInPlanCache(query, sort, projection, planCacheColl) {
     const keyHash = getPlanCacheKeyFromShape(
-        {query: query, projection: projection, sort: sort, collection: coll, db: db});
-    const res =
-        coll.aggregate([{$planCacheStats: {}}, {$match: {planCacheKey: keyHash}}]).toArray();
+        {query: query, projection: projection, sort: sort, collection: planCacheColl, db: db});
+    const res = planCacheColl.aggregate([{$planCacheStats: {}}, {$match: {planCacheKey: keyHash}}])
+                    .toArray();
 
     return res.length > 0;
 }
 
 assert.eq(1, coll.find({a: 1}).itcount());
-assert(existsInPlanCache({a: 1}, {}, {}));
+assert(existsInPlanCache({a: 1}, {}, {}, coll));
 assert.eq(1, coll.find({a: 1, b: 1}).itcount());
-assert(existsInPlanCache({a: 1, b: 1}, {}, {}));
+assert(existsInPlanCache({a: 1, b: 1}, {}, {}, coll));
 assert.eq(1, coll.find({a: 1, b: 1}).sort({a: 1}).itcount());
-assert(existsInPlanCache({a: 1, b: 1}, {a: 1}));
+assert(existsInPlanCache({a: 1, b: 1}, {a: 1}, {}, coll));
 
 assert.eq(coll.aggregate([{$planCacheStats: {}}]).toArray().length, 3);
 
@@ -68,4 +72,33 @@ assert.eq(coll.aggregate([{$planCacheStats: {}}]).toArray().length, 2 + isSbePla
 assert.commandWorked(
     db.runCommand({planCacheSetFilter: collName, query: {a: 1}, indexes: [{a: 1}]}));
 assert.eq(coll.aggregate([{$planCacheStats: {}}]).toArray().length, 1);
+
+// Test that plan cache entries with same query shape but in a different collection won't be cleared
+// when an index filter with the same query shape is set/cleared.
+const collNameOther = "index_filter_commands_invalidate_plan_cache_entries_other";
+const collOther = db[collNameOther];
+
+initCollection(coll);
+initCollection(collOther);
+
+assert.eq(1, coll.find({a: 1, b: 1}).itcount());
+assert(existsInPlanCache({a: 1, b: 1}, {}, {}, coll));
+assert.eq(1, collOther.find({a: 1, b: 1}).itcount());
+assert(existsInPlanCache({a: 1, b: 1}, {}, {}, collOther));
+
+// Test that planCacheClearFilters command invalidates corresponding plan cache entries of correct
+// collection.
+assert.commandWorked(db.runCommand({planCacheClearFilters: collName, query: {a: 1, b: 1}}));
+assert(!existsInPlanCache({a: 1, b: 1}, {}, {}, coll));
+assert(existsInPlanCache({a: 1, b: 1}, {}, {}, collOther));
+
+// Test planCacheSetFilter command invalidates corresponding plan cache entries of correct
+// collection.
+assert.eq(1, coll.find({a: 1, b: 1}).itcount());
+assert(existsInPlanCache({a: 1, b: 1}, {}, {}, coll));
+
+assert.commandWorked(
+    db.runCommand({planCacheSetFilter: collName, query: {a: 1, b: 1}, indexes: [{a: 1}]}));
+assert(!existsInPlanCache({a: 1, b: 1}, {}, {}, coll));
+assert(existsInPlanCache({a: 1, b: 1}, {}, {}, collOther));
 })();
