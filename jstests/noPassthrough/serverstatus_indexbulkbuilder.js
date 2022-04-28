5659527d24b3ae1d22a8e0fde26ac7d3a92310a0
Benety Goh <benety@mongodb.com>
2022-04-28 14:48:29 -0400

    SERVER-64659 add indexBulkBuilder server status section with minimal creation metrics
    (cherry picked from commit adc47cc974960abf0bc10a1b15234dc95ee6fdd4)

---
+++ jstests/noPassthrough/serverstatus_indexbulkbuilder.js
@@ -0,0 +1,40 @@
+/**
+ * Tests that serverStatus contains an indexBuilder section. This section reports
+ * globally-aggregated statistics about index builds and the external sorter.
+ *
+ * @tags: [
+ *   requires_persistence,
+ *   requires_replication,
+ * ]
+ */
+(function() {
+'use strict';
+
+const replSet = new ReplSetTest({
+    nodes: 1,
+});
+replSet.startSet();
+replSet.initiate();
+
+let primary = replSet.getPrimary();
+let testDB = primary.getDB('test');
+let coll = testDB.getCollection('t');
+
+for (let i = 0; i < 10; i++) {
+    assert.commandWorked(coll.insert({
+        _id: i,
+        a: i,
+    }));
+}
+
+assert.commandWorked(coll.createIndex({a: 1}));
+
+let serverStatus = testDB.serverStatus();
+assert(serverStatus.hasOwnProperty('indexBulkBuilder'),
+       'indexBuildBuilder section missing: ' + tojson(serverStatus));
+
+let indexBulkBuilderSection = serverStatus.indexBulkBuilder;
+assert.eq(indexBulkBuilderSection.count, 1, tojson(indexBulkBuilderSection));
+
+replSet.stopSet();
+})();
