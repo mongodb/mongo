be69831f52c4e3b044584d5caf2d50b1e8c958f9
Benety Goh <benety@mongodb.com>
2022-04-29 05:36:03 -0400

    SERVER-64659 add resumable index build counter to indexBulkBuild server status section
    (cherry picked from commit 0c40fc644df33161b34faa3708545e894757327e)

--- jstests/noPassthrough/serverstatus_indexbulkbuilder.js
+++ jstests/noPassthrough/serverstatus_indexbulkbuilder.js
@@ -10,6 +10,8 @@
 (function() {
 'use strict';
 
+load('jstests/noPassthrough/libs/index_build.js');
+
 const replSet = new ReplSetTest({
     nodes: 1,
 });
@@ -35,6 +37,29 @@ assert(serverStatus.hasOwnProperty('indexBulkBuilder'),
 
 let indexBulkBuilderSection = serverStatus.indexBulkBuilder;
 assert.eq(indexBulkBuilderSection.count, 1, tojson(indexBulkBuilderSection));
+assert.eq(indexBulkBuilderSection.resumed, 0, tojson(indexBulkBuilderSection));
+
+// Shut down server during an index to verify 'resumable' value on restart.
+IndexBuildTest.pauseIndexBuilds(primary);
+const createIdx = IndexBuildTest.startIndexBuild(primary,
+                                                 coll.getFullName(),
+                                                 {b: 1},
+                                                 /*options=*/{},
+                                                 [ErrorCodes.InterruptedDueToReplStateChange]);
+IndexBuildTest.waitForIndexBuildToScanCollection(testDB, coll.getName(), 'b_1');
+const buildUUID = extractUUIDFromObject(
+    IndexBuildTest
+        .assertIndexes(coll, 3, ['_id_', 'a_1'], ['b_1'], {includeBuildUUIDs: true})['b_1']
+        .buildUUID);
+replSet.restart(/*nodeId=*/0);
+createIdx();
+primary = replSet.getPrimary();
+testDB = primary.getDB('test');
+coll = testDB.getCollection('t');
+ResumableIndexBuildTest.assertCompleted(primary, coll, [buildUUID], ['a_1', 'b_1']);
+indexBulkBuilderSection = testDB.serverStatus().indexBulkBuilder;
+assert.eq(indexBulkBuilderSection.count, 1, tojson(indexBulkBuilderSection));
+assert.eq(indexBulkBuilderSection.resumed, 1, tojson(indexBulkBuilderSection));
 
 replSet.stopSet();
 })();
