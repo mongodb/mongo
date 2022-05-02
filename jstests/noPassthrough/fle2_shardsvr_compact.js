0d025d08cf2792a19b48678f5c146268a80c193f
Mark Benvenuto <mark.benvenuto@mongodb.com>
2022-05-02 11:10:13 -0400

    SERVER-65832 Block mongod CompactStructuredEncryptionData on shard server
    (cherry picked from commit 671486e7ba73cfac7e769f9839de9c7be023be21)

---
+++ jstests/noPassthrough/fle2_shardsvr_compact.js
@@ -0,0 +1,38 @@
+/**
+ * Cannot run compact against a shard server
+ *
+ * @tags: [
+ * requires_fcv_60
+ * ]
+ */
+load("jstests/fle2/libs/encrypted_client_util.js");
+
+(function() {
+'use strict';
+
+function runTest(mongosConn, shardConn) {
+    let dbName = 'testdb';
+
+    let clientMongoS = new EncryptedClient(mongosConn, dbName);
+
+    assert.commandWorked(clientMongoS.createEncryptionCollection("basic", {
+        encryptedFields: {
+            "fields":
+                [{"path": "first", "bsonType": "string", "queries": {"queryType": "equality"}}]
+        }
+    }));
+
+    let clientShard = new EncryptedClient(shardConn, dbName);
+
+    assert.commandFailedWithCode(clientShard.getDB().basic.compact(), 6583201);
+}
+
+jsTestLog("Sharding: Testing fle2 drop collection warning");
+{
+    const st = new ShardingTest({shards: 1, mongos: 1, config: 1});
+
+    runTest(st.s, st.shard0);
+
+    st.stop();
+}
+}());
