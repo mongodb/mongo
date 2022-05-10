1abb269878e6514dc21a2478d33f185d92d24aa2
Vesselina Ratcheva <vesselina.ratcheva@10gen.com>
2022-05-10 18:16:49 +0000

    SERVER-64628 Add more targeted tests for adding nodes to sharded sets
    (cherry picked from commit 181a405baf42fc204e924dcde8f246d6f40a09bb)

---
+++ jstests/sharding/live_shard_logical_initial_sync.js
@@ -0,0 +1,27 @@
+/**
+ * Tests that sharding state is properly initialized on new shard members that were added into live
+ * shards using logical initial sync.
+ *
+ * We control our own failovers, and we also need the RSM to react reasonably quickly to those.
+ * @tags: [does_not_support_stepdowns, requires_streamable_rsm]
+ */
+
+(function() {
+"use strict";
+
+load("jstests/sharding/libs/sharding_state_test.js");
+
+const st = new ShardingTest({config: 1, shards: {rs0: {nodes: 1}}});
+const rs = st.rs0;
+
+const newNode = ShardingStateTest.addReplSetNode({replSet: rs, serverTypeFlag: "shardsvr"});
+
+jsTestLog("Checking sharding state before failover.");
+ShardingStateTest.checkShardingState(st);
+
+jsTestLog("Checking sharding state after failover.");
+ShardingStateTest.failoverToMember(rs, newNode);
+ShardingStateTest.checkShardingState(st);
+
+st.stop();
+})();
