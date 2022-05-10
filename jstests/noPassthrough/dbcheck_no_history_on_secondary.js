2daf151f5a547c9e4a213f996d72adc67ae2e832
Benety Goh <benety@mongodb.com>
2022-05-10 10:20:26 -0400

    SERVER-66319 use bulk inserts to speed up dbcheck test

--- jstests/noPassthrough/dbcheck_no_history_on_secondary.js
+++ jstests/noPassthrough/dbcheck_no_history_on_secondary.js
@@ -30,9 +30,11 @@ const primary = replTest.getPrimary();
 const secondary = replTest.getSecondary();
 
 const testDB = primary.getDB('test');
+let docs = [];
 for (let i = 0; i < 100; i++) {
-    assert.commandWorked(testDB.foo.insert({a: i}));
+    docs.push({a: i});
 }
+assert.commandWorked(testDB.foo.insert(docs));
 
 const sleepMs = 3000;
 const fp = configureFailPoint(primary, 'SleepDbCheckInBatch', {sleepMs: sleepMs});
@@ -41,9 +43,11 @@ const fp = configureFailPoint(primary, 'SleepDbCheckInBatch', {sleepMs: sleepMs}
 assert.commandWorked(testDB.getSiblingDB('test').runCommand({dbCheck: 1}));
 
 // Write some data to advance the durable timestamp while we're waiting for dbCheck to run.
+docs = [];
 for (let i = 0; i < 100; i++) {
-    assert.commandWorked(testDB.foo.insert({_id: i}));
+    docs.push({_id: i});
 }
+assert.commandWorked(testDB.foo.insert(docs));
 
 fp.off();
 
