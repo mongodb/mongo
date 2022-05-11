59bec6ec49ecf623c70ef5fe78e3e9ad969bc160
Richard Samuels <richard.l.samuels@gmail.com>
2022-05-11 10:39:25 -0400

    SERVER-65693 Relocate snmp documentation

--- evergreen/do_snmp_setup.sh
+++ evergreen/do_snmp_setup.sh
@@ -3,4 +3,4 @@ set -o verbose
 
 cd src
 mkdir -p snmpconf
-cp -f src/mongo/db/modules/enterprise/docs/mongod.conf.master snmpconf/mongod.conf
+cp -f src/mongo/db/modules/enterprise/docs/snmp/mongod.conf.master snmpconf/mongod.conf
