1cd743c746d94603d965c828b3187852a59d991c
Tausif Rahman <tausif.rahman@mongodb.com>
2022-05-12 15:38:26 +0000

    SERVER-66311 Set fassertOnLockTimeoutForStepUpDown to 0 for Antithesis

--- buildscripts/antithesis/topologies/sharded_cluster/scripts/mongos_init.sh
+++ buildscripts/antithesis/topologies/sharded_cluster/scripts/mongos_init.sh
@@ -1,7 +1,7 @@
 sleep 5s
 mongo --host configsvr1 --port 27019 --eval "config={\"_id\" : \"ConfigServerReplSet\",\"configsvr\" : true,\"protocolVersion\" : 1,\"members\" : [{\"_id\" : 0,\"host\" : \"configsvr1:27019\"}, {\"_id\" : 1,\"host\" : \"configsvr2:27019\"}, {\"_id\" : 2,\"host\" : \"configsvr3:27019\"} ],\"settings\" : {\"chainingAllowed\" : false,\"electionTimeoutMillis\" : 2000, \"heartbeatTimeoutSecs\" : 1, \"catchUpTimeoutMillis\": 0}}; rs.initiate(config)"
 
-mongos --bind_ip 0.0.0.0 --configdb ConfigServerReplSet/configsvr1:27019,configsvr2:27019,configsvr3:27019 --logpath /var/log/mongodb/mongodb.log --setParameter enableTestCommands=1
+mongos --bind_ip 0.0.0.0 --configdb ConfigServerReplSet/configsvr1:27019,configsvr2:27019,configsvr3:27019 --logpath /var/log/mongodb/mongodb.log --setParameter enableTestCommands=1 --setParameter fassertOnLockTimeoutForStepUpDown=0
 
 # this cryptic statement keeps the container running.
 tail -f /dev/null
