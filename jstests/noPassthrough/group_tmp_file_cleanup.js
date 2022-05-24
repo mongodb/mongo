218f4a72dcb54bb6f6b88354cda9296bce9fe606
romanskas <30618745+romanskas@users.noreply.github.com>
2022-05-24 09:45:46 +0000

    SERVER-59834 Add test for _tmp files cleanup for $group

---
+++ jstests/noPassthrough/group_tmp_file_cleanup.js
@@ -0,0 +1,34 @@
+/**
+ * Test that $group cleans up temporary files under dbpath + '/_tmp'.
+ */
+
+(function() {
+"use strict";
+
+const memoryLimitMb = 1;
+const memoryLimitBytes = memoryLimitMb * 1024 * 1024;
+
+// Start mongod with reduced memory limit for the $group stage.
+const conn = MongoRunner.runMongod({
+    setParameter: {
+        internalDocumentSourceGroupMaxMemoryBytes: memoryLimitBytes,
+    }
+});
+const testDb = conn.getDB(jsTestName());
+
+// Create a collection exceeding the memory limit.
+testDb.largeColl.drop();
+const largeStr = "A".repeat(1024 * 1024);  // 1MB string
+for (let i = 0; i < memoryLimitMb + 1; ++i)
+    assert.commandWorked(testDb.largeColl.insert({x: i, largeStr: largeStr + i}));
+
+const pipeline = [{$group: {_id: '$largeStr', minId: {$min: '$_id'}}}];
+// Make sure that the pipeline needs to spill to disk.
+assert.throwsWithCode(() => testDb.largeColl.aggregate(pipeline, {allowDiskUse: false}),
+                      ErrorCodes.QueryExceededMemoryLimitNoDiskUseAllowed);
+
+testDb.largeColl.aggregate(pipeline);
+assert.eq(listFiles(conn.dbpath + "/_tmp").length, 0);
+
+MongoRunner.stopMongod(conn);
+})();
