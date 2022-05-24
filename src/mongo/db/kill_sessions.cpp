94c1703d264f8b73a7e900809de916c7eb53e4bf
Sara Golemon <sara.golemon@mongodb.com>
2022-05-24 10:58:41 -0500

    SERVER-66726 Fix serialization in killAllSessionsByPattern

--- src/mongo/db/kill_sessions.cpp
+++ src/mongo/db/kill_sessions.cpp
@@ -87,10 +87,10 @@ getKillAllSessionsByPatternImpersonateData(const KillAllSessionsByPattern& patte
     }
 
     if (pattern.getRoles()) {
-        roles.reserve(pattern.getUsers()->size());
+        roles.reserve(pattern.getRoles()->size());
 
-        for (auto&& user : pattern.getUsers().get()) {
-            roles.emplace_back(user.getUser(), user.getDb());
+        for (auto&& role : pattern.getRoles().get()) {
+            roles.emplace_back(role.getRole(), role.getDb());
         }
     }
 
