fcc6648d163a60c81bea2f3bf962480d34d96a6b
Billy Donahue <billy.donahue@mongodb.com>
2022-05-26 15:47:31 +0000

    SERVER-66801 regex_limit.js: Probe the exact limit instead of pass/fail

--- jstests/core/regex_limit.js
+++ jstests/core/regex_limit.js
@@ -12,6 +12,43 @@ const kMaxRegexPatternLen = 32761;
 // Populate the collection with a document containing a very long string.
 assert.commandWorked(coll.insert({z: "c".repeat(100000)}));
 
+function tryRegexLength(coll, n) {
+    const re = "c".repeat(n);
+    const res = coll.runCommand("find", {filter: {z: {$regex: re}}});
+    if (res.ok == 1)
+        return true;
+    if (res.ok == 0 && res.code == 51091)
+        return false;
+    // Something else went wrong.
+    assert.commandWorked(res);
+}
+
+// Returns the longest regex length the server will accept.
+function probeMaxPatternLength(coll) {
+    // Exponential search and bisect to find the exact failure point.
+    // Half-open interval `range`: [lastKnownGood, firstKnownBad).
+    let range = [1 << 0, 1 << 0];
+    while (true) {
+        if (!tryRegexLength(coll, range[1]))
+            break;
+        range[0] = range[1];
+        range[1] *= 2;
+    }
+    while (true) {
+        let mid = Math.floor((range[0] + range[1]) / 2);
+        if (mid == range[0])
+            break;
+        if (tryRegexLength(coll, mid)) {
+            range[0] = mid;
+        } else {
+            range[1] = mid;
+        }
+    }
+    return range[0];
+}
+
+assert.eq(probeMaxPatternLength(coll), kMaxRegexPatternLen);
+
 // Test that a regex exactly at the maximum allowable pattern length can find a document.
 const patternMaxLen = "c".repeat(kMaxRegexPatternLen);
 assert.eq(1, coll.find({z: {$regex: patternMaxLen}}).itcount());
