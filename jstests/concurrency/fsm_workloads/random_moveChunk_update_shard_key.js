0048e74c6723d7f7030748967a3efc362e0c015b
Allison Easton <allison.easton@mongodb.com>
2022-05-26 15:01:52 +0000

    SERVER-66653 Remove usage of this.tid from assertDocWasUpdated

--- jstests/concurrency/fsm_workloads/random_moveChunk_update_shard_key.js
+++ jstests/concurrency/fsm_workloads/random_moveChunk_update_shard_key.js
@@ -143,7 +143,7 @@ var $config = extendWorkload($config, function($config, $super) {
         const replacementStyle = (Math.floor(Math.random() * 2) == 0);
 
         if (replacementStyle) {
-            return {_id: currentId, skey: newShardKey, counter: counterForId + 1};
+            return {_id: currentId, skey: newShardKey, tid: this.tid, counter: counterForId + 1};
         } else {
             // Op style
             return {$set: {skey: newShardKey}, $inc: {counter: 1}};
@@ -193,11 +193,11 @@ var $config = extendWorkload($config, function($config, $super) {
         jsTestLog(logString);
     };
 
-    function assertDocWasUpdated(collection, idToUpdate, currentShardKey, newShardKey, newCounter) {
+    function assertDocWasUpdated(
+        collection, idToUpdate, currentShardKey, newShardKey, newCounter, tid) {
         assertWhenOwnColl.isnull(collection.findOne({_id: idToUpdate, skey: currentShardKey}));
-        assertWhenOwnColl.eq(
-            collection.findOne({_id: idToUpdate, skey: newShardKey}),
-            {_id: idToUpdate, skey: newShardKey, tid: this.tid, counter: newCounter});
+        assertWhenOwnColl.eq(collection.findOne({_id: idToUpdate, skey: newShardKey}),
+                             {_id: idToUpdate, skey: newShardKey, tid: tid, counter: newCounter});
     }
 
     function wasDocUpdated(collection, idToUpdate, currentShardKey) {
@@ -247,8 +247,12 @@ var $config = extendWorkload($config, function($config, $super) {
                     // write succeeded, so we can treat this error as success.
                     if (this.updateDocumentShardKeyUsingTransactionApiEnabled) {
                         print("Internal transactions are on so assuming the operation succeeded");
-                        assertDocWasUpdated(
-                            collection, idToUpdate, currentShardKey, newShardKey, counterForId + 1);
+                        assertDocWasUpdated(collection,
+                                            idToUpdate,
+                                            currentShardKey,
+                                            newShardKey,
+                                            counterForId + 1,
+                                            this.tid);
                         this.expectedCounters[idToUpdate] = counterForId + 1;
                         return;
                     }
@@ -325,8 +329,12 @@ var $config = extendWorkload($config, function($config, $super) {
                     // write succeeded, so we can treat this error as success.
                     if (this.updateDocumentShardKeyUsingTransactionApiEnabled) {
                         print("Internal transactions are on so assuming the operation succeeded");
-                        assertDocWasUpdated(
-                            collection, idToUpdate, currentShardKey, newShardKey, counterForId + 1);
+                        assertDocWasUpdated(collection,
+                                            idToUpdate,
+                                            currentShardKey,
+                                            newShardKey,
+                                            counterForId + 1,
+                                            this.tid);
                         this.expectedCounters[idToUpdate] = counterForId + 1;
                         return;
                     }
