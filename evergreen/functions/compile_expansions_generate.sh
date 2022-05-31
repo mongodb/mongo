79c61c07d61102ca7ca387880a23543faf156ca3
Ryan Egesdahl <ryan.egesdahl@mongodb.com>
2022-05-31 19:08:04 +0000

    SERVER-63191 Generate build expansions early

--- evergreen/functions/compile_expansions_generate.sh
+++ evergreen/functions/compile_expansions_generate.sh
@@ -5,14 +5,6 @@ cd src
 
 set -o errexit
 set -o verbose
-# We get the raw version string (r1.2.3-45-gabcdef) from git
-MONGO_VERSION=$(git describe --abbrev=7)
-# If this is a patch build, we add the patch version id to the version string so we know
-# this build was a patch, and which evergreen task it came from
-if [ "${is_patch}" = "true" ]; then
-  MONGO_VERSION="$MONGO_VERSION-patch-${version_id}"
-fi
-echo "MONGO_VERSION = ${MONGO_VERSION}"
 activate_venv
 # shared scons cache testing
 # if 'scons_cache_scope' enabled and project level 'disable_shared_scons_cache' is not true
@@ -43,12 +35,12 @@ if [ ! -z ${scons_cache_scope} ]; then
     set -o errexit
   fi
   echo "Shared Cache with setting: ${scons_cache_scope}"
-  MONGO_VERSION=$MONGO_VERSION SCONS_CACHE_MODE=${scons_cache_mode} SCONS_CACHE_SCOPE=$scons_cache_scope IS_PATCH=${is_patch} IS_COMMIT_QUEUE=${is_commit_queue} $python buildscripts/generate_compile_expansions_shared_cache.py --out compile_expansions.yml
+  SCONS_CACHE_MODE=${scons_cache_mode} SCONS_CACHE_SCOPE=$scons_cache_scope IS_PATCH=${is_patch} IS_COMMIT_QUEUE=${is_commit_queue} $python buildscripts/generate_compile_expansions_shared_cache.py --out compile_expansions.yml
 # Legacy Expansion generation
 else
   echo "Using legacy expansion generation"
   # Proceed with regular expansions generated
   # This script converts the generated version string into a sanitized version string for
   # use by scons and uploading artifacts as well as information about for the scons cache.
-  MONGO_VERSION=$MONGO_VERSION SCONS_CACHE_MODE=${scons_cache_mode} USE_SCONS_CACHE=${use_scons_cache} $python buildscripts/generate_compile_expansions.py --out compile_expansions.yml
+  SCONS_CACHE_MODE=${scons_cache_mode} USE_SCONS_CACHE=${use_scons_cache} IS_PATCH=${is_patch} IS_COMMIT_QUEUE=${is_commit_queue} $python buildscripts/generate_compile_expansions.py --out compile_expansions.yml
 fi
