4d6a40567f2d18fd10dbe6d2272fbe648be1a459
Benety Goh <benety@mongodb.com>
2022-06-01 06:47:46 -0400

    SERVER-64491 clean up storefunc.js. remove requires_fastcount tag

--- jstests/core/storefunc.js
+++ jstests/core/storefunc.js
@@ -1,36 +1,38 @@
 // @tags: [
 //   requires_non_retryable_commands,
 //   requires_non_retryable_writes,
-//   requires_fastcount,
 // ]
 
+(function() {
+'use strict';
+
 // Use a private sister database to avoid conflicts with other tests that use system.js
-var testdb = db.getSiblingDB("storefunc");
-var res;
+const testdb = db.getSiblingDB("storefunc");
+let res;
 
-s = testdb.system.js;
-s.remove({});
-assert.eq(0, s.count(), "setup - A");
+const s = testdb.system.js;
+assert.commandWorked(s.remove({}));
+assert.eq(0, s.countDocuments({}));
 
-res = s.save({_id: "x", value: "3"});
-assert(!res.hasWriteError(), "setup - B");
-assert.eq(1, s.count(), "setup - C");
+assert.commandWorked(s.insert({_id: "x", value: "3"}));
+assert.eq(1, s.countDocuments({}));
 
-s.remove({_id: "x"});
-assert.eq(0, s.count(), "setup - D");
-s.save({_id: "x", value: "4"});
-assert.eq(1, s.count(), "setup - E");
+assert.commandWorked(s.remove({_id: "x"}));
+assert.eq(0, s.countDocuments({}));
+assert.commandWorked(s.insert({_id: "x", value: "4"}));
+assert.eq(1, s.countDocuments({}));
 
 assert.eq(4, s.findOne({_id: "x"}).value, "E2 ");
 
 assert.eq(4, s.findOne().value, "setup - F");
-s.update({_id: "x"}, {$set: {value: 5}});
-assert.eq(1, s.count(), "setup - G");
+assert.commandWorked(s.update({_id: "x"}, {$set: {value: 5}}));
+assert.eq(1, s.countDocuments({}));
 assert.eq(5, s.findOne().value, "setup - H");
 
-s.update({_id: "x"}, {$set: {value: 6}});
-assert.eq(1, s.count(), "setup2 - A");
+assert.commandWorked(s.update({_id: "x"}, {$set: {value: 6}}));
+assert.eq(1, s.countDocuments({}));
 assert.eq(6, s.findOne().value, "setup - B");
 
 assert(s.getIndexKeys().length > 0, "no indexes");
 assert(s.getIndexKeys()[0]._id, "no _id index");
+})();
