1bf1161e5db69fcee1aaa135b18ae6d9e10e94e5
Allison Easton <allison.easton@mongodb.com>
2022-06-09 07:44:40 +0000

    SERVER-59658 Change durationMillis to duration

--- src/mongo/db/s/shard_server_catalog_cache_loader.cpp
+++ src/mongo/db/s/shard_server_catalog_cache_loader.cpp
@@ -651,7 +651,7 @@ StatusWith<CollectionAndChangedChunks> ShardServerCatalogCacheLoader::_runSecond
                               "Cache loader on secondary successfully waited for primary refresh "
                               "and replication of collection",
                               "namespace"_attr = nss,
-                              "durationMillis"_attr = Milliseconds(t.millis()));
+                              "duration"_attr = Milliseconds(t.millis()));
 
     // Read the local metadata.
 
@@ -787,7 +787,7 @@ StatusWith<DatabaseType> ShardServerCatalogCacheLoader::_runSecondaryGetDatabase
                               "Cache loader on secondary successfully waited for primary refresh "
                               "and replication of database",
                               "db"_attr = dbName,
-                              "durationMillis"_attr = Milliseconds(t.millis()));
+                              "duration"_attr = Milliseconds(t.millis()));
     return readShardDatabasesEntry(opCtx, dbName);
 }
 
