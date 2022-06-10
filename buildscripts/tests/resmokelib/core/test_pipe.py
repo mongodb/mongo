8fca1562eae7c9441c664a5868c220b4b2aad050
David Bradford <david.bradford@mongodb.com>
2022-06-10 20:30:02 +0000

    SERVER-67073: Split log lines larger than logkeeper's max

--- buildscripts/tests/resmokelib/core/test_pipe.py
+++ buildscripts/tests/resmokelib/core/test_pipe.py
@@ -10,7 +10,7 @@ import mock
 
 from buildscripts.resmokelib.core import pipe as _pipe
 
-# pylint: disable=missing-docstring
+# pylint: disable=missing-docstring,protected-access
 
 
 class TestLoggerPipe(unittest.TestCase):
@@ -43,3 +43,40 @@ class TestLoggerPipe(unittest.TestCase):
     def test_escapes_null_bytes(self):
         calls = self._get_log_calls(b"a\0b")
         self.assertEqual(calls, [mock.call(self.LOG_LEVEL, u"a\\0b")])
+
+
+class TestFormatLineForLogging(unittest.TestCase):
+    def test_strips_trailing_whitespace(self):
+        line = b" a "
+
+        line_output = _pipe.LoggerPipe._format_line_for_logging(line)
+
+        self.assertEqual([u" a"], line_output)
+
+    def test_strips_trailing_newlines(self):
+        line = b"a\r\n"
+
+        line_output = _pipe.LoggerPipe._format_line_for_logging(line)
+
+        self.assertEqual([u"a"], line_output)
+
+    def test_handles_invalid_utf8(self):
+        line = b"a\x80b"
+
+        line_output = _pipe.LoggerPipe._format_line_for_logging(line)
+
+        self.assertEqual([u"a\ufffdb"], line_output)
+
+    def test_escapes_null_bytes(self):
+        line = b"a\0b"
+
+        line_output = _pipe.LoggerPipe._format_line_for_logging(line)
+
+        self.assertEqual([u"a\\0b"], line_output)
+
+    def test_long_lines_are_split(self):
+        line = b"a" * 4_000_000
+
+        line_output = _pipe.LoggerPipe._format_line_for_logging(line)
+
+        self.assertEqual(2, len(line_output))
