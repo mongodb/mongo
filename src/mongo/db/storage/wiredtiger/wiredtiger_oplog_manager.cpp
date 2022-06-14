4e85654b613ab91f9507cd97997db5fa62c3076e
Dianna Hohensee <dianna.hohensee@mongodb.com>
2022-06-14 14:34:08 +0000

    SERVER-66529 OplogManager must not release mutex, so as to avoid wrongly fowarding the oplogReadTimestamp after a cappedTruncateAfter operation sets it back.
    (cherry picked from commit d9e643fe4dfb7da4ab68d6929477613f244ad361)

--- src/mongo/db/storage/wiredtiger/wiredtiger_oplog_manager.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_oplog_manager.cpp
@@ -237,8 +237,6 @@ void WiredTigerOplogManager::_updateOplogVisibilityLoop(WiredTigerSessionCache* 
         invariant(_triggerOplogVisibilityUpdate);
         _triggerOplogVisibilityUpdate = false;
 
-        lk.unlock();
-
         // Fetch the all_durable timestamp from the storage engine, which is guaranteed not to have
         // any holes behind it in-memory.
         const uint64_t newTimestamp = sessionCache->getKVEngine()->getAllDurableTimestamp().asULL();
@@ -254,7 +252,6 @@ void WiredTigerOplogManager::_updateOplogVisibilityLoop(WiredTigerSessionCache* 
             continue;
         }
 
-        lk.lock();
         // Publish the new timestamp value. Avoid going backward.
         auto currentVisibleTimestamp = getOplogReadTimestamp();
         if (newTimestamp > currentVisibleTimestamp) {
