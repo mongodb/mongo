5f63fb90c806f3f9d8264eb98345d8f1ff4b45c1
Gabriel Marks <gabriel.marks@mongodb.com>
2022-07-25 17:06:48 +0000

    SERVER-67532 Fail oplog server status section less

--- src/mongo/db/repl/replication_info.cpp
+++ src/mongo/db/repl/replication_info.cpp
@@ -248,11 +248,11 @@ public:
         BSONObjBuilder result;
         result.append("latestOptime", replCoord->getMyLastAppliedOpTime().getTimestamp());
 
-        auto earliestOplogTimestampFetch = [&]() -> StatusWith<Timestamp> {
+        auto earliestOplogTimestampFetch = [&]() -> Timestamp {
             auto oplog = CollectionCatalog::get(opCtx)->lookupCollectionByNamespaceForRead(
                 opCtx, NamespaceString::kRsOplogNamespace);
             if (!oplog) {
-                return StatusWith<Timestamp>(ErrorCodes::NamespaceNotFound, "oplog doesn't exist");
+                return Timestamp();
             }
 
             // Try to get the lock. If it's already locked, immediately return null timestamp.
@@ -282,13 +282,13 @@ public:
                     return o["ts"].timestamp();
                 }
             }
-
-            return swEarliestOplogTimestamp;
+            if (!swEarliestOplogTimestamp.isOK()) {
+                return Timestamp();
+            }
+            return swEarliestOplogTimestamp.getValue();
         }();
 
-        uassert(
-            17347, "Problem reading earliest entry from oplog", earliestOplogTimestampFetch.isOK());
-        result.append("earliestOptime", earliestOplogTimestampFetch.getValue());
+        result.append("earliestOptime", earliestOplogTimestampFetch);
 
         return result.obj();
     }
