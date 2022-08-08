953a8c7c168ba406242fa844e635aae4ac6edd23
Sviatlana Zuiko <sviatlana.zuiko@mongodb.com>
2022-08-08 21:42:11 +0000

    Revert "SERVER-68119 Standardized mongo[/d/s/q] lookup"
    This reverts commit 152fde419e0d31a80f1211738f4b264892c85797.

--- src/mongo/shell/servers.js
+++ src/mongo/shell/servers.js
@@ -77,18 +77,10 @@ MongoRunner = function() {};
 MongoRunner.dataDir = "/data/db";
 MongoRunner.dataPath = "/data/db/";
 
-var installDir = _getEnv("INSTALL_DIR");
-if (installDir) {
-    MongoRunner.mongodPath = installDir + "/mongod";
-    MongoRunner.mongosPath = installDir + "/mongos";
-    MongoRunner.mongoqPath = installDir + "/mongoqd";
-    MongoRunner.mongoShellPath = installDir + "/mongo";
-} else {
-    MongoRunner.mongodPath = "mongod";
-    MongoRunner.mongosPath = "mongos";
-    MongoRunner.mongoqPath = "mongoqd";
-    MongoRunner.mongoShellPath = "mongo";
-}
+MongoRunner.mongodPath = "mongod";
+MongoRunner.mongosPath = "mongos";
+MongoRunner.mongoqPath = "mongoqd";
+MongoRunner.mongoShellPath = "mongo";
 
 MongoRunner.VersionSub = function(pattern, version) {
     this.pattern = pattern;
