2ac2df5c9e1bd3ed28245320b584121ada0ab781
Zixuan <leozzx@users.noreply.github.com>
2022-08-08 17:52:25 +0000

    SERVER-68609 Check sbe feature flag across all nodes for sbe_ixscan_explain.js

--- jstests/core/sbe/sbe_ixscan_explain.js
+++ jstests/core/sbe/sbe_ixscan_explain.js
@@ -12,7 +12,7 @@
 load('jstests/libs/analyze_plan.js');  // For getPlanStages
 load("jstests/libs/sbe_util.js");      // For checkSBEEnabled.
 
-const isSBEEnabled = checkSBEEnabled(db, ["featureFlagSbeFull"]);
+const isSBEEnabled = checkSBEEnabled(db, ["featureFlagSbeFull"], true /* checkAllNodes */);
 if (!isSBEEnabled) {
     jsTestLog("Skipping test because the SBE feature flag is disabled");
     return;
@@ -36,6 +36,8 @@ assert.commandWorked(coll.insertMany([
 
 let explain = coll.find({a: 3}).hint({a: 1}).explain("executionStats");
 assert(isIxscan(db, getWinningPlan(explain.queryPlanner)));
+// Ensure the query is run on sbe engine.
+assert('slotBasedPlan' in explain.queryPlanner.winningPlan);
 
 let ixscanStages = getPlanStages(explain.executionStats.executionStages, "ixseek");
 assert(ixscanStages.length !== 0);
