4d99339bf9593b91bb6f9d16029dc1da2e44905d
Luke Chen <luke.chen@mongodb.com>
2022-08-08 11:54:04 +1000

    Import wiredtiger: c93ddc3da4f8384a084e9b585960fc284ef5a540 from branch mongodb-5.0
    ref: 9bdf1abbc3..c93ddc3da4
    for: 5.0.11
    
    WT-9595 Disable FLCS in test_prepare21 (#8180)

--- src/third_party/wiredtiger/test/suite/test_prepare21.py
+++ src/third_party/wiredtiger/test/suite/test_prepare21.py
@@ -40,7 +40,6 @@ class test_prepare21(test_rollback_to_stable_base):
 
     format_values = [
         ('column', dict(key_format='r', value_format='S')),
-        ('column_fix', dict(key_format='r', value_format='8t')),
         ('row_integer', dict(key_format='i', value_format='S')),
     ]
 
@@ -69,16 +68,10 @@ class test_prepare21(test_rollback_to_stable_base):
         ds = SimpleDataSet(self, uri, 0, key_format=self.key_format, value_format=self.value_format)
         ds.populate()
 
-        if self.value_format == '8t':
-             value_a = 97
-             value_b = 98
-             value_c = 99
-             value_d = 100
-        else:
-             value_a = "aaaaa" * 100
-             value_b = "bbbbb" * 100
-             value_c = "ccccc" * 100
-             value_d = "ddddd" * 100
+        value_a = "aaaaa" * 100
+        value_b = "bbbbb" * 100
+        value_c = "ccccc" * 100
+        value_d = "ddddd" * 100
 
         # Pin oldest and stable to timestamp 10.
         self.conn.set_timestamp('oldest_timestamp=' + self.timestamp_str(10) +
