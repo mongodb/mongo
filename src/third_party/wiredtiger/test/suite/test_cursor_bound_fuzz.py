ff4696b6224b14edaa27c638eae7c354ce4a709e
Will Korteland <will.korteland@mongodb.com>
2022-08-10 23:43:03 +0000

    Import wiredtiger: ee14e0ec50ebecf265591b6049b6428d315b1b21 from branch mongodb-master
    ref: 934a2c8412..ee14e0ec50
    for: 6.1.0-rc0
    
    WT-9722 Fix cursor bound fuzz test only testing previous scenarios (#8192)

--- src/third_party/wiredtiger/test/suite/test_cursor_bound_fuzz.py
+++ src/third_party/wiredtiger/test/suite/test_cursor_bound_fuzz.py
@@ -195,7 +195,7 @@ class test_cursor_bound_fuzz(wttest.WiredTigerTestCase):
     def run_search(self, bound_set, cursor):
         # Choose a N random keys and preform a search on each
         for i in range(0, self.search_count):
-            rand_key = random.randrange(self.key_count)
+            rand_key = random.randrange(self.min_key, self.max_key)
             cursor.set_key(rand_key)
             ret = cursor.search()
             if (ret == wiredtiger.WT_PREPARE_CONFLICT):
@@ -287,12 +287,8 @@ class test_cursor_bound_fuzz(wttest.WiredTigerTestCase):
                         raise Exception('Illegal state found in search_near')
 
     def run_bound_scenarios(self, bound_set, cursor):
-        if (self.data_format == 'column'):
-            scenario = random.choice([bound_scenarios.NEXT, bound_scenarios.SEARCH])
-        else:
-            scenario = random.choice(list(bound_scenarios))
+        scenario = random.choice(list(bound_scenarios))
 
-        scenario = bound_scenarios.PREV
         if (scenario is bound_scenarios.NEXT):
             self.run_next_prev(bound_set, True, cursor)
         elif (scenario is bound_scenarios.PREV):
