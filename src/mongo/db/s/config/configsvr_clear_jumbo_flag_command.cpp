e6e0476a97835623f23df55d1d0f68e500b441ef
Tommaso Tocci <tommaso.tocci@mongodb.com>
2022-08-11 12:48:00 +0000

    SERVER-68630 Review serialization mechanisms of clearJumboFlag command

--- src/mongo/db/s/config/configsvr_clear_jumbo_flag_command.cpp
+++ src/mongo/db/s/config/configsvr_clear_jumbo_flag_command.cpp
@@ -28,14 +28,10 @@
  */
 
 
-#include "mongo/platform/basic.h"
-
-#include "mongo/db/audit.h"
 #include "mongo/db/auth/authorization_session.h"
 #include "mongo/db/commands.h"
 #include "mongo/db/repl/repl_client_info.h"
 #include "mongo/db/s/config/sharding_catalog_manager.h"
-#include "mongo/db/s/ddl_lock_manager.h"
 #include "mongo/s/grid.h"
 #include "mongo/s/request_types/sharded_ddl_commands_gen.h"
 
@@ -68,12 +64,6 @@ public:
                 repl::ReadConcernArgs(repl::ReadConcernLevel::kLocalReadConcern);
 
             const auto catalogClient = Grid::get(opCtx)->catalogClient();
-
-            // Acquire distlocks on the namespace's database and collection.
-            const auto dbDDLock = DDLLockManager::get(opCtx)->lock(
-                opCtx, nss.db(), "clearJumboFlag", DDLLockManager::kDefaultLockTimeout);
-            const auto collDDLLock = DDLLockManager::get(opCtx)->lock(
-                opCtx, nss.ns(), "clearJumboFlag", DDLLockManager::kDefaultLockTimeout);
 
             CollectionType collType;
             try {
