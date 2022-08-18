88c907d6b704935a66318ab17e0674462e174552
Yuhong Zhang <yuhong.zhang@mongodb.com>
2022-08-18 15:06:56 +0000

    SERVER-66012 Use the WiredTiger version cursor to print the index entries metadata when corruption is detected during validation
    (cherry picked from commit 712a3e7a91c460adaa31e3583f02094caceb809a)

--- src/mongo/db/catalog/index_consistency.cpp
+++ src/mongo/db/catalog/index_consistency.cpp
@@ -407,8 +407,10 @@ void IndexConsistency::addDocKey(OperationContext* opCtx,
         _missingIndexEntries.insert(
             std::make_pair(key, IndexEntryInfo(*indexInfo, recordId, idKeyBuilder.obj(), ks)));
 
-        // Prints the collection document's metadata.
+        // Prints the collection document's and index entry's metadata.
         _validateState->getCollection()->getRecordStore()->printRecordMetadata(opCtx, recordId);
+        indexInfo->accessMethod->asSortedData()->getSortedDataInterface()->printIndexEntryMetadata(
+            opCtx, ks);
     }
 }
 
@@ -481,9 +483,12 @@ void IndexConsistency::addIndexKey(OperationContext* opCtx,
                 SimpleBSONObjSet infoSet = {info};
                 _extraIndexEntries.insert(std::make_pair(key, infoSet));
 
-                // Prints the collection document's metadata.
+                // Prints the collection document's and index entry's metadata.
                 _validateState->getCollection()->getRecordStore()->printRecordMetadata(opCtx,
                                                                                        recordId);
+                indexInfo->accessMethod->asSortedData()
+                    ->getSortedDataInterface()
+                    ->printIndexEntryMetadata(opCtx, ks);
                 return;
             }
             search->second.insert(info);
