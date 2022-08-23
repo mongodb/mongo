29b2944a389c1937c294fd061f54526a14f49498
Reo Kimura <reo.kimura@mongodb.com>
2022-08-23 15:44:49 +0000

    SERVER-69007 Loosen test restriction in listener_processing_server_status_metrics.js

--- jstests/sharding/listener_processing_server_status_metrics.js
+++ jstests/sharding/listener_processing_server_status_metrics.js
@@ -2,8 +2,8 @@
  *  @tags: [requires_fcv_60, featureFlagConnHealthMetrics, __TEMPORARILY_DISABLED__]
  *
  * Tests that listener processing times for connections are properly reported in server status
- * metrics. With each new connection to the same host, the value of the metric should always
- * be strictly increasing to reflect its rolling sum nature.
+ * metrics. With each new connection to the same host, the value of the metric should be
+ * monotonically increasing.
  */
 
 (function() {
@@ -13,21 +13,18 @@ const numConnections = 10;
 const st = new ShardingTest({shards: 1, mongos: 1});
 const admin = st.s.getDB("admin");
 
-assert(admin.adminCommand({getParameter: 1, featureFlagConnHealthMetrics: 1})
-           .featureFlagConnHealthMetrics.value,
-       'featureFlagConnHealthMetrics should be enabled for this test');
-
-const uri = "mongodb://" + admin.getMongo().host;
-const testDB = "listenerProcessingTest";
-
 let previous = 0;
 for (var i = 0; i < numConnections; i++) {
-    const conn = new Mongo(uri);
+    const conn = new Mongo(admin.getMongo().host);
     const t =
         assert.commandWorked(admin.serverStatus()).network.listenerProcessingTime["durationMicros"];
-    assert.gt(t, previous);
+    assert.gte(t, previous);
     previous = t;
 }
+
+// While there is an off-chance that the timer doesn't increment for a connection, this is rare and
+// the metric should have increased for most if not all connections.
+assert.gt(previous, 0);
 
 st.stop();
 })();
