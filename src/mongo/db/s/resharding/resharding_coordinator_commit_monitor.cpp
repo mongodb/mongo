abd6330d793235c8fbc5de7bf3ec53855ebea9d3
Andrew Witten <andrew.witten@mongodb.com>
2022-08-24 15:11:12 +0000

    SERVER-67653 don't initiate critical section if remainingMillis is omitted

--- src/mongo/db/s/resharding/resharding_coordinator_commit_monitor.cpp
+++ src/mongo/db/s/resharding/resharding_coordinator_commit_monitor.cpp
@@ -167,12 +167,20 @@ CoordinatorCommitMonitor::queryRemainingOperationTimeForRecipients() const {
         uassertStatusOKWithContext(status, errorContext);
 
         const auto remainingTime = extractOperationRemainingTime(shardResponse.data);
-        // A recipient shard does not report the remaining operation time when there is no data
-        // to copy and no oplog entry to apply.
-        if (remainingTime && remainingTime.value() < minRemainingTime) {
+
+        // If any recipient omits the "remainingMillis" field of the response then
+        // we cannot conclude that it is safe to begin the critical section.
+        // It is possible that the recipient just had a failover and
+        // was not able to restore its metrics before it replied to the
+        // _shardsvrReshardingOperationTime command.
+        if (!remainingTime) {
+            maxRemainingTime = Milliseconds::max();
+            continue;
+        }
+        if (remainingTime.value() < minRemainingTime) {
             minRemainingTime = remainingTime.value();
         }
-        if (remainingTime && remainingTime.value() > maxRemainingTime) {
+        if (remainingTime.value() > maxRemainingTime) {
             maxRemainingTime = remainingTime.value();
         }
     }
@@ -206,11 +214,18 @@ ExecutorFuture<void> CoordinatorCommitMonitor::_makeFuture() const {
                           "Encountered an error while querying recipients, will retry shortly",
                           "error"_attr = status);
 
-            return RemainingOperationTimes{Milliseconds(0), Milliseconds::max()};
+            // On error we definitely cannot begin the critical section.  Therefore,
+            // return Milliseconds::max for remainingTimes.max (remainingTimes.max is used
+            // for determining whether the critical section should begin).
+            return RemainingOperationTimes{Milliseconds(-1), Milliseconds::max()};
         })
         .then([this, anchor = shared_from_this()](RemainingOperationTimes remainingTimes) {
-            _metrics->setCoordinatorHighEstimateRemainingTimeMillis(remainingTimes.max);
-            _metrics->setCoordinatorLowEstimateRemainingTimeMillis(remainingTimes.min);
+            // If remainingTimes.max (or remainingTimes.min) is Milliseconds::max, then use -1 so
+            // that the scale of the y-axis is still useful when looking at FTDC metrics.
+            _metrics->setCoordinatorHighEstimateRemainingTimeMillis(
+                remainingTimes.max == Milliseconds::max() ? Milliseconds(-1) : remainingTimes.max);
+            _metrics->setCoordinatorLowEstimateRemainingTimeMillis(
+                remainingTimes.min == Milliseconds::max() ? Milliseconds(-1) : remainingTimes.min);
 
             // Check if all recipient shards are within the commit threshold.
             if (remainingTimes.max <= _threshold)
