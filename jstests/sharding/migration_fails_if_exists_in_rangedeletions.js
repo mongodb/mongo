91debf5444a815d35b309a2a6b3db33dccc73d5c
Sergi Mateo Bellido <sergi.mateo-bellido@mongodb.com>
2022-08-26 12:26:36 +0000

    SERVER-68627 Increasing range deletion timeout value

--- jstests/sharding/migration_fails_if_exists_in_rangedeletions.js
+++ jstests/sharding/migration_fails_if_exists_in_rangedeletions.js
@@ -17,6 +17,12 @@ const ns = dbName + "." + collName;
 let st =
     new ShardingTest({shards: {rs0: {nodes: 3}, rs1: {nodes: 3}}, other: {enableBalancer: false}});
 
+// Increase timeout for range deletion of overlapping range on recipient.
+st.shard0.rs.getPrimary().adminCommand(
+    {setParameter: 1, receiveChunkWaitForRangeDeleterTimeoutMS: 90000});
+st.shard1.rs.getPrimary().adminCommand(
+    {setParameter: 1, receiveChunkWaitForRangeDeleterTimeoutMS: 90000});
+
 (() => {
     jsTestLog("Test simple shard key");
 
@@ -27,10 +33,6 @@ let st =
 
     // Pause range deletion on shard0.
     let suspendRangeDeletionFailpoint = configureFailPoint(st.shard0, "suspendRangeDeletion");
-
-    // Increase timeout for range deletion of overlapping range on recipient.
-    st.shard1.rs.getPrimary().adminCommand(
-        {setParameter: 1, receiveChunkWaitForRangeDeleterTimeoutMS: 90000});
 
     // Move the only chunk from shard0 to shard1. This will leave orphans on shard0 since we paused
     // range deletion.
