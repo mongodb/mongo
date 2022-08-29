3bf0d522db3017a344e5f093706539e97659897b
Benety Goh <benety@mongodb.com>
2022-08-29 19:42:58 -0400

    SERVER-68477 createIndexes overwrites NaN expireAfterSeconds before starting index build
    (cherry picked from commit 58796facf40c99ddf8bb537adf563dc43aa0a863)

--- jstests/noPassthrough/ttl_expire_nan_warning_on_startup.js
+++ jstests/noPassthrough/ttl_expire_nan_warning_on_startup.js
@@ -10,6 +10,7 @@
 (function() {
 'use strict';
 
+load("jstests/libs/fail_point_util.js");
 load('jstests/noPassthrough/libs/index_build.js');
 
 const rst = new ReplSetTest({nodes: [{}, {rsConfig: {votes: 0, priority: 0}}]});
@@ -20,7 +21,16 @@ let primary = rst.getPrimary();
 const db = primary.getDB('test');
 const coll = db.t;
 
-assert.commandWorked(coll.createIndex({t: 1}, {expireAfterSeconds: NaN}));
+// The test cases here revolve around having a TTL index in the catalog with a NaN
+// 'expireAfterSeconds'. The current createIndexes behavior will overwrite NaN with int32::max
+// unless we use a fail point.
+const fp = configureFailPoint(primary, 'skipTTLIndexNaNExpireAfterSecondsValidation');
+try {
+    assert.commandWorked(coll.createIndex({t: 1}, {expireAfterSeconds: NaN}));
+} finally {
+    fp.off();
+}
+
 assert.commandWorked(coll.insert({_id: 0, t: ISODate()}));
 
 // Force checkpoint in storage engine to ensure index is part of the catalog in
