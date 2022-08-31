cc9c6561cd3e39f3304bf0a401ec21f1e1a96f17
Yuhong Zhang <yuhong.zhang@mongodb.com>
2022-08-31 20:04:11 +0000

    SERVER-69176 Fix linking for BSON fuzzers

--- src/mongo/rpc/protocol_fuzzer.cpp
+++ src/mongo/rpc/protocol_fuzzer.cpp
@@ -51,10 +51,6 @@ struct CompressionInfrastructure {
     MessageCompressorManager manager;
 };
 
-void validateBSON(const BSONObj& obj) {
-    validateBSON(obj.objdata(), obj.objsize()).ignore();
-}
-
 void doFuzzing(ConstDataRangeCursor fuzzedData) try {
     if (fuzzedData.length() < sizeof(MSGHEADER::Layout)) {
         return;
@@ -79,10 +75,10 @@ void doFuzzing(ConstDataRangeCursor fuzzedData) try {
     switch (msg.operation()) {
         case dbMsg: {
             auto request = OpMsgRequest::parseOwned(msg);
-            validateBSON(request.body);
+            validateBSON(request.body).ignore();
             for (const auto& docSeq : request.sequences) {
                 for (const auto& doc : docSeq.objs) {
-                    validateBSON(doc);
+                    validateBSON(doc).ignore();
                 }
             }
         } break;
