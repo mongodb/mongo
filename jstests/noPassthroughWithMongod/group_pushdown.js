012cefe1d28a809b1f29a6dad17687c6e47b5280
Drew Paroski <drew.paroski@mongodb.com>
2022-09-08 16:04:58 +0000

    SERVER-69482 Fix "group_pushdown.js" test

--- jstests/noPassthroughWithMongod/group_pushdown.js
+++ jstests/noPassthroughWithMongod/group_pushdown.js
@@ -478,7 +478,7 @@ assertResultsMatchWithAndWithoutPushdown(
 assertResultsMatchWithAndWithoutPushdown(
     coll,
     [{$sortByCount: {$cond: [{$eq: ["$item", {$const: "a"}]}, "$price", "$quantity"]}}],
-    [{_id: 5, count: 4}, {_id: 10, count: 1}]);
+    [{_id: 10, count: 3}, {_id: 1, count: 1}, {_id: 5, count: 1}]);
 
 // When at the mongos-side in a sharded environment or we are spilling $doingMerge is set to true.
 // We should bail out and not push down $group stages and the suffix of the pipeline when we
