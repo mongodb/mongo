324a0479e22eda1a568d147a2dff1955512c9232
Gabriel Marks <gabriel.marks@mongodb.com>
2022-09-09 15:43:17 +0000

    Revert "SERVER-68340 Update SetClusterParameterCoordinator to be tenant-aware"
    This reverts commit 2e15c33f728f7d23de49a716a7d689c198d7f7f5.

--- src/mongo/db/s/config/configsvr_set_cluster_parameter_command.cpp
+++ src/mongo/db/s/config/configsvr_set_cluster_parameter_command.cpp
@@ -79,14 +79,10 @@ public:
                                               parameterName,
                                               request().getDbName().tenantId());
 
-                auto tenantId = request().getDbName().tenantId();
-
                 SetClusterParameterCoordinatorDocument coordinatorDoc;
-                ConfigsvrCoordinatorId cid(ConfigsvrCoordinatorTypeEnum::kSetClusterParameter);
-                cid.setSubId(StringData(tenantId ? tenantId->toString() : ""));
-                coordinatorDoc.setConfigsvrCoordinatorMetadata({cid});
+                coordinatorDoc.setConfigsvrCoordinatorMetadata(
+                    {ConfigsvrCoordinatorTypeEnum::kSetClusterParameter});
                 coordinatorDoc.setParameter(request().getCommandParameter());
-                coordinatorDoc.setTenantId(tenantId);
 
                 const auto service = ConfigsvrCoordinatorService::getService(opCtx);
                 const auto instance = service->getOrCreateService(opCtx, coordinatorDoc.toBSON());
