ac1111d677d6fd5fd662bd13bc6f356a7bfa7356
Spencer Jackson <spencer.jackson@mongodb.com>
2022-09-13 15:51:30 +0000

    SERVER-69348 Remove repairDatabase command
    (cherry picked from commit fa534d676f5f673a6a7f3ea656f650ff108b7f92)

--- jstests/replsets/tenant_migration_concurrent_writes_on_donor_util.js
+++ jstests/replsets/tenant_migration_concurrent_writes_on_donor_util.js
@@ -711,7 +711,6 @@ TenantMigrationConcurrentWriteUtil.testCases = {
             assert(!collectionExists(db, collName + "Renamed"));
         }
     },
-    repairDatabase: {skip: isDeprecated},
     replSetAbortPrimaryCatchUp: {skip: isNotRunOnUserDatabase},
     replSetFreeze: {skip: isNotRunOnUserDatabase},
     replSetGetConfig: {skip: isNotRunOnUserDatabase},
