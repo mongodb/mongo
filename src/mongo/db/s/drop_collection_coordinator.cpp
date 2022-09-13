d6b251841c09766b1a2efef583151289f50b0a88
Sergi Mateo Bellido <sergi.mateo-bellido@mongodb.com>
2022-09-13 11:40:49 +0000

    SERVER-69134 Adding a way to clear the metadata manager
    (cherry picked from commit fa94f5fb6216a1cc1e23f5ad4df05295b380070e)

--- src/mongo/db/s/drop_collection_coordinator.cpp
+++ src/mongo/db/s/drop_collection_coordinator.cpp
@@ -51,7 +51,7 @@ DropReply DropCollectionCoordinator::dropCollectionLocally(OperationContext* opC
         Lock::DBLock dbLock(opCtx, nss.dbName(), MODE_IX);
         Lock::CollectionLock collLock(opCtx, nss, MODE_IX);
         auto* csr = CollectionShardingRuntime::get(opCtx, nss);
-        csr->clearFilteringMetadata(opCtx);
+        csr->clearFilteringMetadataForDroppedCollection(opCtx);
     }
 
     DropReply result;
