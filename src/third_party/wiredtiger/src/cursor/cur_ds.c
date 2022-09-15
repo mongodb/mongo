32cabedc3d83446279ef0e78ed7de81cc4b3fb81
Will Korteland <will.korteland@mongodb.com>
2022-09-15 06:07:48 +0000

    Import wiredtiger: b0d1b062b29133950081c5497a638ba8f574c988 from branch mongodb-master
    ref: f2c48d93f0..b0d1b062b2
    for: 6.2.0-rc0
    
    WT-9577 Implement bounded cursor API for the Datastore cursor (#8264)

--- src/third_party/wiredtiger/src/cursor/cur_ds.c
+++ src/third_party/wiredtiger/src/cursor/cur_ds.c
@@ -95,6 +95,28 @@ __curds_cursor_resolve(WT_CURSOR *cursor, int ret)
 }
 
 /*
+ * __curds_bound --
+ *     WT_CURSOR.bound method for the data-source cursor type.
+ */
+static int
+__curds_bound(WT_CURSOR *cursor, const char *config)
+{
+    WT_CURSOR *source;
+    WT_DECL_RET;
+    WT_SESSION_IMPL *session;
+
+    source = ((WT_CURSOR_DATA_SOURCE *)cursor)->source;
+
+    CURSOR_API_CALL(cursor, session, bound, NULL);
+
+    WT_ERR(__curds_key_set(cursor));
+    ret = __curds_cursor_resolve(cursor, source->bound(source, config));
+
+err:
+    API_END_RET(session, ret);
+}
+
+/*
  * __curds_compare --
  *     WT_CURSOR.compare method for the data-source cursor type.
  */
@@ -426,7 +448,7 @@ __wt_curds_open(WT_SESSION_IMPL *session, const char *uri, WT_CURSOR *owner, con
       __curds_reserve,                                /* reserve */
       __wt_cursor_config_notsup,                      /* reconfigure */
       __wt_cursor_notsup,                             /* largest_key */
-      __wt_cursor_config_notsup,                      /* bound */
+      __curds_bound,                                  /* bound */
       __wt_cursor_notsup,                             /* cache */
       __wt_cursor_reopen_notsup,                      /* reopen */
       __wt_cursor_checkpoint_id,                      /* checkpoint ID */
