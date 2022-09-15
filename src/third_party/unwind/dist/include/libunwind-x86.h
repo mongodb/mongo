3e661ebcc1fc8624472ae7de43a593a556ca8b4f
Alexander Neben <alexander.neben@mongodb.com>
2022-09-15 14:51:34 +0000

    SERVER-69590 Fixed clang arm compiles
    https://github.com/mongodb-forks/libunwind/commit/4e9596735f8f19fcf6afe0b411ce66df5cfb243b
    This is the commit for the corresponding change to mongo-forks

--- src/third_party/unwind/dist/include/libunwind-x86.h
+++ src/third_party/unwind/dist/include/libunwind-x86.h
@@ -158,6 +158,7 @@ x86_regnum_t;
 typedef struct unw_tdep_save_loc
   {
     /* Additional target-dependent info on a save location.  */
+    char unused;
   }
 unw_tdep_save_loc_t;
 
@@ -169,6 +170,7 @@ typedef ucontext_t unw_tdep_context_t;
 typedef struct
   {
     /* no x86-specific auxiliary proc-info */
+    char unused;
   }
 unw_tdep_proc_info_t;
 
