8bd3a016765aa131b9235f5f5e6a2b65690721f9
Allison Easton <allison.easton@mongodb.com>
2022-09-20 07:18:04 +0000

    SERVER-69400 Rename global_indexes_cache.* to global_index_cache.*

--- src/mongo/s/global_index_cache.h
+++ src/mongo/s/global_index_cache.h
@@ -31,6 +31,7 @@
 
 #include "mongo/db/namespace_string.h"
 #include "mongo/s/catalog/type_index_catalog_gen.h"
+#include "mongo/s/index_version.h"
 
 namespace mongo {
 
@@ -58,5 +59,94 @@ public:
 private:
     boost::optional<Timestamp> _indexVersion;
     IndexCatalogTypeMap _indexes;
+};
+
+/**
+ * Constructed to be used exclusively by the CatalogCache as a vector clock (Time) to drive
+ * IndexCache's lookups.
+ *
+ * The CollectionIndexes class contains a timestamp for the collection generation which resets to 0
+ * if the collection is dropped, in which case the versions cannot be compared.
+ *
+ * This class wraps a CollectionIndexes object with a node-local sequence number
+ * (_epochDisambiguatingSequenceNum) that allows the comparision.
+ *
+ */
+class ComparableIndexVersion {
+public:
+    /**
+     * Creates a ComparableIndexVersion that wraps the given CollectionIndexes.
+     * Each object created through this method will have a local sequence number greater than the
+     * previously created ones.
+     */
+    static ComparableIndexVersion makeComparableIndexVersion(const CollectionIndexes& version);
+
+    /**
+     * Creates a new instance which will artificially be greater than any previously created
+     * ComparableIndexVersion and smaller than any instance created afterwards. Used as means to
+     * cause the collections cache to attempt a refresh in situations where causal consistency
+     * cannot be inferred.
+     */
+    static ComparableIndexVersion makeComparableIndexVersionForForcedRefresh();
+
+    /**
+     * Empty constructor needed by the ReadThroughCache.
+     *
+     * Instances created through this constructor will be always less then the ones created through
+     * the two static constructors, but they do not carry any meaningful value and can only be used
+     * for comparison purposes.
+     */
+    ComparableIndexVersion() = default;
+
+    std::string toString() const;
+
+    bool operator==(const ComparableIndexVersion& other) const;
+
+    bool operator!=(const ComparableIndexVersion& other) const {
+        return !(*this == other);
+    }
+
+    /**
+     * In case the two compared instances have different generations, the most recently created one
+     * will be greater, otherwise the comparision will be driven by the index versions of the
+     * underlying CollectionIndexes.
+     */
+    bool operator<(const ComparableIndexVersion& other) const;
+
+    bool operator>(const ComparableIndexVersion& other) const {
+        return other < *this;
+    }
+
+    bool operator<=(const ComparableIndexVersion& other) const {
+        return !(*this > other);
+    }
+
+    bool operator>=(const ComparableIndexVersion& other) const {
+        return !(*this < other);
+    }
+
+private:
+    friend class CatalogCache;
+
+    static AtomicWord<uint64_t> _epochDisambiguatingSequenceNumSource;
+    static AtomicWord<uint64_t> _forcedRefreshSequenceNumSource;
+
+    ComparableIndexVersion(uint64_t forcedRefreshSequenceNum,
+                           boost::optional<CollectionIndexes> version,
+                           uint64_t epochDisambiguatingSequenceNum)
+        : _forcedRefreshSequenceNum(forcedRefreshSequenceNum),
+          _indexVersion(std::move(version)),
+          _epochDisambiguatingSequenceNum(epochDisambiguatingSequenceNum) {}
+
+    void setCollectionIndexes(const CollectionIndexes& version);
+
+    uint64_t _forcedRefreshSequenceNum{0};
+
+    boost::optional<CollectionIndexes> _indexVersion;
+
+    // Locally incremented sequence number that allows to compare two colection versions with
+    // different generations. Each new ComparableIndexVersion will have a greater sequence number
+    // than the ones created before.
+    uint64_t _epochDisambiguatingSequenceNum{0};
 };
 }  // namespace mongo
