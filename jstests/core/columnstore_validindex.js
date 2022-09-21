725bcd9f69ce412cbe3043d4a1bc1fb9bd2cd478
Arun Banala <arun.banala@mongodb.com>
2022-09-21 15:25:46 -0700

    SERVER-69767 Add 'no_selinux' tag to disallow running the featureFlag tests on selinux

--- jstests/core/columnstore_validindex.js
+++ jstests/core/columnstore_validindex.js
@@ -6,6 +6,8 @@
  *   requires_fcv_62,
  *   uses_column_store_index,
  *   featureFlagColumnstoreIndexes,
+ *   # TODO SERVER-69884: featureFlag guarded tests shouldn't require explicit 'no_selinux' tag.
+ *   no_selinux,
  * ]
  */
 (function() {
