b61927c6cdb116d1334789f563fc6558d1e775ed
Sergey Galtsev <sergey.galtsev@mongodb.com>
2022-10-10 15:08:14 +0000

    SERVER-69884 filter out tests with featureFlagXXX from selinux

--- jstests/selinux/core.js
+++ jstests/selinux/core.js
@@ -40,8 +40,18 @@ class TestDefinition extends SelinuxBaseTest {
                     runNonMongoProgram(python,
                                        "buildscripts/resmokelib/utils/check_has_tag.py",
                                        t,
-                                       "no_selinux")) {
+                                       "^no_selinux$")) {
                     jsTest.log("Skipping test due to no_selinux tag: " + t);
+                    continue;
+                }
+
+                // Tests relying on featureFlagXXX will not work
+                if (HAS_TAG ==
+                    runNonMongoProgram(python,
+                                       "buildscripts/resmokelib/utils/check_has_tag.py",
+                                       t,
+                                       "^featureFlag.+$")) {
+                    jsTest.log("Skipping test due to feature flag tag: " + t);
                     continue;
                 }
 
