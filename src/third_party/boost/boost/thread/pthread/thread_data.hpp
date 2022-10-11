0f665ae234f7dd59785528b7ea6b5cca45785dac
Andrew Morrow <acm@mongodb.com>
2022-10-11 13:47:49 -0400

    SERVER-70475 Cherry pick signed integer compairson warning from newer boost on master

--- src/third_party/boost/boost/thread/pthread/thread_data.hpp
+++ src/third_party/boost/boost/thread/pthread/thread_data.hpp
@@ -58,7 +58,9 @@ namespace boost
           std::size_t page_size = ::sysconf( _SC_PAGESIZE);
 #endif
 #ifdef PTHREAD_STACK_MIN
-          if (size<PTHREAD_STACK_MIN) size=PTHREAD_STACK_MIN;
+          // MongoDB Modification for SERVER-70475: Cherry-picked
+          // static_cast back from boost 1.79 on master
+          if (size<static_cast<std::size_t>(PTHREAD_STACK_MIN)) size=PTHREAD_STACK_MIN;
 #endif
           size = ((size+page_size-1)/page_size)*page_size;
           int res = pthread_attr_setstacksize(&val_, size);
