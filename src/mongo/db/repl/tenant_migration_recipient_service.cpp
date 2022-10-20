21c50cca8176fa618aa45878eca007e6fb50b31f
mathisbessamdb <mathis.bessa@mongodb.com>
2022-10-20 14:47:33 +0000

    SERVER-67926 Delete non-existing garbage collectable tenant migration data should not cause a ConflictingInProgress error

--- src/mongo/db/repl/tenant_migration_recipient_service.cpp
+++ src/mongo/db/repl/tenant_migration_recipient_service.cpp
@@ -179,6 +179,8 @@ MONGO_FAIL_POINT_DEFINE(fpWaitUntilTimestampMajorityCommitted);
 MONGO_FAIL_POINT_DEFINE(hangAfterUpdatingTransactionEntry);
 MONGO_FAIL_POINT_DEFINE(fpBeforeAdvancingStableTimestamp);
 MONGO_FAIL_POINT_DEFINE(hangMigrationBeforeRetryCheck);
+MONGO_FAIL_POINT_DEFINE(skipCreatingIndexDuringRebuildService);
+MONGO_FAIL_POINT_DEFINE(pauseTenantMigrationRecipientInstanceBeforeDeletingOldStateDoc);
 
 namespace {
 // We never restart just the oplog fetcher.  If a failure occurs, we restart the whole state machine
@@ -297,6 +299,9 @@ void TenantMigrationRecipientService::abortAllMigrations(OperationContext* opCtx
 ExecutorFuture<void> TenantMigrationRecipientService::_rebuildService(
     std::shared_ptr<executor::ScopedTaskExecutor> executor, const CancellationToken& token) {
     return AsyncTry([this] {
+               if (MONGO_unlikely(skipCreatingIndexDuringRebuildService.shouldFail())) {
+                   return;
+               }
                auto nss = getStateDocumentsNS();
 
                AllowOpCtxWhenServiceRebuildingBlock allowOpCtxBlock(Client::getCurrent());
@@ -2742,15 +2747,32 @@ SemiFuture<void> TenantMigrationRecipientService::Instance::run(
                            // Otherwise, there is a real conflict so we should throw
                            // ConflictingInProgress.
                            lk.unlock();
-                           auto deleted =
-                               uassertStatusOK(tenantMigrationRecipientEntryHelpers::
-                                                   deleteStateDocIfMarkedAsGarbageCollectable(
-                                                       opCtx.get(), _tenantId));
+
+                           auto existingStateDoc =
+                               tenantMigrationRecipientEntryHelpers::getStateDoc(
+                                   opCtx.get(), mtab->getMigrationId());
+                           uassertStatusOK(existingStateDoc.getStatus());
+
                            uassert(ErrorCodes::ConflictingOperationInProgress,
                                    str::stream()
                                        << "Found active migration for tenantId \"" << _tenantId
                                        << "\" with migration id " << mtab->getMigrationId(),
-                                   deleted);
+                                   existingStateDoc.getValue().getExpireAt());
+
+                           pauseTenantMigrationRecipientInstanceBeforeDeletingOldStateDoc
+                               .pauseWhileSet();
+
+                           auto deleted =
+                               uassertStatusOK(tenantMigrationRecipientEntryHelpers::
+                                                   deleteStateDocIfMarkedAsGarbageCollectable(
+                                                       opCtx.get(), _tenantId));
+                           // The doc has an expireAt but was deleted before we had time to delete
+                           // it above therefore it's safe to pursue since it has been cleaned up.
+                           if (!deleted) {
+                               LOGV2_WARNING(6792601,
+                                             "Existing state document was deleted before we could "
+                                             "delete it ourselves.");
+                           }
                            lk.lock();
                        }
 
