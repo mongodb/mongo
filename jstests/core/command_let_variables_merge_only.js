da7a8b14f26ef0310015a199e95e5edf30ddd526
Alexander Ignatyev <alexander.ignatyev@mongodb.com>
2022-10-21 17:34:07 +0000

    SERVER-70757 Tag command_let_variables_merge_only.js with uses_out

--- jstests/core/command_let_variables_merge_only.js
+++ jstests/core/command_let_variables_merge_only.js
@@ -2,9 +2,7 @@
  * Tests that the aggregate command can use command-level let variables with $merge.
  *
  * @tags: [
- *   assumes_unsharded_collection,
- *   does_not_support_stepdowns,
- *   does_not_support_causal_consistency,
+ *   uses_$out,
  * ]
  */
 (function() {
