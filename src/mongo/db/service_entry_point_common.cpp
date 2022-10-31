1ac82c418b0ff6fc21734962e7e355677fea9634
Amirsaman Memaripour <amirsaman.memaripour@mongodb.com>
2022-10-31 21:27:32 +0000

    SERVER-60553 Temporarily suppress mirroring errors on recovering secondaries

--- src/mongo/db/service_entry_point_common.cpp
+++ src/mongo/db/service_entry_point_common.cpp
@@ -2360,8 +2360,24 @@ BSONObj ServiceEntryPointCommon::getRedactedCopyForLogging(const Command* comman
     return bob.obj();
 }
 
-void onHandleRequestException(const Status& status) {
+void logHandleRequestFailure(const Status& status) {
     LOGV2_ERROR(4879802, "Failed to handle request", "error"_attr = redact(status));
+}
+
+void onHandleRequestException(const HandleRequest& hr, const Status& status) {
+    auto isMirrorOp = [&] {
+        const auto& obj = hr.executionContext->getRequest().body;
+        if (auto e = obj.getField("mirrored"); MONGO_unlikely(e.ok() && e.boolean()))
+            return true;
+        return false;
+    };
+
+    // TODO SERVER-70510 revert changes introduced by SERVER-60553 that suppresses errors occurred
+    // during handling of mirroring operations on recovering secondaries.
+    if (MONGO_unlikely(status == ErrorCodes::NotWritablePrimary && isMirrorOp()))
+        return;
+
+    logHandleRequestFailure(status);
 }
 
 Future<DbResponse> ServiceEntryPointCommon::handleRequest(
@@ -2375,7 +2391,7 @@ Future<DbResponse> ServiceEntryPointCommon::handleRequest(
     invariant(opRunner);
 
     return opRunner->run()
-        .then([hr = std::move(hr)](DbResponse response) mutable {
+        .then([&hr](DbResponse response) mutable {
             hr.completeOperation(response);
 
             auto opCtx = hr.executionContext->getOpCtx();
@@ -2390,10 +2406,10 @@ Future<DbResponse> ServiceEntryPointCommon::handleRequest(
 
             return response;
         })
-        .tapError([](Status status) { onHandleRequestException(status); });
+        .tapError([hr = std::move(hr)](Status status) { onHandleRequestException(hr, status); });
 } catch (const DBException& ex) {
     auto status = ex.toStatus();
-    onHandleRequestException(status);
+    logHandleRequestFailure(status);
     return status;
 }
 
