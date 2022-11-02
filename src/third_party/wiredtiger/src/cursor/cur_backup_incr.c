35e593da151e7bc08bb5409b9d770942431d64ae
Chenhao Qu <chenhao.qu@mongodb.com>
2022-11-02 11:57:23 +1100

    Import wiredtiger: dd47ef1cde035e67010bdc95c456d1e59b0144e7 from branch mongodb-master
    ref: 0fc80dd1e7..dd47ef1cde
    for: 6.2.0-rc0
    
    WT-10054 Review btree and backup cursor verbosity

--- src/third_party/wiredtiger/src/cursor/cur_backup_incr.c
+++ src/third_party/wiredtiger/src/cursor/cur_backup_incr.c
@@ -71,10 +71,10 @@ __curbackup_incr_blkmod(WT_SESSION_IMPL *session, WT_BTREE *btree, WT_CURSOR_BAC
         WT_ERR(__wt_config_subgets(session, &v, "offset", &b));
         cb->offset = (uint64_t)b.val;
 
-        __wt_verbose(session, WT_VERB_BACKUP,
+        __wt_verbose_debug2(session, WT_VERB_BACKUP,
           "Found modified incr block gran %" PRIu64 " nbits %" PRIu64 " offset %" PRIu64,
           cb->granularity, cb->nbits, cb->offset);
-        __wt_verbose(session, WT_VERB_BACKUP, "Modified incr block config: \"%s\"", config);
+        __wt_verbose_debug2(session, WT_VERB_BACKUP, "Modified incr block config: \"%s\"", config);
 
         /*
          * The rename configuration string component was added later. So don't error if we don't
@@ -154,7 +154,7 @@ __curbackup_incr_next(WT_CURSOR *cursor)
          * incremental cursor below and return WT_NOTFOUND.
          */
         F_SET(cb, WT_CURBACKUP_INCR_INIT);
-        __wt_verbose(session, WT_VERB_BACKUP, "Set key WT_BACKUP_FILE %s size %" PRIuMAX,
+        __wt_verbose_debug2(session, WT_VERB_BACKUP, "Set key WT_BACKUP_FILE %s size %" PRIuMAX,
           cb->incr_file, (uintmax_t)size);
         __wt_cursor_set_key(cursor, 0, size, WT_BACKUP_FILE);
     } else {
@@ -183,7 +183,7 @@ __curbackup_incr_next(WT_CURSOR *cursor)
                 if (F_ISSET(cb, WT_CURBACKUP_RENAME) ||
                   (F_ISSET(cb, WT_CURBACKUP_CKPT_FAKE) && F_ISSET(cb, WT_CURBACKUP_HAS_CB_INFO))) {
                     WT_ERR(__wt_fs_size(session, cb->incr_file, &size));
-                    __wt_verbose(session, WT_VERB_BACKUP,
+                    __wt_verbose_debug2(session, WT_VERB_BACKUP,
                       "Set key WT_BACKUP_FILE %s size %" PRIuMAX, cb->incr_file, (uintmax_t)size);
                     __wt_cursor_set_key(cursor, 0, size, WT_BACKUP_FILE);
                     goto done;
@@ -220,7 +220,7 @@ __curbackup_incr_next(WT_CURSOR *cursor)
             WT_ERR(WT_NOTFOUND);
         WT_ASSERT(session, cb->granularity != 0);
         WT_ASSERT(session, total_len != 0);
-        __wt_verbose(session, WT_VERB_BACKUP,
+        __wt_verbose_debug2(session, WT_VERB_BACKUP,
           "Set key WT_BACKUP_RANGE %s offset %" PRIu64 " length %" PRIu64, cb->incr_file,
           cb->offset + cb->granularity * start_bitoff, total_len);
         __wt_cursor_set_key(
