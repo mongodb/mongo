d2fb5646942d989da1819e49ec2ab924927fa270
Tural Farhadov <tural.ferhadov@gmail.com>
2022-11-02 18:11:01 +0000

    SERVER-70805: Add test publish tasks to misc_release.yml

--- evergreen/packages_publish_test.sh
+++ evergreen/packages_publish_test.sh
@@ -11,4 +11,4 @@ set -o verbose
 CURATOR_RELEASE=${curator_release}
 curl -L -O http://boxes.10gen.com/build/curator/curator-dist-rhel70-$CURATOR_RELEASE.tar.gz
 tar -zxvf curator-dist-rhel70-$CURATOR_RELEASE.tar.gz
-./curator repo submit --service ${barque_url_test} --config ./etc/repo_config_test.yaml --distro ${packager_distro} --edition ${repo_edition} --version ${version} --arch ${packager_arch} --packages https://s3.amazonaws.com/mciuploads/${project}/${build_variant}/${revision}/artifacts/${build_id}-packages.tgz
+NOTARY_KEY_NAME=${signing_key_name_test} NOTARY_TOKEN=${signing_auth_token_test} BARQUE_API_KEY=${barque_api_key_test} BARQUE_USERNAME=${barque_user_test} ./curator repo submit --service ${barque_url_test} --config ./etc/repo_config_test.yaml --distro ${packager_distro} --edition ${repo_edition} --version ${version} --arch ${packager_arch} --packages https://s3.amazonaws.com/mciuploads/${project}/${build_variant}/${revision}/artifacts/${build_id}-packages.tgz
