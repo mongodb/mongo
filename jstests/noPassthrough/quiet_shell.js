493b63c41b1f5c143d24470e21ae6c0714f3f650
Shreyas Kalyan <shreyas.kalyan@mongodb.com>
2022-11-14 17:22:46 -0500

    SERVER-70416 add legacy shell --quiet test

---
+++ jstests/noPassthrough/quiet_shell.js
@@ -0,0 +1,26 @@
+(function() {
+'use strict';
+
+const mongo = MongoRunner.runMongod();
+const regexMatch = /sh([0-9]{5}|[0-9]{4})\|/;
+
+// Test that a normal mongo shell gives us some noise in the raw output.
+{
+    const out = runMongoProgram('mongo', '--port', mongo.port, '--eval', ';');
+    const mongoOutput = rawMongoProgramOutput();
+
+    assert.gte(mongoOutput.match(regexMatch).length, 1);
+}
+
+clearRawMongoProgramOutput();
+
+// Test that a quiet shell does not output anything.
+{
+    const out = runMongoProgram('mongo', '--port', mongo.port, '--quiet', '--eval', ';');
+    const mongoOutput = rawMongoProgramOutput();
+
+    assert.eq(mongoOutput.match(regexMatch), null);
+}
+
+MongoRunner.stopMongod(mongo);
+})();
