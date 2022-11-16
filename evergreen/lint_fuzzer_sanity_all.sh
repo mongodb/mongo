4f8c870d1ea7b76997e6587f7facc0ae9065464b
Alexander Neben <alexander.neben@mongodb.com>
2022-11-16 22:09:22 +0000

    SERVER-71424 Excluded third_party js files from linting
    (cherry picked from commit be0adb109bfac64bc1177b7f43eb3a4accb6d3e9)

--- evergreen/lint_fuzzer_sanity_all.sh
+++ evergreen/lint_fuzzer_sanity_all.sh
@@ -9,7 +9,8 @@ set -o verbose
 add_nodejs_to_path
 
 # Run parse-jsfiles on 50 files at a time with 32 processes in parallel.
-find "$PWD/jstests" "$PWD/src/mongo/db/modules/enterprise" -name "*.js" -print | xargs -P 32 -L 50 npm run --prefix jstestfuzz parse-jsfiles -- | tee lint_fuzzer_sanity.log
+# Skip javascript files in third_party directory
+find "$PWD/jstests" "$PWD/src/mongo/db/modules/enterprise" -path "$PWD/jstests/third_party" -prune -o -name "*.js" -print | xargs -P 32 -L 50 npm run --prefix jstestfuzz parse-jsfiles -- | tee lint_fuzzer_sanity.log
 exit_code=$?
 
 activate_venv
