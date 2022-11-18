8c19a7bcf29fe698fee342a527e37a42f19379f7
Alexander Neben <alexander.neben@mongodb.com>
2022-11-18 20:07:00 +0000

    SERVER-71471 Fixed deployment process for jstestfuzz
    (cherry picked from commit 45400e2540977847771a1172de82a01fb12ec8b4)

--- evergreen/lint_fuzzer_sanity_all.sh
+++ evergreen/lint_fuzzer_sanity_all.sh
@@ -6,11 +6,9 @@ cd src
 set -o pipefail
 set -o verbose
 
-add_nodejs_to_path
-
 # Run parse-jsfiles on 50 files at a time with 32 processes in parallel.
 # Skip javascript files in third_party directory
-find "$PWD/jstests" "$PWD/src/mongo/db/modules/enterprise" -path "$PWD/jstests/third_party" -prune -o -name "*.js" -print | xargs -P 32 -L 50 npm run --prefix jstestfuzz parse-jsfiles -- | tee lint_fuzzer_sanity.log
+find "$PWD/jstests" "$PWD/src/mongo/db/modules/enterprise" -path "$PWD/jstests/third_party" -prune -o -name "*.js" -print | xargs -P 32 -L 50 ./jstestfuzz/src/scripts/npm_run.sh --prefix jstestfuzz parse-jsfiles -- | tee lint_fuzzer_sanity.log
 exit_code=$?
 
 activate_venv
