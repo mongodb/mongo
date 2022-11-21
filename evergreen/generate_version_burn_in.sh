8683d7af187bc9d40655336d3eb07f5088bb9d37
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2022-11-21 17:21:35 +0000

    SERVER-71518 Use S3 test stats in mongo-task-generator
    (cherry picked from commit f590e944138f1e2d091926df9413f7412c9f477f)

--- evergreen/generate_version_burn_in.sh
+++ evergreen/generate_version_burn_in.sh
@@ -13,5 +13,6 @@ PATH=$PATH:$HOME:/ ./mongo-task-generator \
   --evg-auth-file ./.evergreen.yml \
   --evg-project-file ${evergreen_config_file_path} \
   --generate-sub-tasks-config etc/generate_subtasks_config.yml \
+  --s3-test-stats-endpoint https://mongo-test-stats.s3.amazonaws.com \
   --burn-in \
   $@
