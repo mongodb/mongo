605d9ce64399c3222d07454521eb413d851430ba
Alexander Neben <alexander.neben@mongodb.com>
2022-11-22 23:08:04 +0000

    SERVER-71534 Fixed fuzzer linting scripts
    (cherry picked from commit abb9e2114622a4bf9052e5adfcaf28792ce189e9)

--- evergreen/lint_fuzzer_sanity_patch.sh
+++ evergreen/lint_fuzzer_sanity_patch.sh
@@ -8,11 +8,14 @@ set -o verbose
 
 mkdir -p jstestfuzzinput jstestfuzzoutput
 
-indir="$(pwd)/jstestfuzzinput"
-outdir="$(pwd)/jstestfuzzoutput"
+# We need to be the jstestfuzz repo for node to install/run
+cd jstestfuzz
+
+indir="$(pwd)/../jstestfuzzinput"
+outdir="$(pwd)/../jstestfuzzoutput"
 
 # Grep all the js files from modified_and_created_patch_files.txt and put them into $indir.
-(grep -v "\.tpl\.js$" modified_and_created_patch_files.txt | grep ".*jstests/.*\.js$" | xargs -I {} cp {} $indir || true)
+(grep -v "\.tpl\.js$" ../modified_and_created_patch_files.txt | grep ".*jstests/.*\.js$" | xargs -I {} cp {} $indir || true)
 
 # Count the number of files in $indir.
 if [[ "$(ls -A $indir)" ]]; then
@@ -23,8 +26,8 @@ if [[ "$(ls -A $indir)" ]]; then
     num_files=50
   fi
 
-  ./jstestfuzz/src/scripts/npm_run.sh --prefix jstestfuzz jstestfuzz -- --jsTestsDir $indir --out $outdir --numSourceFiles $num_files --numGeneratedFiles 50
+  ./src/scripts/npm_run.sh jstestfuzz -- --jsTestsDir $indir --out $outdir --numSourceFiles $num_files --numGeneratedFiles 50
 
   # Run parse-jsfiles on 50 files at a time with 32 processes in parallel.
-  ls -1 -d $outdir/* | xargs -P 32 -L 50 ./jstestfuzz/src/scripts/npm_run.sh --prefix jstestfuzz parse-jsfiles --
+  ls -1 -d $outdir/* | xargs -P 32 -L 50 ./src/scripts/npm_run.sh parse-jsfiles --
 fi
