b5e8544cb77adf0c459d0e630c87e280e348ea56
Alexander Neben <alexander.neben@mongodb.com>
2022-11-22 23:08:04 +0000

    SERVER-71534 Fixed fuzzer linting scripts
    (cherry picked from commit abb9e2114622a4bf9052e5adfcaf28792ce189e9)

--- evergreen/lint_fuzzer_sanity_all.sh
+++ evergreen/lint_fuzzer_sanity_all.sh
@@ -1,11 +1,14 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 . "$DIR/prelude.sh"
 
-cd src
+cd src/jstestfuzz
 
 set -eo pipefail
 set -o verbose
 
+# Run first with help which will do the install
+# Then we can run it in parallel
+./src/scripts/npm_run.sh --help
 # Run parse-jsfiles on 50 files at a time with 32 processes in parallel.
 # Skip javascript files in third_party directory
-find "$PWD/jstests" "$PWD/src/mongo/db/modules/enterprise" -path "$PWD/jstests/third_party" -prune -o -name "*.js" -print | xargs -P 32 -L 50 ./jstestfuzz/src/scripts/npm_run.sh --prefix jstestfuzz parse-jsfiles --
+find "$PWD/../jstests" "$PWD/../src/mongo/db/modules/enterprise" -path "$PWD/../jstests/third_party" -prune -o -name "*.js" -print | xargs -P 32 -L 50 ./src/scripts/npm_run.sh parse-jsfiles --
--- evergreen/lint_fuzzer_sanity_patch.sh
+++ evergreen/lint_fuzzer_sanity_patch.sh
@@ -1,11 +1,14 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 . "$DIR/prelude.sh"
 
-cd src
+cd src/jstestfuzz
 
 set -eo pipefail
 set -o verbose
 
+# Run first with help which will do the install
+# Then we can run it in parallel
+./src/scripts/npm_run.sh --help
 # Run parse-jsfiles on 50 files at a time with 32 processes in parallel.
 # Grep returns 1 if it fails to find a match.
-(grep -v "\.tpl\.js$" modified_and_created_patch_files.txt | grep "\.js$" || true) | xargs -P 32 -L 50 ./jstestfuzz/src/scripts/npm_run.sh --prefix jstestfuzz parse-jsfiles --
+(grep -v "\.tpl\.js$" ../modified_and_created_patch_files.txt | grep "\.js$" || true) | xargs -P 32 -L 50 ./src/scripts/npm_run.sh parse-jsfiles --
