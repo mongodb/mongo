ac877eecf0c5138b7f0a057ba86c92d9aeb284e7
Paolo Polato <paolo.polato@mongodb.com>
2022-12-08 15:18:06 +0000

    SERVER-71833 Fix failures causes by ErrorCode::Interrupted in autosplit_setFCV_downgrade_from_60.js

--- jstests/concurrency/fsm_workloads/autosplit_setFCV_downgrade_from_60.js
+++ jstests/concurrency/fsm_workloads/autosplit_setFCV_downgrade_from_60.js
@@ -27,6 +27,34 @@ function getRandomCollectionName(configDB, prefix) {
     }
 }
 
+// The test is overriding some cluster parameters (which original values will be overridden at
+// teardown time)
+let originalChunkDefragmentationThrottlingMS;
+let originalChunkSizeMB;
+let balancerEnabledInSuite;
+
+const maxChunkSizeMB = 1;
+
+function createShardedCollection(db, collName) {
+    let newColl = db.getName() + "." + collName;
+    jsTest.log("Creating collection " + newColl);
+    assertAlways.commandWorked(db.adminCommand({enableSharding: db.getName()}));
+    assertAlways.commandWorked(db.adminCommand({shardCollection: newColl, key: {_id: 1}}));
+}
+
+function insertIntoCollection(db, collName) {
+    jsTest.log("Inserting documents into collection " + collName);
+    let coll = db[collName];
+    let randomKey = Random.randInt(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
+    const bigString = "X".repeat(256 * 1024);
+    let bulk = coll.initializeUnorderedBulkOp();
+    for (let i = 0; i < 10; i++) {
+        bulk.insert({_id: randomKey + i, bigString: bigString});
+    }
+    assertAlways.commandWorked(bulk.execute());
+    jsTest.log("Inserts completed on collection " + collName + " starting from key: " + randomKey);
+}
+
 var $config = (function() {
     var states = {
         init: function init(db, collName, connCache) {
@@ -34,15 +62,10 @@ var $config = (function() {
             this.num = 0;
         },
 
-        create: function create(db, collName, connCache) {
+        createAndInsert: function createAndInsert(db, collName, connCache) {
             let newColl = db.getName() + "." + this.prefix + "_" + this.num++;
-            jsTest.log("Creating collection " + newColl);
             try {
-                assertAlways.commandWorked(db.adminCommand({enableSharding: db.getName()}));
-                assertAlways.commandWorked(
-                    db.adminCommand({shardCollection: newColl, key: {_id: 1}}));
-                assertAlways.commandWorked(db.adminCommand(
-                    {configureCollectionBalancing: newColl, enableAutoSplitter: false}));
+                createShardedCollection(db, newColl);
             } catch (e) {
                 if (e.code != ErrorCodes.NamespaceNotFound &&
                     e.code != ErrorCodes.NamespaceNotSharded &&
@@ -54,6 +77,7 @@ var $config = (function() {
                 return;
             }
             jsTest.log("Created collection " + newColl);
+            insertIntoCollection(db, newColl);
         },
 
         drop: function drop(db, collName, connCache) {
@@ -70,23 +94,12 @@ var $config = (function() {
             let randomColl = getRandomCollectionName(db.getSiblingDB("config"), this.prefix);
             if (randomColl === undefined)
                 return;
-            jsTest.log("Inserting documents into collection " + randomColl);
-            let coll = db[randomColl];
-            let randomKey = Random.randInt(Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER);
-            const bigString = "X".repeat(256 * 1024);
-            let bulk = coll.initializeUnorderedBulkOp();
-            for (let i = 0; i < 10; i++) {
-                bulk.insert({_id: randomKey + i, bigString: bigString});
-            }
-            assertAlways.commandWorked(bulk.execute());
-            jsTest.log("Inserts completed on collection " + randomColl +
-                       " starting from key: " + randomKey);
+            insertIntoCollection(db, randomColl);
         },
 
         setFCV: function setFCV(db, collName, connCache) {
-            let randomFCVVal = Random.randInt(2);
-            let targetFCV = randomFCVVal === 1 ? lastContinuousFCV : latestFCV;
-            jsTest.log("Setting FCV to " + targetFCV);
+            const performDowngrade = Random.randInt(2) === 1;
+            const targetFCV = performDowngrade ? lastLTSFCV : latestFCV;
             try {
                 assertAlways.commandWorked(
                     db.adminCommand({setFeatureCompatibilityVersion: targetFCV}));
@@ -102,33 +115,43 @@ var $config = (function() {
     };
 
     var transitions = {
-        init: {create: 0.5, setFCV: 0.5},
-        create: {drop: 0.165, insert: 0.495, setFCV: 0.33},
-        drop: {create: 0.33, insert: 0.33, setFCV: 0.33},
-        insert: {create: 0.495, drop: 0.165, setFCV: 0.33},
-        setFCV: {create: 0.33, drop: 0.165, insert: 0.495}
+        init: {createAndInsert: 1},
+        createAndInsert: {drop: 0.165, insert: 0.33, setFCV: 0.495},
+        drop: {createAndInsert: 0.33, insert: 0.33, setFCV: 0.33},
+        insert: {createAndInsert: 0.495, drop: 0.165, setFCV: 0.33},
+        setFCV: {createAndInsert: 0.33, drop: 0.165, insert: 0.495}
     };
-
-    let defaultChunkDefragmentationThrottlingMS;
-
     function setup(db, collName, cluster) {
-        let configDB = cluster.getDB('config');
-        configDB.settings.save({_id: "chunksize", value: /*<sizeInMB>*/ 1});
+        balancerEnabledInSuite = sh.getBalancerState();
+        const configDB = cluster.getDB('config');
+        originalChunkSizeMB = configDB.settings.findOne({_id: "chunkSize"});
+        assert.commandWorked(db.adminCommand({balancerStop: 1}));
+        configDB.settings.save({_id: "chunksize", value: maxChunkSizeMB});
         cluster.executeOnConfigNodes((db) => {
             const res = db.adminCommand({setParameter: 1, chunkDefragmentationThrottlingMS: 0});
             assert.commandWorked(res);
-            defaultChunkDefragmentationThrottlingMS = res.was;
+            originalChunkDefragmentationThrottlingMS = res.was;
         });
     }
 
     function teardown(db, collName, cluster) {
+        const configDB = cluster.getDB('config');
         assert.commandWorked(db.adminCommand({setFeatureCompatibilityVersion: latestFCV}));
+        if (originalChunkSizeMB) {
+            configDB.settings.save({_id: "chunksize", value: originalChunkSizeMB.value});
+        } else {
+            configDB.settings.deleteOne({_id: "chunksize"});
+        }
         cluster.executeOnConfigNodes((db) => {
             assert.commandWorked(db.adminCommand({
                 setParameter: 1,
-                chunkDefragmentationThrottlingMS: defaultChunkDefragmentationThrottlingMS
+                chunkDefragmentationThrottlingMS: originalChunkDefragmentationThrottlingMS
             }));
         });
+
+        if (balancerEnabledInSuite) {
+            assert.commandWorked(db.adminCommand({balancerStart: 1}));
+        }
     }
 
     return {
