10bddc04546f2f758a482ee46d53cc09f93b40e7
Will Korteland <will.korteland@mongodb.com>
2022-12-12 05:33:06 +0000

    Import wiredtiger: 06ffe0a3541f5b7894aa00c9ac44c0444057306b from branch mongodb-master
    ref: 8447470b58..06ffe0a354
    for: 6.3.0-rc0
    
    WT-9872 Perform input validation for create_test.sh

--- src/third_party/wiredtiger/test/cppsuite/create_test.sh
+++ src/third_party/wiredtiger/test/cppsuite/create_test.sh
@@ -8,10 +8,10 @@ if [ $# -eq 0 ]
 fi
 
 # Check the test name
-if [[ $1 =~ ^[0-9a-zA-Z_-]+$ ]];then
+if [[ $1 =~ ^[a-z][_a-z0-9]+$ ]]; then
     echo "Generating test: $1..."
 else
-    echo "Invalid test name. Only alphanumeric characters are allowed. \"_\" and \"-\" can be used too."
+    echo "Invalid test name. Test name must match the regex '[a-z][_a-z0-9]+$'"
     exit 128
 fi
 
