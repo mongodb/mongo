23f812d3c1b7c019b293ba65f0174ad3bc00bf10
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-12 10:58:06 +0000

    Adding tarfile member sanitization to extractall()

--- buildscripts/resmokelib/powercycle/powercycle.py
+++ buildscripts/resmokelib/powercycle/powercycle.py
@@ -403,7 +403,26 @@ def install_tarball(tarball, root_dir):
     ext = get_extension(tarball)
     if ext == ".tgz":
         with tarfile.open(tarball, "r:gz") as tar_handle:
-            tar_handle.extractall(path=root_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar_handle, path=root_dir)
             output = "Unzipped {} to {}: {}".format(tarball, root_dir, tar_handle.getnames())
         ret = 0
     elif ext == ".zip":
--- buildscripts/resmokelib/undodb/fetch.py
+++ buildscripts/resmokelib/undodb/fetch.py
@@ -93,7 +93,26 @@ def _download_archive(url: str) -> Optional[str]:
 def _extract_archive(fname: str):
     print(f"Extracting to '{os.getcwd()}'")
     with tarfile.open(fname) as tfile:
-        tfile.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tfile)
 
 
 def _cleanup(out_file: str):
