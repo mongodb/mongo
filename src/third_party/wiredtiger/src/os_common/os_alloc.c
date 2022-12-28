aaf2e3510e109fd50db2763ef23233301d3457e1
Luke Chen <luke.chen@mongodb.com>
2022-12-28 15:55:19 +1100

    Import wiredtiger: 0d2249387780dbe01031184a31b1502b6b990b30 from branch mongodb-master
    ref: e01aa00db0..0d22493877
    for: 6.3.0-rc0
    
    WT-10142 Fix NULL pointer in realloc function

--- src/third_party/wiredtiger/src/os_common/os_alloc.c
+++ src/third_party/wiredtiger/src/os_common/os_alloc.c
@@ -143,9 +143,11 @@ __realloc_func(WT_SESSION_IMPL *session, size_t *bytes_allocated_ret, size_t byt
         if ((p = malloc(bytes_to_allocate)) == NULL)
             WT_RET_MSG(session, __wt_errno(), "memory allocation of %" WT_SIZET_FMT " bytes failed",
               bytes_to_allocate);
-        memcpy(p, tmpp, *bytes_allocated_ret);
-        __wt_explicit_overwrite(tmpp, bytes_allocated);
-        __wt_free(session, tmpp);
+        if (tmpp != NULL) {
+            memcpy(p, tmpp, *bytes_allocated_ret);
+            __wt_explicit_overwrite(tmpp, bytes_allocated);
+            __wt_free(session, tmpp);
+        }
     } else {
         if ((p = realloc(p, bytes_to_allocate)) == NULL)
             WT_RET_MSG(session, __wt_errno(), "memory allocation of %" WT_SIZET_FMT " bytes failed",
