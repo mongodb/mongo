0f6e6e3057aedba686e44cfc51ff68d295a78264
Will Korteland <will.korteland@mongodb.com>
2023-01-04 21:45:54 +0000

    Import wiredtiger: 828743c5bd484fa7e21295be79532e9bec0652e8 from branch mongodb-master
    ref: 8330439378..828743c5bd
    for: 6.3.0-rc0
    
    WT-10443 Change 10000 to use WT_THOUSAND.

--- src/third_party/wiredtiger/src/conn/conn_cache.c
+++ src/third_party/wiredtiger/src/conn/conn_cache.c
@@ -252,7 +252,7 @@ __wt_cache_create(WT_SESSION_IMPL *session, const char *cfg[])
           "eviction updates target must be lower than the eviction updates trigger");
 
     WT_RET(__wt_cond_auto_alloc(
-      session, "cache eviction server", 10000, WT_MILLION, &cache->evict_cond));
+      session, "cache eviction server", 10 * WT_THOUSAND, WT_MILLION, &cache->evict_cond));
     WT_RET(__wt_spin_init(session, &cache->evict_pass_lock, "evict pass"));
     WT_RET(__wt_spin_init(session, &cache->evict_queue_lock, "cache eviction queue"));
     WT_RET(__wt_spin_init(session, &cache->evict_walk_lock, "cache walk"));
