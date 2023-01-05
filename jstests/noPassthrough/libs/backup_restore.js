40e94d0e8284279553b5ad24dc6fdbb03e5eee8a
Yu Jin Kang Park <yujin.kang@mongodb.com>
2023-01-05 09:57:43 +0000

    SERVER-72449 backup_restore.js should consider exit code 2 valid for fsm process
    (cherry picked from commit a403f10dde8ad4b470950a0a33bf8a44df5437d5)

--- jstests/noPassthrough/libs/backup_restore.js
+++ jstests/noPassthrough/libs/backup_restore.js
@@ -309,9 +309,10 @@ var BackupRestoreTest = function(options) {
             // The mongo shell calls TerminateProcess() on Windows rather than more gracefully
             // interrupting resmoke.py test execution.
 
-            // resmoke.py may exit cleanly on SIGINT returning 130, or uncleanly in which case
-            // stopMongoProgramByPid returns -SIGINT.
-            assert(exitCode == 130 || exitCode == -kSIGINT,
+            // resmoke.py may exit cleanly on SIGINT, returning 130 if the suite tests were running
+            // and returning SIGINT otherwise. It may also exit uncleanly, in which case
+            // stopMongoProgramByPid returns -SIGINT. See SERVER-67390 and SERVER-72449.
+            assert(exitCode == 130 || exitCode == -kSIGINT || exitCode == kSIGINT,
                    'expected resmoke.py to exit due to being interrupted, but exited with code: ' +
                        exitCode);
         }
