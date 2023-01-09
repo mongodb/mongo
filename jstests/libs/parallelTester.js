d07d44357f54f59c6f2196b7006ba8059c1e5a0f
Ian Boros <ian.boros@mongodb.com>
2023-01-09 19:04:47 +0000

    SERVER-72636 Ban tests that set up CSI from parallel tests

--- jstests/libs/parallelTester.js
+++ jstests/libs/parallelTester.js
@@ -256,6 +256,19 @@ if (typeof _threadInject != "undefined") {
             // TODO (SERVER-63228): Remove this exclusion once the feature flag is enabled by
             // default.
             "timeseries/timeseries_index_ttl_partial.js",
+
+            // TODO SERVER-72447: Avoid this.
+            "column_scan_skip_row_store_projection.js",
+            "column_store_index_compression.js",
+            "columnstore_eligibility.js",
+            "columnstore_index.js",
+            "columnstore_index_correctness.js",
+            "columnstore_index_per_path_filters.js",
+            "columnstore_large_array_index_correctness.js",
+            "columnstore_validindex.js",
+            "hidden_index.js",
+            "null_query_semantics.js",
+            "projection_semantics.js",
         ]);
 
         // Get files, including files in subdirectories.
