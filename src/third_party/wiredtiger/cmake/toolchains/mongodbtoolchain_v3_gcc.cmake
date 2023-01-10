60a0707e70ef177e2aae1ba7f8b364911b8e699c
Chenhao Qu <chenhao.qu@mongodb.com>
2023-01-11 09:22:45 +1100

    Import wiredtiger: 321773c34917b00cb01b23db1e0d20af1fc6aecc from branch mongodb-master
    ref: a437fcdc7d..321773c349
    for: 6.3.0-rc0
    
    WT-9968 Upgrade to v4 toolchain for evergreen tests

--- src/third_party/wiredtiger/cmake/toolchains/mongodbtoolchain_v3_gcc.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION 3.10.0)
-
-if(NOT TOOLCHAIN_ROOT)
-    set(TOOLCHAIN_ROOT "/opt/mongodbtoolchain/v3")
-endif()
-
-set(CMAKE_C_COMPILER "${TOOLCHAIN_ROOT}/bin/gcc")
-set(CMAKE_CXX_COMPILER "${TOOLCHAIN_ROOT}/bin/g++")
-set(CMAKE_ASM_COMPILER "${TOOLCHAIN_ROOT}/bin/gcc")
