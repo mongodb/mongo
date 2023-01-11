b66becda76d7542e580fce07d9c07528b7b2d277
Rachita Dhawan <rachita.dhawan@gmail.com>
2023-01-11 17:43:07 +0000

    SERVER-68388 Limit concurrency setting
    (cherry picked from commit 07b44516a04e0d3b3ebadc4fa554142788095bb9)

--- src/mongo/db/s/migration_batch_fetcher_test.cpp
+++ src/mongo/db/s/migration_batch_fetcher_test.cpp
@@ -182,7 +182,7 @@ TEST_F(MigrationBatchFetcherTestFixture, BasicEmptyFetchingTest) {
     int concurrency = 30;
     RAIIServerParameterControllerForTest featureFlagController(
         "featureFlagConcurrencyInChunkMigration", true);
-    RAIIServerParameterControllerForTest setMigrationConcurrencyParam{"migrationConcurrency",
+    RAIIServerParameterControllerForTest setMigrationConcurrencyParam{"chunkMigrationConcurrency",
                                                                       concurrency};
 
     AlternativeClientRegion acr(newClient);
@@ -237,7 +237,7 @@ TEST_F(MigrationBatchFetcherTestFixture, BasicFetching) {
     int concurrency = 30;
     RAIIServerParameterControllerForTest featureFlagController(
         "featureFlagConcurrencyInChunkMigration", true);
-    RAIIServerParameterControllerForTest setMigrationConcurrencyParam{"migrationConcurrency",
+    RAIIServerParameterControllerForTest setMigrationConcurrencyParam{"chunkMigrationConcurrency",
                                                                       concurrency};
 
     auto fetcher = std::make_unique<MigrationBatchFetcher<MigrationBatchMockInserter>>(
