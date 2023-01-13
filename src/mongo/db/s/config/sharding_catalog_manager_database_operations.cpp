25213a5de37ba8238c2a9d35540afb35e83f7de0
Jordi Serra Torrens <jordi.serra-torrens@mongodb.com>
2023-01-13 08:31:41 +0000

    SERVER-72535 Disallow creating the 'admin', 'local', and 'config' databases with alternative casings on sharded clusters
    (cherry picked from commit 93dba70eb3d2f94e8afb076da9482854b97be397)

--- src/mongo/db/s/config/sharding_catalog_manager_database_operations.cpp
+++ src/mongo/db/s/config/sharding_catalog_manager_database_operations.cpp
@@ -94,9 +94,14 @@ DatabaseType ShardingCatalogManager::createDatabase(OperationContext* opCtx,
             dbName.toString(), ShardId::kConfigServerId, true, DatabaseVersion::makeFixed());
     }
 
+    // It is not allowed to create the 'admin' or 'local' databases, including any alternative
+    // casing. It is allowed to create the 'config' database (handled by the early return above),
+    // but only with that exact casing.
     uassert(ErrorCodes::InvalidOptions,
             str::stream() << "Cannot manually create or shard database '" << dbName << "'",
-            dbName != NamespaceString::kAdminDb && dbName != NamespaceString::kLocalDb);
+            !dbName.equalCaseInsensitive(NamespaceString::kAdminDb) &&
+                !dbName.equalCaseInsensitive(NamespaceString::kLocalDb) &&
+                !dbName.equalCaseInsensitive(NamespaceString::kConfigDb));
 
     uassert(ErrorCodes::InvalidNamespace,
             str::stream() << "Invalid db name specified: " << dbName,
