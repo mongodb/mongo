544d9e6c34b76575662d8ab5eec5d5e4247e93f2
Ryan Egesdahl <ryan.egesdahl@mongodb.com>
2023-02-03 20:38:22 +0000

    SERVER-70427 Remove Chef+Kitchen package test infrastructure
    (cherry picked from commit 09ca39216efd0c28c71144d25f55b72a9bba24a4)

--- buildscripts/package_test/rhel-user-data.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sed -i '/^PubkeyAcceptedAlgorithms/s/$/,ssh-rsa/'  /etc/crypto-policies/back-ends/opensshserver.config
-sed -i '/^HostKeyAlgorithms/s/$/,ssh-rsa/'  /etc/crypto-policies/back-ends/opensshserver.config
-update-crypto-policies --set LEGACY
-systemctl restart sshd
-
