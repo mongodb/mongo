cd9db950597ce9ac14f30de12754172fdda289cb
Matt Broadstone <mbroadst@mongodb.com>
2023-02-05 13:23:15 +0000

    SERVER-73585 Exclude multi-tenanted tests in merge passthroughs

--- jstests/core/dbcase.js
+++ jstests/core/dbcase.js
@@ -1,8 +1,8 @@
 // Check db name duplication constraint SERVER-2111
 // @tags: [
-//   # The inject_tenant_prefix override in shard split mode might choose different
+//   # The inject_tenant_prefix override in multi-tenant mode might choose different
 //   # prefixes for each sibling DB in this test.
-//   shard_split_incompatible,
+//   multiple_tenants_incompatible,
 // ]
 
 a = db.getSiblingDB("dbcasetest_dbnamea");
--- jstests/core/dbcase2.js
+++ jstests/core/dbcase2.js
@@ -1,9 +1,9 @@
 // SERVER-2111 Check that an in memory db name will block creation of a db with a similar but
 // differently cased name.
 // @tags: [
-//   # The inject_tenant_prefix override in shard split mode might choose different
+//   # The inject_tenant_prefix override in multi-tenant mode might choose different
 //   # prefixes for each sibling DB in this test.
-//   shard_split_incompatible,
+//   multiple_tenants_incompatible,
 // ]
 
 var dbLowerCase = db.getSiblingDB("dbcase2test_dbnamea");
