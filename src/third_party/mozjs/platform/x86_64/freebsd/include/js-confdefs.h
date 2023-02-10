2a294cfe9492fa44a9fe16a6adf1e945f63c7bcc
Mark Benvenuto <mark.benvenuto@mongodb.com>
2023-02-10 18:47:30 -0500

    SERVER-73904 Update FreeBSD spidermonkey configuration
    (cherry picked from commit 5adf42d89dda28355ba5d60d755c0fbc89073208)

--- src/third_party/mozjs/platform/x86_64/freebsd/include/js-confdefs.h
+++ src/third_party/mozjs/platform/x86_64/freebsd/include/js-confdefs.h
@@ -7,12 +7,11 @@
 #define js_confdefs_h
 
 // Expands to all the defines from configure.
-#define CROSS_COMPILE 
+#define CROSS_COMPILE
 #define ENABLE_SHARED_MEMORY 1
 #define ENABLE_WASM_SIMD 1
 #define ENABLE_WASM_SIMD_WORMHOLE 1
 #define HAVE_64BIT_BUILD 1
-#define HAVE_ALLOCA_H 1
 #define HAVE_BYTESWAP_H 1
 #define HAVE_CLOCK_MONOTONIC 1
 #define HAVE_CPUID_H 1
@@ -25,10 +24,6 @@
 #define HAVE_INTTYPES_H 1
 #define HAVE_LANGINFO_CODESET 1
 #define HAVE_LIBM 1
-#define HAVE_LINUX_IF_ADDR_H 1
-#define HAVE_LINUX_PERF_EVENT_H 1
-#define HAVE_LINUX_QUOTA_H 1
-#define HAVE_LINUX_RTNETLINK_H 1
 #define HAVE_LOCALECONV 1
 #define HAVE_LOCALTIME_R 1
 #define HAVE_MALLOC_H 1
@@ -41,17 +36,15 @@
 #define HAVE_POSIX_FALLOCATE 1
 #define HAVE_POSIX_MEMALIGN 1
 #define HAVE_PTHREAD_GETNAME_NP 1
+#define HAVE_PTHREAD_GET_NAME_NP 1
 #define HAVE_RES_NINIT 1
 #define HAVE_SSIZE_T 1
 #define HAVE_STDINT_H 1
 #define HAVE_STRNDUP 1
 #define HAVE_SYS_MOUNT_H 1
 #define HAVE_SYS_QUEUE_H 1
-#define HAVE_SYS_QUOTA_H 1
-#define HAVE_SYS_STATFS_H 1
 #define HAVE_SYS_STATVFS_H 1
 #define HAVE_SYS_TYPES_H 1
-#define HAVE_SYS_VFS_H 1
 #define HAVE_THREAD_TLS_KEYWORD 1
 #define HAVE_TM_ZONE_TM_GMTOFF 1
 #define HAVE_UNISTD_H 1
@@ -68,7 +61,7 @@
 #define JS_STANDALONE 1
 #define JS_WITHOUT_NSPR 1
 #define MALLOC_H <malloc.h>
-#define MALLOC_USABLE_SIZE_CONST_PTR 
+#define MALLOC_USABLE_SIZE_CONST_PTR const
 #define MOZILLA_UAVERSION "91.0"
 #define MOZILLA_VERSION "91.3.0"
 #define MOZILLA_VERSION_U 91.3.0
@@ -85,8 +78,10 @@
 #define STDC_HEADERS 1
 #define U_STATIC_IMPLEMENTATION 1
 #define VA_COPY va_copy
-#define XP_LINUX 1
+#define XP_FREEBSD 1
 #define XP_UNIX 1
+#define X_DISPLAY_MISSING 1
 #define _REENTRANT 1
+#define _THREAD_SAFE 1
 
 #endif /* js_confdefs_h */
