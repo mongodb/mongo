b1473f653d0bd8f6fd17c6e4cfbe6b291f4bc5b4
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2023-02-16 18:07:03 +0000

    SERVER-74104 Disable journal flusher in validate_out_of_order.js

--- jstests/noPassthrough/validate_out_of_order.js
+++ jstests/noPassthrough/validate_out_of_order.js
@@ -1,9 +1,13 @@
 /**
  * Tests that out-of-order keys are detected by validation during both the collection and index scan
  * phases.
+ *
+ * @tags: [requires_persistence]
  */
 (function() {
 "use strict";
+
+load("jstests/libs/fail_point_util.js");
 
 const rst = new ReplSetTest({nodes: 1});
 rst.startSet();
@@ -16,6 +20,11 @@ assert.commandWorked(coll.createIndex({x: 1}));
 for (let i = 0; i < 5; i++) {
     assert.commandWorked(coll.insert({x: i}));
 }
+
+// Disable the journal flusher for the remainder of the test so that it will not encounter the
+// out-of-order uassert.
+const journalFp = configureFailPoint(primary, "pauseJournalFlusherThread");
+journalFp.wait();
 
 // Test record store out-of-order detection.
 assert.commandWorked(
