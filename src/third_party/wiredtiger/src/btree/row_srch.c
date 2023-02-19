ddff84309d85c5c4031675cc3afcf0f6e1dedb5c
Monica Ng <monica.ng@mongodb.com>
2023-02-19 23:24:26 +0000

    Import wiredtiger: 13d6a243747eae6006d5c0aae618f6a1ed1c59e2 from branch mongodb-master
    ref: 9b8bc17652..13d6a24374
    for: 7.0.0-rc0
    
    WT-10561 - Add standalone test for WT-10461

--- src/third_party/wiredtiger/src/btree/row_srch.c
+++ src/third_party/wiredtiger/src/btree/row_srch.c
@@ -8,6 +8,9 @@
 
 #include "wt_internal.h"
 
+static inline int __validate_next_stack(
+  WT_SESSION_IMPL *session, WT_INSERT *next_stack[WT_SKIP_MAXDEPTH], WT_ITEM *srch_key);
+
 /*
  * __search_insert_append --
  *     Fast append search of a row-store insert list, creating a skiplist stack as we go.
@@ -197,6 +200,85 @@ __wt_search_insert(
     cbt->compare = -cmp;
     cbt->ins = (ins != NULL) ? ins : last_ins;
     cbt->ins_head = ins_head;
+
+    /*
+     * This is an expensive call on a performance-critical path, so we only want to enable it behind
+     * the stress_skiplist session flag.
+     */
+    if (FLD_ISSET(S2C(session)->debug_flags, WT_CONN_DEBUG_STRESS_SKIPLIST))
+        WT_RET(__validate_next_stack(session, cbt->next_stack, srch_key));
+
+    return (0);
+}
+
+/*
+ * __validate_next_stack --
+ *     Verify that for each level in the provided next_stack that higher levels on the stack point
+ *     to larger inserts than lower levels, and all inserts are larger than the srch_key used in
+ *     building the next_stack.
+ */
+static inline int
+__validate_next_stack(
+  WT_SESSION_IMPL *session, WT_INSERT *next_stack[WT_SKIP_MAXDEPTH], WT_ITEM *srch_key)
+{
+
+    WT_ITEM upper_key, lower_key;
+    int32_t i, cmp;
+    WT_COLLATOR *collator;
+
+    /*
+     * Hide the flag check for non-diagnostics builds, too.
+     */
+#ifndef HAVE_DIAGNOSTIC
+    return (0);
+#endif
+
+    collator = S2BT(session)->collator;
+    WT_CLEAR(upper_key);
+    WT_CLEAR(lower_key);
+    cmp = 0;
+
+    for (i = WT_SKIP_MAXDEPTH - 2; i >= 0; i--) {
+
+        /* If lower levels point to the end of the skiplist, higher levels must as well. */
+        if (next_stack[i] == NULL)
+            WT_ASSERT_ALWAYS(session, next_stack[i + 1] == NULL,
+              "Invalid next_stack: Level %d is NULL but higher level %d has pointer %p", i, i + 1,
+              (void *)next_stack[i + 1]);
+
+        /* We only need to compare when both levels point to different, non-NULL inserts. */
+        if (next_stack[i] == NULL || next_stack[i + 1] == NULL ||
+          next_stack[i] == next_stack[i + 1])
+            continue;
+
+        lower_key.data = WT_INSERT_KEY(next_stack[i]);
+        lower_key.size = WT_INSERT_KEY_SIZE(next_stack[i]);
+
+        upper_key.data = WT_INSERT_KEY(next_stack[i + 1]);
+        upper_key.size = WT_INSERT_KEY_SIZE(next_stack[i + 1]);
+
+        WT_RET(__wt_compare(session, collator, &upper_key, &lower_key, &cmp));
+        WT_ASSERT_ALWAYS(session, cmp >= 0,
+          "Invalid next_stack: Lower level points to larger key: Level %d = %s, Level %d = %s", i,
+          (char *)lower_key.data, i + 1, (char *)upper_key.data);
+    }
+
+    if (next_stack[0] != NULL) {
+        /*
+         * Finally, confirm that next_stack[0] is greater than srch_key. We've already confirmed
+         * that all keys on higher levels are larger than next_stack[0] and therefore also larger
+         * than srch_key.
+         */
+        lower_key.data = WT_INSERT_KEY(next_stack[0]);
+        lower_key.size = WT_INSERT_KEY_SIZE(next_stack[0]);
+
+        WT_RET(__wt_compare(session, collator, srch_key, &lower_key, &cmp));
+        WT_ASSERT_ALWAYS(session, cmp < 0,
+          "Invalid next_stack: Search key is larger than keys on next_stack: srch_key = %s, "
+          "next_stack[0] = %s",
+          (char *)srch_key->data, (char *)lower_key.data);
+    }
+
     return (0);
 }
 
