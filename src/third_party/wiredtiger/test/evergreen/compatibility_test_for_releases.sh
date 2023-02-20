9e2eb9d9710bc27affd12941aaf3c07563c3dbc8
Monica Ng <monica.ng@mongodb.com>
2023-02-20 22:56:59 +0000

    Import wiredtiger: 0da47cd80a1ae543d38b52b301e710b4cc38ed2a from branch mongodb-master
    ref: 5c981e88d8..0da47cd80a
    for: 7.0.0-rc0
    
    WT-10594 Disable cursor_reposition in test/format for older releases

--- src/third_party/wiredtiger/test/evergreen/compatibility_test_for_releases.sh
+++ src/third_party/wiredtiger/test/evergreen/compatibility_test_for_releases.sh
@@ -160,6 +160,7 @@ create_configs()
     echo "checkpoints=1"  >> $file_name             # Force periodic writes
     echo "compression=snappy"  >> $file_name        # We only built with snappy, force the choice
     echo "data_source=table" >> $file_name
+    echo "debug.cursor_reposition=0" >> $file_name  # WT-10594 - Not supported by older releases
     echo "debug.log_retention=0" >> $file_name      # WT-10434 - Not supported by older releases
     echo "debug.realloc_malloc=0" >> $file_name     # WT-10111 - Not supported by older releases
     echo "huffman_key=0" >> $file_name              # WT-6893 - Not supported by newer releases
