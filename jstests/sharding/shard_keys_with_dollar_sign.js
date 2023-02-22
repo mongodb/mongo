8f3946c68aa48d85472ff005a4bd03d9df2ba4b9
Cheahuychou Mao <mao.cheahuychou@gmail.com>
2023-02-22 21:35:58 +0000

    SERVER-74124 Disallow shard key pattern with fields that have parts that start with '$'
    (cherry picked from commit 71372698d18785a374aa09bdd646d3c5dc2f227a)

---
+++ jstests/sharding/shard_keys_with_dollar_sign.js
@@ -0,0 +1,57 @@
+/**
+ * Tests that the shardCollection command and reshardCollection command correctly reject a shard key
+ * that has a field name with parts that start with '$'.
+ */
+(function() {
+"use strict";
+
+const st = new ShardingTest({shards: 1});
+
+const dbName = "testDb";
+const ns0 = dbName + ".testColl0";
+const ns1 = dbName + ".testColl1";
+const ns2 = dbName + ".testColl2";
+const db = st.s.getDB(dbName);
+
+function testValidation(key, {isValidIndexKey, isValidShardKey}) {
+    jsTest.log(`Testing ${tojson({key, isValidIndexKey, isValidShardKey})}`);
+    assert.commandWorked(st.s.adminCommand({enableSharding: dbName}));
+    st.ensurePrimaryShard(dbName, st.shard0.name);
+
+    const createIndexRes = db.getCollection(ns0).createIndex(key);
+    if (isValidIndexKey) {
+        assert.commandWorked(createIndexRes);
+    } else {
+        assert.commandFailedWithCode(createIndexRes, ErrorCodes.CannotCreateIndex);
+    }
+
+    const shardCollectionRes = st.s.adminCommand({shardCollection: ns1, key});
+    if (isValidShardKey) {
+        assert.commandWorked(shardCollectionRes);
+    } else {
+        assert.commandFailedWithCode(shardCollectionRes, ErrorCodes.BadValue);
+    }
+
+    assert.commandWorked(st.s.adminCommand({shardCollection: ns2, key: {_id: 1}}));
+    const reshardCollectionRes = st.s.adminCommand({reshardCollection: ns2, key});
+    if (isValidShardKey) {
+        assert.commandWorked(reshardCollectionRes);
+    } else {
+        assert.commandFailedWithCode(reshardCollectionRes, ErrorCodes.BadValue);
+    }
+
+    assert.commandWorked(db.dropDatabase());
+}
+
+testValidation({"$x": 1}, {isValidIndexKey: false, isValidShardKey: false});
+testValidation({"x.$y": 1}, {isValidIndexKey: false, isValidShardKey: false});
+testValidation({"$**": 1}, {isValidIndexKey: true, isValidShardKey: false});
+testValidation({"x.$**": 1}, {isValidIndexKey: true, isValidShardKey: false});
+testValidation({"$": 1}, {isValidIndexKey: false, isValidShardKey: false});
+
+testValidation({"x$": 1}, {isValidIndexKey: true, isValidShardKey: true});
+testValidation({"x$.y": 1}, {isValidIndexKey: true, isValidShardKey: true});
+testValidation({"x.y$": 1}, {isValidIndexKey: true, isValidShardKey: true});
+
+st.stop();
+})();
