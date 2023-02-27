2285ffddafaab361411832956e15c20fd42513b2
Jordi Olivares Provencio <jordi.olivares-provencio@mongodb.com>
2023-02-27 16:37:42 +0000

    SERVER-74206 Use getSiblingDB if getDB doesn't exist

--- jstests/libs/feature_flag_util.js
+++ jstests/libs/feature_flag_util.js
@@ -46,7 +46,13 @@ var FeatureFlagUtil = (function() {
         } catch (err) {
             // Some db-like objects (e.g. ShardingTest.shard0) aren't supported by FixtureHelpers,
             // but we can replace it with an object that should work and try again.
-            setConn(db.getDB(db.defaultDB));
+            if (db.getDB === undefined) {
+                // Some db-like objects (e.g ShardedClusterFixture) have a getSiblingDB method
+                // instead of getDB, use that here to avoid an undefined error.
+                setConn(db.getSiblingDB(db.defaultDB));
+            } else {
+                setConn(db.getDB(db.defaultDB));
+            }
         }
 
         if (user) {
