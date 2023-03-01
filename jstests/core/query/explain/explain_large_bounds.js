11f2e57f4b243bf65bb4f925dd0e8f706481db8e
Enrico Golfieri <enrico.golfieri@mongodb.com>
2023-03-01 14:19:17 +0000

    SERVER-73512 Update the multi_stmt_txn_jscore_passthrough_with_migration suit with random balancer behaviour

--- jstests/core/query/explain/explain_large_bounds.js
+++ jstests/core/query/explain/explain_large_bounds.js
@@ -1,7 +1,13 @@
 /**
  * Make sure explain succeeds even when the index bounds are really big.
  * @tags: [
- *   resource_intensive,
+ * resource_intensive,
+ * # Exclude the test from every suit in which the balancer is enabled. The migrations
+ * # might cause a StaleConfig errors which will be retried for a max number of times before being
+ * # reported to the user and cause the test to fail. It has been observed the StaleConfig error to
+ * # have high chance of occuring. Most likely given by the slow execution time of the explain
+ * # command when the index is large.
+ * assumes_balancer_off,
  * ]
  */
 (function() {
