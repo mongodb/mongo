be760bbf5facb11496202c9a3e32cb69d951ba97
Nicholas Zolnierz <nicholas.zolnierz@mongodb.com>
2023-03-01 12:41:52 +0000

    SERVER-74431 [v4.4] Mark partial_index_logical.js as requires_fcv_44

--- jstests/core/partial_index_logical.js
+++ jstests/core/partial_index_logical.js
@@ -13,6 +13,8 @@
  *  assumes_unsharded_collection,
  *  # Plan cache state is node-local and will not get migrated alongside tenant data.
  *  tenant_migration_incompatible,
+ *  # Sharding support for $planCacheStats requires all nodes to be binary version 4.4.
+ *  requires_fcv_44,
  * ]
  */
 (function() {
