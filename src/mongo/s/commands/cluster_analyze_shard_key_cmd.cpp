8d29060501879098863f926712babfdc77fbbd6f
Cheahuychou Mao <mao.cheahuychou@gmail.com>
2023-03-01 04:36:15 +0000

    SERVER-74416 Make sure analyzeShardKey and configureQueryAnalyzer command use database and shard versioning

--- src/mongo/s/commands/cluster_analyze_shard_key_cmd.cpp
+++ src/mongo/s/commands/cluster_analyze_shard_key_cmd.cpp
@@ -86,8 +86,15 @@ public:
             }
 
             PseudoRandom random{SecureRandom{}.nextInt64()};
-            const auto unversionedCmdObj =
-                CommandHelpers::filterCommandRequestForPassthrough(request().toBSON({}));
+
+            // On secondaries, the database and shard version check is only performed for commands
+            // that specify a readConcern (that is not "available"). So to opt into the check,
+            // explicitly set the readConcern to "local".
+            const auto readConcern =
+                repl::ReadConcernArgs(repl::ReadConcernLevel::kLocalReadConcern);
+            const auto unversionedCmdObj = CommandHelpers::filterCommandRequestForPassthrough(
+                request().toBSON(readConcern.toBSON()));
+
             while (true) {
                 // Select a random shard.
                 invariant(!candidateShardIds.empty());
