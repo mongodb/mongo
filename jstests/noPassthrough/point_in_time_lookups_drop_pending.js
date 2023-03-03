e2f623e87cbfb462d9b826c16fd6e4073f554414
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2023-03-03 20:49:59 +0000

    SERVER-72069 Test instantiating a point-in-time collection using no shared state

---
+++ jstests/noPassthrough/point_in_time_lookups_drop_pending.js
@@ -0,0 +1,60 @@
+/**
+ * Tests the expected point-in-time lookup behaviour when instantiating collections using no shared
+ * state.
+ *
+ * @tags: [
+ *     requires_persistence,
+ *     requires_replication,
+ *     featureFlagPointInTimeCatalogLookups,
+ * ]
+ */
+(function() {
+"use strict";
+
+const rst = new ReplSetTest({
+    nodes: 1,
+    nodeOptions: {
+        setParameter: {
+            // Set the history window to 1 hour to prevent the oldest timestamp from advancing in
+            // order for drop pending tables to stick around.
+            minSnapshotHistoryWindowInSeconds: 60 * 60,
+            logComponentVerbosity: tojson({storage: 1}),
+        }
+    }
+});
+rst.startSet();
+rst.initiate();
+
+const primary = rst.getPrimary();
+
+const dbName = "test";
+const db = primary.getDB(dbName);
+const coll = db.getCollection(jsTestName());
+
+const kNumDocs = 5;
+for (let i = 0; i < kNumDocs; i++) {
+    assert.commandWorked(coll.insert({x: i}));
+}
+
+const createIndexTS = assert.commandWorked(coll.createIndex({x: 1})).operationTime;
+jsTestLog("Create index timestamp: " + tojson(createIndexTS));
+
+const dropTS = assert.commandWorked(db.runCommand({drop: jsTestName()})).operationTime;
+jsTestLog("Drop collection timestamp: " + tojson(dropTS));
+
+// Instantiate a new collection without any shared state.
+assert(checkLog.checkContainsWithCountJson(db, 6825401, {}, 0));
+
+// Test that we can perform a point-in-time read from a drop pending table using an index.
+let res = assert.commandWorked(db.runCommand({
+    find: jsTestName(),
+    hint: {x: 1},
+    readConcern: {level: "snapshot", atClusterTime: createIndexTS}
+}));
+assert.eq(kNumDocs, res.cursor.firstBatch.length);
+
+// Instantiate a new collection without any shared state.
+assert(checkLog.checkContainsWithCountJson(db, 6825401, {}, 1));
+
+rst.stopSet();
+})();
