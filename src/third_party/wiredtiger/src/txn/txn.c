db50695951e89cc6c720687eeb0c451b55c7e3cc
Tammy Bailey <tammy.bailey@mongodb.com>
2023-03-06 00:24:29 +0000

    Import wiredtiger: 1e1ed737431e2299bd7bb234f2a4584a1d7a5ba6 from branch mongodb-master
    ref: d466fba0cd..1e1ed73743
    for: 7.0.0-rc0
    
    WT-10384 Don't allow WT_ROLLBACK for read only operations not in a transaction

--- src/third_party/wiredtiger/src/txn/txn.c
+++ src/third_party/wiredtiger/src/txn/txn.c
@@ -2480,6 +2480,15 @@ __wt_txn_is_blocking(WT_SESSION_IMPL *session)
      */
     if (txn->mod_count == 0 && !__wt_op_timer_fired(session))
         return (0);
+#else
+    /*
+     * Most applications that are not using transactions to read/walk with a cursor cannot handle
+     * having rollback returned nor should the API reset and retry the operation, losing the
+     * cursor's position. Skip the check if there are no updates, the thread operation did not time
+     * out and the operation is not running in a transaction.
+     */
+    if (txn->mod_count == 0 && !__wt_op_timer_fired(session) && !F_ISSET(txn, WT_TXN_RUNNING))
+        return (0);
 #endif
 
     /*
