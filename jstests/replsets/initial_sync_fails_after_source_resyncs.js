dd2220a2224fd855743af88060b0a94e65a378cd
Jiawei Yang <jiawei.yang@mongodb.com>
2023-03-07 16:25:11 +0000

    SERVER-74599 fix shutdown before configure failpoint in initial_sync_fails_after_source_resyncs

--- jstests/replsets/initial_sync_fails_after_source_resyncs.js
+++ jstests/replsets/initial_sync_fails_after_source_resyncs.js
@@ -73,13 +73,16 @@ const res = assert.commandWorked(initialSyncNode.adminCommand({replSetGetStatus:
 
 // The initial sync should have failed.
 assert.eq(res.initialSyncStatus.failedInitialSyncAttempts, 1);
-beforeFinishFailPoint.off();
 
 // Release the initial sync source and sync node oplog fetcher so the test completes.
 assert.commandWorked(initialSyncNodeDb.adminCommand(
     {configureFailPoint: "hangBeforeStartingOplogFetcher", mode: "off"}));
 assert.commandWorked(initialSyncSource.getDB("admin").adminCommand(
     {configureFailPoint: "initialSyncHangBeforeFinish", mode: "off"}));
+
+// We need to turn off the above failpoints before this one, otherwise the server may already have
+// shutdown before we turn the failpoints off, causing the above commands to fail.
+beforeFinishFailPoint.off();
 
 // We want to ensure the initialSyncNode encounters the InitialSyncFailure error and shuts down.
 assert.soon(() => {
