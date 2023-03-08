08d230f3880926ce9d88f555ba8f0b47df6c7379
Tammy Bailey <tammy.bailey@mongodb.com>
2023-03-08 01:58:25 +0000

    Import wiredtiger: 25ba9e9eda009bbd568f2be510e77fee2499bb4d from branch mongodb-master
    ref: b1c657d963..25ba9e9eda
    for: 7.0.0-rc0
    
    WT-10710 Emit macro debug information in diagnostic builds

--- src/third_party/wiredtiger/cmake/configs/base.cmake
+++ src/third_party/wiredtiger/cmake/configs/base.cmake
@@ -375,8 +375,17 @@ if(ENABLE_DEBUG_INFO)
         # Ensure a PDB file can be generated for debugging symbols.
         set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /DEBUG")
     else()
-        add_compile_options(-g)
-        add_compile_options(-ggdb)
+        # Higher debug levels `-g3`/`-ggdb3` emit additional debug information, including 
+        # macro definitions that allow us to evaluate macros such as `p S2C(session)` inside of gdb.
+        # This needs to be in DWARF version 2 format or later - and should be by default - but 
+        # we'll specify version 4 here to be safe.
+        add_compile_options(-g3)
+        add_compile_options(-ggdb3)
+        add_compile_options(-gdwarf-4)
+        if("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang")
+            # Clang requires one additional flag to output macro debug information.
+            add_compile_options(-fdebug-macro)
+        endif()
     endif()
 endif()
 
