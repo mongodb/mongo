d21348f684ad55f39ff1fab4bdfeea4e7b1d3128
Israel Hsu <israel.hsu@mongodb.com>
2023-03-14 14:31:40 +0000

    SERVER-74755 Make QueryAnalysisSampleCounters refresh config atomic with sampler refresh

--- src/mongo/s/query_analysis_sampler.cpp
+++ src/mongo/s/query_analysis_sampler.cpp
@@ -275,8 +275,6 @@ void QueryAnalysisSampler::_refreshConfigurations(OperationContext* opCtx) {
               "response"_attr = response);
     }
 
-    QueryAnalysisSampleCounters::get(opCtx).refreshConfigurations(response.getConfigurations());
-
     stdx::lock_guard<Latch> lk(_mutex);
     std::map<NamespaceString, SampleRateLimiter> sampleRateLimiters;
 
@@ -303,6 +301,8 @@ void QueryAnalysisSampler::_refreshConfigurations(OperationContext* opCtx) {
         }
     }
     _sampleRateLimiters = std::move(sampleRateLimiters);
+
+    QueryAnalysisSampleCounters::get(opCtx).refreshConfigurations(response.getConfigurations());
 }
 
 void QueryAnalysisSampler::_incrementCounters(OperationContext* opCtx,
