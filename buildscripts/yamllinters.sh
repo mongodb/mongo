a135d9aeaa678dd1e9facf1df7830554e752454d
Alexander Neben <alexander.neben@mongodb.com>
2023-03-20 19:22:44 +0000

    SERVER-75034 Deleted evergreen lint todo

--- buildscripts/yamllinters.sh
+++ buildscripts/yamllinters.sh
@@ -4,7 +4,3 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR/../"
 
 yamllint -c etc/yamllint_config.yml buildscripts etc jstests
-
-# TODO: SERVER-64923 re-enable YAML linters.
-#evergreen evaluate ${evergreen_config_file_path} > etc/evaluated_evergreen.yml
-#python -m evergreen_lint -c ./etc/evergreen_lint.yml lint
