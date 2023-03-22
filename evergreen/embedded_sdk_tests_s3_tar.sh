efbf23327c4e3486ed4f84d0b77edfa5fff0b7e7
Alexander Neben <alexander.neben@mongodb.com>
2023-03-22 16:24:14 +0000

    SERVER-75123 Deleted embedded tests

--- evergreen/embedded_sdk_tests_s3_tar.sh
+++
@@ -1,9 +0,0 @@
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
-. "$DIR/prelude.sh"
-
-cd src/build
-
-set -o errexit
-set -o verbose
-
-tar cfvz embedded-sdk-tests.tgz mongo-embedded-sdk-${version}
