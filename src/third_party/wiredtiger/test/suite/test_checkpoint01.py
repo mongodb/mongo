4a9bf1aaf4fcd2c4ceb7a31744d8f93e20d58aee
Andrew Morton <andrew.morton@mongodb.com>
2023-03-30 22:51:15 +0000

    Import wiredtiger: b67dbbcf1facc0f253278b553a5537b92acb1371 from branch mongodb-master
    ref: 0b3626540b..b67dbbcf1f
    for: 7.0.0-rc0
    
    WT-9902 Fix python test test_checkpoint01.test_checkpoint_target with timestamps

--- src/third_party/wiredtiger/test/suite/test_checkpoint01.py
+++ src/third_party/wiredtiger/test/suite/test_checkpoint01.py
@@ -218,9 +218,8 @@ class test_checkpoint_target(wttest.WiredTigerTestCase):
         self.assertEquals(cursor[ds.key(10)], value)
         cursor.close()
 
-    # FIXME-WT-9902
-    @wttest.skip_for_hook("tiered", "strange interaction with tiered and named checkpoints using target")
-    @wttest.skip_for_hook("timestamp", "strange interaction with timestamps and named checkpoints using target")
+    # FIXME-WT-10836
+    # @wttest.skip_for_hook("tiered", "strange interaction with tiered and named checkpoints using target")
     def test_checkpoint_target(self):
         # Create 3 objects, change one record to an easily recognizable string.
         uri = self.uri + '1'
