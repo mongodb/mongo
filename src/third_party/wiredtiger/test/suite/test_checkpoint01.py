ca5b684d63552e6ac3463aa4d1254d61fa0ded92
Clarisse Cheah <clarisse.cheah@mongodb.com>
2023-04-03 00:59:13 +0000

    Import wiredtiger: b1b290cdbc9163a0c844421c14546e9a56c2dff1 from branch mongodb-master
    ref: 7dd95f2b2b..b1b290cdbc
    for: 7.0.0-rc0
    
    WT-10851 Disable tiered hook for test_checkpoint01.py

--- src/third_party/wiredtiger/test/suite/test_checkpoint01.py
+++ src/third_party/wiredtiger/test/suite/test_checkpoint01.py
@@ -219,7 +219,7 @@ class test_checkpoint_target(wttest.WiredTigerTestCase):
         cursor.close()
 
     # FIXME-WT-10836
-    # @wttest.skip_for_hook("tiered", "strange interaction with tiered and named checkpoints using target")
+    @wttest.skip_for_hook("tiered", "strange interaction with tiered and named checkpoints using target")
     def test_checkpoint_target(self):
         # Create 3 objects, change one record to an easily recognizable string.
         uri = self.uri + '1'
