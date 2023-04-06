f14c82b214e42722c4cf944a1405d8298d3fd664
Tural Farhadov <tural.ferhadov@gmail.com>
2023-04-06 22:04:19 +0000

    SERVER-75652: add checksum generation before signing in push task

--- evergreen/garasign_gpg_sign.sh
+++ evergreen/garasign_gpg_sign.sh
@@ -17,6 +17,22 @@ mv mh.tgz mh-${push_name}-${push_arch}-${suffix}.${ext} || true
 mv mongo-debugsymbols.tgz mongodb-${push_name}-${push_arch}-debugsymbols-${suffix}.${ext} || true
 mv distsrc.${ext} mongodb-src-${src_suffix}.${long_ext} || true
 
+# generating checksums
+function gen_checksums() {
+  if [ -e $1 ]; then
+    shasum -a 1 $1 | tee $1.sha1
+    shasum -a 256 $1 | tee $1.sha256
+    md5sum $1 | tee $1.md5
+  else
+    echo "$1 does not exist. Skipping checksum generation"
+  fi
+}
+
+gen_checksums mongodb-$push_name-$push_arch-$suffix.$ext
+gen_checksums mongodb-$push_name-$push_arch-debugsymbols-$suffix.$ext
+gen_checksums mongodb-src-$src_suffix.$long_ext
+gen_checksums mongodb-cryptd-$push_name-$push_arch-$suffix.$ext
+
 # signing linux artifacts with gpg
 cat << 'EOF' > gpg_signing_commands.sh
 gpgloader # loading gpg keys.
