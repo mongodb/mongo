988d132f379634c14635b429bb06a9d466744afc
Marc Butler <135883+marcbutler@users.noreply.github.com>
2023-04-06 00:38:14 +0000

    Import wiredtiger: 301819539a5de604857534c0a73b0cc8a268ad88 from branch mongodb-master
    ref: 031230ed4e..301819539a
    for: 7.0.0-rc0
    
    WT-10880 Print the HS key in a human readable format in HS validation

--- src/third_party/wiredtiger/src/history/hs_verify.c
+++ src/third_party/wiredtiger/src/history/hs_verify.c
@@ -73,9 +73,10 @@ __hs_verify_id(
 
         if (ds_cbt->compare != 0) {
             F_SET(S2C(session), WT_CONN_DATA_CORRUPTION);
+            /* Note that we are reformatting the HS key here. */
             WT_ERR_PANIC(session, WT_PANIC,
               "the associated history store key %s was not found in the data store %s",
-              __wt_buf_set_printable(session, key.data, key.size, false, prev_key),
+              __wt_key_string(session, key.data, key.size, CUR2BT(ds_cbt)->key_format, &key),
               session->dhandle->name);
         }
 
