0d27f0404b7ee96b24cd9a0c53adbfae171d0868
Mark Benvenuto <mark.benvenuto@mongodb.com>
2023-04-06 14:40:37 -0400

    SERVER-75795 Update aws_e2e_ec2.js for evergreen changes to AWS test identity

--- evergreen/external_auth_aws_setup.sh
+++ evergreen/external_auth_aws_setup.sh
@@ -17,10 +17,6 @@ cat << EOF > aws_e2e_setup.json
 
     "iam_auth_assume_aws_account" : "${iam_auth_assume_aws_account}",
     "iam_auth_assume_aws_secret_access_key" : "${iam_auth_assume_aws_secret_access_key}",
-    "iam_auth_assume_role_name" : "${iam_auth_assume_role_name}",
-
-    "iam_auth_ec2_instance_account" : "${iam_auth_ec2_instance_account}",
-    "iam_auth_ec2_instance_secret_access_key" : "${iam_auth_ec2_instance_secret_access_key}",
-    "iam_auth_ec2_instance_profile" : "${iam_auth_ec2_instance_profile}"
+    "iam_auth_assume_role_name" : "${iam_auth_assume_role_name}"
 }
 EOF
