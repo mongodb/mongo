b943337be56e74f143afbb85075f93afe3979d12
Cheahuychou Mao <mao.cheahuychou@gmail.com>
2023-04-07 14:43:14 +0000

    SERVER-75826 Add the error codes that  would throw when WT random cursor fails to find a non-duplicate document to list of expected errors in the analyzeShardKey hook

--- jstests/hooks/run_analyze_shard_key_background.js
+++ jstests/hooks/run_analyze_shard_key_background.js
@@ -196,6 +196,14 @@ function analyzeShardKey(ns, shardKey, indexKey) {
             tojsononeline(res)}`);
         return res;
     }
+    if (res.code == 28799 || res.code == 4952606) {
+        // (WT-8003) 28799 is the error that $sample throws when it fails to find a
+        // non-duplicate document using a random cursor. 4952606 is the error that the sampling
+        // based split policy throws if it fails to find the specified number of split points.
+        print(`Failed to analyze the shard key due to duplicate keys returned by random cursor ${
+            tojsononeline(res)}`);
+        return res;
+    }
 
     assert.commandWorked(res);
     jsTest.log(`Finished analyzing the shard key: ${tojsononeline(res)}`);
