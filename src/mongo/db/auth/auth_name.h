a759429a26c33a176a7b85c2e240934d471f7c6e
Sara Golemon <sara.golemon@mongodb.com>
2023-04-20 16:07:36 -0500

    SERVER-76378 Estimate metadata size

--- src/mongo/db/auth/auth_name.h
+++ src/mongo/db/auth/auth_name.h
@@ -96,6 +96,8 @@ public:
     void appendToBSON(BSONObjBuilder* bob, bool encodeTenant = false) const;
     BSONObj toBSON(bool encodeTenant = false) const;
 
+    std::size_t getBSONObjSize() const;
+
     /**
      * Gets the name part of a AuthName.
      */
