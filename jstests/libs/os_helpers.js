f8d9519f2178be5f7c53ddcaf26f092b6fb5b580
Mark Benvenuto <mark.benvenuto@mongodb.com>
2023-04-28 14:28:16 -0400

    SERVER-75989 Add support for OpenSSL 3.0 FIPS
    (cherry picked from commit 376eb991dcc36b8a6f0d8ed698693fca8918b4f3)

--- jstests/libs/os_helpers.js
+++ jstests/libs/os_helpers.js
@@ -6,19 +6,89 @@ function isLinux() {
     return getBuildInfo().buildEnvironment.target_os == "linux";
 }
 
-function isRHEL8() {
-    if (_isWindows()) {
-        return false;
+// See "man 5 os-release" for documentation
+function readOsRelease() {
+    try {
+        const os_release = cat("/etc/os-release");
+
+        let lines = os_release.split("\n");
+
+        let tags = {};
+
+        for (let line of lines) {
+            let vp = line.replaceAll("\"", "").split("=");
+            tags[vp[0]] = vp[1];
+        }
+
+        return tags;
+    } catch {
+        // ignore
     }
 
+    assert(!isLinux(), "Linux hosts should always have /etc/os-release.");
+
+    return {};
+}
+
+/**
+ * Check if Linux OS is given identifier. Identifiers are always lower case strings.
+ *
+ * @param {string} distro ID of the distro in os-release
+ * @returns
+ */
+function isDistro(distro) {
+    let tags = readOsRelease();
+    return tags.hasOwnProperty("ID") && tags["ID"] === distro;
+}
+
+/**
+ * Check if Linux OS is given identifier and specific version. Do not use for matching major
+ * versions like RHEL 8, isRHELMajorVerison.
+ *
+ * @param {string} distro ID of the distro in os-release
+ * @returns
+ */
+function isDistroVersion(distro, version) {
+    let tags = readOsRelease();
+    return tags.hasOwnProperty("ID") && tags["ID"] === distro &&
+        tags.hasOwnProperty("VERSION_ID") && tags["VERSION_ID"] === version;
+}
+
+/**
+ * Is it RHEL and is it 7, 8 or 9?
+ * @param {string} majorVersion
+ * @returns True if majorVersion = 8 and version is 8.1, 8.2 etc.
+ */
+function isRHELMajorVerison(majorVersion) {
+    let tags = readOsRelease();
+    return tags.hasOwnProperty("ID") && tags["ID"] === "rhel" &&
+        tags.hasOwnProperty("VERSION_ID") && tags["VERSION_ID"].startsWith(majorVersion);
+}
+
+/**
+ * Example
+NAME="Red Hat Enterprise Linux"
+VERSION="8.7 (Ootpa)"
+ID="rhel"
+ID_LIKE="fedora"
+VERSION_ID="8.7"
+PLATFORM_ID="platform:el8"
+PRETTY_NAME="Red Hat Enterprise Linux 8.7 (Ootpa)"
+ANSI_COLOR="0;31"
+CPE_NAME="cpe:/o:redhat:enterprise_linux:8::baseos"
+HOME_URL="https://www.redhat.com/"
+DOCUMENTATION_URL="https://access.redhat.com/documentation/red_hat_enterprise_linux/8/"
+BUG_REPORT_URL="https://bugzilla.redhat.com/"
+
+REDHAT_BUGZILLA_PRODUCT="Red Hat Enterprise Linux 8"
+REDHAT_BUGZILLA_PRODUCT_VERSION=8.7
+REDHAT_SUPPORT_PRODUCT="Red Hat Enterprise Linux"
+REDHAT_SUPPORT_PRODUCT_VERSION="8.7"
+ */
+function isRHEL8() {
     // RHEL 8 disables TLS 1.0 and TLS 1.1 as part their default crypto policy
     // We skip tests on RHEL 8 that require these versions as a result.
-    const grep_result = runProgram('grep', 'Ootpa', '/etc/redhat-release');
-    if (grep_result == 0) {
-        return true;
-    }
-
-    return false;
+    return isRHELMajorVerison("8");
 }
 
 function isSUSE15SP1() {
@@ -38,69 +108,100 @@ function isSUSE15SP1() {
 }
 
 function isUbuntu() {
-    if (_isWindows()) {
-        return false;
-    }
-
     // Ubuntu 18.04 and later compiles openldap against gnutls which does not
     // support SHA1 signed certificates. ldaptest.10gen.cc uses a SHA1 cert.
-    const grep_result = runProgram('grep', 'ID=ubuntu', '/etc/os-release');
-    if (grep_result == 0) {
-        return true;
-    }
-
-    return false;
+    return isDistro("ubuntu");
 }
 
+/**
+ * Example:
+NAME="Ubuntu"
+VERSION="18.04.6 LTS (Bionic Beaver)"
+ID=ubuntu
+ID_LIKE=debian
+PRETTY_NAME="Ubuntu 18.04.6 LTS"
+VERSION_ID="18.04"
+HOME_URL="https://www.ubuntu.com/"
+SUPPORT_URL="https://help.ubuntu.com/"
+BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
+PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
+VERSION_CODENAME=bionic
+UBUNTU_CODENAME=bionic
+ */
 function isUbuntu1804() {
-    if (_isWindows()) {
-        return false;
-    }
-
     // Ubuntu 18.04's TLS 1.3 implementation has an issue with OCSP stapling. We have disabled
     // stapling on this build variant, so we need to ensure that tests that require stapling
     // do not run on this machine.
-    const grep_result = runProgram('grep', 'bionic', '/etc/os-release');
-    if (grep_result === 0) {
-        return true;
-    }
-
-    return false;
+    return isDistroVersion("ubuntu", "18.04");
 }
 
 function isUbuntu2004() {
-    if (_isWindows()) {
-        return false;
-    }
-
     // Ubuntu 20.04 disables TLS 1.0 and TLS 1.1 as part their default crypto policy
     // We skip tests on Ubuntu 20.04 that require these versions as a result.
-    const grep_result = runProgram('grep', 'focal', '/etc/os-release');
-    if (grep_result == 0) {
-        return true;
-    }
-
-    return false;
+    return isDistroVersion("ubuntu", "20.04");
 }
 
-function isDebian10() {
-    if (_isWindows()) {
-        return false;
-    }
+/**
+ * Example:
+PRETTY_NAME="Debian GNU/Linux 12 (bookworm)"
+NAME="Debian GNU/Linux"
+VERSION_ID="12"
+VERSION="12 (bookworm)"
+VERSION_CODENAME=bookworm
+ID=debian
+HOME_URL="https://www.debian.org/"
+SUPPORT_URL="https://www.debian.org/support"
+BUG_REPORT_URL="https://bugs.debian.org/"
+ */
+function isDebian() {
+    return isDistro("debian");
+}
 
-    // Debian 10 disables TLS 1.0 and TLS 1.1 as part their default crypto policy
-    // We skip tests on Debian 10 that require these versions as a result.
-    try {
-        // this file exists on systemd-based systems, necessary to avoid mischaracterizing debian
-        // derivatives as stock debian
-        const releaseFile = cat("/etc/os-release").toLowerCase();
-        const prettyName = releaseFile.split('\n').find(function(line) {
-            return line.startsWith("pretty_name");
-        });
-        return prettyName.includes("debian") &&
-            (prettyName.includes("10") || prettyName.includes("buster") ||
-             prettyName.includes("bullseye"));
-    } catch (e) {
-        return false;
-    }
+/**
+ * Example:
+NAME="Fedora Linux"
+VERSION="38 (Workstation Edition)"
+ID=fedora
+VERSION_ID=38
+VERSION_CODENAME=""
+PLATFORM_ID="platform:f38"
+PRETTY_NAME="Fedora Linux 38 (Workstation Edition)"
+ANSI_COLOR="0;38;2;60;110;180"
+LOGO=fedora-logo-icon
+CPE_NAME="cpe:/o:fedoraproject:fedora:38"
+DEFAULT_HOSTNAME="fedora"
+HOME_URL="https://fedoraproject.org/"
+DOCUMENTATION_URL="https://docs.fedoraproject.org/en-US/fedora/f38/system-administrators-guide/"
+SUPPORT_URL="https://ask.fedoraproject.org/"
+BUG_REPORT_URL="https://bugzilla.redhat.com/"
+REDHAT_BUGZILLA_PRODUCT="Fedora"
+REDHAT_BUGZILLA_PRODUCT_VERSION=38
+REDHAT_SUPPORT_PRODUCT="Fedora"
+REDHAT_SUPPORT_PRODUCT_VERSION=38
+SUPPORT_END=2024-05-14
+VARIANT="Workstation Edition"
+VARIANT_ID=workstation
+ */
+function isFedora() {
+    return isDistro("fedora");
+}
+
+/**
+ * Note: Amazon 2022 was never released for production. It became Amazon 2023.
+ *
+ * Example:
+NAME="Amazon Linux"
+VERSION="2022"
+ID="amzn"
+ID_LIKE="fedora"
+VERSION_ID="2022"
+PLATFORM_ID="platform:al2022"
+PRETTY_NAME="Amazon Linux 2022"
+ANSI_COLOR="0;33"
+CPE_NAME="cpe:2.3:o:amazon:amazon_linux:2022"
+HOME_URL="https://aws.amazon.com/linux/"
+BUG_REPORT_URL="https://github.com/amazonlinux/amazon-linux-2022"
+*/
+function isAmazon2023() {
+    return isDistroVersion("amzn", "2022") || isDistroVersion("amzn", "2023");
 }
--- src/mongo/util/net/openssl_init.cpp
+++ src/mongo/util/net/openssl_init.cpp
@@ -45,6 +45,10 @@
 #include <stack>
 #include <vector>
 
+#if OPENSSL_VERSION_NUMBER > 0x30000000L
+#include <openssl/provider.h>
+#endif
+
 #define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kNetwork
 
 
@@ -146,21 +150,54 @@ private:
     }
 };
 
-void setupFIPS() {
-// Turn on FIPS mode if requested, OPENSSL_FIPS must be defined by the OpenSSL headers
-#if defined(MONGO_CONFIG_HAVE_FIPS_MODE_SET)
+#if OPENSSL_VERSION_NUMBER > 0x30000000L
+#define _SUPPORT_FIPS 1
+
+OSSL_PROVIDER* fipsProvider;
+OSSL_PROVIDER* baseProvider;
+
+void initFIPS() {
+    // OpenSSL 3 has a different FIPS design then previous OpenSSL. To load FIPS, we use the FIPS
+    // algorithm provider which we load into the "default" library context.
+    fipsProvider = OSSL_PROVIDER_load(NULL, "fips");
+    if (fipsProvider == NULL) {
+        LOGV2_FATAL_NOTRACE(
+            7585801,
+            "Failed to load OpenSSL 3 FIPS provider. OpenSSL was not compiled with FIPS support.",
+            "error"_attr = SSLManagerInterface::getSSLErrorMessage(ERR_get_error()));
+    }
+
+    // Base provide has non-cryptographic algorihms (like encoding/decoding keys)
+    baseProvider = OSSL_PROVIDER_load(NULL, "base");
+    if (baseProvider == NULL) {
+        LOGV2_FATAL_NOTRACE(7585802,
+                            "Failed to load OpenSSL 3 Base provider",
+                            "error"_attr =
+                                SSLManagerInterface::getSSLErrorMessage(ERR_get_error()));
+    }
+}
+#elif defined(MONGO_CONFIG_HAVE_FIPS_MODE_SET)
+
+#define _SUPPORT_FIPS 1
+
+void initFIPS() {
     int status = FIPS_mode_set(1);
     if (!status) {
-        LOGV2_FATAL(23173,
-                    "can't activate FIPS mode: {error}",
-                    "Can't activate FIPS mode",
-                    "error"_attr = SSLManagerInterface::getSSLErrorMessage(ERR_get_error()));
-        fassertFailedNoTrace(16703);
+        LOGV2_FATAL_NOTRACE(23173,
+                            "Can't activate FIPS mode",
+                            "error"_attr =
+                                SSLManagerInterface::getSSLErrorMessage(ERR_get_error()));
     }
+}
+#endif
+
+void setupFIPS() {
+// Turn on FIPS mode if requested, OPENSSL_FIPS must be defined by the OpenSSL headers
+#if defined(_SUPPORT_FIPS)
+    initFIPS();
     LOGV2(23172, "FIPS 140-2 mode activated");
 #else
-    LOGV2_FATAL(23174, "this version of mongodb was not compiled with FIPS support");
-    fassertFailedNoTrace(17089);
+    LOGV2_FATAL_NOTRACE(23174, "this version of mongodb was not compiled with FIPS support");
 #endif
 }
 
