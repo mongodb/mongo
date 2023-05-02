58406809e382b066db143dd40abb0fe7d74d2bc1
Juan Gu <juan.gu@mongodb.com>
2023-05-02 17:13:57 +0000

    Revert "SERVER-76690 Special handling for PYTHONPATH when activate venv"
    This reverts commit d35ce8621f542a4479eb5cd91a8da1d39dd62c3d.

--- evergreen/prelude_venv.sh
+++ evergreen/prelude_venv.sh
@@ -23,18 +23,7 @@ function activate_venv {
   if [ "Windows_NT" = "$OS" ]; then
     export PYTHONPATH="$PYTHONPATH;$(cygpath -w ${workdir}/src)"
   else
-    python_version=$($python -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')
-    site_packages="${workdir}/venv/lib/python${python_version}/site-packages"
-    python -c "import sys; print(sys.path)"
-
-    # Check if site_packages is already in sys.path
-    in_sys_path=$($python -c "import sys; print('$site_packages' in sys.path)")
-    if [ "$in_sys_path" = "False" ]; then
-      export PYTHONPATH="${site_packages}:${PYTHONPATH}:${workdir}/src"
-    else
-      export PYTHONPATH="$PYTHONPATH:${workdir}/src"
-    fi
-    python -c "import sys; print(sys.path)"
+    export PYTHONPATH="$PYTHONPATH:${workdir}/src"
   fi
 
   echo "python set to $(which $python)"
