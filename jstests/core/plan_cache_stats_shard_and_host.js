b0d9762b40aa6d3557339561bb2ceac4f1c0295f
Adityavardhan Agrawal <adi.agrawal@mongodb.com>
2023-05-12 22:36:07 +0000

    SERVER-77033 Tag plan_cache_stats_shard_and_host.js with requires_fcv

--- jstests/core/plan_cache_stats_shard_and_host.js
+++ jstests/core/plan_cache_stats_shard_and_host.js
@@ -5,7 +5,8 @@
 //   assumes_balancer_off,
 //   assumes_read_concern_unchanged,
 //   assumes_read_preference_unchanged,
-//   does_not_support_stepdowns
+//   does_not_support_stepdowns,
+//   requires_fcv_60,
 // ]
 (function() {
 "use strict";
