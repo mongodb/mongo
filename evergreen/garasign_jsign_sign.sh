fffec156278087b44b7817587d3cb62c382e9292
Tural Farhadov <tural.ferhadov@gmail.com>
2023-05-23 18:34:46 +0000

    SERVER-77383: generate checksums after signing MSIs
    (cherry picked from commit 97683bf244362678d65db79b418d7f6cb5032d66)

--- evergreen/garasign_jsign_sign.sh
+++ evergreen/garasign_jsign_sign.sh
@@ -6,15 +6,6 @@ cd src
 msi_filename=mongodb-${push_name}-${push_arch}-${suffix}.msi
 /usr/bin/find build/ -type f | grep msi$ | xargs -I original_filename cp original_filename $msi_filename || true
 
-# generating checksums
-if [ -e $msi_filename ]; then
-  shasum -a 1 $msi_filename | tee $msi_filename.sha1
-  shasum -a 256 $msi_filename | tee $msi_filename.sha256
-  md5sum $msi_filename | tee $msi_filename.md5
-else
-  echo "$msi_filename does not exist. Skipping checksum generation"
-fi
-
 # signing windows artifacts with jsign
 cat << 'EOF' > jsign_signing_commands.sh
 function sign(){
@@ -37,3 +28,12 @@ podman run \
   -v $(pwd):$(pwd) -w $(pwd) \
   ${garasign_jsign_image} \
   /bin/bash -c "$(cat ./jsign_signing_commands.sh)"
+
+# generating checksums
+if [ -e $msi_filename ]; then
+  shasum -a 1 $msi_filename | tee $msi_filename.sha1
+  shasum -a 256 $msi_filename | tee $msi_filename.sha256
+  md5sum $msi_filename | tee $msi_filename.md5
+else
+  echo "$msi_filename does not exist. Skipping checksum generation"
+fi
