d18d730001a818ce2bc0d8b6d0cfdd1074aa34b1
Randolph Tan <randolph@10gen.com>
2023-05-24 15:38:14 +0000

    SERVER-77426 Install replset change listener after shard registry is initialized

--- src/mongo/db/s/sharding_initialization_mongod.cpp
+++ src/mongo/db/s/sharding_initialization_mongod.cpp
@@ -570,8 +570,6 @@ void initializeGlobalShardingStateForConfigServerIfNeeded(OperationContext* opCt
 
     const auto service = opCtx->getServiceContext();
 
-    ShardingInitializationMongoD::get(opCtx)->installReplicaSetChangeListener(service);
-
     auto configCS = []() -> boost::optional<ConnectionString> {
         if (gFeatureFlagCatalogShard.isEnabledAndIgnoreFCVUnsafeAtStartup()) {
             // When the config server can operate as a shard, it sets up a ShardRemote for the
@@ -600,6 +598,8 @@ void initializeGlobalShardingStateForConfigServerIfNeeded(OperationContext* opCt
     }
 
     initializeGlobalShardingStateForMongoD(opCtx, configCS);
+
+    ShardingInitializationMongoD::get(opCtx)->installReplicaSetChangeListener(service);
 
     // ShardLocal to use for explicitly local commands on the config server.
     auto localConfigShard = Grid::get(opCtx)->shardRegistry()->createLocalConfigShard();
@@ -706,8 +706,6 @@ void ShardingInitializationMongoD::_initializeShardingEnvironmentOnShardServer(
     OperationContext* opCtx, const ShardIdentity& shardIdentity) {
     auto const service = opCtx->getServiceContext();
 
-    installReplicaSetChangeListener(service);
-
     // Determine primary/secondary/standalone state in order to properly initialize sharding
     // components.
     const auto replCoord = repl::ReplicationCoordinator::get(opCtx);
@@ -727,6 +725,24 @@ void ShardingInitializationMongoD::_initializeShardingEnvironmentOnShardServer(
 
         initializeGlobalShardingStateForMongoD(opCtx,
                                                {shardIdentity.getConfigsvrConnectionString()});
+
+        installReplicaSetChangeListener(service);
+
+        // Reset the shard register config connection string in case it missed the replica set
+        // monitor notification. Config server does not need to do this since it gets the connection
+        // string directly from the replication coordinator.
+        auto configShardConnStr = Grid::get(opCtx->getServiceContext())
+                                      ->shardRegistry()
+                                      ->getConfigServerConnectionString();
+        if (configShardConnStr.type() == ConnectionString::ConnectionType::kReplicaSet) {
+            ConnectionString rsMonitorConfigConnStr(
+                ReplicaSetMonitor::get(configShardConnStr.getSetName())->getServerAddress(),
+                ConnectionString::ConnectionType::kReplicaSet);
+            Grid::get(opCtx->getServiceContext())
+                ->shardRegistry()
+                ->updateReplSetHosts(rsMonitorConfigConnStr,
+                                     ShardRegistry::ConnectionStringUpdateType::kConfirmed);
+        }
 
         CatalogCacheLoader::get(opCtx).initializeReplicaSetRole(isStandaloneOrPrimary);
 
