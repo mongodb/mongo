1770cbb6cfeca3ca11821d0ecb4e64846ef2e83c
Trevor Guidry <trevor.guidry@mongodb.com>
2023-05-26 21:53:05 +0000

    SERVER-77356 [v4.4] add ubuntu22 variants

--- src/third_party/boost-1.70.0/boost/thread/pthread/thread_data.hpp
+++ src/third_party/boost-1.70.0/boost/thread/pthread/thread_data.hpp
@@ -57,8 +57,8 @@ namespace boost
 #else
           std::size_t page_size = ::sysconf( _SC_PAGESIZE);
 #endif
-#if PTHREAD_STACK_MIN > 0
-          if (size<PTHREAD_STACK_MIN) size=PTHREAD_STACK_MIN;
+#ifdef PTHREAD_STACK_MIN
+          if (size<static_cast<std::size_t>(PTHREAD_STACK_MIN)) size=PTHREAD_STACK_MIN;
 #endif
           size = ((size+page_size-1)/page_size)*page_size;
           int res = pthread_attr_setstacksize(&val_, size);
