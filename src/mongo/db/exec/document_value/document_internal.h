ae410caffdd77e58bb167c0089e5f2270d71193a
Dan Larkin-York <dan.larkin-york@mongodb.com>
2023-06-01 18:02:17 +0000

    SERVER-77363 Use absl::Hash for FieldNameHasher
    (cherry picked from commit 679baecbcfdcd3cd88a01133fc121477805d691f)

--- src/mongo/db/exec/document_value/document_internal.h
+++ src/mongo/db/exec/document_value/document_internal.h
@@ -29,7 +29,6 @@
 
 #pragma once
 
-#include <MurmurHash3.h>
 #include <bitset>
 #include <boost/intrusive_ptr.hpp>
 
@@ -329,10 +328,8 @@ struct FieldNameHasher {
     using is_transparent = void;
 
     std::size_t operator()(StringData sd) const {
-        // TODO consider FNV-1a once we have a better benchmark corpus
-        unsigned out;
-        MurmurHash3_x86_32(sd.rawData(), sd.size(), 0, &out);
-        return out;
+        // Use the default absl string hasher.
+        return absl::Hash<absl::string_view>{}(absl::string_view(sd.rawData(), sd.size()));
     }
 
     std::size_t operator()(const std::string& s) const {
