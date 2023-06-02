f57d0405c3e11b51198cfe223b338903428cb324
Shin Yee Tan <shinyee.tan@mongodb.com>
2023-06-02 20:41:59 +0000

    Revert "SERVER-76051 enable throughputProbing in config fuzzer"
    This reverts commit a2d6eca38ea8b02702d1fe9b559a51cb321f8a29.

--- buildscripts/resmokelib/mongo_fuzzer_configs.py
+++ buildscripts/resmokelib/mongo_fuzzer_configs.py
@@ -134,8 +134,7 @@ def generate_mongod_parameters(rng, mode):
     ret["syncdelay"] = rng.choice([60, rng.randint(15, 180)])
     ret["wiredTigerCursorCacheSize"] = rng.randint(-100, 100)
     ret["wiredTigerSessionCloseIdleTimeSecs"] = rng.randint(0, 300)
-    ret["storageEngineConcurrencyAdjustmentAlgorithm"] = rng.choice(
-        ["throughputProbing", "fixedConcurrentTransactions"], p=[0.9, 0.1])
+    ret["storageEngineConcurrencyAdjustmentAlgorithm"] = "fixedConcurrentTransactions"
     if rng.choice(3 * [True] + [False]):
         # The old retryable writes format is used by other variants. Weight towards turning on the
         # new retryable writes format on in this one.
