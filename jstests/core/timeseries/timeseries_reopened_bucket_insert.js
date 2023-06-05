6513251719c77e09a1bb0a6b8c9a771034f65db6
Gregory Wlodarek <gregory.wlodarek@mongodb.com>
2023-06-05 15:10:01 +0000

    SERVER-77795 Fix featureFlagTimeseriesAlwaysUseCompressedBuckets usage in timeseries_reopened_bucket_insert.js

--- jstests/core/timeseries/timeseries_reopened_bucket_insert.js
+++ jstests/core/timeseries/timeseries_reopened_bucket_insert.js
@@ -218,7 +218,7 @@ const expectToReopenArchivedBuckets = function() {
 }();
 
 const expectToReopenCompressedBuckets = function() {
-    if (!FeatureFlagUtil.isEnabled(db, "TimeseriesAlwaysUseCompressedBuckets")) {
+    if (!FeatureFlagUtil.isPresentAndEnabled(db, "TimeseriesAlwaysUseCompressedBuckets")) {
         return;
     }
 
