876f89082ce4115d3f391d3c4bee20f4ed6432e5
Max Hirschhorn <max.hirschhorn@mongodb.com>
2023-06-22 14:46:25 +0000

    Revert "SERVER-73509 timeseries_lastpoint_top.js is no longer working"
    This reverts commit 7643cfe957bf85e05c023d67390dff1899bf2390.

--- jstests/core/timeseries/timeseries_lastpoint_top.js
+++ jstests/core/timeseries/timeseries_lastpoint_top.js
@@ -13,8 +13,6 @@
  *   does_not_support_stepdowns,
  *   # We need a timeseries collection.
  *   requires_timeseries,
- *   # Implicit index creation may change the plan/engine used.
- *   assumes_no_implicit_index_creation,
  * ]
  */
 (function() {
@@ -28,6 +26,12 @@ load("jstests/libs/feature_flag_util.js");
 
 const testDB = TimeseriesAggTests.getTestDb();
 assert.commandWorked(testDB.dropDatabase());
+
+// TODO SERVER-73509 The test doesn't work yet, even though this feature flag is gone.
+if (true /* previously guarded by featureFlagLastPointQuery */) {
+    jsTestLog("Skipping the test.");
+    return;
+}
 
 /**
  * Returns a lastpoint $group stage of the form:
