58a1441efb124ccca00022d923b15ed7736defba
Rui Liu <lriuui0x0@gmail.com>
2023-06-22 17:09:10 +0000

    SERVER-78274 Fix invariance failure caused by rehydrating unexpanded wildcard index pattern
    (cherry picked from commit 5c7694b7afd519eaa01b2f14292d49e90a3a1a58)

---
+++ jstests/core/index/wildcard/compound_wildcard_index_unbounded.js
@@ -0,0 +1,54 @@
+/**
+ * Tests compound wildcard index with unbounded scans including multikey metadata entries doesn't
+ * cause any errors.
+ *
+ * @tags: [
+ *   featureFlagCompoundWildcardIndexes,
+ *   requires_fcv_70,
+ * ]
+ */
+(function() {
+"use strict";
+
+load("jstests/libs/analyze_plan.js");
+
+const coll = db.compound_wildcard_index_unbounded;
+coll.drop();
+const keyPattern = {
+    a: 1,
+    "$**": 1
+};
+const keyProjection = {
+    wildcardProjection: {a: 0}
+};
+assert.commandWorked(coll.createIndex(keyPattern, keyProjection));
+assert.commandWorked(coll.insert({a: 1, b: 1}));
+// Add an array field in order to make wildcard insert a multikey path metadata entry.
+assert.commandWorked(coll.insert({b: [1, 2]}));
+
+const query = {
+    a: {$exists: true}
+};
+const explain = coll.find(query).hint(keyPattern).explain('executionStats');
+const plan = getWinningPlan(explain.queryPlanner);
+const ixscans = getPlanStages(plan, "IXSCAN");
+// Asserting that we have unbounded index scans on $_path so that multikey metadata will also be
+// included in the scan.
+assert.gt(ixscans.length, 0, explain);
+ixscans.forEach(ixscan => {
+    assert.eq({a: 1, $_path: 1}, ixscan.keyPattern, explain);
+    assert.eq({a: ["[MinKey, MaxKey]"], $_path: ["[MinKey, MaxKey]"]}, ixscan.indexBounds, explain);
+});
+
+// TODO SERVER-78307: Fix the erroneous index corruption result.
+const assertIndexCorruption = (executionStats) => {
+    if (typeof executionStats === 'object') {
+        if ("executionSuccess" in executionStats) {
+            assert.eq(false, executionStats.executionSuccess, explain);
+            assert.eq(ErrorCodes.DataCorruptionDetected, executionStats.errorCode, explain);
+        }
+    } else if (Array.isArray(executionStats)) {
+        executionStats.forEach(stats => assertIndexCorruption(stats));
+    }
+};
+})();
--- src/mongo/db/query/sbe_stage_builder_index_scan.cpp
+++ src/mongo/db/query/sbe_stage_builder_index_scan.cpp
@@ -627,7 +627,7 @@ std::pair<std::unique_ptr<sbe::PlanStage>, PlanStageSlots> generateIndexScan(
                                      accessMethod->getSortedDataInterface()->getKeyStringVersion(),
                                      accessMethod->getSortedDataInterface()->getOrdering());
 
-    auto keyPattern = descriptor->keyPattern();
+    auto keyPattern = ixn->index.keyPattern;
 
     // Determine the set of fields from the index required to apply the filter and union those with
     // the set of fields from the index required by the parent stage.
@@ -838,7 +838,7 @@ std::pair<std::unique_ptr<sbe::PlanStage>, PlanStageSlots> generateIndexScanWith
     sbe::value::SlotId recordIdSlot;
     ParameterizedIndexScanSlots parameterizedScanSlots;
 
-    auto keyPattern = descriptor->keyPattern();
+    auto keyPattern = ixn->index.keyPattern;
 
     // Determine the set of fields from the index required to apply the filter and union those with
     // the set of fields from the index required by the parent stage.
