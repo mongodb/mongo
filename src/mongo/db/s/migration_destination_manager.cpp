453091a36400607b1e0c57aff596cffe22a82eed
Max Hirschhorn <max.hirschhorn@mongodb.com>
2023-06-26 23:18:53 +0000

    SERVER-78414 Apply _transferMods batches until size returned == 0.
    It is possible for the entire _transferMods command response to contain
    changes which aren't relevant for the range being migrated. The
    recipient shard should continue running the _transferMods command on the
    donor shard primary in this case until it learns there are no further
    changes.
    
    (cherry picked from commit a8fdfb121dfc5e35e20a036c8198f2e9240d5b61)

--- src/mongo/db/s/migration_destination_manager.cpp
+++ src/mongo/db/s/migration_destination_manager.cpp
@@ -337,6 +337,7 @@ MONGO_FAIL_POINT_DEFINE(migrateThreadHangAtStep3);
 MONGO_FAIL_POINT_DEFINE(migrateThreadHangAtStep4);
 MONGO_FAIL_POINT_DEFINE(migrateThreadHangAtStep5);
 MONGO_FAIL_POINT_DEFINE(migrateThreadHangAtStep6);
+MONGO_FAIL_POINT_DEFINE(migrateThreadHangAfterSteadyTransition);
 MONGO_FAIL_POINT_DEFINE(migrateThreadHangAtStep7);
 
 MONGO_FAIL_POINT_DEFINE(failMigrationOnRecipient);
@@ -1551,6 +1552,7 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* outerOpCtx,
         {
             // 6. Wait for commit
             _setState(kSteady);
+            migrateThreadHangAfterSteadyTransition.pauseWhileSet();
 
             bool transferAfterCommit = false;
             while (getState() == kSteady || getState() == kCommitStart) {
@@ -1578,7 +1580,8 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* outerOpCtx,
 
                 auto mods = res.response;
 
-                if (mods["size"].number() > 0 && _applyMigrateOp(opCtx, mods)) {
+                if (mods["size"].number() > 0) {
+                    (void)_applyMigrateOp(opCtx, mods);
                     lastOpApplied = repl::ReplClientInfo::forClient(opCtx->getClient()).getLastOp();
                     continue;
                 }
