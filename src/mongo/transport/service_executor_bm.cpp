aa78d65edb7e195f48f7ca23abe3a47ddc9d8948
Kaloian Manassiev <kaloian.manassiev@mongodb.com>
2023-06-29 15:01:34 +0000

    SERVER-47204 Fix broken service_executor_bm

--- src/mongo/transport/service_executor_bm.cpp
+++ src/mongo/transport/service_executor_bm.cpp
@@ -74,6 +74,9 @@ class ServiceExecutorSynchronousBm : public benchmark::Fixture {
 public:
     void firstSetup() {
         setGlobalServiceContext(ServiceContext::make());
+        auto service = ServiceContext::make();
+        sc = service.get();
+        setGlobalServiceContext(std::move(service));
         (void)executor()->start();
     }
 
