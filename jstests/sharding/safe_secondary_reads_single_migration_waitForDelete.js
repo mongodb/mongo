ac6e36e9a6ebe8eadd2dfc23c53f68f02eb22d10
Cheahuychou Mao <mao.cheahuychou@gmail.com>
2023-07-03 23:51:40 +0000

    SERVER-78544 Re-enable multiversion testing for analyzeShardKey and configureQueryAnalyzer commands

--- jstests/sharding/safe_secondary_reads_single_migration_waitForDelete.js
+++ jstests/sharding/safe_secondary_reads_single_migration_waitForDelete.js
@@ -116,23 +116,21 @@ let testCases = {
     },
     analyze: {skip: "primary only"},
     analyzeShardKey: {
-        // TODO: Re-enable multiversion testing for PM-1858.
-        skip: "not implemented"
-        // setUp: function(mongosConn) {
-        //     const docs = [];
-        //     for (let i = 1; i <= 1000; i++) {
-        //         docs.push({x: i});
-        //     }
-        //     assert.commandWorked(mongosConn.getCollection(nss).insert(docs));
-        // },
-        // command: {analyzeShardKey: nss, key: {x: 1}},
-        // runsAgainstAdminDb: true,
-        // checkResults: function(res) {
-        //     // The command should work and return correct results.
-        //     assert.commandWorked(res);
-        //     assert.eq(res.keyCharacteristics.numDocsTotal, 1000, res);
-        // },
-        // behavior: "versioned"
+        setUp: function(mongosConn) {
+            const docs = [];
+            for (let i = 1; i <= 1000; i++) {
+                docs.push({x: i});
+            }
+            assert.commandWorked(mongosConn.getCollection(nss).insert(docs));
+        },
+        command: {analyzeShardKey: nss, key: {x: 1}},
+        runsAgainstAdminDb: true,
+        checkResults: function(res) {
+            // The command should work and return correct results.
+            assert.commandWorked(res);
+            assert.eq(res.keyCharacteristics.numDocsTotal, 1000, res);
+        },
+        behavior: "versioned"
     },
     appendOplogNote: {skip: "primary only"},
     applyOps: {skip: "primary only"},
