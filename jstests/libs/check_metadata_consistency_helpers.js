f9d057ff9ec77a87d8c05df481bc0411efbd5072
Pol Pinol Castuera <pol.pinol@mongodb.com>
2023-07-03 11:00:23 +0000

    SERVER-78545 Make CheckMetadataConsistency hook transient to FailedToSatisfyReadPreference error
    (cherry picked from commit ea8f29ffa43c6245551993d3761c271332a87e66)

--- jstests/libs/check_metadata_consistency_helpers.js
+++ jstests/libs/check_metadata_consistency_helpers.js
@@ -17,6 +17,31 @@ var MetadataConsistencyChecker = (function() {
             return;
         }
 
+        // The isTransientError() function is responsible for setting an error as transient and
+        // abort the metadata consistency check to be retried in the future.
+        const isTransientError = function(e) {
+            if (ErrorCodes.isRetriableError(e.code) || ErrorCodes.isInterruption(e.code)) {
+                return true;
+            }
+
+            // TODO SERVER-78117: Remove once checkMetadataConsistency command is robust to
+            // ShardNotFound
+            if (e.code === ErrorCodes.ShardNotFound) {
+                // Metadata consistency check can fail with ShardNotFound if the router's
+                // ShardRegistry reloads after choosing which shards to target and a chosen
+                // shard is no longer in the cluster.
+                return true;
+            }
+
+            if (e.code === ErrorCodes.FailedToSatisfyReadPreference) {
+                // Metadata consistency check can fail with FailedToSatisfyReadPreference error
+                // response when the primary of the shard is permanently down.
+                return true;
+            }
+
+            return false;
+        };
+
         const checkMetadataConsistency = function() {
             jsTest.log('Started metadata consistency check');
 
@@ -40,11 +65,7 @@ var MetadataConsistencyChecker = (function() {
         try {
             checkMetadataConsistency();
         } catch (e) {
-            // Metadata consistency check can fail with ShardNotFound if the router's ShardRegistry
-            // reloads after choosing which shards to target and a chosen shard is no longer in the
-            // cluster. This error should be transient, so it can be retried on.
-            if (ErrorCodes.isRetriableError(e.code) || ErrorCodes.isInterruption(e.code) ||
-                e.code === ErrorCodes.ShardNotFound) {
+            if (isTransientError(e)) {
                 jsTest.log(`Aborted metadata consistency check due to retriable error: ${e}`);
             } else {
                 throw e;
