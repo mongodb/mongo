0ffd4c9a4ef54c95767dfc574c2238e6a578d600
Alexander Gorrod <alexander.gorrod@mongodb.com>
2023-07-04 23:45:50 +0000

    Import wiredtiger: 2c140a4fbeab786e36d39d1df00d7d3dd9bdf328 from branch mongodb-master
    ref: b5cf91a853..2c140a4fbe
    for: 7.1.0-rc0
    
    WT-11025 Make test_compact04 robust to spurious test failures (which are expected)

--- src/third_party/wiredtiger/test/suite/test_compact04.py
+++ src/third_party/wiredtiger/test/suite/test_compact04.py
@@ -36,12 +36,11 @@ from wiredtiger import stat
 # Test the accuracy of compact work estimation.
 class test_compact04(wttest.WiredTigerTestCase):
 
-    conn_config = 'statistics=(all),cache_size=100MB'
-    # Uncomment for debugging:
-    # conn_config += ',verbose=(compact_progress,compact:4)'
+    # Keep debug messages on by default. This is useful for diagnosing spurious test failures.
+    conn_config = 'statistics=(all),cache_size=100MB,verbose=(compact_progress,compact:4)'
     create_params = 'key_format=i,value_format=S,allocation_size=4KB,leaf_page_max=32KB,'
     table_numkv = 100 * 1000
-    table_uri='table:test_compact04'
+    table_uri_prefix='table:test_compact04-'
 
     delete_range_len = 10 * 1000
     delete_ranges_count = 4
@@ -50,40 +49,65 @@ class test_compact04(wttest.WiredTigerTestCase):
     # Create the table in a way that it creates a mostly empty file.
     def test_compact04(self):
 
-        # Create the table and populate it with a lot of data
-        self.session.create(self.table_uri, self.create_params)
-        c = self.session.open_cursor(self.table_uri, None)
-        for k in range(self.table_numkv):
-            c[k] = ('%07d' % k) + '_' + 'abcd' * ((self.value_size // 4) - 2)
-        c.close()
-        self.session.checkpoint()
+        # Run the test several times. It is practically impossible to design a progress prediction
+        # mechanism that is always correct, so it is possible to get a spurious prediction that is
+        # considerably less accurate than we'd like. We just need to make sure that we don't get
+        # those wrong predictions too frequently.
+        num_failures = 0
+        num_successes = 0
 
-        # Now let's delete a lot of data ranges. Create enough space so that compact runs in more
-        # than one iteration.
-        c = self.session.open_cursor(self.table_uri, None)
-        for r in range(self.delete_ranges_count):
-            start = r * self.table_numkv // self.delete_ranges_count
-            for i in range(self.delete_range_len):
-                c.set_key(start + i)
-                c.remove()
-        c.close()
+        for iteration in range(0, 10):
+            table_uri = self.table_uri_prefix + str(iteration)
 
-        # Compact!
-        self.session.compact(self.table_uri, None)
-        self.ignoreStdoutPatternIfExists('WT_VERB_COMPACT')
-        self.ignoreStderrPatternIfExists('WT_VERB_COMPACT')
+            # Create the table and populate it with a lot of data
+            self.session.create(table_uri, self.create_params)
+            c = self.session.open_cursor(table_uri, None)
+            for k in range(self.table_numkv):
+                c[k] = ('%07d' % k) + '_' + 'abcd' * ((self.value_size // 4) - 2)
+            c.close()
+            self.session.checkpoint()
 
-        # Verify the compact progress stats.
-        c_stat = self.session.open_cursor('statistics:' + self.table_uri, None, 'statistics=(all)')
-        pages_rewritten = c_stat[stat.dsrc.btree_compact_pages_rewritten][2]
-        pages_rewritten_expected = c_stat[stat.dsrc.btree_compact_pages_rewritten_expected][2]
-        c_stat.close()
+            # Now let's delete a lot of data ranges. Create enough space so that compact runs in more
+            # than one iteration.
+            c = self.session.open_cursor(table_uri, None)
+            for r in range(self.delete_ranges_count):
+                start = r * self.table_numkv // self.delete_ranges_count
+                for i in range(self.delete_range_len):
+                    c.set_key(start + i)
+                    c.remove()
+            c.close()
 
-        self.assertGreater(pages_rewritten, 0)
-        self.assertGreater(pages_rewritten_expected, 0)
+            # Compact!
+            self.session.compact(table_uri, None)
+            self.ignoreStdoutPatternIfExists('WT_VERB_COMPACT')
+            self.ignoreStderrPatternIfExists('WT_VERB_COMPACT')
 
-        d = abs(pages_rewritten - pages_rewritten_expected) / pages_rewritten
-        self.assertLess(d, 0.15)
+            # Verify the compact progress stats.
+            c_stat = self.session.open_cursor('statistics:' + table_uri, None, 'statistics=(all)')
+            pages_rewritten = c_stat[stat.dsrc.btree_compact_pages_rewritten][2]
+            pages_rewritten_expected = c_stat[stat.dsrc.btree_compact_pages_rewritten_expected][2]
+            c_stat.close()
+
+            self.assertGreater(pages_rewritten, 0)
+            self.assertGreater(pages_rewritten_expected, 0)
+
+            d = abs(pages_rewritten - pages_rewritten_expected) / pages_rewritten
+
+            # Check whether we succeeded. Terminate the test early if things are going well. If we
+            # experience even one failure, run through all iterations to ensure that the failures
+            # are rare. Each iteration runs on the order of seconds, so this test will complete
+            # quickly even if we have to run through all iterations.
+            message = 'Compacting %s: Prediction error: %0.2f%%' % (table_uri, d * 100)
+            if d < 0.15:
+                self.pr(message)
+                num_successes += 1
+                if num_successes >= 1 and num_failures == 0:
+                    self.pr('Finishing the test early')
+                    return
+            else:
+                self.pr(message + ' (FAILURE)')
+                num_failures += 1
+                self.assertLessEqual(num_failures, 2)
 
 if __name__ == '__main__':
     wttest.run()
