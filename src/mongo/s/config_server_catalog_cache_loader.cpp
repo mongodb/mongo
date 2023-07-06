f6c48222cc1cf0c68935a3414f79067687059d1a
Allison Easton <allison.easton@mongodb.com>
2023-07-06 09:17:09 +0000

    SERVER-62987 Secondary refreshes should be interrupted on replication rollback
    (cherry picked from commit 2196021e412bb0ad1c470f4ba664551a9bbb56fe)

--- src/mongo/s/config_server_catalog_cache_loader.cpp
+++ src/mongo/s/config_server_catalog_cache_loader.cpp
@@ -114,6 +114,10 @@ void ConfigServerCatalogCacheLoader::onStepUp() {
     MONGO_UNREACHABLE;
 }
 
+void ConfigServerCatalogCacheLoader::onReplicationRollback() {
+    MONGO_UNREACHABLE;
+}
+
 void ConfigServerCatalogCacheLoader::shutDown() {
     _executor->shutdown();
     _executor->join();
