a99131232992f32e18a7f55909df76d5ca11eb19
Richard Hausman <richard.hausman@mongodb.com>
2023-07-07 17:39:42 +0000

    SERVER-78778 : Added cqf_incompatible tag to disable test

--- jstests/noPassthrough/collection_scan_low_priority.js
+++ jstests/noPassthrough/collection_scan_low_priority.js
@@ -2,6 +2,8 @@
  * Tests that unbounded collections scans access the storage engine with low priority.
  *
  * @tags: [
+ *   cqf_incompatible, # TODO SERVER-64007: This test requires plans which yield in order to count
+ *   # low-priority transactions, which CQF cannot generate until this ticket is complete.
  *   requires_wiredtiger,
  * ]
  */
