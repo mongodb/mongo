e8a45ebe9b6c0e040a7c7188bdea5ee9bfa458d2
Ruby Chen <ruby.chen@mongodb.com>
2023-07-12 04:00:33 +0000

    Import wiredtiger: e298381e00d85ce80592126c03ceadf430096f18 from branch mongodb-master
    ref: 90645ed3f1..e298381e00
    for: 7.1.0-rc0
    
    Revert "WT-11278 Add MSan origin tracking

--- src/third_party/wiredtiger/cmake/configs/modes.cmake
+++ src/third_party/wiredtiger/cmake/configs/modes.cmake
@@ -138,8 +138,8 @@ set(ubsan_compiler_cxx_flag "-fsanitize=undefined")
 
 # MSAN build variant flags.
 set(msan_link_flags "-fsanitize=memory")
-set(msan_compiler_c_flag "-fsanitize=memory" "-fno-optimize-sibling-calls" "-fsanitize-memory-track-origins=2")
-set(msan_compiler_cxx_flag "-fsanitize=memory" "-fno-optimize-sibling-calls" "-fsanitize-memory-track-origins=2")
+set(msan_compiler_c_flag "-fsanitize=memory" "-fno-optimize-sibling-calls")
+set(msan_compiler_cxx_flag "-fsanitize=memory" "-fno-optimize-sibling-calls")
 
 # TSAN build variant flags.
 set(tsan_link_flags "-fsanitize=thread")
