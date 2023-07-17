39e12d7dd8a107bb59f0b69640bafa0d73abeeb0
Sean04 <sean.watt@mongodb.com>
2023-07-17 03:50:36 +0000

    Import wiredtiger: 24e6dec1867bab6069be41f1e18d8a063f8d799a from branch mongodb-master
    ref: 0a320fd026..24e6dec186
    for: 7.1.0-rc0
    
    WT-10117 Retry pread in file copy

--- src/third_party/wiredtiger/test/utility/file.c
+++ src/third_party/wiredtiger/test/utility/file.c
@@ -251,9 +251,11 @@ copy_on_file(const char *path, const file_info_t *info, void *user_data)
 #else
     struct utimbuf t;
 #endif
+    WT_DECL_RET;
     ssize_t n;
     int rfd, wfd;
     char *buf;
+    wt_off_t offset;
 #endif
     char dest_path[PATH_MAX];
 
@@ -294,8 +296,9 @@ copy_on_file(const char *path, const file_info_t *info, void *user_data)
       (wfd = open(dest_path, O_WRONLY | O_CREAT | O_TRUNC, info->stat.st_mode)) > 0);
 
     buf = dmalloc(COPY_BUF_SIZE);
-    for (;;) {
-        testutil_assert_errno((n = read(rfd, buf, COPY_BUF_SIZE)) >= 0);
+    for (offset = 0, n = 0;; offset += n) {
+        WT_SYSCALL_RETRY((n = pread(rfd, buf, COPY_BUF_SIZE, offset)) < 0 ? -1 : 0, ret);
+        testutil_check(ret);
         if (n == 0)
             break;
         testutil_assert_errno(write(wfd, buf, (size_t)n) == n);
