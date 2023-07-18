0e21b9dc7ad65845ae0b38ba67e8a8417a927de6
Sean04 <sean.watt@mongodb.com>
2023-07-18 00:41:02 +0000

    Import wiredtiger: 5b7de4cb3e22a351d79b045ad9af1bdac374f309 from branch mongodb-master
    ref: 1cd02d158f..5b7de4cb3e
    for: 7.1.0-rc0
    
    WT-11221 Ignore unexpected Eviction took more than 1 minute warning in stdout

--- src/third_party/wiredtiger/test/suite/test_prepare_hs01.py
+++ src/third_party/wiredtiger/test/suite/test_prepare_hs01.py
@@ -152,5 +152,7 @@ class test_prepare_hs01(wttest.WiredTigerTestCase):
         nkeys = 4000
         self.prepare_updates(uri, ds, nrows, nsessions, nkeys)
 
+        self.ignoreStdoutPatternIfExists('Eviction took more than 1 minute')
+
 if __name__ == '__main__':
     wttest.run()
