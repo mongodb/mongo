cb69a1f5a9b2da823793bde019be59256999f25b
Mathis Bessa <mathis.bessa@mongodb.com>
2023-07-20 21:33:41 +0000

    SERVER-79153 removed unused 'db' from analyzeCommandAsAggregationCommand

--- src/mongo/db/commands/analyze_cmd.cpp
+++ src/mongo/db/commands/analyze_cmd.cpp
@@ -74,7 +74,6 @@ namespace mongo {
 namespace {
 
 StatusWith<BSONObj> analyzeCommandAsAggregationCommand(OperationContext* opCtx,
-                                                       StringData db,
                                                        StringData collection,
                                                        StringData keyPath,
                                                        boost::optional<double> sampleRate,
@@ -241,15 +240,12 @@ public:
 
                 // Run Aggregate
                 BSONObj analyzeResult;
-                client.runCommand(nss.dbName(),
-                                  analyzeCommandAsAggregationCommand(opCtx,
-                                                                     nss.db(),
-                                                                     nss.coll(),
-                                                                     key->toString(),
-                                                                     sampleRate,
-                                                                     cmd.getNumberBuckets())
-                                      .getValue(),
-                                  analyzeResult);
+                client.runCommand(
+                    nss.dbName(),
+                    analyzeCommandAsAggregationCommand(
+                        opCtx, nss.coll(), key->toString(), sampleRate, cmd.getNumberBuckets())
+                        .getValue(),
+                    analyzeResult);
 
                 // We must reset the internal flag.
                 if (!wasInternalClient) {
