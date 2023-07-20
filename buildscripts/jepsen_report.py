b17bf0232e97aeb0d23eb0a27ce2d8cd4730d262
Juan Gu <juan.gu@mongodb.com>
2023-07-20 20:30:25 +0000

    SERVER-79167 Upload mongod and mongos logs when Jepsen tests fail

--- buildscripts/jepsen_report.py
+++ buildscripts/jepsen_report.py
@@ -66,15 +66,52 @@ UNKNOWN_RE = re.compile("([0-9]+) unknown")
 FAIL_RE = re.compile("([0-9]+) failures")
 
 
-def parse(text: List[str]) -> ParserOutput:
-    """Given a List of strings representing jepsen log file split by newlines, return the ParserOutput struct."""
+def parse(text: List[str]) -> ParserOutput:  # noqa: D406
+    """Given a List of strings representing jepsen log file split by newlines, return the ParserOutput struct.
+
+    Args:
+        text (List[str]): List of strings representing the jepsen log file split by newlines.
+
+    Returns:
+        ParserOutput: The parsed output struct.
+
+    Raises:
+        AssertionError: If there is a mismatch between the count of matches and the corresponding test list.
+    """
 
     successful_tests: List[str] = []
     indeterminate_tests: List[str] = []
     crashed_tests: List[str] = []
     failed_tests: List[str] = []
     target = None
-    table_matches = 0
+    success_table_matches = 0
+    unknown_table_matches = 0
+    crash_table_matches = 0
+    fail_table_matches = 0
+
+    # we're parsing this kind of table through entire file:
+
+    # # Successful tests
+    # store/mongodb list-append w:majority r:majority tw:majority tr:snapshot partition/20230718T215050.000Z
+    # 1 successes
+    # 0 unknown
+    # 0 crashed
+    # 0 failures
+
+    # # Crashed tests
+    # store/mongodb list-append w:majority r:majority tw:majority tr:snapshot partition/20230718T005314.000Z
+    # 0 successes
+    # 0 unknown
+    # 1 crashed
+    # 0 failures
+
+    # # Failed tests tests
+    # store/mongodb list-append w:majority r:majority tw:majority tr:snapshot partition/20230718T220100.000Z
+    # 0 successes
+    # 0 unknown
+    # 0 crashed
+    # 1 failures
+
     for line in text:
         if "# Successful tests" in line:
             target = successful_tests
@@ -89,37 +126,35 @@ def parse(text: List[str]) -> ParserOutput:
             target = failed_tests
             continue
 
-        # at this point we're parsing this table:
-        # 29 successes
-        # 0 unknown
-        # 1 crashed
-        # 0 failures
         s_match = SUCCESS_RE.match(line)
         if s_match:
             target = None
-            assert int(s_match[1]) == len(successful_tests)
-            table_matches += 1
+            success_table_matches += int(s_match[1])
 
         u_match = UNKNOWN_RE.match(line)
         if u_match:
             target = None
-            assert int(u_match[1]) == len(indeterminate_tests)
-            table_matches += 1
+            unknown_table_matches += int(u_match[1])
         c_match = CRASH_RE.match(line)
         if c_match:
             target = None
-            assert int(c_match[1]) == len(crashed_tests)
-            table_matches += 1
+            crash_table_matches += int(c_match[1])
         f_match = FAIL_RE.match(line)
         if f_match:
             target = None
-            assert int(f_match[1]) == len(failed_tests)
-            table_matches += 1
-
+            fail_table_matches += int(f_match[1])
         if target is not None and line.strip():
             target.append(line)
 
-    assert table_matches == 4, f"Failed to parse summary table. Expected 4, found {table_matches}"
+    assert success_table_matches == len(
+        successful_tests), "Mismatch in success_table_matches and length of successful_tests"
+    assert unknown_table_matches == len(
+        indeterminate_tests), "Mismatch in unknown_table_matches and length of indeterminate_tests"
+    assert crash_table_matches == len(
+        crashed_tests), "Mismatch in crash_table_matches and length of crashed_tests"
+    assert fail_table_matches == len(
+        failed_tests), "Mismatch in fail_table_matches and length of failed_tests"
+
     return ParserOutput({
         'success': successful_tests,
         'unknown': indeterminate_tests,
--- evergreen/jepsen_docker/list-append.sh
+++ evergreen/jepsen_docker/list-append.sh
@@ -6,20 +6,67 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 # This whole script must run to ensure the report is generated and test
 # artifacts are placed in the right location.
 
-cd jepsen/docker
-
 # actually run the tests
 start_time=$(date +%s)
-sudo docker exec jepsen-control bash --login -c "cd /jepsen/mongodb && lein run test-all -w list-append -n n1 -n n2 -n n3 -n n4 -n n5 -n n6 -n n7 -n n8 -n n9 -r 1000 --concurrency 3n --time-limit 240 --max-writes-per-key 128 --read-concern majority --write-concern majority --txn-read-concern snapshot --txn-write-concern majority --nemesis-interval 1 --nemesis partition --test-count 30" | tee jepsen_${task_name}_${execution}.log
+
+# Previously, we use the "--test-count 30" option to repeat the Jepen test 30 times.
+# However, to ensure the preservation of all the mongod and mongos logs for each failed test,
+# we have implemented a for loop to iterate the test 30 times.
+for i in {1..30}; do
+  cd jepsen/docker
+
+  sudo docker exec jepsen-control bash --login -c "\
+  cd /jepsen/mongodb && \
+  lein run test-all -w list-append -n n1 -n n2 -n n3 -n n4 -n n5 -n n6 -n n7 -n n8 -n n9 -r 1000 \
+  --concurrency 3n --time-limit 240 --max-writes-per-key 128 \
+  --read-concern majority --write-concern majority \
+  --txn-read-concern snapshot --txn-write-concern majority \
+  --nemesis-interval 1 --nemesis partition --test-count 1" | tee jepsen_test_${i}.log
+
+  cd ../../
+
+  # copy files to expected locations for archiving
+  mkdir -p src/jepsen-mongodb/store/test-index${i}
+  sudo docker cp jepsen-control:/jepsen/mongodb/store src/jepsen-mongodb/store/test-index${i}
+  cp jepsen/docker/jepsen_test_${i}.log src/jepsen-mongodb/
+  sudo docker cp jepsen-control:/jepsen/mongodb src/jepsen-workdir
+
+  # Get the last five lines of the log file like below example
+  # 1 successes
+  # 0 unknown
+  # 0 crashed
+  # 0 failures
+  #
+  last_five_lines=$(tail -n 5 "jepsen/docker/jepsen_test_${i}.log")
+
+  # Check if the "1 successes" string is in the last eight lines
+  if echo "$last_five_lines" | grep -q "1 successes"; then
+    echo "Test is successful, mongod and mongos logs are not needed"
+  else
+    echo "Test is not successful, save mongod and mongos logs for uploading"
+
+    # copy mongod logs
+    mkdir -p src/jepsen-mongodb/mongodlogs/test_${i}
+    loop 9 docker containers
+    for n in {1..9}; do
+      sudo docker cp jepsen-n${n}:/var/log/mongodb/mongod.log src/jepsen-mongodb/mongodlogs/test_${i}/jepson-n${n}-mongod.log
+    done
+    sudo chmod +r src/jepsen-mongodb/mongodlogs/test_${i}/*.log
+
+    # copy mongos logs
+    mkdir -p src/jepsen-mongodb/mongoslogs/test_${i}
+    for n in {1..9}; do
+      sudo docker cp jepsen-n${n}:/var/log/mongodb/mongos.stdout src/jepsen-mongodb/mongoslogs/test_${i}/jepson-n${n}-mongos.stdout
+    done
+    sudo chmod +r src/jepsen-mongodb/mongoslogs/test_${i}/*.stdout
+  fi
+done
+
+# Merge all jepsen_test_${i}.log into a single file
+cat src/jepsen-mongodb/jepsen_test_*.log > src/jepsen-mongodb/jepsen_${task_name}_${execution}.log
+
 end_time=$(date +%s)
 elapsed_secs=$((end_time - start_time))
-
-# copy files to expected locations for archiving
-cd ../../
-mkdir -p src/jepsen-mongodb
-sudo docker cp jepsen-control:/jepsen/mongodb/store src/jepsen-mongodb/store
-cp jepsen/docker/jepsen_${task_name}_${execution}.log src/jepsen-mongodb/
-sudo docker cp jepsen-control:/jepsen/mongodb src/jepsen-workdir
 
 cd src
 activate_venv
