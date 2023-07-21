90ccb40d125d0f0a1069c967ce89fcd031ca733c
Silvia Surroca <silvia.surroca@mongodb.com>
2023-07-21 14:06:39 +0000

    SERVER-79047 Fix invariant on sharding_write_router_bm

--- src/mongo/db/s/sharding_write_router_bm.cpp
+++ src/mongo/db/s/sharding_write_router_bm.cpp
@@ -49,6 +49,7 @@
 #include "mongo/bson/timestamp.h"
 #include "mongo/db/client.h"
 #include "mongo/db/cluster_role.h"
+#include "mongo/db/concurrency/d_concurrency.h"
 #include "mongo/db/keypattern.h"
 #include "mongo/db/namespace_string.h"
 #include "mongo/db/operation_context.h"
@@ -95,197 +96,251 @@ namespace {
 
 const NamespaceString kNss = NamespaceString::createNamespaceString_forTest("test", "foo");
 
-ShardId pessimalShardSelector(int i, int nShards, int nChunks) {
-    return ShardId(str::stream() << "shard" << (i % nShards));
-}
+class WriteRouterTestFixture : public benchmark::Fixture {
+public:
+    void firstSetup() {
+        auto service = ServiceContext::make();
+        auto serviceContext = service.get();
+        setGlobalServiceContext(std::move(service));
 
-ChunkRange getRangeForChunk(int i, int nChunks) {
-    invariant(i >= 0);
-    invariant(nChunks > 0);
-    invariant(i < nChunks);
-    if (i == 0) {
-        return {BSON("_id" << MINKEY), BSON("_id" << 0)};
-    }
-    if (i + 1 == nChunks) {
-        return {BSON("_id" << (i - 1) * 100), BSON("_id" << MAXKEY)};
-    }
-    return {BSON("_id" << (i - 1) * 100), BSON("_id" << i * 100)};
-}
-
-RoutingTableHistoryValueHandle makeStandaloneRoutingTableHistory(RoutingTableHistory rt) {
-    const auto version = rt.getVersion();
-    return RoutingTableHistoryValueHandle(
-        std::make_shared<RoutingTableHistory>(std::move(rt)),
-        ComparableChunkVersion::makeComparableChunkVersion(version));
-}
-
-std::pair<std::vector<mongo::ChunkType>, mongo::ChunkManager> createChunks(
-    size_t nShards, uint32_t nChunks, std::vector<ShardId> shards) {
-    invariant(shards.size() == nShards);
-
-    const auto collIdentifier = UUID::gen();
-    const auto shardKeyPattern = KeyPattern(BSON("_id" << 1));
-    const auto reshardKeyPattern = KeyPattern(BSON("y" << 1));
-    const auto collEpoch = OID::gen();
-    const auto collTimestamp = Timestamp(100, 5);
-    const auto tempNss = NamespaceString::createNamespaceString_forTest(
-        kNss.db_forSharding(),
-        fmt::format("{}{}",
-                    NamespaceString::kTemporaryReshardingCollectionPrefix,
-                    collIdentifier.toString()));
-
-    std::vector<ChunkType> chunks;
-    chunks.reserve(nChunks);
-
-    for (uint32_t i = 0; i < nChunks; ++i) {
-        chunks.emplace_back(collIdentifier,
-                            getRangeForChunk(i, nChunks),
-                            ChunkVersion({collEpoch, collTimestamp}, {i + 1, 0}),
-                            pessimalShardSelector(i, nShards, nChunks));
+        setupCatalogCacheMock(serviceContext, _withShardedCollection);
     }
 
-    TypeCollectionReshardingFields reshardingFields{UUID::gen()};
-    reshardingFields.setState(CoordinatorStateEnum::kPreparingToDonate);
-    // ShardingWriteRouter is only meant to be used by the donor.
-    reshardingFields.setDonorFields(TypeCollectionDonorFields{tempNss, reshardKeyPattern, shards});
+    void lastTearDown() {
+        setGlobalServiceContext({});
+    }
 
-    ChunkManager cm(shards[0],
-                    DatabaseVersion(UUID::gen(), Timestamp(1, 0)),
-                    makeStandaloneRoutingTableHistory(
-                        RoutingTableHistory::makeNew(kNss,
-                                                     collIdentifier,
-                                                     shardKeyPattern,
-                                                     nullptr,
-                                                     false,
-                                                     collEpoch,
-                                                     collTimestamp,
-                                                     boost::none /* timeseriesFields */,
-                                                     reshardingFields, /* reshardingFields */
-                                                     true,
-                                                     chunks)),
-                    boost::none);
+    ServiceContext* serviceContext() {
+        return getGlobalServiceContext();
+    }
 
-    return std::make_pair(chunks, cm);
-}
+protected:
+    void SetUp(benchmark::State& state) override {
+        if (state.thread_index == 0) {
+            firstSetup();
+        }
+    }
 
-void setupCatalogCacheMock(OperationContext* opCtx, bool withShardedCollection) {
-    auto catalogCache = CatalogCacheMock::make();
+    void TearDown(benchmark::State& state) override {
+        if (state.thread_index == 0) {
+            lastTearDown();
+        }
+    }
 
-    if (withShardedCollection) {
-        const size_t nShards = 1;
-        const uint32_t nChunks = 60;
-        const auto clusterId = OID::gen();
-        const auto shards = std::vector<ShardId>{ShardId("shard0")};
-        const auto originatorShard = shards[0];
+    void setupCatalogCacheMock(ServiceContext* serviceContext, bool withShardedCollection) {
+        const auto client = serviceContext->makeClient("test-setup");
+        const auto opCtxHolder = client->makeOperationContext();
+        OperationContext* opCtx = opCtxHolder.get();
 
-        const auto [chunks, chunkManager] = createChunks(nShards, nChunks, shards);
+        auto catalogCache = CatalogCacheMock::make();
 
-        ShardingState::get(opCtx->getServiceContext())->setInitialized(originatorShard, clusterId);
+        if (withShardedCollection) {
+            const size_t nShards = 1;
+            const uint32_t nChunks = 60;
+            const auto clusterId = OID::gen();
+            const auto shards = std::vector<ShardId>{ShardId("shard0")};
+            const auto originatorShard = shards[0];
 
-        CollectionShardingStateFactory::set(
-            opCtx->getServiceContext(),
-            std::make_unique<CollectionShardingStateFactoryShard>(opCtx->getServiceContext()));
+            const auto [chunks, chunkManager] = createChunks(nShards, nChunks, shards);
+
+            ShardingState::get(serviceContext)->setInitialized(originatorShard, clusterId);
+
+            CollectionShardingStateFactory::set(
+                serviceContext,
+                std::make_unique<CollectionShardingStateFactoryShard>(serviceContext));
+
+            _shardVersion.emplace(ShardVersionFactory::make(
+                chunkManager, originatorShard, boost::optional<CollectionIndexes>(boost::none)));
+
+            OperationShardingState::setShardRole(
+                opCtx, kNss, _shardVersion, boost::none /* databaseVersion */);
+
+            // Configuring the filtering metadata such that calls to getCollectionDescription
+            // what we want. Specifically the reshardingFields are what we use. Its specified by
+            // the chunkManager.
+            Lock::DBLock dbLock{opCtx, kNss.dbName(), MODE_IX};
+            Lock::CollectionLock collLock{opCtx, kNss, MODE_IX};
+            CollectionShardingRuntime::assertCollectionLockedAndAcquireExclusive(opCtx, kNss)
+                ->setFilteringMetadata(opCtx, CollectionMetadata(chunkManager, originatorShard));
+
+            catalogCache->setChunkManagerReturnValue(chunkManager);
+        }
+
+        auto mockNetwork = std::make_unique<executor::NetworkInterfaceMock>();
+
+        auto const grid = Grid::get(opCtx);
+        grid->init(std::make_unique<ShardingCatalogClientImpl>(nullptr),
+                   std::move(catalogCache),
+                   std::make_unique<ShardRegistry>(serviceContext, nullptr, boost::none),
+                   std::make_unique<ClusterCursorManager>(serviceContext->getPreciseClockSource()),
+                   std::make_unique<BalancerConfiguration>(),
+                   std::make_unique<executor::TaskExecutorPool>(),
+                   mockNetwork.get());
+
+        // Note: mockNetwork in Grid will become a dangling pointer after this function, this
+        // is fine since the test shouldn't be using it.
+    }
+
+    ShardId pessimalShardSelector(int i, int nShards, int nChunks) {
+        return ShardId(str::stream() << "shard" << (i % nShards));
+    }
+
+    ChunkRange getRangeForChunk(int i, int nChunks) {
+        invariant(i >= 0);
+        invariant(nChunks > 0);
+        invariant(i < nChunks);
+        if (i == 0) {
+            return {BSON("_id" << MINKEY), BSON("_id" << 0)};
+        }
+        if (i + 1 == nChunks) {
+            return {BSON("_id" << (i - 1) * 100), BSON("_id" << MAXKEY)};
+        }
+        return {BSON("_id" << (i - 1) * 100), BSON("_id" << i * 100)};
+    }
+
+    RoutingTableHistoryValueHandle makeStandaloneRoutingTableHistory(RoutingTableHistory rt) {
+        const auto version = rt.getVersion();
+        return RoutingTableHistoryValueHandle(
+            std::make_shared<RoutingTableHistory>(std::move(rt)),
+            ComparableChunkVersion::makeComparableChunkVersion(version));
+    }
+
+    std::pair<std::vector<mongo::ChunkType>, mongo::ChunkManager> createChunks(
+        size_t nShards, uint32_t nChunks, std::vector<ShardId> shards) {
+        invariant(shards.size() == nShards);
+
+        const auto collIdentifier = UUID::gen();
+        const auto shardKeyPattern = KeyPattern(BSON("_id" << 1));
+        const auto reshardKeyPattern = KeyPattern(BSON("y" << 1));
+        const auto collEpoch = OID::gen();
+        const auto collTimestamp = Timestamp(100, 5);
+        const auto tempNss = NamespaceString::createNamespaceString_forTest(
+            kNss.db_forSharding(),
+            fmt::format("{}{}",
+                        NamespaceString::kTemporaryReshardingCollectionPrefix,
+                        collIdentifier.toString()));
+
+        std::vector<ChunkType> chunks;
+        chunks.reserve(nChunks);
+
+        for (uint32_t i = 0; i < nChunks; ++i) {
+            chunks.emplace_back(collIdentifier,
+                                getRangeForChunk(i, nChunks),
+                                ChunkVersion({collEpoch, collTimestamp}, {i + 1, 0}),
+                                pessimalShardSelector(i, nShards, nChunks));
+        }
+
+        TypeCollectionReshardingFields reshardingFields{UUID::gen()};
+        reshardingFields.setState(CoordinatorStateEnum::kPreparingToDonate);
+        // ShardingWriteRouter is only meant to be used by the donor.
+        reshardingFields.setDonorFields(
+            TypeCollectionDonorFields{tempNss, reshardKeyPattern, shards});
+
+        ChunkManager cm(shards[0],
+                        DatabaseVersion(UUID::gen(), Timestamp(1, 0)),
+                        makeStandaloneRoutingTableHistory(
+                            RoutingTableHistory::makeNew(kNss,
+                                                         collIdentifier,
+                                                         shardKeyPattern,
+                                                         nullptr,
+                                                         false,
+                                                         collEpoch,
+                                                         collTimestamp,
+                                                         boost::none /* timeseriesFields */,
+                                                         reshardingFields, /* reshardingFields */
+                                                         true,
+                                                         chunks)),
+                        boost::none);
+
+        return std::make_pair(chunks, cm);
+    }
+
+protected:
+    bool _withShardedCollection{false};
+    boost::optional<ShardVersion> _shardVersion;
+};
+
+class ShardingWriteRouterTestFixture : public WriteRouterTestFixture {
+public:
+    ShardingWriteRouterTestFixture() {
+        _withShardedCollection = true;
+    }
+};
+
+BENCHMARK_DEFINE_F(ShardingWriteRouterTestFixture, BM_InsertGetDestinedRecipient)
+(benchmark::State& state) {
+    // ShardingWriteRouter currently requires the ShardServer cluster role.
+    serverGlobalParams.clusterRole = ClusterRole::ShardServer;
+
+    for (auto keepRunning : state) {
+        benchmark::ClobberMemory();
+
+        const auto client = serviceContext()->makeClient("test");
+        const auto opCtx = client->makeOperationContext();
 
         OperationShardingState::setShardRole(
-            opCtx,
-            kNss,
-            ShardVersionFactory::make(
-                chunkManager,
-                originatorShard,
-                boost::optional<CollectionIndexes>(boost::none)) /* shardVersion */,
-            boost::none /* databaseVersion */);
+            opCtx.get(), kNss, _shardVersion, boost::none /* databaseVersion */);
 
-        // Configuring the filtering metadata such that calls to getCollectionDescription return
-        // what we want. Specifically the reshardingFields are what we use. Its specified by the
-        // chunkManager.
-        CollectionShardingRuntime::assertCollectionLockedAndAcquireExclusive(opCtx, kNss)
-            ->setFilteringMetadata(opCtx, CollectionMetadata(chunkManager, originatorShard));
+        Lock::DBLock dbLock{opCtx.get(), kNss.dbName(), MODE_IX};
+        Lock::CollectionLock collLock{opCtx.get(), kNss, MODE_IX};
 
-        catalogCache->setChunkManagerReturnValue(chunkManager);
-    }
-
-    auto mockNetwork = std::make_unique<executor::NetworkInterfaceMock>();
-
-    auto const grid = Grid::get(opCtx);
-    grid->init(
-        std::make_unique<ShardingCatalogClientImpl>(nullptr),
-        std::move(catalogCache),
-        std::make_unique<ShardRegistry>(opCtx->getServiceContext(), nullptr, boost::none),
-        std::make_unique<ClusterCursorManager>(opCtx->getServiceContext()->getPreciseClockSource()),
-        std::make_unique<BalancerConfiguration>(),
-        std::make_unique<executor::TaskExecutorPool>(),
-        mockNetwork.get());
-
-    // Note: mockNetwork in Grid will become a dangling pointer after this function, this
-    // is fine since the test shouldn't be using it.
-}
-
-void BM_InsertGetDestinedRecipient(benchmark::State& state) {
-    // ShardingWriteRouter currently requires the ShardServer cluster role.
-    serverGlobalParams.clusterRole = ClusterRole::ShardServer;
-
-    auto serviceContext = ServiceContext::make();
-    const auto client = serviceContext->makeClient("test");
-    const auto opCtx = client->makeOperationContext();
-
-    setupCatalogCacheMock(opCtx.get(), true /* withShardedColl */);
-    ShardingWriteRouter writeRouter(opCtx.get(), kNss);
-
-    for (auto keepRunning : state) {
-        benchmark::ClobberMemory();
-        auto shardId = writeRouter.getReshardingDestinedRecipient(BSON("_id" << 0));
-        ASSERT(shardId != boost::none);
-    }
-}
-
-void BM_UpdateGetDestinedRecipient(benchmark::State& state) {
-    // ShardingWriteRouter currently requires the ShardServer cluster role.
-    serverGlobalParams.clusterRole = ClusterRole::ShardServer;
-
-    auto serviceContext = ServiceContext::make();
-    const auto client = serviceContext->makeClient("test");
-    const auto opCtx = client->makeOperationContext();
-
-    setupCatalogCacheMock(opCtx.get(), true /* withShardedColl */);
-
-    for (auto keepRunning : state) {
-        benchmark::ClobberMemory();
         ShardingWriteRouter writeRouter(opCtx.get(), kNss);
         auto shardId = writeRouter.getReshardingDestinedRecipient(BSON("_id" << 0));
         ASSERT(shardId != boost::none);
     }
 }
 
-void BM_UnshardedDestinedRecipient(benchmark::State& state) {
-    serverGlobalParams.clusterRole = ClusterRole::None;
-
-    auto serviceContext = ServiceContext::make();
-    const auto client = serviceContext->makeClient("test");
-    const auto opCtx = client->makeOperationContext();
-
-    CollectionShardingStateFactory::set(
-        opCtx->getServiceContext(),
-        std::make_unique<CollectionShardingStateFactoryStandalone>(opCtx->getServiceContext()));
-
-    setupCatalogCacheMock(opCtx.get(), false /* withShardedColl */);
+BENCHMARK_DEFINE_F(ShardingWriteRouterTestFixture, BM_UpdateGetDestinedRecipient)
+(benchmark::State& state) {
+    // ShardingWriteRouter currently requires the ShardServer cluster role.
+    serverGlobalParams.clusterRole = ClusterRole::ShardServer;
 
     for (auto keepRunning : state) {
         benchmark::ClobberMemory();
+
+        const auto client = serviceContext()->makeClient("test");
+        const auto opCtx = client->makeOperationContext();
+
+        OperationShardingState::setShardRole(
+            opCtx.get(), kNss, _shardVersion, boost::none /* databaseVersion */);
+
+        Lock::DBLock dbLock{opCtx.get(), kNss.dbName(), MODE_IX};
+        Lock::CollectionLock collLock{opCtx.get(), kNss, MODE_IX};
+
+        ShardingWriteRouter writeRouter(opCtx.get(), kNss);
+        auto shardId = writeRouter.getReshardingDestinedRecipient(BSON("_id" << 0));
+        ASSERT(shardId != boost::none);
+    }
+}
+
+BENCHMARK_DEFINE_F(WriteRouterTestFixture, BM_UnshardedDestinedRecipient)
+(benchmark::State& state) {
+    serverGlobalParams.clusterRole = ClusterRole::None;
+
+    for (auto keepRunning : state) {
+        benchmark::ClobberMemory();
+
+        const auto client = serviceContext()->makeClient("test");
+        const auto opCtx = client->makeOperationContext();
+
+        Lock::DBLock dbLock{opCtx.get(), kNss.dbName(), MODE_IX};
+        Lock::CollectionLock collLock{opCtx.get(), kNss, MODE_IX};
+
         ShardingWriteRouter writeRouter(opCtx.get(), kNss);
         auto shardId = writeRouter.getReshardingDestinedRecipient(BSON("_id" << 0));
         ASSERT(shardId == boost::none);
     }
 }
 
-BENCHMARK(BM_InsertGetDestinedRecipient)
+BENCHMARK_REGISTER_F(ShardingWriteRouterTestFixture, BM_InsertGetDestinedRecipient)
     ->Range(1, 1 << 4)
     ->ThreadRange(1, ProcessInfo::getNumAvailableCores());
 
-BENCHMARK(BM_UpdateGetDestinedRecipient)
+BENCHMARK_REGISTER_F(ShardingWriteRouterTestFixture, BM_UpdateGetDestinedRecipient)
     ->Range(1, 1 << 4)
     ->ThreadRange(1, ProcessInfo::getNumAvailableCores());
 
-BENCHMARK(BM_UnshardedDestinedRecipient)
+BENCHMARK_REGISTER_F(WriteRouterTestFixture, BM_UnshardedDestinedRecipient)
     ->Range(1, 1 << 4)
     ->ThreadRange(1, ProcessInfo::getNumAvailableCores());
 
