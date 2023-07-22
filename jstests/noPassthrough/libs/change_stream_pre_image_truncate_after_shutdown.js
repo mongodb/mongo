1c341e93835667ab3f5b0297967c0bac64f02d10
Benety Goh <benety@mongodb.com>
2023-07-22 10:06:16 -0400

    Revert "SERVER-79098 change_stream_pre_image_truncate_after_shutdown.js doesn't retry after failed replTestStepUp"
    This reverts commit beccf5b230280d172b8b9f25c54888083244c30f.

--- jstests/noPassthrough/libs/change_stream_pre_image_truncate_after_shutdown.js
+++ jstests/noPassthrough/libs/change_stream_pre_image_truncate_after_shutdown.js
@@ -4,7 +4,6 @@ load('jstests/replsets/rslib.js');                 // For getLatestOp, getFirstO
 load("jstests/libs/collection_drop_recreate.js");  // For assertDropAndRecreateCollection.
 load("jstests/libs/change_stream_util.js");        // For kPreImagesCollectionDatabase,
                                                    // kPreImagesCollectionName, getPreImages.
-load("jstests/replsets/libs/rollback_test.js");    // For stepUp.
 
 /**
  * Test fixture which exposes several methods to test different pre-image truncate behaviors after
@@ -207,7 +206,7 @@ var PreImageTruncateAfterShutdownTest = class {
 
         if (runAsPrimary) {
             // The 'checkPreImageCollection' check relies on there being a writable primary.
-            stepUp(conn);
+            assert.commandWorked(conn.adminCommand({replSetStepUp: 1}));
         }
 
         this._assertNumPreImagesAfterShutdown({
