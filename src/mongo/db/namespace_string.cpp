a3204c5f71e09d21ba11e69b0dc70ef073f21228
Abdul Qadeer <abdul.qadeer@mongodb.com>
2023-07-31 20:20:01 +0000

    SERVER-79386 Cleanup movePrimary unused code

--- src/mongo/db/namespace_string.cpp
+++ src/mongo/db/namespace_string.cpp
@@ -202,21 +202,6 @@ NamespaceString NamespaceString::makeGlobalIndexNSS(const UUID& id) {
                            NamespaceString::kGlobalIndexCollectionPrefix + id.toString());
 }
 
-NamespaceString NamespaceString::makeMovePrimaryOplogBufferNSS(const UUID& migrationId) {
-    return NamespaceString(DatabaseName::kConfig,
-                           "movePrimaryOplogBuffer." + migrationId.toString());
-}
-
-NamespaceString NamespaceString::makeMovePrimaryCollectionsToCloneNSS(const UUID& migrationId) {
-    return NamespaceString(DatabaseName::kConfig,
-                           "movePrimaryCollectionsToClone." + migrationId.toString());
-}
-
-NamespaceString NamespaceString::makeMovePrimaryTempCollectionsPrefix(const UUID& migrationId) {
-    return NamespaceString(DatabaseName::kConfig,
-                           "movePrimaryRecipient." + migrationId.toString() + ".willBeDeleted.");
-}
-
 NamespaceString NamespaceString::makePreImageCollectionNSS(
     const boost::optional<TenantId>& tenantId) {
     return NamespaceString{tenantId, DatabaseName::kConfig.db(), kPreImagesCollectionName};
