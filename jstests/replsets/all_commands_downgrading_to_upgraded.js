7034c8a5e6a486cdbf6d1f39494252ff02b0b47e
Samy Lanka <samy.lanka@mongodb.com>
2023-07-31 17:59:39 +0000

    SERVER-74867 Remove commandIsDisabledOnLastLTS from relevant commands in all_commands_downgrading_to_upgraded.js

--- jstests/replsets/all_commands_downgrading_to_upgraded.js
+++ jstests/replsets/all_commands_downgrading_to_upgraded.js
@@ -231,13 +231,11 @@ const allCommands = {
         },
     },
     analyzeShardKey: {
-        // TODO SERVER-74867: Remove the skip once 7.0 is lastLTS.
-        skip: commandIsDisabledOnLastLTS,
         setUp: function(conn) {
             assert.commandWorked(conn.getDB(dbName).runCommand({create: collName}));
             assert.commandWorked(
                 conn.getDB('admin').runCommand({shardCollection: fullNs, key: {_id: 1}}));
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 1000; i++) {
                 assert.commandWorked(conn.getCollection(fullNs).insert({a: i}));
             }
         },
@@ -298,8 +296,6 @@ const allCommands = {
         isAdminCommand: true,
     },
     bulkWrite: {
-        // TODO SERVER-74867: Remove the skip once 7.0 is lastLTS.
-        skip: commandIsDisabledOnLastLTS,
         // TODO SERVER-67711: Remove check when this feature flag is removed.
         checkFeatureFlag: "BulkWriteCommand",
         isAdminCommand: true,
@@ -324,8 +320,6 @@ const allCommands = {
     checkMetadataConsistency: {
         isAdminCommand: true,
         isShardedOnly: true,
-        // TODO SERVER-74867: Remove the skip once 7.0 is lastLTS.
-        skip: commandIsDisabledOnLastLTS,
         // TODO SERVER-70396: Remove check when this feature flag is removed.
         checkFeatureFlag: "CheckMetadataConsistency",
         command: {checkMetadataConsistency: 1},
@@ -497,8 +491,6 @@ const allCommands = {
         isAdminCommand: true,
     },
     configureQueryAnalyzer: {
-        // TODO SERVER-74867: Remove the skip once 7.0 is lastLTS.
-        skip: commandIsDisabledOnLastLTS,
         setUp: function(conn) {
             assert.commandWorked(conn.getDB(dbName).runCommand({create: collName}));
             for (let i = 0; i < 10; i++) {
@@ -1224,6 +1216,7 @@ const allCommands = {
         skip: isDeprecated,
     },
     removeShard: {
+        // We cannot test removeShard because we need to be able to run addShard during set up.
         // This will be tested in FCV upgrade/downgrade passthroughs in the sharding
         // directory.
         skip: "cannot add shard while in downgrading FCV state",
@@ -1314,32 +1307,7 @@ const allCommands = {
         isShardedOnly: true,
         isAdminCommand: true,
     },
-    reshardCollection: {
-        // TODO SERVER-74867: Remove the skip once 7.0 is lastLTS.
-        skip: commandIsDisabledOnLastLTS,
-        isShardedOnly: true,
-        isAdminCommand: true,
-        setUp: function(conn) {
-            assert.commandWorked(conn.getDB(dbName).runCommand({create: collName}));
-            const testColl = conn.getCollection(fullNs);
-            assert.commandWorked(
-                conn.getDB('admin').runCommand({shardCollection: fullNs, key: {_id: 1}}));
-
-            // Build an index on the collection to support the resharding operation.
-            assert.commandWorked(testColl.createIndex({a: 1}));
-
-            // Insert some documents that will be resharded.
-            assert.commandWorked(testColl.insert({_id: 0, a: 0}));
-            assert.commandWorked(testColl.insert({_id: 1, a: 1}));
-        },
-        command: {
-            reshardCollection: fullNs,
-            key: {a: 1},
-        },
-        teardown: function(conn) {
-            assert.commandWorked(conn.getDB(dbName).runCommand({drop: collName}));
-        },
-    },
+    reshardCollection: {skip: cannotRunWhileDowngrading},
     revokePrivilegesFromRole: {
         setUp: function(conn) {
             assert.commandWorked(conn.getDB(dbName).runCommand({create: collName}));
@@ -1441,8 +1409,6 @@ const allCommands = {
         skip: "requires cloudFreeMonitoringEndpointURL setup",
     },
     setProfilingFilterGlobally: {
-        // TODO SERVER-74867: Remove the skip once 7.0 is lastLTS.
-        skip: commandIsDisabledOnLastLTS,
         command: {setProfilingFilterGlobally: 1, filter: {nreturned: 0}},
         expectFailure: true,
         expectedErrorCode:
@@ -1562,18 +1528,14 @@ const allCommands = {
         doesNotRunOnMongos: true,
     },
     transitionFromDedicatedConfigServer: {
-        // TODO SERVER-74867: Remove the skip once 7.0 is lastLTS.
-        skip: commandIsDisabledOnLastLTS,
-        command: {transitionFromDedicatedConfigServer: 1},
-        isShardedOnly: true,
-        isAdminCommand: true,
+        // This command uses addShard logic internally, so is not able to run with a transitionary
+        // FCV.
+        skip: cannotRunWhileDowngrading
     },
     transitionToDedicatedConfigServer: {
-        // TODO SERVER-74867: Remove the skip once 7.0 is lastLTS.
-        skip: commandIsDisabledOnLastLTS,
-        command: {transitionToDedicatedConfigServer: 1},
-        isShardedOnly: true,
-        isAdminCommand: true,
+        // In order to properly run this command, we'd need to add a shard or transition from a
+        // dedicated config server, which is not allowed in a transitionary FCV.
+        skip: cannotRunWhileDowngrading
     },
     update: {
         setUp: function(conn) {
