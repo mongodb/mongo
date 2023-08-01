2b16b28abc96c5e9aa545da9bc0293d058b2b708
Will Korteland <will.korteland@mongodb.com>
2023-08-01 04:09:19 +0000

    Import wiredtiger: e3cfaefe392309b44a9f7d79bfff317361cc0a9b from branch mongodb-master
    ref: 40888466a5..e3cfaefe39
    for: 7.1.0-rc0
    
    WT-11333 Update test/format to generate random compaction "free_space_target" values for foreground compaction

--- src/third_party/wiredtiger/test/format/compact.c
+++ src/third_party/wiredtiger/test/format/compact.c
@@ -41,6 +41,7 @@ compact(void *arg)
     WT_DECL_RET;
     WT_SESSION *session;
     u_int period;
+    char config_buf[128];
 
     (void)(arg);
 
@@ -71,7 +72,9 @@ compact(void *arg)
          * don't configure a timeout and occasionally exceed the default of 1200 seconds.
          */
         table = table_select(NULL, false);
-        ret = session->compact(session, table->uri, NULL);
+        testutil_snprintf(config_buf, sizeof(config_buf), "free_space_target=%" PRIu32 "MB",
+          GV(COMPACT_FREE_SPACE_TARGET));
+        ret = session->compact(session, table->uri, config_buf);
         testutil_assertfmt(ret == 0 || ret == EBUSY || ret == ETIMEDOUT || ret == WT_CACHE_FULL ||
             ret == WT_ROLLBACK,
           "WT_SESSION.compact failed: %s: %d", table->uri, ret);
