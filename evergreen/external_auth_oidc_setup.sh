343a5ec7e78460bac6ee3b1edf7d3b7649f31671
Adrian Gonzalez <adriangonzalezmontemayor@gmail.com>
2023-08-03 21:43:07 +0000

    SERVER-77913 Extend OIDC jstests to use Ping Identity for Human User Workflows

--- evergreen/external_auth_oidc_setup.sh
+++ evergreen/external_auth_oidc_setup.sh
@@ -8,6 +8,9 @@ set -o errexit
 # Should output contents to new file in home directory.
 cat << EOF > $HOME/oidc_e2e_setup.json
 {
+    "testserversecurityone@ping-test.com" : "${oidc_ping_test_user_account_one_secret}",
+    "testserversecuritytwo@ping-test.com" : "${oidc_ping_test_user_account_two_secret}",
+    "testserversecuritythree@ping-test.com" : "${oidc_ping_test_user_account_three_secret}",
     "tD548GwE1@outlook.com" : "${oidc_azure_test_user_account_one_secret}",
     "tD548GwE2@outlook.com" : "${oidc_azure_test_user_account_two_secret}",
     "tD548GwE3@outlook.com" : "${oidc_azure_test_user_account_three_secret}",
