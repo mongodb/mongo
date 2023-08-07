14e9e4cbf50aa44307507c9aa23d416336ab40dd
Steve Gross <steve.gross@mongodb.com>
2023-08-07 14:22:01 +0000

    SERVER-79733 Add mechanism to build shared library

--- site_scons/site_tools/integrate_bazel.py
+++ site_scons/site_tools/integrate_bazel.py
@@ -20,22 +20,33 @@ def generate(env):
         # Get info about targets & paths.
         # For a target such as 'fsync_locked', the variables would be:
         #   target_to_build: fsync_locked
-        #   cwd:             /home/ubuntu/mongo/build/fast/mongo/db/commands
-        #   bazel_dir:       src/mongo/db/command
-        #   bazel_target:    //src/mongo/db/commands:fsync_locked
-        #   bazel_outfile:   bazel-bin/src/mongo/db/commands/libfsync_locked.a
-        #   scons_outfile:   /home/ubuntu/mongo/build/fast/mongo/db/commands/libfsync_locked.a
+        #   cwd:                 /home/ubuntu/mongo/build/fast/mongo/db/commands
+        #   bazel_dir:           src/mongo/db/command
+        #   bazel_target:        //src/mongo/db/commands:fsync_locked
+        #   bazel_outfile_base:  bazel-bin/src/mongo/db/commands/libfsync_locked
+        #   scons_outfile_base:  /home/ubuntu/mongo/build/fast/mongo/db/commands/libfsync_locked
         target_to_build = target[0]
         cwd = os.getcwd()
         bazel_dir = os.path.dirname(env.File(os.path.join(cwd, target_to_build)).srcnode().path)
         bazel_target = f"//{bazel_dir}:{target_to_build}"
-        bazel_outfile = f"bazel-bin/{bazel_dir}/lib{target_to_build}.a"
-        scons_outfile = f"{cwd}/lib{target_to_build}.a"
+        bazel_outfile_base = f"bazel-bin/{bazel_dir}/lib{target_to_build}"
+        scons_outfile_base = f"{cwd}/lib{target_to_build}"
 
-        # Craft an action that builds the target and colocates it with Scons output, and invoke the action:
-        action = f"{BAZELISK_PATH} build {bazel_target} && cp -f {bazel_outfile} {scons_outfile}"
-        env['BUILDERS']['_BazelBuild'] = SCons.Builder.Builder(action=action)
-        return env._BazelBuild(target=scons_outfile,
+        # Craft series of build actions:
+        env['BUILDERS']['_BazelBuild'] = SCons.Builder.Builder(action=" && ".join([
+            # Build the target via Bazel
+            f"{BAZELISK_PATH} build {bazel_target}",
+
+            # Colocate the Bazel static library with the SCons output:
+            f"cp -f {bazel_outfile_base}.a {scons_outfile_base}.a",
+
+            # Colocate the Bazel dynamic library with the SCons output:
+            f"cp -f {bazel_outfile_base}.so {scons_outfile_base}.so"
+        ]))
+
+        # Invoke the build (note that we only specify static library target; the build action
+        # will build both static and dynamic as a result):
+        return env._BazelBuild(target=f"{scons_outfile_base}.a",
                                source=[])  # `source` is required, even though it is empty
 
     if env.get("BAZEL_BUILD_ENABLED"):
