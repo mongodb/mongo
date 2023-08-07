9f727bde6c0ae42265f7d4e5e281df52acef1b2a
Lingzhi Deng <lingzhi.deng@mongodb.com>
2023-08-07 11:23:55 -0400

    Revert "SERVER-78813: Fix commit point propagation for exhaust oplog cursors"
    This reverts commit 2f36b8afb61df115001b5c7b201d98a4a227fca4.

--- src/mongo/db/query/plan_insert_listener.cpp
+++ src/mongo/db/query/plan_insert_listener.cpp
@@ -65,10 +65,9 @@ bool shouldWaitForInserts(OperationContext* opCtx,
         // coordinator's lastCommittedOpTime has progressed past the client's lastCommittedOpTime.
         // In that case, we will return early so that we can inform the client of the new
         // lastCommittedOpTime immediately.
-        if (clientsLastKnownCommittedOpTime(opCtx)) {
+        if (!clientsLastKnownCommittedOpTime(opCtx).isNull()) {
             auto replCoord = repl::ReplicationCoordinator::get(opCtx);
-            return clientsLastKnownCommittedOpTime(opCtx).value() >=
-                replCoord->getLastCommittedOpTime();
+            return clientsLastKnownCommittedOpTime(opCtx) >= replCoord->getLastCommittedOpTime();
         }
         return true;
     }
