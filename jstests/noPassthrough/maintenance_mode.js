20740e4e35cd0c83bf4c4a36a45f0edd20e7ab85
wenqinYe <wenqin908@gmail.com>
2023-08-11 20:35:19 +0000

    SERVER-79618: Allow mongod to run in replica set mode when --maintenanceMode is replicaSet

--- jstests/noPassthrough/maintenance_mode.js
+++ jstests/noPassthrough/maintenance_mode.js
@@ -1,5 +1,5 @@
 /*
- * Test that mongod accepts the --maintenanceMode parameter.
+ * Test that mongod accepts the --maintenanceMode parameter and that it works as expected.
  *
  * @tags: [
  *   multiversion_incompatible,
@@ -9,41 +9,73 @@
 (function() {
 "use strict";
 
-// --maintenanceMode=replicaSet should succeed
-let node = MongoRunner.runMongod({
-    maintenanceMode: "replicaSet",
-    setParameter: {
-        featureFlagAllMongodsAreSharded: true,
-    },
-});
-MongoRunner.stopMongod(node);
-
-// --maintenanceMode=standalone should succeed
-node = MongoRunner.runMongod({
-    maintenanceMode: "standalone",
-    setParameter: {
-        featureFlagAllMongodsAreSharded: true,
-    },
-});
-MongoRunner.stopMongod(node);
-
-// --maintenanceMode should fail
-assert.throws(() => {
-    MongoRunner.runMongod({
-        maintenanceMode: "",
+{
+    // --maintenanceMode=replicaSet should auto-initiate a replicaSet but skip auto-bootstrapping a
+    // configShard.
+    const node = MongoRunner.runMongod({
+        maintenanceMode: "replicaSet",
         setParameter: {
             featureFlagAllMongodsAreSharded: true,
         },
     });
-});
 
-// --maintenanceMode=nonValidString should fail
-assert.throws(() => {
-    MongoRunner.runMongod({
-        maintenanceMode: "nonValidString",
+    // Node should not be a config server.
+    assert.soon(() => node.adminCommand({hello: 1}).isWritablePrimary);
+    const config = assert.commandWorked(node.adminCommand({replSetGetConfig: 1})).config;
+    assert(!config.hasOwnProperty('configsvr'), tojson(config));
+
+    // Sharding should not be enabled.
+    let findCmd = {
+        find: 'user',
+        filter: {_id: 9876},
+        // Attaching databaseVersion tells the server we are running a sharding command.
+        databaseVersion: {
+            uuid: new UUID("ebee4d32-ef8b-40cb-b75e-b45fbe4042dc"),
+            timestamp: new Timestamp(1691525961, 12),
+            lastMod: NumberInt(5),
+        }
+    };
+    assert.commandFailedWithCode(node.getDB('test').runCommand(findCmd),
+                                 ErrorCodes.NoShardingEnabled);
+
+    // Inserts into replicated collections should succeed.
+    assert.commandWorked(node.getDB('test').runCommand({insert: 'testColl', documents: [{x: 1}]}));
+
+    MongoRunner.stopMongod(node);
+}
+
+{
+    // --maintenanceMode=standalone should succeed.
+    const node = MongoRunner.runMongod({
+        maintenanceMode: "standalone",
         setParameter: {
             featureFlagAllMongodsAreSharded: true,
         },
     });
-});
+    MongoRunner.stopMongod(node);
+}
+
+{
+    // --maintenanceMode should fail.
+    assert.throws(() => {
+        MongoRunner.runMongod({
+            maintenanceMode: "",
+            setParameter: {
+                featureFlagAllMongodsAreSharded: true,
+            },
+        });
+    });
+}
+
+{
+    // --maintenanceMode=nonValidString should fail.
+    assert.throws(() => {
+        MongoRunner.runMongod({
+            maintenanceMode: "nonValidString",
+            setParameter: {
+                featureFlagAllMongodsAreSharded: true,
+            },
+        });
+    });
+}
 })();
