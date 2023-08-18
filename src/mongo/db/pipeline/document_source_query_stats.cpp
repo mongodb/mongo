ca32244a6da94061ea93fe4861c784fbdaa6e652
Naama Bareket <naama.bareket@mongodb.com>
2023-08-18 15:02:53 +0000

    SERVER-78083: Add ability to log $queryStats output on each invocation

--- src/mongo/db/pipeline/document_source_query_stats.cpp
+++ src/mongo/db/pipeline/document_source_query_stats.cpp
@@ -58,7 +58,7 @@
 #include "mongo/util/str.h"
 #include "mongo/util/time_support.h"
 
-#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kQuery
+#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kQueryStats
 
 namespace mongo {
 namespace {
@@ -148,6 +148,12 @@ boost::intrusive_ptr<DocumentSource> DocumentSourceQueryStats::createFromBson(
             "$queryStats must be run against the 'admin' database with {aggregate: 1}",
             nss.isAdminDB() && nss.isCollectionlessAggregateNS());
 
+    LOGV2_DEBUG_OPTIONS(7808300,
+                        1,
+                        {logv2::LogTruncation::Disabled},
+                        "Logging invocation $queryStats",
+                        "commandSpec"_attr =
+                            spec.Obj().redact(BSONObj::RedactLevel::sensitiveOnly));
     return parseSpec(spec, [&](TransformAlgorithmEnum algorithm, std::string hmacKey) {
         return new DocumentSourceQueryStats(pExpCtx, algorithm, hmacKey);
     });
@@ -182,12 +188,20 @@ DocumentSource::GetNextResult DocumentSourceQueryStats::doGetNext() {
      * The inner iterator iterates over a materialized container of all entries in the partition.
      * This is done to reduce the time under which the partition lock is held.
      */
+    bool shouldLog = _algorithm != TransformAlgorithmEnum::kNone;
     while (true) {
         // First, attempt to exhaust all elements in the materialized partition.
         if (!_materializedPartition.empty()) {
             // Move out of the container reference.
             auto doc = std::move(_materializedPartition.front());
             _materializedPartition.pop_front();
+            if (shouldLog) {
+                LOGV2_DEBUG_OPTIONS(7808301,
+                                    3,
+                                    {logv2::LogTruncation::Disabled},
+                                    "Logging all outputs of $queryStats",
+                                    "thisOutput"_attr = doc);
+            }
             return {std::move(doc)};
         }
 
@@ -196,6 +210,12 @@ DocumentSource::GetNextResult DocumentSourceQueryStats::doGetNext() {
         // Materialized partition is exhausted, move to the next.
         _currentPartition++;
         if (_currentPartition >= _queryStatsStore.numPartitions()) {
+            if (shouldLog) {
+                LOGV2_DEBUG_OPTIONS(7808302,
+                                    3,
+                                    {logv2::LogTruncation::Disabled},
+                                    "Finished logging outout of $queryStats");
+            }
             return DocumentSource::GetNextResult::makeEOF();
         }
 
