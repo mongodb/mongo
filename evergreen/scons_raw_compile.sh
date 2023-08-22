67a8627cbc91078f46e5826a1b8f3c1a5717f453
Zack Winter <zack.winter@mongodb.com>
2023-08-22 18:38:56 +0000

    SERVER-70715 Add dev compilation tests for windows

---
+++ evergreen/scons_raw_compile.sh
@@ -0,0 +1,23 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+. "$DIR/prelude.sh"
+
+# Compile without extra args from evergreen to better simulate local dev compilation.
+
+cd src
+
+set -o errexit
+set -o verbose
+
+if [ "Windows_NT" = "$OS" ]; then
+  vcvars="$(vswhere -latest -property installationPath | tr '\\' '/' | dos2unix.exe)/VC/Auxiliary/Build/"
+  export PATH="$(echo "$(cd "$vcvars" && cmd /C "vcvarsall.bat amd64 && C:/cygwin/bin/bash -c 'echo \$PATH'")" | tail -n +6)":$PATH
+fi
+activate_venv
+
+set -o pipefail
+
+eval ${compile_env} $python ./buildscripts/scons.py \
+  ${compile_flags} ${task_compile_flags} \
+  ${targets} | tee scons_stdout.log
+
+exit $?
