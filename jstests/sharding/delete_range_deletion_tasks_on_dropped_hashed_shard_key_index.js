df06d8718f78715c7b5f2be3f40a4708493dd671
Sanika Phanse <sanika.phanse@mongodb.com>
2023-08-24 14:39:22 +0000

    SERVER-79632 Stop range deletion when hashed shard key index does not exist

---
+++ jstests/sharding/delete_range_deletion_tasks_on_dropped_hashed_shard_key_index.js
@@ -0,0 +1,76 @@
+/**
+ * Tests that deleting the index on a hashed shard key blocks its orphan documents from being
+ * deleted and allows other range deletion processes to continue.
+ *
+ * @tags: [
+ *   requires_fcv_71,
+ *   featureFlagShardKeyIndexOptionalHashedSharding
+ * ]
+ */
+import {findChunksUtil} from "jstests/sharding/libs/find_chunks_util.js";
+
+const rangeDeleterBatchSize = 50;
+
+const st = new ShardingTest({
+    other: {
+        enableBalancer: false,
+        shardOptions: {setParameter: {rangeDeleterBatchSize: rangeDeleterBatchSize}},
+    }
+})
+
+// Setup database and collection for test
+const dbName = 'db';
+const db = st.getDB(dbName);
+assert.commandWorked(
+    st.s.adminCommand({enableSharding: dbName, primaryShard: st.shard0.shardName}));
+const coll = db['test'];
+const collWithIndex = db['collWithIndex'];
+
+function setUpCollection(collectionName, nss) {
+    // This creates an index on the hashed shard key.
+    assert.commandWorked(st.s.adminCommand({shardCollection: nss, key: {_id: 'hashed'}}));
+
+    // Insert some documents into the collection.
+    const numDocs = 1000;
+    let bulk = collectionName.initializeUnorderedBulkOp();
+    for (let i = 0; i < numDocs; i++) {
+        bulk.insert({_id: i});
+    }
+    assert.commandWorked(bulk.execute());
+
+    // Move a chunk to create orphan documents.
+    const chunk =
+        findChunksUtil.findOneChunkByNs(st.s.getDB('config'), nss, {shard: st.shard0.shardName});
+    assert.commandWorked(
+        db.adminCommand({moveChunk: nss, bounds: [chunk.min, chunk.max], to: st.shard1.shardName}));
+}
+
+setUpCollection(coll, coll.getFullName());
+setUpCollection(collWithIndex, collWithIndex.getFullName());
+assert.commandWorked(coll.dropIndex({"_id": "hashed"}));
+
+// Verify that the range deletion document for db.test persists while the document for
+// db.collWithIndex is successfully deleted.
+assert.eq(1, st.shard0.getDB("config").getCollection("rangeDeletions").countDocuments({
+    nss: coll.getFullName()
+}));
+
+assert.soon(() => {
+    return st.shard0.getDB("config").getCollection("rangeDeletions").countDocuments({
+        nss: collWithIndex.getFullName()
+    }) === 0;
+});
+
+assert.eq(1, st.shard0.getDB("config").getCollection("rangeDeletions").countDocuments({
+    nss: coll.getFullName()
+}));
+
+// Rebuild the hashed shard key index for db.test and ensure the pending range deletion completes.
+assert.commandWorked(coll.createIndex({"_id": "hashed"}));
+assert.soon(() => {
+    return st.shard0.getDB("config").getCollection("rangeDeletions").countDocuments({
+        nss: coll.getFullName()
+    }) === 0;
+});
+
+st.stop();
