b021f397d84367bd32705e04e0bce4f06b1fcf04
Gil Alon <gil.alon@mongodb.com>
2023-08-24 19:18:18 +0000

    SERVER-80077 Add more feature flag tests for timeseries fixed bucketing flag

--- jstests/multiVersion/targetedTestsLastLtsFeatures/timeseries_fixed_param_flag.js
+++ jstests/multiVersion/targetedTestsLastLtsFeatures/timeseries_fixed_param_flag.js
@@ -59,5 +59,47 @@ output = assert.commandWorked(
     testDB.runCommand({timeseriesCatalogBucketParamsChanged: 'system.buckets.coll'}));
 assert.eq(output.changed, true);
 
+// Now that the feature is turned on, new time-series collections by default will have the flag set
+// to 'false'.
+const otherCollName = "other_coll";
+const otherCollBucketsName = "system.buckets.other_coll";
+assert.commandWorked(testDB.createCollection(
+    otherCollName,
+    {timeseries: {timeField: 't', bucketRoundingSeconds: 3600, bucketMaxSpanSeconds: 3600}}));
+output = assert.commandWorked(
+    testDB.runCommand({timeseriesCatalogBucketParamsChanged: otherCollBucketsName}));
+assert.eq(output.changed, false);
+
+// We will now test the flag is set correctly if the FCV downgrade fails after only reaching the
+// `downgrading` FCV state, and then is fully upgraded again.
+jsTestLog("Turning the failpoint on.");
+assert.commandWorked(
+    rst.getPrimary().adminCommand({configureFailPoint: 'failDowngrading', mode: "alwaysOn"}));
+assert.commandFailedWithCode(
+    testDB.adminCommand({setFeatureCompatibilityVersion: '7.0', confirm: true}), 549181);
+
+// Confirm feature is still enabled since the FCV downgrade failed.
+output = assert.commandWorked(
+    testDB.runCommand({timeseriesCatalogBucketParamsChanged: otherCollBucketsName}));
+assert.eq(output.changed, false);
+
+assert.commandWorked(rst.getPrimary().getDB("admin").runCommand(
+    {setFeatureCompatibilityVersion: '7.1', confirm: true}));
+
+// Confirm the feature remains enabled when the FCV is upgraded. Since the FCV is already at '7.1'
+// the value of the flag should not change.
+output = assert.commandWorked(
+    testDB.runCommand({timeseriesCatalogBucketParamsChanged: otherCollBucketsName}));
+assert.eq(output.changed, false);
+
+// The flag should be set to true after a collMod command.
+assert.commandWorked(testDB.runCommand({
+    collMod: otherCollName,
+    timeseries: {bucketRoundingSeconds: 100000, bucketMaxSpanSeconds: 100000}
+}));
+output = assert.commandWorked(
+    testDB.runCommand({timeseriesCatalogBucketParamsChanged: otherCollBucketsName}));
+assert.eq(output.changed, true);
+
 rst.stopSet();
 })();
