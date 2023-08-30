c5c3086dcae61481f80664f1de1dcd965aee2147
Pol Pinol Castuera <pol.pinol@mongodb.com>
2023-08-30 06:21:33 +0000

    SERVER-78318 ConfigsvrCollMod command should not support retryable writes

--- src/mongo/db/s/config/configsvr_collmod_command.cpp
+++ src/mongo/db/s/config/configsvr_collmod_command.cpp
@@ -79,10 +79,6 @@ public:
         return Command::AllowedOnSecondary::kNever;
     }
 
-    bool supportsRetryableWrite() const final {
-        return true;
-    }
-
     class Invocation final : public InvocationBase {
     public:
         using InvocationBase::InvocationBase;
