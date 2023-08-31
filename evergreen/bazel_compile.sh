d41b142da0c91f1905d6332b5e7bd741f2cdfdc2
Steve Gross <steve.gross@mongodb.com>
2023-08-31 19:24:22 +0000

    SERVER-80381 Amend Bazel task to include clang option

--- evergreen/bazel_compile.sh
+++ evergreen/bazel_compile.sh
@@ -1,3 +1,10 @@
+# Usage:
+#   bazel_compile [arguments]
+#
+# Required environment variables:
+# * ${targets} - List of build targets
+# * ${compiler} - One of [clang|gcc]
+
 # Needed for evergreen scripts that use evergreen expansions and utility methods.
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 . "$DIR/prelude.sh"
@@ -7,4 +14,4 @@ cd src
 set -o errexit
 set -o verbose
 
-./bazelisk build --verbose_failures ${targets}
+./bazelisk build --verbose_failures --//bazel_config:compiler_type={$compiler} ${targets}
