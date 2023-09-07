95f8ff78f52cd48415f82d9ee87a96d73c7e3497
Ali Mir <ali.mir@mongodb.com>
2023-09-07 16:54:23 +0000

    SERVER-66018 Create read preference metrics counters

---
+++ jstests/noPassthrough/read_preference_metrics.js
@@ -0,0 +1,28 @@
+/**
+ * Tests that read preference metrics are correctly collected on mongod.
+ */
+
+// Test that mongod tracks metrics around read preference.
+const rst = new ReplSetTest({nodes: 1});
+
+rst.startSet();
+rst.initiate();
+const primary = rst.getPrimary();
+
+let serverStatus = assert.commandWorked(primary.getDB("admin").runCommand({serverStatus: 1}));
+assert(serverStatus.process.startsWith("mongod"), tojson(serverStatus));
+assert(serverStatus.hasOwnProperty("readPreferenceCounters"), tojson(serverStatus));
+rst.stopSet();
+
+// Test that mongos omits metrics around read preference, and shard servers include them.
+const st = new ShardingTest({shards: 1});
+
+serverStatus = assert.commandWorked(st.s.getDB("admin").runCommand({serverStatus: 1}));
+assert(serverStatus.process.startsWith("mongos"), tojson(serverStatus));
+assert(!serverStatus.hasOwnProperty("readPreferenceCounters"), tojson(serverStatus));
+
+serverStatus = assert.commandWorked(st.shard0.getDB("admin").runCommand({serverStatus: 1}));
+assert(serverStatus.process.startsWith("mongod"), tojson(serverStatus));
+assert(serverStatus.hasOwnProperty("readPreferenceCounters"), tojson(serverStatus));
+
+st.stop();
