67c83724523c97a4905e65a90ca81b248476b8c9
Ali Mir <ali.mir@mongodb.com>
2023-09-07 16:54:23 +0000

    SERVER-66018 Create read preference metrics counters
    (cherry picked from commit 95f8ff78f52cd48415f82d9ee87a96d73c7e3497)

--- src/mongo/db/stats/read_preference_metrics.h
+++ src/mongo/db/stats/read_preference_metrics.h
@@ -29,10 +29,21 @@
 
 #pragma once
 
+#include "mongo/client/read_preference.h"
 #include "mongo/db/operation_context.h"
+#include "mongo/db/repl/member_state.h"
 #include "mongo/db/service_context.h"
+#include "mongo/db/stats/read_preference_metrics_gen.h"
 
 namespace mongo {
+
+/**
+ * Contains server-wide metrics on read preference usage. Metrics are first split into two
+ * sub-documents that specify if the operation containing the read preference executed while the
+ * node was a primary or secondary. For each sub-document, metrics are collected for each read
+ * preference mode and if a tag set list was used. For each metric, operations are
+ * further split into internal and external operations.
+ */
 
 class ReadPreferenceMetrics {
     ReadPreferenceMetrics(const ReadPreferenceMetrics&) = delete;
@@ -44,7 +55,40 @@ public:
     static ReadPreferenceMetrics* get(ServiceContext* service);
     static ReadPreferenceMetrics* get(OperationContext* opCtx);
 
-    BSONObj toBSON() const;
+    void recordReadPreference(ReadPreferenceSetting readPref,
+                              bool isInternal,
+                              repl::MemberState state);
+
+    // Generates the overall read preference metrics document, with two sub-documents for operations
+    // that executed while in primary state and secondary state. This function and functions called
+    // within it construct the metrics IDL classes, fill their values, and serialize them to BSON
+    // for serverStatus.
+    void generateMetricsDoc(ReadPreferenceMetricsDoc* stats);
+
+private:
+    struct Counter {
+        AtomicWord<unsigned long long> internal{0};
+        AtomicWord<unsigned long long> external{0};
+
+        // Loads an individual counter's internal and external operation counters into a
+        // 'ReadPrefOps' object.
+        ReadPrefOps toReadPrefOps() const;
+    };
+    struct Counters {
+        Counter primary;
+        Counter primaryPreferred;
+        Counter secondary;
+        Counter secondaryPreferred;
+        Counter nearest;
+        Counter tagged;
+
+        // Flushes all the individual read preference metrics while in a particular replica set
+        // state, which is part of generating the overall read preference metrics document.
+        void flushCounters(ReadPrefDoc* doc);
+    };
+
+    Counters primaryCounters;
+    Counters secondaryCounters;
 };
 
 }  // namespace mongo
