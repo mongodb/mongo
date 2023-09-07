0fbfd003fe27c33faf649e2a8fecc38786827075
Steve Gross <steve.gross@mongodb.com>
2023-09-07 14:38:20 +0000

    SERVER-80547 Fix scons/bazel build, and add it to Evergreen CI

--- site_scons/site_tools/integrate_bazel.py
+++ site_scons/site_tools/integrate_bazel.py
@@ -19,23 +19,29 @@ def generate(env):
     def bazel_library(env, target, source, *args, **kwargs):
         # Get info about targets & paths.
         # For a target such as 'fsync_locked', the variables would be:
-        #   target_to_build: fsync_locked
-        #   cwd:                 /home/ubuntu/mongo/build/fast/mongo/db/commands
-        #   bazel_dir:           src/mongo/db/command
-        #   bazel_target:        //src/mongo/db/commands:fsync_locked
-        #   bazel_outfile_base:  bazel-bin/src/mongo/db/commands/libfsync_locked
-        #   scons_outfile_base:  /home/ubuntu/mongo/build/fast/mongo/db/commands/libfsync_locked
+        #   target_to_build:       fsync_locked
+        #   cwd:                   /home/ubuntu/mongo/src/mongo/db/commands
+        #   bazel_dir:             src/mongo/db/commands
+        #   bazel_target:          //src/mongo/db/commands:fsync_locked
+        #   bazel_outfile_base:    bazel-bin/src/mongo/db/commands/libfsync_locked
+        #   base_dir_from_scons:   build/fast
+        #   scons_outfile_relpath: build/fast/mongo/db/commands
+        #   scons_outfile_base:    /home/ubuntu/mongo/build/fast/mongo/db/commands/libfsync_locked
         target_to_build = target[0]
         cwd = os.getcwd()
         bazel_dir = os.path.dirname(env.File(os.path.join(cwd, target_to_build)).srcnode().path)
         bazel_target = f"//{bazel_dir}:{target_to_build}"
         bazel_outfile_base = f"bazel-bin/{bazel_dir}/lib{target_to_build}"
-        scons_outfile_base = f"{cwd}/lib{target_to_build}"
-
+        # Note: The IDLCFLAGS are of the form: [..., '--base_dir', 'build/fast', ...]; the logic
+        # below fetches the value immediately after "--base_dir".
+        base_dir_from_scons = env.Dictionary()['IDLCFLAGS'][
+            env.Dictionary()['IDLCFLAGS'].index('--base_dir') + 1]
+        scons_outfile_relpath = bazel_dir.replace('src/', f'{base_dir_from_scons}/')
+        scons_outfile_base = cwd.replace(bazel_dir, f'{scons_outfile_relpath}/lib{target_to_build}')
         static_link = env.GetOption("link-model") in ["auto", "static"]
         if not env.ToolchainIs('gcc', 'clang'):
             raise Exception(f"Toolchain {env.ToolchainName()} is neither `gcc` nor `clang`")
-        compile_args = [f"--//bazel_config:compiler_type={env.ToolchainName()}"]
+        compile_args = [f"--//bazel/config:compiler_type={env.ToolchainName()}"]
         link_args = ["--dynamic_mode=off"] if static_link else ["--dynamic_mode=fully"]
 
         # Craft series of build actions:
