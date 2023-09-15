909f17054c729cd05e3b4c40a0ca284222b833bc
Nandini Bhartiya <nandini.bhartiya@mongodb.com>
2023-09-15 20:29:36 +0000

    SERVER-79682: Ensure the opCtx is interrupted during a stepdown for shardSvrReshardCollection cmd.

--- src/mongo/db/s/shardsvr_reshard_collection_command.cpp
+++ src/mongo/db/s/shardsvr_reshard_collection_command.cpp
@@ -85,6 +85,7 @@ public:
         using InvocationBase::InvocationBase;
 
         void typedRun(OperationContext* opCtx) {
+            opCtx->setAlwaysInterruptAtStepDownOrUp_UNSAFE();
             uassertStatusOK(ShardingState::get(opCtx)->canAcceptShardedCommands());
             uassert(ErrorCodes::IllegalOperation,
                     "Can't reshard a collection in the config database",
