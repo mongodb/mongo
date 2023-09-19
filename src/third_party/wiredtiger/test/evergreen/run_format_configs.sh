b3fc46f234deb340704ff248b56d4b255b29a75b
Etienne Petrel <etienne.petrel@mongodb.com>
2023-09-19 01:19:51 +0000

    Import wiredtiger: c2bffff104f1c8538b815770ff4f7586bf3fe1de from branch mongodb-7.0
    ref: 8735a1966f..c2bffff104
    for: 7.0.2
    
    WT-11222 Fix run_format_configs to execute failed configs in parallel

--- src/third_party/wiredtiger/test/evergreen/run_format_configs.sh
+++ src/third_party/wiredtiger/test/evergreen/run_format_configs.sh
@@ -3,8 +3,7 @@
 # Cycle through a list of test/format configurations that failed previously,
 # and run format test against each of those configurations to capture issues.
 #
-
-set -u
+# Note - The failed configs must not require restart (such as abort)
 
 set -e
 # Switch to the Git repo toplevel directory
@@ -21,19 +20,101 @@ fi
 
 success=0
 failure=0
+running=0
+parallel_jobs=8
+cleanup=0
+PID=""
+declare -a PID_LIST
 
+usage() {
+	echo "usage: $0 "
+	echo "    -j <num_of_jobs>  number of jobs to execute in parallel (defaults to 8)"
+
+	exit 1
+}
+
+# Wait for other format runs.
+wait_for_process()
+{
+	while true ; do
+		for process in ${PID_LIST[@]};do
+			ps $process > /dev/null; ps_exit_status=$?
+			if [ ${ps_exit_status} -eq "1" ] ; then
+				# The process is completed so remove the process id from the list of processes.
+				PID_LIST=(${PID_LIST[@]/$process})
+
+				# Wait for the process to get the exit status.
+				wait $process
+				exit_status=$?
+
+				let "running--"
+
+				config_name=`egrep $process $tmp_file | awk -F ":" '{print $2}' | rev | awk -F "/" '{print $1}' | rev`
+				if [ $exit_status -ne "0" ]; then
+					let "failure++"
+					[ -f WT_TEST_${config_name}/CONFIG ] && cat WT_TEST_${config_name}/CONFIG
+				else
+					let "success++"
+					# Remove database files of successful jobs.
+					[ -d WT_TEST_${config_name} ] && rm -rf WT_TEST_${config_name}
+				fi
+
+				echo "Exit status of config ${config_name} is ${exit_status}"
+				# Continue checking other runnung process status before exiting the for loop.
+				continue
+			fi
+		done
+
+		if [ ${cleanup} -ne 0 ]; then
+			# Cleanup, wait for all the remaining processes to complete.
+			[ ${#PID_LIST[@]} -eq 0 ] && break
+		elif [ $running -lt $parallel_jobs ]; then
+			# Break and invoke a new process if any of the process have completed.
+			break
+		fi
+
+		# Sleep for 2 seconds before iterating the list of running processes.
+		sleep 2
+	done
+}
+
+while true ; do
+	case "$1" in
+	-j)
+		parallel_jobs="$2"
+		[[ "$parallel_jobs" =~ ^[1-9][0-9]*$ ]] ||
+			fatal_msg "-j option argument must be a non-zero integer"
+		shift ; shift ;;
+	-*)
+		usage ;;
+	*)
+		break ;;
+	esac
+done
+
+tmp_file="format_list.txt"
+touch $tmp_file
 # Cycle through format CONFIGs recorded under the "failure_configs" directory
 for config in $(find ../../../test/format/failure_configs/ -name "CONFIG.*" | sort)
 do
 	echo -e "\nTesting CONFIG $config ...\n"
-	if (./t -1 -c $config -h WT_TEST); then
-		let "success++"
-	else
-		let "failure++"
-		[ -f WT_TEST/CONFIG ] && cat WT_TEST/CONFIG
-		mv WT_TEST WT_TEST_$(basename $config)
+	basename_config=$(basename $config)
+	./t -1 -c $config -h WT_TEST_$basename_config &
+	let "running++"
+
+	PID="$!"
+	PID_LIST+=("$PID")
+
+	echo "$PID:$config" >> $tmp_file
+
+	if [ ${running} -ge ${parallel_jobs} ]; then
+		wait_for_process
 	fi
 done
+
+# Cleanup, wait for all the remaining processes to complete.
+cleanup=1
+wait_for_process
 
 echo -e "\nSummary of '$(basename $0)': $success successful CONFIG(s), $failure failed CONFIG(s)\n"
 
