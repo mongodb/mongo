fd7c872769f97690718393731e217bb3fc2c96d6
Yuhong Zhang <yuhong.zhang@mongodb.com>
2023-09-20 19:54:31 +0000

    SERVER-81222 Separate time-series arbitrary updates in concurrency sharding test

--- jstests/concurrency/fsm_workloads/random_moveChunk_timeseries_meta_updates.js
+++ jstests/concurrency/fsm_workloads/random_moveChunk_timeseries_meta_updates.js
@@ -14,20 +14,10 @@ import {extendWorkload} from "jstests/concurrency/fsm_libs/extend_workload.js";
 import {
     $config as $baseConfig
 } from 'jstests/concurrency/fsm_workloads/random_moveChunk_timeseries_inserts.js';
-import {TimeseriesTest} from "jstests/core/timeseries/libs/timeseries.js";
 
 const numValues = 10;
-const logCollection = "log_collection";
 
 export const $config = extendWorkload($baseConfig, function($config, $super) {
-    $config.setup = function setup(db, collName, cluster) {
-        $super.setup.apply(this, arguments);
-
-        // Drop and create a collection to log the number of arbitrary updates we perform.
-        db[logCollection].drop();
-        assert.commandWorked(db.createCollection(logCollection));
-    };
-
     $config.data.generateMetaFieldValueForInitialInserts = () => {
         let meta = {};
         // Insert a document with a field for every thread to test concurrent updates on the
@@ -54,21 +44,6 @@ export const $config = extendWorkload($baseConfig, function($config, $super) {
             {[updateField]: {$gte: oldValue}}, {$inc: {[updateField]: 1}}, {multi: true}));
     };
 
-    // Perform arbitrary updates on metric fields of measurements.
-    $config.states.arbitraryUpdate = function(db, collName, connCache) {
-        if (TimeseriesTest.arbitraryUpdatesEnabled(db)) {
-            const shardedColl = db[collName];
-
-            // Updates measurements by adding/incrementing a field.
-            jsTestLog("Executing arbitrary update on: " + collName);
-            const res = assert.commandWorked(
-                shardedColl.update({}, {$inc: {"updateCount": 1}}, {multi: true}));
-
-            // Log the number of updates performed.
-            assert.commandWorked(db[logCollection].insert({updateCount: res.nModified}));
-        }
-    };
-
     $config.data.validateCollection = function validate(db, collName) {
         // Since we can't use a 'snapshot' read concern for timeseries updates, updates on the
         // sharded collection may not see the exact same records as the non-sharded, so the
@@ -78,27 +53,13 @@ export const $config = extendWorkload($baseConfig, function($config, $super) {
                                                     db[this.nonShardCollName].aggregate(pipeline));
         assertAlways.eq(
             diff, {docsWithDifferentContents: [], docsMissingOnFirst: [], docsMissingOnSecond: []});
-
-        if (TimeseriesTest.arbitraryUpdatesEnabled(db)) {
-            // Check the sum of 'updateCount' against the logged values.
-            const updateCountAggregation = {$group: {_id: null, count: {$sum: "$updateCount"}}};
-            const totalUpdateCount =
-                db[collName].aggregate(updateCountAggregation).toArray()[0].count;
-            const loggedUpdateCount =
-                db[logCollection].aggregate(updateCountAggregation).toArray()[0].count;
-
-            assert.eq(totalUpdateCount, loggedUpdateCount, `Mismatch in update counts.`);
-        }
     };
 
     $config.transitions = {
         init: {insert: 1},
-        insert: {insert: 0.2, moveChunk: 0.1, bucketLevelUpdate: 0.25, arbitraryUpdate: 0.45},
-        bucketLevelUpdate:
-            {insert: 0.2, moveChunk: 0.1, bucketLevelUpdate: 0.2, arbitraryUpdate: 0.5},
-        arbitraryUpdate:
-            {insert: 0.2, moveChunk: 0.1, bucketLevelUpdate: 0.2, arbitraryUpdate: 0.5},
-        moveChunk: {insert: 0.2, moveChunk: 0.1, bucketLevelUpdate: 0.25, arbitraryUpdate: 0.45},
+        insert: {insert: 0.2, moveChunk: 0.1, bucketLevelUpdate: 0.25},
+        bucketLevelUpdate: {insert: 0.2, moveChunk: 0.1, bucketLevelUpdate: 0.2},
+        moveChunk: {insert: 0.2, moveChunk: 0.1, bucketLevelUpdate: 0.25},
     };
 
     // Reduced iteration and document counts to avoid timeouts.
