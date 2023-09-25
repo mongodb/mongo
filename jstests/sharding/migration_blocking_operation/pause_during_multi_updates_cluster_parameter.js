8d2e7868c98533eab9b486606d2cf062e10e015b
Brett Nawrocki <brett.nawrocki@mongodb.com>
2023-09-25 21:10:05 +0000

    SERVER-81273 Add pauseMigrationsDuringMultiUpdates cluster parameter

---
+++ jstests/sharding/migration_blocking_operation/pause_during_multi_updates_cluster_parameter.js
@@ -0,0 +1,23 @@
+/*
+ * Tests that pauseMigrationsDuringMultiUpdates cluster parameter is off by default.
+ * @tags: [
+ *  featureFlagPauseMigrationsDuringMultiUpdatesAvailable,
+ *  requires_fcv_72
+ * ]
+ */
+
+const numShards = 1;
+const st = new ShardingTest({shards: numShards});
+const allRs = [st.configRS];
+for (let i = 0; i < numShards; i++) {
+    allRs.push(st[`rs${i}`]);
+}
+
+// Ensure pauseMigrationsDuringMultiUpdates defaults to false.
+for (const rs of allRs) {
+    const response = assert.commandWorked(
+        rs.getPrimary().adminCommand({getClusterParameter: "pauseMigrationsDuringMultiUpdates"}));
+    assert.eq(response.clusterParameters[0].enabled, false);
+}
+
+st.stop();
