6c67ed7d2a227e40daf43c5f80f30c58fbea9a73
Hugh Tong <hugh.tong@mongodb.com>
2023-09-27 15:57:23 +0000

    SERVER-80753 Modify runCheckOnReplSet to call isMaster on admin

--- jstests/hooks/run_check_repl_oplogs.js
+++ jstests/hooks/run_check_repl_oplogs.js
@@ -2,7 +2,7 @@ var startTime = Date.now();
 assert.neq(typeof db, 'undefined', 'No `db` object, is the shell connected to a mongod?');
 
 let runCheckOnReplSet = function(db) {
-    let primaryInfo = db.isMaster();
+    let primaryInfo = db.adminCommand("isMaster");
 
     assert(primaryInfo.ismaster,
            'shell is not connected to the primary or master node: ' + tojson(primaryInfo));
@@ -17,13 +17,13 @@ if (db.getMongo().isMongos()) {
     // Run check on every shard.
     configDB.shards.find().forEach(shardEntry => {
         let newConn = new Mongo(shardEntry.host);
-        runCheckOnReplSet(newConn.getDB('test'));
+        runCheckOnReplSet(newConn.getDB('admin'));
     });
 
     // Run check on config server.
     let cmdLineOpts = db.adminCommand({getCmdLineOpts: 1});
     let configConn = new Mongo(cmdLineOpts.parsed.sharding.configDB);
-    runCheckOnReplSet(configConn.getDB('test'));
+    runCheckOnReplSet(configConn.getDB('admin'));
 } else {
     runCheckOnReplSet(db);
 }
