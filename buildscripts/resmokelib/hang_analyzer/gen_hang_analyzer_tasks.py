2314b0b28db255d6f7929e394b183d4503506d50
Trevor Guidry <trevor.guidry@mongodb.com>
2023-09-28 20:00:38 +0000

    SERVER-81529 Switch core analyzer task to use large distros

--- buildscripts/resmokelib/hang_analyzer/gen_hang_analyzer_tasks.py
+++ buildscripts/resmokelib/hang_analyzer/gen_hang_analyzer_tasks.py
@@ -93,7 +93,8 @@ def generate(expansions_file: str = "../expansions.yml",
 
     # gather information from the current task being run
     expansions = read_config_file(expansions_file)
-    distro = expansions.get("distro_id")
+    distro = expansions.get(
+        "large_distro_name") if "large_distro_name" in expansions else expansions.get("distro_id")
     current_task_name = expansions.get("task_name")
     task_id = expansions.get("task_id")
     execution = expansions.get("execution")
