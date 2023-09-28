c3b59564c0b0de425136aebc6f4adf41f9a1c79b
Ravi Giri <ravi.giri@mongodb.com>
2023-09-28 02:21:04 +0000

    Import wiredtiger: 448f0355f7b6aa5aec4c194fd40407b8d8739194 from branch mongodb-master
    ref: 31f6d20bb5..448f0355f7
    for: 7.2.0-rc0
    
    WT-11715 Test additional branches in the compatibility test suite

--- src/third_party/wiredtiger/test/compatibility/common/compatibility_config.py
+++ src/third_party/wiredtiger/test/compatibility/common/compatibility_config.py
@@ -31,9 +31,10 @@
 # The branches we use for the testing. We support special branch name 'this' that refers to the
 # current branch. This is useful when debugging a compatibility issue on the current branch, but it
 # should not be enabled when testing on Evergreen.
-#BRANCHES = ['develop', 'mongodb-7.0', 'mongodb-6.3', 'mongodb-6.0']
-#BRANCHES = ['this', 'mongodb-6.0']
-BRANCHES = ['mongodb-7.0', 'mongodb-6.0']
+BRANCHES = ['develop', 'mongodb-7.1', 'mongodb-7.0', 'mongodb-6.3', 'mongodb-6.0']
+
+# Example use of the 'this' branch (useful for debugging):
+# BRANCHES = ['this', 'mongodb-6.0']
 
 # The default directory to which the test will check out other branches, relative to the project's
 # top-level directory.
