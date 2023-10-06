c79b1066b5087dd72cdc80e32718c67edee5ef18
Wenqin Ye <wenqin908@gmail.com>
2023-10-06 14:54:23 +0000

    SERVER-81579: Relax config session checks in configsvr_retries_createindex_on_stale_config.js

--- jstests/libs/sessions_collection.js
+++ jstests/libs/sessions_collection.js
@@ -1,6 +1,27 @@
 // Helpers for testing the logical sessions collection.
 
 /**
+ * Assumes that there is only one session chunk and two shards (shard0 and shard1).
+ * Returns two shards, where the first shard returned is the shard that has the session chunk
+ * and the second shard returned is the one that does not have the session chunk.
+ */
+export function getShardsWithAndWithoutChunk(st, shard0, shard1) {
+    let configDB = st.s.getDB('config');
+    let configSessionsUUID = configDB.collections.findOne({_id: 'config.system.sessions'}).uuid;
+    let sessionChunks = configDB.chunks.find({uuid: configSessionsUUID}).toArray();
+
+    assert.eq(1, sessionChunks.length, tojson(sessionChunks));
+    let sessionChunk = sessionChunks[0];
+
+    if (sessionChunk.shard == shard0.shardName) {
+        return {shardWithSessionChunk: shard0, shardWithoutSessionChunk: shard1};
+    } else {
+        assert.eq(sessionChunk.shard, shard1.shardName, tojson(sessionChunk));
+        return {shardWithSessionChunk: shard1, shardWithoutSessionChunk: shard0};
+    }
+}
+
+/**
  * Validates that the sessions collection exists if we expect it to,
  * and has a TTL index on the lastUse field, if we expect it to.
  */
