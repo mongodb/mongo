613bffe276f98758508dea8e279a9f3a5f99164c
Aitor Esteve Alvarado <aitor.esteve@mongodb.com>
2023-10-18 09:54:29 +0000

    SERVER-81985 Enforce _flushRoutingTableCacheUpdates only on collections

--- src/mongo/db/s/flush_routing_table_cache_updates_command.cpp
+++ src/mongo/db/s/flush_routing_table_cache_updates_command.cpp
@@ -126,6 +126,11 @@ public:
                                   << " if in read-only mode",
                     !opCtx->readOnly());
 
+            uassert(ErrorCodes::IllegalOperation,
+                    str::stream() << "Can only call " << Derived::Request::kCommandName
+                                  << " on collections",
+                    !ns().coll().empty());
+
             boost::optional<SharedSemiFuture<void>> criticalSectionSignal;
 
             {
