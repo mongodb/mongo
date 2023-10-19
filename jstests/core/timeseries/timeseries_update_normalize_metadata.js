6e180e268569a76b6c3a5346de84e1cc9da41a03
Yuhong Zhang <yuhong.zhang@mongodb.com>
2023-10-19 13:35:19 +0000

    SERVER-81457 Test meta field normalization with time-series updates and upserts

---
+++ jstests/core/timeseries/timeseries_update_normalize_metadata.js
@@ -0,0 +1,64 @@
+/**
+ * Tests measurements from time-series updates have their metadata fields normalized correctly.
+ *
+ * @tags: [
+ *   requires_multi_updates,
+ *   requires_timeseries,
+ *   requires_non_retryable_writes,
+ *   featureFlagTimeseriesUpdatesSupport,
+ * ]
+ */
+
+const collName = "test";
+const testDB = db.getSiblingDB(jsTestName());
+assert.commandWorked(testDB.dropDatabase());
+
+function verifyMetaFields(coll) {
+    const metaFields = coll.find({}, {time: 0, _id: 0}).toArray();
+    for (const metaField of metaFields) {
+        assert.eq(0, bsonWoCompare(metaField, {tag: {a: 1, b: {c: 1, d: 1}}}), tojson(metaField));
+    }
+}
+
+// Insert the initial measurements.
+const coll = testDB.getCollection(collName);
+assert.commandWorked(testDB.createCollection(coll.getName(), {
+    timeseries: {timeField: "time", metaField: "tag"},
+}));
+for (let i = 0; i < 100; ++i) {
+    assert.commandWorked(coll.insert({time: new Date(), tag: {a: 1, b: {c: 1, d: 1}}, _id: i}));
+}
+
+assert.commandWorked(db.runCommand({
+    update: coll.getName(),
+    updates: [{q: {}, u: {$set: {tag: {b: {d: 1, c: 1}, a: 1}}}, multi: true}]
+}));
+verifyMetaFields(coll);
+
+assert.commandWorked(db.runCommand({
+    update: coll.getName(),
+    updates: [{q: {}, u: {$set: {tag: {b: {d: 1, c: 1}, a: 1}}}, multi: false}]
+}));
+verifyMetaFields(coll);
+
+assert.commandWorked(db.runCommand({
+    update: coll.getName(),
+    updates: [{
+        q: {time: new Date()},
+        u: {$set: {tag: {b: {d: 1, c: 1}, a: 1}}},
+        multi: true,
+        upsert: true
+    }]
+}));
+verifyMetaFields(coll);
+
+assert.commandWorked(db.runCommand({
+    update: coll.getName(),
+    updates: [{
+        q: {time: new Date()},
+        u: {$set: {tag: {b: {d: 1, c: 1}, a: 1}}},
+        multi: false,
+        upsert: true
+    }]
+}));
+verifyMetaFields(coll);
---
+++ jstests/sharding/timeseries_update_normalize_metadata.js
@@ -0,0 +1,54 @@
+/**
+ * Tests that when updating on a sharded time-series collection, the new bucket documents inserted
+ * as part of the update/upsert operations take into account metadata normalization.
+ *
+ * @tags: [
+ *   requires_multi_updates,
+ *   requires_timeseries,
+ *   requires_non_retryable_writes,
+ *   featureFlagTimeseriesUpdatesSupport,
+ * ]
+ */
+
+const st = new ShardingTest({shards: 2});
+
+const db = st.s0.getDB(jsTestName());
+const coll = db.coll;
+const bucketsColl = db.system.buckets.coll;
+
+assert.commandWorked(
+    db.createCollection(coll.getName(), {timeseries: {timeField: "t", metaField: "m"}}));
+assert.commandWorked(db.adminCommand({shardCollection: coll.getFullName(), key: {m: 1}}));
+assert.commandWorked(st.splitAt(bucketsColl.getFullName(), {meta: {a: 5, b: 5}}));
+assert.commandWorked(
+    st.moveChunk(bucketsColl.getFullName(), {meta: {a: 0, b: 0}}, st.shard0.shardName));
+assert.commandWorked(
+    st.moveChunk(bucketsColl.getFullName(), {meta: {a: 10, b: 10}}, st.shard1.shardName));
+
+assert.commandWorked(
+    coll.insert({_id: 0, t: ISODate("2023-01-01T12:00:00.000Z"), m: {a: 1, b: 1}}));
+assert.commandWorked(
+    coll.insert({_id: 1, t: ISODate("2023-01-01T12:00:01.000Z"), m: {b: 1, a: 1}}));
+// If the metaField is not normalized, it will become an orphan and not found.
+assert.eq(coll.find().itcount(), 2);
+
+assert.commandWorked(db.runCommand(
+    {update: coll.getName(), updates: [{q: {}, u: {$set: {m: {b: 1, a: 1}}}, multi: true}]}));
+assert.eq(coll.find().itcount(), 2);
+
+assert.commandWorked(db.runCommand(
+    {update: coll.getName(), updates: [{q: {}, u: {$set: {m: {b: 1, a: 1}}}, multi: false}]}));
+assert.eq(coll.find().itcount(), 2);
+
+assert.commandWorked(db.runCommand({
+    update: coll.getName(),
+    updates: [{
+        q: {t: ISODate("2023-01-01T12:00:02.000Z")},
+        u: {$set: {m: {b: 1, a: 1}}},
+        multi: false,
+        upsert: true
+    }]
+}));
+assert.eq(coll.find().itcount(), 3);
+
+st.stop();
