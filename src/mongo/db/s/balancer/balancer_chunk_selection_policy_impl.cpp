7a3c2a47cee449b5c02392b08dc5b33265cbe11b
Tommaso Tocci <tommaso.tocci@mongodb.com>
2023-10-20 13:28:22 -0400

    SERVER-82325 Config server could invariant during balancer round
    This effectively reverts SERVER-40459
    
    GitOrigin-RevId: 595d8110122be5b1ca2d951c6f71cd8ecb29b8cb

--- src/mongo/db/s/balancer/balancer_chunk_selection_policy_impl.cpp
+++ src/mongo/db/s/balancer/balancer_chunk_selection_policy_impl.cpp
@@ -47,9 +47,9 @@
 #include "mongo/s/catalog/type_collection.h"
 #include "mongo/s/catalog/type_tags.h"
 #include "mongo/s/catalog_cache.h"
-#include "mongo/s/chunk_manager.h"
 #include "mongo/s/grid.h"
 #include "mongo/util/str.h"
+
 namespace mongo {
 
 using MigrateInfoVector = BalancerChunkSelectionPolicy::MigrateInfoVector;
@@ -90,6 +90,27 @@ StatusWith<ZoneInfo> createCollectionZoneInfo(OperationContext* opCtx,
  */
 StatusWith<DistributionStatus> createCollectionDistributionStatus(
     OperationContext* opCtx, const ShardStatisticsVector& allShards, ChunkManager* chunkMgr) {
+    ShardToChunksMap shardToChunksMap;
+
+    // Makes sure there is an entry in shardToChunksMap for every shard, so empty shards will also
+    // be accounted for
+    for (const auto& stat : allShards) {
+        shardToChunksMap[stat.shardId];
+    }
+
+    chunkMgr->forEachChunk([&](const auto& chunkEntry) {
+        ChunkType chunk;
+        chunk.setNS(chunkMgr->getns());
+        chunk.setMin(chunkEntry.getMin());
+        chunk.setMax(chunkEntry.getMax());
+        chunk.setJumbo(chunkEntry.isJumbo());
+        chunk.setShard(chunkEntry.getShardId());
+        chunk.setVersion(chunkEntry.getLastmod());
+
+        shardToChunksMap[chunkEntry.getShardId()].push_back(chunk);
+
+        return true;
+    });
 
     auto swZoneInfo = createCollectionZoneInfo(
         opCtx, chunkMgr->getns(), chunkMgr->getShardKeyPattern().getKeyPattern());
@@ -97,7 +118,8 @@ StatusWith<DistributionStatus> createCollectionDistributionStatus(
         return swZoneInfo.getStatus();
     }
 
-    return {DistributionStatus{chunkMgr->getns(), std::move(swZoneInfo.getValue()), chunkMgr}};
+    return {DistributionStatus{
+        chunkMgr->getns(), std::move(shardToChunksMap), std::move(swZoneInfo.getValue())}};
 }
 
 /**
@@ -526,7 +548,7 @@ Status BalancerChunkSelectionPolicyImpl::checkMoveAllowed(OperationContext* opCt
     }
 
     return BalancerPolicy::isShardSuitableReceiver(*newShardIterator,
-                                                   distribution.getTagForRange(chunk.getRange()));
+                                                   distribution.getTagForChunk(chunk));
 }
 
 StatusWith<SplitInfoVector> BalancerChunkSelectionPolicyImpl::_getSplitCandidatesForCollection(
@@ -586,7 +608,7 @@ StatusWith<MigrateInfoVector> BalancerChunkSelectionPolicyImpl::_getMigrateCandi
 
     const DistributionStatus& distribution = collInfoStatus.getValue();
 
-    for (const auto& tagRangeEntry : distribution.getZoneInfo().zoneRanges()) {
+    for (const auto& tagRangeEntry : distribution.tagRanges()) {
         const auto& tagRange = tagRangeEntry.second;
 
         const auto chunkAtZoneMin = cm->findIntersectingChunkWithSimpleCollation(tagRange.min);
