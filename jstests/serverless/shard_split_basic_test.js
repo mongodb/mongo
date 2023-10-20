4ca048cf06cc0db265bcc516d03e879de5391387
Mathis Bessa <mathis.bessa@mongodb.com>
2023-10-20 16:28:22 +0000

    SERVER-79993 Test Shard Split in multitenant environment

--- jstests/serverless/shard_split_basic_test.js
+++ jstests/serverless/shard_split_basic_test.js
@@ -9,27 +9,43 @@ import {
     ShardSplitTest
 } from "jstests/serverless/libs/shard_split_test.js";
 
-const tenantIds = [ObjectId(), ObjectId()];
-const test = new ShardSplitTest({quickGarbageCollection: true});
-test.addRecipientNodes();
-test.donor.awaitSecondaryNodes();
+function runBasicShardSplitTest({useMultitenancy} = {
+    multitenancy: false
+}) {
+    jsTestLog("Starting to run shard split with multitenancy " +
+              (useMultitenancy ? "enabled" : "disabled"));
+    const tenantIds = [ObjectId(), ObjectId()];
+    const testOptions = {quickGarbageCollection: true};
+    if (useMultitenancy) {
+        Object.assign(testOptions, {
+            nodeOptions:
+                {setParameter: {multitenancySupport: true, dbCheckHealthLogEveryNBatches: 1}}
+        });
+    }
+    const test = new ShardSplitTest(testOptions);
+    test.addRecipientNodes();
+    test.donor.awaitSecondaryNodes();
 
-const donorPrimary = test.getDonorPrimary();
-const operation = test.createSplitOperation(tenantIds);
-const result = assert.commandWorked(operation.commit());
-assertMigrationState(donorPrimary, operation.migrationId, "committed");
+    const donorPrimary = test.getDonorPrimary();
+    const operation = test.createSplitOperation(tenantIds);
+    const result = assert.commandWorked(operation.commit());
+    assertMigrationState(donorPrimary, operation.migrationId, "committed");
 
-// Confirm blockOpTime in result matches the state document before forgetting the operation
-const stateDoc = findSplitOperation(donorPrimary, operation.migrationId);
-assert.eq(stateDoc.blockOpTime.ts, result.blockOpTime.ts);
+    // Confirm blockOpTime in result matches the state document before forgetting the operation
+    const stateDoc = findSplitOperation(donorPrimary, operation.migrationId);
+    assert.eq(stateDoc.blockOpTime.ts, result.blockOpTime.ts);
 
-operation.forget();
+    operation.forget();
 
-const status = donorPrimary.adminCommand({serverStatus: 1});
-assert.eq(status.shardSplits.totalCommitted, 1);
-assert.eq(status.shardSplits.totalAborted, 0);
-assert.gt(status.shardSplits.totalCommittedDurationMillis, 0);
-assert.gt(status.shardSplits.totalCommittedDurationWithoutCatchupMillis, 0);
+    const status = donorPrimary.adminCommand({serverStatus: 1});
+    assert.eq(status.shardSplits.totalCommitted, 1);
+    assert.eq(status.shardSplits.totalAborted, 0);
+    assert.gt(status.shardSplits.totalCommittedDurationMillis, 0);
+    assert.gt(status.shardSplits.totalCommittedDurationWithoutCatchupMillis, 0);
 
-test.cleanupSuccesfulCommitted(operation.migrationId, tenantIds);
-test.stop();
+    test.cleanupSuccesfulCommitted(operation.migrationId, tenantIds);
+    test.stop();
+}
+
+runBasicShardSplitTest({useMultitenancy: false});
+runBasicShardSplitTest({useMultitenancy: true});
