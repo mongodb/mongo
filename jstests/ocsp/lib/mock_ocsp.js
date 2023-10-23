de09d8b85e40c803716586ac5a0ce9dc4e24acb8
Alex Neben <alex.neben@mongodb.com>
2023-10-23 23:23:35 +0000

    SERVER-80770 Loosened python version requirements

--- jstests/ocsp/lib/mock_ocsp.js
+++ jstests/ocsp/lib/mock_ocsp.js
@@ -115,7 +115,7 @@ export class MockOCSPServer {
 
         assert.soon(function() {
             // Change this line if the OCSP endpoint changes
-            return rawMongoProgramOutput().search("Running on http://127.0.0.1:8100/") !== -1;
+            return rawMongoProgramOutput().search("Launching debugserver on port 8100") !== -1;
         });
 
         sleep(2000);
