de14cf56adca1c90942b1fb4a2887e64be16edfe
Adi Zaimi <adi.zaimi@mongodb.com>
2023-10-28 14:45:11 +0000

    SERVER-82428 Include idle connections in currentOp aggregate cmd in fsync.js
    Include idle connections in the aggregate command currentOp
    in waitUntilOpCountIs() function for fsync.js test.

--- jstests/core/fsync.js
+++ jstests/core/fsync.js
@@ -18,7 +18,7 @@ function waitUntilOpCountIs(opFilter, num) {
     assert.soon(() => {
         let ops = db.getSiblingDB('admin')
                       .aggregate([
-                          {$currentOp: {}},
+                          {$currentOp: {allUsers: true, idleConnections: true}},
                           {$match: opFilter},
                       ])
                       .toArray();
