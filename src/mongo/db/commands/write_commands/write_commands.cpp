5b5beb79c99fb2fb6eb20d3351750ca2b1b8558f
nandinibhartiyaMDB <104035932+nandinibhartiyaMDB@users.noreply.github.com>
2023-10-30 15:10:10 -0500

    SERVER-72532: Add shard & DB version field later in the BSON object. (#16519)
    GitOrigin-RevId: 3feee8d776027aa084d188ed79c780b87870e437

--- src/mongo/db/commands/write_commands/write_commands.cpp
+++ src/mongo/db/commands/write_commands/write_commands.cpp
@@ -379,19 +379,20 @@ private:
 
             invariant(!_commandObj.isEmpty());
 
-            if (const auto& shardVersion = _commandObj.getField("shardVersion");
-                !shardVersion.eoo()) {
-                bob->append(shardVersion);
-            }
-            if (const auto& databaseVersion = _commandObj.getField("databaseVersion");
-                !databaseVersion.eoo()) {
-                bob->append(databaseVersion);
-            }
-
             bob->append("find", _commandObj["update"].String());
             extractQueryDetails(_updateOpObj, bob);
             bob->append("batchSize", 1);
             bob->append("singleBatch", true);
+
+            if (const auto& shardVersion = _commandObj.getField("shardVersion");
+                !shardVersion.eoo()) {
+                bob->append(shardVersion);
+            }
+
+            if (const auto& databaseVersion = _commandObj.getField("databaseVersion");
+                !databaseVersion.eoo()) {
+                bob->append(databaseVersion);
+            }
         }
 
     private:
