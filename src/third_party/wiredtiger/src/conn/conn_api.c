5a4c949e8126cf7874d5de43814d419bcbad8324
Will Korteland <will.korteland@mongodb.com>
2023-11-01 22:46:42 +0000

    Import wiredtiger: ec91e6ac5eb8be030c77338bc3a1332df509a02c from branch mongodb-master
    ref: 3e5f82128d..ec91e6ac5e
    for: 7.2.0-rc0
    
    WT-11447 Explicitly name external sessions.

--- src/third_party/wiredtiger/src/conn/conn_api.c
+++ src/third_party/wiredtiger/src/conn/conn_api.c
@@ -1322,6 +1322,7 @@ __conn_open_session(WT_CONNECTION *wt_conn, WT_EVENT_HANDLER *event_handler, con
 
     session_ret = NULL;
     WT_ERR(__wt_open_session(conn, event_handler, config, true, &session_ret));
+    session_ret->name = "connection-open-session";
     *wt_sessionp = &session_ret->iface;
 
 err:
