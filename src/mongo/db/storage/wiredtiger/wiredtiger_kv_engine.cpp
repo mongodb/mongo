df5fc7311495613475db7de73ef67398373eb2e5
3058078489@qq.com <3058078489@qq.com>
2023-11-02 20:50:41 +0800

    bug fix

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
@@ -367,14 +367,11 @@ WiredTigerKVEngine::WiredTigerKVEngine(OperationContext* opCtx,
     }
 
 #define DEFAULT_WIREDTIGER_MAX_SESSIONS 33000
-//reserve 100 sessions for wiredtiger's internal session
-#define DEFAULT_WIREDTIGER_INTERNAL_SESSIONS 100
-
     std::stringstream ss;
     ss << "create,";
     ss << "cache_size=" << cacheSizeMB << "M,";
     if (serverGlobalParams.maxConns <= DEFAULT_WIREDTIGER_MAX_SESSIONS)
-        ss << "session_max=" << serverGlobalParams.maxConns + DEFAULT_WIREDTIGER_INTERNAL_SESSIONS << ",";
+        ss << "session_max=" << DEFAULT_WIREDTIGER_MAX_SESSIONS << ",";
     else
         ss << "session_max=" << serverGlobalParams.maxConns << ",";
     ss << "eviction=(threads_min=4,threads_max=4),";
