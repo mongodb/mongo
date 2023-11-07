1f820ec1a41a07107b0ce500a8c47d0cfebf67ce
Zack Winter <zack.winter@mongodb.com>
2023-11-07 21:52:30 +0000

    SERVER-79137 Add evergreen target to exercise 'bazel run'

---
+++ evergreen/bazel_run.sh
@@ -0,0 +1,20 @@
+# Usage:
+#   bazel_compile [arguments]
+#
+# Required environment variables:
+# * ${target} - Build target
+# * ${args} - Extra command line args to pass to "bazel run"
+
+# Needed for evergreen scripts that use evergreen expansions and utility methods.
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+. "$DIR/prelude.sh"
+
+cd src
+
+set -o errexit
+set -o verbose
+
+# Use `eval` to force evaluation of the environment variables in the echo statement:
+eval echo "Execution environment: Args: ${args} Target: ${target}"
+
+./bazelisk run ${args} ${target}
