bc2e4b24b53d2dd54683d6279ea6cc9e5a5a54e8
Haley Connelly <haley.connelly@mongodb.com>
2023-11-13 11:54:07 +0000

    SERVER-81978 handle StorageUnavailableExceptions in repl::applyCommand_inlock()

--- src/mongo/db/repl/oplog.cpp
+++ src/mongo/db/repl/oplog.cpp
@@ -2310,19 +2310,15 @@ Status applyCommand_inlock(OperationContext* opCtx,
                 // their timestamp at commit.
                 TimestampBlock tsBlock(opCtx, writeTime);
                 return curOpToApply.applyFunc(opCtx, op, mode);
+            } catch (const StorageUnavailableException&) {
+                // Retriable error.
+                throw;
             } catch (const DBException& ex) {
                 return ex.toStatus();
             }
         }();
 
         switch (status.code()) {
-            case ErrorCodes::WriteConflict: {
-                // Need to throw this up to a higher level where it will be caught and the
-                // operation retried.
-                throwWriteConflictException(str::stream()
-                                            << "WriteConflict caught during oplog application."
-                                            << " Original error: " << status.reason());
-            }
             case ErrorCodes::BackgroundOperationInProgressForDatabase: {
                 invariant(mode == OplogApplication::Mode::kInitialSync);
 
