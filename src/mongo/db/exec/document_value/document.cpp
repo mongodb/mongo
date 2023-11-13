21e6e8e11a45dfbdb7ca6cf95fa8c5f859e2b118
Mickey. J Winters <mickey.winters@mongodb.com>
2023-11-13 17:59:11 +0000

    SERVER-83150 make Document::shred() copy document metadata

--- src/mongo/db/exec/document_value/document.cpp
+++ src/mongo/db/exec/document_value/document.cpp
@@ -427,6 +427,7 @@ Document DocumentStorage::shred() const {
             md[it.fieldName()] = valueElem.val.shred();
         }
     }
+    md.setMetadata(DocumentMetadataFields(metadata()));
     return md.freeze();
 }
 
