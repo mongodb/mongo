122a1fc68fc7544f5f9affd07055d014bb5db4a0
Luke Chen <luke.chen@mongodb.com>
2023-11-15 12:21:27 +1100

    Import wiredtiger: f84cde5b5a24e543d0504e76b1cf3c0d777e6035 from branch mongodb-7.2
    ref: de98396412..f84cde5b5a
    for: 7.2.0-rc2
    
    WT-11778 Restricting dirty eviction to sessions with snapshot isolation and of metadata pages

--- src/third_party/wiredtiger/src/reconcile/rec_visibility.c
+++ src/third_party/wiredtiger/src/reconcile/rec_visibility.c
@@ -531,6 +531,10 @@ __rec_upd_select(WT_SESSION_IMPL *session, WT_RECONCILE *r, WT_UPDATE *first_upd
         if ((txnid = upd->txnid) == WT_TXN_ABORTED)
             continue;
 
+        /* Give up if the update is from this transaction and on the metadata file.*/
+        if (WT_IS_METADATA(session->dhandle) && txnid == session_txnid)
+            return (__wt_set_return(session, EBUSY));
+
         /*
          * Track the first update in the chain that is not aborted and the maximum transaction ID.
          */
