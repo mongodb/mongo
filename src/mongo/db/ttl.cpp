2dc1d0567a3749a5401b3656345a02026e77e36c
Gregory Noma <gregory.noma@gmail.com>
2023-11-16 15:21:01 +0000

    SERVER-76560 Honor `expireAfterSeconds` for time-series pre-2018
    (cherry picked from commit 21b7b619eeb6b23bee47ff493b708255fdf6d95e)

--- src/mongo/db/ttl.cpp
+++ src/mongo/db/ttl.cpp
@@ -780,7 +780,7 @@ bool TTLMonitor::_deleteExpiredWithCollscan(OperationContext* opCtx,
 
     LOGV2_DEBUG(5400704, 1, "running TTL job for clustered collection", logAttrs(collection->ns()));
 
-    const auto startId = makeCollScanStartBound(collection, Date_t::min());
+    const auto startId = makeCollScanStartBound(collection, Date_t{});
 
     const auto expirationDate = safeExpirationDate(opCtx, collection, *expireAfterSeconds);
     const auto endId = makeCollScanEndBound(collection, expirationDate);
