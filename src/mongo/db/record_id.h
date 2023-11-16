92f490aaa8f58d2b1107ae882c42398e7d92bdca
Yuhong Zhang <yuhong.zhang@mongodb.com>
2023-11-16 21:41:36 +0000

    SERVER-82730 Convert invariants when decoding keystring RecordId to assertions
    (cherry picked from commit bf607bda808e7a9ab70ed5d5260161d548c035ce)
    (cherry picked from commit 28b205ae6f53c271b851c635cbac7d986ee94546)

--- src/mongo/db/record_id.h
+++ src/mongo/db/record_id.h
@@ -89,7 +89,7 @@ public:
      * retrieved using getStr().
      */
     explicit RecordId(const char* str, int32_t size) {
-        invariant(size > 0, "key size must be greater than 0");
+        uassert(8273007, fmt::format("key size must be greater than 0. size: {}", size), size > 0);
         if (size <= kSmallStrMaxSize) {
             _format = Format::kSmallStr;
             // Must fit into the buffer minus 1 byte for size.
