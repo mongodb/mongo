0cb7a0fb18c281aa8ac41380c54b3a2187f908f7
Foteini Alvanaki <foteini.alvanaki@mongodb.com>
2023-11-20 17:45:11 +0000

    SERVER-83393 Add serialisation tests for multiset

--- src/mongo/db/exec/sbe/values/value_serialization_test.cpp
+++ src/mongo/db/exec/sbe/values/value_serialization_test.cpp
@@ -134,6 +134,17 @@ TEST(ValueSerializeForSorter, Serialize) {
     auto [oidTag, oidVal] = value::makeCopyObjectId({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
     testData->push_back(oidTag, oidVal);
 
+    auto [arrayMultiSetTag, arrayMultiSetVal] = value::makeNewArrayMultiSet();
+    object->push_back("mset", arrayMultiSetTag, arrayMultiSetVal);
+
+    value::ArrayMultiSet* arrayMultiSet = value::getArrayMultiSetView(arrayMultiSetVal);
+    arrayMultiSet->push_back(value::TypeTags::NumberInt64, value::bitcastFrom<int64_t>(6));
+    arrayMultiSet->push_back(value::TypeTags::NumberInt64, value::bitcastFrom<int64_t>(6));
+    arrayMultiSet->push_back(value::TypeTags::NumberInt64, value::bitcastFrom<int64_t>(7));
+
+    auto [msetTag, msetVal] = value::makeCopyArrayMultiSet(*arrayMultiSet);
+    testData->push_back(msetTag, msetVal);
+
     uint8_t byteArray[] = {8, 7, 6, 5, 4, 3, 2, 1};
     auto bson =
         BSON("obj" << BSON("a" << 1 << "b" << 2) << "arr" << BSON_ARRAY(1 << 2 << 3)  //
@@ -303,6 +314,18 @@ TEST_F(ValueSerializeForKeyString, ArraySet) {
     arraySet->push_back(value::TypeTags::NumberInt32, value::bitcastFrom<int32_t>(1));
     arraySet->push_back(value::TypeTags::NumberInt64, value::bitcastFrom<int64_t>(2));
     arraySet->push_back(value::TypeTags::NumberDouble, value::bitcastFrom<double>(3.0));
+
+    runTest({{tag, val}});
+}
+
+TEST_F(ValueSerializeForKeyString, ArrayMultiSet) {
+    auto [tag, val] = sbe::value::makeNewArrayMultiSet();
+    sbe::value::ValueGuard guard{tag, val};
+    auto* arrayMultiSet = sbe::value::getArrayMultiSetView(val);
+
+    arrayMultiSet->push_back(value::TypeTags::NumberInt32, value::bitcastFrom<int32_t>(1));
+    arrayMultiSet->push_back(value::TypeTags::NumberInt64, value::bitcastFrom<int64_t>(1));
+    arrayMultiSet->push_back(value::TypeTags::NumberDouble, value::bitcastFrom<double>(2.0));
 
     runTest({{tag, val}});
 }
