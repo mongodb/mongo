dcda39c85dce45ac8ea215c1ea1375380e0dd6f0
Luke Chen <luke.chen@mongodb.com>
2023-11-20 13:48:49 +1100

    Import wiredtiger: a613341e2bdd4558b1a3515c75c88edb6d1a4f7b from branch mongodb-7.2
    ref: d409259e2b..a613341e2b
    for: 7.2.0-rc2
    
    WT-11992 Bump WiredTiger version to 11.3.0

--- src/third_party/wiredtiger/test/suite/test_compat03.py
+++ src/third_party/wiredtiger/test/suite/test_compat03.py
@@ -58,6 +58,7 @@ class test_compat03(wttest.WiredTigerTestCase, suite_subprocess):
     compat_release = [
         ('future_rel', dict(rel=future_rel, log_rel=future_logv)),
         ('def_rel', dict(rel='none', log_rel=5)),
+        ('113_rel', dict(rel="11.3", log_rel=5)),
         ('112_rel', dict(rel="11.2", log_rel=5)),
         ('111_rel', dict(rel="11.1", log_rel=5)),
         ('110_rel', dict(rel="11.0", log_rel=5)),
@@ -79,7 +80,7 @@ class test_compat03(wttest.WiredTigerTestCase, suite_subprocess):
     compat_max = [
         ('future_max', dict(max_req=future_rel, log_max=future_logv)),
         ('def_max', dict(max_req='none', log_max=5)),
-        ('112_max', dict(max_req="11.2", log_max=5)),
+        ('113_max', dict(max_req="11.3", log_max=5)),
         ('33_max', dict(max_req="3.3", log_max=4)),
         ('32_max', dict(max_req="3.2", log_max=3)),
         ('30_max', dict(max_req="3.0", log_max=2)),
