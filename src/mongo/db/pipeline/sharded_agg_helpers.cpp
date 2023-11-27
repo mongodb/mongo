3d67df1b60f447fddf5ebbf6ac219d2fbcf6029c
Mihai Andrei <mihai.andrei@mongodb.com>
2023-11-27 17:54:06 +0000

    SERVER-79580 Remove HostTypeRequirement::kPrimaryShard from $lookup and add basic test coverage

--- src/mongo/db/pipeline/sharded_agg_helpers.cpp
+++ src/mongo/db/pipeline/sharded_agg_helpers.cpp
@@ -1471,7 +1471,7 @@ Status appendExplainResults(DispatchShardPipelineResults&& dispatchResults,
         auto* mergePipeline = dispatchResults.splitPipeline->mergePipeline.get();
         auto specificMergeShardId = mergePipeline->needsSpecificShardMerger();
         auto mergeType = [&]() -> std::string {
-            if (mergePipeline->canRunOnMongos()) {
+            if (mergePipeline->canRunOnMongos() && !specificMergeShardId) {
                 if (mergeCtx->inMongos) {
                     return "mongos";
                 }
