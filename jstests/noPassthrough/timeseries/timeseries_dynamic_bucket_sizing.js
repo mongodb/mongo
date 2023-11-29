66d182d1e50b7bfa8866ca2c44917638a3758d79
Damian Wasilewicz <damian.wasilewicz@mongodb.com>
2023-11-29 15:05:11 +0000

    SERVER-83377 Make timeseries_dynamic_bucket_sizing.js more robust

--- jstests/noPassthrough/timeseries/timeseries_dynamic_bucket_sizing.js
+++ jstests/noPassthrough/timeseries/timeseries_dynamic_bucket_sizing.js
@@ -20,9 +20,10 @@ const minBucketCount = 10;
 const timeFieldName = 'time';
 const metaFieldName = 'meta';
 const timestamp = ISODate('2023-02-13T01:00:00Z');
+const collName = 't';
 
-// A cardinality higher than this calculated value will call for smaller bucket size limit caused by
-// cache pressure.
+// A cardinality higher than this calculated value will call for smaller bucket size limit caused
+// by cache pressure.
 const cardinalityForCachePressure = Math.ceil(cacheSize / (2 * defaultBucketMaxSize));  // 1000
 
 const replSet = new ReplSetTest({
@@ -36,7 +37,7 @@ const db = replSet.getPrimary().getDB(jsTestName());
 
 const alwaysUseCompressedBuckets = TimeseriesTest.timeseriesAlwaysUseCompressedBucketsEnabled(db);
 
-const coll = db.getCollection('t');
+let coll = db.getCollection(collName + '1');
 coll.drop();
 assert.commandWorked(db.createCollection(
     coll.getName(), {timeseries: {timeField: timeFieldName, metaField: metaFieldName}}));
@@ -47,7 +48,8 @@ const formatStatsLog = ((stats) => {
 });
 
 // Inserts documents into the collection with increasing meta fields to generate N buckets. We make
-// sure to exceed the bucket min count per bucket to bypass large measurement checks.
+// sure to exceed the bucket min count per bucket to bypass large measurement checks. After this
+// call we should have numOfBuckets buckets each with size of around ~12KB.
 const initializeBucketsPastMinCount = function(numOfBuckets = 1) {
     jsTestLog("Inserting and generating buckets. Targeting '" + numOfBuckets + "' buckets.");
     let bulk = coll.initializeUnorderedBulkOp();
@@ -78,49 +80,65 @@ assert.eq(bucketsClosedDueToSize, 0, formatStatsLog(timeseriesStats));
 assert.eq(bucketsClosedDueToCachePressure, 0, formatStatsLog(timeseriesStats));
 assert.eq(timeseriesStats.bucketCount, belowCardinalityThreshold, formatStatsLog(timeseriesStats));
 
-// We only end up doing two passes before we start to close buckets due to size limits.
-while (bucketsClosedDueToSize == 0) {
-    jsTestLog("Inserting 50000 bytes of data into buckets.");
-    let bulk = coll.initializeUnorderedBulkOp();
-    for (let i = 0; i < belowCardinalityThreshold; i++) {
-        bulk.insert({
-            _id: '00' + i,
-            [timeFieldName]: timestamp,
-            [metaFieldName]: i,
-            value: "a".repeat(30000)
-        });
-        bulk.insert({
-            _id: '00' + i,
-            [timeFieldName]: timestamp,
-            [metaFieldName]: i,
-            value: "a".repeat(20000)
-        });
-    }
-    assert.commandWorked(bulk.execute());
-
-    timeseriesStats = assert.commandWorked(coll.stats()).timeseries;
-    bucketsClosedDueToSize = timeseriesStats.numBucketsClosedDueToSize;
-    bucketsClosedDueToCachePressure = timeseriesStats.numBucketsClosedDueToCachePressure;
-    compressedBuckets = timeseriesStats.numCompressedBuckets;
+// We insert enough data to cause buckets to roll over due to their size exceeding the maximum
+// bucket size. Because the cardinality is below the threshold at which the maximum bucket size
+// derived from cache pressure is smaller than the default maximum size, no buckets should be
+// closed due to cache pressure. Before this insertion each bucket should have a size of about
+// 12KB, so attempting an insertion of 120KB puts it at around 132KB, which is large enough to
+// roll over due to the default maximum bucket size (128KB) but not the cache derived maximum
+// bucket size (137KB).
+let bulk = coll.initializeUnorderedBulkOp();
+for (let i = 0; i < belowCardinalityThreshold; i++) {
+    bulk.insert(
+        {_id: '00' + i, [timeFieldName]: timestamp, [metaFieldName]: i, value: "a".repeat(120000)});
 }
 
-// On the second pass of inserts, we will close buckets due to the default size constraints. No
-// buckets should be closed due to cache pressure.
+assert.commandWorked(bulk.execute());
+timeseriesStats = assert.commandWorked(coll.stats()).timeseries;
+bucketsClosedDueToSize = timeseriesStats.numBucketsClosedDueToSize;
+bucketsClosedDueToCachePressure = timeseriesStats.numBucketsClosedDueToCachePressure;
+compressedBuckets = timeseriesStats.numCompressedBuckets;
+
+// We should be closing buckets due to the default size constraints. No buckets should be closed
+// due to cache pressure.
 assert.eq(bucketsClosedDueToSize, cardinalityForCachePressure, formatStatsLog(timeseriesStats));
 assert.eq(bucketsClosedDueToCachePressure, 0, formatStatsLog(timeseriesStats));
 if (!alwaysUseCompressedBuckets) {
     assert.eq(compressedBuckets, cardinalityForCachePressure, formatStatsLog(timeseriesStats));
 }
 
-// If we pass the cardinality point to simulate cache pressure, we will begin to see buckets closed
-// due to 'CachePressure' and not 'DueToSize'.
+// Create a new collection to test closing buckets due to cache pressure. Since the cardinality of
+// buckets is now high enough to make the cache derived maximum bucket size smaller than the
+// default maximum bucket size, buckets should begin to close because of cache pressure rather than
+// size.
+coll = db.getCollection(collName + '2');
+coll.drop();
+assert.commandWorked(db.createCollection(
+    coll.getName(), {timeseries: {timeField: timeFieldName, metaField: metaFieldName}}));
+
+// If we pass the cardinality point to simulate cache pressure, we will begin to see buckets
+// closed due to 'CachePressure' and not 'DueToSize'.
 const aboveCardinalityThreshold = cardinalityForCachePressure * 3 / 2;
 initializeBucketsPastMinCount(aboveCardinalityThreshold);
 
-let bulk = coll.initializeUnorderedBulkOp();
+timeseriesStats = assert.commandWorked(coll.stats()).timeseries;
+bucketsClosedDueToSize = timeseriesStats.numBucketsClosedDueToSize;
+bucketsClosedDueToCachePressure = timeseriesStats.numBucketsClosedDueToCachePressure;
+compressedBuckets = timeseriesStats.numCompressedBuckets;
+
+// Ensure we have not closed any buckets due to size or cache pressure.
+assert.eq(bucketsClosedDueToSize, 0, formatStatsLog(timeseriesStats));
+assert.eq(bucketsClosedDueToCachePressure, 0, formatStatsLog(timeseriesStats));
+
+// We insert 80KB of data into the buckets. After initialization the buckets should have a size of
+// about 10KB, and after adding 80KB of data they should be at around 90KB. This should be greater
+// than the cache derived maximum bucket size, should be about 55KB, but significantly below the
+// default maximum bucket size, which is still 128KB. Therefore, we should see buckets closing
+// due to cache pressure but none due to size.
+bulk = coll.initializeUnorderedBulkOp();
 for (let i = 0; i < aboveCardinalityThreshold; i++) {
     bulk.insert(
-        {_id: '00' + i, [timeFieldName]: timestamp, [metaFieldName]: i, value: "a".repeat(20000)});
+        {_id: '00' + i, [timeFieldName]: timestamp, [metaFieldName]: i, value: "a".repeat(80000)});
 }
 assert.commandWorked(bulk.execute());
 
@@ -129,21 +147,17 @@ bucketsClosedDueToSize = timeseriesStats.numBucketsClosedDueToSize;
 bucketsClosedDueToCachePressure = timeseriesStats.numBucketsClosedDueToCachePressure;
 compressedBuckets = timeseriesStats.numCompressedBuckets;
 
-// We expect 'bucketsClosedDueToSize' to remain the same but 'bucketsClosedDueToCachePressure' to
-// increase.
-assert.eq(bucketsClosedDueToSize, cardinalityForCachePressure, formatStatsLog(timeseriesStats));
+// We expect 'bucketsClosedDueToSize' to be 0.
+assert.eq(bucketsClosedDueToSize, 0, formatStatsLog(timeseriesStats));
 
-// Previously, the bucket max size was 128000 bytes, but under cache pressure using
-// 'aboveCardinalityThreshold', the max size drops to roughly ~85334. This means the old
-// measurements (up to 'cardinalityForCachePressure') will need to be closed since they are sized at
-// ~120000 bytes. The newly inserted measurements are only sized at ~(20000 * 3) bytes so stay open.
+// Previously, the bucket max size was 128KB, but under cache pressure using
+// 'aboveCardinalityThreshold', the max size drops to roughly ~55KB. Therfore, all of the buckets
+// should have been closed due to cache pressure.
 assert.eq(
-    bucketsClosedDueToCachePressure, cardinalityForCachePressure, formatStatsLog(timeseriesStats));
+    bucketsClosedDueToCachePressure, aboveCardinalityThreshold, formatStatsLog(timeseriesStats));
 
 if (!alwaysUseCompressedBuckets) {
-    // We expect the number of compressed buckets to double (independent to whether the buckets were
-    // closed due to size or cache pressure).
-    assert.eq(compressedBuckets, 2 * cardinalityForCachePressure, formatStatsLog(timeseriesStats));
+    assert.eq(compressedBuckets, aboveCardinalityThreshold, formatStatsLog(timeseriesStats));
 }
 
 replSet.stopSet();
