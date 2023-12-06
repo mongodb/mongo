864c94b7f376b0a9da2ab9210469da71f3de8a46
Etienne Petrel <etienne.petrel@mongodb.com>
2023-12-06 20:55:24 +0000

    Import wiredtiger: 5cd225f1f7020dfa0c48668fb15dc9aac671e1c5 from branch mongodb-master
    ref: 3afcf4ab0e..5cd225f1f7
    for: 7.3.0-rc0
    
    WT-12079 Disable pre-fetching for tiered tables
    
    GitOrigin-RevId: 8a9bc9da60bd340ca69789651fd1bf049f3c14ce

--- src/third_party/wiredtiger/src/session/session_prefetch.c
+++ src/third_party/wiredtiger/src/session/session_prefetch.c
@@ -15,6 +15,11 @@
 bool
 __wt_session_prefetch_check(WT_SESSION_IMPL *session, WT_REF *ref)
 {
+    /* Disable pre-fetch work on tiered tables. */
+    if (session->dhandle->type == WT_DHANDLE_TYPE_TIERED ||
+      session->dhandle->type == WT_DHANDLE_TYPE_TIERED_TREE)
+        return (false);
+
     if (S2C(session)->prefetch_queue_count > WT_MAX_PREFETCH_QUEUE)
         return (false);
 
