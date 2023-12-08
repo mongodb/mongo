f2f2ac663a07e4f98e2090c0ad2f03faf5516bb2
Brad Cater <brad.cater@mongodb.com>
2023-12-08 19:10:06 +0000

    SERVER-83863 Re-enable inmem_full.js test.
    GitOrigin-RevId: f23dcbda7024be64f56fc895c28b026ba4cdc8a6

--- jstests/noPassthrough/inmem_full.js
+++ jstests/noPassthrough/inmem_full.js
@@ -6,7 +6,8 @@ if (jsTest.options().storageEngine !== "inMemory") {
 
 Random.setRandomSeed();
 
-// Return array of approximately 1kB worth of random numbers.
+// Return array of approximately 680 bytes worth of random numbers.
+// This size was chosen to make `randomDoc()` ~10kB.
 function randomArray() {
     var arr = [];
     for (var j = 0; j < 85; j++)
@@ -19,6 +20,9 @@ function randomDoc() {
     var doc = {};
     for (var c of "abcdefghij")
         doc[c] = randomArray();
+    const docSizeKB = Object.bsonsize(doc) / 1024;
+    assert.lte(9.9, docSizeKB, "randomDoc() should be at least 9.9kB");
+    assert.gte(10, docSizeKB, "randomDoc() should be no more than 10kB");
     return doc;
 }
 
@@ -28,6 +32,18 @@ function randomBatch(batchSize) {
     for (var j = 0; j < batchSize; j++)
         batch[j] = randomDoc();
     return batch;
+}
+
+function randomBlob(sizeInKB) {
+    var s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var b = Array(sizeInKB * 1024)
+                .join()
+                .split(',')
+                .map(function() {
+                    return s.charAt(Math.floor(Random.rand() * s.length));
+                })
+                .join('');
+    return b;
 }
 
 const cacheMB = 128;
@@ -47,7 +63,9 @@ var t = db.large;
 // Insert documents until full.
 var res;
 var count = 0;
-for (var j = 0; j < 1000; j++) {
+for (var j = 0; j < 2 /* The size should never be off by more than a factor of 2. */ * cacheKB /
+         (docSizeKB * batchSize);
+     j++) {
     res = t.insert(batch);
     assert.gte(res.nInserted, 0, tojson(res));
     count += res.nInserted;
@@ -57,7 +75,7 @@ for (var j = 0; j < 1000; j++) {
     print("Inserted " + count + " documents");
 }
 assert.writeError(res, "didn't get ExceededMemoryLimit but should have");
-print("Inserted " + count + " documents");
+print(`Inserted ${count} documents`);
 
 // Should have encountered exactly one memory full error.
 assert.eq(res.getWriteErrorCount(), 1, tojson(res));
@@ -67,17 +85,29 @@ assert.eq(res.getWriteErrorAt(0).code, ErrorCodes.ExceededMemoryLimit, tojson(re
 assert.gt(count * docSizeKB, cacheKB * 0.75, "inserted data size is at least 75% of capacity");
 assert.lt(count * docSizeKB, cacheKB * 1.50, "inserted data size is at most 150% of capacity");
 
+// Adding a large field to all existing documents should run out of memory.
+// This is the repro reported in SERVER-22599.
+const blob = randomBlob(docSizeKB);
+assert.commandFailedWithCode(t.update({}, {$set: {blob: blob}}, false, true),
+                             ErrorCodes.ExceededMemoryLimit);
+
 // Indexes are sufficiently large that it should be impossible to add a new one.
 assert.commandFailedWithCode(t.createIndex({a: 1}), ErrorCodes.ExceededMemoryLimit);
 
 // An aggregate copying all 'a' and 'b' fields should run out of memory.
 // Can't test the specific error code, because it depends on whether the collection
 // creation already fails, or just the writing. Agg wraps the original error code.
-assert.commandFailed(
-    t.runCommand("aggregate", {pipeline: [{$project: {a: 1, b: 1}}, {$out: "test.out"}]}));
+// The `cursor` option became required in 3.6 unless you use `explain`; see
+// https://www.mongodb.com/docs/manual/reference/command/aggregate/#dbcmd.aggregate
+assert.commandFailedWithCode(
+    t.runCommand("aggregate",
+                 {pipeline: [{$project: {a: 1, b: 1}}, {$out: "test.out"}], cursor: {}}),
+    ErrorCodes.ExceededMemoryLimit);
 
 // Should still be able to query.
 assert.eq(t.find({}).itcount(), count, "cannot find expected number of documents");
 assert.eq(t.aggregate([{$group: {_id: null, count: {$sum: 1}}}]).next().count,
           count,
           "cannot aggregate expected number of documents");
+
+MongoRunner.stopMongod(mongod);
