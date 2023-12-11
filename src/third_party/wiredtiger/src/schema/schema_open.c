87b18329285463cac184a60cc2e0f62ccec17bcb
Luke Chen <luke.chen@mongodb.com>
2023-12-11 13:44:41 +1100

    Import wiredtiger: 4724bfae95006562b2a9b30803a04d04cd61c1b7 from branch mongodb-7.0
    ref: a8964f34f5..4724bfae95
    for: 7.0.5
    
    WT-7929 Investigate a solution to avoid FTDC stalls during checkpoint
    
    GitOrigin-RevId: b983adf16fce33dd03e2adf682d5361df5aaadd5

--- src/third_party/wiredtiger/src/schema/schema_open.c
+++ src/third_party/wiredtiger/src/schema/schema_open.c
@@ -261,10 +261,6 @@ __schema_open_index(
     const char *idxconf, *name, *tablename, *uri;
     bool match;
 
-    /* Check if we've already done the work. */
-    if (idxname == NULL && table->idx_complete)
-        return (0);
-
     cursor = NULL;
     idx = NULL;
     match = false;
@@ -387,6 +383,10 @@ __wt_schema_open_index(
   WT_SESSION_IMPL *session, WT_TABLE *table, const char *idxname, size_t len, WT_INDEX **indexp)
 {
     WT_DECL_RET;
+
+    /* Check if we've already done the work. */
+    if (idxname == NULL && table->idx_complete)
+        return (0);
 
     WT_WITH_TABLE_WRITE_LOCK(session,
       WT_WITH_TXN_ISOLATION(session, WT_ISO_READ_UNCOMMITTED,
