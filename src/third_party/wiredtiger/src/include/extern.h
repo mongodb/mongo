fc10c9997f58cbf8d2737fe8858bbec4cfcd4c1e
Hari Babu Kommi <haribabu.kommi@mongodb.com>
2023-12-13 12:04:30 +1100

    Import wiredtiger: 554d4ece6683275dc7169061282b896506afecce from branch mongodb-master
    ref: 4c678bc342..554d4ece66
    for: 7.3.0-rc0
    
    WT-11470 Remove prepare state check from assertion
    
    GitOrigin-RevId: 486b12523c12e62abdad3f4ca0e1ee23e443609b

--- src/third_party/wiredtiger/src/include/extern.h
+++ src/third_party/wiredtiger/src/include/extern.h
@@ -2372,8 +2372,6 @@ static inline int __wt_txn_idle_cache_check(WT_SESSION_IMPL *session)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 static inline int __wt_txn_modify(WT_SESSION_IMPL *session, WT_UPDATE *upd)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
-static inline int __wt_txn_modify_block(WT_SESSION_IMPL *session, WT_CURSOR_BTREE *cbt,
-  WT_UPDATE *upd, wt_timestamp_t *prev_tsp) WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
 static inline int __wt_txn_modify_check(WT_SESSION_IMPL *session, WT_CURSOR_BTREE *cbt,
   WT_UPDATE *upd, wt_timestamp_t *prev_tsp, u_int modify_type)
   WT_GCC_FUNC_DECL_ATTRIBUTE((warn_unused_result));
