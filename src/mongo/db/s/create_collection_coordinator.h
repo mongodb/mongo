f7274170f3c47be81ae748b8cfbbaa796b4735c5
Pol Pinol Castuera <pol.pinol@mongodb.com>
2023-12-14 14:51:04 +0000

    Revert "SERVER-83497 Simplify commit function for the new create collection"
    This reverts commit 36b030b0a538b65a9056678dd798353d3118cc48.
    
    GitOrigin-RevId: 230c18aefc374137152b59d9793d31087c1e1060

--- src/mongo/db/s/create_collection_coordinator.h
+++ src/mongo/db/s/create_collection_coordinator.h
@@ -217,11 +217,9 @@ private:
     void _createCollectionOnParticipants(
         const std::shared_ptr<executor::ScopedTaskExecutor>& executor);
 
-    // Commits the create collection operation to the sharding catalog within a transaction.
+    // Commits the create collection operation to the sharding catalog within a transaction. After
+    // that, it clears the filtering metadata on the primary shard.
     void _commitOnShardingCatalog(const std::shared_ptr<executor::ScopedTaskExecutor>& executor);
-
-    // Ensure that the change stream event gets emitted and install the new filtering metadata.
-    void _setPostCommitMetadata(const std::shared_ptr<executor::ScopedTaskExecutor>& executor);
 
     // Exit from the critical section on all the shards, unblocking reads and writes. On the
     // participant shards, it is set to clear the filtering metadata after exiting the critical
@@ -235,6 +233,8 @@ private:
                                              std::shared_ptr<executor::ScopedTaskExecutor> executor,
                                              const CancellationToken& token);
 
+    bool _validateCreateCollectionAlreadyCommitted(OperationContext* opCtx);
+
     mongo::ShardsvrCreateCollectionRequest _request;
 
     const BSONObj _critSecReason;
@@ -242,8 +242,8 @@ private:
     // Set on successful completion of the coordinator
     boost::optional<CreateCollectionResponse> _result;
 
-    // The fields below are populated on the first execution. They will need to be re-calculated on
-    // following executions of the coordinator in case of error.
+    // The fields below are only populated for the first execution, they will not be present if it
+    // is not the first run.
     boost::optional<UUID> _uuid;
     boost::optional<bool> _collectionEmpty;
     boost::optional<InitialSplitPolicy::ShardCollectionConfig> _initialChunks;
