ce67816901ba3d1c160a98e456b1a2abdfce8086
Wenqin Ye <wenqin908@gmail.com>
2023-12-15 16:12:32 +0000

    SERVER-82838: Change ReshardingOplogApplier to use w:1 write concern when persisting resharding oplog application progress
    GitOrigin-RevId: e6d13d6c92a1fd86144f27b89692a86836621bfd

--- src/mongo/db/s/resharding/resharding_oplog_applier.cpp
+++ src/mongo/db/s/resharding/resharding_oplog_applier.cpp
@@ -231,7 +231,8 @@ void ReshardingOplogApplier::_clearAppliedOpsAndStoreProgress(OperationContext* 
     store.upsert(
         opCtx,
         QUERY(ReshardingOplogApplierProgress::kOplogSourceIdFieldName << _sourceId.toBSON()),
-        builder.obj());
+        builder.obj(),
+        WriteConcerns::kLocalWriteConcern);
     _env->metrics()->onOplogEntriesApplied(_currentBatchToApply.size());
 
     _currentBatchToApply.clear();
