8a4cf2c2b1b3d2bfba7c4b5dc9d5b38925a76651
Etienne Petrel <etienne.petrel@mongodb.com>
2023-12-18 22:34:06 +0000

    Import wiredtiger: af5f9d7789ce9c7250b488a0c287ae1100eb5934 from branch mongodb-master
    ref: 86c499ea3a..af5f9d7789
    for: 7.3.0-rc0
    
    WT-12061 Skip compact06 with tiered storage
    
    GitOrigin-RevId: 41a562e8f4dcfbe12ac69afdc42266b4a37f92fd

--- src/third_party/wiredtiger/test/suite/test_compact06.py
+++ src/third_party/wiredtiger/test/suite/test_compact06.py
@@ -64,6 +64,10 @@ class test_compact06(wttest.WiredTigerTestCase):
             time.sleep(1)
 
     def test_background_compact_api(self):
+        # FIXME-WT-11399
+        if self.runningHook('tiered'):
+            self.skipTest("Compaction isn't supported on tiered tables")
+
         # We cannot trigger the background compaction on a specific API. Note that the URI is
         # not relevant here, the corresponding table does not need to exist for this check.
         self.assertRaisesWithMessage(wiredtiger.WiredTigerError, lambda:
