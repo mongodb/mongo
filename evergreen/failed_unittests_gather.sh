091f020384548ab5e2af481543bc60f27f33a7d2
Daniel Moody <dmoody256@gmail.com>
2023-12-19 19:03:15 -0600

    SERVER-84293 prevent unittests binaries from being copied unnecessarily on windows (#17686)
    GitOrigin-RevId: 79cf65503d9915d34b296f5071025cf242cb6b94

--- evergreen/failed_unittests_gather.sh
+++ evergreen/failed_unittests_gather.sh
@@ -26,21 +26,27 @@ while read -r core_file; do
     binary_file=$($gdb -batch --quiet -ex "core $core_file" 2> /dev/null | grep "Core was generated" | cut -f2 -d "\`" | cut -f1 -d "'" | cut -f1 -d " ")
     binary_file_locations=$binary_file
   else
+    echo "Checking core file '$core_file'"
     # Find the base file name from the core file name, note it may be truncated.
     # Remove leading 'dump_' and trailing '.<pid>.core' or '.<pid or time>.mdmp'
     binary_file=$(echo "$core_file" | sed "s/.*\///;s/dump_//;s/\..*\.core//;s/\..*\.mdmp//")
     # Locate the binary file. Since the base file name might be truncated, the find
     # may return more than 1 file.
-    binary_file_locations=$(/usr/bin/find -H . -executable -name "$binary_file*${exe}" 2> /dev/null)
+    if [ "$binary_file" != "" ]; then
+      binary_file_locations=$(/usr/bin/find -H . -executable -name "$binary_file*${exe}" 2> /dev/null)
+    fi
   fi
 
   if [ -z "$binary_file_locations" ]; then
     echo "Cannot locate the unittest binary file ($binary_file) that generated the core file $core_file"
+  else
+    echo "Files to save: $binary_file_locations"
   fi
 
   for binary_file_location in $binary_file_locations; do
     new_binary_file=$unittest_bin_dir/$(echo "$binary_file_location" | sed "s/.*\///")
     if [ -f "$binary_file_location" ] && [ ! -f "$new_binary_file" ]; then
+      echo "Direct Copy $binary_file_location to $new_binary_file"
       cp "$binary_file_location" "$new_binary_file"
     fi
 
@@ -48,6 +54,7 @@ while read -r core_file; do
     pdb_file=$(echo "$binary_file_location" | sed "s/\.exe/.pdb/")
     if [ -f "$pdb_file" ]; then
       new_pdb_file=$unittest_bin_dir/$(echo "$pdb_file" | sed "s/.*\///")
+      echo "PDB Copy $pdb_file to $new_pdb_file"
       cp "$pdb_file" "$new_pdb_file"
     fi
 
@@ -55,12 +62,14 @@ while read -r core_file; do
     # in the archive
     debug_file=$binary_file_location.debug
     if [ -f "$debug_file" ]; then
+      echo "debug Copy $debug_file to $unittest_bin_dir"
       cp "$debug_file" "$unittest_bin_dir"
     fi
 
     # On macOS, these are called .dSYM and they are directories
     dsym_dir=$binary_file_location.dSYM
     if [ -d "$dsym_dir" ]; then
+      echo "dsym Copy $dsym_dir to $unittest_bin_dir"
       cp -r "$dsym_dir" "$unittest_bin_dir"
     fi
 
