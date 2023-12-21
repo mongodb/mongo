15a26740776ce49b45b24693cce2c2b2a43c4980
huayu-ouyang <huayu.ouyang@mongodb.com>
2023-12-21 12:56:54 -0500

    SERVER-83955 Fix wrong warning messages in ReplSetGetStatus command (#17731)
    (cherry picked from commit 29c229f091ecfaf9d030283a04c4feecbd08fb6c)
    
    GitOrigin-RevId: 13ec5806527c24aeed223381b374b5bb35e94327

--- src/mongo/db/repl/repl_set_commands.cpp
+++ src/mongo/db/repl/repl_set_commands.cpp
@@ -153,14 +153,17 @@ public:
                         result.append("lastStableRecoveryTimestamp", ts.get());
                     }
                 } else {
-                    LOGV2_WARNING(6100700,
-                                  "Failed to get last stable recovery timestamp due to {error}",
-                                  "error"_attr = "lock acquire timeout"_sd);
+                    LOGV2_WARNING(
+                        6100700,
+                        "Failed to get last stable recovery timestamp due to lock acquire timeout. "
+                        "Note this is expected if shutdown is in progress.");
                 }
             } catch (const ExceptionForCat<ErrorCategory::Interruption>& ex) {
-                LOGV2_WARNING(6100701,
-                              "Failed to get last stable recovery timestamp due to {error}",
-                              "error"_attr = redact(ex));
+                LOGV2_WARNING(
+                    6100701,
+                    "Failed to get last stable recovery timestamp due to cancellation error. Note "
+                    "this is expected if shutdown is in progress.",
+                    "error"_attr = redact(ex));
             }
             return true;
         } else if (cmdObj.hasElement("restartHeartbeats")) {
