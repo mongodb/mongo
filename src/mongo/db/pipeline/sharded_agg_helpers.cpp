c6dd04f0b9501ea59165ecfac8cc0aa5ef663baf
Ivan Fefer <ivan.fefer@mongodb.com>
2023-12-27 13:43:00 +0000

    SERVER-82111 In sharded explain, check invariants only if remote response status is ok
    GitOrigin-RevId: 9d3acd628782dfa045a1f67313e635d958d4b8a6

--- src/mongo/db/pipeline/sharded_agg_helpers.cpp
+++ src/mongo/db/pipeline/sharded_agg_helpers.cpp
@@ -1436,10 +1436,10 @@ Status appendExplainResults(DispatchShardPipelineResults&& dispatchResults,
 
     BSONObjBuilder shardExplains(result->subobjStart("shards"));
     for (const auto& shardResult : dispatchResults.remoteExplainOutput) {
-        invariant(shardResult.shardHostAndPort);
-
         uassertStatusOK(shardResult.swResponse.getStatus());
         uassertStatusOK(getStatusFromCommandResult(shardResult.swResponse.getValue().data));
+
+        invariant(shardResult.shardHostAndPort);
 
         auto shardId = shardResult.shardId.toString();
         const auto& data = shardResult.swResponse.getValue().data;
