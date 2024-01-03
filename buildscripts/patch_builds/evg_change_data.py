9d7f46202ace37bb0ff4e14542ba8de5697c250b
stevegrossmongodb <136485666+stevegrossmongodb@users.noreply.github.com>
2024-01-03 15:02:00 -0500

    SERVER-84479 Adjust burn_in_tests to tolerate missing manifest (#17852)
    GitOrigin-RevId: 6ce769e4d0c4b62de3b0d7fd4dc152bd25254882

--- buildscripts/patch_builds/evg_change_data.py
+++ buildscripts/patch_builds/evg_change_data.py
@@ -2,6 +2,9 @@
 from typing import List
 
 from git import Repo
+from requests import HTTPError
+import yaml
+
 from evergreen import EvergreenApi
 
 from buildscripts.patch_builds.change_data import RevisionMap, generate_revision_map
@@ -17,11 +20,20 @@ def generate_revision_map_from_manifest(repos: List[Repo], task_id: str,
     :param evg_api: Evergreen API object.
     :return: Map of repositories to revisions
     """
-    manifest = evg_api.manifest_for_task(task_id)
-    revisions_data = {
-        module_name: module.revision
-        for module_name, module in manifest.modules.items()
-    }
-    revisions_data["mongo"] = manifest.revision
-
-    return generate_revision_map(repos, revisions_data)
+    try:
+        manifest = evg_api.manifest_for_task(task_id)
+        revisions_data = {
+            module_name: module.revision
+            for module_name, module in manifest.modules.items()
+        }
+        revisions_data["mongo"] = manifest.revision
+        return generate_revision_map(repos, revisions_data)
+    except HTTPError as err:
+        # Manifest unavailable from Evergreen? Just make a fake one:
+        if err.response.status_code == 404:
+            with open("../expansions.yml") as file_handle:
+                return generate_revision_map(repos,
+                                             {"mongo": yaml.safe_load(file_handle)["revision"]})
+        # Some other Evergreen error? Raise it up the stack!
+        else:
+            raise err
