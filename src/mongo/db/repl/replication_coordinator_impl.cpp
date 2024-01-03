be8469bbfbd66e2bea2652381592ac76f05da16d
Matthew Russotto <matthew.russotto@mongodb.com>
2024-01-03 15:07:18 -0500

    SERVER-84440 Expose the number of replication waiters in serverStatus
    (cherry picked from commit 916e8966874206ec19f2ad8ebc04bfd632c7831a)
    
    GitOrigin-RevId: 9a9635810809ff4a9decbf5131fcf1046abaf7a4

--- src/mongo/db/repl/replication_coordinator_impl.cpp
+++ src/mongo/db/repl/replication_coordinator_impl.cpp
@@ -180,6 +180,13 @@ ServerStatusMetricField<Counter64> displayNumAutoReconfigs(
     "repl.reconfig.numAutoReconfigsForRemovalOfNewlyAddedFields",
     &numAutoReconfigsForRemovalOfNewlyAddedFields);
 
+Atomic64Metric replicationWaiterListMetric;
+ServerStatusMetricField<Atomic64Metric> displayReplicationWaiterListMetric(
+    "repl.waiters.replication", &replicationWaiterListMetric);
+Atomic64Metric opTimeWaiterListMetric;
+ServerStatusMetricField<Atomic64Metric> displayOpTimeWaiterListMetric("repl.waiters.opTime",
+                                                                      &opTimeWaiterListMetric);
+
 using namespace fmt::literals;
 
 using CallbackArgs = executor::TaskExecutor::CallbackArgs;
@@ -217,15 +224,24 @@ constexpr StringData kQuiesceModeShutdownMessage =
 
 }  // namespace
 
+ReplicationCoordinatorImpl::WaiterList::WaiterList(Atomic64Metric& waiterCountMetric)
+    : _waiterCountMetric(waiterCountMetric) {}
+
+void ReplicationCoordinatorImpl::WaiterList::_updateMetric_inlock() {
+    _waiterCountMetric.set(_waiters.size());
+}
+
 void ReplicationCoordinatorImpl::WaiterList::add_inlock(const OpTime& opTime,
                                                         SharedWaiterHandle waiter) {
     _waiters.emplace(opTime, std::move(waiter));
+    _updateMetric_inlock();
 }
 
 SharedSemiFuture<void> ReplicationCoordinatorImpl::WaiterList::add_inlock(
     const OpTime& opTime, boost::optional<WriteConcernOptions> wc) {
     auto pf = makePromiseFuture<void>();
     _waiters.emplace(opTime, std::make_shared<Waiter>(std::move(pf.promise), std::move(wc)));
+    _updateMetric_inlock();
     return std::move(pf.future);
 }
 
@@ -233,6 +249,7 @@ bool ReplicationCoordinatorImpl::WaiterList::remove_inlock(SharedWaiterHandle wa
     for (auto iter = _waiters.begin(); iter != _waiters.end(); iter++) {
         if (iter->second == waiter) {
             _waiters.erase(iter);
+            _updateMetric_inlock();
             return true;
         }
     }
@@ -256,6 +273,7 @@ void ReplicationCoordinatorImpl::WaiterList::setValueIf_inlock(Func&& func,
             it = _waiters.erase(it);
         }
     }
+    _updateMetric_inlock();
 }
 
 void ReplicationCoordinatorImpl::WaiterList::setValueAll_inlock() {
@@ -263,6 +281,7 @@ void ReplicationCoordinatorImpl::WaiterList::setValueAll_inlock() {
         waiter->promise.emplaceValue();
     }
     _waiters.clear();
+    _updateMetric_inlock();
 }
 
 void ReplicationCoordinatorImpl::WaiterList::setErrorAll_inlock(Status status) {
@@ -271,6 +290,7 @@ void ReplicationCoordinatorImpl::WaiterList::setErrorAll_inlock(Status status) {
         waiter->promise.setError(status);
     }
     _waiters.clear();
+    _updateMetric_inlock();
 }
 
 namespace {
@@ -325,6 +345,8 @@ ReplicationCoordinatorImpl::ReplicationCoordinatorImpl(
       _topCoord(std::move(topCoord)),
       _replExecutor(std::move(executor)),
       _externalState(std::move(externalState)),
+      _replicationWaiterList(replicationWaiterListMetric),
+      _opTimeWaiterList(opTimeWaiterListMetric),
       _inShutdown(false),
       _memberState(MemberState::RS_STARTUP),
       _rsConfigState(kConfigPreStart),
