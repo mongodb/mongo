0f9043d02dd25134ed3165f87020b9a5dcdfd5ce
Luke Pearson <luke.pearson@mongodb.com>
2024-01-04 13:29:30 +1100

    Import wiredtiger: e1652db215fde207bb78a73bc54176a266d48d57 from branch mongodb-master
    ref: 291c7d85e0..e1652db215
    for: 7.3.0-rc0
    
    WT-12200 Resolve inappropriately colorized build failure output
    
    GitOrigin-RevId: 6915e8fda80c2888c9dfda163f33d74b4afa2de1

--- src/third_party/wiredtiger/cmake/configs/base.cmake
+++ src/third_party/wiredtiger/cmake/configs/base.cmake
@@ -171,6 +171,12 @@ config_bool(
 )
 
 config_bool(
+    ENABLE_COLORIZE_OUTPUT
+    "Compile with build error colors enabled"
+    DEFAULT ON
+)
+
+config_bool(
     ENABLE_PYTHON
     "Configure the python API"
     DEFAULT ${default_enable_python}
