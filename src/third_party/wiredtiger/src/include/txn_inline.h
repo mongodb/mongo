dc6c5974799b479eeaaa3bda2202f54c54031741
Marc Butler <135883+marcbutler@users.noreply.github.com>
2024-01-08 01:36:49 +0000

    Import wiredtiger: 7a122e7167895a13b4b4037bb675a5717ce29d11 from branch mongodb-master
    ref: 73ab3d287e..7a122e7167
    for: 7.3.0-rc0
    
    WT-12003 Moving read barrier ahead of second read of prepare state
    
    GitOrigin-RevId: dd42ba85cb0ea76e45fe1ef7c1096ca2a4a7cc41

--- src/third_party/wiredtiger/src/include/txn_inline.h
+++ src/third_party/wiredtiger/src/include/txn_inline.h
@@ -930,7 +930,7 @@ __wt_txn_visible(
 static inline WT_VISIBLE_TYPE
 __wt_txn_upd_visible_type(WT_SESSION_IMPL *session, WT_UPDATE *upd)
 {
-    uint8_t prepare_state, previous_state;
+    uint8_t prepare_state;
     bool upd_visible;
 
     for (;; __wt_yield()) {
@@ -949,10 +949,13 @@ __wt_txn_upd_visible_type(WT_SESSION_IMPL *session, WT_UPDATE *upd)
         /*
          * The visibility check is only valid if the update does not change state. If the state does
          * change, recheck visibility.
+         *
+         * We need to place a read barrier prior to the second read of prepare state as otherwise it
+         * could overlap with the reads of the transaction id and start timestamp. Which would
+         * invalidate this check.
          */
-        previous_state = prepare_state;
-        WT_ORDERED_READ(prepare_state, upd->prepare_state);
-        if (previous_state == prepare_state)
+        WT_READ_BARRIER();
+        if (prepare_state == upd->prepare_state)
             break;
 
         WT_STAT_CONN_INCR(session, prepared_transition_blocked_page);
