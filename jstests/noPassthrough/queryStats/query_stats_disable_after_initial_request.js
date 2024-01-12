d35fa286978de98ced015cfedebef8a21f1ae703
Will Buerger <will.buerger@mongodb.com>
2024-01-12 19:04:57 +0000

    SERVER-84567: writeQueryStats logs an error rather than uassert when the feature is disabled

---
+++ jstests/noPassthrough/queryStats/query_stats_disable_after_initial_request.js
@@ -0,0 +1,98 @@
+/**
+ * Tests that query stats aren't collected if the feature is enabled initially but is disabled
+ * before the lifetime of the request is complete.
+ * @tags: [
+ *   # Query Stats is new in 7.1.
+ *   requires_fcv_71,
+ * ]
+ */
+import {getQueryStatsFindCmd} from "jstests/libs/query_stats_utils.js";
+
+// Test that no query stats entry is written when (1) dispatching an initial find query, (2)
+// disabling query stats, then (3) completing the command. Below, we run variations of this test
+// with combinations of different strategies to disable query stats and to end the command.
+function testStatsAreNotCollectedWhenDisabledBeforeCommandCompletion(
+    {conn, coll, disableQueryStatsFn, endCommandFn, enableQueryStatsFn}) {
+    // Issue a find commannd with a batchSize of 1 so that the query is not exhausted.
+    const cursor = coll.find({foo: 1}).batchSize(1);
+    // Must run .next() to make sure the initial request is executed now.
+    cursor.next();
+
+    // Disable query stats, then end the command,which triggers the path to writeQueryStats.
+    disableQueryStatsFn();
+    endCommandFn(cursor);
+
+    // Must re-enable query stats in order to check via $queryStats that nothing was recorded.
+    enableQueryStatsFn();
+    const res = getQueryStatsFindCmd(conn);
+    assert.eq(res.length, 0, res);
+}
+
+// Turn on the collecting of queryStats metrics.
+let options = {setParameter: {internalQueryStatsRateLimit: -1}};
+
+const conn = MongoRunner.runMongod(options);
+const testDB = conn.getDB('test');
+var coll = testDB[jsTestName()];
+coll.drop();
+
+const bulk = coll.initializeUnorderedBulkOp();
+const numDocs = 100;
+for (let i = 0; i < numDocs / 2; ++i) {
+    bulk.insert({foo: 0, bar: Math.floor(Math.random() * 3)});
+    bulk.insert({foo: 1, bar: Math.floor(Math.random() * -2)});
+}
+assert.commandWorked(bulk.execute());
+
+function setQueryStatsCacheSize(size) {
+    assert.commandWorked(testDB.adminCommand({setParameter: 1, internalQueryStatsCacheSize: size}));
+}
+
+function setFCV(newFCV) {
+    assert.commandWorked(
+        testDB.adminCommand({setFeatureCompatibilityVersion: newFCV, confirm: true}));
+}
+
+// Tests the scenario of disabling query stats by setting internalQueryStatsCacheSize to 0 and
+// ending the command by running it to completion.
+testStatsAreNotCollectedWhenDisabledBeforeCommandCompletion({
+    conn: testDB,
+    coll,
+    disableQueryStatsFn: () => setQueryStatsCacheSize("0MB"),
+    endCommandFn: (cursor) => cursor.itcount(),
+    enableQueryStatsFn: () => setQueryStatsCacheSize("10MB")
+});
+
+// Tests the scenario of disabling query stats by setting internalQueryStatsCacheSize to 0 and
+// ending the command by killing the cursor.
+testStatsAreNotCollectedWhenDisabledBeforeCommandCompletion({
+    conn: testDB,
+    coll,
+    disableQueryStatsFn: () => setQueryStatsCacheSize("0MB"),
+    endCommandFn: (cursor) => assert.commandWorked(
+        testDB.runCommand({killCursors: coll.getName(), cursors: [cursor.getId()]})),
+    enableQueryStatsFn: () => setQueryStatsCacheSize("10MB")
+});
+
+// Tests the scenario of disabling query stats by downgrading the FCV and ending the command by
+// running it to completion.
+testStatsAreNotCollectedWhenDisabledBeforeCommandCompletion({
+    conn: testDB,
+    coll,
+    disableQueryStatsFn: () => setFCV(lastLTSFCV),
+    endCommandFn: (cursor) => cursor.itcount(),
+    enableQueryStatsFn: () => setFCV(binVersionToFCV("latest"))
+});
+
+// Tests the scenario of disabling query stats by downgrading the FCV and ending the command by
+// killing the cursor.
+testStatsAreNotCollectedWhenDisabledBeforeCommandCompletion({
+    conn: testDB,
+    coll,
+    disableQueryStatsFn: () => setFCV(lastLTSFCV),
+    endCommandFn: (cursor) => assert.commandWorked(
+        testDB.runCommand({killCursors: coll.getName(), cursors: [cursor.getId()]})),
+    enableQueryStatsFn: () => setFCV(binVersionToFCV("latest"))
+});
+
+MongoRunner.stopMongod(conn);
--- src/mongo/db/query/query_stats/query_stats.cpp
+++ src/mongo/db/query/query_stats/query_stats.cpp
@@ -207,6 +207,40 @@ void updateStatistics(const QueryStatsStore::Partition& proofOfLock,
     toUpdate.docsReturned.aggregate(docsReturned);
 }
 
+void insertQueryStatsEntry(QueryStatsStore::Partition& proofOfLock,
+                           QueryStatsStore& queryStatsStore,
+                           boost::optional<size_t> queryStatsKeyHash,
+                           std::unique_ptr<Key> key,
+                           const uint64_t queryExecMicros,
+                           const uint64_t firstResponseExecMicros,
+                           const uint64_t docsReturned) {
+    tassert(7315200,
+            "key cannot be null when writing a new entry to the queryStats store",
+            key != nullptr);
+    size_t numEvicted =
+        queryStatsStore.put(*queryStatsKeyHash, QueryStatsEntry(std::move(key)), proofOfLock);
+    queryStatsEvictedMetric.increment(numEvicted);
+    auto newMetrics = proofOfLock->get(*queryStatsKeyHash);
+    if (!newMetrics.isOK()) {
+        // This can happen if the budget is immediately exceeded. Specifically
+        // if the there is not enough room for a single new entry if the number
+        // of partitions is too high relative to the size.
+        queryStatsStoreWriteErrorsMetric.increment();
+        LOGV2_DEBUG(7560900,
+                    0,
+                    "Failed to store queryStats entry.",
+                    "status"_attr = newMetrics.getStatus(),
+                    "queryStatsKeyHash"_attr = queryStatsKeyHash);
+        return;
+    }
+
+    return updateStatistics(proofOfLock,
+                            newMetrics.getValue()->second,
+                            queryExecMicros,
+                            firstResponseExecMicros,
+                            docsReturned);
+}
+
 }  // namespace
 
 /**
@@ -302,7 +336,22 @@ void writeQueryStats(OperationContext* opCtx,
     if (!queryStatsKeyHash) {
         return;
     }
-    auto&& queryStatsStore = getQueryStatsStore(opCtx);
+
+    // It's possible that query stats was enabled in registerRequest but has been disabled since
+    // (e.g., by FCV downgrade or setting the store size to 0). Rather than calling
+    // getQueryStatsStore (which would trigger a uassert if query stats is disabled), we return and
+    // log a message if query stats is disabled, and otherwise grab the query stats store directly.
+    if (!isQueryStatsEnabled(opCtx->getServiceContext(),
+                             /*requiresFullQueryStatsFeatureFlag*/ false)) {
+        LOGV2_DEBUG(8456700,
+                    2,
+                    "Query stats was enabled when the command started but is now disabled. "
+                    "Metrics will not be collected.",
+                    "queryStatsKeyHash"_attr = queryStatsKeyHash);
+        return;
+    }
+    auto&& queryStatsStore =
+        queryStatsStoreDecoration(opCtx->getServiceContext())->getQueryStatsStore();
     auto&& [statusWithMetrics, partitionLock] =
         queryStatsStore.getWithPartitionLock(*queryStatsKeyHash);
     if (statusWithMetrics.isOK()) {
@@ -315,30 +364,12 @@ void writeQueryStats(OperationContext* opCtx,
     }
 
     // Otherwise we didn't find an existing entry. Try to create one.
-    tassert(7315200,
-            "key cannot be null when writing a new entry to the telemetry store",
-            key != nullptr);
-    size_t numEvicted =
-        queryStatsStore.put(*queryStatsKeyHash, QueryStatsEntry(std::move(key)), partitionLock);
-    queryStatsEvictedMetric.increment(numEvicted);
-    auto newMetrics = partitionLock->get(*queryStatsKeyHash);
-    if (!newMetrics.isOK()) {
-        // This can happen if the budget is immediately exceeded. Specifically if the there is
-        // not enough room for a single new entry if the number of partitions is too high
-        // relative to the size.
-        queryStatsStoreWriteErrorsMetric.increment();
-        LOGV2_DEBUG(7560900,
-                    0,
-                    "Failed to store queryStats entry.",
-                    "status"_attr = newMetrics.getStatus(),
-                    "queryStatsKeyHash"_attr = queryStatsKeyHash);
-        return;
-    }
-
-    return updateStatistics(partitionLock,
-                            newMetrics.getValue()->second,
-                            queryExecMicros,
-                            firstResponseExecMicros,
-                            docsReturned);
+    return insertQueryStatsEntry(partitionLock,
+                                 queryStatsStore,
+                                 queryStatsKeyHash,
+                                 std::move(key),
+                                 queryExecMicros,
+                                 firstResponseExecMicros,
+                                 docsReturned);
 }
 }  // namespace mongo::query_stats
