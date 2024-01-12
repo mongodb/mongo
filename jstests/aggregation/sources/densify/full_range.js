09da4a8698e4bc43466bfa8b7d2498aa5dc8df21
Lynne Wang <lynne.wang@mongodb.com>
2024-01-12 18:37:50 -0500

    SERVER-85206 Improve performance of full_range.js and explicit_range.js
    GitOrigin-RevId: 7aa19f3911c8668899c637346731eb7cbf2cd22a

--- jstests/aggregation/sources/densify/full_range.js
+++ jstests/aggregation/sources/densify/full_range.js
@@ -6,43 +6,48 @@
  * ]
  */
 
-load("jstests/aggregation/sources/densify/libs/densify_in_js.js");
+import {
+    densifyUnits,
+    getArithmeticFunctionsForUnit,
+    insertDocumentsOnPredicate,
+    insertDocumentsOnStep,
+    interestingSteps,
+    testDensifyStage,
+} from "jstests/aggregation/sources/densify/libs/densify_in_js.js";
 
-(function() {
-"use strict";
 const collName = jsTestName();
 const coll = db.getCollection(collName);
 coll.drop();
 
 // Run all tests for each date unit and on numeric values.
 for (let i = 0; i < densifyUnits.length; i++) {
-    const unit = densifyUnits[i];
     coll.drop();
+
+    const unit = densifyUnits[i];
     const base = unit ? new ISODate("2021-01-01") : 0;
     const {add} = getArithmeticFunctionsForUnit(unit);
 
     // Run all tests for different step values.
     for (let i = 0; i < interestingSteps.length; i++) {
         const step = interestingSteps[i];
-        const runDensifyFullTest = (msg) =>
-            testDensifyStage({field: "val", range: {step, bounds: "full", unit: unit}}, coll, msg);
+        const stage = {field: "val", range: {step: step, bounds: "full", unit: unit}};
 
-        // Fill in docs between 1 and 99.
+        // Fill in docs between 1 and 10.
         coll.drop();
         coll.insert({val: base});
-        coll.insert({val: add(base, 99)});
-        runDensifyFullTest();
+        coll.insert({val: add(base, 10)});
+        testDensifyStage(stage, coll);
 
         // Negative numbers and dates before the epoch.
         coll.drop();
-        insertDocumentsOnStep({base, min: -100, max: -1, step: 2, addFunc: add, coll: coll});
-        runDensifyFullTest();
+        insertDocumentsOnStep({base, min: -10, max: -1, step: 2, addFunc: add, coll: coll});
+        testDensifyStage(stage, coll);
 
         // Lots of off-step documents.
         coll.drop();
         insertDocumentsOnPredicate(
-            {base, min: 0, max: 50, pred: i => i % 3 == 0 || i % 7 == 0, addFunc: add, coll: coll});
-        runDensifyFullTest();
+            {base, min: 0, max: 10, pred: i => i % 3 == 0 || i % 7 == 0, addFunc: add, coll: coll});
+        testDensifyStage(stage, coll);
 
         // Lots of off-step documents with nulls sprinkled in to confirm that a null value is
         // treated the same as a missing value.
@@ -69,7 +74,16 @@ for (let i = 0; i < densifyUnits.length; i++) {
             coll: coll
         });
 
-        runDensifyFullTest();
+        testDensifyStage(stage, coll);
     }
 }
-})();
+
+// Test that full range does not fail if there's only one document in the collection.
+coll.drop();
+coll.insert({_id: 1, val: 1, orig: true});
+let result = coll.aggregate([
+    {"$densify": {"field": "val", "range": {"step": 2, "bounds": "full"}}},
+]);
+const expected = [{_id: 1, val: 1, orig: true}];
+const resultArray = result.toArray();
+assert.sameMembers(resultArray, expected);
