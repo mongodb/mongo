b32b139dc12a345040088f769ac769650ea82011
Kishore Devireddy <kishore.devireddy@mongodb.com>
2024-01-18 16:29:49 +0000

    SERVER-84002 Benchmark test for Oplog writing
    GitOrigin-RevId: 7a104227ad1e4443b89ffeb4a27a998120c6c7a4

--- src/mongo/db/repl/data_replicator_external_state_mock.cpp
+++ src/mongo/db/repl/data_replicator_external_state_mock.cpp
@@ -58,6 +58,12 @@ public:
           _observer(observer),
           _externalState(externalState) {}
 
+    void scheduleWritesToOplogAndChangeCollection(OperationContext* opCtx,
+                                                  StorageInterface* storageInterface,
+                                                  ThreadPool* writerPool,
+                                                  const std::vector<OplogEntry>& ops,
+                                                  bool skipWritesToOplog) override;
+
 private:
     void _run(OplogBuffer* oplogBuffer) final {}
     StatusWith<OpTime> _applyOplogBatch(OperationContext* opCtx,
@@ -68,6 +74,12 @@ private:
     OplogApplier::Observer* const _observer;
     DataReplicatorExternalStateMock* const _externalState;
 };
+
+void OplogApplierMock::scheduleWritesToOplogAndChangeCollection(OperationContext* opCtx,
+                                                                StorageInterface* storageInterface,
+                                                                ThreadPool* writerPool,
+                                                                const std::vector<OplogEntry>& ops,
+                                                                bool skipWritesToOplog) {}
 
 }  // namespace
 
