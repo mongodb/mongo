fad8ffdf83f6070b0f2f09b377a97051863b6066
Brad Cater <152920274+brad-cater-mongodb@users.noreply.github.com>
2024-01-19 21:08:00 -0500

    SERVER-85297 Disable a listIndexes output test for incompatible versions. (#18103)
    GitOrigin-RevId: 135fb10e8727a27851bb209f437d7fd856a96712

--- jstests/libs/clustered_collections/clustered_collection_util.js
+++ jstests/libs/clustered_collections/clustered_collection_util.js
@@ -81,9 +81,16 @@ export var ClusteredCollectionUtil = class {
             createOptions.expireAfterSeconds !== null) {
             extraData.expireAfterSeconds = createOptions.expireAfterSeconds;
         }
-        const expectedListIndexesOutput =
-            Object.extend(extraData, fullCreateOptions.clusteredIndex);
-        assert.docEq(expectedListIndexesOutput, listIndexes.cursor.firstBatch[0]);
+        // TODO (SERVER-85312): Re-enable this after we branch for 8.1 so that multiversion
+        // will test 8.1 <> 8.0.
+        const version = db.version().split('.');
+        if (version[0] >= 8 || (version[0] == 7 && version[1] == 3) ||
+            typeof (extraData.expireAfterSeconds) === 'undefined' ||
+            extraData.expireAfterSeconds === null) {
+            const expectedListIndexesOutput =
+                Object.extend(extraData, fullCreateOptions.clusteredIndex);
+            assert.docEq(expectedListIndexesOutput, listIndexes.cursor.firstBatch[0]);
+        }
     }
 
     static testBasicClusteredCollection(db, collName, clusterKey) {
