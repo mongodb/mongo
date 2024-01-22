f18f8e23fcb423bfb818be5cdf8d49d4e32a8fb6
Etienne Petrel <77254506+etienneptl@users.noreply.github.com>
2024-01-22 13:07:52 +1100

    SERVER-85428 Update autoCompact help function with runOnce (#18165)
    GitOrigin-RevId: 297b7f547a9f46ca38b4a157f37f2685f98347cd

--- src/mongo/db/commands/auto_compact.cpp
+++ src/mongo/db/commands/auto_compact.cpp
@@ -100,8 +100,10 @@ public:
                "warning: compact operation has blocking behaviour and is slow, enabling auto "
                "compact will allow compact to run on any collection at any time. You can cancel "
                "by disabling auto compact.\n"
-               "{ autoCompact : <bool>, [force:<bool>], [freeSpaceTargetMB:<int64_t>] }\n"
-               "  force - allows to run on a replica set primary\n";
+               "{ autoCompact : <bool>, [force:<bool>], [freeSpaceTargetMB:<int64_t>], "
+               "[runOnce:<bool>] }\n"
+               "  force - allows to run on a replica set primary\n"
+               "  runOnce - executes compaction on the database only once\n";
     }
 };
 
