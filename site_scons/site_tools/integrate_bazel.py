749798bdb6896bf9467e79026dd4a094b92191d1
Zack Winter <3457246+zackwintermdb@users.noreply.github.com>
2024-01-24 16:49:47 -0800

    SERVER-85667 Disable Bazel Remote Execution on Public Releases (#18346) (#18358)
    GitOrigin-RevId: 46defc225c999e47bc7791d11d443bd7ee86db5e

--- site_scons/site_tools/integrate_bazel.py
+++ site_scons/site_tools/integrate_bazel.py
@@ -482,6 +482,12 @@ def generate(env: SCons.Environment.Environment) -> None:
         if normalized_os != "linux" or normalized_arch not in ["arm64", 'amd64']:
             bazel_internal_flags.append('--config=local')
 
+        # Disable remote execution for public release builds.
+        if env.GetOption("release") is not None and (
+                env.GetOption("cache-dir") is None
+                or env.GetOption("cache-dir") == "$BUILD_ROOT/scons/cache"):
+            bazel_internal_flags.append('--config=public-release')
+
         Globals.bazel_base_build_command = [
             os.path.abspath("bazelisk"),
             'build',
