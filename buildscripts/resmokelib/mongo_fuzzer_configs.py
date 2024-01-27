0b3f057e79aa0d3c95908db3f7762ee9a56f0fd5
Evelyn Wu <32441547+evelynwu88@users.noreply.github.com>
2024-01-26 17:50:12 -0800

    SERVER-82952 Add replication parameters to config fuzzer  (#18380)
    GitOrigin-RevId: a587760f760f37d3fe5002df17a87b4bdc0a0732

--- buildscripts/resmokelib/mongo_fuzzer_configs.py
+++ buildscripts/resmokelib/mongo_fuzzer_configs.py
@@ -147,6 +147,23 @@ def generate_mongod_parameters(rng, mode):
     ret["wiredTigerConcurrentReadTransactions"] = rng.randint(5, 32)
     ret["wiredTigerStressConfig"] = False if mode != 'stress' else rng.choice([True, False])
 
+    ret["oplogFetcherUsesExhaust"] = rng.choice([True, False])
+    ret["replWriterThreadCount"] = rng.randint(1, 256)
+    # The actual maximum of `replBatchLimitOperations` is 1000 * 1000 but this range doesn't work
+    # for WINDOWS DEBUG, so that maximum is multiplied by 0.2, which is still a lot more than the
+    # default value of 5000. The reason why the full range [1, 1000*1000] doesn't work on WINDOWS
+    # DEBUG seems to be because it would wait for the batch to fill up to the batch limit
+    # operations, but when that number is too high it would just time out before reaching the
+    # batch limit operations.
+    ret["replBatchLimitOperations"] = rng.randint(1, 0.2 * 1000 * 1000)
+    ret["replBatchLimitBytes"] = rng.randint(16 * 1024 * 1024, 100 * 1024 * 1024)
+    # For `initialSyncSourceReadPreference`, the option `secondary` is excluded from the fuzzer
+    # because the generated mongod parameters are used for every node in the replica set, so the
+    # secondaries in the replica set will not be able to find a valid sync source.
+    ret["initialSyncSourceReadPreference"] = rng.choice(
+        ["nearest", "primary", "primaryPreferred", "secondaryPreferred"])
+    ret["initialSyncMethod"] = rng.choice(["fileCopyBased", "logical"])
+
     # We need a higher timeout to account for test slowness
     ret["receiveChunkWaitForRangeDeleterTimeoutMS"] = 300000
     ret["defaultConfigCommandTimeoutMS"] = 90000
