ce048b2b0564a98a8ae220b15c2c3f9dd22ad03a
Luke Chen <luke.chen@mongodb.com>
2024-01-29 15:28:04 +1100

    Import wiredtiger: de2cf593c444cf3f5b4b34ee3bce0d3435de4b90 from branch mongodb-7.3
    ref: d3faff25c4..de2cf593c4
    for: 7.3.0-rc2
    
    WT-12311 Update compact progress message related to estimated work
    
    GitOrigin-RevId: af83b4d05e28ccf8aa909c757d305716c926fe03

--- src/third_party/wiredtiger/src/block/block_compact.c
+++ src/third_party/wiredtiger/src/block/block_compact.c
@@ -442,10 +442,12 @@ __wt_block_compact_progress(WT_SESSION_IMPL *session, WT_BLOCK *block)
               100);
             __wt_verbose_debug1(session, WT_VERB_COMPACT_PROGRESS,
               "compacting %s for %" PRIu64 " seconds; reviewed %" PRIu64
-              " pages, rewritten %" PRIu64 " pages (%" PRIu64 "MB), approx. %d%% done",
+              " pages, rewritten %" PRIu64 " pages (%" PRIu64
+              "MB). Approx. %d%% of the estimated work done.%s",
               block->name, time_diff_start, block->compact_pages_reviewed,
               block->compact_pages_rewritten, block->compact_bytes_rewritten / WT_MEGABYTE,
-              progress);
+              progress,
+              progress == 100 ? " More work has been discovered since the estimation." : "");
         }
     }
 }
