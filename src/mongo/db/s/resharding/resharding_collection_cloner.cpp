a27e01b24160a9c0d918c3b15da8d33738f80450
Nandini Bhartiya <nandini.bhartiya@mongodb.com>
2024-01-31 22:48:57 +0000

    Revert "SERVER-84463: Replace withOneStaleConfigRetry with shardVersionRetry (#18252)"
    This reverts commit 34dccfb046c91c44a1622dfc054e4b1fdaa19d23.
    
    GitOrigin-RevId: daa9e1ee471d28e79980ca993a8f9a87c1d5e86d

--- src/mongo/db/s/resharding/resharding_collection_cloner.cpp
+++ src/mongo/db/s/resharding/resharding_collection_cloner.cpp
@@ -866,18 +866,13 @@ SemiFuture<void> ReshardingCollectionCloner::run(
                    // We can run into StaleConfig errors when cloning collections. To make it
                    // safer during retry, we retry the whole cloning process and rely on the
                    // resume token to be correct.
-                   shardVersionRetry(opCtx.get(),
-                                     Grid::get(opCtx.get())->catalogCache(),
-                                     _sourceNss,
-                                     "resharding collection clone fetching with natural order"_sd,
-                                     [&] {
-                                         _runOnceWithNaturalOrder(
-                                             opCtx.get(),
-                                             MongoProcessInterface::create(opCtx.get()),
-                                             executor,
-                                             cleanupExecutor,
-                                             cancelToken);
-                                     });
+                   resharding::data_copy::withOneStaleConfigRetry(opCtx.get(), [&] {
+                       _runOnceWithNaturalOrder(opCtx.get(),
+                                                MongoProcessInterface::create(opCtx.get()),
+                                                executor,
+                                                cleanupExecutor,
+                                                cancelToken);
+                   });
                    // If we got here, we succeeded and there is no more to come.  Otherwise
                    // _runOnceWithNaturalOrder would uassert.
                    chainCtx->moreToCome = false;
