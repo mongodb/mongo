e63d2fe0e918db83a38a85029dc00e2b430c4721
Luke Chen <luke.chen@mongodb.com>
2024-02-05 13:59:05 +1100

    Import wiredtiger: 54b19935416fae525c4aed3eeebfa0f8015700e6 from branch mongodb-6.0
    ref: 05522bc83f..54b1993541
    for: 6.0.14
    
    WT-12077 Fix zSeries checksum code
    
    GitOrigin-RevId: da62decc2eb2cca9a3d32edd2c674b1d5c37780e

--- src/third_party/wiredtiger/src/checksum/zseries/crc32-s390x.c
+++ src/third_party/wiredtiger/src/checksum/zseries/crc32-s390x.c
@@ -21,6 +21,7 @@
 
 #include "crc32-s390x.h"
 #include "slicing-consts.h"
+#include "wt_internal.h"
 
 #define VX_MIN_LEN 64
 #define VX_ALIGNMENT 16UL
@@ -51,31 +52,31 @@ __wt_crc32c_le(unsigned int crc, const unsigned char *buf, size_t len)
  * aligned to improve fetch operations of VECTOR LOAD MULTIPLE instructions.
  *
  */
-#define DEFINE_CRC32_VX(___fname, ___crc32_vx, ___crc32_sw)                            \
-    unsigned int ___fname(unsigned int crc, const unsigned char *data, size_t datalen) \
-    {                                                                                  \
-        unsigned long prealign, aligned, remaining;                                    \
-                                                                                       \
-        if ((unsigned long)data & VX_ALIGN_MASK) {                                     \
-            prealign = VX_ALIGNMENT - ((unsigned long)data & VX_ALIGN_MASK);           \
-            datalen -= prealign;                                                       \
-            crc = ___crc32_sw(crc, data, prealign);                                    \
-            data = data + prealign;                                                    \
-        }                                                                              \
-                                                                                       \
-        if (datalen < VX_MIN_LEN)                                                      \
-            return ___crc32_sw(crc, data, datalen);                                    \
-                                                                                       \
-        aligned = datalen & ~VX_ALIGN_MASK;                                            \
-        remaining = datalen & VX_ALIGN_MASK;                                           \
-                                                                                       \
-        crc = ___crc32_vx(crc, data, aligned);                                         \
-        data = data + aligned;                                                         \
-                                                                                       \
-        if (remaining)                                                                 \
-            crc = ___crc32_sw(crc, data, remaining);                                   \
-                                                                                       \
-        return crc;                                                                    \
+#define DEFINE_CRC32_VX(___fname, ___crc32_vx, ___crc32_sw)                                   \
+    unsigned int ___fname(unsigned int crc, const unsigned char *data, size_t datalen)        \
+    {                                                                                         \
+        unsigned long prealign, aligned, remaining;                                           \
+                                                                                              \
+        if ((unsigned long)data & VX_ALIGN_MASK) {                                            \
+            prealign = WT_MIN(datalen, VX_ALIGNMENT - ((unsigned long)data & VX_ALIGN_MASK)); \
+            datalen -= prealign;                                                              \
+            crc = ___crc32_sw(crc, data, prealign);                                           \
+            data = data + prealign;                                                           \
+        }                                                                                     \
+                                                                                              \
+        if (datalen < VX_MIN_LEN)                                                             \
+            return ___crc32_sw(crc, data, datalen);                                           \
+                                                                                              \
+        aligned = datalen & ~VX_ALIGN_MASK;                                                   \
+        remaining = datalen & VX_ALIGN_MASK;                                                  \
+                                                                                              \
+        crc = ___crc32_vx(crc, data, aligned);                                                \
+        data = data + aligned;                                                                \
+                                                                                              \
+        if (remaining)                                                                        \
+            crc = ___crc32_sw(crc, data, remaining);                                          \
+                                                                                              \
+        return crc;                                                                           \
     }
 
 /* Main CRC-32 functions */
@@ -90,14 +91,6 @@ __wt_checksum_hw(const void *chunk, size_t len)
 {
     return (~__wt_crc32c_le_vx(0xffffffff, chunk, len));
 }
-#endif
-
-extern uint32_t __wt_checksum_sw(const void *chunk, size_t len);
-#if defined(__GNUC__)
-extern uint32_t (*wiredtiger_crc32c_func(void))(const void *, size_t)
-  __attribute__((visibility("default")));
-#else
-extern uint32_t (*wiredtiger_crc32c_func(void))(const void *, size_t);
 #endif
 
 /*
--- src/third_party/wiredtiger/test/csuite/wt2695_checksum/main.c
+++ src/third_party/wiredtiger/test/csuite/wt2695_checksum/main.c
@@ -56,7 +56,8 @@ main(int argc, char *argv[])
     size_t len;
     uint32_t hw, sw;
     uint8_t *data;
-    u_int i, j;
+    uint8_t data_ff[32];
+    u_int i, j, length, misalignment;
 
     opts = &_opts;
     memset(opts, 0, sizeof(*opts));
@@ -69,6 +70,7 @@ main(int argc, char *argv[])
 
     /* Allocate aligned memory for the data. */
     data = dcalloc(DATASIZE, sizeof(uint8_t));
+    memset(data_ff, 0xff, sizeof(data_ff));
 
     /*
      * Some simple known checksums.
@@ -130,7 +132,7 @@ main(int argc, char *argv[])
         check(hw, sw, len, "random power-of-two");
 
         len *= 2;
-        if (len > DATASIZE)
+        if (len > DATASIZE || len == 0)
             len = 512;
     }
 
@@ -138,12 +140,25 @@ main(int argc, char *argv[])
      * Checksums of random data chunks.
      */
     for (i = 0; i < 1000; ++i) {
-        len = __wt_random(&rnd) % DATASIZE;
+        do {
+            len = __wt_random(&rnd) % DATASIZE;
+        } while (len == 0);
         for (j = 0; j < len; ++j)
             data[j] = __wt_random(&rnd) & 0xff;
         hw = __wt_checksum(data, len);
         sw = __wt_checksum_sw(data, len);
         check(hw, sw, len, "random");
+    }
+
+    /*
+     * "Strobed" misalignments - test every combo of size/misalignment up to 16B.
+     */
+    for (length = 0; length < 16; length++) {
+        for (misalignment = 0; misalignment < 16; misalignment++) {
+            hw = __wt_checksum(&data_ff[misalignment], length);
+            sw = __wt_checksum_sw(&data_ff[misalignment], length);
+            check(hw, sw, length, "0xff: strobed");
+        }
     }
 
     free(data);
