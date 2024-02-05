15c819e34100e686e7ea5e97164646cce2571e0d
Etienne Petrel <etienne.petrel@mongodb.com>
2024-02-05 21:36:11 +0000

    Import wiredtiger: df11613b8d56667e7ff7e4497250de51ff0a3286 from branch mongodb-master
    ref: 344982bb49..df11613b8d
    for: 8.0.0-rc0
    
    WT-12382 Fix __pack_encode_WT_ITEM with zero size item
    
    GitOrigin-RevId: 6a0f57a12cadb220dbae98fcdfce09e13f00c3f3

--- src/third_party/wiredtiger/dist/log.py
+++ src/third_party/wiredtiger/dist/log.py
@@ -253,9 +253,11 @@ static inline int
 __pack_encode_WT_ITEM(uint8_t **pp, uint8_t *end, WT_ITEM *item)
 {
     WT_RET(__wt_vpack_uint(pp, WT_PTRDIFF(end, *pp), item->size));
-    WT_SIZE_CHECK_PACK(item->size, WT_PTRDIFF(end, *pp));
-    memcpy(*pp, item->data, item->size);
-    *pp += item->size;
+    if (item->size != 0) {
+        WT_SIZE_CHECK_PACK(item->size, WT_PTRDIFF(end, *pp));
+        memcpy(*pp, item->data, item->size);
+        *pp += item->size;
+    }
     return (0);
 }
 
@@ -266,9 +268,11 @@ __pack_encode_WT_ITEM(uint8_t **pp, uint8_t *end, WT_ITEM *item)
 static inline int
 __pack_encode_WT_ITEM_last(uint8_t **pp, uint8_t *end, WT_ITEM *item)
 {
-    WT_SIZE_CHECK_PACK(item->size, WT_PTRDIFF(end, *pp));
-    memcpy(*pp, item->data, item->size);
-    *pp += item->size;
+    if (item->size != 0) {
+        WT_SIZE_CHECK_PACK(item->size, WT_PTRDIFF(end, *pp));
+        memcpy(*pp, item->data, item->size);
+        *pp += item->size;
+    }
     return (0);
 }
 
