14d20f6bd5828678e7b2f6db0a85c192c9991793
Lynne Wang <lynne.wang@mongodb.com>
2024-02-07 17:13:45 -0500

    Revert "SERVER-85206 Improve performance of full_range.js and explicit_range.js"
    This reverts commit 7aa19f3911c8668899c637346731eb7cbf2cd22a.
    
    GitOrigin-RevId: cf4136890e908277be10fbac1dc632b93db7724f

--- jstests/aggregation/sources/densify/explicit_range.js
+++ jstests/aggregation/sources/densify/explicit_range.js
@@ -6,92 +6,76 @@
  * ]
  */
 
-import {
-    densifyUnits,
-    getArithmeticFunctionsForUnit,
-    insertDocumentsOnPredicate,
-    insertDocumentsOnStep,
-    interestingSteps,
-    testDensifyStage,
-} from "jstests/aggregation/sources/densify/libs/densify_in_js.js";
+load("jstests/aggregation/sources/densify/libs/densify_in_js.js");
 
+(function() {
+"use strict";
 const collName = jsTestName();
 const coll = db.getCollection(collName);
 coll.drop();
 
 // Run all tests for each date unit and on numeric values.
 for (let i = 0; i < densifyUnits.length; i++) {
-    coll.drop();
-
     const unit = densifyUnits[i];
+    coll.drop();
     const base = unit ? new ISODate("2021-01-01") : 0;
     const {add} = getArithmeticFunctionsForUnit(unit);
 
-    const getBounds = (lower, upper) => {
-        return [add(base, lower), add(base, upper)];
-    };
+    const runDensifyRangeTest = ({step, bounds}, msg) => testDensifyStage({
+        field: "val",
+        range: {step, bounds: [add(base, bounds[0]), add(base, bounds[1])], unit: unit}
+    },
+                                                                          coll,
+                                                                          msg);
 
     // Run all tests for different step values.
     for (let i = 0; i < interestingSteps.length; i++) {
         const step = interestingSteps[i];
-
         // Generate documents in an empty collection.
-        let stage = {field: "val", range: {step: step, bounds: getBounds(0, 10), unit: unit}};
-        testDensifyStage(stage, coll);
+        runDensifyRangeTest({step, bounds: [0, 10]});
 
         // Fill in some documents between existing docs.
         coll.drop();
         coll.insert({val: base});
-        coll.insert({val: add(base, 30)});
-        // Checking that the upper bound is exclusive.
-        stage = {field: "val", range: {step: step, bounds: getBounds(10, 25), unit: unit}};
-        testDensifyStage(stage, coll);
+        coll.insert({val: add(base, 99)});
+        runDensifyRangeTest(
+            {step, bounds: [10, 25]});  // Checking that the upper bound is exclusive.
 
         // Fill in odd documents.
         coll.drop();
-        insertDocumentsOnStep({base, min: 2, max: 11, step: 2, addFunc: add, coll: coll});
-
-        stage = {field: "val", range: {step: step, bounds: getBounds(1, 12), unit: unit}};
-        testDensifyStage(stage, coll);
-        stage = {field: "val", range: {step: step, bounds: getBounds(1, 11), unit: unit}};
-        testDensifyStage(stage, coll);
-        stage = {field: "val", range: {step: step, bounds: getBounds(1, 10), unit: unit}};
-        testDensifyStage(stage, coll);
+        insertDocumentsOnStep({base, min: 2, max: 21, step: 2, addFunc: add, coll: coll});
+        runDensifyRangeTest({step, bounds: [1, 22]});
+        runDensifyRangeTest({step, bounds: [1, 21]});
+        runDensifyRangeTest({step, bounds: [1, 20]});
 
         // Negative numbers.
         coll.drop();
-        insertDocumentsOnStep({base, min: -20, max: -1, step: 2, addFunc: add, coll: coll});
-        stage = {field: "val", range: {step: step, bounds: getBounds(-10, -1), unit: unit}};
-        testDensifyStage(stage, coll);
-        stage = {field: "val", range: {step: step, bounds: getBounds(-10, 0), unit: unit}};
-        testDensifyStage(stage, coll);
-        stage = {field: "val", range: {step: step, bounds: getBounds(-10, -2), unit: unit}};
-        testDensifyStage(stage, coll);
+        insertDocumentsOnStep({base, min: -100, max: -1, step: 2, addFunc: add, coll: coll});
+        runDensifyRangeTest({step, bounds: [-40, -5]});
+        runDensifyRangeTest({step, bounds: [-60, 0]});
+        runDensifyRangeTest({step, bounds: [-40, -6]});
 
         // Extend range past collection.
         coll.drop();
-        insertDocumentsOnStep({base, min: 0, max: 10, step: 3, addFunc: add, coll: coll});
-        stage = {field: "val", range: {step: step, bounds: getBounds(5, 15), unit: unit}};
-        testDensifyStage(stage, coll);
+        insertDocumentsOnStep({base, min: 0, max: 50, step: 3, addFunc: add, coll: coll});
+        runDensifyRangeTest({step, bounds: [30, 75]});
 
         // Start range before collection.
         coll.drop();
-        insertDocumentsOnStep({base, min: 20, max: 30, step: 2, addFunc: add, coll: coll});
-        stage = {field: "val", range: {step: step, bounds: getBounds(10, 25), unit: unit}};
-        testDensifyStage(stage, coll);
+        insertDocumentsOnStep({base, min: 20, max: 40, step: 2, addFunc: add, coll: coll});
+        runDensifyRangeTest({step, bounds: [10, 25]});
 
         // Extend range in both directions past collection bounds.
         coll.drop();
-        insertDocumentsOnStep({base, min: 20, max: 30, step: 2, addFunc: add, coll: coll});
-        stage = {field: "val", range: {step: step, bounds: getBounds(10, 35), unit: unit}};
-        testDensifyStage(stage, coll);
+        insertDocumentsOnStep({base, min: 20, max: 40, step: 2, addFunc: add, coll: coll});
+        runDensifyRangeTest({step, bounds: [10, 45]});
 
         // Different off-step documents.
         coll.drop();
         insertDocumentsOnPredicate(
-            {base, min: 0, max: 25, pred: i => i % 3 == 0 || i % 7 == 0, addFunc: add, coll: coll});
-        stage = {field: "val", range: {step: step, bounds: getBounds(10, 20), unit: unit}};
-        testDensifyStage(stage, coll);
+            {base, min: 0, max: 50, pred: i => i % 3 == 0 || i % 7 == 0, addFunc: add, coll: coll});
+
+        runDensifyRangeTest({step, bounds: [10, 45]});
 
         // Lots of off-step documents with nulls sprinkled in to confirm that a null value is
         // treated the same as a missing value.
@@ -112,13 +96,12 @@ for (let i = 0; i < densifyUnits.length; i++) {
         insertDocumentsOnPredicate({
             base,
             min: 20,
-            max: 30,
+            max: 50,
             pred: i => i % 3 == 0 || i % 7 == 0,
             addFunc: add,
             coll: coll
         });
-        stage = {field: "val", range: {step: step, bounds: getBounds(10, 25), unit: unit}};
-        testDensifyStage(stage, coll);
+        runDensifyRangeTest({step, bounds: [10, 45]});
     }
 }
 
@@ -141,3 +124,4 @@ let stage = {
     },
 };
 testDensifyStage(stage, coll, "Ensure no docs before range");
+})();
