51d6bb2e5aff745bbc53e43021aece92d58f2ab0
Gil Alon <47804748+galon1@users.noreply.github.com>
2024-02-07 20:41:10 -0500

    SERVER-85176 Run BSONColumn benchmarks on the block based API using the iterator implementation (#18571)
    GitOrigin-RevId: 93070dfa9c71a5d4b19df6968c0de3ad98e6e4ae

--- src/mongo/bson/util/bsoncolumn_bm.cpp
+++ src/mongo/bson/util/bsoncolumn_bm.cpp
@@ -45,11 +45,46 @@
 #include "mongo/bson/timestamp.h"
 #include "mongo/bson/util/bsoncolumn.h"
 #include "mongo/bson/util/bsoncolumnbuilder.h"
+#include "mongo/db/exec/sbe/values/bsoncolumn_materializer.h"
 #include "mongo/util/base64.h"
 #include "mongo/util/time_support.h"
 
 namespace mongo {
 namespace {
+
+enum DecompressMode { kIterator, kBlockBSON, kBlockSBE };
+
+// Simple class that defines a 'Container' for the block-based decompression API. The insert
+// function is a no-op. The block-based API pushes the materialized elements into a container, and
+// thus does more work than the iterator API. This container allows us to fairly compare the
+// runtimes of both implementations, since inserts are no-ops.
+
+template <typename T>
+class NoOpContainerForTest {
+public:
+    NoOpContainerForTest() {}
+    ~NoOpContainerForTest() {}
+
+    // Increment the counter to return the number of elements processed.
+    void insert(int pos, const T& element) {
+        _size++;
+        return;
+    }
+
+    int size() {
+        return _size;
+    }
+
+    // Called by the Collector class. Means nothing in this case, since we do not insert elements
+    // into this container, but we must return something.
+    int end() {
+        return 0;
+    }
+
+private:
+    int _size = 0;
+};
+
 std::mt19937_64 seedGen(1337);
 
 std::vector<BSONObj> generateIntegers(int num, int skipPercentage) {
@@ -174,28 +209,85 @@ void benchmarkDecompression(benchmark::State& state,
                             int skipSize) {
     uint64_t totalElements = 0;
     uint64_t totalBytes = 0;
+
+    // Begin benchmarking loop.
     for (auto _ : state) {
+        benchmark::ClobberMemory();
         BSONColumn col(compressedElement);
-        for (auto&& decompressed : col) {
-            totalBytes += decompressed.size();
+        auto it = col.begin();
+        while (it.more()) {
+            // decompress elements using iterator implementation.
+            benchmark::DoNotOptimize(++it);
             ++totalElements;
         }
+        totalBytes += compressedElement.size();
     }
+
     state.SetItemsProcessed(totalElements);
     state.SetBytesProcessed(totalBytes);
+}
 
-    uint64_t uncompressedSize = 0;
-    BSONColumn col(compressedElement);
-    for (auto&& decompressed : col) {
-        if (decompressed.eoo())
-            uncompressedSize += skipSize;
-        else {
-            uncompressedSize += decompressed.valuesize();
-        }
+void benchmarkBlockBasedDecompression(benchmark::State& state,
+                                      const BSONElement& compressedElement,
+                                      int skipSize) {
+
+    int size = 0;
+    const char* binary = compressedElement.binData(size);
+    BSONBinData bin(binary, size, Column);
+
+    uint64_t totalElements = 0;
+    uint64_t totalBytes = 0;
+
+    auto decompress = [&](NoOpContainerForTest<BSONElement>& container) {
+        bsoncolumn::BSONColumnBlockBased col(bin);
+        boost::intrusive_ptr<ElementStorage> allocator = new ElementStorage();
+        col.decompressIterative<bsoncolumn::BSONElementMaterializer>(container, allocator);
+        return true;
+    };
+
+    // Begin benchmarking loop.
+    for (auto _ : state) {
+        benchmark::ClobberMemory();
+        NoOpContainerForTest<BSONElement> container;
+        benchmark::DoNotOptimize(decompress(container));
+        totalBytes += compressedElement.size();
+        totalElements += container.size();
     }
-    state.SetLabel(
-        fmt::format("compress:{:.1f}%",
-                    100.0 * (1 - ((double)compressedElement.valuesize() / uncompressedSize))));
+
+    state.SetItemsProcessed(totalElements);
+    state.SetBytesProcessed(totalBytes);
+}
+
+void benchmarkBlockBasedDecompression_SBE(benchmark::State& state,
+                                          const BSONElement& compressedElement,
+                                          int skipSize) {
+
+    using SBEMaterializer = sbe::bsoncolumn::SBEColumnMaterializer;
+    int size = 0;
+    const char* binary = compressedElement.binData(size);
+    BSONBinData bin(binary, size, Column);
+
+    uint64_t totalElements = 0;
+    uint64_t totalBytes = 0;
+
+    auto decompress = [&](NoOpContainerForTest<SBEMaterializer::Element>& container) {
+        bsoncolumn::BSONColumnBlockBased col(bin);
+        boost::intrusive_ptr<ElementStorage> allocator = new ElementStorage();
+        col.decompressIterative<SBEMaterializer>(container, allocator);
+        return true;
+    };
+
+    // Begin benchmarking loop.
+    for (auto _ : state) {
+        benchmark::ClobberMemory();
+        NoOpContainerForTest<SBEMaterializer::Element> container;
+        benchmark::DoNotOptimize(decompress(container));
+        totalBytes = compressedElement.size();
+        totalElements += container.size();
+    }
+
+    state.SetItemsProcessed(totalElements);
+    state.SetBytesProcessed(totalBytes);
 }
 
 void benchmarkCompression(benchmark::State& state,
@@ -268,35 +360,90 @@ void benchmarkReopenNaive(benchmark::State& state,
     }
 }
 
-}  // namespace
-
-void BM_decompressIntegers(benchmark::State& state, int skipPercentage) {
+void BM_decompressIntegers(benchmark::State& state, int skipPercentage, DecompressMode mode) {
     BSONObj compressed = buildCompressed(generateIntegers(10000, skipPercentage));
-    benchmarkDecompression(state, compressed.firstElement(), sizeof(int32_t));
+    switch (mode) {
+        case kBlockBSON:
+            benchmarkBlockBasedDecompression(state, compressed.firstElement(), sizeof(int32_t));
+            break;
+        case kBlockSBE:
+            benchmarkBlockBasedDecompression_SBE(state, compressed.firstElement(), sizeof(int32_t));
+            break;
+        case kIterator:
+        default:
+            benchmarkDecompression(state, compressed.firstElement(), sizeof(int32_t));
+            break;
+    }
 }
 
-void BM_decompressDoubles(benchmark::State& state, int decimals, int skipPercentage) {
+void BM_decompressDoubles(benchmark::State& state,
+                          int decimals,
+                          int skipPercentage,
+                          DecompressMode mode) {
     auto doubles = generateDoubles(10000, skipPercentage, decimals);
     BSONObj compressed = buildCompressed(doubles);
-    benchmarkDecompression(state, compressed.firstElement(), sizeof(double));
+    switch (mode) {
+        case kBlockBSON:
+            benchmarkBlockBasedDecompression(state, compressed.firstElement(), sizeof(double));
+            break;
+        case kBlockSBE:
+            benchmarkBlockBasedDecompression_SBE(state, compressed.firstElement(), sizeof(double));
+            break;
+        case kIterator:
+        default:
+            benchmarkDecompression(state, compressed.firstElement(), sizeof(double));
+            break;
+    }
 }
 
-void BM_decompressTimestamps(benchmark::State& state,
-                             double mean,
-                             double stddev,
-                             int skipPercentage) {
+void BM_decompressTimestamps(
+    benchmark::State& state, double mean, double stddev, int skipPercentage, DecompressMode mode) {
     BSONObj compressed = buildCompressed(generateTimestamps(10000, skipPercentage, mean, stddev));
-    benchmarkDecompression(state, compressed.firstElement(), sizeof(Timestamp));
+    switch (mode) {
+        case kBlockBSON:
+            benchmarkBlockBasedDecompression(state, compressed.firstElement(), sizeof(Timestamp));
+            break;
+        case kBlockSBE:
+            benchmarkBlockBasedDecompression_SBE(
+                state, compressed.firstElement(), sizeof(Timestamp));
+            break;
+        case kIterator:
+        default:
+            benchmarkDecompression(state, compressed.firstElement(), sizeof(Timestamp));
+            break;
+    }
 }
 
-void BM_decompressObjectIds(benchmark::State& state, int skipPercentage) {
+void BM_decompressObjectIds(benchmark::State& state, int skipPercentage, DecompressMode mode) {
     BSONObj compressed = buildCompressed(generateObjectIds(10000, skipPercentage));
-    benchmarkDecompression(state, compressed.firstElement(), sizeof(OID));
+    switch (mode) {
+        case kBlockBSON:
+            benchmarkBlockBasedDecompression(state, compressed.firstElement(), sizeof(OID));
+            break;
+        case kBlockSBE:
+            benchmarkBlockBasedDecompression_SBE(state, compressed.firstElement(), sizeof(OID));
+            break;
+        case kIterator:
+        default:
+            benchmarkDecompression(state, compressed.firstElement(), sizeof(OID));
+            break;
+    }
 }
 
-void BM_decompressFTDC(benchmark::State& state) {
+void BM_decompressFTDC(benchmark::State& state, DecompressMode mode) {
     BSONObj compressed = getCompressedFTDC();
-    benchmarkDecompression(state, compressed.firstElement(), 0);
+    switch (mode) {
+        case kBlockBSON:
+            benchmarkBlockBasedDecompression(state, compressed.firstElement(), 0);
+            break;
+        case kBlockSBE:
+            benchmarkBlockBasedDecompression_SBE(state, compressed.firstElement(), 0);
+            break;
+        case kIterator:
+        default:
+            benchmarkDecompression(state, compressed.firstElement(), 0);
+            break;
+    }
 }
 
 void BM_compressIntegers(benchmark::State& state, int skipPercentage) {
@@ -337,43 +484,138 @@ void BM_compressFTDC(benchmark::State& state) {
     benchmarkCompression(state, compressed.firstElement(), 0);
 }
 
+// Block-based API benchmarks using the BSONElementMaterializer. We'll run a subset of the
+// benchmarks on the new API.
+BENCHMARK_CAPTURE(BM_decompressIntegers, Block API BSON Skip = 0 %, 0, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressIntegers, Block API BSON Skip = 50 %, 50, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressIntegers, Block API BSON Skip = 99 %, 99, kBlockBSON);
 
-BENCHMARK_CAPTURE(BM_decompressIntegers, Skip = 0 %, 0);
-BENCHMARK_CAPTURE(BM_decompressIntegers, Skip = 10 %, 10);
-BENCHMARK_CAPTURE(BM_decompressIntegers, Skip = 50 %, 50);
-BENCHMARK_CAPTURE(BM_decompressIntegers, Skip = 90 %, 90);
-BENCHMARK_CAPTURE(BM_decompressIntegers, Skip = 99 %, 99);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API BSON Decimals = 0 / Skip = 0 %, 0, 0, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API BSON Decimals = 1 / Skip = 0 %, 1, 0, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API BSON Decimals = 4 / Skip = 0 %, 4, 0, kBlockBSON);
 
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 0 / Skip = 0 %, 0, 0);
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 1 / Skip = 0 %, 1, 0);
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 2 / Skip = 0 %, 2, 0);
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 4 / Skip = 0 %, 4, 0);
+BENCHMARK_CAPTURE(BM_decompressDoubles,
+                  Block API BSON Decimals = 0 / Skip = 10 %
+                  , 0, 10, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressDoubles,
+                  Block API BSON Decimals = 1 / Skip = 10 %
+                  , 1, 10, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressDoubles,
+                  Block API BSON Decimals = 4 / Skip = 10 %
+                  , 4, 10, kBlockBSON);
 
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 0 / Skip = 10 %, 0, 10);
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 1 / Skip = 10 %, 1, 10);
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 2 / Skip = 10 %, 2, 10);
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 4 / Skip = 10 %, 4, 10);
+BENCHMARK_CAPTURE(BM_decompressDoubles,
+                  Block API BSON Decimals = 0 / Skip = 90 %
+                  , 0, 90, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressDoubles,
+                  Block API BSON Decimals = 1 / Skip = 90 %
+                  , 1, 90, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressDoubles,
+                  Block API BSON Decimals = 4 / Skip = 90 %
+                  , 4, 90, kBlockBSON);
 
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 0 / Skip = 90 %, 0, 90);
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 1 / Skip = 90 %, 1, 90);
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 2 / Skip = 90 %, 2, 90);
-BENCHMARK_CAPTURE(BM_decompressDoubles, Decimals = 4 / Skip = 90 %, 4, 90);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Block API BSON Mean = 1 / Stddev = 0 / Skip = 0 %
+                  , 0, 1, 0, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Block API BSON Mean = 5 / Stddev = 2 / Skip = 0 %
+                  , 0, 1, 0, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Block API BSON Mean = 1 / Stddev = 0 / Skip = 90 %
+                  , 0, 1, 90, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Block API BSON Mean = 5 / Stddev = 2 / Skip = 90 %
+                  , 0, 1, 90, kBlockBSON);
 
-BENCHMARK_CAPTURE(BM_decompressTimestamps, Mean = 1 / Stddev = 0 / Skip = 0 %, 0, 1, 0);
-BENCHMARK_CAPTURE(BM_decompressTimestamps, Mean = 5 / Stddev = 2 / Skip = 0 %, 0, 1, 0);
-BENCHMARK_CAPTURE(BM_decompressTimestamps, Mean = 1 / Stddev = 0 / Skip = 10 %, 0, 1, 10);
-BENCHMARK_CAPTURE(BM_decompressTimestamps, Mean = 5 / Stddev = 2 / Skip = 10 %, 0, 1, 10);
-BENCHMARK_CAPTURE(BM_decompressTimestamps, Mean = 1 / Stddev = 0 / Skip = 90 %, 0, 1, 90);
-BENCHMARK_CAPTURE(BM_decompressTimestamps, Mean = 5 / Stddev = 2 / Skip = 90 %, 0, 1, 90);
+BENCHMARK_CAPTURE(BM_decompressObjectIds, Block API BSON Skip = 0 %, 0, kBlockBSON);
+BENCHMARK_CAPTURE(BM_decompressObjectIds, Block API BSON Skip = 90 %, 90, kBlockBSON);
 
-BENCHMARK_CAPTURE(BM_decompressObjectIds, Skip = 0 %, 0);
-BENCHMARK_CAPTURE(BM_decompressObjectIds, Skip = 10 %, 10);
-BENCHMARK_CAPTURE(BM_decompressObjectIds, Skip = 90 %, 90);
+// Block-based API benchmarks using the SBEMaterializer. We'll run a subset of the benchmarks on the
+// new API.
+BENCHMARK_CAPTURE(BM_decompressIntegers, Block API SBE Skip = 0 %, 0, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressIntegers, Block API SBE Skip = 50 %, 50, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressIntegers, Block API SBE Skip = 99 %, 99, kBlockSBE);
+
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API SBE Decimals = 0 / Skip = 0 %, 0, 0, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API SBE Decimals = 1 / Skip = 0 %, 1, 0, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API SBE Decimals = 4 / Skip = 0 %, 4, 0, kBlockSBE);
+
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API SBE Decimals = 0 / Skip = 10 %, 0, 10, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API SBE Decimals = 1 / Skip = 10 %, 1, 10, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API SBE Decimals = 4 / Skip = 10 %, 4, 10, kBlockSBE);
+
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API SBE Decimals = 0 / Skip = 90 %, 0, 90, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API SBE Decimals = 1 / Skip = 90 %, 1, 90, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Block API SBE Decimals = 4 / Skip = 90 %, 4, 90, kBlockSBE);
+
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Block API SBE Mean = 1 / Stddev = 0 / Skip = 0 %
+                  , 0, 1, 0, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Block API SBE Mean = 5 / Stddev = 2 / Skip = 0 %
+                  , 0, 1, 0, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Block API SBE Mean = 1 / Stddev = 0 / Skip = 90 %
+                  , 0, 1, 90, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Block API SBE Mean = 5 / Stddev = 2 / Skip = 90 %
+                  , 0, 1, 90, kBlockSBE);
+
+BENCHMARK_CAPTURE(BM_decompressObjectIds, Block API SBE Skip = 0 %, 0, kBlockSBE);
+BENCHMARK_CAPTURE(BM_decompressObjectIds, Block API SBE Skip = 90 %, 90, kBlockSBE);
+
+// Iterator implementation benchmarks.
+BENCHMARK_CAPTURE(BM_decompressIntegers, Iterator API Skip = 0 %, 0, kIterator);
+BENCHMARK_CAPTURE(BM_decompressIntegers, Iterator API Skip = 10 %, 10, kIterator);
+BENCHMARK_CAPTURE(BM_decompressIntegers, Iterator API Skip = 50 %, 50, kIterator);
+BENCHMARK_CAPTURE(BM_decompressIntegers, Iterator API Skip = 90 %, 90, kIterator);
+BENCHMARK_CAPTURE(BM_decompressIntegers, Iterator API Skip = 99 %, 99, kIterator);
+
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 0 / Skip = 0 %, 0, 0, kIterator);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 1 / Skip = 0 %, 1, 0, kIterator);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 2 / Skip = 0 %, 2, 0, kIterator);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 4 / Skip = 0 %, 4, 0, kIterator);
+
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 0 / Skip = 10 %, 0, 10, kIterator);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 1 / Skip = 10 %, 1, 10, kIterator);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 2 / Skip = 10 %, 2, 10, kIterator);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 4 / Skip = 10 %, 4, 10, kIterator);
+
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 0 / Skip = 90 %, 0, 90, kIterator);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 1 / Skip = 90 %, 1, 90, kIterator);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 2 / Skip = 90 %, 2, 90, kIterator);
+BENCHMARK_CAPTURE(BM_decompressDoubles, Iterator API Decimals = 4 / Skip = 90 %, 4, 90, kIterator);
+
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Iterator API Mean = 1 / Stddev = 0 / Skip = 0 %
+                  , 0, 1, 0, kIterator);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Iterator API Mean = 5 / Stddev = 2 / Skip = 0 %
+                  , 0, 1, 0, kIterator);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Iterator API Mean = 1 / Stddev = 0 / Skip = 10 %
+                  , 0, 1, 10, kIterator);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Iterator API Mean = 5 / Stddev = 2 / Skip = 10 %
+                  , 0, 1, 10, kIterator);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Iterator API Mean = 1 / Stddev = 0 / Skip = 90 %
+                  , 0, 1, 90, kIterator);
+BENCHMARK_CAPTURE(BM_decompressTimestamps,
+                  Iterator API Mean = 5 / Stddev = 2 / Skip = 90 %
+                  , 0, 1, 90, kIterator);
+
+BENCHMARK_CAPTURE(BM_decompressObjectIds, Iterator API Skip = 0 %, 0, kIterator);
+BENCHMARK_CAPTURE(BM_decompressObjectIds, Iterator API Skip = 10 %, 10, kIterator);
+BENCHMARK_CAPTURE(BM_decompressObjectIds, Iterator API Skip = 90 %, 90, kIterator);
 
 // The large literal emits this on Visual Studio: Fatal error C1091: compiler limit: string exceeds
 // 65535 bytes in length
 #if !defined(_MSC_VER)
-BENCHMARK(BM_decompressFTDC);
+// TODO SERVER-84389 enable these benchmarks.
+// BENCHMARK_CAPTURE(BM_decompressFTDC, Iterator API, kIterator);
+// BENCHMARK_CAPTURE(BM_decompressFTDC, Block API BSON, kBlockBSON);
+// BENCHMARK_CAPTURE(BM_decompressFTDC, Block API SBE, kBlockSBE);
 #endif
 
 BENCHMARK_CAPTURE(BM_compressIntegers, Skip = 0 %, 0);
@@ -443,7 +685,9 @@ BENCHMARK_CAPTURE(BM_reopenNaiveIntegers, Skip = 99 % / Num = 10000, 99, 10000);
 // The large literal emits this on Visual Studio: Fatal error C1091: compiler limit: string exceeds
 // 65535 bytes in length
 #if !defined(_MSC_VER)
-BENCHMARK(BM_compressFTDC);
+// TODO SERVER-84389 enable these benchmarks.
+// BENCHMARK(BM_compressFTDC);
 #endif
 
+}  // namespace
 }  // namespace mongo
