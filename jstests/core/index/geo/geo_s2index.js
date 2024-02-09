0e77af784369749a9985b98182c678857a48c906
Ian Boros <87138302+borosaurus@users.noreply.github.com>
2024-02-09 14:53:39 -0500

    SERVER-85332 modernize geo_s2index.js test (#18116)
    GitOrigin-RevId: 639d61a1b06aa5aaeaf6da95ebe69cd4374fe0b1

--- jstests/core/index/geo/geo_s2index.js
+++ jstests/core/index/geo/geo_s2index.js
@@ -1,70 +1,115 @@
-let t = db.geo_s2index;
+const t = db.geo_s2index;
 t.drop();
 
+// Helper used to print debug info if any assertion fails.
+function dumpCollection() {
+    return "Collection contents: " + tojson(t.find().toArray());
+}
+
 // We internally drop adjacent duplicate points in lines.
-let someline = {"type": "LineString", "coordinates": [[40, 5], [40, 5], [40, 5], [41, 6], [41, 6]]};
-t.insert({geo: someline, nonGeo: "someline"});
-t.createIndex({geo: "2dsphere"});
-let foo =
-    t.find({geo: {$geoIntersects: {$geometry: {type: "Point", coordinates: [40, 5]}}}}).next();
-assert.eq(foo.geo, someline);
+const someline = {
+    "type": "LineString",
+    "coordinates": [[40, 5], [40, 5], [40, 5], [41, 6], [41, 6]]
+};
+assert.commandWorked(t.insert({geo: someline, nonGeo: "someline"}));
+assert.commandWorked(t.createIndex({geo: "2dsphere"}));
+const results =
+    t.find({geo: {$geoIntersects: {$geometry: {type: "Point", coordinates: [40, 5]}}}}).toArray();
+assert.eq(results.length, 1, dumpCollection);
+assert.eq(results[0].geo, someline, dumpCollection);
 t.dropIndex({geo: "2dsphere"});
 
-let pointA = {"type": "Point", "coordinates": [40, 5]};
+const pointA = {
+    "type": "Point",
+    "coordinates": [40, 5]
+};
 t.insert({geo: pointA, nonGeo: "pointA"});
 
-let pointD = {"type": "Point", "coordinates": [41.001, 6.001]};
+const pointD = {
+    "type": "Point",
+    "coordinates": [41.001, 6.001]
+};
 t.insert({geo: pointD, nonGeo: "pointD"});
 
-let pointB = {"type": "Point", "coordinates": [41, 6]};
+const pointB = {
+    "type": "Point",
+    "coordinates": [41, 6]
+};
 t.insert({geo: pointB, nonGeo: "pointB"});
 
-let pointC = {"type": "Point", "coordinates": [41, 6]};
+const pointC = {
+    "type": "Point",
+    "coordinates": [41, 6]
+};
 t.insert({geo: pointC});
 
 // Add a point within the polygon but not on the border.  Don't want to be on
 // the path of the polyline.
-let pointE = {"type": "Point", "coordinates": [40.6, 5.4]};
+const pointE = {
+    "type": "Point",
+    "coordinates": [40.6, 5.4]
+};
 t.insert({geo: pointE});
 
 // Make sure we can index this without error.
 t.insert({nonGeo: "noGeoField!"});
 
-let somepoly = {"type": "Polygon", "coordinates": [[[40, 5], [40, 6], [41, 6], [41, 5], [40, 5]]]};
+const somepoly = {
+    "type": "Polygon",
+    "coordinates": [[[40, 5], [40, 6], [41, 6], [41, 5], [40, 5]]]
+};
 t.insert({geo: somepoly, nonGeo: "somepoly"});
 
-var res = t.createIndex({geo: "2dsphere", nonGeo: 1});
-// We have a point without any geo data.  Don't error.
-assert.commandWorked(res);
+{
+    const res = t.createIndex({geo: "2dsphere", nonGeo: 1});
+    // We have a point without any geo data.  Don't error.
+    assert.commandWorked(res);
+}
 
-res = t.find({"geo": {"$geoIntersects": {"$geometry": pointA}}});
-assert.eq(res.itcount(), 3);
+{
+    const res = t.find({"geo": {"$geoIntersects": {"$geometry": pointA}}});
+    assert.eq(res.itcount(), 3, dumpCollection);
+}
 
-res = t.find({"geo": {"$geoIntersects": {"$geometry": pointB}}});
-assert.eq(res.itcount(), 4);
+{
+    const res = t.find({"geo": {"$geoIntersects": {"$geometry": pointB}}});
+    assert.eq(res.itcount(), 4, dumpCollection);
+}
 
-res = t.find({"geo": {"$geoIntersects": {"$geometry": pointD}}});
-assert.eq(res.itcount(), 1);
+{
+    const res = t.find({"geo": {"$geoIntersects": {"$geometry": pointD}}});
+    assert.eq(res.itcount(), 1, dumpCollection);
+}
 
-res = t.find({"geo": {"$geoIntersects": {"$geometry": someline}}});
-assert.eq(res.itcount(), 5);
+{
+    const res = t.find({"geo": {"$geoIntersects": {"$geometry": someline}}});
+    assert.eq(res.itcount(), 5, dumpCollection);
+}
 
-res = t.find({"geo": {"$geoIntersects": {"$geometry": somepoly}}});
-assert.eq(res.itcount(), 6);
+{
+    const res = t.find({"geo": {"$geoIntersects": {"$geometry": somepoly}}});
+    assert.eq(res.itcount(), 6, dumpCollection);
+}
 
-res = t.find({"geo": {"$within": {"$geometry": somepoly}}});
-assert.eq(res.itcount(), 6);
+{
+    const res = t.find({"geo": {"$within": {"$geometry": somepoly}}});
+    assert.eq(res.itcount(), 6, dumpCollection);
+}
 
-res = t.find({"geo": {"$geoIntersects": {"$geometry": somepoly}}}).limit(1);
-assert.eq(res.itcount(), 1);
+{
+    const res = t.find({"geo": {"$geoIntersects": {"$geometry": somepoly}}}).limit(1);
+    assert.eq(res.itcount(), 1, dumpCollection);
+}
 
-res = t.find({"nonGeo": "pointA", "geo": {"$geoIntersects": {"$geometry": somepoly}}});
-assert.eq(res.itcount(), 1);
+{
+    const res = t.find({"nonGeo": "pointA", "geo": {"$geoIntersects": {"$geometry": somepoly}}});
+    assert.eq(res.itcount(), 1, dumpCollection);
+}
 
 // Don't crash mongod if we give it bad input.
 t.drop();
 t.createIndex({loc: "2dsphere", x: 1});
-t.save({loc: [0, 0]});
+assert.commandWorked(t.insert({loc: [0, 0]}));
 assert.throws(function() {
     return t.count({loc: {$foo: [0, 0]}});
 });
@@ -80,79 +125,83 @@ assert.throws(function() {
 // If we specify a datum, it has to be valid (WGS84).
 t.drop();
 t.createIndex({loc: "2dsphere"});
-res = t.insert({
+const failedInsertRes = t.insert({
     loc: {type: 'Point', coordinates: [40, 5], crs: {type: 'name', properties: {name: 'EPSG:2000'}}}
 });
-assert.writeError(res);
-assert.eq(0, t.find().itcount());
-res = t.insert({loc: {type: 'Point', coordinates: [40, 5]}});
-assert.commandWorked(res);
-res = t.insert({
+assert.writeError(failedInsertRes);
+assert.eq(0, t.find().itcount(), dumpCollection);
+
+assert.commandWorked(t.insert({loc: {type: 'Point', coordinates: [40, 5]}}));
+assert.commandWorked(t.insert({
     loc: {type: 'Point', coordinates: [40, 5], crs: {type: 'name', properties: {name: 'EPSG:4326'}}}
-});
-assert.commandWorked(res);
-res = t.insert({
+}));
+assert.commandWorked(t.insert({
     loc: {
         type: 'Point',
         coordinates: [40, 5],
         crs: {type: 'name', properties: {name: 'urn:ogc:def:crs:OGC:1.3:CRS84'}}
     }
-});
-assert.commandWorked(res);
+}));
 
 // We can pass level parameters and we verify that they're valid.
 // 0 <= coarsestIndexedLevel <= finestIndexedLevel <= 30.
 t.drop();
-t.save({loc: [0, 0]});
-res = t.createIndex({loc: "2dsphere"}, {finestIndexedLevel: 17, coarsestIndexedLevel: 5});
-assert.commandWorked(res);
+assert.commandWorked(t.insert({loc: [0, 0]}));
+assert.commandWorked(
+    t.createIndex({loc: "2dsphere"}, {finestIndexedLevel: 17, coarsestIndexedLevel: 5}));
 // Ensure the index actually works at a basic level
-assert.neq(null, t.findOne({loc: {$geoNear: {$geometry: {type: 'Point', coordinates: [0, 0]}}}}));
+assert.neq(null,
+           t.findOne({loc: {$geoNear: {$geometry: {type: 'Point', coordinates: [0, 0]}}}}),
+           dumpCollection);
 
 t.drop();
-t.save({loc: [0, 0]});
-res = t.createIndex({loc: "2dsphere"}, {finestIndexedLevel: 31, coarsestIndexedLevel: 5});
-assert.commandFailed(res);
+assert.commandWorked(t.insert({loc: [0, 0]}));
+assert.commandFailed(
+    t.createIndex({loc: "2dsphere"}, {finestIndexedLevel: 31, coarsestIndexedLevel: 5}));
 
 t.drop();
-t.save({loc: [0, 0]});
-res = t.createIndex({loc: "2dsphere"}, {finestIndexedLevel: 30, coarsestIndexedLevel: 0});
-assert.commandWorked(res);
+assert.commandWorked(t.insert({loc: [0, 0]}));
+assert.commandWorked(
+    t.createIndex({loc: "2dsphere"}, {finestIndexedLevel: 30, coarsestIndexedLevel: 0}));
+
 // Ensure the index actually works at a basic level
-assert.neq(null, t.findOne({loc: {$geoNear: {$geometry: {type: 'Point', coordinates: [0, 0]}}}}));
-let created = t.getIndexes().filter((idx) => idx.hasOwnProperty("2dsphereIndexVersion"))[0];
-assert.eq(created.finestIndexedLevel, 30, created);
-assert.eq(created.coarsestIndexedLevel, 0, created);
+assert.neq(null,
+           t.findOne({loc: {$geoNear: {$geometry: {type: 'Point', coordinates: [0, 0]}}}}),
+           dumpCollection);
+
+{
+    const created = t.getIndexes().filter((idx) => idx.hasOwnProperty("2dsphereIndexVersion"))[0];
+    assert.eq(created.finestIndexedLevel, 30, created, dumpCollection);
+    assert.eq(created.coarsestIndexedLevel, 0, created, dumpCollection);
+}
 
 t.drop();
-t.save({loc: [0, 0]});
-res = t.createIndex({loc: "2dsphere"}, {finestIndexedLevel: 30, coarsestIndexedLevel: -1});
-assert.commandFailed(res);
+assert.commandWorked(t.insert({loc: [0, 0]}));
+assert.commandFailed(
+    t.createIndex({loc: "2dsphere"}, {finestIndexedLevel: 30, coarsestIndexedLevel: -1}));
 
 // SERVER-21491 Verify that 2dsphere index options require correct types.
-res = t.createIndex({loc: '2dsphere'}, {'2dsphereIndexVersion': 'NOT_A_NUMBER'});
-assert.commandFailed(res);
+assert.commandFailedWithCode(
+    t.createIndex({loc: '2dsphere'}, {'2dsphereIndexVersion': 'NOT_A_NUMBER'}),
+    ErrorCodes.TypeMismatch);
 
-res = t.createIndex({loc: '2dsphere'}, {finestIndexedLevel: 'NOT_A_NUMBER'});
-assert.commandFailedWithCode(res, ErrorCodes.TypeMismatch);
-
-res = t.createIndex({loc: '2dsphere'}, {coarsestIndexedLevel: 'NOT_A_NUMBER'});
-assert.commandFailedWithCode(res, ErrorCodes.TypeMismatch);
-
-res = t.createIndex({loc: '2dsphere'}, {finestIndexedLevel: true});
-assert.commandFailedWithCode(res, ErrorCodes.TypeMismatch);
-
-res = t.createIndex({loc: '2dsphere'}, {coarsestIndexedLevel: true});
-assert.commandFailedWithCode(res, ErrorCodes.TypeMismatch);
+assert.commandFailedWithCode(t.createIndex({loc: '2dsphere'}, {finestIndexedLevel: 'NOT_A_NUMBER'}),
+                             ErrorCodes.TypeMismatch);
+assert.commandFailedWithCode(
+    t.createIndex({loc: '2dsphere'}, {coarsestIndexedLevel: 'NOT_A_NUMBER'}),
+    ErrorCodes.TypeMismatch);
+assert.commandFailedWithCode(t.createIndex({loc: '2dsphere'}, {finestIndexedLevel: true}),
+                             ErrorCodes.TypeMismatch);
+assert.commandFailedWithCode(t.createIndex({loc: '2dsphere'}, {coarsestIndexedLevel: true}),
+                             ErrorCodes.TypeMismatch);
 
 // Ensure polygon which previously triggered an assertion error in SERVER-19674
 // is able to be indexed.
 t.drop();
-t.insert({
+assert.commandWorked(t.insert({
     loc: {
         "type": "Polygon",
         "coordinates": [[[-45, 0], [-44.875, 0], [-44.875, 0.125], [-45, 0.125], [-45, 0]]]
     }
-});
-res = t.createIndex({loc: "2dsphere"});
-assert.commandWorked(res);
+}));
+assert.commandWorked(t.createIndex({loc: "2dsphere"}));
