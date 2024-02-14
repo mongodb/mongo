97cb54323f7162ce8f0e847eaefad8057c6c0808
Mariano Shaar <mariano.shaar@mongodb.com>
2024-02-14 02:26:09 +0000

    SERVER-86620 Backport script for sys-perf parameters
    GitOrigin-RevId: b16ceee845c1ee3641fa8e22b4811c4ccf6e5633

--- buildscripts/idl/gen_all_server_params_list.py
+++ buildscripts/idl/gen_all_server_params_list.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-present MongoDB, Inc.
+# Copyright (C) 2023-present MongoDB, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the Server Side Public License, version 1,
@@ -25,9 +25,9 @@
 # exception statement from all source files in the program, then also delete
 # it in the license file.
 """
-Generate a file containing a list of disabled feature flags.
+Generate a file containing a list of all available server parameters.
 
-Used by resmoke.py to run only feature flag tests.
+Used by DSI to conditionally allow configuration of internalQueryStatsRateLimit parameter.
 """
 
 import os
@@ -35,47 +35,41 @@ import sys
 
 from typing import List
 
-import yaml
-
 # Permit imports from "buildscripts".
 sys.path.append(os.path.normpath(os.path.join(os.path.abspath(__file__), '../../..')))
 
 # pylint: disable=wrong-import-position
-import buildscripts.idl.lib as lib
+from buildscripts.idl import lib
 from buildscripts.idl.idl import parser
 
 
-def gen_all_feature_flags(idl_dirs: List[str] = None):
-    """Generate a list of all feature flags."""
-    default_idl_dirs = ["src", "buildscripts"]
+def gen_all_server_params(idl_dirs: List[str] = None):
+    """Generate a list of all server parameters."""
+    default_idl_dirs = ["src"]
 
     if not idl_dirs:
         idl_dirs = default_idl_dirs
 
-    all_flags = []
+    all_params = []
     for idl_dir in idl_dirs:
         for idl_path in sorted(lib.list_idls(idl_dir)):
             if lib.is_third_party_idl(idl_path):
                 continue
-            # Most IDL files do not contain feature flags.
+            # Most IDL files do not contain server parameters.
             # We can discard these quickly without expensive YAML parsing.
             with open(idl_path) as idl_file:
-                if 'feature_flags' not in idl_file.read():
+                if 'server_parameters' not in idl_file.read():
                     continue
             with open(idl_path) as idl_file:
                 doc = parser.parse_file(idl_file, idl_path)
-            for feature_flag in doc.spec.feature_flags:
-                if feature_flag.default.literal != "true":
-                    all_flags.append(feature_flag.name)
+            for server_param in doc.spec.server_parameters:
+                all_params.append(server_param.name)
 
-    with open("buildscripts/resmokeconfig/fully_disabled_feature_flags.yml") as fully_disabled_ffs:
-        force_disabled_flags = yaml.safe_load(fully_disabled_ffs)
-
-    return list(set(all_flags) - set(force_disabled_flags))
+    return all_params
 
 
-def gen_all_feature_flags_file(filename: str = lib.ALL_FEATURE_FLAG_FILE):
-    flags = gen_all_feature_flags()
+def gen_all_server_params_file(filename: str = "all_server_params.txt"):
+    flags = gen_all_server_params()
     with open(filename, "w") as output_file:
         output_file.write("\n".join(flags))
         print("Generated: ", os.path.realpath(output_file.name))
@@ -83,7 +77,7 @@ def gen_all_feature_flags_file(filename: str = lib.ALL_FEATURE_FLAG_FILE):
 
 def main():
     """Run the main function."""
-    gen_all_feature_flags_file()
+    gen_all_server_params_file()
 
 
 if __name__ == '__main__':
