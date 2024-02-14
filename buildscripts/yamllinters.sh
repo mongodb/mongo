43758b167f8823a101dcb6d89f94f36fa59a18a7
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2024-02-14 12:30:02 +0200

    SERVER-83836 Enable linter to enforce having assigned_to_jira_team tags on tasks (#18820)
    GitOrigin-RevId: 994d82b1e4b69733643884036d3d3b245eebd8a9

--- buildscripts/yamllinters.sh
+++ buildscripts/yamllinters.sh
@@ -4,3 +4,6 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR/../"
 
 yamllint -c etc/yamllint_config.yml buildscripts etc jstests
+
+PATH="$PATH:$HOME" evergreen evaluate $EVERGREEN_CONFIG_FILE_PATH > etc/evaluated_evergreen.yml
+python -m evergreen_lint -c ./etc/evergreen_lint.yml lint
