5e983f236664831031a87bef92364d6a57cd6650
Enrico Golfieri <enrico.golfieri@mongodb.com>
2024-02-16 13:26:50 +0100

    SERVER-85769 Increase test coverage for creating or sharding a "bucket" collection (#18409)
    GitOrigin-RevId: 9a0e4b4967bdaa98df16729213973f09c513f8a5

--- jstests/core/timeseries/timeseries_user_system_buckets.js
+++ jstests/core/timeseries/timeseries_user_system_buckets.js
@@ -1,18 +1,21 @@
 /**
- * Technically this is not time series colleciton test; however, due to legacy behavior, a user
- * inserts into a collection in time series bucket namespace is required not to fail.  Please note
- * this behavior is likely going to be corrected in 6.3 or after. The presence of this test does
- * not imply such behavior is supported.
- *
- * As this tests code path relevant to time series, the requires_tiemseries flag is set to avoid
- * incompatible behavior related to multi statement transactions.
+ * A user creating directly a bucket namespace is required not to fail. This test attempts to test
+ * all possible combination of creating a collection/timeseries (with a non-bucket/bucket nss) with
+ * an already created collection/timeseries (with a non-bucket/bucket nss). This test also attempts
+ * to be a reference on what behaviour expect when creating a bucket namespaces and performing an
+ * insert on it.
  *
  *  @tags: [
  *   # We need a timeseries collection.
  *   requires_timeseries,
- *   requires_fcv_63,
- *   # TODO SERVER-85382: re-enable in ugprade/downgrade suites
- *   cannot_run_during_upgrade_downgrade
+ *   # "Overriding safe failed response for :: create"
+ *   does_not_support_stepdowns,
+ *   # Running shardCollection instead of createCollection returns different error types which are
+ *   # not expected by the test
+ *   assumes_unsharded_collection,
+ *   # TODO SERVER-85382 re-enable upgrade/downgrade now that the feature flag is no longer checked
+ *   cannot_run_during_upgrade_downgrade,
+ *
  * ]
  */
 import {FeatureFlagUtil} from "jstests/libs/feature_flag_util.js";
@@ -23,37 +26,198 @@ const isTrackingUnsplittableCollections = FeatureFlagUtil.isPresentAndEnabled(
 // TODO SERVER-85382 re-enable this test with tracked collection once create collection coordinator
 // support all timeseries/bucket namespace cases
 if (!isTrackingUnsplittableCollections) {
-    let userCollSystemBuckets = db.system.buckets.coll;
-    let userColl = db.coll;
+    const tsOptions = {timeField: "timestamp", metaField: "metadata"};
+    const tsOptions2 = {timeField: "timestamp", metaField: "metadata2"};
+    const kColl = "coll"
+    const kBucket = "system.buckets.coll"
 
-    userCollSystemBuckets.drop();
-    userColl.drop();
+    function createWorked(nss, tsOptions = {}) {
+        if (Object.keys(tsOptions).length === 0) {
+            assert.commandWorked(db.createCollection(nss));
+        } else {
+            assert.commandWorked(db.createCollection(nss, {timeseries: tsOptions}));
+        }
+        return db.getCollection(nss);
+    }
 
-    // inserting into a user defined system buckets collection is possible
-    assert.commandWorked(userCollSystemBuckets.insert({a: 1}));
+    function createFailed(nss, tsOptions, errorCode) {
+        if (Object.keys(tsOptions).length === 0) {
+            assert.commandFailedWithCode(db.createCollection(nss), errorCode);
+        } else {
+            let res = db.createCollection(nss, {timeseries: tsOptions});
+            assert.commandFailedWithCode(res, errorCode);
+        }
+    }
 
-    // A user collection with the same postfix should not be considered time series collection
-    assert.commandWorked(userColl.insert({a: 2}));
+    function runTest(testCase, minRequiredVersion = null) {
+        if (minRequiredVersion != null) {
+            const res = db.getSiblingDB("admin")
+                            .system.version.find({_id: "featureCompatibilityVersion"})
+                            .toArray();
+            if (res.length == 0) {
+                // For specific suites like multitenancy we don't have the privileges to access to
+                // specific databases. If we cannot establish the version, let's skip the test case.
+                return;
+            }
+            if (MongoRunner.compareBinVersions(res[0].version, minRequiredVersion) < 0) {
+                return;
+            }
+        }
+        testCase()
+        db.dropDatabase();
+    }
 
-    let docs = userColl.find().toArray();
-    assert.eq(1, docs.length);
+    // Reset any previous run state.
+    db.dropDatabase()
 
-    let docsSystemBuckets = userCollSystemBuckets.find().toArray();
-    assert.eq(1, docsSystemBuckets.length);
+    // Case prexisting collection: standard.
+    {
+        jsTest.log("Case collection: standard / collection: standard.");
+        runTest(() => {
+            createWorked(kColl);
+            createWorked(kColl, {});
+        });
 
-    userCollSystemBuckets.drop();
-    userColl.drop();
+        jsTest.log("Case collection: standard / collection: timeseries.");
+        runTest(() => {
+            createWorked(kColl);
+            createFailed(kColl, tsOptions, ErrorCodes.NamespaceExists);
+        });
 
-    // the sequence in different order should also work
-    assert.commandWorked(userColl.insert({a: 2}));
-    assert.commandWorked(userCollSystemBuckets.insert({a: 1}));
+        jsTest.log("Case collection: standard / collection: bucket.");
+        runTest(() => {
+            let coll = createWorked(kColl);
+            let bucket = createWorked(kBucket);
 
-    docs = userColl.find().toArray();
-    assert.eq(1, docs.length);
+            bucket.insert({x: 1});
+            var docsSystemBuckets = bucket.find().toArray();
+            assert.eq(1, docsSystemBuckets.length);
 
-    docsSystemBuckets = userCollSystemBuckets.find().toArray();
-    assert.eq(1, docsSystemBuckets.length);
+            coll.insert({x: 1})
+            var docs = coll.find().toArray();
+            assert.eq(1, docs.length);
+        });
 
-    userCollSystemBuckets.drop();
-    userColl.drop();
+        jsTest.log("Case collection: standard / collection: bucket timeseries.");
+        runTest(() => {
+            let coll = createWorked(kColl);
+            // TODO SERVER-85855 creating bucket timeseries for a collection already created works,
+            // but it will redirect all the insertion into the bucket collection. Replace
+            // createWorked with createFailed once this bug is fixed. We expect now the bucket
+            // namespace to fail attempting to creating the view.
+            createWorked(kBucket, tsOptions);
+
+            assert.commandFailed(coll.insert({x: 1}));
+        });
+    }
+
+    // Case prexisting collection: timeseries.
+    {
+        jsTest.log("Case collection: timeseries / collection: standard.");
+        runTest(() => {
+            createWorked(kColl, tsOptions);
+            createFailed(kColl, {}, ErrorCodes.NamespaceExists);
+        });
+
+        jsTest.log("Case collection: timeseries / collection: timeseries.");
+        runTest(
+            () => {
+                createWorked(kColl, tsOptions);
+                createWorked(kColl, tsOptions);
+            },
+            // On 7.0 this test case wrongly fails with NamespaceExists and should not be tested.
+            "7.1"  // minRequiredVersion
+        );
+
+        jsTest.log("Case collection: timeseries / collection: timeseries with different options.");
+        runTest(() => {
+            createWorked(kColl, tsOptions);
+            createFailed(kColl, tsOptions2, ErrorCodes.NamespaceExists);
+        });
+        jsTest.log("Case collection: timeseries / collection: bucket.");
+        runTest(() => {
+            createWorked(kColl, tsOptions);
+            createFailed(kBucket, {}, ErrorCodes.NamespaceExists);
+        });
+
+        jsTest.log("Case collection: timeseries / collection: bucket timeseries.");
+        runTest(() => {
+            createWorked(kColl, tsOptions);
+            createFailed(kBucket, tsOptions, ErrorCodes.NamespaceExists);
+        });
+    }
+
+    // Case prexisting bucket collection.
+    jsTest.log("Case collection: bucket / collection: standard.");
+    {
+        runTest(() => {
+            let bucket = createWorked(kBucket);
+            let coll = createWorked(kColl);
+
+            bucket.insert({x: 1});
+            var docsSystemBuckets = bucket.find().toArray();
+            assert.eq(1, docsSystemBuckets.length);
+
+            coll.insert({x: 1})
+            var docs = coll.find().toArray();
+            assert.eq(1, docs.length);
+        });
+
+        jsTest.log("Case collection: bucket / collection: timeseries.");
+        runTest(() => {
+            createWorked(kBucket);
+            createFailed(kColl, tsOptions, ErrorCodes.NamespaceExists);
+        });
+
+        jsTest.log("Case collection: bucket / collection: bucket.");
+        runTest(() => {
+            createWorked(kBucket);
+            createWorked(kBucket);
+        });
+
+        jsTest.log("Case collection: bucket / collection: bucket timeseries.");
+        runTest(() => {
+            createWorked(kBucket);
+            createFailed(kBucket, tsOptions, ErrorCodes.NamespaceExists);
+        });
+    }
+
+    // Case prexisting bucket collection timeseries.
+    {
+        jsTest.log("Case collection: bucket timeseries / collection: standard.");
+        runTest(() => {
+            createWorked(kBucket, tsOptions)
+            // TODO SERVER-85855 creating a collection with an already created bucket timeseries
+            // works, but it will redirect all the insertion into the bucket collection. Replace
+            // createWorked with createFailed once this bug is fixed and the related view is created
+            // by the operation above
+            let coll = createWorked(kColl);
+            assert.commandFailed(coll.insert({x: 1}));
+        });
+
+        jsTest.log("Case collection: bucket timeseries / collection: timeseries.");
+        runTest(() => {
+            createWorked(kBucket, tsOptions)
+            createFailed(kColl, tsOptions, ErrorCodes.NamespaceExists);
+        });
+
+        jsTest.log("Case collection: bucket timeseries / collection: bucket.");
+        runTest(() => {
+            createWorked(kBucket, tsOptions);
+            createFailed(kBucket, {}, ErrorCodes.NamespaceExists);
+        });
+
+        jsTest.log("Case collection: bucket timeseries / collection: bucket timeseries.");
+        runTest(
+            () => {
+                createWorked(kBucket, tsOptions);
+                // Creating 2 times a bucket timeseries will fail with NamespaceExist instead of
+                // being idempotent
+                // TODO SERVER-85855 replace createFailed with createWorked
+                createFailed(kBucket, tsOptions, ErrorCodes.NamespaceExists);
+            },
+            // On 7.0 this test case used to correclty behave as idempotent operation.
+            "7.1"  // minRequiredVersion
+        );
+    }
 }
--- jstests/sharding/timeseries_user_system_buckets_sharding.js
+++ jstests/sharding/timeseries_user_system_buckets_sharding.js
@@ -10,24 +10,269 @@
  * @tags: [
  *   # We need a timeseries collection.
  *   requires_timeseries,
- *   requires_fcv_63
+ *   requires_fcv_63,
+ *   assumes_no_implicit_collection_creation_on_get_collection
  * ]
  */
 var st = new ShardingTest({shards: 2});
 assert.commandWorked(
     st.s0.adminCommand({enableSharding: 'test', primaryShard: st.shard1.shardName}));
-var testDB = st.getDB('test');
 
-// shard the collection on x
-assert.commandWorked(st.s0.adminCommand({shardcollection: "test.coll", key: {x: 1}}));
+const tsOptions = {
+    timeField: "timestamp",
+    metaField: "metadata"
+};
 
-assert.commandWorked(testDB.system.buckets.coll.insert({a: 1}));
-assert.commandWorked(testDB.coll.insert({b: 1}));
+const tsOptions2 = {
+    timeField: "timestamp",
+    metaField: "metadata2"
+};
 
-var docs = testDB.coll.find().toArray();
-assert.eq(1, docs.length);
+const kDbName = "test"
+const kColl = "coll"
+const kBucket = "system.buckets.coll"
 
-var docsSystemBuckets = testDB.system.buckets.coll.find().toArray();
-assert.eq(1, docsSystemBuckets.length);
+var db = st.getDB(kDbName);
+
+function createWorked(nss, tsOptions = {}) {
+    if (Object.keys(tsOptions).length === 0) {
+        assert.commandWorked(db.createCollection(nss));
+    } else {
+        assert.commandWorked(db.createCollection(nss, {timeseries: tsOptions}));
+    }
+    return db.getCollection(nss);
+}
+
+function createFailed(nss, tsOptions, errorCode) {
+    if (Object.keys(tsOptions).length === 0) {
+        assert.commandFailedWithCode(db.createCollection(nss), errorCode);
+    } else {
+        assert.commandFailedWithCode(db.createCollection(nss, {timeseries: tsOptions}), errorCode);
+    }
+}
+
+function shardCollectionWorked(coll, tsOptions = {}) {
+    let nss = kDbName + "." + coll
+    if (Object.keys(tsOptions).length === 0) {
+        assert.commandWorked(st.s.adminCommand({shardCollection: nss, key: {x: 1}}));
+    }
+    else {
+        assert.commandWorked(
+            st.s.adminCommand({shardCollection: nss, key: {timestamp: 1}, timeseries: tsOptions}));
+    }
+    return db.getCollection(nss);
+}
+
+function shardCollectionFailed(coll, tsOptions, errorCode) {
+    let nss = kDbName + "." + coll
+    if (Object.keys(tsOptions).length === 0) {
+        assert.commandFailedWithCode(st.s.adminCommand({shardCollection: nss, key: {x: 1}}),
+                                     errorCode);
+    }
+    else {
+        assert.commandFailedWithCode(
+            st.s.adminCommand({shardCollection: nss, key: {timestamp: 1}, timeseries: tsOptions}),
+            errorCode);
+    }
+}
+
+function runTest(testCase, minRequiredVersion = null) {
+    if (minRequiredVersion) {
+        const res =
+            st.s.getDB("admin").system.version.find({_id: "featureCompatibilityVersion"}).toArray();
+        if (MongoRunner.compareBinVersions(res[0].version, minRequiredVersion) < 0) {
+            return;
+        }
+    }
+    testCase()
+    db.dropDatabase();
+}
+
+// Case prexisting collection: standard.
+{
+    jsTest.log("Case collection: standard / collection: sharded standard.");
+    runTest(() => {
+        createWorked(kColl);
+        shardCollectionWorked(kColl);
+    });
+
+    jsTest.log("Case collection: standard / collection: sharded timeseries.");
+    runTest(
+        () => {
+            createWorked(kColl);
+            shardCollectionWorked(kColl, tsOptions);
+        },
+        // On 7.0 this test case used to wrongly fail with NamespaceExists.
+        "7.1"  // minRequiredVersion
+    );
+}
+
+// Case prexisting collection: timeseries.
+{
+    jsTest.log("Case collection: timeseries / collection: sharded standard.");
+    runTest(() => {
+        createWorked(kColl, tsOptions);
+        shardCollectionFailed(kColl, {}, 5914001);
+    });
+
+    jsTest.log("Case collection: timeseries / collection: sharded timeseries.");
+    runTest(() => {
+        createWorked(kColl, tsOptions);
+        shardCollectionWorked(kColl, tsOptions);
+    });
+
+    jsTest.log("Case collection: timeseries / collection: sharded timeseries with different opts.");
+    runTest(() => {
+        createWorked(kColl, tsOptions);
+        shardCollectionFailed(kColl, tsOptions2, 5731500);
+    });
+}
+
+// Case prexisting collection: bucket standard.
+{
+    jsTest.log("Case collection: bucket standard / collection: sharded standard.");
+    runTest(() => {
+        createWorked(kBucket);
+        shardCollectionFailed(kColl, {}, 6159000);
+    });
+
+    jsTest.log("Case collection: bucket standard / collection: sharded timeseries.");
+    runTest(() => {
+        createWorked(kBucket);
+        shardCollectionFailed(kColl, tsOptions, 6159000);
+    });
+}
+
+// Case prexisting collection: bucket timeseries.
+{
+    jsTest.log("Case collection: bucket timeseries / collection: sharded standard.");
+    runTest(() => {
+        createWorked(kBucket, tsOptions);
+        shardCollectionFailed(kColl, {}, 5914001);
+    });
+
+    jsTest.log("Case collection: bucket timeseries / collection: sharded timeseries.");
+    runTest(
+        () => {
+            createWorked(kBucket, tsOptions);
+            let coll = shardCollectionWorked(kColl, tsOptions);
+
+            // TODO SERVER-85855 remove all the code below once creating a timeseries bucket
+            // collection will create the related view. Before the fix, the shardCollection returns
+            // a pointer to a "test.test.coll" which doesn't exist. An invalid insert afterwords
+            // will work and create the collection as unsharded. After the fix, we expect this case
+            // to behave same as sharding an existing timeseries collection.
+            assert.commandWorked(coll.insert({x: 1}));
+            let listCollections = db.runCommand({listCollections: 1}).cursor.firstBatch;
+            listCollections.sort((a, b) => a.name < b.name);  // collection name alphabetic order
+
+            assert.eq(2, listCollections.length);
+            assert.eq(kBucket, listCollections[1].name);
+            // note the collection name is "test.coll and not just coll"
+            assert.eq("test.coll", listCollections[0].name);
+        },
+        // TODO SERVER-85855 On 7.0 this test case used to cause a primary node crash.
+        "7.1"  // minRequiredVersion
+    );
+}
+
+// Case pre-existing collection: sharded standard
+{
+    jsTest.log("Case collection: sharded standard / collection: standard.");
+    runTest(() => {
+        shardCollectionWorked(kColl);
+        createWorked(kColl);
+    });
+
+    jsTest.log("Case collection: sharded standard / collection: timeseries.");
+    runTest(() => {
+        shardCollectionWorked(kColl);
+        createFailed(kColl, tsOptions, ErrorCodes.NamespaceExists);
+    });
+
+    jsTest.log("Case collection: sharded standard / collection: bucket.");
+    runTest(() => {
+        let coll = shardCollectionWorked(kColl);
+        let bucket = createWorked(kBucket);
+
+        bucket.insert({x: 1});
+        var docsSystemBuckets = bucket.find().toArray();
+        assert.eq(1, docsSystemBuckets.length);
+
+        coll.insert({x: 1})
+        var docs = coll.find().toArray();
+        assert.eq(1, docs.length);
+    });
+
+    jsTest.log("Case collection: sharded standard / collection: bucket timeseries.");
+    runTest(() => {
+        let coll = shardCollectionWorked(kColl);
+        createWorked(kBucket, tsOptions);
+
+        coll.insert({x: 1})
+        var docs = coll.find().toArray();
+        assert.eq(1, docs.length);
+    });
+
+    jsTest.log("Case collection: sharded standard / collection: sharded standard.");
+    runTest(() => {
+        shardCollectionWorked(kColl);
+        shardCollectionWorked(kColl);
+    });
+
+    jsTest.log("Case collection: sharded standard / collection: sharded timeseries.");
+    runTest(() => {
+        shardCollectionWorked(kColl);
+        shardCollectionFailed(kColl, tsOptions, ErrorCodes.AlreadyInitialized);
+    });
+}
+
+// Case pre-existing collection: sharded timeseries
+{
+    jsTest.log("Case collection: sharded timeseries / collection: standard.");
+    runTest(() => {
+        shardCollectionWorked(kColl, tsOptions);
+        createFailed(kColl, {}, ErrorCodes.NamespaceExists);
+    });
+
+    jsTest.log("Case collection: sharded timeseries / collection: timeseries.");
+    runTest(
+        () => {
+            shardCollectionWorked(kColl, tsOptions);
+            createWorked(kColl, tsOptions);
+        },
+        // On 7.0 this test case used to wrongly fail with NamespaceExists.
+        "7.1"  // minRequiredVersion
+    );
+
+    jsTest.log("Case collection: sharded timeseries / collection: timeseries with different opts.");
+    runTest(() => {
+        shardCollectionWorked(kColl, tsOptions);
+        createFailed(kColl, tsOptions2, ErrorCodes.NamespaceExists);
+    });
+    jsTest.log("Case collection: sharded timeseries / collection: bucket.");
+    runTest(() => {
+        shardCollectionWorked(kColl, tsOptions);
+        createFailed(kBucket, {}, ErrorCodes.NamespaceExists);
+    });
+
+    jsTest.log("Case collection: sharded timeseries / collection: bucket timeseries.");
+    runTest(() => {
+        shardCollectionWorked(kColl, tsOptions);
+        createFailed(kBucket, tsOptions, ErrorCodes.NamespaceExists);
+    });
+
+    jsTest.log("Case collection: sharded timeseries / collection: sharded standard.");
+    runTest(() => {
+        shardCollectionWorked(kColl, tsOptions);
+        shardCollectionFailed(kColl, {}, ErrorCodes.AlreadyInitialized);
+    });
+
+    jsTest.log("Case collection: sharded timeseries / collection: sharded timeseries.");
+    runTest(() => {
+        shardCollectionWorked(kColl, tsOptions);
+        shardCollectionWorked(kColl, tsOptions);
+    });
+}
 
 st.stop();
