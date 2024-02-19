704c07df7b570ba5816dfa3cdde0c384bb6887be
Jordi Olivares Provencio <jordi.olivares-provencio@mongodb.com>
2024-02-19 14:38:55 +0000

    SERVER-86624 Fix RSLocalClient behavior with snapshot readConcern
    GitOrigin-RevId: 32100c26a0cd6a292d4f6a8aa5f81c802fe25765

--- src/mongo/db/rs_local_client.cpp
+++ src/mongo/db/rs_local_client.cpp
@@ -143,43 +143,33 @@ StatusWith<Shard::QueryResponse> RSLocalClient::queryOnce(
                     originalReadSource);
             }
         });
-        if (readConcernLevel == repl::ReadConcernLevel::kMajorityReadConcern) {
-            // Sets up operation context with majority read snapshot so correct optime can be
-            // retrieved.
-            shard_role_details::getRecoveryUnit(opCtx)->setTimestampReadSource(
-                RecoveryUnit::ReadSource::kMajorityCommitted);
-            Status status =
-                shard_role_details::getRecoveryUnit(opCtx)->majorityCommittedSnapshotAvailable();
-            if (!status.isOK()) {
-                return status;
-            }
+        // Sets up operation context with majority read snapshot so correct optime can be
+        // retrieved.
+        shard_role_details::getRecoveryUnit(opCtx)->setTimestampReadSource(
+            RecoveryUnit::ReadSource::kMajorityCommitted);
+        Status status =
+            shard_role_details::getRecoveryUnit(opCtx)->majorityCommittedSnapshotAvailable();
+        if (!status.isOK()) {
+            return status;
+        }
 
-            // Waits for any writes performed by this ShardLocal instance to be committed and
-            // visible.
-            Status readConcernStatus = replCoord->waitUntilOpTimeForRead(
-                opCtx, repl::ReadConcernArgs{_getLastOpTime(), readConcernLevel});
-            if (!readConcernStatus.isOK()) {
-                return readConcernStatus;
-            }
+        // Waits for any writes performed by this ShardLocal instance to be committed and
+        // visible. We hardcode majority here even if using snaphsot as both operations will do the
+        // initial snapshot at the majority timestamp.
+        Status readConcernStatus = replCoord->waitUntilOpTimeForRead(
+            opCtx,
+            repl::ReadConcernArgs{_getLastOpTime(), repl::ReadConcernLevel::kMajorityReadConcern});
+        if (!readConcernStatus.isOK()) {
+            return readConcernStatus;
+        }
 
-            // Informs the storage engine to read from the committed snapshot for the rest of this
-            // operation.
-            status =
-                shard_role_details::getRecoveryUnit(opCtx)->majorityCommittedSnapshotAvailable();
-            if (!status.isOK()) {
-                return status;
-            }
-        } else {
-            // Waits for any writes performed by this ShardLocal instance to be committed and
-            // visible. We use majority readConcern here since the snaphsot model will open it at
-            // the majority timestamp.
-            Status readConcernStatus = replCoord->waitUntilOpTimeForRead(
-                opCtx,
-                repl::ReadConcernArgs{_getLastOpTime(),
-                                      repl::ReadConcernLevel::kMajorityReadConcern});
-            if (!readConcernStatus.isOK()) {
-                return readConcernStatus;
-            }
+        status = shard_role_details::getRecoveryUnit(opCtx)->majorityCommittedSnapshotAvailable();
+        if (!status.isOK()) {
+            return status;
+        }
+        if (readConcernLevel == repl::ReadConcernLevel::kSnapshotReadConcern) {
+            // Snapshot readConcern starts a snapshot at the majority timestamp, acquire the
+            // timestamp now and overwrite the majority readConcern used above.
             auto opTime = replCoord->getCurrentCommittedSnapshotOpTime();
             shard_role_details::getRecoveryUnit(opCtx)->setTimestampReadSource(
                 RecoveryUnit::ReadSource::kProvided, opTime.getTimestamp());
