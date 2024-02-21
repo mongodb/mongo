bde5d91b4db637b32f3dfa4bafabd96e7b6fb9fe
huayu-ouyang <huayu.ouyang@mongodb.com>
2024-02-20 23:02:58 -0500

    SERVER-86772 Fix racy watchdog_test (#19113)
    (cherry picked from commit 80a1b2a847f3e1a352a6b9da1a06978679852816 and c2df3b2f2cd9cedfe70fb59b1f8f8d7c1740012a)
    (cherry picked from commit 6a1d44f418b25d3649545abd9d1c3cc0e9e24431)
    
    GitOrigin-RevId: e8ea9a219b1c64d4f4832f2b3e8ebcca1a56a6d5

--- src/mongo/watchdog/watchdog_test.cpp
+++ src/mongo/watchdog/watchdog_test.cpp
@@ -381,6 +381,8 @@ TEST_F(WatchdogMonitorThreadTest, SleepyHungCheck) {
 
     monitorThread.start();
 
+    sleepmillis(100);
+
     deathEvent.wait();
 
     monitorThread.shutdown();
@@ -414,6 +416,8 @@ TEST_F(WatchdogMonitorTest, SleepyHungCheck) {
     ASSERT_EQ(monitor.getMonitorGeneration(), 0);
 
     monitor.start();
+
+    sleepmillis(100);
 
     deathEvent.wait();
 
@@ -492,8 +496,10 @@ TEST_F(WatchdogMonitorTest, PauseAndResume) {
     monitor.setPeriod(Milliseconds(1007));
 
     counterCheckPtr->waitForCount();
-    // Sleep to ensure the monitor runs at least once.
-    sleepmillis(1007);
+    // Wait for monitor to run at least once.
+    while (monitor.getMonitorGeneration() < 1) {
+        sleepmillis(100);
+    }
 
     monitor.shutdown();
 
