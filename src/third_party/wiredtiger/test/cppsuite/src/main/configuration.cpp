9034386744c0a5dde9f18bcaf8b1b5d84d96172a
Ruby Chen <ruby.chen@mongodb.com>
2024-02-28 02:33:56 +0000

    Import wiredtiger: 95a23d08566c39a1a03cd81fc5a2e8f319971a08 from branch mongodb-master
    ref: 9004e72c20..95a23d0856
    for: 8.0.0-rc0
    
    WT-12466 Remove test instances of test config validate
    
    GitOrigin-RevId: 5d8d3e27676e437872fec5750983fcf4fc0b06a0

--- src/third_party/wiredtiger/test/cppsuite/src/main/configuration.cpp
+++ src/third_party/wiredtiger/test/cppsuite/src/main/configuration.cpp
@@ -82,10 +82,7 @@ configuration::configuration(const std::string &test_config_name, const std::str
     logger::log_msg(LOG_INFO, "Full config: " + _config);
 
     int ret =
-      wiredtiger_test_config_validate(nullptr, nullptr, test_config_name.c_str(), _config.c_str());
-    if (ret != 0)
-        testutil_die(EINVAL, "failed to validate given config, ensure test config exists");
-    ret = wiredtiger_config_parser_open(nullptr, _config.c_str(), _config.size(), &_config_parser);
+      wiredtiger_config_parser_open(nullptr, _config.c_str(), _config.size(), &_config_parser);
     if (ret != 0)
         testutil_die(EINVAL, "failed to create configuration parser for provided config");
 }
