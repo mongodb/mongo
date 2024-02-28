293a3bddd095231703ab2cd2d07162d90c0f7a27
Ruby Chen <ruby.chen@mongodb.com>
2024-02-28 23:47:32 +0000

    Import wiredtiger: 19c6c4f2526f82a6749cf00130ec0d8e45d850d4 from branch mongodb-master
    ref: 2cb1fa57b2..19c6c4f252
    for: 8.0.0-rc0
    
    WT-12524 Common mechanism for parallel execution.
    
    GitOrigin-RevId: 33204527eb006ce5f33f4897526ee17289d6d319

--- src/third_party/wiredtiger/dist/common_functions.sh
+++ src/third_party/wiredtiger/dist/common_functions.sh
@@ -42,7 +42,7 @@ while len(pids):
 }
 
 # Do normal cleanup on exit or abort.
-setup_trap() { trap 'kill `allchildpids` 2>/dev/null; rm -f $t' 0 1 2 3 13 15; }
+setup_trap() { trap 'kill `allchildpids` 2>/dev/null; [[ -n "$t" ]] && rm -f $t*; '"$@" 0 1 2 3 13 15; }
 
 # Absolute path to the `dist/` directory.
 DIST_DIR="$(realpath "$(dirname -- "${BASH_SOURCE[0]}")")"
@@ -64,16 +64,34 @@ check_fast_mode() {
 
 # Check if the fast mode is requested.
 is_fast_mode() {
+  FAST_FLAG=""   # Indicates if the fast mode was requested by flag.
   [[ -n "${WT_FAST:-}" ]] && return 0;
   local arg;
   for arg in "${BASH_ARGV[@]:-}"; do
-    [[ "$arg" == "-F" ]] && export WT_FAST="-F" && return 0;
+    [[ "$arg" == "-F" ]] && FAST_FLAG="-F" && export WT_FAST="-F" && return 0;
+  done
+  return 1
+}
+
+# Check if the script has file arguments.
+# Assume files are any argument that doesn't begin with a "-".
+has_file_args() {
+  for arg in "${BASH_ARGV[@]:-}"; do
+    [[ "${arg:0:1}" != "-" ]] && return 0;
   done
   return 1
 }
 
 # Check if the fast mode is requested and is available.
-check_fast_mode_flag() { is_fast_mode && check_fast_mode; }
+# Set variables appropriately or exit with an error.
+check_fast_mode_flag() {
+  is_fast_mode || return
+  check_fast_mode
+  if [[ -n "${FAST_FLAG:-}" ]] && has_file_args; then
+    echo "ERROR: Fast mode (-F flag) cannot be used with file arguments"
+    exit 1
+  fi
+}
 
 # Get the earliest common commit from the dev branch.
 last_commit_from_dev() { python3 "$DIST_DIR"/common_functions.py last_commit_from_dev; }
@@ -105,4 +123,69 @@ filter_if_fast() {
   else
     cat
   fi
+}
+
+# Try running xargs with the -P option. If it succeeds, set XARGS_P accordingly.
+# POSIX xargs does not support -P option - it's a GNU extension.
+# -P option limits the maximum number of subprocesses created by xargs.
+# Without -P, xargs will sun only one subprocess at a time.
+# Usage:
+#   check_xargs_P [NUM_PROCESSES]
+#   xargs $XARGS_P ...
+check_xargs_P() {
+  [[ -z "${XARGS_P:+x}" ]] &&
+    XARGS_P=$(echo echo | xargs -P 1 echo >/dev/null 2>&1 && echo "-P ${1:-20}")
+}
+
+# Check if the script is run in a recursive mode (with files args).
+# File arguments are those that don't start with a "-".
+is_recursive_mode() {
+  [[ -n "${S_RECURSE:-}" ]] && return 0         # S_RECURSE is set.
+  [[ ${BASH_ARGC:-} -eq 0 ]] && return 1        # No args at all
+  for arg in "${BASH_ARGV[@]:-}"; do
+    [[ "${arg:0:1}" == "-" ]] && return 1;      # Option arg found.
+  done
+  return 0
+}
+
+# Returns true if the script has to find files to process (non-recursive call).
+# The top level script generates a list of files to process and calls itself in parallel mode.
+is_main_run() {
+  ! is_recursive_mode       # Return the inverted result of is_recursive_mode.
+}
+
+# Run the same the script in parallel mode.
+# The list of files is provided via stdin.
+# filter_if_fast is automatically applied to the list.
+# This function has protection against sub-recursive calls from child processes.
+#
+# Example usage:
+#    is_main_run && {
+#        find bench examples ext src test -name '*.[ch]' | do_in_parallel
+#        exit $?
+#    }
+#    ... code for processing files when run as parallel or with file arguments ...
+#
+#  - `is_main_run` checks if the script is run in non-recursive mode (has no file args).
+#  - `find ...` lists files to process.
+#  - `do_in_parallel` restarts the same script in parallel mode.
+#  - `exit $?` exits the script with the return code of `do_in_parallel`.
+#
+# NOTE: `do_in_parallel` can't do `exit` itself since Bash is allowed to execute piped
+# commands in subshells. Calling `exit` from a subshell will not terminate the main program.
+# The caller should use `exit` after `do_in_parallel` to terminate the script.
+#
+# NOTE: Fast mode works only for scripts starting in the TOP directory because
+# filter_if_fast uses empty prefix.
+#
+do_in_parallel() {
+  [[ -n "${S_RECURSE:-}" ]] && { echo "ERROR: Possible infinite recursion detected."; exit 1; }
+  export S_RECURSE=1
+  local name=$(basename $0)
+  check_xargs_P
+  filter_if_fast | xargs $XARGS_P -n $(( ${#WT_FAST} ? 5 : 30 )) "$@" -- bash -c "
+      bash $DIST_DIR/$name \"\$@\" 2>&1 > $t-$name-par-\$\$.out
+      cat $t-$name-par-\$\$.out 2>/dev/null
+      rm -f $t-$name-par-\$\$.out" -bash
+  # implicit: return $?
 }
