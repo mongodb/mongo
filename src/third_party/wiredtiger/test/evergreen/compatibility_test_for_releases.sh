c4426b051f2c63209ef945eaa5c0a9afd8689e21
Sean04 <sean.watt@mongodb.com>
2024-03-04 03:57:08 +0000

    Import wiredtiger: 35bd6fd39ec5a8b7099c5160f1e1a58e788f104a from branch mongodb-master
    ref: 4104cc7ab4..35bd6fd39e
    for: 8.0.0-rc0
    
    WT-12571 Fix newer releases compatibility test for Huffman coding
    
    GitOrigin-RevId: 29f4bcbb4d4c10fd22efc059185ef78ee6ee2eac

--- src/third_party/wiredtiger/test/evergreen/compatibility_test_for_releases.sh
+++ src/third_party/wiredtiger/test/evergreen/compatibility_test_for_releases.sh
@@ -154,6 +154,7 @@ create_configs()
 
     echo "##################################################" > $file_name
     echo "runs.type=row" >> $file_name              # WT-7379 - Temporarily disable column store tests
+    echo "btree.huffman_value=0" >> $file_name      # WT-12456 - Never used, removed from newer releases
     echo "btree.prefix=0" >> $file_name             # WT-7579 - Prefix testing isn't portable between releases
     echo "cache=80" >> $file_name                   # Medium cache so there's eviction
     echo "checksum=on" >> $file_name                # WT-7851 Fix illegal checksum configuration
