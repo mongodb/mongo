fd77ae6bb488713f6f3089ee3ac9d88caff2b25e
Trevor <trevorguidry6@gmail.com>
2024-03-05 01:36:28 -0600

    SERVER-81251 Ensure future git tag variant always runs lastContinuous suites (#19452)
    GitOrigin-RevId: 228b9fbf1ab379204eeaff19ff70b865ad20c902

--- evergreen/prelude_mongo_task_generator.sh
+++ evergreen/prelude_mongo_task_generator.sh
@@ -1,6 +1,6 @@
 function setup_mongo_task_generator {
   if [ ! -f mongo-task-generator ]; then
-    curl -L https://github.com/mongodb/mongo-task-generator/releases/download/v0.7.11/mongo-task-generator --output mongo-task-generator
+    curl -L https://github.com/mongodb/mongo-task-generator/releases/download/v0.7.12/mongo-task-generator --output mongo-task-generator
     chmod +x mongo-task-generator
   fi
 }
