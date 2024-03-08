ce278c7121a1aad1d39a2fbed5beff8f8079727d
Pierlauro Sciarelli <pierlauro.sciarelli@mongodb.com>
2024-03-08 20:44:08 +0100

    SERVER-87679 Blacklist core tests invoking `convertToCapped` from suites with balancer (#19748)
    GitOrigin-RevId: 6458c182f7a8b46ce7976561b20c6f53516ce349

--- jstests/core/ddl/convert_to_capped.js
+++ jstests/core/ddl/convert_to_capped.js
@@ -6,6 +6,8 @@
  *   not_allowed_with_signed_security_token,
  *   requires_non_retryable_commands,
  *   requires_capped,
+ *   # SERVER-85772 enable testing with balancer once convertToCapped supported on arbitrary shards
+ *   assumes_balancer_off,
  * ]
  */
 
