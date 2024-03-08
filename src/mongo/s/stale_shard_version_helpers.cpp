07d85e32dccc5d6b641ecd2a1e482164dbd91d97
Tommaso Tocci <58224719+toto-dev@users.noreply.github.com>
2024-03-08 22:18:17 +0100

    SERVER-87610 Relax shardVersionRetry tripwires on the namespace of received stale exceptions (#19737)
    (cherry picked from commit aef11e7e3ade447f28ae0d8ebc23256237263b87)
    (cherry picked from commit 73283dc5c7ab73ee9901231f17cd9bf0256c1634)
    
    GitOrigin-RevId: b5a03a99e201733fbb296647139adb5cfd427793

--- src/mongo/s/stale_shard_version_helpers.cpp
+++ src/mongo/s/stale_shard_version_helpers.cpp
@@ -62,10 +62,6 @@ void checkErrorStatusAndMaxRetries(const Status& status,
     if (status == ErrorCodes::StaleDbVersion) {
         auto staleInfo = status.extraInfo<
             error_details::ErrorExtraInfoForImpl<ErrorCodes::StaleDbVersion>::type>();
-        invariant(staleInfo->getDb() == nss.db(),
-                  str::stream() << "StaleDbVersion error on unexpected database. Expected "
-                                << nss.db() << ", received " << staleInfo->getDb());
-
         // If the database version is stale, refresh its entry in the catalog cache.
         catalogCache->onStaleDatabaseVersion(staleInfo->getDb(), staleInfo->getVersionWanted());
 
@@ -78,9 +74,6 @@ void checkErrorStatusAndMaxRetries(const Status& status,
         // If the cache currently considers the collection to be unsharded, this will trigger an
         // epoch refresh. If no shard is provided, then the epoch is stale and we must refresh.
         if (auto staleInfo = status.extraInfo<StaleConfigInfo>()) {
-            invariant(staleInfo->getNss() == nss,
-                      str::stream() << "StaleConfig error on unexpected namespace. Expected " << nss
-                                    << ", received " << staleInfo->getNss());
             catalogCache->invalidateShardOrEntireCollectionEntryForShardedCollection(
                 nss, staleInfo->getVersionWanted(), staleInfo->getShardId());
         } else {
