b9e5ac1ef8e736b418505409cb217f2ab30dea30
Kaitlin Mahar <kaitlin.mahar@mongodb.com>
2024-03-12 13:52:57 -0400

    SERVER-87737 Make bulk_write_max_replies_size_mongos.js insert routing more deterministic (#19794)
    GitOrigin-RevId: 691770ee4f6a0d39b23b0b92aee23992f9902d6b

--- jstests/noPassthrough/bulk_write_max_replies_size_mongos.js
+++ jstests/noPassthrough/bulk_write_max_replies_size_mongos.js
@@ -10,35 +10,61 @@
  *   requires_fcv_80,
  * ]
  */
-import {cursorEntryValidator} from "jstests/libs/bulk_write_utils.js";
+import {cursorEntryValidator, cursorSizeValidator} from "jstests/libs/bulk_write_utils.js";
+import {ShardVersioningUtil} from "jstests/sharding/libs/shard_versioning_util.js";
 
 const st = new ShardingTest({
     mongos: 1,
     shards: 2,
     rs: {nodes: 1},
-    mongosOptions: {setParameter: {featureFlagBulkWriteCommand: true}}
+    mongosOptions: {
+        setParameter: {
+            featureFlagBulkWriteCommand: true,
+            logComponentVerbosity: tojson({command: 4, sharding: 4})
+        }
+    }
 });
 
 const dbName = "test";
 const db = st.getDB(dbName);
-var coll = db.getCollection("banana");
+const coll1 = db.getCollection("banana");
+const coll2 = db.getCollection("orange");
 
-jsTestLog("Shard the collection.");
-assert.commandWorked(coll.createIndex({a: 1}));
+const ns1 = "test.banana";
+const ns2 = "test.orange";
+
+jsTestLog("Shard the collections.");
+assert.commandWorked(coll1.createIndex({a: 1}));
+assert.commandWorked(coll2.createIndex({a: 1}));
 assert.commandWorked(db.adminCommand({enableSharding: "test"}));
-assert.commandWorked(db.adminCommand({shardCollection: "test.banana", key: {a: 1}}));
+assert.commandWorked(db.adminCommand({shardCollection: ns1, key: {a: 1}}));
+assert.commandWorked(db.adminCommand({shardCollection: ns2, key: {a: 1}}));
 
 jsTestLog("Create chunks, then move them.");
-assert.commandWorked(db.adminCommand({split: "test.banana", middle: {a: 2}}));
-assert.commandWorked(
-    db.adminCommand({moveChunk: "test.banana", find: {a: 0}, to: st.shard0.shardName}));
-assert.commandWorked(
-    db.adminCommand({moveChunk: "test.banana", find: {a: 3}, to: st.shard1.shardName}));
+assert.commandWorked(db.adminCommand({split: ns1, middle: {a: 2}}));
+assert.commandWorked(db.adminCommand({moveChunk: ns1, find: {a: 0}, to: st.shard0.shardName}));
+assert.commandWorked(db.adminCommand({moveChunk: ns1, find: {a: 3}, to: st.shard1.shardName}));
+
+// Ensure coll2 initially is on shard0.
+assert.commandWorked(db.adminCommand({moveChunk: ns2, find: {a: 0}, to: st.shard0.shardName}));
+// Then move its only chunk to shard1 but do not refresh shard1, so that the first write to the
+// collection will get a staleness error.
+ShardVersioningUtil.moveChunkNotRefreshRecipient(st.s, ns2, null, st.shard1, {a: 0});
 
 assert.commandWorked(
     st.s.adminCommand({"setParameter": 1, "bulkWriteMaxRepliesSize": NumberInt(20)}));
 
-// Test that replies size limit is hit when bulkWriteMaxRepliesSize is set and ordered = false
+// Test that replies size limit is hit when bulkWriteMaxRepliesSize is set and ordered = false.
+// This request should generate two child requests that will be executed in parallel on mongos:
+// 1. one to shard0 with the first insert. We should get a single success reply back from the shard.
+// 2. one to shard1 with the second and third inserts. We should get a single StaleConfig reply
+// back from the shard (even though we are unordered we stop on StaleConfig.)
+
+// Regardless of which reply we get first, after that round of execution we should end up with
+// the first insert being marked complete and the next two having been reset to ready. At that
+// point, before doing another round of execution, we will check and see we have hit
+// bulkWriteMaxRepliesSize and abort execution, marking the second write as failed due to exceeding
+// the memory limit.
 let res = st.s.adminCommand({
     bulkWrite: 1,
     ops: [
@@ -46,7 +72,7 @@ let res = st.s.adminCommand({
         {insert: 1, document: {a: 1}},
         {insert: 0, document: {a: 4}}
     ],
-    nsInfo: [{ns: "test.banana"}, {ns: "test.orange"}],
+    nsInfo: [{ns: ns1}, {ns: ns2}],
     ordered: false
 });
 
@@ -55,13 +81,16 @@ jsTestLog(res);
 
 assert.commandWorked(res);
 assert.eq(res.nErrors, 1);
+cursorSizeValidator(res, 2);
 
 cursorEntryValidator(res.cursor.firstBatch[0], {ok: 1, idx: 0, n: 1});
 cursorEntryValidator(res.cursor.firstBatch[1],
                      {ok: 0, idx: 1, n: 0, code: ErrorCodes.ExceededMemoryLimit});
-assert(!res.cursor.firstBatch[2]);
 
-// Test that replies size limit is hit when bulkWriteMaxRepliesSize is set and ordered = true
+// Test that replies size limit is hit when bulkWriteMaxRepliesSize is set and ordered = true.
+// This request should generate a child request to shard0 containing the first write, which
+// will succeed. After executing it, we will see we have hit the memory limit and stop executing
+// the rest of the batch.
 res = st.s.adminCommand({
     bulkWrite: 1,
     ops: [
@@ -69,16 +98,16 @@ res = st.s.adminCommand({
         {insert: 1, document: {a: 1}},
         {insert: 0, document: {a: 4}}
     ],
-    nsInfo: [{ns: "test.banana"}, {ns: "test.orange"}],
+    nsInfo: [{ns: ns1}, {ns: ns2}],
     ordered: true
 });
 
 assert.commandWorked(res);
 assert.eq(res.nErrors, 1);
+cursorSizeValidator(res, 2);
 
 cursorEntryValidator(res.cursor.firstBatch[0], {ok: 1, idx: 0, n: 1});
 cursorEntryValidator(res.cursor.firstBatch[1],
                      {ok: 0, idx: 1, n: 0, code: ErrorCodes.ExceededMemoryLimit});
-assert(!res.cursor.firstBatch[2]);
 
 st.stop();
