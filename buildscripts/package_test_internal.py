c1badff717afe871dc62b28f92c6c47b17623163
Daniel Moody <dmoody256@gmail.com>
2024-03-22 02:53:16 -0500

    SERVER-88350 test_packages should only install_compass on the support platforms (#20273)
    GitOrigin-RevId: ce91ba62b7dd3259ec297234f4af4c330f79f1c0

--- buildscripts/package_test_internal.py
+++ buildscripts/package_test_internal.py
@@ -4,6 +4,7 @@
 # pylint: disable=redefined-outer-name,invalid-name,subprocess-run-check
 
 import grp
+import json
 import logging
 import os
 import pathlib
@@ -71,8 +72,9 @@ def download_extract_package(package: str) -> List[str]:
 def download_extract_all_packages(package_urls: List[str]) -> List[str]:
     all_packages = []  # type: List[str]
     for package_url in package_urls:
-        package_names = download_extract_package(package_url)
-        all_packages.extend(["./" + package_name for package_name in package_names])
+        if package_url:
+            package_names = download_extract_package(package_url)
+            all_packages.extend(["./" + package_name for package_name in package_names])
     return all_packages
 
 
@@ -414,6 +416,13 @@ def test_stop():
 
 
 def test_install_compass(test_args: TestArgs):
+
+    if test_args['arch'] != "x86_64" or test_args['os_name'] not in ["ubuntu", "almalinux"]:
+        logging.info(
+            "Not installing compass on unsupported platform, see the docs: https://www.mongodb.com/docs/compass/current/install/"
+        )
+        return
+
     logging.info("Installing Compass.")
 
     cmd = []  # type: List[str]
@@ -421,15 +430,7 @@ def test_install_compass(test_args: TestArgs):
         cmd += ["DEBIAN_FRONTEND=noninteractive"]
     cmd += ["install_compass"]
 
-    exec_result = run_and_log(" ".join(cmd), end_on_error=False)
-
-    # install-compass currently cannot use zypper to install packages. Ignore error code.
-    if test_args['package_manager'] == 'zypper' or exec_result.returncode not in [
-            0,  # success
-            3,  # unsupported arch (compass only supports x86_64)
-            6,  # unsupported linux version
-    ]:
-        raise RuntimeError("Failed to install compass")
+    run_and_log(" ".join(cmd))
 
 
 def test_uninstall(test_args: TestArgs):
@@ -489,6 +490,7 @@ else:
     sys.exit(1)
 
 test_args = get_test_args(package_manager, package_files)
+logging.info("Test Args:\n%s", json.dumps(test_args, sort_keys=True, indent=4))
 setup(test_args)
 install_fake_systemd(test_args)
 
