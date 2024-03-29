b9e4b8f0135f80e37716b8985f72279e049313b6
Daniel Moody <dmoody256@gmail.com>
2024-03-29 18:27:04 +0000

    SERVER-82684 partial revert to get passed github evergreen merge issue.
    GitOrigin-RevId: ca5486367db5ff3962349ff2e2da1991a5669108

---
+++ evergreen/run_clang_tidy.sh
@@ -0,0 +1,9 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+. "$DIR/prelude.sh"
+
+set -o pipefail
+set -o verbose
+
+cd src
+activate_venv
+$python buildscripts/clang_tidy.py --clang-tidy-cfg ${clang_tidy_file} --clang-tidy-toolchain ${clang_tidy_toolchain}
