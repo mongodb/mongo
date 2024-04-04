78e1452ec12980f74bba5b20153fde732366d974
kmznam <97981975+kmznam@users.noreply.github.com>
2024-04-04 13:17:15 -0400

    SERVER-82895: removing database creation in $out test (#20687)
    GitOrigin-RevId: e0533573681f7367d85500516f7d2ce26c9d8500

--- jstests/aggregation/sources/out/required_last_position.js
+++ jstests/aggregation/sources/out/required_last_position.js
@@ -2,13 +2,6 @@
 import {assertErrorCode} from "jstests/aggregation/extras/utils.js";
 import {FixtureHelpers} from "jstests/libs/fixture_helpers.js";
 
-// TODO SERVER-82460 remove creation of database once
-// $out behavior will be equal in both standalone and sharded cluster
-if (FixtureHelpers.isMongos(db)) {
-    // Create database
-    assert.commandWorked(db.adminCommand({'enableSharding': db.getName()}));
-}
-
 const coll = db.require_out_last;
 coll.drop();
 
