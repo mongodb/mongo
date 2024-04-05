607798565ab43267aecfd75c3a957dc8587cbe2d
madelinezec <mez2113@columbia.edu>
2024-04-05 15:55:54 +0000

    SERVER-88393 make mongot-localdev work for sharded clusters
    GitOrigin-RevId: dd5e9153049c6e28429b2cf35d84c455f714cea0

---
+++ jstests/with_mongot/e2e_infrastructure_tests/verify_coll_is_sharded.js
@@ -0,0 +1,22 @@
+// Jstests rely on the shell eval option, implicitly_shard_accessed_collections.js, to override
+// any command that calls DB.prototype.getCollection() with function that attempts to shard
+// the collection before returning it. This test confirms that this sharding test override works
+// for sharded deployments with mongot enabled. In other words, this test confirms that the
+// collections in sharded search e2e suite's tests are indeed sharded.
+
+const outputColl = db.out;
+const inputColl = db.input;
+inputColl.drop();
+outputColl.drop();
+
+inputColl.insert({a: -1});
+inputColl.insert({a: -10});
+inputColl.insert({a: 100});
+outputColl.insert({a: -1000});
+outputColl.insert({a: 1000});
+outputColl.insert({a: 1});
+
+// You cannot specify a sharded collection as the output collection, so this should throw if
+// collection is indeed sharded.
+assert.throws(() => {inputColl.aggregate(
+                  [{$group: {_id: "$_id", sum: {$sum: "$a"}}}, {$out: outputColl.getName()}])})
