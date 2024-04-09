53f4cb72a278e1a494d6863263c542811fced3e7
Juan Gu <111318390+juangugit@users.noreply.github.com>
2024-04-08 18:02:07 -0700

    SERVER-87505 find/remove all uses of mongo-enterprise-modules in 10ge… (#20252)
    GitOrigin-RevId: e1d818e8a77b27355e8554777b2d3d5e78cb1643

--- evergreen/run_jstestfuzz/clone_repos.sh
+++ evergreen/run_jstestfuzz/clone_repos.sh
@@ -2,5 +2,5 @@ set -o errexit
 set -o verbose
 
 cd src
-git clone --depth 1 git@github.com:10gen/mongo-enterprise-modules.git jstests/enterprise_tests
 git clone --depth 1 git@github.com:10gen/QA.git jstests/qa_tests
+cp -r src/mongo/db/modules/enterprise jstests/enterprise_tests
