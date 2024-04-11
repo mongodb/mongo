174003ce8e511d6a9c21155bc9d6c2df0869fc5a
Trevor Guidry <trevor.guidry@mongodb.com>
2024-04-11 17:49:27 -0500

    SERVER-87594 Add compile variant that simulates a workstation build (#21010)
    GitOrigin-RevId: 885acdd13d1f221aa3c3508ce2a12bc58fe8475a

--- evergreen/ninja_compile.sh
+++ evergreen/ninja_compile.sh
@@ -8,14 +8,27 @@ set -o verbose
 
 activate_venv
 
+if [[ -n "${ninja_jobs}" ]]; then
+  JOBS_ARG="-j${ninja_jobs}"
+fi
+
 if [ "Windows_NT" = "$OS" ]; then
   vcvars="$(vswhere -latest -property installationPath | tr '\\' '/' | dos2unix.exe)/VC/Auxiliary/Build/"
   echo "call \"$vcvars/vcvarsall.bat\" amd64" > msvc.bat
   for i in "${compile_env[@]}"; do
     echo "set $i" >> msvc.bat
   done
-  echo "ninja -f ${ninja_file} ${targets}" >> msvc.bat
-  cmd /C msvc.bat
+  echo "ninja -f ${ninja_file} ${JOBS_ARG} ${targets}" >> msvc.bat
+  for i in $(seq ${ninja_retries:=1}); do
+    cmd /C msvc.bat && RET=0 && break || RET=$? && sleep 1
+  done
 else
-  eval ${compile_env} ninja -f ${ninja_file} ${targets}
+  for i in $(seq ${ninja_retries:=1}); do
+    eval ${compile_env} ninja -f ${ninja_file} ${JOBS_ARG} ${targets} && RET=0 && break || RET=$? && sleep 1
+  done
+fi
+
+if [ $RET -ne 0 ]; then
+  echo "Ninja command failed"
+  exit $RET
 fi
