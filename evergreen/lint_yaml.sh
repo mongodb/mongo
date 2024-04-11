e5b653299060989bd1396024f9c7ab613b2848e8
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2024-04-11 12:31:36 +0300

    SERVER-83064 Use variant tags to mark required/suggested variants (#20917)
    GitOrigin-RevId: 4f65f57ee31d4027e3e6a8d129c5abf1c28169b9

--- evergreen/lint_yaml.sh
+++ evergreen/lint_yaml.sh
@@ -6,7 +6,7 @@ set -o pipefail
 cd src
 
 activate_venv
-EVERGREEN_CONFIG_FILE_PATH=${evergreen_config_file_path} ./buildscripts/yamllinters.sh | tee yamllinters.log
+./buildscripts/yamllinters.sh | tee yamllinters.log
 exit_code=$?
 
 $python ./buildscripts/simple_report.py --test-name yamllinters --log-file yamllinters.log --exit-code $exit_code
