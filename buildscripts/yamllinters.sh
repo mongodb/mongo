37c443c7a104b4ef12413d374513aee85415012e
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2024-04-11 12:31:36 +0300

    SERVER-83064 Use variant tags to mark required/suggested variants (#20917)
    GitOrigin-RevId: 9aa527817b271cd9a9ddd5ee74d0ff87fe909fd4

--- buildscripts/yamllinters.sh
+++ buildscripts/yamllinters.sh
@@ -5,5 +5,8 @@ cd "$BASEDIR/../"
 
 yamllint -c etc/yamllint_config.yml buildscripts etc jstests
 
-PATH="$PATH:$HOME" evergreen evaluate $EVERGREEN_CONFIG_FILE_PATH > etc/evaluated_evergreen.yml
+PATH="$PATH:$HOME" evergreen evaluate etc/evergreen.yml > etc/evaluated_evergreen.yml
+PATH="$PATH:$HOME" evergreen evaluate etc/evergreen_nightly.yml > etc/evaluated_evergreen_nightly.yml
+PATH="$PATH:$HOME" evergreen evaluate etc/system_perf.yml > etc/evaluated_system_perf.yml
+
 python -m evergreen_lint -c ./etc/evergreen_lint.yml lint
