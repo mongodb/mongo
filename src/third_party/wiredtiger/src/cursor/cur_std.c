c2b54f9294f84a97f060d29ca43418c3b596efcf
Chenhao Qu <chenhao.qu@mongodb.com>
2024-04-15 11:00:29 +1000

    Import wiredtiger: 69e82b2b9dec3bb1beadcfb152deefa3719ac280 from branch mongodb-master
    ref: 829cf68c2d..69e82b2b9d
    for: 8.1.0-rc0
    
    WT-12834 Make perf improvements in setting/getting keys/values
    
    GitOrigin-RevId: 6c87b5b4cae9ab101928f1bf08a389ebf1ba3813

--- src/third_party/wiredtiger/src/cursor/cur_std.c
+++ src/third_party/wiredtiger/src/cursor/cur_std.c
@@ -409,9 +409,12 @@ __wt_cursor_get_keyv(WT_CURSOR *cursor, uint64_t flags, va_list ap)
         } else
             *va_arg(ap, uint64_t *) = cursor->recno;
     } else {
-        /* Fast path some common cases. */
+        /*
+         * Fast path some common cases. The case we care most about being fast is "u", check that
+         * first.
+         */
         fmt = cursor->key_format;
-        if (LF_ISSET(WT_CURSOR_RAW_OK) || WT_STREQ(fmt, "u")) {
+        if (WT_STREQ(fmt, "u") || LF_ISSET(WT_CURSOR_RAW_OK)) {
             key = va_arg(ap, WT_ITEM *);
             key->data = cursor->key.data;
             key->size = cursor->key.size;
@@ -463,9 +466,12 @@ __wt_cursor_set_keyv(WT_CURSOR *cursor, uint64_t flags, va_list ap)
         buf->data = &cursor->recno;
         sz = sizeof(cursor->recno);
     } else {
-        /* Fast path some common cases and special case WT_ITEMs. */
+        /*
+         * Fast path some common cases and special case WT_ITEMs. The case we care most about being
+         * fast is "u", check that first.
+         */
         fmt = cursor->key_format;
-        if (LF_ISSET(WT_CURSOR_RAW_OK | WT_CURSTD_DUMP_JSON) || WT_STREQ(fmt, "u")) {
+        if (WT_STREQ(fmt, "u") || LF_ISSET(WT_CURSOR_RAW_OK | WT_CURSTD_DUMP_JSON)) {
             item = va_arg(ap, WT_ITEM *);
             sz = item->size;
             buf->data = item->data;
@@ -547,9 +553,11 @@ __wt_cursor_get_valuev(WT_CURSOR *cursor, va_list ap)
     if (FLD_ISSET(S2C(session)->debug_flags, WT_CONN_DEBUG_CURSOR_COPY))
         WT_ERR(__wt_buf_grow(session, &cursor->value, cursor->value.size));
 
-    /* Fast path some common cases. */
+    /*
+     * Fast path some common cases. The case we care most about being fast is "u", check that first.
+     */
     fmt = cursor->value_format;
-    if (F_ISSET(cursor, WT_CURSOR_RAW_OK) || WT_STREQ(fmt, "u")) {
+    if (WT_STREQ(fmt, "u") || F_ISSET(cursor, WT_CURSOR_RAW_OK)) {
         value = va_arg(ap, WT_ITEM *);
         value->data = cursor->value.data;
         value->size = cursor->value.size;
@@ -641,8 +649,10 @@ __wt_cursor_set_valuev(WT_CURSOR *cursor, const char *fmt, va_list ap)
 
     F_CLR(cursor, WT_CURSTD_VALUE_SET);
 
-    /* Fast path some common cases. */
-    if (F_ISSET(cursor, WT_CURSOR_RAW_OK | WT_CURSTD_DUMP_JSON) || WT_STREQ(fmt, "u")) {
+    /*
+     * Fast path some common cases. The case we care most about being fast is "u", check that first.
+     */
+    if (WT_STREQ(fmt, "u") || F_ISSET(cursor, WT_CURSOR_RAW_OK | WT_CURSTD_DUMP_JSON)) {
         item = va_arg(ap, WT_ITEM *);
         sz = item->size;
         buf->data = item->data;
@@ -1547,7 +1557,7 @@ __wt_cursor_init(
      * WT_CURSOR.modify supported on 'S' and 'u' value formats, but may have been already
      * initialized (file cursors have a faster implementation).
      */
-    if ((WT_STREQ(cursor->value_format, "S") || WT_STREQ(cursor->value_format, "u")) &&
+    if ((WT_STREQ(cursor->value_format, "u") || WT_STREQ(cursor->value_format, "S")) &&
       cursor->modify == __wt_cursor_modify_value_format_notsup)
         cursor->modify = __cursor_modify;
 
