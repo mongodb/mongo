3194794301a21fde59d9c0ff2c25a396fe1d1182
Jennifer Wang <jennifer.wang@mongodb.com>
2024-04-16 20:46:48 +0000

    SERVER-77530 Join the mirroring executor on shutdown
    GitOrigin-RevId: a2e585cf7bec6df942d9cd6a8e8c1cafc8395ddc

--- src/mongo/db/mirror_maestro.cpp
+++ src/mongo/db/mirror_maestro.cpp
@@ -506,6 +506,7 @@ void MirrorMaestroImpl::shutdown() noexcept {
 
     if (_executor) {
         _executor->shutdown();
+        _executor->join();
     }
 
     // Set _initGuard.liveness to kShutdown
