a42f7e31179158b3b969b8f6dc7edc0c216a5522
Adityavardhan Agrawal <adi.agrawal@mongodb.com>
2024-04-18 17:29:08 -0400

    SERVER-88949 Fix skipAmount in explain (#21199)
    (cherry picked from commit 78453425ac98a10d59947385f001be0a89fbee2d)
    
    GitOrigin-RevId: 9c48196a5bd033fc031b94ef225853d1f068dfe3

---
+++ jstests/core/query/explain/explain_skip.js
@@ -0,0 +1,28 @@
+/**
+ * Tests that explain reports the skip stage statistics correctly.
+ *
+ * @tags: [
+ *   assumes_unsharded_collection,
+ *   assumes_against_mongod_not_mongos,
+ * ]
+ */
+import {getPlanStage, getWinningPlan} from "jstests/libs/analyze_plan.js";
+
+const coll = db[jsTestName()];
+coll.drop();
+
+assert.commandWorked(coll.insert({a: 1, b: 1}));
+assert.commandWorked(coll.insert({a: 1, b: 2}));
+assert.commandWorked(coll.insert({a: 1, b: 3}));
+assert.commandWorked(coll.insert({a: 2, b: 1}));
+assert.commandWorked(coll.insert({a: 2, b: 2}));
+assert.commandWorked(coll.insert({a: 2, b: 3}));
+
+assert.commandWorked(coll.createIndex({a: 1}));
+assert.commandWorked(coll.createIndex({b: 1}));
+
+const explain = coll.find({a: 1, b: 1}).sort({_id: 1}).skip(5).explain();
+const winningPlan = getWinningPlan(explain.queryPlanner);
+const skipStage = getPlanStage(winningPlan, "SKIP");
+assert.neq(null, skipStage, explain);
+assert.eq(5, skipStage.skipAmount, explain);
