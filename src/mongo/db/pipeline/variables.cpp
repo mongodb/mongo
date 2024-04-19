d4994f6bc059b3a2eecb31a4afd416c7234ea649
HanaPearlman <hana.pearlman@mongodb.com>
2024-04-19 18:16:02 -0400

    SERVER-89268: Serialize 'let' variables by wrapping in $literal, serializing missing 'let' variables with $$REMOVE (#21227)
    SERVER-89268: Serialize 'let' variables by wrapping in $literal,
    serializing missing 'let' variables with $$REMOVE
    
    When serializing values, for example to populate the 'let' parameter of
    a command to be sent over the wire, we need to be careful. First,
    missing values should be serialized as "$$REMOVE", otherwise they might
    be incorrectly omitted or serialized as empty objects ({}). We should
    wrap other kinds of values in $literal to avoid a scenario like the
    following: suppose we had a user-defined 'let' specified as {let: {a:
    {$literal: "$notAFieldName"}}}. On mongos, this will be evaluated to the
    string "$notAFieldName". When we serialize it again for the shard
    commands, it must appear as {$literal: "$notAFieldName"}, not simply
    "$notAFieldName", since the latter will be treated as a field name by
    mongods.
    GitOrigin-RevId: 85267363ae64e5c6238698d46a36f7809081da96

--- src/mongo/db/pipeline/variables.cpp
+++ src/mongo/db/pipeline/variables.cpp
@@ -61,6 +61,21 @@
 
 namespace mongo {
 
+namespace {
+
+// We need to be careful when serializing values, e.g. to populate the 'let' parameter of a command
+// to be sent over the wire. First, missing values should be serialied as $$REMOVE, otherwise they
+// might be incorrectly omitted or serialized as empty objects ({}). Also, we should wrap values in
+// $literal to avoid a scenario like the following: suppose we had a user-defined 'let' specified as
+// {let: {a: {$literal: "$notAFieldName"}}}. On mongos, this will be evaluated to the string
+// "$notAFieldName". When we serialize it again for the shard commands, it must appear as {$literal:
+// "$notAFieldName"}, not simply "$notAFieldName", since the latter will be treated as a field name
+// by mongods.
+Value serializeValue(Value val) {
+    return val.missing() ? Value("$$REMOVE"_sd) : Value(DOC("$literal" << val));
+}
+}  // namespace
+
 using namespace std::string_literals;
 
 constexpr Variables::Id Variables::kRootId;
@@ -311,11 +326,12 @@ void Variables::seedVariablesWithLetParameters(ExpressionContext* const expCtx,
         }
     }
 }
+
 BSONObj Variables::toBSON(const VariablesParseState& vps, const BSONObj& varsToSerialize) const {
     BSONObjBuilder result;
     for (BSONElement elem : varsToSerialize) {
         StringData name = elem.fieldNameStringData();
-        result << name << getUserDefinedValue(vps.getVariable(name));
+        result << name << serializeValue(getUserDefinedValue(vps.getVariable(name)));
     }
     return result.obj();
 }
@@ -469,8 +485,9 @@ std::set<Variables::Id> VariablesParseState::getDefinedVariableIDs() const {
 BSONObj VariablesParseState::serialize(const Variables& vars) const {
     auto bob = BSONObjBuilder{};
     for (auto&& [var_name, id] : _variables)
-        if (vars.hasValue(id))
-            bob << var_name << Value(DOC("$literal" << vars.getValue(id)));
+        if (vars.hasValue(id)) {
+            bob << var_name << serializeValue(vars.getValue(id));
+        }
 
     // System variables have to be added separately since the variable IDs are reserved and not
     // allocated like normal variables, and so not present in '_variables'.
@@ -482,8 +499,9 @@ std::pair<LegacyRuntimeConstants, BSONObj> VariablesParseState::transitionalComp
     const Variables& vars) const {
     auto bob = BSONObjBuilder{};
     for (auto&& [var_name, id] : _variables)
-        if (vars.hasValue(id))
-            bob << var_name << Value(DOC("$literal" << vars.getValue(id)));
+        if (vars.hasValue(id)) {
+            bob << var_name << serializeValue(vars.getValue(id));
+        }
 
     return {vars.transitionalExtractRuntimeConstants(), bob.obj()};
 }
