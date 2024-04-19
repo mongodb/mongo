2925da46e12f2f87ccbc911ddc3581cd076f8744
Yu Jin Kang Park <yujin.kang@mongodb.com>
2024-04-19 07:54:29 +0000

    SERVER-79774 Add balancer warning shard draining is blocked due to collection balancing being disabled (#19599)
    GitOrigin-RevId: 66ae6b1ea9329fe4b4383b2fea09b2968ef433fd

---
+++ jstests/sharding/libs/remove_shard_util.js
@@ -0,0 +1,34 @@
+function removeShard(shardingTestOrConn, shardName, timeout) {
+    if (timeout == undefined) {
+        timeout = 10 * 60 * 1000;  // 10 minutes
+    }
+
+    var s;
+    if (shardingTestOrConn instanceof ShardingTest) {
+        s = shardingTestOrConn.s;
+    } else {
+        s = shardingTestOrConn;
+    }
+
+    assert.soon(function() {
+        let res;
+        if (TestData.configShard && shardName == "config") {
+            // Need to use transitionToDedicatedConfigServer if trying
+            // to remove config server as a shard
+            res = s.adminCommand({transitionToDedicatedConfigServer: shardName});
+        } else {
+            res = s.adminCommand({removeShard: shardName});
+        }
+        if (!res.ok && res.code === ErrorCodes.ShardNotFound) {
+            // If the config server primary steps down right after removing the config.shards doc
+            // for the shard but before responding with "state": "completed", the mongos would retry
+            // the _configsvrRemoveShard command against the new config server primary, which would
+            // not find the removed shard in its ShardRegistry if it has done a ShardRegistry reload
+            // after the config.shards doc for the shard was removed. This would cause the command
+            // to fail with ShardNotFound.
+            return true;
+        }
+        assert.commandWorked(res);
+        return res.state == 'completed';
+    }, "failed to remove shard " + shardName + " within " + timeout + "ms", timeout);
+}
