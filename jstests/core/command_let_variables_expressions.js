e9950adf3ce01dd07bc9eae1c2e37d3f50471450
HanaPearlman <hana.pearlman@mongodb.com>
2024-04-23 16:13:21 -0400

    SERVER-89717: Allow command_let_variables_expressions.js to run in multiversion tests with 7.0 (#21409)
    GitOrigin-RevId: 13b983633d1a5814d6ebeaf33093e72564dc2041

--- jstests/core/command_let_variables_expressions.js
+++ jstests/core/command_let_variables_expressions.js
@@ -120,7 +120,7 @@ const literalLetParam = {
 
     assert.commandWorked(db.runCommand({
         findAndModify: coll.getName(),
-        query: {$expr: {$eq: ["$a", "$$c"]}},
+        query: {_id: 3, $expr: {$eq: ["$a", "$$c"]}},
         update: [{$set: {c: "updated"}}],
         let : {c: missingLetParam}
     }));
@@ -134,7 +134,7 @@ const literalLetParam = {
 
     assert.commandWorked(db.runCommand({
         findAndModify: coll.getName(),
-        query: {$expr: {$eq: ["$a", "$$c"]}},
+        query: {_id: 4, $expr: {$eq: ["$a", "$$c"]}},
         update: [{$set: {c: "updated"}}],
         let : {c: literalLetParam}
     }));
@@ -156,7 +156,7 @@ const literalLetParam = {
         delete: coll.getName(),
         deletes: [{
             q: {$expr: {$eq: ["$a", "$$c"]}},
-            limit: 1,
+            limit: 0,  // multi
         }],
         let : {c: missingLetParam}
     }));
@@ -172,7 +172,7 @@ const literalLetParam = {
         delete: coll.getName(),
         deletes: [{
             q: {$expr: {$eq: ["$a", "$$c"]}},
-            limit: 1,
+            limit: 0,  // multi
         }],
         let : {c: literalLetParam}
     }));
