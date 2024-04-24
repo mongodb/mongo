4c2767ff0b8d2cef898e8971944f98b32c04aa84
kshitij <kshitij.gupta@mongodb.com>
2024-04-24 15:53:03 -0400

    SERVER-88003: Add tests for timeseries resharding. (#21450)
    (cherry picked from commit 0591aaff1fce20eb26c960816261439542989cc9)
    
    GitOrigin-RevId: ab4bd4d0bae2e06046e5bfd42c54aaba1cbd7221

---
+++ jstests/sharding/resharding_timeseries/reshard_timeseries_disallow_writes.js
@@ -0,0 +1,150 @@
+/**
+ * Tests that writes are disallowed while in kCommitted for timeseries resharded collections.
+ *
+ * @tags: [
+ *   featureFlagReshardingForTimeseries,
+ * ]
+ */
+import {ReshardingTest} from "jstests/sharding/libs/resharding_test_fixture.js";
+
+const reshardingTest = new ReshardingTest({numDonors: 2, numRecipients: 2, reshardInPlace: false});
+reshardingTest.setup();
+
+const dbName = "test";
+const collName = "foo";
+const ns = dbName + "." + collName;
+
+const timeseriesInfo = {
+    timeField: 'ts',
+    metaField: 'meta'
+}
+
+const donorShardNames = reshardingTest.donorShardNames;
+
+const timeseriesCollection = reshardingTest.createShardedCollection({
+    ns: ns,
+    shardKeyPattern: {'meta.x': 1},
+    chunks: [
+        {min: {'meta.x': MinKey}, max: {'meta.x': 0}, shard: donorShardNames[0]},
+        {min: {'meta.x': 0}, max: {'meta.x': MaxKey}, shard: donorShardNames[1]},
+    ],
+    collOptions: {
+        timeseries: timeseriesInfo,
+    }
+});
+
+assert.commandWorked(
+    timeseriesCollection.insert({data: 1, ts: new Date(), meta: {x: -1, y: -1, z: 0}}));
+assert.commandWorked(timeseriesCollection.createIndexes(
+    [{indexToDropDuringResharding: 1}, {indexToDropAfterResharding: 1}]));
+
+const bucketsCollName = "system.buckets.foo";
+const bucketsColl = reshardingTest._st.s.getDB("test").getCollection('system.buckets.foo');
+
+const recipientShardNames = reshardingTest.recipientShardNames;
+reshardingTest.withReshardingInBackground(
+    {
+        newShardKeyPattern: {'meta.y': 1},
+        newChunks: [
+            {min: {'meta.y': MinKey}, max: {'meta.y': 0}, shard: recipientShardNames[0]},
+            {min: {'meta.y': 0}, max: {'meta.y': MaxKey}, shard: recipientShardNames[1]},
+        ],
+    },
+    () => {},
+    {
+        postCheckConsistencyFn: () => {
+            jsTestLog("Attempting insert");
+            let res = timeseriesCollection.runCommand({
+                insert: collName,
+                documents: [{data: 3, ts: new Date(), meta: {x: -2, y: -2}}],
+                maxTimeMS: 3000
+            });
+            assert(ErrorCodes.isExceededTimeLimitError(res.writeErrors[0].code));
+
+            res = bucketsColl.runCommand({
+                insert: bucketsCollName,
+                documents: [{data: 3, ts: new Date(), meta: {x: -2, y: -2}}],
+                maxTimeMS: 3000
+            });
+            assert(ErrorCodes.isExceededTimeLimitError(res.writeErrors[0].code));
+
+            jsTestLog("Attempting update");
+            res = timeseriesCollection.runCommand({
+                update: collName,
+                updates: [{q: {'meta.x': -1}, u: {$set: {'meta.x': -15}}, multi: true}],
+                maxTimeMS: 3000
+            });
+            assert(ErrorCodes.isExceededTimeLimitError(res.writeErrors[0].code));
+
+            res = bucketsColl.runCommand({
+                update: bucketsCollName,
+                updates: [{q: {'meta.x': -1}, u: {$set: {'meta.x': -15}}, multi: true}],
+                maxTimeMS: 3000
+            });
+            assert(ErrorCodes.isExceededTimeLimitError(res.writeErrors[0].code));
+
+            jsTestLog("Attempting delete");
+            res = timeseriesCollection.runCommand(
+                {delete: collName, deletes: [{q: {'meta.x': -1}, limit: 1}], maxTimeMS: 3000});
+            assert(ErrorCodes.isExceededTimeLimitError(res.writeErrors[0].code));
+
+            res = bucketsColl.runCommand({
+                delete: bucketsCollName,
+                deletes: [{q: {'meta.x': -1}, limit: 1}],
+                maxTimeMS: 3000
+            });
+            assert(ErrorCodes.isExceededTimeLimitError(res.writeErrors[0].code));
+
+            jsTestLog("Attempting createIndex");
+            res = timeseriesCollection.runCommand({
+                createIndexes: collName,
+                indexes: [{key: {'meta.z': 1}, name: "metaz_0"}],
+                maxTimeMS: 3000
+            });
+            assert(ErrorCodes.isExceededTimeLimitError(res.code));
+
+            jsTestLog("Attempting collMod");
+            // The collMod is serialized with the resharding command, so we explicitly add an
+            // timeout to the command so that it doesn't get blocked and timeout the test.
+            res = timeseriesCollection.runCommand({collMod: collName, maxTimeMS: 3000});
+            assert(ErrorCodes.isExceededTimeLimitError(res.code));
+
+            jsTestLog("Attempting drop index");
+            res = timeseriesCollection.runCommand(
+                {dropIndexes: collName, index: {indexToDropDuringResharding: 1}, maxTimeMS: 3000});
+            assert(ErrorCodes.isExceededTimeLimitError(res.code));
+
+            jsTestLog("Completed operations");
+        },
+        afterReshardingFn: () => {
+            jsTestLog("Join possible ongoing collMod command");
+            assert.commandWorked(timeseriesCollection.runCommand("collMod"));
+        }
+    });
+
+jsTestLog("Verify that writes succeed after resharding operation has completed");
+
+assert.commandWorked(timeseriesCollection.runCommand({
+    insert: collName,
+    documents: [{data: 3, ts: new Date(), meta: {x: -2, y: -2}}],
+    maxTimeMS: 3000
+}));
+
+assert.commandWorked(timeseriesCollection.runCommand({
+    update: collName,
+    updates: [{q: {'meta.x': -1}, u: {$set: {'meta.x': -15}}, multi: true}],
+    maxTimeMS: 3000
+}));
+
+assert.commandWorked(timeseriesCollection.runCommand(
+    {delete: collName, deletes: [{q: {'meta.x': -1}, limit: 1}], maxTimeMS: 3000}));
+
+assert.commandWorked(timeseriesCollection.runCommand(
+    {createIndexes: collName, indexes: [{key: {'meta.z': 1}, name: "metaz_0"}], maxTimeMS: 3000}));
+
+assert.commandWorked(timeseriesCollection.runCommand({collMod: collName, maxTimeMS: 3000}));
+
+assert.commandWorked(timeseriesCollection.runCommand(
+    {dropIndexes: collName, index: {indexToDropDuringResharding: 1}, maxTimeMS: 3000}));
+
+reshardingTest.teardown();
---
+++ jstests/sharding/resharding_timeseries/resharding_ts_txn_cloner.js
@@ -0,0 +1,100 @@
+/**
+ * Tests the timeseries resharding recipient shards handles config.transactions entries from the
+ * source shards.
+ *
+ * @tags: [
+ *   uses_atclustertime,
+ *   featureFlagReshardingForTimeseries,
+ * ]
+ */
+import {ReshardingTest} from "jstests/sharding/libs/resharding_test_fixture.js";
+
+const reshardingTest = new ReshardingTest({numDonors: 3, numRecipients: 3, reshardInPlace: true});
+
+reshardingTest.setup();
+
+const dbName = "test";
+const collName = "foo";
+const ns = dbName + "." + collName;
+
+const timeseriesInfo = {
+    timeField: 'ts',
+    metaField: 'meta'
+}
+
+const donorShardNames = reshardingTest.donorShardNames;
+const inputCollection = reshardingTest.createShardedCollection({
+    ns: ns,
+    shardKeyPattern: {'meta.x': 1},
+    chunks: [
+        {min: {'meta.x': MinKey}, max: {'meta.x': 0}, shard: donorShardNames[0]},
+        {min: {'meta.x': 0}, max: {'meta.x': 100}, shard: donorShardNames[1]},
+        {min: {'meta.x': 100}, max: {'meta.x': MaxKey}, shard: donorShardNames[2]},
+    ],
+    collOptions: {
+        timeseries: timeseriesInfo,
+    }
+});
+
+let lsidList = [];
+lsidList.push(UUID());
+lsidList.push(UUID());
+lsidList.push(UUID());
+
+let execRetryableInsert = function(lsid, docs) {
+    return inputCollection.getDB('test').runCommand({
+        insert: 'foo',
+        documents: docs,
+        ordered: false,
+        lsid: {id: lsid},
+        txnNumber: NumberLong(1),
+    });
+};
+
+assert.commandWorked(execRetryableInsert(lsidList[0], [
+    {data: 3, ts: new Date(), 'meta.x': -10, 'meta.y': 0},
+    {data: 2, ts: new Date(), 'meta.x': 10, 'meta.y': -19}
+]));
+assert.commandWorked(execRetryableInsert(lsidList[1], [
+    {data: -9, ts: new Date(), 'meta.x': 0, 'meta.y': 100},
+    {data: 0, ts: new Date(), 'meta.x': 105, 'meta.y': -16}
+]));
+assert.commandWorked(
+    execRetryableInsert(lsidList[2], [{data: 16, ts: new Date(), 'meta.x': 100, 'meta.y': -10}]));
+
+const recipientShardNames = reshardingTest.recipientShardNames;
+reshardingTest.withReshardingInBackground({
+    newShardKeyPattern: {'meta.y': 1},
+    newChunks: [
+        {min: {'meta.y': MinKey}, max: {'meta.y': 0}, shard: recipientShardNames[1]},
+        {min: {'meta.y': 0}, max: {'meta.y': 100}, shard: recipientShardNames[2]},
+        {min: {'meta.y': 100}, max: {'meta.y': MaxKey}, shard: recipientShardNames[0]},
+    ],
+});
+
+// If we don't refresh mongos, writes will be targetted based on the chunk distribution before
+// resharding. Even though the shard versions don't match, it will not cause a stale config
+// exception because the write was already executed on the shard being targetted, resulting in a
+// no-op write, and thus, no shard version checking. This behavior is not wrong, but since we
+// want to test the retry behavior after resharding, we force the mongos to refresh.
+const mongos = inputCollection.getMongo();
+assert.commandWorked(mongos.adminCommand({flushRouterConfig: 1}));
+
+assert.commandFailedWithCode(
+    execRetryableInsert(lsidList[0], [{data: 2, ts: new Date(), 'meta.x': 10, 'meta.y': -19}]),
+    ErrorCodes.IncompleteTransactionHistory);
+assert.commandFailedWithCode(
+    execRetryableInsert(lsidList[0], [{data: 3, ts: new Date(), 'meta.x': -10, 'meta.y': 0}]),
+    ErrorCodes.IncompleteTransactionHistory);
+assert.commandFailedWithCode(
+    execRetryableInsert(lsidList[1],
+                        [
+                            {data: -9, ts: new Date(), 'meta.x': 0, 'meta.y': 100},
+                            {data: 0, ts: new Date(), 'meta.x': 105, 'meta.y': -16}
+                        ]),
+    ErrorCodes.IncompleteTransactionHistory);
+assert.commandFailedWithCode(
+    execRetryableInsert(lsidList[2], [{data: 16, ts: new Date(), 'meta.x': 100, 'meta.y': -10}]),
+    ErrorCodes.IncompleteTransactionHistory);
+
+reshardingTest.teardown();
