223e3c24f8b77bd56c8a80bb4665cf5fb6eec318
Trevor Guidry <trevor.guidry@mongodb.com>
2024-04-23 19:36:00 -0500

    SERVER-89752 Make logkeeper buildIDs stay unique when branching (#21432)
    (cherry picked from commit 558637538525610e117e35031a3a23dd3cf8db86)
    
    GitOrigin-RevId: a5bb52e2c64ca27b7bab49cadb6f628cf91e8d50

--- evergreen/functions/credentials_setup.sh
+++ evergreen/functions/credentials_setup.sh
@@ -6,7 +6,7 @@ cd src
 cat > mci.buildlogger << END_OF_CREDS
 slavename='${slave}'
 passwd='${passwd}'
-builder='${build_variant}_master'
+builder='${build_variant}_${project}'
 build_num=${builder_num}
 build_phase='${task_name}_${execution}'
 END_OF_CREDS
