5f428d45f5abf1eb47424894f15e2771cac48c8c
kshitij <kshitij.gupta@mongodb.com>
2024-04-25 18:04:19 -0400

    SERVER-88003: Test requestResumeToken with timeseries collections. (#21457)
    GitOrigin-RevId: 1e0a32064169a628128b5f51658ecca1abe2c2dd

---
+++ jstests/sharding/resharding_timeseries/resharding_ts_resume_agg_token.js
@@ -0,0 +1,78 @@
+/**
+ * Tests $_requestResumeToken in aggregate command on timeseries buckets collection.
+ *
+ * @tags: [
+ *  featureFlagReshardingForTimeseries,
+ * ]
+ */
+
+const st = new ShardingTest({mongos: 1, shards: 2});
+const kDbName = 'db';
+const kCollName = 'foo';
+const ns = kDbName + '.' + kCollName;
+const sDB = st.s.getDB(kDbName);
+
+assert.commandWorked(
+    sDB.adminCommand({enableSharding: kDbName, primaryShard: st.shard0.shardName}));
+
+const timeFieldName = 'time';
+const metaFieldName = 'meta';
+
+const timeseriesOptions = {
+    timeField: timeFieldName,
+    metaField: metaFieldName,
+};
+
+assert.commandWorked(st.s.adminCommand({
+    shardCollection: ns,
+    key: {'meta.x': 1},
+    timeseries: timeseriesOptions,
+}));
+
+const kBucketCollName = "system.buckets.foo"
+
+const doc1 = {
+    data: 1,
+    time: new Date(),
+    meta: {x: 1, y: 5}
+};
+const doc2 = {
+    data: 3,
+    time: new Date(),
+    meta: {x: 2, y: 3}
+};
+// Insert some docs
+assert.commandWorked(sDB.getCollection(kCollName).insert([
+    doc1,
+    doc2,
+    {data: 3, time: new Date(), meta: {x: 3, y: 2}},
+    {data: 1, time: new Date(), meta: {x: 4, y: 1}}
+]));
+
+const db = st.rs0.getPrimary().getDB(kDbName);
+jsTest.log(
+    "aggregate with $requestResumeToken should return PBRT with recordId and an initialSyncId.");
+let res = assert.commandWorked(db.runCommand({
+    aggregate: kBucketCollName,
+    pipeline: [],
+    $_requestResumeToken: true,
+    hint: {$natural: 1},
+    cursor: {batchSize: 1}
+}));
+assert.hasFields(res.cursor, ["postBatchResumeToken"]);
+assert.hasFields(res.cursor.postBatchResumeToken, ["$recordId"]);
+assert.hasFields(res.cursor.postBatchResumeToken, ["$initialSyncId"]);
+assert.eq(res.cursor.firstBatch[0].meta, doc1.meta);
+const resumeToken = res.cursor.postBatchResumeToken;
+
+res = assert.commandWorked(db.runCommand({
+    aggregate: kBucketCollName,
+    pipeline: [],
+    $_requestResumeToken: true,
+    hint: {$natural: 1},
+    $_resumeAfter: resumeToken,
+    cursor: {batchSize: 1}
+}));
+assert.eq(res.cursor.firstBatch[0].meta, doc2.meta);
+
+st.stop();
