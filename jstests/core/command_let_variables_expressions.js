dd14ffadf7493a875aee50d6e9aec57c7bc0ff0b
HanaPearlman <hana.pearlman@mongodb.com>
2024-04-30 17:10:30 -0400

    SERVER-90013: Do not use jsTestName in command_let_variables_expressions.js (#21686)
    GitOrigin-RevId: be8f410d0ccbd1416b6e70a43b9fe2dd82cfbc01

--- jstests/core/command_let_variables_expressions.js
+++ jstests/core/command_let_variables_expressions.js
@@ -8,7 +8,7 @@
 // ]
 import {assertArrayEq} from "jstests/aggregation/extras/utils.js";
 
-const coll = db.getCollection(jsTestName());
+const coll = db.getCollection('command_let_variables_expressions');
 
 function setupColl() {
     coll.drop();
