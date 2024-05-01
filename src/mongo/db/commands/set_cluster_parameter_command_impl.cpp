ddf17d229128f4de24ed5fbe0562ad1143f70c5d
Gabriel Marks <gabriel.marks@mongodb.com>
2024-05-01 17:35:43 -0400

    SERVER-89954 Move setClusterParameter failpoint inside FixedFCVRegion (#21694)
    GitOrigin-RevId: 7051b50271d5737e97e44447a680b5b7efb500d5

--- src/mongo/db/commands/set_cluster_parameter_command_impl.cpp
+++ src/mongo/db/commands/set_cluster_parameter_command_impl.cpp
@@ -93,8 +93,11 @@ void setClusterParameterImplShard(OperationContext* opCtx,
             "setClusterParameter can only run on mongos in sharded clusters",
             (serverGlobalParams.clusterRole.has(ClusterRole::None)));
 
+    // setClusterParameter is serialized against setFeatureCompatibilityVersion.
+    FixedFCVRegion fcvRegion(opCtx);
+
     if (!feature_flags::gFeatureFlagAuditConfigClusterParameter.isEnabled(
-            serverGlobalParams.featureCompatibility.acquireFCVSnapshot())) {
+            fcvRegion->acquireFCVSnapshot())) {
         uassert(ErrorCodes::IllegalOperation,
                 str::stream() << SetClusterParameter::kCommandName
                               << " cannot be run on standalones",
@@ -102,9 +105,6 @@ void setClusterParameterImplShard(OperationContext* opCtx,
     }
 
     hangInSetClusterParameterFailPointCheck(request);
-
-    // setClusterParameter is serialized against setFeatureCompatibilityVersion.
-    FixedFCVRegion fcvRegion(opCtx);
 
     std::unique_ptr<ServerParameterService> parameterService =
         std::make_unique<ClusterParameterService>();
