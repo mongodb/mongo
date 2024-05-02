00bc3dcecd8d0bf4ea2ad629650162cc8807d6df
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2024-05-02 10:43:50 +0300

    SERVER-85446: Add retry on network error to config fetch in multiversionconstants.py (#21684)
    GitOrigin-RevId: 842370a25ca2a9400ed337487aecd8d944da02eb

--- buildscripts/resmokelib/multiversionconstants.py
+++ buildscripts/resmokelib/multiversionconstants.py
@@ -2,7 +2,9 @@
 import os
 import shutil
 from subprocess import DEVNULL, STDOUT, CalledProcessError, call, check_output
+import http
 import requests
+from retry import retry
 
 import structlog
 
@@ -17,7 +19,7 @@ LAST_CONTINUOUS = "last_continuous"
 # We use the "releases.yml" file from "master" because it is guaranteed to be up-to-date
 # with the latest EOL versions. If a "last-continuous" version is EOL, we don't include
 # it in the multiversion config and therefore don't test against it.
-MASTER_RELEASES_FILE = "https://raw.githubusercontent.com/mongodb/mongo/master/src/mongo/util/version/releases.yml"
+MASTER_RELEASES_REMOTE_FILE = "https://raw.githubusercontent.com/mongodb/mongo/master/src/mongo/util/version/releases.yml"
 
 LOGGER = structlog.getLogger(__name__)
 
@@ -36,11 +38,16 @@ def generate_mongo_version_file():
         mongo_version_fh.write("mongo_version: " + res)
 
 
+@retry(tries=5, delay=3)
 def generate_releases_file():
     """Generate the releases constants file."""
     # Copy the 'releases.yml' file from the source tree.
     with open(RELEASES_YAML, "wb") as file:
-        file.write(requests.get(MASTER_RELEASES_FILE).content)
+        response = requests.get(MASTER_RELEASES_REMOTE_FILE)
+        if response.status_code != http.HTTPStatus.OK:
+            raise RuntimeError("Http response for releases yml file was not 200 but was " +
+                               response.status_code)
+        file.write(response.content)
 
 
 def in_git_root_dir():
