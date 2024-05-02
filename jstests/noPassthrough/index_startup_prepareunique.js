6fc1f2205614bd68ff62bf1abc6603bc6eb7230d
Gavin Peters <gavin.peters@mongodb.com>
2024-05-02 12:54:01 -0400

    SERVER-87373 Skip crashing prepareUnique FCV check on startup (#20922)
    GitOrigin-RevId: 784dda22ff1038ead4eaf09b44c8105f63f6cc52

---
+++ jstests/noPassthrough/index_startup_prepareunique.js
@@ -0,0 +1,23 @@
+/**
+ * Checks that a prepareUnique index does not block startup.
+ */
+(function() {
+'use strict';
+
+const rst = new ReplSetTest({nodes: 1});
+rst.startSet();
+rst.initiate();
+
+let primary = rst.getPrimary();
+assert.commandWorked(primary.adminCommand({getParameter: 1, featureFlagCollModIndexUnique: 1}));
+assert.commandWorked(primary.getDB("admin").system.version.createIndex({"version": 1}));
+assert.commandWorked(primary.getDB("admin").runCommand(
+    {collMod: "system.version", index: {keyPattern: {version: 1}, prepareUnique: true}}));
+
+// Restarting the node after prepareUnique: true should succeed.
+rst.restart(primary);
+rst.waitForPrimary();
+primary = rst.getPrimary();
+
+rst.stopSet();
+})();
--- src/mongo/db/index/index_descriptor.cpp
+++ src/mongo/db/index/index_descriptor.cpp
@@ -175,10 +175,6 @@ IndexDescriptor::SharedState::SharedState(const std::string& accessMethodName, B
     }
 
     if (BSONElement prepareUniqueElement = _infoObj[kPrepareUniqueFieldName]) {
-        uassert(ErrorCodes::InvalidOptions,
-                "Index does not support the 'prepareUnique' field",
-                feature_flags::gCollModIndexUnique.isEnabled(
-                    serverGlobalParams.featureCompatibility.acquireFCVSnapshot()));
         _prepareUnique = prepareUniqueElement.trueValue();
     }
 
