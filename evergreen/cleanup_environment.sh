1fe27776c3bb42d3938868836bad209a08e4003d
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2024-05-06 19:32:41 +0300

    SERVER-90187 Cleanup multiversion binaries before running resmoke tests (#21872)
    GitOrigin-RevId: 01d863df83ff1e43337b2f2304581445968e5e03

--- evergreen/cleanup_environment.sh
+++ evergreen/cleanup_environment.sh
@@ -1,4 +1,13 @@
 set -o verbose
 
-rm -rf /data/db/* mongo-diskstats* mongo-*.tgz ~/.aws ~/.boto venv
+rm -rf \
+  /data/db/* \
+  mongo-diskstats* \
+  mongo-*.tgz \
+  ~/.aws \
+  ~/.boto \
+  venv \
+  /data/install \
+  /data/multiversion
+
 exit 0
