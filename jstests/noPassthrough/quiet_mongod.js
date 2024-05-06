1aa1c3c513d32d7fa9420d395079a9ff4fa0fba9
James Bronsted <32047428+jpbronsted@users.noreply.github.com>
2024-05-06 13:15:09 -0400

    SERVER-90180 wait long enough to be able to see the log line in quiet_mongod.js (#21857)
    GitOrigin-RevId: 0ce6552ba68d30cf7070d15851f286940f64148f

--- jstests/noPassthrough/quiet_mongod.js
+++ jstests/noPassthrough/quiet_mongod.js
@@ -1,29 +1,49 @@
+/**
+ * Verifies that client metadata is not logged when in quiet mode, and is logged otherwise.
+ */
+
 (function() {
 'use strict';
 
-const regexMatch = /\"NETWORK.+51800.+client metadata\"/;
+const dbName = jsTestName();
+const firstCommandLogLineRegex = /\"NETWORK.+6788700.+Received first command/;
+const clientMetadataLogLineRegex = /\"NETWORK.+51800.+client metadata\"/;
 
 // Test that a normal mongod has client metadata.
 {
-    clearRawMongoProgramOutput();
     const conn = MongoRunner.runMongod();
 
+    // Issue a command and wait for a log line acknowledging it to be sure any client metadata log
+    // lines have been flushed.
+    conn.getDB(dbName).runCommand({ping: 1});
+    assert.soon(() => rawMongoProgramOutput().match(firstCommandLogLineRegex),
+                "did not see log line acknowledging first command");
+
     const mongoOutput = rawMongoProgramOutput();
-    assert.gte(mongoOutput.match(regexMatch).length, 1);
-    clearRawMongoProgramOutput();  // Clears output for next logging.
+    assert.gte(mongoOutput.match(clientMetadataLogLineRegex).length,
+               1,
+               "did not see client metadata log line while not in quiet mode");
 
     MongoRunner.stopMongod(conn);
+    clearRawMongoProgramOutput();  // Clears output for next logging.
 }
 
 // Test that a quiet mongod does not output client metadata.
 {
-    clearRawMongoProgramOutput();
     const conn = MongoRunner.runMongod({quiet: ''});
 
+    // Issue a command and wait for a log line acknowledging it to be sure any client metadata log
+    // lines have been flushed.
+    conn.getDB(dbName).runCommand({ping: 1});
+    assert.soon(() => rawMongoProgramOutput().match(firstCommandLogLineRegex),
+                "did not see log line acknowledging first command");
+
     const mongoOutput = rawMongoProgramOutput();
-    assert.eq(mongoOutput.match(regexMatch), null);
-    clearRawMongoProgramOutput();  // Clears output for next logging.
+    assert.eq(mongoOutput.match(clientMetadataLogLineRegex),
+              null,
+              "saw client metadata log line while in quiet mode");
 
     MongoRunner.stopMongod(conn);
+    clearRawMongoProgramOutput();  // Clears output for next logging.
 }
 })();
