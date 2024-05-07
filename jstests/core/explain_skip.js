823ccb4a373743021518578c8b374d2f2c975ddd
Adityavardhan Agrawal <adi.agrawal@mongodb.com>
2024-05-07 20:04:55 +0000

    SERVER-90170 Avoid using jsTestName in explain_skip.js (#21905)
    (cherry picked from commit 89968d6577a46489d2ecf42f9f907c42ed5042c9)
    
    GitOrigin-RevId: 77bd4b5c47db37b5213716b98beacc6a59c52e3a

--- jstests/core/explain_skip.js
+++ jstests/core/explain_skip.js
@@ -8,7 +8,7 @@
  */
 load("jstests/libs/analyze_plan.js");  // For getWinningPlan.
 
-const coll = db[jsTestName()];
+var coll = db.explain_skip;
 coll.drop();
 
 assert.commandWorked(coll.insert({a: 1, b: 1}));
@@ -21,8 +21,8 @@ assert.commandWorked(coll.insert({a: 2, b: 3}));
 assert.commandWorked(coll.createIndex({a: 1}));
 assert.commandWorked(coll.createIndex({b: 1}));
 
-const explain = coll.find({a: 1, b: 1}).sort({_id: 1}).skip(5).explain();
-const winningPlan = getWinningPlan(explain.queryPlanner);
-const skipStage = getPlanStage(winningPlan, "SKIP");
+var explain = coll.find({a: 1, b: 1}).sort({_id: 1}).skip(5).explain();
+var winningPlan = getWinningPlan(explain.queryPlanner);
+var skipStage = getPlanStage(winningPlan, "SKIP");
 assert.neq(null, skipStage, explain);
 assert.eq(5, skipStage.skipAmount, explain);
