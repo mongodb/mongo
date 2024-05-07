12c288f153900b9df8b394a26982cb35feba48f1
Enrico Golfieri <enrico.golfieri@mongodb.com>
2024-05-07 17:13:59 +0200

    SERVER-87787 Adapt timeseries_multiple_mongos.js to unsplittable timeseries collection (#21894)
    GitOrigin-RevId: a6dd1dfebbbb509a27ff9f8493cf7599b96d2d9d

--- jstests/sharding/timeseries_multiple_mongos.js
+++ jstests/sharding/timeseries_multiple_mongos.js
@@ -8,8 +8,6 @@
  */
 
 import {TimeseriesTest} from "jstests/core/timeseries/libs/timeseries.js";
-import {FeatureFlagUtil} from "jstests/libs/feature_flag_util.js";
-import {ShardVersioningUtil} from "jstests/sharding/libs/shard_versioning_util.js";
 
 Random.setRandomSeed();
 
@@ -24,46 +22,15 @@ const testTimestamp = ISODate();
 const st = new ShardingTest({mongos: 2, shards: 2, rs: {nodes: 2}});
 const mongos0 = st.s0.getDB(dbName);
 const mongos1 = st.s1.getDB(dbName);
-const shard0DB = st.shard0.getDB(dbName);
-const shard1DB = st.shard1.getDB(dbName);
 
 // Databases and collections.
 assert.commandWorked(mongos0.adminCommand({enableSharding: dbName}));
-
-// Helpers.
-let currentId = 0;
-function generateId() {
-    return currentId++;
-}
-
-// TODO SERVER-87797 remove completely the unsharded field. The test starts by creating an
-// unsharded collection with mongo1 and sharding it with mongo0. The command specified in the test
-// case is then run with mongos1 (stale). The sharded collection is then dropped and re-created as
-// unsharded from mongos0 and re-attempted the command from mongos1. The test searches in
-// system.profile that the command run 2 times (1 per shard) for the sharded case and 1 time for the
-// unsharded case. The test is testing specifically the interaction within the sharding protocol
-// which is way beyond the scope of the test. Rewrite this test to check the command simply works on
-// the stale mongos
-const isTrackUnshardedEnabled = FeatureFlagUtil.isPresentAndEnabled(
-                                    st.s.getDB('admin'), "TrackUnshardedCollectionsUponCreation") ||
-    FeatureFlagUtil.isPresentAndEnabled(st.s.getDB('admin'),
-                                        "TrackUnshardedCollectionsUponMoveCollection");
 
 /**
  * The test runs drop, create and shardCollection commands using mongos0, then validates the given
  * command by running against mongos1 with stale config.
  */
-function runTest({shardKey, cmdObj, numProfilerEntries}) {
-    if (isTrackUnshardedEnabled) {
-        return;
-    }
-
-    const isInsert = cmdObj["insert"] !== undefined;
-    const isDelete = cmdObj["delete"] !== undefined;
-    const isUpdate = cmdObj["update"] !== undefined;
-    const isCollMod = cmdObj["collMod"] !== undefined;
-    const isDropIndex = cmdObj["dropIndexes"] !== undefined;
-    const cmdCollName = cmdObj[Object.keys(cmdObj)[0]];
+function runTest({shardKey, cmdObj}) {
     const shardKeyHasMetaField = shardKey[metaField] !== undefined;
 
     // Insert some dummy data using 'mongos1' as the router, so that the cache is initialized on the
@@ -93,104 +60,7 @@ function runTest({shardKey, cmdObj, numProfilerEntries}) {
         _waitForDelete: true
     }));
 
-    // Validate the command by running against 'mongos1' as the router.
-    function validateCommand(cmdCollName, numEntries, unVersioned) {
-        // Restart profiler.
-        for (let shardDB of [shard0DB, shard1DB]) {
-            shardDB.setProfilingLevel(0);
-            shardDB.system.profile.drop();
-            shardDB.setProfilingLevel(2);
-        }
-
-        const res = mongos1.runCommand(cmdObj);
-        if (numEntries > 0) {
-            assert.commandWorked(res);
-        } else {
-            assert.commandFailed(res);
-        }
-
-        const queryField = `command.${Object.keys(cmdObj)[0]}`;
-        let filter = {
-            [queryField]: cmdCollName,
-            "$or": [
-                {
-                    "$and": [
-                        {"command.shardVersion.0": {"$exists": true}},
-                        {
-                            "command.shardVersion.0":
-                                {$ne: ShardVersioningUtil.kIgnoredShardVersion.v}
-                        },
-                    ]
-                },
-                {
-                    "$and": [
-                        {"command.shardVersion.v": {"$exists": true}},
-                        {
-                            "command.shardVersion.v":
-                                {$ne: ShardVersioningUtil.kIgnoredShardVersion.v}
-                        },
-                    ]
-                },
-            ]
-        };
-
-        if (isUpdate) {
-            // We currently do not log 'shardVersion' for updates. See SERVER-60354 for details.
-            filter = {
-                $or: [
-                    {op: 'update'},
-                    {op: 'bulkWrite', "command.update": {$exists: true}},
-                ],
-                "ns": `${dbName}.${cmdCollName}`,
-                "ok": {$ne: 0}
-            };
-        } else if (isDelete) {
-            filter = {
-                $or: [
-                    {op: 'remove'},
-                    {op: 'bulkWrite', "command.delete": {$exists: true}},
-                ],
-                "ns": `${dbName}.${cmdCollName}`,
-                "ok": {$ne: 0}
-            };
-        } else if (isCollMod) {
-            const command = unVersioned ? "_shardsvrCollMod" : "_shardsvrCollModParticipant";
-            filter = {[`command.${command}`]: cmdCollName, "ok": {$ne: 0}};
-        } else if (unVersioned && !isCollMod) {
-            filter = {
-                [queryField]: cmdCollName,
-                "$or": [
-                    {"command.shardVersion.0": ShardVersioningUtil.kIgnoredShardVersion.v},
-                    {"command.shardVersion.v": ShardVersioningUtil.kIgnoredShardVersion.v},
-                ]
-            };
-        }
-
-        if (isInsert) {
-            filter = {
-                $or: [
-                    filter,
-                    {op: 'bulkWrite', "command.insert": {$exists: true}},
-                ]
-            }
-        }
-
-        // Filter out the profiler entries with $indexStats pipeline stage, as the
-        // PeriodicShardedIndexConsistencyChecker can issue an aggregate command with
-        // '[$indexStats: {}]' pipeline periodically. We don't want these entries to be accounted
-        // here.
-        filter["command.pipeline.0.$indexStats"] = {$exists: false};
-
-        const shard0Entries = shard0DB.system.profile.find(filter).toArray();
-        const shard1Entries = shard1DB.system.profile.find(filter).toArray();
-        assert.eq(shard0Entries.length + shard1Entries.length,
-                  numEntries,
-                  {shard0Entries: shard0Entries, shard1Entries: shard1Entries});
-    }
-
-    // The update command is always logged as being on the user-provided namespace.
-    validateCommand((isUpdate || isDelete || isCollMod) ? cmdCollName : bucketsCollName,
-                    numProfilerEntries.sharded);
+    assert.commandWorked(mongos1.runCommand(cmdObj))
 
     // Insert dummy data so that the 'mongos1' sees the collection as sharded.
     assert.commandWorked(mongos1.getCollection(collName).insert({[timeField]: ISODate()}));
@@ -200,10 +70,7 @@ function runTest({shardKey, cmdObj, numProfilerEntries}) {
     assert.commandWorked(mongos0.createCollection(
         collName, {timeseries: {timeField: timeField, metaField: metaField}}));
 
-    // When unsharded, the command should be run against the user requested namespace, except for
-    // dropIndex.
-    validateCommand(
-        isDropIndex ? bucketsCollName : cmdCollName, numProfilerEntries.unsharded, true);
+    assert.commandWorked(mongos1.runCommand(cmdObj))
 }
 
 /**
@@ -214,54 +81,34 @@ runTest({
     cmdObj: {
         createIndexes: collName,
         indexes: [{key: {[timeField]: 1}, name: "index_on_time"}],
-    },
-    numProfilerEntries: {sharded: 2, unsharded: 1}
+    }
 });
 
-runTest({
-    shardKey: {[metaField]: 1},
-    cmdObj: {listIndexes: collName},
-    numProfilerEntries: {sharded: 1, unsharded: 1}
-});
+runTest({shardKey: {[metaField]: 1}, cmdObj: {listIndexes: collName}});
 
 runTest({
     shardKey: {[metaField]: 1},
     cmdObj: {
         dropIndexes: collName,
         index: "*",
-    },
-    numProfilerEntries: {sharded: 2, unsharded: 1}
+    }
+});
+
+runTest({shardKey: {[metaField]: 1}, cmdObj: {collMod: collName, expireAfterSeconds: 3600}});
+
+runTest({
+    shardKey: {[metaField]: 1},
+    cmdObj: {insert: collName, documents: [{[timeField]: ISODate()}]}
 });
 
 runTest({
     shardKey: {[metaField]: 1},
-    cmdObj: {collMod: collName, expireAfterSeconds: 3600},
-    numProfilerEntries: {sharded: 2, unsharded: 1}
+    cmdObj: {insert: collName, documents: [{[timeField]: ISODate()}]}
 });
 
-runTest({
-    shardKey: {[metaField]: 1},
-    cmdObj: {insert: collName, documents: [{[timeField]: ISODate()}]},
-    numProfilerEntries: {sharded: 1, unsharded: 1}
-});
+runTest({shardKey: {[metaField]: 1}, cmdObj: {aggregate: collName, pipeline: [], cursor: {}}});
 
-runTest({
-    shardKey: {[metaField]: 1},
-    cmdObj: {insert: collName, documents: [{[timeField]: ISODate()}]},
-    numProfilerEntries: {sharded: 1, unsharded: 1}
-});
-
-runTest({
-    shardKey: {[metaField]: 1},
-    cmdObj: {aggregate: collName, pipeline: [], cursor: {}},
-    numProfilerEntries: {sharded: 2, unsharded: 1}
-});
-
-runTest({
-    shardKey: {[metaField]: 1},
-    cmdObj: {collStats: collName},
-    numProfilerEntries: {sharded: 2, unsharded: 1}
-});
+runTest({shardKey: {[metaField]: 1}, cmdObj: {collStats: collName}});
 
 /**
  * On system.buckets namespace
@@ -271,36 +118,23 @@ runTest({
     cmdObj: {
         createIndexes: bucketsCollName,
         indexes: [{key: {[timeField]: 1}, name: "index_on_time"}],
-    },
-    numProfilerEntries: {sharded: 2, unsharded: 1},
+    }
 });
 
-runTest({
-    shardKey: {[metaField]: 1},
-    cmdObj: {listIndexes: bucketsCollName},
-    numProfilerEntries: {sharded: 1, unsharded: 1},
-});
+runTest({shardKey: {[metaField]: 1}, cmdObj: {listIndexes: bucketsCollName}});
 
 runTest({
     shardKey: {[metaField]: 1},
     cmdObj: {
         dropIndexes: bucketsCollName,
         index: "*",
-    },
-    numProfilerEntries: {sharded: 2, unsharded: 1},
+    }
 });
 
-runTest({
-    shardKey: {[metaField]: 1},
-    cmdObj: {collMod: bucketsCollName, expireAfterSeconds: 3600},
-    numProfilerEntries: {sharded: 2, unsharded: 1},
-});
+runTest({shardKey: {[metaField]: 1}, cmdObj: {collMod: bucketsCollName, expireAfterSeconds: 3600}});
 
-runTest({
-    shardKey: {[metaField]: 1},
-    cmdObj: {aggregate: bucketsCollName, pipeline: [], cursor: {}},
-    numProfilerEntries: {sharded: 2, unsharded: 1},
-});
+runTest(
+    {shardKey: {[metaField]: 1}, cmdObj: {aggregate: bucketsCollName, pipeline: [], cursor: {}}});
 
 runTest({
     shardKey: {[metaField]: 1},
@@ -315,8 +149,7 @@ runTest({
             },
             data: {[timeField]: {0: testTimestamp}},
         }]
-    },
-    numProfilerEntries: {sharded: 1, unsharded: 1},
+    }
 });
 
 // Tests for updates.
@@ -329,8 +162,7 @@ runTest({
             u: {$inc: {[metaField + ".b"]: -1}},
             multi: true,
         }]
-    },
-    numProfilerEntries: {sharded: 1, unsharded: 1},
+    }
 });
 
 runTest({
@@ -342,8 +174,7 @@ runTest({
             u: {$inc: {["meta.b"]: -1}},
             multi: true,
         }]
-    },
-    numProfilerEntries: {sharded: 1, unsharded: 1},
+    }
 });
 
 // Tests for deletes.
@@ -355,8 +186,7 @@ runTest({
             q: {[metaField]: 0},
             limit: 0,
         }],
-    },
-    numProfilerEntries: {sharded: 1, unsharded: 1},
+    }
 });
 
 runTest({
@@ -367,8 +197,7 @@ runTest({
             q: {meta: 0},
             limit: 0,
         }],
-    },
-    numProfilerEntries: {sharded: 1, unsharded: 1},
+    }
 });
 
 st.stop();
