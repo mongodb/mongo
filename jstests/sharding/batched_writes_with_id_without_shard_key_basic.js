a3681113e92078a12ba69bbfd98f6bafda71697e
nandinibhartiyaMDB <104035932+nandinibhartiyaMDB@users.noreply.github.com>
2024-05-07 16:03:03 -0500

    SERVER-89571: Fix usage of shouldStopCurrentRound (#21510)
    GitOrigin-RevId: 74c276e8a0222698870a5e560a5685049cd8f5ce

--- jstests/sharding/batched_writes_with_id_without_shard_key_basic.js
+++ jstests/sharding/batched_writes_with_id_without_shard_key_basic.js
@@ -19,44 +19,52 @@ CreateShardedCollectionUtil.shardCollectionWithChunks(coll, {x: 1}, [
     {min: {x: -100}, max: {x: 0}, shard: st.shard0.shardName},
     {min: {x: 0}, max: {x: MaxKey}, shard: st.shard1.shardName},
 ]);
+const performOps =
+    function(ordered, numOps) {
+    jsTest.log("Perform write with ordered: " + ordered);
+    // Write two documents.
+    assert.commandWorked(coll.insert({x: -1, _id: -1}));
+    assert.commandWorked(coll.insert({x: 1, _id: 1}));
 
-// Write two documents.
-assert.commandWorked(coll.insert({x: -1, _id: -1}));
-assert.commandWorked(coll.insert({x: 1, _id: 1}));
+    assert.commandWorked(
+        db.adminCommand({moveChunk: coll.getFullName(), find: {x: -1}, to: st.shard1.shardName}));
 
-assert.commandWorked(
-    db.adminCommand({moveChunk: coll.getFullName(), find: {x: -1}, to: st.shard1.shardName}));
+    // Test that retryable writes use broadcast protocol per PM-3190
+    let session = st.s.startSession({retryWrites: true});
+    const lsid = session.getSessionId();
+    let ret = assert.commandWorked(
+        db.getSiblingDB(db.getName()).getCollection(coll.getName()).runCommand("update", {
+            updates: [
+                {q: {_id: -1}, u: {$inc: {counter: 1}}},
+                {q: {_id: 1}, u: {$inc: {counter: 1}}},
+            ],
+            lsid: lsid,
+            txnNumber: NumberLong(5),
+            ordered: ordered,
+        }));
+    assert.eq(ret.n, 2);
 
-// Test that retryable writes use broadcast protocol per PM-3190
-let session = st.s.startSession({retryWrites: true});
-const lsid = session.getSessionId();
-let ret = assert.commandWorked(
-    db.getSiblingDB(db.getName()).getCollection(coll.getName()).runCommand("update", {
-        updates: [
-            {q: {_id: -1}, u: {$inc: {counter: 1}}},
-            {q: {_id: 1}, u: {$inc: {counter: 1}}},
-        ],
-        lsid: lsid,
-        txnNumber: NumberLong(5),
-        ordered: false,
-    }));
-assert.eq(ret.n, 2);
+    ret = assert.commandWorked(
+        db.getSiblingDB(db.getName()).getCollection(coll.getName()).runCommand("delete", {
+            deletes: [
+                {q: {_id: -1}, limit: 1},
+                {q: {_id: 1}, limit: 1},
+            ],
+            lsid: lsid,
+            txnNumber: NumberLong(6),
+            ordered: ordered,
+        }));
+    assert.eq(ret.n, 2);
 
-ret = assert.commandWorked(
-    db.getSiblingDB(db.getName()).getCollection(coll.getName()).runCommand("delete", {
-        deletes: [
-            {q: {_id: -1}, limit: 1},
-            {q: {_id: 1}, limit: 1},
-        ],
-        lsid: lsid,
-        txnNumber: NumberLong(6),
-        ordered: false,
-    }));
-assert.eq(ret.n, 2);
+    let mongosServerStatus =
+        assert.commandWorked(mongos.getDB(jsTestName()).adminCommand({serverStatus: 1}));
+    assert.eq(numOps, mongosServerStatus.metrics.query.updateOneNonTargetedShardedCount);
+    assert.eq(numOps, mongosServerStatus.metrics.query.deleteOneWithoutShardKeyWithIdCount);
+    session.endSession();
+}
 
-let mongosServerStatus =
-    assert.commandWorked(mongos.getDB(jsTestName()).adminCommand({serverStatus: 1}));
-assert.eq(2, mongosServerStatus.metrics.query.updateOneNonTargetedShardedCount);
-assert.eq(2, mongosServerStatus.metrics.query.deleteOneWithoutShardKeyWithIdCount);
-session.endSession();
+// Test batched ops with ordered: true and ordered: false.
+performOps(true, 2);
+performOps(false, 4);
+
 st.stop();
