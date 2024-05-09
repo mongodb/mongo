ce8f85fc0eb9ac696623fd340b2acb58345a6f84
seanzimm <102551488+seanzimm@users.noreply.github.com>
2024-05-09 14:23:38 -0400

    SERVER-87428: Add testing for transactions around magic restore (#21790)
    GitOrigin-RevId: 814be5532127a2a80bfdac547e437c7af0c2c7e6

--- src/mongo/db/storage/wiredtiger/wiredtiger_util.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_util.cpp
@@ -1015,9 +1015,11 @@ bool WiredTigerUtil::useTableLogging(const NamespaceString& nss) {
 
     // We only turn off logging in the case of:
     // 1) Replication is enabled (the typical deployment), or
-    // 2) We're running as a standalone with recoverFromOplogAsStandalone=true
+    // 2) We're running as a standalone with recoverFromOplogAsStandalone=true or
+    // 3) We're running in magic restore mode
     const bool journalWritesBecauseStandalone = !getGlobalReplSettings().isReplSet() &&
-        !repl::ReplSettings::shouldRecoverFromOplogAsStandalone();
+        !repl::ReplSettings::shouldRecoverFromOplogAsStandalone() &&
+        !storageGlobalParams.magicRestore;
     if (journalWritesBecauseStandalone) {
         return true;
     }
