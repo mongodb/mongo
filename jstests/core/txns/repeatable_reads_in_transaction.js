cdd13b9cb6ca1b63c05cd6dfa6dd0d289c4c024a
Tommaso Tocci <58224719+toto-dev@users.noreply.github.com>
2024-05-11 11:53:05 +0200

    SERVER-90364 Retry txns on transient errors in repeatable_reads_in_transaction.js (#22075)
    GitOrigin-RevId: e647063e1ad5028b6aa4891587618ae6f96ca04c

--- jstests/core/txns/repeatable_reads_in_transaction.js
+++ jstests/core/txns/repeatable_reads_in_transaction.js
@@ -8,9 +8,7 @@
 //   uses_snapshot_read_concern
 // ]
 // TODO (SERVER-39704): Remove the following load after SERVER-39704 is completed
-import {
-    retryOnceOnTransientAndRestartTxnOnMongos
-} from "jstests/libs/auto_retry_transaction_in_sharding.js";
+import {withRetryOnTransientTxnError} from "jstests/libs/auto_retry_transaction_in_sharding.js";
 
 const dbName = "test";
 const collName = "repeatable_reads_in_transaction";
@@ -33,51 +31,58 @@ const session2 = testDB.getMongo().startSession(sessionOptions);
 const session2Db = session2.getDatabase(dbName);
 const session2Coll = session2Db.getCollection(collName);
 
-jsTest.log("Prepopulate the collection.");
-assert.commandWorked(
-    testColl.insert([{_id: 0}, {_id: 1}, {_id: 2}], {writeConcern: {w: "majority"}}));
-
 // Create a constant array of documents we expect to be returned during a read-only transaction.
 // The value should not change since external changes should not be visible within this
 // transaction.
 const expectedDocs = [{_id: 0}, {_id: 1}, {_id: 2}];
 
-jsTestLog("Start a read-only transaction on the first session.");
-session.startTransaction({writeConcern: {w: "majority"}});
+withRetryOnTransientTxnError(
+    () => {
+        jsTest.log("Prepopulate the collection.");
+        assert.commandWorked(testColl.insert(expectedDocs, {writeConcern: {w: "majority"}}));
 
-assert.sameMembers(expectedDocs, sessionColl.find().toArray());
+        jsTestLog("Start a read-only transaction on the first session.");
+        session.startTransaction({writeConcern: {w: "majority"}});
 
-jsTestLog("Start a transaction on the second session that modifies the same collection.");
-session2.startTransaction({readConcern: {level: "snapshot"}, writeConcern: {w: "majority"}});
+        assert.sameMembers(expectedDocs, sessionColl.find().toArray());
 
-retryOnceOnTransientAndRestartTxnOnMongos(session2, () => {
-    assert.commandWorked(session2Coll.insert({_id: 3}));
-    assert.commandWorked(session2Coll.update({_id: 1}, {$set: {a: 1}}));
-    assert.commandWorked(session2Coll.deleteOne({_id: 2}));
-}, {readConcern: {level: "snapshot"}, writeConcern: {w: "majority"}});
+        jsTestLog("Start a transaction on the second session that modifies the same collection.");
+        session2.startTransaction(
+            {readConcern: {level: "snapshot"}, writeConcern: {w: "majority"}});
 
-jsTestLog(
-    "Continue reading in the first transaction. Changes from the second transaction should not be visible.");
+        assert.commandWorked(session2Coll.insert({_id: 3}));
+        assert.commandWorked(session2Coll.update({_id: 1}, {$set: {a: 1}}));
+        assert.commandWorked(session2Coll.deleteOne({_id: 2}));
 
-assert.sameMembers(expectedDocs, sessionColl.find().toArray());
+        jsTestLog(
+            "Continue reading in the first transaction. Changes from the second transaction should not be visible.");
 
-jsTestLog("Committing the second transaction.");
-assert.commandWorked(session2.commitTransaction_forTesting());
+        assert.sameMembers(expectedDocs, sessionColl.find().toArray());
 
-jsTestLog(
-    "Committed changes from the second transaction should still not be visible to the first.");
+        jsTestLog("Committing the second transaction.");
+        assert.commandWorked(session2.commitTransaction_forTesting());
 
-assert.sameMembers(expectedDocs, sessionColl.find().toArray());
+        jsTestLog(
+            "Committed changes from the second transaction should still not be visible to the first.");
 
-jsTestLog(
-    "Writes that occur outside of a transaction should not be visible to a read only transaction.");
+        assert.sameMembers(expectedDocs, sessionColl.find().toArray());
 
-assert.commandWorked(testColl.insert({_id: 4}, {writeConcern: {w: "majority"}}));
+        jsTestLog(
+            "Writes that occur outside of a transaction should not be visible to a read only transaction.");
 
-assert.sameMembers(expectedDocs, sessionColl.find().toArray());
+        assert.commandWorked(testColl.insert({_id: 4}, {writeConcern: {w: "majority"}}));
 
-jsTestLog("Committing first transaction.");
-assert.commandWorked(session.commitTransaction_forTesting());
+        assert.sameMembers(expectedDocs, sessionColl.find().toArray());
+
+        jsTestLog("Committing first transaction.");
+        assert.commandWorked(session.commitTransaction_forTesting());
+    },
+    () => {
+        session.abortTransaction_forTesting();
+        session2.abortTransaction_forTesting();
+        // Clear all documents inside collection
+        testColl.remove({});
+    });
 
 // Make sure the correct documents exist after committing the second transaction.
 assert.sameMembers([{_id: 0}, {_id: 1, a: 1}, {_id: 3}, {_id: 4}], sessionColl.find().toArray());
