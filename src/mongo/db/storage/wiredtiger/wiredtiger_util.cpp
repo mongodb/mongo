7cb93df5b75544aefa4c3aa1d72bde336fd82eb4
Ali Mir <ali.mir@mongodb.com>
2024-05-16 01:16:44 +0600

    SERVER-90535 Remove magic restore check in wiredtiger_util.cpp (#22219)
    GitOrigin-RevId: 46059cafd99cba12bca69767c0482dc3e89c7809

--- src/mongo/db/storage/wiredtiger/wiredtiger_util.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_util.cpp
@@ -1015,11 +1015,9 @@ bool WiredTigerUtil::useTableLogging(const NamespaceString& nss) {
 
     // We only turn off logging in the case of:
     // 1) Replication is enabled (the typical deployment), or
-    // 2) We're running as a standalone with recoverFromOplogAsStandalone=true or
-    // 3) We're running in magic restore mode
+    // 2) We're running as a standalone with recoverFromOplogAsStandalone=true
     const bool journalWritesBecauseStandalone = !getGlobalReplSettings().isReplSet() &&
-        !repl::ReplSettings::shouldRecoverFromOplogAsStandalone() &&
-        !storageGlobalParams.magicRestore;
+        !repl::ReplSettings::shouldRecoverFromOplogAsStandalone();
     if (journalWritesBecauseStandalone) {
         return true;
     }
