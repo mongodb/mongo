4c7a9930bc47b0b061b90f6ec882652b9feadb47
Steve McClure <steve.mcclure@mongodb.com>
2024-05-16 17:59:09 -0400

    SERVER-90583: Add Ruff to bazel //:format target  (#22268)
    GitOrigin-RevId: c7bf1f40180c861f4418898fd34aa1097438456c

--- bazel/format/format.py
+++ bazel/format/format.py
@@ -5,12 +5,20 @@ import subprocess
 
 
 def run_prettier(prettier: pathlib.Path, check: bool) -> int:
+    command = [prettier, ".", "--check" if check else "--write"]
+    cmd_status = run_formatter(command, check)
+    return cmd_status
+
+
+def run_ruff(ruff: pathlib.Path, check: bool) -> int:
+    command = [ruff, "format"]
+    if check:
+        command.append("--check")
+    cmd_status = run_formatter(command, check)
+    return cmd_status
+
+def run_formatter(command: str, check: bool) -> int:
     try:
-        command = [prettier, "."]
-        if check:
-            command.append("--check")
-        else:
-            command.append("--write")
         print(f"Running command: '{command}'")
         subprocess.run(command, check=True)
     except subprocess.CalledProcessError:
@@ -37,14 +45,22 @@ def main() -> int:
                                      description='This script formats code in mongodb')
 
     parser.add_argument("--check", help="Run in check mode", default=False, action="store_true")
+    parser.add_argument("--ruff", help="Set the path to ruff", required=True,
+                        type=pathlib.Path)
     parser.add_argument("--prettier", help="Set the path to prettier", required=True,
                         type=pathlib.Path)
 
     args = parser.parse_args()
     prettier_path: pathlib.Path = args.prettier.resolve()
+    ruff_path: pathlib.Path = args.ruff.resolve()
 
     os.chdir(default_dir)
-    return run_prettier(prettier_path, args.check)
+
+    status = 0
+    status |= run_ruff(ruff_path, args.check)
+    status |= run_prettier(prettier_path, args.check)
+
+    return status
 
 
 if __name__ == "__main__":
