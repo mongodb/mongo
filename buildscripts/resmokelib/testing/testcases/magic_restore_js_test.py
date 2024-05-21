275639fdb14b129c6b104e62e2fa6430f51ea506
seanzimm <102551488+seanzimm@users.noreply.github.com>
2024-05-21 09:57:05 -0400

    SERVER-89647 Override drop and assert for magic restore testing (#22321)
    [evg
    patch](https://spruce.mongodb.com/version/663e5832b144dd0007172e5c/tasks?page=0&sorts=STATUS%3AASC%3BBASE_STATUS%3ADESC)
    
    This PR overrides drops and asserts to make sure we get more coverage
    for magic restore testing since the magic restore hook relies on data
    existing on the nodes to restore. We found before this PR we were
    getting lackluster coverage since most jstests drop their data when the
    test ends which leads to little data remaining in nodes when the hook
    runs.
    
    GitOrigin-RevId: 2423d1cfd2be22639b1439c3f550bb866d202178

---
+++ buildscripts/resmokelib/testing/testcases/magic_restore_js_test.py
@@ -0,0 +1,33 @@
+"""The unittest.TestCase to run tests in a try/catch block."""
+
+import hashlib
+import threading
+
+from buildscripts.resmokelib.testing.testcases import interface
+from buildscripts.resmokelib.testing.testcases import jsrunnerfile
+
+
+class MagicRestoreTestCase(jsrunnerfile.JSRunnerFileTestCase):
+    """A test to execute for running tests in a try/catch block."""
+
+    REGISTERED_NAME = "magic_restore_js_test"
+
+    def __init__(self, logger, js_filename, shell_executable=None, shell_options=None):
+        """Initialize the MagicRestoreTestCase."""
+        jsrunnerfile.JSRunnerFileTestCase.__init__(
+            self,
+            logger,
+            "MagicRestore Test",
+            js_filename,
+            test_runner_file="jstests/libs/magic_restore_passthrough_runner.js",
+            shell_executable=shell_executable,
+            shell_options=shell_options,
+        )
+
+    @property
+    def js_filename(self):
+        """Return the name of the test file."""
+        return self.test_name
+
+    def _populate_test_data(self, test_data):
+        test_data["jsTestFile"] = self.js_filename
