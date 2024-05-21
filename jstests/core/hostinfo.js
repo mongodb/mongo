cac85fe9975aca54c092390f82568f717023b1a8
James Bronsted <32047428+jpbronsted@users.noreply.github.com>
2024-05-21 18:51:17 -0400

    SERVER-90279 correct jstests/core/hostinfo.js to check against all disallowed values (#22065)
    (cherry picked from commit 9987e4d83bf507b534ebf990146f5f05a9f8cd81)
    
    GitOrigin-RevId: f2c236eeeb533c49f1ed76f06e64ab0397a16c0c

--- jstests/core/hostinfo.js
+++ jstests/core/hostinfo.js
@@ -1,56 +1,70 @@
+// Ensures the hostInfo command returns complete and consistent information. Does not evaluate
+// correctness of the information.
 // @tags: [
 //   # The test runs commands that are not allowed with security token: hostInfo.
 //   not_allowed_with_signed_security_token,
 // ]
 // SERVER-4615:  Ensure hostInfo() command returns expected results on each platform
 
-assert.commandWorked(db.hostInfo());
-var hostinfo = db.hostInfo();
+(function() {
+'use strict';
+
+function assertNoneOf(expr, values, msg) {
+    for (const value of values) {
+        // Use strict equality check so that we correctly evaluate `false` as distinct from `null`.
+        assert(expr !== value,
+               tojson(expr) + " should be none of " + tojson(values) + ", but is equivalent to " +
+                   tojson(value) + " : " + msg);
+    }
+}
+
+const hostinfo = assert.commandWorked(db.hostInfo());
 
 // test for os-specific fields
 if (hostinfo.os.type == "Windows") {
-    assert.neq(hostinfo.os.name, "" || null, "Missing Windows os name");
-    assert.neq(hostinfo.os.version, "" || null, "Missing Windows version");
+    assertNoneOf(hostinfo.os.name, ["", null], "Missing Windows os name");
+    assertNoneOf(hostinfo.os.version, ["", null], "Missing Windows version");
 
 } else if (hostinfo.os.type == "Linux") {
-    assert.neq(hostinfo.os.name, "" || null, "Missing Linux os/distro name");
-    assert.neq(hostinfo.os.version, "" || null, "Missing Lindows version");
+    assertNoneOf(hostinfo.os.name, ["", null], "Missing Linux os/distro name");
+    assertNoneOf(hostinfo.os.version, ["", null], "Missing Linux version");
 
 } else if (hostinfo.os.type == "Darwin") {
-    assert.neq(hostinfo.os.name, "" || null, "Missing Darwin os name");
-    assert.neq(hostinfo.os.version, "" || null, "Missing Darwin version");
+    assertNoneOf(hostinfo.os.name, ["", null], "Missing Darwin os name");
+    assertNoneOf(hostinfo.os.version, ["", null], "Missing Darwin version");
 
 } else if (hostinfo.os.type == "BSD") {
-    assert.neq(hostinfo.os.name, "" || null, "Missing FreeBSD os name");
-    assert.neq(hostinfo.os.version, "" || null, "Missing FreeBSD version");
+    assertNoneOf(hostinfo.os.name, ["", null], "Missing FreeBSD os name");
+    assertNoneOf(hostinfo.os.version, ["", null], "Missing FreeBSD version");
 }
 
 jsTest.log(hostinfo);
 // comment out this block for systems which have not implemented hostinfo.
 if (hostinfo.os.type != "") {
-    assert.neq(hostinfo.system.hostname, "" || null, "Missing Hostname");
-    assert.neq(hostinfo.system.currentTime, "" || null, "Missing Current Time");
-    assert.neq(hostinfo.system.cpuAddrSize, "" || null || 0, "Missing CPU Address Size");
-    assert.neq(hostinfo.system.memSizeMB, "" || null, "Missing Memory Size");
-    assert.neq(hostinfo.system.numCores, "" || null || 0, "Missing Number of Logical Cores");
+    assertNoneOf(hostinfo.system.hostname, ["", null], "Missing Hostname");
+    assertNoneOf(hostinfo.system.currentTime, ["", null], "Missing Current Time");
+    assertNoneOf(hostinfo.system.cpuAddrSize, ["", null, 0], "Missing CPU Address Size");
+    assertNoneOf(hostinfo.system.memSizeMB, ["", null], "Missing Memory Size");
+    assertNoneOf(hostinfo.system.numCores, ["", null, 0], "Missing Number of Logical Cores");
     // Check that numCoresAvailableToProcess != -1 as that indicates syscall failure.
-    assert.neq(hostinfo.system.numCoresAvailableToProcess,
-               "" || null || -1,
-               "Missing Number of Cores Available To Process");
-    assert.neq(
-        hostinfo.system.numPhysicalCores, "" || null || 0, "Missing Number of Physical Cores");
-    assert.neq(hostinfo.system.numCpuSockets, "" || null || 0, "Missing Number of CPU Sockets");
-    assert.neq(hostinfo.system.cpuArch, "" || null, "Missing CPU Architecture");
-    assert.neq(hostinfo.system.numaEnabled, "" || null, "Missing NUMA flag");
-    assert.neq(hostinfo.system.numNumaNodes, "" || null || 0, "Missing Number of NUMA Nodes");
+    assertNoneOf(hostinfo.system.numCoresAvailableToProcess,
+                 ["", null, -1],
+                 "Missing Number of Cores Available To Process");
+    assertNoneOf(
+        hostinfo.system.numPhysicalCores, ["", null, 0], "Missing Number of Physical Cores");
+    assertNoneOf(hostinfo.system.numCpuSockets, ["", null, 0], "Missing Number of CPU Sockets");
+    assertNoneOf(hostinfo.system.cpuArch, ["", null], "Missing CPU Architecture");
+    assertNoneOf(hostinfo.system.numaEnabled, ["", null], "Missing NUMA flag");
+    assertNoneOf(hostinfo.system.numNumaNodes, ["", null, 0], "Missing Number of NUMA Nodes");
 }
 
-var buildInfo = assert.commandWorked(db.runCommand({buildInfo: 1}));
+const buildInfo = assert.commandWorked(db.runCommand({buildInfo: 1}));
 if (buildInfo.buildEnvironment && buildInfo.buildEnvironment.target_arch) {
-    let targetArch = buildInfo.buildEnvironment.target_arch;
+    const targetArch = buildInfo.buildEnvironment.target_arch;
     if (targetArch == "i386")
         assert.eq(hostinfo.system.cpuAddrSize, 32);
     else
         assert.eq(hostinfo.system.cpuAddrSize, 64);
     assert.eq(hostinfo.system.cpuAddrSize, buildInfo.bits);
 }
+})();
