6cf744c8b61cfdafe6b3b49a4f6652fc8a40e17a
Matthew Russotto <matthew.russotto@mongodb.com>
2024-05-22 14:56:29 -0400

    SERVER-90701 Use force sync source candidate FP in catchup_ignores_ol… (#22412)
    GitOrigin-RevId: 7fea3d41428768bf8f74a195de08c912bce4d376

--- jstests/replsets/catchup_ignores_old_heartbeats.js
+++ jstests/replsets/catchup_ignores_old_heartbeats.js
@@ -1,6 +1,7 @@
 // Makes sure an old heartbeat that is being processed when primary catchup starts does not cause
 // primary catchup to think we're already caught up.
 import {configureFailPoint} from "jstests/libs/fail_point_util.js";
+import {syncFrom} from "jstests/replsets/rslib.js";
 
 var name = TestData.testName;
 var rst = new ReplSetTest({
@@ -31,8 +32,12 @@ assert.commandWorked(primary.adminCommand(
     {setDefaultRWConcern: 1, defaultWriteConcern: {w: 1}, writeConcern: {w: "majority"}}));
 
 assert(primary.host == rst.nodes[0].host);
-// Make us chain node 1 (the node which will become the new primary) from node 2.
-assert.commandWorked(rst.nodes[1].adminCommand({replSetSyncFrom: rst.nodes[2].host}));
+// Make us chain node 1 (the node which will become the new primary) from node 2.  Don't allow
+// node 1 to switch back.
+const forceSyncSource = configureFailPoint(
+    rst.nodes[1], "forceSyncSourceCandidate", {"hostAndPort": rst.nodes[2].host});
+syncFrom(rst.nodes[2], rst.nodes[0], rst);
+syncFrom(rst.nodes[1], rst.nodes[2], rst);
 const RBIDBeforeStepUp = assert.commandWorked(primary.adminCommand({replSetGetRBID: 1}));
 
 // Disconnect the primary from the node syncing from it.
@@ -48,11 +53,14 @@ jsTestLog("Stepping up new primary");
 assert.commandWorked(newPrimary.adminCommand({replSetStepUp: 1}));
 // Allow the "stuck" heartbeat to proceed.
 hbfp.off();
+// The step-up command waits for the election to complete, but not catch-up. Reconnect the old
+// primary to the new primary's sync source to allow replication.
+primary.reconnect(rst.nodes[2]);
 rst.awaitReplication();
 // The new primary should still be primary.
 assert.eq(newPrimary.host, rst.getPrimary().host);
 // No rollbacks should have happened.
 const RBIDAfterStepUp = assert.commandWorked(primary.adminCommand({replSetGetRBID: 1}));
 assert.eq(RBIDBeforeStepUp.rbid, RBIDAfterStepUp.rbid);
-primary.reconnect(rst.nodes[2]);
+forceSyncSource.off();
 rst.stopSet();
