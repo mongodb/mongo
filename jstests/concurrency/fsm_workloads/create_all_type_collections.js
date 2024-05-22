3b2f4ab5e107444351bc352f086a7bb350724a65
Yuhong Zhang <yuhong.zhang@mongodb.com>
2024-05-22 15:17:01 +0100

    SERVER-90744 Run create_all_type_collections.js with the new create ddl coordinator (#22438)
    GitOrigin-RevId: c25d02aad39993113ba5a5158f3a183b064b8dd9

--- jstests/concurrency/fsm_workloads/create_all_type_collections.js
+++ jstests/concurrency/fsm_workloads/create_all_type_collections.js
@@ -6,6 +6,7 @@
  *  # TODO SERVER-84659: remove the following tag once the old create collection
  *  # coordinator is fixed
  *  featureFlagAuthoritativeShardCollection,
+ *  featureFlagTrackUnshardedCollectionsUponCreation,
  *  # this test swallow expected errors caused by concurrent DDL operations
  *  catches_command_failures,
  *  requires_fcv_80
