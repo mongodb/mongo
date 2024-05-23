a7b3f924adeaa697800d53581f885c959f20f3f5
Alexander Neben <alex.neben@mongodb.com>
2024-05-23 13:34:04 -0700

    SERVER-90830 Added retires to cloning QA.git (#22517)
    GitOrigin-RevId: 72f52729095ddee1958f885c7b9eef9dcf910473

--- evergreen/run_jstestfuzz/clone_repos.sh
+++ evergreen/run_jstestfuzz/clone_repos.sh
@@ -2,5 +2,14 @@ set -o errexit
 set -o verbose
 
 cd src
-git clone --depth 1 git@github.com:10gen/QA.git jstests/qa_tests
+for i in {1..5}; do
+  git clone --depth 1 git@github.com:10gen/QA.git jstests/qa_tests && RET=0 && break || RET=$? && sleep 1
+  echo "Failed to clone git@github.com:10gen/QA.git, retrying..."
+done
+
+if [ $RET -ne 0 ]; then
+  echo "Failed to clone git@github.com:10gen/QA.git"
+  exit $RET
+fi
+
 cp -r src/mongo/db/modules/enterprise jstests/enterprise_tests
