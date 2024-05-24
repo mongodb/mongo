4e12773ab9b561cc11606a54cf17cd2ae85e5071
Reilly McBride <reilly.mcbride@mongodb.com>
2024-05-24 13:24:05 -0400

    SERVER-90644 added function which logs mongot version for search e2e tests (#22359)
    GitOrigin-RevId: 6bd14865cd1a2e506f3ef24e9314b278f69bc5fc

---
+++ evergreen/functions/get_mongot_version.sh
@@ -0,0 +1,5 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+. "$DIR/../prelude.sh"
+
+cd src
+echo "Currently running mongot version: $(./${install_dir}/mongot-localdev/mongot --version)"
