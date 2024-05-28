6165588c0646d83e019cc881a1e3189c263cdbe0
Jack Mulrow <jack.mulrow@mongodb.com>
2024-05-28 10:37:50 -0400

    SERVER-90886 Test workarounds for lack of movePrimary and createIndexes synchronization (#22588)
    (cherry picked from commit c7f35fb3f9654e84f712187afd405bf306c311c4)
    
    GitOrigin-RevId: 059afe8e08bdcd168220fb21d1d073654a8a4da9

--- jstests/libs/override_methods/implicitly_retry_on_config_transition_errors.js
+++ jstests/libs/override_methods/implicitly_retry_on_config_transition_errors.js
@@ -18,7 +18,10 @@ const kRetryableErrors = [
     {
         code: ErrorCodes.ConflictingOperationInProgress,
         errmsg: "Another ConfigsvrCoordinator with different arguments is already running"
-    }
+    },
+    // TODO SERVER-90609: Stop ignoring this error. Currently an index build may fail because a
+    // concurrent movePrimary triggered by the transition hook drops the collection.
+    {code: ErrorCodes.IndexBuildAborted},
 ];
 
 // Commands known not to work with transitions so tests can fail immediately with a clear error.
