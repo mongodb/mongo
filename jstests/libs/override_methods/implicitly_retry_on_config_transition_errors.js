c69f5acc81201d9d1c31e2fe850a81c3e5a78b2e
Jason Zhang <jz1242@users.noreply.github.com>
2024-05-29 11:56:26 -0400

    SERVER-90166 Enable Aggregation_unsplittable_collections_on_random_sh… (#22648)
    GitOrigin-RevId: 5d938b4b954842592c4fefea467446fa35b2e20e

--- jstests/libs/override_methods/implicitly_retry_on_config_transition_errors.js
+++ jstests/libs/override_methods/implicitly_retry_on_config_transition_errors.js
@@ -2,6 +2,7 @@
  * Overrides runCommand so operations that encounter errors from a config shard transitioning in and
  * out of dedicated mode retry.
  */
+import {getCommandName} from "jstests/libs/cmd_object_utils.js";
 import {OverrideHelpers} from "jstests/libs/override_methods/override_helpers.js";
 
 const kTimeout = 20 * 60 * 1000;
@@ -19,6 +20,10 @@ const kRetryableErrors = [
         code: ErrorCodes.ConflictingOperationInProgress,
         errmsg: "Another ConfigsvrCoordinator with different arguments is already running"
     },
+    // A query can be killed if it is still selecting a query plan after a config transition has
+    // completed range deletion and drops the collection. Since orphanCleanUpDelaySecs is set to be
+    // lower in testing than in production, dropCollection is scheduled almost immediately.
+    {code: ErrorCodes.QueryPlanKilled},
     // TODO SERVER-90609: Stop ignoring this error. Currently an index build may fail because a
     // concurrent movePrimary triggered by the transition hook drops the collection.
     {code: ErrorCodes.IndexBuildAborted},
@@ -49,6 +54,12 @@ function isRetryableError(error) {
 function shouldRetry(cmdObj, res) {
     if (cmdObj.hasOwnProperty("autocommit")) {
         // Retries in a transaction must come from whatever is running the transaction.
+        return false;
+    }
+
+    // getMore errors cannot be retried since a client may not know if previous getMore advanced the
+    // cursor.
+    if (getCommandName(cmdObj) === "getMore") {
         return false;
     }
 
