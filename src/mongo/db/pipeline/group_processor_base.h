dd4297775866f976c72ab10dc0efbf03e83e6f72
Sandeep Dhoot <sandeep.dhoot@mongodb.com>
2024-05-30 19:42:43 +0000

    SERVER-84420: Remove unnecessary tasserts.
    GitOrigin-RevId: 0ed764def55f8da546d7f100244445f1fb4073bd

--- src/mongo/db/pipeline/group_processor_base.h
+++ src/mongo/db/pipeline/group_processor_base.h
@@ -132,7 +132,6 @@ public:
      * Should not be used once execution has begun.
      */
     std::vector<boost::intrusive_ptr<Expression>>& getMutableIdExpressions() {
-        tassert(7020503, "Can't mutate _id fields after initialization", !_executionStarted);
         return _idExpressions;
     }
 
@@ -148,8 +147,6 @@ public:
      * Should not be used once execution has begun.
      */
     std::vector<AccumulationStatement>& getMutableAccumulationStatements() {
-        tassert(
-            7020504, "Can't mutate accumulated fields after initialization", !_executionStarted);
         return _accumulatedFields;
     }
 
