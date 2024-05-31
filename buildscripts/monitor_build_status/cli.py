9e3fb34c957ab931492123e7f2fb985ee5a92c70
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2024-05-31 21:24:51 +0300

    SERVER-90908 Gather Evergreen data and analyze for lockdown criteria (#22783)
    GitOrigin-RevId: c23b72ce2f2ce343f3bb2ee4aafd2b78bde05e2b

--- buildscripts/monitor_build_status/cli.py
+++ buildscripts/monitor_build_status/cli.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from datetime import datetime, timedelta, timezone
 from typing import Iterable, List
 
 import structlog
@@ -8,7 +9,11 @@ from typing_extensions import Annotated
 
 from buildscripts.client.jiraclient import JiraAuth, JiraClient
 from buildscripts.monitor_build_status.bfs_report import BFsReport
-from buildscripts.monitor_build_status.evergreen_service import EvergreenService, EvgProjectsInfo
+from buildscripts.monitor_build_status.evergreen_service import (
+    EvergreenService,
+    EvgProjectsInfo,
+    TaskStatusCounts,
+)
 from buildscripts.monitor_build_status.jira_service import (
     BfTemperature,
     JiraCustomFieldNames,
@@ -23,13 +28,16 @@ LOGGER = structlog.get_logger(__name__)
 JIRA_SERVER = "https://jira.mongodb.org"
 DEFAULT_REPO = "10gen/mongo"
 DEFAULT_BRANCH = "master"
+EVERGREEN_DAYS_LOOKBACK = 1
 
-GLOBAL_CORRECTNESS_HOT_BF_COUNT_LIMIT = 30
-GLOBAL_CORRECTNESS_COLD_BF_COUNT_LIMIT = 100
-GLOBAL_PERFORMANCE_BF_COUNT_LIMIT = 30
-PER_TEAM_CORRECTNESS_HOT_BF_COUNT_LIMIT = 7
-PER_TEAM_CORRECTNESS_COLD_BF_COUNT_LIMIT = 20
-PER_TEAM_PERFORMANCE_BF_COUNT_LIMIT = 10
+GLOBAL_CORRECTNESS_HOT_BF_COUNT_LIMIT = 0
+GLOBAL_CORRECTNESS_COLD_BF_COUNT_LIMIT = 0
+GLOBAL_PERFORMANCE_BF_COUNT_LIMIT = 0
+PER_TEAM_CORRECTNESS_HOT_BF_COUNT_LIMIT = 0
+PER_TEAM_CORRECTNESS_COLD_BF_COUNT_LIMIT = 0
+PER_TEAM_PERFORMANCE_BF_COUNT_LIMIT = 0
+EVERGREEN_WATERFALL_FAILURE_RATE_LIMIT = 0.00
+EVERGREEN_PATCH_FAILURE_RATE_LIMIT = 0.00
 
 
 def iterable_to_jql(entries: Iterable[str]) -> str:
@@ -58,13 +66,36 @@ class MonitorBuildStatusOrchestrator:
 
         LOGGER.info("Getting Evergreen projects data")
         evg_projects_info = self.evg_service.get_evg_project_info(repo, branch)
+        evg_project_names = evg_projects_info.branch_to_projects_map[branch]
         LOGGER.info("Got Evergreen projects data")
 
         bfs_report = self._make_bfs_report(evg_projects_info)
         failures.extend(self._check_bf_counts(bfs_report, branch))
 
-        # TODO SERVER-90908: fetch evergreen data
-        # TODO SERVER-90908: check evergreen redness
+        # We are looking for versions and patches starting window_end the beginning of yesterday
+        # to give them time to complete
+        window_end = datetime.utcnow().replace(
+            hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc
+        ) - timedelta(days=1)
+        window_start = window_end - timedelta(days=EVERGREEN_DAYS_LOOKBACK)
+
+        waterfall_report = self._make_waterfall_report(
+            evg_project_names=evg_project_names, window_start=window_start, window_end=window_end
+        )
+        failures.extend(
+            self._check_waterfall_failure_rate(
+                reports=waterfall_report, window_start=window_start, window_end=window_end
+            )
+        )
+
+        patch_report = self._make_patch_report(
+            evg_project_names=evg_project_names, window_start=window_start, window_end=window_end
+        )
+        failures.extend(
+            self._check_patch_failure_rate(
+                reports=patch_report, window_start=window_start, window_end=window_end
+            )
+        )
 
         for failure in failures:
             LOGGER.error(failure)
@@ -89,7 +120,8 @@ class MonitorBuildStatusOrchestrator:
     def _check_bf_counts(bfs_report: BFsReport, branch: str) -> List[str]:
         failures = []
         bf_count_failure_msg = (
-            "BF count check failed: {scope}: {bf_type} count ({bf_count}) is more than {bf_limit}"
+            "BF count check failed: {scope}: {bf_type}"
+            " count ({bf_count}) exceeds threshold {bf_limit}"
         )
 
         global_correctness_hot_bf_count = bfs_report.get_bf_count(
@@ -178,6 +210,89 @@ class MonitorBuildStatusOrchestrator:
                         bf_count=per_team_performance_bf_count,
                         bf_limit=PER_TEAM_PERFORMANCE_BF_COUNT_LIMIT,
                     )
+                )
+
+        return failures
+
+    def _make_waterfall_report(
+        self, evg_project_names: List[str], window_start: datetime, window_end: datetime
+    ) -> List[TaskStatusCounts]:
+        LOGGER.info(
+            "Getting Evergreen waterfall data",
+            projects=evg_project_names,
+            window_start=window_start.isoformat(),
+            window_end=window_end.isoformat(),
+        )
+        waterfall_status = self.evg_service.get_waterfall_status(
+            evg_project_names=evg_project_names, window_start=window_start, window_end=window_end
+        )
+
+        return self._accumulate_project_statuses(evg_project_names, waterfall_status)
+
+    def _make_patch_report(
+        self, evg_project_names: List[str], window_start: datetime, window_end: datetime
+    ) -> List[TaskStatusCounts]:
+        LOGGER.info(
+            "Getting Evergreen patches data",
+            projects=evg_project_names,
+            window_start=window_start.isoformat(),
+            window_end=window_end.isoformat(),
+        )
+        waterfall_status = self.evg_service.get_patch_statuses(
+            evg_project_names=evg_project_names, window_start=window_start, window_end=window_end
+        )
+
+        return self._accumulate_project_statuses(evg_project_names, waterfall_status)
+
+    @staticmethod
+    def _accumulate_project_statuses(
+        evg_project_names: List[str], build_statuses: List[TaskStatusCounts]
+    ) -> List[TaskStatusCounts]:
+        project_statuses = []
+
+        for evg_project_name in evg_project_names:
+            project_status = TaskStatusCounts(project=evg_project_name)
+            for build_status in build_statuses:
+                if build_status.project == evg_project_name:
+                    project_status += build_status
+            project_statuses.append(project_status)
+
+        return project_statuses
+
+    @staticmethod
+    def _check_waterfall_failure_rate(
+        reports: List[TaskStatusCounts], window_start: datetime, window_end: datetime
+    ) -> List[str]:
+        failures = []
+
+        for project_status in reports:
+            failure_rate = project_status.failure_rate()
+            if failure_rate > EVERGREEN_WATERFALL_FAILURE_RATE_LIMIT:
+                failures.append(
+                    f"Waterfall redness check failed:"
+                    f" Project({project_status.project}):"
+                    f" Failure rate ({failure_rate})"
+                    f" exceeds threshold {EVERGREEN_WATERFALL_FAILURE_RATE_LIMIT}"
+                    f" between {window_start.isoformat()} and {window_end.isoformat()}"
+                )
+
+        return failures
+
+    @staticmethod
+    def _check_patch_failure_rate(
+        reports: List[TaskStatusCounts], window_start: datetime, window_end: datetime
+    ) -> List[str]:
+        failures = []
+
+        for project_status in reports:
+            failure_rate = project_status.failure_rate()
+            if failure_rate > EVERGREEN_WATERFALL_FAILURE_RATE_LIMIT:
+                failures.append(
+                    f"Patch redness check failed:"
+                    f" Project({project_status.project}):"
+                    f" Failure rate ({failure_rate})"
+                    f" exceeds threshold {EVERGREEN_WATERFALL_FAILURE_RATE_LIMIT}"
+                    f" between {window_start.isoformat()} and {window_end.isoformat()}"
                 )
 
         return failures
--- buildscripts/monitor_build_status/evergreen_service.py
+++ buildscripts/monitor_build_status/evergreen_service.py
@@ -1,8 +1,17 @@
 from __future__ import annotations
 
-from typing import Dict, List, NamedTuple
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
+from typing import Any, Dict, List, NamedTuple, Optional
 
-from evergreen import EvergreenApi
+import structlog
+
+from evergreen import EvergreenApi, Patch
+
+LOGGER = structlog.get_logger(__name__)
+
+TASK_FAILED_STATUSES = ["failed", "timed_out"]
+TASK_END_STATUSES = ["failed", "timed_out", "succeeded"]
 
 
 class EvgProjectsInfo(NamedTuple):
@@ -33,6 +42,51 @@ class EvgProjectsInfo(NamedTuple):
         )
 
 
+class TaskStatusCounts(NamedTuple):
+    project: Optional[str] = None
+    version_id: Optional[str] = None
+    build_id: Optional[str] = None
+    failed: Optional[int] = 0
+    completed: Optional[int] = 0
+
+    def failure_rate(self) -> float:
+        """
+        Calculate failure rate.
+
+        :return: Failure rate.
+        """
+        if self.completed == 0:
+            return 0.0
+        return self.failed / self.completed
+
+    def __add__(self, other: Any) -> TaskStatusCounts:
+        """
+        Create a new `TaskStatusCounts` object that has a sum of failed and
+        completed counts of the current object and of the other object.
+
+        If any values of project, version_id, build_id do not match they will
+        be `None` in a new `TaskStatusCounts` object.
+
+        :param other: Other object to add.
+        :return: `TaskStatusCounts` object.
+        :raises TypeError: If other object is not `TaskStatusCounts`.
+        """
+        if isinstance(other, self.__class__):
+            project = self.project if self.project == other.project else None
+            version_id = self.version_id if self.version_id == other.version_id else None
+            build_id = self.build_id if self.build_id == other.build_id else None
+
+            return TaskStatusCounts(
+                project=project,
+                version_id=version_id,
+                build_id=build_id,
+                failed=self.failed + other.failed,
+                completed=self.completed + other.completed,
+            )
+
+        raise TypeError
+
+
 class EvergreenService:
     def __init__(self, evg_api: EvergreenApi) -> None:
         self.evg_api = evg_api
@@ -57,3 +111,131 @@ class EvergreenService:
         project_branch_map = {project.identifier: project.branch_name for project in evg_projects}
 
         return EvgProjectsInfo.from_project_branch_map(project_branch_map)
+
+    def get_waterfall_status(
+        self, evg_project_names: List[str], window_start: datetime, window_end: datetime
+    ) -> List[TaskStatusCounts]:
+        """
+        Get task status counts of all builds for a given Evergreen projects.
+
+        :param evg_project_names: Evergreen project names.
+        :param window_start: Look for waterfall versions after this date.
+        :param window_end: Look for waterfall versions before this date.
+        :return: Task status counts of all builds.
+        """
+        all_build_ids = []
+
+        for evg_project_name in evg_project_names:
+            versions = self.evg_api.versions_by_project_time_window(
+                project_id=evg_project_name, after=window_start, before=window_end
+            )
+
+            for version in versions:
+                LOGGER.info(
+                    "Getting build ids from version",
+                    project=evg_project_name,
+                    version_id=version.version_id,
+                )
+                build_ids = [bvs.build_id for bvs in version.build_variants_status]
+                all_build_ids.extend(build_ids)
+
+        return self.get_build_statuses(all_build_ids)
+
+    def get_patch_statuses(
+        self, evg_project_names: List[str], window_start: datetime, window_end: datetime
+    ) -> List[TaskStatusCounts]:
+        """
+        Get task status counts of all patch builds for a given Evergreen projects.
+
+        :param evg_project_names: Evergreen project names.
+        :param window_start: Look for patches after this date.
+        :param window_end: Look for patches before this date.
+        :return: Task status counts of all patch builds.
+        """
+        all_build_ids = []
+
+        for evg_project_name in evg_project_names:
+            patches = self.evg_api.patches_by_project_time_window(
+                project_id=evg_project_name, after=window_start, before=window_end
+            )
+
+            with ThreadPoolExecutor() as executor:
+                futures = [
+                    executor.submit(
+                        self._get_build_ids_from_patch,
+                        project=evg_project_name,
+                        patch=patch,
+                    )
+                    for patch in patches
+                ]
+                for future in futures:
+                    all_build_ids.extend(future.result())
+
+        return self.get_build_statuses(all_build_ids)
+
+    @staticmethod
+    def _get_build_ids_from_patch(project: str, patch: Patch) -> List[str]:
+        LOGGER.info(
+            "Getting build ids from patch",
+            project=project,
+            patch_id=patch.patch_id,
+        )
+        if patch.version:
+            version = patch.get_version()
+            return [bvs.build_id for bvs in version.build_variants_status]
+
+        LOGGER.warning("Patch does not have a version", project=project, patch_id=patch.patch_id)
+        return []
+
+    def get_build_statuses(self, build_ids: List[str]) -> List[TaskStatusCounts]:
+        """
+        Get task status counts of Evergreen builds for the given build ids.
+
+        :param build_ids: Evergreen build ids.
+        :return: Task status counts of the builds.
+        """
+        LOGGER.info("Getting Evergreen build statuses", num_builds=len(build_ids))
+
+        with ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(
+                    self.get_build_status,
+                    build_id=build_id,
+                )
+                for build_id in build_ids
+            ]
+            build_statuses = [future.result() for future in futures]
+        LOGGER.info("Got Evergreen build statuses", num_builds=len(build_statuses))
+
+        return build_statuses
+
+    def get_build_status(self, build_id: str) -> TaskStatusCounts:
+        """
+        Get task status counts of Evergreen build for the given build id.
+
+        :param build_id: Evergreen build id.
+        :return: Task status counts of the build.
+        """
+        build = self.evg_api.build_by_id(build_id)
+
+        failed_tasks_count = sum(
+            count
+            for status, count in build.status_counts.json.items()
+            if status in TASK_FAILED_STATUSES
+        )
+        completed_tasks_count = sum(
+            count
+            for status, count in build.status_counts.json.items()
+            if status in TASK_END_STATUSES
+        )
+
+        task_status_counts = TaskStatusCounts(
+            project=build.project_identifier,
+            version_id=build.version,
+            build_id=build.id,
+            failed=failed_tasks_count,
+            completed=completed_tasks_count,
+        )
+        LOGGER.info("Got Evergreen build status", task_status_counts=task_status_counts)
+
+        return task_status_counts
