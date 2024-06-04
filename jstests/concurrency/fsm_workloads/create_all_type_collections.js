6545cda3c771aa4ff31e03c5e486b9e4195be142
Yuhong Zhang <yuhong.zhang@mongodb.com>
2024-06-04 15:43:07 +0100

    SERVER-91062 Avoid creating orphaned time-series buckets collection in create_all_type_collections.js (#22863) (#22917)
    GitOrigin-RevId: e77169331d8c939a4a543b5f6ca2587f708b4a4f

--- jstests/concurrency/fsm_workloads/create_all_type_collections.js
+++ jstests/concurrency/fsm_workloads/create_all_type_collections.js
@@ -15,6 +15,7 @@
 import {
     uniformDistTransitions
 } from "jstests/concurrency/fsm_workload_helpers/state_transition_utils.js";
+import {FeatureFlagUtil} from "jstests/libs/feature_flag_util.js";
 
 const testDbName = jsTestName() + "_DB";
 const collPrefix = "coll_";
@@ -99,6 +100,11 @@ export const $config = (function() {
             if (TestData.runInsideTransaction) {
                 // TODO SERVER-50484: creating a timeseries/view is not supported
                 // in multi-document transactions.
+                return;
+            }
+            if (TestData.runningWithShardStepdowns &&
+                !FeatureFlagUtil.isPresentAndEnabled(db, "TrackUnshardedCollectionsUponCreation")) {
+                // TODO SERVER-90742: Remove the early exit.
                 return;
             }
             const coll = getRandomCollection(db);
