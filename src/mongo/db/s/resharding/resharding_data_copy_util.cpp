f3197ea4567bfefe766e0b72ed6cb26d2ad01920
ben-gawel <167240029+ben-gawel@users.noreply.github.com>
2024-06-05 09:19:54 -0400

    SERVER-89529 Preserve namespace from donor session oplog entry when resharding (#22955)
    GitOrigin-RevId: 5cc6a6bdd326fac8682e6141ef8a7493c82d5eb0

--- src/mongo/db/s/resharding/resharding_data_copy_util.cpp
+++ src/mongo/db/s/resharding/resharding_data_copy_util.cpp
@@ -551,7 +551,8 @@ void updateSessionRecord(OperationContext* opCtx,
                          BSONObj o2Field,
                          std::vector<StmtId> stmtIds,
                          boost::optional<repl::OpTime> preImageOpTime,
-                         boost::optional<repl::OpTime> postImageOpTime) {
+                         boost::optional<repl::OpTime> postImageOpTime,
+                         NamespaceString sourceNss) {
     invariant(opCtx->getLogicalSessionId());
     invariant(opCtx->getTxnNumber());
 
@@ -565,7 +566,7 @@ void updateSessionRecord(OperationContext* opCtx,
     oplogEntry.setOpType(repl::OpTypeEnum::kNoop);
     oplogEntry.setObject(SessionCatalogMigration::kSessionOplogTag);
     oplogEntry.setObject2(std::move(o2Field));
-    oplogEntry.setNss({});
+    oplogEntry.setNss(std::move(sourceNss));
     oplogEntry.setSessionId(sessionId);
     oplogEntry.setTxnNumber(txnNumber);
     oplogEntry.setStatementIds(stmtIds);
