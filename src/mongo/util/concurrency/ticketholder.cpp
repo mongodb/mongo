5dfcab67e08e38885c42cc8d92649bf8c42875c2
Matt Kneiser <matt.kneiser@mongodb.com>
2024-06-10 14:34:05 -0700

    SERVER-91068 Remove read_ticket_exhaustion_with_stepdown test (#23257)
    GitOrigin-RevId: 4b37bd46c6dc691720baabbb5b56b9ae2e4f92a5

--- src/mongo/util/concurrency/ticketholder.cpp
+++ src/mongo/util/concurrency/ticketholder.cpp
@@ -37,14 +37,11 @@
 #include <iostream>
 
 #include "mongo/logv2/log.h"
-#include "mongo/util/fail_point.h"
 #include "mongo/util/str.h"
 
 #define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kDefault
 
 namespace mongo {
-
-MONGO_FAIL_POINT_DEFINE(hangTicketRelease);
 
 namespace {
 void updateQueueStatsOnRelease(ServiceContext* serviceContext,
@@ -100,12 +97,6 @@ void TicketHolder::appendStats(BSONObjBuilder& b) const {
 }
 
 void TicketHolder::_releaseToTicketPool(AdmissionContext* admCtx) noexcept {
-    if (MONGO_unlikely(hangTicketRelease.shouldFail())) {
-        LOGV2(8435300,
-              "Hanging hangTicketRelease in _releaseToTicketPool() due to 'hangTicketRelease' "
-              "failpoint");
-        hangTicketRelease.pauseWhileSet();
-    }
     auto& queueStats = _getQueueStatsToUse(admCtx);
     updateQueueStatsOnRelease(_serviceContext, queueStats, admCtx);
     _releaseToTicketPoolImpl(admCtx);
