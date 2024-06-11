80e0787e4ec36b54f34cbde856dccbc371344232
Wenqin <wenqinYe@users.noreply.github.com>
2024-06-11 10:17:36 -0400

    SERVER-91166 [v6.0] Transactions in retryable_write_error_labels.js should update different documents (#23137)
    GitOrigin-RevId: 84a93d72b78b229ae64d7efa52183c0ed8260aa0

--- jstests/sharding/retryable_write_error_labels.js
+++ jstests/sharding/retryable_write_error_labels.js
@@ -137,6 +137,10 @@ function testMongodError(errorCode, isWCError) {
 function testMongosError() {
     const shard0Primary = st.rs0.getPrimary();
 
+    // Insert initial documents used by the test.
+    const docs = [{k: 0, x: 0}, {k: 1, x: 1}];
+    assert.commandWorked(shard0Primary.getDB(dbName)[collName].insert(docs));
+
     // Test retryable writes.
     jsTestLog("Retryable write should return mongos shutdown error with RetryableWriteError label");
 
@@ -211,7 +215,7 @@ function testMongosError() {
         const sessionDb = session.getDatabase(dbName);
         const sessionColl = sessionDb.getCollection(collName);
         session.startTransaction();
-        assert.commandWorked(sessionColl.update({}, {$inc: {x: 1}}));
+        assert.commandWorked(sessionColl.update({k: 0}, {$inc: {x: 1}}));
         return sessionDb.adminCommand({
             commitTransaction: 1,
             txnNumber: NumberLong(session.getTxnNumber_forTesting()),
@@ -248,7 +252,7 @@ function testMongosError() {
         const sessionDb = session.getDatabase(dbName);
         const sessionColl = sessionDb.getCollection(collName);
         session.startTransaction();
-        assert.commandWorked(sessionColl.update({}, {$inc: {x: 1}}));
+        assert.commandWorked(sessionColl.update({k: 1}, {$inc: {x: 1}}));
         return sessionDb.adminCommand({
             abortTransaction: 1,
             txnNumber: NumberLong(session.getTxnNumber_forTesting()),
