a9b3e74fc67e172be889fac1bcb3bfeba7572b07
Allison Easton <allison.easton@mongodb.com>
2024-06-11 13:05:39 +0200

    SERVER-91203 AutoGetters should establish capped snapshots for secondary namespaces (#23157)
    GitOrigin-RevId: 8426ffda43d081c4ee4b92a9fe3f43fa13fe77c7

---
+++ jstests/noPassthrough/lookup_profile.js
@@ -0,0 +1,22 @@
+// Tests that lookups on local capped collections acquire a snapshot on the capped collection
+// correctly. Tests the scenario fixed by SERVER-91203 no longer causes a crash.
+
+let rst = new ReplSetTest({nodes: {n0: {profile: "0"}}});
+rst.startSet();
+rst.initiate();
+
+const dbName = "test";
+const collName = "foo";
+
+let testDB = rst.getPrimary().getDB(dbName);
+let testColl = testDB.getCollection(collName);
+
+testColl.insert({a: 1});
+
+testDB.setProfilingLevel(2);
+
+const pipeline =
+    [{$lookup: {from: 'system.profile', localField: 'key', foreignField: 'key', as: 'results'}}];
+testColl.aggregate(pipeline).toArray();
+
+rst.stopSet();
