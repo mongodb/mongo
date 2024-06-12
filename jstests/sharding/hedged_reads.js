7bdda3bc6add25ff62cd474e16b8b6e9fed8c428
Dominic <dominic.hernandez@mongodb.com>
2024-06-12 14:22:24 -0700

    SERVER-91375: Change hedged read tests to multiversion incompatible (#23287)
    GitOrigin-RevId: 065f0eb5542e10d2b0c06015a60f0cdeeb70875d

--- jstests/sharding/hedged_reads.js
+++ jstests/sharding/hedged_reads.js
@@ -1,9 +1,11 @@
 /**
  * Tests hedging metrics in the serverStatus output.
  * @tags: [
- *    requires_fcv_70,
  *    # TODO (SERVER-88125): Re-enable this test or add an explanation why it is incompatible.
  *    embedded_router_incompatible,
+ *    # Hedged Reads are deprecated in v8.0 and have different defaults than older versions. This
+ *    # test will be removed in 8.0+
+ *    multiversion_incompatible,
  * ]
  */
 import {configureFailPoint} from "jstests/libs/fail_point_util.js";
