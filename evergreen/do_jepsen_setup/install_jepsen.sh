8f9ada375f283daa90eebfdf19fa95c4ba3a5765
Steve McClure <steve.mcclure@mongodb.com>
2024-06-13 17:06:11 -0400

    SERVER-91504: remove unnecessary branch/commit diagnostic info (#23476)
    GitOrigin-RevId: fc943276edf871ac68a99aba02f236fbd0be3135

--- evergreen/do_jepsen_setup/install_jepsen.sh
+++ evergreen/do_jepsen_setup/install_jepsen.sh
@@ -3,8 +3,5 @@ set -o errexit
 cd src
 git clone --branch=v0.2.0-jepsen-mongodb-master --depth=1 git@github.com:10gen/jepsen.git jepsen-mongodb
 cd jepsen-mongodb
-branch=$(git symbolic-ref --short HEAD)
-commit=$(git show -s --pretty=format:"%h - %an, %ar: %s")
-echo "Git branch: $branch, commit: $commit"
 
 lein install
