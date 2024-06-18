cdfce75e815864f66c5ed695284aa1ceaa1cb376
Justin Seyster <justin.seyster@mongodb.com>
2024-06-17 20:52:21 -0400

    SERVER-90988 PrepareExecutionHelper destructor should use the cached value of CanonicalQuery::toStringShort that I specifically added just for that destructor to use (#23473)
    GitOrigin-RevId: f0c85d6c861d30d99029a2f2dfe51a9796c5f31e

--- src/mongo/db/query/get_executor.cpp
+++ src/mongo/db/query/get_executor.cpp
@@ -758,7 +758,7 @@ public:
                 LOGV2_DEBUG(8276400,
                             4,
                             "Stopping the planningTime timer",
-                            "query"_attr = redact(_cq->toStringShort()));
+                            "query"_attr = redact(_queryStringForDebugLog));
                 curOp->stopQueryPlanningTimer();
             }
         }
