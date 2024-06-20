fe79ac77c414f076e270050b96e43b6e6f7c045c
Adam Farkas <adam.farkas@mongodb.com>
2024-06-20 15:21:03 +0200

    SERVER-90330 Creation of DDL coordinator hang indefinetly if executed on secondary node (#23708)
    SERVER-90330 Creation of DDL coordinator hang indefinetly if executed on
    secondary node (#22506)
    
    SERVER-91247 Ensure that DDLCoordinator creation does not survive node
    stepDown-stepUp (#23089)
    
    SERVER-91366 Fix possible crash in coordinator completion (#23272)
    
    Co-authored-by: Tommaso Tocci <58224719+toto-dev@users.noreply.github.com>
    GitOrigin-RevId: bd59cb82ffb2611236976bfeeb6009bd38b08b92

--- src/mongo/db/repl/primary_only_service.h
+++ src/mongo/db/repl/primary_only_service.h
@@ -376,6 +376,15 @@ protected:
                                                                    bool checkOptions = true);
 
     /**
+     * Return the executor where Instances of this PrimaryOnlyService are executed
+     *
+     * Tasks running on this executor will not survive a step down - step up cycle.
+     * This executor is shut down when this node steps down and joined on step up before rebuilding
+     * new instances.
+     */
+    std::shared_ptr<executor::ScopedTaskExecutor> getInstanceExecutor() const;
+
+    /**
      * Since, scoped task executor shuts down on stepdown, we might need to run some instance work,
      * like cleanup, even while the node is not primary. So, use the parent executor in that case.
      */
@@ -465,6 +474,11 @@ private:
         std::shared_ptr<executor::ScopedTaskExecutor> executor, const CancellationToken& token) {
         return ExecutorFuture<void>(**executor, Status::OK());
     };
+
+    /**
+     * Called as part of the POS onStepUp hook before rebuilding the service.
+     */
+    virtual void _onServiceInitialization() {}
 
     /**
      * Called as part of the POS onStepDown/onShutdown hooks before interrupting instances.
