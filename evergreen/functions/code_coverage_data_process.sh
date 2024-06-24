4175d7751db9a14e956740ca17b3d4d464700622
Steve McClure <steve.mcclure@mongodb.com>
2024-06-24 14:51:06 -0400

    SERVER-91056: Collect code coverage data for Coveralls consumption (#23570)
    GitOrigin-RevId: b49f4bb30bca0c6091e3cfc3f50e9911b108327b

--- evergreen/functions/code_coverage_data_process.sh
+++ evergreen/functions/code_coverage_data_process.sh
@@ -1,35 +1,38 @@
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+. "$DIR/../prelude.sh"
+
 set +o errexit
+
+cd src
+
+if [ ! -d "./build/debug" ]; then
+  echo "No code coverage to process - no 'build/debug' directory found."
+  exit 0
+fi
+
+file_list=$(find ./build/debug -type f -name "*.gcno")
+if [ ! -n "$file_list" ]; then
+  echo "No code coverage to process - no '.gcno' files found."
+  exit 0
+fi
 
 if [ -z "${GCOV_TOOL:-}" ]; then
   echo "No coverage tool defined. Set the gcov_tool expansion in evergreen.yml" >&2
-  exit 1
+  exit 0
 fi
 
-cd src
-if [ -d "./build" ]; then
-  file_list=$(find ./build -type f -name "*.gcda")
-  if [ -n "$file_list" ]; then
-    for gcda_file in $file_list; do
-      echo "Processing file $gcda_file"
-      ${GCOV_TOOL[@]} -i "$gcda_file"
-      base_name=$(echo "$gcda_file" | rev | cut -f1 -d '/' | cut -f2 -d '.' | rev)
-      gcov_file=$base_name.gcda.gcov
-      if [ -f "$gcov_file" ]; then
-        # Add a prefix to the intermediate file, since it does not have a unique name.
-        # Convert the '/' to '#' in the file path.
-        file_prefix=$(echo "$gcda_file" | sed -e 's,^\./,,' | rev | cut -f2- -d '/' | rev | tr -s '/' '#')
-        new_gcov_file="$file_prefix #$base_name.gcda.gcov"
-        if [ ! -f "$new_gcov_file" ]; then
-          echo "Renaming gcov intermediate file $gcov_file to $new_gcov_file"
-          mv "$gcov_file" "$new_gcov_file"
-        else
-          # We treat this as a fatal condition and remove all of the coverage files.
-          echo "Not renaming $gcov_file as $new_gcov_file already exists!"
-          rm -f ./*.gcda.gcov
-          exit 1
-        fi
-      fi
-      rm "$gcda_file"
-    done
-  fi
-fi
+echo "Found code coverage files:"
+find ./build/debug -type f -name "*.gcno"
+
+activate_venv
+
+pipx install "cpp-coveralls==0.4.2" || exit 1
+
+# run coveralls - currently dumps to a json file
+cpp-coveralls \
+  --root ./build/debug \
+  --build-root . \
+  --verbose \
+  --gcov ${GCOV_TOOL[@]} \
+  --dump coveralls.json \
+  --exclude src/third_party/
