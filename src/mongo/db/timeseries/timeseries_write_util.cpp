7af935b559aa5383a7c3d824d5f4ccc56195d07a
Matt Kneiser <matt.kneiser@mongodb.com>
2024-06-27 17:26:10 -0700

    SERVER-91772 Deadlock issue in timeseries insert (#24089)
    GitOrigin-RevId: 1ce377cfd03140c5e9df53a5c634dbc099f15cf0

--- src/mongo/db/timeseries/timeseries_write_util.cpp
+++ src/mongo/db/timeseries/timeseries_write_util.cpp
@@ -1116,6 +1116,20 @@ StatusWith<bucket_catalog::InsertResult> attemptInsertIntoBucket(
                         // other scenario this will happen is when an uncompressed bucket is
                         // encountered and must be compressed before reattempting a write to the
                         // compressed bucket.
+
+                        for (auto& closedBucket : std::get<bucket_catalog::InsertContext>(
+                                                      insertContextAndDate.getValue())
+                                                      .closedBuckets) {
+                            compressAndWriteBucketFunc(opCtx,
+                                                       closedBucket.bucketId.oid,
+                                                       bucketsColl->ns(),
+                                                       bucketsColl->uuid(),
+                                                       closedBucket.timeField);
+                        }
+
+                        // Will update state in the bucket catalog to clear out the closed buckets.
+                        std::get<bucket_catalog::InsertContext>(insertContextAndDate.getValue())
+                            .closedBuckets.clear();
                         continue;
                     } else if (result.getStatus().code() ==
                                ErrorCodes::TimeseriesBucketCompressionFailed) {
