6ecbc5d572654a2e43e4a87591d5435f1ea50b7e
Gregory Noma <gregory.noma@gmail.com>
2024-07-03 13:38:51 -0400

    SERVER-84699 SERVER-92045 Add a check to validation to check for mixed-schema buckets for time-series (#24246)
    GitOrigin-RevId: 021fcf848a358fb56014e45d865444b82f7ab1c8

--- src/mongo/db/catalog/coll_mod.cpp
+++ src/mongo/db/catalog/coll_mod.cpp
@@ -118,6 +118,7 @@ namespace {
 MONGO_FAIL_POINT_DEFINE(hangAfterDatabaseLock);
 MONGO_FAIL_POINT_DEFINE(hangAfterCollModIndexUniqueFullIndexScan);
 MONGO_FAIL_POINT_DEFINE(hangAfterCollModIndexUniqueReleaseIXLock);
+MONGO_FAIL_POINT_DEFINE(allowSetTimeseriesBucketsMayHaveMixedSchemaDataFalse);
 
 void assertNoMovePrimaryInProgress(OperationContext* opCtx, NamespaceString const& nss) {
     try {
@@ -646,7 +647,8 @@ StatusWith<std::pair<ParsedCollModRequest, BSONObj>> parseCollModRequest(
                 CollMod::kTimeseriesBucketsMayHaveMixedSchemaDataFieldName);
         }
 
-        if (!*mixedSchema) {
+        if (!*mixedSchema &&
+            !MONGO_unlikely(allowSetTimeseriesBucketsMayHaveMixedSchemaDataFalse.shouldFail())) {
             return {ErrorCodes::InvalidOptions,
                     "Cannot set timeseriesBucketsMayHaveMixedSchemaData to false"};
         }
