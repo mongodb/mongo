8da759011c619f8f0d6cc7e9f9c995b6f12666d6
Xuerui Fa <xuerui.fa@mongodb.com>
2024-07-08 12:30:16 -0400

    SERVER-92022: Remove TODO for retry in ReplSetTest (#24407)
    GitOrigin-RevId: 6a9b79252fc82639ff1dd89ed754a890241b392d

--- src/mongo/shell/replsettest.js
+++ src/mongo/shell/replsettest.js
@@ -4048,7 +4048,6 @@ var ReplSetTest = function ReplSetTest(opts) {
     // If opts is passed in as a string, let it pass unmodified since strings are pass-by-value.
     // if it is an object, though, pass in a deep copy.
     if (typeof opts === 'string' || opts instanceof String) {
-        // TODO SERVER-92022: Re-evaluate retry on NotYetInitialized errors
         retryOnRetryableError(() => {
             // The primary may unexpectedly step down during startup if under heavy load
             // and too slowly processing heartbeats. When it steps down, it closes all of
