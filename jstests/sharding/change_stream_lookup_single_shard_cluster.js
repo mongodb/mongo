3a0baf403b098873d4d20326935149f364dad17a
HanaPearlman <hana.pearlman@mongodb.com>
2024-07-10 09:07:07 -0400

    SERVER-92199: Ban change_stream_lookup_single_shard_cluster.js from stepdown suites (#24517)
    GitOrigin-RevId: 5f7bdb01217cdf979fdda30aa69ed7849b36cc66

--- jstests/sharding/change_stream_lookup_single_shard_cluster.js
+++ jstests/sharding/change_stream_lookup_single_shard_cluster.js
@@ -2,6 +2,8 @@
 // cluster only has a single shard, and that it can therefore successfully look up a document in a
 // sharded collection.
 // @tags: [
+//   # If a rollback is triggered during a stepdown, the change stream cursor can become invalid.
+//   does_not_support_stepdowns,
 //   requires_majority_read_concern,
 //   uses_change_streams,
 // ]
