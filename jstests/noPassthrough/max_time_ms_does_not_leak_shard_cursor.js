cf6d8b107064ee6cf5a385312a7143b8e6fc67a2
Ruoxin Xu <ruoxin.xu@mongodb.com>
2024-07-11 14:30:19 +0100

    SERVER-91701 Robustify max_time_ms_does_not_leak_shard_cursor.js test (#24321)
    GitOrigin-RevId: e72d2c32b5f227352f89b14b728cd05222c0fc69

--- jstests/noPassthrough/max_time_ms_does_not_leak_shard_cursor.js
+++ jstests/noPassthrough/max_time_ms_does_not_leak_shard_cursor.js
@@ -8,6 +8,13 @@
 
 load("jstests/libs/fail_point_util.js");  // for 'configureFailPoint()'
 
+const st = new ShardingTest({shards: 1, mongos: 1, config: 1});
+
+const dbName = "test";
+const collName = jsTestName();
+const coll = st.s.getCollection(dbName + "." + collName);
+const db = st.s.getDB(dbName);
+
 function getIdleCursors(conn, collName) {
     return conn.getDB('admin')
         .aggregate([
@@ -17,41 +24,25 @@ function getIdleCursors(conn, collName) {
         .toArray();
 }
 
-function assertNoIdleCursors(conn, collName) {
-    const sleepTimeMS = 10 * 1000;
-    const retries = 2;
+function assertNoIdleCursors(conn, collName, curToKill = null) {
+    const sleepTimeMS = 20 * 1000;
+    const retries = 4;
     assert.soon(() => (getIdleCursors(conn, collName).length === 0),
                 () => tojson(getIdleCursors(conn, collName)),
                 retries * sleepTimeMS,
                 sleepTimeMS,
                 {runHangAnalyzer: false});
+
+    if (curToKill !== null && curToKill._cursor) {
+        db.runCommand({killCursors: collName, cursors: [curToKill.getId()]});
+    }
 }
-
-const st = new ShardingTest({shards: 1, mongos: 1, config: 1});
-
-const dbName = "test";
-const collName = jsTestName();
-const coll = st.s.getCollection(dbName + "." + collName);
 
 // Insert some data (1000 simple documents) into the collection.
 assert.commandWorked(coll.insert(Array.from({length: 1000}, _ => ({a: 1}))));
 
 // Ensure there are no idle cursors on shart0 before tests begin.
 assertNoIdleCursors(st.shard0, collName);
-
-// Ensure the timeout happens on the shard after on exit from 'getMore' call. The query might
-// occasionally return the 'NetworkInterfaceExceededTimeLimit' error.
-{
-    const curs = coll.find().batchSize(2).maxTimeMS(100);
-    const fp = configureFailPoint(st.shard0,
-                                  "waitBeforeUnpinningOrDeletingCursorAfterGetMoreBatch",
-                                  {shouldCheckForInterrupt: true});
-    assert.throwsWithCode(
-        () => curs.itcount(),
-        [ErrorCodes.MaxTimeMSExpired, ErrorCodes.NetworkInterfaceExceededTimeLimit]);
-    fp.off();
-    assertNoIdleCursors(st.shard0, collName);
-}
 
 // Perform a query that sleeps after retrieving each document. This is guaranteed to exceed the
 // specified maxTimeMS limit. The timeout may happen either on mongoS or on shard. The query might
@@ -69,7 +60,21 @@ assertNoIdleCursors(st.shard0, collName);
     assert.throwsWithCode(
         () => curs.itcount(),
         [ErrorCodes.MaxTimeMSExpired, ErrorCodes.NetworkInterfaceExceededTimeLimit]);
-    assertNoIdleCursors(st.shard0, collName);
+    assertNoIdleCursors(st.shard0, collName, curs);
+}
+
+// Ensure the timeout happens on the shard after on exit from 'getMore' call. The query might
+// occasionally return the 'NetworkInterfaceExceededTimeLimit' error.
+{
+    const curs = coll.find().batchSize(2).maxTimeMS(100);
+    const fp = configureFailPoint(st.shard0,
+                                  "waitBeforeUnpinningOrDeletingCursorAfterGetMoreBatch",
+                                  {shouldCheckForInterrupt: true});
+    assert.throwsWithCode(
+        () => curs.itcount(),
+        [ErrorCodes.MaxTimeMSExpired, ErrorCodes.NetworkInterfaceExceededTimeLimit]);
+    fp.off();
+    assertNoIdleCursors(st.shard0, collName, curs);
 }
 
 // Ensure the timeout happens on mongoS.
@@ -78,7 +83,7 @@ assertNoIdleCursors(st.shard0, collName);
     const fp = configureFailPoint(st.s, "maxTimeAlwaysTimeOut", {}, "alwaysOn");
     assert.throwsWithCode(() => curs.itcount(), ErrorCodes.MaxTimeMSExpired);
     fp.off();
-    assertNoIdleCursors(st.shard0, collName);
+    assertNoIdleCursors(st.shard0, collName, curs);
 }
 
 // Ensure the timeout happens on the shard.
@@ -87,7 +92,7 @@ assertNoIdleCursors(st.shard0, collName);
     const fp = configureFailPoint(st.shard0, "maxTimeAlwaysTimeOut", {}, "alwaysOn");
     assert.throwsWithCode(() => curs.itcount(), ErrorCodes.MaxTimeMSExpired);
     fp.off();
-    assertNoIdleCursors(st.shard0, collName);
+    assertNoIdleCursors(st.shard0, collName, curs);
 }
 
 st.stop();
