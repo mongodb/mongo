305467a3543973936228800f09fbbb0b42c229d4
Zixuan <leozzx@users.noreply.github.com>
2024-07-16 12:55:29 -0700

    SERVER-92131 Fix index missing during SBE multiplanning (#24505)
    https://spruce.mongodb.com/version/669207221748d400072bd577/tasks?page=0&sorts=STATUS%3AASC%3BBASE_STATUS%3ADESC
    (The timeout task is generated fuzzer test, each test runs slower than
    before, but this is not related to changes in this PR, [this
    patch](https://spruce.mongodb.com/version/6695733f52ea7700073f28c9/tasks?sorts=STATUS%3AASC%3BBASE_STATUS%3ADESC)
    reverted the change but still timed out)
    
    ---------
    
    Co-authored-by: Catalin Sumanaru <catalin.sumanaru@mongodb.com>
    GitOrigin-RevId: 475101b19cda96947dc3145be5d4e3b7ccf4688d

--- src/mongo/db/query/sbe_multi_planner.cpp
+++ src/mongo/db/query/sbe_multi_planner.cpp
@@ -62,6 +62,7 @@
 #include "mongo/platform/atomic_word.h"
 #include "mongo/util/assert_util.h"
 #include "mongo/util/duration.h"
+#include "mongo/util/fail_point.h"
 #include "mongo/util/str.h"
 #include "mongo/util/tick_source.h"
 
@@ -69,6 +70,9 @@
 
 namespace mongo::sbe {
 namespace {
+
+MONGO_FAIL_POINT_DEFINE(hangMultiPlannerBeforeAggPipelineRebuild);
+
 /**
  * An element in this histogram is the number of plans in the candidate set of an invocation (of the
  * SBE multiplanner).
@@ -342,6 +346,11 @@ CandidatePlans MultiPlanner::finalizeExecutionPlans(
     if (!_cq.cqPipeline().empty()) {
         winner.root->close();
         _yieldPolicy->clearRegisteredPlans();
+        if (MONGO_unlikely(hangMultiPlannerBeforeAggPipelineRebuild.shouldFail())) {
+            LOGV2(9213101, "Hanging due to hangMultiPlannerBeforeAggPipelineRebuild fail point");
+            hangMultiPlannerBeforeAggPipelineRebuild.pauseWhileSet(_opCtx);
+        }
+        _indexExistenceChecker.check(_opCtx, _collections);
         auto solution = QueryPlanner::extendWithAggPipeline(
             _cq, std::move(winner.solution), _queryParams.secondaryCollectionsInfo);
         auto [rootStage, data] = stage_builder::buildSlotBasedExecutableTree(
@@ -373,6 +382,7 @@ CandidatePlans MultiPlanner::finalizeExecutionPlans(
                 if (i == winnerIdx)
                     continue;  // have already done the winner
 
+                _indexExistenceChecker.check(_opCtx, _collections);
                 auto solution = QueryPlanner::extendWithAggPipeline(
                     _cq, std::move(candidates[i].solution), _queryParams.secondaryCollectionsInfo);
                 auto&& [rootStage, data] = stage_builder::buildSlotBasedExecutableTree(
