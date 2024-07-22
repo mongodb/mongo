56499efea53e8a451a16c0991247683bddfd329e
niazpavelatmongo <150834679+niazpavelatmongo@users.noreply.github.com>
2024-07-22 08:30:08 -0700

    SERVER-81109:Fix internal_transactions_sharded_from_mongod.js's handling of causal consistency across sessions (#24784)
    GitOrigin-RevId: 92fa8926e2b284dfd18cc6b1e8d6e7b7a572c213

--- jstests/concurrency/fsm_workloads/internal_transactions_unsharded.js
+++ jstests/concurrency/fsm_workloads/internal_transactions_unsharded.js
@@ -639,6 +639,14 @@ export function extendWithInternalTransactionsUnsharded($config, $super) {
         // The read below should not be done inside a transaction (and use readConcern level
         // "snapshot").
         fsm.forceRunningOutsideTransaction(this);
+        const session = db.getMongo().startSession();
+        const sessionDb = session.getDatabase(db.getName());
+        for (const s of this.sessions) {
+            if (s.getClusterTime() !== undefined)
+                session.advanceClusterTime(s.getClusterTime());
+            if (s.getClusterTime() !== undefined)
+                session.advanceOperationTime(s.getOperationTime());
+        }
 
         const numDocsExpected = Object.keys(this.expectedCounters).length;
         const findCmdObj = {
@@ -652,7 +660,7 @@ export function extendWithInternalTransactionsUnsharded($config, $super) {
                 findCmdObj.readConcern.level = "majority";
             }
         }
-        const docs = assert.commandWorked(db.runCommand(findCmdObj)).cursor.firstBatch;
+        const docs = assert.commandWorked(sessionDb.runCommand(findCmdObj)).cursor.firstBatch;
         print("verifyDocuments " +
               tojsononeline(
                   {findCmdObj, numDocsFound: docs.length, numDocsExpected: numDocsExpected}));
