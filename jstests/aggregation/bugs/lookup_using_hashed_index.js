00d97646e8d346c64876a9a776483f024e9eac98
Kevin Cherkauer <111792207+kevin-cherkauer@users.noreply.github.com>
2024-07-23 15:22:23 -0700

    SERVER-92668 Fix wrong result with SBE, hash index, $lookup (#25158)
    GitOrigin-RevId: 7bf48bc837239b9b67526b4413e4d3ce7c186249

---
+++ jstests/aggregation/bugs/lookup_using_hashed_index.js
@@ -0,0 +1,30 @@
+/**
+ * Tests that a hashed index used by a $lookup will produce the correct results whether or not the
+ * field being used in that index is the hashed one (SERVER-92668).
+ */
+import {resultsEq} from "jstests/aggregation/extras/utils.js";
+
+const coll = db.lookup_using_hashed_index;
+const collName = coll.getName();
+const testIndexName = "testIndex";
+coll.drop();
+
+const allDocs = [{_id: 0}, {_id: 1, a: 3}, {_id: 2, a: "3"}];
+assert.commandWorked(coll.insert(allDocs));
+
+// Each document should look itself up in every test.
+const expected = allDocs.map(doc => Object.merge(doc, {relookup: [doc]}));
+
+const pipeline = [{$lookup: {from: collName, localField: "a", foreignField: "a", as: "relookup"}}];
+
+// Test 1: foreignField IS the hashed field.
+assert.commandWorked(coll.createIndex({a: "hashed", b: 1}, {name: testIndexName}));
+let results = coll.aggregate(pipeline).toArray();
+assert(resultsEq(results, expected, true), [results, expected]);
+assert.commandWorked(coll.dropIndex(testIndexName));
+
+// Test 2: foreignField is NOT the hashed field.
+assert.commandWorked(coll.createIndex({a: 1, b: "hashed"}, {name: testIndexName}));
+results = coll.aggregate(pipeline).toArray();
+assert(resultsEq(results, expected, true), [results, expected]);
+assert.commandWorked(coll.dropIndex(testIndexName));
