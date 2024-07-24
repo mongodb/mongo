84cbb1e1b6a001b12e35c805561c8737ed627010
atesteve <aitor.esteve@mongodb.com>
2024-07-24 14:39:13 +0200

    SERVER-91566 Suppress checkIndexesConsistentAcrossCluster in index_operations_abort_concurrent_outgoing_migrations.js (#25256)
    GitOrigin-RevId: aa30f9e01f93a01345fd42b0d475d3abe6e63777

--- jstests/sharding/index_operations_abort_concurrent_outgoing_migrations.js
+++ jstests/sharding/index_operations_abort_concurrent_outgoing_migrations.js
@@ -11,6 +11,8 @@ load("jstests/sharding/libs/find_chunks_util.js");
 
 // Test deliberately inserts orphans outside of migration.
 TestData.skipCheckOrphans = true;
+// TODO (SERVER-91380): remove skipCheckingIndexesConsistentAcrossCluster flag.
+TestData.skipCheckingIndexesConsistentAcrossCluster = true;
 
 /*
  * Runs moveChunk on the host to move the chunk to the given shard.
