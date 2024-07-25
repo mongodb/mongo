9515921bddee6fb46e13484e656842f8226f4550
kshitij <kshitij.gupta@mongodb.com>
2024-07-25 11:29:55 -0400

    SERVER-92712: progress_monitor_doesnt_crash_on_not_critical should use a smaller value of PROGRESS_TIMEOUT_SECONDS and wait longer. (#25274)
    GitOrigin-RevId: 450b00949ba3f0c548c036eb460fb306f6cf2a86

--- jstests/sharding/health_monitor/progress_monitor_doesnt_crash_on_not_critical.js
+++ jstests/sharding/health_monitor/progress_monitor_doesnt_crash_on_not_critical.js
@@ -5,7 +5,7 @@
  *     embedded_router_incompatible,
  * ]
  */
-const PROGRESS_TIMEOUT_SECONDS = 5;
+const PROGRESS_TIMEOUT_SECONDS = 3;
 
 const params = {
     setParameter: {
@@ -46,7 +46,7 @@ assert.commandWorked(
     st.s1.adminCommand({"configureFailPoint": 'hangTestHealthObserver', "mode": "alwaysOn"}));
 
 // Wait for the progress monitor timeout to elapse.
-sleep(1.1 * PROGRESS_TIMEOUT_SECONDS * 1000);
+sleep(2.1 * PROGRESS_TIMEOUT_SECONDS * 1000);
 
 // Servers should be still be alive.
 jsTestLog("Expect mongos processes to still be alive");
@@ -56,7 +56,7 @@ jsTestLog("Change observer to critical");
 changeObserverIntensity(st.s1, 'test', 'critical');
 
 // Wait for the progress monitor timeout to elapse.
-sleep((1.1 * PROGRESS_TIMEOUT_SECONDS * 1000) + 1000);
+sleep((2.1 * PROGRESS_TIMEOUT_SECONDS * 1000) + 1000);
 
 jsTestLog("Done sleeping");
 // Server should be still be alive.
