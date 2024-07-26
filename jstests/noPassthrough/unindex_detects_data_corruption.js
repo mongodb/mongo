c738cd0c0188cb115a212960fe4a23e0fd6974e0
Gregory Noma <gregory.noma@gmail.com>
2024-07-26 13:42:20 -0400

    SERVER-76232 Throw instead of fatally asserting on data corruption detection in WiredTigerIndex (#25156)
    GitOrigin-RevId: 3dae3669cf1228f0ab4446cad9561d100179a940

---
+++ jstests/noPassthrough/unindex_detects_data_corruption.js
@@ -0,0 +1,42 @@
+/**
+ * This tests that errors are logged when unindexing _id finds evidence of corruption, the server
+ * does not crash, and the appropriate error is returned.
+ *
+ * @tags: [
+ *   requires_replication,
+ * ]
+ */
+(function() {
+
+const replSet = new ReplSetTest({nodes: 1});
+replSet.startSet();
+replSet.initiate();
+
+const primary = replSet.getPrimary();
+
+const db = primary.getDB('test');
+const collName = 'coll';
+const coll = db[collName];
+
+assert.commandWorked(coll.insert({a: "first"}));
+
+assert.commandWorked(primary.adminCommand(
+    {configureFailPoint: "WTIndexUassertDuplicateRecordForKeyOnIdUnindex", mode: "alwaysOn"}));
+
+assert.commandFailedWithCode(coll.remove({a: "first"}), ErrorCodes.DataCorruptionDetected);
+
+assert.commandWorked(primary.adminCommand(
+    {configureFailPoint: "WTIndexUassertDuplicateRecordForKeyOnIdUnindex", mode: "off"}));
+
+assert.soonNoExcept(() => {
+    // The health log entry is written asynchronously by a background worker, expect it to be
+    // eventually found.
+    let entry = primary.getDB('local').system.healthlog.findOne({severity: 'error'});
+    assert(entry, "No healthlog entry found on " + tojson(primary));
+    assert.eq("Un-index seeing multiple records for key", entry.msg, tojson(entry));
+    assert.eq(1, primary.getDB('local').system.healthlog.count({severity: 'error'}));
+    return true;
+});
+
+replSet.stopSet();
+})();
