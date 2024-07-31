c639e39338f2e434dff31d7d7b70e3a18dffee47
Justin Seyster <justin.seyster@mongodb.com>
2024-07-31 15:54:15 -0400

    SERVER-92201 Make currentop_shell.js as robust as a strong cup of coffee in the morning (#25506)
    GitOrigin-RevId: bc0f0ecf5b8e0d20d5b347fb6bb91ac35a8c02bd

--- jstests/core/administrative/current_op/currentop_shell.js
+++ jstests/core/administrative/current_op/currentop_shell.js
@@ -2,6 +2,10 @@
  * Tests that the shell helper db.currentOpCursor isn't constrained by the legacy currentOp server
  * command - ie. the result set isn't limited to 16MB and long operations aren't truncated.
  *
+ * Note: On newer branches, this test contains additional cases for the currentOp command (without a
+ * shell helper) and the $currentOp pipeline stage, which are not included here. Those cases would
+ * behave unreliably because of SERVER-92284.
+ *
  * @tags: [
  *   # The collection may be completely moved to another shard, which results in currentOp not
  *   # returning the expected command.
@@ -22,6 +26,7 @@
  */
 
 import {FixtureHelpers} from "jstests/libs/fixture_helpers.js";
+import {funWithArgs} from "jstests/libs/parallel_shell_helpers.js";
 
 const coll = db.currentOp_cursor;
 coll.drop();
@@ -30,23 +35,36 @@ for (let i = 0; i < 100; i++) {
     assert.commandWorked(coll.insert({val: 1}));
 }
 
+//
 // Test that db.currentOpCursor() returns an iterable cursor.
-let res = db.currentOpCursor();
-assert(res.hasNext());
-assert(res.next());
+//
+const cursorFromCurrentOp = db.currentOpCursor();
+assert(cursorFromCurrentOp.hasNext());
+assert(cursorFromCurrentOp.next());
 
-// Test that db.currentOp() interface does not change.
-res = db.currentOp();
-assert("inprog" in res, "Result contains 'inprog' field");
-assert("ok" in res, "Result contains 'ok' field");
+//
+// Test that db.currentOp() returns an object in the expected format.
+//
+const currentOpRes = db.currentOp();
+assert("inprog" in currentOpRes, "Result contains 'inprog' field");
+assert("ok" in currentOpRes, "Result contains 'ok' field");
 
-// Attempting to access the fsyncLock field from the results throws with an error message.
-let error = assert.throws(() => res.fsyncLock);
+//
+// Test that attempting to access the fsyncLock field from the results throws with an error message.
+//
+const error = assert.throws(() => currentOpRes.fsyncLock);
 assert(
     /fsyncLock is no longer included in the currentOp shell helper, run db\.runCommand\({currentOp: 1}\) instead/
         .test(error));
 
-function shellOp() {
+//
+// Start a pipeline with a large command object in a parallel shell and then test three different
+// methods of executing "currentOp" queries to ensure that they all observe the operation and that
+// they do or do not truncate its command object (according to each one's specification).
+//
+
+// Starts the query. Intended to b e called from a parallel shell.
+function startLongRunningAggregation(collName, comment) {
     function createLargeDoc() {
         let doc = {};
         for (let i = 0; i < 100; i++) {
@@ -56,80 +74,95 @@ function shellOp() {
     }
 
     assert.commandFailedWithCode(db.runCommand({
-        aggregate: "currentOp_cursor",
+        aggregate: collName,
         pipeline: [{
             $addFields: {
                 newVal: {$function: {args: [], body: "sleep(1000000)", lang: "js"}},
                 bigDoc: createLargeDoc()
             }
         }],
-        comment: TestData.comment,
+        comment: comment,
         cursor: {}
     }),
                                  ErrorCodes.Interrupted);
 }
 
-function startShellWithOp(comment) {
-    TestData.comment = comment;
-    const awaitShell = startParallelShell(shellOp);
-
-    // Confirm that the operation has started in the parallel shell.
+// Repeatedly executes 'getOperationsFunction()' until it returns exactly one operation for each
+// shard in a sharded collection or exactly one operation for an unsharded collection.
+function awaitOperations(getOperationsFunction) {
+    let operations;
     assert.soon(
         function() {
-            let shards = FixtureHelpers.numberOfShardsForCollection(coll);
-            let aggRes =
-                db.getSiblingDB("admin")
-                    .aggregate([
-                        {$currentOp: {}},
-                        {$match: {ns: "test.currentOp_cursor", "command.comment": TestData.comment}}
-                    ])
-                    .toArray();
-            return aggRes.length >= shards;
+            const numShards = FixtureHelpers.numberOfShardsForCollection(coll);
+            operations = getOperationsFunction();
+
+            // No shard should have more than one operation matching the query comment. First check
+            // that the total number of operations is no greater than the total number of shards.
+            assert.lte(operations.length, numShards, operations);
+
+            // Also explicitly check that each shard appears no more than once in the list of
+            // operations.
+            const distinctShardNames = new Set(operations.map(op => "shard" in op ? op.shard : ""));
+            assert.eq(operations.length, distinctShardNames.size, {operations, numShards});
+
+            if (operations.length < numShards) {
+                print(`Found ${operations.length} operation(s); waiting until there are ${
+                    numShards} operation(s)`);
+                return false;
+            } else if (operations.some(op => op.op !== "getmore" && "cursor" in op &&
+                                           op.cursor.batchSize === 0)) {
+                print(`Found command with empty 'batchSize' value; waiting for getmore: ${
+                    tojson(operations)}`);
+                return false;
+            }
+
+            return true;
         },
         function() {
             return "Failed to find parallel shell operation in $currentOp output: " +
                 tojson(db.currentOp());
         });
-    return awaitShell;
+
+    return operations;
 }
 
-/**
- * Tests the currentOp behaviour by first starting a parallel shell, where a long running command is
- * being executed. After that a 'commandFn' is called, which is retrieving currentOps either through
- * a dedicated command or a shell extension function.
- */
-function testCurrentOp(comment, commandFn, shouldTruncate) {
-    const serverCommandTest = startShellWithOp(comment);
-    const res = commandFn({
-        $and: [
-            {"ns": "test.currentOp_cursor"},
-            {"command.comment": comment},
-            // On the replica set endpoint, currentOp reports both router and shard
-            // operations. So filter out one of them.
-            TestData.testingReplicaSetEndpoint ? {role: "ClusterRole{router}"}
-                                               : {role: {$exists: false}}
-        ]
-    });
-    assert.eq(res.inprog.length, FixtureHelpers.numberOfShardsForCollection(coll), res);
-    res.inprog.forEach((result) => {
-        if (result.op === 'command') {
-            assert.eq(shouldTruncate, result.command.hasOwnProperty("$truncated"), res);
-        } else {
-            assert.eq(result.op, 'getmore', res);
-            assert.eq(
-                shouldTruncate, result.cursor.originatingCommand.hasOwnProperty("$truncated"), res);
-        }
-    });
-
-    res.inprog.forEach((op) => {
-        assert.commandWorked(db.killOp(op.opid));
-    });
-
-    serverCommandTest();
+function getCommandFromCurrentOpEntry(entry) {
+    if (entry.op === "command" && "command" in entry) {
+        return entry.command;
+    } else if (entry.op === "getmore" && "cursor" in entry &&
+               "originatingCommand" in entry.cursor) {
+        return entry.cursor.originatingCommand;
+    } else {
+        assert(false, entry);
+    }
 }
 
-testCurrentOp("currentOp_server",
-              (args) => db.adminCommand({currentOp: true, ...args}),
-              true /* shouldTruncate */);
+const comment = "long_running_aggregation";
+const awaitShell =
+    startParallelShell(funWithArgs(startLongRunningAggregation, coll.getName(), comment));
 
-testCurrentOp("currentOp_shell", (args) => db.currentOp(args), false /* shouldTruncate */);
+const filter = {
+    ns: coll.getFullName(),
+    "command.comment": comment,
+
+    // On the replica set endpoint, currentOp reports both router and shard operations. So filter
+    // out one of them.
+    role: TestData.testingReplicaSetEndpoint ? "ClusterRole{router}" : {$exists: false}
+};
+
+// The 'currentOp' shell helper should _not_ truncate the command.
+const operationsViaCurrentOpShellHelper = awaitOperations(function() {
+    return db.currentOp(filter).inprog;
+});
+assert(operationsViaCurrentOpShellHelper.every(op => {
+    const command = getCommandFromCurrentOpEntry(op);
+    return !("$truncated" in command) && command.aggregate == coll.getName();
+}),
+       operationsViaCurrentOpShellHelper);
+
+// Finish the test by killing the long-running aggregation pipeline and joining the parallel shell
+// that launched it.
+for (let op of operationsViaCurrentOpShellHelper) {
+    assert.commandWorked(db.killOp(op.opid));
+}
+awaitShell();
