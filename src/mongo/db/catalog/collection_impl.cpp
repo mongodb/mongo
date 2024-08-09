c182e977793f6876450b0b9f64ed736730e919dc
Silvia Surroca <silvia.surroca@mongodb.com>
2024-08-09 15:01:18 +0200

    SERVER-93015 Replace timeseriesBucketsMayHaveMixedSchemaData direct checks with getTimeseriesBucketsMayHaveMixedSchemaData() (#25956)
    GitOrigin-RevId: fa59f6df63fc857f7d28c646355e4d6e480ce8c4

--- src/mongo/db/catalog/collection_impl.cpp
+++ src/mongo/db/catalog/collection_impl.cpp
@@ -1615,18 +1615,15 @@ Status CollectionImpl::prepareForIndexBuild(OperationContext* opCtx,
               str::stream() << "index " << imd.nameStringData()
                             << " is already in current metadata: " << _metadata->toBSON());
 
-    if (getTimeseriesOptions() &&
+    if (getTimeseriesBucketsMayHaveMixedSchemaData() &&
         timeseries::doesBucketsIndexIncludeMeasurement(
             opCtx, ns(), *getTimeseriesOptions(), spec->infoObj())) {
-        invariant(_metadata->timeseriesBucketsMayHaveMixedSchemaData);
-        if (*_metadata->timeseriesBucketsMayHaveMixedSchemaData) {
-            LOGV2(6057502,
-                  "Detected that this time-series collection may have mixed-schema data. "
-                  "Attempting to build the index.",
-                  logAttrs(ns()),
-                  logAttrs(uuid()),
-                  "spec"_attr = spec->infoObj());
-        }
+        LOGV2(6057502,
+              "Detected that this time-series collection may have mixed-schema data. "
+              "Attempting to build the index.",
+              logAttrs(ns()),
+              logAttrs(uuid()),
+              "spec"_attr = spec->infoObj());
     }
 
     _writeMetadata(
