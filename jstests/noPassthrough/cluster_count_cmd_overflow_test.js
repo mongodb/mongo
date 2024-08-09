c660af1ef83604bbad6c198200a0f037d69f7c66
Joseph Prince <57415450+josephdprince@users.noreply.github.com>
2024-08-09 08:43:50 -0700

    SERVER-93128 SERVER-93696 Prevent overflow of the CountCommandRequest::_limit field (#25733)
    GitOrigin-RevId: f453c2916ea0d4a152677683378dc373f6a5a07d

---
+++ jstests/noPassthrough/cluster_count_cmd_overflow_test.js
@@ -0,0 +1,17 @@
+/**
+ * Verify that invalid inputs to the cluster count command will throw an overflow error.
+ *
+ * @tags: [
+ *   requires_replication,
+ *   requires_sharding,
+ * ]
+ */
+
+const st = new ShardingTest({mongos: 1, shards: 1, config: 1});
+const conn = st.rs0.getPrimary();
+
+assert.commandWorked(conn.adminCommand({clusterCount: "x", skip: 200, limit: 20}));
+assert.commandFailedWithCode(conn.adminCommand({clusterCount: "x", skip: Infinity, limit: 20}),
+                             ErrorCodes.Overflow);
+
+st.stop();
