9e1642d14d79b7245c746d6ae8fd4360914260e7
Juan Gu <111318390+juangugit@users.noreply.github.com>
2024-08-13 12:53:22 -0700

    SERVER-93326 Add retries to cloneing jstestfuzz.git (#25908) (#25947)
    GitOrigin-RevId: c06aaa0711e06bc014eb6b4d0b265ee77ad4ac35

--- evergreen/jstestfuzz_setup.sh
+++ evergreen/jstestfuzz_setup.sh
@@ -6,4 +6,12 @@ cd src
 set -o errexit
 set -o verbose
 
-git clone git@github.com:10gen/jstestfuzz.git
+for i in {1..5}; do
+  git clone git@github.com:10gen/jstestfuzz.git && RET=0 && break || RET=$? && sleep 5
+  echo "Failed to clone git@github.com:10gen/jstestfuzz.git, retrying..."
+done
+
+if [ $RET -ne 0 ]; then
+  echo "Failed to clone git@github.com:10gen/jstestfuzz.git"
+  exit $RET
+fi
