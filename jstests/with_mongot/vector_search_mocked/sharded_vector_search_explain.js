51070b36bfd72bf0a96ce41c356381367d52b476
erinzhu001 <erin.zhu@mongodb.com>
2024-08-20 13:04:41 -0400

    SERVER-93629 Add sharded vector search explain test (#26310)
    GitOrigin-RevId: 87db8aa824d40494045c823d4c7e5acc35f7ef43

--- jstests/with_mongot/vector_search_mocked/sharded_vector_search_explain.js
+++ jstests/with_mongot/vector_search_mocked/sharded_vector_search_explain.js
@@ -2,16 +2,22 @@
  * Sharding tests for using "explain" with the $vectorSearch aggregation stage.
  * @tags: [
  *  featureFlagVectorSearchPublicPreview,
- *  featureFlagSearchExplainExecutionStats_incompatible
  * ]
  */
 import {getAggPlanStages} from "jstests/libs/analyze_plan.js";
+import {FeatureFlagUtil} from "jstests/libs/feature_flag_util.js";
 import {getUUIDFromListCollections} from "jstests/libs/uuid_util.js";
 import {mongotCommandForVectorSearchQuery} from "jstests/with_mongot/mongotmock/lib/mongotmock.js";
 import {
     ShardingTestWithMongotMock
 } from "jstests/with_mongot/mongotmock/lib/shardingtest_with_mongotmock.js";
-import {prepCollection} from "jstests/with_mongot/mongotmock/lib/utils.js";
+import {
+    getDefaultLastExplainContents,
+    getShardedSearchStagesAndVerifyExplainOutput,
+    setUpMongotReturnExplain,
+    setUpMongotReturnExplainAndCursor,
+    setUpMongotReturnExplainAndCursorGetMore,
+} from "jstests/with_mongot/mongotmock/lib/utils.js";
 
 const dbName = jsTestName();
 const collName = jsTestName();
@@ -33,8 +39,14 @@ assert.commandWorked(
     mongos.getDB("admin").runCommand({enableSharding: dbName, primaryShard: st.shard0.name}));
 
 const coll = testDB.getCollection(collName);
-prepCollection(mongos, dbName, collName);
-
+assert.commandWorked(coll.insert({_id: 1, name: "Sozin", element: "fire"}));
+assert.commandWorked(coll.insert({_id: 2, name: "Zuko", element: "fire"}));
+assert.commandWorked(coll.insert({_id: 3, name: "Rangi", element: "fire"}));
+assert.commandWorked(coll.insert({_id: 4, name: "Azulon", element: "fire"}));
+assert.commandWorked(coll.insert({_id: 11, name: "Iroh", element: "fire"}));
+assert.commandWorked(coll.insert({_id: 12, name: "Mako", element: "fire"}));
+assert.commandWorked(coll.insert({_id: 13, name: "Azula", element: "fire"}));
+assert.commandWorked(coll.insert({_id: 14, name: "Aang", element: "air"}));
 // Shard the test collection, split it at {_id: 10}, and move the higher chunk to shard1.
 st.shardColl(coll, {_id: 1}, {_id: 10}, {_id: 10 + 1});
 
@@ -52,12 +64,7 @@ const vectorSearchQuery = {
 const lowerUserLimit = vectorSearchQuery.limit - 1;
 const higherUserLimit = vectorSearchQuery.limit + 1;
 
-const explainContents = {
-    str: "this is a string",
-    obj: {x: 1, y: 2},
-};
-
-const cursorId = NumberLong(123);
+const expectedExplainContents = getDefaultLastExplainContents();
 
 function runTestOnPrimaries(testFn) {
     testDB.getMongo().setReadPref("primary");
@@ -69,7 +76,10 @@ function runTestOnSecondaries(testFn) {
     testFn(st.rs0.getSecondary(), st.rs1.getSecondary());
 }
 
-function testExplainVerbosity(shard0Conn, shard1Conn, verbosity, userLimit) {
+function testLimit(shard0Conn, shard1Conn, verbosity, userLimit) {
+    // sX is shard num X.
+    const s0Mongot = stWithMock.getMockConnectedToHost(shard0Conn);
+    const s1Mongot = stWithMock.getMockConnectedToHost(shard1Conn);
     // Ensure there is never a staleShardVersionException to cause a retry on any shard.
     // If a retry happens on one shard and not another, then the shard that did not retry
     // will see multiple instances of the explain command, which the test does not expect,
@@ -84,25 +94,40 @@ function testExplainVerbosity(shard0Conn, shard1Conn, verbosity, userLimit) {
         collectionUUID,
     });
 
-    const history = [{
-        expectedCommand: vectorSearchCmd,
-        response: {explain: explainContents, ok: 1},
-    }];
-
-    const s0Mongot = stWithMock.getMockConnectedToHost(shard0Conn);
-    s0Mongot.setMockResponses(history, cursorId);
-
-    const s1Mongot = stWithMock.getMockConnectedToHost(shard1Conn);
-    s1Mongot.setMockResponses(history, cursorId);
-
-    let pipeline = [{$vectorSearch: vectorSearchQuery}];
-
-    if (userLimit) {
-        pipeline.push({$limit: userLimit});
+    if (verbosity === "queryPlanner") {
+        setUpMongotReturnExplain({
+            searchCmd: vectorSearchCmd,
+            mongotMock: s0Mongot,
+        });
+        setUpMongotReturnExplain({
+            searchCmd: vectorSearchCmd,
+            mongotMock: s1Mongot,
+        });
+    } else {
+        setUpMongotReturnExplainAndCursor({
+            mongotMock: s0Mongot,
+            coll,
+            searchCmd: vectorSearchCmd,
+            nextBatch: [
+                {_id: 3, $vectorSearchScore: 100},
+                {_id: 2, $vectorSearchScore: 10},
+                {_id: 4, $vectorSearchScore: 1},
+                {_id: 1, $vectorSearchScore: 0.2},
+            ],
+        });
+        setUpMongotReturnExplainAndCursor({
+            mongotMock: s1Mongot,
+            coll,
+            searchCmd: vectorSearchCmd,
+            nextBatch: [
+                {_id: 11, $vectorSearchScore: 100},
+                {_id: 12, $vectorSearchScore: 10},
+                {_id: 13, $vectorSearchScore: 1},
+            ],
+        });
     }
-
-    const result = coll.explain(verbosity).aggregate(pipeline);
-
+    const result = coll.explain(verbosity).aggregate(
+        [{$vectorSearch: vectorSearchQuery}, {$limit: userLimit}]);
     // We should have a $limit on each shard.
     const limitStages = getAggPlanStages(result, "$limit");
     assert.eq(limitStages.length, 2, tojson(result));
@@ -113,7 +138,6 @@ function testExplainVerbosity(shard0Conn, shard1Conn, verbosity, userLimit) {
     for (const limitStage of limitStages) {
         assert.eq(expectedLimitVal, limitStage["$limit"], tojson(result));
     }
-
     // The merging pipeline should also have a $limit with the minimum of the $vectorSearch limit
     // and user-specified $limit.
     assert(result.splitPipeline.mergerPart[0].hasOwnProperty("$mergeCursors"),
@@ -122,56 +146,207 @@ function testExplainVerbosity(shard0Conn, shard1Conn, verbosity, userLimit) {
            tojson(result.splitPipeline));
     assert.eq(
         result.splitPipeline.mergerPart[1].$limit, expectedLimitVal, tojson(result.splitPipeline));
+}
+function runExplainTest(verbosity) {
+    // Ensure there is never a staleShardVersionException to cause a retry on any shard.
+    // If a retry happens on one shard and not another, then the shard that did not retry
+    // will see multiple instances of the explain command, which the test does not expect,
+    // causing an error.
+    st.refreshCatalogCacheForNs(mongos, coll.getFullName());
 
-    // Each shard should have an $_internalSearchIdLookup stage.
-    const idLookupStages = getAggPlanStages(result, "$_internalSearchIdLookup");
-    assert.eq(idLookupStages.length, 2, tojson(idLookupStages));
+    const vectorSearchCmd = mongotCommandForVectorSearchQuery({
+        ...vectorSearchQuery,
+        explain: {verbosity},
+        collName,
+        dbName,
+        collectionUUID,
+    });
+    const pipeline = [{$vectorSearch: vectorSearchQuery}];
 
-    // Each shard should have a $vectorSearch stage with explain info populated.
-    const vectorSearchStages = getAggPlanStages(result, "$vectorSearch");
-    assert.eq(vectorSearchStages.length, 2, tojson(vectorSearchStages));
-    for (const vectorSearchStage of vectorSearchStages) {
-        const vectorSearchInfo = vectorSearchStage["$vectorSearch"];
-        assert(vectorSearchInfo.hasOwnProperty("explain"), tojson(vectorSearchInfo));
-        assert.eq(explainContents, vectorSearchInfo.explain);
-        assert.eq(
-            vectorSearchQuery.queryVector, vectorSearchInfo.queryVector, tojson(vectorSearchInfo));
-        assert.eq(vectorSearchQuery.path, vectorSearchInfo.path, tojson(vectorSearchInfo));
-        assert.eq(vectorSearchQuery.numCandidates,
-                  vectorSearchInfo.numCandidates,
-                  tojson(vectorSearchInfo));
-        assert.eq(vectorSearchQuery.limit, vectorSearchInfo.limit, tojson(vectorSearchInfo));
-        assert.eq(vectorSearchQuery.index, vectorSearchInfo.index, tojson(vectorSearchInfo));
-        assert.eq(vectorSearchQuery.filter, vectorSearchInfo.filter, tojson(vectorSearchInfo));
+    function runExplainQueryPlannerTest(shard0Conn, shard1Conn) {
+        assert.eq(verbosity, "queryPlanner");
+        // sX is shard num X.
+        const s0Mongot = stWithMock.getMockConnectedToHost(shard0Conn);
+        const s1Mongot = stWithMock.getMockConnectedToHost(shard1Conn);
+        {
+            setUpMongotReturnExplain({
+                searchCmd: vectorSearchCmd,
+                mongotMock: s0Mongot,
+            });
+            setUpMongotReturnExplain({
+                searchCmd: vectorSearchCmd,
+                mongotMock: s1Mongot,
+            });
+            const result = coll.explain(verbosity).aggregate(pipeline);
+
+            getShardedSearchStagesAndVerifyExplainOutput({
+                result: result,
+                stageType: "$vectorSearch",
+                verbosity: verbosity,
+                expectedNumStages: 2,
+                nReturnedList: [NumberLong(0), NumberLong(0)],
+                expectedExplainContents: expectedExplainContents,
+            });
+            getShardedSearchStagesAndVerifyExplainOutput({
+                result: result,
+                stageType: "$_internalSearchIdLookup",
+                expectedNumStages: 2,
+                nReturnedList: [NumberLong(0), NumberLong(0)],
+                verbosity: verbosity,
+            });
+        }
+    }
+    function runExplainExecutionStatsTest(shard0Conn, shard1Conn) {
+        assert.neq(verbosity, "queryPlanner");
+        // sX is shard num X.
+        const s0Mongot = stWithMock.getMockConnectedToHost(shard0Conn);
+        const s1Mongot = stWithMock.getMockConnectedToHost(shard1Conn);
+
+        // TODO SERVER-91594: setUpMongotReturnExplain() can be removed when mongot always
+        // returns a cursor alongside explain for execution stats.
+        {
+            setUpMongotReturnExplain({
+                searchCmd: vectorSearchCmd,
+                mongotMock: s0Mongot,
+            });
+            setUpMongotReturnExplain({
+                searchCmd: vectorSearchCmd,
+                mongotMock: s1Mongot,
+            });
+            // When querying an older version of mongot for explain, the query is sent twice. This
+            // uses a different cursorId than the default one for setUpMongotReturnExplain() so the
+            // mock will return the response correctly.
+            setUpMongotReturnExplain({
+                searchCmd: vectorSearchCmd,
+                mongotMock: s0Mongot,
+                cursorId: NumberLong(124),
+            });
+            setUpMongotReturnExplain({
+                searchCmd: vectorSearchCmd,
+                mongotMock: s1Mongot,
+                cursorId: NumberLong(124),
+            });
+
+            const result = coll.explain(verbosity).aggregate(pipeline);
+            getShardedSearchStagesAndVerifyExplainOutput({
+                result,
+                stageType: "$vectorSearch",
+                expectedNumStages: 2,
+                verbosity,
+                nReturnedList: [NumberLong(0), NumberLong(0)],
+                expectedExplainContents,
+            });
+            getShardedSearchStagesAndVerifyExplainOutput({
+                result,
+                stageType: "$_internalSearchIdLookup",
+                expectedNumStages: 2,
+                verbosity,
+                nReturnedList: [NumberLong(0), NumberLong(0)]
+            });
+        }
+        {
+            setUpMongotReturnExplainAndCursor({
+                mongotMock: s0Mongot,
+                coll,
+                searchCmd: vectorSearchCmd,
+                nextBatch: [
+                    {_id: 3, $vectorSearchScore: 100},
+                    {_id: 2, $vectorSearchScore: 10},
+                    {_id: 4, $vectorSearchScore: 1},
+                    {_id: 1, $vectorSearchScore: 0.2},
+                ],
+            });
+            setUpMongotReturnExplainAndCursor({
+                mongotMock: s1Mongot,
+                coll,
+                searchCmd: vectorSearchCmd,
+                nextBatch: [
+                    {_id: 11, $vectorSearchScore: 100},
+                    {_id: 12, $vectorSearchScore: 10},
+                    {_id: 13, $vectorSearchScore: 1},
+                ],
+            });
+            const result = coll.explain(verbosity).aggregate(pipeline);
+            getShardedSearchStagesAndVerifyExplainOutput({
+                result,
+                stageType: "$vectorSearch",
+                expectedNumStages: 2,
+                verbosity,
+                nReturnedList: [NumberLong(4), NumberLong(3)],
+                expectedExplainContents,
+            });
+            getShardedSearchStagesAndVerifyExplainOutput({
+                result,
+                stageType: "$_internalSearchIdLookup",
+                expectedNumStages: 2,
+                verbosity,
+                nReturnedList: [NumberLong(4), NumberLong(3)]
+            });
+        }
+        {
+            setUpMongotReturnExplainAndCursorGetMore({
+                searchCmd: vectorSearchCmd,
+                mongotMock: s0Mongot,
+                coll: coll,
+                batchList: [
+                    [{_id: 3, $vectorSearchScore: 100}, {_id: 2, $vectorSearchScore: 1}],
+                    [{_id: 4, $vectorSearchScore: 0.99}, {_id: 1, $vectorSearchScore: 0.98}],
+                ]
+            });
+            setUpMongotReturnExplainAndCursorGetMore({
+                searchCmd: vectorSearchCmd,
+                mongotMock: s1Mongot,
+                coll: coll,
+                batchList: [
+                    [{_id: 11, $vectorSearchScore: 100}, {_id: 12, $vectorSearchScore: 1}],
+                    [{_id: 13, $vectorSearchScore: 0.99}],
+                ]
+            });
+            const result = coll.explain(verbosity).aggregate(pipeline, {cursor: {batchSize: 2}});
+            getShardedSearchStagesAndVerifyExplainOutput({
+                result,
+                stageType: "$vectorSearch",
+                expectedNumStages: 2,
+                verbosity,
+                nReturnedList: [NumberLong(4), NumberLong(3)],
+                expectedExplainContents,
+            });
+            getShardedSearchStagesAndVerifyExplainOutput({
+                result,
+                stageType: "$_internalSearchIdLookup",
+                expectedNumStages: 2,
+                verbosity,
+                nReturnedList: [NumberLong(4), NumberLong(3)]
+            });
+        }
+    }
+    if (verbosity == "queryPlanner") {
+        runTestOnPrimaries(runExplainQueryPlannerTest);
+        runTestOnSecondaries(runExplainQueryPlannerTest);
+    } else {
+        runTestOnPrimaries(runExplainExecutionStatsTest);
+        runTestOnSecondaries(runExplainExecutionStatsTest);
     }
 }
 
 // Test that $vectorSearch works with each explain verbosity.
+runExplainTest("queryPlanner");
 runTestOnPrimaries((shard0Conn, shard1Conn) =>
-                       testExplainVerbosity(shard0Conn, shard1Conn, "queryPlanner"));
-runTestOnPrimaries((shard0Conn, shard1Conn) => testExplainVerbosity(
-                       shard0Conn, shard1Conn, "queryPlanner", lowerUserLimit));
-runTestOnPrimaries((shard0Conn, shard1Conn) => testExplainVerbosity(
-                       shard0Conn, shard1Conn, "queryPlanner", higherUserLimit));
-runTestOnSecondaries((shard0Conn, shard1Conn) =>
-                         testExplainVerbosity(shard0Conn, shard1Conn, "queryPlanner"));
-
+                       testLimit(shard0Conn, shard1Conn, "queryPlanner", lowerUserLimit));
 runTestOnPrimaries((shard0Conn, shard1Conn) =>
-                       testExplainVerbosity(shard0Conn, shard1Conn, "executionStats"));
-runTestOnPrimaries((shard0Conn, shard1Conn) => testExplainVerbosity(
-                       shard0Conn, shard1Conn, "executionStats", lowerUserLimit));
-runTestOnPrimaries((shard0Conn, shard1Conn) => testExplainVerbosity(
-                       shard0Conn, shard1Conn, "executionStats", higherUserLimit));
-runTestOnSecondaries((shard0Conn, shard1Conn) =>
-                         testExplainVerbosity(shard0Conn, shard1Conn, "executionStats"));
+                       testLimit(shard0Conn, shard1Conn, "queryPlanner", higherUserLimit));
 
-runTestOnPrimaries((shard0Conn, shard1Conn) =>
-                       testExplainVerbosity(shard0Conn, shard1Conn, "allPlansExecution"));
-runTestOnPrimaries((shard0Conn, shard1Conn) => testExplainVerbosity(
-                       shard0Conn, shard1Conn, "allPlansExecution", lowerUserLimit));
-runTestOnPrimaries((shard0Conn, shard1Conn) => testExplainVerbosity(
-                       shard0Conn, shard1Conn, "allPlansExecution", higherUserLimit));
-runTestOnSecondaries((shard0Conn, shard1Conn) =>
-                         testExplainVerbosity(shard0Conn, shard1Conn, "allPlansExecution"));
+if (FeatureFlagUtil.isEnabled(testDB.getMongo(), 'SearchExplainExecutionStats')) {
+    runExplainTest("executionStats");
+    runTestOnPrimaries((shard0Conn, shard1Conn) =>
+                           testLimit(shard0Conn, shard1Conn, "executionStats", lowerUserLimit));
+    runTestOnPrimaries((shard0Conn, shard1Conn) =>
+                           testLimit(shard0Conn, shard1Conn, "executionStats", higherUserLimit));
 
+    runExplainTest("allPlansExecution");
+    runTestOnPrimaries((shard0Conn, shard1Conn) =>
+                           testLimit(shard0Conn, shard1Conn, "allPlansExecution", lowerUserLimit));
+    runTestOnPrimaries((shard0Conn, shard1Conn) =>
+                           testLimit(shard0Conn, shard1Conn, "allPlansExecution", higherUserLimit));
+}
 stWithMock.stop();
