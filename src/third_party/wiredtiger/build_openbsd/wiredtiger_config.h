14ac764fbd5c81819384837361bd0cd562a8a73c
Alexander Neben <alex.neben@mongodb.com>
2024-08-23 17:08:43 -0700

    Revert "SERVER-90718 port wiredtiger to bazel (#25991)" (#26480)
    GitOrigin-RevId: 0ab66b945dbc575d818d07c2d1d426c76f8fac8a

--- src/third_party/wiredtiger/build_openbsd/wiredtiger_config.h
+++ src/third_party/wiredtiger/build_openbsd/wiredtiger_config.h
@@ -7,8 +7,8 @@
 /* Define to 1 to pause for debugger attach on failure. */
 /* #undef HAVE_ATTACH */
 
-/* LZ4 support automatically loaded. */
-/* #undef HAVE_BUILTIN_EXTENSION_LZ4 */
+/* Build the LevelDB API with Basho LevelDB support. */
+/* #undef HAVE_BASHOLEVELDB */
 
 /* Snappy support automatically loaded. */
 /* #undef HAVE_BUILTIN_EXTENSION_SNAPPY */
@@ -16,47 +16,47 @@
 /* Zlib support automatically loaded. */
 /* #undef HAVE_BUILTIN_EXTENSION_ZLIB */
 
-/* ZSTD support automatically loaded. */
-/* #undef HAVE_BUILTIN_EXTENSION_ZSTD */
-
-/* libsodium support automatically loaded. */
-/* #undef HAVE_BUILTIN_EXTENSION_SODIUM */
-
 /* Define to 1 if you have the `clock_gettime' function. */
-/* #undef HAVE_CLOCK_GETTIME */
+#define HAVE_CLOCK_GETTIME 1
 
 /* Define to 1 for diagnostic tests. */
 /* #undef HAVE_DIAGNOSTIC */
 
 /* Define to 1 if you have the <dlfcn.h> header file. */
-/* #undef HAVE_DLFCN_H */
+#define HAVE_DLFCN_H 1
 
 /* Define to 1 if you have the `fallocate' function. */
-@HAVE_FALLOCATE@
+/* #undef HAVE_FALLOCATE */
+
+/* Define to 1 if you have the `fcntl' function. */
+#define HAVE_FCNTL 1
 
 /* Define to 1 if you have the `fdatasync' function. */
-/* #undef HAVE_FDATASYNC */
+#define HAVE_FDATASYNC 1
+
+/* Define to 1 if you have the `fread_unlocked' function. */
+/* #undef HAVE_FREAD_UNLOCKED */
 
 /* Define to 1 if you have the `ftruncate' function. */
-/* #undef HAVE_FTRUNCATE */
+#define HAVE_FTRUNCATE 1
 
 /* Define to 1 if you have the `gettimeofday' function. */
-/* #undef HAVE_GETTIMEOFDAY */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Build the LevelDB API with HyperLevelDB support. */
+/* #undef HAVE_HYPERLEVELDB */
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
+/* Define to 1 if you have the `bz2' library (-lbz2). */
+/* #undef HAVE_LIBBZ2 */
+
 /* Define to 1 if you have the `dl' library (-ldl). */
 /* #undef HAVE_LIBDL */
 
-/* Define to 1 if you have the `lz4' library (-llz4). */
-/* #undef HAVE_LIBLZ4 */
-
-/* Define to 1 if you have the `memkind' library (-lmemkind). */
-/* #undef HAVE_LIBMEMKIND */
-
 /* Define to 1 if you have the `pthread' library (-lpthread). */
-/* #undef HAVE_LIBPTHREAD */
+#define HAVE_LIBPTHREAD 1
 
 /* Define to 1 if you have the `rt' library (-lrt). */
 /* #undef HAVE_LIBRT */
@@ -64,32 +64,11 @@
 /* Define to 1 if you have the `snappy' library (-lsnappy). */
 /* #undef HAVE_LIBSNAPPY */
 
-/* Define to 1 if you have the `tcmalloc' library (-ltcmalloc). */
-/* #undef HAVE_LIBTCMALLOC */
-
 /* Define to 1 if you have the `z' library (-lz). */
 /* #undef HAVE_LIBZ */
 
-/* Define to 1 if you have the `zstd' library (-lzstd). */
-/* #undef HAVE_LIBZSTD */
-
-/* Define to 1 if you have the `sodium' library (-lsodium). */
-/* #undef HAVE_LIBSODIUM */
-
 /* Define to 1 if you have the <memory.h> header file. */
-/* #undef HAVE_MEMORY_H */
-
-/* Define to 1 to disable any crc32 hardware support. */
-/* #undef HAVE_NO_CRC32_HARDWARE */
-
-/* Define to 1 to disable standalone wiredtiger build. */
-/* #undef WT_STANDALONE_BUILD */
-
-/* Define to 1 if pthread condition variables support monotonic clocks. */
-/* #undef HAVE_PTHREAD_COND_MONOTONIC */
-
-/* Define to 1 if you have the `setrlimit' function. */
-/* #undef HAVE_SETRLIMIT */
+#define HAVE_MEMORY_H 1
 
 /* Define to 1 if you have the `posix_fadvise' function. */
 /* #undef HAVE_POSIX_FADVISE */
@@ -98,10 +77,16 @@
 /* #undef HAVE_POSIX_FALLOCATE */
 
 /* Define to 1 if you have the `posix_madvise' function. */
-/* #undef HAVE_POSIX_MADVISE */
+#define HAVE_POSIX_MADVISE 1
 
 /* Define to 1 if you have the `posix_memalign' function. */
-/* #undef HAVE_POSIX_MEMALIGN */
+#define HAVE_POSIX_MEMALIGN 1
+
+/* Define to 1 if you have the <pthread_np.h> header file. */
+/* #undef HAVE_PTHREAD_NP_H */
+
+/* Build the LevelDB API with RocksDB support. */
+/* #undef HAVE_ROCKSDB */
 
 /* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
@@ -110,16 +95,16 @@
 #define HAVE_STDLIB_H 1
 
 /* Define to 1 if you have the <strings.h> header file. */
-/* #undef HAVE_STRINGS_H */
+#define HAVE_STRINGS_H 1
 
 /* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
 
 /* Define to 1 if you have the `strtouq' function. */
-/* #undef HAVE_STRTOUQ */
+#define HAVE_STRTOUQ 1
 
 /* Define to 1 if you have the `sync_file_range' function. */
-@HAVE_SYNC_FILE_RANGE@
+/* #undef HAVE_SYNC_FILE_RANGE */
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
@@ -127,17 +112,17 @@
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
-/* Define to 1 if you have the `timer_create' function. */
-/* #undef HAVE_TIMER_CREATE */
-
 /* Define to 1 if you have the <unistd.h> header file. */
-/* #undef HAVE_UNISTD_H */
+#define HAVE_UNISTD_H 1
+
+/* Enable verbose message configuration. */
+/* #undef HAVE_VERBOSE */
 
 /* Define to 1 if you have the <x86intrin.h> header file. */
-#define HAVE_X86INTRIN_H 1
+/* #undef HAVE_X86INTRIN_H */
 
 /* Spinlock type from mutex.h. */
-#define SPINLOCK_TYPE SPINLOCK_MSVC
+#define SPINLOCK_TYPE SPINLOCK_PTHREAD_MUTEX
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
@@ -157,16 +142,16 @@
 /* Default alignment of buffers used for I/O */
 #define WT_BUFFER_ALIGNMENT_DEFAULT 0
 
+/* Size of a transaction timestamp in bytes */
+#define WT_TIMESTAMP_SIZE 8
+
 /* Enable large inode numbers on Mac OS X 10.5.  */
-/* #ifndef _DARWIN_USE_64_BIT_INODE */
-/* # define _DARWIN_USE_64_BIT_INODE 1 */
-/* #endif */
+#ifndef _DARWIN_USE_64_BIT_INODE
+# define _DARWIN_USE_64_BIT_INODE 1
+#endif
 
 /* Number of bits in a file offset, on hosts where this is settable. */
 /* #undef _FILE_OFFSET_BITS */
 
 /* Define for large files, on AIX-style hosts. */
 /* #undef _LARGE_FILES */
-
-/* Define for arm neon intrinsics. */
-@HAVE_ARM_NEON_INTRIN_H@
