00da7f442804d6118cc4bbc57caf9862e9811e2b
Daniel Moody <dmoody256@gmail.com>
2024-08-27 11:31:07 -0500

    SERVER-94055 fix bazel header list gen to not list gen headers redundantly (#26535)
    GitOrigin-RevId: ef54bd205937b736caf6053a899e505a1d77638c

--- buildscripts/convert_bazel_headers.py
+++ buildscripts/convert_bazel_headers.py
@@ -18,6 +18,7 @@ def work(target_library: str, silent: bool, cpu_count: int, cc: List[str]):
     original_headers = set()
 
     def get_headers(line):
+        nonlocal headers
         try:
             with open(target_library + ".bazel_headers") as f:
                 bazel_headers = [line.strip() for line in f.readlines()]
@@ -103,6 +104,10 @@ def work(target_library: str, silent: bool, cpu_count: int, cc: List[str]):
             line = line.strip()
             line = line.replace("build/opt", "//src")
             line = line[: line.find(".")] + ".cpp"
+            src_header = os.path.splitext(line[2:])[0] + ".h"
+            if os.path.exists(src_header):
+                src_header = "//" + ":".join(src_header.rsplit("/", 1))
+                headers.add(src_header)
             line = ":".join(line.rsplit("/", 1))
             if line.endswith("_gen.cpp"):
                 line = line[:-4]
@@ -160,26 +165,33 @@ def work(target_library: str, silent: bool, cpu_count: int, cc: List[str]):
 
     recommended_deps = set()
     minimal_headers = set()
+
+    basename_sources = [os.path.splitext(src.rsplit(":", 1)[1])[0] for src in sources]
     for header in headers:
+        header_basename = os.path.splitext(header.rsplit(":", 1)[1])[0]
+        if header_basename in basename_sources:
+            minimal_headers.add(header)
+            continue
+
         if header in reverse_header_map:
             found = False
             for lib in reverse_header_map[header]:
-                if lib in gen_header_map:
-                    minimal_headers.add(lib)
-                    found = True
-                    break
-            if not found:
-                for lib in reverse_header_map[header]:
-                    recommended_deps.add(lib)
+                recommended_deps.add(lib)
         else:
             if not header.endswith(global_headers):
                 minimal_headers.add(header)
 
     deps_order_by_height = []
     deps_queries = {}
-    for dep in recommended_deps:
+
+    with open(target_library + ".bazel_deps") as f:
+        original_deps = [line.strip() for line in f.readlines()]
+
+    for dep in recommended_deps | set(original_deps):
         p = subprocess.run(
-            [bazel_exec, "cquery"] + bazel_config + [f'kind("extract_debuginfo", deps("@{dep}"))'],
+            [bazel_exec, "cquery"]
+            + bazel_config
+            + [f'kind("extract_debuginfo|idl_generator|render_template", deps("@{dep}"))'],
             capture_output=True,
             text=True,
         )
@@ -199,9 +211,26 @@ def work(target_library: str, silent: bool, cpu_count: int, cc: List[str]):
         path_header = "/".join(header.rsplit(":", 1))
         path_header = path_header[2:]
         for dep in deps_order_by_height:
-            if path_header in header_map[dep]:
+            if dep in header_map and path_header in header_map[dep]:
                 optimal_header_deps.add(dep)
                 break
+            found = False
+            for other_dep in deps_order_by_height:
+                if other_dep in gen_header_map:
+                    continue
+                if dep in deps_queries[other_dep]:
+                    optimal_header_deps.add(other_dep)
+                    found = True
+                    break
+            if found:
+                continue
+            if dep in gen_header_map:
+                minimal_headers.add(dep)
+            else:
+                raise Exception(
+                    f"Should not happen, did not find way to add dep {dep} for {target_library}"
+                )
+
     optimal_header_deps = list(optimal_header_deps)
 
     working_deps = optimal_header_deps.copy()
@@ -213,12 +242,9 @@ def work(target_library: str, silent: bool, cpu_count: int, cc: List[str]):
                 if test_dep in working_deps and test_dep in deps_queries[dep]:
                     working_deps.remove(test_dep)
 
-    with open(target_library + ".bazel_deps") as f:
-        original_deps = f.readlines()
-
     link_deps = []
     header_deps = []
-    for dep in sorted(list(working_deps) + list(set(original_deps))):
+    for dep in sorted(list(set(list(working_deps) + list(set(original_deps))))):
         if dep in original_deps:
             link_deps.append(dep)
         else:
@@ -228,28 +254,30 @@ def work(target_library: str, silent: bool, cpu_count: int, cc: List[str]):
     if target_name.startswith("lib"):
         target_name = target_name[3:]
 
+    bazel_target = f"{target_library}\n"
+    bazel_target += "=" * 50 + "\n"
     local_bazel_path = os.path.dirname(target_library.replace("build/opt", "//src")) + ":"
-    bazel_target = "mongo_cc_library(\n"
+    bazel_target += "mongo_cc_library(\n"
     bazel_target += f'    name = "{target_name}",\n'
     if sources:
         bazel_target += "    srcs = [\n"
-        for src in sources:
-            bazel_target += f'        "{src.replace(local_bazel_path, "")}",\n'
+        for src in sorted([src.replace(local_bazel_path, "") for src in sources]):
+            bazel_target += f'        "{src}",\n'
         bazel_target += "    ],\n"
     if minimal_headers:
         bazel_target += "    hdrs = [\n"
-        for header in sorted(minimal_headers):
-            bazel_target += f'        "{header.replace(local_bazel_path, "")}",\n'
+        for header in sorted([header.replace(local_bazel_path, "") for header in minimal_headers]):
+            bazel_target += f'        "{header}",\n'
         bazel_target += "    ],\n"
     if header_deps:
         bazel_target += "    header_deps = [\n"
-        for dep in sorted(header_deps):
-            bazel_target += f'        "{dep.strip().replace(local_bazel_path, "")}",\n'
+        for dep in sorted([dep.strip().replace(local_bazel_path, "") for dep in header_deps]):
+            bazel_target += f'        "{dep}",\n'
         bazel_target += "    ],\n"
     if link_deps:
         bazel_target += "    deps = [\n"
-        for dep in sorted(link_deps):
-            bazel_target += f'        "{dep.strip().replace(local_bazel_path, "")}",\n'
+        for dep in sorted([dep.strip().replace(local_bazel_path, "") for dep in link_deps]):
+            bazel_target += f'        "{dep}",\n'
         bazel_target += "    ],\n"
     bazel_target += ")\n"
     return bazel_target
@@ -259,6 +287,7 @@ def main(
     target_libraries: Annotated[List[str], typer.Argument()],
     silent: Annotated[bool, typer.Option()] = False,
     skip_scons: Annotated[bool, typer.Option()] = False,
+    debug_mode: Annotated[bool, typer.Option()] = False,
 ):
     extra_args = []
     if os.name == "nt":
@@ -314,18 +343,25 @@ def main(
 
     with open("compile_commands.json") as f:
         cc = json.load(f)
-
     if platform.system() == "Linux":
         cpu_count = len(os.sched_getaffinity(0)) + 4
     else:
         cpu_count = os.cpu_count() + 4
 
-    with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count) as executor:
-        jobs = {
-            executor.submit(work, target_library, silent, cpu_count, cc): target_library
-            for target_library in target_libraries
-        }
-        bazel_targets = [job.result() for job in concurrent.futures.as_completed(jobs)]
+    # Process pool makes it harder to debug what is happening
+    # so for debug mode, we disabled process pool so things happen in order
+    # you can just print from the process.
+    if debug_mode:
+        bazel_targets = []
+        for target_library in target_libraries:
+            bazel_targets += [work(target_library, silent, cpu_count, cc)]
+    else:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=cpu_count) as executor:
+            jobs = {
+                executor.submit(work, target_library, silent, cpu_count, cc): target_library
+                for target_library in target_libraries
+            }
+            bazel_targets = [job.result() for job in concurrent.futures.as_completed(jobs)]
 
     print("====== Bazel Targets ======\n")
     print("\n".join(bazel_targets))
