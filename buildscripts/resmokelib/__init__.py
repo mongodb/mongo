829edcabf584130d2ef8719d8eabb3c2aec46289
Juan Gu <111318390+juangugit@users.noreply.github.com>
2024-08-30 15:38:23 -0700

    SERVER-94305 Revert cryptography update to resolve deprecated warning (#26670)
    GitOrigin-RevId: 03ae245aede237e404628e103e838667b60131bf

--- buildscripts/resmokelib/__init__.py
+++ buildscripts/resmokelib/__init__.py
@@ -9,10 +9,3 @@ from buildscripts.resmokelib import sighandler
 from buildscripts.resmokelib import suitesconfig
 from buildscripts.resmokelib import testing
 from buildscripts.resmokelib import utils
-
-import warnings
-
-# TODO：SERVER-93552： Bumping pymongo to the new version
-warnings.filterwarnings(
-    "ignore", message="Properties that return a naïve datetime", category=UserWarning
-)
