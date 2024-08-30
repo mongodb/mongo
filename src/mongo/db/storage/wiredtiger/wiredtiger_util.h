c4545396e6975a54470444faf5ae99e40ab14214
Varun Ravichandran <varun.ravichandran@mongodb.com>
2024-08-30 15:32:18 -0500

    SERVER-94050: Lock mutex at the start of WiredTigerEventHandler::getWtConnReadyStatus (#26616)
    GitOrigin-RevId: 0ed24f52f011fc16cd968368ace216fe7e747723

--- src/mongo/db/storage/wiredtiger/wiredtiger_util.h
+++ src/mongo/db/storage/wiredtiger/wiredtiger_util.h
@@ -203,6 +203,7 @@ public:
      * true. This function should only be used for tests.
      */
     bool getWtConnReadyStatus() {
+        stdx::unique_lock<mongo::Mutex> lock(_mutex);
         return _wtConnReady;
     }
 
