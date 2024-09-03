0549e26bb73e360adffa2829b6a65244ed536836
Yuhong Zhang <yuhong.zhang@mongodb.com>
2024-09-03 17:11:57 +0100

    SERVER-94360 Use its own test database for tailable_cursor_invalidation.js (#26693)
    GitOrigin-RevId: 637bd8fd8d949ff659b852f3a40631f599abf43d

--- jstests/core/query/cursor/tailable_cursor_invalidation.js
+++ jstests/core/query/cursor/tailable_cursor_invalidation.js
@@ -16,18 +16,20 @@ import {FixtureHelpers} from "jstests/libs/fixture_helpers.js";
 // Tests for the behavior of tailable cursors when a collection is dropped or the cursor is
 // otherwise invalidated.
 
-const collName = "tailable_cursor_invalidation";
-const coll = db[collName];
+const dbName = jsTestName();
+const collName = jsTestName();
+const testDB = db.getSiblingDB(dbName);
+const coll = testDB[collName];
 coll.drop();
 
 // Test that you cannot open a tailable cursor on a non-existent collection.
-assert.eq(0, assert.commandWorked(db.runCommand({find: collName})).cursor.id);
-assert.eq(0, assert.commandWorked(db.runCommand({find: collName, tailable: true})).cursor.id);
+assert.eq(0, assert.commandWorked(testDB.runCommand({find: collName})).cursor.id);
+assert.eq(0, assert.commandWorked(testDB.runCommand({find: collName, tailable: true})).cursor.id);
 assert.eq(0,
-          assert.commandWorked(db.runCommand({find: collName, tailable: true, awaitData: true}))
+          assert.commandWorked(testDB.runCommand({find: collName, tailable: true, awaitData: true}))
               .cursor.id);
 
-if (FixtureHelpers.isMongos(db) || TestData.testingReplicaSetEndpoint) {
+if (FixtureHelpers.isMongos(testDB) || TestData.testingReplicaSetEndpoint) {
     // In sharded cluster, if the database exists, the mongos will let you establish a cursor with
     // batch size 0 and return to you before it realizes the shard's cursor is exhausted. The next
     // getMore should return a 0 cursor id though.
@@ -36,18 +38,18 @@ if (FixtureHelpers.isMongos(db) || TestData.testingReplicaSetEndpoint) {
     // collection doesn't exist, and return a 0 cursor id.
     //
     // Drop the database to make sure mongos will behave the same as mongod would.
-    db.dropDatabase();
+    testDB.dropDatabase();
 }
 
 assert.eq(0,
           assert
-              .commandWorked(
-                  db.runCommand({find: collName, tailable: true, awaitData: true, batchSize: 0}))
+              .commandWorked(testDB.runCommand(
+                  {find: collName, tailable: true, awaitData: true, batchSize: 0}))
               .cursor.id);
 
 function dropAndRecreateColl() {
     coll.drop();
-    assert.commandWorked(db.createCollection(collName, {capped: true, size: 1024}));
+    assert.commandWorked(testDB.createCollection(collName, {capped: true, size: 1024}));
     const numDocs = 4;
     const bulk = coll.initializeUnorderedBulkOp();
     for (let i = 0; i < numDocs; ++i) {
@@ -63,7 +65,7 @@ dropAndRecreateColl();
  */
 function openCursor({tailable, awaitData}) {
     const findRes = assert.commandWorked(
-        db.runCommand({find: collName, tailable: tailable, awaitData: awaitData}));
+        testDB.runCommand({find: collName, tailable: tailable, awaitData: awaitData}));
     assert.neq(findRes.cursor.id, 0);
     assert.eq(findRes.cursor.ns, coll.getFullName());
     return findRes.cursor.id;
@@ -72,9 +74,9 @@ function openCursor({tailable, awaitData}) {
 // Test that the cursor dies on getMore if the collection has been dropped.
 let cursorId = openCursor({tailable: true, awaitData: false});
 dropAndRecreateColl();
-assert.commandFailedWithCode(db.runCommand({getMore: cursorId, collection: collName}),
+assert.commandFailedWithCode(testDB.runCommand({getMore: cursorId, collection: collName}),
                              [ErrorCodes.QueryPlanKilled, ErrorCodes.NamespaceNotFound]);
 cursorId = openCursor({tailable: true, awaitData: true});
 dropAndRecreateColl();
-assert.commandFailedWithCode(db.runCommand({getMore: cursorId, collection: collName}),
+assert.commandFailedWithCode(testDB.runCommand({getMore: cursorId, collection: collName}),
                              [ErrorCodes.QueryPlanKilled, ErrorCodes.NamespaceNotFound]);
