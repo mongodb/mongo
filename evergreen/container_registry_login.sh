0df99863f75fe8a8a2c79935004573d3cf2e9c15
Zakhar Kleyman <zakhar.kleyman@mongodb.com>
2024-09-13 18:18:11 -0400

    SERVER-94794 Add retries to container registry login command (#27065)
    GitOrigin-RevId: 2d768787bd3ca56d96568f3e5b3dab273797adc2

--- evergreen/container_registry_login.sh
+++ evergreen/container_registry_login.sh
@@ -4,4 +4,11 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 cd src
 
 set -oe
-echo "${release_tools_container_registry_password}" | podman login --password-stdin --username ${release_tools_container_registry_username} ${release_tools_container_registry}
+attempts=0
+max_attempts=4
+
+while ! echo "${release_tools_container_registry_password}" | podman login --password-stdin --username ${release_tools_container_registry_username} ${release_tools_container_registry}; do
+  [ "$attempts" -ge "$max_attempts" ] && exit 1
+  ((attempts++))
+  sleep 10
+done
