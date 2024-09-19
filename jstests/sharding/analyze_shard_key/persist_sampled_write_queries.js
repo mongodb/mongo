aad47e03871378e633fc37a1691ca05d85fb6dc5
Kruti Shah <70412403+krutishah139@users.noreply.github.com>
2024-09-19 11:56:44 -0400

    SERVER-89055 Fixes unique namespace generation for query sampling tests (#24593) (#27233)
    GitOrigin-RevId: 3c05b711905aeacb75c5bc5acd835e48d417e131

--- jstests/sharding/analyze_shard_key/persist_sampled_write_queries.js
+++ jstests/sharding/analyze_shard_key/persist_sampled_write_queries.js
@@ -8,6 +8,7 @@
 "use strict";
 
 load("jstests/libs/config_shard_util.js");
+load("jstests/libs/uuid_util.js");
 load("jstests/sharding/analyze_shard_key/libs/query_sampling_util.js");
 
 const supportedTestCases = [
@@ -27,7 +28,7 @@ function testWriteCmd(rst, cmdOpts, testCase) {
     // If running on the config server, use "config" as the database name since it is illegal to
     // create a user database on the config server.
     const dbName = rst.isConfigRS ? "config" : "testDb";
-    const collName = "testColl-" + cmdOpts.cmdName + "-" + QuerySamplingUtil.generateRandomString();
+    const collName = "testColl-" + cmdOpts.cmdName + "-" + extractUUIDFromObject(UUID());
     const ns = dbName + "." + collName;
 
     const primary = rst.getPrimary();
@@ -199,7 +200,7 @@ function testFindAndModifyCmd(rst, testCases) {
 
 function testInsertCmd(rst) {
     const dbName = "testDb";
-    const collName = "testColl-insert-" + QuerySamplingUtil.generateRandomString();
+    const collName = "testColl-insert-" + extractUUIDFromObject(UUID());
     const primary = rst.getPrimary();
     const db = primary.getDB(dbName);
     // Verify that no mongods support persisting sampled insert queries. Specifically, "sampleId"
