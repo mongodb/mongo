e9016c5fc19ccd3fea5444a5f417a7232d479c63
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2024-09-20 10:18:40 +0300

    SERVER-94785 Detect file changes in the commit-queue for burn-in (#27155)
    GitOrigin-RevId: 1532772c8efaa1fa25b3cfa4d86e489d6f74f92e

--- evergreen/generate_version_burn_in.sh
+++ evergreen/generate_version_burn_in.sh
@@ -8,7 +8,12 @@ set -o verbose
 
 setup_mongo_task_generator
 activate_venv
+
+base_revision="$(git merge-base ${revision} HEAD)"
+echo "Base patch revision: $base_revision"
+
 $python buildscripts/burn_in_tests.py generate-test-membership-map-file-for-ci
+
 RUST_BACKTRACE=full PATH=$PATH:$HOME:/ ./mongo-task-generator \
   --expansion-file ../expansions.yml \
   --evg-auth-file ./.evergreen.yml \
@@ -16,4 +21,5 @@ RUST_BACKTRACE=full PATH=$PATH:$HOME:/ ./mongo-task-generator \
   --generate-sub-tasks-config etc/generate_subtasks_config.yml \
   --s3-test-stats-endpoint https://mongo-test-stats.s3.amazonaws.com \
   --burn-in \
+  --burn-in-tests-command "python buildscripts/burn_in_tests.py run --origin-rev=$base_revision" \
   $@
