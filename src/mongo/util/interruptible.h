a85bf29a5872a4ccb7191160fefff4fe5f784301
George Wangensteen <george.wangensteen@mongodb.com>
2024-09-24 17:32:15 -0400

    SERVER-94963 Cleanup Interruptible post removal of diagnostic latch machinery (#27285)
    GitOrigin-RevId: 4dc51ac85415e9af440bda1cc5720735fece16d6

--- src/mongo/util/interruptible.h
+++ src/mongo/util/interruptible.h
@@ -33,7 +33,6 @@
 #include <boost/none.hpp>
 #include <boost/optional/optional.hpp>
 // IWYU pragma: no_include "cxxabi.h"
-#include <algorithm>
 #include <mutex>
 #include <utility>
 #include <vector>
@@ -65,134 +64,17 @@ auto doWithoutLock(BasicLockableAdapter m, Callable&& callable) {
 }  // namespace interruptible_detail
 
 /**
- * A type which can be used to wait on condition variables with a level triggered one-way interrupt.
- * I.e. after the interrupt is triggered (via some non-public api call) subsequent calls to
- * waitForConditionXXX will fail.  Interrupts must unblock all callers of waitForConditionXXX.
+ * A type which can be used to perform an interruptible wait on a stdx::condition_variable.
  *
- * This class should never be derived from directly. Instead, please derive from Interruptible.
+ * The interrupt is a one-time event; after the interrupt is triggered (via some non-public api
+ * call), _all_ subsequent calls to waitForConditionXXX will fail. Interrupts must unblock all
+ * callers of waitForConditionXXX.
  */
-class InterruptibleBase {
+class Interruptible {
 public:
-    virtual ~InterruptibleBase() = default;
-
-    /**
-     * Returns the deadline for this InterruptibleBase, or Date_t::max() if there is no deadline.
-     */
-    virtual Date_t getDeadline() const = 0;
-
-    /**
-     * Returns Status::OK() unless this operation is in a killed state.
-     */
-    virtual Status checkForInterruptNoAssert() noexcept = 0;
-
-protected:
-    /**
-     * Same as Interruptible::waitForConditionOrInterruptUntil(), except returns
-     * StatusWith<stdx::cv_status> and a non-ok status indicates the error instead of a DBException.
-     */
-    virtual StatusWith<stdx::cv_status> waitForConditionOrInterruptNoAssertUntil(
-        stdx::condition_variable& cv, BasicLockableAdapter m, Date_t deadline) noexcept = 0;
-
-    struct DeadlineState {
-        Date_t deadline;
-        ErrorCodes::Error error;
-        bool hasArtificialDeadline;
-    };
-
-    /**
-     * Pushes a subsidiary deadline into the InterruptibleBase. Until an associated
-     * popArtificialDeadline() is invoked, the InterruptibleBase will fail checkForInterrupt() and
-     * waitForConditionOrInterrupt() calls with the passed error code if the deadline has passed.
-     *
-     * Note that deadline's higher than the current value are constrained (such that the passed
-     * error code will be returned/thrown, but after the min(oldDeadline, newDeadline) has passed).
-     *
-     * Returns state needed to pop the deadline.
-     */
-    virtual DeadlineState pushArtificialDeadline(Date_t deadline, ErrorCodes::Error error) = 0;
-
-    /**
-     * Pops the subsidiary deadline introduced by push.
-     */
-    virtual void popArtificialDeadline(DeadlineState) = 0;
-
-    /**
-     * Returns the equivalent of Date_t::now() + waitFor for the InterruptibleBase's clock
-     */
-    virtual Date_t getExpirationDateForWaitForValue(Milliseconds waitFor) = 0;
-};
-
-/**
- * An derived class of InterruptibleBase which provides a variety of helper functions
- *
- * Please derive from this class instead of InterruptibleBase.
- */
-class Interruptible : public InterruptibleBase {
-private:
-    /**
-     * Helper class to properly set _isWaiting for Interruptible instances.
-     * Every call sequence that waits on a condition/interrupt must hold an instance of WaitContext.
-     */
-    class WaitContext {
-    public:
-        WaitContext(Interruptible* interruptible) : _interruptible(interruptible) {
-            _interruptible->_isWaiting.store(true);
-        }
-
-        ~WaitContext() {
-            _interruptible->_isWaiting.store(false);
-        }
-
-    private:
-        Interruptible* const _interruptible;
-    };
-
-    /**
-     * A deadline guard provides a subsidiary deadline to the parent.
-     */
-    class DeadlineGuard {
-    public:
-        DeadlineGuard(const DeadlineGuard&) = delete;
-        DeadlineGuard& operator=(const DeadlineGuard&) = delete;
-
-        DeadlineGuard(DeadlineGuard&& other)
-            : _interruptible(other._interruptible), _oldDeadline(other._oldDeadline) {
-            other._interruptible = nullptr;
-        }
-
-        DeadlineGuard& operator=(DeadlineGuard&& other) = delete;
-
-        ~DeadlineGuard() {
-            if (_interruptible) {
-                _interruptible->popArtificialDeadline(_oldDeadline);
-            }
-        }
-
-    private:
-        friend Interruptible;
-
-        explicit DeadlineGuard(Interruptible& interruptible,
-                               Date_t newDeadline,
-                               ErrorCodes::Error error)
-            : _interruptible(&interruptible),
-              _oldDeadline(_interruptible->pushArtificialDeadline(newDeadline, error)) {}
-
-        Interruptible* _interruptible;
-        DeadlineState _oldDeadline;
-    };
-
-    DeadlineGuard makeDeadlineGuard(Date_t deadline, ErrorCodes::Error error) {
-        return DeadlineGuard(*this, deadline, error);
-    }
-
-public:
-    class WaitListener;
-
-    Interruptible() : _isWaiting(false) {}
-
     /**
      * Returns true if currently waiting for a condition/interrupt.
-     * This function relies on instances of WaitContext to properly set _isWaiting.
+     * This function relies on waitForConditionOrInterrupt to properly set _isWaiting.
      * Note that _isWaiting remains true until waitForConditionOrInterruptUntil() returns.
      */
     bool isWaitingForConditionOrInterrupt() const {
@@ -205,28 +87,7 @@ public:
     enum class WakeReason {
         kPredicate,
         kTimeout,
-        kInterrupt,
     };
-
-    static constexpr auto kFastWakeTimeout = Milliseconds(100);
-
-    /**
-     * Enum to convey if an Interruptible woke up before or after kFastWakeTimeout
-     */
-    enum class WakeSpeed {
-        kFast,
-        kSlow,
-    };
-
-    /**
-     * This function adds a WaitListener subclass to the triggers for certain actions.
-     *
-     * WaitListeners can only be added and not removed. If you wish to deactivate a WaitListeners
-     * subclass, please provide the switch on that subclass to noop its functions. It is only safe
-     * to add a WaitListener during a MONGO_INITIALIZER.
-     */
-    template <typename WaitListenerT>
-    static void installWaitListener();
 
     /**
      * Returns a statically allocated instance that cannot be interrupted.  Useful as a default
@@ -237,8 +98,9 @@ public:
     /**
      * Invokes the passed callback with a deadline guard active initialized with the passed
      * deadline.  Additionally handles the dance of try/catching the invocation and checking
-     * checkForInterrupt with the guard inactive (to allow a higher level timeout to override a
-     * lower level one)
+     * checkForInterrupt with the guard inactive, to allow an interruption or timeout error _not_
+     * caused by the provided deadline to propogate out / take precedence over an error produced
+     * due to the provided deadline.
      */
     template <typename Callback>
     decltype(auto) runWithDeadline(Date_t deadline, ErrorCodes::Error error, Callback&& cb) {
@@ -266,14 +128,6 @@ public:
     }
 
     /**
-     * Get a placeholder name for an arbitrary type
-     */
-    template <typename LockT>
-    static constexpr StringData getLatchName(const LockT&) {
-        return "AnonymousLockable"_sd;
-    }
-
-    /**
      * Waits on condition "cv" for "pred" until "pred" returns true, or the given "deadline"
      * expires, or this operation is interrupted, or this operation's own deadline expires.
      *
@@ -284,89 +138,63 @@ public:
     template <typename LockT, typename PredicateT>
     bool waitForConditionOrInterruptUntil(stdx::condition_variable& cv,
                                           LockT& m,
-                                          Date_t finalDeadline,
+                                          Date_t deadline,
                                           PredicateT pred) {
-        WaitContext waitContext(this);
-        auto latchName = getLatchName(m);
+        _isWaiting.store(true);
+        ON_BLOCK_EXIT([this] { _isWaiting.store(false); });
 
-        auto handleInterruptAndAssert = [&](Status status, WakeSpeed speed) {
-            _onWake(latchName, WakeReason::kInterrupt, speed);
-            iassert(std::move(status));
-        };
-
-        auto checkForInterruptWithoutLockAndAssert = [&](WakeSpeed speed) {
+        auto checkForInterruptWithoutLockAndAssert = [&]() {
             // We drop the lock before checking for interrupt since checkForInterruptNoAssert can
             // sometimes try to reacquire the same lock.
             if (auto status = interruptible_detail::doWithoutLock(
                     m, [&] { return checkForInterruptNoAssert(); });
                 !status.isOK()) {
-                handleInterruptAndAssert(status, speed);
+                iassert(std::move(status));
             }
         };
 
-        auto waitUntil = [&](Date_t deadline, WakeSpeed speed) -> boost::optional<WakeReason> {
+        auto waitUntilOptionalWakeReason = [&](Date_t deadline) -> boost::optional<WakeReason> {
             // If the result of waitForConditionOrInterruptNoAssertUntil() is non-spurious, return
             // a WakeReason. Otherwise, return boost::none
 
             auto swResult = waitForConditionOrInterruptNoAssertUntil(cv, m, deadline);
-            if (!swResult.isOK()) {
-                handleInterruptAndAssert(swResult.getStatus(), speed);
-            }
+            iassert(swResult);
 
             // Check if an interrupt occurred while waiting.
-            checkForInterruptWithoutLockAndAssert(speed);
+            checkForInterruptWithoutLockAndAssert();
 
             // Check the predicate after re-acquiring the lock.
             if (pred()) {
-                _onWake(latchName, WakeReason::kPredicate, speed);
                 return WakeReason::kPredicate;
             }
 
             if (swResult.getValue() == stdx::cv_status::timeout) {
-                _onWake(latchName, WakeReason::kTimeout, speed);
                 return WakeReason::kTimeout;
             }
 
             return boost::none;
         };
 
-        auto waitUntilNonSpurious = [&](Date_t deadline, WakeSpeed speed) -> WakeReason {
+        auto waitUntilRealWakeReason = [&](Date_t deadline) -> WakeReason {
             // Check for interrupt before waiting.
-            checkForInterruptWithoutLockAndAssert(speed);
+            checkForInterruptWithoutLockAndAssert();
 
             // Check the predicate after re-acquiring the lock and before waiting.
             if (pred()) {
-                _onWake(latchName, WakeReason::kPredicate, speed);
                 return WakeReason::kPredicate;
             }
 
-            // Check waitUntil() in a loop until it says it has a genuine WakeReason
-            auto maybeWakeReason = waitUntil(deadline, speed);
+            // Check waitUntilOptionalWakeReason() in a loop until it says it has a genuine
+            // WakeReason
+            auto maybeWakeReason = waitUntilOptionalWakeReason(deadline);
             while (!maybeWakeReason) {
-                maybeWakeReason = waitUntil(deadline, speed);
+                maybeWakeReason = waitUntilOptionalWakeReason(deadline);
             };
 
             return *maybeWakeReason;
         };
 
-        const auto traceDeadline = getExpirationDateForWaitForValue(kFastWakeTimeout);
-        const auto firstDeadline = std::min(traceDeadline, finalDeadline);
-
-        // Wait for the first deadline
-        if (auto wakeReason = waitUntilNonSpurious(firstDeadline, WakeSpeed::kFast);
-            wakeReason == WakeReason::kPredicate) {
-            // If our first wait fulfilled our predicate then return true
-            return true;
-        } else if (firstDeadline == finalDeadline) {
-            // If we didn't fulfill our predicate but finalDeadline was less than traceDeadline,
-            // then the wait should return false
-            return false;
-        }
-
-        _onLongSleep(latchName);
-
-        // Wait for the final deadline
-        if (auto wakeReason = waitUntilNonSpurious(finalDeadline, WakeSpeed::kSlow);
+        if (auto wakeReason = waitUntilRealWakeReason(deadline);
             wakeReason == WakeReason::kPredicate) {
             return true;
         } else {
@@ -418,78 +246,95 @@ public:
         invariant(!waitForConditionOrInterruptFor(cv, lk, duration, [] { return false; }));
     }
 
-protected:
-    class NotInterruptible;
+    struct DeadlineState {
+        Date_t deadline;
+        ErrorCodes::Error error;
+        bool hasArtificialDeadline;
+    };
 
-    void _onLongSleep(StringData name);
-    void _onWake(StringData name, WakeReason reason, WakeSpeed speed);
+    /**
+     * A deadline guard provides a subsidiary deadline to the parent.
+     */
+    class DeadlineGuard {
+    public:
+        DeadlineGuard(const DeadlineGuard&) = delete;
+        DeadlineGuard& operator=(const DeadlineGuard&) = delete;
 
-    static auto& _getListenerState() {
-        struct State {
-            std::vector<WaitListener*> list;
-        };
+        DeadlineGuard(DeadlineGuard&& other)
+            : _interruptible(other._interruptible), _oldDeadline(other._oldDeadline) {
+            other._interruptible = nullptr;
+        }
 
-        // Note that state should no longer be mutated after init-time (ala MONGO_INITIALIZERS). If
-        // this changes, than this state needs to be synchronized.
-        static State state;
-        return state;
+        DeadlineGuard& operator=(DeadlineGuard&& other) = delete;
+
+        ~DeadlineGuard() {
+            if (_interruptible) {
+                _interruptible->popArtificialDeadline(_oldDeadline);
+            }
+        }
+
+        DeadlineGuard(Interruptible& interruptible, Date_t newDeadline, ErrorCodes::Error error)
+            : _interruptible(&interruptible),
+              _oldDeadline(_interruptible->pushArtificialDeadline(newDeadline, error)) {}
+
+        Interruptible* _interruptible;
+        DeadlineState _oldDeadline;
+    };
+
+    DeadlineGuard makeDeadlineGuard(Date_t deadline, ErrorCodes::Error error) {
+        return DeadlineGuard(*this, deadline, error);
     }
+
+    /**
+     * Same as Interruptible::waitForConditionOrInterruptUntil(), except returns
+     * StatusWith<stdx::cv_status> and a non-ok status indicates the error instead of a DBException.
+     *
+     * The inheritor of Interruptible must provide this core implementation, which the remainder of
+     * the waitForConditionOrInterrupt... family of functions delegate to. The function should
+     * return with an appropriate status when `cv` is notified, `deadline` expires, or the
+     * Interruptible is interrupted.
+     */
+    virtual StatusWith<stdx::cv_status> waitForConditionOrInterruptNoAssertUntil(
+        stdx::condition_variable& cv, BasicLockableAdapter m, Date_t deadline) noexcept = 0;
+
+    virtual Date_t getDeadline() const = 0;
+
+    virtual Status checkForInterruptNoAssert() noexcept = 0;
+
+    /**
+     * Pushes a subsidiary deadline into the Interruptible. Until an associated
+     * popArtificialDeadline() is invoked, the Interruptible will fail checkForInterrupt() and
+     * waitForConditionOrInterrupt() calls with the passed error code if the deadline has passed.
+     *
+     * If a deadline later than the Interruptible's current deadline is provided, the provided
+     * error code will be used, but after the earlier of the existing deadline and new deadline has
+     * passed. In other words, this function cannot be used to 'extend' the current deadline of an
+     * interruptible, only to temporarily 'shorten' it or replace the error code used.
+     *
+     * Returns state needed to pop the deadline.
+     */
+    virtual DeadlineState pushArtificialDeadline(Date_t deadline, ErrorCodes::Error error) = 0;
+
+    /**
+     * Pops the subsidiary deadline introduced by push.
+     */
+    virtual void popArtificialDeadline(DeadlineState) = 0;
+
+    /**
+     * Returns the equivalent of Date_t::now() + waitFor for the Interruptible's clock
+     */
+    virtual Date_t getExpirationDateForWaitForValue(Milliseconds waitFor) = 0;
+
 
 private:
-    AtomicWord<bool> _isWaiting;
+    AtomicWord<bool> _isWaiting{false};
 };
-
-/**
- * A set of event handles for an Interruptible type
- */
-class Interruptible::WaitListener {
-public:
-    /**
-     * Action to do when a wait does not resolve quickly
-     *
-     * Any implementation of this function must be safe to invoke when an Interruptible-associated
-     * latch is held. As this is hard to reason about, avoid external latches whenever possible.
-     */
-    virtual void onLongSleep(StringData name) = 0;
-
-    /**
-     * Action to do when a wait resolves after a sleep
-     *
-     * Any implementation of this function must be safe to invoke when an Interruptible-associated
-     * latch is held. As this is hard to reason about, avoid external latches whenever possible.
-     */
-    virtual void onWake(StringData name, WakeReason reason, WakeSpeed speed) = 0;
-};
-
-template <typename WaitListenerT>
-inline void Interruptible::installWaitListener() {
-    auto& state = _getListenerState();
-
-    static auto* listener = new WaitListenerT();  // Intentionally leaked!
-    state.list.push_back(listener);
-}
-
-inline void Interruptible::_onLongSleep(StringData name) {
-    auto& state = _getListenerState();
-
-    for (auto listener : state.list) {
-        listener->onLongSleep(name);
-    }
-}
-
-inline void Interruptible::_onWake(StringData name, WakeReason reason, WakeSpeed speed) {
-    auto& state = _getListenerState();
-
-    for (auto listener : state.list) {
-        listener->onWake(name, reason, speed);
-    }
-}
 
 /**
  * A non-interruptible type which can be used as a lightweight default arg for Interruptible-taking
  * functions.
  */
-class Interruptible::NotInterruptible final : public Interruptible {
+class NotInterruptible final : public Interruptible {
     StatusWith<stdx::cv_status> waitForConditionOrInterruptNoAssertUntil(
         stdx::condition_variable& cv, BasicLockableAdapter m, Date_t deadline) noexcept override {
 
