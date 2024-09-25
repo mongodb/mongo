9f2f3908f67daa0abb1a63b54511cf8f3024c893
Yuhong Zhang <yuhong.zhang@mongodb.com>
2024-09-25 13:35:54 +0200

    Revert "SERVER-94542 Avoid race between consistency checks and shutdown in standalone_in_queryable_backup_mode.js (#27421)" (#27460)
    GitOrigin-RevId: 86f190da393d1bcba6b8fb1b61dc11da38b184c6

--- jstests/sharding/standalone_in_queryable_backup_mode.js
+++ jstests/sharding/standalone_in_queryable_backup_mode.js
@@ -12,6 +12,14 @@
 
 import {ShardingTest} from "jstests/libs/shardingtest.js";
 
+// This test shuts down a shard.
+TestData.skipCheckingUUIDsConsistentAcrossCluster = true;
+TestData.skipCheckingIndexesConsistentAcrossCluster = true;
+TestData.skipCheckDBHashes = true;
+TestData.skipCheckOrphans = true;
+TestData.skipCheckShardFilteringMetadata = true;
+TestData.skipCheckMetadataConsistency = true;
+
 (function() {
 'use strict';
 
@@ -42,8 +50,7 @@ jsTest.log("Going to hold the stable timestamp of the secondary node at " +
            tojson(recoveryTimestamp));
 // Hold back the recovery timestamp before doing another write so we have some oplog entries to
 // apply when restart in queryableBackupMode with recoverToOplogTimestamp.
-const secondary = st.rs0.getSecondary();
-assert.commandWorked(secondary.getDB('admin').adminCommand({
+assert.commandWorked(st.rs0.getSecondary().getDB('admin').adminCommand({
     "configureFailPoint": 'holdStableTimestampAtSpecificTimestamp',
     "mode": 'alwaysOn',
     "data": {"timestamp": recoveryTimestamp}
@@ -65,11 +72,9 @@ st.rs0.awaitReplication();
 jsTest.log("Going to stop the secondary node of the shard");
 const operationTime =
     assert.commandWorked(st.rs0.getPrimary().getDB(kDbName).runCommand({ping: 1})).operationTime;
-const secondaryPort = secondary.port;
-const secondaryDbPath = secondary.dbpath;
-// Remove the secondary from the cluster since we will restart it in queryable backup mode later.
-const secondaryId = st.rs0.getNodeId(secondary);
-st.rs0.remove(secondaryId);
+const secondaryPort = st.rs0.getSecondary().port;
+const secondaryDbPath = st.rs0.getSecondary().dbpath;
+MongoRunner.stopMongod(st.rs0.getSecondary());
 
 jsTest.log(
     "Going to start a mongod process with --shardsvr, --queryableBackupMode and recoverToOplogTimestamp");
