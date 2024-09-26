d2cdfc5677cd95ef1be652d6063a94525aba19ca
John Daniels <john.daniels@mongodb.com>
2024-09-26 11:59:01 -0400

    SERVER-92780: Move sys-perf tasks/variants into DSI. (#27392)
    GitOrigin-RevId: 116e196f5257a3ec91c6bb686f5f960d2d51b7ae

--- buildscripts/yamllinters.sh
+++ buildscripts/yamllinters.sh
@@ -7,6 +7,12 @@ yamllint -c etc/yamllint_config.yml buildscripts etc jstests
 
 PATH="$PATH:$HOME" evergreen evaluate etc/evergreen.yml > etc/evaluated_evergreen.yml
 PATH="$PATH:$HOME" evergreen evaluate etc/evergreen_nightly.yml > etc/evaluated_evergreen_nightly.yml
-PATH="$PATH:$HOME" evergreen evaluate etc/system_perf.yml > etc/evaluated_system_perf.yml
+
+# Remove references to the DSI repo before evergreen evaluate.
+# The DSI module references break 'evaluate', the system_perf config should
+# parse without them, and we don't want changes to the DSI repository to
+# break checking that the rest of the imports etc. work.
+awk '/lint_yaml trim start/{drop=1} /lint_yaml trim end/{drop=0} !drop' etc/system_perf.yml > etc/trimmed_system_perf.yml
+PATH="$PATH:$HOME" evergreen evaluate etc/trimmed_system_perf.yml > etc/evaluated_system_perf.yml
 
 python -m evergreen_lint -c ./etc/evergreen_lint.yml lint
