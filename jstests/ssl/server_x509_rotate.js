9144e9bc7276b6887550ee3fc792c316602ce04b
adriangzz <adriangonzalezmontemayor@gmail.com>
2024-10-02 19:19:43 -0600

    SERVER-87958 Update ssl/server_x509_rotate.js (#27007)
    GitOrigin-RevId: d62f777e43c3ccda7f4d458d049f7e48f58982a3

--- jstests/ssl/server_x509_rotate.js
+++ jstests/ssl/server_x509_rotate.js
@@ -2,52 +2,46 @@
 
 import {copyCertificateFile} from "jstests/ssl/libs/ssl_helpers.js";
 
+const OLD_SERVER = "jstests/libs/server.pem";
+const OLD_CLIENT = "jstests/libs/client.pem";
+const OLD_CA = "jstests/libs/ca.pem";
+
+const NEW_SERVER = "jstests/libs/trusted-server.pem";
+const NEW_CLIENT = "jstests/libs/trusted-client.pem";
+const NEW_CA = "jstests/libs/trusted-ca.pem";
+
 const dbPath = MongoRunner.toRealDir("$dataDir/cluster_x509_rotate_test/");
 mkdir(dbPath);
 
-copyCertificateFile("jstests/libs/ca.pem", dbPath + "/ca-test.pem");
-copyCertificateFile("jstests/libs/server.pem", dbPath + "/server-test.pem");
+copyCertificateFile(OLD_CA, dbPath + "/ca-test.pem");
+copyCertificateFile(OLD_SERVER, dbPath + "/server-test.pem");
 
 const mongod = MongoRunner.runMongod({
     tlsMode: "requireTLS",
     tlsCertificateKeyFile: dbPath + "/server-test.pem",
     tlsCAFile: dbPath + "/ca-test.pem"
 });
+const host = "localhost:" + mongod.port;
+
+// Assert we can connect using old certificates.
+const conn = new Mongo(host, undefined, {tls: {certificateKeyFile: OLD_CLIENT, CAFile: OLD_CA}});
+assert.commandWorked(conn.adminCommand({connectionStatus: 1}));
 
 // Rotate in new certificates
-copyCertificateFile("jstests/libs/trusted-ca.pem", dbPath + "/ca-test.pem");
-copyCertificateFile("jstests/libs/trusted-server.pem", dbPath + "/server-test.pem");
+copyCertificateFile(NEW_CA, dbPath + "/ca-test.pem");
+copyCertificateFile(NEW_SERVER, dbPath + "/server-test.pem");
 
 assert.commandWorked(mongod.getDB("test").rotateCertificates("Rotated!"));
 // make sure that mongo is still connected after rotation
 assert.commandWorked(mongod.adminCommand({connectionStatus: 1}));
 
-const host = "localhost:" + mongod.port;
-
 // Start shell with old certificates and make sure it can't connect
-let out = runMongoProgram("mongo",
-                          "--host",
-                          host,
-                          "--tls",
-                          "--tlsCertificateKeyFile",
-                          "jstests/libs/client.pem",
-                          "--tlsCAFile",
-                          "jstests/libs/ca.pem",
-                          "--eval",
-                          ";");
-assert.neq(out, 0, "Mongo invocation did not fail");
+assert.throws(() => {
+    new Mongo(host, undefined, {tls: {certificateKeyFile: OLD_CLIENT, CAFile: OLD_CA}});
+});
 
 // Start shell with new certificates and make sure it can connect
-out = runMongoProgram("mongo",
-                      "--host",
-                      host,
-                      "--tls",
-                      "--tlsCertificateKeyFile",
-                      "jstests/libs/trusted-client.pem",
-                      "--tlsCAFile",
-                      "jstests/libs/trusted-ca.pem",
-                      "--eval",
-                      ";");
-assert.eq(out, 0, "Mongo invocation failed");
+const conn2 = new Mongo(host, undefined, {tls: {certificateKeyFile: NEW_CLIENT, CAFile: NEW_CA}});
+assert.commandWorked(conn2.adminCommand({connectionStatus: 1}));
 
 MongoRunner.stopMongod(mongod);
