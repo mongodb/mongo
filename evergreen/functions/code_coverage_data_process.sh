738e20d92850845cd570efbd549072bb20db2bc6
Steve McClure <steve.mcclure@mongodb.com>
2024-10-04 14:30:19 -0400

    SERVER-95146: Support hybrid Scons/Bazel targets in code coverage (#27748)
    GitOrigin-RevId: 5aa8ccfc378134562afaeae7197dd18dbc092da6

--- evergreen/functions/code_coverage_data_process.sh
+++ evergreen/functions/code_coverage_data_process.sh
@@ -47,25 +47,21 @@ if [ -n "$gcno_files" ]; then
   fi
 fi
 
-# Look for evidence of SCons-produced artifacts
-if [ ! -d "./build/debug" ]; then
-  echo "No code coverage to process - no 'build/debug' directory found."
-  exit 0
-fi
-
-gcno_files=$(find ./build/debug -type f -name "*.gcno")
+# Look for evidence of instrumented artifacts
+gcno_files=$(find $BAZEL_OUTPUT_PATH -type f -name "*.gcno")
 if [ ! -n "$gcno_files" ]; then
   echo "No code coverage to process - no '.gcno' files found."
   exit 0
 fi
 
+echo "Found code coverage files:"
+find . -type f -name "*.gcno"
+find $BAZEL_OUTPUT_PATH -type f -name "*.gcno"
+
 if [ -z "${gcov_tool}" ]; then
   echo "No coverage tool defined. Set the gcov_tool expansion in evergreen.yml" >&2
   exit 0
 fi
-
-echo "Found code coverage files:"
-find ./build/debug -type f -name "*.gcno"
 
 activate_venv
 
@@ -79,26 +75,38 @@ export COVERALLS_REPO_TOKEN=${coveralls_token}
 export TRAVIS_PULL_REQUEST=${github_pr_number}
 export TRAVIS_JOB_ID=${revision_order_id}
 
+# SCons-built tests on SCons-built src:
+#   Object files found in the build/debug/ directory.
+#
+# SCons-built tests on Bazel-built src:
+#   Object files found in the (symlink) bazel-out/ directory.
 gcovr \
   --output gcovr-coveralls.json \
   --coveralls-pretty \
-  --exclude 'build/debug/.*_gen\.(h|hpp|cpp)' \
-  --exclude build/debug/mongo/db/cst/grammar.yy \
-  --exclude build/debug/mongo/idl/ \
-  --exclude 'src/mongo/.*_test\.(h|hpp|cpp)' \
-  --exclude src/mongo/db/modules/enterprise/src/streams/third_party/ \
-  --exclude src/mongo/dbtests/ \
-  --exclude src/mongo/unittest/ \
-  --exclude src/third_party/ \
+  --txt gcovr-coveralls.txt \
+  --print-summary \
+  --exclude 'build/debug/.*' \
+  --exclude '.*bazel-out/.*' \
+  --exclude '.*external/mongo_toolchain/.*' \
+  --exclude '.*src/.*_gen\.(h|hpp|cpp)' \
+  --exclude '.*src/mongo/db/cst/grammar\.yy' \
+  --exclude '.*src/mongo/idl/.*' \
+  --exclude '.*src/mongo/.*_test\.(h|hpp|cpp)' \
+  --exclude '.*src/mongo/dbtests/.*' \
+  --exclude '.*src/mongo/unittest/.*' \
+  --exclude '.*/third_party/.*' \
   --gcov-ignore-errors source_not_found \
   --gcov-ignore-parse-errors negative_hits.warn \
-  --gcov-exclude-directories build/debug/mongo/db/modules/enterprise/src/streams/third_party \
-  --gcov-exclude-directories build/debug/mongo/dbtests/ \
-  --gcov-exclude-directories build/debug/mongo/idl/ \
-  --gcov-exclude-directories build/debug/mongo/unittest/ \
-  --gcov-exclude-directories build/debug/third_party/ \
+  --gcov-exclude-directories '.*src/mongo/dbtests/.*' \
+  --gcov-exclude-directories '.*src/mongo/idl/.*' \
+  --gcov-exclude-directories '.*src/mongo/unittest/.*' \
+  --gcov-exclude-directories '.*/third_party/.*' \
   --gcov-executable ${gcov_tool} \
-  build/debug
+  build/debug \
+  $BAZEL_OUTPUT_PATH
+
+# display tabulated stats - easy to parse and spot-check in test logs
+cat gcovr-coveralls.txt
 
 # Upload json data; view at https://coveralls.io/github/10gen/mongo
 curl -X POST https://coveralls.io/api/v1/jobs -F 'json_file=@gcovr-coveralls.json'
