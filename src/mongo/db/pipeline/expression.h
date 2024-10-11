26c9edbe63549eb3fca7c06ef55d8e06e1a8b570
Henri Nikku <henri.nikku@mongodb.com>
2024-10-11 10:51:03 +0100

    SERVER-95347 Enforce additional checks in $setField (#27751)
    GitOrigin-RevId: 59f662198b6e7484e3183ab89c04859022e57e33

--- src/mongo/db/pipeline/expression.h
+++ src/mongo/db/pipeline/expression.h
@@ -4497,7 +4497,8 @@ public:
                        boost::intrusive_ptr<Expression> field,
                        boost::intrusive_ptr<Expression> input,
                        boost::intrusive_ptr<Expression> value)
-        : Expression(expCtx, {std::move(field), std::move(input), std::move(value)}) {
+        : Expression(expCtx, {std::move(field), std::move(input), std::move(value)}),
+          _fieldName(getValidFieldName(_children[_kField])) {
         expCtx->sbeCompatibility = SbeCompatibility::notCompatible;
     }
 
@@ -4518,6 +4519,15 @@ public:
     static constexpr auto kExpressionName = "$setField"_sd;
 
 private:
+    /**
+     * Ensures 'fieldExpr' is a constant string representing a valid field name and returns it as a
+     * string. If 'fieldExpr' is not valid, this function will throw a 'uassert()'.
+     */
+    std::string getValidFieldName(boost::intrusive_ptr<Expression> fieldExpr);
+
+    // This is pre-validated by the constructor.
+    const std::string _fieldName;
+
     static constexpr size_t _kField = 0;
     static constexpr size_t _kInput = 1;
     static constexpr size_t _kValue = 2;
