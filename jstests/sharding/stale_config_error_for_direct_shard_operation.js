16f5048edafc00b43dd384933f99a7ed91f99e88
atesteve <aitor.esteve@mongodb.com>
2024-10-14 10:54:42 +0200

    SERVER-95488 stale_config_error_for_direct_shard_operation.js: Repeat the test setup before retrying the transaction (#28002)
    GitOrigin-RevId: 89643d8af84f3b9af91885b0dc577fd52e0944ec

--- jstests/sharding/stale_config_error_for_direct_shard_operation.js
+++ jstests/sharding/stale_config_error_for_direct_shard_operation.js
@@ -15,31 +15,42 @@ let rst = new ReplSetTest({
 rst.startSet({configsvr: ""});
 rst.initiate();
 
-let primary = rst.getPrimary();
-let testDB = primary.getDB("test");
-const kSessionId = {
-    id: UUID()
-};
-for (let i = 0; i < 5; i++) {
-    testDB.runCommand({insert: `test${i}`, documents: [{x: 1}]});
-}
+let base = 0;
+let nextBase = 0;
+let primary;
+let testDB;
+let sessionId;
 
-withRetryOnTransientTxnError(() => {
-    // Step-up a new primary so that the first writes for collections test{0-4} will fail with a
-    // StaleConfig error. This happens because the new primary will initially not have the sharding
-    // metadata for the collections. The metadata will only be recovered after the first failing
-    // write for each collection.
+function setup() {
+    base = nextBase;
+    nextBase += 5;
+    primary = rst.getPrimary();
+    testDB = primary.getDB("test");
+    sessionId = {id: UUID()};
+
+    for (let i = 0; i < 5; i++) {
+        testDB.runCommand({insert: `test${i + base}`, documents: [{x: 1}]});
+    }
+
+    // Step-up a new primary so that the first writes for collections test{base + 0-4} will fail
+    // with a StaleConfig error. This happens because the new primary will initially not have the
+    // sharding metadata for the collections. The metadata will only be recovered after the first
+    // failing write for each collection.
     rst.stepUp(rst.getSecondaries()[0]);
     primary = rst.getPrimary();
     testDB = primary.getDB("test");
+}
 
+setup();
+
+withRetryOnTransientTxnError(() => {
     // Initial write in a transaction that fails with StaleConfig should internally retry and
     // succeed.
     let command = {
-        insert: "test0",
+        insert: "test" + (base + 0),
         documents: [{x: 1}],
 
-        lsid: kSessionId,
+        lsid: sessionId,
         txnNumber: NumberLong(0),
         autocommit: false,
         startTransaction: true,
@@ -49,10 +60,10 @@ withRetryOnTransientTxnError(() => {
     // Continued writes in a transaction that fails with StaleConfig should fail with the
     // TransientTransactionError label.
     command = {
-        insert: "test1",
+        insert: "test" + (base + 1),
         documents: [{x: 2}],
 
-        lsid: kSessionId,
+        lsid: sessionId,
         txnNumber: NumberLong(0),
         autocommit: false,
     };
@@ -61,10 +72,10 @@ withRetryOnTransientTxnError(() => {
 
     // A retryable write that fails with StaleConfig should fail with the RetryableWriteError label.
     command = {
-        insert: "test2",
+        insert: "test" + (base + 2),
         documents: [{x: 2}],
 
-        lsid: kSessionId,
+        lsid: sessionId,
         txnNumber: NumberLong(1),
     };
     res = assert.commandFailedWithCode(testDB.runCommand(command), ErrorCodes.StaleConfig);
@@ -73,7 +84,7 @@ withRetryOnTransientTxnError(() => {
     // A non-retryable write that fails with StaleConfig should have a StaleConfig writeError and no
     // error labels.
     command = {
-        insert: "test3",
+        insert: "test" + (base + 3),
         documents: [{x: 2}],
     };
     res = assert.commandFailed(testDB.runCommand(command));
@@ -82,8 +93,8 @@ withRetryOnTransientTxnError(() => {
     assert.eq(res.writeErrors[0].code, ErrorCodes.StaleConfig);
 
     // A read should succeed even if the collection sharding metadata is not initially available.
-    command = {find: "test4", filter: {x: 2}};
+    command = {find: "test" + (base + 4), filter: {x: 2}};
     assert.commandWorked(testDB.runCommand(command));
-});
+}, setup);
 
 rst.stopSet();
