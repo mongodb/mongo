7ab81476543c11f63e8ea944579a12d9545f8d34
Udita <83602637+ubatmongo@users.noreply.github.com>
2024-10-14 11:46:18 -0400

    SERVER-94440: generate token, proper distros (#27132)
    GitOrigin-RevId: c94e1bebc8d797cbf390f5ab764ce810d2f18049

--- evergreen/run_jstestfuzz/clone_repos.sh
+++ evergreen/run_jstestfuzz/clone_repos.sh
@@ -2,9 +2,10 @@ set -o errexit
 set -o verbose
 
 cd src
+
 for i in {1..5}; do
-  git clone --depth 1 git@github.com:10gen/QA.git jstests/qa_tests && RET=0 && break || RET=$? && sleep 5
-  echo "Failed to clone git@github.com:10gen/QA.git, retrying..."
+  git clone --depth 1 https://x-access-token:${github_token}@github.com/10gen/QA.git jstests/qa_tests && RET=0 && break || RET=$? && sleep 1
+  echo "Failed to clone github.com:10gen/QA.git, retrying..."
 done
 
 if [ $RET -ne 0 ]; then
