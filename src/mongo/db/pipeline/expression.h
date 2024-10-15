314f4e92313eaa25bd58f5f4a2af5cc16c7cf73e
Henri Nikku <henri.nikku@mongodb.com>
2024-10-15 19:43:20 +0100

    SERVER-95347 Enforce additional checks in $setField (#27751) (#27997)
    GitOrigin-RevId: 5cf16ad7a3910a0c7cad093a9c2781d0cb20efb2

--- src/mongo/db/pipeline/expression.h
+++ src/mongo/db/pipeline/expression.h
@@ -3608,7 +3608,8 @@ public:
         : Expression(expCtx, {std::move(field), std::move(input), std::move(value)}),
           _field(_children[0]),
           _input(_children[1]),
-          _value(_children[2]) {
+          _value(_children[2]),
+          _fieldName(getValidFieldName(_children[0])) {
         expCtx->sbeCompatible = false;
     }
 
@@ -3628,9 +3629,18 @@ protected:
     void _doAddDependencies(DepsTracker* deps) const final override;
 
 private:
+    /**
+     * Ensures 'fieldExpr' is a constant string representing a valid field name and returns it as a
+     * string. If 'fieldExpr' is not valid, this function will throw a 'uassert()'.
+     */
+    std::string getValidFieldName(boost::intrusive_ptr<Expression> fieldExpr);
+
     boost::intrusive_ptr<Expression>& _field;
     boost::intrusive_ptr<Expression>& _input;
     boost::intrusive_ptr<Expression>& _value;
+
+    // This is pre-validated by the constructor.
+    const std::string _fieldName;
 };
 
 /**
