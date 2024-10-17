6b62da35714e3f1b6af26b9ffd9a394fe75df8d7
Charlie Swanson <charlie.swanson@mongodb.com>
2024-10-17 12:55:07 -0400

    SERVER-95162 Refactor multiversion query test to make it more reusable (#27431)
    GitOrigin-RevId: d42cb3d6899ac7f882c8b933816f91d84540681e

---
+++ jstests/multiVersion/libs/mixed_version_fixture_test.js
@@ -0,0 +1,50 @@
+
+import "jstests/multiVersion/libs/multi_rs.js";
+
+import {ReplSetTest} from "jstests/libs/replsettest.js";
+
+export function testPerformUpgradeDowngradeReplSet({
+    setupFn,
+    whenFullyDowngraded,
+    whenSecondariesAreLatestBinary,
+    whenFullyUpgraded,
+    whenBinariesAreLatestAndFCVIsLastLTS
+}) {
+    const lastLTSVersion = {binVersion: "last-lts"};
+    const latestVersion = {binVersion: "latest"};
+
+    const rst = new ReplSetTest({
+        name: jsTestName(),
+        nodes: [
+            {...lastLTSVersion},
+            {...lastLTSVersion},
+        ],
+    });
+    rst.startSet();
+    rst.initiate();
+
+    let primaryConnection = rst.getPrimary();
+    const getAdminDB = () => primaryConnection.getDB("admin");
+
+    setupFn(primaryConnection);
+
+    whenFullyDowngraded(primaryConnection);
+
+    // Upgrade the binaries and the FCV.
+    rst.upgradeSet({...latestVersion});
+    primaryConnection = rst.getPrimary();
+    whenBinariesAreLatestAndFCVIsLastLTS(primaryConnection);
+
+    assert.commandWorked(
+        getAdminDB().runCommand({setFeatureCompatibilityVersion: latestFCV, confirm: true}));
+
+    whenFullyUpgraded(primaryConnection);
+
+    // Downgrade FCV without restarting.
+    assert.commandWorked(
+        getAdminDB().runCommand({setFeatureCompatibilityVersion: lastLTSFCV, confirm: true}));
+
+    whenBinariesAreLatestAndFCVIsLastLTS(primaryConnection);
+
+    rst.stopSet();
+}
--- jstests/multiVersion/targetedTestsLastLtsFeatures/bindata_convert_numeric_view_upgrade_downgrade.js
+++ jstests/multiVersion/targetedTestsLastLtsFeatures/bindata_convert_numeric_view_upgrade_downgrade.js
@@ -1,51 +1,13 @@
 /**
  * Verifies that BinData $convert behaves correctly in FCV upgrade/downgrade scenarios.
  */
-import "jstests/multiVersion/libs/multi_rs.js";
 
 import {assertDropAndRecreateCollection} from "jstests/libs/collection_drop_recreate.js";
-import {ReplSetTest} from "jstests/libs/replsettest.js";
-
-const lastLTSVersion = {
-    binVersion: "last-lts"
-};
-const latestVersion = {
-    binVersion: "latest"
-};
+import {
+    testPerformUpgradeDowngradeReplSet
+} from "jstests/multiVersion/libs/mixed_version_fixture_test.js";
 
 const collectionName = "coll";
-
-const rst = new ReplSetTest({
-    name: jsTestName(),
-    nodes: [
-        {...lastLTSVersion},
-        {...lastLTSVersion},
-    ],
-});
-rst.startSet();
-rst.initiate();
-
-const getAdminDB = () => rst.getPrimary().getDB("admin");
-const getDB = () => rst.getPrimary().getDB(jsTestName());
-
-const coll = assertDropAndRecreateCollection(getDB(), collectionName);
-assert.commandWorked(coll.insertMany([
-    {
-        _id: 0,
-        asInt: NumberInt(42),
-        asBinData: BinData(0, "AAAAKg=="),
-    },
-    {
-        _id: 1,
-        asLong: NumberLong(674),
-        asBinData: BinData(0, "ogIAAA=="),
-    },
-    {
-        _id: 2,
-        asDouble: 13.199999809265137,
-        asBinData: BinData(0, "QVMzMw=="),
-    },
-]));
 
 const toIntViewName = "toIntView";
 const toIntPipeline = [{$project: {intFromBindata: {$toInt: "$asBinData"}}}];
@@ -56,8 +18,31 @@ const toLongPipeline = [{$project: {longFromBindata: {$toLong: "$asBinData"}}}];
 const toDoubleViewName = "toDoubleView";
 const toDoublePipeline = [{$project: {doubleFromBindata: {$toDouble: "$asBinData"}}}];
 
-{
-    const db = getDB();
+const getDB = (primaryConnection) => primaryConnection.getDB(jsTestName());
+
+function setupCollection(primaryConnection) {
+    const coll = assertDropAndRecreateCollection(getDB(primaryConnection), collectionName);
+    assert.commandWorked(coll.insertMany([
+        {
+            _id: 0,
+            asInt: NumberInt(42),
+            asBinData: BinData(0, "AAAAKg=="),
+        },
+        {
+            _id: 1,
+            asLong: NumberLong(674),
+            asBinData: BinData(0, "ogIAAA=="),
+        },
+        {
+            _id: 2,
+            asDouble: 13.199999809265137,
+            asBinData: BinData(0, "QVMzMw=="),
+        },
+    ]));
+}
+
+function assertViewCanBeCreatedButNotExecuted(primaryConnection) {
+    const db = getDB(primaryConnection);
 
     // View creation succeeds, but queries on the views fail due to unsupported bindata-to-int,
     // bindata-to-double and bindata-to-long conversions.
@@ -77,14 +62,8 @@ const toDoublePipeline = [{$project: {doubleFromBindata: {$toDouble: "$asBinData
                                  ErrorCodes.ConversionFailure);
 }
 
-// Upgrade the binaries and the FCV.
-rst.upgradeSet({...latestVersion});
-assert.commandWorked(
-    getAdminDB().runCommand({setFeatureCompatibilityVersion: latestFCV, confirm: true}));
-
-{
-    // View creation and usage both succeed after upgrade.
-    const db = getDB();
+function assertViewCanBeCreatedAndExecuted(primaryConnection) {
+    const db = getDB(primaryConnection);
 
     db[toIntViewName].drop();
     assert.commandWorked(db.createView(toIntViewName, collectionName, toIntPipeline));
@@ -99,12 +78,8 @@ assert.commandWorked(
     assert.commandWorked(db.runCommand({find: toDoubleViewName, filter: {}}));
 }
 
-// Downgrade FCV without restarting.
-assert.commandWorked(
-    getAdminDB().runCommand({setFeatureCompatibilityVersion: lastLTSFCV, confirm: true}));
-
-{
-    const db = getDB();
+function assertQueriesOnViewsFail(primaryConnection) {
+    const db = getDB(primaryConnection);
 
     // Queries on views using BinData $convert numeric should fail after downgrading the FCV.
     assert.commandFailedWithCode(db.runCommand({find: toIntViewName, filter: {}}),
@@ -272,4 +247,10 @@ assert.commandWorked(
     );
 }
 
-rst.stopSet();
+testPerformUpgradeDowngradeReplSet({
+    setupFn: setupCollection,
+    whenFullyDowngraded: assertViewCanBeCreatedButNotExecuted,
+    whenSecondariesAreLatestBinary: assertViewCanBeCreatedButNotExecuted,
+    whenFullyUpgraded: assertViewCanBeCreatedAndExecuted,
+    whenBinariesAreLatestAndFCVIsLastLTS: assertQueriesOnViewsFail,
+});
