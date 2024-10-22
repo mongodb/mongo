e36c441e57932916bf180dbc7aecd7e4f5c94ba6
Silvia Surroca <silvia.surroca@mongodb.com>
2024-10-22 17:57:46 +0200

    SERVER-95054 Retry HostUnreachable error upon removeShard call for config stepdown suites (#28341)
    GitOrigin-RevId: b8996c03d3616ea7d731a48803731504cd441c52

--- jstests/sharding/libs/remove_shard_util.js
+++ jstests/sharding/libs/remove_shard_util.js
@@ -1,3 +1,7 @@
+// Make it easier to understand whether or not returns from the assert.soon are being retried.
+const kRetry = false;
+const kNoRetry = true;
+
 function removeShard(shardingTestOrConn, shardName, timeout) {
     if (timeout == undefined) {
         timeout = 10 * 60 * 1000;  // 10 minutes
@@ -19,14 +23,22 @@ function removeShard(shardingTestOrConn, shardName, timeout) {
         } else {
             res = s.adminCommand({removeShard: shardName});
         }
-        if (!res.ok && res.code === ErrorCodes.ShardNotFound) {
-            // If the config server primary steps down right after removing the config.shards doc
-            // for the shard but before responding with "state": "completed", the mongos would retry
-            // the _configsvrRemoveShard command against the new config server primary, which would
-            // not find the removed shard in its ShardRegistry if it has done a ShardRegistry reload
-            // after the config.shards doc for the shard was removed. This would cause the command
-            // to fail with ShardNotFound.
-            return true;
+        if (!res.ok) {
+            if (res.code === ErrorCodes.ShardNotFound) {
+                // If the config server primary steps down right after removing the config.shards
+                // doc for the shard but before responding with "state": "completed", the mongos
+                // would retry the _configsvrRemoveShard command against the new config server
+                // primary, which would not find the removed shard in its ShardRegistry if it has
+                // done a ShardRegistry reload after the config.shards doc for the shard was
+                // removed. This would cause the command to fail with ShardNotFound.
+                return kNoRetry;
+            }
+            if (res.code === ErrorCodes.HostUnreachable && TestData.runningWithConfigStepdowns) {
+                // The mongos may exhaust its retries due to having consecutive config stepdowns. In
+                // this case, the mongos will return a HostUnreachable error.
+                // We should retry the operation when this happens.
+                return kRetry;
+            }
         }
         assert.commandWorked(res);
         return res.state == 'completed';
