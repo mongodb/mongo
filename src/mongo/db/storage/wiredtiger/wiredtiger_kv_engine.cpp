0f71e97d9475b316375668d98c34ad8f242fd3a4
Damian Wasilewicz <33820523+DamianWasilewicz@users.noreply.github.com>
2024-11-04 14:19:08 -0500

    SERVER-95136: Retry when closing a connection during shutdown returns EBUSY (#28756)
    GitOrigin-RevId: b05b4055ad8dab3d2f0730f3b69f402d32377310

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
@@ -811,7 +811,18 @@ void WiredTigerKVEngine::cleanShutdown() {
 
     auto startTime = Date_t::now();
     LOGV2(4795902, "Closing WiredTiger", "closeConfig"_attr = closeConfig);
-    invariantWTOK(_conn->close(_conn, closeConfig.c_str()), nullptr);
+    // Attempting to close the connection might transiently fail with EBUSY.
+    int retCode = 0;
+    for (int attempts = 0; attempts < gWiredTigerMaxAttemptsToCloseConnectionOnShutdown.load();
+         ++attempts) {
+        retCode = _conn->close(_conn, closeConfig.c_str());
+        if (retCode != EBUSY) {
+            break;
+        }
+        LOGV2(9513600, "Attempt to close WT returned EBUSY, retrying", "attempts"_attr = attempts);
+        sleepmillis(100);
+    }
+    fassert(9513601, retCode == 0);
     LOGV2(4795901, "WiredTiger closed", "duration"_attr = Date_t::now() - startTime);
     _conn = nullptr;
 }
