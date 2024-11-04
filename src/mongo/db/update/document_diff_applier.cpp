b19e7ed6fdaf5eea7a9851955bfdce1be767fa53
henrikedin <henrik.edin@mongodb.com>
2024-11-04 09:18:30 -0500

    SERVER-96409 Log preimage in time-series compression validator (#28660)
    GitOrigin-RevId: 7463d8cf87e44c9ec6d5a397a6ae195287906830

--- src/mongo/db/update/document_diff_applier.cpp
+++ src/mongo/db/update/document_diff_applier.cpp
@@ -758,7 +758,7 @@ BSONObj applyDiff(const BSONObj& pre,
     applier.applyDiffToObject(pre, &path, &reader, &out);
     auto obj = out.obj();
     if (verifierFunction) {
-        verifierFunction(obj);
+        verifierFunction(obj, pre);
     }
     return obj;
 }
