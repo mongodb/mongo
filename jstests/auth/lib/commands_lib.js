d3f7b9861bc754129dcc200be7d2acf68e0429af
nandinibhartiyaMDB <104035932+nandinibhartiyaMDB@users.noreply.github.com>
2024-11-11 16:08:18 -0600

    SERVER-89642: Fix fsyncUnlock permissions (#27072)
    GitOrigin-RevId: f6cfb62ef59e2f7c06c2c6a76c026fbee435153a

--- jstests/auth/lib/commands_lib.js
+++ jstests/auth/lib/commands_lib.js
@@ -4783,7 +4783,6 @@ export const authCommandsLib = {
         {
           testname: "fsyncUnlock",
           command: {fsyncUnlock: 1},
-          skipSharded: true,  // TODO: remove when fsyncUnlock is implemented in mongos
           testcases: [
               {
                 runOnDb: adminDbName,
