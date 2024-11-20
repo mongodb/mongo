c324fee0744a8aa24323a99e2d882ed275905377
Udita <83602637+ubatmongo@users.noreply.github.com>
2024-11-20 16:08:42 -0500

    SERVER-94440: generate token, proper distros (#27132) (#28972)
    GitOrigin-RevId: 6d08fedf482ee96f6e46191a4059d4c49edc8831

--- evergreen/do_jepsen_setup/install_jepsen.sh
+++ evergreen/do_jepsen_setup/install_jepsen.sh
@@ -3,7 +3,7 @@ set -o errexit
 cd src
 # "v0.2.0-jepsen-mongodb-master" is the latest jepsen version that works with v5.0,
 # because JS engine on v5.0 does not know the "import" statement, that was added in later jepsen versions
-git clone --branch=v0.2.0-jepsen-mongodb-master --depth=1 git@github.com:10gen/jepsen.git jepsen-mongodb
+git clone --branch=v0.2.0-jepsen-mongodb-master --depth=1 https://x-access-token:${github_token}@github.com/10gen/jepsen.git jepsen-mongodb
 cd jepsen-mongodb
 
 lein install
