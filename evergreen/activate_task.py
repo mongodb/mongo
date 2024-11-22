64a30c4225f2eaac43ea210a2f11d7a274684f72
SimonEismann <31125121+SimonEismann@users.noreply.github.com>
2024-11-22 22:28:10 +0100

    Revert "SERVER-97469 Do not automatically schedule archive_dist_test_… (#29511)
    GitOrigin-RevId: a8b50be5cc10fc1ff785f1d0dca80e408a819bf0

--- evergreen/activate_task.py
+++ evergreen/activate_task.py
@@ -1,21 +1,15 @@
 """Script that activates the input task on the same variant as the running task"""
 
-from typing import Optional
-
 import typer
 
 from buildscripts.resmokelib.utils import evergreen_conn
 from buildscripts.util.read_config import read_config_file
 
 
-def main(task_name: str, skip_for_patch_author: Optional[str] = None):
+def main(task_name: str):
     expansions_file = "../expansions.yml"
     expansions = read_config_file(expansions_file)
     evg_api = evergreen_conn.get_evergreen_api()
-
-    # Skip activation if the patch author is the excluded user
-    if expansions.get("author") == skip_for_patch_author:
-        return
 
     variant_id = expansions.get("build_id")
     variant = evg_api.build_by_id(variant_id)
