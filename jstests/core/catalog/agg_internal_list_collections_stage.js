6a75d9b32b53b6e424c7de5b7afca1fa64fd009f
Silvia Surroca <silvia.surroca@mongodb.com>
2024-12-05 15:22:20 +0100

    SERVER-97859 $_internalListCollections test doesn't need to support multitenancy (#29934)
    GitOrigin-RevId: fcfbbd64e29eae6b0463a34ca473b4065e52b782

--- jstests/core/catalog/agg_internal_list_collections_stage.js
+++ jstests/core/catalog/agg_internal_list_collections_stage.js
@@ -7,7 +7,8 @@
  *    requires_fcv_81,
  *    # $_internalListCollections only supports local read concern
  *    assumes_read_concern_unchanged,
- *    # TODO SERVER-97215: remove `command_not_supported_in_serverless`
+ *    # There is no need to support multitenancy, as it has been canceled and was never in
+ *    # production (see SERVER-97215 for more information)
  *    command_not_supported_in_serverless,
  *    does_not_support_causal_consistency,
  *    does_not_support_transactions,
