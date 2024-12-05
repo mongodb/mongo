729c40b6b1268fbccee41e49dfda5993259eb3eb
Jan <jsteemann@users.noreply.github.com>
2024-12-05 17:19:51 +0100

    SERVER-97937 Clean up leftover declaration from SERVER-88142 (#29939)
    GitOrigin-RevId: cb97d3d50e8d1eb1ad1121947f281634f5fa0553

--- src/mongo/db/pipeline/document_source_change_stream.h
+++ src/mongo/db/pipeline/document_source_change_stream.h
@@ -166,9 +166,10 @@ public:
 
     // The name of the field where the operation description of the non-CRUD operations will be
     // located. This is complementary to the 'documentKey' for CRUD operations.
-    // Note that the operationDescription of an event will be part of the event's resumeToken. Thus
-    // the operationDescription for an existing event should never be changed, because otherwise the
-    // changestream resumability between different versions of MongoDB may be jeopardized.
+    // Note that the operation description of an event will be part of the event's resume token.
+    // Thus the operation description for an existing event should never be changed, because
+    // otherwise the changestream resumability between different versions of MongoDB may be
+    // jeopardized.
     static constexpr StringData kOperationDescriptionField = "operationDescription"_sd;
 
     // The name of the field where the pre-image document will be found, if requested and available.
@@ -238,9 +239,6 @@ public:
 
     // The target namespace of a rename operation.
     static constexpr StringData kRenameTargetNssField = "to"_sd;
-
-    // The collection type created by a create operation.
-    static constexpr StringData kCollectionTypeField = "type"_sd;
 
     // Wall time of the corresponding oplog entry.
     static constexpr StringData kWallTimeField = "wallTime"_sd;
