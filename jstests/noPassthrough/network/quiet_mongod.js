7bcada4ef035d7e038abd86e5942a7787c8d8f21
Brad Cater <152920274+brad-cater-mongodb@users.noreply.github.com>
2024-12-06 17:10:25 -0500

    SERVER-98042 Make quiet_mongodb.js more resilient to log truncation (#29992)
    GitOrigin-RevId: a754bc216c39c95ed1f08c15709871f5efe3735b

--- jstests/noPassthrough/network/quiet_mongod.js
+++ jstests/noPassthrough/network/quiet_mongod.js
@@ -6,8 +6,10 @@
 'use strict';
 
 const dbName = jsTestName();
-const firstCommandLogLineRegex = /\"NETWORK.+6788700.+Received first command/;
-const clientMetadataLogLineRegex = /\"NETWORK.+51800.+client metadata\"/;
+const firstCommandLogLineRegexS = "\"NETWORK.+6788700.+Received first command";
+const firstCommandLogLineRegex = new RegExp(firstCommandLogLineRegexS);
+const clientMetadataLogLineRegexS = "\"NETWORK.+51800.+client metadata\"";
+const clientMetadataLogLineRegex = new RegExp(clientMetadataLogLineRegexS);
 
 // Test that a normal mongod has client metadata.
 {
@@ -16,10 +18,11 @@ const clientMetadataLogLineRegex = /\"NETWORK.+51800.+client metadata\"/;
     // Issue a command and wait for a log line acknowledging it to be sure any client metadata log
     // lines have been flushed.
     conn.getDB(dbName).runCommand({ping: 1});
-    assert.soon(() => rawMongoProgramOutput(".*").match(firstCommandLogLineRegex),
-                "did not see log line acknowledging first command");
+    assert.soon(
+        () => rawMongoProgramOutput(firstCommandLogLineRegexS).match(firstCommandLogLineRegex),
+        "did not see log line acknowledging first command");
 
-    const mongoOutput = rawMongoProgramOutput(".*");
+    const mongoOutput = rawMongoProgramOutput(clientMetadataLogLineRegexS);
     assert.gte(mongoOutput.match(clientMetadataLogLineRegex).length,
                1,
                "did not see client metadata log line while not in quiet mode");
@@ -35,10 +38,11 @@ const clientMetadataLogLineRegex = /\"NETWORK.+51800.+client metadata\"/;
     // Issue a command and wait for a log line acknowledging it to be sure any client metadata log
     // lines have been flushed.
     conn.getDB(dbName).runCommand({ping: 1});
-    assert.soon(() => rawMongoProgramOutput(".*").match(firstCommandLogLineRegex),
-                "did not see log line acknowledging first command");
+    assert.soon(
+        () => rawMongoProgramOutput(firstCommandLogLineRegexS).match(firstCommandLogLineRegex),
+        "did not see log line acknowledging first command");
 
-    const mongoOutput = rawMongoProgramOutput(".*");
+    const mongoOutput = rawMongoProgramOutput(clientMetadataLogLineRegexS);
     assert.eq(mongoOutput.match(clientMetadataLogLineRegex),
               null,
               "saw client metadata log line while in quiet mode");
