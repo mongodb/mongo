a6cac448d32b92c3d58591d9f58214768cffa114
Matt Kneiser <matt.kneiser@mongodb.com>
2024-12-06 09:54:38 -0800

    SERVER-89918 Add full ident name tracking to backup/restore (#27799)
    GitOrigin-RevId: ced5826db43f1dcb06824032b063d0eaf4e70ebd

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.h
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.h
@@ -60,6 +60,26 @@ class WiredTigerSessionCache;
 class WiredTigerSizeStorer;
 class WiredTigerEngineRuntimeConfigParameter;
 
+/**
+ * With the absolute path to an ident and the parent dbpath, return the ident.
+ *
+ * Note that the ident can have 4 different forms depending on the combination
+ * of server parameters present (directoryperdb / wiredTigerDirectoryForIndexes).
+ * With any one of these server parameters enabled, a directory could be included
+ * in the returned ident.
+ * See the unit test WiredTigerKVEngineTest::ExtractIdentFromPath for example usage.
+ *
+ * Note (2) idents use unix-style separators (always, see
+ * durable_catalog.cpp:generateUniqueIdent) but ident paths are platform-dependant.
+ * This method returns the unix-style "/" separators always.
+ */
+std::string extractIdentFromPath(const boost::filesystem::path& dbpath,
+                                 const boost::filesystem::path& identAbsolutePath);
+
+
+Status validateExtraDiagnostics(const std::vector<std::string>& value,
+                                const boost::optional<TenantId>& tenantId);
+
 struct WiredTigerFileVersion {
     // MongoDB 4.4+ will not open on datafiles left behind by 4.2.5 and earlier. MongoDB 4.4
     // shutting down in FCV 4.2 will leave data files that 4.2.6+ will understand
