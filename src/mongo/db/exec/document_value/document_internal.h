55234aaf7d095390bc09ea95d9a437da2d869e50
Ivan Fefer <ivan.fefer@mongodb.com>
2024-12-09 17:47:52 +0100

    SERVER-91339 Load redacted document into cache to avoid cache reallocation (#24215)
    GitOrigin-RevId: b1e9e2c4c8136e29875be5a34c3a07f4dec5dcc5

--- src/mongo/db/exec/document_value/document_internal.h
+++ src/mongo/db/exec/document_value/document_internal.h
@@ -385,6 +385,11 @@ public:
      */
     Document shred() const;
 
+    /**
+     * Loads the whole document into cache.
+     */
+    void loadIntoCache() const;
+
     static const DocumentStorage& emptyDoc() {
         return kEmptyDoc;
     }
