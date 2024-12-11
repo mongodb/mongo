85db2079c3dfecba54af4053dc22b45f4ad224a9
Sulabh Mahajan <sulabh.mahajan@mongodb.com>
2024-12-11 16:55:36 +0100

    SERVER-97220 2-phase drop test: Wait for the stable to move past drop optime (#29805) (#30203)
    GitOrigin-RevId: 8bdc3773ffdbbe3187215739ef871aa3475b683c

--- jstests/noPassthrough/collection_catalog_two_phase_drops.js
+++ jstests/noPassthrough/collection_catalog_two_phase_drops.js
@@ -78,18 +78,43 @@ checkLog.containsJson(primary, 6825301, {
     }
 });
 
+// Wait until majority read concern optime has advanced past the given timestamp. Then take a
+// checkpoint and assert that the checkpoint's stable time is past the oplog entry.
+const advanceMajorityThenCheckpoint = function(timestamp) {
+    assert.soon(
+        () => {
+            const replSetStatus = assert.commandWorked(primary.adminCommand({replSetGetStatus: 1}));
+            const majorityOpTime = replSetStatus.optimes.readConcernMajorityOpTime.ts;
+            jsTestLog("Awaiting read concern majority optime to advance. " +
+                      `Current: ${tojson(majorityOpTime)}, ` +
+                      `target: ${tojson(timestamp)}`);
+            return ((timestampCmp(majorityOpTime, timestamp) >= 0));
+        },
+        "Timeout waiting for read concern majority optime to catch up",
+        ReplSetTest.kDefaultTimeoutMS,
+        2000);
+
+    assert.commandWorked(db.adminCommand({fsync: 1}));
+
+    const replSetStatus = assert.commandWorked(primary.adminCommand({replSetGetStatus: 1}));
+    const checkpointStableTs = replSetStatus.lastStableRecoveryTimestamp;
+    assert.gte(checkpointStableTs, timestamp);
+};
+
 // Advance the timestamp and wait for journaling, so that by completion, the stable timestamp will
 // include this command.
-assert.commandWorked(db.adminCommand({
-    appendOplogNote: 1,
-    data: {msg: "advance timestamp"},
-    writeConcern: {w: "majority", j: true}
-}));
+const advTimeIdxDrop = assert
+                           .commandWorked(db.adminCommand({
+                               appendOplogNote: 1,
+                               data: {msg: "advance timestamp after dropping index"},
+                               writeConcern: {w: "majority", j: true}
+                           }))
+                           .operationTime;
 
-// Trigger a checkpoint. This will advance the checkpoint timestamp to the stable timestamp, and
-// allows for the timestamp monitor to notify the 2-phase drop that it can proceed with dropping
-// the table.
-assert.commandWorked(db.adminCommand({fsync: 1}));
+// Wait for the read concern majority time to advance. Then trigger a checkpoint. This will advance
+// the checkpoint timestamp to the stable timestamp, and allows for the timestamp monitor to notify
+// the 2-phase drop that it can proceed with dropping the table.
+advanceMajorityThenCheckpoint(advTimeIdxDrop);
 
 // "The ident was successfully dropped".
 checkLog.containsJson(primary, 6776600, {
@@ -138,16 +163,18 @@ checkLog.containsJson(primary, 6825300, {
 
 // Advance the timestamp and wait for journaling, so that by completion, the stable timestamp will
 // include this command.
-assert.commandWorked(db.adminCommand({
-    appendOplogNote: 1,
-    data: {msg: "advance timestamp"},
-    writeConcern: {w: "majority", j: true}
-}));
+const advTimeCollDrop = assert
+                            .commandWorked(db.adminCommand({
+                                appendOplogNote: 1,
+                                data: {msg: "advance timestamp after dropping collection"},
+                                writeConcern: {w: "majority", j: true}
+                            }))
+                            .operationTime;
 
-// Trigger a checkpoint. This will advance the checkpoint timestamp to the stable timestamp, and
-// allows for the timestamp monitor to notify the 2-phase drop that it can proceed with dropping
-// the table.
-assert.commandWorked(db.adminCommand({fsync: 1}));
+// Wait for the read concern majority time to advance. Then trigger a checkpoint. This will advance
+// the checkpoint timestamp to the stable timestamp, and allows for the timestamp monitor to notify
+// the 2-phase drop that it can proceed with dropping the table.
+advanceMajorityThenCheckpoint(advTimeCollDrop);
 
 // "The ident was successfully dropped".
 checkLog.containsJson(primary, 6776600, {
