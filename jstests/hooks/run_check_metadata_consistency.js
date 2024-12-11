5157bf67f0dc75f6d5ea63cac8654a0517fcf516
Pol Piñol Castuera <pol.pinol@mongodb.com>
2024-12-11 09:27:37 +0100

    SERVER-94979 Make CheckMetadataConsistency hook don't rely on the discovery topology helpers (#30069)
    GitOrigin-RevId: ea643d909b6f5c79ac5bbe983667421280d1fb67

--- jstests/hooks/run_check_metadata_consistency.js
+++ jstests/hooks/run_check_metadata_consistency.js
@@ -1,33 +1,27 @@
 import {MetadataConsistencyChecker} from "jstests/libs/check_metadata_consistency_helpers.js";
 import {DiscoverTopology, Topology} from "jstests/libs/discover_topology.js";
-import {RetryableWritesUtil} from "jstests/libs/retryable_writes_util.js";
 
 assert.neq(typeof db, 'undefined', 'No `db` object, is the shell connected to a server?');
 
 const conn = db.getMongo();
 
-{
-    // Check that we are running on a sharded cluster
-    let topology;
+const assertNonShardedCluster = (conn) => {
     try {
-        topology = DiscoverTopology.findConnectedNodes(conn);
+        const topology = DiscoverTopology.findConnectedNodes(conn);
+        assert(topology.type != Topology.kShardedCluster &&
+                   !(topology.type == Topology.kReplicaSet && topology.configsvr &&
+                     TestData.testingReplicaSetEndpoint),
+               "Metadata consistency check must be run against a sharded cluster");
     } catch (e) {
-        if (ErrorCodes.isRetriableError(e.code) || ErrorCodes.isInterruption(e.code) ||
-            ErrorCodes.isNetworkTimeoutError(e.code) || isNetworkError(e) ||
-            e.code === ErrorCodes.FailedToSatisfyReadPreference ||
-            RetryableWritesUtil.isFailedToSatisfyPrimaryReadPreferenceError(e)) {
-            jsTest.log(
-                `Aborted metadata consistency check due to retriable error during topology discovery: ${
-                    e}`);
-            quit();
-        } else {
-            throw e;
-        }
+        jsTest.log(
+            `Aborted metadata consistency check due to an error during topology discovery: ${e}`);
     }
-    assert(topology.type == Topology.kShardedCluster ||
-               (topology.type == Topology.kReplicaSet && topology.configsvr &&
-                TestData.testingReplicaSetEndpoint),
-           "Metadata consistency check must be run against a sharded cluster");
-}
+};
 
-MetadataConsistencyChecker.run(conn);
+try {
+    MetadataConsistencyChecker.run(conn);
+} catch (e) {
+    if (e.code === ErrorCodes.CommandNotFound) {
+        assertNonShardedCluster(conn);
+    }
+}
