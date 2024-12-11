2ceada8128587eb2c300faa9a60afc93c30cac92
Lynne Wang <lynne.wang@mongodb.com>
2024-12-10 23:48:09 -0500

    SERVER-98297 Run test_simple_compares.py and test_simple_run.py in serial (#30175)
    GitOrigin-RevId: 0f7ffd25498b64341bbb3354e088c5c04b32a19c

--- src/mongo/db/query/query_tester/tests/selfTests/test_simple_run.py
+++
@@ -1,5 +0,0 @@
-from testlib.test_utils import ExitCode, Mode, assert_exit_code, run_mongotest
-
-test_file_names = ("escapedQuotes", "getMoreTest", "indexOptions", "multiline", "norm", "testA")
-exit_code, output = run_mongotest(test_file_names, Mode.RUN)
-assert_exit_code(exit_code, ExitCode.SUCCESS, output)
