266e944c6d198c3756d07dd7e9c09b9526947542
Lynne Wang <lynne.wang@mongodb.com>
2024-12-18 10:36:06 -0500

    SERVER-94592 Check inner arguments for explain (#27373) (#28255)
    GitOrigin-RevId: f85f913b0095562c1a238d96b58ec458198c8e86

---
+++ jstests/auth/lsid_auth_checks.js
@@ -0,0 +1,133 @@
+/**
+ * Test that a sharded explain command with a value set for the lsid's uid field in the inner
+ * command invocation will ignore the lsid field and succeed so long as the user is authorized to
+ * run the command being explained. Also tests that a user can only set a lsid + uid in the the
+ * top-level explain command if they are authorized to do so.
+ *
+ * @tags: [requires_sharding]
+ */
+
+const st = new ShardingTest({keyFile: "jstests/libs/key1", shards: {rs0: {nodes: 2}}});
+const mongosConn = st.s;
+
+// Setup: create user roles.
+const adminDB = mongosConn.getDB("admin");
+adminDB.createUser({user: "admin", pwd: "admin", roles: ["root"]});
+adminDB.auth("admin", "admin");
+
+const testDB = mongosConn.getDB("test");
+testDB.test.insert({a: 1});
+
+testDB.createUser({user: "readOnlyUser", pwd: "user", roles: jsTest.readOnlyUserRoles});
+testDB.createUser({user: "readWriteUser", pwd: "user", roles: jsTest.basicUserRoles});
+adminDB.createRole({
+    role: "impersonateRole",
+    roles: [],
+    privileges: [
+        {resource: {db: "test", collection: "test"}, actions: ["find", "update"]},
+        {resource: {cluster: true}, actions: ["impersonate"]}
+    ]
+});
+adminDB.createUser({user: "impersonateUser", pwd: "user", roles: ["impersonateRole"]});
+adminDB.logout();
+
+// LSID helper functions.
+const cmdLsid = UUID();
+const invalidUid = computeSHA256Block("abc");
+const getUserUid = function() {
+    const user = adminDB.runCommand({connectionStatus: 1}).authInfo.authenticatedUsers[0];
+    return user ? computeSHA256Block(user.user + "@" + user.db) : computeSHA256Block("");
+};
+
+const createLsidObj = (uid) => ({lsid: {id: cmdLsid, uid}});
+const commandWithLsid = (cmd, lsidObj, isOuter = false) => {
+    return isOuter ? Object.assign({explain: cmd}, lsidObj)
+                   : {explain: Object.assign({}, cmd, lsidObj)};
+};
+
+// Explain command helper and runners.
+const assertExplain = (cmd, uid, shouldFail = false, isOuter = false) => {
+    const lsidObj = createLsidObj(uid);
+    const command = commandWithLsid(cmd, lsidObj, isOuter);
+
+    if (shouldFail) {
+        assert.commandFailedWithCode(testDB.runCommand(command), ErrorCodes.Unauthorized);
+    } else {
+        assert.commandWorked(testDB.runCommand(command));
+    }
+};
+
+const assertInnerLsidExplainIsUnauthorized = (cmd, uid) => assertExplain(cmd, uid, true);
+const assertInnerLsidExplainWorked = (cmd, uid) => assertExplain(cmd, uid, false);
+const assertTopLevelLsidExplainIsUnauthorized = (cmd, uid) => assertExplain(cmd, uid, true, true);
+const assertTopLevelLsidExplainWorked = (cmd, uid) => assertExplain(cmd, uid, false, true);
+
+const runCommandWithLsidTest =
+    (isAuthZforInnerCmd, canSetTopLevelLsid, cmd, invalidUid, userUid) => {
+        // A user can put any arbitrary lsid/generic argument in the inner command invocation, as
+        // this will be pruned when the command is wrapped into an explain command. The explain will
+        // succeed so long as the user is authorized to run the command being explained. A user can
+        // only specify an lsid in the top-level explain command if they are authorized to do so, or
+        // if the uid in the lsid is the same as the user digest in the opCtx. The explain command
+        // will succeed if the user is authorized to run the command being explained.
+        if (isAuthZforInnerCmd) {
+            assertInnerLsidExplainWorked(cmd, invalidUid);
+            assertInnerLsidExplainWorked(cmd, userUid);
+            assertTopLevelLsidExplainWorked(cmd, userUid);
+        } else {
+            assertInnerLsidExplainIsUnauthorized(cmd, invalidUid);
+            assertInnerLsidExplainIsUnauthorized(cmd, userUid);
+            assertTopLevelLsidExplainIsUnauthorized(cmd, userUid);
+        }
+
+        if (canSetTopLevelLsid) {
+            assertTopLevelLsidExplainWorked(cmd, invalidUid);
+        } else {
+            assertTopLevelLsidExplainIsUnauthorized(cmd, invalidUid);
+        }
+    };
+
+// Commands to run explain on.
+const commands = [
+    {cmd: {distinct: "test", key: "_id"}, isWrite: false},
+    {cmd: {count: "test"}, isWrite: false},
+    {cmd: {find: "test", filter: {a: 1}}, isWrite: false},
+    {
+        cmd: {findAndModify: "test", query: {a: 1}, update: {$set: {b: 2}}, "new": true},
+        isWrite: true
+    },
+    {cmd: {update: "test", updates: [{q: {a: 1}, u: {c: "foo"}}]}, isWrite: true}
+];
+
+// A user is only authorized to run an explain on a command if they:
+// 1. Are authorized to run the command being explained on the associated resource.
+// 2. Are authorized to set any generic arguments attached to the top-level invocation of the
+// explain. In particular, a user can only set a different value in the uid field of an lsid if they
+// have the "impersonate" role. If a user specifies the same uid value as their user digest, the
+// command will succeed if (1) is true.
+const users = [
+    {user: "readOnlyUser", pwd: "user", authDB: testDB, canWrite: false, canSetTopLevelLsid: false},
+    {user: "readWriteUser", pwd: "user", authDB: testDB, canWrite: true, canSetTopLevelLsid: false},
+    {
+        user: "impersonateUser",
+        pwd: "user",
+        authDB: adminDB,
+        canWrite: true,
+        canSetTopLevelLsid: true
+    },
+    {user: "admin", pwd: "admin", authDB: adminDB, canWrite: true, canSetTopLevelLsid: false},
+];
+
+users.forEach(({user, pwd, authDB, canWrite, canSetTopLevelLsid}) => {
+    authDB.auth(user, pwd);
+    const userUid = getUserUid();
+
+    commands.forEach(({cmd, isWrite}) => {
+        const isAuthZforInnerCmd = canWrite || !isWrite;
+        runCommandWithLsidTest(isAuthZforInnerCmd, canSetTopLevelLsid, cmd, invalidUid, userUid);
+    });
+
+    authDB.logout();
+});
+
+st.stop();
---
+++ src/mongo/s/commands/cluster_explain_test.cpp
@@ -0,0 +1,103 @@
+/**
+ *    Copyright (C) 2024-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/db/query/explain_verbosity_gen.h"
+#include "mongo/idl/command_generic_argument.h"
+#include "mongo/s/commands/cluster_explain.h"
+#include "mongo/unittest/bson_test_util.h"
+#include "mongo/unittest/unittest.h"
+#include "mongo/util/assert_util.h"
+
+namespace mongo {
+namespace {
+void testPruneGenericArgs(const std::string& genericArg) {
+    auto internalCmd = fromjson("{find: 'test', filter: {a: 1}, " + genericArg + "}");
+    auto verbosity = explain::VerbosityEnum::kQueryPlanner;
+    auto expected =
+        fromjson("{explain: {find: 'test', filter: {a: 1}}, verbosity: 'queryPlanner'}");
+    ASSERT_BSONOBJ_EQ(ClusterExplain::wrapAsExplain(internalCmd, verbosity), expected);
+}
+
+void testPropagateGenericArgs(const std::string& genericArg) {
+    auto internalCmd = fromjson("{find: 'test', filter: {a: 1}, " + genericArg + "}");
+    auto verbosity = explain::VerbosityEnum::kQueryPlanner;
+    auto expected = fromjson(
+        "{explain: {find: 'test', filter: {a: 1}}, verbosity: 'queryPlanner', " + genericArg + "}");
+    ASSERT_BSONOBJ_EQ(ClusterExplain::wrapAsExplain(internalCmd, verbosity), expected);
+}
+
+TEST(ClusterExplainTest, PruneMaxTimeMS) {
+    std::string maxTimeMS = "maxTimeMS: 1";
+    testPruneGenericArgs(maxTimeMS);
+}
+
+TEST(ClusterExplainTest, PruneWriteConcern) {
+    std::string writeConcern = "writeConcern: {w: 1}";
+    testPruneGenericArgs(writeConcern);
+}
+
+TEST(ClusterExplainTest, PruneLsid) {
+    auto internalCmd = BSON("find"
+                            << "test"
+                            << "filter" << BSON("a" << 1) << "lsid"
+                            << BSON("id" << mongo::UUID::gen()));
+    auto verbosity = explain::VerbosityEnum::kQueryPlanner;
+    auto expected =
+        fromjson("{explain: {find: 'test', filter: {a: 1}}, verbosity: 'queryPlanner'}");
+    ASSERT_BSONOBJ_EQ(ClusterExplain::wrapAsExplain(internalCmd, verbosity), expected);
+}
+
+TEST(ClusterExplainTest, PruneReadPreference) {
+    std::string readPreference = "$queryOptions: {$readPreference: 'secondary'}";
+    testPruneGenericArgs(readPreference);
+}
+
+TEST(ClusterExplainTest, PruneClusterTime) {
+    auto internalCmd = BSON("find"
+                            << "test"
+                            << "filter" << BSON("a" << 1) << "$clusterTime"
+                            << BSON("clusterTime" << Timestamp(2, 2)) << "$configTime"
+                            << Timestamp(2, 2) << "$topologyTime" << Timestamp(2, 2));
+    auto verbosity = explain::VerbosityEnum::kQueryPlanner;
+    auto expected =
+        fromjson("{explain: {find: 'test', filter: {a: 1}}, verbosity: 'queryPlanner'}");
+    ASSERT_BSONOBJ_EQ(ClusterExplain::wrapAsExplain(internalCmd, verbosity), expected);
+}
+
+TEST(ClusterExplainTest, PropagateComment) {
+    std::string comment = "comment: 'Quetzlcoatl'";
+    testPropagateGenericArgs(comment);
+}
+
+TEST(ClusterExplainTest, PropagateReadConcern) {
+    std::string readConcern = "readConcern: {level: 'linearizable'}";
+    testPropagateGenericArgs(readConcern);
+}
+}  // namespace
+}  // namespace mongo
