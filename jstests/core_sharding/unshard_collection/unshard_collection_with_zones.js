3311d7c3e9ffd423a44ce353986305e9efb9b84e
janet.io <96701821+janetio@users.noreply.github.com>
2024-12-18 15:09:23 -0500

    SERVER-98632 Use unique database each time unshard_collection_with_zones.js is called and blacklist from stepdown suites (#30492)
    GitOrigin-RevId: a8802e9764de76e31fc2a4399f98cbb7990fbc8b

--- jstests/core_sharding/unshard_collection/unshard_collection_with_zones.js
+++ jstests/core_sharding/unshard_collection/unshard_collection_with_zones.js
@@ -1,6 +1,9 @@
 /**
  * Tests that a collection with zone definitions has its zones dropped when it is unsharded.
- * @tags: [assumes_balancer_off]
+ * @tags: [
+ * assumes_balancer_off,
+ * does_not_support_stepdowns
+ * ]
  */
 
 import {Thread} from "jstests/libs/parallelTester.js";
@@ -13,33 +16,37 @@ if (shards.length < 2) {
 }
 
 Random.setRandomSeed();
-const dbName = db.getName();
+
+const testDb = db.getSiblingDB(jsTestName());
+testDb.dropDatabase();
+
+const dbName = testDb.getName();
 
 let shard0 = shards[0];
 let shard1 = shards[1];
 
-assert.commandWorked(db.adminCommand({enableSharding: dbName, primaryShard: shard0._id}));
+assert.commandWorked(testDb.adminCommand({enableSharding: dbName, primaryShard: shard0._id}));
 
-assert.commandWorked(db.adminCommand({addShardToZone: shard0._id, zone: "zone1"}));
-assert.commandWorked(db.adminCommand({addShardToZone: shard1._id, zone: "zone2"}));
+assert.commandWorked(testDb.adminCommand({addShardToZone: shard0._id, zone: "zone1"}));
+assert.commandWorked(testDb.adminCommand({addShardToZone: shard1._id, zone: "zone2"}));
 
 const collName = jsTestName();
 const ns = dbName + '.' + collName;
 
-assert.commandWorked(db.createCollection(collName));
-assert.commandWorked(db.adminCommand({shardCollection: ns, key: {x: 1}}));
-assert.commandWorked(db.adminCommand({split: ns, middle: {x: 0}}));
+assert.commandWorked(testDb.createCollection(collName));
+assert.commandWorked(testDb.adminCommand({shardCollection: ns, key: {x: 1}}));
+assert.commandWorked(testDb.adminCommand({split: ns, middle: {x: 0}}));
 
-assert.commandWorked(db.adminCommand({moveChunk: ns, find: {x: 0}, to: shard1._id}));
+assert.commandWorked(testDb.adminCommand({moveChunk: ns, find: {x: 0}, to: shard1._id}));
 
 // Define zones for test collection.
 assert.commandWorked(
-    db.adminCommand({updateZoneKeyRange: ns, min: {x: MinKey}, max: {x: 0}, zone: "zone1"}));
+    testDb.adminCommand({updateZoneKeyRange: ns, min: {x: MinKey}, max: {x: 0}, zone: "zone1"}));
 assert.commandWorked(
-    db.adminCommand({updateZoneKeyRange: ns, min: {x: 0}, max: {x: MaxKey}, zone: "zone2"}));
+    testDb.adminCommand({updateZoneKeyRange: ns, min: {x: 0}, max: {x: MaxKey}, zone: "zone2"}));
 
 for (let i = 0; i < 5000; i++) {
-    db.getCollection(ns).insert({x: i});
+    testDb.getCollection(ns).insert({x: i});
 }
 
 const unshardCollectionThread = function(host, ns) {
@@ -47,19 +54,19 @@ const unshardCollectionThread = function(host, ns) {
     mongos.adminCommand({unshardCollection: ns});
 };
 
-let threadForTest = new Thread(unshardCollectionThread, db.getMongo().host, ns);
+let threadForTest = new Thread(unshardCollectionThread, testDb.getMongo().host, ns);
 threadForTest.start();
 
 // Since we sleep for a random interval, there is a chance the unsharding will succeed
 // before the abort can happen.
 sleep(Random.randInt(10 * 1000));
 
-db.adminCommand({abortUnshardCollection: ns});
+testDb.adminCommand({abortUnshardCollection: ns});
 
 threadForTest.join();
 
-let configCollectionDoc = db.getSiblingDB('config').collections.findOne({_id: ns}).key;
-const tags = db.getSiblingDB('config').tags.find({ns: ns}).toArray();
+let configCollectionDoc = testDb.getSiblingDB('config').collections.findOne({_id: ns}).key;
+const tags = testDb.getSiblingDB('config').tags.find({ns: ns}).toArray();
 
 // If we successfully unsharded our collection, we should have 0 zones in config.tags.
 // If unsharding was unsuccessful, we should retain 2 tags since we specified 2 zones for
