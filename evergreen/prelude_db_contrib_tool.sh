4dc7edb423d44ac8094b554ac769dd0abf40077a
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2025-01-03 23:42:39 +0200

    SERVER-94662 Retry pipx install db-contrib-tool (#27259)
    GitOrigin-RevId: a7e9a29ec4b338b1919a4a196c19f8d3b9acdd4d

--- evergreen/prelude_db_contrib_tool.sh
+++ evergreen/prelude_db_contrib_tool.sh
@@ -5,9 +5,35 @@ function setup_db_contrib_tool {
   export PIPX_BIN_DIR="${workdir}/pipx/bin"
   export PATH="$PATH:$PIPX_BIN_DIR"
 
-  python -m pip --disable-pip-version-check install "pip==21.0.1" "wheel==0.37.0" || exit 1
-  # We force reinstall here because when we download the previous venv the shebang
-  # in pipx still points to the old machines python location.
-  python -m pip --disable-pip-version-check install --force-reinstall --no-deps "pipx==1.4.3" || exit 1
-  pipx install "db-contrib-tool==0.8.3" --pip-args="--no-cache-dir" || exit 1
+  for i in {1..5}; do
+    python -m pip --disable-pip-version-check install "pip==21.0.1" "wheel==0.37.0" && RET=0 && break || RET=$? && sleep 1
+    echo "Failed to install pip and wheel, retrying..."
+  done
+
+  if [ $RET -ne 0 ]; then
+    echo "Failed to install pip and wheel"
+    exit $RET
+  fi
+
+  for i in {1..5}; do
+    # We force reinstall here because when we download the previous venv the shebang
+    # in pipx still points to the old machines python location.
+    python -m pip --disable-pip-version-check install --force-reinstall --no-deps "pipx==1.4.3" && RET=0 && break || RET=$? && sleep 1
+    echo "Failed to install pipx, retrying..."
+  done
+
+  if [ $RET -ne 0 ]; then
+    echo "Failed to install pipx"
+    exit $RET
+  fi
+
+  for i in {1..5}; do
+    pipx install --force "db-contrib-tool==0.8.3" --pip-args="--no-cache-dir" && RET=0 && break || RET=$? && sleep 1
+    echo "Failed to install db-contrib-tool, retrying..."
+  done
+
+  if [ $RET -ne 0 ]; then
+    echo "Failed to install db-contrib-tool"
+    exit $RET
+  fi
 }
