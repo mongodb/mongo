0ce8ebbd16d16aeb14ffe8bf315e130684ac6faf
Damian Wasilewicz <33820523+DamianWasilewicz@users.noreply.github.com>
2025-01-06 14:31:27 -0500

    Revert "SERVER-95136: Log warning if WT_CONNECTION::close returns EBUSY (#28897)" (#30808)
    GitOrigin-RevId: 93eac27129e2847d87fd5045b600a9fd991f33ca

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
@@ -816,15 +816,8 @@ void WiredTigerKVEngine::cleanShutdown() {
 
     auto startTime = Date_t::now();
     LOGV2(4795902, "Closing WiredTiger", "closeConfig"_attr = closeConfig);
-    int ret = _conn->close(_conn, closeConfig.c_str());
-    if (ret == EBUSY) {
-        LOGV2(9513600,
-              "Encountered EBUSY trying to close WiredTiger",
-              "duration"_attr = Date_t::now() - startTime);
-    } else {
-        fassert(9513601, ret == 0);
-        LOGV2(4795901, "WiredTiger closed", "duration"_attr = Date_t::now() - startTime);
-    }
+    invariantWTOK(_conn->close(_conn, closeConfig.c_str()), nullptr);
+    LOGV2(4795901, "WiredTiger closed", "duration"_attr = Date_t::now() - startTime);
     _conn = nullptr;
 }
 
