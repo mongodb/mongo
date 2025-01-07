aa0b2c0e4489ca8a9026ace7837dfb0e52dd82a5
Adityavardhan Agrawal <adi.agrawal@mongodb.com>
2025-01-08 02:01:39 +0530

    SERVER-92964 Ensure collation is followed when hashing field in index… (#29961)
    GitOrigin-RevId: 6f8eed9687ea4ff0ec2386558fe51a6f631c01f2

--- jstests/aggregation/bugs/lookup_using_hashed_index.js
+++ jstests/aggregation/bugs/lookup_using_hashed_index.js
@@ -5,26 +5,74 @@
 import {resultsEq} from "jstests/aggregation/extras/utils.js";
 
 const coll = db.lookup_using_hashed_index;
-const collName = coll.getName();
-const testIndexName = "testIndex";
 coll.drop();
+
+const foreignColl = db.lookup_using_hashed_index_foreign;
+const foreignCollName = foreignColl.getName();
+foreignColl.drop();
+
+const testIndexName = "testIndex";
+
+// Creates 'indexForeignColl' on foreignColl with 'options', and checks that the result of running
+// an aggregation with 'pipeline' with 'options' matches 'expected'.
+function runTest(indexForeignColl, pipeline, expected, options = {}) {
+    assert.commandWorked(
+        foreignColl.createIndex(indexForeignColl, {name: testIndexName, ...options}));
+    const results = coll.aggregate(pipeline, options).toArray();
+    assert(resultsEq(results, expected, true), [results, expected]);
+    assert.commandWorked(foreignColl.dropIndex(testIndexName));
+}
 
 const allDocs = [{_id: 0}, {_id: 1, a: 3}, {_id: 2, a: "3"}];
 assert.commandWorked(coll.insert(allDocs));
+assert.commandWorked(foreignColl.insert(allDocs));
 
-// Each document should look itself up in every test.
+// Each document should look up the equivalent document in the foreign collection since both
+// collections are identical.
 const expected = allDocs.map(doc => Object.merge(doc, {relookup: [doc]}));
 
-const pipeline = [{$lookup: {from: collName, localField: "a", foreignField: "a", as: "relookup"}}];
+const pipeline =
+    [{$lookup: {from: foreignCollName, localField: "a", foreignField: "a", as: "relookup"}}];
 
 // Test 1: foreignField IS the hashed field.
-assert.commandWorked(coll.createIndex({a: "hashed", b: 1}, {name: testIndexName}));
-let results = coll.aggregate(pipeline).toArray();
-assert(resultsEq(results, expected, true), [results, expected]);
-assert.commandWorked(coll.dropIndex(testIndexName));
+runTest({a: "hashed", b: 1}, pipeline, expected);
 
 // Test 2: foreignField is NOT the hashed field.
-assert.commandWorked(coll.createIndex({a: 1, b: "hashed"}, {name: testIndexName}));
-results = coll.aggregate(pipeline).toArray();
-assert(resultsEq(results, expected, true), [results, expected]);
-assert.commandWorked(coll.dropIndex(testIndexName));
+runTest({a: 1, b: "hashed"}, pipeline, expected);
+
+assert.commandWorked(coll.insert({_id: 3, str: "strstrstr"}));
+assert.commandWorked(foreignColl.insert({_id: 5, str: "STRSTRSTR"}));
+assert.commandWorked(foreignColl.insert({_id: 6, str: null}));
+const collationOptions = {
+    collation: {
+        locale: 'ru',
+        strength: 1,
+    },
+};
+const collatedPipeline =
+    [{$lookup: {from: foreignCollName, localField: "str", foreignField: "str", as: "relookup"}}];
+const collatedExpected = [
+    {
+        _id: 0,
+        relookup: [{"_id": 0}, {"_id": 1, "a": 3}, {"_id": 2, "a": "3"}, {"_id": 6, "str": null}]
+    },
+    {
+        _id: 1,
+        a: 3,
+        relookup: [{"_id": 0}, {"_id": 1, "a": 3}, {"_id": 2, "a": "3"}, {"_id": 6, "str": null}]
+    },
+    {
+        _id: 2,
+        a: "3",
+        relookup: [{"_id": 0}, {"_id": 1, "a": 3}, {"_id": 2, "a": "3"}, {"_id": 6, "str": null}]
+    },
+    {_id: 3, str: "strstrstr", relookup: [{_id: 5, str: "STRSTRSTR"}]}
+];
+// Test 3: collscan.
+runTest({nonExistentField: 1}, collatedPipeline, collatedExpected, collationOptions);
+
+// Test 4: foreignField IS the hashed field with collation specified.
+runTest({str: "hashed", a: 1}, collatedPipeline, collatedExpected, collationOptions);
+
+// Test 5: foreignField is NOT the hashed field with collation specified.
+runTest({str: 1, a: "hashed"}, collatedPipeline, collatedExpected, collationOptions);
