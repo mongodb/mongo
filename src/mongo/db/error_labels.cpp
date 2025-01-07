7b7bf14dc5c4e1c6a9ad9fac027e699300711668
Mindaugas Malinauskas <mindaugas.malin@gmail.com>
2025-01-07 22:33:41 +0200

    SERVER-92239 Made the change stream to return change stream resumable error when a rollback of a node occurs (#26114) (#29097)
    GitOrigin-RevId: 55f91b711fe8f44374054a365a8feda50f3dcba6

--- src/mongo/db/error_labels.cpp
+++ src/mongo/db/error_labels.cpp
@@ -130,7 +130,9 @@ bool ErrorLabelBuilder::isResumableChangeStreamError() const {
         (ErrorCodes::isRetriableError(*_code) || ErrorCodes::isNetworkError(*_code) ||
          ErrorCodes::isNeedRetargettingError(*_code) || _code == ErrorCodes::RetryChangeStream ||
          _code == ErrorCodes::FailedToSatisfyReadPreference ||
-         _code == ErrorCodes::ResumeTenantChangeStream);
+         _code == ErrorCodes::QueryPlanKilled /* the change stream cursor gets killed upon node
+                                                 rollback */
+         || _code == ErrorCodes::ResumeTenantChangeStream);
 
     // If the command or exception is not relevant, bail out early.
     if (!mayNeedResumableChangeStreamErrorLabel) {
