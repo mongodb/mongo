409bbea8c1816d84286a2dcdbeda137a88c3bc3a
Mikhail Shchatko <mikhail.shchatko@mongodb.com>
2025-01-08 21:22:22 +0200

    SERVER-94211 Pin to v0.3.0-jepsen-mongodb-master (#26765)
    GitOrigin-RevId: dae032c13e5edd079a4e4d32539fc1f54789f670

--- evergreen/do_jepsen_setup/install_jepsen.sh
+++ evergreen/do_jepsen_setup/install_jepsen.sh
@@ -2,7 +2,7 @@ set -o errexit
 
 cd src
 
-git clone --branch=v0.2.0-jepsen-mongodb-master --depth=1 https://x-access-token:${github_token}@github.com/10gen/jepsen.git jepsen-mongodb
+git clone --branch=v0.3.0-jepsen-mongodb-master --depth=1 https://x-access-token:${github_token}@github.com/10gen/jepsen.git jepsen-mongodb
 cd jepsen-mongodb
 
 lein install
