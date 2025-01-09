529a084a75d6819ccba5dc3e05739c2d83e3a754
ben-gawel <167240029+ben-gawel@users.noreply.github.com>
2025-01-08 21:17:01 -0500

    SERVER-95610 Update version check for reshardingDelayBeforeRemainingOperationTimeQueryMillis in servers.js (#30874)
    GitOrigin-RevId: bd3c94eea83162e97923ac72526828cd96d42eee

--- src/mongo/shell/servers.js
+++ src/mongo/shell/servers.js
@@ -226,10 +226,15 @@ var convertVersionStringToArray = function(versionString) {
 
 /**
  * Returns an integer
+ * This function will not work if the minor version is greater than or equal to 10
  */
 var convertVersionStringToInteger = function(versionString) {
     const [major, minor, point] = _convertVersionToIntegerArray(versionString);
-    return major * 100 + minor + 10;
+    assert(
+        minor < 10,
+        `Cannot convert a minor version greater than ten to an integer value since the minor version is in the tens position in the integer. Minor version attempting to convert: ${
+            minor}`);
+    return (major * 100) + (minor * 10);
 };
 
 /**
@@ -1479,7 +1484,13 @@ function appendSetParameterArgs(argArray) {
                 argArray.push(...["--setParameter", "backtraceLogFile=" + backtraceLogFilePath]);
             }
 
-            if (programMajorMinorVersion >= 800) {
+            // We are choosing an arbitrary jsTestOptions value to test that we are running as a
+            // part of a test, and not via some other method. We are assuming if someone is running
+            // MongoRunner.runMongod via the command line directly that this value would not be set.
+            if (jsTestOptions().hasOwnProperty('auth') &&
+                (programMajorMinorVersion >= 800 ||
+                 (programMajorMinorVersion >= 700 && programMajorMinorVersion < 710) ||
+                 (programMajorMinorVersion >= 600 && programMajorMinorVersion < 610))) {
                 const parameters = jsTest.options().setParameters;
                 const reshardingDefaults = {
                     'reshardingDelayBeforeRemainingOperationTimeQueryMillis': 0
@@ -1576,7 +1587,7 @@ function appendSetParameterArgs(argArray) {
 
             // Increase the default value for `receiveChunkWaitForRangeDeleterTimeoutMS` to 90
             // seconds to prevent failures due to occasional slow range deletions
-            if (programMajorMinorVersion >= 420) {
+            if (programMajorMinorVersion >= 440) {
                 if (!argArrayContainsSetParameterValue(
                         'receiveChunkWaitForRangeDeleterTimeoutMS=')) {
                     argArray.push(
