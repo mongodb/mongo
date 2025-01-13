86290bb964b8b94594ebd7519c7c113afe6d058d
Jess Balint <jbalint@gmail.com>
2025-01-13 11:51:08 -0600

    SERVER-98534 Git sparse checkout to allow running tests before clone is (#30582)
    GitOrigin-RevId: 96e512924f6d26775bf9e34abc99b659e64dc5d9

--- buildscripts/resmokelib/testing/testcases/query_tester_server_test.py
+++ buildscripts/resmokelib/testing/testcases/query_tester_server_test.py
@@ -1,10 +1,22 @@
 """The unittest.TestCase for QueryTester server correctness tests."""
 
 import os
+import time
 
 from buildscripts.resmokelib import config as _config
 from buildscripts.resmokelib import core, logging
 from buildscripts.resmokelib.testing.testcases import interface
+
+
+def wait_for_file(file_path, sleep_interval=2):
+    """Wait for `file_path` to be present on the filesystem. Sleep for
+    `sleep_interval` between each existence check and fail if we have
+    waited longer than `timeout` seconds."""
+    # Returns time in seconds
+    while True:
+        if os.path.exists(file_path):
+            return
+        time.sleep(sleep_interval)
 
 
 class QueryTesterServerTestCase(interface.ProcessTestCase):
@@ -19,18 +31,28 @@ class QueryTesterServerTestCase(interface.ProcessTestCase):
         """
         assert len(test_dir) == 1
         interface.ProcessTestCase.__init__(self, logger, "QueryTesterServerTest", test_dir[0])
-        self.test_files = [
-            os.path.join(test_dir[0], f) for f in os.listdir(test_dir[0]) if f.endswith(".test")
-        ]
+
+        self.test_dir = test_dir[0]
 
     def _make_process(self):
+        # Ensure the test files are all available. See
+        # query_tester_test_sparse_checkout.sh for how this process
+        # works. This must be done outside of the constructor to allow
+        # resmoke to proceed with initializing the complete set of
+        # tests before beginning execution.
+        wait_for_file(os.path.join(self.test_dir, ".sparse-checkout-done"))
+
+        test_files = [
+            os.path.join(self.test_dir, f) for f in os.listdir(self.test_dir) if f.endswith(".test")
+        ]
+
         return core.programs.generic_program(
             self.logger,
             [
                 _config.DEFAULT_MONGOTEST_EXECUTABLE,
                 "--uri",
                 self.fixture.get_internal_connection_string(),
-                *[cmd for f in self.test_files for cmd in ("-t", f)],
+                *[cmd for f in test_files for cmd in ("-t", f)],
                 "--drop",
                 "--load",
                 "--mode",
