bb3fb0516be0224b1c77ed08285ac883ca0e560e
Sean Lyons <sean.lyons@mongodb.com>
2025-01-21 13:10:08 -0500

    SERVER-99643 update mongo-task-generator to v0.7.21 to include more logging (#31386)
    GitOrigin-RevId: 5016a12d116d7915c3bbd25274e205b7b566518e

--- evergreen/prelude_mongo_task_generator.sh
+++ evergreen/prelude_mongo_task_generator.sh
@@ -1,19 +1,19 @@
 function setup_mongo_task_generator {
   if [ ! -f mongo-task-generator ]; then
-    curl -L https://github.com/mongodb/mongo-task-generator/releases/download/v0.7.20/mongo-task-generator --output mongo-task-generator
+    curl -L https://github.com/mongodb/mongo-task-generator/releases/download/v0.7.21/mongo-task-generator --output mongo-task-generator
     chmod +x mongo-task-generator
   fi
 }
 
 ## Comment above and uncomment below to test unreleased mongo-task-generator changes that are
-## pushed to the `<branch-name>` of the `git@github.com:<user-name>/mongo-task-generator.git`
+## pushed to the `<branch-name>` of the `https://github.com/mongodb/mongo-task-generator.git`
 ## repo
 #function setup_mongo_task_generator {
 #  if [ ! -f mongo-task-generator ]; then
 #
 #    curl https://sh.rustup.rs -sSf | sh -s -- -y
 #    source "$HOME/.cargo/env"
-#    git clone git@github.com:<user-name>/mongo-task-generator.git unreleased-mongo-task-generator
+#    git clone https://github.com/mongodb/mongo-task-generator.git unreleased-mongo-task-generator
 #    pushd unreleased-mongo-task-generator
 #    git checkout <branch-name>
 #    cargo build --release --locked
