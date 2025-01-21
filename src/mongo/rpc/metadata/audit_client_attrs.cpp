7c940961a3025ee1abf7e1a6cacd3697fb90190d
Varun Ravichandran <varun.ravichandran@mongodb.com>
2025-01-20 23:39:34 -0600

    SERVER-97908 Propagate AuditClientAttrs via ForwardableOperationMetadata (#30659)
    GitOrigin-RevId: d2546bd9a978f4dcf6c6473b11963e98cb438665

--- src/mongo/rpc/metadata/audit_client_attrs.cpp
+++ src/mongo/rpc/metadata/audit_client_attrs.cpp
@@ -35,6 +35,11 @@ namespace {
 const auto getAuditClientAttrs =
     Client::declareDecoration<synchronized_value<boost::optional<AuditClientAttrs>>>();
 
+// AuditClientAttrs BSON field names.
+constexpr auto kLocalFieldName = "local"_sd;
+constexpr auto kRemoteFieldName = "remote"_sd;
+constexpr auto kProxiesFieldName = "proxies"_sd;
+
 }  // namespace
 
 boost::optional<AuditClientAttrs> AuditClientAttrs::get(Client* client) {
@@ -43,6 +48,93 @@ boost::optional<AuditClientAttrs> AuditClientAttrs::get(Client* client) {
 
 void AuditClientAttrs::set(Client* client, AuditClientAttrs clientAttrs) {
     *getAuditClientAttrs(client) = std::move(clientAttrs);
+}
+
+AuditClientAttrs AuditClientAttrs::parseFromBSON(BSONObj obj) {
+    std::bitset<3> usedFields;
+    constexpr size_t kLocalFieldBit = 0;
+    constexpr size_t kRemoteFieldBit = 1;
+    constexpr size_t kProxiesFieldBit = 2;
+
+    HostAndPort local, remote;
+    std::vector<HostAndPort> proxies{};
+
+    const auto validateTopLevelField =
+        [&](const BSONElement& elem, const size_t bit, BSONType expectedType) {
+            auto fieldName = elem.fieldNameStringData();
+            uassert(ErrorCodes::BadValue,
+                    str::stream() << "Encountered field named " << fieldName << " with type "
+                                  << typeName(elem.type())
+                                  << ". AuditClientAttrs expects this field to have type "
+                                  << typeName(expectedType),
+                    elem.type() == expectedType);
+            uassert(ErrorCodes::BadValue,
+                    str::stream() << "Encountered more than one field named: " << fieldName,
+                    !usedFields[bit]);
+        };
+
+    for (const auto& element : obj) {
+        auto fieldName = element.fieldNameStringData();
+        if (fieldName == kLocalFieldName) {
+            // Check that the field is a string, then parse to HostAndPort.
+            validateTopLevelField(element, kLocalFieldBit, String);
+            local = HostAndPort::parseThrowing(element.valueStringDataSafe());
+            usedFields.set(kLocalFieldBit);
+        } else if (fieldName == kRemoteFieldName) {
+            // Check that the field is a string, then parse to HostAndPort.
+            validateTopLevelField(element, kRemoteFieldBit, String);
+            remote = HostAndPort::parseThrowing(element.valueStringDataSafe());
+            usedFields.set(kRemoteFieldBit);
+        } else if (fieldName == kProxiesFieldName) {
+            // Check that the field is an Array, then individually parse each element to a
+            // HostAndPort.
+            validateTopLevelField(element, kProxiesFieldBit, Array);
+            for (const auto& proxyElem : element.Array()) {
+                uassert(ErrorCodes::BadValue,
+                        str::stream()
+                            << "Encountered entry in 'proxies' array of type "
+                            << typeName(proxyElem.type()) << ", but was expecting 'String'",
+                        proxyElem.type() == String);
+                proxies.push_back(HostAndPort::parseThrowing(proxyElem.valueStringDataSafe()));
+            }
+            usedFields.set(kProxiesFieldBit);
+        } else {
+            // For now, parse AuditClientAttrs strictly and throw on unrecognized fields.
+            uasserted(ErrorCodes::BadValue,
+                      str::stream() << "Encountered unknown field named: '" << fieldName
+                                    << " when parsing BSON to AuditClientAttrs");
+        }
+    }
+
+    uassert(ErrorCodes::BadValue,
+            "BSON being parsed into AuditClientAttrs must contain a field named 'local'.",
+            usedFields[kLocalFieldBit]);
+    uassert(ErrorCodes::BadValue,
+            str::stream()
+                << "BSON being parsed into AuditClientAttrs must contain a field named 'remote'",
+            usedFields[kRemoteFieldBit]);
+    uassert(ErrorCodes::BadValue,
+            str::stream()
+                << "BSON being parsed into AuditClientAttrs must contain a field named 'proxies'",
+            usedFields[kProxiesFieldBit]);
+
+    return AuditClientAttrs(local, remote, proxies);
+}
+
+BSONObj AuditClientAttrs::serialize() const {
+    BSONObjBuilder builder;
+    builder << kLocalFieldName << _local.toString() << kRemoteFieldName << _remote.toString();
+    if (!_proxies.empty()) {
+        BSONArrayBuilder arrBuilder(builder.subarrayStart(kProxiesFieldName));
+        for (const auto& proxyHost : _proxies) {
+            arrBuilder << proxyHost.toString();
+        }
+        arrBuilder.done();
+    } else {
+        builder << kProxiesFieldName << BSONArray();
+    }
+
+    return builder.obj();
 }
 
 }  // namespace mongo::rpc
