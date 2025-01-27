ca760488fd968055ed77f055f8c24274544eb3d2
adam r <adamtron@users.noreply.github.com>
2025-01-27 07:07:49 -0800

    SERVER-91097 Update OIDC Integration testing APIs to wait for infrastructure and avoid test run conflicts, add automated python formatting (#31597)
    GitOrigin-RevId: 6a7408a117f1e36dbd84269f6a3d5ea88ecc4799

--- evergreen/external_auth_azure_teardown.sh
+++ evergreen/external_auth_azure_teardown.sh
@@ -1,6 +1,8 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 . "$DIR/prelude.sh"
 
+set -o errexit
+
 # Only run this script for the external_auth_oidc_azure task.
 if [ "${task_name}" != "external_auth_oidc_azure" ]; then
   exit 0
@@ -16,10 +18,16 @@ if [ -f "${HOME}/oidc_azure_container_key" ]; then
   echo "Cleaned up container key"
 fi
 
-python src/mongo/db/modules/enterprise/jstests/external_auth_oidc_azure/lib/toggle_ingress.py disable --config_file=$HOME/azure_e2e_config.json
+python src/mongo/db/modules/enterprise/jstests/external_auth_oidc_azure/lib/toggle_ingress.py disable --config_file=$HOME/azure_e2e_config.json --lock_file=/tmp/azure_oidc.lock
 
 # Clean up the config file, if it exists
 if [ -f "${HOME}/azure_e2e_config.json" ]; then
   rm -f $HOME/azure_e2e_config.json
   echo "Cleaned up azure_e2e_config.json"
+fi
+
+# Clean up the lock file, if it exists
+if [ -f "/tmp/azure_oidc.lock" ]; then
+  rm -f /tmp/azure_oidc.lock
+  echo "Cleaned up /tmp/azure_oidc.lock"
 fi
