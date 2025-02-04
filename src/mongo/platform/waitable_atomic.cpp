5daa46ff811d83b885095e7219e4a5072993b74e
Ronald Klop <ronald@klop.ws>
2025-02-04 11:27:20 +0100

    Reimplement waitable_atomic.cpp to use absolute times.
    Similar to the Linux implementation instead of the MacOS version.

--- src/mongo/platform/waitable_atomic.cpp
+++ src/mongo/platform/waitable_atomic.cpp
@@ -241,47 +241,40 @@ bool waitUntil(const void* uaddr,
 #elif defined(__FreeBSD__)
 
 void notifyOne(const void* uaddr) {
-    _umtx_op(const_cast<void*>(uaddr), UMTX_OP_WAKE, 1, NULL, NULL);
+    _umtx_op(const_cast<void*>(uaddr), UMTX_OP_WAKE_PRIVATE, 1, NULL, NULL);
 }
 
 void notifyMany(const void* uaddr, int nToWake) {
-    _umtx_op(const_cast<void*>(uaddr), UMTX_OP_WAKE, nToWake, NULL, NULL);
+    _umtx_op(const_cast<void*>(uaddr), UMTX_OP_WAKE_PRIVATE, nToWake, NULL, NULL);
 }
 
 void notifyAll(const void* uaddr) {
-    _umtx_op(const_cast<void*>(uaddr), UMTX_OP_WAKE, INT_MAX, NULL, NULL);
+    _umtx_op(const_cast<void*>(uaddr), UMTX_OP_WAKE_PRIVATE, INT_MAX, NULL, NULL);
 }
 
 bool waitUntil(const void* uaddr,
                uint32_t old,
                boost::optional<system_clock::time_point> deadline) {
-    struct timespec timeout;
-    bool timeoutOverflow = false;
+    struct _umtx_time umtx_deadline;
+    void* uaddr2 = nullptr;
+
     if (deadline) {
-        int64_t micros = durationCount<Microseconds>(*deadline - system_clock::now());
-        if (micros <= 0) {
-            return false;  // Synthesize a timeout.
-        }
-
-        if (micros > int64_t(std::numeric_limits<uint32_t>::max())) {
-            // 2**32 micros is a little over an hour. If this happens, we wait as long as we can,
-            // then return as-if a spurious wakeup happened, rather than a timeout. This will cause
-            // the caller to loop and we will compute a smaller time each pass, eventually reaching
-            // a representable timeout.
-            micros = std::numeric_limits<uint32_t>::max();
-            timeoutOverflow = true;
-        }
-
-	timeout.tv_sec = micros / 1000;
-	timeout.tv_nsec = (micros % 1000) * 1000;
+        umtx_deadline._timeout.tv_sec = durationCount<Seconds>(deadline->time_since_epoch());
+        umtx_deadline._timeout.tv_nsec = durationCount<Nanoseconds>(
+                deadline->time_since_epoch() - stdx::chrono::seconds(umtx_deadline._timeout.tv_sec));
+        umtx_deadline._flags = UMTX_ABSTIME;
+        umtx_deadline._clockid = CLOCK_REALTIME_FAST;
+        uaddr2 = &umtx_deadline;
     }
 
-    if (_umtx_op(const_cast<void*>(uaddr), UMTX_OP_WAIT, old, (void*)sizeof(struct timespec), &timeout) != -1)
-        return true;
-
-    // There isn't a good list of possible errors, so assuming that anything other than a timeout
-    // error is a possible spurious wakeup.
-    return timeoutOverflow || errno != ETIMEDOUT;
+    int umtxOpRet;
+    if ((umtxOpRet = _umtx_op(const_cast<void*>(uaddr), UMTX_OP_WAIT_UINT_PRIVATE, old, (void*)sizeof(struct _umtx_time), uaddr2)) != 0) {
+        if (errno == ETIMEDOUT) {
+            return false;
+        }
+        invariant(umtxOpRet == 0, errorMessage(lastSystemError()));
+    }
+    return true;
 }
 
 #else
