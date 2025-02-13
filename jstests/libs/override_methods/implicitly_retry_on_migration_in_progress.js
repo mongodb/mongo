47d39a82143fd303079b03b4e67c0aad2f230594
Silvia Surroca <silvia.surroca@mongodb.com>
2025-02-13 08:16:56 +0100

    SERVER-100645 Set internalQueryExecYield to their default values on tests and retry upon QueryPlanKilled error (#32197)
    GitOrigin-RevId: 9a557026e861013df13a0c02b6701b753b83d31f

--- jstests/libs/override_methods/implicitly_retry_on_migration_in_progress.js
+++ jstests/libs/override_methods/implicitly_retry_on_migration_in_progress.js
@@ -6,16 +6,33 @@
 import {OverrideHelpers} from "jstests/libs/override_methods/override_helpers.js";
 
 // These are all commands that can return BackgroundOperationInProgress error codes.
-const commandsToRetry = new Set([
+const kCommandsToRetry = new Set([
     "createIndexes",
     "moveCollection",
     "reshardCollection",
     "unshardCollection",
+    "find",
+    "aggregate",
+]);
+
+const kErrorsToRetry = new Set([
+    ErrorCodes.ReshardCollectionInProgress,
+    ErrorCodes.ConflictingOperationInProgress,
+    // Queries may return a QueryPlanKilled errors if there are background moveCollections or range
+    // deletions.
+    ErrorCodes.QueryPlanKilled,
 ]);
 
 // Commands known not to work with migrations so tests can fail immediately with a clear error.
 const kDisallowedCommandsInsideTxns = [];
-const kDisallowedCommandsOutsideTxns = ["getMore"];
+const kDisallowedCommandsOutsideTxns = [
+    // Disallow running 'getMore' command outside a txn with random moveCollection or chunk
+    // migrations in the background. This is because getMore may fail with a QueryPlanKilled error.
+    // When this happens, there is no way to implicitly retry the operation since the entire
+    // operation should be retried from the beginning.
+    // When this is the case, the test should be tagged as `requires_getmore`.
+    "getMore"
+];
 
 const kTimeout = 10 * 60 * 1000;
 const kInterval = 50;  // milliseconds
@@ -23,12 +40,6 @@ const kInterval = 50;  // milliseconds
 // Make it easier to understand whether or not returns from the assert.soon are being retried.
 const kNoRetry = true;
 const kRetry = false;
-
-function hasConflictingMigrationpInProgress(res) {
-    // Only these are retryable.
-    return res.code === ErrorCodes.ReshardCollectionInProgress ||
-        res.code === ErrorCodes.ConflictingOperationInProgress;
-}
 
 // function runCommandWithRetries(conn, dbName, commandName, commandObj, func, makeFuncArgs) {
 function runCommandWithMigrationRetries(conn, dbName, commandName, commandObj, func, makeFuncArgs) {
@@ -42,9 +53,8 @@ function runCommandWithMigrationRetries(conn, dbName, commandName, commandObj, f
 
     if (disallowedCommands.includes(commandName)) {
         throw new Error(`Cowardly refusing to run command ${
-            (inTransaction
-                 ? "inside"
-                 : "outside")} of transaction with random moveCollection in the backgorund ${
+            (inTransaction ? "inside" : "outside")}
+            " of transaction with random moveCollection or chunk migrations in the backgorund ${
             tojson(commandObj)}`);
     }
 
@@ -60,23 +70,23 @@ function runCommandWithMigrationRetries(conn, dbName, commandName, commandObj, f
                 return kNoRetry;
             }
 
-            // Commands that are not in the allowlist should never fail with this error code.
-            if (!commandsToRetry.has(commandName)) {
+            // Commands that are not in the allowlist should not be retried.
+            if (!kCommandsToRetry.has(commandName)) {
                 return kNoRetry;
             }
 
-            let message = "Retrying the " + commandName +
-                " command because a migration operation is in progress (attempt " + attempt +
-                "): " + tojson(res);
-
-            // This handles the retry case when run against a standalone, replica set, or mongos
-            // where both shards returned the same response.
-            if (hasConflictingMigrationpInProgress(res)) {
-                print(message);
+            // Retry if the errors is in the `kErrorsToRetry` list.
+            if (kErrorsToRetry.has(res.code)) {
+                jsTestLog("Retrying the " + commandName +
+                          " command due to concurrent migrations/range deletions (attempt " +
+                          attempt + "): " + tojson(res));
                 return kRetry;
             }
 
-            print("done retrying " + commandName);
+            if (attempt > 1) {
+                jsTestLog("Done retrying " + commandName +
+                          " got a non-retryable error: " + tojson(res));
+            }
             return kNoRetry;
         },
         () => "Timed out while retrying command '" + tojson(commandObj) +
