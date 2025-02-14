7956523002611820babb44646471c4cbfccaa52e
Moustafa Maher <m.maher@mongodb.com>
2025-02-14 07:06:28 -0800

    SERVER-100595 Increase maxFailedInitialSyncAttempts for Jepsen_config_fuzzer suites (#32418)
    GitOrigin-RevId: c0ea0a643187595d644c23540e7be87e84e13c3d

--- buildscripts/resmokelib/generate_fuzz_config/__init__.py
+++ buildscripts/resmokelib/generate_fuzz_config/__init__.py
@@ -40,6 +40,10 @@ class GenerateFuzzConfig(Subcommand):
         set_parameters = utils.load_yaml(set_parameters)
         # This is moved from Jepsen mongod.conf to have only one setParameter key value pair.
         set_parameters["enableTestCommands"] = True
+        # Increase the number of retry attempts for FCBIS due to the limitation of having only one
+        # $backupCursor open at a time. This restriction causes nodes to wait for others to complete
+        # their synchronization, leading to repeated failures.
+        set_parameters["numInitialSyncAttempts"] = 20
         set_parameters["testingDiagnosticsEnabled"] = True
         conf = {
             "setParameter": set_parameters,
