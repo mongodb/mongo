6a37740f02d8e95707cba44235f2651a409d187f
Victor Ghita <tomi.gvf@gmail.com>
2025-02-19 20:45:34 +0000

    SERVER-100461 Extend the Query Shape Stability suite to assert on all discoverable nodes (#32054)
    GitOrigin-RevId: 234e71ec91d1c1f73ab14f1ae8a4aba5ab982202

--- jstests/libs/override_methods/query_shape_hash_stability.js
+++ jstests/libs/override_methods/query_shape_hash_stability.js
@@ -4,30 +4,38 @@
  * same.
  **/
 import {getCommandName, getExplainCommand, getInnerCommand} from "jstests/libs/cmd_object_utils.js";
+import {DiscoverTopology} from "jstests/libs/discover_topology.js";
 import {OverrideHelpers} from "jstests/libs/override_methods/override_helpers.js";
 import {QuerySettingsUtils} from "jstests/libs/query/query_settings_utils.js";
 
+/**
+ * Assert that all elements of 'array' are identical.
+ */
 function assertAllEqual(array) {
     assert(array.every(x => x === array[0]), `not all elements are same: ${array}`);
 }
 
-function runCommandOverride(conn, dbName, cmdName, cmdObj, clientFunction, makeFuncArgs) {
-    function assertQueryShapeHashStability() {
-        const innerCmd = getInnerCommand(cmdObj);
-        if (!QuerySettingsUtils.isSupportedCommand(getCommandName(innerCmd))) {
-            return;
+/**
+ * Given a connection, discover all the cluster connected nodes (both mongod and mongos), and
+ * assert that all the explain results for 'explainCmd' have identical query shape hashes.
+ */
+export function assertQueryShapeHashStability(conn, dbName, explainCmd) {
+    try {
+        const queryShapeHashes = DiscoverTopology.findNonConfigNodes(conn)
+                                     .map(host => new Mongo(host).getDB(dbName))
+                                     .map(db => assert.commandWorked(db.runCommand(explainCmd)))
+                                     .map(explain => explain.queryShapeHash);
+        assertAllEqual(queryShapeHashes);
+    } catch (ex) {
+        const expectedErrorCodes =
+            [ErrorCodes.CommandOnShardedViewNotSupportedOnMongod, ErrorCodes.NamespaceNotFound];
+        if (!expectedErrorCodes.includes(ex.code)) {
+            throw ex;
         }
-
-        // Wrap command into explain, if it's not explain yet.
-        const db = conn.getDB(dbName);
-        const explainCmd = getExplainCommand(innerCmd);
-
-        // Run the explain three times and ensure that all 'queryShapeHash' values are equal.
-        assertAllEqual([explainCmd, explainCmd, explainCmd]
-                           .map(cmd => assert.commandWorked(db.runCommand(cmd)))
-                           .map(explain => explain.queryShapeHash));
     }
+}
 
+function runCommandOverride(conn, dbName, cmdName, cmdObj, clientFunction, makeFuncArgs) {
     // Do not run explain on queries that have 'batchSize' set to zero, as in majority of the tests
     // we are expecting an error when calling getMore() on that cursor.
     const hasBatchSizeZero = cmdObj.cursor && cmdObj.cursor.batchSize === 0;
@@ -35,7 +43,15 @@ function runCommandOverride(conn, dbName, cmdName, cmdObj, clientFunction, makeF
     if (res.ok && !hasBatchSizeZero) {
         // Only run the test if the original command works. Some tests assert on commands failing,
         // so we should simply bubble these commands through without any additional checks.
-        OverrideHelpers.withPreOverrideRunCommand(assertQueryShapeHashStability);
+        OverrideHelpers.withPreOverrideRunCommand(() => {
+            const innerCmd = getInnerCommand(cmdObj);
+            if (!QuerySettingsUtils.isSupportedCommand(getCommandName(innerCmd))) {
+                return;
+            }
+            // Wrap command into explain, if it's not explain yet.
+            const explainCmd = getExplainCommand(innerCmd);
+            assertQueryShapeHashStability(conn, dbName, explainCmd);
+        });
     }
     return res;
 }
