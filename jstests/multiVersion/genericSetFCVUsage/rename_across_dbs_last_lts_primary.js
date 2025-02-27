4d6b7c1923c3d33d455a4c158130d46015a6f128
Ernesto Rodriguez Reina <erreina@users.noreply.github.com>
2025-02-27 15:22:16 -0500

    SERVER-75003 Remove rename_across_dbs tests from genericSetFCVUsage. (#32885)
    GitOrigin-RevId: 8c929efe49bd880307bf9b57a3f69b8f41e8ff39

--- jstests/multiVersion/genericSetFCVUsage/rename_across_dbs_last_lts_primary.js
+++
@@ -1,9 +0,0 @@
-import {RenameAcrossDatabasesTest} from "jstests/replsets/libs/rename_across_dbs.js";
-
-const nodes = [{binVersion: 'last-lts'}, {binVersion: 'latest'}, {}];
-const options = {
-    nodes: nodes,
-    setFeatureCompatibilityVersion: lastLTSFCV,
-};
-
-new RenameAcrossDatabasesTest(options).run();
--- jstests/multiVersion/genericSetFCVUsage/rename_across_dbs_last_lts_secondary.js
+++
@@ -1,9 +0,0 @@
-import {RenameAcrossDatabasesTest} from "jstests/replsets/libs/rename_across_dbs.js";
-
-const nodes = [{binVersion: 'latest'}, {binVersion: 'last-lts'}, {}];
-const options = {
-    nodes: nodes,
-    setFeatureCompatibilityVersion: lastLTSFCV,
-};
-
-new RenameAcrossDatabasesTest(options).run();
