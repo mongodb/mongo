36b904742a4b909a87543e447ae742ae80a3113d
Allison Easton <allison.easton@mongodb.com>
2025-03-10 11:04:16 +0100

    Revert "SERVER-101855 Check preconditions in the remove shard commit coordinator (#33238)" (#33297)
    GitOrigin-RevId: a3a2c7beacd30e11ab0a611bc1a0c4505003a106

--- src/mongo/db/s/remove_shard_commit_coordinator.h
+++ src/mongo/db/s/remove_shard_commit_coordinator.h
@@ -70,11 +70,8 @@ private:
                                          const CancellationToken& token,
                                          const Status& status) noexcept override;
 
-    // Checks that the shard still exists in the cluster and that the draining flag is still set.
-    void _checkShardExistsAndIsDraining(OperationContext* opCtx);
-
-    // Joins migrations on the config server if we are transitioning from dedicated and checks
-    // if there are range deletions to wait for.
+    // Joins migrations on the config server if we are transitioning from dedicated and checks if
+    // there are range deletions to wait for.
     void _joinMigrationsAndCheckRangeDeletions();
 
     // Stops ongoing ddl operations (excluding topology changes) and waits for any ongoing
