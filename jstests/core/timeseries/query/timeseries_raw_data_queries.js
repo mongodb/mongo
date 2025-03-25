62c131aa50c1e2e3a1f3adc4e261ebcb3463d975
Malik Endsley <malik.endsley@mongodb.com>
2025-03-25 13:27:39 -0400

    SERVER-101294: Split timeseries_buckets_crud_ops.js in two (#33953)
    GitOrigin-RevId: e5d1dae8b1cbe3ff5e81c3d02649bb930d12e889

---
+++ jstests/core/timeseries/query/timeseries_raw_data_queries.js
@@ -0,0 +1,75 @@
+/**
+ * Performs basic read operations on a time-series buckets collection through its time-series view
+ * namespace using rawData.
+ * @tags: [
+ *   requires_timeseries,
+ *   featureFlagRawDataCrudOperations,
+ *   does_not_support_transactions,
+ *   # TODO (SERVER-101293): Remove this tag.
+ *   known_query_shape_computation_problem,
+ * ]
+ */
+
+// Set up some testing buckets to work with
+const timeField = "t";
+const metaField = "m";
+const t = new Date("2002-05-29T00:00:00Z");
+
+const coll = db[jsTestName()];
+
+assert.commandWorked(db.createCollection(
+    coll.getName(), {timeseries: {timeField: timeField, metaField: metaField}}));
+assert.commandWorked(coll.insertMany([
+    {[timeField]: t, [metaField]: "1", v: "replacement"},
+    {[timeField]: t, [metaField]: "2", v: "baz"},
+    {[timeField]: t, [metaField]: "2", v: "qux"},
+]));
+
+assert(coll.drop());
+
+function crudTest(fn, addStartingMeasurements = true) {
+    db.createCollection(coll.getName(), {timeseries: {timeField: timeField, metaField: metaField}});
+    if (addStartingMeasurements) {
+        assert.commandWorked(coll.insertMany([
+            {[timeField]: t, [metaField]: "1", v: "foo"},
+            {[timeField]: t, [metaField]: "1", v: "bar"},
+            {[timeField]: t, [metaField]: "2", v: "baz"},
+        ]));
+    }
+    fn();
+    assert(coll.drop());
+}
+
+// aggregate()
+crudTest(() => {
+    const agg = coll.aggregate(
+        [
+            {$match: {"control.count": 2}},
+        ],
+        {rawData: true});
+    assert.eq(agg.toArray().length, 1);
+});
+
+// count()
+crudTest(() => {
+    assert.eq(coll.count({"control.count": 2}, {rawData: true}), 1);
+});
+
+// distinct()
+crudTest(() => {
+    assert.eq(coll.distinct("control.count", {}, {rawData: true}).sort(), [1, 2]);
+});
+
+// find()
+crudTest(() => {
+    assert.eq(coll.find().rawData().length(), 2);
+    assert.eq(coll.find({"control.count": 2}).rawData().length(), 1);
+});
+
+// findOne()
+crudTest(() => {
+    const retrievedBucket =
+        coll.findOne({"control.count": 2}, null, null, null, null, true /* rawData */);
+    assert.eq(retrievedBucket.control.count, 2);
+    assert.eq(retrievedBucket.meta, "1");
+});
--- jstests/core/timeseries/write/timeseries_raw_data_writes.js
+++ jstests/core/timeseries/write/timeseries_raw_data_writes.js
@@ -1,5 +1,6 @@
 /**
- * Performs basic CRUD operations on a time-series buckets collection.
+ * Performs basic write operations on a time-series buckets collection through its time-series view
+ * namespace using rawData.
  * @tags: [
  *   requires_timeseries,
  *   does_not_support_transactions,
@@ -10,28 +11,26 @@
  * ]
  */
 
-import {FixtureHelpers} from "jstests/libs/fixture_helpers.js";
-
 // Set up some testing buckets to work with
 const timeField = "t";
 const metaField = "m";
 const t = new Date("2002-05-29T00:00:00Z");
 
 const coll = db[jsTestName()];
-const bucketsColl = db["system.buckets." + coll.getName()];
 
-db.createCollection(coll.getName(), {timeseries: {timeField: timeField, metaField: metaField}});
+assert.commandWorked(db.createCollection(
+    coll.getName(), {timeseries: {timeField: timeField, metaField: metaField}}));
 assert.commandWorked(coll.insertMany([
     {[timeField]: t, [metaField]: "1", v: "replacement"},
     {[timeField]: t, [metaField]: "2", v: "baz"},
     {[timeField]: t, [metaField]: "2", v: "qux"},
 ]));
 
-const testBuckets = bucketsColl.find().toArray();
+const testBuckets = coll.find().rawData().toArray();
 const insertBucket = testBuckets.filter(bucket => bucket.meta == "2")[0];
 const replaceOneBucket = testBuckets.filter(bucket => bucket.meta == "1")[0];
 
-coll.drop();
+assert(coll.drop());
 
 function crudTest(fn, addStartingMeasurements = true) {
     db.createCollection(coll.getName(), {timeseries: {timeField: timeField, metaField: metaField}});
@@ -45,21 +44,6 @@ function crudTest(fn, addStartingMeasurements = true) {
     fn();
     assert(coll.drop());
 }
-
-// aggregate()
-crudTest(() => {
-    const agg = coll.aggregate(
-        [
-            {$match: {"control.count": 2}},
-        ],
-        {rawData: true});
-    assert.eq(agg.toArray().length, 1);
-});
-
-// count()
-crudTest(() => {
-    assert.eq(coll.count({"control.count": 2}, {rawData: true}), 1);
-});
 
 // remove()
 crudTest(() => {
@@ -76,25 +60,6 @@ crudTest(() => {
     assert.eq(coll.deleteMany({"control.version": 2}, {rawData: true}).deletedCount, 2);
 });
 
-// distinct()
-crudTest(() => {
-    assert.eq(coll.distinct("control.count", {}, {rawData: true}).sort(), [1, 2]);
-});
-
-// find()
-crudTest(() => {
-    assert.eq(coll.find().rawData().length(), 2);
-    assert.eq(coll.find({"control.count": 2}).rawData().length(), 1);
-});
-
-// findOne()
-crudTest(() => {
-    const retrievedBucket =
-        coll.findOne({"control.count": 2}, null, null, null, null, true /* rawData */);
-    assert.eq(retrievedBucket.control.count, 2);
-    assert.eq(retrievedBucket.meta, "1");
-});
-
 // findAndModify()
 crudTest(() => {
     const newBucket = coll.findAndModify(
@@ -106,17 +71,19 @@ crudTest(() => {
 crudTest(() => {
     const deletedBucket = coll.findOneAndDelete({"control.count": 2}, {rawData: true});
     assert.eq(deletedBucket.control.count, 2);
-    assert.eq(bucketsColl.count({"control.count": 2}), 0);
+    assert.eq(coll.count({"control.count": 2}, {rawData: true}), 0);
 });
 
 // findOneAndReplace()
 crudTest(() => {
-    const updatedBucket = bucketsColl.findOne({"control.count": 2});
+    const updatedBucket =
+        coll.findOne({"control.count": 2}, null, null, null, null, true /* rawData */);
     updatedBucket.meta = "3";
     const replacedBucket =
         coll.findOneAndReplace({"control.count": 2}, updatedBucket, {rawData: true});
     assert.eq(replacedBucket.meta, "1");
-    assert.eq(bucketsColl.findOne({"control.count": 2}).meta, "3");
+    assert.eq(coll.findOne({"control.count": 2}, null, null, null, null, true /* rawData */).meta,
+              "3");
 });
 
 // findOneAndUpdate()
@@ -124,33 +91,34 @@ crudTest(() => {
     const updatedBucket =
         coll.findOneAndUpdate({"control.count": 2}, {$set: {meta: "3"}}, {rawData: true});
     assert.eq(updatedBucket.meta, "1");
-    assert.eq(bucketsColl.findOne({"control.count": 2}).meta, "3");
+    assert.eq(coll.findOne({"control.count": 2}, null, null, null, null, true /* rawData */).meta,
+              "3");
 });
 
 // insert()
 crudTest(() => {
     coll.insert(insertBucket, {rawData: true});
-    assert.eq(bucketsColl.findOne(), insertBucket);
+    assert.eq(coll.findOne({}, null, null, null, null, true /* rawData */), insertBucket);
 }, false);
 
 // insertOne()
 crudTest(() => {
     coll.insertOne(insertBucket, {rawData: true});
-    assert.eq(bucketsColl.findOne(), insertBucket);
-    assert.eq(bucketsColl.find().length(), 1);
+    assert.eq(coll.findOne({}, null, null, null, null, true /* rawData */), insertBucket);
+    assert.eq(coll.find().rawData().length(), 1);
 }, false);
 
 // insertMany()
 crudTest(() => {
     coll.insertMany(testBuckets, {rawData: true});
-    assert.eq(bucketsColl.find().length(), 2);
+    assert.eq(coll.find().rawData().length(), 2);
 }, false);
 
 // update()
 crudTest(() => {
     assert.eq(coll.update({"control.count": 2}, {$set: {meta: "3"}}, {rawData: true}).nModified, 1);
-    assert.eq(bucketsColl.find({"control.count": 2}).length(), 1);
-    assert.eq(bucketsColl.find({"control.count": 2})[0].meta, "3");
+    assert.eq(coll.find({"control.count": 2}).rawData().length(), 1);
+    assert.eq(coll.find({"control.count": 2}).rawData()[0].meta, "3");
 });
 
 // updateOne()
@@ -158,8 +126,8 @@ crudTest(() => {
     assert.eq(
         coll.updateOne({"control.count": 2}, {$set: {meta: "3"}}, {rawData: true}).modifiedCount,
         1);
-    assert.eq(bucketsColl.find({"control.count": 2}).length(), 1);
-    assert.eq(bucketsColl.find({"control.count": 2})[0].meta, "3");
+    assert.eq(coll.find({"control.count": 2}).rawData().length(), 1);
+    assert.eq(coll.find({"control.count": 2}).rawData()[0].meta, "3");
 });
 
 // updateMany()
@@ -167,7 +135,7 @@ crudTest(() => {
     assert.eq(
         coll.updateMany({"control.version": 2}, {$set: {meta: "3"}}, {rawData: true}).modifiedCount,
         2);
-    assert.eq(bucketsColl.find({"meta": "3"}).length(), 2);
+    assert.eq(coll.find({"meta": "3"}).rawData().length(), 2);
 });
 
 // replaceOne()
@@ -176,7 +144,7 @@ delete replaceOneBucket["_id"];
 crudTest(() => {
     assert.eq(
         coll.replaceOne({"control.count": 2}, replaceOneBucket, {rawData: true}).modifiedCount, 1);
-    assert.eq(bucketsColl.find({"meta": "1"}).length(), 1);
-    assert.eq(bucketsColl.find({"meta": "1"})[0].meta, "1");
-    assert.eq(bucketsColl.find({"control.max.v": "replacement"}).length(), 1);
+    assert.eq(coll.find({"meta": "1"}).rawData().length(), 1);
+    assert.eq(coll.find({"meta": "1"}).rawData()[0].meta, "1");
+    assert.eq(coll.find({"control.max.v": "replacement"}).rawData().length(), 1);
 });
