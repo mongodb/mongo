9026c2868c11beed9a5390578af2515eaaeabaa2
Erwin Pe <erwin.pe@mongodb.com>
2025-04-03 13:23:41 -0400

    SERVER-101637 Define FLE2FindTextPayload type (#34435)
    GitOrigin-RevId: 3a2cae6a73bdf58401a77d4c0f1c535c98114df5

--- src/mongo/crypto/fle_fields_util.h
+++ src/mongo/crypto/fle_fields_util.h
@@ -37,6 +37,7 @@ class FLE2EncryptionPlaceholder;
 class FLE2RangeFindSpec;
 class FLE2RangeInsertSpec;
 class FLE2TextSearchInsertSpec;
+class FLE2FindTextPayload;
 /**
  * Extra validation for the placeholder struct to verify that range placeholders have min/max
  * endpoints. Will throw a uassert if the placeholder does not pass validation.
@@ -45,6 +46,7 @@ void validateIDLFLE2EncryptionPlaceholder(const FLE2EncryptionPlaceholder* place
 void validateIDLFLE2RangeFindSpec(const FLE2RangeFindSpec* placeholder);
 void validateIDLFLE2RangeInsertSpec(const FLE2RangeInsertSpec* spec);
 void validateIDLFLE2TextSearchInsertSpec(const FLE2TextSearchInsertSpec* spec);
+void validateIDLFLE2FindTextPayload(const FLE2FindTextPayload* spec);
 void validateQueryBounds(BSONType indexType, ImplicitValue lb, ImplicitValue ub);
 bool isInfinite(ImplicitValue val);
 }  // namespace mongo
