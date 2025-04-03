9e5f95b7b9c42d5baaad3a742dde350f3a7dded7
Malik Endsley <malik.endsley@mongodb.com>
2025-04-03 16:40:33 -0400

    SERVER-103313: Exclude timeseries_raw_data_internal_getmore.js from incompatible suites (#34522)
    GitOrigin-RevId: b9f3b2018636d30aa5232439e0d7d6561dc90708

--- jstests/core/timeseries/query/timeseries_raw_data_internal_getmore.js
+++ jstests/core/timeseries/query/timeseries_raw_data_internal_getmore.js
@@ -8,6 +8,10 @@
  *   # Refusing to run a test that issues commands that may return different values after a failover
  *   does_not_support_stepdowns,
  *   known_query_shape_computation_problem,  # TODO (SERVER-103069): Remove this tag.
+ *   # This test will hang if initial sync ignores the setParameter command.
+ *   incompatible_with_initial_sync,
+ *   # Running setParameter is not allowed with a signed security token.
+ *   not_allowed_with_signed_security_token,
  * ]
  */
 
