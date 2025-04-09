5cd99676f694a7fd41b5a4157c97c98caec9e7be
Zack Winter <3457246+zackwintermdb@users.noreply.github.com>
2025-04-09 14:07:21 -0700

    SERVER-103642 Rename sdp-triager to devprod-build-automation (#34842)
    GitOrigin-RevId: ec12204baa979dd1acd04b1592e66da02afbe339

--- buildscripts/sync_repo_with_copybara.py
+++ buildscripts/sync_repo_with_copybara.py
@@ -171,7 +171,7 @@ def send_failure_message_to_slack(expansions):
 
     evg_api = RetryingEvergreenApi.get_api(config_file=".evergreen.yml")
     evg_api.send_slack_message(
-        target="#sdp-triager",
+        target="#devprod-build-automation",
         msg=error_msg,
     )
 
@@ -481,7 +481,7 @@ def main():
         ):
             return
 
-        # Send a failure message to #sdp-triager if the Copybara sync task fails.
+        # Send a failure message to #devprod-build-automation if the Copybara sync task fails.
         send_failure_message_to_slack(expansions)
 
 
