596bb3ce99dd19a65487d4f3199ebca24861568d
Shreyas Kalyan <35750327+shreyaskalyan@users.noreply.github.com>
2025-04-15 12:30:26 -0400

    SERVER-103790 Extend the FindReplicaSetHostTimeoutMS timeout in slow test variants (#35031)
    Co-authored-by: Shreyas Kalyan <shreyas.kalyan@mongodb.com>
    Co-authored-by: Steve McClure <steve.mcclure@mongodb.com>
    GitOrigin-RevId: e119e11e38e11a6e63520985330cdfd6f62a07b6

--- src/mongo/shell/shell_options.cpp
+++ src/mongo/shell/shell_options.cpp
@@ -88,6 +88,7 @@ const std::set<std::string> kSetShellParameterAllowlist = {
     "skipShellCursorFinalize",
     "tlsOCSPSlowResponderWarningSecs",
     "enableDetailedConnectionHealthMetricLogLines",
+    "defaultFindReplicaSetHostTimeoutMS",
     "multitenancySupport"};
 
 std::string getMongoShellHelp(StringData name, const moe::OptionSection& options) {
