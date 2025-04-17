fe46a0d4f1485c5d9a8043b19938e2cb5027ba06
Parker Felix <68665637+parker-felix@users.noreply.github.com>
2025-04-17 16:28:25 -0400

    SERVER-103848 Fix debug output in sbe_explain_rejected_plans.js (#35067)
    GitOrigin-RevId: 4c70bb7348b046824150a83c406072569ef88e2e

--- jstests/core/query/sbe/sbe_explain_rejected_plans.js
+++ jstests/core/query/sbe/sbe_explain_rejected_plans.js
@@ -42,15 +42,16 @@ const explain = coll.find({a: 7, b: 9}).explain("executionStats");
 let ixscans = getPlanStages(getWinningPlanFromExplain(explain), "IXSCAN");
 assert.neq(ixscans.length, 0, explain);
 for (let ixscan of ixscans) {
-    assert.eq(ixscan.indexName, a1b1IndexName, "Expected a1b1 index to be used", {explain});
+    assert.eq(ixscan.indexName, a1b1IndexName, {reason: "Expected a1b1 index to be used", explain});
 }
 
 // Verify that the winning SBE plan has index scan stage on 'a_1_b_1'.
 const executionStages = getExecutionStages(explain);
-assert.neq(executionStages.length, 0, "Expected explain stages to be present", {explain});
+assert.neq(executionStages.length, 0, {reason: "Expected explain stages to be present", explain});
 for (let executionStage of executionStages) {
     let ixscans = getPlanStages(executionStage, "ixseek");
-    assert.neq(ixscans.length, 0, "Expected ixseek to be present", {explain, executionStages});
+    assert.neq(
+        ixscans.length, 0, {reason: "Expected ixseek to be present", explain, executionStages});
     for (let ixscan of ixscans) {
         assert.eq(ixscan.indexName,
                   a1b1IndexName,
@@ -62,11 +63,15 @@ for (let executionStage of executionStages) {
 // Verify that rejected plans should have index scan on 'a_1' or 'b_1'.
 for (let rejectedPlan of getRejectedPlans(explain)) {
     let ixscans = getPlanStages(getRejectedPlan(rejectedPlan), "IXSCAN");
-    assert.neq(ixscans.length, 0, "Expected rejected plans to use a1 or b1 index", {explain});
+    assert.neq(
+        ixscans.length, 0, {reason: "Expected rejected plans to use a1 or b1 index", explain});
     for (let ixscan of ixscans) {
-        assert.contains(ixscan.indexName,
-                        [a1IndexName, b1IndexName],
-                        "Expected rejected plans to use a1 or b1 index",
-                        {ixscan, ixscans, rejectedPlan, explain});
+        assert.contains(ixscan.indexName, [a1IndexName, b1IndexName], {
+            reason: "Expected rejected plans to use a1 or b1 index",
+            ixscan,
+            ixscans,
+            rejectedPlan,
+            explain
+        });
     }
 }
