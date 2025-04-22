06d1b0c95e5eb315a049fe5ddb9f0f13f07100e8
wolfee <adam.farkas@mongodb.com>
2025-04-22 19:10:41 +0200

    SERVER-102818 Add a test suite that does add/remove shard in the background (#35156)
    GitOrigin-RevId: 08135babc4eae4559bf1877af39ab6f8cdc39ccc

--- buildscripts/resmokelib/suite_hierarchy.py
+++ buildscripts/resmokelib/suite_hierarchy.py
@@ -53,7 +53,7 @@ SUITE_HIERARCHY = {
     "concurrency_sharded_local_read_write_multi_stmt_txn_with_balancer": {
         "concurrency_sharded_local_read_write_multi_stmt_txn": {}
     },
-    "concurrency_sharded_multi_stmt_txn_with_balancer_and_config_transitions": {
+    "concurrency_sharded_multi_stmt_txn_with_balancer_and_config_transitions_and_add_remove_shard": {
         "concurrency_sharded_multi_stmt_txn_with_balancer": {
             "concurrency_sharded_multi_stmt_txn": {}
         }
@@ -61,7 +61,7 @@ SUITE_HIERARCHY = {
     "concurrency_sharded_multi_stmt_txn_stepdown_terminate_kill_primary": {
         "concurrency_sharded_multi_stmt_txn": {}
     },
-    "concurrency_sharded_stepdown_terminate_kill_primary_with_balancer_and_config_transitions": {
+    "concurrency_sharded_stepdown_terminate_kill_primary_with_balancer_and_config_transitions_and_add_remove_shard": {
         "concurrency_sharded_stepdown_terminate_kill_primary_with_balancer": {
             # The stepdown suite is not considered a superset of concurrency_sharded_replication
             # because the stepdown suite uses retryable writes whereas the vanilla suite does not.
