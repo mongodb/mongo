a278c3c6959ace4c54a3427cab26a6eb6e3af6a6
Varun Ravichandran <varun.ravichandran@mongodb.com>
2025-04-30 00:41:37 -0700

    SERVER-104205: more specific netstat parsing (#35301) (#35562)
    GitOrigin-RevId: 621b4dcc119e051ba28a400b0c82a7b8548e37e1

--- src/mongo/shell/check_log.js
+++ src/mongo/shell/check_log.js
@@ -143,7 +143,7 @@ checkLog = (function() {
 
     /*
      * Calls the 'getLog' function on the provided connection 'conn' to see if a log with the
-     * provided id is found in the logs. If the id is found it looks up the specified attrribute by
+     * provided id is found in the logs. If the id is found it looks up the specified attribute by
      * attrName and checks if the msg is found in its value. Note: this function does not throw an
      * exception, so the return value should not be ignored.
      */
