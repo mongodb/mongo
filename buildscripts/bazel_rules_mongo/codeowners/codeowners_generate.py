a66bf2b770abb0147904b4bb31ade909a81c11a1
Steve McClure <steve.mcclure@mongodb.com>
2025-05-02 08:56:03 -0400

    SERVER-104638: Temporarily disable requiring new files to have CodeOwners (#35678)
    GitOrigin-RevId: 9aeab143b665f162c2150c427d401b2f909f1e92

--- buildscripts/bazel_rules_mongo/codeowners/codeowners_generate.py
+++ buildscripts/bazel_rules_mongo/codeowners/codeowners_generate.py
@@ -256,10 +256,12 @@ def check_new_files(codeowners_binary_path: str, expansions_file: str, branch: s
         for file in unowned_new_files:
             print(f"- {file}", file=sys.stderr)
         print(
-            "New files are required to have code owners. See http://go/codeowners-ug",
+            "New files will soon be required to have code owners. See http://go/codeowners-ug",
             file=sys.stderr,
         )
-        return 1
+        # TODO: SERVER-104638
+        # Set back to 1 when we are ready to enforce this
+        return 0
 
     print("There are no new files added that are unowned.")
     return 0
