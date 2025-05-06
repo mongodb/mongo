bd3f3022e9e32228784f641d40c33dcfb046b739
Pol Piñol Castuera <pol.pinol@mongodb.com>
2025-05-06 23:35:46 +0200

    SERVER-98664  checkMetadataConsistency to ignore the 'admin' database in the global catalog (#33190)
    GitOrigin-RevId: 91649bc98b9f7c0a76abf5e95d68ebc769f3d02d

--- jstests/sharding/check_metadata_consistency.js
+++ jstests/sharding/check_metadata_consistency.js
@@ -363,5 +363,23 @@ function assertNoInconsistencies() {
     assertNoInconsistencies();
 })();
 
+(function testUnexpectedAdminDatabaseInGlobalCatalog() {
+    jsTest.log("Executing testUnexpectedAdminDatabaseInGlobalCatalog");
+
+    // Register the 'admin' database in the global catalog.
+    assert.commandWorked(st.configRS.getPrimary().getDB("config").databases.insertOne({
+        _id: "admin",
+        primary: "config",
+        version: {uuid: UUID(), lastMod: NumberInt(1), timestamp: Timestamp(1, 0)}
+    }));
+
+    // Check no inconsistencies are present, but with a successful run of checkMetadataConsistency.
+    assertNoInconsistencies();
+
+    // Clean up the inconsistency for following test cases.
+    assert.commandWorked(
+        st.configRS.getPrimary().getDB("config").databases.deleteOne({_id: "admin"}));
+})();
+
 st.stop();
 })();
