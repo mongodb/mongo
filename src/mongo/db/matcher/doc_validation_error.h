1394f5d05e42824e69623b89ac18be247669469a
Mihai Andrei <mihai.andrei@mongodb.com>
2025-05-09 09:40:35 -0400

    SERVER-97403 Replace invariants with tasserts in document validation error generation (#30687) (#31811)
    GitOrigin-RevId: d1ee9d21b26defc19aa922ea1f946b8de3b11f09

--- src/mongo/db/matcher/doc_validation_error.h
+++ src/mongo/db/matcher/doc_validation_error.h
@@ -53,7 +53,9 @@ public:
     static constexpr auto code = ErrorCodes::DocumentValidationFailure;
     static std::shared_ptr<const ErrorExtraInfo> parse(const BSONObj& obj);
     explicit DocumentValidationFailureInfo(const BSONObj& err) : _details(err.getOwned()) {
-        invariant(!err.isEmpty());
+        tassert(9740340,
+                "Cannot construct 'DocumentValidationFailureInfo' with non-empty error",
+                !err.isEmpty());
     }
     const BSONObj& getDetails() const;
     void serialize(BSONObjBuilder* bob) const override;
