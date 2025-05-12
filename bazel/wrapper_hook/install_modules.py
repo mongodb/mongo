d15b96d6718da37ea07b1ecf3bf9f105ae6bb409
Daniel Moody <dmoody256@gmail.com>
2025-05-12 09:26:07 -0500

    SERVER-105026 fall back to local to install python modules (#36065)
    GitOrigin-RevId: adfe208408dc651b46c1cb32ca76379ad03be646

--- bazel/wrapper_hook/install_modules.py
+++ bazel/wrapper_hook/install_modules.py
@@ -119,16 +119,26 @@ def install_modules(bazel):
         deps_needed = deps
 
     if need_to_install:
-        subprocess.run(
-            [
-                bazel,
-                "build",
+        cmd = [
+            bazel,
+            "build",
+        ] + ["@poetry//:library_" + dep for dep in deps_needed]
+        proc = subprocess.run(
+            cmd
+            + [
                 "--remote_download_all",
                 "--bes_backend=",
                 "--bes_results_url=",
             ]
-            + ["@poetry//:library_" + dep for dep in deps_needed]
         )
+        if proc.returncode != 0:
+            print("Failed to install modules using remote exec/cache, falling back to local...")
+            proc = subprocess.run(
+                cmd
+                + [
+                    "--config=local",
+                ]
+            )
         deps_missing = search_for_modules(deps_needed, deps_installed)
         if deps_missing:
             raise Exception(f"Failed to install python deps {deps_missing}")
