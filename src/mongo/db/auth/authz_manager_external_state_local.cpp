991917f0bb8de72594daa89cb05c79a68e1a2f2e
Gabriel Marks <gabriel.marks@mongodb.com>
2025-05-20 09:43:45 -0400

    SERVER-93497 SERVER-93616 Defer user cache invalidation to onCommit handler (#27743)
    GitOrigin-RevId: d600b61a35043dbe8c02420abe9c5bdec3de3324

--- src/mongo/db/auth/authz_manager_external_state_local.cpp
+++ src/mongo/db/auth/authz_manager_external_state_local.cpp
@@ -822,6 +822,37 @@ constexpr auto kOpInsert = "i"_sd;
 constexpr auto kOpUpdate = "u"_sd;
 constexpr auto kOpDelete = "d"_sd;
 
+using InvalidateFn = std::function<void(OperationContext*, AuthorizationManager*)>;
+
+/**
+ * When we are currently in a WriteUnitOfWork, invalidation of the user cache must wait until
+ * after the operation causing the invalidation commits. This is because if in a different thread,
+ * the cache is read after invalidation but before the related commit occurs, the cache will be
+ * populated with stale data until the next invalidation.
+ */
+void invalidateUserCacheOnCommit(OperationContext* opCtx, InvalidateFn invalidate) {
+    auto unit = opCtx->recoveryUnit();
+    if (unit) {
+        auto state = unit->getState();
+        if (state == RecoveryUnit::State::kInactiveInUnitOfWork ||
+            state == RecoveryUnit::State::kActive) {
+            LOGV2_DEBUG(
+                9349700,
+                5,
+                "In WriteUnitOfWork, deferring user cache invalidation to onCommit handler");
+            unit->onCommit([invalidate = std::move(invalidate)](boost::optional<Timestamp>) {
+                LOGV2_DEBUG(9349701, 3, "Invalidating user cache in onCommit handler");
+                if (auto opCtx = cc().getOperationContext()) {
+                    invalidate(opCtx, AuthorizationManager::get(opCtx->getServiceContext()));
+                }
+            });
+            return;
+        }
+    }
+    LOGV2_DEBUG(9349702, 3, "Not in WriteUnitOfWork, invalidating user cache immediately");
+    invalidate(opCtx, AuthorizationManager::get(opCtx->getServiceContext()));
+}
+
 void _invalidateUserCache(OperationContext* opCtx,
                           AuthorizationManagerImpl* authzManager,
                           StringData op,
@@ -842,15 +873,22 @@ void _invalidateUserCache(OperationContext* opCtx,
                                      str::stream() << "_id entries for user documents must be of "
                                                       "the form <dbname>.<username>.  Found: "
                                                    << id));
-            authzManager->invalidateUserCache(opCtx);
+
+            invalidateUserCacheOnCommit(
+                opCtx, [](auto* opCtx, auto* am) { am->invalidateUserCache(opCtx); });
             return;
         }
         UserName userName(id.substr(splitPoint + 1), id.substr(0, splitPoint), coll.getTenant());
-        authzManager->invalidateUserByName(opCtx, userName);
+        invalidateUserCacheOnCommit(opCtx, [userName = std::move(userName)](auto* opCtx, auto* am) {
+            am->invalidateUserByName(opCtx, userName);
+        });
     } else if (const auto& tenant = coll.getTenant()) {
-        authzManager->invalidateUsersByTenant(opCtx, tenant.get());
+        invalidateUserCacheOnCommit(opCtx, [tenantId = tenant.get()](auto* opCtx, auto* am) {
+            am->invalidateUsersByTenant(opCtx, tenantId);
+        });
     } else {
-        authzManager->invalidateUserCache(opCtx);
+        invalidateUserCacheOnCommit(opCtx,
+                                    [](auto* opCtx, auto* am) { am->invalidateUserCache(opCtx); });
     }
 }
 }  // namespace
