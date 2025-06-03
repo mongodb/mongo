3e61217b3561d3b71bcd4fc71d0416ef5f042931
Zack Winter <3457246+zackwintermdb@users.noreply.github.com>
2025-06-03 13:51:50 -0700

    SERVER-103931 Add task to build index for SourceGraph via scip-clang (#36830)
    GitOrigin-RevId: 8cd77a3ec23d5dfa93db9e45dbf3cd2d4f114d5f

--- evergreen/bazel_run.sh
+++ evergreen/bazel_run.sh
@@ -4,6 +4,8 @@
 # Required environment variables:
 # * ${target} - Build target
 # * ${args} - Extra command line args to pass to "bazel run"
+# * ${env} - Env variable string to set (ex. ENV_VAR_ABC=123)
+# * ${redact_args} - If set, redact the args in the report
 
 # Needed for evergreen scripts that use evergreen expansions and utility methods.
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
@@ -17,7 +19,7 @@ set -o verbose
 activate_venv
 
 # Use `eval` to force evaluation of the environment variables in the echo statement:
-eval echo "Execution environment: Args: ${args} Target: ${target}"
+eval echo "Execution environment: Args: ${args} Target: ${target} Env: ${env} redact_args: ${redact_args}"
 
 source ./evergreen/bazel_utility_functions.sh
 source ./evergreen/bazel_RBE_supported.sh
@@ -46,17 +48,23 @@ if [[ -f /etc/os-release ]]; then
   fi
 fi
 
+if [[ -n "$redact_args" ]]; then
+  INVOCATION_WITH_REDACTION="${target}"
+else
+  INVOCATION_WITH_REDACTION="${target} ${args}"
+fi
+
 # Print command being run to file that can be uploaded
 echo "python buildscripts/install_bazel.py" > bazel-invocation.txt
-echo "bazel run --verbose_failures ${bazel_compile_flags} ${task_compile_flags} ${LOCAL_ARG} ${args} ${target}" >> bazel-invocation.txt
+echo "bazel run --verbose_failures ${bazel_compile_flags} ${task_compile_flags} ${LOCAL_ARG} ${INVOCATION_WITH_REDACTION}" >> bazel-invocation.txt
 
 # Run bazel command, retrying up to five times
 MAX_ATTEMPTS=5
 for ((i = 1; i <= $MAX_ATTEMPTS; i++)); do
-  eval $BAZEL_BINARY run --verbose_failures $LOCAL_ARG ${args} ${target} >> bazel_output.log 2>&1 && RET=0 && break || RET=$? && sleep 60
+  eval $env $BAZEL_BINARY run --verbose_failures $LOCAL_ARG ${target} ${args} >> bazel_output.log 2>&1 && RET=0 && break || RET=$? && sleep 10
   if [ $i -lt $MAX_ATTEMPTS ]; then echo "Bazel failed to execute, retrying ($(($i + 1)) of $MAX_ATTEMPTS attempts)... " >> bazel_output.log 2>&1; fi
   $BAZEL_BINARY shutdown
 done
 
-$python ./buildscripts/simple_report.py --test-name "bazel run ${args} ${target}" --log-file bazel_output.log --exit-code $RET
+$python ./buildscripts/simple_report.py --test-name "bazel run ${INVOCATION_WITH_REDACTION}" --log-file bazel_output.log --exit-code $RET
 exit $RET
