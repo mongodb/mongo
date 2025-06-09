31463b2ac10e3c2fb5b612d00f236c0f6da2d544
LucyMongoDB <lucy.wu@mongodb.com>
2025-06-09 14:10:04 -0400

    SERVER-96382 Accept transient transaction errors as an expected failure in the write_without_shard_key_single test (#37055)
    GitOrigin-RevId: 29b0205d50a820502de58800a2e279af54ae27c0

--- jstests/sharding/updateOne_without_shard_key/write_without_shard_key_single_shard_data_placement_change.js
+++ jstests/sharding/updateOne_without_shard_key/write_without_shard_key_single_shard_data_placement_change.js
@@ -9,6 +9,7 @@
  * ]
  */
 
+import {withRetryOnTransientTxnError} from "jstests/libs/auto_retry_transaction_in_sharding.js";
 import {configureFailPoint} from "jstests/libs/fail_point_util.js";
 import {ShardingTest} from "jstests/libs/shardingtest.js";
 import {
@@ -50,31 +51,33 @@ function getDB(session, testCase) {
 }
 
 function runTest(testCase) {
-    // Do not test the bulkWrite case in multiversion
-    // TODO (SERVER-103224) remove this check after the solution is backported.
-    if (!is82OrAbove() && testCase.cmdObj.bulkWrite)
-        return;
-    const session = st.s.startSession();
-    session.startTransaction({readConcern: {level: "snapshot"}});
-    session.getDatabase(dbName).getCollection(collName2).insert({x: 1});
-    let hangDonorAtStartOfRangeDel =
-        configureFailPoint(st.rs1.getPrimary(), "suspendRangeDeletion");
+    withRetryOnTransientTxnError(() => {
+        // Do not test the bulkWrite case in multiversion
+        // TODO (SERVER-103224) remove this check after the solution is backported.
+        if (!is82OrAbove() && testCase.cmdObj.bulkWrite)
+            return;
+        const session = st.s.startSession();
+        session.startTransaction({readConcern: {level: "snapshot"}});
+        session.getDatabase(dbName).getCollection(collName2).insert({x: 1});
+        let hangDonorAtStartOfRangeDel =
+            configureFailPoint(st.rs1.getPrimary(), "suspendRangeDeletion");
 
-    // Move all chunks for testDb.testColl to shard0.
-    assert.commandWorked(
-        st.s.adminCommand({moveChunk: nss, find: {x: 0}, to: st.shard0.shardName}));
-    hangDonorAtStartOfRangeDel.wait();
+        // Move all chunks for testDb.testColl to shard0.
+        assert.commandWorked(
+            st.s.adminCommand({moveChunk: nss, find: {x: 0}, to: st.shard0.shardName}));
+        hangDonorAtStartOfRangeDel.wait();
 
-    // This write cmd MUST fail, the data moved to another shard, we can't try on shard0 nor
-    // shard1 with the original clusterTime of the transaction.
-    assert.commandFailedWithCode(getDB(session, testCase).runCommand(testCase.cmdObj),
-                                 ErrorCodes.MigrationConflict);
+        // This write cmd MUST fail, the data moved to another shard, we can't try on shard0 nor
+        // shard1 with the original clusterTime of the transaction.
+        assert.commandFailedWithCode(getDB(session, testCase).runCommand(testCase.cmdObj),
+                                     ErrorCodes.MigrationConflict);
 
-    hangDonorAtStartOfRangeDel.off();
+        hangDonorAtStartOfRangeDel.off();
 
-    // Reset the chunk distribution for the next test.
-    assert.commandWorked(
-        st.s.adminCommand({moveChunk: nss, find: {x: 0}, to: st.shard1.shardName}));
+        // Reset the chunk distribution for the next test.
+        assert.commandWorked(
+            st.s.adminCommand({moveChunk: nss, find: {x: 0}, to: st.shard1.shardName}));
+    });
 }
 
 let testCases = [
