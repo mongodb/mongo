ee820248226112f915942d937e59d3a8b61b149a
Will Buerger <59492746+wbuerger46@users.noreply.github.com>
2025-06-10 11:11:08 -0400

    SERVER-106063: Add initial Extensions API files with codeowners (#37066)
    GitOrigin-RevId: 5b3647eddc68a1a65016e83a614d8d5fff17fc74

---
+++ src/mongo/db/extension/host/document_source_extension.h
@@ -0,0 +1,68 @@
+/**
+ *    Copyright (C) 2025-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+#pragma once
+
+#include "mongo/base/string_data.h"
+#include "mongo/bson/bsonobj.h"
+#include "mongo/db/pipeline/document_source.h"
+
+namespace mongo {
+
+/**
+ * A DocumentSource wrapper for an extension aggregation stage.
+ */
+class DocumentSourceExtension : public DocumentSource, public exec::agg::Stage {
+public:
+    const char* getSourceName() const override;
+
+    Id getId() const override;
+
+    GetNextResult doGetNext() override;
+
+    boost::optional<DistributedPlanLogic> distributedPlanLogic() override;
+
+    void addVariableRefs(std::set<Variables::Id>* refs) const override {}
+
+    StageConstraints constraints(Pipeline::SplitState pipeState) const override;
+
+    Value serialize(const SerializationOptions& opts) const override;
+
+private:
+    DocumentSourceExtension(StringData name, boost::intrusive_ptr<ExpressionContext> expCtx, Id id);
+
+    // Do not support copy or move.
+    DocumentSourceExtension(const DocumentSourceExtension&) = delete;
+    DocumentSourceExtension(DocumentSourceExtension&&) = delete;
+    DocumentSourceExtension& operator=(const DocumentSourceExtension&) = delete;
+    DocumentSourceExtension& operator=(DocumentSourceExtension&&) = delete;
+
+    std::string _stageName;
+    Id _id;
+};
+}  // namespace mongo
