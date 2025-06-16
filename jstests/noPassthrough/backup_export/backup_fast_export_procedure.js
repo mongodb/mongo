2170f040ed56383cba40c2ec285f1f0cf2eef9c7
Marcos Grillo <marcos.grillo@mongodb.com>
2025-06-16 12:36:37 +0200

    SERVER-106127 Add authentication to backup's fast export procedure test (#37248)
    GitOrigin-RevId: c6930dcdc3d69bad376dea779b791cd9aea92e37

--- jstests/noPassthrough/backup_export/backup_fast_export_procedure.js
+++ jstests/noPassthrough/backup_export/backup_fast_export_procedure.js
@@ -10,48 +10,98 @@ TestData.disableImplicitSessions = true;
 import {configureFailPoint} from "jstests/libs/fail_point_util.js";
 import {ShardingTest} from "jstests/libs/shardingtest.js";
 
-let kDbName = 'db';
-let kCollName = 'coll';
-const kNss = kDbName + '.' + kCollName;
-let st = new ShardingTest({shards: 3});
-const kShard0Name = st.shard0.shardName;
-const kShard1Name = st.shard1.shardName;
-const kShard2Name = st.shard2.shardName;
-assert.commandWorked(st.s.adminCommand({enableSharding: kDbName, primaryShard: kShard0Name}));
-assert.commandWorked(st.s.adminCommand({shardCollection: kNss, key: {x: 1}}));
-assert.commandWorked(st.s.getCollection(kNss).insert({x: -10001}));
-assert.commandWorked(st.s.getCollection(kNss).insert({x: 0}));
-assert.commandWorked(st.s.getCollection(kNss).insert({x: 10001}));
-const suspendRangeDeletionShard0Fp =
-    configureFailPoint(st.rs0.getPrimary(), "suspendRangeDeletion");
-// Set up a sharded collection with a chunk and a document on each shard.
-assert.commandWorked(st.s.adminCommand({split: kNss, middle: {x: -10000}}));
-assert.commandWorked(st.s.adminCommand({split: kNss, middle: {x: 10000}}));
-assert.commandWorked(st.s.adminCommand({moveChunk: kNss, find: {x: -10000}, to: kShard1Name}));
-suspendRangeDeletionShard0Fp.wait();
-assert.commandWorked(st.s.adminCommand({moveChunk: kNss, find: {x: 10000}, to: kShard2Name}));
-suspendRangeDeletionShard0Fp.wait();
-// Open a cursor with a comment to be consumed on the shard directly.
-assert.commandWorked(st.s.getDB(kDbName).runCommand(
-    {"find": kCollName, batchSize: 0, sort: {$natural: 1}, comment: kNss}));
-let shardCursors = st.s.getDB("admin")
-                       .aggregate([
-                           {$currentOp: {idleCursors: true}},
-                           {$match: {type: "idleCursor", "cursor.originatingCommand.comment": kNss}}
-                       ])
-                       .map(x => {
-                           // By using the comment as an identifier, open a cursor directly on each
-                           // shard which should take into consideration ownership of data.
-                           let kDbName = x.ns.split(".", 1);
-                           let conn = new Mongo(x.host);
-                           let cursor = new DBCommandCursor(
-                               conn.getDB(kDbName),
-                               {ok: 1, cursor: {id: x.cursor.cursorId, ns: x.ns, firstBatch: []}});
-                           return cursor;
-                       });
+function setupCluster(st, routerConn, kDbName, kCollName, kNss, cursorComment) {
+    const kShard0Name = st.shard0.shardName;
+    const kShard1Name = st.shard1.shardName;
+    const kShard2Name = st.shard2.shardName;
 
-assert.eq(1, shardCursors[0].toArray().length);
-assert.eq(1, shardCursors[1].toArray().length);
-assert.eq(1, shardCursors[2].toArray().length);
-suspendRangeDeletionShard0Fp.off();
-st.stop();
+    assert.commandWorked(
+        routerConn.adminCommand({enableSharding: kDbName, primaryShard: kShard0Name}));
+    assert.commandWorked(routerConn.adminCommand({shardCollection: kNss, key: {x: 1}}));
+    assert.commandWorked(routerConn.getCollection(kCollName).insert({x: -10001}));
+    assert.commandWorked(routerConn.getCollection(kCollName).insert({x: 0}));
+    assert.commandWorked(routerConn.getCollection(kCollName).insert({x: 10001}));
+    let suspendRangeDeletionShard0Fp =
+        configureFailPoint(st.rs0.getPrimary(), "suspendRangeDeletion");
+    // Set up a sharded collection with a chunk and a document on each shard.
+    assert.commandWorked(routerConn.adminCommand({split: kNss, middle: {x: -10000}}));
+    assert.commandWorked(routerConn.adminCommand({split: kNss, middle: {x: 10000}}));
+    assert.commandWorked(
+        routerConn.adminCommand({moveChunk: kNss, find: {x: -10000}, to: kShard1Name}));
+    suspendRangeDeletionShard0Fp.wait();
+    assert.commandWorked(
+        routerConn.adminCommand({moveChunk: kNss, find: {x: 10000}, to: kShard2Name}));
+    suspendRangeDeletionShard0Fp.wait();
+    // Open a cursor with a comment to be consumed on the shard directly.
+    assert.commandWorked(routerConn.runCommand(
+        {"find": kCollName, batchSize: 0, sort: {$natural: 1}, comment: cursorComment}));
+    return suspendRangeDeletionShard0Fp;
+}
+
+function createCursors(routerConn, cursorComment, setupShardConn) {
+    let shardCursors =
+        routerConn
+            .aggregate([
+                {$currentOp: {idleCursors: true}},
+                {$match: {type: "idleCursor", "cursor.originatingCommand.comment": cursorComment}}
+            ])
+            .map(x => {
+                // By using the comment as an identifier, open a cursor directly on each
+                // shard which should take into consideration ownership of data.
+                let kDbName = x.ns.split(".", 1);
+                let cursor = new DBCommandCursor(
+                    setupShardConn(x.host, kDbName),
+                    {ok: 1, cursor: {id: x.cursor.cursorId, ns: x.ns, firstBatch: []}});
+                return cursor;
+            });
+    return shardCursors;
+}
+
+jsTestLog('Check the fast export procedure without auth');
+{
+    let kDbName = 'db';
+    let kCollName = 'coll';
+    const kNss = kDbName + '.' + kCollName;
+    let st = new ShardingTest({shards: 3});
+    let suspendRangeDeletionShard0Fp =
+        setupCluster(st, st.s.getDB(kDbName), kDbName, kCollName, kNss, kNss);
+    function setupShardConn(host, kDbName) {
+        let conn = new Mongo(host);
+        return conn.getDB(kDbName);
+    }
+    let shardCursors = createCursors(st.s.getDB('admin'), kNss, setupShardConn);
+    assert.eq(1, shardCursors[0].toArray().length);
+    assert.eq(1, shardCursors[1].toArray().length);
+    assert.eq(1, shardCursors[2].toArray().length);
+    suspendRangeDeletionShard0Fp.off();
+    st.stop();
+}
+
+jsTestLog('Check the fast export procedure with auth');
+{
+    let kDbName = 'db';
+    let kCollName = 'coll';
+    const kNss = kDbName + '.' + kCollName;
+    let st = new ShardingTest({
+        shards: 3,
+        keyFile: 'jstests/libs/key1',
+        other: {mongosOptions: {auth: null}, configOptions: {auth: null}, rsOptions: {auth: null}}
+    });
+    let admin = st.s.getDB('admin');
+    admin.createUser({user: 'admin', pwd: 'password', roles: jsTest.adminUserRoles});
+    admin.auth('admin', 'password');
+    let suspendRangeDeletionShard0Fp =
+        setupCluster(st, admin.getSiblingDB(kDbName), kDbName, kCollName, kNss, kNss);
+    function setupShardConn(host, kDbName) {
+        let conn = new Mongo(host);
+        let local = conn.getDB('local');
+        local.auth('__system', 'foopdedoop');
+        return local.getSiblingDB(kDbName);
+    }
+    let shardCursors = createCursors(admin, kNss, setupShardConn);
+    assert.eq(1, shardCursors[0].toArray().length);
+    assert.eq(1, shardCursors[1].toArray().length);
+    assert.eq(1, shardCursors[2].toArray().length);
+    suspendRangeDeletionShard0Fp.off();
+    st.stop();
+}
