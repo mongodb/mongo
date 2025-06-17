d3ecc8623a56db4ae6e0dd932e0dd87dfc9a643f
adriangzz <adriangonzalezmontemayor@gmail.com>
2025-06-17 14:38:30 -0400

    SERVER-106351 Split mixed_mode_sharded_nossl to prevent timeouts (#37363)
    GitOrigin-RevId: b271a283521a784ad55cc7bcb1ba79b8f2a2a377

--- jstests/sslSpecial/mixed_mode_sharded_nossl_part_1.js
+++ jstests/sslSpecial/mixed_mode_sharded_nossl_part_1.js
@@ -1,9 +1,10 @@
 /**
  * This test checks if different mixtures of ssl modes
- * in a sharded clutster can or cannot function
+ * in a sharded clutster can or cannot function. This test is split in 2 parts since it was hitting
+ * timeouts in slow variants.
  */
 
-import {allowTLS, disabled, mixedShardTest, preferTLS} from "jstests/ssl/libs/ssl_helpers.js";
+import {disabled, mixedShardTest, preferTLS} from "jstests/ssl/libs/ssl_helpers.js";
 
 // Due to mixed SSL mode settings, a shard will be unable to establish an outgoing
 // connection to the config server in order to load relevant collection UUIDs into
@@ -17,11 +18,3 @@ mixedShardTest(disabled, disabled, true);
 print("=== Testing disabled/preferTLS cluster - SHOULD FAIL ===");
 mixedShardTest(disabled, preferTLS, false);
 mixedShardTest(preferTLS, disabled, false);
-
-print("=== Testing allowTLS/disabled cluster ===");
-mixedShardTest(disabled, allowTLS, true);
-mixedShardTest(allowTLS, disabled, true);
-
-print("=== Testing allowTLS/preferTLS cluster ===");
-mixedShardTest(preferTLS, allowTLS, true);
-mixedShardTest(allowTLS, preferTLS, true);
