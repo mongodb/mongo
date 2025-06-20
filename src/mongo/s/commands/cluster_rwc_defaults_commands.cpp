7e10e59514cdccce8f417e55d38f5eab3e7aa75c
Pierlauro Sciarelli <pierlauro.sciarelli@mongodb.com>
2025-06-20 17:01:45 +0200

    SERVER-100942 make sure WCE is always returned by setDefaultRWConcern when present (#37466)
    GitOrigin-RevId: cb7ae32f8c23c84c32324cf2568161e85c93415e

--- src/mongo/s/commands/cluster_rwc_defaults_commands.cpp
+++ src/mongo/s/commands/cluster_rwc_defaults_commands.cpp
@@ -94,8 +94,11 @@ public:
                 opCtx->getWriteConcern()),
             Shard::RetryPolicy::kNotIdempotent));
 
-        uassertStatusOK(cmdResponse.commandStatus);
-        uassertStatusOK(cmdResponse.writeConcernStatus);
+        CommandHelpers::filterCommandReplyForPassthrough(cmdResponse.response, &result);
+
+        if (!cmdResponse.commandStatus.isOK() || !cmdResponse.writeConcernStatus.isOK()) {
+            return false;
+        }
 
         // Quickly pick up the new defaults by setting them in the cache.
         auto newDefaults = RWConcernDefault::parse(IDLParserContext("ClusterSetDefaultRWConcern"),
@@ -112,7 +115,6 @@ public:
         }
         ReadWriteConcernDefaults::get(opCtx).setDefault(opCtx, std::move(newDefaults));
 
-        CommandHelpers::filterCommandReplyForPassthrough(cmdResponse.response, &result);
         return true;
     }
 
