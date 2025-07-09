fd39d1dc6ed078c60bdfd88b31e201a057efebcf
Vishnu K <vishnu.kaushik@mongodb.com>
2025-07-09 11:56:03 -0400

    SERVER-101495 set priority:0 on nodes that shouldn't become primary. (#32895) (#38213)
    GitOrigin-RevId: 56639dbcfb39207e3a6031131c275b58a3c26d72

--- jstests/replsets/auth_no_pri.js
+++ jstests/replsets/auth_no_pri.js
@@ -1,25 +1,31 @@
 // Test that you can still authenticate a replset connection to a RS with no primary (SERVER-6665).
-var NODE_COUNT = 3;
-const rs = new ReplSetTest({"nodes": NODE_COUNT, keyFile: "jstests/libs/key1"});
-var nodes = rs.startSet();
+const NODE_COUNT = 2;
+const rs = new ReplSetTest({
+    "nodes": [
+        {},
+        // Set priority: 0 to ensure that this node can't become the primary.
+        {rsConfig: {priority: 0}},
+    ],
+    keyFile: "jstests/libs/key1"
+});
+const nodes = rs.startSet();
 rs.initiate();
 
 // Add user
-var primary = rs.getPrimary();
+const primary = rs.getPrimary();
 primary.getDB("admin").createUser({user: "admin", pwd: "pwd", roles: ["root"]}, {w: NODE_COUNT});
 
 // Can authenticate replset connection when whole set is up.
-var conn = new Mongo(rs.getURL());
+const conn = new Mongo(rs.getURL());
 assert(conn.getDB('admin').auth('admin', 'pwd'));
 assert.commandWorked(conn.getDB('admin').foo.insert({a: 1}, {writeConcern: {w: NODE_COUNT}}));
 
 // Make sure there is no primary
 rs.stop(0);
-rs.stop(1);
-rs.waitForState(nodes[2], ReplSetTest.State.SECONDARY);
+rs.waitForState(nodes[1], ReplSetTest.State.SECONDARY);
 
 // Make sure you can still authenticate a replset connection with no primary
-var conn2 = new Mongo(rs.getURL());
+const conn2 = new Mongo(rs.getURL());
 conn2.setSecondaryOk();
 assert(conn2.getDB('admin').auth({user: 'admin', pwd: 'pwd', mechanism: "SCRAM-SHA-1"}));
 assert.eq(1, conn2.getDB('admin').foo.findOne().a);
