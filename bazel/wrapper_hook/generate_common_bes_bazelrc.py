a0d81e9b5cd2f1dd9e45ceb619729c6fd02cf54b
Zack Winter <3457246+zackwintermdb@users.noreply.github.com>
2025-07-21 11:57:08 -0700

    SERVER-107854 Dump Bazel Command line in JSON instead of a raw string (#38825)
    GitOrigin-RevId: d4c57ab649d0ebef3f3d3fe2a8a67411105e150b

--- bazel/wrapper_hook/generate_common_bes_bazelrc.py
+++ bazel/wrapper_hook/generate_common_bes_bazelrc.py
@@ -1,5 +1,6 @@
 import base64
 import hashlib
+import json
 import os
 import pathlib
 import socket
@@ -74,8 +75,7 @@ def write_workstation_bazelrc(args):
         filtered_args = filtered_args[:filtered_args.index("--")] + ["--", "(REDACTED)"]
 
     developer_build = os.environ.get("CI") is None
-    filtered_command_line = " ".join(filtered_args)
-    b64_cmd_line = base64.b64encode(filtered_command_line.encode()).decode()
+    b64_cmd_line = base64.b64encode(json.dumps(filtered_args).encode()).decode()
     normalized_os = sys.platform.replace("win32", "windows").replace("darwin", "macos")
     bazelrc_contents = f"""\
 # Generated file, do not modify
