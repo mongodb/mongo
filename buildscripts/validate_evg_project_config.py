7a1b05c781271e9c3f60f1ccd940566e0832ee25
Steve McClure <steve.mcclure@mongodb.com>
2025-07-23 13:18:46 -0400

    SERVER-107073: 8.2 Build and Verification Branching Task (#38920)
    GitOrigin-RevId: e1ed2e7bda6f8a7b926a672c3115671c8cc12451

--- buildscripts/validate_evg_project_config.py
+++ buildscripts/validate_evg_project_config.py
@@ -2,7 +2,6 @@ import os.path
 import re
 import subprocess
 import sys
-from collections import defaultdict
 
 import structlog
 import typer
@@ -17,8 +16,6 @@ LOGGER = structlog.get_logger(__name__)
 DEFAULT_LOCAL_EVG_AUTH_CONFIG = os.path.expanduser("~/.evergreen.yml")
 
 DEFAULT_EVG_PROJECT_NAME = "mongodb-mongo-master"
-DEFAULT_EVG_NIGHTLY_PROJECT_NAME = "mongodb-mongo-master-nightly"
-DEFAULT_EVG_PROJECT_CONFIG = "etc/evergreen.yml"
 DEFAULT_EVG_NIGHTLY_PROJECT_CONFIG = "etc/evergreen_nightly.yml"
 
 UNMATCHED_REGEXES = [
@@ -69,74 +66,23 @@ def main(
     evg_project_config_map = {evg_project_name: DEFAULT_EVG_NIGHTLY_PROJECT_CONFIG}
     if evg_project_name == DEFAULT_EVG_PROJECT_NAME:
         evg_project_config_map = {
-            DEFAULT_EVG_PROJECT_NAME: DEFAULT_EVG_PROJECT_CONFIG,
             DEFAULT_EVG_NIGHTLY_PROJECT_NAME: DEFAULT_EVG_NIGHTLY_PROJECT_CONFIG,
         }
 
-    shared_evg_validate_messages = []
-    error_on_evg_validate_messages = defaultdict(list)
-
-    num_of_projects = len(evg_project_config_map)
     evergreen_bin = find_evergreen_binary("evergreen")
-    for project, project_config in evg_project_config_map.items():
+    for _, project_config in evg_project_config_map.items():
         cmd = [
             evergreen_bin,
             "--config",
             evg_auth_config,
-            "validate",
-            "--project",
-            project,
+            "evaluate",
             "--path",
             project_config,
         ]
         LOGGER.info(f"Running command: {cmd}")
-        result = subprocess.run(cmd, capture_output=True, text=True)
-        interesting_messages = result.stdout.strip().split("\n")[:-1]
+        subprocess.run(cmd, capture_output=True, text=True, check=True)
 
-        (error_on_evg_validate_messages[project], allowed_if_not_shared) = messages_to_report(
-            interesting_messages, num_of_projects
-        )
-        shared_evg_validate_messages.extend(allowed_if_not_shared)
-
-    error_on_shared_evg_validate_messages = []
-    for message in set(shared_evg_validate_messages):
-        if shared_evg_validate_messages.count(message) == num_of_projects:
-            error_on_shared_evg_validate_messages.append(message)
-
-    exit_code = 0
-    all_configs = list(evg_project_config_map.values())
-    all_projects = list(evg_project_config_map.keys())
-
-    for project, errors in error_on_evg_validate_messages.items():
-        if len(errors) > 0:
-            exit_code = 1
-            project_config = evg_project_config_map[project]
-            LOGGER.info(HORIZONTAL_LINE)
-            LOGGER.error(f"Config '{project_config}' for '{project}' evergreen project has errors:")
-            for error in errors:
-                LOGGER.error(error)
-                if any(regex.match(error) for regex in UNMATCHED_REGEXES):
-                    LOGGER.info(
-                        "Unmatched selector/criteria are allowed if they are tagged based (using '!' or '.'), but not if they directly name a task/task group"
-                    )
-
-    if len(error_on_shared_evg_validate_messages) > 0:
-        exit_code = 1
-        LOGGER.info(HORIZONTAL_LINE)
-        LOGGER.error(
-            f"Configs {all_configs} for evergreen projects {all_projects} have errors"
-            f" (they can be fixed in either config):"
-        )
-        for error in error_on_shared_evg_validate_messages:
-            LOGGER.error(error)
-
-    if exit_code == 0:
-        LOGGER.info(HORIZONTAL_LINE)
-        LOGGER.info(
-            f"Config(s) {all_configs} for evergreen project(s) {all_projects} is(are) valid"
-        )
-
-    sys.exit(exit_code)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
