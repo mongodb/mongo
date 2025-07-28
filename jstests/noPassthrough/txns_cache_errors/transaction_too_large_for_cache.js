f25b75a1381af0e2cfc869dce11f657ce4a1fd49
Aaron B <aaron.balsara@mongodb.com>
2025-07-28 11:31:01 +1000

    SERVER-107751 Lower cache threshold for transaction_too_large_for_cache (#38791)
    Co-authored-by: Adeline Chen <adeline.chen@mongodb.com>
    GitOrigin-RevId: 793b848134269dc944f16aa91b1ec4f0b43195b6

--- jstests/noPassthrough/txns_cache_errors/transaction_too_large_for_cache.js
+++ jstests/noPassthrough/txns_cache_errors/transaction_too_large_for_cache.js
@@ -12,7 +12,20 @@
 import {ReplSetTest} from "jstests/libs/replsettest.js";
 import {TxnUtil} from "jstests/libs/txns/txn_util.js";
 
-const replSet = new ReplSetTest({nodes: 1});
+const replSet = new ReplSetTest({
+    nodes: 1,
+    nodeOptions: {
+        // Shrink the cache to cause cache pressure sooner.
+        wiredTigerEngineConfigString: "cache_size=256M",
+        setParameter: {
+            enableTemporarilyUnavailableExceptions: false,
+            // Disable the transaction too large for cache check.
+            transactionTooLargeForCacheThreshold: 0.1,
+            // Disable the periodic cache pressure rollback runner.
+            cachePressureQueryPeriodMilliseconds: 0,
+        }
+    }
+});
 replSet.startSet();
 replSet.initiate();
 
@@ -33,7 +46,7 @@ coll.drop();
 // A single collection can only have up to 64 indexes. Cap at _id + 1 text index + 62 indexes.
 const serverStatus = assert.commandWorked(replSet.getPrimary().getDB('admin').serverStatus());
 const wtCacheSizeBytes = serverStatus.wiredTiger.cache["maximum bytes configured"];
-const nIndexes = Math.min(Math.ceil(wtCacheSizeBytes * 2 / (1024 * 1024 * 1024)), 62);
+const nIndexes = 2;
 assert.commandWorked(coll.createIndex({x: "text"}));
 for (let i = 0; i < nIndexes; i++) {
     assert.commandWorked(coll.createIndex({x: 1, ["field" + i]: 1}));
@@ -41,37 +54,41 @@ for (let i = 0; i < nIndexes; i++) {
 
 // Retry the transaction until we eventually hit the TransactionTooLargeForCache. Only retry on
 // WriteConflict error, which is the only expected error besides TransactionTooLargeForCache.
-assert.soon(() => {
-    session.startTransaction();
+assert.soon(
+    () => {
+        session.startTransaction();
 
-    // Keep inserting documents in the transaction until we eventually hit the cache limit.
-    let insertCount = 0;
-    let result;
-    try {
-        while (true) {
-            try {
-                ++insertCount;
-                result = coll.insert(doc1);
-                assert.commandWorked(result);
-            } catch (e) {
-                session.abortTransaction();
-                assert.commandFailedWithCode(result, ErrorCodes.TransactionTooLargeForCache);
-                break;
+        // Keep inserting documents in the transaction until we eventually hit the cache limit.
+        let insertCount = 0;
+        let result;
+        try {
+            while (true) {
+                try {
+                    ++insertCount;
+                    result = coll.insert(doc1);
+                    assert.commandWorked(result);
+                } catch (e) {
+                    session.abortTransaction();
+                    assert.commandFailedWithCode(result, ErrorCodes.TransactionTooLargeForCache);
+                    break;
+                }
             }
+        } catch (e) {
+            assert.commandFailedWithCode(result, ErrorCodes.WriteConflict);
+            jsTestLog("Failed with an insertCount of " + insertCount);
+            return false;
         }
-    } catch (e) {
-        assert.commandFailedWithCode(result, ErrorCodes.WriteConflict);
-        return false;
-    }
 
-    // The error should not have a transient transaction error label. At this point the error must
-    // have been TransactionTooLargeForCache. We do this check here to avoid having to check
-    // exception types in the outermost catch, in case this assertion fires.
-    assert(!TxnUtil.isTransientTransactionError(result), result);
+        // The error should not have a transient transaction error label. At this point the error
+        // must have been TransactionTooLargeForCache. We do this check here to avoid having to
+        // check exception types in the outermost catch, in case this assertion fires.
+        assert(!TxnUtil.isTransientTransactionError(result), result);
 
-    jsTestLog("Iterations until TransactionTooLargeForCache occured: " + insertCount);
+        jsTestLog("Iterations until TransactionTooLargeForCache occured: " + insertCount);
 
-    return true;
-}, "Expected a transaction to eventually fail with TransactionTooLargeForCache error.");
+        return true;
+    },
+    "Expected a transaction to eventually fail with TransactionTooLargeForCache error.",
+    15 * 60 * 1000);
 
 replSet.stopSet();
