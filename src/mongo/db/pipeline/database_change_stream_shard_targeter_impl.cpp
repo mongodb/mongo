fc611f0a9931ef0c6abaab252be88b5e8eadc0d9
Denis Grebennicov <denis.grebennicov@mongodb.com>
2025-07-28 18:25:18 +0200

    SERVER-107839 Create collection, database and allDatabases shard targeters (#39152)
    GitOrigin-RevId: 5c80347460e8af889aeb63836af0f270a7d01c43

---
+++ src/mongo/db/pipeline/database_change_stream_shard_targeter_impl.cpp
@@ -0,0 +1,52 @@
+/**
+ *    Copyright (C) 2025-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/db/pipeline/database_change_stream_shard_targeter_impl.h"
+
+#include "mongo/util/assert_util.h"
+
+namespace mongo {
+
+ShardTargeterDecision DatabaseChangeStreamShardTargeterImpl::initialize(
+    Timestamp atClusterTime, ChangeStreamReaderContext& context) {
+    MONGO_UNIMPLEMENTED_TASSERT(10783903);
+}
+
+ShardTargeterDecision DatabaseChangeStreamShardTargeterImpl::handleEvent(
+    const Document& event, ChangeStreamReaderContext& context) {
+    MONGO_UNIMPLEMENTED_TASSERT(10783904);
+}
+
+std::pair<ShardTargeterDecision, boost::optional<Timestamp>>
+DatabaseChangeStreamShardTargeterImpl::startChangeStreamSegment(
+    Timestamp atClusterTime, ChangeStreamReaderContext& context) {
+    MONGO_UNIMPLEMENTED_TASSERT(10783905);
+}
+
+}  // namespace mongo
