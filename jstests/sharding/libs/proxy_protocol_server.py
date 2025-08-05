9b9ebfade68d7095a68289415dfa2e6291ba0291
David Goffredo <david.goffredo@mongodb.com>
2025-08-05 12:06:51 -0400

    SERVER-108794: add some more documentation to proxy_protocol_server.py (#39626)
    GitOrigin-RevId: 5e709d647b8226eae7f7f0aa2d9bf958d4185803

--- jstests/sharding/libs/proxy_protocol_server.py
+++ jstests/sharding/libs/proxy_protocol_server.py
@@ -1,6 +1,15 @@
 #! /usr/bin/env python3
 """
 Python script to interact with proxy protocol server.
+
+This script is a wrapper around the [proxy-protocol][1] package.
+The most recent source code is available on [GitHub][2].
+
+The installed version is listed in the
+"[tool.poetry.group.testing.dependencies]" section of `pyproject.toml`.
+
+[1]: https://pypi.org/project/proxy-protocol/
+[2]: https://github.com/icgood/proxy-protocol/
 """
 
 import sys
