26f90410402d02b91f582a650d5d157dc3dd5ca1
Daniel Moody <dmoody256@gmail.com>
2025-08-06 09:42:17 -0500

    SERVER-108778 Fix coverity task to use evergreen MONGO_VERSION (#39613)
    GitOrigin-RevId: 1550bb57696cc6acc7798bcc5e4ecbba771a6779

--- evergreen/coverity_build.sh
+++ evergreen/coverity_build.sh
@@ -1,23 +1,22 @@
 #!/bin/env bash
-
-set -eo pipefail
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+. "$DIR/prelude.sh"
 
 cd src
 
-. evergreen/prelude_venv.sh
+set -eo pipefail
+
 activate_venv
-python buildscripts/install_bazel.py
-bazel_bin="$HOME/.local/bin/bazelisk"
 # number of parallel jobs to use for build.
 # Even with scale=0 (the default), bc command adds decimal digits in case of multiplication. Division by 1 gives us a whole number with scale=0
 bazel_jobs=$(bc <<<"$(grep -c '^processor' /proc/cpuinfo) * .85 / 1")
-build_config="--config=local --jobs=$bazel_jobs --compiler_type=gcc --opt=off --dbg=False --allocator=system"
+build_config="--config=local --jobs=$bazel_jobs --compiler_type=gcc --opt=off --dbg=False --allocator=system --define=MONGO_VERSION=${version}"
 bazel_query='mnemonic("CppCompile|LinkCompile", filter(//src/mongo, deps(//:install-core)) except //src/mongo/db/modules/enterprise/src/streams/third_party/...)'
 bazel_cache="--output_user_root=$workdir/bazel_cache"
 
-python bazel/coverity/generate_coverity_command.py --bazel_executable=$bazel_bin --bazel_cache=$bazel_cache --bazel_query="$bazel_query" $build_config --noinclude_artifacts
-$bazel_bin $bazel_cache build $build_config --build_tag_filters=gen_source //src/...
-bazelBuildCommand="$bazel_bin $bazel_cache build $build_config //src/mongo/db/modules/enterprise/coverity:enterprise_coverity_build"
+python bazel/coverity/generate_coverity_command.py --bazel_executable="bazel" --bazel_cache=$bazel_cache --bazel_query="$bazel_query" $build_config --noinclude_artifacts
+bazel $bazel_cache build $build_config --build_tag_filters=gen_source //src/...
+bazelBuildCommand="bazel $bazel_cache build $build_config //src/mongo/db/modules/enterprise/coverity:enterprise_coverity_build"
 echo "Bazel Build Command: $bazelBuildCommand"
 covIdir="$workdir/covIdir"
 if [ -d "$covIdir" ]; then
