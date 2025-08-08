3c8e0bd81666c2a4af9b218b890d730ca366aa93
Joshua Siegel <39130209+joshbsiegel@users.noreply.github.com>
2025-08-08 18:39:25 -0400

    SERVER-108999 Remove `archive_extensions` and stale functionality (#39833)
    GitOrigin-RevId: 71a96839898c12f7688bc2b487ac5839df3730d3

--- evergreen/functions/find_extensions.sh
+++ evergreen/functions/find_extensions.sh
@@ -4,17 +4,24 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 . "${DIR}/../prelude.sh"
 
-cd src
+# DIR = src/evergreen/functions/
+TOP_LEVEL_DIR="${DIR}/../../.."
+
+# Add extension_paths.yml as the expansion.update command will fail if the file does not exist.
+touch "${TOP_LEVEL_DIR}/extension_paths.yml"
 
 if [[ "${extensions_required:-false}" != "true" ]]; then
-    echo "Skipping task 'extract_extensions' because 'extensions_required' is not set to 'true'."
+    echo "Skipping task 'find_extensions' because 'extensions_required' is not set to 'true'."
     exit 0
+fi
+
+LIB_SRC="${TOP_LEVEL_DIR}/src/dist-test/lib"
 
 # Check if a specific extension name is provided as an argument.
-elif [[ -n "${EXTENSION_NAME}" ]]; then
+if [[ -n "${EXTENSION_NAME}" ]]; then
     echo "Finding specific extension: ${EXTENSION_NAME}."
 
-    EXTENSION_PATH=$(find lib -name "${EXTENSION_NAME}")
+    EXTENSION_PATH=$(find "${LIB_SRC}" -name "${EXTENSION_NAME}")
 
     if [[ -z "${EXTENSION_PATH}" ]]; then
         echo "Error: Could not find the specified extension file: ${EXTENSION_NAME}."
@@ -22,17 +29,17 @@ elif [[ -n "${EXTENSION_NAME}" ]]; then
     fi
 
     echo "Found extension: ${EXTENSION_PATH}."
-    echo "extension_paths: \"${EXTENSION_PATH}\"" >extension_paths.yml
+    echo "extension_paths: \"${EXTENSION_PATH}\"" >"${TOP_LEVEL_DIR}/extension_paths.yml"
 else
     echo "EXTENSION_NAME not provided. Finding all unpacked extensions."
-    # Find all .so files and create a comma-separated list.
-    EXTENSIONS_LIST=$(find lib -name "*.so" | paste -sd, -)
+    # Find all *_extension.so files and create a comma-separated list.
+    EXTENSIONS_LIST=$(find "${LIB_SRC}" -name "*_extension.so" | paste -sd, -)
 
     if [[ -z "${EXTENSIONS_LIST}" ]]; then
-        echo "Could not find any extracted extension files under lib/. Not outputting extension_paths.yml file."
-        exit 0
+        echo "Could not find any extracted extension files under lib/."
+        exit 1
     fi
 
     echo "Found extensions: ${EXTENSIONS_LIST}."
-    echo "extension_paths: \"${EXTENSIONS_LIST}\"" >extension_paths.yml
+    echo "extension_paths: \"${EXTENSIONS_LIST}\"" >"${TOP_LEVEL_DIR}/extension_paths.yml"
 fi
