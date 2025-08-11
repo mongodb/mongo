f0361f42a9790a6992744c907eed7d8395a6e990
Joan Bruguera Micó (at MongoDB) <joan.bruguera-mico@mongodb.com>
2025-08-11 07:32:04 +0000

    SERVER-108923 Update list of enabled tests in viewless timeseries suites (#39767)
    GitOrigin-RevId: 7bb190fd4441f212b9f067ab3a7e93fb118aac4c

--- jstests/core/timeseries/write/timeseries_insert_mixed_schema_docs.js
+++ jstests/core/timeseries/write/timeseries_insert_mixed_schema_docs.js
@@ -2,17 +2,13 @@
  * Tests directly inserting a time-series bucket with mixed schema.
  *
  * @tags: [
- *   # $listCatalog does not include the tenant prefix in its results.
- *   command_not_supported_in_serverless,
  *   requires_timeseries,
- *   does_not_support_viewless_timeseries_yet,
  *   # TODO(SERVER-108445) Reenable this test
  *   multiversion_incompatible,
  * ]
  */
 
-import {TimeseriesTest} from "jstests/core/timeseries/libs/timeseries.js";
-import {getTimeseriesBucketsColl} from "jstests/core/timeseries/libs/viewless_timeseries_util.js";
+import {getTimeseriesCollForRawOps} from "jstests/core/libs/raw_operation_utils.js";
 
 const testDB = db.getSiblingDB(jsTestName());
 const collName = "ts";
@@ -33,25 +29,28 @@ const collName = "ts";
     };
     assert.commandWorked(coll.insert(doc));
     assert.eq(1, coll.find({"m": "meta"}).toArray().length);
-    assert.eq(1, getTimeseriesBucketsColl(coll).find({}).toArray().length);
+    assert.eq(1, getTimeseriesCollForRawOps(coll).find({}).rawData().toArray().length);
 
     // new measurement, same bucket
     doc.a = "bar";
     assert.commandWorked(coll.insert(doc));
     assert.eq(2, coll.find({"m": "meta"}).toArray().length);
-    assert.eq(1, getTimeseriesBucketsColl(coll).find({"meta": "meta"}).toArray().length);
+    assert.eq(1,
+              getTimeseriesCollForRawOps(coll).find({"meta": "meta"}).rawData().toArray().length);
 
     // new measurement, because schema changed, we have a new bucket
     doc.a = 1;
     assert.commandWorked(coll.insert(doc));
     assert.eq(3, coll.find({"m": "meta"}).toArray().length);
-    assert.eq(2, getTimeseriesBucketsColl(coll).find({"meta": "meta"}).toArray().length);
+    assert.eq(2,
+              getTimeseriesCollForRawOps(coll).find({"meta": "meta"}).rawData().toArray().length);
 
     // new measurement, schema changed back, necessitating a third bucket
     doc.a = "bam";
     assert.commandWorked(coll.insert(doc));
     assert.eq(4, coll.find({"m": "meta"}).toArray().length);
-    assert.eq(3, getTimeseriesBucketsColl(coll).find({"meta": "meta"}).toArray().length);
+    assert.eq(3,
+              getTimeseriesCollForRawOps(coll).find({"meta": "meta"}).rawData().toArray().length);
 })();
 
 /**
@@ -66,7 +65,7 @@ const collName = "ts";
     var doc = {t: ISODate(), m: "meta", a: 1, payload: "small"};
     assert.commandWorked(coll.insert(doc));
     assert.eq(1, coll.find({"m": "meta"}).toArray().length);
-    assert.eq(1, getTimeseriesBucketsColl(coll).find({}).toArray().length);
+    assert.eq(1, getTimeseriesCollForRawOps(coll).find({}).rawData().toArray().length);
 
     // new measurement, because schema changed, we have a new bucket
     // previous bug would skip mixed schema check due to handling of large measurements
@@ -74,12 +73,14 @@ const collName = "ts";
     doc.payload = 'A'.repeat(130000);
     assert.commandWorked(coll.insert(doc));
     assert.eq(2, coll.find({"m": "meta"}).toArray().length);
-    assert.eq(2, getTimeseriesBucketsColl(coll).find({"meta": "meta"}).toArray().length);
+    assert.eq(2,
+              getTimeseriesCollForRawOps(coll).find({"meta": "meta"}).rawData().toArray().length);
 
     // new measurement, schema changed back, necessitating a third bucket
     doc.a = 2;
     doc.payload = "small";
     assert.commandWorked(coll.insert(doc));
     assert.eq(3, coll.find({"m": "meta"}).toArray().length);
-    assert.eq(3, getTimeseriesBucketsColl(coll).find({"meta": "meta"}).toArray().length);
+    assert.eq(3,
+              getTimeseriesCollForRawOps(coll).find({"meta": "meta"}).rawData().toArray().length);
 })();
