2ea060b0d8a9b4241601ee19f112a554da985f0b
Saman Memaripour <amirsaman.memaripour@mongodb.com>
2025-08-18 16:17:03 -0400

    SERVER-109362 Make `SeveritySuppressor` lock free (#40157)
    GitOrigin-RevId: c3698d026a5c71c19939fc0931fe7d629c1e6305

--- src/mongo/logv2/log_severity_suppressor.h
+++ src/mongo/logv2/log_severity_suppressor.h
@@ -37,10 +37,10 @@
 #include <boost/multi_index_container.hpp>
 // IWYU pragma: no_include "boost/multi_index/detail/adl_swap.hpp"
 #include "mongo/logv2/log_severity.h"
-#include "mongo/stdx/mutex.h"
-#include "mongo/stdx/unordered_map.h"
+#include "mongo/platform/atomic.h"
 #include "mongo/util/clock_source.h"
 #include "mongo/util/duration.h"
+#include "mongo/util/system_clock_source.h"
 #include "mongo/util/time_support.h"
 
 #include <functional>
@@ -122,35 +122,32 @@ private:
 class SeveritySuppressor {
 public:
     SeveritySuppressor(ClockSource* cs, Milliseconds period, LogSeverity normal, LogSeverity quiet)
-        : _clock(cs), _period{period}, _normal{normal}, _quiet{quiet} {}
+        : _stopWatch(cs ? cs : SystemClockSource::get()),
+          _period{period},
+          _normal{normal},
+          _quiet{quiet} {}
 
     SeveritySuppressor(Milliseconds period, LogSeverity normal, LogSeverity quiet)
         : SeveritySuppressor(nullptr, period, normal, quiet) {}
 
     LogSeverity operator()() {
-        auto now = _now();
-        auto lg = stdx::lock_guard(_mutex);
-        if (_expire <= now) {
-            _expire = now + _period;
-            return _normal;
+        auto elapsed = _stopWatch.elapsed();
+        auto expiresAtMillis = _expiresAtMillis.loadRelaxed();
+        if (expiresAtMillis <= durationCount<Milliseconds>(elapsed)) {
+            const auto nextExpiresAtMillis = durationCount<Milliseconds>(elapsed + _period);
+            if (_expiresAtMillis.compareAndSwap(&expiresAtMillis, nextExpiresAtMillis)) {
+                return _normal;
+            }
         }
         return _quiet;
     }
 
 private:
-    Date_t _now() {
-        if (_clock) {
-            return _clock->now();
-        }
-        return Date_t::now();
-    }
-
-    ClockSource* _clock;
-    Milliseconds _period;
-    LogSeverity _normal;
-    LogSeverity _quiet;
-    stdx::mutex _mutex;
-    Date_t _expire;
+    ClockSource::StopWatch _stopWatch;
+    const Milliseconds _period;
+    const LogSeverity _normal;
+    const LogSeverity _quiet;
+    Atomic<int64_t> _expiresAtMillis{0};
 };
 
 }  // namespace mongo::logv2
