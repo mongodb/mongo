6d907191b0fc3919846a7bd97c52f81e9b2e1ced
Sean Lyons <sean.lyons@mongodb.com>
2025-08-19 17:32:07 -0400

    SERVER-104460 Save bazel invocation for resmoke tests (#40271)
    GitOrigin-RevId: 3bcf82e5098b023854a581cc936ea5056b1cec3b

--- evergreen/resmoke_tests_execute_bazel.sh
+++ evergreen/resmoke_tests_execute_bazel.sh
@@ -27,51 +27,51 @@ if [ -n "${build_timeout_seconds}" ]; then
     TIMEOUT_CMD="timeout ${build_timeout_seconds}"
 fi
 
+ci_flags="--//bazel/resmoke:in_evergreen --test_output=all"
+
 if [ ${should_shuffle} = true ]; then
-    bazel_args="${bazel_args} --test_arg=--shuffle"
+    ci_flags+=" --test_arg=--shuffle"
 elif [ ${should_shuffle} = false ]; then
-    bazel_args="${bazel_args} --test_arg=--shuffleMode=off"
+    ci_flags+=" --test_arg=--shuffleMode=off"
 fi
 
 if [ "${is_patch}" = "true" ]; then
-    bazel_args="${bazel_args} --test_arg=--patchBuild"
+    ci_flags+=" --test_arg=--patchBuild"
 fi
 
 if [ "${skip_symbolization}" = "true" ]; then
-    bazel_args="${bazel_args} --test_arg=--skipSymbolization"
+    ci_flags+=" --test_arg=--skipSymbolization"
 fi
 
 # Add test selection flag based on patch parameter
 if [ "${enable_evergreen_api_test_selection}" = "true" ]; then
-    bazel_args="${bazel_args} --test_arg=--enableEvergreenApiTestSelection"
+    ci_flags+=" --test_arg=--enableEvergreenApiTestSelection"
 fi
 
 # Split comma separated list of strategies
 IFS=',' read -a strategies <<<"$test_selection_strategies_array"
 for strategy in "${strategies[@]}"; do
-    bazel_args+=" --test_arg=--evergreenTestSelectionStrategy=${strategy}"
+    ci_flags+=" --test_arg=--evergreenTestSelectionStrategy=${strategy}"
 done
-
-ALL_FLAGS="${bazel_args} ${bazel_compile_flags} ${task_compile_flags} ${patch_compile_flags}"
 
 # If not explicitly specified on the target, pick a shard count that will fully utilize the current machine.
 BUILD_INFO=$(bazel query ${targets} --output build)
 if [[ "$BUILD_INFO" != *"shard_count ="* ]]; then
     CPUS=$(nproc)
     SIZE=$(echo $BUILD_INFO | grep "size =" | cut -d '"' -f2)
-    TEST_RESOURCES_CPU=$(echo ${ALL_FLAGS} | awk -F'--default_test_resources=cpu=' '{print $2}')
+    TEST_RESOURCES_CPU=$(echo ${bazel_args} ${bazel_compile_flags} ${task_compile_flags} ${patch_compile_flags} | awk -F'--default_test_resources=cpu=' '{print $2}')
     TEST_RESOURCES_CPU=(${TEST_RESOURCES_CPU//,/ })
     declare -A SIZES
     SIZES=(["small"]=0 ["medium"]=1 ["large"]=2 ["enormous"]=3)
     CPUS_PER_SHARD=${TEST_RESOURCES_CPU[${SIZES[$SIZE]}]}
     SHARD_COUNT=$((CPUS / $CPUS_PER_SHARD))
-    ALL_FLAGS+=" --test_sharding_strategy=forced=$SHARD_COUNT"
+    ci_flags+=" --test_sharding_strategy=forced=$SHARD_COUNT"
 fi
 
 set +o errexit
 
 for i in {1..3}; do
-    eval ${TIMEOUT_CMD} ${BAZEL_BINARY} fetch ${ALL_FLAGS} ${targets} && RET=0 && break || RET=$? && sleep 60
+    eval ${TIMEOUT_CMD} ${BAZEL_BINARY} fetch ${ci_flags} ${bazel_args} ${bazel_compile_flags} ${task_compile_flags} ${patch_compile_flags} ${targets} && RET=0 && break || RET=$? && sleep 60
     if [ $RET -eq 124 ]; then
         echo "Bazel fetch timed out after ${build_timeout_seconds} seconds, retrying..."
     else
@@ -80,7 +80,11 @@ for i in {1..3}; do
     $BAZEL_BINARY shutdown
 done
 
-eval ${BAZEL_BINARY} test ${ALL_FLAGS} ${targets}
+# Save the invocation, intentionally excluding ci_flags.
+echo "python buildscripts/install_bazel.py" >bazel-invocation.txt
+echo "bazel test ${bazel_args} ${bazel_compile_flags} ${task_compile_flags} ${patch_compile_flags} ${targets}" >>bazel-invocation.txt
+
+eval ${BAZEL_BINARY} test ${ci_flags} ${bazel_args} ${bazel_compile_flags} ${task_compile_flags} ${patch_compile_flags} ${targets}
 RET=$?
 
 set -o errexit
