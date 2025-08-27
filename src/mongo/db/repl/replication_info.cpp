9d36ba6188aec9b1afaf37b79effc700340656b4
Evelyn Wu <32441547+evelynwu88@users.noreply.github.com>
2025-08-27 16:49:31 -0400

    SERVER-97201 Add a metric in FTDC for replication coordinator mutex wait time (#36539) (#40214)
    GitOrigin-RevId: 3c63e7044ec5f0a268a050ebf3ce3f94dec6054f

--- src/mongo/db/repl/replication_info.cpp
+++ src/mongo/db/repl/replication_info.cpp
@@ -40,6 +40,7 @@
 #include <boost/none.hpp>
 #include <boost/optional/optional.hpp>
 
+#include "mongo/base/counter.h"
 #include "mongo/base/error_codes.h"
 #include "mongo/base/status.h"
 #include "mongo/base/status_with.h"
@@ -120,6 +121,12 @@
 #define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kFTDC
 
 namespace mongo {
+
+auto& replCoordMutexTotalWaitTimeInOplogServerStatus =
+    *MetricBuilder<Counter64>{"repl.waiters.replCoordMutexTotalWaitTimeInOplogServerStatusMillis"};
+
+auto& numReplCoordMutexAcquisitionsInOplogServerStatus =
+    *MetricBuilder<Counter64>("repl.waiters.numReplCoordMutexAcquisitionsInOplogServerStatus");
 
 // Hangs in the beginning of each hello command when set.
 MONGO_FAIL_POINT_DEFINE(shardWaitInHello);
@@ -296,7 +303,12 @@ public:
         }
 
         BSONObjBuilder result;
+
+        // Time the total amount of time spent waiting for repl coord mutex.
+        Timer timer;
         result.append("latestOptime", replCoord->getMyLastAppliedOpTime().getTimestamp());
+        replCoordMutexTotalWaitTimeInOplogServerStatus.increment(timer.millis());
+        numReplCoordMutexAcquisitionsInOplogServerStatus.increment(1);
 
         auto earliestOplogTimestampFetch = [&]() -> Timestamp {
             boost::optional<AutoGetOplogFastPath> oplog = boost::none;
