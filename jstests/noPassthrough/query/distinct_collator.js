9d414256173ad7b8da6afb2179a96e71f27a6115
Jan <jsteemann@users.noreply.github.com>
2025-09-02 18:29:12 +0200

    SERVER-100108 Fix distinct command with custom collator (#37801)
    GitOrigin-RevId: 50454010c4d09432a1c7251f6a0c9c82b67cd49e

---
+++ jstests/noPassthrough/query/distinct_collator.js
@@ -0,0 +1,67 @@
+/**
+ * Verify that distinct works correctly when setting a collator (SERVER-100108).
+ */
+
+import {assertDropAndRecreateCollection} from "jstests/libs/collection_drop_recreate.js";
+const conn = MongoRunner.runMongod();
+const db = conn.getDB("test");
+
+const coll = assertDropAndRecreateCollection(db, jsTestName());
+
+let docs = [];
+docs.push({_id: {a: []}});
+docs.push({_id: {a: [2]}});
+docs.push({_id: {a: [2, 3]}});
+docs.push({_id: {a: [2, 3]}});
+docs.push({_id: 5});
+coll.insert(docs);
+
+const locales = [
+    "bn@collation=traditional",
+    "de@collation=phonebook",
+    "ca",
+    "fr_CA",
+    "simple",
+];
+
+locales.forEach((locale) => {
+    jsTest.log(`Testing locale ${locale}...`);
+
+    // Test distinct command.
+    {
+        const res = coll.runCommand("distinct", {
+            key: "_id.a.0",
+            query: {_id: {$in: [{a: [2, 3]}, 5]}},
+            collation: {locale},
+        });
+        assert.commandWorked(res);
+        assert.eq([2], res.values);
+    }
+
+    // Test find command.
+    {
+        const res = db.runCommand({
+            find: coll.getName(),
+            filter: {_id: {$in: [{a: [2, 3]}, 5]}},
+            collation: {locale},
+        });
+        assert.commandWorked(res);
+        assert.eq([{_id: {a: [2, 3]}}], res.cursor.firstBatch);
+    }
+
+    // Test aggregate command.
+    {
+        const res = db.runCommand({
+            aggregate: coll.getName(),
+            pipeline: [{$match: {_id: {$in: [{a: [2, 3]}, 5]}}}],
+            collation: {locale},
+            cursor: {}
+        });
+        assert.commandWorked(res);
+        assert.eq([{_id: {a: [2, 3]}}], res.cursor.firstBatch);
+    }
+});
+
+coll.drop();
+
+MongoRunner.stopMongod(conn);
