473add959e5a9d270eb1d7beb92166e4e3676624
James H <james.harrison@mongodb.com>
2025-09-05 13:47:22 +0100

    SERVER-109650: Allow trafficRecordingDirectory to be set at runtime (#40533)
    GitOrigin-RevId: 85ff5881cf9e243e2bfef4a5855ddcef4b44c92a

--- jstests/noPassthrough/traffic_recording/traffic_recording.js
+++ jstests/noPassthrough/traffic_recording/traffic_recording.js
@@ -184,3 +184,56 @@ function runTest(client, restartCommand) {
 
     removeFile(recordingDir);
 }
+
+{
+    const dirA = MongoRunner.toRealDir("$dataDir/traffic_recording/");
+    const dirB = MongoRunner.toRealDir("$dataDir/traffic_recording_different/");
+
+    const startMongod = () => {
+        const mongod = MongoRunner.runMongod({auth: ""});
+        return [mongod, () => MongoRunner.stopMongod(mongod)];
+    };
+    const startSharded = () => {
+        let sharded = new ShardingTest({
+            config: 1,
+            mongos: 1,
+            shards: 0,
+        });
+        return [sharded.s, () => sharded.stop()];
+    };
+
+    const setRecordingDirAndVerify = (db, dirname) => {
+        assert.commandWorked(db.adminCommand({"setParameter": 1, "trafficRecordingDirectory": dirname}));
+
+        assert(!fileExists(dirname + "/subdir"));
+
+        assert.commandWorked(db.runCommand({"startTrafficRecording": 1, "destination": "subdir"}));
+        assert.commandWorked(db.runCommand({"stopTrafficRecording": 1}));
+
+        assert(fileExists(dirname + "/subdir"));
+    };
+
+    for (let start of [startMongod, startSharded]) {
+        // Cleanup in case a previous run failed.
+        removeFile(dirA);
+        removeFile(dirB);
+
+        // Directory set by parameter must exist; subdirectory will be created.
+        mkdir(dirA);
+        mkdir(dirB);
+
+        let [m, stop] = start();
+        let db = m.getDB("admin");
+        db.createUser({user: "admin", pwd: "pwd", roles: ["root"], mechanisms: ["SCRAM-SHA-1"]});
+        db.auth("admin", "pwd");
+
+        setRecordingDirAndVerify(db, dirA);
+        setRecordingDirAndVerify(db, dirB);
+
+        stop();
+
+        // Cleanup files.
+        removeFile(dirA);
+        removeFile(dirB);
+    }
+}
