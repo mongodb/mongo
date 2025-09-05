9a4c605f8e6aa6abb61f2f12394e5f3f101f5b79
Chi-I Huang <8468853+chiihuang@users.noreply.github.com>
2025-09-05 12:26:31 -0700

    SERVER-87485 Creat feature flag featureFlagQueryStatsWriteCommand (#40960)
    GitOrigin-RevId: 9ebaffd5605f2e95d38bcaf0d340cade42ff595a

---
+++ jstests/noPassthrough/query/queryStats/query_stats_write_command_feature_flag.js
@@ -0,0 +1,18 @@
+/**
+ * Test that checks the value of the query stats write command feature flag.
+ */
+import {FeatureFlagUtil} from "jstests/libs/feature_flag_util.js";
+
+// TODO SERVER-109623 this test can be removed when the feature flag is removed.
+const conn = MongoRunner.runMongod({
+    setParameter: {featureFlagQueryStatsWriteCommand: false},
+});
+assert.neq(null, conn, "failed to start mongod");
+const testDB = conn.getDB("test");
+
+assert(
+    FeatureFlagUtil.isPresentAndDisabled(testDB, "QueryStatsWriteCommand"),
+    "featureFlagQueryStatsWriteCommand is undefined or enabled",
+);
+
+MongoRunner.stopMongod(conn);
