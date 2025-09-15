7e3f82a5e0d50f6e8485ac5c7c524b11cbb0dd8c
Erwin Pe <erwin.pe@mongodb.com>
2025-09-15 11:51:12 -0400

    SERVER-110716 Authenticate before issuing getParameter in commands_lib.js (#41215)
    GitOrigin-RevId: 2f35e977d493bff3b042dc993736804a1e0eaed8

--- jstests/auth/lib/commands_lib.js
+++ jstests/auth/lib/commands_lib.js
@@ -2583,6 +2583,7 @@ export const authCommandsLib = {
                     runOnDb: adminDbName,
                     roles: roles_hostManager,
                     privileges: [{resource: {cluster: true}, actions: ["compact"]}],
+                    expectFail: true,
                 },
             ],
         },
@@ -9053,7 +9054,7 @@ export const authCommandsLib = {
      *  An array of strings. Each string in the array reports
      *  a particular test error.
      */
-    runOneTest: function (conn, t, impls, options) {
+    runOneTest: function (conn, t, impls, options, sideChannelConn) {
         options = options || {};
 
         const isMongos = !!options.isMongos || this.isMongos(conn);
@@ -9061,7 +9062,7 @@ export const authCommandsLib = {
             shard0name = options.shard0Name;
         }
 
-        if (t.skipTest && t.skipTest(conn)) {
+        if (t.skipTest && t.skipTest(sideChannelConn)) {
             jsTest.log("Skipping test: " + t.testname);
             return [];
         }
@@ -9139,7 +9140,7 @@ export const authCommandsLib = {
         let failures = [];
 
         for (let i = 0; i < this.tests.length; i++) {
-            const res = this.runOneTest(conn, this.tests[i], impls, options);
+            const res = this.runOneTest(conn, this.tests[i], impls, options, setupConn);
             failures = failures.concat(res);
         }
 
@@ -9159,8 +9160,10 @@ function isStandalone(conn) {
 
 function isFeatureEnabled(conn, ...features) {
     const adminDb = conn.getDB(adminDbName);
+    assert(adminDb.auth("admin", "password"));
     const request = Object.fromEntries(features.map((k) => [k, 1]));
-    const res = adminDb.runCommand({getParameter: 1, ...request});
+    const res = assert.commandWorked(adminDb.runCommand({getParameter: 1, ...request}));
+    adminDb.logout();
     return features.every((key) => res[key]?.value);
 }
 
