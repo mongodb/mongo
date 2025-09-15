a87555389a8e5e39f87db6b9b6a9309c56723378
mongodb-matthew-normyle <120420718+mongodb-matthew-normyle@users.noreply.github.com>
2025-09-14 19:33:56 -0500

    SERVER-110576: Iceberg: get all $iceberg tests running in evergreen (#41259)
    GitOrigin-RevId: b5a5af2a2ed437516be224e8b34d62445ee1c975

--- buildscripts/evergreen_gen_streams_build_and_publish_task.py
+++ buildscripts/evergreen_gen_streams_build_and_publish_task.py
@@ -54,7 +54,7 @@ def main(
     expansions = read_config_file(expansions_file)
     version_id = expansions.get("version_id")
     build_variant_name = expansions.get("build_variant")
-    required_tasks = {"streams", "streams_kafka"}
+    required_tasks = {"streams", "streams_kafka", "streams_aspio"}
     evg_version = evg_api.version_by_id(version_id)
     variant = evg_version.build_by_variant(build_variant_name)
     task_deps = []
