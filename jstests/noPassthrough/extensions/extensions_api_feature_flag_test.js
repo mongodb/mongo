bfc502c4bfc50893ffe9a22b53952ebb1ad6773f
Daniel Segel <71151509+dhsegel@users.noreply.github.com>
2025-09-18 13:26:27 -0400

    SERVER-110446 Add test to verify the behavior of featureFlagExtensionsAPI  (#41525)
    GitOrigin-RevId: 99ea277813008fb301686f263823713fd78948bc

---
+++ jstests/noPassthrough/extensions/extensions_api_feature_flag_test.js
@@ -0,0 +1,43 @@
+/**
+ * Confirm --loadExtensions and $listExtensions fail when 'featureFlagExtensionsAPI' is disabled.
+ *
+ * TODO SERVER-106932: Remove this test when 'featureFlagExtensionsAPI' is removed.
+ */
+import {deleteExtensionConfigs, generateExtensionConfigs} from "jstests/noPassthrough/libs/extension_helpers.js";
+
+const pathToExtensionFoo = MongoRunner.getExtensionPath("libfoo_mongo_extension.so");
+const extensions = generateExtensionConfigs([pathToExtensionFoo]);
+
+try {
+    // The 'loadExtensions' startup parameter should fail when featureFlagExtensionsAPI is off.
+    {
+        try {
+            const conn = MongoRunner.runMongod({
+                setParameter: {featureFlagExtensionsAPI: false},
+                // Use a real extension name to ensure that the feature flag is causing the failure,
+                // not a missing extension.
+                loadExtensions: extensions[0],
+            });
+            // If we've reached this point, startup did not fail as expected.
+            MongoRunner.stopMongod(conn);
+            assert(false, "Expected startup to fail but it succeeded");
+        } catch (e) {
+            assert.eq(e.returnCode, MongoRunner.EXIT_BADOPTIONS, e);
+        }
+    }
+
+    // Confirm that $listExtensions fails when featureFlagExtensionsAPI is off.
+    {
+        const conn = MongoRunner.runMongod({setParameter: {featureFlagExtensionsAPI: false}});
+        assert.neq(null, conn, "failed to start mongod");
+        const adminDB = conn.getDB("admin");
+
+        // $listExtensions should fail when the feature flag is off.
+        const res = adminDB.runCommand({aggregate: 1, pipeline: [{$listExtensions: {}}], cursor: {}});
+        assert.commandFailedWithCode(res, ErrorCodes.QueryFeatureNotAllowed);
+
+        MongoRunner.stopMongod(conn);
+    }
+} finally {
+    deleteExtensionConfigs(extensions);
+}
--- jstests/noPassthrough/extensions/extensions_loading_error_cases.js
+++ jstests/noPassthrough/extensions/extensions_loading_error_cases.js
@@ -10,7 +10,7 @@
  */
 import {isLinux} from "jstests/libs/os_helpers.js";
 import {ShardingTest} from "jstests/libs/shardingtest.js";
-import {generateExtensionConfigs, deleteExtensionConfigs} from "jstests/noPassthrough/libs/extension_helpers.js";
+import {deleteExtensionConfigs, generateExtensionConfigs} from "jstests/noPassthrough/libs/extension_helpers.js";
 
 const pathToExtensionFoo = MongoRunner.getExtensionPath("libfoo_mongo_extension.so");
 const pathToMissingSymbolExtension = MongoRunner.getExtensionPath("libno_symbol_bad_extension.so");
@@ -54,13 +54,9 @@ function runTest({options, validateExitCode = true}) {
 
 try {
     if (isLinux()) {
-        // Empty filename (equivalent to no argument being provided to the parameter, so failure happens
-        // at a higher level when parsing parameters).
+        // Empty filename (equivalent to no argument being provided to the parameter, so failure
+        // happens at a higher level when parsing parameters).
         runTest({options: {loadExtensions: ""}, validateExitCode: false});
-        // Extensions parameter is not allowed when the feature flag is disabled.
-        runTest({
-            options: {loadExtensions: extensionNames[0], setParameter: {featureFlagExtensionsAPI: false}},
-        });
         // Extensions is a scalar, non-string.
         runTest({options: {loadExtensions: 12345}});
         // Path to extension does not exist.
@@ -70,8 +66,8 @@ try {
         // Path to extension that attempts to register duplicate stage descriptors.
         runTest({options: {loadExtensions: extensionNames[2]}});
     } else {
-        // Startup should fail because we are attempting to load an extension on a platform that is not
-        // linux.
+        // Startup should fail because we are attempting to load an extension on a platform that is
+        // not linux.
         runTest({options: {loadExtensions: extensionNames[0]}});
     }
 } finally {
