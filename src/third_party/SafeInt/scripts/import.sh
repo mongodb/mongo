5e0157a6c321a8859a1dd988fb165cebe7475e8e
Alex Li <98560274+lia394126@users.noreply.github.com>
2025-09-22 00:23:23 -0400

    SERVER-106764 Upgrade SafeInt to 3.0.28a (#41536)
    GitOrigin-RevId: 841dde43cde78bfff34efd7dc8e36d59cc092f49

--- src/third_party/SafeInt/scripts/import.sh
+++ src/third_party/SafeInt/scripts/import.sh
@@ -5,11 +5,15 @@ set -o errexit
 # This script downloads and imports SafeInt.
 
 SAFEINT_GIT_URL="https://raw.githubusercontent.com/mongodb-forks/SafeInt"
-VERSION=3.0.26
+# VERSION isn't used to get the SafeInt header here, but marks the release
+# version that the file was retrieved from, and silences the sbom_linter.py which
+# expects a VERSION.
+VERSION="3.0.28a"
+REVISION="3.0.28a-mongo"
 SAFEINT_GIT_DIR="$(git rev-parse --show-toplevel)/src/third_party/SafeInt"
 
 mkdir -p "${SAFEINT_GIT_DIR}"
 
-wget "${SAFEINT_GIT_URL}/${VERSION}/SafeInt.hpp" \
+wget "${SAFEINT_GIT_URL}/${REVISION}/SafeInt.hpp" \
     -O "${SAFEINT_GIT_DIR}/SafeInt.hpp"
 
