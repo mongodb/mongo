c03535aee31a52a4a2308b0a3187a242c4144503
Sean Lyons <sean.lyons@mongodb.com>
2025-09-25 17:55:25 -0400

    SERVER-111502 Update core pattern in unit test wrapper (#41840)
    GitOrigin-RevId: 91d2159137cf200595b1f4deb4ec07cb29e33c19

--- bazel/test_wrapper.sh
+++ bazel/test_wrapper.sh
@@ -5,7 +5,7 @@ ulimit -c unlimited
 $1
 RET=$?
 
-CORE_FILE=$(find -L ./ -name "core")
+CORE_FILE=$(find -L ./ -name "*.core")
 if [ -f "$CORE_FILE" ]; then
     CORE_FILENAME="dump_$(date +%s%N).core"
     mv $CORE_FILE "$TEST_UNDECLARED_OUTPUTS_DIR/$CORE_FILENAME"
