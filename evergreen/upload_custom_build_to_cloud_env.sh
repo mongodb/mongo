bf843f32c152da9d45f6f1f5567536b80abf3a7e
Dylan Richardson <dylan.richardson@mongodb.com>
2025-09-25 10:08:43 -0500

    SERVER-111439: Enable custom build promotion to any env (#41797)
    GitOrigin-RevId: b74e7c452f89f0cc0d20d4a0b614f3a05a7d138c

--- evergreen/upload_custom_build_to_cloud_env.sh
+++ evergreen/upload_custom_build_to_cloud_env.sh
@@ -1,9 +1,13 @@
 # see SERVER-107057 for information about this script
 
-if [ "$PROMOTE_TO_CLOUD_DEV" = '' ]; then
-    echo "Skipping promotion to cloud-dev"
+cloud_env="${CLOUD_ENV:-'cloud-dev'}"
+
+if [ "$PROMOTE_TO_CLOUD_ENV" = '' ]; then
+    echo "Skipping promotion to $cloud_env"
     exit 0
 fi
+
+echo "promoting build to environment '$cloud_env'"
 
 case $PROMOTE_BUILD_VARIANT in
 *"arm64"* | *"aarch64"*)
@@ -13,7 +17,7 @@ case $PROMOTE_BUILD_VARIANT in
     promote_arch="amd64"
     ;;
 *)
-    echo "Could not parse architecture for build variant ${PROMOTE_BUILD_VARIANT} skipping promotion to cloud-dev"
+    echo "Could not parse architecture for build variant ${PROMOTE_BUILD_VARIANT} skipping promotion to cloud environment"
     exit 0
     ;;
 esac
@@ -21,7 +25,7 @@ esac
 case $PROMOTE_BUILD_VARIANT in
 *"amazon"*) ;;
 *)
-    echo "buildvariant ${PROMOTE_BUILD_VARIANT} doesn't appear to be an amazon buildvariant, skipping promotion to cloud-dev"
+    echo "buildvariant ${PROMOTE_BUILD_VARIANT} doesn't appear to be an amazon buildvariant, skipping promotion to cloud environment"
     exit 0
     ;;
 esac
@@ -36,7 +40,7 @@ case $PROMOTE_BUILD_VARIANT in
     promote_min_os_version="2"
     ;;
 *)
-    echo "Could not parse flavor for build variant ${PROMOTE_BUILD_VARIANT} skipping promotion to cloud-dev"
+    echo "Could not parse flavor for build variant ${PROMOTE_BUILD_VARIANT} skipping promotion to cloud environment"
     exit 0
     ;;
 esac
@@ -67,8 +71,8 @@ response=$(curl -sS --fail-with-body \
     --digest \
     --header "Content-Type: application/json" \
     --data @body.json \
-    -u "${CLOUD_DEV_API_PUBLIC_KEY}:${CLOUD_DEV_API_PRIVATE_KEY}" \
-    https://cloud-dev.mongodb.com/api/private/nds/customMongoDbBuild)
+    -u "${CLOUD_ENV_API_PUBLIC_KEY}:${CLOUD_ENV_API_PRIVATE_KEY}" \
+    "https://${cloud_env}.mongodb.com/api/private/nds/customMongoDbBuild")
 
 result=$?
 
