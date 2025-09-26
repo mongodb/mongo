21c0852b51652a38433db4cce2938c7b5cd6fc81
Tommaso Tocci <58224719+toto-dev@users.noreply.github.com>
2025-09-26 12:14:07 +0200

    SERVER-111515 Adapt timeseries_clustered_index_options.js for viewless timeseries (#41853)
    GitOrigin-RevId: de5365f92092a3c79d427f032349dcbfad38c4ea

--- jstests/core/timeseries/ddl/timeseries_clustered_index_options.js
+++ jstests/core/timeseries/ddl/timeseries_clustered_index_options.js
@@ -6,16 +6,21 @@
  * @tags: [
  *   does_not_support_stepdowns,
  *   incompatible_with_preimages_by_default,
- *   does_not_support_viewless_timeseries_yet,
  * ]
  */
+import {
+    getTimeseriesCollForDDLOps,
+    areViewlessTimeseriesEnabled,
+} from "jstests/core/timeseries/libs/viewless_timeseries_util.js";
+
 const testDB = db.getSiblingDB(jsTestName());
 const tsColl = testDB.clustered_index_options;
-const tsCollName = tsColl.getName();
-const bucketsCollName = "system.buckets." + tsCollName;
 
 function testInvalidCreateBucketsCollectionOptions(invalidOptions, expectedErrorCodes) {
-    assert.commandFailedWithCode(testDB.createCollection(bucketsCollName, invalidOptions), expectedErrorCodes);
+    assert.commandFailedWithCode(
+        testDB.createCollection(getTimeseriesCollForDDLOps(testDB, tsColl).getName(), invalidOptions),
+        expectedErrorCodes,
+    );
 }
 
 // Tests time-series creation can be (somewhat)round-tripped to time-series buckets collection
@@ -28,16 +33,29 @@ function roundTripTimeseriesToBucketsCreateOptions() {
     assert.commandWorked(testDB.dropDatabase());
 
     assert.commandWorked(
-        testDB.createCollection(tsCollName, {timeseries: {timeField: "time"}, expireAfterSeconds: 10}),
+        testDB.createCollection(tsColl.getName(), {timeseries: {timeField: "time"}, expireAfterSeconds: 10}),
     );
-    let res = assert.commandWorked(testDB.runCommand({listCollections: 1, filter: {name: bucketsCollName}}));
+    let res = assert.commandWorked(
+        testDB.runCommand({listCollections: 1, filter: {name: getTimeseriesCollForDDLOps(testDB, tsColl).getName()}}),
+    );
     const options = res.cursor.firstBatch[0].options;
-    // Ensure the 'clusteredIndex' legacy format is used {'clusteredIndex': true>}.
-    assert.eq(tojson(options.clusteredIndex), tojson(true));
+
+    if (areViewlessTimeseriesEnabled(testDB)) {
+        // Ensure clusteredIndex options is never returned on the listCollection logical format output
+        assert(
+            !options.hasOwnProperty("clusteredIndex"),
+            `Found clusteredIndex property in logical collection metadata: ${options}`,
+        );
+    } else {
+        // Ensure the 'clusteredIndex' legacy format is used {'clusteredIndex': true>}.
+        assert.eq(tojson(options.clusteredIndex), tojson(true));
+    }
     assert(tsColl.drop());
 
-    assert.commandWorked(testDB.createCollection(bucketsCollName, options));
-    res = assert.commandWorked(testDB.runCommand({listCollections: 1, filter: {name: bucketsCollName}}));
+    assert.commandWorked(testDB.createCollection(getTimeseriesCollForDDLOps(testDB, tsColl).getName(), options));
+    res = assert.commandWorked(
+        testDB.runCommand({listCollections: 1, filter: {name: getTimeseriesCollForDDLOps(testDB, tsColl).getName()}}),
+    );
     assert.eq(options, res.cursor.firstBatch[0].options);
     assert.commandWorked(testDB.dropDatabase());
     return options;
@@ -53,7 +71,11 @@ testInvalidCreateBucketsCollectionOptions({...options, clusteredIndex: false}, E
 
 // Test the time-series buckets collection cannot be created with the generalized 'clusteredIndex'
 // format used to create standard clustered collections.
-testInvalidCreateBucketsCollectionOptions({...options, clusteredIndex: {key: {_id: 1}, unique: true}}, 5979703);
+// TODO SERVER-101614 remove custom error code from expected error list
+testInvalidCreateBucketsCollectionOptions({...options, clusteredIndex: {key: {_id: 1}, unique: true}}, [
+    5979703,
+    ErrorCodes.InvalidOptions,
+]);
 
 // Validate additional 'idIndex' field fails to create the buckets collection when added to
 // otherwise valid create options.
