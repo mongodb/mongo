9893093bfaa509af5d82ce30fd506357bbc9ea2d
Ruoxin Xu <ruoxin.xu@mongodb.com>
2025-09-26 12:50:50 +0100

    SERVER-105625 Add initial replay validation (#40910)
    GitOrigin-RevId: b9140d578abfc72ea0464062ae02d0f542a9d8d6

--- jstests/noPassthrough/traffic_recording/traffic_replaying.js
+++ jstests/noPassthrough/traffic_recording/traffic_replaying.js
@@ -1,20 +1,11 @@
 // tests for the traffic recording and replaying commands.
 // @tags: [requires_auth]
-function createDirectories(baseDir, customSubDir) {
-    const pathSep = _isWindows() ? "\\" : "/";
-    const recordingDirGlobal = MongoRunner.toRealDir("$dataDir" + pathSep + baseDir);
-    const recordingDir = MongoRunner.toRealDir(recordingDirGlobal + pathSep + customSubDir + pathSep);
-    jsTest.log("Creating a new directory: " + recordingDirGlobal);
-    jsTest.log("Creating a new directory: " + recordingDir);
-    assert(mkdir(recordingDirGlobal));
-    assert(mkdir(recordingDir));
-    return {recordingDirGlobal, recordingDir};
-}
 
-function cleanUpDirectory(directoryPath) {
-    jsTest.log(`Cleaning up directory: ${directoryPath}`);
-    removeFile(directoryPath); // Deletes the directory and its contents
-}
+import {
+    createDirectories,
+    cleanUpDirectory,
+    recordOperations,
+} from "jstests/noPassthrough/traffic_recording/traffic_replaying_lib.js";
 
 function parseRecordedTraffic(recordingFilePath) {
     const recordedTraffic = convertTrafficRecordingToBSON(recordingFilePath);
@@ -26,41 +17,27 @@ function parseRecordedTraffic(recordingFilePath) {
     return {opTypes, recordedTraffic};
 }
 
-function recordOperations(recordingDirGlobal, customRecordingDir, workflowCallback) {
-    const opts = {auth: "", setParameter: "trafficRecordingDirectory=" + recordingDirGlobal};
-    const mongodInstance = MongoRunner.runMongod(opts);
-
-    const adminDB = mongodInstance.getDB("admin");
-    const testDB = mongodInstance.getDB("test");
-    const coll = testDB.getCollection("foo");
-
-    adminDB.createUser({user: "admin", pwd: "pass", roles: jsTest.adminUserRoles});
-    adminDB.auth("admin", "pass");
-
-    assert.commandWorked(adminDB.runCommand({startTrafficRecording: 1, destination: customRecordingDir}));
-
-    const dbContext = {adminDB, testDB, coll, serverURI: `mongodb://${mongodInstance.host}`};
-
-    workflowCallback(dbContext);
-
-    const serverStatus = assert.commandWorked(testDB.runCommand({serverStatus: 1}));
-    const recordingFilePath = serverStatus.trafficRecording.recordingDir;
-
-    assert.commandWorked(adminDB.runCommand({stopTrafficRecording: 1}));
+function recordAndParseOperations(recordingDirGlobal, customRecordingDir, workflowCallback) {
+    const {mongodInstance, coll, recordingFilePath} = recordOperations(
+        recordingDirGlobal,
+        customRecordingDir,
+        workflowCallback,
+    );
+    const serverURI = `mongodb://${mongodInstance.host}`;
 
     MongoRunner.stopMongod(mongodInstance, null, {user: "admin", pwd: "pass"});
 
     return {
         ...parseRecordedTraffic(recordingFilePath),
         recordingFilePath,
-        serverURI: `mongodb://${mongodInstance.host}`,
+        serverURI,
         recordingDirGlobal,
     };
 }
 
 function runInstances(baseDir, customSubDir, workflowCallback) {
     const {recordingDirGlobal, recordingDir} = createDirectories(baseDir, customSubDir);
-    return recordOperations(recordingDirGlobal, customSubDir, workflowCallback);
+    return recordAndParseOperations(recordingDirGlobal, customSubDir, workflowCallback);
 }
 
 const defaultOperationsLambda = (dbContext) => {
