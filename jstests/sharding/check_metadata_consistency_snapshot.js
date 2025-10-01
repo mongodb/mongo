8cbf8708bc6f430d4a38ea5897645c3a5e67250a
Daniel Gómez Ferro <daniel.gomezferro@mongodb.com>
2025-10-01 23:35:49 +0200

    SERVER-106194 Set ReadConcern in opCtx for DBDirectClient (#37534) (#37624)
    GitOrigin-RevId: fbcf3216b4b8409eef10cbe5b3082989def8b7ef

---
+++ jstests/sharding/check_metadata_consistency_snapshot.js
@@ -0,0 +1,30 @@
+/*
+ * Test to validate checkMetadataConsistency uses snapshot read concern.
+ */
+const st = new ShardingTest({shards: 1, name: jsTestName(), other: {enableBalancer: false}});
+
+const dbName = jsTestName();
+const db = st.s.getDB(dbName);
+const collName = "check_metadata_consistency";
+const coll = db[collName];
+
+assert.commandWorked(db.adminCommand({shardCollection: coll.getFullName(), key: {account: 1}}));
+
+jsTest.log("Log all queries");
+assert.commandWorked(st.configRS.getPrimary().getDB("config").setProfilingLevel(0, {slowms: 0}));
+
+jsTest.log("Run checkMetadataConsistency");
+assert.commandWorked(db.adminCommand({checkMetadataConsistency: 1}));
+
+assert.commandWorked(st.configRS.getPrimary().getDB("config").setProfilingLevel(0, {slowms: 100}));
+
+jsTest.log("Checking logs for snapshot read concern");
+
+// Make sure we've used snapshot read concern for the query on config.chunks
+// `"readConcern":{"level":"snapshot"` should appear twice, once from the passed in command and once
+// from the actual readConcern used.
+assert(checkLog.checkContainsOnce(st.configRS.getPrimary(),
+                                  /"config.chunks"(.*"readConcern":{"level":"snapshot"){2}/),
+       "The query on config.chunks should use snapshot read concern");
+
+st.stop();
