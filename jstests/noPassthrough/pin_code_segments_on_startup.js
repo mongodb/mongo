18f6a2a2886804a6629b5a5231ae43f9d621b6b5
Saman Memaripour <amirsaman.memaripour@mongodb.com>
2025-10-15 11:07:03 -0400

    SERVER-111427 SERVER-111460 Load SASL before locking code pages (#41776) (#42333)
    GitOrigin-RevId: d2fe364d5125497c682c2ffd3254cb1df815ead2

--- jstests/noPassthrough/pin_code_segments_on_startup.js
+++ jstests/noPassthrough/pin_code_segments_on_startup.js
@@ -1,15 +1,24 @@
 /**
- * Tests that a standalone mongod is able to pin code segments on startup when
+ * Tests that a standalone mongod and a mongos are able to pin code segments on startup when
  * 'lockCodeSegmentsInMemory=true'.
  * TODO (SERVER-75632): Re-enable this test on amazon linux once ulimits are configured.
  * @tags: [requires_increased_memlock_limits, incompatible_with_macos,
  * incompatible_with_windows_tls, incompatible_with_amazon_linux]
  */
 
-(function() {
-"use strict";
+const connD = MongoRunner.runMongod({setParameter: {lockCodeSegmentsInMemory: true}});
+assert.neq(null, connD, "mongod was unable to start up");
+assert.eq(0, MongoRunner.stopMongod(connD));
 
-const conn = MongoRunner.runMongod({setParameter: {lockCodeSegmentsInMemory: true}});
-assert.neq(null, conn, "mongod was unable to start up");
-assert.eq(0, MongoRunner.stopMongod(conn));
-}());
+let configRS = new ReplSetTest({name: "configRS", nodes: 1});
+configRS.startSet({configsvr: ""});
+let replConfig = configRS.getReplSetConfig();
+replConfig.configsvr = true;
+configRS.initiate(replConfig);
+
+const connS = MongoRunner.runMongos(
+    {configdb: configRS.getURL(), setParameter: {lockCodeSegmentsInMemory: true}});
+assert.neq(null, connS, "mongos was unable to start up");
+assert.eq(0, MongoRunner.stopMongos(connS));
+
+configRS.stopSet();
