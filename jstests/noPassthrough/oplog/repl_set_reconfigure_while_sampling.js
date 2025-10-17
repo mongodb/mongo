58dca72b1123b997db6d4158f981b0b2f155c6d8
Nic <nic.hollingum@mongodb.com>
2025-10-17 12:00:30 +1100

    SERVER-112123: Test that async oplog sampling doesn't block replication configuration. (#42599)
    GitOrigin-RevId: fc4b21358072074b7532d826ada3e5558fde1507

---
+++ jstests/noPassthrough/oplog/repl_set_reconfigure_while_sampling.js
@@ -0,0 +1,57 @@
+/**
+ * Checks that, when async oplog sampling enabled, we can reconfigure/initiate the replica set.
+ *
+ * @tags: [requires_replication, requires_persistence]
+ */
+import {ReplSetTest} from "jstests/libs/replsettest.js";
+
+function samplingIsIncomplete(primary) {
+    const status = primary.getDB("local").serverStatus();
+    assert.commandWorked(status);
+    jsTest.log.info(status.oplogTruncation);
+    return (!status.oplogTruncation.hasOwnProperty("processingMethod") ||
+            status.oplogTruncation.processingMethod == "in progress");
+}
+
+// Initialize a single-node replica set with a slow oplog that doesn't truncate.
+const rst = new ReplSetTest({
+    nodes: 1,
+    nodeOptions: {
+        setParameter: {
+            "oplogSamplingAsyncEnabled": true,
+            useSlowCollectionTruncateMarkerScanning: true,
+            "failpoint.hangOplogCapMaintainerThread": tojson({mode: "alwaysOn"}),
+        },
+    },
+});
+rst.startSet();
+rst.initiate();
+
+// Insert initial documents.
+jsTest.log.info("Inserting initial set of documents into the collection.");
+for (let i = 0; i < 100; i++) {
+    rst.getPrimary().getDB("test").getCollection("markers").insert({a: i});
+}
+
+// Stop and restart the replica set.
+rst.stopSet(null, true);
+rst.startSet(null, true);
+jsTest.log.info("Replica set restarted.");
+
+// We have 100 oplog entries to sample, 1 second each.
+const primary = rst.getPrimary();
+assert(samplingIsIncomplete(primary));
+
+// Add a member and reconfigure.
+const newNode = rst.add({
+    rsConfig: {priority: 0, votes: 0},
+    setParameter: {
+        "oplogSamplingAsyncEnabled": true,
+    },
+});
+rst.reInitiate();
+
+// We were able to add a new member and we're still sampling.
+assert(samplingIsIncomplete(primary));
+
+rst.stopSet();
