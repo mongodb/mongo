427194347f5f8cd628ae2d775c3032ea0595cd26
Max Hirschhorn <max.hirschhorn@mongodb.com>
2025-10-21 11:09:49 -0400

    SERVER-110899 New "readBackupFile" auth privilege for $_backupFile. (#42675)
    GitOrigin-RevId: 61239dacc4e623ee66069fc2095f2afd92192643

--- jstests/auth/lib/commands_lib.js
+++ jstests/auth/lib/commands_lib.js
@@ -7071,10 +7071,8 @@ export const authCommandsLib = {
                   !getBuildInfo().modules.includes("enterprise"),
           testcases: [{
               runOnDb: adminDbName,
-              roles: {__system: 1},
-              privileges: [
-                  {resource: {cluster: true}, actions: ["internal"]},
-              ],
+              roles: {backup: 1, root: 1, __system: 1},
+              privileges: [{resource: {cluster: true}, actions: ["readBackupFile"]}],
               expectFail: true
           }],
         },
