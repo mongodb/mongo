6466b2fb4855ed62ba8a10b2ab1a8ac4217bef3c
Varun Ravichandran <varun.ravichandran@mongodb.com>
2025-10-22 10:39:55 -0700

    SERVER-106946 Reject x.509 certificates with mismatched client|serverAuth EKU (#42974)
    GitOrigin-RevId: f1aa7c70c6d003e2496ac51f7eba4904b3bb4159

--- jstests/ssl_linear/ssl_cert_selector.js
+++ jstests/ssl_linear/ssl_cert_selector.js
@@ -14,14 +14,21 @@ import {
 
 const clientThumbprint = cat('jstests/libs/trusted-client.pem.digest.sha1');
 const serverThumbprint = cat('jstests/libs/trusted-server.pem.digest.sha1');
+const clusterServerThumbprint = cat('jstests/libs/trusted-cluster-server.pem.digest.sha1');
 const CLIENT = 'CN=Trusted Kernel Test Client,OU=Kernel,O=MongoDB,L=New York City,ST=New York,C=US';
 const SERVER = 'CN=Trusted Kernel Test Server,OU=Kernel,O=MongoDB,L=New York City,ST=New York,C=US';
+const CLUSTER_SERVER =
+    'CN=Trusted Kernel Test Cluster Server,OU=Kernel,O=MongoDB,L=New York City,ST=New York,C=US';
 
 const testCases = [
+    // Configure server with only a certificateSelector - we expect this to be used instead of
+    // the --tlsCertificateKeyFile by the server for both ingress (server) and egress (client)
+    // traffic for both cluster and other communication
+    //
     {
-        selector: `thumbprint=${serverThumbprint}`,
-        expectIngressKeyUsed: SERVER,
-        expectEgressKeyUsed: SERVER
+        selector: `thumbprint=${clusterServerThumbprint}`,
+        expectIngressKeyUsed: CLUSTER_SERVER,
+        expectEgressKeyUsed: CLUSTER_SERVER,
     },
     {
         selector: `thumbprint=${serverThumbprint}`,
@@ -138,6 +145,7 @@ requireSSLProvider('windows', function() {
         };
         assert.eq(0, importPfx("jstests\\libs\\trusted-client.pfx"));
         assert.eq(0, importPfx("jstests\\libs\\trusted-server.pfx"));
+        assert.eq(0, importPfx("jstests\\libs\\trusted-cluster-server.pfx"));
     }
 
     try {
