7274da23036a1cc73f7adec2b89422e9d89fde4e
Binh Vo <146007384+binhvomongodb@users.noreply.github.com>
2025-10-23 14:37:07 -0400

    SERVER-111007 Validation distinguishes between root causes when finding mixed schema data (#42940)
    GitOrigin-RevId: fca08f9a742a471662bffeb3ebb19364e1c50dcd

--- src/mongo/db/validate/validate_adaptor.cpp
+++ src/mongo/db/validate/validate_adaptor.cpp
@@ -1150,7 +1150,7 @@ void ValidateAdaptor::traverseRecordStore(OperationContext* opCtx,
                                           {logv2::LogTruncation::Disabled},
                                           kMalformedMinMaxTimeseriesBucket,
                                           logAttrs(coll->ns()),
-                                          "bucketId"_attr = record->id,
+                                          "recordId"_attr = record->id,
                                           "error"_attr =
                                               containsMixedSchemaDataResponse.getStatus());
                 } else if (containsMixedSchemaDataResponse.isOK() &&
@@ -1163,14 +1163,20 @@ void ValidateAdaptor::traverseRecordStore(OperationContext* opCtx,
                                               {logv2::LogTruncation::Disabled},
                                               kExpectedMixedSchemaTimeseriesWarning,
                                               logAttrs(coll->ns()),
-                                              "bucketId"_attr = record->id);
+                                              "recordId"_attr = record->id);
                     } else if (!mixedSchemaAllowed &&
                                results->addError(kUnexpectedMixedSchemaTimeseriesError)) {
+                        const auto& controlField =
+                            recordBson.getField(timeseries::kBucketControlFieldName).Obj();
+                        int count =
+                            controlField.getIntField(timeseries::kBucketControlCountFieldName);
                         LOGV2_WARNING_OPTIONS(8469902,
                                               {logv2::LogTruncation::Disabled},
                                               kUnexpectedMixedSchemaTimeseriesError,
                                               logAttrs(coll->ns()),
-                                              "bucketId"_attr = record->id);
+                                              "recordId"_attr = record->id,
+                                              "objSize"_attr = recordBson.objsize(),
+                                              "measurementCount"_attr = count);
                     }
                 }
             }
