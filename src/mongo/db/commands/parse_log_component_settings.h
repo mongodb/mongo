8e754f06235a6b59806ebe2ac44cc4fe757fd0c7
Saman Memaripour <amirsaman.memaripour@mongodb.com>
2025-10-24 09:43:48 -0400

    SERVER-112850 Mark module boundaries for observability and networking commands (#43043)
    GitOrigin-RevId: 58a277aa81396e052d217c3619a4efcbebf79566

--- src/mongo/db/commands/parse_log_component_settings.h
+++ src/mongo/db/commands/parse_log_component_settings.h
@@ -30,6 +30,7 @@
 #pragma once
 
 #include "mongo/logv2/log_component.h"
+#include "mongo/util/modules.h"
 
 #include <vector>
 
@@ -42,7 +43,7 @@ class StatusWith;
 /**
  * One parsed LogComponent and desired log level
  */
-struct LogComponentSetting {
+struct MONGO_MOD_PUBLIC LogComponentSetting {
     LogComponentSetting(logv2::LogComponent c, int lvl) : component(c), level(lvl) {}
 
     logv2::LogComponent component;
@@ -55,6 +56,7 @@ struct LogComponentSetting {
  * Returns an error status describing why parsing failed, or a vector of LogComponentSettings,
  * each describing how to change a particular log components verbosity level.
  */
-StatusWith<std::vector<LogComponentSetting>> parseLogComponentSettings(const BSONObj& settings);
+MONGO_MOD_PUBLIC StatusWith<std::vector<LogComponentSetting>> parseLogComponentSettings(
+    const BSONObj& settings);
 
 }  // namespace mongo
