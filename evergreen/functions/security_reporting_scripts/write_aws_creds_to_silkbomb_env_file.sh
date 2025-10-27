f6f7d3cf8f037a28b864fdfb329ffe98aaf48077
ekovalets <eduard.kovalets@mongodb.com>
2025-10-27 12:18:32 -0700

    SERVER-112109: Evergreen Task and scripts to run Server SBOM upload to Google Drive at release (#42893)
    GitOrigin-RevId: 808122c5304ee055149964ad08ab64b79c4e2f1f

---
+++ evergreen/functions/security_reporting_scripts/write_aws_creds_to_silkbomb_env_file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# This is an auxiliary script that writes AWS credentials to an env file.
+# Being used by augment_sbom.sh script and SBOM upload tasks to prepare the env file for SilkBomb.
+#
+# Usage:
+#   write_aws_creds_to_silkbomb_env_file.sh
+#
+# Required system environment variables:
+#   AWS_ACCESS_KEY_ID
+#   AWS_SECRET_ACCESS_KEY
+#   AWS_SESSION_TOKEN
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+. "$DIR/../../prelude.sh"
+
+cat <<EOF >"${workdir}/silkbomb.env"
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+AWS_SESSION_TOKEN=${AWS_SESSION_TOKEN}
+EOF
