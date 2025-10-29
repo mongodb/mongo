4b8db8114506107661cea0522623ad9a8a0f0c4d
Evan Bergeron <evanbergeron@users.noreply.github.com>
2025-10-29 15:49:24 -0400

    SERVER-112334 Add property-based test for extract_field_paths (#43112)
    GitOrigin-RevId: 46499e5e5233ee5f53a8ea991aa8edff2555dd02

--- jstests/core/query/sbe/sbe_extract_field_paths.js
+++ jstests/core/query/sbe/sbe_extract_field_paths.js
@@ -14,16 +14,171 @@
  *    does_not_support_transactions,
  *    not_allowed_with_signed_security_token,
  *    requires_fcv_83,
+ *    query_intensive_pbt,
  * ]
  */
 import {getEngine} from "jstests/libs/query/analyze_plan.js";
 import {getSbePlanStages} from "jstests/libs/query/sbe_explain_helpers.js";
 import {resultsEq} from "jstests/aggregation/extras/utils.js";
+import {fc} from "jstests/third_party/fast_check/fc-3.1.0.js";
 
-function runTestWithParameter(documents, pipeline, useExtract, numExpectedExtractStages) {
-    db.c.deleteMany({});
-    db.c.insertMany(documents);
+function getNestedDocumentModel() {
+    const scalarArb = fc.oneof(
+        fc.integer({min: -100, max: 100}),
+        fc.boolean(),
+        fc.string({maxLength: 10}),
+        fc.constant(null),
+    );
+    const nestedObjectArb = fc.letrec((tie) => ({
+        value: fc.oneof(scalarArb, fc.array(tie("value"), {maxLength: 3}), tie("object")),
+        object: fc.record({
+            a: tie("value"),
+            b: tie("value"),
+            c: tie("value"),
+            d: tie("value"),
+        }),
+    })).object;
+    const arrayOfNestedObjectArb = fc.array(nestedObjectArb, {maxLength: 4});
+    return fc.record({
+        a: fc.oneof(scalarArb, nestedObjectArb, arrayOfNestedObjectArb),
+        b: fc.oneof(scalarArb, nestedObjectArb, arrayOfNestedObjectArb),
+        c: fc.oneof(scalarArb, nestedObjectArb, arrayOfNestedObjectArb),
+        d: fc.oneof(scalarArb, nestedObjectArb, arrayOfNestedObjectArb),
+    });
+}
 
+function getSampleNestedDocumentModel() {
+    const sampleDocuments = [
+        {a: "foo", b: {c: ["value"]}},
+        {a: 1, b: 3},
+        {a: 1, c: 2},
+        {a: 1},
+        {a: 2},
+        {a: [1, 2], b: [{c: [3, 4]}]},
+        {a: [1, 2]},
+        {a: [1, [1]]},
+        {a: [1, []]},
+        {a: [1, [{a: 1}]]},
+        {a: [1, {a: 1}, []]},
+        {a: [1, {a: 1}]},
+        {a: [1, {a: [1]}, [{a: 1}]]},
+        {a: [1, {a: [1]}]},
+        {a: [1, {a: []}, [1]]},
+        {a: [[1], {a: 1}]},
+        {a: [[[42]]]},
+        {a: [[], {a: 1}]},
+        {a: [[{a: 1}], [{a: 1}]]},
+        {a: [[{a: 1}], [{b: 1}]]},
+        {a: [[{a: 1}]]},
+        {a: [[{a: {a: 1}}], [{a: {a: 1}}]]},
+        {a: [[{a: {a: 1}}], [{a: {b: 1}}]]},
+        {a: [[{a: {a: 1}}], [{b: {a: 1}}]]},
+        {a: [[{a: {a: 1}}]]},
+        {a: [[{b: 2}]]},
+        {a: [[{b: 3}], {b: 4}]},
+        {a: []},
+        {a: [{a: 1, b: 1, c: 1}]},
+        {a: [{a: 1, b: [], c: 1}]},
+        {a: [{a: 1, b: []}]},
+        {a: [{a: 1}, [1]]},
+        {a: [{a: 1}, []]},
+        {a: [{a: 1}, [{a: 1}]]},
+        {a: [{a: 1}, {a: 1}]},
+        {a: [{a: 1}, {b: 1}, []]},
+        {a: [{a: [1, 1]}, {a: [1, 1]}]},
+        {a: [{a: [1, 1]}]},
+        {a: [{a: [1]}, [{a: 1}]]},
+        {a: [{a: [1]}, {a: [1]}]},
+        {a: [{a: [1]}, {b: [1]}, [1]]},
+        {a: [{a: [1]}, {b: [1]}]},
+        {a: [{a: [1]}]},
+        {a: [{a: [], b: 1, c: 1}]},
+        {a: [{a: [], b: 1}]},
+        {a: [{a: [], b: [], c: []}]},
+        {a: [{a: [], b: []}]},
+        {a: [{a: []}, [1]]},
+        {a: [{a: []}, {a: [1]}]},
+        {a: [{a: []}, {a: []}]},
+        {a: [{a: []}, {b: []}, []]},
+        {a: [{a: []}, {b: []}]},
+        {a: [{a: []}]},
+        {a: [{a: {a: 1}}, {a: {a: 1}}]},
+        {a: [{a: {a: 1}}, {a: {b: 2}}]},
+        {a: [{a: {a: 1}}]},
+        {a: [{a: {a: [1]}}]},
+        {a: [{a: {a: {a: []}}}]},
+        {a: [{a: {b: 1}}]},
+        {
+            a: [
+                {b: 1, c: 2},
+                {b: 3, c: 4},
+            ],
+        },
+        {a: [{b: 1}, {b: 2}]},
+        {a: [{b: 1}]},
+        {a: [{b: 2}]},
+        {
+            a: [{b: [{c: 1}, {c: 2}, {d: 3}]}, {b: {d: 4, c: 5}}, {b: [{d: 6}, {c: 7}, {d: 8}]}],
+        },
+        {a: [{b: [{c: 1}, {c: 2}]}]},
+        {a: [{b: [{c: 1}]}, {d: [{e: 2}]}]},
+        {a: [{b: [{c: 3}]}, {b: [{c: 4}]}]},
+        {a: {a: {a: [{a: 1}, []], b: [1, []], c: [[1], {a: 1}]}}},
+        {a: {a: {a: [{a: 1}], b: [{a: 1}], c: [{a: 1}]}}},
+        {a: {b: 1, c: 3}, d: 5},
+        {a: {b: 1}},
+        {a: {b: 2}},
+        {a: {b: ["foo", 42]}},
+        {b: 2, a: 3},
+        {b: 4, a: 2},
+        {d: 6, a: {c: 4, b: 2}},
+    ];
+    return fc.constantFrom(...sampleDocuments);
+}
+
+function getDocumentModel() {
+    return fc.oneof(getNestedDocumentModel(), getSampleNestedDocumentModel());
+}
+
+function generateFieldPath() {
+    const fields = ["a", "b", "c", "d"];
+    const field = fc.sample(fc.constantFrom(...fields), 1)[0];
+    const depth = fc.sample(fc.integer({min: 1, max: 4}), 1)[0];
+    let path = `$${field}`;
+    for (let i = 1; i < depth; i++) {
+        const nextField = fc.sample(fc.constantFrom(...fields), 1)[0];
+        path += `.${nextField}`;
+    }
+    return path;
+}
+
+function getFieldPathModel() {
+    const sampleFieldPaths = [
+        "$a",
+        "$a.a",
+        "$a.a.a",
+        "$a.a.a.a",
+        "$a.a.b",
+        "$a.a.c",
+        "$a.b",
+        "$a.b.a",
+        "$a.b.c",
+        "$a.b.d",
+        "$a.c",
+        "$a.d.e",
+        "$b",
+        "$b.c",
+        "$c",
+        "$d",
+        generateFieldPath(),
+        generateFieldPath(),
+        generateFieldPath(),
+    ];
+    jsTest.log({"sampleFieldPaths": sampleFieldPaths});
+    return fc.constantFrom(...sampleFieldPaths);
+}
+
+function runTestWithParameter(pipeline, useExtract, numExpectedExtractStages) {
     assert.commandWorked(
         db.adminCommand({
             setParameter: 1,
@@ -49,10 +204,10 @@ function runTestWithParameter(documents, pipeline, useExtract, numExpectedExtrac
     return results;
 }
 
-function run(documents, pipeline, numExpectedExtractStages) {
+function run(pipeline, numExpectedExtractStages) {
     jsTest.log({"Pipeline": pipeline});
-    const resultsWithExtract = runTestWithParameter(documents, pipeline, true, numExpectedExtractStages);
-    const resultsWithoutExtract = runTestWithParameter(documents, pipeline, false, numExpectedExtractStages);
+    const resultsWithExtract = runTestWithParameter(pipeline, true, numExpectedExtractStages);
+    const resultsWithoutExtract = runTestWithParameter(pipeline, false, numExpectedExtractStages);
     assert(resultsEq(resultsWithExtract, resultsWithoutExtract));
 }
 
@@ -62,112 +217,24 @@ const originalFeatureFlagExtract = db.adminCommand({getParameter: 1, featureFlag
 try {
     assert.commandWorked(db.adminCommand({setParameter: 1, internalQueryFrameworkControl: "trySbeEngine"}));
 
+    const n = 10;
     const documents = [
-        {_id: 0, a: "foo", b: {c: ["value"]}},
-        {_id: 1, a: 1, b: 3},
-        {_id: 2, a: 1, c: 2},
-        {_id: 3, a: 1},
-        {_id: 4, a: 2},
-        {_id: 5, a: [1, 2], b: [{c: [3, 4]}]},
-        {_id: 6, a: [1, 2]},
-        {_id: 7, a: [1, [1]]},
-        {_id: 8, a: [1, []]},
-        {_id: 9, a: [1, [{a: 1}]]},
-        {_id: 10, a: [1, {a: 1}, []]},
-        {_id: 11, a: [1, {a: 1}]},
-        {_id: 12, a: [1, {a: [1]}, [{a: 1}]]},
-        {_id: 13, a: [1, {a: [1]}]},
-        {_id: 14, a: [1, {a: []}, [1]]},
-        {_id: 15, a: [[1], {a: 1}]},
-        {_id: 16, a: [[[42]]]},
-        {_id: 17, a: [[], {a: 1}]},
-        {_id: 18, a: [[{a: 1}], [{a: 1}]]},
-        {_id: 19, a: [[{a: 1}], [{b: 1}]]},
-        {_id: 20, a: [[{a: 1}]]},
-        {_id: 21, a: [[{a: {a: 1}}], [{a: {a: 1}}]]},
-        {_id: 22, a: [[{a: {a: 1}}], [{a: {b: 1}}]]},
-        {_id: 23, a: [[{a: {a: 1}}], [{b: {a: 1}}]]},
-        {_id: 24, a: [[{a: {a: 1}}]]},
-        {_id: 25, a: [[{b: 2}]]},
-        {_id: 26, a: [[{b: 3}], {b: 4}]},
-        {_id: 27, a: []},
-        {_id: 28, a: [{a: 1, b: 1, c: 1}]},
-        {_id: 29, a: [{a: 1, b: [], c: 1}]},
-        {_id: 30, a: [{a: 1, b: []}]},
-        {_id: 31, a: [{a: 1}, [1]]},
-        {_id: 32, a: [{a: 1}, []]},
-        {_id: 33, a: [{a: 1}, [{a: 1}]]},
-        {_id: 34, a: [{a: 1}, {a: 1}]},
-        {_id: 35, a: [{a: 1}, {b: 1}, []]},
-        {_id: 36, a: [{a: [1, 1]}, {a: [1, 1]}]},
-        {_id: 37, a: [{a: [1, 1]}]},
-        {_id: 38, a: [{a: [1]}, [{a: 1}]]},
-        {_id: 39, a: [{a: [1]}, {a: [1]}]},
-        {_id: 40, a: [{a: [1]}, {b: [1]}, [1]]},
-        {_id: 41, a: [{a: [1]}, {b: [1]}]},
-        {_id: 42, a: [{a: [1]}]},
-        {_id: 43, a: [{a: [], b: 1, c: 1}]},
-        {_id: 44, a: [{a: [], b: 1}]},
-        {_id: 45, a: [{a: [], b: [], c: []}]},
-        {_id: 46, a: [{a: [], b: []}]},
-        {_id: 47, a: [{a: []}, [1]]},
-        {_id: 48, a: [{a: []}, {a: [1]}]},
-        {_id: 49, a: [{a: []}, {a: []}]},
-        {_id: 50, a: [{a: []}, {b: []}, []]},
-        {_id: 51, a: [{a: []}, {b: []}]},
-        {_id: 52, a: [{a: []}]},
-        {_id: 53, a: [{a: {a: 1}}, {a: {a: 1}}]},
-        {_id: 54, a: [{a: {a: 1}}, {a: {b: 2}}]},
-        {_id: 55, a: [{a: {a: 1}}]},
-        {_id: 56, a: [{a: {a: [1]}}]},
-        {_id: 57, a: [{a: {a: {a: []}}}]},
-        {_id: 58, a: [{a: {b: 1}}]},
-        {
-            _id: 59,
-            a: [
-                {b: 1, c: 2},
-                {b: 3, c: 4},
-            ],
-        },
-        {_id: 60, a: [{b: 1}, {b: 2}]},
-        {_id: 61, a: [{b: 1}]},
-        {_id: 62, a: [{b: 2}]},
-        {
-            _id: 63,
-            a: [{b: [{c: 1}, {c: 2}, {d: 3}]}, {b: {d: 4, c: 5}}, {b: [{d: 6}, {c: 7}, {d: 8}]}],
-        },
-        {_id: 64, a: [{b: [{c: 1}, {c: 2}]}]},
-        {_id: 65, a: [{b: [{c: 1}]}, {d: [{e: 2}]}]},
-        {_id: 66, a: [{b: [{c: 3}]}, {b: [{c: 4}]}]},
-        {_id: 67, a: {a: {a: [{a: 1}, []], b: [1, []], c: [[1], {a: 1}]}}},
-        {_id: 68, a: {a: {a: [{a: 1}], b: [{a: 1}], c: [{a: 1}]}}},
-        {_id: 69, a: {b: 1, c: 3}, d: 5},
-        {_id: 70, a: {b: 1}},
-        {_id: 71, a: {b: 2}},
-        {_id: 72, a: {b: ["foo", 42]}},
-        {_id: 73, b: 2, a: 3},
-        {_id: 74, b: 4, a: 2},
-        {_id: 75, d: 6, a: {c: 4, b: 2}},
+        ...(() => {
+            const nestedModel = getDocumentModel();
+            const generatedDocs = fc.sample(nestedModel, n).map((doc, index) => Object.assign(doc, {_id: index}));
+            // Quadratic.
+            return generatedDocs.filter(function (item, pos) {
+                return generatedDocs.indexOf(item) == pos;
+            });
+        })(),
     ];
+    jsTest.log({"documents": documents});
 
-    const fieldPaths = [
-        "$a",
-        "$a.a",
-        "$a.a.a",
-        "$a.a.a.a",
-        "$a.a.b",
-        "$a.a.c",
-        "$a.b",
-        "$a.b.a",
-        "$a.b.c",
-        "$a.b.d",
-        "$a.c",
-        "$a.d.e",
-        "$b",
-        "$b.c",
-        "$c",
-        "$d",
-    ];
+    db.c.drop();
+    db.c.insertMany(documents);
+
+    const fieldPaths = fc.sample(getFieldPathModel(), n);
+    jsTest.log({"fieldPaths": fieldPaths});
 
     for (let fp0 of fieldPaths) {
         for (let fp1 of fieldPaths) {
@@ -178,7 +245,7 @@ try {
             assert.commandWorked(db.c.createIndex({[indexField]: 1}));
             const coveredIndexPipeline = [{$match: {[indexField]: {$gt: 0}}}, {$project: {x: fp0, _id: 0}}];
             jsTest.log({"coveredIndexPipeline": coveredIndexPipeline});
-            run(documents, coveredIndexPipeline, coveredPlanExpectExtractStage ? 1 : 0);
+            run(coveredIndexPipeline, coveredPlanExpectExtractStage ? 1 : 0);
 
             // Test $match then $project with fetch plan.
             const fetchPlanExpectExtractStage = fp1.includes(".");
@@ -187,7 +254,7 @@ try {
                 {$project: {x: fp1 /*use the other field*/, _id: 0}},
             ];
             jsTest.log({"fetchIndexPipeline": fetchIndexPipeline});
-            run(documents, fetchIndexPipeline, fetchPlanExpectExtractStage ? 1 : 0);
+            run(fetchIndexPipeline, fetchPlanExpectExtractStage ? 1 : 0);
 
             assert(db.c.getIndexes().length > 1, "Index should still exist");
             assert.commandWorked(db.c.dropIndex({[indexField]: 1}));
@@ -200,7 +267,7 @@ try {
                 [{$group: {_id: {path: fp0}, pathSum: {$sum: fp1}}}],
             ];
             for (let pipeline of oneExtractStagePipelines) {
-                run(documents, pipeline, hasDottedPaths ? 1 : 0 /*numExpectedExtractStages*/);
+                run(pipeline, hasDottedPaths ? 1 : 0 /*numExpectedExtractStages*/);
             }
 
             // Test $group then $project and $project then $group.
@@ -225,11 +292,7 @@ try {
                 const numExpectedExtractStages = twoExtractStagePipelines[i].numExpectedExtractStages;
                 const numExpectedExtractStagesNoDottedPaths =
                     twoExtractStagePipelines[i].numExpectedExtractStagesNoDottedPaths;
-                run(
-                    documents,
-                    pipeline,
-                    hasDottedPaths ? numExpectedExtractStages : numExpectedExtractStagesNoDottedPaths,
-                );
+                run(pipeline, hasDottedPaths ? numExpectedExtractStages : numExpectedExtractStagesNoDottedPaths);
             }
         }
     }
