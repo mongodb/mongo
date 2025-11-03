4b911a0c254c0822f4085b27d7c98f71ca06c1bb
karlbozdogan <karl.bozdogan@mongodb.com>
2025-11-03 19:06:01 +0100

    SERVER-84547 Fix tests (#43449)
    GitOrigin-RevId: c0dcc97b1348bdd99bb685c5adae6e989c0f176d

--- jstests/core_sharding/query/trivially_empty_queries.js
+++ jstests/core_sharding/query/trivially_empty_queries.js
@@ -1,7 +1,15 @@
-//
-// Tests that only one shard is targeted for trivially empty queries.
-// @tags: [requires_fcv_83, requires_2_or_more_shards]
-//
+/**
+ * Tests that only one shard is targeted for trivially empty queries.
+ * @tags: [
+ *   requires_fcv_83,
+ *   requires_2_or_more_shards,
+ *   # Avoid implicitly sharding the test collection.
+ *   assumes_no_implicit_collection_creation_on_get_collection,
+ *   # "Refusing to run a test that issues an aggregation command with
+ *   # explain because it may return incomplete results if interrupted by a stepdown."
+ *   does_not_support_stepdowns,
+ * ]
+ */
 
 import {getShards} from "jstests/sharding/libs/sharding_util.js";
 
@@ -21,8 +29,13 @@ let shardName1 = shard1._id;
 
 coll.drop();
 assert.commandWorked(db.adminCommand({shardCollection: ns, key: {"_id": 1}}));
-assert.commandWorked(db.adminCommand({moveRange: ns, min: {_id: 0}, max: {_id: 5}, toShard: shardName0}));
-assert.commandWorked(db.adminCommand({moveRange: ns, min: {_id: 5}, max: {_id: 10}, toShard: shardName1}));
+// These commands sometimes conflict with the balancer,
+// so we retry on ConflictingOperationInProgress errors.
+assert.soonRetryOnAcceptableErrors(() => {
+    assert.commandWorked(db.adminCommand({moveRange: ns, min: {_id: 0}, max: {_id: 5}, toShard: shardName0}));
+    assert.commandWorked(db.adminCommand({moveRange: ns, min: {_id: 5}, max: {_id: 10}, toShard: shardName1}));
+    return true;
+}, ErrorCodes.ConflictingOperationInProgress);
 
 const res1 = coll.explain().aggregate([{$match: {$expr: false}}]);
 assert.eq(Object.keys(res1.shards).length, 1);
