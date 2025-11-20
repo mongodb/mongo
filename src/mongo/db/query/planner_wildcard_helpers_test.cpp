6b4e49e15b7b7425be84c4a57a6d4900d70a151d
Militsa Sotirova <militsa.sotirova@mongodb.com>
2025-11-20 10:12:57 -0500

    SERVER-113900 Add runtime check to plan enumerator code (#44003)
    GitOrigin-RevId: 399c5c4f5cd0977310c7a76b5b2ef3c7a5f54cdb

--- src/mongo/db/query/planner_wildcard_helpers_test.cpp
+++ src/mongo/db/query/planner_wildcard_helpers_test.cpp
@@ -45,6 +45,7 @@
 #include "mongo/db/query/compiler/physical_model/query_solution/query_solution.h"
 #include "mongo/db/query/planner_wildcard_helpers.h"
 #include "mongo/db/query/wildcard_test_utils.h"
+#include "mongo/unittest/death_test.h"
 #include "mongo/unittest/unittest.h"
 
 
@@ -228,4 +229,19 @@ TEST(PlannerWildcardHelpersTest, Expand_CompoundWildcardIndex_NumericComponents)
     ASSERT_FALSE(expandedIndexes.front().multikey);
     ASSERT_EQ(expectedMks, expandedIndexes.front().multikeyPaths);
 }
+
+DEATH_TEST(PlannerWildcardHelpersTest, InvalidIndexExpansion, "11390001") {
+    WildcardIndexEntryMock wildcardIndex{BSON("a" << 1 << "$**" << 1), BSON("_id" << 0), {}};
+    std::set<std::string> fields{"a"};
+    std::vector<IndexEntry> expandedIndexes{};
+    expandWildcardIndexEntry(*wildcardIndex.indexEntry, fields, &expandedIndexes);
+}
+
+DEATH_TEST(PlannerWildcardHelpersTest, AnotherInvalidIndexExpansion, "11390001") {
+    WildcardIndexEntryMock wildcardIndex{BSON("$**" << 1 << "a" << 1), BSON("_id" << 0), {}};
+    std::set<std::string> fields{"a"};
+    std::vector<IndexEntry> expandedIndexes{};
+    expandWildcardIndexEntry(*wildcardIndex.indexEntry, fields, &expandedIndexes);
+}
+
 }  // namespace mongo::wildcard_planning
