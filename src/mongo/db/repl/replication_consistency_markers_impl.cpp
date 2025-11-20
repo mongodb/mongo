7d1dffad88893a0129f30ff54cb0ce6d3b3ae23e
Jordi Olivares Provencio <jordi.olivares-provencio@mongodb.com>
2025-11-20 12:03:18 +0100

    SERVER-108364 Allow an untimestamped write to the minvalid document to avoid timestamp violations (#40067)
    GitOrigin-RevId: 483e5a67ab20a354133f6be543626fb19d471cf4

--- src/mongo/db/repl/replication_consistency_markers_impl.cpp
+++ src/mongo/db/repl/replication_consistency_markers_impl.cpp
@@ -118,7 +118,11 @@ boost::optional<MinValidDocument> ReplicationConsistencyMarkersImpl::_getMinVali
 
 void ReplicationConsistencyMarkersImpl::_updateMinValidDocument(OperationContext* opCtx,
                                                                 const BSONObj& updateSpec) {
-    // Writes on minValid document should always be untimestamped.
+    // TODO SERVER-104434: Writes on minValid document should always be untimestamped even if the
+    // collection is timestamped. We allow an untimestamped write in this instance so that WT
+    // doesn't complain about timestamp violations if it encounters a timestamped entry during very
+    // rapid upgrades.
+    shard_role_details::getRecoveryUnit(opCtx)->allowOneUntimestampedWrite();
     Status status = _storageInterface->putSingleton(opCtx, _minValidNss, {updateSpec, Timestamp()});
     invariant(status);
 }
@@ -136,7 +140,7 @@ void ReplicationConsistencyMarkersImpl::initializeMinValidDocument(OperationCont
     BSONObj upsert = BSON("$max" << BSON(MinValidDocument::kMinValidTimestampFieldName
                                          << Timestamp() << MinValidDocument::kMinValidTermFieldName
                                          << OpTime::kUninitializedTerm));
-    fassert(40467, _storageInterface->putSingleton(opCtx, _minValidNss, {upsert, Timestamp()}));
+    _updateMinValidDocument(opCtx, upsert);
 }
 
 bool ReplicationConsistencyMarkersImpl::getInitialSyncFlag(OperationContext* opCtx) const {
