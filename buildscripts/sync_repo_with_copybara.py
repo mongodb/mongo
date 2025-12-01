7eebd53e77870344d3125b8c1084e9c1c5e7b335
Zack Winter <3457246+zackwintermdb@users.noreply.github.com>
2025-12-01 09:01:20 -0800

    SERVER-113700 [v8.0] Update copybara to v20251110 (#43804) (#44161)
    GitOrigin-RevId: dad2cd9bf9b8ff6a52c7ceca4df987875a0ac1b6

--- buildscripts/sync_repo_with_copybara.py
+++ buildscripts/sync_repo_with_copybara.py
@@ -16,6 +16,9 @@ from github import GithubIntegration
 from buildscripts.util.read_config import read_config_file
 from evergreen.api import RetryingEvergreenApi
 
+# Commit hash of Copybara to use (v20251110)
+COPYBARA_COMMIT_HASH = "3f050c9e08b84aeda98875bf1b02a3288d351333"
+
 
 class CopybaraRepoConfig(NamedTuple):
     """Copybara source and destination repo sync configuration."""
@@ -362,7 +365,10 @@ def main():
     if os.path.exists('copybara'):
         print("Copybara directory already exists.")
     else:
-        run_command("git clone --branch d03973e9fe1 https://github.com/10gen/copybara.git")
+        run_command("git clone https://github.com/10gen/copybara.git")
+
+    # Checkout the specific commit of Copybara we want to use
+    run_command(f"cd copybara && git checkout {COPYBARA_COMMIT_HASH}")
 
     # Navigate to the Copybara directory and build the Copybara Docker image
     run_command("cd copybara && docker build --rm -t copybara .")
