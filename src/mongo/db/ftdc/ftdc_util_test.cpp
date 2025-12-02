99e7660882b980855fd00020220185ca48e19d32
Cole Harbeck <cole.harbeck@mongodb.com>
2025-12-02 13:06:59 -0500

    SERVER-114492: Add unit tests for metric and document FTDC utils (#44477)
    GitOrigin-RevId: 8b4a28ca4adc9f7cf46bfe6bbbf6c7a191be162d

--- src/mongo/db/ftdc/ftdc_util_test.cpp
+++ src/mongo/db/ftdc/ftdc_util_test.cpp
@@ -80,58 +80,53 @@ TEST(FTDCUtilTest, TestMongoSPath) {
 }
 
 TEST(FTDCUtilTest, applyExtractionConversionsToDocumentWorks) {
-    BSONObjBuilder refBuilder;
+    BSONObjBuilder refBuilder, expectedBuilder;
     refBuilder.append("field1", 10.5);
+    expectedBuilder.append("field1", 10.5);
+
     refBuilder.append("field2", 1);
+    expectedBuilder.append("field2", 1);
+
     refBuilder.append("field3", INT64_MAX);
+    expectedBuilder.append("field3", INT64_MAX);
+
     refBuilder.append("field4", BSONElement::kLongLongMaxPlusOneAsDouble);
+    expectedBuilder.append("field4", INT64_MAX);
+
     double lessThanInt64Min = -9223372036854777856.0;
     refBuilder.append("field5", lessThanInt64Min);
-
-    BSONObjBuilder subObjBuilder1;
-    subObjBuilder1.append("subField1", true);
-    subObjBuilder1.append("subField2", Date_t::fromMillisSinceEpoch(100000000000000));
-
-    refBuilder.append("field6", subObjBuilder1.obj());
-
-    BSONObjBuilder subObjBuilder2;
-    subObjBuilder2.append("subField3", Timestamp(100, 10));
-    subObjBuilder2.append("subField4", std::nan(""));
-
-    refBuilder.appendArray("field7", subObjBuilder2.obj());
-
-
-    BSONObjBuilder expectedBuilder;
-    expectedBuilder.append("field1", 10.5);
-    expectedBuilder.append("field2", 1);
-    expectedBuilder.append("field3", INT64_MAX);
-    expectedBuilder.append("field4", INT64_MAX);
     expectedBuilder.append("field5", INT64_MIN);
 
-    BSONObjBuilder expectedSubObjBuilder1;
+    BSONObjBuilder subObjBuilder1, expectedSubObjBuilder1;
+    subObjBuilder1.append("subField1", true);
     expectedSubObjBuilder1.append("subField1", true);
+
+    subObjBuilder1.append("subField2", Date_t::fromMillisSinceEpoch(100000000000000));
     expectedSubObjBuilder1.append("subField2", Date_t::fromMillisSinceEpoch(100000000000000));
 
+    refBuilder.append("field6", subObjBuilder1.obj());
     expectedBuilder.append("field6", expectedSubObjBuilder1.obj());
 
-    BSONObjBuilder expectedSubObjBuilder2;
+    BSONObjBuilder subObjBuilder2, expectedSubObjBuilder2;
+    subObjBuilder2.append("subField3", Timestamp(100, 10));
     expectedSubObjBuilder2.append("subField3", Timestamp(100, 10));
+
+    subObjBuilder2.append("subField4", std::nan(""));
     expectedSubObjBuilder2.append("subField4", 0);
 
+    refBuilder.appendArray("field7", subObjBuilder2.obj());
     expectedBuilder.appendArray("field7", expectedSubObjBuilder2.obj());
 
     auto swResultDoc = FTDCBSONUtil::applyExtractionConversionsToDocument(refBuilder.obj());
     ASSERT_OK(swResultDoc);
 
     BSONObj resultDoc = swResultDoc.getValue();
-
-    ASSERT_BSONOBJ_EQ(expectedBuilder.obj(), resultDoc);
+    ASSERT_BSONOBJ_EQ(resultDoc, expectedBuilder.obj());
 }
 
-TEST(FTDCUtilTest, applyExtractionConversionsToDocumentFailsAtMaxRecursion) {
+TEST(FTDCUtilTest, metricUtilsFailAtMaxRecursion) {
     BSONObjBuilder builder1;
     builder1.append("1", 1);
-
     BSONObjBuilder builder2;
     builder2.append("2", builder1.obj());
     BSONObjBuilder builder3;
@@ -153,16 +148,255 @@ TEST(FTDCUtilTest, applyExtractionConversionsToDocumentFailsAtMaxRecursion) {
     BSONObjBuilder builder11;
     builder11.append("11", builder10.obj());
 
-    BSONObj nestedObj = builder11.obj();
+    BSONObj okNestedObj = builder11.obj();
 
-    auto swOkResultDoc = FTDCBSONUtil::applyExtractionConversionsToDocument(nestedObj);
-    ASSERT_OK(swOkResultDoc);
+    auto swOkConversions = FTDCBSONUtil::applyExtractionConversionsToDocument(okNestedObj);
+    ASSERT_OK(swOkConversions);
+
+    std::vector<std::uint64_t> extractionOkMetrics;
+    auto swOkExtractions =
+        FTDCBSONUtil::extractMetricsFromDocument(okNestedObj, okNestedObj, &extractionOkMetrics);
+    ASSERT_OK(swOkExtractions);
+
+    std::vector<std::uint64_t> constructionMetrics = {1};
+    auto swOkConstructions =
+        FTDCBSONUtil::constructDocumentFromMetrics(okNestedObj, constructionMetrics);
+    ASSERT_OK(swOkConstructions);
 
     BSONObjBuilder builder12;
-    builder12.append("12", nestedObj);
+    builder12.append("12", okNestedObj);
 
-    auto swNotOkResultDoc = FTDCBSONUtil::applyExtractionConversionsToDocument(builder12.obj());
+    BSONObj notOkNestedObj = builder12.obj();
+
+    auto swNotOkResultDoc = FTDCBSONUtil::applyExtractionConversionsToDocument(notOkNestedObj);
     ASSERT_NOT_OK(swNotOkResultDoc);
+
+    std::vector<std::uint64_t> extractionNotOkMetrics;
+    auto swNotOkExtractions = FTDCBSONUtil::extractMetricsFromDocument(
+        notOkNestedObj, notOkNestedObj, &extractionNotOkMetrics);
+    ASSERT_NOT_OK(swNotOkExtractions);
+
+    auto swNotOkConstructions =
+        FTDCBSONUtil::constructDocumentFromMetrics(notOkNestedObj, constructionMetrics);
+    ASSERT_NOT_OK(swNotOkConstructions);
+}
+
+TEST(FTDCUtilTest, extractMetricsFromDocumentWorksForSameDoc) {
+    BSONObjBuilder refBuilder;
+    std::vector<std::uint64_t> expectedMetrics;
+
+    refBuilder.append("field1", 10.5);
+    expectedMetrics.push_back(10);
+
+    refBuilder.append("field2", 1);
+    expectedMetrics.push_back(1);
+
+    refBuilder.append("field3", INT64_MAX);
+    expectedMetrics.push_back(INT64_MAX);
+
+    refBuilder.append("field4", BSONElement::kLongLongMaxPlusOneAsDouble);
+    expectedMetrics.push_back(INT64_MAX);
+
+    double lessThanInt64Min = -9223372036854777856.0;
+    refBuilder.append("field5", lessThanInt64Min);
+    expectedMetrics.push_back(INT64_MIN);
+
+    BSONObjBuilder subObjBuilder1;
+    subObjBuilder1.append("subField1", true);
+    expectedMetrics.push_back(true);
+
+    subObjBuilder1.append("subField2", Date_t::fromMillisSinceEpoch(100000000000000));
+    expectedMetrics.push_back(100000000000000);
+
+    refBuilder.append("field6", subObjBuilder1.obj());
+
+    BSONObjBuilder subObjBuilder2;
+    subObjBuilder2.append("subField3", Timestamp(100, 10));
+    expectedMetrics.push_back(100);
+    expectedMetrics.push_back(10);
+
+    subObjBuilder2.append("subField4", std::nan(""));
+    expectedMetrics.push_back(0);
+
+    refBuilder.appendArray("field7", subObjBuilder2.obj());
+
+    BSONObj refObj = refBuilder.obj();
+
+    std::vector<std::uint64_t> actualMetrics;
+    auto swMatchesReference =
+        FTDCBSONUtil::extractMetricsFromDocument(refObj, refObj, &actualMetrics);
+
+    ASSERT_OK(swMatchesReference);
+    ASSERT_TRUE(swMatchesReference.getValue());
+    ASSERT_EQ(actualMetrics, expectedMetrics);
+}
+
+TEST(FTDCUtilTest, extractMetricsFromDocumentWorksForChangingMetrics) {
+    BSONObjBuilder refBuilder, docBuilder;
+    std::vector<std::uint64_t> expectedMetrics;
+
+    refBuilder.append("field1", 1);
+    docBuilder.append("field1", 2);
+    expectedMetrics.push_back(2);
+
+    refBuilder.append("field2", 10.5);
+    docBuilder.append("field2", 11.5);
+    expectedMetrics.push_back(11);
+
+    std::vector<std::uint64_t> actualMetrics;
+    auto swMatchesReference = FTDCBSONUtil::extractMetricsFromDocument(
+        refBuilder.obj(), docBuilder.obj(), &actualMetrics);
+
+    ASSERT_OK(swMatchesReference);
+    ASSERT_TRUE(swMatchesReference.getValue());
+    ASSERT_EQ(actualMetrics, expectedMetrics);
+}
+
+TEST(FTDCUtilTest, extractMetricsFromDocumentDocumentsDoNotMatch) {
+    std::vector<std::uint64_t> metrics;
+    BSONObjBuilder refBuilder1, docBuilder1;
+
+    refBuilder1.append("fieldName", 1);
+    docBuilder1.append("differentFieldName", 1);
+
+    auto swMatchesReference1 =
+        FTDCBSONUtil::extractMetricsFromDocument(refBuilder1.obj(), docBuilder1.obj(), &metrics);
+    ASSERT_OK(swMatchesReference1);
+    ASSERT_FALSE(swMatchesReference1.getValue());
+
+    BSONObjBuilder refBuilder2, docBuilder2;
+
+    refBuilder2.append("fieldName", 1);
+    docBuilder2.append("fieldName", 1);
+    docBuilder2.append("extraFieldName", 1);
+
+    metrics.clear();
+    auto swMatchesReference2 =
+        FTDCBSONUtil::extractMetricsFromDocument(refBuilder2.obj(), docBuilder2.obj(), &metrics);
+    ASSERT_OK(swMatchesReference2);
+    ASSERT_FALSE(swMatchesReference2.getValue());
+
+    BSONObjBuilder refBuilder3, docBuilder3;
+
+    refBuilder3.append("shouldBeIntFieldName", 1);
+    docBuilder3.append("shouldBeIntFieldName", Timestamp(100, 10));
+
+    metrics.clear();
+    auto swMatchesReference3 =
+        FTDCBSONUtil::extractMetricsFromDocument(refBuilder3.obj(), docBuilder3.obj(), &metrics);
+    ASSERT_OK(swMatchesReference3);
+    ASSERT_FALSE(swMatchesReference3.getValue());
+
+    BSONObjBuilder refBuilder4, docBuilder4;
+
+    refBuilder4.append("fieldName", 1);
+    refBuilder4.append("extraFieldName", 1);
+    docBuilder4.append("fieldName", 1);
+
+    metrics.clear();
+    auto swMatchesReference4 =
+        FTDCBSONUtil::extractMetricsFromDocument(refBuilder4.obj(), docBuilder4.obj(), &metrics);
+    ASSERT_OK(swMatchesReference4);
+    ASSERT_FALSE(swMatchesReference4.getValue());
+}
+
+TEST(FTDCUtilTest, constructDocumentFromMetricsWorks) {
+    BSONObjBuilder refBuilder, expectedObjBuilder;
+    std::vector<std::uint64_t> metrics;
+
+    metrics.push_back(10);
+    refBuilder.append("field1", 10.5);
+    expectedObjBuilder.append("field1", 10);
+
+    metrics.push_back(1);
+    refBuilder.append("field2", 1);
+    expectedObjBuilder.append("field2", 1);
+
+    metrics.push_back(INT64_MAX);
+    refBuilder.append("field3", INT64_MAX);
+    expectedObjBuilder.append("field3", INT64_MAX);
+
+    metrics.push_back(INT64_MAX);
+    refBuilder.append("field4", BSONElement::kLongLongMaxPlusOneAsDouble);
+    expectedObjBuilder.append("field4", INT64_MAX);
+
+    double lessThanInt64Min = -9223372036854777856.0;
+    metrics.push_back(INT64_MIN);
+    refBuilder.append("field5", lessThanInt64Min);
+    expectedObjBuilder.append("field5", INT64_MIN);
+
+    BSONObjBuilder subObjBuilder1;
+    metrics.push_back(true);
+    subObjBuilder1.append("subField1", true);
+
+    metrics.push_back(100000000000000);
+    subObjBuilder1.append("subField2", Date_t::fromMillisSinceEpoch(100000000000000));
+
+    BSONObj subObj1 = subObjBuilder1.obj();
+
+    refBuilder.append("field6", subObj1);
+    expectedObjBuilder.append("field6", subObj1);
+
+    BSONObjBuilder subObjBuilder2, expectedSubObjBuilder2;
+    metrics.push_back(100);
+    metrics.push_back(10);
+    subObjBuilder2.append("subField3", Timestamp(100, 10));
+    expectedSubObjBuilder2.append("subField3", Timestamp(100, 10));
+
+    metrics.push_back(0);
+    subObjBuilder2.append("subField4", std::nan(""));
+    expectedSubObjBuilder2.append("subField4", 0);
+
+    refBuilder.appendArray("field7", subObjBuilder2.obj());
+    expectedObjBuilder.appendArray("field7", expectedSubObjBuilder2.obj());
+
+    auto swActualDoc = FTDCBSONUtil::constructDocumentFromMetrics(refBuilder.obj(), metrics);
+
+    ASSERT_OK(swActualDoc);
+    ASSERT_BSONOBJ_EQ(swActualDoc.getValue(), expectedObjBuilder.obj());
+}
+
+TEST(FTDCUtilTest, constructDocumentFromMetricsDocumentAndMetricsDoNotMatch) {
+    BSONObjBuilder refBuilder1;
+    std::vector<std::uint64_t> metrics1;
+
+    refBuilder1.append("intField", 1);
+    refBuilder1.append("extraIntField", 1);
+    metrics1.push_back(1);
+
+    auto swObj1 = FTDCBSONUtil::constructDocumentFromMetrics(refBuilder1.obj(), metrics1);
+    ASSERT_NOT_OK(swObj1);
+
+    BSONObjBuilder refBuilder2;
+    std::vector<std::uint64_t> metrics2;
+
+    refBuilder2.append("boolField", true);
+    refBuilder2.append("extraBoolField", false);
+    metrics2.push_back(true);
+
+    auto swObj2 = FTDCBSONUtil::constructDocumentFromMetrics(refBuilder2.obj(), metrics2);
+    ASSERT_NOT_OK(swObj2);
+
+    BSONObjBuilder refBuilder3;
+    std::vector<std::uint64_t> metrics3;
+
+    refBuilder3.append("dateField", Date_t::fromMillisSinceEpoch(100000000000000));
+    refBuilder3.append("extraDateField", Date_t::fromMillisSinceEpoch(100000000000001));
+    metrics3.push_back(100000000000000);
+
+    auto swObj3 = FTDCBSONUtil::constructDocumentFromMetrics(refBuilder3.obj(), metrics3);
+    ASSERT_NOT_OK(swObj3);
+
+    BSONObjBuilder refBuilder4;
+    std::vector<std::uint64_t> metrics4;
+
+    refBuilder4.append("timestamp", Timestamp(100, 10));
+    refBuilder4.append("extraTimestamp", Timestamp(101, 11));
+    metrics4.push_back(100);
+    metrics4.push_back(10);
+
+    auto swObj4 = FTDCBSONUtil::constructDocumentFromMetrics(refBuilder4.obj(), metrics4);
+    ASSERT_NOT_OK(swObj4);
 }
 
 }  // namespace mongo
