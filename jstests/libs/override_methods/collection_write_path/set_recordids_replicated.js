523e6393321124aeaf6abc22ecd866e875d9db66
Malik Endsley <malik.endsley@mongodb.com>
2025-12-29 13:34:02 -0500

    SERVER-114873: Refactor set_recordids_replicated.js (#45602)
    GitOrigin-RevId: 67ef9b5c6bf3b366208bf01a9fba9569072c74d3

--- jstests/libs/override_methods/collection_write_path/set_recordids_replicated.js
+++ jstests/libs/override_methods/collection_write_path/set_recordids_replicated.js
@@ -2,7 +2,7 @@
  * Use prototype overrides to create collection with replicated record ids while running tests.
  *
  * The create command is overridden by blindly setting recordIdsReplicated to true.
- * The insert command is overriden by calling an additional recordIdsReplicated create command
+ * The insert command is overridden by calling an additional recordIdsReplicated create command
  * before insert.
  *
  * A test can use the overrides by loading this library before the test is run.
@@ -23,103 +23,131 @@ function hasError(res) {
 }
 
 function runCommandWithRecordIdsReplicated(conn, dbName, commandName, commandObj, func, makeFuncArgs) {
+    if (typeof commandObj !== "object" || commandObj === null) {
+        return func.apply(conn, makeFuncArgs(commandObj));
+    }
+
     const collName = commandObj[commandName];
     const ns = dbName + "." + collName;
+    const runCommand = (obj) => func.apply(conn, makeFuncArgs(obj));
+
+    // Don't disturb errors that would be thrown for invalid collection names.
+    if (!isValidCollectionName(collName)) {
+        return runCommand(commandObj);
+    }
+
+    // Check drop before skipping tracked collections to avoid skipping drops.
     if (commandName === "drop") {
         collectionsKnownToExist.delete(ns);
-        return func.apply(conn, makeFuncArgs(commandObj));
+        return runCommand(commandObj);
     }
-    if (
-        !commandsToOverride.has(commandName) ||
-        collectionsKnownToExist.has(ns) ||
-        typeof commandObj !== "object" ||
-        commandObj === null
-    ) {
-        return func.apply(conn, makeFuncArgs(commandObj));
-    }
-    if (!isValidCollectionName(collName)) {
-        // Avoid issuing listCollections for invalid namespaces (e.g., embedded nulls) so we don't
-        // mask the command's own InvalidNamespace error with our override logic.
-        return func.apply(conn, makeFuncArgs(commandObj));
-    }
-    if (commandName === "create") {
-        if (commandObj.hasOwnProperty("clusteredIndex")) {
-            return func.apply(conn, makeFuncArgs(commandObj));
-        }
-        if (commandObj.hasOwnProperty("timeseries")) {
-            return func.apply(conn, makeFuncArgs(commandObj));
-        }
-        commandObj = Object.assign({}, commandObj);
-        if (!commandObj.hasOwnProperty("recordIdsReplicated")) {
-            commandObj.recordIdsReplicated = true;
-        }
-        const res = func.apply(conn, makeFuncArgs(commandObj));
-        if (hasError(res)) {
-            jsTestLog("create error: " + tojsononeline(res));
-        }
-        if (!hasError(res) || res.code === ErrorCodes.NamespaceExists) {
-            collectionsKnownToExist.add(ns);
-        }
-        return res;
-    } else {
-        if (commandName === "createIndexes") {
-            const clustered = "clustered";
-            const clusteredTrue = commandObj["indexes"].some((obj) => clustered in obj && obj.unique === true);
-            if (clusteredTrue) {
-                collectionsKnownToExist.add(ns);
-                return func.apply(conn, makeFuncArgs(commandObj));
-            }
-        }
-        if ((commandName === "insert" || commandName === "update") && commandObj.hasOwnProperty("documents")) {
-            const id = "_id";
-            const idUndefined = commandObj["documents"].some((obj) => id in obj && obj.id === undefined);
-            if (idUndefined) {
-                return func.apply(conn, makeFuncArgs(commandObj));
-            }
-        }
-        // If the collection already existed, don't clean up even if the inner command fails.
-        const collExists = conn.getDB(dbName).getCollectionInfos({name: collName}).length > 0;
-        if (collExists) {
-            collectionsKnownToExist.add(ns);
-            return func.apply(conn, makeFuncArgs(commandObj));
-        }
-        const createObj = {create: collName, recordIdsReplicated: true};
-        ["lsid", "$clusterTime", "writeConcern", "collectionUUID"].forEach((option) => {
-            if (commandObj.hasOwnProperty(option)) {
-                createObj[option] = commandObj[option];
-            }
-        });
 
-        const createRes = func.apply(conn, makeFuncArgs(createObj));
-        let createdByOverride = false;
-        if (hasError(createRes)) {
-            jsTest.log.info(
-                "Error while creating collection for set_recordids_replicated.js override: " + tojsononeline(createRes),
-            );
-        } else {
-            createdByOverride = true;
-        }
-        if (!hasError(createRes) || createRes.code === ErrorCodes.NamespaceExists) {
-            collectionsKnownToExist.add(ns);
-        }
+    if (!commandsToOverride.has(commandName)) {
+        return runCommand(commandObj);
+    }
 
-        const wrappedCmdRes = func.apply(conn, makeFuncArgs(commandObj));
-        if (createdByOverride && hasError(wrappedCmdRes) && commandName === "createIndexes") {
-            jsTest.log.info(
-                "Cleaning up collection created for set_recordids_replicated.js override after createIndexes error: " +
-                    tojsononeline(wrappedCmdRes),
-            );
-            const dropRes = func.apply(conn, makeFuncArgs({drop: collName}));
-            if (hasError(dropRes)) {
-                jsTest.log.info(
-                    "Error while cleaning up collection for set_recordids_replicated.js override: " +
-                        tojsononeline(dropRes),
+    if (collectionsKnownToExist.has(ns)) {
+        return runCommand(commandObj);
+    }
+
+    switch (commandName) {
+        case "create": {
+            if (commandObj.hasOwnProperty("clusteredIndex") || commandObj.hasOwnProperty("timeseries")) {
+                return runCommand(commandObj);
+            }
+
+            // Perform the modification.
+            const createCmd = Object.assign({}, commandObj);
+            if (!createCmd.hasOwnProperty("recordIdsReplicated")) {
+                createCmd.recordIdsReplicated = true;
+            }
+
+            const res = runCommand(createCmd);
+            if (hasError(res)) {
+                jsTestLog(
+                    "Error while creating collection via set_recordids_replicated.js override: " + tojsononeline(res),
                 );
             }
-            collectionsKnownToExist.delete(ns);
+            if (!hasError(res) || res.code === ErrorCodes.NamespaceExists) {
+                collectionsKnownToExist.add(ns);
+            }
+            return res;
         }
-        return wrappedCmdRes;
+        case "createIndexes": {
+            const creatingClusteredIndex = commandObj["indexes"].some(
+                (obj) => "clustered" in obj && obj.unique === true,
+            );
+            if (creatingClusteredIndex) {
+                collectionsKnownToExist.add(ns);
+                return runCommand(commandObj);
+            }
+            break;
+        }
+        case "insert":
+        case "update": {
+            if (commandObj.hasOwnProperty("documents")) {
+                const idUndefinedInAnyDocument = commandObj["documents"].some(
+                    (obj) => "_id" in obj && obj.id === undefined,
+                );
+                if (idUndefinedInAnyDocument) {
+                    return runCommand(commandObj);
+                }
+            }
+            break;
+        }
+        default:
+            throw new Error(
+                "Unexpected overridden command in set_recordids_replicated.js override: " + tojsononeline(commandObj),
+            );
     }
+
+    // If the collection already existed, don't clean up even if the inner command fails.
+    const collExists = conn.getDB(dbName).getCollectionInfos({name: collName}).length > 0;
+    if (collExists) {
+        collectionsKnownToExist.add(ns);
+        return runCommand(commandObj);
+    }
+
+    const createObj = {create: collName, recordIdsReplicated: true};
+    for (const option of ["lsid", "$clusterTime", "writeConcern", "collectionUUID"]) {
+        if (commandObj.hasOwnProperty(option)) {
+            createObj[option] = commandObj[option];
+        }
+    }
+
+    // Perform the modification by pre-creating the collection with recordIdsReplicated:true.
+    const createRes = runCommand(createObj);
+    let createdByOverride = false;
+    if (hasError(createRes)) {
+        jsTest.log.info(
+            "Error while creating collection via set_recordids_replicated.js override: " + tojsononeline(createRes),
+        );
+    } else {
+        createdByOverride = true;
+    }
+    if (!hasError(createRes) || createRes.code === ErrorCodes.NamespaceExists) {
+        collectionsKnownToExist.add(ns);
+    }
+
+    // Now run the original command.
+    const wrappedCmdRes = runCommand(commandObj);
+    if (createdByOverride && hasError(wrappedCmdRes) && commandName === "createIndexes") {
+        // Drop the collection we created to match implicit-collection semantics on createIndexes failure.
+        jsTest.log.info(
+            "Cleaning up collection created via set_recordids_replicated.js override after createIndexes error: " +
+                tojsononeline(wrappedCmdRes),
+        );
+        const dropRes = runCommand({drop: collName});
+        if (hasError(dropRes)) {
+            jsTest.log.info(
+                "Error while cleaning up collection via set_recordids_replicated.js override: " +
+                    tojsononeline(dropRes),
+            );
+        }
+        collectionsKnownToExist.delete(ns);
+    }
+
+    return wrappedCmdRes;
 }
 
 OverrideHelpers.prependOverrideInParallelShell(
