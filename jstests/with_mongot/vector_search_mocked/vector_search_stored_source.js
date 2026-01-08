4abb512eb79e0e9d21749ea1548db5aab8b52d67
Joshua Siegel <39130209+joshbsiegel@users.noreply.github.com>
2026-01-08 10:31:42 -0500

    SERVER-12786 Enable `internalVectorSearchStoredSource` knob by default (#43717)
    GitOrigin-RevId: c9bf0e476cbddef939f97c97620a6ab422ab4fb9

--- jstests/with_mongot/vector_search_mocked/vector_search_stored_source.js
+++ jstests/with_mongot/vector_search_mocked/vector_search_stored_source.js
@@ -243,10 +243,6 @@ function testShardedVectorSearchStoredSourceParameterOff(stWithMock, testDB, col
         {_id: 3, title: "burger king"},
     ]);
 
-    // Enable cluster parameter.
-    assert.commandWorked(
-        testDB.adminCommand({setClusterParameter: {internalVectorSearchStoredSource: {enabled: true}}}),
-    );
     testStandaloneVectorSearchStoredSourceParameterOn(mongotMock, testDB, collectionUUID);
 
     // Disable the cluster parameter and run tests again.
