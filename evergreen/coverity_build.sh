4f90a1722a85488b79d1555b6a51850f68ab0bff
Daniel Moody <dmoody256@gmail.com>
2026-01-12 13:29:22 -0600

    SERVER-116969 reduce coverity jobs (#46230)
    GitOrigin-RevId: f52dfaffe092cb456eb883ac10179ab1568ff610

--- evergreen/coverity_build.sh
+++ evergreen/coverity_build.sh
@@ -18,6 +18,8 @@ export MONGO_WRAPPER_OUTPUT_ALL=1
 # number of parallel jobs to use for build.
 # Even with scale=0 (the default), bc command adds decimal digits in case of multiplication. Division by 1 gives us a whole number with scale=0
 bazel_jobs=$(bc <<<"$(grep -c '^processor' /proc/cpuinfo) * .85 / 1")
+cov_jobs=$(bc <<<"$(grep -c '^processor' /proc/cpuinfo) * .40 / 1")
+
 build_config="--config=local --jobs=$bazel_jobs --build_atlas=True --compiler_type=gcc --opt=off --dbg=False --allocator=system --define=MONGO_VERSION=${version}"
 bazel_query='mnemonic("CppCompile|LinkCompile", filter(//src/mongo, deps(//:install-core)) except //src/mongo/db/modules/enterprise/src/streams/third_party/...)'
 bazel_cache="--output_user_root=$workdir/bazel_cache"
@@ -32,7 +34,7 @@ buildCommand="bazel \
     --target_pattern_file=coverity_targets.list"
 echo Building $buildCommand
 cat coverity_targets.list
-if ! $workdir/coverity/bin/cov-build --dir "$covIdir" --verbose 0 -j $bazel_jobs --return-emit-failures --parse-error-threshold=99 --bazel \
+if ! $workdir/coverity/bin/cov-build --dir "$covIdir" --verbose 0 -j $cov_jobs --return-emit-failures --parse-error-threshold=99 --bazel \
     $buildCommand; then
     ret=$?
     echo "cov-build failed with exit code $ret"
