aece98cf8ce1f93409a20671e972c78e55b6cf7a
Will Buerger <59492746+wbuerger46@users.noreply.github.com>
2026-01-21 14:59:22 -0500

    SERVER-117541: Document limitations of returning errors from get_mongodb_extension (#46714)
    GitOrigin-RevId: 12f9db0279fbdcb2d337e1255456b93ec0e4c382

--- src/mongo/db/extension/public/api.h
+++ src/mongo/db/extension/public/api.h
@@ -1136,6 +1136,15 @@ typedef struct MongoExtensionVTable {
  *
  * NOTE: You must define this symbol in your extension shared library and avoid name mangling (for
  * example, with 'extern "C"') so that the MongoDB server can find it at loadtime.
+ *
+ * IMPORTANT: We require that extensions throw exceptions using the HostServices' user_asserted()
+ * and tripwire_asserted() functions instead of throwing C++ exceptions across the API boundary.
+ * However, the HostServices is only initialized after this function is called (during the call
+ * to MongoExtension::initialize). Attempting to use HostServices in the body of
+ * get_mongodb_extension would result in a crash. Therefore, if your extension needs to report
+ * errors during get_mongodb_extension, you must build a "hand-crafted" MongoExtensionStatus object
+ * and return it directly from this function.
+ * TODO SERVER-115700: Fix this design limitation.
  */
 #define GET_MONGODB_EXTENSION_SYMBOL "get_mongodb_extension"
 typedef MongoExtensionStatus* (*get_mongo_extension_t)(
