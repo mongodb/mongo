17c31ad3e18b6ef0c7ced0d10d5d66966f68ac56
Jordi Serra Torrens <jordist@users.noreply.github.com>
2026-01-21 15:39:29 +0100

    SERVER-116089 Add lock acquisition timeouts to migration's recipient critical section (#46575)
    Co-authored-by: Meryama <meryama.nadim@mongodb.com>
    GitOrigin-RevId: dd30e04b39108bace403f54ce6f80147505a3b03

--- src/mongo/db/s/migration_destination_manager.cpp
+++ src/mongo/db/s/migration_destination_manager.cpp
@@ -1781,38 +1781,70 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* outerOpCtx,
 
         const auto critSecReason = criticalSectionReason(*_sessionId);
 
-        runWithoutSession(outerOpCtx, [&] {
-            MigrationRecipientRecoveryDocument recoveryDoc;
-            {
-                stdx::lock_guard<stdx::mutex> lg(_mutex);
-                recoveryDoc = {
-                    *_migrationId, _nss, *_sessionId, range, _fromShard, _lsid, _txnNumber};
-            }
-            // Persist the migration recipient recovery document so that in case of failover,
-            // the new primary will resume the MigrationDestinationManager and retake the
-            // critical section.
-            migrationutil::persistMigrationRecipientRecoveryDocument(opCtx, recoveryDoc);
+        try {
+            runWithoutSession(outerOpCtx, [&] {
+                MigrationRecipientRecoveryDocument recoveryDoc;
+                {
+                    stdx::lock_guard<stdx::mutex> lg(_mutex);
+                    recoveryDoc = {
+                        *_migrationId, _nss, *_sessionId, range, _fromShard, _lsid, _txnNumber};
+                }
+                // Persist the migration recipient recovery document so that in case of failover,
+                // the new primary will resume the MigrationDestinationManager and retake the
+                // critical section.
+                migrationutil::persistMigrationRecipientRecoveryDocument(opCtx, recoveryDoc);
 
-            LOGV2_DEBUG(5899113,
-                        2,
-                        "Persisted migration recipient recovery document",
-                        "sessionId"_attr = _sessionId,
-                        logAttrs(_nss));
+                LOGV2_DEBUG(5899113,
+                            2,
+                            "Persisted migration recipient recovery document",
+                            "sessionId"_attr = _sessionId,
+                            logAttrs(_nss));
 
-            // Enter critical section. Ensure it has been majority commited before _recvChunkCommit
-            // returns success to the donor, so that if the recipient steps down, the critical
-            // section is kept taken while the donor commits the migration.
-            ShardingRecoveryService::get(opCtx)->acquireRecoverableCriticalSectionBlockWrites(
-                opCtx, _nss, critSecReason, defaultMajorityWriteConcernDoNotUse());
+                // Enter critical section. Ensure it has been majority commited before
+                // _recvChunkCommit returns success to the donor, so that if the recipient steps
+                // down, the critical section is kept taken while the donor commits the migration.
+                ShardingRecoveryService::get(opCtx)->acquireRecoverableCriticalSectionBlockWrites(
+                    opCtx,
+                    _nss,
+                    critSecReason,
+                    defaultMajorityWriteConcernDoNotUse(),
+                    true, /* (default) clearDbMetadata */
+                    Milliseconds(migrationLockAcquisitionMaxWaitMS.load()));
 
-            LOGV2(5899114, "Entered migration recipient critical section", logAttrs(_nss));
-            timeInCriticalSection.emplace();
-        });
+                LOGV2(5899114, "Entered migration recipient critical section", logAttrs(_nss));
+                timeInCriticalSection.emplace();
+            });
+        } catch (const DBException& ex) {
+            // Something failed when acquiring the critical section. Clean up and fail the
+            // migration.
+            LOGV2(11608900,
+                  "Failed to acquire the migration recipient critical section",
+                  "ex"_attr = ex);
 
-        if (getState() == kFail || getState() == kAbort) {
-            _setStateFail("timed out waiting for critical section acquisition");
+            // Make sure the critical section is completely released.
+            ShardingRecoveryService::get(opCtx)->releaseRecoverableCriticalSection(
+                opCtx,
+                _nss,
+                critSecReason,
+                defaultMajorityWriteConcernDoNotUse(),
+                ShardingRecoveryService::NoCustomAction());
+
+            // Delete the recovery document, since the critical section is definitely not
+            // active.
+            migrationutil::deleteMigrationRecipientRecoveryDocument(opCtx, *_migrationId);
+            LOGV2_DEBUG(
+                11608901,
+                1,
+                "Deleted migration recipient recovery document after critical acquisition failure");
+
+            // Fail the migration and end the recipient state machine here.
+            _setStateFail(str::stream()
+                          << "Failed when acquiring the migration recipient critical section: "
+                          << redact(ex.toString()));
+            return;
         }
 
+        // At this point, the critical section has been engaged.
         {
             // Make sure we don't overwrite a FAIL or ABORT state.
             stdx::lock_guard<stdx::mutex> sl(_mutex);
@@ -1832,6 +1864,8 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* outerOpCtx,
             cc().makeOperationContext(), outerOpCtx->getCancellationToken(), executor);
         auto opCtx = newOpCtxPtr.get();
 
+        // Note: Not honoring 'migrationLockAcquisitionMaxWaitMS' because this is a recovery path
+        // where the critical section had (potentially) already been granted previously.
         ShardingRecoveryService::get(opCtx)->acquireRecoverableCriticalSectionBlockWrites(
             opCtx, _nss, criticalSectionReason(*_sessionId), defaultMajorityWriteConcernDoNotUse());
 
