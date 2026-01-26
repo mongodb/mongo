b061ffb18b96bfc0109a528a679c32aa2b61cb63
Zack Winter <3457246+zackwintermdb@users.noreply.github.com>
2026-01-26 11:23:07 -0800

    SERVER-117275 Compress coredumps in unit test wrapper (#46919)
    GitOrigin-RevId: e63046efaa6b4a4c28282ca8a6180ac8e7f80636

--- bazel/test_wrapper.sh
+++ bazel/test_wrapper.sh
@@ -56,8 +56,8 @@ RET=$?
 
 CORE_FILE=$(find -L ./ -name "*.core")
 if [ -f "$CORE_FILE" ]; then
-    CORE_FILENAME="dump_$(date +%s%N).core"
-    mv $CORE_FILE "$TEST_UNDECLARED_OUTPUTS_DIR/$CORE_FILENAME"
+    CORE_FILENAME="dump_$(date +%s%N).core.gz"
+    gzip -c $CORE_FILE >"$TEST_UNDECLARED_OUTPUTS_DIR/$CORE_FILENAME"
     echo "Writing coredump to $CORE_FILENAME..."
 fi
 
