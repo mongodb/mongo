1dfc94c10e98be2aea9e0a7c75e9f9558298e0dd
mongodb-matthew-normyle <120420718+mongodb-matthew-normyle@users.noreply.github.com>
2026-01-27 11:09:09 -0600

    SERVER-117630: Move all streams-only deps to third_party/private (#46964)
    GitOrigin-RevId: d1cb12a32264c1865eb8ffa87493621128fb4a36

--- src/third_party/aws-sdk/dist/aws-sdk/src/aws-cpp-sdk-core/include/aws/core/platform/Platform.h
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
- */
-
-#include <aws/core/Core_EXPORTS.h>
-
-#include <aws/core/platform/Android.h>
-
