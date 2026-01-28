580f426372c299cb0105f10a2cfd9ec14077eb9c
Alexander Ignatyev <alexander.ignatyev@mongodb.com>
2026-01-28 17:01:30 +0000

    SERVER-110803 Take into account sort direction in group stage when performing distinct scan optimization (#43784)
    GitOrigin-RevId: 14b78e182a020a8d10767f1c1fc8103ad713921d

---
+++ jstests/aggregation/sort_group_to_distinct_scan.js
@@ -0,0 +1,451 @@
+/**
+ * Validates that optimizations from $sort+$group to distinct scan and index scan plans are
+ * performed when expected and return valid output.
+ *
+ * @tags: [
+ *   # The sharding and $facet passthrough suites modifiy aggregation pipelines in a way that
+ *   # prevents the DISTINCT_SCAN optimization from being applied, which breaks the test.
+ *   assumes_unsharded_collection,
+ *   do_not_wrap_aggregations_in_facets,
+ *   # Index filter commands do not support causal consistency.
+ *   does_not_support_causal_consistency,
+ *   requires_fcv_80,
+ *   not_allowed_with_signed_security_token,
+ * ]
+ */
+
+import {
+    assertPlanUsesDistinctScan,
+    assertPlanUsesIndexScan
+} from "jstests/aggregation/libs/group_to_distinct_scan_utils.js";
+
+const docs = [
+    {
+        kind: "ice hockey",
+        season: "winter",
+        year: 1919,
+    },
+    {
+        kind: "olympic",
+        season: "winter",
+        year: 1920,
+    },
+    {
+        kind: "olympic",
+        season: "summer",
+        year: 1922,
+    },
+    {
+        kind: "olympic",
+        season: "winter",
+        year: 1924,
+    },
+    {
+        kind: "olympic",
+        season: "summer",
+        year: 1926,
+    },
+    {
+        kind: "olympic",
+        season: "winter",
+        year: 1928,
+    },
+    {
+        kind: "olympic",
+        season: "summer",
+        year: 1930,
+    },
+    {
+        kind: "ice hockey",
+        season: "winter",
+        year: 1920,
+    },
+    {
+        kind: "ice hockey",
+        season: "winter",
+        year: 1921,
+    },
+    {
+        kind: "ice hockey",
+        season: "winter",
+        year: 1922,
+    },
+    {
+        kind: "ice hockey",
+        season: "winter",
+        year: 1923,
+    },
+];
+
+const indexes = [
+    {kind: 1, season: 1, year: 1},
+    {season: 1, year: 1},
+];
+
+const distictScanTestCases = [
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$bottom: {output: "$year", sortBy: {season: 1, year: 1}}}
+                }
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group:
+                    {_id: "$season", year: {$top: {output: "$year", sortBy: {season: 1, year: 1}}}}
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {$group: {_id: "$season", year: {$bottom: {output: "$year", sortBy: {year: 1}}}}},
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {$group: {_id: "$season", year: {$bottom: {output: "$year", sortBy: {year: -1}}}}},
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {$group: {_id: "$season", year: {$top: {output: "$year", sortBy: {year: 1}}}}},
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {$group: {_id: "$season", year: {$top: {output: "$year", sortBy: {year: -1}}}}},
+        ],
+        index: {season: 1, year: 1},
+    },
+    // Multiple accumulators
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$bottom: {output: "$year", sortBy: {year: 1}}},
+                    kind: {$bottom: {output: "$kind", sortBy: {year: 1}}},
+                },
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$bottom: {output: "$year", sortBy: {year: -1}}},
+                    kind: {$bottom: {output: "$kind", sortBy: {year: -1}}},
+                },
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$top: {output: "$year", sortBy: {year: 1}}},
+                    kind: {$top: {output: "$kind", sortBy: {year: 1}}},
+                },
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$top: {output: "$year", sortBy: {year: -1}}},
+                    kind: {$top: {output: "$kind", sortBy: {year: -1}}},
+                },
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: 1, season: 1, year: 1}},
+            {
+                $group:
+                    {_id: "$kind", year: {$bottom: {output: "$year", sortBy: {season: 1, year: 1}}}}
+            },
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: 1, season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$kind",
+                    year: {$bottom: {output: "$year", sortBy: {season: -1, year: -1}}}
+                }
+            },
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: 1, season: 1, year: 1}},
+            {$group: {_id: "$kind", year: {$top: {output: "$year", sortBy: {season: 1, year: 1}}}}},
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: 1, season: 1, year: 1}},
+            {
+                $group:
+                    {_id: "$kind", year: {$top: {output: "$year", sortBy: {season: -1, year: -1}}}}
+            },
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: -1, season: -1, year: -1}},
+            {
+                $group:
+                    {_id: "$kind", year: {$bottom: {output: "$year", sortBy: {season: 1, year: 1}}}}
+            },
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: -1, season: -1, year: -1}},
+            {
+                $group: {
+                    _id: "$kind",
+                    year: {$bottom: {output: "$year", sortBy: {season: -1, year: -1}}}
+                }
+            },
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: -1, season: -1, year: -1}},
+            {$group: {_id: "$kind", year: {$top: {output: "$year", sortBy: {season: 1, year: 1}}}}},
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: -1, season: -1, year: -1}},
+            {
+                $group:
+                    {_id: "$kind", year: {$top: {output: "$year", sortBy: {season: -1, year: -1}}}}
+            },
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+];
+
+const indexScanTestCases = [
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$bottom: {output: "$year", sortBy: {season: 1, year: -1}}}
+                }
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group:
+                    {_id: "$season", year: {$top: {output: "$year", sortBy: {season: 1, year: -1}}}}
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: 1, season: 1, year: 1}},
+            {$group: {_id: "$kind", year: {$bottom: {output: "$year", sortBy: {year: 1}}}}},
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: 1, season: 1, year: 1}},
+            {$group: {_id: "$kind", year: {$bottom: {output: "$year", sortBy: {year: -1}}}}},
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: 1, season: 1, year: 1}},
+            {$group: {_id: "$kind", year: {$top: {output: "$year", sortBy: {year: 1}}}}},
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {kind: 1, season: 1, year: 1}},
+            {$group: {_id: "$kind", year: {$top: {output: "$year", sortBy: {year: -1}}}}},
+        ],
+        index: {kind: 1, season: 1, year: 1},
+    },
+    // Mixed accumulators
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$bottom: {output: "$year", sortBy: {year: 1}}},
+                    kind: {$top: {output: "$kind", sortBy: {year: 1}}},
+                },
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$bottom: {output: "$year", sortBy: {year: -1}}},
+                    kind: {$top: {output: "$kind", sortBy: {year: -1}}},
+                },
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    // Mixed sortings in accumulators
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$bottom: {output: "$year", sortBy: {year: -1}}},
+                    kind: {$bottom: {output: "$kind", sortBy: {year: 1}}},
+                },
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group: {
+                    _id: "$season",
+                    year: {$top: {output: "$year", sortBy: {year: -1}}},
+                    kind: {$top: {output: "$kind", sortBy: {year: 1}}},
+                },
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    // The change from SERVER-90017, available in v8.1+, treats $bottomN, $topN with N = 1 as
+    // $bottom and $top.
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group:
+                    {_id: "$season", year: {$bottomN: {n: 1, output: "$year", sortBy: {year: 1}}}}
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {
+                $group:
+                    {_id: "$season", year: {$bottomN: {n: 1, output: "$year", sortBy: {year: -1}}}}
+            },
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {$group: {_id: "$season", year: {$topN: {n: 1, output: "$year", sortBy: {year: 1}}}}},
+        ],
+        index: {season: 1, year: 1},
+    },
+    {
+        pipeline: [
+            {$sort: {season: 1, year: 1}},
+            {$group: {_id: "$season", year: {$topN: {n: 1, output: "$year", sortBy: {year: -1}}}}},
+        ],
+        index: {season: 1, year: 1},
+    },
+];
+
+function runTests() {
+    const coll = db[jsTest.name()];
+    coll.drop();
+    assert.commandWorked(coll.insertMany(docs));
+
+    function assertPipeline(pipeline, validateExplain) {
+        // Collect the expected output which is produced by the same query on the collection without
+        // indexes, which guarantees that the tested optimizations are not made.
+        assert.commandWorked(coll.dropIndexes());
+        const expectedOutput = coll.aggregate(pipeline).toArray();
+
+        assert.commandWorked(coll.createIndexes(indexes));
+
+        const actualOutput = coll.aggregate(pipeline).toArray();
+        const explain = coll.explain().aggregate(pipeline);
+
+        assert.sameMembers(expectedOutput, actualOutput, explain);
+        validateExplain(explain);
+    }
+
+    function assertDistinctScan(pipeline, expectedIndex) {
+        const validateExplain = (explain) => assertPlanUsesDistinctScan(explain, expectedIndex);
+        assertPipeline(pipeline, validateExplain);
+    }
+
+    function assertIndexScan(pipeline, expectedIndex) {
+        const validateExplain = (explain) => assertPlanUsesIndexScan(explain, expectedIndex);
+        assertPipeline(pipeline, validateExplain);
+    }
+
+    for (const testCase of distictScanTestCases) {
+        assertDistinctScan(testCase.pipeline, testCase.index);
+    }
+
+    for (const testCase of indexScanTestCases) {
+        assertIndexScan(testCase.pipeline, testCase.index);
+    }
+}
+
+runTests();
