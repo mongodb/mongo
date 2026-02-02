e636e8bb1e327d3a9612b07105b1c6a839722dde
Benjamin Pearce <benjamin.pearce@mongodb.com>
2026-02-02 15:28:09 -0500

    SERVER-118711 Revert SERVER-113888 SERVER-113889 (#47353)
    GitOrigin-RevId: a471a13094434666c48a1f75451f2efa49f8f5df

--- src/mongo/db/index_names.h
+++ src/mongo/db/index_names.h
@@ -86,11 +86,6 @@ public:
      * Convert an index name to an IndexType.
      */
     static IndexType nameToType(StringData accessMethod);
-
-    /**
-     * Index is not intended to be user facing.
-     */
-    static bool isInternalOnly(const std::string& name);
 };
 
 /**
