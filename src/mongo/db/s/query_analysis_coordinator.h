5c2a99152007f4e1eb5f43f86ddfc654402c792e
Brett Nawrocki <90278537+brettnawrocki@users.noreply.github.com>
2026-02-03 12:34:23 -0500

    SERVER-103404 Fix invariant in QueryAnalysisCoordinator (#34717) (#47253)
    GitOrigin-RevId: 8ea64f13e9bb6de5b50b407b7b03851298ab5930

--- src/mongo/db/s/query_analysis_coordinator.h
+++ src/mongo/db/s/query_analysis_coordinator.h
@@ -182,6 +182,7 @@ private:
      * Returns the minimum last ping time for a sampler to be considered as active.
      */
     Date_t _getMinLastPingTime();
+    Date_t _getMinLastPingTime(Date_t now);
 
     mutable Mutex _mutex = MONGO_MAKE_LATCH("QueryAnalysisCoordinator::_mutex");
 
