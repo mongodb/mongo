e03c939eb65bf0ea129a2342e25e50750c674c8d
Ivan Fefer <ivan.fefer@mongodb.com>
2026-02-11 15:53:25 +0100

    SERVER-118849 Fix hash lookup when duplicate key causes spilling (#47636)
    GitOrigin-RevId: 83751c5457fea4cdc4a4058bee4f3b8b08245b46

--- src/mongo/db/exec/sbe/stages/lookup_hash_table.cpp
+++ src/mongo/db/exec/sbe/stages/lookup_hash_table.cpp
@@ -239,13 +239,10 @@ void LookupHashTable::addHashTableEntry(value::SlotAccessor* keyAccessor, size_t
                 makeTemporaryRecordStore();
             }
 
-            value::MaterializedRow key{1};
-            key.reset(0, true, tagKeyView, valKeyView);
             _computedTotalMemUsage -= size_estimator::estimate(tagKeyView, valKeyView);
 
             // Evict the hash table value.
             _computedTotalMemUsage -= htIt->second.size() * sizeof(size_t);
-            htIt->second.push_back(valueIndex);
             spillIndicesToRecordStore(_recordStoreHt.get(), tagKeyView, valKeyView, htIt->second);
             _memoryHt->erase(htIt);
         }
