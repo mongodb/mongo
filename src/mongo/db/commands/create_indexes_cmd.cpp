87cf77251e82ffad018459f7e3e3ebddda671213
Benjamin Pearce <benjamin.pearce@mongodb.com>
2026-02-14 06:00:43 -0500

    SERVER-118713 Revert SERVER-113888 SERVER-113889 (#48091)
    GitOrigin-RevId: 0cc64265fbc1d6432f88e8c601fe64d865f49e4c

--- src/mongo/db/commands/create_indexes_cmd.cpp
+++ src/mongo/db/commands/create_indexes_cmd.cpp
@@ -179,26 +179,6 @@ void validateTTLOptions(OperationContext* opCtx,
     }
 }
 
-/**
- * Ensures that the user is authorized to create an index of a given type.
- */
-void validateIndexType(OperationContext* opCtx, const CreateIndexesCommand& cmd) {
-    const bool isAuthForInternal =
-        AuthorizationSession::get(opCtx->getClient())
-            ->isAuthorizedForActionsOnResource(ResourcePattern::forClusterResource(),
-                                               ActionType::internal);
-    for (const auto& elem : cmd.getIndexes()) {
-        for (const auto& key : elem.getField("key").Obj()) {
-            const auto type = key.str();  // will return "" for btree
-            if (IndexNames::isInternalOnly(type)) {
-                uassert(ErrorCodes::IndexOptionsConflict,
-                        fmt::format("Index Type {} is for internal use only", type),
-                        isAuthForInternal);
-            }
-        }
-    }
-}
-
 void checkEncryptedFieldIndexRestrictions(OperationContext* opCtx,
                                           const NamespaceString& ns,
                                           const CreateIndexesCommand& cmd) {
@@ -528,7 +508,6 @@ CreateIndexesReply runCreateIndexesWithCoordinator(OperationContext* opCtx,
             }
 
             validateTTLOptions(opCtx, collection.getCollection(), cmd);
-            validateIndexType(opCtx, cmd);
 
             if (collection && collection->isCapped() &&
                 MONGO_likely(!ignoreTTLIndexCappedCollectionCheck.shouldFail())) {
