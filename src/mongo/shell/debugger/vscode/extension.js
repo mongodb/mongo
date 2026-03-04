dc8614f6891dc182819141101f389f38ebdc589a
Steve McClure <steve.mcclure@mongodb.com>
2026-03-04 11:51:46 -0500

    SERVER-119412: Add update detection and support to VSCode JS debugger extension (#48992)
    GitOrigin-RevId: a0b7132ea8b6d72ddfef80a05b60196ed73f6715

--- src/mongo/shell/debugger/vscode/extension.js
+++ src/mongo/shell/debugger/vscode/extension.js
@@ -9,6 +9,8 @@ const path = require("path");
 
 // Extension entry point
 function activate(context) {
+    checkIfNewerVersionAvailable(context);
+
     // Register debug config provider
     context.subscriptions.push(
         vscode.debug.registerDebugConfigurationProvider("mongo-shell", {
@@ -40,6 +42,48 @@ function provideDebugConfigurations(_folder) {
 function createDebugAdapter(context, _session) {
     const adapterPath = path.join(context.extensionPath, "adapter.js");
     return new vscode.DebugAdapterExecutable("node", [adapterPath]);
+}
+
+function checkIfNewerVersionAvailable(context) {
+    const extensionSrcPath = "src/mongo/shell/debugger/vscode";
+
+    try {
+        const mongoRepo = process.env["MONGO_REPO"];
+        if (!mongoRepo) {
+            return;
+        }
+
+        const fs = require("fs");
+        const gitTreePackageJsonPath = path.join(mongoRepo, extensionSrcPath, "package.json");
+
+        if (!fs.existsSync(gitTreePackageJsonPath)) {
+            return;
+        }
+
+        const installedVersion = context.extension.packageJSON.version;
+        const gitTreePackageJson = JSON.parse(fs.readFileSync(gitTreePackageJsonPath, "utf8"));
+        const gitTreeVersion = gitTreePackageJson.version;
+
+        if (installedVersion !== gitTreeVersion) {
+            const installScriptPath = path.join(extensionSrcPath, "install.sh");
+
+            vscode.window
+                .showWarningMessage(
+                    `MongoDB Shell Debugger extension update available (installed: ${installedVersion}, available: ${gitTreeVersion}). Run the install.sh to update.`,
+                    "Copy Install Command",
+                )
+                .then((selection) => {
+                    if (selection === "Copy Install Command") {
+                        vscode.env.clipboard.writeText(installScriptPath).then(() => {
+                            vscode.window.showInformationMessage("Install command copied to clipboard!");
+                        });
+                    }
+                });
+        }
+    } catch (error) {
+        // Silently fail - don't disrupt extension activation
+        console.error("Version check failed:", error);
+    }
 }
 
 module.exports = {activate, deactivate};
