b597f2bbde93eb22b4af1152b5b682291a528241
Billy Donahue <BillyDonahue@users.noreply.github.com>
2026-03-14 20:49:04 -0400

    SERVER-120620 rm auto_clear_ptr.h (#49447)
    GitOrigin-RevId: e6fd34ae8df337d30f376277dcedaab74fed01cd

--- src/mongo/db/index/preallocated_container_pool.h
+++ src/mongo/db/index/preallocated_container_pool.h
@@ -32,10 +32,10 @@
 #include "mongo/db/index/multikey_paths.h"
 #include "mongo/db/operation_context.h"
 #include "mongo/db/storage/key_string/key_string.h"
-#include "mongo/util/auto_clear_ptr.h"
 #include "mongo/util/modules.h"
 
 MONGO_MOD_PUBLIC;
+
 namespace mongo {
 
 /**
@@ -43,23 +43,42 @@ namespace mongo {
  * Users of any members should never leave any state behind.
  */
 class PreallocatedContainerPool {
+private:
+    struct Clear {
+        void operator()(auto* c) const {
+            c->clear();
+        }
+    };
+
 public:
+    /** Non-owning smart pointer that calls clear() on destruction. */
+    template <typename C>
+    using ClearingPtr = std::unique_ptr<C, Clear>;
+
     static const OperationContext::Decoration<PreallocatedContainerPool> get;
 
-    AutoClearPtr<KeyStringSet> keys() {
-        return makeAutoClearPtr(&_keys);
+    ClearingPtr<KeyStringSet> keys() {
+        return _makeClearingPtr(&_keys);
     }
-    AutoClearPtr<KeyStringSet> multikeyMetadataKeys() {
-        return makeAutoClearPtr(&_multikeyMetadataKeys);
+    ClearingPtr<KeyStringSet> multikeyMetadataKeys() {
+        return _makeClearingPtr(&_multikeyMetadataKeys);
     }
-    AutoClearPtr<MultikeyPaths> multikeyPaths() {
-        return makeAutoClearPtr(&_multikeyPaths);
+    ClearingPtr<MultikeyPaths> multikeyPaths() {
+        return _makeClearingPtr(&_multikeyPaths);
     }
 
 private:
+    template <typename C>
+    static ClearingPtr<C> _makeClearingPtr(C* c) {
+        // It should always be cleared when it is accessed
+        dassert(c);
+        dassert(c->empty());
+        return ClearingPtr<C>{c};
+    }
+
     KeyStringSet _keys;
     KeyStringSet _multikeyMetadataKeys;
     MultikeyPaths _multikeyPaths;
 };
 
-};  // namespace mongo
+}  // namespace mongo
