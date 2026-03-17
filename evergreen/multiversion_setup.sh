005df1b001835cb0558b0f330563f727be5ec703
Sean Lyons <sean.lyons@mongodb.com>
2026-03-17 13:02:53 -0400

    SERVER-114974 SERVER-112491 Avoid conflicts with pipx-installed versions of db-contrib-tool (#45796)
    GitOrigin-RevId: f31525efed4c27c9acaa9e98c197cc4c1490d9c1

--- evergreen/multiversion_setup.sh
+++ evergreen/multiversion_setup.sh
@@ -9,10 +9,6 @@ set -o verbose
 activate_venv
 setup_db_contrib_tool
 
-export PIPX_HOME="${workdir}/pipx"
-export PIPX_BIN_DIR="${workdir}/pipx/bin"
-export PATH="$PATH:$PIPX_BIN_DIR"
-
 rm -rf /data/install /data/multiversion
 
 command="db-contrib-tool setup-repro-env multiversion-downloads.json --installDir /data/install --linkDir /data/multiversion --debug"
