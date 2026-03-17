fd9d782f241356790f153e85bd7a3c5b5efacbea
Joan Bruguera Micó (at MongoDB) <joan.bruguera-mico@mongodb.com>
2026-03-17 17:04:29 +0000

    SERVER-107819 Concurrent createIndex+collMod can leave behind inconsistent collection options (#43880)
    GitOrigin-RevId: 84cd4eb9653dd65353b621021bfd0034ebb34f7a

--- src/mongo/db/s/shardsvr_collmod_participant_command.cpp
+++ src/mongo/db/s/shardsvr_collmod_participant_command.cpp
@@ -30,6 +30,7 @@
 
 #include <memory>
 #include <string>
+#include <tuple>
 
 #include "mongo/base/error_codes.h"
 #include "mongo/base/string_data.h"
@@ -48,6 +49,7 @@
 #include "mongo/db/concurrency/lock_manager_defs.h"
 #include "mongo/db/database_name.h"
 #include "mongo/db/dbdirectclient.h"
+#include "mongo/db/index_builds_coordinator.h"
 #include "mongo/db/namespace_string.h"
 #include "mongo/db/operation_context.h"
 #include "mongo/db/s/collection_sharding_runtime.h"
@@ -55,6 +57,7 @@
 #include "mongo/db/s/sharded_collmod_gen.h"
 #include "mongo/db/s/sharding_recovery_service.h"
 #include "mongo/db/service_context.h"
+#include "mongo/db/shard_role.h"
 #include "mongo/db/timeseries/catalog_helper.h"
 #include "mongo/db/timeseries/timeseries_collmod.h"
 #include "mongo/db/transaction/transaction_participant.h"
@@ -102,6 +105,64 @@ void releaseCriticalSectionInEmptySession(OperationContext* opCtx,
             reason,
             ShardingCatalogClient::kLocalWriteConcern,
             ShardingRecoveryService::FilteringMetadataClearer());
+    }
+}
+
+// If collMod command fails with BackgroundOperationInProgressForNamespace (index build in progress,
+// non retriable error), transforms it to an error that the collMod coordinator will retry.
+// This prevents it from failing in a midway state (SERVER-107819).
+// TODO SERVER-75675: Remove this once collMod serializes with index builds.
+template <typename T>
+auto retryOnBackgroundOperationInProgressForNamespace(OperationContext* opCtx,
+                                                      const NamespaceString& ns,
+                                                      bool abortIndexBuilds,
+                                                      T&& func) {
+    if (abortIndexBuilds) {
+        try {
+            return func();
+        } catch (const ExceptionFor<ErrorCodes::BackgroundOperationInProgressForNamespace>& ex) {
+            LOGV2(10781900,
+                  "collMod DDL participant failed due to a background operation. "
+                  "Aborting in-progress index builds.",
+                  "ex"_attr = redact(ex));
+
+            // TODO SERVER-105548 switch back to acquireCollection once 9.0 becomes last LTS
+            auto [translatedNs, uuid] = [&]() {
+                // Hold reference to the catalog for collection lookup without locks to be safe.
+                auto catalog = CollectionCatalog::get(opCtx);
+
+                if (!ns.isTimeseriesBucketsCollection()) {
+                    auto bucketsNs = ns.makeTimeseriesBucketsNamespace();
+                    auto bucketsColl = catalog->lookupCollectionByNamespace(opCtx, bucketsNs);
+                    if (bucketsColl) {
+                        return std::make_tuple(bucketsColl->ns(), bucketsColl->uuid());
+                    }
+                }
+
+                auto coll = catalog->lookupCollectionByNamespace(opCtx, ns);
+                tassert(10768102, "Collection not found in collMod after index build error", coll);
+
+                return std::make_tuple(coll->ns(), coll->uuid());
+            }();
+
+            IndexBuildsCoordinator::get(opCtx)->abortCollectionIndexBuilds(
+                opCtx, translatedNs, uuid, "ShardSvrCollModParticipantCommand");
+        }
+
+        // Fall-through and immediately retry. If it fails again due to a newly started index build,
+        // we will still bubble the error up to the coordinator, which will retry after a backoff.
+    }
+
+    try {
+        return func();
+    } catch (const ExceptionFor<ErrorCodes::BackgroundOperationInProgressForNamespace>& ex) {
+        // We can not wait for index builds here, since we have a session checked out.
+        // Bubble the error up to the DDL coordinator so it retries later, with a backoff.
+        LOGV2(10781901,
+              "collMod DDL participant failed due to a background operation. "
+              "Re-throwing as a retriable error for the DDL coordinator to retry.",
+              "ex"_attr = redact(ex));
+        uasserted(ErrorCodes::LockBusy, "collMod failed due to a background operation");
     }
 }
 
@@ -172,10 +233,16 @@ public:
             // This flag is set from the collMod coordinator. We do not allow view definition change
             // on non-primary shards since it's not in the view catalog.
             auto performViewChange = request().getPerformViewChange();
-            uassertStatusOK(timeseries::processCollModCommandWithTimeSeriesTranslation(
-                opCtx, ns(), cmd, performViewChange, &builder));
-            auto collmodReply =
-                CollModReply::parse(IDLParserContext("CollModReply"), builder.obj());
+            // If this collMod required blocking CRUD operations, prefer aborting the index builds
+            // in progress in order to not delay unblocking of CRUD operations in other shards.
+            // Otherwise, prefer waiting for the index builds to complete.
+            auto abortIndexBuilds = request().getNeedsUnblock();
+            auto collmodReply = retryOnBackgroundOperationInProgressForNamespace(
+                opCtx, ns(), abortIndexBuilds, [&]() {
+                    uassertStatusOK(timeseries::processCollModCommandWithTimeSeriesTranslation(
+                        opCtx, ns(), cmd, performViewChange, &builder));
+                    return CollModReply::parse(IDLParserContext("CollModReply"), builder.obj());
+                });
 
             // Since no write that generated a retryable write oplog entry with this sessionId
             // and txnNumber happened, we need to make a dummy write so that the session gets
