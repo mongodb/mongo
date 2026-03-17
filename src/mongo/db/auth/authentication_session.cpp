cdb286a689a8fd2439363b2804dfb3d19b6508de
adriangzz <adriangonzalezmontemayor@gmail.com>
2026-03-17 11:04:51 -0600

    SERVER-108040 Update markFailed to check current step (#41221) (#43140)
    GitOrigin-RevId: d1e5771f37d4bc383b50675052c8f358eb01fad0

--- src/mongo/db/auth/authentication_session.cpp
+++ src/mongo/db/auth/authentication_session.cpp
@@ -370,13 +370,14 @@ void AuthenticationSession::markSuccessful() {
     }
 }
 
-void AuthenticationSession::markFailed(const Status& status) {
+void AuthenticationSession::markFailed(const Status& status,
+                                       boost::optional<StepType> currentStep) {
     _finish();
 
     // If we have made it to SaslContinue, that means that the attempt
     // is effectively no longer speculative. We should continue auditing
     // in this case.
-    if (!_isSpeculative || (_lastStep && _lastStep == StepType::kSaslContinue)) {
+    if (!_isSpeculative || (currentStep && currentStep == StepType::kSaslContinue)) {
         auto event = audit::AuthenticateEvent(_mechName,
                                               _userName.getDB(),
                                               _userName.getUser(),
