bd96d1f1f478d432cd05eded8df81c475706ba18
Calvin Nix <6693710+Calvinnix@users.noreply.github.com>
2026-03-19 09:52:22 -0400

    SERVER-120818: Build stream's binaries in streams_build_and_push (#49265)
    GitOrigin-RevId: 94b4f40e713be4899114ceab0fcb342e16431fc2

--- evergreen/streams_docker_manifest.sh
+++ evergreen/streams_docker_manifest.sh
@@ -4,7 +4,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 set -o errexit
 
 REGISTRY="664315256653.dkr.ecr.us-east-1.amazonaws.com"
-REPO="mongo/mongostream"
+REPO="${streams_ecr_repo:-mongo/mongostream-testing}"
 IMAGE="$REGISTRY/$REPO"
 GITSHA="$github_commit"
 DISTRO="$packager_distro"
