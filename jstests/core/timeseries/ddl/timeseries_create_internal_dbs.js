24198730b6e0053cc829db6b595a438e23af0722
Joan Bruguera Micó (at MongoDB) <joan.bruguera-mico@mongodb.com>
2026-03-20 21:11:28 +0000

    SERVER-122276 Disable creating timeseries collections on 'local' in viewful/viewless upgrade suites (#50089)
    GitOrigin-RevId: 20b41fc3e37a642657575b6a4d0b1b54084bcd04

--- jstests/core/timeseries/ddl/timeseries_create_internal_dbs.js
+++ jstests/core/timeseries/ddl/timeseries_create_internal_dbs.js
@@ -7,12 +7,22 @@
  */
 
 import {FixtureHelpers} from "jstests/libs/fixture_helpers.js";
+import {
+    isViewfulTimeseriesOnlySuite,
+    isViewlessTimeseriesOnlySuite,
+} from "jstests/core/timeseries/libs/viewless_timeseries_util.js";
 
 let internal_dbs = ["admin", "config"];
 if (!FixtureHelpers.isMongos(db)) {
     // Can't use local DB through mongos
     internal_dbs.push("local");
 }
+
+// TODO(SERVER-122270): Remove once _mdb_catalog and local.system.views are recovered consistently.
+if (!isViewfulTimeseriesOnlySuite(db) && !isViewlessTimeseriesOnlySuite(db)) {
+    internal_dbs = internal_dbs.filter((dbName) => dbName !== "local");
+}
+
 const collName = `coll_${UUID()}`;
 
 for (const dbName of internal_dbs) {
