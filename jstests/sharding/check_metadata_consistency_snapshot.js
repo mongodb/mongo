9e63eebeac0d9b66e20c671d7b2d78b900a5d503
Daniel Gómez Ferro <daniel.gomezferro@mongodb.com>
2026-03-23 10:26:33 -0400

    SERVER-122034 Account for stepdowns in check_metadata_consistency_snapshot.js (#50170)
    GitOrigin-RevId: 002dd0d177898f97682e8ec9223cc03b21519e66

--- jstests/sharding/check_metadata_consistency_snapshot.js
+++ jstests/sharding/check_metadata_consistency_snapshot.js
@@ -13,21 +13,27 @@ const coll = db[collName];
 
 assert.commandWorked(db.adminCommand({shardCollection: coll.getFullName(), key: {account: 1}}));
 
-jsTest.log("Log all queries");
-assert.commandWorked(st.configRS.getPrimary().getDB("config").setProfilingLevel(0, {slowms: 0}));
+jsTest.log.info("Log all queries");
+for (const node of st.configRS.nodes) {
+    assert.commandWorked(node.getDB("config").setProfilingLevel(0, {slowms: 0}));
+}
 
-jsTest.log("Run checkMetadataConsistency");
+jsTest.log.info("Run checkMetadataConsistency");
 assert.commandWorked(db.adminCommand({checkMetadataConsistency: 1}));
 
-assert.commandWorked(st.configRS.getPrimary().getDB("config").setProfilingLevel(0, {slowms: 100}));
+for (const node of st.configRS.nodes) {
+    assert.commandWorked(node.getDB("config").setProfilingLevel(0, {slowms: 0}));
+}
 
-jsTest.log("Checking logs for snapshot read concern");
+jsTest.log.info("Checking logs for snapshot read concern");
 
 // Make sure we've used snapshot read concern for the query on config.chunks
 // `"readConcern":{"level":"snapshot"` should appear twice, once from the passed in command and once
-// from the actual readConcern used.
+// from the actual readConcern used. Check all nodes to account for stepdowns.
 assert(
-    checkLog.checkContainsOnce(st.configRS.getPrimary(), /"config.chunks"(.*"readConcern":{"level":"snapshot"){2}/),
+    st.configRS.nodes.some((node) =>
+        checkLog.checkContainsOnce(node, /"config.chunks"(.*"readConcern":{"level":"snapshot"){2}/),
+    ),
     "The query on config.chunks should use snapshot read concern",
 );
 
