0cd35c0de05fa3fb4c423d8f324f6a6b03bc213e
Yujin Kang Park <36410031+ykangpark@users.noreply.github.com>
2026-03-24 11:26:02 +0100

    SERVER-118687 Fix check metadata consistency skip list bazel paths (#50203)
    GitOrigin-RevId: ae7788294e6299a9c76e6b73d86ab8ae134d3be3

--- buildscripts/resmokelib/testing/hooks/metadata_consistency.py
+++ buildscripts/resmokelib/testing/hooks/metadata_consistency.py
@@ -26,9 +26,9 @@ class CheckMetadataConsistencyInBackground(jsfile.PerClusterDataConsistencyHook)
     # The 'CheckMetadataConsistency' hook relies on the 'isMaster' command to asses if the fixture cluster is sharded.
     SKIP_TESTS = [
         # Skip tests that set a failPoint to make the 'isMaster' command unconditionally fail.
-        "bazel-bin/install/executor_integration_test",
-        "bazel-bin/install/rpc_integration_test",
-        "bazel-bin/install/transport_integration_test",
+        "bazel-bin/install/bin/executor_integration_test",
+        "bazel-bin/install/bin/rpc_integration_test",
+        "bazel-bin/install/bin/transport_integration_test",
         # Skip tests that update the internalDocumentSourceGroupMaxMemoryBytes parameter and make
         # checkMetadataConsistency fail with QueryExceededMemoryLimitNoDiskUseAllowed error.
         "jstests/aggregation/sources/unionWith/unionWith.js",
