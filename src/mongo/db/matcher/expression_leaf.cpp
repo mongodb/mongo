445db18dfb2b18bb3137cb4f25a9eabc77145d8a
Ian Boros <87138302+borosaurus@users.noreply.github.com>
2026-03-30 17:07:51 -0400

    SERVER-122449 Disable $bit* expressions in SBE and add knob to limit their memory use (#50788)
    GitOrigin-RevId: d5deca976b230d0726c7c9e26a3fe00378479697

--- src/mongo/db/matcher/expression_leaf.cpp
+++ src/mongo/db/matcher/expression_leaf.cpp
@@ -46,6 +46,7 @@
 #include "mongo/db/query/query_execution_knobs_gen.h"
 #include "mongo/db/query/query_integration_knobs_gen.h"
 #include "mongo/db/query/query_optimization_knobs_gen.h"
+#include "mongo/logv2/log.h"
 #include "mongo/util/errno_util.h"
 #include "mongo/util/pcre.h"
 #include "mongo/util/pcre_util.h"
@@ -54,6 +55,8 @@
 #include <algorithm>
 #include <cmath>
 #include <memory>
+
+#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kQuery
 
 namespace mongo {
 
@@ -475,6 +478,7 @@ BitTestMatchExpression::BitTestMatchExpression(MatchType type,
                                                uint32_t bitMaskLen,
                                                clonable_ptr<ErrorAnnotation> annotation)
     : LeafMatchExpression(type, path, std::move(annotation)) {
+    const auto maxPositions = internalQueryMaxBitTestIntermediatePositions.load();
     for (uint32_t byte = 0; byte < bitMaskLen; byte++) {
         char byteAt = bitMaskBinary[byte];
         if (!byteAt) {
@@ -493,7 +497,19 @@ BitTestMatchExpression::BitTestMatchExpression(MatchType type,
 
         for (int bit = 0; bit < 8; bit++) {
             if (byteAt & (1 << bit)) {
+                uassert(12244901,
+                        str::stream() << "BinData bitmask for " << name()
+                                      << " has too many set bits; maximum is " << maxPositions
+                                      << " (controlled by "
+                                         "internalQueryMaxBitTestIntermediatePositions)",
+                        _bitPositions.size() < static_cast<size_t>(maxPositions));
                 _bitPositions.push_back(8 * byte + bit);
+                if (_bitPositions.size() ==
+                    static_cast<size_t>(internalQueryBitTestPositionsLogThreshold.load())) {
+                    LOGV2(12244900,
+                          "Creating large bitPosition vector",
+                          "size"_attr = _bitPositions.size());
+                }
             }
         }
     }
