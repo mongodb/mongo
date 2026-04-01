7415e7d4aab3ec45bc0b3b836a620c9609f2e4ec
Mike Nugent <mike.nugent@mongodb.com>
2026-04-01 11:39:38 -0400

    SERVER-123072: Add test utils for matching otel attribute values  (#50842)
    GitOrigin-RevId: 535df3b25a01e7f0460f17384ddda366e8db288e

---
+++ src/mongo/otel/metrics/metrics_attributes_test_utils.h
@@ -0,0 +1,50 @@
+/**
+ *    Copyright (C) 2026-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+/**
+ * Functions and matchers for helping test attribute implementations.
+ */
+#pragma once
+
+#include "mongo/otel/metrics/metrics_attributes.h"
+#include "mongo/unittest/unittest.h"
+
+namespace mongo::otel::metrics {
+
+/**
+ * Returns whether the two AttributeNameAndValue are equal. This is non-trivial due to the fact that
+ * the values can be std::spans of different types, which don't support == by default.
+ */
+bool operator==(const AttributeNameAndValue& lhs, const AttributeNameAndValue& rhs);
+
+/** Allows using matchers on the values and attributes of an AttributesAndValue. */
+MATCHER_P2(IsAttributesAndValue, attrsMatcher, valueMatcher, "") {
+    return testing::ExplainMatchResult(attrsMatcher, arg.attributes, result_listener) &&
+        testing::ExplainMatchResult(valueMatcher, arg.value, result_listener);
+}
+}  // namespace mongo::otel::metrics
---
+++ src/mongo/otel/metrics/metrics_attributes_test_utils_test.cpp
@@ -0,0 +1,115 @@
+/**
+ *    Copyright (C) 2026-present MongoDB, Inc.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the Server Side Public License, version 1,
+ *    as published by MongoDB, Inc.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    Server Side Public License for more details.
+ *
+ *    You should have received a copy of the Server Side Public License
+ *    along with this program. If not, see
+ *    <http://www.mongodb.com/licensing/server-side-public-license>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the Server Side Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+#include "mongo/otel/metrics/metrics_attributes_test_utils.h"
+
+#include "mongo/unittest/unittest.h"
+
+#include <string>
+
+namespace mongo::otel::metrics {
+namespace {
+
+using testing::_;
+using testing::AllOf;
+using testing::ElementsAre;
+using testing::Gt;
+using testing::Not;
+
+// Googletest uses operator== to check for equality
+TEST(AttributeNameAndValueEqualsTest, Equals) {
+    EXPECT_EQ((AttributeNameAndValue{.name = "foo", .value = 3}),
+              (AttributeNameAndValue{.name = "foo", .value = 3}));
+
+    EXPECT_EQ((AttributeNameAndValue{.name = "foo", .value = "bar"}),
+              (AttributeNameAndValue{.name = "foo", .value = "bar"}));
+
+    std::vector<int32_t> intVec1{1, 2, 3};
+    std::vector<int32_t> intVec2 = intVec1;
+    EXPECT_EQ((AttributeNameAndValue{.name = "foo", .value = intVec1}),
+              (AttributeNameAndValue{.name = "foo", .value = intVec2}));
+
+    std::vector<StringData> stringDataVec1{"foo", "bar", "baz"};
+    // Make these explicit strings to help guarantee that they don't reside in the same spot in
+    // memory.
+    std::vector<std::string> stringVec{"foo", "bar", "baz"};
+    std::vector<StringData> stringDataVec2{stringVec[0], stringVec[1], stringVec[2]};
+    EXPECT_EQ((AttributeNameAndValue{.name = "foo", .value = stringDataVec1}),
+              (AttributeNameAndValue{.name = "foo", .value = stringDataVec2}));
+}
+
+TEST(AttributeNameAndValueEqualsTest, NotEquals) {
+    EXPECT_NE((AttributeNameAndValue{.name = "foo", .value = 3}),
+              (AttributeNameAndValue{.name = "foo", .value = 4}));
+
+    EXPECT_NE((AttributeNameAndValue{.name = "foo", .value = 3}),
+              (AttributeNameAndValue{.name = "bar", .value = 3}));
+
+    EXPECT_NE((AttributeNameAndValue{.name = "foo", .value = "bar"}),
+              (AttributeNameAndValue{.name = "foo", .value = "baz"}));
+
+    std::vector<int32_t> intVec1{1, 2, 3};
+    std::vector<int32_t> intVec2{1, 3, 2};
+    EXPECT_NE((AttributeNameAndValue{.name = "foo", .value = intVec1}),
+              (AttributeNameAndValue{.name = "foo", .value = intVec2}));
+
+    intVec2 = std::vector{1, 2};
+    EXPECT_NE((AttributeNameAndValue{.name = "foo", .value = intVec1}),
+              (AttributeNameAndValue{.name = "foo", .value = intVec2}));
+
+    std::vector<StringData> stringDataVec1{"foo", "bar", "baz"};
+    // Make these explicit strings to help guarantee that they don't reside in the same spot in
+    // memory.
+    std::vector<std::string> stringVec{"foo", "baz", "baz"};
+    std::vector<StringData> stringDataVec2{stringVec[0], stringVec[1], stringVec[2]};
+    EXPECT_NE((AttributeNameAndValue{.name = "foo", .value = stringDataVec1}),
+              (AttributeNameAndValue{.name = "foo", .value = stringDataVec2}));
+}
+
+TEST(IsAttributesAndValueTest, MatchesAttributes) {
+    EXPECT_THAT(
+        (AttributesAndValue{.attributes = {AttributeNameAndValue{.name = "foo", .value = 1}},
+                            .value = 5}),
+        AllOf(
+            IsAttributesAndValue(ElementsAre(AttributeNameAndValue{.name = "foo", .value = 1}), _),
+            Not(IsAttributesAndValue(ElementsAre(AttributeNameAndValue{.name = "bar", .value = 1}),
+                                     _)),
+            Not(IsAttributesAndValue(ElementsAre(AttributeNameAndValue{.name = "foo", .value = 2}),
+                                     _))));
+}
+
+TEST(IsAttributesAndValueTest, MatchesValue) {
+    EXPECT_THAT((AttributesAndValue{
+                    .attributes = {AttributeNameAndValue{.name = "foo", .value = 1}}, .value = 5}),
+                AllOf(IsAttributesAndValue(_, 5),
+                      IsAttributesAndValue(_, Gt(4)),
+                      Not(IsAttributesAndValue(_, 4))));
+}
+
+}  // namespace
+}  // namespace mongo::otel::metrics
