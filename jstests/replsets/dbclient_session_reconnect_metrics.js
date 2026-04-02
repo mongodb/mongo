bace90bea77a6d78ab4b1cd8f9ec3450ea159d3f
Cole Harbeck <cole.harbeck@mongodb.com>
2026-04-02 13:04:08 -0400

    SERVER-103650: Add FTDC metrics for reconnect attempts in DBClientSession (#51054)
    GitOrigin-RevId: 8fec0f1ac9d74c783c0645a0254f37607cf9ac09

---
+++ jstests/replsets/dbclient_session_reconnect_metrics.js
@@ -0,0 +1,38 @@
+/**
+ * Test that reconnect metrics for DBClientSession exist and are appropriately incremented.
+ *
+ * This test cannot be run on older binaries because the metrics do not exist yet.
+ * * @tags: [multiversion_incompatible]
+ */
+
+import {ReplSetTest} from "jstests/libs/replsettest.js";
+
+function getMetrics(conn) {
+    const status = assert.commandWorked(conn.adminCommand({serverStatus: 1}));
+    return Object.fromEntries([
+        'dbClientSessionReconnectAttempts',
+        'dbClientSessionWithoutAutoReconnectFailures',
+    ].map(name => [name, status.metrics.network[name]]));
+}
+
+const rst = new ReplSetTest(
+    {name: 'testSet', useBridge: true, nodes: 3, settings: {chainingAllowed: false}});
+const nodes = rst.startSet();
+rst.initiate();
+const primary = rst.getPrimary();
+rst.awaitSecondaryNodes();
+const secondary = rst.getSecondaries()[0];
+
+const before_disconnect = getMetrics(secondary);
+// dbClientSessionWithoutAutoReconnectFailures should never be incremented because the connection
+// created by the oplog fetcher is always created with autoReconnect = true.
+assert.eq(before_disconnect.dbClientSessionWithoutAutoReconnectFailures, 0);
+
+secondary.disconnect(primary);
+assert.soon(() => {
+    const after_disconnect = getMetrics(secondary);
+    return after_disconnect.dbClientSessionReconnectAttempts >
+        before_disconnect.dbClientSessionReconnectAttempts;
+});
+
+rst.stopSet();
