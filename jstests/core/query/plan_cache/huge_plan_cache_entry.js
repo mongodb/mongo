fcecda1336691d4d30811d1774034ca894df7561
natalie-hill <natalie.hill@mongodb.com>
2026-04-09 14:19:10 -0400

    SERVER-123500 Move huge_plan_cache_entry.js to passthrough and add tags to restrict it from incompatible suites (#51519)
    GitOrigin-RevId: d311d170d2650d4e242806df83384c8cc03a2b6f

--- jstests/core/query/plan_cache/huge_plan_cache_entry.js
+++ jstests/core/query/plan_cache/huge_plan_cache_entry.js
@@ -7,6 +7,21 @@
  *   query_intensive_pbt,
  *   do_not_wrap_aggregations_in_facets,
  *   assumes_against_mongod_not_mongos,
+ *   # This test attempts to perform queries and introspect the server's plan cache entries. The
+ *   # former operation may be routed to a secondary in the replica set, whereas the latter must be
+ *   # routed to the primary.
+ *   assumes_read_preference_unchanged,
+ *   # $planCacheStats cannot run within a multi-document transaction.
+ *   does_not_support_transactions,
+ *   # $planCacheStats requires readConcern local and reading from the same node.
+ *   assumes_read_concern_local,
+ *   assumes_read_concern_unchanged,
+ *   # TODO(SERVER-75416) This test should work with timeseries.
+ *   exclude_from_timeseries_crud_passthrough,
+ *   # Wildcard index creation conflicts with this test.
+ *   assumes_no_implicit_index_creation,
+ *   # A fix was added in this version to prevent $planCacheStats output from exceeding the BSON size limit for large entries.
+ *   requires_fcv_90,
  * ]
  */
 
