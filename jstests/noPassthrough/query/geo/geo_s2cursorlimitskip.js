594432dff54ba45e3d2b331ea701a1b3e7f13cb8
Finley Lau <finley.lau@mongodb.com>
2026-04-14 16:24:28 -0400

    SERVER-122774 [BACKPORT-27323] [v8.3] Make geo_s2cursorlimitskip.js a no passthrough test (#51737)
    GitOrigin-RevId: 03350b70242297dd8ba498b3a401c1fa6e583173

--- jstests/noPassthrough/query/geo/geo_s2cursorlimitskip.js
+++ jstests/noPassthrough/query/geo/geo_s2cursorlimitskip.js
@@ -1,54 +1,42 @@
-// Test various cursor behaviors
-//
-// @tags: [
-//   # The test runs commands that are not allowed with security token: profile, setProfilingLevel.
-//   not_allowed_with_signed_security_token,
-//   # This test attempts to enable profiling on a server and then get profiling data by reading
-//   # nodes the "system.profile" collection. The former operation must be routed to the primary in
-//   # a replica set, whereas the latter may be routed to a secondary.
-//   assumes_read_preference_unchanged,
-//   does_not_support_stepdowns,
-//   requires_capped,
-//   requires_getmore,
-//   requires_profiling,
-//   # The test does not expect concurrent reads against its test collections (e.g. the checks
-//   # aren't expecting concurrent reads but initial sync will be reading those collections).
-//   does_not_support_concurrent_reads
-// ]
+/**
+ * Test various cursor behaviors with 2dsphere index: getmore, limit, and skip.
+ *
+ * We insert a bunch of points and get a cursor. We then fetch some of the points and verify that
+ * the number of query and getmore operations are correct via the profiler. Finally, we fetch the
+ * rest of the points and again verify that the number of query and getmore operations are correct.
+ *
+ * @tags: [requires_profiling]
+ */
 
 import {add2dsphereVersionIfNeeded} from "jstests/libs/query/geo_index_version_helpers.js";
 
-let testDB = db.getSiblingDB("geo_s2cursorlimitskip");
-let t = testDB.geo_s2getmmm;
+const conn = MongoRunner.runMongod();
+assert.neq(null, conn, "mongod was unable to start up");
+
+const testDB = conn.getDB("geo_s2cursorlimitskip");
+const t = testDB.geo_s2getmmm;
 t.drop();
 t.createIndex({geo: "2dsphere"}, add2dsphereVersionIfNeeded());
 
 Random.setRandomSeed();
-let random = Random.rand;
+const random = Random.rand;
 
-/*
- * To test that getmore is working within 2dsphere index.
- * We insert a bunch of points and get a cursor. We then
- * fetch some of the points and verify that the number of
- * query and getmore operations are correct. Finally, we
- * fetch the rest of the points and again verify that the
- * number of query and getmore operations are correct.
- */
 function sign() {
     return random() > 0.5 ? 1 : -1;
 }
+
 function insertRandomPoints(num, minDist, maxDist) {
     for (let i = 0; i < num; i++) {
-        let lat = sign() * (minDist + random() * (maxDist - minDist));
-        let lng = sign() * (minDist + random() * (maxDist - minDist));
-        let point = {geo: {type: "Point", coordinates: [lng, lat]}};
+        const lat = sign() * (minDist + random() * (maxDist - minDist));
+        const lng = sign() * (minDist + random() * (maxDist - minDist));
+        const point = {geo: {type: "Point", coordinates: [lng, lat]}};
         assert.commandWorked(t.insert(point));
     }
 }
 
-let totalPointCount = 200;
-let initialAdvance = 10;
-let batchSize = 4;
+const totalPointCount = 200;
+const initialAdvance = 10;
+const batchSize = 4;
 
 // Insert points between 0.01 and 1.0 away.
 insertRandomPoints(totalPointCount, 0.01, 1.0);
@@ -61,18 +49,13 @@ let cursor = t
 assert.eq(cursor.count(), totalPointCount);
 
 // Disable profiling in order to drop the system.profile collection.
-// Then enable profiling for all operations. This is acceptable because
-// our test is denylisted from the parallel suite.
+// Then enable profiling for all operations.
 testDB.setProfilingLevel(0);
 testDB.system.profile.drop();
 // Create 4MB system.profile collection to prevent the 'getmore' operations from overwriting the
 // original query.
 assert.commandWorked(testDB.createCollection("system.profile", {capped: true, size: 4 * 1024 * 1024}));
-// Don't profile the setFCV command, which could be run during this test in the
-// fcv_upgrade_downgrade_replica_sets_jscore_passthrough suite.
-assert.commandWorked(
-    testDB.setProfilingLevel(1, {filter: {"command.setFeatureCompatibilityVersion": {"$exists": false}}}),
-);
+assert.commandWorked(testDB.setProfilingLevel(2));
 
 for (let j = 0; j < initialAdvance; j++) {
     assert(cursor.hasNext());
@@ -87,21 +70,21 @@ assert.eq(2, testDB.system.profile.count({op: "getmore", ns: t.getFullName()}));
 
 for (let k = initialAdvance; k < totalPointCount; k++) {
     assert(cursor.hasNext());
-    let tmpPoint = cursor.next();
+    cursor.next();
 }
 
 // Cursor was advanced 200 times, batchSize=4 => 1 query + 49 getmore.
 assert.eq(1, testDB.system.profile.count({op: "query", ns: t.getFullName()}));
 assert.eq(49, testDB.system.profile.count({op: "getmore", ns: t.getFullName()}));
 
-// Disable profiling again - no longer needed for remainder of test
+// Disable profiling again - no longer needed for remainder of test.
 testDB.setProfilingLevel(0);
 testDB.system.profile.drop();
 
 // Shouldn't be any more points to look at now.
 assert(!cursor.hasNext());
 
-let someLimit = 23;
+const someLimit = 23;
 // Make sure limit does something.
 cursor = t
     .find({
@@ -110,8 +93,9 @@ cursor = t
     .limit(someLimit);
 // Count doesn't work here -- ignores limit/skip, so we use itcount.
 assert.eq(cursor.itcount(), someLimit);
+
 // Make sure skip works by skipping some stuff ourselves.
-let someSkip = 3;
+const someSkip = 3;
 cursor = t
     .find({
         geo: {$geoNear: {$geometry: {type: "Point", coordinates: [0.0, 0.0]}}},
@@ -128,3 +112,5 @@ while (cursor.hasNext()) {
     assert(cursor2.hasNext());
     assert.eq(cursor.next(), cursor2.next());
 }
+
+MongoRunner.stopMongod(conn);
