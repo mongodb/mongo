3929cf5957e415a42b4b41768c2935566ee7c63c
Didier Nadeau <didier.nadeau@mongodb.com>
2026-04-28 07:01:06 -0400

    SERVER-120144 Update zlib to 1.3.2 (#52001)
    GitOrigin-RevId: 86c9803757de7b162e1ee00e62b6447051574856

--- src/third_party/scripts/zlib_get_sources.sh
+++ src/third_party/scripts/zlib_get_sources.sh
@@ -8,7 +8,7 @@ set -o errexit
 # This script is designed to run on most unix-like OSes
 #
 
-VERSION=1.3.1
+VERSION=1.3.2
 NAME=zlib
 TARBALL=${NAME}-${VERSION}.tar.gz
 TARBALL_DEST_DIR=${NAME}-${VERSION}
