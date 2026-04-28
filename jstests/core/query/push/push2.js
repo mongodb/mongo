cd2df71cef3e2616584d143b48ee51f9407817a6
Wenqin <wenqinYe@users.noreply.github.com>
2026-04-28 15:29:04 -0400

    SERVER-117723 push2.js fails in DSC transaction jscore passthrough suites due to cache pressure automatically killing transaction the test is running under (#49683)
    GitOrigin-RevId: e48be33533b0450508103a83c37670f79e2aa5f6

--- jstests/core/query/push/push2.js
+++ jstests/core/query/push/push2.js
@@ -3,11 +3,13 @@ t.drop();
 
 t.save({_id: 1, a: []});
 
-let s = new Array(700000).toString();
+// 3.5 MB chunk: 4 pushes succeed (~14 MB array), the 5th exceeds the 16 MB BSON
+// document limit and is rejected.
+let s = "x".repeat(3.5 * 1024 * 1024);
 
 let gotError = null;
 
-for (let x = 0; x < 100; x++) {
+for (let x = 0; x < 5; x++) {
     print(x + " pushes");
     let res = t.update({}, {$push: {a: s}});
     gotError = res.hasWriteError();
