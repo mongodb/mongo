a60606d23eef1bfbf0036844abc243810bc424be
Mihai Andrei <mihai.andrei@mongodb.com>
2026-05-05 09:55:33 -0400

    SERVER-125414 Fix express write recovery policy for secondaries (#53163)
    GitOrigin-RevId: dc10bc5d5bc465437634eaa2fa6db607ed653ef8

--- src/mongo/db/exec/express/plan_executor_express.cpp
+++ src/mongo/db/exec/express/plan_executor_express.cpp
@@ -160,7 +160,7 @@ public:
     }
 };
 
-static const RecoveryPolicyForPrimary recoveryPolicyForSecondary;
+static const RecoveryPolicyForSecondary recoveryPolicyForSecondary;
 
 template <class Plan>
 class PlanExecutorExpress final : public PlanExecutor {
