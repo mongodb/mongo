9e169f0803a85c462f569b7cf403d52247eadeb8
James H <james.harrison@mongodb.com>
2026-05-19 13:34:06 +0100

    SERVER-126803: Use internal client in exchange_keyrange_maxkey.js [8.3] (#53997)
    GitOrigin-RevId: 82a2c25b43a896dcf2d62ad525f5ae5ae9a005b8

--- jstests/noPassthroughWithMongod/query/exchange_keyrange_maxkey.js
+++ jstests/noPassthroughWithMongod/query/exchange_keyrange_maxkey.js
@@ -4,6 +4,19 @@
 
 TestData.disableImplicitSessions = true;
 
+// Exchange requires an internal client connection.
+const internalConn = (() => {
+    const conn = new Mongo(db.getMongo().host);
+    assert.commandWorked(
+        conn.getDB("admin").runCommand({
+            hello: 1,
+            internalClient: {minWireVersion: NumberInt(0), maxWireVersion: NumberInt(7)},
+        }),
+    );
+    return conn;
+})();
+const internalDB = internalConn.getDB(db.getName());
+
 const coll = db[jsTestName()];
 coll.drop();
 
@@ -11,7 +24,7 @@ assert.commandWorked(coll.insertMany([{x: 1}, {x: 2}, {x: "a"}, {x: null}]));
 
 // Run the aggregation with a pipeline that emits MaxKey as the 'x' value,
 // and an exchange that routes on 'x' with boundaries [MinKey, MaxKey].
-const res = db.runCommand({
+const res = internalDB.runCommand({
     aggregate: coll.getName(),
     pipeline: [{$project: {x: {$literal: MaxKey()}, _id: 0}}],
     cursor: {batchSize: 10},
@@ -22,6 +35,8 @@ const res = db.runCommand({
         boundaries: [{x: MinKey()}, {x: MaxKey()}],
         consumerIds: [NumberInt(0)],
     },
+    readConcern: {},
+    writeConcern: {},
 });
 
 // Validate that the command succeeds and that MaxKey documents are routed to the last
@@ -29,7 +44,7 @@ const res = db.runCommand({
 assert.commandWorked(res);
 
 // All four documents should appear (each projected to {x: MaxKey}).
-const cursor = new DBCommandCursor(db, res);
+const cursor = new DBCommandCursor(internalDB, res);
 const docs = cursor.toArray();
 assert.eq(4, docs.length, "Expected 4 documents, got: " + tojson(docs));
 for (const doc of docs) {
