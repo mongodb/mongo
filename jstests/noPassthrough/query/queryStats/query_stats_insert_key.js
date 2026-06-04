6520746d74968d6119909870f9ec7a732146019f
Jinfeng Ni <jinfeng.ni@mongodb.com>
2026-06-04 15:37:37 -0700

    Revert "SERVER-122076 Add tests coverage for collect query stats for insert in sharded cluster" (#55170)
    GitOrigin-RevId: 09d3b1eb548ec7315e8bbc69575fa4e161c91d63

--- jstests/noPassthrough/query/queryStats/query_stats_insert_key.js
+++ jstests/noPassthrough/query/queryStats/query_stats_insert_key.js
@@ -143,6 +143,9 @@ runInsertKeyTests(
     (fixture) => MongoRunner.stopMongod(fixture),
 );
 
+// TODO SERVER-122076: Enable the Sharded variant once query stats collection for inserts is wired
+// through mongos and the sharded write path. The current branch only supports standalone.
+/*
 runInsertKeyTests(
     "Sharded",
     () => {
@@ -158,3 +161,4 @@ runInsertKeyTests(
     },
     (st) => st.stop(),
 );
+*/
