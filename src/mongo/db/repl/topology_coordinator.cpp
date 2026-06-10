facaa8eb1adbb4422fcd9521ebe7ec84ffd0d06f
Ruchitha Rajaghatta <77162985+ruchitharajaghatta@users.noreply.github.com>
2026-06-10 13:01:36 -0400

    SERVER-124981: Do not log isEligibleSyncSource logs on arbiters (#53231)
    GitOrigin-RevId: 0a062c777dfae1a37477304a6bb0d167c1ee837a

--- src/mongo/db/repl/topology_coordinator.cpp
+++ src/mongo/db/repl/topology_coordinator.cpp
@@ -476,10 +476,15 @@ bool TopologyCoordinator::_isEligibleSyncSource(int candidateIndex,
     const auto syncSourceCandidate = memberConfig.getHostAndPort();
     const auto memberData = _memberData[candidateIndex];
 
-    // If limitLogFrequency is true, only log this message if it has not been logged in the last
-    // second, otherwise always log.
-    bool shouldLogIneligibleCandidate = (!limitLogFrequency) ||
-        ((_recentSyncSourceChanges.lastLoggedIneligibleSrc + Milliseconds(1000)) < now);
+    // Do not log ineligibility reason if node is an arbiter. If limitLogFrequency is true, only log
+    // this message if it has not been logged in the last second; otherwise, always log.
+    bool shouldLogIneligibleCandidate = true;
+    if (_selfConfig().isArbiter()) {
+        shouldLogIneligibleCandidate = false;
+    } else if (limitLogFrequency) {
+        shouldLogIneligibleCandidate =
+            (_recentSyncSourceChanges.lastLoggedIneligibleSrc + Milliseconds(1000)) < now;
+    }
 
 
     // Candidate must be up to be considered.
