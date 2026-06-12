50c6bf4d9f0363008634b20efbea1dfec9b00b9a
Mathias Stearn <mathias@mongodb.com>
2026-06-12 17:12:25 +0200

    SERVER-127419 [v7.0] Message Filter Plugin (#55661)
    Co-authored-by: David Goffredo <david.goffredo@mongodb.com>
    GitOrigin-RevId: 1c4a87717298814627256f6800871e5c08e75c55

--- src/mongo/db/mongod_main.cpp
+++ src/mongo/db/mongod_main.cpp
@@ -212,6 +212,7 @@
 #include "mongo/stdx/future.h"
 #include "mongo/stdx/thread.h"
 #include "mongo/transport/ingress_handshake_metrics.h"
+#include "mongo/transport/message_filter_hooks.h"
 #include "mongo/transport/transport_layer_manager.h"
 #include "mongo/util/assert_util.h"
 #include "mongo/util/background.h"
@@ -496,6 +497,8 @@ ExitCode _initAndListen(ServiceContext* serviceContext, int listenPort) {
     initializeCommandHooks(serviceContext);
 
     serviceContext->setServiceEntryPoint(std::make_unique<ServiceEntryPointMongod>(serviceContext));
+
+    transport::initMessageFilterPluginLoader("mongod");
 
     // Set up the periodic runner for background job execution. This is required to be running
     // before both the storage engine or the transport layer are initialized.
