b1505d8391c33807cfb06d670ed6f112243a350d
Max Verbinnen <64088654+Max-Verbinnen@users.noreply.github.com>
2026-06-18 11:14:08 +0100

    SERVER-128433 Ensure 'runtimeConstants.userRoles' cannot be set by an external client [v8.2] (#55718)
    GitOrigin-RevId: 5dfb2e3f6362a33e365de36052de0905364fd894

--- src/mongo/db/commands/query_cmd/find_cmd.cpp
+++ src/mongo/db/commands/query_cmd/find_cmd.cpp
@@ -1108,6 +1108,9 @@ private:
             CommandHelpers::ensureValidCollectionName(nss.nss());
         }
 
+        Variables::validateRuntimeConstantsArePermitted(opCtx,
+                                                        findCommand->getLegacyRuntimeConstants());
+
         // TODO: SERVER-73632 Remove Feature Flag for PM-635.
         // Forbid users from passing 'querySettings' explicitly.
         uassert(7746901,
