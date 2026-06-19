3f77bb2ecf77a3b2a0bf84ecfd79040d1fc831d8
Silvia Surroca <silvia.surroca@mongodb.com>
2026-06-19 13:00:45 +0200

    SERVER-129484 Fix move_primary_remove_destination_shard.js (#56417)
    GitOrigin-RevId: 21221ebe65bda4f76f9d56fa88039f61e9a82d1d

--- jstests/sharding/move_primary_remove_destination_shard.js
+++ jstests/sharding/move_primary_remove_destination_shard.js
@@ -4,13 +4,12 @@
  *
  * The test pauses movePrimary just before it commits the database-primary metadata to the config
  * server, then marks the destination shard as draining via startShardDraining. When movePrimary
- * resumes, ConfigsvrCommitMovePrimary validates that the destination shard is not draining and
+ * resumes, ConfigsvrCommitMovePrimary validates that the destination shard is in draining mode and
  * throws ShardNotFound.
  *
  * @tags: [
  *   does_not_support_stepdowns,
  *   featureFlagAuthoritativeShardsDDL,
- *   featureFlagAuthoritativeShardsCRUD,
  * ]
  */
 
@@ -21,8 +20,12 @@ import {ShardingTest} from "jstests/libs/shardingtest.js";
 
 describe("movePrimary with destination shard remove", function () {
     let st;
+    let primaryShard;
+    let drainingShard;
     const dbName = "testDb";
-    const shardedCollNs = `${dbName}.shardedData`;
+    const shardedCollNs = `${dbName}.shardedColl`;
+    const untrackedCollNs = `${dbName}.untrackedColl`;
+    const expectedDocs = [{x: 1}, {x: 2}, {x: 3}, {x: 4}, {x: 5}];
 
     before(function () {
         st = new ShardingTest({
@@ -30,18 +33,19 @@ describe("movePrimary with destination shard remove", function () {
             rs: {nodes: 3},
         });
 
+        primaryShard = st.shard0;
+        drainingShard = st.shard1;
+
         assert.commandWorked(
-            st.s.adminCommand({enableSharding: dbName, primaryShard: st.shard0.shardName}),
+            st.s.adminCommand({enableSharding: dbName, primaryShard: primaryShard.shardName}),
         );
 
         // Tracked collection on primary shard
         assert.commandWorked(st.s.adminCommand({shardCollection: shardedCollNs, key: {x: 1}}));
-        assert.commandWorked(
-            st.s.getDB(dbName).shardedData.insert([{x: 1}, {x: 2}, {x: 3}, {x: 4}, {x: 5}]),
-        );
+        assert.commandWorked(st.s.getCollection(shardedCollNs).insert(expectedDocs));
 
         // Untracked collection on the primary shard
-        assert.commandWorked(st.s.getDB(dbName).untrackedData.insert([{a: 1}, {a: 2}, {a: 3}]));
+        assert.commandWorked(st.s.getCollection(untrackedCollNs).insert(expectedDocs));
     });
 
     after(function () {
@@ -60,7 +64,7 @@ describe("movePrimary with destination shard remove", function () {
                     );
                 },
                 dbName,
-                st.shard1.shardName,
+                drainingShard.shardName,
             ),
             st.s.port,
         );
@@ -68,17 +72,18 @@ describe("movePrimary with destination shard remove", function () {
         jsTest.log("Waiting for movePrimary to hit the fail point");
         fp.wait();
 
-        // At this point movePrimary has cloned untracked collections and committed
-        // collection/chunk metadata to the recipient's local shard catalog, but has
-        // not yet committed to the config server.
+        // At this point movePrimary has cloned untracked collections and committed collection/chunk
+        // metadata to the recipient's local shard catalog, but has not yet committed to the config
+        // server.
         assert.soon(
-            () => st.rs1.getPrimary().getDB(dbName).untrackedData.find().itcount() === 3,
-            "untrackedData documents should be cloned to recipient shard",
+            () =>
+                drainingShard.getCollection(untrackedCollNs).find().itcount() ===
+                expectedDocs.length,
+            "untrackedColl documents should be cloned to recipient shard",
         );
         assert.soon(
             () =>
-                st.rs1
-                    .getPrimary()
+                drainingShard
                     .getDB("config")
                     .getCollection("shard.catalog.collections")
                     .find({_id: shardedCollNs})
@@ -87,45 +92,34 @@ describe("movePrimary with destination shard remove", function () {
         );
 
         jsTest.log("Marking destination shard as draining while movePrimary is paused");
-        assert.commandWorked(st.s.adminCommand({startShardDraining: st.shard1.shardName}));
+        assert.commandWorked(st.s.adminCommand({startShardDraining: drainingShard.shardName}));
 
         jsTest.log("Resuming movePrimary after destination shard started draining");
         fp.off();
 
-        // When movePrimary resumes it should detect that destination shard is missing
+        // When movePrimary resumes it should detect that destination shard is draining and abort
         awaitMovePrimary();
 
         // Primary didn't change
         const dbEntry = st.s.getDB("config").databases.findOne({_id: dbName});
         assert.eq(dbEntry.primary, st.shard0.shardName);
 
-        // Committed untracked collections should be reverted during abort
-        assert.eq(st.rs1.getPrimary().getDB(dbName).untrackedData.find().itcount(), 0);
-        assert.eq(st.rs0.getPrimary().getDB(dbName).untrackedData.find().itcount(), 3);
-
-        // Committed config.shard.catalog.collections entries should be reverted during abort
-        assert.soon(
-            () =>
-                st.rs1
-                    .getPrimary()
-                    .getDB("config")
-                    .getCollection("shard.catalog.collections")
-                    .find({_id: new RegExp(`^${dbName}\\.`)})
-                    .itcount() === 0,
-            "shard.catalog.collections entries on recipient should be cleaned up after abort",
-        );
-
-        // Committed config.shard.catalog.chunks entries should be reverted during abort
+        assert.eq(drainingShard.getCollection(untrackedCollNs).find().itcount(), 0);
         assert.eq(
-            st.rs1
-                .getPrimary()
-                .getDB("config")
-                .getCollection("shard.catalog.chunks")
-                .find()
-                .itcount(),
-            0,
+            primaryShard.getCollection(untrackedCollNs).find().itcount(),
+            expectedDocs.length,
         );
 
-        assert.commandWorked(st.s.adminCommand({stopShardDraining: st.shard1.shardName}));
+        // Verify that both collections return the expected docs
+        assert.sameMembers(
+            st.s.getCollection(untrackedCollNs).find({}, {_id: 0}).toArray(),
+            expectedDocs,
+        );
+        assert.sameMembers(
+            st.s.getCollection(shardedCollNs).find({}, {_id: 0}).toArray(),
+            expectedDocs,
+        );
+
+        assert.commandWorked(st.s.adminCommand({stopShardDraining: drainingShard.shardName}));
     });
 });
