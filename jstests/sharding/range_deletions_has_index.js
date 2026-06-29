233d9ae1a1a3d26589380e5d197f2f9a344e3924
Kruti Shah <70412403+krutishah139@users.noreply.github.com>
2026-06-29 18:05:18 -0400

    SERVER-130143 Use replSetTest stepup helper in range_deletions_has_index.js to avoid election race (#57106)
    GitOrigin-RevId: 2e4b6383827bcc46f73bb26688f2176d025fed9d

--- jstests/sharding/range_deletions_has_index.js
+++ jstests/sharding/range_deletions_has_index.js
@@ -12,9 +12,7 @@ const st = new ShardingTest({shards: {rs0: {nodes: 2}}});
 
 // Test index gets created with an empty range deletions collection
 let newPrimary = st.rs0.getSecondaries()[0];
-st.rs0.awaitReplication();
-assert.commandWorked(newPrimary.adminCommand({replSetStepUp: 1}));
-st.rs0.waitForPrimary();
+st.rs0.stepUp(newPrimary);
 const rangeDeletionColl = newPrimary.getDB("config").getCollection("rangeDeletions");
 let res = rangeDeletionColl.runCommand({listIndexes: rangeDeletionColl.getName()});
 assert.commandWorked(res);
