40e7205a2d06e19ee0350b6aee51659ce32773df
Rustam Gusein-zade <rustam.guseinzade@mongodb.com>
2026-07-07 09:50:36 +0100

    SERVER-129938 re-enable distinct scan tests (#57542)
    GitOrigin-RevId: 7bd528ada74c66405e3bccc587a87ee50d9708de

--- jstests/sharding/query/distinct_scan_read_concern.js
+++ jstests/sharding/query/distinct_scan_read_concern.js
@@ -9,7 +9,6 @@
  */
 import {getPlanStages, getWinningPlanFromExplain} from "jstests/libs/query/analyze_plan.js";
 import {setupShardedCollectionWithOrphans} from "jstests/libs/query/golden_sharding_utils.js";
-import {skipTestIfAuthoritativeShardsEnabled} from "jstests/sharding/libs/sharding_util.js";
 
 TestData.skipCheckOrphans = true; // Deliberately inserts orphans.
 
@@ -182,8 +181,6 @@ const readConcernAvailable = {
 };
 
 const {shardingTest, coll} = setupShardedCollectionWithOrphans();
-// TODO (SERVER-129938) Re-enable once this gets fixed.
-skipTestIfAuthoritativeShardsEnabled(shardingTest.s, () => shardingTest.stop());
 // The setup function already created indexes 'shardKey_1' and 'shardKey_1_notShardKey_1'.
 coll.createIndex({notShardKey: 1});
 
