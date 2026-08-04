ec4aaba0c9b5017f2222904a324a7e520338e3c1
Alexander Ignatyev <alexander.ignatyev@mongodb.com>
2026-08-04 17:41:21 +0100

    SERVER-128253 Cap explodeForSort scan count to avoid overflow bypassing the limit (#56181)
    GitOrigin-RevId: 0cb023e54a2ca65cce7a9cb1f011a03920d2cd5c

--- src/mongo/db/query/planner_analysis.cpp
+++ src/mongo/db/query/planner_analysis.cpp
@@ -40,6 +40,7 @@
 #include <s2cellid.h>
 // IWYU pragma: no_include "ext/alloc_traits.h"
 #include <algorithm>
+#include <limits>
 #include <ostream>
 #include <set>
 #include <type_traits>
@@ -1068,6 +1069,10 @@ BSONObj QueryPlannerAnalysis::getSortPattern(const BSONObj& indexKeyPattern) {
 // static
 bool QueryPlannerAnalysis::explodeForSort(const CanonicalQuery& query,
                                           std::unique_ptr<QuerySolutionNode>* solnRoot) {
+    // Upper bound the number of scan leaves is capped at, so it can't overflow and wrap past the
+    // cap.
+    static constexpr size_t kMaxValue = std::numeric_limits<size_t>::max();
+
     vector<QuerySolutionNode*> explodableNodes;
 
     std::unique_ptr<QuerySolutionNode>* toReplace = structureOKForExplode(solnRoot);
@@ -1125,7 +1130,16 @@ bool QueryPlannerAnalysis::explodeForSort(const CanonicalQuery& query,
             if (!isOilExplodable(oil, iet)) {
                 break;
             }
-            numScans *= oil.intervals.size();
+
+            // If multiplying would overflow size_t, use the max value so the cap below still
+            // catches it.
+            const size_t numIntervals = oil.intervals.size();
+            if (numIntervals != 0 && numScans > kMaxValue / numIntervals) {
+                numScans = kMaxValue;
+            } else {
+                numScans *= numIntervals;
+            }
+
             kpIt.next();
             ++boundsIdx;
         }
@@ -1184,8 +1198,13 @@ bool QueryPlannerAnalysis::explodeForSort(const CanonicalQuery& query,
             }
         }
 
-        // Do some bookkeeping to see how many ixscans we'll create total.
-        totalNumScans += numScans;
+        // Do some bookkeeping to see how many ixscans we'll create total. If addition would
+        // overflow size_t, use the max value so the cap below still catches it.
+        if (totalNumScans > kMaxValue - numScans) {
+            totalNumScans = kMaxValue;
+        } else {
+            totalNumScans += numScans;
+        }
 
         // And for this scan how many fields we expand.
         fieldsToExplode.push_back(boundsIdx);
