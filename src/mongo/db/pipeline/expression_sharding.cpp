16ca17728b5aa9de717e7b1cae92b8ebabc8f2d1
Matt Olma <67564577+mattsimply@users.noreply.github.com>
2026-08-06 20:31:12 +0300

    SERVER-130571: Limit use of $_internalIndexKey to internal clients only (#59295)
    GitOrigin-RevId: d69d016dec70e59841cde91077d83a816ab8b38d

--- src/mongo/db/pipeline/expression_sharding.cpp
+++ src/mongo/db/pipeline/expression_sharding.cpp
@@ -120,6 +120,12 @@ Value ExpressionInternalIndexKey::evaluate(const Document& root, Variables* vari
 }
 
 REGISTER_STABLE_EXPRESSION(_internalOwningShard, ExpressionInternalOwningShard::parse);
-REGISTER_STABLE_EXPRESSION(_internalIndexKey, ExpressionInternalIndexKey::parse);
+REGISTER_EXPRESSION_CONDITIONALLY(_internalIndexKey,
+                                  ExpressionInternalIndexKey::parse,
+                                  AllowedWithApiStrict::kInternal,
+                                  AllowedWithClientType::kInternal,
+                                  nullptr, /* featureFlag */
+                                  false,   /* shouldOmitDiagnosticInformation */
+                                  true);
 
 }  // namespace mongo
