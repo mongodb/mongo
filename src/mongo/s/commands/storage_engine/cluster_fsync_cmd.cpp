dc936dcbfed8863ba9c559cf1459ed9ec16b2562
Billy Donahue <BillyDonahue@users.noreply.github.com>
2026-08-17 18:18:57 +0000

    SERVER-133415 convert cluster fsync from ErrmsgCommandDeprecated to TypedCommand (#60971)
    GitOrigin-RevId: 0e06e9210b3f67fd54ad49e098dd34e79b04a6f8

--- src/mongo/s/commands/storage_engine/cluster_fsync_cmd.cpp
+++ src/mongo/s/commands/storage_engine/cluster_fsync_cmd.cpp
@@ -2,10 +2,6 @@
 // SPDX-License-Identifier: SSPL-1.0
 
 #include "mongo/base/error_codes.h"
-#include "mongo/base/status.h"
-#include "mongo/base/status_with.h"
-#include "mongo/bson/bsonelement.h"
-#include "mongo/bson/bsonmisc.h"
 #include "mongo/bson/bsonobj.h"
 #include "mongo/bson/bsonobjbuilder.h"
 #include "mongo/client/read_preference.h"
@@ -13,31 +9,35 @@
 #include "mongo/db/auth/authorization_session.h"
 #include "mongo/db/auth/resource_pattern.h"
 #include "mongo/db/commands.h"
-#include "mongo/db/database_name.h"
+#include "mongo/db/commands/fsync_gen.h"
+#include "mongo/db/namespace_string.h"
 #include "mongo/db/operation_context.h"
 #include "mongo/db/router_role/cluster_commands_helpers.h"
 #include "mongo/db/service_context.h"
 #include "mongo/db/sharding_environment/client/shard.h"
-#include "mongo/db/sharding_environment/grid.h"
-#include "mongo/db/sharding_environment/shard_id.h"
-#include "mongo/db/sharding_environment/sharding_feature_flags_gen.h"
-#include "mongo/db/topology/shard_registry.h"
-#include "mongo/logv2/log.h"
 #include "mongo/util/assert_util.h"
 
-#include <memory>
 #include <string>
 #include <utility>
-#include <vector>
-
-#define MONGO_LOGV2_DEFAULT_COMPONENT ::mongo::logv2::LogComponent::kCommand
 
 namespace mongo {
 namespace {
 
-class FsyncCommand : public ErrmsgCommandDeprecated {
+class FSyncCommand : public TypedCommand<FSyncCommand> {
 public:
-    FsyncCommand() : ErrmsgCommandDeprecated("fsync") {}
+    class Response {
+    public:
+        explicit Response(BSONObj obj) : _obj{std::move(obj)} {}
+
+        void serialize(BSONObjBuilder* builder) const {
+            builder->appendElements(_obj);
+        }
+
+    private:
+        BSONObj _obj;
+    };
+
+    using Request = FSyncRequest;
 
     std::string help() const override {
         return "invoke fsync on all shards belonging to the cluster";
@@ -51,66 +51,65 @@ public:
         return true;
     }
 
-    bool supportsWriteConcern(const BSONObj& cmd) const override {
-        return false;
-    }
+    class Invocation final : public InvocationBase {
+    public:
+        using InvocationBase::InvocationBase;
 
-    Status checkAuthForOperation(OperationContext* opCtx,
-                                 const DatabaseName& dbName,
-                                 const BSONObj&) const override {
-        auto* as = AuthorizationSession::get(opCtx->getClient());
-        if (!as->isAuthorizedForActionsOnResource(
-                ResourcePattern::forClusterResource(dbName.tenantId()), ActionType::fsync)) {
-            return {ErrorCodes::Unauthorized, "unauthorized"};
-        }
-
-        return Status::OK();
-    }
-
-    void unlockLockedShards(OperationContext* opCtx, const DatabaseName& dbname) {
-        auto request = OpMsgRequestBuilder::create(
-            auth::ValidatedTenancyScope::get(opCtx), dbname, BSON("fsyncUnlock" << 1));
-        auto response = CommandHelpers::runCommandDirectly(opCtx, request);
-    }
-
-    bool errmsgRun(OperationContext* opCtx,
-                   const DatabaseName& dbName,
-                   const BSONObj& cmdObj,
-                   std::string& errmsg,
-                   BSONObjBuilder& result) override {
-        BSONObj fsyncCmdObj = cmdObj;
-
-        if (cmdObj["lock"].trueValue()) {
-            auto forBackupField = BSON("forBackup" << true);
-            fsyncCmdObj = fsyncCmdObj.addFields(forBackupField);
-        }
-
-        auto shardResults = scatterGatherUnversionedTargetConfigServerAndShards(
-            opCtx,
-            dbName,
-            applyReadWriteConcern(
-                opCtx, this, CommandHelpers::filterCommandRequestForPassthrough(fsyncCmdObj)),
-            ReadPreferenceSetting(ReadPreference::PrimaryOnly),
-            Shard::RetryPolicy::kIdempotent);
-
-        BSONObjBuilder rawResult;
-        const auto response = appendRawResponses(opCtx, &errmsg, &rawResult, shardResults);
-
-        // This field has had dummy value since MMAP went away. It is undocumented.
-        // Maintaining it so as not to cause unnecessary user pain across upgrades.
-        result.append("numFiles", 1);
-        result.append("all", rawResult.obj());
-        if (!response.responseOK) {
-            if (cmdObj["lock"].trueValue()) {
-                unlockLockedShards(opCtx, dbName);
-            }
+        bool supportsWriteConcern() const override {
             return false;
         }
 
-        return true;
-    }
+        NamespaceString ns() const override {
+            return {};
+        }
+
+        void doCheckAuthorization(OperationContext* opCtx) const override {
+            auto* as = AuthorizationSession::get(opCtx->getClient());
+            if (!as->isAuthorizedForActionsOnResource(
+                    ResourcePattern::forClusterResource(request().getDbName().tenantId()),
+                    ActionType::fsync)) {
+                uasserted(ErrorCodes::Unauthorized, "unauthorized");
+            }
+        }
+
+        Response typedRun(OperationContext* opCtx) {
+            if (request().getLock()) {
+                request().setForBackup(true);
+            }
+
+            setReadWriteConcern(opCtx, request(), this);
+            auto shardResults = scatterGatherUnversionedTargetConfigServerAndShards(
+                opCtx,
+                request().getDbName(),
+                CommandHelpers::filterCommandRequestForPassthrough(request().toBSON()),
+                ReadPreferenceSetting(ReadPreference::PrimaryOnly),
+                Shard::RetryPolicy::kIdempotent);
+
+            std::string errmsg;
+            BSONObjBuilder rawResult;
+            const auto response = appendRawResponses(opCtx, &errmsg, &rawResult, shardResults);
+
+            BSONObjBuilder result;
+            // This field has had dummy value since MMAP went away. It is undocumented.
+            // Maintaining it so as not to cause unnecessary user pain across upgrades.
+            result.append("numFiles", 1);
+            result.append("all", rawResult.obj());
+            if (!response.responseOK) {
+                if (request().getLock()) {
+                    CommandHelpers::runCommandDirectly(
+                        opCtx,
+                        OpMsgRequestBuilder::create(auth::ValidatedTenancyScope::get(opCtx),
+                                                    request().getDbName(),
+                                                    BSON("fsyncUnlock" << 1)));
+                }
+                CommandHelpers::appendSimpleCommandStatus(result, false, errmsg);
+            }
+
+            return Response{result.obj()};
+        }
+    };
 };
-MONGO_REGISTER_COMMAND(FsyncCommand).forRouter();
+MONGO_REGISTER_COMMAND(FSyncCommand).forRouter();
 
 }  // namespace
 }  // namespace mongo
