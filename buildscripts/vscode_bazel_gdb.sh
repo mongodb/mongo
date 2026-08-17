fc06d647ba8c0a68d53875bd7aa1fb3c9aa5a491
Daniel Moody <dmoody256@gmail.com>
2026-08-17 23:26:32 +0000

    SERVER-133418 Use Bazel GDB in VSCode launcher (#61177)
    GitOrigin-RevId: 0a3411a26f6f29b8ba73eb45fc9c0d82f78f7de1

--- buildscripts/vscode_bazel_gdb.sh
+++ buildscripts/vscode_bazel_gdb.sh
@@ -5,10 +5,9 @@ set -euo pipefail
 cd "$(dirname "$0")"/..
 WORKSPACE="$(pwd)"
 
-# TODO(SERVER-127579): The current bazel gdb_v5 target produces a GDB with a bug
-# preventing breakpoints from being set for binaries that are built with split DWARF.
-# Revert back to bazel gdb_v5 when the target builds an upgraded GDB.
-exec /opt/mongodbtoolchain/v5/bin/gdb \
+# Use the hermetic Bazel GDB toolchain, which supports breakpoints in binaries
+# built with split DWARF.
+exec bazel run gdb -- \
     -iex "set auto-load safe-path $WORKSPACE/.gdbinit" \
     -iex "set substitute-path ./external $WORKSPACE/bazel-$(basename "$WORKSPACE")/external" \
     -iex "set substitute-path . $WORKSPACE" \
