ffd3caf461f19bcf97beff4d8ee0aa2fb936a10d
Evan Bergeron <evanbergeron@users.noreply.github.com>
2026-08-19 19:55:22 +0000

    SERVER-133696 Use $count instead of $out in agg_match.js (#61364)
    GitOrigin-RevId: cb0fdd695d53c3d087f80bebb4164086cd38b889

--- jstests/concurrency/fsm_workloads/query/agg/agg_match.js
+++ jstests/concurrency/fsm_workloads/query/agg/agg_match.js
@@ -15,28 +15,12 @@ import {extendWorkload} from "jstests/concurrency/fsm_libs/extend_workload.js";
 import {$config as $baseConfig} from "jstests/concurrency/fsm_workloads/query/agg/agg_base.js";
 
 export const $config = extendWorkload($baseConfig, function ($config, $super) {
-    $config.data.getOutCollName = function getOutCollName(collName) {
-        return collName + "_out_agg_match";
-    };
-
     $config.states.query = function query(db, collName) {
-        // note that all threads output to the same collection
-        let otherCollName = this.getOutCollName(collName);
-        let cursor = db[collName].aggregate([{$match: {flag: true}}, {$out: otherCollName}]);
-        assert.eq(0, cursor.itcount(), "cursor returned by $out should always be empty");
+        let res = db[collName].aggregate([{$match: {flag: true}}, {$count: "count"}]).toArray();
         // NOTE: This relies on the fast-path for .count().
         // NOTE: There's a bug, SERVER-33753, where "fast" .count() is wrong on sharded
         // collections, so we denylisted this test for sharded clusters.
-        assert.eq(db[collName].count() / 2, db[otherCollName].count());
-    };
-
-    $config.setup = function setup(db, collName, cluster) {
-        $super.setup.apply(this, arguments);
-
-        // Create the collection to avoid a race in the initial aggregations. If the collection
-        // doesn't exist, only one $out can create it, and the others will see their target has been
-        // changed, and throw an error.
-        assert.commandWorked(db.runCommand({create: this.getOutCollName(collName)}));
+        assert.eq(db[collName].count() / 2, res[0].count);
     };
 
     return $config;
