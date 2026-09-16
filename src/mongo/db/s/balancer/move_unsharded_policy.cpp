d63eaccd6af6aa4bb7a5588d1b7f97747e7c5f7d
Joan Bruguera Micó (at MongoDB) <joan.bruguera-mico@mongodb.com>
2026-09-16 07:30:35 +0000

    SERVER-134588 Remove misleading ignore for sharded collections in configDB in move unsharded balancer policy (#62355)
    GitOrigin-RevId: c98932a870c81e94fedcfccf6726db2f177bb656

--- src/mongo/db/s/balancer/move_unsharded_policy.cpp
+++ src/mongo/db/s/balancer/move_unsharded_policy.cpp
@@ -31,19 +31,13 @@ int64_t getRandomIndex(const std::vector<T>& items) {
 }
 
 /**
- * Returns whether or not the cluster contains any sharded collections that can be balanced. If we
- * are draining, this includes config collections, otherwise this excludes any config collections.
+ * Returns whether or not the cluster contains any sharded collections that can be balanced.
  */
-bool clusterHasShardedCollections(OperationContext* opCtx, bool draining) {
+bool clusterHasShardedCollections(OperationContext* opCtx) {
     auto client = ShardingCatalogManager::get(opCtx)->localCatalogClient();
 
     BSONObjBuilder matchBuilder;
     matchBuilder.append(CollectionType::kUnsplittableFieldName, BSON("$ne" << true));
-    // Skip config.system.sessions if we are not draining as it isn't balanced as part of the random
-    // migrations failpoint. If we are draining shards, though, we need to include this collection.
-    if (!draining) {
-        matchBuilder.append(CollectionType::kNssFieldName, BSON("$regex" << "^(?!config\\.).*"));
-    }
 
     std::vector<BSONObj> rawPipelineStages{
         BSON("$match" << matchBuilder.obj()),
@@ -448,7 +442,7 @@ MigrateInfoVector MoveUnshardedPolicy::selectCollectionsToMove(
 
         // Randomly skip moveCollections if there are sharded collections that could be balanced.
         if (opCtx->getClient()->getPrng().trueWithProbability(skipMoveCollectionThreshold) &&
-            clusterHasShardedCollections(opCtx, isDraining)) {
+            clusterHasShardedCollections(opCtx)) {
             return result;
         }
 
